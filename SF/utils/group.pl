bfd9c22eacea322856d46859467733bae7f1e7fb
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2001-09-18 08:40:16 +0000

    Initial revision introduced for private project handling in cvsweb.cgi

---
+++ SF/utils/group.pl
@@ -0,0 +1,42 @@
+#
+# CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
+# Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001. All Rights Reserved
+# http://codex.xerox.com
+#
+# $Id$
+#
+#  License:
+#    This file is subject to the terms and conditions of the GNU General Public
+#    license. See the file COPYING in the main directory of this archive for
+#    more details.
+#
+# Purpose:
+#    This Perl include file mimics some of the fucntion in www/include/Group.class
+#    to allow Perl scripts to handle group information
+
+my ($GROUP_INFO);
+
+sub set_group_info_from_name {
+
+  my ($gname) = @_;
+  my ($query, $c, $res);
+
+  $query = "SELECT * FROM groups WHERE unix_group_name='$gname'";
+  $c = $dbh->prepare($query);
+  $res = $c->execute();
+
+  if (!$res || ($c->rows < 1)) {
+    return 0;
+  } else {
+    $GROUP_INFO = $c->fetchrow_hashref;
+  }
+
+  return $$GROUP_INFO{'group_id'};
+    
+}
+
+sub isGroupPublic {
+  return $$GROUP_INFO{'is_public'};
+}
+
+1;
