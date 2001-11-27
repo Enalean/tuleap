16f0e7404c121e6f5229aa4e90149cffb84d6c25
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2001-11-27 09:16:24 +0000

    Updated CodeX staff page with John's departure and Pete's arrival.

--- SF/www/staff.php
+++ SF/www/staff.php
@@ -16,6 +16,20 @@ $HTML->header(array('title'=>'Codex Staff'));
 <table border=0 cellspacing=2 cellpadding=2 bgcolor="">
 
 <tr valign=top>
+<td><a href="/sendmessage.php?touser=390">Steve Alexander</a></td>
+<td>JS</td>
+<td>Steve joined the CodeX Team in February 2001. He is a Unix system administrator at the Wilson Research Center where the <a href="display_doc.php?docid=26&group_id=1">CodeX server</a> is physically located. He is responsible for the configuration, installation and backup of the CodeX site. Steve and Vicki Tuttle (see below) work in tandem.
+</td>
+</tr>
+
+<tr valign=top>
+<td><a href="/sendmessage.php?touser=390">Pete Earnshaw</a></td>
+<td>JS</td>
+<td>Pete joined the CodeX Team in November 2001. He works as a part time developer both on the CodeX and the AlphaAvenue Web sites which are based on the same <a href="/docman/display_doc.php?docid=27&group_id=1">software components</a>.
+</td>
+</tr>
+
+<tr valign=top>
 <td><a href="/sendmessage.php?touser=103">Laurent Julliard</a></td>
 <td>LJ</td>
 <td>
@@ -26,54 +40,39 @@ Laurent founded <A HREF="http://xww.linux.world.xerox.com/">Linux@Xerox</A> and 
 </tr>
 
 <tr valign=top>
-<td><a href="/sendmessage.php?touser=101">John Stidd</a></td>
-<td>JS</td>
-<td>Member of the Xerox Open Source team with Laurent Julliard (See above).  Hacked the SourceForge front-end and back end (especially the mailman stuff to build the Xerox CodeX.
-<P>
-John founded the <A HREF="http://bazaar.adoc.xerox.com/">Xerox Open Source</A> Web Site with Laurent.
-</td>
-</tr>
-
-<tr valign=top>
 <td><a href="/sendmessage.php?touser=102">Vicki Tuttle</a></td>
 <td>Vicki</td>
 <td>Vicki is a Unix system administrator at the Wilson Research Center where the <a href="display_doc.php?docid=26&group_id=1">CodeX hardware</a> is physically located. She is responsible for the configuration, installation and backup of the CodeX site.</td>
 </tr>
 
 
-<TR><TD COLSPAN="2"><B>Founders:</B></TD></TR>
+<TR><TD COLSPAN="3"><B>Founders:</B></TD></TR>
 
 <tr valign=top>
-<td><a href="/sendmessage.php?touser=103">Laurent Julliard</a></td>
-</tr>
-
-
-<tr valign=top>
-<td><a href="/sendmessage.php?touser=101">John Stidd</a></td>
-</tr>
-
-
-<tr valign=top>
-<td><a href="/sendmessage.php?touser=102">Vicki Tuttle</a></td>
+<td  COLSPAN="3"><a href="/sendmessage.php?touser=103">Laurent Julliard</a>,
+<a href="/sendmessage.php?touser=101">John Stidd</a>, 
+<a href="/sendmessage.php?touser=102">Vicki Tuttle.</a></td>
+<br><br>
 </TR>
 
 
 <tr valign=top>
-<td COLSPAN="4"><strong>And of course our CodeX site would not exist without the remarkable work of the world famous <a href="http://sourceforge.net/">SourceForge.net</a> Web site created by the <a href="http://sourceforge.net/staff.php">SourceForge staff and founders</a>. Many thanks for their work.</strong></td></TR>
+<td COLSPAN="4">And of course our CodeX site would not exist without the remarkable work of the world famous <a href="http://sourceforge.net/">SourceForge.net</a> Web site created by the <a href="http://sourceforge.net/staff.php">SourceForge staff and founders</a>. Many thanks for their work.</td></TR>
 
 
-<TR><TD COLSPAN="3">&nbsp;<P><B>Significant Outside Contributors:</B></TD></TR>
+<TR><TD COLSPAN="3">&nbsp;<P><B>CodeX Hall of Fame:</B></TD></TR>
 
 <tr valign=top>
 <td COLSPAN="4">The hall of fame is wide open ! We are impatiently waiting for your contributions. If you want to help please <a href="contact.php">contact us</a>
 </td>
-<!--
-</tr><tr valign=top>
-<td><a href="/sendmessage.php?toaddress=tbd">TBD</a></td>
-<td>(tbd)</td>
-<td>bla,bla,bla....
+
+<tr valign=top>
+<td><a href="JohnStidd@earthlink.net">John Stidd</a></td>
+<td>JS</td>
+<td>Founding Member of the Xerox Open Source team with Laurent Julliard (See above), John was a key player in the success of the CodeX Initiative.  He hacked the SourceForge front-end and back end (especially the mailman stuff to build the Xerox CodeX. He also spent a lot of time developing CodeX realtionship with the Business Groups. John retired from Xerox in November 2001. 
+<P>
+John also created the <A HREF="http://bazaar.adoc.xerox.com/">Xerox Open Source</A> Web Site with Laurent Julliard.
 </td>
--->
 </tr>
 
 
