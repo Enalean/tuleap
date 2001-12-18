c997bb5c2c4e93fe394f7591da1586052f03817d
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2001-12-18 14:25:31 +0000

    If http_domain specified, use it as the project web server name

--- SF/utils/underworld-dummy/apache_conf.pl
+++ SF/utils/underworld-dummy/apache_conf.pl
@@ -25,12 +25,31 @@ while(my ($http_domain,$unix_group_name,$group_name,$unix_box) = $c->fetchrow())
 	@blah = unpack('C4', $addrs[0]);
 	$ip = join(".", @blah);
 
-# LJ Custom log used to be on a per project basis but is now
-# in one single file
-#    "  CustomLog /home/groups/$unix_group_name/log/combined_log combined\n",
+
+	# LJ Custom log used to be on a per project basis but is now
+	# in one single file
+	#    "  CustomLog /home/groups/$unix_group_name/log/combined_log combined\n",
+	# if the HTTP domain given in CodeX is a customized one then use it 
+	# as the Server name and create an alias for projectname.codex.xerox.com
+	# Note: the DNS entry for the customized HTTP domain must exist somewhere
+	# in some DNS server in the Corp.
+	$http_domain =~ tr/A-Z/a-z/;
+	$codex_domain = "$unix_group_name.codex.xerox.com";
+	if ($http_domain ne $codex_domain)
+	  {
+	    $server_name = "  ServerName $http_domain\n";
+	    $server_alias = "  ServerAlias $codex_domain\n";
+	  }
+	else
+	  {
+	    $server_name = "  ServerName $codex_domain\n";
+	    $server_alias = "";
+	  }
+
 	push @apache_zone,
 	( "<VirtualHost $ip>\n",
-	  "  ServerName $unix_group_name.codex.xerox.com\n",
+	  "$server_name",
+	  "$server_alias",
           "  User dummy\n",
           "  Group $unix_group_name\n",
 	  "  DocumentRoot /home/groups/$unix_group_name/htdocs/\n",
