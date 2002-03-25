8755d035249a4420235fd867f16575b5e9265cad
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2002-03-25 15:08:49 +0000

    reformatting and up/down arrows are PNG format

--- SF/www/help/browse_bug_sort.php
+++ SF/www/help/browse_bug_sort.php
@@ -5,6 +5,9 @@
 // http://codex.xerox.com
 //
 // $Id$
+//
+//	Originally written by Laurent Julliard 2001, 2002, CodeX Team, Xerox
+//
 
 require "pre.php";
 
@@ -12,7 +15,8 @@ help_header("Bug Search -  Sorting results");
 
 ?>
 
-<p>The CodeX Bug Tracking System (BTS) offers a rich set of features to help you sort your bug selection in the most convenient way. 
+<p>The Bug Tracking System (BTS) offers a rich set of features to help
+you sort your bug selection in the most convenient way.
 
 <h3>Basic Sort</h3>
 
@@ -20,8 +24,8 @@ To sort your bug report according to a given criteria simply click on the
 corresponding column heading in the bug table. Clicking twice on the same
 column will change the sorting direction from ascending to descending and
 vice-versa. The sorting direction is shown by a small up 
-(<img src="/images/up_arrow.gif" border="0">) or 
-down (<img src="/images/dn_arrow.gif" border="0">) arrow 
+(<img src="/images/up_arrow.png" border="0">) or 
+down (<img src="/images/dn_arrow.png" border="0">) arrow 
  next to the sort criteria.
 
 <h3>Mutli-column Sort</h3>
@@ -31,10 +35,11 @@ For more sophisticated processing you can also activate the multi-column sort.
 So you can for instance click 'Severity' first and 'Assigned To' second to 
 see who in the team is assigned critical bugs and how many.
 
-<p>Also notice that At any point in the multi-column sort process, a click
-on one of the sort criteria displayed in the list (criteria 1 > criteria 2 > criteria 3...) 
-will bring you back in the sort criteria list. Using this feature you can
-easily test various sorting strategies. 
+<p>Also notice that At any point in the multi-column sort process, a
+click on one of the sort criteria displayed in the list (criteria 1 >
+criteria 2 > criteria 3...)  will bring you back in the sort criteria
+list. Using this feature you can easily test various sorting
+strategies.
 
 <?php
 help_footer();
