0b9866bb2c41aedac08acf909d569439ea8b46cd
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2002-03-25 15:06:45 +0000

    Change bg color and add banner argument (task #256)

--- SF/www/include/help.php
+++ SF/www/include/help.php
@@ -6,27 +6,46 @@
 //
 // $Id$
 
+/**
+* help_button() - Show a help button.
+*
+* @param        string    The button type
+* @param        int        The trove category ID
+*/
 function help_button($type,$helpid) {
 	if ($type == 'trove_cat') {
 		return ('<A href="javascript:help_window(\'/help/trove_cat.php'
 			.'?trove_cat_id='.$helpid.'\')"><B>(?)</B></A>');
+	} else {
+	    // Generic processing derives the script name from the help type
+	    $script = '/help/'.$type.'.php';
+	    return ('<A href="javascript:help_window(\''.$script.
+		    '?helpid='.$helpid.'\')"><B>(?)</B></A>');
 	}
 }
 
-function help_header($title) {
+/**
+* help_header() - Show a help page header
+*
+* @param        string    Header title
+*/
+function help_header($title, $help_banner=true) {
 ?>
 <HTML>
 <HEAD>
 <TITLE><?php print $title; ?></TITLE>
 <LINK rel="stylesheet" href="/sourceforge.css" type="text/css">
 </HEAD>
-<BODY bgcolor="#FFFFFF">
-<H4>CodeX Site Help System:</H4>
+<BODY bgcolor="#bcbcad">
+<?php print ($help_banner ? '<H4>CodeX Site Help System</H4>' : ''); ?>
 <H2><?php print $title; ?></H2>
 <HR>
 <?php
 }
 
+/**
+* help_footer() - Show a help page footer
+*/
 function help_footer() {
 ?>
 </BODY>
