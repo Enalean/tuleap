0dc6a8d60419d1629e6020414e80b0ea78dde5c3
sbouhet <sbouhet@69b26aa9-6588-4999-a7e6-a99cda329b54>
2002-11-13 14:54:12 +0000

    Bug fixes and some improvements: Export $PATH and add some options: -v (verbose) -f (force generation) -h (help)

--- SF/utils/generate_doc.sh
+++ SF/utils/generate_doc.sh
@@ -13,35 +13,94 @@
 # Purpose:
 #    Automatically re-generate online documentation
 #
+
+FORCE=0
+HELP=0
+VERBOSE=0
+
+# Check arguments
+while	((1))	# look for options
+do	case	"$1" in
+	\-v*)	VERBOSE=1;;
+	\-f*)	FORCE=1;;
+	\-h*)	HELP=1;;
+	*)	if [ ! -z "$1" ];
+	    then
+	        echo "Invalid option $1";
+	        echo "Use -h flag to see all the valid options";
+	        exit 2;
+	    fi
+	    break;;
+	esac
+	shift # next argument
+done
+
+if [ $HELP == 1 ]
+then
+    echo "Usage: generate_doc.sh [-f] [-v] [-h]";
+    echo "  -f : force to generate the documentation witout checking CVS";
+    echo "  -v : verbose";
+    echo "  -h : help";
+    exit 2;
+fi
+
 CURRENTDIR=`pwd`
-BASEDIR="/home/httpd/documentation"
+# honor BASEDIR if defined
+if [ -z "$BASEDIR" ]; then 
+    BASEDIR=/home/httpd/documentation
+fi
 CMDDIR=$BASEDIR/user_guide/cmd
 cd $BASEDIR/user_guide/xml/en_US
-# check if some need some update
-COUNT=`cvs -q -d:pserver:sbouhet@cvs.codex.codex.xerox.com:/cvsroot/codex update | wc -l`
-if [ $COUNT == 0 ]
+
+if [ $FORCE != 1 ]
 then
+    # check if some need some update
+    COUNT=`cvs -q -d:pserver:sbouhet@cvs.codex.codex.xerox.com:/cvsroot/codex update | wc -l`
+    if [ $COUNT == 0 ]
+    then
         # No changes in the documentation
+        if [ $VERBOSE == 1 ]
+        then
+            echo "No changes in the documentation"
+        fi
         exit 0
+    fi
 fi
+
 $CMDDIR/xml2html.sh CodeX_User_Guide.xml ../../html/en_US/ 2>1 >/tmp/log_xml2html_$$
 if [ $? != 0 ]
 then
-        echo "CodeX documentation generation failed!"
+    echo "CodeX documentation generation failed!"
 	echo "See error log below:"
 	echo ""
 	cat /tmp/log_xml2html_$$
-        exit 1
+    exit 1
 fi
-$CMDDIR/xml2pdf.sh CodeX_User_Guide.xml ../../pdf/en_US/CodeX_User_Guide_new.pdf 2>1 >/tmp/log_xml2pdf_$$
+if [ $VERBOSE == 1 ]
+then
+    cat /tmp/log_xml2html_$$
+fi
+
+# set the path
+OLD_PATH=${PATH}
+export PATH=${PATH}:${BASEDIR}/user_guide/cmd
+
+$CMDDIR/xml2pdf.sh CodeX_User_Guide.xml $BASEDIR/user_guide/pdf/en_US/CodeX_User_Guide_new.pdf 2>1 >/tmp/log_xml2pdf_$$
 if [ $? != 0 ]
 then
-        echo "CodeX documentation generation failed!"
+    echo "CodeX documentation generation failed!"
 	echo "See error log below:"
-        echo ""
-        cat /tmp/log_xml2pdf_$$
-        exit 1
+    echo ""
+    cat /tmp/log_xml2pdf_$$
+    export PATH=${OLD_PATH}
+    exit 1
 fi
+if [ $VERBOSE == 1 ]
+then
+    cat /tmp/log_xml2pdf_$$
+fi
+export PATH=${OLD_PATH}
+
 cd $BASEDIR/user_guide/pdf/en_US
 cp -f CodeX_User_Guide.pdf CodeX_User_Guide_old.pdf
 mv CodeX_User_Guide_new.pdf CodeX_User_Guide.pdf
