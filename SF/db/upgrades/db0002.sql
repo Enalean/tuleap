3ba7d2b9f6ed7601072bea2dfd4f3d879ecec47f
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2002-11-15 15:36:47 +0000

    Removed the extraneous newline from the template and also db0002.sql file

--- SF/db/upgrades/db0002.sql
+++ SF/db/upgrades/db0002.sql
@@ -1,6 +1,5 @@
 # CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
-# Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001-2002. All Rights Re
-served
+# Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001-2002. All Rights Reserved
 # http://codex.xerox.com
 #
 # $Id$
