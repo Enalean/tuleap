6cb51e68503edae8e3638fcfdb3998cc58a2969e
sbouhet <sbouhet@69b26aa9-6588-4999-a7e6-a99cda329b54>
2002-11-15 08:51:20 +0000

    Add this forgotten new file in the repository. Related task: task #2025

---
+++ SF/www/current_css.php
@@ -0,0 +1,14 @@
+<?
+//
+// CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
+// Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001-2002. All Rights Reserved
+// http://codex.xerox.com
+//
+// $Id$
+//
+//
+
+    require ('pre.php');
+    
+    session_redirect("/css/".$theme."/".$theme.getFontsizeName($font_size).".css");
+?>
