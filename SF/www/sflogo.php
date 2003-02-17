503622e9d23c0f8e67f5259f0945acc7258b001c
jmayer <jmayer@69b26aa9-6588-4999-a7e6-a99cda329b54>
2003-02-17 20:09:04 +0000

    Modified so that the codex_logo is read from the system's theme specific image directory (bug #18286).

--- SF/www/sflogo.php
+++ SF/www/sflogo.php
@@ -38,9 +38,9 @@ if (!$group_id) {
 
 // LJ updated with the CodeX logo image
 if ($type == 1) {
-	echo readfile ($sys_urlroot.'images/codex_logo.png');
+	echo readfile ($sys_urlroot.'images/'.$sys_themedefault.'.theme/codex_logo.png');
 }  else { // default
-	echo readfile ($sys_urlroot.'images/codex_logo.png');
+	echo readfile ($sys_urlroot.'images/'.$sys_themedefault.'.theme/codex_logo.png');
 } 
 
 ?>
