e68953fc668e27f945b2c8276894a4ae2f7c4122
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2003-06-20 15:45:33 +0000

    Wrongly override the db0011.sql file. Restore it to previous version and commit db0012.sql. Relates to commit #396 and sr #102165)
    Note: Administrators please apply db0012.sql and not db0011.sql !!!

---
+++ SF/db/upgrades/db0012.sql
@@ -0,0 +1,28 @@
+# CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
+# Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001-2002. All Rights Reserved
+# http://codex.xerox.com
+#
+# $Id$
+#
+# Database upgrade script (see dbXXXX_template for instructions)
+#
+# This SQL script allows you to upgrade the CodeX database. In most cases
+# this sql script relates to a well indentified modification in the 
+# CodeX source code. All references are included
+# below.
+#
+# Description
+# Insert a new file type in the frs_filetype table (binary installer)
+#
+#
+# References:
+# See SR #102165
+#
+# Dependencies:
+# None
+#
+# 
+# SQL script comes next...
+#
+
+INSERT INTO frs_filetype VALUES ('3150','Binary installer');
