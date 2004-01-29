f5a1bd721445784f917da83c5cd11e4abd72d75c
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-01-29 10:07:52 +0000

    (This is a merge from branch CX_2_0_SUP - See Commit #4219)
    The first change made in commit #4052 was not sufficient as granting
    'ALL PRIVILEGES' to user 'sourceforge' does not include the 'GRANT OPTION'
    privilege. Without this privilege the 'sourceforge' user cannot grant
    access to user 'cxuser' on the exported project database.
    (see bug #33222)

--- codex_tools/codex_install.sh
+++ codex_tools/codex_install.sh
@@ -429,7 +429,7 @@ if [ ! -d "/var/lib/mysql/sourceforge" ]; then
     freshdb=1
     $MYSQL -u root $pass_opt -e "create database sourceforge"
     $CAT <<EOF | $MYSQL -u root mysql $pass_opt
-GRANT ALL PRIVILEGES on *.* to sourceforge@localhost identified by '$sf_passwd';
+GRANT ALL PRIVILEGES on *.* to sourceforge@localhost identified by '$sf_passwd' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES on *.* to root@localhost identified by '$rt_passwd';
 FLUSH PRIVILEGES;
 EOF
