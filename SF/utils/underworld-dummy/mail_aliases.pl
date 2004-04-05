ddb88d14704a85044bfa178f4f4d9c1697dfa360
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-04-05 11:57:24 +0000

    Starting with mailman 2.1 the mailman wrapper is called 'mailman' and no longer 'wrapper'. So test which one to use for mailing list aliases

--- SF/utils/underworld-dummy/mail_aliases.pl
+++ SF/utils/underworld-dummy/mail_aliases.pl
@@ -7,12 +7,21 @@ use DBI;
 require("../include.pl");  # Include all the predefined functions
 
 $admin_list = ("precision,fusion94,dtype,bigdisk");
+$mailman_dir = "/home/mailman";
 
 &db_connect;
 
 @alias_array = open_array_file("aliases.zone");
 
 push @alias_array, "\n\n### Begin Mailing List Aliases ###\n\n";
+
+# Determine the name of the mailman wrapper
+# Before 2.1  the name is wrapper, after it's mailman
+if ( -x "$mailman_dir/mail/wrapper") {
+  $mm_wrapper = "$mailman_dir/mail/wrapper";
+} else {
+  $mm_wrapper = "$mailman_dir/mail/mailman";
+}
 
 # Select mailing list that public or private but not 'Deleted'
 $query = "SELECT list_name from mail_group_list where is_public IN (0,1)";
