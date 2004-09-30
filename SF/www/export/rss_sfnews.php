d4623d50cca9fc2c18ffc28fab01679a6be4c7f1
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-09-30 09:29:29 +0000

    (This is a merge from branch CX_2_0_SUP - See Commit #12048)
    Better customize the RSS feed for project news and change pointers to
    access the pieces of news directly. Also show all news for a project,
    not only those approved. (Follow-up of commit #12046)

--- SF/www/export/rss_sfnews.php
+++ SF/www/export/rss_sfnews.php
@@ -12,19 +12,30 @@ if (!$limit) $limit = 10;
 if ($limit > 100) $limit = 100;
 
 if ($group_id) {
-    $where_group = " AND group_id=$group_id ";
+    $project = new Project($group_id);
+    $where_clause = " group_id=$group_id ";
+} else {
+    $where_clause = " is_approved=1 ";
 }
+    
 
 $res = db_query('SELECT forum_id,summary,date,details,group_id FROM news_bytes '
-	.'WHERE is_approved=1 '.$where_group.' ORDER BY date DESC LIMIT '.$limit);
+	.'WHERE '.$where_clause.' ORDER BY date DESC LIMIT '.$limit);
 
 // ## one time output
 print " <channel>\n";
 print "  <copyright>Copyright (c) ".$GLOBALS['sys_long_org_name'].", ".$GLOBALS['sys_name']." Team, 2001-".date('Y',time()).". All Rights Reserved</copyright>\n";
 print "  <pubDate>".gmdate('D, d M Y G:i:s',time())." GMT</pubDate>\n";
-print "  <description>".$GLOBALS['sys_name']." Project News Highlights</description>\n";
-print "  <link>http://".$GLOBALS['sys_default_domain']."</link>\n";
-print "  <title>".$GLOBALS['sys_name']." News</title>\n";
+
+if ($group_id) {
+    print "  <description>".$GLOBALS['sys_name']." Project News Highlights - ".$project->getPublicName()."</description>\n";
+    print "  <link>http://".$GLOBALS['sys_default_domain']."/project/?group_id=$group_id</link>\n";
+    print "  <title>".$GLOBALS['sys_name']." News - ".$project->getPublicName()."</title>\n";
+} else {
+    print "  <description>".$GLOBALS['sys_name']." Project News Highlights</description>\n";
+    print "  <link>http://".$GLOBALS['sys_default_domain']."</link>\n";
+    print "  <title>".$GLOBALS['sys_name']." News</title>\n";
+}
 list($host,$port) = explode(':',$GLOBALS['sys_default_domain']);		
 print "  <webMaster>webmaster@".$host."</webMaster>\n";
 print "  <language>en-us</language>\n";
@@ -34,7 +45,7 @@ while ($row = db_fetch_array($res)) {
 	print "   <title>".htmlspecialchars($row[summary])."</title>\n";
 	// if news group, link is main page
 	if ($row[group_id] != $GLOBALS['sys_news_group']) {
-		print "   <link>http://".$GLOBALS['sys_default_domain']."/project/?group_id=$row[group_id]</link>\n";
+		print "   <link>http://".$GLOBALS['sys_default_domain']."/forum/forum.php?forum_id=$row[forum_id]</link>\n";
 	} else {
 		print "   <link>http://".$GLOBALS['sys_default_domain']."/</link>\n";
 	}
