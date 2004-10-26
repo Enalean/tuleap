cd90b57a5cff47c77d0ea741f56738ccf93d29ff
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-10-26 16:55:18 +0000

    Adding an index for directory /file/ Redirect to showfiles.php when no script name is given. Avoid listing content of the directory!

---
+++ SF/www/file/index.php
@@ -0,0 +1,17 @@
+<?php
+//
+// CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
+// Copyright (c) Xerox Corporation, CodeX, 2001-2004. All Rights Reserved
+// http://codex.xerox.com
+//
+// $Id$
+//
+// Originally written by Nicolas Guerin 2004, CodeX Team, Xerox
+//
+
+// Redirect to showfiles.php when no script name is given.
+// Avoid listing content of the directory!
+
+header ("Location: /file/showfiles.php?group_id=$group_id");
+
+?>
