b81a13d59e4b8f9e798c470e0acbb6954fcd9950
schneide <schneide@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-11-03 14:51:32 +0000

    (This is a merge from CX_2_2_SUP - See Commit #13698) fix SR 135 on partners: * insert always comment type 100 for details history even if the Comment Type field is not used. By doing so, there is no problem to switch from use/not use the comment type in a tracker. * correct the comment type for follow-up comments in the artifact_history that have been set by error on NULL instead of 100 (='None') with db0022.sql
    (this bug has been introduced with
    commit #11074 on trunk and branch CX_2_2_SUP and
    commit #12656 on branch CX_2_0_SUP)
    
    System Administrators:
    Please apply patch db0022.sql no matter whether you are on branch CX_2_0_SUP,
    CX_2_2_SUP or on the trunk

---
+++ SF/db/upgrades/db0022.sql
@@ -0,0 +1,32 @@
+# CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
+# Copyright (c) Xerox Corporation, CodeX / CodeX Team, 2001-2002. All Rights Reserved
+# http://codex.xerox.com
+#
+# $Id$
+#
+# Database upgrade script (see dbXXXX_template for instructions)
+#
+# This SQL script allows you to upgrade the CodeX database. In most cases
+# this sql script relates to a well indentified modification in the
+# CodeX source code. All references are included
+# below.
+#
+# Description
+# Correct the comment type for follow-up comments in the artifact_history that
+# have been set by error on NULL instead of 100 (='None') (possible since
+# commit #11074 on trunk and branch CX_2_2_SUP and commit #12656 on branch
+# CX_2_0_SUP)
+#
+# Please apply this patch no matter whether you are on branch CX_2_0_SUP, 
+# CX_2_2_SUP or on the trunk
+#
+#
+# References:
+# See SR 135 on partners
+#
+# Dependencies:
+# none
+#
+#
+
+UPDATE artifact_history SET type = '100' WHERE field_name = 'details' AND type is NULL;
