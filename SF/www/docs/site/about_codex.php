b58c1d0dff5d5622f7b846a4b81ac011e4fa119d
ljulliar <ljulliar@69b26aa9-6588-4999-a7e6-a99cda329b54>
2004-12-22 13:55:34 +0000

    7th commit for localization (See task #1308)
    - All scripts in SF/www/docs localized. There are only a few scripts
    in here and and they have a lot to do with the document manager. So
    the localized messages goes into the docman message catalog.
    - About CodeX test has been extracted from the about_codex.php script and stored in site content.

--- SF/www/docs/site/about_codex.php
+++ SF/www/docs/site/about_codex.php
@@ -6,59 +6,14 @@
 //
 // $Id$
 
-require($DOCUMENT_ROOT.'/include/pre.php');    // Initial db and session library, opens session
-$HTML->header(array(title=>"About the ".$GLOBALS['sys_name']." Site"));
-?>
+require($DOCUMENT_ROOT.'/include/pre.php');
 
-<h2>About the <?php print $GLOBALS['sys_name']; ?> Site</h2>
+$LANG->loadLanguageMsg('docman/docman');
 
-<P><?php print $GLOBALS['sys_name']; ?> is a service to <B>all <?php print $GLOBALS['sys_org_name']; ?> 
-software development team</B>s who want to <b>internally</B> share the
-source code of their software projects with other <?php print $GLOBALS['sys_org_name']; ?> development
-teams. <?php print $GLOBALS['sys_name']; ?> aims at transposing inside
-the <?php print $GLOBALS['sys_long_org_name']; ?> environment, the software development culture and
-methods that have proven to be efficient in the external Open Source
-world.<p>
+$HTML->header(array(title=>$LANG->getText('docs_site_about','title', array($GLOBALS['sys_name']))));
 
-To make things very clear and to avoid any confusion, <?php print
-$GLOBALS['sys_name']; ?> is not about making <?php print $GLOBALS['sys_org_name']; ?> software Open
-Source outside of <?php print $GLOBALS['sys_org_name']; ?>. <?php print $GLOBALS['sys_name']; ?> is about
-Inner Source: it means that all the projects hosted on the <?php print $GLOBALS['sys_name']; ?> site
-are for <u><?php print $GLOBALS['sys_org_name']; ?> internal use and sharing only</u>.<P>
+include(util_get_content('docman/about_codex'));
 
-The <?php print $GLOBALS['sys_name']; ?> team wants to make internal
-source code sharing easy and to provide a single place where to go to
-when you as a developer, a project leader or a manager are looking for
-existing pieces of software and/or technology that you could re-use,
-adapt and improve to speed up your own Time To Market while
-capitalizing on <?php print $GLOBALS['sys_org_name']; ?> knowledge. For a consistent and fruitful
-approach of internal software sharing, the <?php print
-$GLOBALS['sys_name']; ?> Team has put together the <b><A
-href="/docman/display_doc.php?docid=16&group_id=1"><?php print $GLOBALS['sys_org_name']; ?> Code eXchange
-Policy</a></b>. It governs internal code sharing and re-use and is the
-cornerstone of the <?php print $GLOBALS['sys_name']; ?> edifice. Make
-sure you <A href="/docman/display_doc.php?docid=16&group_id=1">read it</a>. <P>
-
-<?php print $GLOBALS['sys_name']; ?> also offers a full-featured,
-purely web-based and easy to use project management and communication
-environment. Avoid the recurrent burden of setting up a project
-management environment in your own team. Using <?php print
-$GLOBALS['sys_name']; ?> can help you save time/money and better focus
-on what really matters: software development and users/developers
-relationship.
-
-We strongly invite you to read this document to better understand
-where <?php print $GLOBALS['sys_org_name']; ?> is today in terms of source code sharing and where we want
-it to be in the near future. If you want to help convert <?php print $GLOBALS['sys_org_name']; ?> to a
-more open and spontaneous internal code sharing culture then spread
-the word about <?php print $GLOBALS['sys_name']; ?> and publish your
-projects on the <?php print $GLOBALS['sys_name']; ?> site.<P>
-
-We thank you for your support!<P>
-
-The <a href="/staff.php"><?php print $GLOBALS['sys_name']; ?> Team</a>.
-
-<?php
 $HTML->footer(array());
 
 ?>
--- SF/www/docs/site/index.php
+++ SF/www/docs/site/index.php
@@ -11,16 +11,18 @@
         Docmentation Manager
         by Quentin Cregan, SourceForge 06/2000
 */
-// GLOBAL $HTML
+
 require($DOCUMENT_ROOT.'/include/pre.php');
+
+$LANG->loadLanguageMsg('docman/docman');
 
 $group_id = 1;
 
 $usermem = user_ismember($group_id);
 
-echo $HTML->header(array('title'=> $GLOBALS['sys_name']." Site Documentation"));
+echo $HTML->header(array('title'=> $LANG->getText('docs_site_index','title')));
 
-echo '<H2>'.$GLOBALS['sys_name'].' Site Documentation</H2>';
+echo '<H2>'.$LANG->getText('docs_site_index','title').'</H2>';
 
 //get a list of group numbers that this project owns
 $query = "select * "
@@ -31,7 +33,7 @@ $result = db_query($query);
 
 //otherwise, throw up an error
 if (db_numrows($result) < 1) {
-	print "<b>This project has no categorized data.</b><p>";
+	print "<b>".$LANG->getText('docs_site_index','nodoc')."</b><p>";
 } else { 
 	// get the groupings and display them with their members.
 	while ($row = db_fetch_array($result)) {
@@ -46,13 +48,10 @@ if (db_numrows($result) < 1) {
 		if (!(db_numrows($subresult) < 1)) {
 			print "<p><H3>".$row['groupname']."</H3>\n<ul>\n";
 			while ($subrow = db_fetch_array($subresult)) {
-// LJ We want the title and the description to
-// possibly contain HTML and php code so unconvert
-// the initially encoded HTML chars and eval the text
 				print "<li><a href=\"../../docman/display_doc.php?docid=".$subrow['docid']."&group_id=".$group_id."\">";
 				eval('?>'.util_unconvert_htmlspecialchars($subrow['title']));
 				print "</a>";
-				print "<BR><i>Description:</i> ";
+				print "<BR><i>".$LANG->getText('docman_index','description').":</i> ";
 				eval('?>'.util_unconvert_htmlspecialchars($subrow['description'])); 
 
 			}
