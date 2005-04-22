3f3e6e35258dc99093c3f88f95978e62dbe45ae8
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2005-04-22 15:42:57 +0000

    Added multiple language support. Use '-s' to get only the summary. Current status: en_US: 98.7% complete fr_FR: 65% complete

--- SF/utils/analyse_language_files.pl
+++ SF/utils/analyse_language_files.pl
@@ -13,10 +13,9 @@
 #
 # Purpose:
 #  Simple utility to analyse Language data.
-#  Currently limited to one language.
 #
 # Usage:
-#  Edit the directories below, and run the script without argument!
+#  Edit the directories below, and run the script. Use '-s' for silent mode (summary only)
 #
 # Originaly written by Nicolas Guerin - 2005, CodeX Team, Xerox
 #
@@ -28,21 +27,32 @@ use strict;
 # Location of CodeX sources to analyse
 my $source_dir="../../SF";
 # Language specific dir
-my $tab_dir="../../site-content/en_US";
+my $tab_dir="../../site-content/";
 
 # end of configuration
 
 my %firstkey;
 my %keys;
-my %tab_keys;
 
-my $tab_lines=0;
+my %missing_keys_array;
+my %tab_lines_array;
+my %duplicate_keys_array;
+my %unused_keys_array;
+my %incorrect_keys_array;
 my $used_lines=0;
-my $duplicate_keys=0;
-my $missing_keys=0;
-my $unused_keys=0;
-my $incorrect_keys=0;
+my $total_keys=0;
+my $silent_mode=0;
 
+my $usage="usage: analyse_language_file.pl [-s] \
+    -s: silent mode \
+";
+
+
+if ($ARGV[0]) {
+  if ($ARGV[0] eq '-s') {
+    $silent_mode=1;
+  } else { print $usage; }
+}
 
 my @files=`find $source_dir -name "*.class"`;
 push @files,`find $source_dir -name "*.php"`;
@@ -51,9 +61,9 @@ push @files,"$source_dir/www/projects";
 push @files,"$source_dir/www/users";
 
 
-print "***\n";
-print "*** Reading Source Code\n";
-print "***\n";
+print "***\n" unless ($silent_mode);
+print "*** Reading Source Code\n" unless ($silent_mode);
+print "***\n" unless ($silent_mode);
 
 # Read all messages in the source code.
 foreach my $filename (@files) {
@@ -64,6 +74,7 @@ foreach my $filename (@files) {
     while (/\$Language->getText\(.([^,\'\"\s]+).[\s]*\,[\s]*.([^,\'\"\s]+)[^\)][\)\,]/) {
       #print "\t$1\t$2\n";
       $firstkey{"$1"}=1;
+      if (!$keys{"$1"}{"$2"}) { $total_keys++;}
       $keys{"$1"}{"$2"}.="$filename ";
       $used_lines++;
       # for multiple occurences on same line
@@ -73,87 +84,122 @@ foreach my $filename (@files) {
   close FILE;
 }
 
-
-print "***\n";
-print "*** Reading Tab files\n";
-print "***\n";
-
-# Read messages in tab files
-my @tab_files=`find $tab_dir -name "*.tab"`;
-foreach my $filename (@tab_files) {
-  next if ($filename =~ /\/Base\.tab$/); # GForge file
-  chomp $filename;
-
-  # For readability
-  my $fileonly;
-  if ($filename=~m|.*/([^/]+)$|) {
-    $fileonly=$1;
-  } else {
-    $fileonly=$filename;
-  }
-
-  open FILE,"$filename";
-  #print "File: $filename\n";
-  my $linecount=0;
-  while(<FILE>) {
-    my $line=$_;
-    $linecount++;
-    next if (/^\#/); # comment
-    next if (/^$/); # empty
-    if (/^([^\t]*)\t([^\t]*)\t(.*)$/) {
-      my $key1=$1;
-      my $key2=$2;
-      my $val=$3;
-      if ($tab_keys{"$key1"}{"$key2"}) {
-        print "$fileonly($linecount) - Duplicate keys:$key1 $key2 - $line";
-        $duplicate_keys++;
-      } else {
-        $tab_keys{"$1"}{"$2"}.="$3";
-        $tab_lines++;
-      }
-    } else {
-      print "$fileonly($linecount) - Error: $line";
-      $incorrect_keys++;
-    }
-  }
-  close FILE;
+if (!$silent_mode) {
+  print "***\n";
+  print "*** Reading Tab files\n";
+  print "***\n";
 }
 
+my @lang_tab_dir=`/bin/ls $tab_dir | grep -v CVS`;
 
-print "***\n";
-print "*** Checking missing keys\n";
-print "***\n";
-foreach my $key1 (keys %keys) {
-  foreach my $key2 (keys %{ $keys{"$key1"}}) {
+foreach my $my_tab_dir (@lang_tab_dir) {
+  chomp $my_tab_dir;
+  print "*** $my_tab_dir\n\n" unless ($silent_mode);
+  my $missing_keys=0;
+  my $tab_lines=0;
+  my $duplicate_keys=0;
+  my $unused_keys=0;
+  my $incorrect_keys=0;
+  my %tab_keys;
+
+  # Read messages in tab files
+  my @tab_files=`find $tab_dir/$my_tab_dir -name "*.tab"`;
+  foreach my $filename (@tab_files) {
+    next if ($filename =~ /\/Base\.tab$/); # GForge file
+    chomp $filename;
+    
+    # For readability
+    my $fileonly;
+    if ($filename=~m|.*/([^/]+)$|) {
+      $fileonly=$1;
+    } else {
+      $fileonly=$filename;
+    }
+
+    open FILE,"$filename";
+    #print "File: $filename\n";
+    my $linecount=0;
+    while(<FILE>) {
+      my $line=$_;
+      $linecount++;
+      next if (/^\#/); # comment
+      next if (/^$/); # empty
+      if (/^([^\t]*)\t([^\t]*)\t(.*)$/) {
+        my $key1=$1;
+        my $key2=$2;
+        my $val=$3;
+        if ($tab_keys{"$key1"}{"$key2"}) {
+          print "$fileonly($linecount) - Duplicate keys:$key1 $key2 - $line" unless ($silent_mode);
+          $duplicate_keys++;
+        } else {
+          $tab_keys{"$1"}{"$2"}.="$3";
+          $tab_lines++;
+        }
+      } else {
+        print "$fileonly($linecount) - Error: $line" unless ($silent_mode);
+        $incorrect_keys++;
+      }
+    }
+    close FILE;
+  }
+
+
+  if (!$silent_mode) {
+    print "***\n";
+    print "*** Checking missing keys\n";
+    print "***\n";
+  }
+  foreach my $key1 (keys %keys) {
+    foreach my $key2 (keys %{ $keys{"$key1"}}) {
       if (! $tab_keys{"$key1"}{"$key2"}) {
-        print "Missing:  $key1\t$key2\t (".$keys{"$key1"}{"$key2"}.")\n";
+        print "Missing:  $key1\t$key2\t (".$keys{"$key1"}{"$key2"}.")\n" unless ($silent_mode);
         $missing_keys++;
       }
+    }
   }
-}
+  $missing_keys_array{$my_tab_dir}=$missing_keys;
 
-print "***\n";
-print "*** Checking unused keys\n";
-print "***\n";
-foreach my $key1 (keys %tab_keys) {
-  foreach my $key2 (keys %{ $tab_keys{"$key1"}}) {
+
+  if (!$silent_mode) {
+    print "***\n";
+    print "*** Checking unused keys\n";
+    print "***\n";
+  }
+  foreach my $key1 (keys %tab_keys) {
+    foreach my $key2 (keys %{ $tab_keys{"$key1"}}) {
       if (! $keys{"$key1"}{"$key2"}) {
-        print "Unused:  $key1\t$key2\t (".$tab_keys{"$key1"}{"$key2"}.")\n";
+        print "Unused:  $key1\t$key2\t (".$tab_keys{"$key1"}{"$key2"}.")\n" unless ($silent_mode);
         $unused_keys++
       }
+    }
   }
+
+
+  $missing_keys_array{$my_tab_dir}=$missing_keys;
+  $tab_lines_array{$my_tab_dir}=$tab_lines;
+  $duplicate_keys_array{$my_tab_dir}=$duplicate_keys;
+  $unused_keys_array{$my_tab_dir}=$unused_keys;
+  $incorrect_keys_array{$my_tab_dir}=$incorrect_keys;
+
 }
+
 
 
 print "\nReport:\n";
 print "*******\n";
-print "$tab_lines \tentries in .tab files\n";
 print "$used_lines \tgetText() calls\n";
-print "$duplicate_keys \tduplicate keys\n";
-print "$missing_keys \tmissing keys\n";
-print "$unused_keys \tunused keys\n";
-print "$incorrect_keys \tincorrect keys\n";
-
+print "$total_keys \tunique keys\n";
+foreach my $lang (keys %missing_keys_array) {
+  my $percent=100*(1 - ($total_keys-($tab_lines_array{$lang}-$duplicate_keys_array{$lang}-$unused_keys_array{$lang}-$incorrect_keys_array{$lang}))/$total_keys);
+  $percent = sprintf('%.2f', $percent);
+  print "Language: $lang:\n";
+  print "  ".$tab_lines_array{$lang}." \tentries in .tab files\n";
+  print "  ".$duplicate_keys_array{$lang}." \tduplicate keys\n";
+  print "  ".$missing_keys_array{$lang}." \tmissing keys\n";
+  print "  ".$unused_keys_array{$lang}." \tunused keys\n";
+  print "  ".$incorrect_keys_array{$lang}." \tincorrect keys\n";
+  print "  -> $percent% complete\n";
+}
 exit 0;
 
 foreach my $key1 (keys %keys) {
