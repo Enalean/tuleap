a611da7558c6543face802bf7fc9aa65c2b97dc4
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2005-06-29 13:55:04 +0000

    (This is a merge from CX_2_4_1_SUP - See Commit #22697)
    Redirect user to 'my' page instead of 'site' page.

--- SF/www/include/pre.php
+++ SF/www/include/pre.php
@@ -186,10 +186,14 @@ if ($SERVER_NAME != 'localhost' &&
     $SCRIPT_NAME != '/account/lostpw-confirm.php' &&
     $SCRIPT_NAME != '/account/pending-resend.php' &&
     $SCRIPT_NAME != '/account/verify.php' ) {
-    if ($GLOBALS['sys_force_ssl'] == 1 || $_SERVER['HTTPS'] === 'on')
-	header("Location: https://".$GLOBALS['sys_https_host']."/account/login.php?return_to=".urlencode($REQUEST_URI));
-    else
-	header("Location: http://".$GLOBALS['sys_default_domain']."/account/login.php?return_to=".urlencode($REQUEST_URI));
+    
+    $return_to = urlencode((($REQUEST_URI === "/")?"/my/":$REQUEST_URI));
+
+    if ($GLOBALS['sys_force_ssl'] == 1 || $_SERVER['HTTPS'] === 'on') {
+        header("Location: https://".$GLOBALS['sys_https_host']."/account/login.php?return_to=".$return_to);
+    } else {
+        header("Location: http://".$GLOBALS['sys_default_domain']."/account/login.php?return_to=".$return_to);
+    }
     exit;
 }
 
@@ -199,4 +203,6 @@ if (user_isrestricted()) {
     }
 }
 
+require_once('common/include/Feedback.class');
+$GLOBALS['new_feedback'] =& new Feedback();
 ?>
