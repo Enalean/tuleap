b415e148701d44381945ca6d5ce252cb7262657b
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2005-08-01 14:29:51 +0000

    Improve CODEX_RUNNER mechanism. A UnitTest can now inherit from another UnitTest without reporting conflict.

--- codex_tools/tests/TemplateTest.php
+++ codex_tools/tests/TemplateTest.php
@@ -1,4 +1,10 @@
 <?php
+//We want to be able to run one test AND many tests
+if (! defined('CODEX_RUNNER')) {
+    define('CODEX_RUNNER', __FILE__);
+    require_once('tests/CodexReporter.class');
+}
+
 require_once('tests/simpletest/unit_tester.php');
 
 /**
@@ -6,15 +12,15 @@ require_once('tests/simpletest/unit_tester.php');
  * 
  * $Id$
  *
- * Test the class ...
+ * Tests the class Template
  */
 class TemplateTest extends UnitTestCase {
     /**
      * Constructor of the test. Can be ommitted.
      * Usefull to set the name of the test
      */
-    function TemplateTest() {
-        $this->UnitTestCase('Template test');
+    function TemplateTest($name = 'Template test') {
+        $this->UnitTestCase($name);
     }
 
     /**
@@ -26,11 +32,7 @@ class TemplateTest extends UnitTestCase {
     }	
 }
 
-//We want to be able to run one test AND many tests
-if (! defined('CODEX_RUNNER')) {
-    define('CODEX_RUNNER', true);
-    require_once('tests/CodexReporter.class');	
-        	
+if (CODEX_RUNNER === __FILE__) {
     $test = &new TemplateTest();
     $test->run(new CodexReporter());
  }
