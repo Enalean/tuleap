219472dcf63987cc096d99d2aee0f0a2d0835ed0
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2005-10-27 14:16:08 +0000

    Use now mysql-real-escape-string instead of relying on magic_quotes_gpc to prevent SQL Injections in dao package. This follows php recommendation at http://php.net/mysql-real-escape-string

--- codex_tools/tests/common/dao/include/DataAccessTest.php
+++ codex_tools/tests/common/dao/include/DataAccessTest.php
@@ -40,6 +40,33 @@ class DataAccessTest extends UnitTestCase {
         $this->assertFalse($da->isError());
         $this->assertIsA($da->fetch("select *"),'DataAccessResult');
     }
+    
+    function testQuoteSmart() {
+        require(getenv('SF_LOCAL_INC_PREFIX').'/etc/codex/conf/local.inc');
+        $da =& new DataAccess($sys_dbhost, $sys_dbuser, $sys_dbpasswd, $sys_dbname);
+        $this->assertIdentical('123', $da->quoteSmart("123"), "An integer is not quoted");
+        $this->assertIdentical('12.3', $da->quoteSmart("12.3"), "A float is not quoted");
+        $this->assertIdentical("'value'", $da->quoteSmart("value"), "A string is quoted");
+        if (get_magic_quotes_gpc()) {
+            $this->assertIdentical('\'evil\\\'s value\'', $da->quoteSmart("evil\\'s value"));
+            $this->assertIdentical("'\\\\x00'", $da->quoteSmart('\\\\x00'));
+            $this->assertIdentical("'\\\\n'", $da->quoteSmart('\\\\n'));
+            $this->assertIdentical("'\\\\r'", $da->quoteSmart("\\\\r"));
+            $this->assertIdentical("'\\\\'", $da->quoteSmart("\\\\"));
+            $this->assertIdentical("'\\''", $da->quoteSmart("\\'"));
+            $this->assertIdentical("'\\\"'", $da->quoteSmart("\\\""));
+            $this->assertIdentical("'\\\\x1a'", $da->quoteSmart("\\\\x1a"));
+        } else {
+            $this->assertIdentical("'evil\\'s value'", $da->quoteSmart("evil's value"));
+            $this->assertIdentical("'\\\\x00'", $da->quoteSmart('\\x00'));
+            $this->assertIdentical("'\\\\n'", $da->quoteSmart('\\n'));
+            $this->assertIdentical("'\\\\r'", $da->quoteSmart("\\r"));
+            $this->assertIdentical("'\\\\'", $da->quoteSmart("\\"));
+            $this->assertIdentical("'\\''", $da->quoteSmart("'"));
+            $this->assertIdentical("'\\\"'", $da->quoteSmart("\""));
+            $this->assertIdentical("'\\\\x1a'", $da->quoteSmart("\\x1a"));
+        }
+    }
 }
 
 if (CODEX_RUNNER === __FILE__) {
