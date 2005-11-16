eedb20e8cb02594fd3d6a69830c076595997b9e0
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2005-11-16 17:19:23 +0000

    Fixed backticks in SQL script. Follow-up to rev #2268

--- codex_tools/migration_26.sh
+++ codex_tools/migration_26.sh
@@ -444,28 +444,28 @@ UPDATE user SET theme = 'CodeX' WHERE theme = 'codex';
 ALTER TABLE artifact_file ADD INDEX ( artifact_id );
 
 -- ugroup localization
-UPDATE `ugroup` SET `description` = 'ugroup_anonymous_users_desc_key' WHERE `group_id` = '100' AND `name` = 'anonymous_users';
-UPDATE `ugroup` SET `description` = 'ugroup_registered_users_desc_key' WHERE `group_id` = '100' AND `name` = 'registered_users';
-UPDATE `ugroup` SET `description` = 'ugroup_project_members_desc_key' WHERE `group_id` = '100' AND `name` = 'project_members';
-UPDATE `ugroup` SET `description` = 'ugroup_project_admins_desc_key' WHERE `group_id` = '100' AND `name` = 'project_admins';
-UPDATE `ugroup` SET `description` = 'ugroup_file_manager_admin_desc_key' WHERE `group_id` = '100' AND `name` = 'file_manager_admin';
-UPDATE `ugroup` SET `description` = 'ugroup_document_tech_desc_key' WHERE `group_id` = '100' AND `name` = 'document_tech';
-UPDATE `ugroup` SET `description` = 'ugroup_document_admin_desc_key' WHERE `group_id` = '100' AND `name` = 'document_admin';
-UPDATE `ugroup` SET `description` = 'ugroup_wiki_admin_desc_key' WHERE `group_id` = '100' AND `name` = 'wiki_admin';
-UPDATE `ugroup` SET `description` = 'ugroup_tracker_admins_desc_key' WHERE `group_id` = '100' AND `name` = 'tracker_admins';
-UPDATE `ugroup` SET `description` = 'ugroup_tracker_techs_desc_key' WHERE `group_id` = '100' AND `name` = 'tracker_techs';
-UPDATE `ugroup` SET `description` = 'ugroup_nobody_desc_key' WHERE `group_id` = '100' AND `name` = 'nobody';
+UPDATE ugroup SET description = 'ugroup_anonymous_users_desc_key' WHERE group_id = '100' AND name = 'anonymous_users';
+UPDATE ugroup SET description = 'ugroup_registered_users_desc_key' WHERE group_id = '100' AND name = 'registered_users';
+UPDATE ugroup SET description = 'ugroup_project_members_desc_key' WHERE group_id = '100' AND name = 'project_members';
+UPDATE ugroup SET description = 'ugroup_project_admins_desc_key' WHERE group_id = '100' AND name = 'project_admins';
+UPDATE ugroup SET description = 'ugroup_file_manager_admin_desc_key' WHERE group_id = '100' AND name = 'file_manager_admin';
+UPDATE ugroup SET description = 'ugroup_document_tech_desc_key' WHERE group_id = '100' AND name = 'document_tech';
+UPDATE ugroup SET description = 'ugroup_document_admin_desc_key' WHERE group_id = '100' AND name = 'document_admin';
+UPDATE ugroup SET description = 'ugroup_wiki_admin_desc_key' WHERE group_id = '100' AND name = 'wiki_admin';
+UPDATE ugroup SET description = 'ugroup_tracker_admins_desc_key' WHERE group_id = '100' AND name = 'tracker_admins';
+UPDATE ugroup SET description = 'ugroup_tracker_techs_desc_key' WHERE group_id = '100' AND name = 'tracker_techs';
+UPDATE ugroup SET description = 'ugroup_nobody_desc_key' WHERE group_id = '100' AND name = 'nobody';
 
-UPDATE `ugroup` SET `name` = 'ugroup_anonymous_users_name_key' WHERE `group_id` = '100' AND `name` = 'anonymous_users';
-UPDATE `ugroup` SET `name` = 'ugroup_registered_users_name_key' WHERE `group_id` = '100' AND `name` = 'registered_users';
-UPDATE `ugroup` SET `name` = 'ugroup_project_members_name_key' WHERE `group_id` = '100' AND `name` = 'project_members';
-UPDATE `ugroup` SET `name` = 'ugroup_project_admins_name_key' WHERE `group_id` = '100' AND `name` = 'project_admins';
-UPDATE `ugroup` SET `name` = 'ugroup_file_manager_admin_name_key' WHERE `group_id` = '100' AND `name` = 'file_manager_admin';
-UPDATE `ugroup` SET `name` = 'ugroup_document_tech_name_key' WHERE `group_id` = '100' AND `name` = 'document_tech';
-UPDATE `ugroup` SET `name` = 'ugroup_document_admin_name_key' WHERE `group_id` = '100' AND `name` = 'document_admin';
-UPDATE `ugroup` SET `name` = 'ugroup_wiki_admin_name_key' WHERE `group_id` = '100' AND `name` = 'wiki_admin';
-UPDATE `ugroup` SET `name` = 'ugroup_tracker_admins_name_key' WHERE `group_id` = '100' AND `name` = 'tracker_admins';
-UPDATE `ugroup` SET `name` = 'ugroup_tracker_techs_name_key' WHERE `group_id` = '100' AND `name` = 'tracker_techs';
+UPDATE ugroup SET name = 'ugroup_anonymous_users_name_key' WHERE group_id = '100' AND name = 'anonymous_users';
+UPDATE ugroup SET name = 'ugroup_registered_users_name_key' WHERE group_id = '100' AND name = 'registered_users';
+UPDATE ugroup SET name = 'ugroup_project_members_name_key' WHERE group_id = '100' AND name = 'project_members';
+UPDATE ugroup SET name = 'ugroup_project_admins_name_key' WHERE group_id = '100' AND name = 'project_admins';
+UPDATE ugroup SET name = 'ugroup_file_manager_admin_name_key' WHERE group_id = '100' AND name = 'file_manager_admin';
+UPDATE ugroup SET name = 'ugroup_document_tech_name_key' WHERE group_id = '100' AND name = 'document_tech';
+UPDATE ugroup SET name = 'ugroup_document_admin_name_key' WHERE group_id = '100' AND name = 'document_admin';
+UPDATE ugroup SET name = 'ugroup_wiki_admin_name_key' WHERE group_id = '100' AND name = 'wiki_admin';
+UPDATE ugroup SET name = 'ugroup_tracker_admins_name_key' WHERE group_id = '100' AND name = 'tracker_admins';
+UPDATE ugroup SET name = 'ugroup_tracker_techs_name_key' WHERE group_id = '100' AND name = 'tracker_techs';
 
 
 EOF
