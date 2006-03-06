88ddc662d2df66e844cdaedb905a8460b5657b62
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-03-06 14:38:00 +0000

    - Add a new format for the user guide : a single-page HTML documentation. - Add a chapter in the programmer guide about the documentation process and directory structure.

--- documentation/user_guide/cmd/xml2html.sh
+++ documentation/user_guide/cmd/xml2html.sh
@@ -27,6 +27,8 @@ CP=${SAXON_HOME}/saxon.jar
 echo Using CLASSPATH: ${CP}
 PREV_DIR=`pwd`
 cd $2
+
+# Generate the multi-page documentation (documentation splitted into sub-chapter html files)
 ${JAVA_HOME}/java -cp ${CP} com.icl.saxon.StyleSheet ${PREV_DIR}/$1 ${DOC_HOME}/user_guide/xsl/htmlhelp/htmlhelp_$3.xsl > /dev/null
 if [ $? != 0 ]
 then 
@@ -34,6 +36,16 @@ then
         echo "Failed!"
         exit 1
 fi
+
+# Generate the single-page documentation (one html file for all the documentation)
+${JAVA_HOME}/java -cp ${CP} com.icl.saxon.StyleSheet ${PREV_DIR}/$1 ${DOC_HOME}/user_guide/xsl/htmlhelp/htmlhelp_onechunk_$3.xsl > /dev/null
+if [ $? != 0 ]
+then 
+	cd ${PREV_DIR}
+        echo "Failed!"
+        exit 1
+fi
+
 cd ${PREV_DIR}
 echo "Done!"
 exit 0
