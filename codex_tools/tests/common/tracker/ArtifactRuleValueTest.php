438bc3c44042689fa6194b7e3df54b85859669ac
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-03-16 10:07:59 +0000

    Fix bug in test

--- codex_tools/tests/common/tracker/ArtifactRuleValueTest.php
+++ codex_tools/tests/common/tracker/ArtifactRuleValueTest.php
@@ -26,20 +26,33 @@ class ArtifactRuleValueTest extends UnitTestCase {
         $this->UnitTestCase($name);
     }
     
-    function testPass() {
+    function testApplyTo() {
         $arv =& new ArtifactRuleValue('id', 'group_artifact_id', 'source_field', 'source_value', 'target_field', 'target_value');
         
-        $this->assertTrue( $arv->pass('group_artifact_id',       'source_field',        'source_value',       'target_field',       'target_value'      ));
-        $this->assertTrue( $arv->pass('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'farget_value'      ));
-        $this->assertTrue( $arv->pass('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'false_target_value'));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_value'       ));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_target_value'));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_value'       ));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_target_value'));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_value'       ));
-        $this->assertTrue( $arv->pass('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_target_value'));
+        $this->assertTrue( $arv->applyTo('group_artifact_id',       'source_field',        'source_value',       'target_field',       'target_value'      ));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'source_field',        'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->applyTo('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'farget_value'      ));
+        $this->assertFalse($arv->applyTo('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_value'       ));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_value'       ));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_target_value'));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_value'       ));
+        $this->assertFalse($arv->applyTo('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_target_value'));
+    }
+    function testCanApplyTo() {
+        $arv =& new ArtifactRuleValue('id', 'group_artifact_id', 'source_field', 'source_value', 'target_field', 'target_value');
         
-        $this->assertFalse($arv->pass('group_artifact_id',       'source_field',        'source_value',       'target_field',       'false_target_value'));
+        $this->assertTrue( $arv->canApplyTo('group_artifact_id',       'source_field',        'source_value',       'target_field',       'target_value'      ));
+        $this->assertTrue( $arv->canApplyTo('group_artifact_id',       'source_field',        'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->canApplyTo('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'farget_value'      ));
+        $this->assertFalse($arv->canApplyTo('false_group_artifact_id', 'source_field',        'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_value'       ));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'target_source_field', 'source_value',       'target_field',       'false_target_value'));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_value'       ));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'source_field',        'false_source_value', 'target_field',       'false_target_value'));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_value'       ));
+        $this->assertFalse($arv->canApplyTo('group_artifact_id',       'source_field',        'source_value',       'false_target_field', 'false_target_value'));
     }
 }
 
