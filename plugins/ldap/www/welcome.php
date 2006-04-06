41904890703102fd1141b744fa1e45cc333a1624
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-04-06 16:03:07 +0000

    (this is a merge from rev #2873 on contrib/st/bugfix/420_ldap_misc/ branch)
    r5128@crx1348 (orig r1490):  manuel_vacelet | 2006-03-29 15:09:05 +0200
    A little bit of enhancement of LDAP account creation screen (with a look close
    to the pluginadministration panel one)

--- plugins/ldap/www/welcome.php
+++ plugins/ldap/www/welcome.php
@@ -38,6 +38,16 @@ require_once('timezones.php');
 require_once('account.php');
 require_once('../include/UserLdap.class');
 
+function welcome_exit_error($title,$text) {
+    global $HTML,$Language;
+    $GLOBALS['feedback'] .= $title;
+    site_header(array('title'=>$Language->getText('include_exit','exit_error'),
+                      'registeration_process' => true));
+    echo '<p>',$text,'</p>';
+	$HTML->footer(array('showfeedback' => false));
+	exit;
+}
+
 function update_account($user_id, $tz='None', $mail_site=0, $mail_va=0) {
     global $TZs;
 
@@ -71,8 +81,8 @@ $user_id = user_getid();
 
 if($_POST['action'] == 'update_reg') {   
     if ($_POST['timezone'] == 'None') {
-        exit_error($Language->getText('plugin_ldap', 'welcome_error_up'),
-                   $Language->getText('plugin_ldap', 'welcome_err_notz'));        
+        welcome_exit_error($Language->getText('plugin_ldap', 'welcome_error_up'),
+                           $Language->getText('plugin_ldap', 'welcome_err_notz'));        
     }
 
     // Update DB
@@ -80,8 +90,8 @@ if($_POST['action'] == 'update_reg') {
                        $_POST['timezone'],
                        $_POST['form_mail_site'],
                        $_POST['form_mail_va'])) {
-        exit_error($Language->getText('plugin_ldap', 'welcome_error_up'),
-                   $Language->getText('plugin_ldap', 'welcome_error_up_expl', array(db_error())));
+        welcome_exit_error($Language->getText('plugin_ldap', 'welcome_error_up'),
+                           $Language->getText('plugin_ldap', 'welcome_error_up_expl', array(db_error())));
     }
             
     account_redirect_after_login();
@@ -109,7 +119,9 @@ else {
 
     print '<p>'.$Language->getText('plugin_ldap', 'welcome_fill_form', array($GLOBALS['sys_name'])).'</p>';
 
-    print '<div style="border: 1px solid black; padding: 4px;">';
+    print '<fieldset>';
+    
+    print '<legend>'.$Language->getText('plugin_ldap', 'welcome_preferences').'</legend>';
 
     $return_to = '';
     if(array_key_exists('return_to', $_REQUEST) && $_REQUEST['return_to'] != '') {
@@ -134,11 +146,10 @@ else {
     print '<p>'.$Language->getText('plugin_ldap', 'welcome_mandatory', array($star)).'</p>';
 
     print '<p><input type="submit" name="update_reg" value="'.$Language->getText('plugin_ldap', 'welcome_btn_update').'"></p>';
-    print '</div>';
+    print '</fieldset>';
 
-    print '<p>'.$Language->getText('plugin_ldap', 'welcome_your_data', array($GLOBALS['sys_org_name'])).'</p>';
-
-    print '<div style="border: 1px solid black; padding: 4px;">';
+    print '<fieldset>';
+    print '<legend>'.$Language->getText('plugin_ldap', 'welcome_your_data', array($GLOBALS['sys_org_name'])).'</legend>';
 
     print '<table>
 <tr>
@@ -157,7 +168,7 @@ else {
 </tr>
 </table>';
 
-    print '</div>';
+    print '</fieldset>';    
 
     $HTML->footer(array());
 }
