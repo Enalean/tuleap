d412c4f7a6043c51a07d333d78bce2118ecc266d
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-06-05 16:32:16 +0000

    Restoring file deleted in a bad handling (in rev #3180)
    Here is the history of the file :
    
    ------------------------------------------------------------------------
    r2580 | nterray | 2006-02-17 15:26:26 +0100 (Fri, 17 Feb 2006) | 4 lines
    
    Refactoring:
    - Move javascript files from /include/ to /scripts/
    
    ------------------------------------------------------------------------
    r2076 | nterray | 2005-09-28 13:43:42 +0200 (Wed, 28 Sep 2005) | 2 lines
    
    Apply stylesheets of parent window in calendar popup.
    
    ------------------------------------------------------------------------
    r1895 | guerin | 2005-08-18 13:34:01 +0200 (Thu, 18 Aug 2005) | 2 lines
    
    Refactoring: moving trunk to 'dev' directory
    
    ------------------------------------------------------------------------
    r805 | ljulliar | 2004-01-29 13:34:24 +0100 (Thu, 29 Jan 2004) | 4 lines
    
    (This is a merge from branch CX_2_0_SUP - See Commit #4222)
    The calendar popup window now displays correctly with custom themes
    (css and images). (see Bug #33629)
    
    ------------------------------------------------------------------------
    r397 | sbouhet | 2002-11-14 15:14:57 +0100 (Thu, 14 Nov 2002) | 3 lines
    
    Bug fix: add .css in the url of link stylesheet TAG.
    Related task: task #2025, task #2301
    
    ------------------------------------------------------------------------
    r392 | sbouhet | 2002-11-13 11:22:05 +0100 (Wed, 13 Nov 2002) | 2 lines
    
    Bug fixes for specific windows (calendar and upload confirmation) for CSS management.
    
    ------------------------------------------------------------------------
    r380 | sbouhet | 2002-11-06 14:49:15 +0100 (Wed, 06 Nov 2002) | 3 lines
    
    Add dn_arrow.png image, fixed bug for the use of "middle" value for the align attribute of TD tag and
    some CSS improvements.
    
    ------------------------------------------------------------------------
    r378 | sbouhet | 2002-11-05 14:46:57 +0100 (Tue, 05 Nov 2002) | 3 lines
    
    Enable the customizable UI (See Task #2301).
    Change statics HTML tags and attributes (font, color, ...) to CSS classes.
    
    ------------------------------------------------------------------------
    r184 | ljulliar | 2002-03-25 15:47:17 +0100 (Mon, 25 Mar 2002) | 2 lines
    
    Initial revision (task #1526)
    
    ------------------------------------------------------------------------

---
+++ SF/www/scripts/calendar.js
@@ -0,0 +1,212 @@
+// Title: Timestamp picker
+// Description: See the demo at url
+// URL: http://www.geocities.com/tspicker/
+// Version: 1.0.a (Date selector only) reworked by Richard Perry
+// Date: 12-12-2001 (mm-dd-yyyy)
+// Version 1.0.b reworked by Eddie May. Added time display and made compliant with Netscape 6.2.
+// Date 22/01/2002 (dd-mm-yyyy).
+// Author: Denis Gritcyuk <denis@softcomplex.com>; <tspicker@yahoo.com>
+// Notes: Permission given to use this script in any kind of applications if
+//    header lines are left unchanged. Feel free to contact the author
+//    for feature requests and/or donations
+//
+// Modified by Laurent Julliard for CodeX project
+// $Id: calendar.js 2580 2006-02-17 14:26:26Z nterray $
+
+function show_calendar(str_target, str_datetime, css_theme_file, img_theme_path) {
+        var arr_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var week_days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+        var n_weekstart = 1; // day week starts from (normally 0 or 1)
+
+	// If no date/time given then default to today at 00:00
+	if (str_datetime == null || str_datetime =="") {
+	//	var dt_datetime = str2dt(dt2dtstr(new Date()) + "00:00");
+		var dt_datetime = new Date();
+	} else {
+           	var dt_datetime = str2dt(str_datetime);
+	}
+
+	//var dt_datetime = (str_datetime == null || str_datetime =="" ?  new Date() : str2dt(str_datetime));
+		
+	var dt_prev_month = new Date(dt_datetime);
+	dt_prev_month.setMonth(dt_datetime.getMonth()-1);
+	if (dt_datetime.getMonth()%12 != (dt_prev_month.getMonth()+1)%12) {
+		dt_prev_month.setMonth(dt_datetime.getMonth());
+		dt_prev_month.setDate(0);
+	}
+	var dt_next_month = new Date(dt_datetime);
+	dt_next_month.setMonth(dt_datetime.getMonth()+1);
+	if ((dt_datetime.getMonth() + 1)%12 != dt_next_month.getMonth()%12)
+		dt_next_month.setDate(0);
+	
+
+        var dt_prev_year = new Date(dt_datetime);
+        //dt_prev_year.setYear(dt_datetime.getYear()-1);
+		dt_prev_year.setYear(dt_datetime.getFullYear()-1);
+       
+        var dt_next_year = new Date(dt_datetime);
+        //dt_next_year.setYear(dt_datetime.getYear()+1);
+		dt_next_year.setYear(dt_datetime.getFullYear()+1);
+        
+        var dt_firstday = new Date(dt_datetime);
+        dt_firstday.setDate(1);
+        dt_firstday.setDate(1-(7+dt_firstday.getDay()-n_weekstart)%7);
+        var dt_lastday = new Date(dt_next_month);
+        dt_lastday.setDate(0);
+
+		//{{{ look for stylesheets in parent document
+		style_files = '<link rel="stylesheet" type="text/css" href="'+css_theme_file+'">';
+		style_inline = "<style type=\"text/css\">\n";
+		if (document.getElementsByTagName) {
+			links  = document.getElementsByTagName('link');
+			styles = document.getElementsByTagName('style');
+		} else if (document.all) {
+			links  = document.all.tags('link');
+			styles = document.all.tags('style');
+		} else {
+			//N4 can not retrieve links
+			links  = null;
+			styles = null;
+		}
+		if (links) {
+			nblinks = links.length;
+			for(i = 0 ; i < nblinks ; i++) {
+				if (links[i].rel) {
+					if (links[i].rel.match(/stylesheet/i)) {
+						style_files += '<link rel="stylesheet" type="text/css" href="'+links[i].href+'">';
+					}
+				}
+			}
+		}
+		if (styles) {
+			nbstyles = styles.length
+			for(i = 0 ; i < nbstyles ; i++) {
+				if(styles[i].type.match(/text\/css/i)) {
+					style_inline += styles[i].innerHTML+"\n";
+				}
+			}
+		}
+		style_inline += "\n</style>";
+		//}}}
+		
+        // html generation (feel free to tune it for your particular application)
+        // print calendar header
+      var str_buffer = new String (
+                "<html>\n"+
+                "<head>\n"+
+                "        <title>Calendar</title>\n"+
+                style_files+
+                style_inline+
+                "</head>\n"+
+                "<body>\n"+
+                "<table class=\"clsOTable\" cellspacing=\"0\" border=\"0\" width=\"100%\">\n"+
+                "<tr><td class=\"calendar_month\">\n"+
+                "<table cellspacing=\"1\" cellpadding=\"3\" border=\"0\" width=\"100%\">\n"+
+                "<tr>\n"+
+                "        <td class=\"calendar_month\"><a href=\"javascript:window.opener.show_calendar('"+
+                //str_target+"', '"+dt2dtstr(dt_prev_year)+"'+document.cal.time.value);\">"+
+                str_target+"', '"+dt2dtstr(dt_prev_year)+"','"+css_theme_file+"','"+img_theme_path+"');\">"+
+		"<img src=\""+img_theme_path+"/calendar/prev_year.png\" width=\"16\" height=\"16\" border=\"0\""+
+                " alt=\"previous year\"></a></td>\n"+
+                "        <td class=\"calendar_month\"><a href=\"javascript:window.opener.show_calendar('"+
+                //str_target+"', '"+ dt2dtstr(dt_prev_month)+"'+document.cal.time.value);\">"+
+		str_target+"', '"+ dt2dtstr(dt_prev_month)+"','"+css_theme_file+"','"+img_theme_path+"');\">"+
+                "<img src=\""+img_theme_path+"/calendar/prev.png\" width=\"16\" height=\"16\" border=\"0\""+
+                " alt=\"previous month\"></a></td>\n"+
+
+                "        <td class=\"calendar_month\" colspan=\"3\" align=\"center\">"+
+                "<span class=\"calendar_font_month\">"
+                +arr_months[dt_datetime.getMonth()]+" "+dt_datetime.getFullYear()+"</span></td>\n"+
+
+                "        <td class=\"calendar_month\" align=\"right\"><a href=\"javascript:window.opener.show_calendar('"
+                //+str_target+"', '"+dt2dtstr(dt_next_month)+"'+document.cal.time.value);\">"+
+                +str_target+"', '"+dt2dtstr(dt_next_month)+"','"+css_theme_file+"','"+img_theme_path+"');\">"+
+                "<img src=\""+img_theme_path+"/calendar/next.png\" width=\"16\" height=\"16\" border=\"0\""+
+                " alt=\"next month\"></a></td>\n"+
+                "        <td class=\"calendar_month\" align=\"right\"><a href=\"javascript:window.opener.show_calendar('"
+                //+str_target+"', '"+dt2dtstr(dt_next_year)+"'+document.cal.time.value);\">"+
+		+str_target+"', '"+dt2dtstr(dt_next_year)+"','"+css_theme_file+"','"+img_theme_path+"');\">"+
+                "<img src=\""+img_theme_path+"/calendar/next_year.png\" width=\"16\" height=\"16\" border=\"0\""+
+                " alt=\"next year\"></a></td>\n"+
+                "</tr>\n");
+        var dt_current_day = new Date(dt_firstday);
+        // print weekdays titles
+        str_buffer += "<tr>\n";
+        for (var n=0; n<7; n++)
+                str_buffer += "        <td class=\"calendar_day\">"+
+                "<span  class=\"calendar_font_day\">"+
+                week_days[(n_weekstart+n)%7]+"</span></td>\n";
+        // print calendar table
+        str_buffer += "</tr>\n";
+        while (dt_current_day.getMonth() == dt_datetime.getMonth() ||
+                dt_current_day.getMonth() == dt_firstday.getMonth()) {
+                // print row header
+                str_buffer += "<tr>\n";
+                for (var n_current_wday=0; n_current_wday<7; n_current_wday++) {
+                                if (dt_current_day.getDate() == dt_datetime.getDate() &&
+                                        dt_current_day.getMonth() == dt_datetime.getMonth())
+                                        // print current date
+                                        str_buffer += "        <td class=\"calendar_currentday\" align=\"right\">";
+                                else if (dt_current_day.getDay() == 0 || dt_current_day.getDay() == 6)
+                                        // weekend days
+                                        str_buffer += "        <td class=\"calendar_nextmonth\" align=\"right\">";
+                                else
+                                        // print working days of current month
+                                        str_buffer += "        <td class=\"calendar_daymonth\" align=\"right\">";
+
+                                if (dt_current_day.getMonth() == dt_datetime.getMonth())
+                                        // print days of current month
+                                        str_buffer += "<a href=\"javascript:window.opener."+str_target+
+					    //".value='"+dt2dtstr(dt_current_day)+"'+document.cal.time.value; window.close();\">"+
+                                        ".value='"+dt2dtstr(dt_current_day)+"'; window.close();\">"+
+                                        "<span class=\"calendar_font\">";
+                                else
+                                        // print days of other months
+                                        str_buffer += "<a href=\"javascript:window.opener."+str_target+
+					    //".value='"+dt2dtstr(dt_current_day)+"'+document.cal.time.value; window.close();\">"+
+                                        ".value='"+dt2dtstr(dt_current_day)+"'; window.close();\">"+
+                                        "<span class=\"calendar_font_othermonth\">";
+                                str_buffer += dt_current_day.getDate()+"</span></a></td>\n";
+                                dt_current_day.setDate(dt_current_day.getDate()+1);
+                }
+                // print row footer
+                str_buffer += "</tr>\n";
+        }
+        // print calendar footer
+
+          str_buffer +=
+	         "</table>\n" +
+                  "</tr>\n</td>\n</table>\n" +
+                  "</body>\n" +
+                  "</html>\n";
+
+
+        var vWinCal = window.open("", "Calendar",
+                "width=200,height=187,status=no,resizable=yes,top=200,left=200");
+        vWinCal.opener = self;
+        vWinCal.focus();
+        var calc_doc = vWinCal.document;
+        calc_doc.write (str_buffer);
+        calc_doc.close();
+		
+}
+// datetime parsing and formatting routines. modify them if you wish other datetime format
+function str2dt(str_datetime) {
+   //var re_date = /^(\d+)\-(\d+)\-(\d+)\s+(\d+)\:(\d+)$/;
+   var re_date = /^(\d+)\-(\d+)\-(\d+)\s*$/;
+	if (!re_date.exec(str_datetime))
+		return alert("Invalid Datetime format: "+ str_datetime);
+	return (new Date (RegExp.$1, RegExp.$2-1, RegExp.$3, RegExp.$4, RegExp.$5,0));
+}
+
+function dt2dtstr(dt_datetime) {
+	return (new String (
+			dt_datetime.getFullYear()+"-"+(dt_datetime.getMonth()+1)+"-"+dt_datetime.getDate()+" "));
+}
+
+function dt2tmstr(dt_datetime) {
+	return (new String (
+			dt_datetime.getHours()+":"+dt_datetime.getMinutes()));
+}
+
