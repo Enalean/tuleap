e9f022a4205dff4cdce0e5b1a305a14820a6e04c
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-08-24 15:51:21 +0000

    Add a web page definition for the service tracker.

---
+++ SF/www/soap/tracker/index.php
@@ -0,0 +1,27 @@
+<?php
+require_once ('pre.php');
+require_once ('nusoap/lib/nusoap.php');
+
+define ('permission_denied_fault', '3016');
+
+$uri = 'http://'.$sys_default_domain;
+
+// Instantiate server object
+$server = new soap_server();
+//configureWSDL($serviceName,$namespace = false,$endpoint = false,$style='rpc', $transport = 'http://schemas.xmlsoap.org/soap/http');
+$server->configureWSDL('CodeXAPI',$uri,false,'rpc','http://schemas.xmlsoap.org/soap/http',$uri);
+
+
+//include the common TYPES API
+require_once('../common/types.php');
+
+// include the TRACKER API
+require_once('tracker.php');
+
+
+// Call the service method to initiate the transaction and send the response
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$server->service($HTTP_RAW_POST_DATA);
+
+
+?>
