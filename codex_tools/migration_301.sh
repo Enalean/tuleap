c9b9569a0bbe2b49dc7809e4c99e2a6aad1f6ad3
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-11-24 15:14:46 +0000

    Add a database insertion about new question type in survey manager.

--- codex_tools/migration_301.sh
+++ codex_tools/migration_301.sh
@@ -1,3 +1,7 @@
 ###############################################################################
 # Phpwiki 1.3.12
 ALTER TABLE wiki_page ADD cached_html MEDIUMBLOB;
+
+###############################################################################
+# Survey enhancement: new question type
+INSERT INTO survey_question_types (id, type, rank) VALUES (7,'select_box', '23');
