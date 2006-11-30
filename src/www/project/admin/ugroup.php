7429a639831a0373c6ab8d0d4910d429ffbf3f08
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-11-30 17:14:29 +0000

    Do not display legacy docman ugroups (Tech & Admin) if the service is not used

--- src/www/project/admin/ugroup.php
+++ src/www/project/admin/ugroup.php
@@ -63,13 +63,15 @@ $row_num=0;
 
 $result = db_query("SELECT * FROM ugroup WHERE group_id=100 ORDER BY ugroup_id");
 while ($row = db_fetch_array($result)) {
-    echo '<TR class="'. util_get_alt_row_color($row_num) .'">
-            <TD>'.util_translate_name_ugroup($row['name']).' *</TD>';
-    echo '<TD>'.util_translate_desc_ugroup($row['description']).'</TD>
-<TD align="center">-</TD>
-<TD align="center">-</TD>
-</TR>';
-    $row_num++;
+    if ($project->usesDocman() || ($row['name'] != 'ugroup_document_tech_name_key' && $row['name'] != 'ugroup_document_admin_name_key')) {
+        echo '<TR class="'. util_get_alt_row_color($row_num) .'">
+                <TD>'.util_translate_name_ugroup($row['name']).' *</TD>';
+        echo '<TD>'.util_translate_desc_ugroup($row['description']).'</TD>
+    <TD align="center">-</TD>
+    <TD align="center">-</TD>
+    </TR>';
+        $row_num++;
+    }
 }
 
 
