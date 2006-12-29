9f1a5379743758713f3c7ef2741395baefa70476
mchaari <mchaari@69b26aa9-6588-4999-a7e6-a99cda329b54>
2006-12-29 10:57:37 +0000

    display a threaded view to let user choose the thread(s) to monitor

--- src/www/forum/monitor_thread.php
+++ src/www/forum/monitor_thread.php
@@ -34,6 +34,20 @@ if ( !user_isloggedin()) {
 
 if ($forum_id) {
 
+    // Check permissions
+    if (!forum_utils_access_allowed($forum_id)) {
+        exit_error($Language->getText('global','error'),$Language->getText('forum_forum','forum_restricted'));            
+    }
+
+    //If the forum is associated to a private news, non-allowed users shouldn't be able to save their places in this forum
+    $qry = "SELECT * FROM news_bytes WHERE forum_id='$forum_id'";
+    $res = db_query($qry);
+    if (db_numrows($res) > 0) {
+        if (!forum_utils_news_access($forum_id)) {	    
+	    exit_error($Language->getText('global','error'),$Language->getText('news_admin_index','permission_denied'));
+	}
+    }
+
     $result=db_query("SELECT group_id,forum_name,is_public FROM forum_group_list WHERE group_forum_id='$forum_id'");
     $group_id=db_result($result,0,'group_id');
     $forum_name=db_result($result,0,'forum_name');
@@ -41,6 +55,55 @@ if ($forum_id) {
     $params=array('title'=>group_getname($group_id).' forum: '.$forum_name,
                       'pv'   =>isset($pv)?$pv:false);
     forum_header($params);
+    
+    if ((!isset($offset)) || ($offset < 0)) {
+	$offset=0;
+    } 
+
+    if (!isset($max_rows) || $max_rows < 5) {
+	$max_rows=25;
+    }  
+    
+    $sql="SELECT user.user_name,user.realname,forum.has_followups,user.user_id,forum.msg_id,forum.group_forum_id,forum.subject,forum.thread_id,forum.body,forum.date,forum.is_followup_to, forum_group_list.group_id ".
+	"FROM forum,user,forum_group_list WHERE forum.group_forum_id='$forum_id' AND user.user_id=forum.posted_by AND forum.is_followup_to=0 AND forum_group_list.group_forum_id = forum.group_forum_id ".
+	"ORDER BY forum.date DESC LIMIT $offset,".($max_rows+1);
+
+    $result=db_query($sql);
+    $rows=db_numrows($result);
+
+    if ($rows > $max_rows) {
+	$rows=$max_rows;
+    }
+
+    if (!$result || $rows < 1) {
+	//empty forum
+	$ret_val .= $Language->getText('forum_forum','no_msg',$forum_name) .'<P>'. db_error();
+    } else {
+        $title_arr=array();
+        $title_arr[]=$Language->getText('forum_forum','thread');
+        $title_arr[]=$Language->getText('forum_forum','author');
+	$title_arr[]=$Language->getText('forum_forum','date');
+
+        $ret_val .= html_build_list_table_top ($title_arr);
+    
+        $total_rows=0;
+        $i=0;
+        while (($total_rows < $max_rows) && ($i < $rows)) {
+	    $total_rows++;  
+	    $ret_val .= '
+		        <TR class="'. util_get_alt_row_color($total_rows) .'"><TD><A HREF="/forum/message.php?msg_id='.
+		        db_result($result, $i, 'msg_id').'">'.
+		        '<IMG SRC="'.util_get_image_theme("msg.png").'" BORDER=0 HEIGHT=12 WIDTH=10> ';
+	    $ret_val .= db_result($result, $i, 'subject').'</A></TD>'.
+			'<TD>'.db_result($result, $i, 'user_name').'</TD>'.
+			'<TD>'.format_date($GLOBALS['sys_datefmt'],db_result($result,$i,'date')).'</TD></TR>';	
+  	    $i++;
+        }
+	$ret_val .= '</TABLE>';
+    }	
+    
+    echo $ret_val;
+    
     forum_footer($params);
 
 } else {
