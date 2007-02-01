fcd70e567794344203bc8602579503435106e1d2
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-02-01 17:36:27 +0000

    Add copyright in the Template for unit tests

--- plugins/template/tests/TemplateTest.php
+++ plugins/template/tests/TemplateTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+* Copyright (c) Xerox Corporation, CodeX Team, 2001-2007. All rights reserved
+*
+* $Id$
+*/
 
 class TemplatePluginTest extends UnitTestCase {
     function TemplatePluginTest($name = 'Template Plugin test') {
