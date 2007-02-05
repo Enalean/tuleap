c91808fd79a794cee66526c5ef220119933f4296
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-02-05 10:40:46 +0000

    Replace group_id=XXX by group_id=YYY instead of XXX by YYY during service creation.

--- src/www/include/service.php
+++ src/www/include/service.php
@@ -22,7 +22,7 @@ function service_create_service($arr, $group_id, $template, $force_enable = fals
     } else {
       //for non-system templates
       $link=str_replace($template['name'],group_getunixname($group_id),$link);
-      $link=str_replace($template['id'],$group_id,$link);
+      $link=preg_replace('/group_id='. $template['id'] .'([^\d]|$)/', 'group_id='. $group_id .'$1', $link);
     }
 
     $sql    = "INSERT INTO service (group_id, label, description, short_name, link, is_active, is_used, scope, rank) VALUES ($group_id, '".$arr['label']."', '".$arr['description']."', '".$arr['short_name']."', '".$link."', ".$arr['is_active'].", ". ($force_enable ? 1 : $arr['is_used']) .", '".$arr['scope']."', ".$arr['rank'].")";
