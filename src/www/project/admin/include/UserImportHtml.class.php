db79144ffc0fa4fc13fad8256e2976e35eb5ac0c
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-03-07 10:50:00 +0000

    Integrate ST contrib from branch contrib/st/enhancement/638_import_project_members_list/ (see associated SR #638 for details).
    We add the possibility to import login as well as email addresses.

---
+++ src/www/project/admin/include/UserImportHtml.class.php
@@ -0,0 +1,171 @@
+<?php
+
+/* 
+ * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
+ *
+ * Originally written by Mohamed CHAARI, 2006. STMicroelectronics.
+ *
+ * This file is a part of CodeX.
+ *
+ * CodeX is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CodeX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CodeX; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+require_once('www/project/admin/project_admin_utils.php');
+require_once('common/include/UserImport.class.php');
+
+$Language->loadLanguageMsg('project/project');
+$Language->loadLanguageMsg('include/include');
+
+class UserImportHtml extends UserImport {
+
+    /**
+     * Constructor.
+     *
+     * @return boolean success.
+     */
+    function UserImportHtml($group) {
+        return $this->UserImport($group);
+    }
+
+
+    /**
+     * Show the parse report
+     *
+     *
+     */
+    function displayParse($user_filename) {
+        global $Language, $feedback;
+        
+        if (!file_exists($user_filename) || !is_readable($user_filename)) {
+            $feedback = $Language->getText('project_admin_userimport','missing_file');
+            $this->displayInput();
+            project_admin_footer(array());   
+            exit;
+        }
+        
+        $ok = $this->parse($user_filename,$errors,$parsed_users);
+
+        if (!$ok) {        
+            $this->displayInput();
+            $this->showErrors($errors); 
+        } else {
+            echo '<h2>'.$Language->getText('project_admin_userimport','parse_report').'</h2>';
+            $this->showParseResults($parsed_users);
+        }
+    }
+  
+
+    function showErrors($errors) {
+        global $feedback;
+        
+        $feedback .= $errors;
+    }
+
+
+    /**
+     * create the html output to visualize what has been parsed
+     * @param $users: array containing all the users (User Object) that are in the import file
+     */
+    function showParseResults($parsed_users) {
+        global $PHP_SELF,$Language;
+
+        echo $Language->getText('project_admin_userimport','ready')."<br><br>\n";
+    
+        //Display table containing the list of users to be imported
+        $title_arr = array($Language->getText('project_admin_userimport','username'),$Language->getText('project_admin_userimport','mail_addr'));
+        echo html_build_list_table_top ($title_arr);   
+        for ($i=0;$i<count($parsed_users);$i++) {
+            $current_user = $parsed_users[$i];    
+            echo '<TR class="'.util_get_alt_row_color($i).'">'."\n";
+            echo '<TD>'.$current_user->getName().'</TD>'."\n";
+            echo '<TD>'.$current_user->getEmail().'</TD></TR>'."\n";
+        }
+        echo "</TABLE>\n";
+    
+        // Add 'import'  button to confirm import
+        echo '<FORM NAME="acceptimportdata" action="'.$PHP_SELF.'" method="POST" enctype="multipart/form-data">
+            <p align="left"><INPUT TYPE="SUBMIT" NAME="submit" VALUE="'.$Language->getText('project_admin_userimport','import').'"></p>
+            <INPUT TYPE="HIDDEN" NAME="group_id" VALUE="'.$this->group_id.'">
+            <INPUT TYPE="HIDDEN" NAME="func" VALUE="import">
+            <INPUT TYPE="HIDDEN" NAME="mode" VALUE="import">';
+        
+        foreach ($parsed_users as $current_user) {
+            echo '<INPUT TYPE="HIDDEN" NAME="parsed_users[]" VALUE="'.$current_user->getName().'">';
+        }
+    
+        echo '</FORM><A href="/project/admin/userimport.php?group_id='.$this->group_id.'"> ['.$Language->getText('global','back').']</A>';
+    }
+  
+    /**
+     * Import users that has been accepted from the parse report and update DB.
+     *
+     * @param array $parsed_users array of users. The array has the form of $parsed_users[] -> username
+     */
+    function displayImport($parsed_users) {
+        global $feedback,$Language;
+
+        //use Codex logins to add project members in DB, 
+        //mail addresses are not used because it will fail in case plugin ldap is disabled/unplugged
+        $ok = $this->updateDB($parsed_users);
+        if ($ok) { 
+            $feedback = $Language->getText('project_admin_userimport','success_import')." ";
+        } else { 
+            $feedback = $Language->getText('project_admin_userimport','fail_import')." ";
+        }   
+
+        $this->displayInput();
+    }
+  
+  
+    /**
+     * Display screen showing the allowed input format of the users file
+     *
+     *
+     */
+    function displayShowFormat() {
+        global $Language;
+    
+        $this->displayInput();
+        echo '<hr><h2>'.$Language->getText('project_admin_userimport','format_hdr').'</h2>';
+        echo $Language->getText('project_admin_userimport','import_format',array(user_getemail(user_getid())));
+    }
+  
+  
+    /**
+     * Display screen accepting the user file to be parsed  
+     *     
+     *     
+     */
+    function displayInput() {
+        global $PHP_SELF, $Language;
+    
+        echo '<h2>'.$Language->getText('project_admin_userimport','import_members', array(help_button('UserImport.html'))).'</h2>';
+        echo $Language->getText('project_admin_userimport','import_welcome',array('/project/admin/userimport.php?group_id='.$this->group_id.'&mode=showformat&func=import'));
+
+        echo '<FORM NAME="importdata" action="'.$PHP_SELF.'" method="POST" enctype="multipart/form-data">
+            <INPUT TYPE="hidden" name="group_id" value="'.$this->group_id.'">                
+            <INPUT TYPE="hidden" name="func" value="import">
+            <INPUT TYPE="hidden" name="mode" value="parse"> 
+            <TABLE border="0" width="75%">
+            <TR><B>'.$Language->getText('project_admin_userimport','upload_file').'</B></TR>
+            <TR><TD> <INPUT TYPE="file" name="user_filename" size="50"></TD></TR>
+            </TABLE><P>
+            <INPUT TYPE="submit" name="submit" value="'.$Language->getText('project_admin_userimport','submit').'">
+            </FORM> ';     
+    }
+
+}
+
+?>
