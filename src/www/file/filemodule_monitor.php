1813c9fd851641a5a5121ca97fc96c6791fc718d
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-03-13 10:55:33 +0000

    Refactoring using the new feedback.

--- src/www/file/filemodule_monitor.php
+++ src/www/file/filemodule_monitor.php
@@ -15,57 +15,47 @@ if (user_isloggedin()) {
 		User obviously has to be logged in to monitor
 		a file module
 	*/
-
-	$HTML->header(array('title'=>$Language->getText('file_filemodule_monitor','monitor_a_package')));
-
-	if ($filemodule_id) {
+	if (isset($filemodule_id)) {
 		/*
 			First check to see if they are already monitoring
 			this thread. If they are, say so and quit.
 			If they are NOT, then insert a row into the db
 		*/
-
-		echo '
-			<H2>'.$Language->getText('file_filemodule_monitor','monitor_package').'</H2>';
 		$frsfmf = new FileModuleMonitorFactory();
-
 
 		if (!$frsfmf->isMonitoring($filemodule_id)) {
 			/*
 				User is not already monitoring this filemodule, so 
 				insert a row so monitoring can begin
 			*/
-			
+            
 			$result = $frsfmf->setMonitor($filemodule_id);
 
 			if (!$result) {
-				echo '
-					<span class="highlight">'.$Language->getText('file_filemodule_monitor','insert_err').'</span>';
+				$GLOBALS['Response']->addFeedback('error', $Language->getText('file_filemodule_monitor','insert_err'));
 			} else {
-				echo '
-					<span class="highlight"><H3>'.$Language->getText('file_filemodule_monitor','p_monitored').'</H3></span>
-					<P>
-					'.$Language->getText('file_filemodule_monitor','now_emails').'
-					<P>
-					'.$Language->getText('file_filemodule_monitor','turn_monitor_off');
+			    $GLOBALS['Response']->addFeedback('info', $Language->getText('file_filemodule_monitor','p_monitored'));
+                $GLOBALS['Response']->addFeedback('info', $Language->getText('file_filemodule_monitor','now_emails'));
+                $GLOBALS['Response']->addFeedback('info', $Language->getText('file_filemodule_monitor','turn_monitor_off'));
 			}
 
 		} else {
-
 			$result = $frsfmf->stopMonitor($filemodule_id);
-			echo '
-				<span class="highlight"><H3>'.$Language->getText('file_filemodule_monitor','monitor_turned_off').'</H3></span>
-				<P>
-				'.$Language->getText('file_filemodule_monitor','no_emails');
-
+            $GLOBALS['Response']->addFeedback('info', $Language->getText('file_filemodule_monitor','monitor_turned_off'));
+            $GLOBALS['Response']->addFeedback('info', $Language->getText('file_filemodule_monitor','no_emails'));
 		}
 
 	} else {
-		echo '
-			<H1>'.$Language->getText('file_filemodule_monitor','choose_p').'</H1>';
-	} 
-
-	$HTML->footer(array());
+        $GLOBALS['Response']->addFeedback('error', $Language->getText('file_filemodule_monitor','choose_p'));
+	}
+    
+    // redirect the user to the page she went
+    if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+        $redirection_url = $_SERVER['HTTP_REFERER'];
+    } else {
+        $redirection_url = '../my/';
+    }
+    $GLOBALS['Response']->redirect($redirection_url);
 
 } else {
 	exit_not_logged_in();
