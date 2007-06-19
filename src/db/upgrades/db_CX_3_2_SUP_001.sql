522c30ecb8d1561537ee7eaef06fd742db3deb55
mchaari <mchaari@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-06-19 09:32:55 +0000

    add sql upgrade script

---
+++ src/db/upgrades/db_CX_3_2_SUP_001.sql
@@ -0,0 +1,49 @@
+# 
+# Copyright (c) STMicroelectronics, 2007. All Rights Reserved.
+#
+# Originally written by Mohamed CHAARI, 2007. STMicroelectronics.
+#
+# This file is a part of CodeX.
+#
+# CodeX is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# CodeX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CodeX; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#
+#
+# Database upgrade script 
+#
+# This SQL script allows you to upgrade the CodeX database. In most cases
+# this sql script relates to a well indentified modification in the 
+# CodeX source code. All references are included
+# below.
+#
+# Description
+# Adds a new field 'username_display' in 'user' table, to choose the way users names are displayed:
+# Codex internal login OR LDAP real name.
+#
+#
+# References:
+# no
+#
+# Dependencies:
+# no
+#
+# 
+# SQL script comes next...
+#
+#
+#
+#
+
+ALTER TABLE user ADD username_display int(11) NOT NULL DEFAULT 0 AFTER language_id;
