f3af40e0b29eba9a6c7517b1a211f9fb76f47724
labbenes <labbenes@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-06-25 11:27:15 +0000

    Put the caret inside an inserted Table of contents plugin at options place.

--- src/common/wiki/phpwiki/themes/default/Wikiwyg/Wikiwyg/Wysiwyg.js
+++ src/common/wiki/phpwiki/themes/default/Wikiwyg/Wikiwyg/Wysiwyg.js
@@ -339,7 +339,7 @@ proto.insert_table = function(html) { // See IE
 proto.do_toc = function() {
     var html = '<p><div style="background-color:#D3D3D3;font-size:smaller;">'+
               'Wikitext { <br> &lt;?plugin  CreateToc  ?&gt; <br>}</div></p>';
-    this.insert_html(html);
+    this.insert_html(html, 5);
 }
 
 proto.do_wikitext = function() {
