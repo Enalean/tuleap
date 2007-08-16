44e42e7693f00d34f37395f3683e4a04f2276250
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-08-16 10:17:16 +0000

    (This is a merge from CX_3_2_SUP. See revision #6670 for details)
    User can now create a project based on one of its projects of which he is admin
    
    See ART #860

--- src/common/project/RegisterProjectStep_Template.class.php
+++ src/common/project/RegisterProjectStep_Template.class.php
@@ -39,60 +39,52 @@ class RegisterProjectStep_Template extends RegisterProjectStep {
           <TABLE width="100%">';
         
           for ($i=0; $i<$rows; $i++) {
-            
-                print '
-              <TR>';
-            
-            $group_id = db_result($this->db_templates,$i,'group_id');
-            $check = "";
-            $title = '<B>'.db_result($this->db_templates,$i,'group_name').
-            '</B> (' . date($GLOBALS['sys_datefmt_short'],db_result($this->db_templates,$i,'register_time')) . ')';
-            if ($group_id == '100') {
-              $check = "checked";
-            } else {
-              $title = '<A href="/projects/'.db_result($this->db_templates,$i,'unix_group_name').'" > '.$title.' </A>';
-            }
-        
-            print '
-                <TD><input type="radio" name="built_from_template" value="'.$group_id.'" '.$check.'></TD>
-                <TD>'.$title.'
-                <TD rowspan="2" align="left" valign="top"><I>'.db_result($this->db_templates,$i,'short_description').'</I></TD>
-              </TR>
-        ';
-        
-            // Get Project admin as contacts
-            if ($group_id == '100') {
-              $res_admin = db_query("SELECT user_name AS user_name "
-                      . "FROM user "
-                      . "WHERE user_id='101'");
-            } else {
-            $res_admin = db_query("SELECT user.user_name AS user_name "
-                      . "FROM user,user_group "
-                      . "WHERE user_group.user_id=user.user_id AND user_group.group_id=$group_id AND "
-                      . "user_group.admin_flags = 'A'");
-            }
-            $admins = array();
-            while ($row_admin = db_fetch_array($res_admin)) {
-              $admins[] = '<A href="/users/'.$row_admin['user_name'].'/">'.$row_admin['user_name'].'</A>';
-            }
-            print '
-              <TR>
-                 <TD> &nbsp</TD>
-                 <TD><I>'.$GLOBALS['Language']->getText('new_index','contact').': '.join(',',$admins).'</I></TD>
-              </TR>
-              <TR><TD colspan="3"><HR></TD></TR>';
+                $this->_displayProject(
+                    db_result($this->db_templates,$i,'group_id'),
+                    db_result($this->db_templates,$i,'group_name'),
+                    db_result($this->db_templates,$i,'register_time'),
+                    db_result($this->db_templates,$i,'unix_group_name'),
+                    db_result($this->db_templates,$i,'short_description')
+                );
           }
         
-          print '
-          </TABLE>';
+          print '</TABLE>';
           $GLOBALS['HTML']->box1_bottom();
           //echo '</blockquote>';
           
         }
-        //echo '<h3>Or from other projects</h3><blockquote>';
-        //echo 'Project: <input type="text" size="40" />';
-        //echo '</blockquote>';
-        //echo '<br />';
+        
+        //{{{ Projects where current user is admin
+        $result = db_query("SELECT groups.group_name AS group_name, "
+            . "groups.group_id AS group_id, "
+            . "groups.unix_group_name AS unix_group_name, "
+            . "groups.register_time AS register_time, "
+            . "groups.short_description AS short_description "
+            . "FROM groups, user_group "
+            . "WHERE groups.group_id = user_group.group_id "
+            . "AND user_group.user_id = '". user_getid() ."' "
+            . "AND user_group.admin_flags = 'A' "
+            . "AND groups.status='A' ORDER BY group_name");
+        echo db_error($result);
+        $rows = db_numrows($result);
+        if ($result && $rows) {
+            echo '<br />';
+            $GLOBALS['HTML']->box1_top($GLOBALS['Language']->getText('register_template','choose_admin'));
+            print '<TABLE width="100%">';
+            for ($i=0; $i<$rows; $i++) {
+                $this->_displayProject(
+                    db_result($result,$i,'group_id'),
+                    db_result($result,$i,'group_name'),
+                    db_result($result,$i,'register_time'),
+                    db_result($result,$i,'unix_group_name'),
+                    db_result($result,$i,'short_description')
+                );
+            }
+            print '</TABLE>';
+            $GLOBALS['HTML']->box1_bottom();
+        }
+        //}}}
+        
         echo '</fieldset>';
         
         echo '<fieldset><legend style="font-size:1.2em;">Choose the project type</legend>';
@@ -112,6 +104,47 @@ class RegisterProjectStep_Template extends RegisterProjectStep {
             return false;
         }
         return true;
+    }
+    
+    function _displayProject($group_id, $group_name, $register_time, $unix_group_name, $short_description) {
+        print '<TR>';
+        $check = "";
+        $title = '<B>'. $group_name .
+        '</B> (' . date($GLOBALS['sys_datefmt_short'], $register_time) . ')';
+        if ($group_id == '100') {
+            $check = "checked";
+        } else {
+            $title = '<A href="/projects/'. $unix_group_name .'" > '. $title .' </A>';
+        }
+        
+        print '
+        <TD><input type="radio" name="built_from_template" value="'.$group_id.'" '.$check.'></TD>
+        <TD>'.$title.'</td>
+        <TD rowspan="2" align="left" valign="top"><I>'. $short_description .'</I></TD>
+        </TR>
+        ';
+        
+        // Get Project admin as contacts
+        if ($group_id == '100') {
+            $res_admin = db_query("SELECT user_name AS user_name "
+            . "FROM user "
+            . "WHERE user_id='101'");
+        } else {
+            $res_admin = db_query("SELECT user.user_name AS user_name "
+            . "FROM user,user_group "
+            . "WHERE user_group.user_id=user.user_id AND user_group.group_id=$group_id AND "
+            . "user_group.admin_flags = 'A'");
+        }
+        $admins = array();
+        while ($row_admin = db_fetch_array($res_admin)) {
+            $admins[] = '<A href="/users/'.$row_admin['user_name'].'/">'.$row_admin['user_name'].'</A>';
+        }
+        print '
+        <TR>
+        <TD> &nbsp</TD>
+        <TD><I>'.$GLOBALS['Language']->getText('new_index','contact').': '.join(',',$admins).'</I></TD>
+        </TR>
+        <TR><TD colspan="3"><HR></TD></TR>';
     }
 }
 
