612651fd995704a16464197492b4c935c364a5af
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-08-20 14:20:47 +0000

    reset_db.php doesn't need pre.php

--- codex_tools/plugins/tests/www/reset_db.php
+++ codex_tools/plugins/tests/www/reset_db.php
@@ -1,5 +1,6 @@
 <?php
-require_once('pre.php');
+require(getenv('CODEX_LOCAL_INC')?getenv('CODEX_LOCAL_INC'):'/etc/codex/conf/local.inc');
+require($GLOBALS['db_config_file']);
 echo '<pre>';
 $cmd = 'mysql -h '. $GLOBALS['sys_dbhost'] .' -u '. $GLOBALS['sys_dbuser'] .' -p'. $GLOBALS['sys_dbpasswd'] .' '. $GLOBALS['sys_dbname'] .' < db.sql';
 echo $cmd ."\n";
