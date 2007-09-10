fd8aba2650fb924dcb13b1e6bdc96f12a294765a
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-09-10 13:22:12 +0000

    Fix security flaw in project registration.
    level=normal

--- src/common/project/RegisterProjectStep_Template.class.php
+++ src/common/project/RegisterProjectStep_Template.class.php
@@ -102,6 +102,13 @@ class RegisterProjectStep_Template extends RegisterProjectStep {
         if (!$data['project']['built_from_template']) {
             $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('register_projectname', 'info_missed'));
             return false;
+        } else {
+            $p =& group_get_object($data['project']['built_from_template']);
+            var_dump($data['project']['built_from_template'], user_ismember($data['project']['built_from_template'],'A'));
+            if (!$p->isTemplate() && !user_ismember($data['project']['built_from_template'],'A')) {
+                $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('global', 'perm_denied'));
+                return false;
+            }
         }
         return true;
     }
