f7d5287360bef126226ec77981f9c45cd329fe16
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-09-26 15:22:00 +0000

    typo

--- src/www/export/rss_sfnews.php
+++ src/www/export/rss_sfnews.php
@@ -4,7 +4,7 @@
 require_once('pre.php');
 require_once('www/news/news_utils.php');
 require_once('common/rss/RSS.class.php');
-require('./rss_utils.inc');
+require('rss_utils.inc');
 $Language->loadLanguageMsg('export/export');
 
 //First, check for valid group_id
