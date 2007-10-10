53572e32bc3cd8741994c6cd84e5f444d2b98b55
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-10-10 14:55:18 +0000

    Fix SR #971 - User password change doesn't work with $sys_allow_anon = 0; On session_login_valid, if the password expired no session are created but the user is redirected on /change_pw.php Unfortunatly this is not allowed by pre.php that redirect on login.
    level=normal

--- src/www/include/pre.php
+++ src/www/include/pre.php
@@ -256,6 +256,7 @@ if ($SERVER_NAME != 'localhost' &&
     $SCRIPT_NAME != '/current_css.php'  && 
     $SCRIPT_NAME != '/account/login.php'  && 
     $SCRIPT_NAME != '/account/register.php'&& 
+    $SCRIPT_NAME != '/account/change_pw.php'&& 
     $SCRIPT_NAME != '/include/check_pw.php'&& 
     $SCRIPT_NAME != '/account/lostpw.php' &&
     $SCRIPT_NAME != '/account/lostlogin.php' &&
