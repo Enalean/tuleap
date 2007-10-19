96fe5047ba3b2a3f59886b13087969773df71ae4
manuel_vacelet <manuel_vacelet@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-10-19 15:59:44 +0000

    Update generated phpdoc (much usable template).

--- src/utils/generate_phpdoc.sh
+++ src/utils/generate_phpdoc.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+set +x
+
 # Default tools
 CURRENTDIR=`pwd`
 GREP="/bin/grep"
+EGREP="/bin/egrep"
 SED="/bin/sed"
 
 if [ -z "$CODEX_LOCAL_INC" ]; then
@@ -35,16 +38,57 @@ programmer_guide_dir=${BASEDOCDIR}/programmer_guide
 
 phpdoc_output=${programmer_guide_dir}/phpdoc
 
+if [ "${PREPARE_SVN}" == "1" ]; then
+    cd ${phpdoc_output}
+    svn up -q
+    find -type d -not -path '*.svn*' | while read d; do
+	(cd $d; rm -f *.html *.css)
+    done
+    cd ${CURRENTDIR}
+fi
+
 ${BASESRCDIR}/src/utils/php-launcher -d output_buffering=1 ${PHPDOC} \
     --quiet on \
     --defaultpackagename "CodeX" \
     --title "CodeX Framework Documentation" \
+    --output "HTML:Smarty:HandS" \
     --target ${phpdoc_output} \
     --examplesdir ${programmer_guide_dir}/examples \
     --directory ${BASESRCDIR}/src/common/filter \
     --filename ${BASESRCDIR}/src/common/include/HTTPRequest.class.php,${BASESRCDIR}/src/common/include/CodeX_Request.class.php,${BASESRCDIR}/src/common/include/SOAPRequest.class.php
 
+if [ "${PREPARE_SVN}" == "1" ]; then
+    cd ${phpdoc_output}
+    svn st | cut -d' ' -f1,7 | while read status file; do
+	case "$status"
+	    in
+	    '!')
+		svn rm $file
+		;;
+	    '?')
+		svn add $file
+		if $(echo $file | ${EGREP} '\.html$' 2>&1 >/dev/null); then
+		    svn propset --quiet svn:mime-type 'text/html' $file
+		else
+		    if $(echo $file | ${EGREP} '\.css$' 2>&1 >/dev/null); then
+			svn propset --quiet svn:mime-type 'text/css' $file
+		    else
+			if $(echo $file | ${EGREP} '\.png$' 2>&1 >/dev/null); then
+			    svn propset --quiet svn:mime-type 'image/png' $file
+			else
+			    echo "No mime type for $file"
+			fi
+		    fi
+		fi
+		;;
+	esac
+    done
+    cd ${CURRENTDIR}
+fi
+
 ### Set good mime-type.
 #
 #find ${phpdoc_output} -name "*.html" -exec svn propset --quiet svn:mime-type text/html {} \;
 #find ${phpdoc_output} -name "*.css" -exec svn propset --quiet  svn:mime-type text/css {} \;
+
+
