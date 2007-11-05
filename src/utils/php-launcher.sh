8b5621c1c53a2de0bc037bc5e152b092b79a4aff
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-11-05 17:00:12 +0000

    Added ".sh" extension to php-launcher for better readability/uniformity.

---
+++ src/utils/php-launcher.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (c) STMicroelectronics, 2005. All Rights Reserved.
+#
+# Originally written by Manuel Vacelet, 2005
+#
+# This file is a part of CodeX.
+#
+# CodeX is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# CodeX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CodeX; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+set -e
+
+# PHP path and parameters
+PHP="/usr/bin/php"
+# Include path is only defined in php.conf (and not php.ini)
+PHP_PARAMS="-q -d include_path=/usr/share/codex/src/www/include:/usr/share/codex/src:."
+
+# Common functions
+error() {
+    echo "Error $@"
+    exit 1
+}
+
+# Check if CODEX_LOCAL_INC variable exists in the environement
+# or set a default value
+if [ ! -f "${CODEX_LOCAL_INC}" ]; then
+    CODEX_LOCAL_INC="/etc/codex/conf/local.inc"
+fi
+if [ ! -f "${CODEX_LOCAL_INC}" ]; then
+    error "No valid CODEX_LOCAL_INC found. Please update your environnement."
+fi
+export CODEX_LOCAL_INC
+
+# Finaly runs php interpretor
+exec "${PHP}" ${PHP_PARAMS} $@
