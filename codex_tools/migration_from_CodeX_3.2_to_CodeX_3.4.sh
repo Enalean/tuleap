5612b29dd6694bd4f2afd1f79155baed70d033b8
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-11-09 10:59:03 +0000

    Add docman migration

--- codex_tools/migration_from_CodeX_3.2_to_CodeX_3.4.sh
+++ codex_tools/migration_from_CodeX_3.2_to_CodeX_3.4.sh
@@ -532,19 +532,127 @@ CREATE TABLE plugin_docman_report_filter (
 alter table plugin_docman_metadata_value add FULLTEXT fltxt (valueText, valueString);
 alter table plugin_docman_metadata_value add FULLTEXT fltxt_txt (valueText);
 alter table plugin_docman_metadata_value add FULLTEXT fltxt_str (valueString);
+ALTER TABLE plugin_docman_metadata_value DROP INDEX idx_field_id;
+ALTER TABLE plugin_docman_metadata_value DROP INDEX idx_artifact_id;
+ALTER TABLE plugin_docman_metadata_value ADD INDEX idx_field_item_id (field_id, item_id);
 
 alter table plugin_docman_item add FULLTEXT fltxt_title (title);
 alter table plugin_docman_item add FULLTEXT fltxt_description (description);
 alter table plugin_docman_item add FULLTEXT fltxt (title, description);
+ALTER TABLE plugin_docman_item add INDEX idx_group_id (group_id);
+ALTER TABLE plugin_docman_item ADD INDEX parent_id (parent_id);
+ALTER TABLE plugin_docman_item ADD INDEX rank (rank);
 
 alter table plugin_docman_version add FULLTEXT fltxt (label, changelog, filename);
+ALTER TABLE plugin_docman_version ADD INDEX idx_item_id (item_id);
 
 update plugin_docman_item set status = status + 100;
 alter table plugin_docman_item change column status status TINYINT(4) DEFAULT 100 NOT NULL;
 
 ALTER TABLE plugin_docman_metadata DROP PRIMARY KEY, ADD PRIMARY KEY  (field_id);
+ALTER TABLE plugin_docman_metadata DROP INDEX idx_name;
+ALTER TABLE plugin_docman_metadata ADD INDEX idx_name (name (10));
+
+ALTER TABLE plugin_docman_metadata_love DROP INDEX idx_fv_value_id;
+ALTER TABLE plugin_docman_metadata_love ADD INDEX rank (rank);
+ALTER TABLE plugin_docman_metadata_love ADD INDEX name (name (10));
 EOF
 
+/usr/share/codex/src/utils/php-launcher.sh /usr/share/codex/plugins/docman/db/upgrade_v2.6.php
+
+# Upgrade templates projects with a default docman if none are existing.
+$PERL <<'EOF'
+use DBI;
+
+$root_path = "../../../src/";
+require $root_path."utils/include.pl";
+
+&db_connect;
+
+sub create_item {
+    my ($group_id) = @_;
+
+    $qry = "INSERT INTO plugin_docman_item (parent_id, group_id, title, description, create_date, update_date, delete_date, user_id, status, obsolescence_date, rank, item_type, link_url, wiki_page, file_is_embedded) VALUES (0, $group_id, 'roottitle_lbl_key', '', UNIX_TIMESTAMP(NOW()), UNIX_TIMESTAMP(NOW()), NULL, 101, 0, 0, 0, 1, NULL, NULL, NULL)";
+
+    $create_item = $dbh->prepare($qry);
+    $create_item->execute();
+}
+
+sub create_permission {
+    my ($group_id, $perm, $ugroup_id) = @_;
+
+    $qry_insert_perms = "INSERT INTO permissions(permission_type, ugroup_id, object_id)".
+	" SELECT '$perm', $ugroup_id, item_id".
+	" FROM plugin_docman_item".
+	" WHERE group_id = $group_id";
+    
+    $insert_perms = $dbh->prepare($qry_insert_perms);
+    $insert_perms->execute();
+}
+
+sub create_all_perms {
+    my ($group_id) = @_;
+
+    create_permission($group_id, "PLUGIN_DOCMAN_READ", 2);
+    create_permission($group_id, "PLUGIN_DOCMAN_WRITE", 3);
+    create_permission($group_id, "PLUGIN_DOCMAN_MANAGE", 4);
+
+    $qry_admin_perms = "INSERT INTO permissions(permission_type, ugroup_id, object_id)".
+	"VALUES ('PLUGIN_DOCMAN_ADMIN', 4, $group_id)";
+    $admin_perms = $dbh->prepare($qry_admin_perms);
+    $admin_perms->execute();
+}
+
+
+sub create_settings {
+    my ($group_id) = @_;
+
+    $qry_settings = "INSERT INTO  plugin_docman_project_settings (group_id, view, use_obsolescence_date, use_status)".
+	"VALUES ($group_id, 'Tree', 0, 0)";
+    $settings = $dbh->prepare($qry_settings);
+    $settings->execute();
+}
+
+sub create_docman {
+    my ($group_id) = @_;
+
+    create_item($group_id);
+    create_all_perms($group_id);
+    create_settings($group_id)
+}
+
+sub docman_exist {
+    my ($group_id) = @_;
+
+    $qry = "SELECT count(*) as nb FROM plugin_docman_item WHERE group_id = $group_id AND delete_date IS NULL";
+    $c2 = $dbh->prepare($qry);
+    $c2->execute();
+    my ($nb) = $c2->fetchrow();
+    if($nb gt 0) {
+	return 1;
+    }
+    else {
+	return 0;
+    }
+}
+
+sub create_docman_for_template_projects {
+    $qry = "SELECT group_id FROM groups WHERE type='2' and status IN ('A','s')";
+    $c = $dbh->prepare($qry);
+    $c->execute();
+    while (my ($group_id) = $c->fetchrow()) {
+	if(docman_exist($group_id)) {
+	    print "Docman exist for group $group_id\n";
+	}
+	else {
+	    print "Create docman for group $group_id\n";
+	    create_docman($group_id);
+	}
+    }
+}
+
+create_docman_for_template_projects;
+EOF
 
 ###############################################################################
 # Personalizeable layout
