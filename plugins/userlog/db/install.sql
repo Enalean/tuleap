8b7947b8a51731db0ec83114e17a3cdbae802bdd
manuel_vacelet <manuel_vacelet@69b26aa9-6588-4999-a7e6-a99cda329b54>
2007-11-29 08:42:36 +0000

    Add new plugin for an enforced user activity logging.

---
+++ plugins/userlog/db/install.sql
@@ -0,0 +1,3 @@
+##
+## Sql Install Script
+##
---
+++ plugins/userlog/tests/UserLogTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Copyright (c) Xerox Corporation, CodeX Team, 2001-2007. All rights reserved
+*
+* 
+*/
+
+class TemplatePluginTest extends UnitTestCase {
+    function TemplatePluginTest($name = 'Template Plugin test') {
+        $this->UnitTestCase($name);
+    }
+    
+    function testSomething() {
+        $this->pass();
+    }
+}
+?>
