7a2ba3809cd1ba8934fecd50e5426f7ca41e3ba8
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-01-09 14:13:21 +0000

    Artificial increase of script numbers. Just to be sure that next scripts will start @ 004 and that removal of serverupdate widget has been done.
    level=normal
    update=db

---
+++ src/updates/scripts/003_remove_server_update_widget.class.php
@@ -0,0 +1,22 @@
+<?php
+//
+// Copyright (c) Xerox Corporation, CodeX Team, 2001-2008. All rights reserved
+//
+// 
+
+
+require_once('CodeXUpgrade.class.php');
+
+class Update_003 extends CodeXUpgrade {
+
+    function _process() {
+        echo $this->getLineSeparator();
+        db_query("DELETE FROM layouts_contents WHERE name = 'myserverupdate'");
+        if(db_error()) {
+            $this->addUpgradeError(db_error());
+        }
+        echo $this->getLineSeparator();
+    }
+
+}
+?>
