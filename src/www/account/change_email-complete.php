d06f102eb04b5053fda0792efbbf615ac65cb459
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-01-28 09:19:31 +0000

    (This is a merge from CX_3_4_SUP support branch, see rev #7584)
    Fix PHP notice.

--- src/www/account/change_email-complete.php
+++ src/www/account/change_email-complete.php
@@ -40,7 +40,7 @@ $HTML->header(array('title'=>$Language->getText('account_change_email-complete',
 ?>
 <p><b><?php echo $Language->getText('account_change_email-complete', 'title'); ?></b>
 <P><?php echo $Language->getText('account_change_email-complete', 'message',
-			     array( $row_user['realname'], $row_user[email_new],
+			     array( $row_user['realname'], $row_user['email_new'],
 				    $GLOBALS['sys_name'], $row_user['user_name'])); ?>
 
 <P><A href="/">[ <?php echo $Language->getText('global', 'back_home'); ?> ]</A>
