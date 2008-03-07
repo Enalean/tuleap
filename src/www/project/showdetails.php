d52569bcfb26989dbc84036ca84810a0251def77
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-03-07 09:23:47 +0000

    handle properly html and entities for project name and description

--- src/www/project/showdetails.php
+++ src/www/project/showdetails.php
@@ -32,7 +32,7 @@ if (!$result || db_numrows($result) < 1) {
 	echo db_error();
 	exit_error($Language->getText('project_showdetails','proj_not_found'),$Language->getText('project_showdetails','no_detail'));
 }
-
+$hp = CodeX_HTMLPurifier::instance();
 	$register_purpose = db_result($result,0,'register_purpose');
 	$patents_ips = db_result($result,0,'patents_ips');
 	$required_software = db_result($result,0,'required_software');
@@ -42,26 +42,26 @@ if (!$result || db_numrows($result) < 1) {
 
 <P>
 <b><u><?php echo $Language->getText('project_admin_editgroupinfo','long_desc'); ?></u></b>
-<P><?php echo ($register_purpose == '') ? $Language->getText('global','none').'.' : util_make_links( nl2br ( $register_purpose)) ; ?>
+<P><?php echo ($register_purpose == '') ? $Language->getText('global','none').'.' :  $hp->purify(util_unconvert_htmlspecialchars($register_purpose), CODEX_PURIFIER_LIGHT, $group_id)  ; ?>
 
 <P>
 <b><u><?php echo $Language->getText('project_admin_editgroupinfo','patents'); ?></u></b>
-<P><?php echo ($patents_ips == '') ? $Language->getText('global','none').'.' : util_make_links( nl2br ($patents_ips)) ; ?>
+<P><?php echo ($patents_ips == '') ? $Language->getText('global','none').'.' :  $hp->purify(util_unconvert_htmlspecialchars($patents_ips), CODEX_PURIFIER_BASIC, $group_id)  ; ?>
 
 <P>
 <b><u><?php echo $Language->getText('project_admin_editgroupinfo','soft_required'); ?></u></b>
-<P><?php echo ($required_software == '') ? $Language->getText('global','none').'.' : util_make_links( nl2br ($required_software)) ; ?>
+<P><?php echo ($required_software == '') ? $Language->getText('global','none').'.' :  $hp->purify(util_unconvert_htmlspecialchars($required_software), CODEX_PURIFIER_BASIC, $group_id)  ; ?>
 
 <P>
 <b><u><?php echo $Language->getText('project_admin_editgroupinfo','comments'); ?></u></b>
-<P><?php echo ($other_comments == '') ? $Language->getText('global','none').'.' : util_make_links( nl2br ($other_comments)) ; ?>
+<P><?php echo ($other_comments == '') ? $Language->getText('global','none').'.' :  $hp->purify(util_unconvert_htmlspecialchars($other_comments), CODEX_PURIFIER_BASIC, $group_id)  ; ?>
 
 <?php
 
 if ($license_other != '') {
 	print '<P>';
 	print '<b><u>'.$Language->getText('project_admin_editgroupinfo','license_comment').'</u></b>';
-	print '<P>'.util_make_links( nl2br ($license_other));
+	print '<P>'.$hp->purify(util_unconvert_htmlspecialchars($license_other), CODEX_PURIFIER_BASIC, $group_id);
 }
 
 print '<P><a href="/project/?group_id='.$group_id .'"> '.$Language->getText('project_showdetails','back_main').' </a>';
