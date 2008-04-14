d8816a2a7a62e9c9cd5fa75b6eac712cf6b50334
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-04-14 12:30:25 +0000

    Default charset for supported languages (en_US & fr_FR) is utf8

--- src/db/mysql/database_initvalues.sql
+++ src/db/mysql/database_initvalues.sql
@@ -2354,9 +2354,9 @@ INSERT INTO permissions (permission_type,object_id,ugroup_id) VALUES ('TRACKER_F
 --- Create the list of supported languages for that site
 
 INSERT INTO supported_languages VALUES \
-(1,'English','English_US.tab','en_US','ISO-8859-1',1);
+(1,'English','English_US.tab','en_US','UTF-8',1);
 INSERT INTO supported_languages VALUES \
-(2,'Français','French_FR.tab','fr_FR','ISO-8859-1',1);
+(2,'Français','French_FR.tab','fr_FR','UTF-8',1);
 
 
 -- install and enable pluginsadministration
