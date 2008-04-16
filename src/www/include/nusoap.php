d99929e2bd3c92a5726de20279da9284ab78e0a2
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-04-16 12:59:10 +0000

    default encoding in nusoap is now UTF-8

--- src/www/include/nusoap.php
+++ src/www/include/nusoap.php
@@ -128,8 +128,7 @@ class nusoap_base {
 	* @var      string
 	* @access   public
 	*/
-    var $soap_defencoding = 'ISO-8859-1';
-	//var $soap_defencoding = 'UTF-8';
+	var $soap_defencoding = 'UTF-8';
 
 	/**
 	* namespaces in an array of prefix => uri
