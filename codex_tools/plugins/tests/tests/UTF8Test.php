76034da941771ee804c953b85bbb61a1c363b040
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-04-22 10:22:55 +0000

    Add tool to detect file encoding of xml/html files (file -bi cannot be used for those files)

--- codex_tools/plugins/tests/tests/UTF8Test.php
+++ codex_tools/plugins/tests/tests/UTF8Test.php
@@ -33,5 +33,29 @@ class UTF8Test extends UnitTestCase {
             $this->pass();
         }
     }
+    
+    function testHtmlEncoding() {
+        //file -i does not work well on text/xml files
+        $this->_parseHtmlFiles($GLOBALS['codex_dir'].'/documentation/user_guide');
+    }
+    
+    private function _parseHtmlFiles($file) {
+        if (is_dir($file) && !in_array(basename($file), array('.', '..', '.svn'))) {
+            foreach(glob($file .'/*') as $f) {
+                $this->_parseHtmlFiles($f);
+            }
+        } else if (preg_match('/\.(xsl|xml|html)$/i', basename($file))) {
+            $cmd = 'java -classpath '. dirname(__FILE__) .'/chardet.jar org.mozilla.intl.chardet.HtmlCharsetDetector file://'. $file;
+            $result = `$cmd`;
+            
+            //ascii files are allowed
+            //utf-8 files are allowed
+            if ((strstr($result, 'CHARSET = ASCII') === FALSE) && 
+                (strstr($result, 'Probable Charset = UTF-8') === FALSE)
+            ) {
+                $this->fail('The file [ '. $file . ' ] has '. implode(', ', explode("\n", $result)));
+            }
+        }
+    }
 }
 ?>
