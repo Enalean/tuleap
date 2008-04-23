a97f0a886ee09bd29f9c745a0c21e9493a9a82de
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-04-23 09:52:39 +0000

    display the time taken by the tests

--- codex_tools/plugins/tests/www/CodeXReporter.class.php
+++ codex_tools/plugins/tests/www/CodeXReporter.class.php
@@ -4,26 +4,27 @@ require_once('../include/simpletest/reporter.php');
 
 
 class CodeXHtmlReporter extends HtmlReporter {
+    protected $_timer;
     function paintHeader($test_name) {
         print "<h1>$test_name</h1>\n";
+        $this->_timer = microtime(true);
         flush();
     }
     function paintFooter($test_name) {
-        $colour = ($this->getFailCount() + $this->getExceptionCount() > 0 ? "red" : "green");
-        print "<div style=\"";
-        print "padding: 8px; margin-top: 1em; background-color: $colour; color: white;";
-        print "\">";
-        print $this->getTestCaseProgress() . "/" . $this->getTestCaseCount();
-        print " test cases complete:\n";
-        print "<strong>" . $this->getPassCount() . "</strong> passes, ";
-        print "<strong>" . $this->getFailCount() . "</strong> fails and ";
-        print "<strong>" . $this->getExceptionCount() . "</strong> exceptions.";
-        print "</div>\n";
+        $duration = microtime(true) - $this->_timer;
+        $micro = round($duration - floor($duration), 2);
+        $seconds = floor($duration);
+        $minutes = floor($seconds / 60);
+        $seconds = $seconds % 60;
+        $d = $minutes ? $minutes .' minute' .($minutes > 1 ? 's ' : ' ') : '';
+        $d .= ($seconds + $micro) .' seconds';
+        echo '<div style="border:1px solid orange; background: lightyellow; color:orange">Time taken: '. $d .'</div>'; 
+        parent::paintFooter($test_name);
     }
     function paintPass($message) {
         parent::paintPass($message);
         if (isset($_REQUEST['show_pass'])) {
-            print "<span class=\"pass\">Pass</span>: ";
+            print '<span class="pass">Pass</span>: ';
             $breadcrumb = $this->getTestList();
             array_shift($breadcrumb);
             print implode(" -&gt; ", $breadcrumb);
