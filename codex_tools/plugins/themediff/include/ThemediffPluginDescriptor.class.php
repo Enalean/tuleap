9c40b0bd9de1b91799083f7187095de51d564299
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-04-29 14:49:43 +0000

    This plugin makes a diff between different themes. Usefull to know if an image is missing and where.

---
+++ codex_tools/plugins/themediff/include/ThemediffPluginDescriptor.class.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('common/plugin/PluginDescriptor.class.php');
+
+$GLOBALS['Language']->loadLanguageMsg('Themediff', 'Themediff');
+
+class ThemediffPluginDescriptor extends PluginDescriptor {
+    
+    function ThemediffPluginDescriptor() {
+        $this->PluginDescriptor($GLOBALS['Language']->getText('plugin_themediff', 'descriptor_name'), 'v1.0', $GLOBALS['Language']->getText('plugin_themediff', 'descriptor_description'));
+    }
+}
+?>
