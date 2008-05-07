7ab03d1c6f6279e4c314fdd770c1fa42dee21069
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-05-07 15:52:06 +0000

    add [Add Group] link on the top of the group main page

--- src/common/admin/view/group/GroupSearchDisplay.class.php
+++ src/common/admin/view/group/GroupSearchDisplay.class.php
@@ -138,6 +138,9 @@ class GroupSearchDisplay extends AdminSearchDisplay {
         session_require(array('group'=>'1','admin_flags'=>'A'));
        
         $GLOBALS['HTML']->header(array('title'=>$GLOBALS['Language']->getText('admin_grouplist','title')));
+
+        print "<br><a href=\"groupedit-add.php\">[".$GLOBALS['Language']->getText('admin_grouplist','add_group')."]</a>";
+
               
         parent::displayHeader($GLOBALS['Language']->getText('admin_grouplist','for_categ').' <b>'.$GLOBALS['Language']->getText('admin_grouplist','all_categ').'</b>');
     }
