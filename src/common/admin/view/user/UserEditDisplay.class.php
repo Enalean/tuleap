046e1f19fa5973298276aa4d475526bddc54b5ca
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-05-14 12:40:55 +0000

    develop the Unix Acount Information display

--- src/common/admin/view/user/UserEditDisplay.class.php
+++ src/common/admin/view/user/UserEditDisplay.class.php
@@ -47,7 +47,59 @@ class UserEditDisplay extends AdminEditDisplay {
         session_require(array('group'=>'1','admin_flas'=>'A'));
         
         $GLOBALS['HTML']->header(array('title'=>$GLOBALS['Language']->getText('admin_usergroup','title')));
-        parent::displayHeader($GLOBALS['Language']->getText('admin_usergroup','header').': <b>');
+        parent::displayHeader($GLOBALS['Language']->getText('admin_usergroup','header').': ');
+    }
+
+
+    /**
+     * displayUnixAccountInformation()
+     *
+     */
+    function displayUnixAccountInformation() {
+
+        print $GLOBALS['Language']->getText('admin_usergroup','account_info');
+
+        print '<form method="post" name="update_user" action="index.php">';
+        
+        print '<p>Shell:';
+
+        print '<select name="form_shell">';
+        print '<option value="/bin/sh">/bin/sh</option>';
+        print '<option selected="selected" value="/bin/bash">/bin/bash</option>';
+        print '<option value="/sbin/nologin">/sbin/nologin</option>';
+        print '<option value="/bin/bash2">/bin/bash2</option>';
+        print '<option value="/bin/ash">/bin/ash</option>';
+        print '<option value="/bin/bsh">/bin/bsh</option>';
+        print '<option value="/bin/ksh">/bin/ksh</option>';
+        print '<option value="/bin/tcsh">/bin/tcsh</option>';
+        print '<option value="/bin/csh">/bin/csh</option>';
+        print '<option value="/bin/zsh">/bin/zsh</option>';
+        print '</select>';
+
+        print '</p><p>Unix Account Status:';
+        print '<select name="form_unixstatus">';
+        print '<option value="N">No Unix Account</option>';
+        print '<option selected="selected" value="A">Active</option>';
+        print '<option value="S">Suspended</option>';
+        print '<option value="D">Deleted</option>';
+        print '</select>';
+
+        print '</p><p>Email:';
+        print '<input name="email" value="codex-admin@_DOMAIN_NAME_" size="35" maxlength="55" type="text">';
+
+        print '</p><p>Expiry Date:';
+
+        print '<input id="expiry_date" name="expiry_date" value="" size="15" maxlength="10" type="text">';
+        print '<a href="javascript:show_calendar(\'document.update_user.expiry_date\', $(\'expiry_date\').value,\'/themes/CodeXTab/css/CodeXTab_normal.css\',\'/themes/CodeXTab/images/\');"><img src="/themes/CodeXTab/images/calendar/cal.png" alt="Click Here to Pick up a date " border="0" height="16" width="16"></a>';
+
+        print '</p><p>';
+
+        print '<input name="Update_Unix" value="Update" type="submit">';
+
+        print '</p><hr>';
+
+
+
     }
 
 
@@ -66,6 +118,8 @@ class UserEditDisplay extends AdminEditDisplay {
     function display() {
 
         $this->displayHeader();
+
+        $this->displayUnixAccountInformation();
 
         $this->displayFooter();
     }
