ca84b54ea28c3263ee8ec7820d03cfae3f7bea44
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-05-30 08:49:44 +0000

    Improve Docman uploader/downloader. This is a merge of the contribution at SR #1407 (also merged in 3.4_SUP, rev #8669, and 3.4_SECURITY, rev #8670)

--- src/utils/DocmanLegacyDownloader.pl
+++ src/utils/DocmanLegacyDownloader.pl
@@ -20,10 +20,22 @@
 #######################################################################
 
 use strict;
+use warnings;
 use DBI;
 use Term::ReadKey;
 
-my $_codex_server="codex.xerox.com";
+# See counter part in php (src/www/utils.php)
+sub util_unconvert_htmlspecialchars {
+    my ($data) = @_;
+    $data =~ s/&nbsp;/ /gi;
+    $data =~ s/&quot;/"/gi;
+    $data =~ s/&gt;/>/gi;
+    $data =~ s/&lt;/</gi;
+    $data =~ s/&amp;/&/gi;
+    return $data;
+}
+
+my $_codex_server="codex-test.cro.st.com";
 my $_codex_db="codex";
 
 print "Database User : ";
@@ -44,7 +56,7 @@ my $source ="dbi:mysql:dbname=$_codex_db;host=$_codex_server";
 my $base = DBI->connect($source,$user,$passwd) or die DBI::errstr;
 
 print "Creating directory: project_documentation \n";
-`mkdir project_documentation`;
+mkdir "project_documentation", 0755;
 chdir ('project_documentation');
 
 my $sql_doc_groups = "select * from doc_groups where group_id = $group_id";
@@ -56,20 +68,39 @@ $req_doc_groups->execute() or die($base->errstr());
 while (my @z =$req_doc_groups->fetchrow_array())
 {
     print " ---- Creating directory :$z[1]  \n";
-    `mkdir $z[1]`;
-    chdir ($z[1]);
-    my $sql = "select data,filename from doc_data where doc_group=$z[0]";
+    mkdir "$z[1]", 0755;
+    chdir ("$z[1]");
+    my $sql = "select data,filename,title,filetype from doc_data where doc_group=$z[0]";
     my $req=$base->prepare($sql) or die($base->errstr());
     $req->execute() or die($base->errstr());
-    while (my @t =$req->fetchrow_array())
+    while (my ($data, $filename, $title, $filetype) = $req->fetchrow_array())
     {
-        print " -------- Creating file : $t[1] \n";
-        open(FILE,">$t[1]");
+	my $fname;
+	if($filename ne "") {
+	    $fname = $filename;
+	} else {
+	    # No filename, we are probably in a context of copied/pasted file.
+	    $fname = $title;
+	    if($filetype eq 'text/html') {
+		$fname .= ".html";
+		# Need to unconver html special char (stored encoded in DB)
+		$data = util_unconvert_htmlspecialchars($data);
+	    }
+	    elsif($filetype eq 'text/plain') {
+		$fname .= ".txt";
+		# Need to unconver html special char (stored encoded in DB)
+		$data = util_unconvert_htmlspecialchars($data);
+	    }
+	    else {
+		print "Unknown file type: ".$filetype."\n";
+	    }
+	}
+        print " -------- Creating file : ".$fname." \n";
+        open(FILE,">$fname");
         binmode(FILE);
-        print FILE $t[0];
+        print FILE $data;
         close(FILE);
     }
     chdir ('../');
-
 }
 
