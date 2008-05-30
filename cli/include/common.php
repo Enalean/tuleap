9e27b908f53a81649ee78fddd17b142edfa3e91c
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-05-30 08:52:51 +0000

    Follow-up to rev #8637 : $msg is not always an object.

--- cli/include/common.php
+++ cli/include/common.php
@@ -17,7 +17,11 @@
  * exit_error - Exits the program displaying an error and returning an error code
  */
 function exit_error($msg, $errcode=1) {
-    echo "Fatal error: [".$msg->faultcode."] ".$msg->faultstring."\n";
+    if (is_string($msg)) {
+        echo "Fatal error: ".$msg."\n";
+    } elseif (is_object($msg)) {
+        echo "Fatal error: [".$msg->faultcode."] ".$msg->faultstring."\n";
+    }
     exit (intval($errcode));
 }
 
