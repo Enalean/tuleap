824f4f50584ee60a58172a1e6223d273cc6665f1
Chris Han <xiphux@gmail.com>
2008-06-01 21:37:23 -0400

    No need for ob_start to be this high

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
- ob_start();
 /*
  *  index.php
  *  gitphp: A PHP git repository browser
@@ -21,6 +20,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+ ob_start();
 
  /*
   * Version
