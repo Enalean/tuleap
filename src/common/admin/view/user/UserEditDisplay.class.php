b38401d52cf8896fe48816795fe4e7c30d3dfbb4
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-06-02 14:44:13 +0000

    refactor shell list in EditUserDisplay()

--- src/common/admin/view/user/UserEditDisplay.class.php
+++ src/common/admin/view/user/UserEditDisplay.class.php
@@ -76,6 +76,8 @@ class UserEditDisplay extends AdminEditDisplay {
      */
     function displayUnixAccountInformation() {
 
+        $shell = array('/bin/sh', '/bin/bash', 'sbin/nologin', '/bin/bash2', '/bin/ash', '/bin/bsh', '/bin/ksh', '/bin/tcsh', '/bin/csh', '/bin/zsh');
+
         print '<h3>'.$GLOBALS['Language']->getText('admin_usergroup','account_info').'</h3>';
 
       
@@ -90,50 +92,14 @@ class UserEditDisplay extends AdminEditDisplay {
             print '<p>Shell:';
 
             print '<select name="form_shell">';
-                     
-            print '<option value="/bin/sh"';
-            if($this->userparam['shell'] == '/bin/sh') print 'selected="selected"';
-            print ' >/bin/sh</option>';
-        
-            print '<option value="/bin/bash"';
-            if($this->userparam['shell'] == '/bin/bash') print 'selected="selected"';
-            print '>/bin/bash</option>';
+
+            for ($i = 0; $i < count($shell); $i++) {
+                print '<option value="'.$shell[$i].'" ';
+                if($this->userparam['shell'] == $shell[$i]) print 'selected="selected"';
+                print ' >'.$shell[$i].'</option>';
+            }
             
-            print '<option value="/sbin/nologin"';
-            if($this->userparam['shell'] == '/sbin/nologin') print 'selected="selected"';
-            print '>/sbin/nologin</option>';
-    
-            print '<option value="/bin/bash2"';
-            if($this->userparam['shell'] == '/bin/bash2') print 'selected="selected"';
-            print '>/bin/bash2</option>';
-            
-            print '<option value="/bin/ash"';
-            if($this->userparam['shell'] == '/bin/ash') print 'selected="selected"';
-            print '>/bin/ash</option>';
-
-            print '<option value="/bin/bsh"';
-            if($this->userparam['shell'] == '/bin/bsh') print 'selected="selected"';
-            print '>/bin/bsh</option>';
-
-            print '<option value="/bin/ksh"';
-            if($this->userparam['shell'] == '/bin/ksh') print 'selected="selected"';
-            print '>/bin/ksh</option>';
-
-            print '<option value="/bin/tcsh"';
-            if($this->userparam['shell'] == '/bin/tcsh') print 'selected="selected"';
-            print '>/bin/tcsh</option>';
-
-            print '<option value="/bin/csh"';
-            if($this->userparam['shell'] == '/bin/csh') print 'selected="selected"';
-            print '>/bin/csh</option>';
-
-            print '<option value="/bin/zsh"';
-            if($this->userparam['shell'] == '/bin/zsh') print 'selected="selected"';
-            print '>/bin/zsh</option>';
-
-            print '</select>';
-
-            print '</p>';
+            print '</select></p>';
 
 
             print '<p>Codex Account Status:';
@@ -211,46 +177,12 @@ class UserEditDisplay extends AdminEditDisplay {
             
             print '<select name="form_shell">';
             
-            print '<option value="/bin/sh"';
-            if($this->userparam[0]['shell'] == '/bin/sh') print 'selected="selected"';
-            print ' >/bin/sh</option>';
-            
-            print '<option value="/bin/bash"';
-            if($this->userparam[0]['shell'] == '/bin/bash') print 'selected="selected"';
-            print '>/bin/bash</option>';
-
-            print '<option value="/sbin/nologin"';
-            if($this->userparam[0]['shell'] == '/sbin/nologin') print 'selected="selected"';
-            print '>/sbin/nologin</option>';
-    
-            print '<option value="/bin/bash2"';
-            if($this->userparam[0]['shell'] == '/bin/bash2') print 'selected="selected"';
-            print '>/bin/bash2</option>';
-            
-            print '<option value="/bin/ash"';
-            if($this->userparam[0]['shell'] == '/bin/ash') print 'selected="selected"';
-            print '>/bin/ash</option>';
-
-            print '<option value="/bin/bsh"';
-            if($this->userparam[0]['shell'] == '/bin/bsh') print 'selected="selected"';
-            print '>/bin/bsh</option>';
-
-            print '<option value="/bin/ksh"';
-            if($this->userparam[0]['shell'] == '/bin/ksh') print 'selected="selected"';
-            print '>/bin/ksh</option>';
-
-            print '<option value="/bin/tcsh"';
-            if($this->userparam[0]['shell'] == '/bin/tcsh') print 'selected="selected"';
-            print '>/bin/tcsh</option>';
-
-            print '<option value="/bin/csh"';
-            if($this->userparam[0]['shell'] == '/bin/csh') print 'selected="selected"';
-            print '>/bin/csh</option>';
-
-            print '<option value="/bin/zsh"';
-            if($this->userparam[0]['shell'] == '/bin/zsh') print 'selected="selected"';
-            print '>/bin/zsh</option>';
-
+            for ($i = 0; $i < count($shell); $i++) {
+                print '<option value="'.$shell[$i].'" ';
+                if($this->userparam[0]['shell'] == $shell[$i]) print 'selected="selected"';
+                print ' >'.$shell[$i].'</option>';
+            }
+         
             print '</select></p>';
 
         
@@ -329,45 +261,9 @@ class UserEditDisplay extends AdminEditDisplay {
             
             print '<select name="form_shell">';
             
-            print '<option value="/bin/sh"';
-            //if($this->userparam[0]['shell'] == '/bin/sh') print 'selected="selected"';
-            print ' >/bin/sh</option>';
-            
-            print '<option value="/bin/bash"';
-            //if($this->userparam[0]['shell'] == '/bin/bash') print 'selected="selected"';
-            print '>/bin/bash</option>';
-
-            print '<option value="/sbin/nologin"';
-            //if($this->userparam[0]['shell'] == '/sbin/nologin') print 'selected="selected"';
-            print '>/sbin/nologin</option>';
-    
-            print '<option value="/bin/bash2"';
-            //if($this->userparam[0]['shell'] == '/bin/bash2') print 'selected="selected"';
-            print '>/bin/bash2</option>';
-            
-            print '<option value="/bin/ash"';
-            //if($this->userparam[0]['shell'] == '/bin/ash') print 'selected="selected"';
-            print '>/bin/ash</option>';
-
-            print '<option value="/bin/bsh"';
-            //if($this->userparam[0]['shell'] == '/bin/bsh') print 'selected="selected"';
-            print '>/bin/bsh</option>';
-
-            print '<option value="/bin/ksh"';
-            //if($this->userparam[0]['shell'] == '/bin/ksh') print 'selected="selected"';
-            print '>/bin/ksh</option>';
-
-            print '<option value="/bin/tcsh"';
-            //if($this->userparam[0]['shell'] == '/bin/tcsh') print 'selected="selected"';
-            print '>/bin/tcsh</option>';
-
-            print '<option value="/bin/csh"';
-            //if($this->userparam[0]['shell'] == '/bin/csh') print 'selected="selected"';
-            print '>/bin/csh</option>';
-
-            print '<option value="/bin/zsh"';
-            //if($this->userparam[0]['shell'] == '/bin/zsh') print 'selected="selected"';
-            print '>/bin/zsh</option>';
+            for ($i = 0; $i < count($shell); $i++) {
+                print '<option value="'.$shell[$i].'">'.$shell[$i].'</option>';
+            }
 
             print '</select></p>';
 
@@ -376,29 +272,17 @@ class UserEditDisplay extends AdminEditDisplay {
 
             print '<select name="form_codexstatus" id="codexstatus" onChange="autochangeStatus(this.form)">';
 
-            print '<option value="A"';
-            //if ($this->userparam[0]['status'] == 'A') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_userlist','active').'</option>';
+            print '<option value="A">'.$GLOBALS['Language']->getText('admin_userlist','active').'</option>';
             
-            print '<option value="R"';
-            //if ($this->userparam[0]['status'] == 'R' || $this->userparam[0]['status'] == 'W') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_userlist','restricted').'</option>';
+            print '<option value="R">'.$GLOBALS['Language']->getText('admin_userlist','restricted').'</option>';
             
-            print '<option value="V"';
-            //if ($this->userparam[0]['status'] == 'V') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_userlist','validated').'</option>';
+            print '<option value="V">'.$GLOBALS['Language']->getText('admin_userlist','validated').'</option>';
             
-            print '<option value="P"';
-            //if ($this->userparam[0]['status'] == 'P') print 'selected="selected"';
-            print '>Pending</option>';
+            print '<option value="P">Pending</option>';
             
-            print '<option value="D"';
-            //if ($this->userparam[0]['status'] == 'D') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_userlist','deleted').'</option>'; 
+            print '<option value="D">'.$GLOBALS['Language']->getText('admin_userlist','deleted').'</option>'; 
             
-            print '<option value="S"';
-            //if ($this->userparam[0]['status'] == 'S') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_userlist','suspended').'</option>';
+            print '<option value="S">'.$GLOBALS['Language']->getText('admin_userlist','suspended').'</option>';
             
             print '</select></p>';
           
@@ -407,13 +291,9 @@ class UserEditDisplay extends AdminEditDisplay {
             
             print '<select name="form_unixstatus">';
             
-            print '<option value="N"';
-            //if($this->userparam[0]['unix_status'] == 'N') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_usergroup','no_account').'</option>';
+            print '<option value="N">'.$GLOBALS['Language']->getText('admin_usergroup','no_account').'</option>';
 
-            print '<option value="A"';
-            //if($this->userparam[0]['unix_status'] == 'A') print 'selected="selected"';
-            print '>'.$GLOBALS['Language']->getText('admin_usergroup','active').'</option>';
+            print '<option value="A">'.$GLOBALS['Language']->getText('admin_usergroup','active').'</option>';
         
             print '<option value="S">'.$GLOBALS['Language']->getText('admin_usergroup','suspended').'</option>';
             
