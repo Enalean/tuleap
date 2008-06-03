60a7b0448f496d94bfd5a05c7a28a6fc5cfdb9cb
Chris Han <xiphux@gmail.com>
2008-06-03 19:05:24 -0400

    These should have been assigned

--- include/display.git_commitdiff.php
+++ include/display.git_commitdiff.php
@@ -43,6 +43,8 @@ function git_commitdiff($projectroot,$project,$hash,$hash_parent)
 	foreach ($difftree as $i => $line) {
 		if (ereg("^:([0-7]{6}) ([0-7]{6}) ([0-9a-fA-F]{40}) ([0-9a-fA-F]{40}) (.)\t(.*)$",$line,$regs)) {
 			$tpl->clear_all_assign();
+			$tpl->assign("project",$project);
+			$tpl->assign("hash",$hash);
 			$tpl->assign("from_mode",$regs[1]);
 			$tpl->assign("to_mode",$regs[2]);
 			$tpl->assign("from_id",$regs[3]);
