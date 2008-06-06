2b51131d95b9c13e13dcf737da0e53c5315e6b7d
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-06-06 09:35:32 +0000

    'add group' links the old interface to add group

--- src/common/admin/view/group/GroupSearchDisplay.class.php
+++ src/common/admin/view/group/GroupSearchDisplay.class.php
@@ -190,8 +190,7 @@ class GroupSearchDisplay extends AdminSearchDisplay {
        
         $GLOBALS['HTML']->header(array('title'=>$GLOBALS['Language']->getText('admin_grouplist','title')));
 
-        print "<br><a href=\"groupedit-add.php\">[".$GLOBALS['Language']->getText('admin_grouplist','add_group')."]</a>";
-
+        print '<br><a href="/admin/groupedit-add.php">['.$GLOBALS['Language']->getText('admin_grouplist','add_group').']</a>';
               
         parent::displayHeader($GLOBALS['Language']->getText('admin_grouplist','for_categ').' <b>'.$GLOBALS['Language']->getText('admin_grouplist','all_categ').'</b>');
 
