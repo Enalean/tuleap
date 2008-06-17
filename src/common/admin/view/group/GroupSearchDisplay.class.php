7d7d310894535e2a6fc80a99b82eca928625deb6
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-06-17 15:58:40 +0000

    remove 'Add Group' link on Group search page

--- src/common/admin/view/group/GroupSearchDisplay.class.php
+++ src/common/admin/view/group/GroupSearchDisplay.class.php
@@ -189,9 +189,7 @@ class GroupSearchDisplay extends AdminSearchDisplay {
         session_require(array('group'=>'1','admin_flags'=>'A'));
        
         $GLOBALS['HTML']->header(array('title'=>$GLOBALS['Language']->getText('admin_grouplist','title')));
-
-        print '<br><a href="index.php?action=add_group">['.$GLOBALS['Language']->getText('admin_grouplist','add_group').']</a>';
-              
+             
         parent::displayHeader($GLOBALS['Language']->getText('admin_grouplist','for_categ').' <b>'.$GLOBALS['Language']->getText('admin_grouplist','all_categ').'</b>');
 
         ?>
