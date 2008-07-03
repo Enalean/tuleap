42743ce3a0a52ae39b7b6d1e6d805e563253dbdf
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-07-03 17:28:55 +0000

    Reload SELinux modules when command is executed.

--- src/utils/fix_selinux_contexts.pl
+++ src/utils/fix_selinux_contexts.pl
@@ -10,8 +10,11 @@
 #    Automatically fix SELinux contexts to allow proper access to Apache and MySAL
 #
 
-require("include.pl");  # Include all the predefined functions and variables
+$INSTALL_DIR="/usr/share/codex";
+require("$INSTALL_DIR/src/utils/include.pl");  # Include all the predefined functions and variables
 $CHCON='/usr/bin/chcon';
+$SEMODULE='/usr/sbin/semodule';
+
 $context="root:object_r:httpd_sys_content_t";
 
 if (( ! -e $CHCON ) || ( ! -e "/etc/selinux/config" ) || ( `grep -i '^SELINUX=disabled' /etc/selinux/config`)) {
@@ -37,7 +40,7 @@ if (( ! -e $CHCON ) || ( ! -e "/etc/selinux/config" ) || ( `grep -i '^SELINUX=di
 # Allow anonymous FTP writes
 `setsebool -P allow_ftpd_anon_write=1`;
 # Allow access to user's home with FTP
-`setsebool -P ftp_home_dir 1`
+`setsebool -P ftp_home_dir 1`;
 
 # /home/codexadm. Apache needs access to '.subversion' (Server update plugin), '.cvs' (Passerelle plugin)
 `$CHCON -R -h $context /home/$sys_http_user`;
@@ -48,3 +51,16 @@ if (( ! -e $CHCON ) || ( ! -e "/etc/selinux/config" ) || ( `grep -i '^SELINUX=di
 `$CHCON -h $context /svnroot`;
 `$CHCON -h system_u:object_r:cvs_data_t /cvsroot`;
 `$CHCON -R -h system_u:object_r:cvs_data_t /cvsroot/`;
+
+# Allow scripts to connect to the internet (e.g. for external RSS feeds)
+`setsebool -P httpd_can_network_connect=1`;
+
+# Reload SELinux modules
+opendir(DIR, "$INSTALL_DIR/codex_tools/selinux/modules") || die "Can't opendir SELinux modules: $!";
+while($file=readdir(DIR)) {
+  if ($file=~/\.pp$/) {
+      `$SEMODULE -i $INSTALL_DIR/codex_tools/selinux/modules/$file`;
+  }
+}
+closedir DIR;
+
