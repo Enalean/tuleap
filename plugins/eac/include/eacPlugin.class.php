eeffa9e8005fc8840aae046a9696ea39f4988f32
ikram_bououd <ikram_bououd@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-08-12 14:16:13 +0000

    hook configured

--- plugins/eac/include/eacPlugin.class.php
+++ plugins/eac/include/eacPlugin.class.php
@@ -6,6 +6,8 @@ class EacPlugin extends Plugin {
     
     function EacPlugin($id) {
         $this->Plugin($id);
+        $this->setScope(self::SCOPE_PROJECT);
+        $this->_addHook('project_data_export_table', 'project_data_export_table', false);
     }
     
     function &getPluginInfo() {
@@ -15,6 +17,17 @@ class EacPlugin extends Plugin {
         }
         return $this->pluginInfo;
     }
+
+    function project_data_export_table($params)
+    {
+        $url = $this->getPluginPath();
+        $url .= '/export_permissions?group_id='.$params['group_id'];
+
+        $link = '<td align="center"><a href="'.$url.'">Export</a><br><a href ="">Show Format</a></td> <td align="center">- <br> - </td><td align="center">- <br> - </td> ';
+
+        echo '<tr><td><b> Docman : Permissions information '.$link.'</tr>';
+    }
+
 }
 
 ?>
