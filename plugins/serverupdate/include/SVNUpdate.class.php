31334782cb8e61d894cef3fb2f5ec2af5b812d21
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-08-14 13:48:28 +0000

    Do not die when we just can't read the svn log. Raise a warning instead ( /my/ page rendering is not blocked anymore)

--- plugins/serverupdate/include/SVNUpdate.class.php
+++ plugins/serverupdate/include/SVNUpdate.class.php
@@ -559,7 +559,7 @@ class SVNUpdate {
         xml_parser_set_option($xml_parser, XML_OPTION_CASE_FOLDING, 0);
         xml_parser_set_option($xml_parser, XML_OPTION_SKIP_WHITE, 0);
         if (!xml_parse($xml_parser, $xml_commits)) {
-            die(sprintf("XML error: %s at line %d", xml_error_string(xml_get_error_code($xml_parser)), xml_get_current_line_number($xml_parser)));
+            trigger_error(sprintf("Unable to read the svn log: XML error: %s at line %d", xml_error_string(xml_get_error_code($xml_parser)), xml_get_current_line_number($xml_parser)), E_USER_WARNING);
         }
         xml_parser_free($xml_parser);
         
