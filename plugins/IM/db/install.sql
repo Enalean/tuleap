3ef82e5f8bf5ae731942ad90969bcd85d8896023
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-08-21 14:17:33 +0000

    Use ID=99 instead of 47 (conflict on codex.xerox.com, but should be OK on all other servers).

--- plugins/IM/db/install.sql
+++ plugins/IM/db/install.sql
@@ -5,7 +5,7 @@
 # Create IM Admin group
 # Do not add real users to this group.
 INSERT INTO groups SET 
-  group_id = '47', 
+  group_id = '99', 
   group_name = 'IM Administrators', 
   is_public = '0', 
   status = 'A', 
@@ -20,8 +20,8 @@ INSERT INTO groups SET
 
 
 # Services
-insert into service (group_id, label, description, short_name, link, is_active, is_used, scope, rank) VALUES (47, 'service_summary_lbl_key', 'service_summary_desc_key', 'summary', '/projects/sitenews/', 1, 1, 'system', 10);
-insert into service (group_id, label, description, short_name, link, is_active, is_used, scope, rank) VALUES (47, 'service_admin_lbl_key', 'service_admin_desc_key', 'admin', '/project/admin/?group_id=46', 1, 1, 'system', 20);
+insert into service (group_id, label, description, short_name, link, is_active, is_used, scope, rank) VALUES (99, 'service_summary_lbl_key', 'service_summary_desc_key', 'summary', '/projects/sitenews/', 1, 1, 'system', 10);
+insert into service (group_id, label, description, short_name, link, is_active, is_used, scope, rank) VALUES (99, 'service_admin_lbl_key', 'service_admin_desc_key', 'admin', '/project/admin/?group_id=46', 1, 1, 'system', 20);
 
 
 
@@ -29,7 +29,7 @@ insert into service (group_id, label, description, short_name, link, is_active, 
 # This is a "bot": do not try to log on the Web interface or a shell with this user!
 # His passwd is "1M@dm1n" (needed by openfire)
 INSERT INTO user SET 
-        user_id = '47', 
+        user_id = '99', 
         user_name = 'imadmin-bot', 
         email = '', 
         user_pw = '5127685c3da658593e6bac55e441c175', 
@@ -39,7 +39,7 @@ INSERT INTO user SET
         shell = '/bin/false',
         unix_pw = '***',
         unix_status = 'A',
-        unix_uid = 47,
+        unix_uid = 0,
         unix_box = 'shell1',
         ldap_id = NULL, 
         add_date = 940000000, 
@@ -55,7 +55,7 @@ INSERT INTO user SET
 # Make the 'imadmin' user part of the IM Admin Project so that he
 # is also an openfire admin.
 
-INSERT INTO user_group SET  user_id=47, group_id=47, admin_flags='A';
+INSERT INTO user_group SET  user_id=99, group_id=99, admin_flags='A';
 
 
 
--- plugins/IM/db/uninstall.sql
+++ plugins/IM/db/uninstall.sql
@@ -3,8 +3,8 @@
 ##
 
 # Remove IM Admin  user and group.
-DELETE FROM groups WHERE group_id='47';
-DELETE FROM user   WHERE user_id ='47';
-DELETE FROM service WHERE group_id='47';
-DELETE FROM user_group WHERE user_id='47';
+DELETE FROM groups WHERE group_id='99';
+DELETE FROM user   WHERE user_id ='99';
+DELETE FROM service WHERE group_id='99';
+DELETE FROM user_group WHERE user_id='99';
 
