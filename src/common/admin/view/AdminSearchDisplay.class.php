982d03f9563a9ede386b117d02f5930763bfa55e
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-08-26 10:37:26 +0000

    AdminSearchDisplay.class.php follows the coding rules define in CodeSniffer

--- src/common/admin/view/AdminSearchDisplay.class.php
+++ src/common/admin/view/AdminSearchDisplay.class.php
@@ -23,39 +23,53 @@
  * 
  */
 
+require_once 'common/admin/view/AdminDisplay.class.php';
 /**
  * AdminSearchDisplay
  *
  */
-require_once('common/admin/view/AdminDisplay.class.php');
-class AdminSearchDisplay extends AdminDisplay {
-    
-    function __construct() {
+class AdminSearchDisplay extends AdminDisplay
+{
+    /**
+     * constructor
+     */
+    function __construct() 
+    {
         parent::__construct();
     }
 
     /**
      * displayHeader()
      *
-     * @param $header
+     * @param string $header the header of the page
+     *
+     * @return void
      */
-    function displayHeader($header) {
+    function displayHeader($header)
+    {
         parent::displayHeader($header);        
     }
-    
+
     /**
      * displaySearchFilter
      *
-     * @param array $abc_entitled
-     * @param string $letterlink
+     * @param mixed  $abc_entitled contains the value of the shortcut parameter
+     * @param string $link         contains the shortcut parameter
+     * @param int    $offset       the number of the fisrt user to display
+     * @param int    $limit        the limit of user to display
+     *
+     * @return void
      */
-    function displaySearchFilter($abc_entitled, $link,$offset, $limit) {       
+    function displaySearchFilter($abc_entitled, $link, $offset, $limit) 
+    {       
+        $abc_array = array('A','B','C','D','E','F','G','H','I','J',
+                           'K','L','M','N','O','P','Q','R','S','T',
+                           'U','V','W','X','Y','Z','0','1','2','3',
+                           '4','5','6','7','8','9');
 
-        $abc_array = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9');
-        
         print '<p>';
         print $abc_entitled;
-        
+
         for ($i=0; $i < count($abc_array); $i++) {
             print '<a href="index.php?offset='.$offset.'&limit='.$limit.'&'.$link.'='. $abc_array[$i] .'">&nbsp;'. $abc_array[$i] .'&nbsp;</a>';
         }
@@ -65,70 +79,72 @@ class AdminSearchDisplay extends AdminDisplay {
     /**
      * displayBrowse
      *
-     * @param int $start
-     * @param int $end
-     * @param int $offset
-     * @param int $limit
-     * @param int $nbuser
-     * @param int $offsetmax
-     * @param string $username
-     * @param string $group
-     * @param string $status
+     * @param int    $start     the number of the first item displayed
+     * @param int    $end       the number of the last item displayed
+     * @param int    $offset    the offset in the userIterator
+     * @param int    $limit     the limit of user to display
+     * @param int    $nbuser    the number of user displayed
+     * @param int    $offsetmax the offset max in the userIterator
+     * @param string $link      contains the parameters to access to a page
+     *
+     * @return void
      */
-    function displayBrowse($start, $end, $offset, $limit, $nbuser, $offsetmax, $link) {
+    function displayBrowse($start, $end, $offset, $limit, $nbuser, $offsetmax, $link) 
+    {
+        $prev = $offset - $limit;
 
-      $prev = $offset - $limit;
-     
-      print '<table width=100%>';
-        
-      print '<tr>';
+        print '<table width=100%>';
 
-      if ($offset <= 0) {
+        print '<tr>';
 
- print '<td class="browse_left">&lt;&lt; '.$GLOBALS['Language']->getText('global', 'begin').'&lt; '.$GLOBALS['Language']->getText('global', 'prev').' '. $limit .'</span></td>';
-      }
-      else {
-   
-          print '<td class="browse_left"><a href="index.php?offset=0&limit='.$limit.$link.'">&lt;&lt; '.$GLOBALS['Language']->getText('global', 'begin').'</a>  <a href="index.php?offset='.$prev.'&limit='.$limit.$link.'">&lt; '.$GLOBALS['Language']->getText('global', 'prev').' '. $limit .'</a></span></td>';
-      }
+        if ($offset <= 0) {
 
-      print '<td class="browse_center">Items '.$start.' - '.$end.'</td>';
+            print '<td class="browse_left">&lt;&lt; '.$GLOBALS['Language']->getText('global', 'begin').'&lt; '.$GLOBALS['Language']->getText('global', 'prev').' '. $limit .'</span></td>';
 
-      if ($offset >= ($nbuser-$limit)|| $offsetmax <=0) {
-          
- print '<td class="browse_right">'.$GLOBALS['Language']->getText('global', 'next').' '.$limit .'&gt;'.$GLOBALS['Language']->getText('global', 'end').' &gt;&gt;</td>';
-      }
-      else {
-          
-          print '<td class="browse_right"><a href="index.php?offset='.$end.'&limit='.$limit.$link.'">'.$GLOBALS['Language']->getText('global', 'next').' '.$limit .'&gt;</a>  <a href="index.php?offset='.$offsetmax.'&limit='.$limit.$link.'">'.$GLOBALS['Language']->getText('global', 'end').' &gt;&gt;</a></td>';
+        } else {
 
- }
+            print '<td class="browse_left"><a href="index.php?offset=0&limit='.$limit.$link.'">&lt;&lt; '.$GLOBALS['Language']->getText('global', 'begin').'</a>  <a href="index.php?offset='.$prev.'&limit='.$limit.$link.'">&lt; '.$GLOBALS['Language']->getText('global', 'prev').' '. $limit .'</a></span></td>';
+        }
 
-      print '</tr>';
-        
-      print '</table>';
+        print '<td class="browse_center">Items '.$start.' - '.$end.'</td>';
+
+        if ($offset >= ($nbuser-$limit)|| $offsetmax <=0) {
+
+            print '<td class="browse_right">'.$GLOBALS['Language']->getText('global', 'next').' '.$limit .'&gt;'.$GLOBALS['Language']->getText('global', 'end').' &gt;&gt;</td>';
+
+        } else {
+
+            print '<td class="browse_right"><a href="index.php?offset='.$end.'&limit='.$limit.$link.'">'.$GLOBALS['Language']->getText('global', 'next').' '.$limit .'&gt;</a>  <a href="index.php?offset='.$offsetmax.'&limit='.$limit.$link.'">'.$GLOBALS['Language']->getText('global', 'end').' &gt;&gt;</a></td>';
+
+        }
+
+        print '</tr>';
+
+        print '</table>';
     }
 
     /**
      * display
+     *
+     * @return void
      */
-    function display() {
-
+    function display() 
+    {
         //Header
         $this->displayHeader();
-        
+
         //Search
         $this->displaySearchFilter();
-        
+
         //Browsing
         $this->displayBrowse();
-        
+
         //Search table
         $this->displaySearch();
-        
+
         //Browsing
         $this->displayBrowse();
-        
+
         //footer
         $this->displayFooter();
     }
