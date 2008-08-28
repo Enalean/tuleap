7c6c2cc55f279846b9657db59a39e1fee0f59fb5
asalvucci <asalvucci@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-08-28 13:27:03 +0000

    autoselectlist.js follows the coding rules define in CodeSniffer

--- src/www/scripts/autoselectlist.js
+++ src/www/scripts/autoselectlist.js
@@ -1,25 +1,25 @@
 function autochangeStatus(form) {
 
     var codexstatus = form.codexstatus;
-    var unixstatus = form.unixstatus;
-    
+    var unixstatus  = form.unixstatus;
+
     if (codexstatus.value == 'S') {
 
-	for (i=0;i<unixstatus.length;i++) {
+        for (i=0;i<unixstatus.length;i++) {
 
-	    if (unixstatus.options[i].value == 'N') {
-		unixstatus.options[i].selected = true;
-	    }
-	}
+            if (unixstatus.options[i].value == 'N') {
+                unixstatus.options[i].selected = true;
+            }
+        }
     }
 
     if(codexstatus.value == 'D') {
 
-	for (i=0;i<unixstatus.length;i++) {
+        for (i=0;i<unixstatus.length;i++) {
 
-	    if (unixstatus.options[i].value == 'D') {
-		unixstatus.options[i].selected = true;
-	    }
-	}
+            if (unixstatus.options[i].value == 'D') {
+                unixstatus.options[i].selected = true;
+            }
+        }
     }
 }
