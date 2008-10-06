9ca89fbcdea46a301bd9f18c62be050b0dc9c677
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-06 10:45:06 +0000

    fix bug with memory_limit
    level=normal

--- src/updates/upgrade.php
+++ src/updates/upgrade.php
@@ -7,7 +7,7 @@
 // An upgrade process shouldn't end because it takes too much time ot too
 // memory.
 ini_set('max_execution_time', 0);
-ini_set('memory_limit', 0);
+ini_set('memory_limit', -1);
 
 // Special case for command line call
 if (isset($argc)) {
