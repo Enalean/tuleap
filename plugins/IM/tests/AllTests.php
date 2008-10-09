6bbf436bc0ff453b1b5e2fc3cfae4d22fd57665b
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-09 14:34:58 +0000

    Move functional tests to functional directory. Start unit test (set up the environment)

---
+++ plugins/IM/tests/AllTests.php
@@ -0,0 +1,23 @@
+<?php
+//We want to be able to run one test AND many tests
+if (! defined('CODEX_RUNNER')) {
+    define('CODEX_RUNNER', __FILE__);
+    require_once('../../../codex_tools/tests/CodexReporter.class.php');
+}
+
+require_once('../../../codex_tools/tests/simpletest/unit_tester.php');
+require_once('../../../codex_tools/tests/simpletest/mock_objects.php');
+
+//We define a group of test
+class IMGroupTest extends GroupTest {
+    function IMGroupTest($name = 'All IM Plugin tests') {
+        $this->GroupTest($name);
+        
+        $this->addTestFile(dirname(__FILE__).'/IMPluginTest.php');
+    }
+}
+if (CODEX_RUNNER === __FILE__) {
+    $test =& new IMGroupTest();
+    $test->run(new CodexReporter());
+ }
+?>
--- plugins/IM/tests/IMPluginTest.php
+++ plugins/IM/tests/IMPluginTest.php
@@ -1,12 +1,9 @@
 <?php
-/*
- * Created on Jun 26, 2008
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
 
- require_once(dirname(__FILE__).'/../include/IMPlugin.class.php');
+require_once('common/language/BaseLanguage.class.php');
+$GLOBALS['Language'] = new BaseLanguage();
+
+require_once(dirname(__FILE__).'/../include/IMPlugin.class.php');
 
 /**
  * Copyright (c) Xerox Corporation, CodeX Team, 2001-2005. All rights reserved
@@ -23,19 +20,11 @@ class IMPluginTest extends UnitTestCase {
     function IMPluginTest($name = 'IMPlugin test') {
         $this->UnitTestCase($name);
     }
-    function testIm_process () {
-		$grp_id=225;
-		$params=array("group_id"=>$grp_id);
-		//before muc and grp creation
-		$im1=& new IMPlugin(10);
-		$this->assertNull($im1->_get_last_muc_room_name());
-        $this->assertNull($im1->_get_last_muc_grp_name());
-        
-        
-        $im2=& new IMPlugin(10);
-        $im2->im_process($params);
-        $this->assertNull($im1->_get_last_muc_room_name());
-        $this->assertNull($im1->_get_last_muc_grp_name());
-	}
+    
+    function testTrue() {
+        $this->assertTrue(true);
+    }
+    
 }
+
 ?>
