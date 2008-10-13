75b051e5ae0b8b2711b69a7f319e10fd54890802
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-13 06:04:29 +0000

    Dynamically display details when the user mouseover a cross reference

--- src/common/include/CodeX_HTMLPurifier.class.php
+++ src/common/include/CodeX_HTMLPurifier.class.php
@@ -97,13 +97,13 @@ class CodeX_HTMLPurifier {
      * links, and content-based text.
      * Allowed makups:
      * - 'p', 'br'
-     * - 'a[href|title]'
+     * - 'a[href|title|class]'
      * - 'ul', 'ol', 'li'
      * - 'cite', 'code', 'blockquote', 'strong', 'em', 'pre', 'b', 'i'
      */
     function getLightConfigMarkups() {
         $eParagraph       = array('p', 'br');
-        $eLinks           = array('a[href|title]');
+        $eLinks           = array('a[href|title|class]');
         $eList            = array('ul', 'ol', 'li');
         $eContentBasedTxt = array('cite', 'code', 'blockquote', 'strong', 'em',
                                   'pre', 'b', 'i');
