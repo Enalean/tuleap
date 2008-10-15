d6fe25a8ab4df7ca58c1227b88b533e94da02ae4
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-15 09:07:31 +0000

    Add IM instructions in migration script.

--- codex_tools/migration_from_Codendi_3.6_to_Codendi_3.8.sh
+++ codex_tools/migration_from_Codendi_3.6_to_Codendi_3.8.sh
@@ -28,8 +28,23 @@ AND (service_short_name = '' OR service_short_name IS NULL)
 AND link LIKE '/tracker/%func=detail%'
 
 
-
+# IM plugin
+# TODO : stop openfire service ($SERVICE openfire stop)
 # IM plugin : grant privileges for openfireadm on session table (required for webmuc)
-
 GRANT SELECT ON codex.session to openfireadm@localhost;
 FLUSH PRIVILEGES;
+# IM openfire configuration
+# TODO : create database_im.inc in /etc/codex/plugins/IM/etc/
+# Specific configuration for webmuc
+INSERT INTO openfire.jiveProperty (name, propValue) VALUES 
+	("httpbind.enabled", "true"),
+	("httpbind.port.plain", "7070"),
+	("xmpp.httpbind.client.requests.polling", "0"),
+	("xmpp.httpbind.client.requests.wait", "10"),
+	("xmpp.httpbind.scriptSyntax.enabled", "true"),
+	("xmpp.muc.history.type", "all");
+# TODO : Modify openfire/conf/openfire.xml : 
+# TODO : $xml->provider->auth->className update node to CodexJDBCAuth
+# TODO : $xml->jdbcAuthProvider->addChild('codexUserSessionIdSQL', "SELECT session_hash FROM session WHERE session.user_id = (SELECT user_id FROM user WHERE user.user_name = ?)");
+# copy jar file into openfire lib dir
+$CP $INSTALL_DIR/plugins/IM/include/jabbex_api/installation/resources/codendi_auth.jar /opt/openfire/lib/.
