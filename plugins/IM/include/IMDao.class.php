f38d244de21db5258c8e29276ce4cddd946294d0
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-27 14:50:47 +0000

    Fix lockedDate, roomPassword, and try to fix dates in MUC Room creation query.

--- plugins/IM/include/IMDao.class.php
+++ plugins/IM/include/IMDao.class.php
@@ -181,18 +181,18 @@ class IMDao extends DataAccessObject {
 			         		$admin_affiliation = self::OPENFIRE_ADMIN_AFFILIATION;
 			         		$super_admin_affiliation = self::OPENFIRE_SUPER_ADMIN_AFFILIATION;
 			         		
-			         		$creation_date=''.round(1000*microtime(true));
+			         		$creation_date=''.round(microtime(true));
 			         	 	$creation_date=$this->da->quoteSmart($creation_date);
 			         	 	//echo $creation_date;
-					  		$modification_date=''.round(1000*microtime(true));
+					  		$modification_date=''.round(microtime(true));
 					  		$modification_date=$this->da->quoteSmart($modification_date);
 					  		$short_name='';
 					  		$public_name='';
 					  		$owner='';
 					  		$description='';
-					  		$locked_date='000000000000000';
-					  		$locked_date=$this->da->quoteSmart($locked_date);
-					  		$empty_date=''.round(1000*microtime(true));
+					  		$locked_date = '000000000000000';
+					  		$locked_date = $this->da->quoteSmart($locked_date);
+					  		$empty_date=''.round(microtime(true));
 					  		$empty_date=$this->da->quoteSmart($empty_date);
 					  		$change_subject=1;
 					  		$change_subject=$this->da->quoteSmart($change_subject);
@@ -206,8 +206,8 @@ class IMDao extends DataAccessObject {
 					  		$members_only=$this->da->quoteSmart($members_only);
 					  		$can_invite=1;
 					  		$can_invite=$this->da->quoteSmart($can_invite);
-					  		$room_pwd="";
-					  		$room_pwd=$this->da->quoteSmart($room_pwd);
+					  		$room_pwd = NULL;
+					  		$room_pwd = $this->da->quoteSmart($room_pwd);
 					  		$can_discover_JID=1;
 					  		$can_discover_JID=$this->da->quoteSmart($can_discover_JID);
 					  		$log_enabled=1;
@@ -252,7 +252,7 @@ class IMDao extends DataAccessObject {
 								$forma="INSERT INTO ".$this->openfire_db_name.".mucRoom
 								                    (roomID, creationDate, modificationDate, name, naturalName, description, lockedDate, emptyDate, canChangeSubject, maxUsers, publicRoom, moderated, membersOnly, canInvite, roomPassword, canDiscoverJID, logEnabled, subject, rolesToBroadcast, useReservedNick, canChangeNick, canRegister)
 										 VALUES (%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s)";
-								$sql = sprintf($forma,$id,$creation_date,$modification_date,$short_name,$public_name,$description,0000000000,$empty_date,$change_subject,$max_user,$public_room,$moderated,$members_only,$can_invite,$room_pwd,$can_discover_JID,$log_enabled,$subject,$role_to_broadcast,$use_reserved_NICK,$can_changed_nick,$can_register);
+								$sql = sprintf($forma,$id,$creation_date,$modification_date,$short_name,$public_name,$description,$locked_date,$empty_date,$change_subject,$max_user,$public_room,$moderated,$members_only,$can_invite,$room_pwd,$can_discover_JID,$log_enabled,$subject,$role_to_broadcast,$use_reserved_NICK,$can_changed_nick,$can_register);
 								//echo $sql.'<br>';
 								  
 								$tamp=$this->update($sql);
