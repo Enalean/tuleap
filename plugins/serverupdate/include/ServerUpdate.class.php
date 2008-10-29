1f466ac3af4a295287ef3fbd4c41ac7ace4547a5
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-10-29 13:02:23 +0000

    Follow-up to revision #10226

--- plugins/serverupdate/include/ServerUpdate.class.php
+++ plugins/serverupdate/include/ServerUpdate.class.php
@@ -115,7 +115,7 @@ class ServerUpdate extends Controler {
                     echo '({"value":"'. addslashes($value) .'","bgcolor":"'. $bgcolor .'"})';/**/
                     break;
                 default:
-                    if ($this->getSVNUpdate->getRepository() == "") {
+                    if ($this->getSVNUpdate()->getRepository() == "") {
                         $this->view = 'norepository';
                     } else {
                         if ($request->exist('sort')) {
@@ -148,7 +148,7 @@ class ServerUpdate extends Controler {
                     if ($request->exist('revision')) {
                         // We test if we are not going back (reverse to old revision)
                         // which is not allowed. To do this, it's better to use the console.
-                        if ($this->getSVNUpdate->getWorkingCopyRevision() >= $request->get('revision')) {
+                        if ($this->getSVNUpdate()->getWorkingCopyRevision() >= $request->get('revision')) {
                             exit_error($GLOBALS['Language']->getText('plugin_serverupdate_update','UpdateFailed'), $GLOBALS['Language']->getText('plugin_serverupdate_update','NoReverseUpdate'));
                         } else {
                             $this->view = 'testUpdate';
@@ -159,7 +159,7 @@ class ServerUpdate extends Controler {
                     if ($request->exist('revision')) {
                         if (is_numeric($request->get('revision'))) {
                             // We check that a reverse update to a older revision is not attempted
-                            if ($this->getSVNUpdate->getWorkingCopyRevision() >= $request->get('revision')) {
+                            if ($this->getSVNUpdate()->getWorkingCopyRevision() >= $request->get('revision')) {
                                 exit_error($GLOBALS['Language']->getText('plugin_serverupdate_update','UpdateFailed'), $GLOBALS['Language']->getText('plugin_serverupdate_update','NoReverseUpdate'));
                             } else {
                                 $this->view = 'processUpdate';
