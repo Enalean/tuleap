be2b24bba41350e35934ba999ce28e20de8c94cf
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-11-03 11:09:34 +0000

    Fix SR #1627 - Customized help page
    The whole "Site help" page is now customizable.

--- src/www/site/index.php
+++ src/www/site/index.php
@@ -9,15 +9,7 @@ require_once('pre.php');
 $title = $Language->getText('include_layout','Help');
 site_header(array('title' => $title));
 
-echo '<h2>',$title,'</h2>';
-echo '<ul>';
-echo '<li><a href="/documentation/user_guide/html/'.UserManager::instance()->getCurrentUser()->getLocale().'/"><b>'.$Language->getText('include_menu','help_index').'</b></a></li>';
-echo '<li><a href="/plugins/docman/?group_id=1">'.$Language->getText('include_menu','site_doc').'</b></a></li>';
-echo '<li><a href="/mail/?group_id=1">'.$Language->getText('include_menu','dev_channel').'</b></a></li>';
-echo '<li><a href="/forum/?group_id=1">'.$Language->getText('include_menu','discussion_forum').'</b></a><br /></li>';
-echo '</ul>';
-echo '<ul>';
-echo '<li><a href="/contact.php">'.$Language->getText('include_menu','contact_us').'</b></a></li>';
-echo '</ul>';
+include($Language->getContent('help/site'));
+
 site_footer(array());
 ?>
