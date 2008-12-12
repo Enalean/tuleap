253b4a73d72d2d0d08c5e4e8b9e71e1530d41d82
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2008-12-12 16:27:17 +0000

    Please join me to welcome a new, classy, top-of-the-art Codendi theme :D
    Use the DivBasedTabbedLayout contributed by Manuel Vacelet. Thanks!

---
+++ src/www/themes/CodendiBlack/CodendiBlack_Theme.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'DivBasedTabbedLayout.class.php';
+
+class CodendiBlack_Theme extends DivBasedTabbedLayout {
+    function __construct($root) {
+        parent::__construct($root);
+    }
+}
+
+?>
