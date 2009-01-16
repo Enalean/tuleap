7182beffaf5df1872672ad4e549295916248091a
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-01-16 14:29:48 +0000

    Add file used by Hudson to launch unit tests during Codendi build.

---
+++ codex_tools/plugins/tests/www/test_all.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('./utils.php');
+require_once('CodeXReporter.class.php');
+
+function add_test_to_group($test, $categ, $params) {
+    if (is_array($test)) {
+        if ($categ != '_tests') {
+            $g =& new GroupTest($categ .' Results');
+            foreach($test as $c => $t) {
+                add_test_to_group($t, $c, array('group' => &$g, 'path' => $params['path']."/$categ/"));
+            }
+            $params['group']->addTestCase($g);
+        } else {
+            foreach($test as $t) {
+                $params['group']->addTestFile($params['path'] . '/' . $t);
+            }
+        }
+    } else if ($test) {
+        $params['group']->addTestFile($params['path'] . $categ);
+    }
+}
+/**/
+
+$g =& get_group_tests($GLOBALS['tests']);
+
+$j_reporter = new CodeXJUnitXMLReporter();
+$g->run($j_reporter);
+
+$xml = $j_reporter->writeXML('codendi_unit_tests_report.xml');
+
+
+?>
