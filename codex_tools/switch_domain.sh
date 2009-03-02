a8ebb5baf098c9900cef40ab8fe55bac11837e2b
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-03-02 16:57:08 +0000

    Add utility script for changing server IP address and domain in configuration files (but not in DB...).

---
+++ codex_tools/switch_domain.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+PERL='/usr/bin/perl'
+
+[ `id -u` -ne 0 ] && die "Must be root to execute this script"
+
+substitute() {
+  # $1: filename, $2: string to match, $3: replacement string
+  # Allow '/' is $3, so we need to double-escape the string
+  if [ -e $1 ] ; then
+    replacement=`echo $3 | sed "s|/|\\\\\/|g"`
+    $PERL -pi -e "s/$2/$replacement/g" $1
+  fi
+}
+
+old_domain=`grep ServerName /etc/httpd/conf/httpd.conf | grep -v '#' | head -1 | cut -d " " -f 2 ;`
+echo "Current domain = $old_domain"
+read -p "New domain: " new_domain
+
+
+substitute '/etc/codex/conf/local.inc' "$old_domain" "$new_domain" 
+substitute '/etc/codex/conf.d/codex_aliases.conf' "$old_domain" "$new_domain" 
+substitute '/etc/httpd/conf/httpd.conf' "$old_domain" "$new_domain"
+if [ -e '/etc/codex/documentation/user_guide/xml/ParametersLocal.dtd' ] ; then
+    substitute '/etc/codex/documentation/user_guide/xml/ParametersLocal.dtd' "$old_domain" "$new_domain" 
+fi
+if [ -e '/etc/codex/documentation/cli/xml/ParametersLocal.dtd' ] ; then
+    substitute '/etc/codex/documentation/cli/xml/ParametersLocal.dtd' "$old_domain" "$new_domain" 
+fi
+if [ -e '/var/named/chroot/var/named/codex_full.zone' ] ; then
+    substitute '/var/named/chroot/var/named/codex_full.zone' "$old_domain" "$new_domain" 
+fi
+if [ -e '/etc/mail/local-host-names' ] ; then
+    substitute '/etc/mail/local-host-names' "$old_domain" "$new_domain" 
+fi
+
+# TODO
+# Mailman? /usr/lib/mailman/Mailman/mm_cfg.py + existing mailing lists...
+# DB: user email, group http_domain, homepage service
+# OpenFire: must re-install jabbex
