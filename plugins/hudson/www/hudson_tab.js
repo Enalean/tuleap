d031a9c9ca917ed6156ee12c00d1cfbfa8aabb46
mnazaria <mnazaria@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-03-12 14:16:09 +0000

    Speed-up scriptaculous effects

--- plugins/hudson/www/hudson_tab.js
+++ plugins/hudson/www/hudson_tab.js
@@ -1,10 +1,10 @@
 function toggle_addurlform() {
-    Effect.toggle('hudson_add_job', 'slide');
+    Effect.toggle('hudson_add_job', 'slide', { duration: 0.3 });
 }
 
 function toggle_iframe(joburl) {
     if ( ! $('hudson_iframe_div').visible() ) {
-        Effect.toggle('hudson_iframe_div', 'appear');
+        Effect.toggle('hudson_iframe_div', 'appear', { duration: 0.3 });
     }
     $('hudson_iframe').src = joburl;
     $('link_show_only').href = joburl;
