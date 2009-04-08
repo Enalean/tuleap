8c0f002d19e826e9542570b391ce59590c2e764e
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-04-08 12:03:14 +0000

    Add missing files. Follow-up to revision #11671.

---
+++ src/common/dao/ProjectDao.class.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ *
+ * This file is a part of Codendi.
+ *
+ * Codendi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Codendi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ */
+class ProjectDao extends DataAccessObject {
+    public function __construct($da) {
+        parent::__construct($da);
+        $this->table_name = 'groups';
+    }
+    
+    public function searchByStatus($status) {
+        $status = $this->da->quoteSmart($status);
+        $sql = "SELECT *
+                FROM $this->table_name
+                WHERE status = $status";
+        return $this->retrieve($sql);
+    }
+}
+?>
