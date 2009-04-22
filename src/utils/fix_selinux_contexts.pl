d177851159c5741b7a566370037bb9c002a88ad3
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-04-22 09:24:52 +0000

    codendification of fix_selinux_context

--- src/utils/fix_selinux_contexts.pl
+++ src/utils/fix_selinux_contexts.pl
@@ -1,16 +1,29 @@
 #!/usr/bin/perl
 #
-# CodeX: Breaking Down the Barriers to Source Code Sharing inside Xerox
-# Copyright (c) Xerox Corporation, CodeX, 2001-2006. All Rights Reserved
-# This file is licensed under the GNU General Public License
+# Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
 #
-# 
+# This file is a part of Codendi.
 #
+# Codendi is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Codendi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 # Purpose:
 #    Automatically fix SELinux contexts to allow proper access to Apache and MySAL
 #
 
-$INSTALL_DIR="/usr/share/codex";
+$INSTALL_DIR="/usr/share/codendi";
 require("$INSTALL_DIR/src/utils/include.pl");  # Include all the predefined functions and variables
 $CHCON='/usr/bin/chcon';
 $SEMODULE='/usr/sbin/semodule';
@@ -22,27 +35,27 @@ if (( ! -e $CHCON ) || ( ! -e "/etc/selinux/config" ) || ( `grep -i '^SELINUX=di
    exit;
 }
 
-# /usr/share/codex -> CodeX main Web tree, documentation, plugins, etc.
+# /usr/share/codendi -> codendi main Web tree, documentation, plugins, etc.
 `$CHCON -R -h $context $codendi_dir`;
 
-# /etc/codex -> for licence, site-content...
+# /etc/codendi -> for licence, site-content...
 `$CHCON -R -h $context $sys_custom_dir`;
 
-# /var/lib/codex
+# /var/lib/codendi
 `$CHCON -R -h $context $sys_data_dir`;
 
 # FTP directories
 `$CHCON -R -h system_u:object_r:public_content_t $sys_data_dir/ftp`;
 `$CHCON -R -h system_u:object_r:public_content_rw_t  $sys_data_dir/ftp/incoming`;
 # Releases must be accessed from httpd
-`$CHCON -R -h $context $sys_data_dir/ftp/codex`;
+`$CHCON -R -h $context $sys_data_dir/ftp/codendi`;
 
 # Allow anonymous FTP writes
 `setsebool -P allow_ftpd_anon_write=1`;
 # Allow access to user's home with FTP
 `setsebool -P ftp_home_dir 1`;
 
-# /home/codexadm. Apache needs access to '.subversion' (Server update plugin), '.cvs' (Passerelle plugin)
+# /home/codendiadm. Apache needs access to '.subversion' (Server update plugin), '.cvs' (Passerelle plugin)
 `$CHCON -R -h $context /home/$sys_http_user`;
 
 # /home/groups -> project web sites
