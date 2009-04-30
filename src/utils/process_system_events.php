436773e588aefb6b019cd66ed25a829568ab1145
guerin <guerin@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-04-30 13:12:02 +0000

    Make sure that there is only one occurence of the process_system_events script running.

--- src/utils/process_system_events.php
+++ src/utils/process_system_events.php
@@ -19,17 +19,56 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+
+// Only root is allowed to run this script
 $processUser = posix_getpwuid(posix_geteuid());
 $username = $processUser['name'];
-
 if ($username != 'root') {
     echo "Must be root to run this script (currently:".$username.")\n";
     exit(1);
 }
 
+//
+// Check that another instance of this script is not already running
+//
+
+// How it works:
+// This script stores its process ID in /var/run/codendi_process_events.pid,
+// and deletes it when it has finished.
+// If the PID file already exists, it means either that an instance is currently
+// running, or that a previous call to this script failed (and thus did not delete 
+// the PID file). In this case, we send a signal to the process which PID is 
+// in the file. If it returns TRUE, it means that there is a process
+// with this PID running. Then, we check that the command running with this PID 
+// contains the name of this script (process_system_events.php). In this case,
+// we simply exit.
+
+$pathToPidFile='/var/run/codendi_process_events.pid';
+
+// See http://www.php.net/manual/en/function.posix-kill.php#49596
+if (file_exists($pathToPidFile)) {
+    $prevPid = file_get_contents($pathToPidFile);
+    if(($prevPid !== FALSE) && posix_kill(rtrim($prevPid),0)) {
+        // A program using this PID is currently running
+        // It might be a PID number collision: check the program name
+        $result = shell_exec('/bin/ps -A -o pid,command | grep "' . $prevPid . '" | grep process_system_events.php | grep -v "grep"');
+        if ($result != '') {
+            //echo "Error: Server is already running with PID: $prevPid\n";
+            exit(-1);
+        }
+    }
+}
+// Store PID
+if (file_put_contents($pathToPidFile,getmypid()) === FALSE) {
+	echo "Process_system_events: Cannot write PID file\n";
+}
+
+// Now start the real script.
 require_once('pre.php');
 
-// TODO: check that script is not already running
 $system_event_manager->processEvents();
+
+// Remove PID file when finished.
+unlink($pathToPidFile);
 
 ?>
