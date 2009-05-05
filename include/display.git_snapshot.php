f9cc52b73a4a98440659a0c7bbe9fda35520c76f
Chris Han <xiphux@gmail.com>
2009-05-04 20:12:35 -0500

    Name compression consistently

--- include/display.git_snapshot.php
+++ include/display.git_snapshot.php
@@ -17,12 +17,12 @@ function git_snapshot($projectroot,$project,$hash)
 		$hash = "HEAD";
 	$rname = str_replace(array("/",".git"),array("-",""),$project);
 	$tar = git_tar_tree($projectroot . $project, $hash, $rname);
-	if (($gitphp_conf['snapshotformat'] == GITPHP_SNAPSHOT_BZ2) && function_exists("bzcompress")) {
+	if (($gitphp_conf['compressformat'] == GITPHP_COMPRESS_BZ2) && function_exists("bzcompress")) {
 		header("Content-Type: application/x-bzip2");
 		header("Content-Disposition: attachment; filename=" . $rname . ".tar.bz2");
 		echo bzcompress($tar,(isset($gitphp_conf['compresslevel'])?$gitphp_conf['compresslevel']:4));
 		return;
-	} else if (($gitphp_conf['snapshotformat'] == GITPHP_SNAPSHOT_GZ) && function_exists("gzencode")) {
+	} else if (($gitphp_conf['compressformat'] == GITPHP_COMPRESS_GZ) && function_exists("gzencode")) {
 		header("Content-Type: application/x-gzip");
 		header("Content-Disposition: attachment; filename=" . $rname . ".tar.gz");
 		echo gzencode($tar,(isset($gitphp_conf['compresslevel'])?$gitphp_conf['compresslevel']:-1));
