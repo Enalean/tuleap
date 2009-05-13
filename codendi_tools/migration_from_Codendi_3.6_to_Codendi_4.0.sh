8577cf755d02978bca1f03ad2349782154f3d1d8
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-05-13 15:27:48 +0000

    Codendification of /etc/cvsnt/PServer

--- codendi_tools/migration_from_Codendi_3.6_to_Codendi_4.0.sh
+++ codendi_tools/migration_from_Codendi_3.6_to_Codendi_4.0.sh
@@ -408,6 +408,10 @@ $MV /etc/aliases.codex /etc/aliases.codendi
 substitute '/etc/mail/sendmail.cf' '\/etc\/aliases.codex' "/etc/aliases.codendi"
 
 codendification /etc/shells
+
+if [ -f /etc/cvsnt/PServer ]; then
+    codendification /etc/cvsnt/PServer
+fi
 codendification "$ETC_DIR/conf/local.inc"
 codendification "$ETC_DIR/conf/database.inc"
 substitute "$ETC_DIR/conf/local.inc" "sys_themedefault\s*=\s*'CodendiTab'" "sys_themedefault = 'CodeXTab'"
@@ -1640,9 +1644,9 @@ $CP ${newest_rpm}/monitoring.jar /opt/openfire/plugins
 #
 # TODO CODENDIFICATION:
 #
-# - replace "CODEX BLOCK" by "CODENDI BLOCK" in /etc/cvsnt/PServer
 
-# Ask to make a manual update (-u???) on /usr/share/codedni as codendiadm to get new realm 
+
+# Ask to make a manual update (-u???) on /usr/share/codendi as codendiadm to get new realm 
 #??? Mailman: codex-admin??
 # - Warn admins that CODEX_LOCAL_INC was replaced by CODENDI_LOCAL_INC
 # - OpenFire install.
