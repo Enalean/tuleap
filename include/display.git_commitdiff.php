1dcf8034eb0b3ea11c20ef4d4acad8098f6e97f8
Chris Han <xiphux@gmail.com>
2009-07-04 02:39:17 -0500

    Unify commitdiff template

--- include/display.git_commitdiff.php
+++ include/display.git_commitdiff.php
@@ -12,7 +12,7 @@
  require_once('gitutil.git_read_commit.php');
  require_once('gitutil.git_diff_tree.php');
  require_once('gitutil.read_info_ref.php');
- require_once('display.git_diff_print.php');
+ require_once('gitutil.git_diff.php');
 
 function git_commitdiff($projectroot,$project,$hash,$hash_parent)
 {
@@ -33,38 +33,33 @@ function git_commitdiff($projectroot,$project,$hash,$hash_parent)
 	$tpl->assign("hash",$hash);
 	$tpl->assign("tree",$co['tree']);
 	$tpl->assign("hashparent",$hash_parent);
-	$tpl->display("commitdiff_nav.tpl");
 	$tpl->assign("title",$co['title']);
 	if (isset($refs[$co['id']]))
 		$tpl->assign("commitref",$refs[$co['id']]);
 	$tpl->assign("comment",$co['comment']);
-	$tpl->display("commitdiff_header.tpl");
-
+	$difftreelines = array();
 	foreach ($difftree as $i => $line) {
 		if (ereg("^:([0-7]{6}) ([0-7]{6}) ([0-9a-fA-F]{40}) ([0-9a-fA-F]{40}) (.)\t(.*)$",$line,$regs)) {
-			$tpl->clear_all_assign();
-			$tpl->assign("project",$project);
-			$tpl->assign("hash",$hash);
-			$tpl->assign("from_mode",$regs[1]);
-			$tpl->assign("to_mode",$regs[2]);
-			$tpl->assign("from_id",$regs[3]);
-			$tpl->assign("to_id",$regs[4]);
-			$tpl->assign("status",$regs[5]);
-			$tpl->assign("file",$regs[6]);
-			$tpl->assign("from_type",file_type($regs[1]));
-			$tpl->assign("to_type",file_type($regs[2]));
-			$tpl->display("commitdiff_item.tpl");
+			$difftreeline = array();
+			$difftreeline["from_mode"] = $regs[1];
+			$difftreeline["to_mode"] = $regs[2];
+			$difftreeline["from_id"] = $regs[3];
+			$difftreeline["to_id"] = $regs[4];
+			$difftreeline["status"] = $regs[5];
+			$difftreeline["file"] = $regs[6];
+			$difftreeline["from_type"] = file_type($regs[1]);
+			$difftreeline["to_type"] = file_type($regs[2]);
 			if ($regs[5] == "A")
-				git_diff_print($projectroot . $project, null,"/dev/null",$regs[4],"b/" . $regs[6]);
+				$difftreeline['diffout'] = explode("\n",git_diff($projectroot . $project, null,"/dev/null",$regs[4],"b/" . $regs[6]));
 			else if ($regs[5] == "D")
-				git_diff_print($projectroot . $project, $regs[3],"a/" . $regs[6],null,"/dev/null");
+				$difftreeline['diffout'] = explode("\n",git_diff($projectroot . $project, $regs[3],"a/" . $regs[6],null,"/dev/null"));
 			else if (($regs[5] == "M") && ($regs[3] != $regs[4]))
-				git_diff_print($projectroot . $project, $regs[3],"a/" . $regs[6],$regs[4],"b/" . $regs[6]);
+				$difftreeline['diffout'] = explode("\n",git_diff($projectroot . $project, $regs[3],"a/" . $regs[6],$regs[4],"b/" . $regs[6]));
+			$difftreelines[] = $difftreeline;
 		}
 	}
-
-	$tpl->clear_all_assign();
-	$tpl->display("commitdiff_footer.tpl");
+	$tpl->assign("difftreelines",$difftreelines);
+	$tpl->display("commitdiff.tpl");
 }
 
 ?>
--- templates/commitdiff_footer.tpl
+++
@@ -1,9 +0,0 @@
-{*
- *  commitdiff_footer.tpl
- *  gitphp: A PHP git repository browser
- *  Component: Commitdiff view footer template
- *
- *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
- *}
- <br />
- </div>
