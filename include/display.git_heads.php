bd3a029ee8df20b234cdcf9dec755a89fb95b58b
Chris Han <xiphux@gmail.com>
2009-07-04 10:37:46 -0500

    Unify heads template

--- include/display.git_heads.php
+++ include/display.git_heads.php
@@ -17,26 +17,9 @@ function git_heads($projectroot,$project)
 	$tpl->clear_all_assign();
 	$tpl->assign("project",$project);
 	$tpl->assign("head",$head);
-	$tpl->display("heads_nav.tpl");
-	$tpl->display("heads_header.tpl");
-	$taglist = git_read_refs($projectroot, $project, "refs/heads");
-	if (isset($taglist) && (count($taglist) > 0)) {
-		$alternate = FALSE;
-		foreach ($taglist as $i => $entry) {
-			$tpl->clear_all_assign();
-			if ($alternate)
-				$tpl->assign("class","dark");
-			else
-				$tpl->assign("class","light");
-			$alternate = !$alternate;
-			$tpl->assign("project",$project);
-			$tpl->assign("age",$entry['age']);
-			$tpl->assign("name",$entry['name']);
-			$tpl->display("heads_item.tpl");
-		}
-	}
-	$tpl->clear_all_assign();
-	$tpl->display("heads_footer.tpl");
+	$headlist = git_read_refs($projectroot, $project, "refs/heads");
+	$tpl->assign("headlist",$headlist);
+	$tpl->display("heads.tpl");
 }
 
 ?>
--- templates/heads_footer.tpl
+++
@@ -1,8 +0,0 @@
-{*
- *  heads_footer.tpl
- *  gitphp: A PHP git repository browser
- *  Component: Head view footer template
- *
- *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
- *}
- </table>
