cfe5af6c0f2a3c07c190e7ee1bb7d3bb737c0d44
Chris Han <xiphux@gmail.com>
2009-07-04 22:28:28 -0500

    Unify tags template

--- include/display.git_tags.php
+++ include/display.git_tags.php
@@ -17,32 +17,11 @@ function git_tags($projectroot,$project)
 	$tpl->clear_all_assign();
 	$tpl->assign("project",$project);
 	$tpl->assign("head",$head);
-	$tpl->display("tags_nav.tpl");
-	$tpl->display("tags_header.tpl");
 	$taglist = git_read_refs($projectroot, $project, "refs/tags");
 	if (isset($taglist) && (count($taglist) > 0)) {
-		$alternate = FALSE;
-		foreach ($taglist as $i => $entry) {
-			$tpl->clear_all_assign();
-			if ($alternate)
-				$tpl->assign("class","dark");
-			else
-				$tpl->assign("class","light");
-			$alternate = !$alternate;
-			$tpl->assign("project",$project);
-			$tpl->assign("age",$entry['age']);
-			$tpl->assign("name",$entry['name']);
-			$tpl->assign("reftype",$entry['reftype']);
-			$tpl->assign("refid",$entry['refid']);
-			$tpl->assign("id",$entry['id']);
-			$tpl->assign("type",$entry['type']);
-			if (isset($entry['comment']) && isset($entry['comment'][0]))
-				$tpl->assign("comment",$entry['comment'][0]);
-			$tpl->display("tags_item.tpl");
-		}
+		$tpl->assign("taglist",$taglist);
 	}
-	$tpl->clear_all_assign();
-	$tpl->display("tags_footer.tpl");
+	$tpl->display("tags.tpl");
 }
 
 ?>
--- templates/tags_header.tpl
+++
@@ -1,9 +0,0 @@
-{*
- *  tags_header.tpl
- *  gitphp: A PHP git repository browser
- *  Component: Tag view header template
- *
- *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
- *}
- <div><a href="{$SCRIPT_NAME}?p={$project}&a=summary" class="title">&nbsp;</a></div>
- <table cellspacing="0">
