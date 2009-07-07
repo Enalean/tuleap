5e26933c337d2c7c98ff4b1f2434e476abd5311a
labbenes <labbenes@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-07-07 15:45:45 +0000

    - Codendification of purifier level. - Change localisation messages. - Make use of UserManager to deal with users.

--- src/www/include/project_home.php
+++ src/www/include/project_home.php
@@ -23,9 +23,14 @@ $hp =& Codendi_HTMLPurifier::instance();
 $title = $Language->getText('include_project_home','proj_info').' - '. $project->getPublicName();
 
 $HTML->includeJavascriptFile('/scripts/scriptaculous/scriptaculous.js');
+
+require_once 'common/user/User.class.php';
+$uM = UserManager::instance();
+$user = $uM->getCurrentUser();
 require_once('www/include/trove.php');
-if (user_ismember($group_id, 'A') && !trove_project_categorized($group_id) && substr($_SERVER['SCRIPT_NAME'],0,9) == '/projects') {
-    $GLOBALS['Response']->addFeedback('warning',$GLOBALS['Language']->getText('include_html','no_trovcat',array(get_server_url(), $group_id)), CODEX_PURIFIER_DISABLED);
+if ($user->isMember($group_id, 'A') && !trove_project_categorized($group_id) && substr($_SERVER['SCRIPT_NAME'],0,9) == '/projects') {
+    $trove_url = '/project/admin/group_trove.php?group_id='.$group_id;
+    $GLOBALS['Response']->addFeedback('warning',$GLOBALS['Language']->getText('include_html','no_trovcat',array($trove_url)), CODENDI_PURIFIER_DISABLED);
 }
 site_project_header(array('title'=>$title,'group'=>$group_id,'toptab'=>'summary'));
 
