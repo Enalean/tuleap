bbd55eed8b80171821b4acc82330232b37638776
manuel_vacelet <manuel_vacelet@69b26aa9-6588-4999-a7e6-a99cda329b54>
2009-09-15 07:15:07 +0000

    Merge /contrib/st/plugins/1640_forumml_4.0 (r13192 through r13209).

--- plugins/forumml/bin/ml_arch_2_DB.pl
+++ plugins/forumml/bin/ml_arch_2_DB.pl
@@ -2,24 +2,24 @@
 
 #
 # Copyright (c) STMicroelectronics, 2007. All Rights Reserved.
-
- # Originally written by Mohamed CHAARI, 2007
- #
- # This file is a part of codendi.
- #
- # codendi is free software; you can redistribute it and/or modify
- # it under the terms of the GNU General Public License as published by
- # the Free Software Foundation; either version 2 of the License, or
- # (at your option) any later version.
- #
- # codendi is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
- #
- # You should have received a copy of the GNU General Public License
- # along with codendi; if not, write to the Free Software
- # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Originally written by Mohamed CHAARI, 2007
+#
+# This file is a part of codendi.
+#
+# codendi is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# codendi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with codendi; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
 =pod
@@ -29,27 +29,35 @@ Only projects that enabled ForumML plugin are concerned by this migration.
 
 =cut
 
-# mailing-list name should contain only alphabetical characters, '-' and '.' 
-sub validate_listname {
-    my $arg = shift;
-    my $listname = "";
-    my $match = 0;
-
-    # special mailing-lists should not be included
-    my @exclude_ml = ("crolles.codex.system", "grenoble.codex.system");
-
-    if($arg =~ /^([-.\w]+)$/) {
-        foreach (@exclude_ml) {
-        	if ($_ eq $1) {
-    			$match = 1;
-        	}
-    	}
-        if ($match == 0) {
-	        $listname = $1;
-	    }
+# Search if there are lists we shouldn't treat
+my $conf = '/etc/codendi/plugins/forumml/etc/forumml.inc';
+my %excluded_list;
+if (-f $conf) {
+    # Get the variable defined in forumml.inc
+    my @exc_lists;
+    open(FORUMML_INC, "<$conf");
+    while (<FORUMML_INC>) {
+	if (m/^\$forumml_excluded_lists[ ]*=[ ]*"(.*)"[ ]*;[ ]*$/) {
+	    @exc_lists = split(/[ ]*,[ ]*/, $1);
+	}
     }
-    return $listname;
+    close(FORUMML_INC);
+
+    # Test if given list is excluded or not
+    foreach my $list (@exc_lists) {
+	$excluded_list{$list} = 0;
+    }
 }
+
+# Get PHP_PARAMS variable from php-laucher.sh
+my $PHP_PARAMS="";
+open(PHP_LAUNCHER, "</usr/share/codendi/src/utils/php-launcher.sh");
+while (<PHP_LAUNCHER>) {
+    if (m/^[ ]*PHP_PARAMS="(.*)"$/) {
+	$PHP_PARAMS=$1
+    }
+}
+close(PHP_LAUNCHER);
 
 #use strict;
 use DBI;
@@ -62,9 +70,8 @@ my $query = "SELECT list_name, group_id FROM mail_group_list WHERE status = 1";
 my $req = $dbh->prepare($query);
 $req->execute();
 while (my ($list_name,$group_id) = $req->fetchrow()) {
-	my $list = validate_listname($list_name);
-	if(! $list eq "") {
-		print "Processing ".$list." mailing-list ... \n";
-		system("/usr/share/codendi/src/utils/php-launcher.sh /usr/share/codendi/plugins/forumml/bin/mail_2_DB.php $list 2");
-	}
+    if(! exists $excluded_list{$list_name}) {
+	print "Processing ".$list_name." mailing-list ... \n";
+	system("/usr/bin/php $PHP_PARAMS /usr/share/codendi/plugins/forumml/bin/mail_2_DB.php $list_name 2");
+    }
 }
