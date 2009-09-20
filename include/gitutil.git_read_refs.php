742330890d7f0454492710955568cbb6c3a5944d
Chris Han <xiphux@gmail.com>
2009-09-20 12:03:33 -0500

    Packed ref support, based on work by Khee Chin <kheechin@gmail.com>

--- include/gitutil.git_read_refs.php
+++ include/gitutil.git_read_refs.php
@@ -7,12 +7,10 @@
  *  Copyright (C) 2008 Christopher Han <xiphux@gmail.com>
  */
 
- require_once('util.age_string.php');
  require_once('util.epochcmp.php');
- require_once('gitutil.git_get_type.php');
  require_once('gitutil.git_read_hash.php');
- require_once('gitutil.git_read_tag.php');
- require_once('gitutil.git_read_commit.php');
+ require_once('gitutil.git_read_ref.php');
+ require_once('gitutil.git_read_packed_refs.php');
 
 function git_read_refs($projectroot,$project,$refdir)
 {
@@ -40,44 +38,15 @@ function git_read_refs($projectroot,$project,$refdir)
 	$reflist = array();
 	foreach ($refs as $i => $ref_file) {
 		$ref_id = git_read_hash($projectroot . $project . "/" . $refdir . "/" . $ref_file);
-		$type = git_get_type($projectroot . $project, $ref_id);
-		if ($type) {
-			$ref_item = array();
-			$ref_item['type'] = $type;
-			$ref_item['id'] = $ref_id;
-			$ref_item['epoch'] = 0;
-			$ref_item['age_string'] = "unknown";
-
-			if ($type == "tag") {
-				$tag = git_read_tag($projectroot . $project, $ref_id);
-				$ref_item['comment'] = $tag['comment'];
-				if ($tag['type'] == "commit") {
-					$co = git_read_commit($projectroot . $project, $tag['object']);
-					$ref_item['epoch'] = $co['committer_epoch'];
-					$ref_item['age_string'] = $co['age_string'];
-					$ref_item['age'] = $co['age'];
-				} else if (isset($tag['epoch'])) {
-					$age = time() - $tag['epoch'];
-					$ref_item['epoch'] = $tag['epoch'];
-					$ref_item['age_string'] = age_string($age);
-					$ref_item['age'] = $age;
-				}
-				$ref_item['reftype'] = $tag['type'];
-				$ref_item['name'] = $tag['name'];
-				$ref_item['refid'] = $tag['object'];
-			} else if ($type == "commit") {
-				$co = git_read_commit($projectroot . $project, $ref_id);
-				$ref_item['reftype'] = "commit";
-				$ref_item['name'] = $ref_file;
-				$ref_item['title'] = $co['title'];
-				$ref_item['refid'] = $ref_id;
-				$ref_item['epoch'] = $co['committer_epoch'];
-				$ref_item['age_string'] = $co['age_string'];
-				$ref_item['age'] = $co['age'];
-			}
-			$reflist[] = $ref_item;
-		}
+		$refobj = git_read_ref($projectroot, $project, $ref_id, $ref_file);
+		if (isset($refobj))
+			$reflist[] = $refobj;
 	}
+
+	$packedrefs = git_read_packed_refs($projectroot, $project, $refdir);
+	if (isset($packedrefs) && count($packedrefs) > 0)
+		$reflist = array_merge($reflist, $packedrefs);
+
 	usort($reflist,"epochcmp");
 	return $reflist;
 }
