0e561f9c586516f46a3d183e4cb70ba18d345797
Zaran <thibaut.horel@gmail.com>
2010-01-19 21:42:43 -0600

    RSS feed xml error

--- include/display.git_rss.php
+++ include/display.git_rss.php
@@ -36,12 +36,14 @@ function git_rss($projectroot,$project)
 				break;
 			$cd = date_str($co['committer_epoch']);
 			$difftree = array();
-			$diffout = git_diff_tree($projectroot . $project, $co['parent'] . " " . $co['id']);
-			$tok = strtok($diffout,"\n");
-			while ($tok !== false) {
-				if (preg_match("/^:([0-7]{6}) ([0-7]{6}) ([0-9a-fA-F]{40}) ([0-9a-fA-F]{40}) (.)([0-9]{0,3})\t(.*)$/",$tok,$regs))
-					$difftree[] = $regs[7];
-				$tok = strtok("\n");
+			if (isset($co['parent'])) {
+				$diffout = git_diff_tree($projectroot . $project, $co['parent'] . " " . $co['id']);
+				$tok = strtok($diffout,"\n");
+				while ($tok !== false) {
+					if (preg_match("/^:([0-7]{6}) ([0-7]{6}) ([0-9a-fA-F]{40}) ([0-9a-fA-F]{40}) (.)([0-9]{0,3})\t(.*)$/",$tok,$regs))
+						$difftree[] = $regs[7];
+					$tok = strtok("\n");
+				}
 			}
 			$commitline = array();
 			$commitline["cdmday"] = $cd['mday'];
