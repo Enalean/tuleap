902b4c2ddf2be9c682cb608e14b9eadf381c9035
Chris Han <xiphux@gmail.com>
2010-09-06 13:35:30 -0500

    Remove some redundant local variables

--- js/blame.js
+++ js/blame.js
@@ -16,13 +16,15 @@ function initBlame() {
 		if (blameCol && blameCol.size() > 0) {
 			blameCol.show('fast');
 		} else {
-			var colElement = document.createElement('td');
-			var col = jQuery(colElement);
+			var col = jQuery(document.createElement('td'));
 			col.attr('id', 'blameData');
 			col.css('display', 'none');
 
-			var imgElement = document.createElement('img');
-			var img = jQuery(imgElement);
+			var p = jQuery(document.createElement('p'));
+			p.text("Loading blame data...");
+			p.appendTo(col);
+
+			var img = jQuery(document.createElement('img'));
 			img.attr('src', "images/blame-loader.gif");
 			img.attr('alt', "Loading...");
 			img.appendTo(col);
@@ -41,8 +43,7 @@ function initBlame() {
 		return false;
 	},
 	function() {
-		var blameCol = $('table#blobData td#blameData');
-		blameCol.hide('fast');
+		$('table#blobData td#blameData').hide('fast');
 
 		return false;
 	});
