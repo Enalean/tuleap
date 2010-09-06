863cdc27baeba705ae9091e986143d78f9e5a6d1
Chris Han <xiphux@gmail.com>
2010-09-06 16:10:29 -0500

    Adjust blame loading message a bit, add slight fading effect

--- js/blame.js
+++ js/blame.js
@@ -24,19 +24,27 @@ function initBlame() {
 			p.text("Loading blame data...");
 			p.appendTo(col);
 
+			var div = jQuery(document.createElement('div'));
+			div.css('text-align', 'center');
+
 			var img = jQuery(document.createElement('img'));
 			img.attr('src', "images/blame-loader.gif");
 			img.attr('alt', "Loading...");
-			img.appendTo(col);
+			img.appendTo(div);
+
+			div.appendTo(col);
 
 			$('table#blobData tr:first').prepend(col);
 			col.show('fast');
 
 			$.get($('a#blameLink').attr('href'), { o: 'js' },
 			function(data) {
-				$('td#blameData').html(data);
-				$('td#blameData').addClass('de1');
-				initTooltips();
+				$('td#blameData').fadeOut('fast', function() {
+					$('td#blameData').html(data);
+					$('td#blameData').addClass('de1');
+					initTooltips();
+					$('td#blameData').fadeIn('fast');
+				});
 			});
 		}
 
