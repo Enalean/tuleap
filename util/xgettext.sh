5161f943270576439bd2dc413280af46b6bb136d
Chris Han <xiphux@gmail.com>
2010-10-02 17:14:05 -0500

    Specify from code when extracting strings

--- util/xgettext.sh
+++ util/xgettext.sh
@@ -21,14 +21,14 @@ FILE="gitphp.pot"
 
 # Extract from templates
 lib/smarty-gettext/tsmarty2c.php templates > smarty.c
-xgettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" --add-comments --no-location smarty.c
+xgettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" --add-comments --no-location --from-code=utf-8 smarty.c
 rm smarty.c
 
 # Extract from include directory
-find include -iname '*.php' -type f | xgettext -ktranslate -kngettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -L PHP -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" -f -
+find include -iname '*.php' -type f | xgettext -ktranslate -kngettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -L PHP -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" --from-code=utf-8 -f -
 
 # Extract from index
-xgettext -ktranslate -kngettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -L PHP -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" index.php
+xgettext -ktranslate -kngettext -d ${BUNDLE} -o "${FILE}" -p ${DIR} -L PHP -j --package-name="${PKGNAME}" --package-version="${VER}" --msgid-bugs-address="${EMAIL}" --copyright-holder="${COPYRIGHT}" --from-code=utf-8 index.php
 
 # File references from smarty-gettext show up as code comments,
 # convert them back to file references
