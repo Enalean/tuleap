68bf539e467ab7a6bf67190e89f4f7f6758fa385
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-01-05 09:28:55 +0000

    Modify frs_log indexes and indente the sql request code.

--- src/db/mysql/database_structure.sql
+++ src/db/mysql/database_structure.sql
@@ -893,9 +893,7 @@ CREATE TABLE frs_log (
   item_id int(11) NOT NULL,
   action_id int(11) NOT NULL,
   PRIMARY KEY (log_id),
-  KEY idx_frs_log_time (time),
-  KEY idx_frs_log_group (group_id),
-  KEY idx_frs_log_action_item (action_id, item_id)
+  KEY idx_frs_log_group_item (group_id, item_id)
 );
 
 #
