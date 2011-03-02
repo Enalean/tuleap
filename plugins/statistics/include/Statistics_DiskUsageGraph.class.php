258351c96ab4574c31aa389f8ca9c618d1bb2ec3
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-03-02 15:40:25 +0000

    Modify color in graph.

--- plugins/statistics/include/Statistics_DiskUsageGraph.class.php
+++ plugins/statistics/include/Statistics_DiskUsageGraph.class.php
@@ -251,7 +251,7 @@ class Statistics_DiskUsageGraph extends Statistics_DiskUsageOutput {
         $data = array($used, $total-$used);
 
         $usage = new PiePlot($data);
-        $usage->SetSliceColors(array('salmon','green'));
+        $usage->SetSliceColors(array('salmon','darkolivegreen2'));
         $usage->SetLegends(array("Used proportion", "Allowed quota"));
         $graph->legend->SetPos(0.01,0,'right','top');
         $graph->add($usage);
