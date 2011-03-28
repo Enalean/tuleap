7d0197a5dc9c85f5aee24a164707029e2364f893
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-03-28 11:02:30 +0000

    Add wiki attachment deleted table.

---
+++ src/db/mysql/updates/2011/201103281122_add_wiki_attachement_delete_mechanism.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *
+ */
+class b201103281122_add_wiki_attachement_delete_mechanism extends ForgeUpgrade_Bucket {
+
+    public function description() {
+        return <<<EOT
+Add the table wiki_attachment_deleted to manage deleted wiki attachement in order to facilitate their restore later
+EOT;
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $sql = 'ALTER TABLE wiki_attachment ADD delete_date INT(11) UNSIGNED NULL';
+        if ($this->db->tableNameExists('wiki_attachment')) {
+            $res = $this->db->dbh->exec($sql);
+            if ($res === false) {
+                throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occured while adding column delete_date to table wiki_attachement');
+            }
+        }
+
+        $sql = 'CREATE TABLE wiki_attachment_deleted (
+                id INT( 11 ) NOT NULL AUTO_INCREMENT ,
+                group_id INT( 11 ) NOT NULL ,
+                name VARCHAR( 255 ) NOT NULL ,
+                delete_date INT(11) UNSIGNED NULL,
+                purge_date INT(11) UNSIGNED NULL,
+                PRIMARY KEY (id),
+                INDEX idx_delete_date (delete_date),
+                INDEX idx_purge_date (purge_date)
+               );';
+        $this->db->createTable('wiki_attachment_deleted', $sql);
+    }
+
+    public function postUp() {
+        if (!$this->db->columnNameExists('wiki_attachment', 'delete_date')) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('delete_date not created in wiki_attachment');
+        }
+
+        if (!$this->db->tableNameExists('wiki_attachment_deleted')) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotCompleteException('wiki_attachment_deleted table is missing');
+        }
+
+    }
+
+}
+
+?>
