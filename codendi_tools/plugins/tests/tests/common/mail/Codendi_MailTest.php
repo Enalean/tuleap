ea48e1a1b69fbf13c13fc974cd6974fa5c5829cc
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-04-11 10:58:27 +0000

    Add unit test.

---
+++ codendi_tools/plugins/tests/tests/common/mail/Codendi_MailTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) STMicroelectronics, 2004-2011. All rights reserved
+ *
+ * This file is a part of Codendi.
+ *
+ * Codendi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Codendi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+require_once('common/user/User.class.php');
+Mock::generate('User');
+require_once('common/mail/Codendi_Mail.class.php');
+Mock::generatePartial('Codendi_Mail', 'Codendi_MailTestVersion', array());
+
+
+class Codendi_MailTest extends UnitTestCase {
+    /**
+     * Constructor of the test. Can be ommitted.
+     * Usefull to set the name of the test
+     */
+    function Codendi_MailTest() {
+        $this->UnitTestCase('Codendi_Mail test');
+    }
+
+    function testValidateRecipient() {
+        $mail = new Codendi_MailTestVersion($this);
+
+        $user1 = new MockUser();
+        $user1->setReturnValue('getRealName', 'user 1');
+        $user1->setReturnValue('getEmail', 'user_1@codendi.org');
+        $user1->setReturnValue('getStatus', 'A');
+
+        $user2 = new MockUser();
+        $user2->setReturnValue('getRealName', 'user 2');
+        $user2->setReturnValue('getEmail', 'user_2@codendi.org');
+        $user2->setReturnValue('getStatus', 'S');
+
+        $recipArray = array($user1, $user2);
+
+        $recipients = $mail->_validateRecipient($recipArray);
+        $retArray = array(array('real_name' => $user1->getRealName(), 'email' => $user1->getEmail()));
+        $this->assertEqual($recipients, $retArray);
+    }
+}
+?>
--- src/common/mail/Codendi_Mail.class.php
+++ src/common/mail/Codendi_Mail.class.php
@@ -218,7 +218,7 @@ class Codendi_Mail {
     }
 
     function send() {
-        $this->mailHtml->send();
+        return $this->mailHtml->send();
     }
 }
 
