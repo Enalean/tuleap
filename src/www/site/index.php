b96059d1391f41e514144606ccfef52203de20ee
abdelma <abdelma@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-04-20 17:01:59 +0000

    form added on an HTML page (form.html)and a PHP page (index.php: the help tab).

--- src/www/site/index.php
+++ src/www/site/index.php
@@ -10,6 +10,35 @@ $title = $Language->getText('include_layout','Help');
 site_header(array('title' => $title));
 
 include($Language->getContent('help/site'));
+if( isset($_POST['submit']) ){
+	
+	echo "request sent";
+}
+?>
 
+
+
+<form  name="request" action="index.php" method="post" enctype="multipart/form-data">
+    
+     <fieldset ><legend>Submit Help Request:</legend>
+     Type: 
+     <select name="type"> 
+	     <option value"support">Support request</option>
+	     <option value"enhancement">Enhancement request</option>
+     </select><br />
+     Severity: <select name="severity"> 
+     	<option value"minor">Minor</option>
+     	<option value"serious">Serious</option>
+     	<option value"critical">Critical</option>
+     	</select><br />
+     Summary: <input type="text" name="request_summary" /><br />
+     Description:  <textarea name="request_description" cols="50" rows="7"> </textarea> <br />
+     
+ 	 <input enabled = false name="submit" type="submit" value="Submit" />
+	 
+	 </fieldset>
+</form>
+<?php 
 site_footer(array());
+
 ?>
