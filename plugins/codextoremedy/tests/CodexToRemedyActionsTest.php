8ee650cb51d06dbf3924db4d9633187f5197678f
ounish <ounish@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-05-02 17:05:11 +0000

    Add unit tests.

---
+++ plugins/codextoremedy/tests/CodexToRemedyActionsTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) STMicroelectronics, 2011. All Rights Reserved.
+ *
+ * This file is a part of Codendi.
+ *
+ * Codendi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Codendi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('common/include/HTTPRequest.class.php');
+Mock::generate('HTTPRequest');
+require_once('common/valid/ValidFactory.class.php');
+
+require_once(dirname(__FILE__).'/../include/CodexToRemedy.class.php');
+require_once(dirname(__FILE__).'/../include/CodexToRemedyActions.class.php');
+
+class CodexToRemedyActionsTest extends UnitTestCase {
+
+    function testValidateRequestValid() {
+        $request = new MockHTTPRequest();
+        $request->setReturnValue('get', 'valid summary', array('request_summary'));
+        $request->setReturnValue('get', 'valid description', array('request_description'));
+        $request->setReturnValue('get', 1, array('type'));
+        $request->setReturnValue('get', 1, array('severity'));
+        $request->setReturnValue('get', 'john.doe@example.com', array('cc'));
+        $request->setReturnValue('valid', true);
+        $params = CodexToRemedyActions::validateRequest($request);
+        $validParams = array('summary' => 'valid summary',
+                             'description' => 'valid description',
+                             'type' => 1,
+                             'text_type' => 'SUPPORT REQUEST',
+                             'severity' => 1,
+                             'text_severity' => 'Minor',
+                             'cc' => 'john.doe@example.com');
+        $this->assertEqual($params, $validParams);
+    }
+
+    function testValidateRequestNonValid() {
+        $request = new MockHTTPRequest();
+        $request->setReturnValue('get', 'valid summary', array('request_summary'));
+        $request->setReturnValue('get', 'valid description', array('request_description'));
+        $request->setReturnValue('get', 1, array('type'));
+        $request->setReturnValue('get', 1, array('severity'));
+        $request->setReturnValue('valid', false);
+        $params = CodexToRemedyActions::validateRequest($request);
+        $validParams = array('cc' => '');
+        $this->assertEqual($params, $validParams);
+    }
+
+}
+
+?>
