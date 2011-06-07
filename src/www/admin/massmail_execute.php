ea7241c0742c6d2484951606f1a467637c44c2f3
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-06-07 09:24:56 +0000

    -Use Codendi_mail to send HTML mails -Send the mail to a mail address for preview

--- src/www/admin/massmail_execute.php
+++ src/www/admin/massmail_execute.php
@@ -9,6 +9,7 @@
 require_once('pre.php');
 
 require_once('common/mail/Mail.class.php');
+require_once('common/mail/Codendi_Mail.class.php');
 
 
 session_require(array('group'=>1,'admin_flags'=>'A'));
@@ -17,83 +18,104 @@ session_require(array('group'=>1,'admin_flags'=>'A'));
 // LJ The to_name variable has been added here to be used
 // LJ in the mail command later in this script
 switch ($destination) {
-	case 'comm': 
-		$res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) AND mail_va=1 GROUP BY lcase(email)");
-		$to_name = 'Additional Community Mailings Subcribers';
-		break;
-	case 'sf':
-		$res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) AND mail_siteupdates=1 GROUP BY lcase(email)");
-		$to_name = 'Site Updates Subcribers';
-		break;
-	case 'all':
-		$res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) GROUP BY lcase(email)");
-		$to_name = 'All Users';
-		break;
-	case 'admin':
-		$res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
-		."FROM user,user_group WHERE "	
-		."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) AND user_group.admin_flags='A' "
-		."GROUP by lcase(email)");
-		$to_name = 'Project Administrators';
-		break;
-	case 'sfadmin':
-		$res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
-		."FROM user,user_group WHERE "	
-		."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) AND user_group.group_id=1 "
-		."GROUP by lcase(email)");
-		$to_name = $GLOBALS['sys_name'].' Administrators';
-		break;
-	case 'devel':
-		$res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
-		."FROM user,user_group WHERE "
-		."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) GROUP BY lcase(email)");
-		$to_name = 'Project Developers';
-		break;
-	default:
-		exit_error('Unrecognized Post','cannot execute');
+    case 'preview': 
+        break;
+    case 'comm':
+        $res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) AND mail_va=1 GROUP BY lcase(email)");
+        $to_name = 'Additional Community Mailings Subcribers';
+        break;
+    case 'sf':
+        $res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) AND mail_siteupdates=1 GROUP BY lcase(email)");
+        $to_name = 'Site Updates Subcribers';
+        break;
+    case 'all':
+        $res_mail = db_query("SELECT email,user_name FROM user WHERE ( status='A' OR status='R' ) GROUP BY lcase(email)");
+        $to_name = 'All Users';
+        break;
+    case 'admin':
+        $res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
+        ."FROM user,user_group WHERE "
+        ."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) AND user_group.admin_flags='A' "
+        ."GROUP by lcase(email)");
+        $to_name = 'Project Administrators';
+        break;
+    case 'sfadmin':
+        $res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
+        ."FROM user,user_group WHERE "
+        ."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) AND user_group.group_id=1 "
+        ."GROUP by lcase(email)");
+        $to_name = $GLOBALS['sys_name'].' Administrators';
+        break;
+    case 'devel':
+        $res_mail = db_query("SELECT user.email AS email,user.user_name AS user_name "
+        ."FROM user,user_group WHERE "
+        ."user.user_id=user_group.user_id AND ( user.status='A' OR user.status='R' ) GROUP BY lcase(email)");
+        $to_name = 'Project Developers';
+        break;
+    default:
+        exit_error('Unrecognized Post','cannot execute');
 }
 header ('Content-Type: text/plain');
 
 print $Language->getText('admin_massmail_execute','post_recvd')."\n";
 flush();
 
-
-print $Language->getText('admin_massmail_execute','mailing',array(db_numrows($res_mail)))." ($to_name)\n\n";
-flush();
-
-$rows=db_numrows($res_mail);
-
 list($host,$port) = explode(':',$GLOBALS['sys_default_domain']);
-$mail =& new Mail();
+if (isset($body_format) && $body_format) {
+    $mail = new Codendi_Mail();
+} else {
+    $mail = new Mail();
+}
 $mail->setTo($GLOBALS['sys_noreply']);
 $mail->setFrom($GLOBALS['sys_noreply']);
 $mail->setSubject(stripslashes($mail_subject));
 $mail->setBody(stripslashes($mail_message));
 
-$tolist = '';
-for ($i=1; $i<=$rows; $i++) {
-	$tolist .= db_result($res_mail,$i-1,'email').', ';
-    if ($i % 25 == 0) {
-		//spawn sendmail for 25 addresses at a time
+if (isset($res_mail)) {
+    print $Language->getText('admin_massmail_execute','mailing',array(db_numrows($res_mail)))." ($to_name)\n\n";
+    flush();
+
+    $rows=db_numrows($res_mail);
+
+    $tolist = '';
+
+    for ($i=1; $i<=$rows; $i++) {
+        $tolist .= db_result($res_mail,$i-1,'email').', ';
+        if ($i % 25 == 0) {
+            //spawn sendmail for 25 addresses at a time
+            $mail->setBcc($tolist);
+            $mail->setTo($GLOBALS['sys_noreply']);
+            if ($mail->send()) {
+                print "\n".$Language->getText('admin_massmail_execute','sending').": ".$tolist;
+            } else {
+                print "\n".$GLOBALS['Language']->getText('global', 'mail_failed', array($GLOBALS['sys_email_admin'])).": ".$tolist;
+            }
+            flush();
+            usleep(2000000);
+            $tolist='';
+            if (isset($body_format) && $body_format) {
+                $m = $mail->getMail();
+                $m->clearRecipients();
+            }
+        }
+    }
+
+    //send the last of the messages.
+    if (strlen($tolist) > 0) {
         $mail->setBcc($tolist);
+        $mail->setTo($GLOBALS['sys_noreply']);
         if ($mail->send()) {
             print "\n".$Language->getText('admin_massmail_execute','sending').": ".$tolist;
         } else {
             print "\n".$GLOBALS['Language']->getText('global', 'mail_failed', array($GLOBALS['sys_email_admin'])).": ".$tolist;
         }
-		flush();
-        usleep(2000000);
-		$tolist='';
-	}
-}
-
-//send the last of the messages.
-if (strlen($tolist) > 0) {
-    $mail->setBcc($tolist);
+    }
+} else {
+    $mail->setBcc($preview_destination, true);
     if ($mail->send()) {
-        print "\n".$Language->getText('admin_massmail_execute','sending').": ".$tolist;
+        print "\n".$Language->getText('admin_massmail_execute','sending').": ".$preview_destination;
     } else {
-        print "\n".$GLOBALS['Language']->getText('global', 'mail_failed', array($GLOBALS['sys_email_admin'])).": ".$tolist;
+        print "\n".$GLOBALS['Language']->getText('global', 'mail_failed', array($GLOBALS['sys_email_admin'])).": ".$preview_destination;
     }
 }
 print "\n".$Language->getText('admin_massmail_execute','done')."\n";
