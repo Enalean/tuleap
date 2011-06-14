1440f096e832742b23512d6a25f2fea794043f91
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-06-14 20:29:00 +0000

    Fix template

--- plugins/template/include/templatePlugin.class.php
+++ plugins/template/include/templatePlugin.class.php
@@ -1,33 +1,27 @@
 <?php
 
-require_once('common/plugin/Plugin.class.php');
+require_once 'common/plugin/Plugin.class.php';
 
 class TemplatePlugin extends Plugin {
-	
-	function TemplatePlugin($id) {
-		$this->Plugin($id);
-        $this->_addHook('hook_name');
-        $this->_addHook('site_admin_option_hook', 'siteAdminHooks', false);
-	}
-	
+
+    function __construct($id) {
+        parent::__construct($id);
+        $this->setScope(self::SCOPE_PROJECT);
+        $this->_addHook('site_admin_option_hook', 'site_admin_option_hook', false);
+    }
+
     function getPluginInfo() {
         if (!is_a($this->pluginInfo, 'TemplatePluginInfo')) {
-            require_once('TemplatePluginInfo.class.php');
-            $this->pluginInfo =& new TemplatePluginInfo($this);
+            include_once 'TemplatePluginInfo.class.php';
+            $this->pluginInfo = new TemplatePluginInfo($this);
         }
         return $this->pluginInfo;
     }
 
-	function CallHook($hook, $params) {
-		if ($hook == 'hook_name') {
-			//do Something
-		}
-	}
-    
-    function siteAdminHooks($params) {
+    function site_admin_option_hook($params) {
         echo '<li><a href="'.$this->getPluginPath().'/">Template</a></li>';
     }
-    
+
     function process() {
         echo '<h1>Template</h1>';
         echo $this->getPluginInfo()->getpropVal('answer');
