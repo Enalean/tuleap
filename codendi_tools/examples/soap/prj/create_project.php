7687d0e914567879a9b9f73f41e9e6399193a0a5
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-06-15 10:15:18 +0000

    Add comment

--- codendi_tools/examples/soap/prj/create_project.php
+++ codendi_tools/examples/soap/prj/create_project.php
@@ -1,13 +1,34 @@
 <?php
+/**
+ * Copyright (c) Enalean, 2011. All Rights Reserved.
+ *
+ * This file is a part of Codendi.
+ *
+ * Codendi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Codendi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codendi; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 if ($argc < 4) {
     die("Usage: ".$argv[0]." requester shortname longname [member 1] [member 2] [...]\n");
 }
 
+// Establish connexion to the server
 $client = new SoapClient(null, array('location'   => "http://localhost:2080/plugins/soapproject/",
                                      'uri'        => 'http://localhost:2080/plugins/soapproject/',
                                      'cache_wsdl' => WSDL_CACHE_NONE));
 
+//$client->addProject(requester, shortname, longname);
 $prjId = $client->addProject($argv[1], $argv[2], $argv[3]);
 
 echo "New Project ID: $prjId\n";
@@ -16,6 +37,7 @@ for($i = 4; $i < $argc; $i++) {
     var_dump($client->addProjectMember($prjId, $argv[$i]));
 }
 
+/*
 for($i = 4; $i < $argc; $i++) {
     var_dump($client->removeProjectMember($prjId, $argv[$i]));
 }
@@ -23,5 +45,5 @@ for($i = 4; $i < $argc; $i++) {
 for($i = 4; $i < $argc; $i++) {
     var_dump($client->addProjectMember($prjId, $argv[$i]));
 }
-
+*/
 ?>
