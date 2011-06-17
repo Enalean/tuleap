70977e1a80e2b0ad0075664d72da2202dc7f2205
hosniah <hosniah@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-06-17 08:57:31 +0000

    Checking the DataAccessResult object validity.

--- plugins/requesthelp/include/RequestHelpActions.class.php
+++ plugins/requesthelp/include/RequestHelpActions.class.php
@@ -317,7 +317,7 @@ class RequestHelpActions extends PluginAction {
             $ldapUm = new LDAP_UserManager($ldapPlugin->getLdap());
             $userId[] = $user->getId();
             $ldapLogin = $ldapUm->getLdapLoginFromUserIds($userId);
-            if($ldapLogin->rowCount())
+            if($ldapLogin && !$ldapLogin->isError()&& $ldapLogin->rowCount()> 0)
             {
                 $ldapLoginArray = $ldapLogin->getRow();
                 $requester = $ldapLoginArray['ldap_uid'];
