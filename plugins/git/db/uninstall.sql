e96508bbf59237fb615f82ff8e7c8012222ffa25
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-07-07 16:57:41 +0000

    Uninstall

--- plugins/git/db/uninstall.sql
+++ plugins/git/db/uninstall.sql
@@ -5,3 +5,5 @@ DELETE FROM service WHERE short_name='git';
 DELETE FROM reference_group WHERE reference_id=30;
 DELETE FROM reference WHERE id=30;
 
+DELETE FROM permissions_values WHERE permission_type IN ('PLUGIN_GIT_READ', 'PLUGIN_GIT_WRITE', 'PLUGIN_GIT_WPLUS');
+
