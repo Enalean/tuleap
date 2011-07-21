4d9c3ad8435a54991d15b28f4ca7e05375be8490
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-07-21 15:02:16 +0000

    Export project history in a CSV file.

--- src/www/project/admin/history.php
+++ src/www/project/admin/history.php
@@ -8,13 +8,10 @@
 
 require_once('pre.php');    
 require_once('www/project/admin/project_admin_utils.php');
+require_once('www/project/export/project_export_utils.php');
 
 
 session_require(array('group'=>$group_id,'admin_flags'=>'A'));
-
-project_admin_header(array('title'=>$Language->getText('project_admin_history','proj_history'),'group'=>$group_id));
-
-echo $Language->getText('project_admin_history','proj_change_log_msg');
 
 $request = HTTPRequest::instance();
 
@@ -33,8 +30,22 @@ if ( !$offset || $offset < 0 ) {
 }
 $limit  = 50;
 
+if (isset($_REQUEST['SUBMIT'])) {
+    export_grouphistory($group_id);
+    exit;
+}
+
+project_admin_header(array('title'=>$Language->getText('project_admin_history','proj_history'),'group'=>$group_id));
+
+echo $Language->getText('project_admin_history','proj_change_log_msg');
+
 //for pagination
 echo show_grouphistory($group_id, $offset, $limit);
+
+echo '<BR><FORM METHOD="POST" NAME="project_history_export_form">
+    <TABLE align="left"><TR><TD>
+    <INPUT TYPE="SUBMIT" NAME="SUBMIT" VALUE="'.$GLOBALS['Language']->getText('project_stats_source_code_access','logs_export').'">
+    </TD></TR></TABLE></FORM><BR><P>';
 
 project_admin_footer(array());
 ?>
--- src/www/project/admin/project_admin_utils.php
+++ src/www/project/admin/project_admin_utils.php
@@ -83,7 +83,10 @@ function group_get_history ($offset, $limit, $group_id=false) {
     $sql='select SQL_CALC_FOUND_ROWS group_history.field_name,group_history.old_value,group_history.date,user.user_name '.
          'FROM group_history,user '.
          'WHERE group_history.mod_by=user.user_id '.
-         'AND group_id='.db_ei($group_id).' ORDER BY group_history.date DESC LIMIT '.db_ei($offset).', '.db_ei($limit);
+         'AND group_id='.db_ei($group_id).' ORDER BY group_history.date DESC';
+    if ($offset > 0 || $limit > 0) {
+         $sql .= ' LIMIT '.db_ei($offset).', '.db_ei($limit);
+    }
 
     $res = db_query($sql);
     $sql = 'SELECT FOUND_ROWS() as nb';
@@ -217,6 +220,82 @@ function show_grouphistory ($group_id, $offset, $limit) {
 	}       
 }       
 
+/**
+ * Export project history to a csv file
+ *
+ * @param Integer $group_id
+ * @param Integer $offset
+ * @param Integer $limit
+ *
+ * @return void
+ */
+function export_grouphistory ($group_id) {
+    global $Language;
+
+    header ('Content-Type: text/csv');
+    header ('Content-Disposition: filename=project_history.csv');
+
+    $eol = "\n";
+
+    $col_list = array('event', 'val', 'date', 'by');
+    $documents_title = array ('event' => $Language->getText('project_admin_utils','event'),
+                              'val'   => $Language->getText('project_admin_utils','val'),
+                              'date'  => $Language->getText('project_admin_utils','date'),
+                              'by'    => $Language->getText('global','by'));
+    echo build_csv_header($col_list, $documents_title).$eol;
+
+    $res = group_get_history(0, 0, $group_id );
+
+    $hp = Codendi_HTMLPurifier::instance();
+
+    if ($res['numrows'] > 0) {
+        while ($row = db_fetch_array($res['history'])) {
+            $field = $row['field_name'];
+
+            // see if there are any arguments after the message key
+            // format is "msg_key ## arg1||arg2||...
+            // If msg_key cannot be found in the localized message
+            // catalog then display the msg has is because this is very
+            // likely a legacy message (pre-localization version)
+            if (strpos($field," %% ") !== false) {
+                list($msg_key, $args) = explode(" %% ",$field);
+                if ($args) {
+                    $arr_args = explode('||',$args);
+                }
+            } else {
+                $msg_key=$field;
+                $arr_args="";
+            }
+            $msg = $Language->getText('project_admin_utils', $msg_key, $arr_args);
+            if (!(strpos($msg,"*** Unkown msg") === false)) {
+                $msg = $field;
+            }
+
+            $val = $row['old_value'];
+            if ($msg_key == "perm_granted_for_field") {
+                $pattern = '/ugroup_([^ ,]*)_name_key/';
+                preg_match_all($pattern,$val,$matches);
+
+                if (!empty($matches[0])) {
+                    foreach ($matches[0] as $match) {
+                        $val = str_replace($match,$Language->getText('project_ugroup', $match),$val);
+                    }
+                }
+            } else if ($msg_key == "group_type") {
+                $template =& TemplateSingleton::instance();
+                $val = $template->getLabel($val);
+            }
+
+            $documents_body = array ('event' => $hp->purify($msg, CODENDI_PURIFIER_BASIC, $group_id),
+                                     'val'   => $hp->purify($val),
+                                     'date'  => format_date($GLOBALS['Language']->getText('system', 'datefmt'),$row['date']),
+                                     'by'    => user_get_name_display_from_unix($row['user_name']));
+            echo build_csv_record($col_list, $documents_body).$eol;
+        }
+
+    }
+    echo build_csv_header($col_list, array()).$eol;
+}
 
 function project_admin_display_bullet_user($user_id, $action, $url = null) {
     if ($action == 'add') {
