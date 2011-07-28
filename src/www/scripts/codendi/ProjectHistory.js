d6282f3695de8e44dcdf0da7cf60450b9c60d0dd
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-07-28 15:55:43 +0000

    Commit JS also.

--- src/www/scripts/codendi/ProjectHistory.js
+++ src/www/scripts/codendi/ProjectHistory.js
@@ -16,7 +16,7 @@
  */
 
 var ProjectHistory = Class.create({
-    initialize: function (sub_events_array) {
+    initialize: function (sub_events_array, selected_sub_events) {
         if (!sub_events_array) {
             throw 'sub_events_array is mandatory!';
         }
@@ -25,7 +25,9 @@ var ProjectHistory = Class.create({
         title.observe('click', this.toggleForm);
         // We may make the form hidden by default
         //$('project_history_search').hide();
-        Event.observe($('events_box'), 'change', this.SelectSubEvent.bindAsEventListener(this)); 
+        Event.observe($('events_box'), 'change', this.SelectSubEvent.bindAsEventListener(this));
+        // Load sub events content when page loads
+        this.SelectSubEvent(selected_sub_events);
      },
     toggleForm: function() {
         // Toggle search form
@@ -38,85 +40,90 @@ var ProjectHistory = Class.create({
             icon.src = icon.src.replace('toggle_minus.png', 'toggle_plus.png');
         }
     },
-    SelectSubEvent: function() {
+    SelectSubEvent: function(selected_sub_events) {
         this.removeAllOptions($('sub_events_box'));
         this.addOption('choose_event');
         //Try to use a loop instead
         //Permission section
-        if($('events_box').value == 'Permissions'){
-            this.addOption("perm_reset_for_field");
-            this.addOption("perm_reset_for_tracker");  
-            this.addOption("perm_reset_for_package"); 
-            this.addOption("perm_reset_for_release");  
-            this.addOption("perm_reset_for_document"); 
-            this.addOption("perm_reset_for_folder");   
-            this.addOption("perm_reset_for_docgroup"); 
-            this.addOption("perm_reset_for_wiki"); 
-            this.addOption("perm_reset_for_wikipage"); 
-            this.addOption("perm_reset_for_wikiattachment");   
-            this.addOption("perm_reset_for_object");
-            this.addOption("perm_granted_for_field");
-            this.addOption("perm_granted_for_tracker");  
-            this.addOption("perm_granted_for_package"); 
-            this.addOption("perm_granted_for_release");  
-            this.addOption("perm_granted_for_document"); 
-            this.addOption("perm_granted_for_folder");   
-            this.addOption("perm_granted_for_docgroup"); 
-            this.addOption("perm_granted_for_wiki"); 
-            this.addOption("perm_granted_for_wikipage"); 
-            this.addOption("perm_granted_for_wikiattachment");
-            this.addOption("perm_granted_for_object");
+        if ($('events_box').value == 'Permissions') {
+            this.addOption("perm_reset_for_field", selected_sub_events['perm_reset_for_field']);
+            this.addOption("perm_reset_for_tracker", selected_sub_events['perm_reset_for_tracker']);
+            this.addOption("perm_reset_for_package", selected_sub_events['perm_reset_for_package']);
+            this.addOption("perm_reset_for_release", selected_sub_events['perm_reset_for_release']);
+            this.addOption("perm_reset_for_document", selected_sub_events['perm_reset_for_document']);
+            this.addOption("perm_reset_for_folder", selected_sub_events['perm_reset_for_folder']);
+            this.addOption("perm_reset_for_docgroup", selected_sub_events['perm_reset_for_docgroup']);
+            this.addOption("perm_reset_for_wiki", selected_sub_events['perm_reset_for_wiki']);
+            this.addOption("perm_reset_for_wikipage", selected_sub_events['perm_reset_for_wikipage']);
+            this.addOption("perm_reset_for_wikiattachment", selected_sub_events['perm_reset_for_wikiattachment']);
+            this.addOption("perm_reset_for_object", selected_sub_events['perm_reset_for_object']);
+            this.addOption("perm_granted_for_field", selected_sub_events['perm_granted_for_field']);
+            this.addOption("perm_granted_for_tracker", selected_sub_events['perm_granted_for_tracker']);
+            this.addOption("perm_granted_for_package", selected_sub_events['perm_granted_for_package']);
+            this.addOption("perm_granted_for_release", selected_sub_events['perm_granted_for_release']);
+            this.addOption("perm_granted_for_document", selected_sub_events['perm_granted_for_document']);
+            this.addOption("perm_granted_for_folder", selected_sub_events['perm_granted_for_folder']);
+            this.addOption("perm_granted_for_docgroup", selected_sub_events['perm_granted_for_docgroup']);
+            this.addOption("perm_granted_for_wiki", selected_sub_events['perm_granted_for_wiki']);
+            this.addOption("perm_granted_for_wikipage", selected_sub_events['perm_granted_for_wikipage']);
+            this.addOption("perm_granted_for_wikiattachment", selected_sub_events['perm_granted_for_wikiattachment']);
+            this.addOption("perm_granted_for_object", selected_sub_events['perm_granted_for_object']);
         }
 
         //Project section
-        if($('events_box').value == "Project"){
-            this.addOption("rename_done");
-            this.addOption("rename_with_error");
-            this.addOption("approved");
-            this.addOption("deleted");
-            this.addOption("rename_request");
-            this.addOption("is_public");
-            this.addOption("group_type");
-            this.addOption("http_domain");
-            this.addOption("unix_box");
-            this.addOption("changed_public_info");
-            this.addOption("changed_trove");
-            this.addOption("membership_request_updated");
-            this.addOption("import");
-            this.addOption("mass_change");
+        if ($('events_box').value == "Project") {
+            this.addOption("rename_done", selected_sub_events['rename_done']);
+            this.addOption("rename_with_error", selected_sub_events['rename_with_error']);
+            this.addOption("approved", selected_sub_events['approved']);
+            this.addOption("deleted", selected_sub_events['deleted']);
+            this.addOption("rename_request", selected_sub_events['rename_request']);
+            this.addOption("is_public", selected_sub_events['is_public']);
+            this.addOption("group_type", selected_sub_events['group_type']);
+            this.addOption("http_domain", selected_sub_events['http_domain']);
+            this.addOption("unix_box", selected_sub_events['unix_box']);
+            this.addOption("changed_public_info", selected_sub_events['changed_public_info']);
+            this.addOption("changed_trove", selected_sub_events['changed_trove']);
+            this.addOption("membership_request_updated", selected_sub_events['membership_request_updated']);
+            this.addOption("import", selected_sub_events['import']);
+            this.addOption("mass_change", selected_sub_events['mass_change']);
         }
 
         //User group section
-       if($('events_box').value == "User Group"){
-            this.addOption("upd_ug");
-            this.addOption("del_ug");
-            this.addOption("changed_member_perm");
+       if ($('events_box').value == "User Group") {
+            this.addOption("upd_ug", selected_sub_events['upd_ug']);
+            this.addOption("del_ug", selected_sub_events['del_ug']);
+            this.addOption("changed_member_perm", selected_sub_events['changed_member_perm']);
         }
 
         //Users section
-        if($('events_box').value == "Users"){
-            this.addOption("changed_personal_email_notif");
-            this.addOption("added_user");
-            this.addOption("removed_user");
+        if ($('events_box').value == "Users") {
+            this.addOption("changed_personal_email_notif", selected_sub_events['changed_personal_email_notif']);
+            this.addOption("added_user", selected_sub_events['added_user']);
+            this.addOption("removed_user", selected_sub_events['removed_user']);
         }
 
         //Uncatogorised items section
-        if($('events_box').value == "Others"){
-            this.addOption("changed_bts_form_message");
-            this.addOption("changed_bts_allow_anon");
-            this.addOption("changed_patch_mgr_settings");
-            this.addOption("changed_task_mgr_other_settings");
-            this.addOption("changed_sr_settings");
+        if ($('events_box').value == "Others") {
+            this.addOption("changed_bts_form_message", selected_sub_events['changed_bts_form_message']);
+            this.addOption("changed_bts_allow_anon", selected_sub_events['changed_bts_allow_anon']);
+            this.addOption("changed_patch_mgr_settings", selected_sub_events['changed_patch_mgr_settings']);
+            this.addOption("changed_task_mgr_other_settings", selected_sub_events['changed_task_mgr_other_settings']);
+            this.addOption("changed_sr_settings", selected_sub_events['changed_sr_settings']);
         }
     },
     removeAllOptions: function(selectbox) {
         var i;
-        for(i = selectbox.options.length-1; i>=0; i--) {
+        for (i = selectbox.options.length-1; i>=0; i--) {
             selectbox.remove(i);
         }
     },
-    addOption: function(value) {
-        var optn = Builder.node('option', {value:value}, this.sub_events_array[value]);
+    addOption: function(value, selected) {
+        var optn = Builder.node('option', {'value' : value}, this.sub_events_array[value]);
         $('sub_events_box').appendChild(optn);
+        if (selected) {
+            optn.selected = true;
+        } else {
+            optn.selected = false;
+        }
     }
 });
