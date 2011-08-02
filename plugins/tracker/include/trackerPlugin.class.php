aeb7fe58d21b9a90594f09894a4315e1625843fa
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-08-02 14:10:45 +0000

    Plugin tracker skeleton

---
+++ plugins/tracker/include/trackerPlugin.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Copyright (c) Xerox, 2011. All Rights Reserved.
+ *
+ * Originally written by Nicolas Terray, 2011. Xerox Codendi Team.
+ *
+ * This file is a part of Codendi.
+ *
+ * Codendi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Codendi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Codendi; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+require_once('common/plugin/Plugin.class.php');
+
+/**
+ * trackerPlugin
+ */
+class trackerPlugin extends Plugin {
+	
+	function trackerPlugin($id) {
+		$this->Plugin($id);
+        $this->_addHook('site_admin_option_hook', 'siteAdminHooks', false);
+        $this->_addHook('cssfile', 'cssFile', false);
+	}
+	
+    function &getPluginInfo() {
+        if (!is_a($this->pluginInfo, 'trackerPluginInfo')) {
+            require_once('trackerPluginInfo.class.php');
+            $this->pluginInfo =& new trackerPluginInfo($this);
+        }
+        return $this->pluginInfo;
+    }
+    
+    function siteAdminHooks($params) {
+        echo '<li><a href="'.$this->getPluginPath().'/">tracker</a></li>';
+    }
+    
+    function cssFile($params) {
+        // Only show the stylesheet if we're actually in the tracker pages.
+        // This stops styles inadvertently clashing with the main site.
+        if (strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0) {
+            echo '<link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/style.css" />';
+        }
+    }
+    
+    function process() {
+        require_once('tracker.class.php');
+        $controler =& new tracker();
+        $controler->process();
+    }
+}
+
+?>
