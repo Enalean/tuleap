900e01760192345138e4f3fb4a1e97dea6302672
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-08-16 12:36:17 +0000

    Re-import revision #12038 - Patch prototype lib to fix an issue with ajax autocompleter result display (Only first result returned by the ajax call isn't displayed. Next ajax calls gets their results displayed).

--- src/www/scripts/prototype/prototype.js
+++ src/www/scripts/prototype/prototype.js
@@ -2370,7 +2370,7 @@ Element.Methods = {
     if (element.offsetParent) return $(element.offsetParent);
     if (element == document.body) return $(element);
 
-    while ((element = element.parentNode) && element != document.body)
+    while ((element = element.parentNode) && element != document.body && Object.isElement(element))
       if (Element.getStyle(element, 'position') != 'static')
         return $(element);
 
