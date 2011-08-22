c371015c047855a4f46ec501c00a22d88fe79336
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-08-22 08:03:15 +0000

    Merge /contrib/st/enhancement/80_first_integration_of_tracker_v5 (r18247 through r18248). Import LXC unit test build.

--- codendi_tools/continuous_integration/ci_build.sh
+++ codendi_tools/continuous_integration/ci_build.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-#
-# CI build: build Codendi project on a Continuous Integration server
-# Usage: sh ci_build.sh
-# Note: WORKSPACE is a variable of Hudson
-#
+usage() {
+    cat <<EOF
+Usage: $1 [options]
+Options
+  --without-svn-sniff  Disable usage of SVN log to discover new files to sniff
+  --srcdir=<value>    Specify a sourcedir
+  --workspace=<value>  Absolute path to where 'src-dir' stands
 
-sys_default_domain="codendi.org";
-#sys_ip_address='127.0.0.1';
-local_module_directory="codendi-src";
-port="80";
-sys_org_name="Codendi";
-sys_long_org_name="Codendi";
-codendi_dir="$WORKSPACE";
-codendi_src="$WORKSPACE/$local_module_directory"
-
-
-cd "$codendi_src"
-
-
+Note: $WORKSPACE is a variable of Hudson/Jenkins and might replace --workspace
+EOF
+}
 
 substitute() {
   # $1: filename, $2: string to match, $3: replacement string
@@ -28,6 +20,48 @@ substitute() {
   replacement=`echo $3 | sed "s|/|\\\\\/|g"`
   perl -pi -e "s/$2/$replacement/g" $1
 }
+
+##
+## Default values
+##
+sys_default_domain="codendi.org";
+local_module_directory="codendi-src";
+port="80";
+sys_org_name="Codendi";
+sys_long_org_name="Codendi";
+sniff_svn="true"
+
+##
+## Parse options
+##
+options=`getopt -o h -l help,without-svn-sniff,srcdir:,workspace: -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; usage $0 ;exit 1 ; fi
+eval set -- "$options"
+while true
+do
+    case "$1" in
+	-h|--help)
+	    usage $0
+	    exit 0;;
+	--without-svn-sniff)
+	    sniff_svn="false"
+	    shift 1;;
+	--srcdir)
+	    local_module_directory=$2; 
+	    shift 2;;
+	--workspace)
+	    WORKSPACE=$2; 
+	    shift 2;;
+	 *)
+	    break;;
+    esac
+done
+# Options post treatment
+codendi_src="$WORKSPACE/$local_module_directory"
+
+# Go!
+set -x
+cd "$codendi_src"
 
 # Create /var/tmp/codendi_cache dir
 mkdir -p ../var/
@@ -54,15 +88,15 @@ substitute '../etc/codendi/conf/local.inc' '%sys_org_name%' "Xerox"
 substitute '../etc/codendi/conf/local.inc' '%sys_long_org_name%' "Xerox Corp" 
 substitute '../etc/codendi/conf/local.inc' '%sys_fullname%' "$sys_default_domain" 
 substitute '../etc/codendi/conf/local.inc' '%sys_win_domain%' " " 
-substitute '../etc/codendi/conf/local.inc' '\/usr\/share\/codendi' "$codendi_dir/$local_module_directory"
-substitute '../etc/codendi/conf/local.inc' '\/var\/lib\/codendi' "$codendi_dir/var/lib/codendi"
-substitute '../etc/codendi/conf/local.inc' '\/var\/log\/codendi' "$codendi_dir/var/log/codendi"
-substitute '../etc/codendi/conf/local.inc' '\/etc\/codendi' "$codendi_dir/etc/codendi"
-substitute '../etc/codendi/conf/local.inc' '\/usr\/lib\/codendi\/bin' "$codendi_dir/etc/codendi"
+substitute '../etc/codendi/conf/local.inc' '\/usr\/share\/codendi' "$WORKSPACE/$local_module_directory"
+substitute '../etc/codendi/conf/local.inc' '\/var\/lib\/codendi' "$WORKSPACE/var/lib/codendi"
+substitute '../etc/codendi/conf/local.inc' '\/var\/log\/codendi' "$WORKSPACE/var/log/codendi"
+substitute '../etc/codendi/conf/local.inc' '\/etc\/codendi' "$WORKSPACE/etc/codendi"
+substitute '../etc/codendi/conf/local.inc' '\/usr\/lib\/codendi\/bin' "$WORKSPACE/etc/codendi"
 substitute '../etc/codendi/conf/local.inc' '^\$sys_https_host ' "// \\\$sys_https_host"
 substitute '../etc/codendi/conf/local.inc' '\/usr\/share\/htmlpurifier' "/usr/share/htmlpurifier"
 substitute '../etc/codendi/conf/local.inc' '\/usr\/share\/jpgraph' "/usr/share/jpgraph"
-substitute '../etc/codendi/conf/local.inc' '\/var\/tmp' "$codendi_dir/var/tmp"
+substitute '../etc/codendi/conf/local.inc' '\/var\/tmp' "$WORKSPACE/var/tmp"
 
 # Set environment var CODENDI_LOCAL_INC
 export CODENDI_LOCAL_INC="$WORKSPACE/etc/codendi/conf/local.inc"
@@ -79,7 +113,11 @@ php -d include_path="$codendi_src/src/www/include:$codendi_src/src:/usr/share/pe
 # Checkstyle
 pushd .
 cd "$codendi_src"
-files=$(php "$codendi_src/codendi_tools/continuous_integration/findFilesToSniff.php")
+
+files=""
+if [ "$sniff_svn" = "true" ]; then
+    files=$(php "$codendi_src/codendi_tools/continuous_integration/findFilesToSniff.php")
+fi
 
 php -d memory_limit=256M /usr/bin/phpcs --standard="$codendi_src/codendi_tools/utils/phpcs/Codendi" "$codendi_src/src/common/chart" "$codendi_src/src/common/backend" --report=checkstyle -n --ignore=*/phpwiki/* --ignore="*/webdav/lib/*" $files > $WORKSPACE/var/tmp/checkstyle.xml || true
 popd
