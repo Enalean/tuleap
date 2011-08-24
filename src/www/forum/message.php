13cab4de1e5c9575f47d730da226f6897b221608
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-08-24 12:57:47 +0000

    Require_once instead of  require : fix issue when opening a forum message.

--- src/www/forum/message.php
+++ src/www/forum/message.php
@@ -7,8 +7,8 @@
 // 
 
 require_once('pre.php');
-require('../forum/forum_utils.php');
-require('common/reference/CrossReferenceFactory.class.php');
+require_once('../forum/forum_utils.php');
+require_once('common/reference/CrossReferenceFactory.class.php');
 
 $request =& HTTPRequest::instance();
 
