0b393a97dd6e7ccde3426d4731cd40668e8e2889
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-08-24 09:09:37 +0000

    Add an escapeshellarg.

--- plugins/git/include/Git_GitoliteDriver.class.php
+++ plugins/git/include/Git_GitoliteDriver.class.php
@@ -455,7 +455,7 @@ class Git_GitoliteDriver {
            throw new GitDriverErrorException('Empty path or permission denied '.$path);
         }
         $rcode = 0;
-        $output = system('rm -fr '.$path, $rcode);
+        $output = system('rm -fr '.escapeshellarg($path), $rcode);
         if ( $rcode != 0 ) {
            throw new GitDriverErrorException('Unable to delete path '.$path);
         }
