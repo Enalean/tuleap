64b0179c41e3201adede7ed235bbea51d919dd6a
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-09-06 15:06:20 +0000

    Fix technology name in the disclaimer

--- src/www/project/admin/project_history.php
+++ src/www/project/admin/project_history.php
@@ -1,23 +1,19 @@
 <?php
 /**
- * Copyright (c) STMicroelectronics, 2011. All Rights Reserved.
+ * Copyright (c) STMicroelectronics 2011. All rights reserved
  *
- *
- * This file is a part of Codendi.
- *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once('pre.php');
