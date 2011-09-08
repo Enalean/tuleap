fdcad8626b48f594f20e47a0d0ff99a34343427e
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-09-08 16:04:14 +0000

    Testing new ci.

--- codendi_tools/plugins/tests/tests/common/html/HTML_Element_SelectboxTest.php
+++ codendi_tools/plugins/tests/tests/common/html/HTML_Element_SelectboxTest.php
@@ -46,7 +46,9 @@ class HTML_Element_SelectboxTest extends UnitTestCase {
     function testAddMultipleOptions() {
         $selectbox = new HTML_Element_Selectbox('label', 'name', 'value', false);
         $selectbox->addMultipleOptions(array('one' => '1', 'two' => '2'), 'two');
-        $this->assertEqual('<select id="customfield_3" name="name" ><option value="one" >1</option><option value="two" selected="selected">2</option></select>', $selectbox->renderValue());
+
+        // Just to make tests fail will be reverted soon.
+        $this->assertTrue(false);
     }
 
 }
