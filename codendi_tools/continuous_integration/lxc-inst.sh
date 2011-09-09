3bc90227b9ecbea5f8b4fcc0036a61c95f9fb5c0
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-09-09 10:10:29 +0000

    Script able to install and launch a freshly built tuleap.

--- codendi_tools/continuous_integration/lxc-inst.sh
+++ codendi_tools/continuous_integration/lxc-inst.sh
@@ -2,7 +2,42 @@
 
 ip_address=$1
 
-perl -pi -e 's%baseurl=http://mirror.bytemark.co.uk%baseurl=ftp://degaine/mirror.centos.org%' /etc/yum.repos.d/CentOS-Base.repo
-yum install -y --disablerepo=epel tuleap-all
+# Take the local centos mirror
+perl -pi -e 's%baseurl=http://mirror.bytemark.co.uk/centos%baseurl=ftp://degaine.cro.enalean.com/ftp.centos.org%' /etc/yum.repos.d/CentOS-Base.repo
+
+# Configure the Tuleap repositories
+# - Tuleap: the main repo for dependencies
+# - Tuleap-dev: the fresh one
+cat <<'EOF' >/etc/yum.repos.d/Tuleap.repo
+[Tuleap]
+name=Tuleap
+baseurl=ftp://ci.tuleap.net/yum/tuleap/dev/$basearch
+enabled=1
+gpgcheck=0
+exclude=tuleap*
+
+[Tuleap-dev]
+name=Tuleap-dev
+baseurl=http://degaine:8080/job/build_101_graphs_on_tracker_v5/ws/yum/
+enabled=1
+gpgcheck=0
+EOF
+
+# install rpms
+# local centos mirror may produce ftp io errors
+# => we need to retry install
+maxretry=10
+until [ -d "/usr/share/tuleap-install" ]; do
+    if [ "$maxretry" -eq "0" ]; then
+        echo "*** Error: cannot install Tuleap after 10 attempts";
+        exit 1
+    fi
+    yum install -y --disablerepo=epel tuleap-all
+    maxretry=$(($maxretry-1))
+done
+
+# install Tuleap
+# TODO: redirect output and errors in a file for debugging purpose
 bash -x /usr/share/tuleap-install/setup.sh --auto-passwd --without-bind-config --disable-subdomains --sys-default-domain=$ip_address --sys-fullname=$ip_address --sys-ip-address=$ip_address --sys-org-name=Tuleap --sys-long-org-name=Tuleap
+
 
