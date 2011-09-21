b1e46521f242cc44c2613c73575877094e726f20
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-09-21 09:39:21 +0000

    Zend raises an exception if postfix(or any other MTA) is not installed.

--- src/common/tracker/Artifact.class.php
+++ src/common/tracker/Artifact.class.php
@@ -2526,7 +2526,11 @@ class Artifact extends Error {
         $mail->setFrom($GLOBALS['sys_noreply']);
         $mail->setTo(join(',', $to));
         $mail->setSubject($subject);
-        $mail->send();
+        try {
+            $mail->send();
+        } catch (Exception $e) {
+            $GLOBALS['Response']->addFeedback('warning', $GLOBALS['Language']->getText('global', 'mail_failed', Config::get('sys_email_admin')), CODENDI_PURIFIER_DISABLED);
+        }
     }
     
     /** for a certain set of users being part of the same ugroups
