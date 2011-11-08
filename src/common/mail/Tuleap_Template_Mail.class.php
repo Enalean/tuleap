1c7a461539f27d68ae2b53ec13542a411952e2ac
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2011-11-08 10:25:10 +0000

    Add missing file

---
+++ src/common/mail/Tuleap_Template_Mail.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2011. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once('common/include/Tuleap_Template.class.php');
+
+/**
+ * Template class to send beautiful html emails in tuleap
+ */
+class Tuleap_Template_Mail extends Tuleap_Template {
+    public function __construct() {
+        parent::__construct($GLOBALS['Language']->getContent('mail/html_template', 'en_US', null, '.php'));
+        $this->set('txt_display_not_correct', $GLOBALS['Language']->getText('mail_html_template', 'display_not_correct'));
+        $this->set('txt_update_prefs', $GLOBALS['Language']->getText('mail_html_template', 'update_prefs'));
+        $this->set('txt_can_update_prefs', $GLOBALS['Language']->getText('mail_html_template', 'can_update_prefs'));
+        $this->set('http_url', 'http://'. $GLOBALS['sys_default_domain']);
+        $this->set('img_path', 'http://'. $GLOBALS['sys_default_domain'] . $GLOBALS['HTML']->getImagePath(''));
+    }
+}
+?>
