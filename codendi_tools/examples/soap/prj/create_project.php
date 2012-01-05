51474905178eb93ecdcc43865b8e35b4ed4c192c
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-01-05 08:59:18 +0000

    Move soap create project stuff to core

---
+++ codendi_tools/examples/soap/prj/create_project.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2011. All Rights Reserved.
+ *
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+if ($argc < 4) {
+    die("Usage: ".$argv[0]." requester shortname longname [member 1] [member 2] [...]\n");
+}
+
+// Establish connexion to the server
+$client = new SoapClient(null, array('location'   => "http://localhost/soap/project/",
+                                     'uri'        => 'http://localhost/soap/project/',
+                                     'cache_wsdl' => WSDL_CACHE_NONE));
+
+//$client->addProject(requester, shortname, longname);
+$prjId = $client->addProject($argv[1], $argv[2], $argv[3]);
+
+echo "New Project ID: $prjId\n";
+
+for($i = 4; $i < $argc; $i++) {
+    var_dump($client->addProjectMember($prjId, $argv[$i]));
+}
+
+?>
---
+++ src/www/soap/project/index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// Start SoapServer
+require 'SoapProject_Server.class.php';
+
+// Check if we the server is in secure mode or not.
+if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || $GLOBALS['sys_force_ssl'] == 1) {
+    $protocol = "https";
+} else {
+    $protocol = "http";
+}
+$uri = $protocol.'://'.$GLOBALS['sys_default_domain'].'/soap/project';
+
+$server = new SoapServer(null, array('uri' => $uri, 'cache_wsdl' => WSDL_CACHE_NONE));
+
+$server->setClass('SoapProject_Server');
+
+$server->handle();
+
+?>
