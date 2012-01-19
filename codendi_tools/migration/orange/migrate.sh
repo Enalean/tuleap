39de6cc6519524f1ac73228d614cba462fd7bf07
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-01-19 15:24:53 +0000

    No need to use parameters, we already have the information in database.inc (furthemore passwords in parameters are not safe)

--- codendi_tools/migration/orange/migrate.sh
+++ codendi_tools/migration/orange/migrate.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-if ! [ $# -eq 4 ]; then
-  echo "this.sh <dbpass> <dbhost> <dbname> <full-path-to-forge-upgrade-config>"
-  exit 1
-fi
-pass=$1
-dbhost=$2
-dbname=$3
-configfile=$4
+
+db_conf_file=/etc/codendi/conf/database.inc
+forgeupgrade_conf_file=/etc/codendi/forgeupgrade/config.ini
+
+function read_db_conf() {
+    echo "<?php echo \$$1; ?>" | php -d auto_prepend_file=$db_conf_file
+}
+
+dbhost=`read_db_conf sys_dbhost`
+dbname=`read_db_conf sys_dbname`
+dbuser=`read_db_conf sys_dbuser`
+dbpasswd=`read_db_conf sys_dbpasswd`
+
 cd /usr/share/codendi/src/updates/
 sh upgrade.sh 016_docman_lock && sleep 1
 sh upgrade.sh 017_approval_table_modify_index  && sleep 1
@@ -16,6 +21,6 @@ sh upgrade.sh 025_docman_approval_table_index && sleep 1
 sh upgrade.sh 027_reset_artifact_permission && sleep 1
 cd -
 
-/usr/lib/forgeupgrade/bin/forgeupgrade --config=$configfile update
+/usr/lib/forgeupgrade/bin/forgeupgrade --config=$forgeupgrade_conf_file update
 
-mysql -ucodendiadm -p$pass -h$dbhost $dbname < orange2reference.sql 
+mysql -u$dbuser -p$dbpasswd -h$dbhost $dbname < orange2reference.sql 
