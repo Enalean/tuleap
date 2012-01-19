3f9c24aaf8c2c624a90b6627f4d4d413412b19fa
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-01-19 11:03:47 +0000

    Migration script + reference db update.

--- orangediffs/bigScaryMigrationScript.sh
+++ orangediffs/bigScaryMigrationScript.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-pushd /usr/share/codendi/src/updates/
-sh upgrade.sh 016_docman_lock
-sh upgrade.sh 017_approval_table_modify_index
-sh upgrade.sh 018_wiki_fix_textformattingrules
-sh upgrade.sh 024_svn_index
-sh upgrade.sh 025_docman_approval_table_index
-sh upgrade.sh 027_reset_artifact_permission
-popd
+if ! [ $# -eq 4 ]; then
+  echo "this.sh <dbpass> <dbhost> <dbname> <full-path-to-forge-upgrade-config>"
+  exit 1
+fi
+pass=$1
+dbhost=$2
+dbname=$3
+configfile=$4
+cd /usr/share/codendi/src/updates/
+sh upgrade.sh 016_docman_lock && sleep 1
+sh upgrade.sh 017_approval_table_modify_index  && sleep 1
+sh upgrade.sh 018_wiki_fix_textformattingrules && sleep 1
+sh upgrade.sh 024_svn_index && sleep 1
+sh upgrade.sh 025_docman_approval_table_index && sleep 1
+sh upgrade.sh 027_reset_artifact_permission && sleep 1
+cd -
 
-yum install forgeupgrade
+sudo yum install forgeupgrade
 # config???!!!!
 # create tables *_bucket *_log ???
+mysql -ucodendiadm -p$pass -h$dbhost $dbname < forgeupgrade_tables.sql 
 
-/usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/codendi/forgeupgrade/config.ini update
+/usr/lib/forgeupgrade/bin/forgeupgrade --config=$configfile update
 
-mysql -uroot -p????? -h????? -B < orange2reference.sql 
+mysql -ucodendiadm -p$pass -h$dbhost $dbname < orange2reference.sql 
