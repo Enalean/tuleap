53ae7c96d89365f8b31e236c5a66bbb5b8e2d247
johan <johan@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-01-27 15:49:49 +0000

    implemented last part of artifact submission scenario

--- codendi_tools/plugins/tests/functional/features/step_definitions/steps.rb
+++ codendi_tools/plugins/tests/functional/features/step_definitions/steps.rb
@@ -1,3 +1,5 @@
+today = Time.now.localtime.strftime("%Y-%m-%d")
+
 When /^I go to the bugs tracker of Test Project$/ do
   find(:xpath, '//a[text()="Test Project"]').click
   #find(:xpath, '//a[text()="Bugs"]').click
@@ -12,19 +14,17 @@ When /^I submit a new artifact$/ do
   end
   find("input[name='submit_and_stay']").click
   page.should have_css("div#feedback") #click_button('submit_and_continue')
+  within("div#feedback") do
+    find(:xpath, ".").should have_content(today)
+  end
 end
 
 Then /^a message says that the field 'Start Date' has been set to the current date$/ do
-
-  find(:xpath, "//label[@class='tracker_formelement_label']").should have_xpath("../*/input[@value='2012-01-18']")
-  puts find(:xpath, "//label[@class='tracker_formelement_label']/../*/input[@value='2012-01-18']").value
-  puts find(:xpath, "//label[@class='tracker_formelement_label']/../*/input[@type='text']/@value").inspect 
-  #.should have_content(Time.now.localtime.strftime("%Y-%m-%d"))
-
+  find(:xpath, "//label[@class='tracker_formelement_label']/..").should have_xpath("./*/input[@value='#{today}']")
 end
 
 Then /^the artifact has 'Start Date' set to the current date$/ do
-  pending # express the regexp above with the code you wish you had
+  find(:xpath, "//label[@class='tracker_formelement_label']/..").should have_xpath("./*/input[@value='#{today}']")
 end
 
 When /^I logon as "([^"]*)" : "([^"]*)"$/ do |user, pwd|
