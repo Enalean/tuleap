5af5cbf56fa53f9b91337c6f123a326d79eedfc3
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-02-03 10:58:24 +0000

    Check that the field is present in cucmber

--- plugins/tracker/functional/features/step_definitions/shared_fields.rb
+++ plugins/tracker/functional/features/step_definitions/shared_fields.rb
@@ -5,14 +5,16 @@ Given /^I go the fields admin page of the "([^"]*)" of the project "([^"]*)"$/ d
 end
 
 When /^I add the field "([^"]*)" from the tracker "([^"]*)" of the project "([^"]*)"$/ do |field, tracker, project|
+    page.should_not have_select(field)
     field_id = search_id_of(field, tracker, project)
     find(:xpath, "//a[@name='create-formElement[shared]']").click
     fill_in('formElement_data[field_id]', :with => field_id)
     click_on('Submit')
 end
 
-Then /^the field "([^"]*)" is present and has at least the option "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Then /^the field "([^"]*)" is present and has at least the option "([^"]*)"$/ do |field, option|
+    page.should have_select(field)
+    find(:xpath,"//label[contains(@title, '#{field}')]/..//a[contains(@href, 'func=admin-formElement-remove')]").click
 end
 
 def search_id_of(field, tracker, project)
