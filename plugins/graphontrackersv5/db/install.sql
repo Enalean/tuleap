dcc4e8c91336dfc8fe2c1d3d9cd8183962e47417
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-02-28 14:47:12 +0000

    Add spaces after comment to allow load from CLI

--- plugins/graphontrackersv5/db/install.sql
+++ plugins/graphontrackersv5/db/install.sql
@@ -65,12 +65,12 @@ CREATE TABLE plugin_graphontrackersv5_scrum_burndown(
   duration int(11)
 );
 
---DROP TABLE IF EXISTS plugin_graphontrackersv5_scrum_burnup;
---CREATE TABLE plugin_graphontrackersv5_scrum_burnup(
---  id int(11)  NOT NULL PRIMARY KEY ,
---  remaining_field_id int(11),
---  done_field_id int(11),
---  start_date int(11),
---  duration int(11)
---);
+-- DROP TABLE IF EXISTS plugin_graphontrackersv5_scrum_burnup;
+-- CREATE TABLE plugin_graphontrackersv5_scrum_burnup(
+--   id int(11)  NOT NULL PRIMARY KEY ,
+--   remaining_field_id int(11),
+--   done_field_id int(11),
+--   start_date int(11),
+--   duration int(11)
+-- );
 
