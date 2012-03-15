3655356861e4044d5092f109f4b257586305555d
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-15 09:11:09 +0000

    Fix unit tests

--- plugins/elasticsearch/include/ElasticsearchPluginDescriptor.class.php
+++ plugins/elasticsearch/include/ElasticsearchPluginDescriptor.class.php
@@ -6,7 +6,7 @@ require_once 'common/plugin/PluginDescriptor.class.php';
 class ElasticsearchPluginDescriptor extends PluginDescriptor {
     
     function __construct() {
-        parent::__construct($GLOBALS['Language']->getText('plugin_template', 'descriptor_name'), false, $GLOBALS['Language']->getText('plugin_template', 'descriptor_description'));
+        parent::__construct('elastic search', false, 'elastic search');
         $this->setVersionFromFile(dirname(__FILE__).'/../VERSION');
     }
 }
