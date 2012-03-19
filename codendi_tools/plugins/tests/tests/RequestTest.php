99c0f6c260f971cb305573f030905743e23df104
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-19 09:04:58 +0000

    added header

--- codendi_tools/plugins/tests/tests/RequestTest.php
+++ codendi_tools/plugins/tests/tests/RequestTest.php
@@ -1,8 +1,26 @@
 <?php
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 require_once('../include/testsPluginRequest.php');
 
-class testsPluginRequestTest extends TuleapTestCase {
+class PluginRequestTest extends TuleapTestCase {
     
     protected $requestObject;
     
@@ -73,11 +91,11 @@ class testsPluginRequestTest extends TuleapTestCase {
         $this->assertEqual($this->requestObject->getOrder(), 'invert');
         
     }
+    
     public function itCanParseTestsToRunWithArrayArgument() {
         $expected = array('test1'=>true);
         $this->requestObject->parse(array('tests_to_run'=>$expected));
         $this->assertEqual($this->requestObject->getTestsToRun(), $expected);
-        
     }
     
 }
