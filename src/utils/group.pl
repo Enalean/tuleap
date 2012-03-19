84a4dae75f9fc98d4ffd7f3789a9b9c34a35e141
hosniah <hosniah@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-19 11:42:18 +0000

    I <3 perl: use placeholder with RLIKE statement.

--- src/utils/group.pl
+++ src/utils/group.pl
@@ -151,9 +151,9 @@ sub get_emails_by_path {
     my $fieldname = $dbh->quote_identifier($fieldname);
     my $patternMatcher = $dbh->quote($patternMatcher);
 
-    $query = "SELECT $retfieldname FROM $table WHERE $fieldname RLIKE $patternMatcher";
+    $query = "SELECT $retfieldname FROM $table WHERE $fieldname RLIKE ?";
     $sth = $dbh->prepare($query);
-    $res = $sth->execute();
+    $res = $sth->execute($patternMatcher);
     if ($sth->rows >= 1) {
         my @emails = ();
         while (my @row = $sth->fetchrow_array()) {
