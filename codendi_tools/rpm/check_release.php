f6a4544be3b829b004737d6212bd86e3dd51ff1f
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-21 20:36:24 +0000

    Synchronize with trunk

--- codendi_tools/rpm/check_release.php
+++ codendi_tools/rpm/check_release.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Copyright (c) Enalean, 2010. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 require_once 'ReleaseVersionComparator.php';
 
@@ -9,7 +27,7 @@ if (isset($argv[1])) {
 
 // Gather RPMs info
 $rpms = array();
-$spec = file(dirname(__FILE__).'/codendi.spec', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$spec = file(dirname(__FILE__).'/tuleap.spec', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 foreach ($spec as $line) {
     $m1 = array();
     if (preg_match('/^%package (.*)-(.*)$/', trim($line), $m1)) {
