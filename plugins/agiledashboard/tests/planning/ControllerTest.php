e8260a137e9430d16894e5290fcb783c99ef9508
sebn <sebn@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-21 09:29:08 +0000

    Missing mocks in controller test

--- plugins/agiledashboard/tests/planning/ControllerTest.php
+++ plugins/agiledashboard/tests/planning/ControllerTest.php
@@ -30,10 +30,12 @@ if (!defined('TRACKER_BASE_URL')) {
 Mock::generate('Tracker_ArtifactFactory');
 Mock::generate('Tracker_Artifact');
 Mock::generate('TrackerFactory');
+Mock::generate('Tracker_HierarchyFactory');
 Mock::generate('PlanningFactory');
 Mock::generate('Planning');
 Mock::generate('ProjectManager');
 Mock::generate('Project');
+Mock::generate('Tracker_CrossSearch_Search');
 Mock::generate('Tracker_CrossSearch_SearchContentView');
 Mock::generate('Tracker_CrossSearch_ViewBuilder');
 
