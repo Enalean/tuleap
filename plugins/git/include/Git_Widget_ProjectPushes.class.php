b9878725a32fc1db5a0700b3d66d1fc0675d1106
hosniah <hosniah@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-23 16:12:18 +0000

    Add porject widget file.

---
+++ plugins/git/include/Git_Widget_ProjectPushes.class.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright (c) STMicroelectronics, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('common/chart/Chart.class.php');
+/**
+ * TODO: Class comment
+ */
+class Git_Widget_ProjectPushes extends Widget {
+
+    /**
+     * Constructor of the widget.
+     */
+    public function __construct() {
+        parent::__construct('projectpushes');
+    }
+
+    public function canBeUsedByProject($project) {
+        //TODO
+    }
+
+    /**
+     * Get the title of the widget.
+     *
+     * @return string
+     */
+    public function getTitle() {
+        //TODO
+    }
+
+    /**
+     * Compute the content of the widget
+     *
+     * @return string html
+     */
+    public function getContent() {
+        //TODO
+    }
+
+    /**
+     * The category of the widget is scm
+     *
+     * @return string
+     */
+    function getCategory() {
+        return 'scm';
+    }
+
+    /**
+     * Display widget's description
+     *
+     * @return String
+     */
+    function getDescription() {
+        //TODO
+    }
+
+     function isAjax() {
+          //TODO
+     }
+}
+?>
