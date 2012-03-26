d56aede516343795b17d7501245afda6d4280094
sechinard <sechinard@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-26 08:34:16 +0000

    This is a merge from ^/contrib/st/enhancement/531_release_view (Merging r21419 through r21477)

--- plugins/agiledashboard/db/install.sql
+++ plugins/agiledashboard/db/install.sql
@@ -1,4 +1,16 @@
+CREATE TABLE IF NOT EXISTS plugin_agiledashboard_planning(
+    id int(11) NOT NULL auto_increment,
+    name varchar(255) NOT NULL,
+    group_id INT( 11 ) UNSIGNED NOT NULL,
+    planning_tracker_id INT(11) NOT NULL,
+    KEY idx(id, planning_tracker_id)
+);
 
+CREATE TABLE IF NOT EXISTS plugin_agiledashboard_planning_backlog_tracker (
+    planning_id int(11) NOT NULL,
+    tracker_id int(11) NOT NULL,
+    KEY idx(planning_id, tracker_id)
+);
 
 -- Enable service for project 100
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank) 
