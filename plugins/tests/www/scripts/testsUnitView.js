bb71b75ef1dd2c1f63d60bd9c31d3b8004aff72c
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-27 15:49:53 +0000

    clean javascript and make image change when expand/collapse

--- plugins/tests/www/scripts/testsUnitView.js
+++ plugins/tests/www/scripts/testsUnitView.js
@@ -1,42 +1,36 @@
 (function() {
-	function uncheck(element) {
-		if (element && element.id != 'menu') {
-			var len = element.childNodes.length;
-			var found = false;
-			for (var i = 0 ; i < len && !found; ++i) {
-				if (element.childNodes[i].tagName == 'INPUT' && element.childNodes[i]['type'] == 'checkbox') {
-					element.childNodes[i].checked = false;
-					found = true;
-				}
-			}
-			uncheck(element.parentNode);
+
+	var baseSrc  = 'themes/default/images/';
+	var minusSrc = baseSrc + 'minus.png';
+	var plusSrc  = baseSrc + 'plus.png';
+	var currentCategory = 0;
+	
+	function uncheck(category) {
+		var inputBoxes = Form.getInputs(category, 'checkbox');
+		if (inputBoxes.length > 0) {
+			inputBoxes[0].checked = false;
+			uncheck(category.up('li.category'));
 		}
 	}
-	function register_events(element) {
-		if (element.childNodes) {
-			$A(element.childNodes).each(function (child) {
-				var found = false;
-				if (child.tagName == 'INPUT' && child['type'] == 'checkbox') {
-					Event.observe(child, 'change', (function (evt) {
-						var checked = this.checked;
-						var col = this.parentNode.getElementsByTagName('input');
-						var len = col.length;
-						for (var i = 0 ; i < len ; ++i) {
-							if (col[i]['type'] == 'checkbox') {
-								col[i].checked = checked;
-							}
-						}
-						//On remonte
-						if (!checked && this.parentNode.id != 'menu') {
-							uncheck(this.parentNode.parentNode.parentNode);
-						}
-					}).bind(child));
-					found = true;
-				} else {
-					register_events(child);
+	
+	function register_events(category) {
+		var inputBoxes = Form.getInputs(category, 'checkbox');
+		inputBoxes.each(function(inputBox){
+			Event.observe(inputBox, 'change', (function (evt) {
+				var checked = this.checked;
+				var col = this.parentNode.getElementsByTagName('input');
+				var len = col.length;
+				for (var i = 0 ; i < len ; ++i) {
+					if (col[i]['type'] == 'checkbox') {
+						col[i].checked = checked;
+					}
 				}
-			});
-		}
+				//On remonte
+				if (!checked && this.parentNode.id != 'menu') {
+					uncheck(this.parentNode.parentNode.parentNode);
+				}
+			}).bind(inputBox));
+		});
 	}
 	
 	function submitPanelEffect() {
@@ -53,32 +47,29 @@
 	    }
 	}
 	
-	function init() {
-		var plus = 0;
-		var baseSrc  = 'themes/default/images/';
-		var minusSrc = baseSrc + 'minus.png';
-		var plusSrc  = baseSrc + 'plus.png';
-		$$('li.category').each(function (element) {
-			register_events(element);
-			plus++;
-			new Insertion.Top(element, '<a href="" id="plus_' + plus +'"><img src="' + plusSrc +'" /></a>');
-			var uls = $A(element.childNodes).findAll(function (element) {
-				return element.tagName == 'UL';
-			});
-			Event.observe($('plus_' + plus), 'click', function (evt) {
-				uls.each(function (element) {
-					Element.toggle(element);
-				});
-				if (Event.element(evt).src == minusSrc) {
-					Event.element(evt).src = plusSrc;
-				} else {
-					Event.element(evt).src = minusSrc;
-				}
-				Event.stop(evt);
-				return false;
-			});
+	function initCategory(category) {
+		Event.observe($('plus_' + currentCategory), 'click', function (evt) {
+			category.getElementsBySelector('ul').invoke('toggle');
+			var treeImg = category.down('img');
+			if (treeImg.src.indexOf(minusSrc) > 0) {
+				treeImg.src = plusSrc;
+			} else {
+				treeImg.src = minusSrc;
+			}
+			Event.stop(evt);
+			return false;
 		});
+	}
+	
+	function init() {
+		$$('li.category').each(function(category){
+			register_events(category);
+			currentCategory++;
+			new Insertion.Top(category, '<img id="plus_' + currentCategory +'" src="' + minusSrc +'" />');
+			initCategory(category);
+		});
+		
 		submitPanelEffect();
 	}
 	Event.observe(window, 'load', init, true);
-})()
+})();
