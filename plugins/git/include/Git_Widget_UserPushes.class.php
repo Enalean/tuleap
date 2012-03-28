fbde12dcba8de5e95efd70760af97e1ed2ea7877
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-28 15:54:43 +0000

    Another format for the date.

--- plugins/git/include/Git_Widget_UserPushes.class.php
+++ plugins/git/include/Git_Widget_UserPushes.class.php
@@ -71,6 +71,7 @@ class Git_Widget_UserPushes extends Widget {
         $result  = $dao->getLastPushesRepositories($user->getId(), $date);
         $content = '';
         $project = '';
+        $dh      = new DateHelper();
         if ($result && !$result->isError() && $result->rowCount() > 0) {
             foreach ($result as $entry) {
                 $dar = $dao->getLastPushesByUser($user->getId(), $entry['repository_id'], $this->offset, $date);
@@ -100,7 +101,7 @@ class Git_Widget_UserPushes extends Widget {
                     $hp      = Codendi_HTMLPurifier::instance();
                     foreach ($dar as $row) {
                         $content .= '<tr class="'.html_get_alt_row_color(++$i).'">
-                                         <td>'.html_time_ago($hp->purify($row['push_date'])).'</td>
+                                         <td><span title="'.$dh->timeAgoInWords($row['push_date'], true).'">'.$hp->purify(format_date($GLOBALS['Language']->getText('system', 'datefmt'), $row['push_date'])).'</span></td>
                                          <td>'.$hp->purify($row['commits_number']).'</td>
                                      </tr>';
                     }
