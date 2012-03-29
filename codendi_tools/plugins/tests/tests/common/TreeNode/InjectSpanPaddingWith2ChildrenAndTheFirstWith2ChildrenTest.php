09ba7c16538d816454068448c305f78ae82f96d7
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-03-29 16:45:20 +0000

    added test case with 2 children of wich first has 2 children

---
+++ codendi_tools/plugins/tests/tests/common/TreeNode/InjectSpanPaddingWith2ChildrenAndTheFirstWith2ChildrenTest.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+require_once 'common/TreeNode/InjectSpanPaddingInTreeNodeVisitor.class.php';
+require_once dirname(__FILE__).'/InjectSpanPadding.class.php';
+
+class InjectSpanPaddingWith2ChildrenAndTheFirstWith2ChildrenTest extends InjectSpanPadding {
+
+    protected $treeNode;
+    /**
+     * Return this Tree
+     *
+     * ROOT
+     * |
+     * +-Child 1
+     * | |
+     * | |-Child 2
+     * | |
+     * | '-Child 3
+     * |
+     * '-Child 4
+     */
+    protected function given_TwoChildrenWithTheFirstHaving2Children() {
+        $parent  = $this->buildBaseTree();
+        $child1 = $parent->getChild(0);
+        
+        $child3Data = array(
+        'id'                => '10',
+        'last_changeset_id' => '101112',
+                    'title'             => 'As a user I want to search on trackers',
+                    'artifactlinks'     => '',
+                );
+        $child3 = new TreeNode($child3Data);
+        $child3->setId($child3Data['id']);
+        
+        $child1->addChild($child3);
+        /**
+         * Add child 3 artifact link to child 1
+         */
+        $child1Data = $child1->getData();
+        $child1Data['artifactlinks'].=',10';
+        
+        $child1->setData($child1Data);
+        
+        $child4Data = array(
+            'id'                => '12',
+            'last_changeset_id' => '131415',
+            'title'             => 'As a user I want to search on all trackers',
+            'artifactlinks'     => '',
+        );
+        $child4 = new TreeNode($child4Data);
+        $child4->setId($child4Data['id']);
+        
+        $parent->addChild($child4);
+        
+        return $parent;
+    }
+    
+    public function itShouldSetDataToFirstChildThatMatches_IndentPipeTreeIndentMinus_treeAndChild() {
+        $given = $this->given_TwoChildrenWithTheFirstHaving2Children();
+        $this->when_VisitTreeNodeWith_InjectSpanPadding($given);
+        
+        $pattern = '%^(.*)'.$this->getPatternSuite("_indent_pipe_tree_indent_minusTree").'$%ism';
+        $givenChild = $given->getChild(0);
+        
+        $this->then_GivenTreeNodeData_TreePadding_AssertPattern($givenChild, $pattern);
+        $this->then_GivenTreeNodeData_ContentTemplate_AssertPattern($givenChild, '%^(.*)'.$this->getPatternSuite("_child").'$%ism');
+    }
+    
+    public function itShouldSetDataToChild2ThatMatches_IndentPipeBlankIndentPipeIndentMinus() {
+        $given      = $this->given_TwoChildrenWithTheFirstHaving2Children();
+        $this->when_VisitTreeNodeWith_InjectSpanPadding($given);
+        
+        $pattern    = '%^(.*)'.$this->getPatternSuite("_indent_pipe_blank_indent_pipe_indent_minus").'$%ism';
+        $givenChild = $given->getChild(0)->getChild(0);
+        
+        $this->then_GivenTreeNodeData_TreePadding_AssertPattern($givenChild, $pattern);
+    }
+    
+    public function itShouldSetDataToChild3ThatMatches_IndentPipeBlankIndentLastLeftIndentLastRight() {
+        $given      = $this->given_TwoChildrenWithTheFirstHaving2Children();
+        $this->when_VisitTreeNodeWith_InjectSpanPadding($given);
+        
+        $pattern    = '%^(.*)'.$this->getPatternSuite("_indent_pipe_blank_indent_lastLeft_indent_lastRight").'$%ism';
+        $givenChild = $given->getChild(0)->getChild(1);
+        
+        $this->then_GivenTreeNodeData_TreePadding_AssertPattern($givenChild, $pattern);
+    }
+    
+    public function itShouldSetDataToChild4ThatMatches_Last_LeftLast_Right() {
+        $given      = $this->given_TwoChildrenWithTheFirstHaving2Children();
+        $this->when_VisitTreeNodeWith_InjectSpanPadding($given);
+    
+        $pattern    = '%^(.*)'.$this->getPatternSuite("_lastLeft_lastRight").'$%ism';
+        $givenChild = $given->getChild(1);
+    
+        $this->then_GivenTreeNodeData_TreePadding_AssertPattern($givenChild, $pattern);
+    }
+}
+?>
