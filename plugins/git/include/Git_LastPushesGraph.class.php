e8c0cde09c14c42a92b8cdc0cfd3e4d136eeeb14
hosniah <hosniah@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-04 16:27:35 +0000

    follow rev #21748 :Refactoring

--- plugins/git/include/Git_LastPushesGraph.class.php
+++ plugins/git/include/Git_LastPushesGraph.class.php
@@ -20,23 +20,27 @@
 
 require_once 'pre.php';
 require_once 'common/chart/Chart.class.php';
+require_once 'GitDao.class.php';
+require_once 'Git_LogDao.class.php';
 
 class Git_LastPushesGraph {
     
     public $displayChart;
-
+    public $repoList;
+    
     /**
      * Constructor.
      *
      * @return Void
      */
-    public function __construct() {
-        if (empty($this->duration)) {
-            $this->displayChart = false;
-        }
+    public function __construct($groupId) {
+        $dao             = new GitDao();
+		$this->repoList  = $dao->getProjectRepositoryList($groupId);
+		$this->displayChart = false;
     }
 
-    function prepareGraph($nb_repo, $dates) {
+    function prepareGraph($dates) {
+        $nb_repo = count($this->repoList);
         $graph   = new Chart(500, 300+16*$nb_repo);
         $graph->SetScale('textlin');
         $graph->img->SetMargin(40, 20, 20, 80 + 16 * $nb_repo);
@@ -55,6 +59,40 @@ class Git_LastPushesGraph {
         $graph->yaxis->SetLabelAlign('center', 'top');
         $graph->legend->Pos(0.1, 0.98, 'right', 'bottom');
         return $graph;
+    }
+
+    function displayRepositoryPushesByWeek($repoList, $w, $year, $weekNum, $nb_weeks) {
+        $nb_repo = count($this->repoList);
+        $colors    = array_reverse(array_slice($GLOBALS['HTML']->getChartColors(), 0, $nb_repo));
+        $nb_colors = count($colors);
+        $i         = 0;
+        $bplot     = array();
+        foreach ($repoList as $repository) {
+            $pushes = array();
+            $gitLogDao = new Git_LogDao();
+            foreach ($weekNum as $key => $w) {
+                $res = $gitLogDao->getRepositoryPushesByWeek($repository['repository_id'], $w, $year[$key]);
+                if ($res && !$res->isError()) {
+                    if ($res->valid()) {
+                        $row          = $res->current();
+                        $pushes[$key] = intval($row['pushes']);
+                        $res->next();
+                        if ($pushes[$key] > 0) {
+                            $this->displayChart = true;
+                        }
+                    }
+                }
+            $pushes = array_pad($pushes, $nb_weeks, 0);
+            }    
+            if ($this->displayChart) {
+                $b2plot = new BarPlot($pushes);
+                $color  = $colors[$i++ % $nb_colors];   
+                $b2plot->SetFillgradient($color, $color.':0.6', GRAD_VER);
+                $b2plot->SetLegend($repository['repository_name']);
+                $bplot[] = $b2plot;
+            }
+        }
+        return $bplot;
     }
 
     function displayAccumulatedGraph($bplot, $graph) {
@@ -82,8 +120,8 @@ class Git_LastPushesGraph {
         //some of these additional letters should go below it
         $test3="Hjgqp";
         //get the dimension for these two:
-        $box2 = imageTTFBbox(10,0,$ttf->File(FF_USERFONT),$test2);
-        $box3 = imageTTFBbox(10,0,$ttf->File(FF_USERFONT),$test3);
+        $box2 = imageTTFBbox(10, 0, $ttf->File(FF_USERFONT), $test2);
+        $box3 = imageTTFBbox(10, 0, $ttf->File(FF_USERFONT), $test3);
         $baseline = abs((abs($box2[5]) + abs($box2[1])) - (abs($box3[5]) + abs($box3[1])));
         $bbox = imageTTFBbox(10, 0, $ttf->File(FF_USERFONT), $msg);
         if ($im = @imagecreate($bbox[2] - $bbox[6], $bbox[3] - $bbox[5])) {
--- plugins/git/www/project_last_git_pushes_graph.php
+++ plugins/git/www/project_last_git_pushes_graph.php
@@ -19,7 +19,6 @@
  */
 
 require_once 'pre.php';
-require_once dirname(__FILE__).'/../include/GitDao.class.php';
 require_once dirname(__FILE__).'/../include/Git_LastPushesGraph.class.php';
 
 $vGroupId = new Valid_GroupId();
@@ -37,10 +36,10 @@ if ($request->valid($vDuration)) {
 } else {
     header('Location: '.get_server_url());
 }
-$imageRenderer = new Git_LastPushesGraph();
+$imageRenderer = new Git_LastPushesGraph($groupId);
 
-$dao             = new GitDao();
-$repoList        = $dao->getProjectRepositoryList($groupId);
+$repoList = $imageRenderer->repoList;
+
 $day             = 24 * 3600;
 $week            = 7 * $day;
 $today           = $_SERVER['REQUEST_TIME'];
@@ -54,43 +53,12 @@ for ($i = $start_of_period ; $i <= $today ; $i += $week) {
     $weekNum[] = intval(date('W', $i));
     $year[]    = intval(date('Y', $i));
 }
-$nb_repo = count($repoList);
-$graph = $imageRenderer->prepareGraph($nb_repo, $dates);
-$colors    = array_reverse(array_slice($GLOBALS['HTML']->getChartColors(), 0, $nb_repo));
-$nb_colors = count($colors);
-$i         = 0;
-$bplot     = array();
-$displayChart = false;
-foreach ($repoList as $repository) {
-    $pushes = array();
-    $gitLogDao = new Git_LogDao();
-    foreach ($weekNum as $key => $w) {
-        $res = $gitLogDao->getRepositoryPushesByWeek($repository['repository_id'], $w, $year[$key]);
-        if ($res && !$res->isError()) {
-            if ($res->valid()) {
-                $row          = $res->current();
-                $pushes[$key] = intval($row['pushes']);
-                $res->next();
-                if ($pushes[$key] > 0) {
-                    $displayChart = true;
-                }
-            }
-        }
-        $pushes = array_pad($pushes, $nb_weeks, 0);
-    }
-    if ($displayChart) {
-        $b2plot = new BarPlot($pushes);
-        $color  = $colors[$i++ % $nb_colors];   
-        $b2plot->SetFillgradient($color, $color.':0.6', GRAD_VER);
-        $b2plot->SetLegend($repository['repository_name']);
-        $bplot[] = $b2plot;
-    }
-}
 
-if ($displayChart) {
+$graph = $imageRenderer->prepareGraph($dates);
+$bplot = $imageRenderer->displayRepositoryPushesByWeek($repoList, $w, $year, $weekNum, $nb_weeks);
+if ($imageRenderer->displayChart) {
     $imageRenderer->displayAccumulatedGraph($bplot, $graph);
 } else {
-    //$pngErrorMessage = new Git_LastPushesGraph();
     $msg = "There is no logged pushes in the last $nb_weeks weeks";
     $imageRenderer->displayError($msg);
 }
