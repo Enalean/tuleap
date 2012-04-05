61ac901982f1fff65bee8c4e9388b73a2b4659b2
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-05 08:51:16 +0000

    moved ie conditional style to a dedicated sheet

--- plugins/tracker/include/trackerPlugin.class.php
+++ plugins/tracker/include/trackerPlugin.class.php
@@ -82,41 +82,7 @@ class trackerPlugin extends Plugin {
             strpos($_SERVER['REQUEST_URI'], '/widgets/') === 0 ) {
             echo '<link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/style.css" />';
             echo '<link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/print.css" media="print" />';
-            echo '<!--[if lte IE 7]><style type="text/css">
-
-            .node-content {
-             	display:inline;
-            }
-            
-            .node-pipe {
-                zoom:1;
-                height:inherit;
-                vertical-align:top;
-            }
-
-            .node-minus {
-                zoom:1;
-                height:inherit;
-                display:inline;
-            }
-            .node-child {
-                zoom:1;
-                height:inherit;
-                height:0;
-            }
-            .node-minus-tree {
-                zoom:1;
-                height:inherit;
-                display:inline;
-            }
-
-            .node-indent {
-                zoom:1;
-                height:inherit;
-                display:inline;
-            }
-            
-            </style><![endif]-->';
+            echo '<!--[if lte IE 7]><link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/ieStyle.css" /><![endif]-->';
         }
     }
     
