7d5c7488a8eff4f43c93c645fa81afb119541cc6
vaceletm <vaceletm@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-12 09:58:10 +0000

    Import code to prepare and display burndown (crappy)

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.class.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.class.php
@@ -144,7 +144,8 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
     public function process(Tracker_IDisplayTrackerLayout $layout, $request, $current_user) {
         switch ($request->get('func')) {
             case self::FUNC_SHOW_BURNDOWN:
-                $this->fetchBurndownImage();
+                //$artifact = Tracker_ArtifactFactory::instance()->getArtifactById(5261);
+                //$this->fetchBurndownImage($artifact);
                 break;
             default:
                 parent::process($layout, $request, $current_user);
@@ -153,7 +154,11 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
     
     
     
-    public function fetchBurndownImage() {
+    public function fetchBurndownImage(Tracker_Artifact $artifact) {
+        $remaining_effort = $this->getRemainingEffortEvolution($artifact);
+        //var_dump($remaining_effort);
+        $graph = $this->buildGraph($remaining_effort, 10);
+        $graph->stroke();
         //header('Content-type: image/png');
         //readfile(TRACKER_BASE_DIR.'/../www/images/please-configure-your-burndown.png');
     }
@@ -177,6 +182,11 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
     }
     
     public function getRemainingEffortEvolution(Tracker_Artifact $artifact) {
+        $minday = 0;
+        $maxday = 0;
+        
+        $artifact_ids = array();
+        
         $remaining_effort = array();
         $linked_artifacts = $this->getLinkedArtifacts($artifact);
         if (count($linked_artifacts) > 0) {
@@ -184,8 +194,10 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
             $artifact_ids_by_tracker = array();
             
             foreach($linked_artifacts as $linked_artifact) {
-                $tracker_id = $linked_artifact->getTracker()->getId();
-                $artifact_ids_by_tracker[$tracker_id][] = $linked_artifact->getId();
+                $tracker_id  = $linked_artifact->getTracker()->getId();
+                $artifact_id = $linked_artifact->getId();
+                $artifact_ids_by_tracker[$tracker_id][] = $artifact_id;
+                $artifact_ids[]                         = $artifact_id;
             }
             
             foreach ($artifact_ids_by_tracker as $tracker_id => $artifact_ids) {
@@ -197,12 +209,171 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
                     $effort_field_type = $form_element_factory->getType($effort_field);
                     $dar = $burndown_dao->searchRemainingEffort($effort_field_id, $effort_field_type, $artifact_ids);
                     foreach ($dar as $row) {
-                        $remaining_effort[] = $row;
+                        $day   = $row['day'];
+                        $id    = $row['id'];
+                        $value = $row['value'];
+                        
+                        if (!isset($remaining_effort[$day])) {
+                            $remaining_effort[$day] = array();
+                        }
+                        
+                        $remaining_effort[$day][$id] = $value;
+                        
+                        $maxday = max($maxday, $day);
+                        $minday = min($minday, $day);
                     }
                 }
             }
         }
-        return $remaining_effort;
+        
+        $start_date = mktime(0,0,0,4,11,2012);
+        
+        $day = 24 * 60 * 60;
+        $start_date = round($start_date / $day);
+        
+        return $this->getComputedData($remaining_effort, $artifact_ids, $start_date, $minday, $maxday);
+        
+    }
+    
+    private function getComputedData($dbdata, $artifact_ids, $start_date, $minday, $maxday) {
+        /*$dbdata = array();
+        $minday = 0;
+        $maxday = 0;
+        while ($d = db_fetch_array($res)) {
+            if (!isset($dbdata[$d['day']])) {
+                $dbdata[$d['day']] = array();
+            }
+            $dbdata[$d['day']][$d['id']] = $d['value'];
+            if ($d['day'] > $maxday)
+                $maxday = $d['day'];
+            if ($d['day'] < $minday)
+                $minday = $d['day'];
+        }*/
+        $data   = array();
+        for ($day = $start_date; $day <= $maxday; $day++) {
+            if (!isset($data[$start_date])) {
+                $data[$start_date] = array();
+            }
+        }
+        // We assume here that SQL returns effort value order by changeset_id ASC
+        // so we only keep the last value (possible to change effort several times a day)
+
+        foreach ($artifact_ids as $aid) {
+            for ($day = $minday; $day <= $maxday; $day++) {
+                if ($day < $start_date) {
+                    if (isset($dbdata[$day][$aid])) {
+                        $data[$start_date][$aid] = $dbdata[$day][$aid];
+                    }
+                } else if ($day == $start_date) {
+                    if (isset($dbdata[$day][$aid])) {
+                        $data[$day][$aid] = $dbdata[$day][$aid];
+                    } else {
+                        $data[$day][$aid] = 0;
+                    }
+                } else {
+                    if (isset($dbdata[$day][$aid])) {
+                        $data[$day][$aid] = $dbdata[$day][$aid];
+                    } else {
+                        // No update this day: get value from previous day
+                        $data[$day][$aid] = $data[$day - 1][$aid];
+                    }
+                }
+            }
+        }
+        return $data;
+    }
+    
+        /**
+     * @return Chart
+     */
+    public function buildGraph($remaining_effort, $duration) {
+        $this->title  = "Burndown";
+        $this->width  = 640;
+        $this->height = 480;
+        $this->description = "";
+        
+        $this->graph = new Chart($this->width,$this->height);
+        $this->graph->SetScale("datlin");
+        
+        // title setup
+        $this->graph->title->Set($this->title);
+        
+        //description setup
+        if (is_null($this->description)) {
+            $this->description = "";
+        }
+        $this->graph->subtitle->Set($this->description);
+        
+        // order this->data by date asc
+        ksort($remaining_effort);
+        
+        // Initial estimation line: a * x + b
+        // where b is the sum of effort for the first day
+        //       x is the number of days (starting from 0 to duration
+        //       a is the slope of the line equals -b/duration (burn down goes down)
+        
+        
+        // Build data for initial estimation
+        list($first_day, $b) = each($remaining_effort);
+        $b = array_sum($b);
+        $day = 24 * 60 * 60;
+        $start_of_sprint = $first_day;
+        $a = - $b / $duration;
+        $data_initial_estimation = array();
+        $dates = array();
+        //$end_of_weeks = array();
+        $data = array();
+        $previous = $b; // init with sum of effort for the first day
+        // for each day
+        for ($x = 0 ; $x <= $duration ; ++$x) {
+            $data_initial_estimation[] = $a * $x  + $b;
+            $timestamp_current_day = ($start_of_sprint + $x) * $day;
+            $human_dates[] = date('M-d', $timestamp_current_day);
+            if (isset($remaining_effort[$start_of_sprint + $x])) {
+                $nb = array_sum($remaining_effort[$start_of_sprint + $x]);
+            } else {
+                if ($x - 1 < count($remaining_effort) - 1) {
+                    $nb = $previous;
+                } else {
+                    $nb = null;
+                }
+            }
+            $data[] = $nb;
+            $previous = $nb;
+            //$end_of_weeks[] = date('N', $timestamp_current_day) == 7 ? 1 : 0;
+        }
+        $this->graph->xaxis->SetTickLabels($human_dates);
+        /*
+        foreach($end_of_weeks as $i => $w) {
+            if ($w) {
+                $vline = new PlotLine(VERTICAL, $i, "gray9", 1);
+                $this->graph->Add($vline);
+            }
+        }
+        */
+        foreach($remaining_effort as $d) {
+            
+        }
+        $colors = $this->graph->getThemedColors();
+        
+        $current = new LinePlot($data);
+        $current->SetColor($colors[1].':0.7');
+        $current->SetWeight(2);
+        $current->SetLegend('Remaining effort');
+        $current->mark->SetType(MARK_FILLEDCIRCLE);
+        $current->mark->SetColor($colors[1].':0.7');
+        $current->mark->SetFillColor($colors[1]);
+        $current->mark->SetSize(3);
+        $this->graph->Add($current);
+       
+        //Add "initial" after current so it is on top
+        $initial = new LinePlot($data_initial_estimation);
+        $initial->SetColor($colors[0].':1.25');
+        //$initial->SetStyle('dashed');
+        $initial->SetLegend('Ideal Burndown');
+        $this->graph->Add($initial);
+        
+        return $this->graph;
     }
     
     /**
