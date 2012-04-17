e67dd31e232b28d144b6d08de8bc49ad6da2883e
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-17 10:12:54 +0000

    remove a test

--- plugins/tracker/include/Tracker/Chart/Data/LinkedArtifactsDao.class.php
+++ plugins/tracker/include/Tracker/Chart/Data/LinkedArtifactsDao.class.php
@@ -25,11 +25,8 @@ class Tracker_Chart_Burndown_Data_LinkedArtifactsDao extends DataAccessObject {
         $sql = "SELECT c.artifact_id AS id, TO_DAYS(FROM_UNIXTIME(submitted_on)) - TO_DAYS(FROM_UNIXTIME(0)) as day, value
                     FROM tracker_changeset AS c 
                          INNER JOIN tracker_changeset_value AS cv ON(cv.changeset_id = c.id AND cv.field_id = " . $effort_field_id . ")";
-        if ($effort_field_type == 'int') {
-            $sql .= " INNER JOIN tracker_changeset_value_int AS cvi ON(cvi.changeset_value_id = cv.id)";
-        } else {
-            $sql .= " INNER JOIN tracker_changeset_value_float AS cvi ON(cvi.changeset_value_id = cv.id)";
-        }
+        
+        $sql .= " INNER JOIN tracker_changeset_value_$effort_field_type AS cvi ON(cvi.changeset_value_id = cv.id)";
         $sql .= " WHERE c.artifact_id IN (" . implode(',', $artifact_ids) . ")";
         return $this->retrieve($sql);
     }
