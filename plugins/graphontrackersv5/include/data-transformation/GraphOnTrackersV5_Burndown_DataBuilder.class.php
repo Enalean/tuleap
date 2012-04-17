591d91e5b804d06a6f74e02a97e28e349603c3a8
gregsal <gregsal@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-17 15:17:18 +0000

    clean GraphOnTrackersV5_Burndown_DataBuilder

--- plugins/graphontrackersv5/include/data-transformation/GraphOnTrackersV5_Burndown_DataBuilder.class.php
+++ plugins/graphontrackersv5/include/data-transformation/GraphOnTrackersV5_Burndown_DataBuilder.class.php
@@ -25,38 +25,52 @@ require_once 'common/user/UserManager.class.php';
 require_once 'GraphOnTrackersV5_Burndown_Data.class.php';
 
 class GraphOnTrackersV5_Burndown_DataBuilder extends ChartDataBuilderV5 {
-
+    /**
+     * Autorized types for effort field type
+     * 
+     * @var array
+     */
+    const TRACKER_CHANGESET_TYPE = array('int', 'float');
+    
     /**
      * build burndown chart properties
      *
      * @param Burndown_Engine $engine object
      */
-    function buildProperties($engine) {
+    public function buildProperties($engine) {
         parent::buildProperties($engine);
 
-        $fef          = Tracker_FormElementFactory::instance();
-        $effort_field = $fef->getFormElementById($this->chart->getFieldId());
-        $type         = $fef->getType($effort_field);
+        $form_element_fatory = Tracker_FormElementFactory::instance();
+        $effort_field        = $form_element_fatory->getFormElementById($this->chart->getFieldId());
+        $type                = $form_element_fatory->getType($effort_field);
         
-        $artifact_ids = explode(',', $this->artifacts['id']);
-        
-        if ($effort_field && $effort_field->userCanRead(UserManager::instance()->getCurrentUser())) {
-            $sql = "SELECT c.artifact_id AS id, TO_DAYS(FROM_UNIXTIME(submitted_on)) - TO_DAYS(FROM_UNIXTIME(0)) as day, value
-                    FROM tracker_changeset AS c 
-                         INNER JOIN tracker_changeset_value AS cv ON(cv.changeset_id = c.id AND cv.field_id = ". $effort_field->getId() . ")";
-            if ($type == 'int') {
-                $sql .= " INNER JOIN tracker_changeset_value_int AS cvi ON(cvi.changeset_value_id = cv.id)";
-            } else {
-                $sql .= " INNER JOIN tracker_changeset_value_float AS cvi ON(cvi.changeset_value_id = cv.id)";
-            }
-            $sql .= " WHERE c.artifact_id IN (". implode(',', $artifact_ids) .")";
-            $res = db_query($sql);
-            $engine->data = new GraphOnTrackersV5_Burndown_Data($res, $artifact_ids);
+        if ($this->isValidEffortField($effort_field) && $this->isValidType($type)) {
+            $engine->data    = $this->getBurnDownData($effort_field->getId(), $type);
         }
         
-        $engine->legend     = null;
-        $engine->start_date = $this->chart->getStartDate();
-        $engine->duration   = $this->chart->getDuration();
+        $engine->legend      = null;
+        $engine->start_date  = $this->chart->getStartDate();
+        $engine->duration    = $this->chart->getDuration();
+    }
+    
+    protected function getBurnDownData($effort_field_id, $type) {
+        $artifact_ids = explode(',', $this->artifacts['id']);
+        $sql = "SELECT c.artifact_id AS id, TO_DAYS(FROM_UNIXTIME(submitted_on)) - TO_DAYS(FROM_UNIXTIME(0)) as day, value
+                            FROM tracker_changeset AS c 
+                                 INNER JOIN tracker_changeset_value AS cv ON(cv.changeset_id = c.id AND cv.field_id = ". $effort_field_id . ")";
+        
+        $sql .= " INNER JOIN tracker_changeset_value_$type AS cvi ON(cvi.changeset_value_id = cv.id)";
+        $sql .= " WHERE c.artifact_id IN (". implode(',', $artifact_ids) .")";
+        
+        return new GraphOnTrackersV5_Burndown_Data(db_query($sql), $artifact_ids);
+    }
+    
+    protected function isValidEffortField($effort_field, $type) {
+        return $effort_field && $effort_field->userCanRead(UserManager::instance()->getCurrentUser());
+    }
+    
+    protected function isValidType($type) {
+        return in_array($type, self::TRACKER_CHANGESET_TYPE);
     }
 
 }
