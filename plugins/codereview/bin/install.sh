5068b1802be6de87d19301144a909c112485edd7
terzino <terzino@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-20 13:35:00 +0000

    Add site creation while installing the plugin

--- plugins/codereview/bin/install.sh
+++ plugins/codereview/bin/install.sh
@@ -11,6 +11,11 @@ CHOWN='/bin/chown'
 APP_USER='codendiadm'
 APP_DIR='/usr/share/codendi/plugins/codereview/reviewboard'
 APP_PWD=`/bin/grep sys_dbpasswd /etc/codendi/conf/database.inc | cut -d\" -f2`
+
+conf_value() {
+echo $(/bin/grep $1 /etc/codendi/plugins/codereview/etc/codereview.inc | cut -d\" -f2)
+}
+
 ##############################################
 # Installing the ReviewBoard database
 #
@@ -22,22 +27,26 @@ GRANT ALL PRIVILEGES on reviewboard.* to '$APP_USER'@'localhost' identified by '
 FLUSH PRIVILEGES;
 EOF
 }
+db_install
+
 # Install plugin
     $CAT <<EOF | $MYSQL -u$APP_USER codendi -p$APP_PWD
-INSERT INTO plugin (name, available) VALUES ('codereview', '1');
+INSERT INTO plugin (name, available) VALUES ('plugin_codereview', '1');
 EOF
 ##############################################
 # Installing the ReviewBoard Site
 #
 
-# rb-site install --options $APP_DIR/ --console
-##############################################
-# Updating conf
-#
-# httpd.conf append with wsgi conf
+rb-site install --noinput --domain-name=$(conf_value domain_name) --site-root=$(conf_value site_root) --media-url=$(conf_value media_url) --db-type=mysql --db-name=reviewboard --db-user=$APP_USER --db-pass=$APP_PWD --cache-type=memcached --cache-info=$(conf_value cache_info) --web-server-type=apache --python-loader=wsgi --admin-user=$(conf_value admin_user) --admin-password=$(conf_value admin_pwd) --admin-email=$(conf_value admin_mail) $APP_DIR/ --console
+
 ##############################################
 # Updating ownership
 #
 $CHOWN -R $APP_USER:$APP_USER $APP_DIR/htdocs/media/uploaded
 $CHOWN -R $APP_USER:$APP_USER $APP_DIR/data
-db_install
+
+##############################################
+# Updating conf
+#
+# httpd.conf append with wsgi conf
+
