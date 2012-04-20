4153328633adcf5cc1282a64aee00bee6b962052
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-20 08:47:40 +0000

    Fix cli footer

--- site-content/en_US/layout/footer_cli.php
+++ site-content/en_US/layout/footer_cli.php
@@ -22,7 +22,7 @@
 ?><h3>CLI</h3>
 <ul class="unstyled">
     <li><?= $GLOBALS['sys_name']?> provides a Command Line Interface based on the <a href="/soap/index.php">SOAP API</a>.<br />
-        <a href="/downloads/Codendi_CLI.zip">Download</a> and its 
+        <a href="/downloads/Codendi_CLI.zip">Download</a> &mdash;
         <a href="documentation/cli/pdf/en_US/Codendi_CLI.pdf">documentation</a>.
     </li>
 </ul>
