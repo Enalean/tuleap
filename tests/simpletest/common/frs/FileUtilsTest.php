d814185d9dfe281db31ec8d8b4f6e0523f2219ba
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-20 15:59:23 +0000

    Merge branch #724 /contrib/st/bugfix/724_fix_gitphp_issue

--- tests/simpletest/common/frs/FileUtilsTest.php
+++ tests/simpletest/common/frs/FileUtilsTest.php
@@ -14,42 +14,52 @@ class FileUtils extends UnitTestCase {
 
     function testFileUtilsGetSize_1_Mo_File() {
         $file = dirname(__FILE__) . '/_fixtures/File_1_Mo';
+        @copy(dirname(__FILE__) . '/_fixtures/File_1_Mo_sample', $file);
         $this->assertNotNull(file_utils_get_size($file));
         $this->assertTrue(file_utils_get_size($file) > 0);
         $this->assertTrue(file_utils_get_size($file) > 1000000);
         $this->assertTrue(file_utils_get_size($file) < 2000000);
+        unlink($file);
     }
     
     function testFileUtilsGetSize_1_Mo_File_with_spaces() {
         $file = dirname(__FILE__) . '/_fixtures/File 1 Mo';
+        @copy(dirname(__FILE__) . '/_fixtures/File_1_Mo_sample', $file);
         $this->assertNotNull(file_utils_get_size($file));
         $this->assertTrue(file_utils_get_size($file) > 0);
         $this->assertTrue(file_utils_get_size($file) > 1000000);
         $this->assertTrue(file_utils_get_size($file) < 2000000);
+        unlink($file);
     }
 
     function testFileUtilsGetSize_1_Mo_File_with_spaces_and_quotes() {
         $file = dirname(__FILE__) . '/_fixtures/File "1" Mo';
+        @copy(dirname(__FILE__) . '/_fixtures/File_1_Mo_sample', $file);
         $this->assertNotNull(file_utils_get_size($file));
         $this->assertTrue(file_utils_get_size($file) > 0);
         $this->assertTrue(file_utils_get_size($file) > 1000000);
         $this->assertTrue(file_utils_get_size($file) < 2000000);
+        unlink($file);
     }
     
     function testFileUtilsGetSize_1_Mo_File_with_spaces_and_quote() {
         $file = dirname(__FILE__) . '/_fixtures/File "1 Mo';
+        @copy(dirname(__FILE__) . '/_fixtures/File_1_Mo_sample', $file);
         $this->assertNotNull(file_utils_get_size($file));
         $this->assertTrue(file_utils_get_size($file) > 0);
         $this->assertTrue(file_utils_get_size($file) > 1000000);
         $this->assertTrue(file_utils_get_size($file) < 2000000);
+        unlink($file);
     }
     
     function testFileUtilsGetSize_1_Mo_File_with_spaces_and_simple_quote() {
         $file = dirname(__FILE__) . "/_fixtures/File '1 Mo";
+        @copy(dirname(__FILE__) . '/_fixtures/File_1_Mo_sample', $file);
         $this->assertNotNull(file_utils_get_size($file));
         $this->assertTrue(file_utils_get_size($file) > 0);
         $this->assertTrue(file_utils_get_size($file) > 1000000);
         $this->assertTrue(file_utils_get_size($file) < 2000000);
+        unlink($file);
     }
 }
 ?>
