03a77c602e21e1c6e9af10db86a09d3a50afd824
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-26 12:11:02 +0000

    Fix link to cli documentation in footer

--- site-content/en_US/layout/footer_cli.php
+++ site-content/en_US/layout/footer_cli.php
@@ -23,7 +23,7 @@
 <ul class="unstyled">
     <li><?= $GLOBALS['sys_name']?> provides a Command Line Interface based on the <a href="/soap/index.php">SOAP API</a>.<br />
         <a href="/downloads/Codendi_CLI.zip">Download</a> &mdash;
-        <a href="documentation/cli/pdf/en_US/Codendi_CLI.pdf">documentation</a>.
+        <a href="/documentation/cli/html/en_US/">documentation</a>.
     </li>
 </ul>
 <?php
--- site-content/fr_FR/layout/footer_cli.php
+++ site-content/fr_FR/layout/footer_cli.php
@@ -23,7 +23,7 @@
 <ul class="unstyled">
     <li><?= $GLOBALS['sys_name']?> vous fournit un Client en Ligne de 
     Commande (CLI) basé sur <a href="/soap/index.php">l'API SOAP</a>.<br />
-    <a href="/downloads/Codendi_CLI.zip">Télécharger</a> &mdash; <a href="documentation/cli/html/fr_FR/">Documentation</a>
+    <a href="/downloads/Codendi_CLI.zip">Télécharger</a> &mdash; <a href="/documentation/cli/html/fr_FR/">Documentation</a>
     </li>
 </ul>
 <?php
