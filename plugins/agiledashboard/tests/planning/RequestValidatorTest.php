c7d8924362d266fd00da9f91fdaeb9e76ed38e78
sebn <sebn@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-26 08:23:54 +0000

    Extracted planning creation request builder to a separate file

--- plugins/agiledashboard/tests/planning/RequestValidatorTest.php
+++ plugins/agiledashboard/tests/planning/RequestValidatorTest.php
@@ -19,49 +19,7 @@
  */
 
 require_once dirname(__FILE__).'/../../include/Planning/RequestValidator.class.php';
-
-class TestPlanningCreationRequestBuilder {
-    public function __construct() {
-        $this->group_id            = '123';
-        $this->planning_name       = 'My Planning';
-        $this->planning_tracker_id = '1';
-        $this->backlog_tracker_ids = array('2', '3');
-    }
-    
-    public function withGroupId($group_id) {
-        $this->group_id = $group_id;
-        return $this;
-    }
-    
-    public function withPlanningName($planning_name) {
-        $this->planning_name = $planning_name;
-        return $this;
-    }
-    
-    public function withBacklogTrackerIds($backlog_tracker_ids) {
-        $this->backlog_tracker_ids = $backlog_tracker_ids;
-        return $this;
-    }
-    
-    public function withPlanningTrackerId($planning_tracker_id) {
-        $this->planning_tracker_id = $planning_tracker_id;
-        return $this;
-    }
-    
-    public function build() {
-        return new Codendi_Request(array(
-            'group_id'            => $this->group_id,
-            'planning_name'       => $this->planning_name,
-            'planning_tracker_id' => $this->planning_tracker_id,
-            'backlog_tracker_ids' => $this->backlog_tracker_ids
-        ));
-    }
-}
-
-// TODO: extract to a separate file
-function aPlanningCreationRequest() {
-    return new TestPlanningCreationRequestBuilder();
-}
+require_once dirname(__FILE__).'/../builders/planning_creation_request.php';
 
 class RequestValidatorTest extends TuleapTestCase {
     
