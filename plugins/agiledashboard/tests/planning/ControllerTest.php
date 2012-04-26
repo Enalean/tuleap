26d71fb0392141b8ab51f0da206a7866368f8c8e
nterray <nterray@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-04-26 14:04:43 +0000

    Add missing constant definition

--- plugins/agiledashboard/tests/planning/ControllerTest.php
+++ plugins/agiledashboard/tests/planning/ControllerTest.php
@@ -18,6 +18,14 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+if (!defined('TRACKER_BASE_URL')) {                                             // 
+    define('TRACKER_BASE_URL', '/plugins/tracker');                             // 
+}                                                                               // TODO: use constants.php instead
+if (!defined('TRACKER_BASE_DIR')) {                                             //       (available only in trunk)
+    define('TRACKER_BASE_DIR', dirname(__FILE__) .'/../../../tracker/include'); // 
+}
+
+
 require_once dirname(__FILE__).'/../../../tracker/include/Tracker/TrackerManager.class.php';
 require_once(dirname(__FILE__).'/../../include/Planning/Controller.class.php');
 require_once(dirname(__FILE__).'/../../include/Planning/Planning.class.php');
