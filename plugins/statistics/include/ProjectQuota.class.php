4e5aa0dfd2a5f3ae897c1222d11c5e5e536f5fd9
ounis <ounis@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-05-07 14:23:57 +0000

    i18n

--- plugins/statistics/include/ProjectQuota.class.php
+++ plugins/statistics/include/ProjectQuota.class.php
@@ -16,6 +16,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once 'Statistics_ProjectQuotaDao.class.php';
+
 /**
  * Management of custom quota by project
  */
@@ -24,28 +26,39 @@ class ProjectQuota {
     protected $dao;
 
     /**
+     * Constructor of the class
+     *
+     * @return Void
+     */
+    public function __construct() {
+        //$this->dao = new Statistics_ProjectQuotaDao();
+    }
+
+    /**
      * Display the list of projects having a custom quota
      *
      * @return String
      */
     public function displayProjectQuota() {
-        $titles = array($GLOBALS['Language']->getText('global', 'Project'), $GLOBALS['Language']->getText('plugin_statistics', 'quota'), $GLOBALS['Language']->getText('global', 'delete'));
         $output = '';
-        $output .= html_build_list_table_top($titles);
-        $i = 0;
-        $output .= '<form method="post" >';
-        $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
-        $output .= '<td>Project1</td><td>10 GB</td><td><input type="checkbox" name="delete_quota[]" value="Project1" /></td>';
-        $output .= '</tr>';
-        $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
-        $output .= '<td>Project2</td><td>20 GB</td><td><input type="checkbox" name="delete_quota[]" value="Project2" /></td>';
-        $output .= '</tr>';
-        $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
-        $output .= '<input type="hidden" name ="action" value="delete" />';
-        $output .= '<td></td><td></td><td><input type="submit" /></td>';
-        $output .= '</tr>';
-        $output .= '</form>';
-        $output .= '</table>';
+        //$res    = $this->dao->getProjectsCustomQuota();
+        if ($res && !$res->isError()) {
+            $i      = 0;
+            $titles = array($GLOBALS['Language']->getText('global', 'Project'), $GLOBALS['Language']->getText('plugin_statistics', 'quota'), $GLOBALS['Language']->getText('global', 'delete'));
+            $output .= html_build_list_table_top($titles);
+            $output .= '<form method="post" >';
+            foreach ($res as $row) {
+                $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
+                $output .= '<td>'.$row['project'].'</td><td>'.$row[Statistics_ProjectQuotaDao::REQUEST_SIZE].'</td><td><input type="checkbox" name="delete_quota[]" value="'.$row[Statistics_ProjectQuotaDao::GROUP_ID].'" /></td>';
+                $output .= '</tr>';
+            }
+            $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
+            $output .= '<input type="hidden" name ="action" value="delete" />';
+            $output .= '<td></td><td></td><td><input type="submit" /></td>';
+            $output .= '</tr>';
+            $output .= '</form>';
+            $output .= '</table>';
+        }
         $output .= '<table>';
         $output .= '<form method="post" >';
         $output .= '<tr><td colspan="2"><b>'.$GLOBALS['Language']->getText('plugin_statistics', 'set_quota').'</b></td></tr>';
@@ -53,7 +66,7 @@ class ProjectQuota {
         $output .= '<td>'.$GLOBALS['Language']->getText('global', 'Project').'</td><td><input id="project" name="project" /></td>';
         $output .= '</tr>';
         $output .= '<tr>';
-        $output .= '<td>'.$GLOBALS['Language']->getText('plugin_statistics', 'quota').'</td><td><input name="quota" /></td>';
+        $output .= '<td>'.$GLOBALS['Language']->getText('plugin_statistics', 'quota').' (GB) </td><td><input name="quota" /></td>';
         $output .= '</tr>';
         $output .= '<tr>';
         $output .= '<input type="hidden" name ="action" value="add" />';
@@ -61,7 +74,7 @@ class ProjectQuota {
         $output .= '</tr>';
         $output .= '</form>';
         $output .= '</table>';
-        $js = "new ProjectAutoCompleter('project', '".util_get_dir_image_theme()."');";
+        $js     = "new ProjectAutoCompleter('project', '".util_get_dir_image_theme()."');";
         $GLOBALS['Response']->includeFooterJavascriptSnippet($js);
         return $output;
     }
@@ -74,7 +87,6 @@ class ProjectQuota {
      * @return ???
      */
     public function handleRequest($request) {
-        // TODO: i18n in feddback messages
         $validAction = new Valid_WhiteList('action', array('add', 'delete'));
         if($request->valid($validAction)) {
             $action = $request->get('action');
@@ -101,7 +113,7 @@ class ProjectQuota {
                     break;
             }
         } else {
-            $GLOBALS['Response']->addFeedback('error', 'Invalid action');
+            $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_statistics', 'invalic_action'));
         }
     }
 
@@ -115,11 +127,11 @@ class ProjectQuota {
      */
     public function addQuota($project, $quota) {
         if (empty($project)) {
-            $GLOBALS['Response']->addFeedback('error', 'No project');
+            $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_statistics', 'invalid_project'));
         } elseif (empty($quota)) {
-            $GLOBALS['Response']->addFeedback('error', 'No quota');
+            $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_statistics', 'invalid_quota'));
         } else {
-            $GLOBALS['Response']->addFeedback('info', 'Quota for project "'.$project.'" is now '.$quota.' GB');
+            $GLOBALS['Response']->addFeedback('info', $GLOBALS['Language']->getText('plugin_statistics', 'quota_added', array($project, $quota)));
         }
     }
 
@@ -132,9 +144,9 @@ class ProjectQuota {
      */
     public function deleteCustomQuota($projects) {
         if (empty($projects)) {
-            $GLOBALS['Response']->addFeedback('error', 'Nothing to delete');
+            $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_statistics', 'delete_error'));
         } else {
-            $GLOBALS['Response']->addFeedback('info', 'Custom quota deleted for '.join(', ', $projects));
+            $GLOBALS['Response']->addFeedback('info', $GLOBALS['Language']->getText('plugin_statistics', 'quota_added', array(join(', ', $projects))));
         }
     }
 
