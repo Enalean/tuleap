83d09b2f81bbf4faad7d90c577526a1b42386402
hosniah <hosniah@69b26aa9-6588-4999-a7e6-a99cda329b54>
2012-05-09 11:59:12 +0000

    Remove useless git statistics dao class

--- plugins/git/include/GitDao.class.php
+++ plugins/git/include/GitDao.class.php
@@ -481,6 +481,30 @@ class GitDao extends DataAccessObject {
                 LIMIT 1";
         return count($this->retrieve($sql)) > 0;
     }
+
+    /**
+     * Count all Git pushes for the given period
+     *
+     * @param Integer $projectId Id of the project we want to retrieve its git stats
+     * @param String $startDate Period start date
+     * @param String $endDate   Period end date
+     *
+     * @return DataAccessResult
+     */
+    function totalPushes($projectId, $startDate, $endDate) {
+        $sql = "SELECT DATE_FORMAT(FROM_UNIXTIME(push_date), '%M') AS month,
+                YEAR(FROM_UNIXTIME(push_date)) AS year, 
+                COUNT(*) AS pushes_count,
+                COUNT(DISTINCT(repository_id)) AS repositories, 
+                SUM(commits_number) AS commits_count, 
+                COUNT(DISTINCT(user_id)) AS users
+                FROM plugin_git_log JOIN plugin_git USING(repository_id)
+                WHERE push_date BETWEEN UNIX_TIMESTAMP(".$this->da->quoteSmart($startDate).") AND UNIX_TIMESTAMP(".$this->da->quoteSmart($endDate).")
+                  AND project_id = ".$this->da->escapeInt($projectId)." GROUP BY year, month";
+        return $this->retrieve($sql);
+    }
+
+
 }
 
 ?>
