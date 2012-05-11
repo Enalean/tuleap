b1bc936cc26d409d72af50fafc7e8d281baf0c2b
Nicolas Terray <nicolas.terray@enalean.com>
2012-05-11 19:15:36 +0200

    First step to display sprints as release children
    TODO:
    - change the url of the sprint to the url of the corresponding planning
    - (what if there is no planning for the sprnt tracker?)

--- plugins/tracker/include/Tracker/Artifact/Tracker_Artifact.class.php
+++ plugins/tracker/include/Tracker/Artifact/Tracker_Artifact.class.php
@@ -1207,6 +1207,24 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
     }
     
     /**
+     * Get artifacts linked to the current artifact if they belongs to the hierarchy
+     * 
+     * @param User $user The user who should see the artifacts
+     * 
+     * @return Array of Tracker_Artifact
+     */
+    public function getHierarchyLinkedArtifacts(User $user) {
+        $allowed_trackers = $this->getHierarchyFactory()->getChildren($this->getTrackerId());
+        $artifact_links   = $this->getLinkedArtifacts($user);
+        foreach ($artifact_links as $key => $artifact) {
+            if ( ! in_array($artifact->getTracker(), $allowed_trackers)) {
+                unset($artifact_links[$key]);
+            }
+        }
+        return $artifact_links;
+    }
+    
+    /**
      * Get artifacts linked to the current artifact if 
      * they are not in children.
      * 
