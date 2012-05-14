ed8b8762f8e490a5f36a450b40cf7810e59d637d
Gregory <gregory.salvan@enalean.com>
2012-05-14 11:17:39 +0200

    use of parameters builder in searchActionsTest

--- plugins/fulltextsearch/tests/FullTextSearchActionsTest.php
+++ plugins/fulltextsearch/tests/FullTextSearchActionsTest.php
@@ -20,76 +20,50 @@
 
 require_once dirname(__FILE__).'/Constants.php';
 require_once dirname(__FILE__).'/clients/ElasticSearchFakeClient.php';
-require_once dirname(__FILE__).'/builders/Docman_File_Builder.php';
-
-require_once DOCMAN_INCLUDE_PATH.'/Docman_Version.class.php';
+require_once dirname(__FILE__).'/builders/Parameters_Builder.php';
 
 require_once dirname(__FILE__).'/../include/FullTextSearchActions.class.php';
 
 Mock::generate('ElasticSearchFakeClient');
-//Mock::generate('FullTextSearchActions');
-Mock::generate('Docman_Version');
 
 
 class FullTextSearchActionsTests extends TuleapTestCase {
     protected $client;
     protected $actions;
     protected $params;
-    protected $docman_file;
     
     public function setUp() {
         parent::setUp();
-        $this->docman_file = aDocman_File();
         $this->client      = new MockElasticSearchFakeClient();
         $this->actions     = TestHelper::getPartialMock('FullTextSearchActions', array('file_content_encode'));
         $this->actions->__construct($this->client);
-        $this->params      = array(
-        	'item' => '',
-            'version' => new MockDocman_Version(),
-            'user' => '',
-        ); 
+        $this->params      = aSetOfParameters();
     }
     
     public function itCallIndexOnClientWithRightParameters() {
-        $expected_id          = 100;
-        $expected_group_id    = 200;
-        $expected_title       = 'title'; 
-        $expected_description = 'description';
-        $expected_path        = 'path';
-        $permissions          = array('PLUGIN_DOCMAN_READ' =>  array(3, 102));
-        
-        $this->docman_file
-            ->withId($expected_id)
-            ->withGroupId($expected_group_id)
-            ->withTitle($expected_title)
-            ->withDescription($expected_description)
-            ->withPermissions($permissions)
+        $this->params->item->withId(100)
+            ->withGroupId(200)
+            ->withPermissions(array('PLUGIN_DOCMAN_READ' =>  array(3, 102)))
         ;
         
-        $this->params['item'] = $this->docman_file->build();
-        $this->params['version']->setReturnValue('getPath',     $expected_path);
+        $expected_path = '/dir/file.php';
+        $this->params->version->setReturnValue('getPath',     $expected_path);
         $this->actions->setReturnValue('file_content_encode', $expected_path);
         
-        $expected_datas       = array(
-                'title'       => $expected_title,
-                'description' => $expected_description,
-                'file'        => $expected_path,
-                'permissions' => array($expected_group_id => array(3, 102))
-        );
-        $this->client->expectOnce('index', array($expected_datas, $expected_id));
         
-        $this->actions->indexNewDocument($this->params);
+        $this->client->expectOnce('index', $this->params->getClientIndexParameters());
+        
+        $this->actions->indexNewDocument($this->params->build());
     }
     
     
     public function itCanDeleteADocumentFromItsId() {
-        $expected_id          = 101;
-        $this->docman_file->withId($expected_id);
-        $this->params['item'] = $this->docman_file->build();
+        $expected_id = 101;
+        $this->params->item->withId($expected_id);
                 
         $this->client->expectOnce('delete', array($expected_id));
 
-        $this->actions->delete($this->params);
+        $this->actions->delete($this->params->build());
     }
     
     
