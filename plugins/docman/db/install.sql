f02613e49ec6c138d827c855c1472205c526bd08
Hatem Ounis <hatem.ounis@st.com>
2012-05-23 06:34:45 -0400

    Add DB column.

--- plugins/docman/db/install.sql
+++ plugins/docman/db/install.sql
@@ -270,6 +270,7 @@ CREATE TABLE plugin_docman_approval (
   status TINYINT(4) DEFAULT 0 NOT NULL,
   notification TINYINT(4) DEFAULT 0 NOT NULL,
   auto_status TINYINT(4) DEFAULT 0 NOT NULL,
+  notification_occurence INT(11) DEFAULT 0,
   PRIMARY KEY(table_id),
   UNIQUE KEY version_id (version_id),
   UNIQUE KEY item_id(item_id,wiki_version_id)
