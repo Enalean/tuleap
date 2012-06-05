c3a2a9845e88e2451999257d44d0dcc8632b2503
Johan Martinsson <jm1974@hotmail.com>
2012-06-05 17:09:22 +0200

    Added Class PlannifiableItem

---
+++ plugins/agiledashboard/include/Planning/PlannifiableItem.class.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__).'/../../../tracker/include/Tracker/Artifact/Tracker_Artifact.class.php';
+
+class Planning_PlannifiableItem {
+    
+    /**
+     * @var Tracker_Artifact
+     */
+    private $artifact;
+    
+    public function __construct(Tracker_Artifact $artifact) {
+        $this->artifact = $artifact;
+    }
+    
+    public function isPlannifiable() {
+        return true;
+    }
+
+    public function getEditUri() {
+        return $this->artifact->getUri();
+    }
+
+    public function getXRef() {
+        return $this->artifact->getXRef();
+    }
+    
+    public function getTitle() {
+        return $this->artifact->getTitle();
+    }
+
+    public function getId() {
+        return $this->artifact->getId();
+    }
+}
+
+?>
---
+++ plugins/agiledashboard/tests/planning/PlannifiableItemTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__).'/../../include/Planning/PlannifiableItem.class.php';
+
+class Planning_PlannifiableItemTest extends TuleapTestCase {
+    
+    /**
+     *
+     * @var Planning_PlannifiableItem
+     */
+    private $item;
+    public function setUp() {
+        parent::setUp();
+        $this->edit_uri = 'http://someurl';
+        $this->xref     = 'some #xref';
+        $this->title    = 'do something interresting';
+        $this->id       = '234872';
+        
+        $artifact = aMockArtifact()->withUri($this->edit_uri)
+                                   ->withXRef($this->xref)
+                                   ->withTitle($this->title)
+                                   ->withId($this->id)
+                                   ->build();
+        
+        $this->item = new Planning_PlannifiableItem($artifact);
+    }
+    
+    public function itIsPlannifiable() {
+        $this->assertTrue($this->item->isPlannifiable());
+    }
+    
+    public function itHasAnUri() {
+        $this->assertEqual($this->edit_uri, $this->item->getEditUri());
+    }
+    
+    public function itHasAnXRef() {
+        $this->assertEqual($this->xref, $this->item->getXRef());
+    }
+    
+    public function itHasATitle() {
+        $this->assertEqual($this->title, $this->item->getTitle());
+    }
+    
+    public function itHasAnId() {
+        $this->assertEqual($this->id, $this->item->getId());
+    }
+}
+?>
