d1c5fe9ae7ba15acaac834024fd0636c59ed7161
Ahmed HOSNI <ahmed.hosni@st.com>
2012-06-08 13:09:21 +0200

    Display reminders list for a given tracker

--- plugins/tracker/include/Tracker/Tracker_NotificationsManager.class.php
+++ plugins/tracker/include/Tracker/Tracker_NotificationsManager.class.php
@@ -293,19 +293,22 @@ class Tracker_NotificationsManager {
                         $GLOBALS['Language']->getText('plugin_tracker_date_reminder','notification_settings'),
                         $GLOBALS['Language']->getText('global', 'delete'));
         $i=0;
-        $reminderId = 999;
+        $reminderManagerDao = $this->dateReminderManager->getDao();
+        $trackerReminders   = $reminderManagerDao->getDateReminders((int)$this->tracker->id);
         $output .= html_build_list_table_top($titles);
-        $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
-        $output .= '<td>Lorem ipsum dolor sit amet</td>';
-        $output .= '<td>Lorem ipsum dolor sit amet</td>';
-        $output .= '<td>Lorem ipsum dolor sit amet</td>';
-        $output .= '<td>Lorem ipsum dolor sit amet</td>';
-        $output .= '<td><a href="?func=admin-notifications&amp;tracker='. (int)$this->tracker->id .'&amp;action=delete_reminder&amp;reminder_id='.$reminderId.'">'. $GLOBALS['Response']->getimage('ic/trash.png') .'</a></td>';
-        $output .= '</tr>';
+        foreach ($trackerReminders as $reminder) {
+            $reminder = $this->dateReminderManager->getReminder($reminder['reminder_id']);
+            $output .= '<tr class="'. util_get_alt_row_color($i++) .'">';
+            $output .= '<td>'.$reminder->fieldId.'</td>';
+            $output .= '<td>'.$reminder->distance.'</td>';
+            $output .= '<td>'.$reminder->status.'</td>';
+            $output .= '<td>'.$reminder->notificationType.'</td>';
+            $output .= '<td><a href="?func=admin-notifications&amp;tracker='.(int)$this->tracker->id.'&amp;action=delete_reminder&amp;reminder_id='.$reminder->reminderId.'">'. $GLOBALS['Response']->getimage('ic/trash.png') .'</a></td>';
+            $output .= '</tr>';
+        }
         $output .= '</TABLE>';
 
         $output .= '<div id="tracker_reminder">';
-        //$output .= $this->dateReminderManager->getNewDateReminderForm();
         $output .= '</div>';
         $output .= '<p><a href="?func=admin-notifications&amp;tracker='. (int)$this->tracker->id .'&amp;action=add_reminder" id="add_reminder"> Add reminder </a></p>';
         $output .= "<script type=\"text/javascript\">
