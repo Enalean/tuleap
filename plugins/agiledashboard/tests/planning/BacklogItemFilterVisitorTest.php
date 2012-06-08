28cd5cdb686d6f3e0ec9c859a10bccfef52c1b3e
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-06-08 12:13:37 +0200

    Remove trailling whitespace

--- plugins/agiledashboard/tests/planning/BacklogItemFilterVisitorTest.php
+++ plugins/agiledashboard/tests/planning/BacklogItemFilterVisitorTest.php
@@ -21,13 +21,13 @@
 require_once dirname(__FILE__).'/../../include/Planning/BacklogItemFilterVisitor.class.php';
 
 class Planning_BacklogItemFilterVisitorTest extends TuleapTestCase {
-    
+
     public function setUp() {
         parent::setUp();
 
         $this->epic_tracker_id  = 111;
         $this->story_tracker_id = 112;
-        
+
         $this->root   = new TreeNode();
         $this->epic1  = new TreeNode(array('id' => 1, 'tracker_id' => $this->epic_tracker_id));
         $this->story1 = new TreeNode(array('id' => 2, 'tracker_id' => $this->story_tracker_id));
@@ -49,11 +49,11 @@ class Planning_BacklogItemFilterVisitorTest extends TuleapTestCase {
             $this->epic2
         );
     }
-    
+
     public function itKeepsOnlyEpicsAtRoot() {
         $visitor  = new Planning_BacklogItemFilterVisitor($this->epic_tracker_id);
         $new_root = $this->root->accept($visitor);
-                
+
         $this->assertEqual(count($new_root->getChildren()), 2);
         $this->assertEqual($new_root->getChild(0), $this->epic1);
         $this->assertEqual($new_root->getChild(1), $this->epic2);
@@ -68,11 +68,11 @@ class Planning_BacklogItemFilterVisitorTest extends TuleapTestCase {
         $this->assertEqual($epic1->getChild(1), $this->story2);
         $this->assertEqual($epic1->getChild(0)->getChild(0), $this->task);
     }
-    
+
     public function itMovesTheDeepBacklogItemsToTheRoot() {
         $visitor = new Planning_BacklogItemFilterVisitor($this->story_tracker_id);
         $new_root = $this->root->accept($visitor);
-                
+
         $this->assertEqual(count($new_root->getChildren()), 3);
         $this->assertEqual($new_root->getChild(0), $this->story1);
         $this->assertEqual($new_root->getChild(1), $this->story2);
@@ -82,13 +82,13 @@ class Planning_BacklogItemFilterVisitorTest extends TuleapTestCase {
 }
 
 class Planning_BacklogItemFilterVisitor_BigDeepTest extends TuleapTestCase {
-    
+
     public function setUp() {
         parent::setUp();
 
         $this->epic_tracker_id  = 111;
         $this->story_tracker_id = 112;
-        
+
         $this->root   = new TreeNode();
         $this->theme1 = new TreeNode(array('id' => 1, 'tracker_id' => $this->epic_tracker_id));
         $this->epic1  = new TreeNode(array('id' => 2, 'tracker_id' => $this->epic_tracker_id));
@@ -109,11 +109,11 @@ class Planning_BacklogItemFilterVisitor_BigDeepTest extends TuleapTestCase {
             $this->story4
         );
     }
-    
+
     public function itMovesTheDeepBacklogItemsToTheRoot() {
         $visitor = new Planning_BacklogItemFilterVisitor($this->story_tracker_id);
         $new_root = $this->root->accept($visitor);
-                
+
         $this->assertEqual(count($new_root->getChildren()), 3);
         $this->assertEqual($new_root->getChild(0), $this->story1);
         $this->assertEqual($new_root->getChild(1), $this->story2);
