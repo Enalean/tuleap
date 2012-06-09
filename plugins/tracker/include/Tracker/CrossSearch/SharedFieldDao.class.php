874197ea1abea0c53907aaf0e01d935be6042e8e
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-06-09 15:17:21 +0200

    Revert previous commit and add a detailed comment of the bug, hope someone can fix it

--- plugins/tracker/include/Tracker/CrossSearch/SharedFieldDao.class.php
+++ plugins/tracker/include/Tracker/CrossSearch/SharedFieldDao.class.php
@@ -20,12 +20,29 @@
 
 class Tracker_CrossSearch_SharedFieldDao extends DataAccessObject {
     
+    /**
+     * @todo: this query doesn't work when $source_or_target_field_ids is the original field.
+     * 
+     * Given the SharedField criteria in UI is built with the reference field
+     * Then the reference value is not retreived by the following query.
+     * 
+     * Field 1084 (ref)
+     * Field 1107 (original_id = 1084)
+     * Field 1120 (original_id = 1084)
+     * 
+     * $source_or_target_field_ids = 1084 -> [ 1107, 1120 ]
+     * $source_or_target_field_ids = 1107 -> [ 1084, 1107, 1120 ]
+     * $source_or_target_field_ids = 1120 -> [ 1084, 1107, 1120 ]
+     * 
+     * NB: same apply for values method
+     */
     public function searchSharedFieldIds($source_or_target_field_ids) {
         $source_or_target_field_ids = $this->da->escapeInt($source_or_target_field_ids);
         
         $sql_original_ids = "
-            SELECT f.id
+            SELECT original.id
             FROM tracker_field AS f
+                    INNER JOIN tracker_field AS original ON (f.original_field_id = original.id)
             WHERE f.id = $source_or_target_field_ids
         ";
         
@@ -54,8 +71,9 @@ class Tracker_CrossSearch_SharedFieldDao extends DataAccessObject {
         
         $source_or_target_value_ids = implode(',', $source_or_target_value_ids);
         
-        $sql_original_ids = "SELECT v.id
+        $sql_original_ids = "SELECT original.id
                 FROM tracker_field_list_bind_static_value AS v
+                    INNER JOIN tracker_field_list_bind_static_value AS original ON (v.original_value_id = original.id)
                 WHERE v.id IN ($source_or_target_value_ids)";
         
         $sql_target_ids = "SELECT target.id
