13c6389a29b388b39ec74769b7dc73990da26687
Sebastien Nicouleaud <seb@sogilis.com>
2012-06-14 09:46:01 +0200

    Fixed js namespace overlapping bug

--- plugins/statistics/www/js/autocomplete.js
+++ plugins/statistics/www/js/autocomplete.js
@@ -19,10 +19,9 @@
  * Autocompleting project and user inputs within ProjectQuotaHtml::renderNewCustomQuotaForm() form
  */
 
-var tuleap = codendi || { };
 document.observe('dom:loaded', function () {
-    var prjAutocomplete  = new ProjectAutoCompleter('project', tuleap.imgroot);
-    var userAutocomplete = new UserAutoCompleter('requester', tuleap.imgroot);
+    var prjAutocomplete  = new ProjectAutoCompleter('project', codendi.imgroot);
+    var userAutocomplete = new UserAutoCompleter('requester', codendi.imgroot);
     prjAutocomplete.registerOnLoad();
     userAutocomplete.registerOnLoad();
 });
