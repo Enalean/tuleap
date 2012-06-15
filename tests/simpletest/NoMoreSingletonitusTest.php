63890a75b56951cc87fa60bc3ad0cce55bc0d0c0
Johan Martinsson <jm1974@hotmail.com>
2012-06-15 18:28:38 +0200

    fixed naming im test for singleton lookup count

--- tests/simpletest/NoMoreSingletonitusTest.php
+++ tests/simpletest/NoMoreSingletonitusTest.php
@@ -25,16 +25,15 @@
 
 class NoMoreSingletonitusTest extends TuleapTestCase {
     public function testThereAreNoNewClassFilesContainingSingletonLookups() {
-        $current_number_of_singleton_lookups = 746;
-        $systemOutput = $this->getSystemOutput('grep -rc  "::instance()" * | grep -v :0| grep -c ""');
-        $this->assertTrue($systemOutput[0] <= $current_number_of_singleton_lookups, 
+        $expected_singleton_lookups = 746;
+        $actual_singleton_lookups = $this->getSystemOutput('grep -rc  "::instance()" * | grep -v :0| grep -c ""');
+        $this->assertTrue($actual_singleton_lookups[0] <= $expected_singleton_lookups, 
                 "A new singleton lookup was introduced, please check to see if you can avoid this by injecting it
-                    before increasing the allowed number of singleton lookups");
-        $this->assertEqual($systemOutput[0] >= $current_number_of_singleton_lookups, 
+                 before increasing the allowed number of singleton lookups");
+        $this->assertEqual($actual_singleton_lookups[0] >= $expected_singleton_lookups, 
                 "Great job! You removed one or more singleton lookups, you're a Dependency Injection champion!
-                    please decrease the current_number_of_singleton_lookups variable in this test");
-        
-        
+                 please decrease the current_number_of_singleton_lookups variable in this test>
+                 It should be : $actual_singleton_lookups");
     }
     
     private function getSystemOutput($cmd) {
