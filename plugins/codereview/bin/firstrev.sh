78fb9108719d1578a15184b2f5c6655d96889fca
Hanen ELARBI <hanene.ben-elarbi@st.com>
2012-06-22 12:33:30 +0100

    remove the script svndiff & rename svndiff1 to svndiff

--- plugins/codereview/bin/firstrev.sh
+++ plugins/codereview/bin/firstrev.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 #looking for the first revision number using the path
 firstRev=$(svn log --stop-on-copy --xml $1 | grep revision | tail -1 | cut -d'"' -f2 | cut -d'"' -f1)
 echo $firstRev
--- plugins/codereview/bin/lastrev.sh
+++ plugins/codereview/bin/lastrev.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 #looking for the last revision number using the path
 REVISION=$(svn info $1 |grep '^Revision:' | sed -e 's/^Revision: //')
 echo $REVISION
