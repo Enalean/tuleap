05164e528f2c91b78395bfb3dc9bcecafa166a86
Nicolas Terray <nicolas.terray@enalean.com>
2012-06-22 11:20:38 +0200

    Update the rank when modifying the order

--- plugins/tracker/include/Tracker/Artifact/dao/PriorityDao.class.php
+++ plugins/tracker/include/Tracker/Artifact/dao/PriorityDao.class.php
@@ -25,18 +25,34 @@ class Tracker_Artifact_PriorityDao extends DataAccessObject {
     public function artifactHasAHigherPriorityThan($artifact_id, $successor_id) {
         $artifact_id  = $this->da->escapeInt($artifact_id);
         $successor_id = $this->da->escapeInt($successor_id);
-        $sql = "UPDATE tracker_artifact_priority AS new_parent,
-                    tracker_artifact_priority AS item_to_move
-                    INNER JOIN tracker_artifact_priority AS previous_parent
-                            ON (previous_parent.succ_id = item_to_move.curr_id)
+        //TODO: Transaction?
+        $sql = "SELECT rank FROM tracker_artifact_priority WHERE curr_id = $artifact_id";
+        $row = $this->retrieve($sql)->getRow();
 
-                SET previous_parent.succ_id = item_to_move.succ_id,
-                    item_to_move.succ_id    = new_parent.succ_id,
-                    new_parent.succ_id      = item_to_move.curr_id
+        if ($row) {
+            $sql = "UPDATE tracker_artifact_priority AS new_parent,
+                        tracker_artifact_priority AS item_to_move
+                        INNER JOIN tracker_artifact_priority AS previous_parent
+                                ON (previous_parent.succ_id = item_to_move.curr_id)
 
-                WHERE new_parent.succ_id   = $successor_id
-                  AND item_to_move.curr_id = $artifact_id";
-        return $this->update($sql);
+                    SET previous_parent.succ_id = item_to_move.succ_id,
+                        item_to_move.succ_id    = new_parent.succ_id,
+                        item_to_move.rank       = new_parent.rank + IF(item_to_move.rank < new_parent.rank, 0, 1),
+                        new_parent.succ_id      = item_to_move.curr_id
+
+                    WHERE new_parent.succ_id   = $successor_id
+                      AND item_to_move.curr_id = $artifact_id";
+            if ($this->update($sql)) {
+                $previous_rank = $row['rank'];
+                $sql = "UPDATE tracker_artifact_priority AS intermediate
+                            INNER JOIN tracker_artifact_priority AS new_position
+                                    ON (new_position.curr_id = $artifact_id AND intermediate.curr_id <> $artifact_id)
+                        SET intermediate.rank = intermediate.rank + IF($previous_rank < new_position.rank, -1, 1)
+                        WHERE ($previous_rank <= intermediate.rank AND intermediate.rank <= new_position.rank)
+                           OR (new_position.rank <= intermediate.rank AND intermediate.rank <= $previous_rank)";
+                return $this->update($sql);
+            }
+        }
     }
 
     public function artifactHasTheLeastPriority($artifact_id) {
