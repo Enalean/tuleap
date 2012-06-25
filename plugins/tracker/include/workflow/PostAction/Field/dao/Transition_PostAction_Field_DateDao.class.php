6513d6b28228d9e822aefb92e23ad3be14d08ba3
Sebastien Nicouleaud <seb@sogilis.com>
2012-06-25 18:04:43 +0200

    Extracted base class for field post action DAOs

--- plugins/tracker/include/workflow/PostAction/Field/dao/Transition_PostAction_Field_DateDao.class.php
+++ plugins/tracker/include/workflow/PostAction/Field/dao/Transition_PostAction_Field_DateDao.class.php
@@ -18,192 +18,24 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once 'Transition_PostAction_FieldDao.class.php';
+
 /**
  * Data Acces object which deals with tracker_workflow_transition_postactions_field_date table
  */
-class Transition_PostAction_Field_DateDao extends DataAccessObject {
-
-    /**
-     * Create a new postaction entry
-     *
-     * @param int $transition_id The transition the post action belongs to
-     *
-     * @return bool true if success false otherwise
-     */
-    public function create($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "INSERT INTO tracker_workflow_transition_postactions_field_date 
-                (transition_id) 
-                VALUES 
-                ($transition_id)";
-        return $this->updateAndGetLastId($sql);
+class Transition_PostAction_Field_DateDao extends Transition_PostAction_FieldDao {
+    
+    function __construct() {
+        parent::__construct();
+        $this->table_name        = 'tracker_workflow_transition_postactions_field_date';
+        $this->value_column_name = 'value_type';
     }
     
     /**
-     * Update postaction entry
-     *
-     * @param int $id         The id of the postaction 
-     * @param int $field_id   The id of the associated field
-     * @param int $value_type The value_type. 
-     *
-     * @return bool true if success false otherwise
+     * @see Transition_PostAction_FieldDao
      */
-    public function updatePostAction($id, $field_id, $value_type) {
-        $id         = $this->da->escapeInt($id);
-        $field_id   = $this->da->escapeInt($field_id);
-        $value_type = $this->da->escapeInt($value_type);
-        $sql = "UPDATE tracker_workflow_transition_postactions_field_date 
-                SET field_id   = $field_id, 
-                    value_type = $value_type
-                WHERE id = $id";
-        return $this->update($sql);
+    protected function escapeValue($value) {
+        return $this->da->escapeInt($value);
     }
-    
-    /**
-     * Create a full-featured postaction.
-     * 
-     * @param int $transition_id
-     * @param int $field_id
-     * @param int $value_type
-     * 
-     * @return bool
-     */
-    public function save($transition_id, $field_id, $value_type) {
-        if (($post_action_id = $this->create($transition_id)) > 0) {
-            $this->updatePostAction($post_action_id, $field_id, $value_type);
-        }
-    }
-    
-    /**
-     * Search all postactions belonging to a transition
-     *
-     * @param int $transition_id The id of the transition 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByTransitionId($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_date 
-                WHERE transition_id = $transition_id
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    /**
-     * Search all postactions belonging to a transition and a field
-     *
-     * Useful to know if a field is already used in a post action of a transition
-     *
-     * @param int $transition_id The id of the transition 
-     * @param int $field_id      The id of the field 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByTransitionIdAndFieldId($transition_id, $field_id) {
-        $field_id      = $this->da->escapeInt($field_id);
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_date 
-                WHERE field_id      = $field_id 
-                  AND transition_id = $transition_id
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    /**
-     * Search all postactions belonging to a field
-     *
-     * Useful to know if a field is already used in a post action
-     *
-     * @param int $field_id The id of the field 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByFieldId($field_id) {
-        $field_id      = $this->da->escapeInt($field_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_date 
-                WHERE field_id = $field_id 
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    public function countByFieldId($field_id) {
-        return count($this->searchByFieldId($field_id));
-    }
-    
-    /**
-     * Delete a postaction entry
-     *
-     * @param int $id The id of the postaction 
-     *
-     * @return bool true if success false otherwise
-     */
-    public function deletePostAction($id) {
-        $id = $this->da->escapeInt($id);
-        $sql = "DELETE 
-                FROM tracker_workflow_transition_postactions_field_date 
-                WHERE id = $id";
-        return $this->update($sql);
-    }
-    
-    /**
-     * Delete a postaction entry by transition id
-     *
-     * @param int $id The id of the transition
-     *
-     * @return bool true if success false otherwise
-     */
-    public function deletePostActionsByTransitionId($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "DELETE 
-                FROM tracker_workflow_transition_postactions_field_date 
-                WHERE transition_id = $transition_id";
-        return $this->update($sql);
-    }
-    
-   /**
-    * Delete a postaction entries by workflow_id
-    *
-    * @param int $id The id of the workflow
-    *
-    * @return bool true if success false otherwise
-    */
-    public function deletePostActionsByWorkflowId($workflow_id) {
-        $workflow_id = $this->da->escapeInt($workflow_id);
-        $sql = "DELETE P
-                FROM tracker_workflow_transition_postactions_field_date AS P
-                   INNER JOIN tracker_workflow_transition AS T ON P.transition_id = T.transition_id
-                WHERE T.workflow_id = $workflow_id";
-        return $this->update($sql);
-    }
-    
-   /**
-    * Duplicate a postaction 
-    *
-    * @param int $from_transition_id The id of the template transition
-    * @param int $to_transition_id The id of the transition
-    * @param int $from_field_id The id of the field (from template)
-    * @param int $to_field_id The id of the field
-    *
-    * @return bool true if success false otherwise
-    */
-    public function duplicate($from_transition_id, $to_transition_id, $from_field_id, $to_field_id) {
-        
-        $from_transition_id = $this->da->escapeInt($from_transition_id);
-        $to_transition_id = $this->da->escapeInt( $to_transition_id);
-        $from_field_id = $this->da->escapeInt($from_field_id);
-        $to_field_id = $this->da->escapeInt($to_field_id);
-        
-        $sql = "INSERT INTO tracker_workflow_transition_postactions_field_date (transition_id, field_id, value_type)
-                SELECT $to_transition_id, $to_field_id, value_type
-                FROM tracker_workflow_transition_postactions_field_date
-                WHERE field_id = $from_field_id AND 
-                      transition_id = $from_transition_id";
-                      
-        return $this->update($sql);
-    }
-    
 }
 ?>
--- plugins/tracker/include/workflow/PostAction/Field/dao/Transition_PostAction_Field_IntDao.class.php
+++ plugins/tracker/include/workflow/PostAction/Field/dao/Transition_PostAction_Field_IntDao.class.php
@@ -18,7 +18,12 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Transition_PostAction_Field_IntDao extends DataAccessObject {
+require_once 'Transition_PostAction_FieldDao.class.php';
+
+/**
+ * Data Acces object which deals with tracker_workflow_transition_postactions_field_int table
+ */
+class Transition_PostAction_Field_IntDao extends Transition_PostAction_FieldDao {
     
     function __construct() {
         parent::__construct();
@@ -26,173 +31,10 @@ class Transition_PostAction_Field_IntDao extends DataAccessObject {
     }
     
     /**
-     * Create a new postaction entry
-     *
-     * @param int $transition_id The transition the post action belongs to
-     *
-     * @return bool true if success false otherwise
+     * @see Transition_PostAction_FieldDao
      */
-    public function create($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "INSERT INTO tracker_workflow_transition_postactions_field_int
-                (transition_id) 
-                VALUES 
-                ($transition_id)";
-        return $this->updateAndGetLastId($sql);
-    }
-    
-    /**
-     * Create a full-featured postaction.
-     * 
-     * @param int $transition_id
-     * @param int $field_id
-     * @param int $value
-     * 
-     * @return bool
-     */
-    public function save($transition_id, $field_id, $value) {
-        if (($post_action_id = $this->create($transition_id)) > 0) {
-            $this->updatePostAction($post_action_id, $field_id, $value);
-        }
-    }
-    
-        /**
-     * Search all postactions belonging to a transition
-     *
-     * @param int $transition_id The id of the transition 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByTransitionId($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_int
-                WHERE transition_id = $transition_id
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    /**
-     * Search all postactions belonging to a transition and a field
-     *
-     * Useful to know if a field is already used in a post action of a transition
-     *
-     * @param int $transition_id The id of the transition 
-     * @param int $field_id      The id of the field 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByTransitionIdAndFieldId($transition_id, $field_id) {
-        $field_id      = $this->da->escapeInt($field_id);
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_int
-                WHERE field_id      = $field_id 
-                  AND transition_id = $transition_id
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    /**
-     * Search all postactions belonging to a field
-     *
-     * Useful to know if a field is already used in a post action
-     *
-     * @param int $field_id The id of the field 
-     *
-     * @return DataAccessResult
-     */
-    public function searchByFieldId($field_id) {
-        $field_id      = $this->da->escapeInt($field_id);
-        $sql = "SELECT * 
-                FROM tracker_workflow_transition_postactions_field_int
-                WHERE field_id = $field_id 
-                ORDER BY id";
-        return $this->retrieve($sql);
-    }
-    
-    public function countByFieldId($field_id) {
-        return count($this->searchByFieldId($field_id));
-    }
-    
-    public function updatePostAction($id, $field_id, $value) {
-        $id       = $this->da->escapeInt($id);
-        $field_id = $this->da->escapeInt($field_id);
-        $value    = $this->da->escapeInt($value);
-        
-        $sql = <<<SQL
-            UPDATE $this->table_name
-            SET field_id = $field_id, value = $value
-            WHERE id = $id
-SQL;
-        return $this->update($sql);
-    }
-    
-    public function deletePostAction($id) {
-        $id = $this->da->escapeInt($id);
-        $sql = <<<SQL
-            DELETE
-            FROM $this->table_name
-            WHERE id = $id
-SQL;
-        return $this->update($sql);
-    }
-    
-    /**
-     * Delete a postaction entry by transition id
-     *
-     * @param int $id The id of the transition
-     *
-     * @return bool true if success false otherwise
-     */
-    public function deletePostActionsByTransitionId($transition_id) {
-        $transition_id = $this->da->escapeInt($transition_id);
-        $sql = "DELETE 
-                FROM tracker_workflow_transition_postactions_field_int 
-                WHERE transition_id = $transition_id";
-        return $this->update($sql);
-    }
-    
-   /**
-    * Delete a postaction entries by workflow_id
-    *
-    * @param int $id The id of the workflow
-    *
-    * @return bool true if success false otherwise
-    */
-    public function deletePostActionsByWorkflowId($workflow_id) {
-        $workflow_id = $this->da->escapeInt($workflow_id);
-        $sql = "DELETE P
-                FROM tracker_workflow_transition_postactions_field_int AS P
-                   INNER JOIN tracker_workflow_transition AS T ON P.transition_id = T.transition_id
-                WHERE T.workflow_id = $workflow_id";
-        return $this->update($sql);
-    }
-    
-   /**
-    * Duplicate a postaction 
-    *
-    * @param int $from_transition_id The id of the template transition
-    * @param int $to_transition_id The id of the transition
-    * @param int $from_field_id The id of the field (from template)
-    * @param int $to_field_id The id of the field
-    *
-    * @return bool true if success false otherwise
-    */
-    public function duplicate($from_transition_id, $to_transition_id, $from_field_id, $to_field_id) {
-        
-        $from_transition_id = $this->da->escapeInt($from_transition_id);
-        $to_transition_id = $this->da->escapeInt( $to_transition_id);
-        $from_field_id = $this->da->escapeInt($from_field_id);
-        $to_field_id = $this->da->escapeInt($to_field_id);
-        
-        $sql = "INSERT INTO tracker_workflow_transition_postactions_field_int (transition_id, field_id, value)
-                SELECT $to_transition_id, $to_field_id, value
-                FROM tracker_workflow_transition_postactions_field_int
-                WHERE field_id = $from_field_id AND 
-                      transition_id = $from_transition_id";
-                      
-        return $this->update($sql);
+    protected function escapeValue($value) {
+        return $this->da->escapeInt($value);
     }
 }
 ?>
