5fe4dc6191deaccf4a01edc0d461c2ae4d0cc61b
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-06-25 11:52:55 +0200

    Cleanup

--- plugins/tracker/db/mysql/updates/2012/201206251148_add_table_for_aggregate_field.php
+++ plugins/tracker/db/mysql/updates/2012/201206251148_add_table_for_aggregate_field.php
@@ -22,15 +22,15 @@
  *
  */
 class b201206251148_add_table_for_aggregate_field extends ForgeUpgrade_Bucket {
-    
+
     public function description() {
         return 'Add a new table to store aggregate fields';
     }
-    
+
     public function preUp() {
         $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
     }
-    
+
     public function up() {
         $sql = <<<SQL
         CREATE TABLE tracker_field_aggregate (
@@ -39,12 +39,12 @@ class b201206251148_add_table_for_aggregate_field extends ForgeUpgrade_Bucket {
         )
 SQL;
         $result = $this->db->createTable('tracker_field_aggregate', $sql);
-        
+
         if ($result === false) {
             $error_message = implode(', ', $this->db->dbh->errorInfo());
             throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete($error_message);
         }
     }
-    
+
 }
 ?>
