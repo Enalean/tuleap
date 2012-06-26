263d34950d0c6dd0bbb8faeb79b029f20b53b6c4
Johan Martinsson <jm1974@hotmail.com>
2012-06-26 10:54:06 +0200

    basic file for renames

---
+++ tools/utils/class_renamer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+old_name=$1
+new_name=$2
+
+# Replace imports and class names
+files=$(grep -rl  $old_name *)
+gsed -i "s/$old_name/$new_name/" $files
+
+# rename file
+old_file_full_path=$(find . -name $old_name.class.php)
+new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
+git mv $old_file_full_path $new_file_full_path
