5e507e778017c2af007b45c42d4f04448de98841
Hanen ELARBI <hanene.ben-elarbi@st.com>
2012-06-26 10:15:34 +0100

    Change var name

--- plugins/codereview/bin/lastrev.sh
+++ plugins/codereview/bin/lastrev.sh
@@ -1,3 +1,3 @@
 #looking for the last revision number using the path
-REVISION=$(svn info $1 |grep '^Revision:' | sed -e 's/^Revision: //')
-echo $REVISION
+revision=$(svn info $1 |grep '^Revision:' | sed -e 's/^Revision: //')
+echo $revision
