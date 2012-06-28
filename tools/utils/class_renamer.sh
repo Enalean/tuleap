2c8d23c896f5dab3bf602f411d5c94397f96400a
Johan Martinsson <jm1974@hotmail.com>
2012-06-28 12:18:31 +0200

    renamer now handles test files

--- tools/utils/class_renamer.sh
+++ tools/utils/class_renamer.sh
@@ -10,6 +10,11 @@ files=$(grep -rl  $old_name *)
 gsed -i "s/$old_name/$new_name/" $files
 
 # rename file
-old_file_full_path=$(find . -name $old_name.class.php)
+old_file_full_path=$(find . -name ${old_name}.class.php)
+new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
+git mv $old_file_full_path $new_file_full_path
+
+# rename test file
+old_file_full_path=$(find . -name ${old_name}Test.php)
 new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
 git mv $old_file_full_path $new_file_full_path
