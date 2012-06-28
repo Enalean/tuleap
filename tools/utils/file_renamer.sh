c9df4f698ae1d3b8d7ec5c3eb94494f7f05f72d9
Johan Martinsson <jm1974@hotmail.com>
2012-06-28 12:36:25 +0200

    file rename, we have to move the file also!

--- tools/utils/file_renamer.sh
+++ tools/utils/file_renamer.sh
@@ -7,3 +7,8 @@ new_filename=$2.class.php
 
 imports=$(grep -rl -e "require.*$old_filename" *)
 gsed -i "s/$old_filename/$new_filename/" $imports
+
+old_file_full_path=$(find . -name ${old_filename})
+new_file_full_path=$(echo $old_file_full_path | sed "s/$old_filename/$new_filename/")
+
+git mv $old_file_full_path $new_file_full_path
