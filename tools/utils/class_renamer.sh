8471e39ec1f325da2441f2a67f856e9ea93c6c8b
Johan Martinsson <jm1974@hotmail.com>
2012-06-28 15:39:52 +0200

    rename tool now handles non existing test classes

--- tools/utils/class_renamer.sh
+++ tools/utils/class_renamer.sh
@@ -12,11 +12,13 @@ files=$(grep -rl  $old_name *)
 gsed -i "s/$old_name/$new_name/" $files
 
 # rename file
-old_file_full_path=$(find . -name ${old_name}.class.php)
+old_file_full_path=$(find . -name *${old_name}.class.php)
 new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
 git mv $old_file_full_path $new_file_full_path
 
 # rename test file
-old_file_full_path=$(find . -name ${old_name}Test.php)
-new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
-git mv $old_file_full_path $new_file_full_path
+old_file_full_path=$(find . -name *${old_name}Test.php)
+if [ -n "$old_file_full_path" ]; then
+    new_file_full_path=$(echo $old_file_full_path | sed "s/$old_name/$new_name/")
+    git mv $old_file_full_path $new_file_full_path
+fi
