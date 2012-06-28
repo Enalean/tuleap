1efefe7c1462f058cfbbc3c7ff9fa3f4ac058cd5
Nouha Terzi <nouha.terzi@st.com>
2012-06-28 16:04:37 +0100

    Add comment format to updateArtifact soap method

--- plugins/tracker/include/soap.php
+++ plugins/tracker/include/soap.php
@@ -374,7 +374,8 @@ $GLOBALS['server']->register(
         'tracker_id'=>'xsd:int',
         'artifact_id'=>'xsd:int',
         'value'=>'tns:ArrayOfArtifactFieldValue',
-        'comment' => 'xsd:string'
+        'comment' => 'xsd:string',
+        'commentFormat' => 'xsd:int'
     ),
     array('return'=>'xsd:int'),
     $GLOBALS['uri'],
@@ -928,7 +929,8 @@ function addArtifact($sessionKey, $group_id, $tracker_id, $value) {
  * @param int    $tracker_id  the ID of the tracker we want to update the artifact
  * @param int    $artifact_id the ID of the artifact to update
  * @param array{SOAPArtifactFieldValue} $value the fields value to update
- * @param string $comment     the comment associated with the modification, or null if no follow-up comment.
+ * @param string  $comment     the comment associated with the modification, or null if no follow-up comment.
+ * @param string  $commentFormat     The comment (follow-up) type (text/html)
  *
  * @return int The artifact id if update was fine,
  *              or a soap fault if :
@@ -938,7 +940,7 @@ function addArtifact($sessionKey, $group_id, $tracker_id, $value) {
  *              - the given values are breaking a field dependency rule
  *              - the artifact modification failed.
  */
-function updateArtifact($sessionKey, $group_id, $tracker_id, $artifact_id, $value, $comment) {
+function updateArtifact($sessionKey, $group_id, $tracker_id, $artifact_id, $value, $comment, $commentFormat = 0) {
     if (session_continue($sessionKey)) {
         $user = UserManager::instance()->getCurrentUser();
         $pm = ProjectManager::instance();
@@ -999,8 +1001,8 @@ function updateArtifact($sessionKey, $group_id, $tracker_id, $artifact_id, $valu
                     }
                 }
             }
-            
-            if ($artifact->createNewChangeset($fields_data, $comment, $user, null)) {
+
+            if ($artifact->createNewChangeset($fields_data, $comment, $user, null, true, $commentFormat)) {
                 return $artifact_id;
             } else {
                 $response = new Response();
