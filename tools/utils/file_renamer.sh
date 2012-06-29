d7e32cf0ea20a24d5bbd6ab757a3f906403dbb9c
Johan Martinsson <jm1974@hotmail.com>
2012-06-29 11:34:52 +0200

    we can now move to a new folder

--- tools/utils/file_renamer.sh
+++ tools/utils/file_renamer.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # modifies a file name and php imports of this file
+# ex : file_renamer.sh Pane Cardwall_Pane
+# ex : file_renamer.sh Pane Cardwall/Cardwall_Pane
 
 old_filename=$1.class.php
 new_filename=$2.class.php
 
 imports=$(grep -rl -e "require.*$old_filename" *)
-gsed -i "s/$old_filename/$new_filename/" $imports
+gsed -i "s%$old_filename%$new_filename%" $imports
 
 old_file_full_path=$(find . -name ${old_filename})
-new_file_full_path=$(echo $old_file_full_path | sed "s/$old_filename/$new_filename/")
+new_file_full_path=$(echo $old_file_full_path | sed "s%$old_filename%$new_filename%")
 
 git mv $old_file_full_path $new_file_full_path
