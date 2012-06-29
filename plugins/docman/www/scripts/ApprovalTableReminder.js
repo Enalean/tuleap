38dcdb21193392c9a430f932657b3cd463e939e0
Ahmed HOSNI <ahmed.hosni@st.com>
2012-06-29 16:27:03 +0100

    Fix issue with JS var name

--- plugins/docman/www/scripts/ApprovalTableReminder.js
+++ plugins/docman/www/scripts/ApprovalTableReminder.js
@@ -19,9 +19,9 @@
  * Script of the approval table reminder
  */
 
-var tuleap = codendi || { };
+var codendi = codendi || { };
 document.observe('dom:loaded', function () {
-    var userAutocomplete = new UserAutoCompleter('user_list', tuleap.imgroot, true);
+    var userAutocomplete = new UserAutoCompleter('user_list', codendi.imgroot, true);
     userAutocomplete.registerOnLoad();
     if (!$('approval_table_reminder_checkbox').checked) {
         Element.toggle('approval_table_occurence_form', 'slide', { duration: 0 });
