3e38bdac06e51bedc4c7ad81cc778e4a6edf4358
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-07-03 17:44:27 +0200

    Retrieve field value at a given date

--- plugins/tracker/include/Tracker/FormElement/dao/Tracker_FormElement_Field_Value_IntegerDao.class.php
+++ plugins/tracker/include/Tracker/FormElement/dao/Tracker_FormElement_Field_Value_IntegerDao.class.php
@@ -58,5 +58,21 @@ class Tracker_FormElement_Field_Value_IntegerDao extends Tracker_FormElement_Fie
                 WHERE changeset_value_id = $from";
         return $this->update($sql);
     }
+    
+    public function getValueAt($artifact_id, $field_id, $timestamp) {
+        $artifact_id = $this->da->escapeInt($artifact_id);
+        $field_id    = $this->da->escapeInt($field_id);
+        $timestamp   = $this->da->escapeInt($timestamp);
+
+        $sql = "SELECT cvi.value
+                FROM        tracker_changeset       c1 
+                  LEFT JOIN tracker_changeset       c2  ON (c2.artifact_id         = c1.artifact_id AND c1.submitted_on < c2.submitted_on AND c2.submitted_on < $timestamp)
+                  JOIN      tracker_changeset_value cv  ON (cv.changeset_id        = c1.id          AND cv.field_id     = $field_id)
+                  JOIN      $this->table_name       cvi ON (cvi.changeset_value_id = cv.id)
+                WHERE c1.artifact_id  = $artifact_id
+                  AND c1.submitted_on < $timestamp
+                  AND c2.id          IS NULL";
+        return $this->retrieveFirstRow($sql);
+    }
 }
 ?>
