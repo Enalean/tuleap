41b18b3ede2c397db044a469c859033f6b7ef723
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-07-03 18:09:59 +0200

    Centralize implementation of getValueAt on numeric fields

--- plugins/tracker/include/Tracker/FormElement/dao/Tracker_FormElement_Field_Value_IntegerDao.class.php
+++ plugins/tracker/include/Tracker/FormElement/dao/Tracker_FormElement_Field_Value_IntegerDao.class.php
@@ -18,8 +18,9 @@
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('Tracker_FormElement_Field_ValueDao.class.php');
-class Tracker_FormElement_Field_Value_IntegerDao extends Tracker_FormElement_Field_ValueDao {
+require_once('Tracker_FormElement_Field_Value_NumericDao.class.php');
+
+class Tracker_FormElement_Field_Value_IntegerDao extends Tracker_FormElement_Field_Value_NumericDao {
     
     public function __construct() {
         parent::__construct();
@@ -57,22 +58,6 @@ class Tracker_FormElement_Field_Value_IntegerDao extends Tracker_FormElement_Fie
                 FROM $this->table_name
                 WHERE changeset_value_id = $from";
         return $this->update($sql);
-    }
-    
-    public function getValueAt($artifact_id, $field_id, $timestamp) {
-        $artifact_id = $this->da->escapeInt($artifact_id);
-        $field_id    = $this->da->escapeInt($field_id);
-        $timestamp   = $this->da->escapeInt($timestamp);
-
-        $sql = "SELECT cvi.value
-                FROM        tracker_changeset       c1 
-                  LEFT JOIN tracker_changeset       c2  ON (c2.artifact_id         = c1.artifact_id AND c1.submitted_on < c2.submitted_on AND c2.submitted_on < $timestamp)
-                  JOIN      tracker_changeset_value cv  ON (cv.changeset_id        = c1.id          AND cv.field_id     = $field_id)
-                  JOIN      $this->table_name       cvi ON (cvi.changeset_value_id = cv.id)
-                WHERE c1.artifact_id  = $artifact_id
-                  AND c1.submitted_on < $timestamp
-                  AND c2.id          IS NULL";
-        return $this->retrieveFirstRow($sql);
     }
 }
 ?>
