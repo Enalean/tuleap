f8f661a21fbada3ab5dc5d0549e6f55e7911784c
Sandra Echinard <sandra.echinard@enalean.com>
2012-07-04 15:56:23 +0200

    Start display remaining effort on cards

--- plugins/cardwall/include/CardPresenter.class.php
+++ plugins/cardwall/include/CardPresenter.class.php
@@ -18,16 +18,18 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 require_once TRACKER_BASE_DIR .'/Tracker/CardPresenter.class.php';
+require_once 'CardFieldPresenter.class.php';
 
 class Cardwall_CardPresenter implements Tracker_CardPresenter{
-
+    
+    const REMAINING_EFFORT_FIELD_NAME = "remaining_effort";
     /**
      * @var Tracker_Artifact
      */
     private $artifact;
 
     public function __construct(Tracker_Artifact $artifact) {
-        $this->artifact = $artifact;
+        $this->artifact           = $artifact;
     }
 
     /**
@@ -43,7 +45,26 @@ class Cardwall_CardPresenter implements Tracker_CardPresenter{
     public function getTitle() {
         return $this->artifact->getTitle();
     }
-
+    
+    public function getFields() {
+        $value                = '';
+        $user                 = UserManager::instance()->getCurrentUser();
+        $field_name           = Cardwall_CardPresenter::REMAINING_EFFORT_FIELD_NAME;
+        $form_element_factory = Tracker_FormElementFactory::instance();
+        
+        $field = $form_element_factory->getComputableFieldByNameForUser(
+                $this->artifact->getTracker()->getId(),
+                $field_name,
+                $user
+        );
+        
+        if ($field) {
+            $value = $field->getComputedValue($user, $this->artifact);
+        }  
+        
+        return array(new Cardwall_CardFieldPresenter($field, $value));
+    }
+    
     /**
      * @see Tracker_CardPresenter
      */
