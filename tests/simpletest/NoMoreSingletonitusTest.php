30129e4a66c7f50a5255607e61b973fdd5185473
Johan Martinsson <martinsson.johan@gmail.com>
2012-07-19 22:16:19 +0200

    now it can replace the count itself, and in addition the test cleans up after executing, so even if the file was currently modified nothing is changed by running the test

--- tests/simpletest/NoMoreSingletonitusTest.php
+++ tests/simpletest/NoMoreSingletonitusTest.php
@@ -25,9 +25,10 @@
 class NoMoreSingletonitusTest extends TuleapTestCase {
     
     public function testThereAreNoNewSingletonLookups() {
-        $expected_singleton_lookups = file_get_contents(dirname(__FILE__).'/current_singleton_count.txt');
+        $expected_singleton_lookups = file_get_contents(SINGLETON_COUNT_FILE);
         $basedir                    = dirname(__FILE__).'/../..';
-        $actual_singleton_lookups   = $this->countSingletonLookupsInProject($basedir);
+        $singleton_counter          = new SingletonCount();
+        $actual_singleton_lookups   = $singleton_counter->countSingletonLookupsInProject($basedir);
         $new_singletons             = $actual_singleton_lookups - $expected_singleton_lookups;
 
         $this->assertFalse($new_singletons > 0, 
@@ -38,8 +39,37 @@ class NoMoreSingletonitusTest extends TuleapTestCase {
                  please decrease the current_number_of_singleton_lookups variable in this test>
                  It should be : $actual_singleton_lookups");
     }
+}
+
+class SingletonCountTest extends TuleapTestCase {
+    public function setUp() {
+        parent::setUp();
+        $this->current_value = file_get_contents(SINGLETON_COUNT_FILE);
+    }
+    public function itCanReplaceTheCurrentAmount() {
+        $counter = new SingletonCount();
+        file_put_contents(SINGLETON_COUNT_FILE, "0");
+
+        $basedir                    = dirname(__FILE__).'/../..';
+        
+        $this->assertNotEqual(file_get_contents(SINGLETON_COUNT_FILE), $counter->countSingletonLookupsInProject($basedir));
+        $counter->replaceCurrentSingletonCountWithActualCount($basedir);
+        
+        $this->assertEqual(file_get_contents(SINGLETON_COUNT_FILE), $counter->countSingletonLookupsInProject($basedir));
+    }
     
-    private function countSingletonLookupsInProject($basedir) {
+    public function tearDown() {
+        parent::tearDown();
+        file_put_contents(SINGLETON_COUNT_FILE, $this->current_value);
+    }
+
+}
+
+define("SINGLETON_COUNT_FILE", dirname(__FILE__).'/current_singleton_count.txt');
+
+class SingletonCount {
+
+    public function countSingletonLookupsInProject($basedir) {
         $dirs                       = "$basedir/plugins $basedir/src $basedir/tools";
         $count_command              = "grep -rc --exclude='*~' '::instance()' $dirs| awk -F: '{n=n+$2} END { print n}'";
         $output                     = $this->getSystemOutput($count_command);
@@ -50,6 +80,10 @@ class NoMoreSingletonitusTest extends TuleapTestCase {
         $result;
         exec($cmd, $result);
         return $result;
+    }
+   
+    public function replaceCurrentSingletonCountWithActualCount($basedir) {
+        file_put_contents(SINGLETON_COUNT_FILE, $this->countSingletonLookupsInProject($basedir));
     }
 
 }
