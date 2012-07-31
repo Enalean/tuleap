823106ecc9ef9f22ccc2ffa3e79d7e555761e95a
Sebastien Nicouleaud <seb@sogilis.com>
2012-07-31 16:40:08 +0200

    Use DummyPresenter instead of whatever() method

--- tests/simpletest/common/templating/mustache/MustacheDebugRendererTest.php
+++ tests/simpletest/common/templating/mustache/MustacheDebugRendererTest.php
@@ -20,20 +20,23 @@
 
 require_once 'common/templating/mustache/MustacheDebugRenderer.class.php';
 
+class MustacheDebugRenderer_DummyPresenter {
+    function __toString() {
+        return '';
+    }
+}
+
 class MustacheDebugRenderer_TestCase extends TuleapTestCase {
     
     public function setUp() {
         parent::setUp();
         $this->templates_dir = dirname(__FILE__).'/templates';
         $this->renderer      = new MustacheDebugRenderer($this->templates_dir);
+        $this->presenter     = new MustacheDebugRenderer_DummyPresenter();
     }
     
     protected function failMissingException($exception_class_name = 'Exception') {
         $this->fail("Expected $exception_class_name, but nothing raised.");
-    }
-    
-    protected function whatever() {
-        return print_r(new stdClass(), true);
     }
 }
 
@@ -41,9 +44,8 @@ class MustacheDebugRenderer_ValidTemplateTest extends MustacheDebugRenderer_Test
     
     public function itBehavesLikeMustacheRenderer() {
         $mustache_renderer = new MustacheRenderer($this->templates_dir);
-        $presenter         = $this->whatever();
-        $mustache_output   = $mustache_renderer->renderToString('valid-template', $presenter);
-        $debug_output      = $this->renderer->renderToString('valid-template', $presenter);
+        $mustache_output   = $mustache_renderer->renderToString('valid-template', $this->presenter);
+        $debug_output      = $this->renderer->renderToString('valid-template', $this->presenter);
         
         $this->assertEqual($debug_output, $mustache_output);
     }
@@ -53,7 +55,7 @@ class MustacheDebugRenderer_InvalidTemplateTest extends MustacheDebugRenderer_Te
     
     public function itIncludesTheNameOfTheInvalidTemplateInTheErrorMessage() {
         try {
-            $this->renderer->renderToString('invalid-template', $this->whatever());
+            $this->renderer->renderToString('invalid-template', $this->presenter);
             $this->failMissingException();
         } catch(Exception $exception) {
             $this->assertPattern('/ invalid-template.mustache/', $exception->getMessage());
@@ -65,7 +67,7 @@ class MustacheDebug_InvalidPartialRenderingTest extends MustacheDebugRenderer_Te
     public function setUp() {
         parent::setUp();
         try {
-            $this->renderer->renderToString('valid-template-with-invalid-partial', $this->whatever());
+            $this->renderer->renderToString('valid-template-with-invalid-partial', $this->presenter);
             $this->failMissingException();
         } catch(Exception $exception) {
             $this->error_message = $exception->getMessage();
