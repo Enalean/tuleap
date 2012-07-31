ad883571b7cf874802f2ee29cd242c838fd4b65b
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-07-31 13:40:24 +0200

    whitespace cleaning

--- plugins/git/include/GitRepository.class.php
+++ plugins/git/include/GitRepository.class.php
@@ -152,15 +152,15 @@ class GitRepository implements DVCSRepository {
             return true;
         }
         $id = $this->getId();
-        if ( empty($id) ) {            
+        if ( empty($id) ) {
             $this->loaded = $this->getDao()->getProjectRepository($this);
-        } else {            
+        } else {
             $this->loaded = $this->getDao()->getProjectRepositoryById($this);
         }
         //loading failed
         return $this->loaded;
     }
-    
+
     /**
      * Save current GitRepostiroy object to the database
      */
