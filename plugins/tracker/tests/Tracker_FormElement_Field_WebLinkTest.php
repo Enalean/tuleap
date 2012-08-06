c88e915bd82198f96fb4d37e9452b87877136167
jtekt <yoann.celton@jtekt-eu.com>
2012-08-06 09:46:42 +0200

    Modified the test to include alphanum succefully

--- plugins/tracker/tests/Tracker_FormElement_Field_WebLinkTest.php
+++ plugins/tracker/tests/Tracker_FormElement_Field_WebLinkTest.php
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
-
+require_once dirname(__FILE__).'/../include/Tracker/TrackerManager.class.php';
 require_once(dirname(__FILE__).'/../include/Tracker/FormElement/Tracker_FormElement_Field_WebLink.class.php');
 Mock::generatePartial('Tracker_FormElement_Field_WebLink', 'Tracker_FormElement_Field_WebLinkTestVersion', array('getRuleString', 'getRuleNoCr', 'getProperty'));
 
