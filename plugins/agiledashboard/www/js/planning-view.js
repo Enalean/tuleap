71424ca36be108c40cbd6371e6d1870d21c25ebd
Nicolas Terray <nicolas.terray@enalean.com>
2012-08-07 09:38:54 +0200

    Fix issue when card is displayed both on left and right panes of the planning

--- plugins/agiledashboard/www/js/planning-view.js
+++ plugins/agiledashboard/www/js/planning-view.js
@@ -1,10 +1,20 @@
 
 document.observe('dom:loaded', function () {
+
+    $$('.milestone-content .dropdown').each(function (dropdown) {
+        dropdown.id += '-backlog';
+        var a = dropdown.down('a.dropdown-toggle');
+        if (a) {
+            a.writeAttribute('data-target', a.readAttribute('data-target') + '-backlog');
+        }
+    });
+
     $$('.planning-artifact-chooser').each(function (select) {
         select.observe('change', function(evt) {
             select.form.submit();
         });
     });
+
     Ajax.Responders.register({
         onCreate: Planning.toggleFeedback,
         onComplete: Planning.toggleFeedback
