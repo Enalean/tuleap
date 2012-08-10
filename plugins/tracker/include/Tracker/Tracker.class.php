697f4aea4b2872ea4fe7d3ee6710a818df03d000
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-08-10 09:42:25 +0200

    Transfer all code related to artifact creation in a dedicated action class

--- plugins/tracker/include/Tracker/Tracker.class.php
+++ plugins/tracker/include/Tracker/Tracker.class.php
@@ -35,6 +35,7 @@ require_once('FormElement/Tracker_SharedFormElementFactory.class.php');
 require_once('Hierarchy/Controller.class.php');
 require_once('Hierarchy/HierarchyFactory.class.php');
 require_once 'IFetchTrackerSwitcher.class.php';
+require_once 'Action/CreateArtifact.class.php';
 
 require_once('json.php');
 
@@ -547,33 +548,13 @@ class Tracker implements Tracker_Dispatchable_Interface {
                 }
                 break;
             case 'submit-artifact':
-                if ($this->userCanSubmitArtifact($current_user)) {
-                    $link = (int)$request->get('link-artifact-id');
-                    if ($artifact = $this->createArtifact($layout, $request, $current_user)) {
-                        $this->associateImmediatelyIfNeeded($artifact, $link, $request->get('immediate'), $current_user);
-                        
-                        $this->redirectToParentCreationIfNeeded($artifact, $current_user);
-
-                        $artifact->summonArtifactRedirectors($request);
-                        
-                        if ($request->isAjax()) {
-                            header(json_header(array('aid' => $artifact->getId())));
-                            exit;
-                        } else if ($link) {
-                            echo '<script>window.parent.codendi.tracker.artifact.artifactLink.newArtifact('. (int)$artifact->getId() .');</script>';
-                            exit;
-                        } else {
-                            $GLOBALS['Response']->addFeedback('info', $GLOBALS['Language']->getText('plugin_tracker_index', 'create_success', array($artifact->fetchXRefLink())), CODENDI_PURIFIER_LIGHT);
-                            
-                            $url_redirection = $this->redirectUrlAfterArtifactSubmission($request, $this->getId(), $artifact->getId());
-                            $GLOBALS['Response']->redirect($url_redirection);
-                        }
-                    }
-                    $this->displaySubmit($layout, $request, $current_user, $link);
-                } else {
-                    $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_tracker_admin', 'access_denied'));
-                    $GLOBALS['Response']->redirect(TRACKER_BASE_URL.'/?tracker='. $this->getId());
-                }
+                $action = new Tracker_Action_CreateArtifact(
+                    $this,
+                    $this->getTrackerArtifactFactory(),
+                    $this->getTrackerFactory(),
+                    $this->getFormElementFactory()
+                );
+                $action->process($layout, $request, $current_user);
                 break;
             case 'admin-hierarchy':
                 if ($this->userIsAdmin($current_user)) {
@@ -617,36 +598,6 @@ class Tracker implements Tracker_Dispatchable_Interface {
                 break;
         }
         return false;
-    }
-    
-    private function associateImmediatelyIfNeeded(Tracker_Artifact $new_artifact, $link_artifact_id, $doitnow, User $current_user) {
-        if ($link_artifact_id && $doitnow) {
-            $source_artifact = Tracker_ArtifactFactory::instance()->getArtifactById($link_artifact_id);
-            if ($source_artifact) {
-                $source_artifact->linkArtifact($new_artifact->getId(), $current_user);
-            }
-        }
-    }
-
-    protected function redirectToParentCreationIfNeeded(Tracker_Artifact $artifact, User $current_user) {
-        $parent_tracker_id = $this->getHierarchy()->getParent($this->getId());
-        if ($parent_tracker_id) {
-            $parent_tracker = $this->getTrackerFactory()->getTrackerById($parent_tracker_id);
-            if ($parent_tracker) {
-                if (count($artifact->getAllAncestors($current_user)) == 0) {
-                    $art_link = $this->getFormElementFactory()->getAnArtifactLinkField($current_user, $parent_tracker);
-                    if ($art_link) {
-                        $art_link_key = 'artifact['.$art_link->getId().'][new_values]';
-                        $redirect_params = array(
-                            'tracker'     => $parent_tracker_id, 
-                            'func'        => 'new-artifact',
-                            $art_link_key => $artifact->getId()
-                        );
-                        $GLOBALS['Response']->redirect(TRACKER_BASE_URL.'/?'.  http_build_query($redirect_params));
-                    }
-                }
-            }
-        }
     }
 
     private function getHierarchyController($request) {
@@ -1912,26 +1863,7 @@ EOS;
         }
     }
 
-    /**
-     * Add an artefact in the tracker
-     *
-     * @param Tracker_IDisplayTrackerLayout  $layout
-     * @param Codendi_Request                $request
-     * @param User                           $user
-     *
-     * @return Tracker_Artifact the new artifact
-     */
-    public function createArtifact(Tracker_IDisplayTrackerLayout $layout, $request, $user) {
-        $email = null;
-        if ($user->isAnonymous()) {
-            $email = $request->get('email');
-        }
 
-        $fields_data = $request->get('artifact');
-        $this->augmentDataFromRequest($fields_data);
-
-        return Tracker_ArtifactFactory::instance()->createArtifact($this, $fields_data, $user, $email);
-    }
 
     /**
      * Validate the format of the item name
@@ -3087,33 +3019,6 @@ EOS;
     
     public function setSharedFormElementFactory(Tracker_SharedFormElementFactory $factory) {
         $this->sharedFormElementFactory = $factory;
-    }
-
-    public function redirectUrlAfterArtifactSubmission($request, $tracker_id, $artifact_id) {
-        $stay      = $request->get('submit_and_stay');
-        $continue  = $request->get('submit_and_continue');
-        
-        $redirect_params = $this->calculateRedirectParams($tracker_id, $artifact_id, $stay, $continue);
-        EventManager::instance()->processEvent(
-            TRACKER_EVENT_BUILD_ARTIFACT_FORM_ACTION,
-            array(
-                'request'          => $request,
-                'query_parameters' => &$redirect_params,
-            )
-        );
-        return TRACKER_BASE_URL.'/?'.  http_build_query($redirect_params);
-    }
-
-    private function calculateRedirectParams($tracker_id, $artifact_id, $stay, $continue) {
-        $redirect_params = array();
-        $redirect_params['tracker']       = $tracker_id;
-        if ($continue) {
-            $redirect_params['func']      = 'new-artifact';
-        }
-        if ($stay) {
-            $redirect_params['aid']       = $artifact_id;
-        }
-        return array_filter($redirect_params);
     }
 
     /**
