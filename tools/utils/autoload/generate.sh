19e928502a5b83776acee07e730f6afa5e97a283
Nicolas Terray <nicolas.terray@enalean.com>
2012-08-14 11:41:01 +0200

    Prune only based on whole filename, not only the suffix

--- tools/utils/autoload/generate.sh
+++ tools/utils/autoload/generate.sh
@@ -38,7 +38,7 @@ phpab --compat -o autoload.php .
 # check that there isn't any require_once used based on the result of phpab
 inclusions=`phpab -s -t ../../../tools/utils/autoload/filelist.tpl . | cut -d"'" -f2 | sed 's#^.*/##' | tr "\\n" '|' | sed 's/|$//'`
 cd $basedir
-lines_to_delete=`grep --exclude=autoload.php -rnE "require.*($inclusions)" $directory | cut -d: -f1,2`
+lines_to_delete=`grep --exclude=autoload.php -rnE "require.*('|\"|/)($inclusions)" $directory | cut -d: -f1,2`
 previous_file=''
 for line in $lines_to_delete; do
     file=`echo $line | cut -d: -f1`
