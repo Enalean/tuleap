1e0635a0129b60fc814115ab8d41aa4fc17745b0
Nicolas Terray <nicolas.terray@enalean.com>
2012-08-14 11:33:14 +0200

    Prune require automatically

--- tools/utils/autoload/generate.sh
+++ tools/utils/autoload/generate.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Copyright (c) Enalean, 2012. All Rights Reserved.
@@ -27,6 +27,29 @@
 # $ tools/utils/autoload/generate.sh plugins/agiledashboard
 
 
+basedir=`pwd`
 directory=$1
 
-cd $directory && phpab --compat -o autoload.php .
+cd $directory
+
+# generate the autoload file
+phpab --compat -o autoload.php .
+
+# check that there isn't any require_once used based on the result of phpab
+inclusions=`phpab -s -t ../../../tools/utils/autoload/filelist.tpl . | cut -d"'" -f2 | sed 's#^.*/##' | tr "\\n" '|' | sed 's/|$//'`
+cd $basedir
+lines_to_delete=`grep --exclude=autoload.php -rnE "require.*($inclusions)" $directory | cut -d: -f1,2`
+previous_file=''
+for line in $lines_to_delete; do
+    file=`echo $line | cut -d: -f1`
+    line_number=`echo $line | cut -d: -f2`
+    if [ "$previous_file" = "" -o "$previous_file" != "$file" ]; then
+        if [ "$previous_file" != "" -a "$delete_command" != "" ]; then
+            sed -i "$delete_command" $previous_file
+        fi
+        previous_file=$file
+        delete_command=''
+    fi
+    delete_command="$delete_command $line_number d;"
+done
+
