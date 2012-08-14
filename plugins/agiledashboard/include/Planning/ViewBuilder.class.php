a3cf3db240a7fba01da9c1059c270e90f1828ce0
Nicolas Terray <nicolas.terray@enalean.com>
2012-08-14 11:56:13 +0200

    Add items in autoload

--- plugins/agiledashboard/include/Planning/ViewBuilder.class.php
+++ plugins/agiledashboard/include/Planning/ViewBuilder.class.php
@@ -20,7 +20,6 @@
  */
 
 require_once dirname(__FILE__).'/../../../tracker/include/Tracker/CrossSearch/ViewBuilder.class.php';
-require_once 'GroupByParentsVisitor.class.php';
 
 /**
  * This class builds the Planning_SearchContentView that is used to display the right column of the Planning
