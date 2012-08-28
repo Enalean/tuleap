edbaf08f074b9eb37581cb6bfcd4db89c7e7bd5a
Hatem Ounis <hatem.ounis@st.com>
2012-08-28 08:53:43 -0400

    Put back original disclaimer.

--- src/www/file/filemodule_monitor.php
+++ src/www/file/filemodule_monitor.php
@@ -1,20 +1,10 @@
 <?php
-/**
- * Copyright (c) STMicroelectronics 2012. All rights reserved
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
+//
+// SourceForge: Breaking Down the Barriers to Open Source Development
+// Copyright 1999-2000 (c) The SourceForge Crew
+// http://sourceforge.net
+//
+// 
 
 require_once('pre.php');
 require_once('common/frs/FRSPackageFactory.class.php');
