e77a61afd7419a008fd6880530a0dcfe809f5eb9
Hatem Ounis <hatem.ounis@st.com>
2012-09-18 14:17:24 +0000

    Check if edit action is allowed.

--- src/www/project/admin/ugroup_add_users.php
+++ src/www/project/admin/ugroup_add_users.php
@@ -61,7 +61,8 @@ function display_user_result_table($res) {
 
 $ugroup_id = $request->getValidated('ugroup_id', 'uint', 0);
 if ($ugroup_id) {
-    $ugroupUpdateUsersAllowed = true;
+    $ugroupBinding = new UGroupBinding();
+    $ugroupUpdateUsersAllowed = !$ugroupBinding->isBinded($ugroup_id);
     $em->processEvent(Event::UGROUP_UPDATE_USERS_ALLOWED, array('ugroup_id' => $ugroup_id, 'allowed' => &$ugroupUpdateUsersAllowed));
     if ($ugroupUpdateUsersAllowed) {
         $res = ugroup_db_get_ugroup($ugroup_id);
