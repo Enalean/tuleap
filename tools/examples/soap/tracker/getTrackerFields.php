94604167907233fe2808f59c3141a718e0fc1df9
Martin GOYOT <martin.goyot@enalean.com>
2012-09-20 17:35:33 +0200

    adding example for getTrackerFields soap operation

---
+++ tools/examples/soap/tracker/getTrackerFields.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+$serverURL = 'http://recco.cro.enalean.com';
+$soapLogin = new SoapClient($serverURL.'/soap/?wsdl', array('cache_wsdl' => WSDL_CACHE_NONE));
+
+// Establish connection to the server
+$adminSessionHash = $soapLogin->login('admin','siteadmin')->session_hash;
+$requesterSessionHash = $soapLogin->loginAs($adminSessionHash, 'goyotm');
+
+// Group id
+$groupID = 107;
+
+// Tracker id
+$trackerID = 10;
+
+// Connecting to the soap's tracker client
+$soapTracker = new SoapClient($serverURL.'/plugins/tracker/soap/?wsdl', array('cache_wsdl' => WSDL_CACHE_NONE));
+$response = $soapTracker->getTrackerFields($requesterSessionHash, $groupID, $trackerID);
+
+// Printing answer
+var_dump($response);
+
+// Disconnecting from the server
+$soapLogin->logout($adminSessionHash);
+$soapLogin->logout($requesterSessionHash);
+
+
+?>
