5cb7cb8ddca068fa9516d3df668b652327d1edd0
Nicolas Terray <nicolas.terray@enalean.com>
2012-09-24 12:32:58 +0200

    merge both titles

--- plugins/git/include/GitViews.class.php
+++ plugins/git/include/GitViews.class.php
@@ -219,8 +219,7 @@ class GitViews extends PluginViews {
 
         echo "<br/>";
         $this->_getBreadCrumb();
-        echo '<h1>'. $this->_getRepositoryPageUrl($repoId, $repoName) .'</h1>';
-        echo '<h2>'. $this->getText('admin_repo_management') .'</h2>';
+        echo '<h2>'. $this->_getRepositoryPageUrl($repoId, $repoName) .' - '. $this->getText('admin_repo_management') .'</h2>';
 
         $repo_management_view = new GitViews_RepoManagement($repository, $this->controller->getRequest());
         $repo_management_view->display();
