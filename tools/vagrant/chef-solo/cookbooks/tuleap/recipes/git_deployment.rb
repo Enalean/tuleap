4a2a6ce97da51d08c8acf37e40e87f5141726b45
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-10-09 15:06:47 +0200

    Do not uninstall tuleap for development

--- tools/vagrant/chef-solo/cookbooks/tuleap/recipes/git_deployment.rb
+++ tools/vagrant/chef-solo/cookbooks/tuleap/recipes/git_deployment.rb
@@ -11,7 +11,21 @@
 include_recipe 'tuleap::rpm_deployment'
 
 ## Then remove the Tuleap RPM package, but keep the configuration files
-package('tuleap') { action :remove }
+
+# Disable tuleap repositories to avoid accidental yum update that trash
+# tuleap development tree
+disable_yum_repository 'tuleap-dev'
+disable_yum_repository 'tuleap-local'
+disable_yum_repository 'tuleap-stable'
+
+script "Move away tuleap rpm install" do
+  user        'root'
+  interpreter 'bash'
+  environment 'HOME' => '/root'
+  code        <<-SHELL
+                mv /usr/share/codendi /usr/share/codendi_rpm
+              SHELL
+end
 
 ## Symlink the local git repository
 link node['tuleap']['install_dir'] do
