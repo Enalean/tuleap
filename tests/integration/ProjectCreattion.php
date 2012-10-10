925db22d707f984c4ac288da6b6f09a1b12bdcd9
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-10-10 16:38:36 +0200

    Make it work

--- tests/integration/ProjectCreattion.php
+++ tests/integration/ProjectCreattion.php
@@ -19,22 +19,55 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 require_once 'common/project/ProjectCreator.class.php';
+require_once 'common/dao/CodendiDataAccess.class.php';
+require_once 'database.php';
+require_once 'exit.php';
+require_once 'html.php';
+require_once 'user.php';
 
 class ProjectCreationTest extends TuleapTestCase {
-    public function itCreatesAProject() {
-        mysql_connect("localhost", "codendiadm", "pAdK-1?2");
+
+    // GRANT ALL PRIVILEGES on integration_test.* to 'integration_test'@'localhost' identified by 'welcome0';
+    public function setUp() {
+        parent::setUp();
+
+        $GLOBALS['sys_dbhost']   = 'localhost';
+        $GLOBALS['sys_dbuser']   = 'integration_test';
+        $GLOBALS['sys_dbpasswd'] = 'welcome0';
+        $GLOBALS['sys_dbname']   = 'integration_test';
+        
+        mysql_connect($GLOBALS['sys_dbhost'], $GLOBALS['sys_dbuser'], $GLOBALS['sys_dbpasswd']);
         mysql_query("DROP DATABASE IF EXISTS integration_test;");
         mysql_query("CREATE DATABASE integration_test;");
-        `mysql -ucodendiadm -ppAdK-1?2 integration_test < src/db/mysql/database_structure.sql`;
+
+        $mysql_cmd = 'mysql -u'.$GLOBALS['sys_dbuser'].' -p'.$GLOBALS['sys_dbpasswd'].' '.$GLOBALS['sys_dbname'];
         
-        $GLOBALS['sys_dbhost'] = 'localhost';
-        $GLOBALS['sys_dbuser'] = 'codendiadm';
-        $GLOBALS['sys_dbpasswd'] = 'pAdK-1?2';
-        $GLOBALS['sys_dbname'] = 'integration_test';
+        $cmd = $mysql_cmd.' < src/db/mysql/database_structure.sql';
+        system($cmd);
+        $cmd = $mysql_cmd.' < src/db/mysql/database_initvalues.sql';
+        system($cmd);
         
+        $GLOBALS['feedback'] = '';
+        db_connect();
+    }
+
+    public function itCreatesAProject() {
         $projectCreator = new ProjectCreator(ProjectManager::instance(), new Rule_ProjectName(), new Rule_ProjectFullName());
-        $projectCreator->create('toto', 'tata', array());
+        $projectCreator->create('short-name', 'Long name', array(
+            'project' => array(
+                'form_license'           => 'xrx',
+                'form_license_other'     => '',
+                'form_short_description' => '',
+                'is_test'                => false,
+                'is_public'              => false,
+                'services'               => array(),
+                'built_from_template'    => 100,
+            )
+        ));
         
+        $res = db_query('SELECT * FROM groups WHERE unix_group_name="short-name"');
+        $row = db_fetch_array($res);
+        $this->assertEqual($row['group_name'], 'Long name');
     }
 }
 ?>
