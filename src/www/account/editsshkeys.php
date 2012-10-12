ceb17c23d9266c5e6706d188342c9ffcd6953658
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-10-12 17:25:16 +0200

    Transfer sshkey validation logic in a dedicated class

--- src/www/account/editsshkeys.php
+++ src/www/account/editsshkeys.php
@@ -6,12 +6,13 @@
 //
 // 
 
-require_once('pre.php');    
-require_once('account.php');
+require_once 'pre.php';
+require_once 'account.php';
+require_once 'common/user/User_SSHKeyValidator.class.php';
 session_require(array('isloggedin'=>1));
 
-$um   = UserManager::instance();
-$user = $um->getCurrentUser();
+$user_manager = UserManager::instance();
+$user         = $user_manager->getCurrentUser();
 
 $request = HTTPRequest::instance();
 
@@ -19,33 +20,8 @@ if ($request->isPost()
     && $request->exist('Submit')
     && $request->exist('form_authorized_keys')) {
 
-    $form_authorized_keys = trim($request->get('form_authorized_keys'));
-    $form_authorized_keys = ereg_replace("(\r\n)|(\n)","###", $form_authorized_keys);
-
-    $valid_keys = array();
-    $keys       = array_filter(explode('###', $form_authorized_keys));
-    $key_file   = tempnam('/var/tmp/codendi_cache/', 'ssh_key_');
-    foreach ($keys as $key) {
-        $written = file_put_contents($key_file, $key);
-        if ($written === strlen($key)) {
-            $return = 1;
-            $output = array();
-            exec("ssh-keygen -l -f $key_file 2>&1", $output, $return);
-            if ($return === 0) {
-                $valid_keys[] = $key;
-            } else {
-                $GLOBALS['Response']->addFeedback('warning', "Skip invalid key $key");
-            }
-        }
-    }
-    unlink($key_file);
-    $form_authorized_keys = implode('###', $valid_keys);
-
-    $user->setAuthorizedKeys($form_authorized_keys);
-    $um->updateDb($user);
-
-    $em = EventManager::instance();
-    $em->processEvent(Event::EDIT_SSH_KEYS, array('user_id' => $user->getId()));
+    $ssh_validator = new User_SSHKeyValidator($user_manager, EventManager::instance());
+    $ssh_validator->updateUserKeys($user, $request->get('form_authorized_keys'));
 
     $GLOBALS['Response']->redirect('/account');
 }
