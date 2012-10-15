1d2858b13bde6c560f0af9b7e05fe221b540f2a3
Ahmed HOSNI <ahmed.hosni@st.com>
2012-10-15 18:19:46 +0100

    Clean up

--- src/www/project/admin/ugroup_add_users.php
+++ src/www/project/admin/ugroup_add_users.php
@@ -62,7 +62,8 @@ function display_user_result_table($res) {
 $ugroup_id = $request->getValidated('ugroup_id', 'uint', 0);
 if ($ugroup_id) {
     $uGroupMgr = new UGroupManager();
-    $ugroupUpdateUsersAllowed = !$uGroupMgr->isBinded($ugroup_id);
+    $uGroup    = new UGroup();
+    $ugroupUpdateUsersAllowed = !$uGroup->isBound($ugroup_id);
     $em->processEvent(Event::UGROUP_UPDATE_USERS_ALLOWED, array('ugroup_id' => $ugroup_id, 'allowed' => &$ugroupUpdateUsersAllowed));
     if ($ugroupUpdateUsersAllowed) {
         $res = ugroup_db_get_ugroup($ugroup_id);
