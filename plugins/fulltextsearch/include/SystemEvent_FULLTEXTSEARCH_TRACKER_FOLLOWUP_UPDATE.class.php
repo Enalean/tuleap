5b946d4db8846465cd40ab07c630522bfc36a109
Hatem Ounis <hatem.ounis@st.com>
2012-10-16 09:48:09 +0000

    Factorize.

--- plugins/fulltextsearch/include/SystemEvent_FULLTEXTSEARCH_TRACKER_FOLLOWUP_UPDATE.class.php
+++ plugins/fulltextsearch/include/SystemEvent_FULLTEXTSEARCH_TRACKER_FOLLOWUP_UPDATE.class.php
@@ -18,40 +18,9 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once 'common/system_event/SystemEvent.class.php';
+class SystemEvent_FULLTEXTSEARCH_TRACKER_FOLLOWUP_UPDATE extends SystemEvent_FULLTEXTSEARCH_TRACKER_FOLLOWUP {
 
-class SystemEvent_FULLTEXTSEARCH_TRACKER_FOLLOWUP_UPDATE extends SystemEvent {
 
-    /**
-     * Process the system event
-     *
-     * @return Boolean
-     */
-    public function process() {
-        $groupId     = (int)$this->getRequiredParameter(0);
-        $artifactId  = (int)$this->getRequiredParameter(1);
-        $changesetId = (int)$this->getRequiredParameter(2);
-        $text        = $this->getRequiredParameter(3);
-        
-        $this->done();
-        return true;
-    }
-
-    /**
-     * Verbalize parameters
-     *
-     * @param Boolean $withLink
-     *
-     * @return String
-     */
-    public function verbalizeParameters($withLink) {
-        $groupId     = (int)$this->getRequiredParameter(0);
-        $artifactId  = (int)$this->getRequiredParameter(1);
-        $changesetId = (int)$this->getRequiredParameter(2);
-        $text        = $this->getRequiredParameter(3);
-        // @TODO: verbalize artifact id & changeset id would look like /plugins/tracker/?aid=$artifactId#followup_$changesetId
-        return 'Project: '.$this->verbalizeProjectId($groupId, $withLink).', Artifact: '.$artifactId.', Changeset: '.$changesetId.', Text: '.$text;
-    }
 
 }
 
