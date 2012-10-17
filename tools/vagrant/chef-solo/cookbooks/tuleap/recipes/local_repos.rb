f8af7651918c99de41626ccec16d85933ad1bee0
Nicolas Terray <nicolas.terray@enalean.com>
2012-10-17 15:18:28 +0200

    Fix license block

--- tools/vagrant/chef-solo/cookbooks/tuleap/recipes/local_repos.rb
+++ tools/vagrant/chef-solo/cookbooks/tuleap/recipes/local_repos.rb
@@ -2,9 +2,22 @@
 # Cookbook Name:: tuleap
 # Recipe:: local_repos
 #
-# Copyright 2012, Enalean
+# Copyright (c) Enalean, 2012. All Rights Reserved.
 #
-# All rights reserved - Do Not Redistribute
+# This file is a part of Tuleap.
+#
+# Tuleap is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Tuleap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
 #
 
 ## Create all the local Tuleap repos
