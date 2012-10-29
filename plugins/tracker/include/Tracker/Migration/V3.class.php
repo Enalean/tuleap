31522bfc5a884997e2b20e024138b231784cdb9c
Nicolas Terray <nicolas.terray@enalean.com>
2012-10-29 12:25:49 +0100

    Add columns

--- plugins/tracker/include/Tracker/Migration/V3.class.php
+++ plugins/tracker/include/Tracker/Migration/V3.class.php
@@ -30,6 +30,7 @@ require_once 'V3/ReferenceFieldDao.class.php';
 require_once 'V3/SemanticDao.class.php';
 require_once 'V3/CannedDao.class.php';
 require_once 'V3/CcFieldDao.class.php';
+require_once 'V3/ColumnsDao.class.php';
 
 /**
  * This migrate trackers v3 into tracker v5
@@ -96,6 +97,10 @@ class Tracker_Migration_V3 {
 
             // 260
             $fieldset_dao->nowFieldsetsAreStoredAsField($id);
+
+            // 270 & 280
+            $columns_dao = new Tracker_Migration_V3_ColumnsDao();
+            $columns_dao->create($id);
 
             return $this->tracker_factory->getTrackerById($id);
         }
