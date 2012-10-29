210eb75bc89f0ad6faaacdc0831aad9736df04d1
Manuel Vacelet <manuel.vacelet@enalean.com>
2012-10-29 12:51:16 +0100

    source cleanup

--- plugins/tracker/include/Tracker/Migration/V3/CcFieldDao.class.php
+++ plugins/tracker/include/Tracker/Migration/V3/CcFieldDao.class.php
@@ -19,6 +19,7 @@
  */
 
 class Tracker_Migration_V3_CcFieldDao extends DataAccessObject {
+
     public function addCCField($tv5_id) {
         $tv5_id = $this->da->escapeInt($tv5_id);
         $sql = "INSERT INTO tracker_field(tracker_id, parent_id, formElement_type, name, label, description, use_it, rank, scope, required, notifications)
