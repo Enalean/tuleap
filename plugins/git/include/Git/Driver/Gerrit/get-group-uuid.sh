f7de62cf64d3df273393bcab122d3287fa8b7ea2
Johan Martinsson <martinsson.johan@gmail.com>
2012-11-12 16:06:51 +0100

    stripping non uuid lines

--- plugins/git/include/Git/Driver/Gerrit/get-group-uuid.sh
+++ plugins/git/include/Git/Driver/Gerrit/get-group-uuid.sh
@@ -3,4 +3,5 @@ gerritport=$2
 gerritusername=$3
 groupname=$4
 
-ssh $gerritusername@$gerritserver -p $gerritport gerrit gsql  -c "SELECT\ group_uuid\ FROM\ account_groups\ WHERE\ name=\'$groupname\'"
+#replace by --format JSON, to remove sed tail and head
+ssh $gerritusername@$gerritserver -p $gerritport gerrit gsql -c "SELECT\ group_uuid\ FROM\ account_groups\ WHERE\ name=\'$groupname\'" | tail -2 | head -1 | sed 's/ //'
