c061264ab235044ed180852ae76e5bb7aa6ac118
Hatem Ounis <hatem.ounis@st.com>
2012-11-30 09:31:37 +0000

    Restrict the use to lab feature users.

--- plugins/fulltextsearch/include/fulltextsearchPlugin.class.php
+++ plugins/fulltextsearch/include/fulltextsearchPlugin.class.php
@@ -205,20 +205,22 @@ class fulltextsearchPlugin extends Plugin {
      * @return Void
      */
     public function tracker_report_followup_search($params) {
-        $filter = '';
-        if ($params['request'] && $params['request']->get('func') == 'followup_search') {
-            $hp     = Codendi_HTMLPurifier::instance();
-            $filter = $hp->purify($params['request']->getValidated('search_followups', 'string', ''));
+        if ($this->getCurrentUser()->useLabFeatures()) {
+            $filter = '';
+            if ($params['request'] && $params['request']->get('func') == 'followup_search') {
+                $hp     = Codendi_HTMLPurifier::instance();
+                $filter = $hp->purify($params['request']->getValidated('search_followups', 'string', ''));
+            }
+            $params['html'] .= '<div class="tracker_report_query_followup_search">';
+            $params['html'] .= '<h3 class="' . Toggler::getClassname('followups_search') . '" id="followups_search" >'.$GLOBALS['Language']->getText('plugin_fulltextsearch', 'followups_search').'</h3>';
+            $params['html'] .= '<form>';
+            $params['html'] .= '<input type="hidden" value="'.$params['report_id'].'" name="report">';
+            $params['html'] .= '<input type="hidden" value="followup_search" name="func">';
+            $params['html'] .= $GLOBALS['Language']->getText('plugin_fulltextsearch', 'search_followup_comments').': <input name="search_followups" value="'.$filter.'" />';
+            $params['html'] .= '<div align="center" style=""><input type="submit" value="'.$GLOBALS['Language']->getText('global', 'btn_submit').'" ></div>';
+            $params['html'] .= '</form>';
+            $params['html'] .= '</div>';
         }
-        $params['html'] .= '<div class="tracker_report_query_followup_search">';
-        $params['html'] .= '<h3 class="' . Toggler::getClassname('followups_search') . '" id="followups_search" >'.$GLOBALS['Language']->getText('plugin_fulltextsearch', 'followups_search').'</h3>';
-        $params['html'] .= '<form>';
-        $params['html'] .= '<input type="hidden" value="'.$params['report_id'].'" name="report">';
-        $params['html'] .= '<input type="hidden" value="followup_search" name="func">';
-        $params['html'] .= $GLOBALS['Language']->getText('plugin_fulltextsearch', 'search_followup_comments').': <input name="search_followups" value="'.$filter.'" />';
-        $params['html'] .= '<div align="center" style=""><input type="submit" value="'.$GLOBALS['Language']->getText('global', 'btn_submit').'" ></div>';
-        $params['html'] .= '</form>';
-        $params['html'] .= '</div>';
     }
 
     /**
@@ -229,11 +231,13 @@ class fulltextsearchPlugin extends Plugin {
      * @return Void
      */
     public function tracker_report_followup_search_process($params) {
-        $filter = $params['request']->get('search_followups');
-        if ($params['request']->get('func') == 'followup_search' && !empty($filter)) {
-            $controller       = $this->getSearchController('tracker');
-            $params['result'] = $controller->searchFollowups($params['request']);
-            $params['search_performed'] = true;
+        if ($this->getCurrentUser()->useLabFeatures()) {
+            $filter = $params['request']->get('search_followups');
+            if ($params['request']->get('func') == 'followup_search' && !empty($filter)) {
+                $controller       = $this->getSearchController('tracker');
+                $params['result'] = $controller->searchFollowups($params['request']);
+                $params['search_performed'] = true;
+            }
         }
     }
 
