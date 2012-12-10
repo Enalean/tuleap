37de5af1f55a86afafae9b7c5cb840d813a8483f
Nicolas Terray <nicolas.terray@enalean.com>
2012-12-10 12:59:45 +0100

    Fix fatal error

--- plugins/git/include/gitPlugin.class.php
+++ plugins/git/include/gitPlugin.class.php
@@ -605,6 +605,7 @@ class GitPlugin extends Plugin {
     }
 
     private function getProjectCreator() {
+        require_once GIT_BASE_DIR. '/Git/Driver/Gerrit/UserFinder.class.php';
         $user_finder = new Git_Driver_Gerrit_UserFinder(PermissionsManager::instance(), new UGroupManager());
         //$dir, Git_Driver_Gerrit $driver, Git_RemoteServer_GerritServer $server, Git_Driver_Gerrit_UserFinder $user_finder
         $tmp_dir = Config::get('tmp_dir') .'/'. uniqid();
