697910e413d51938787bc8bc0a7d7f8d5ce3d4e1
Dylan Bowden <dylan.bowden@enalean.com>
2012-12-31 14:47:45 +0100

    adding cron-stop file contents and fixing bug in git remote add command

--- plugins/git/include/Git/Driver/Gerrit/ProjectCreator.class.php
+++ plugins/git/include/Git/Driver/Gerrit/ProjectCreator.class.php
@@ -205,7 +205,7 @@ class Git_Driver_Gerrit_ProjectCreator {
 
         // e.g git remote add gerrit "ext::ssh -p 29418 -i $HOME/.ssh/id_rsa-gerrit admin-shunt.cro.enalean.com@gerrit-shunt.cro.enalean.com %S shunt.cro.enalean.com-gerrit-devs/funky"
         // e.g git remote add gerrit "ext::ssh -p 29418 -i /home/codendiadm/.ssh/id_rsa-gerrit  admin-chamoix.cro.enalean.com@chamoix %S chamoix.cro.enalean.com-cod/depot2"
-        `cd $repository_path && git remote add $remote "ext:ssh -p $port -i $identity_file $host_login %S $gerrit_repo"`;  
+        `cd $repository_path && git remote add $remote "ext::ssh -p $port -i $identity_file $host_login %S $gerrit_repo"`;  
     }
 }
 
