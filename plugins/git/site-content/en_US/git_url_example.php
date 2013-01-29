749b45b42b3354262c67c7106e3ca1969c54b1c1
Martin GOYOT <martin.goyot@enalean.com>
2013-01-29 12:37:31 +0100

    fixing testNoBlankBeforeAndAfterClosingPhpTag

--- plugins/git/site-content/en_US/git_url_example.php
+++ plugins/git/site-content/en_US/git_url_example.php
@@ -1,3 +1,21 @@
+<?php
+/**
+ * Copyright (c) Enalean 2013. All rights reserved
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
     <h3>How to use a Git repository:</h3>
     <dl>
         <dt>Clone the repository in order to get your working copy:</dt>
@@ -16,4 +34,3 @@ git checkout -b my-local-tracking-branch <?= $name ?>/master
             </pre>
         </dd>
     </dl>
-
--- plugins/git/site-content/fr_FR/git_url_example.php
+++ plugins/git/site-content/fr_FR/git_url_example.php
@@ -1,3 +1,21 @@
+<?php
+/**
+ * Copyright (c) Enalean 2013. All rights reserved
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
     <h3>Comment utiliser un dépôt Git:</h3>
     <dl>
         <dt>Clonez le dépôt pour obtenir votre copie de travail locale:</dt>
