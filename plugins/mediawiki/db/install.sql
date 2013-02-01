9a4e06cf6c4e5aaf4e21da71baf7cb535b1a6727
Sandra Echinard <sandra.echinard@enalean.com>
2013-02-01 13:54:50 +0100

    Fix mediawiki url rewriting bug

--- plugins/mediawiki/db/install.sql
+++ plugins/mediawiki/db/install.sql
@@ -1,5 +1,5 @@
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank) 
-       VALUES      ( 100, 'plugin_mediawiki:service_lbl_key', 'plugin_mediawiki:service_desc_key', 'plugin_mediawiki', '/plugins/mediawiki/wiki/$unix_group_name', 1, 1, 'system', 160);
+       VALUES      ( 100, 'plugin_mediawiki:service_lbl_key', 'plugin_mediawiki:service_desc_key', 'plugin_mediawiki', '/plugins/mediawiki/wiki/$projectname', 1, 1, 'system', 160);
 
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank)
 SELECT DISTINCT service.group_id , 'plugin_mediawiki:service_lbl_key' , 'plugin_mediawiki:service_desc_key' , 'plugin_mediawiki', CONCAT('/plugins/mediawiki/wiki/', groups.unix_group_name), 1 , 0 , 'system',  160
