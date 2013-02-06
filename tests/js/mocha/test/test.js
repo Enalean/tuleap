b1866bd62b149acce0f7e8a0537fcbbe691891cf
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-02-06 17:18:03 +0100

    Fix tests

--- tests/js/mocha/test/test.js
+++ tests/js/mocha/test/test.js
@@ -4,7 +4,7 @@ var assert = require("assert")
 describe('Array', function(){
     describe('#indexOf()', function(){
         it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(2));
+            assert.equal(-1, [1,2,3].indexOf(5));
             assert.equal(-1, [1,2,3].indexOf(0));
         })
     })
