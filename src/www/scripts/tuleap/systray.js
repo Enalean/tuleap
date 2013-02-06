00b09a4297dc36cc93901699df6c854c579a065d
Nicolas Terray <nicolas.terray@enalean.com>
2013-02-06 09:54:15 +0100

    Save systray in local storage

--- src/www/scripts/tuleap/systray.js
+++ src/www/scripts/tuleap/systray.js
@@ -16,12 +16,13 @@
  */
 
 document.observe('dom:loaded', function () {
-    var cache_duration_2_hours = 2 * 3600,
-        cache_duration_1_week  = 7 * 24 * 3600,
-        systray_collapse       = 'collapse',
-        systray_expand         = 'expand';
-        systray_collapse_key   = 'systray-collapse',
-        collapse_classname     = 'systray-collapsed';
+    var cache_duration_2_hours    = 2 * 3600,
+        cache_duration_1_week     = 7 * 24 * 3600,
+        systray_collapse          = 'collapse',
+        systray_expand            = 'expand';
+        systray_collapse_cachekey = 'systray-collapse',
+        collapse_classname        = 'systray-collapsed',
+        systray_links_cachekey    = 'systray-links';
 
     if (! document.body.hasClassName('lab-mode')) {
         return;
@@ -49,22 +50,43 @@ document.observe('dom:loaded', function () {
     }
 
     function loadLinks(systray) {
-        var systray_links = systray.down('.systray_links'),
-            template      = new Template('<a href="#{href}">#{label}</a>');;
+        var template = new Template('<a href="#{href}">#{label}</a>'),
+            links    = AZHU.storage.load(systray_links_cachekey);
 
-        new Ajax.Request('/systray.json', { onSuccess: getLinksFromJSONRequest });
+        if (links) {
+            insertLinksInSystray(links);
+        } else {
+            new Ajax.Request('/systray.json', { onSuccess: getLinksFromJSONRequest });
+        }
 
         function getLinksFromJSONRequest(transport) {
-            if (transport.responseJSON) {
-                transport.responseJSON.each(function (link) {
-                    systray_links.insert(template.evaluate(link));
-                });
+            var links = transport.responseJSON;
+
+            if (links) {
+                saveLinks(links);
+                insertLinksInSystray(links);
             }
+        }
+
+        function insertLinksInSystray(links) {
+            var systray_links = systray.down('.systray_links');
+
+            links.each(function (link) {
+                systray_links.insert(template.evaluate(link));
+            });
+        }
+
+        function saveLinks(links) {
+            AZHU.storage.save(
+                systray_links_cachekey,
+                links,
+                cache_duration_2_hours
+            );
         }
     }
 
     function loadTogglePreference(systray) {
-        if (AZHU.storage.load(systray_collapse_key) === systray_collapse) {
+        if (AZHU.storage.load(systray_collapse_cachekey) === systray_collapse) {
             toggleSystray(systray);
         }
     }
@@ -76,7 +98,7 @@ document.observe('dom:loaded', function () {
 
     function saveTogglePreference(systray) {
         AZHU.storage.save(
-            systray_collapse_key,
+            systray_collapse_cachekey,
             systray.hasClassName(collapse_classname) ? systray_collapse : systray_expand,
             cache_duration_1_week
         );
