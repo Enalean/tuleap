05f5cb690f71c1ef5ac233edf9911d2ca1a0e97e
Nicolas Terray <nicolas.terray@gmail.com>
2013-02-08 22:41:32 +0100

    License block

--- src/www/scripts/AZHU/storage.js
+++ src/www/scripts/AZHU/storage.js
@@ -1,6 +1,21 @@
-// todo: ask for license
+/**
+ * Copyright (c) Anhang Zhu, 2011. All rights reserved
+ * http://blog.anhangzhu.com/2011/07/20/html-5-local-storage-with-expiration/
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// http://blog.anhangzhu.com/2011/07/20/html-5-local-storage-with-expiration/
 var AZHU = { }
 AZHU.storage = {
   save: function(key, jsonData, expirationSec){
