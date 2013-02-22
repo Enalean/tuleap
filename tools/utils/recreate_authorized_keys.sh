0fa889c63139846fd15c85daa6e1555ffd58ce99
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-02-22 14:55:03 +0100

    refactor shell script

--- tools/utils/recreate_authorized_keys.sh
+++ tools/utils/recreate_authorized_keys.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # redo an authorized_keys file if the old one was purged by a gitolite error
+# You need to be root when executign this script
 
 CAT='/bin/cat'
-#su -
-
 /etc/init.d/codendi stop
-
 
 #Drop the file:
 
@@ -15,23 +13,13 @@ rm /usr/com/gitolite/.ssh/authorized_keys
 #Get the admin ssh key:
 
 public_key=$($CAT /home/codendiadm/.ssh/id_rsa_gl-adm.pub)
-echo "$public_key"
 #Edit '/usr/com/gitolite/.ssh/authorized_keys':
-#authorized_keys= '# gitolite start command="/usr/bin/gl-auth-command id_rsa_gl-adm",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty $public_key # gitolite end'
 
 echo "# gitolite start" >> /usr/com/gitolite/.ssh/authorized_keys
 echo "command=\"/usr/bin/gl-auth-command id_rsa_gl-adm\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ""$public_key" >> /usr/com/gitolite/.ssh/authorized_keys
 echo "# gitolite end" >> /usr/com/gitolite/.ssh/authorized_keys
-#Add:
 
-#'# gitolite start
-#command="/usr/bin/gl-auth-command id_rsa_gl-adm",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty'
-
-#Copy the key after the previous text and add at a new line
-
-#'# gitolite end'
-
-#and change the rights:
+#Change the rights:
 
 chown gitolite:gitolite  /usr/com/gitolite/.ssh/authorized_keys
 
