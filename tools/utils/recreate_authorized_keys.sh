597174908c28dd1a2b940ee343d12e1fa5dc3c9b
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-02-26 11:03:46 +0100

    improve bash script

--- tools/utils/recreate_authorized_keys.sh
+++ tools/utils/recreate_authorized_keys.sh
@@ -6,9 +6,16 @@
 CAT='/bin/cat'
 /etc/init.d/codendi stop
 
+if [ -f /usr/com/gitolite/.ssh/authorized_keys ]
+then
+   DIR='/usr/com'
+else
+   DIR='/var/lib'
+fi
+
 #Drop the file:
 
-rm /usr/com/gitolite/.ssh/authorized_keys
+rm $DIR/gitolite/.ssh/authorized_keys
 
 #Get the admin ssh key:
 
@@ -19,16 +26,16 @@ echo "Recreate authorized_keys and add gl-adm public key"
 
 #Edit '/usr/com/gitolite/.ssh/authorized_keys':
 
-echo "# gitolite start" >> /usr/com/gitolite/.ssh/authorized_keys
-echo "command=\"/usr/bin/gl-auth-command id_rsa_gl-adm\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ""$public_key" >> /usr/com/gitolite/.ssh/authorized_keys
-echo "# gitolite end" >> /usr/com/gitolite/.ssh/authorized_keys
+echo "# gitolite start" >> $DIR/gitolite/.ssh/authorized_keys
+echo "command=\"/usr/bin/gl-auth-command id_rsa_gl-adm\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ""$public_key" >> $DIR/gitolite/.ssh/authorized_keys
+echo "# gitolite end" >> $DIR/gitolite/.ssh/authorized_keys
 
 #Provide feedback
 echo "authorized_keys correctly filled"
 
 #Change the rights:
 
-chown gitolite:gitolite  /usr/com/gitolite/.ssh/authorized_keys
+chown gitolite:gitolite  $DIR/gitolite/.ssh/authorized_keys
 
 #Then you have to push:
 
