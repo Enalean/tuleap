d87a8f89c7fd3bc3a1ea188fbd30ec735952d7f9
jtekt <yoann.celton@jtekt-eu.com>
2013-02-27 09:28:28 +0100

    Updated the copyright block

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactInTrackerId.class.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactInTrackerId.class.php
@@ -1,21 +1,24 @@
 <?php
 /**
- * Copyright (c) Xerox Corporation, Codendi Team, 2001-2010. All rights reserved
+ * Copyright (c) Tuleap, 2013. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * Originally written by Yoann Celton, 2013. Jtekt Europe.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 class Tracker_FormElement_Field_ArtifactInTrackerId extends Tracker_FormElement_Field_ArtifactId {
