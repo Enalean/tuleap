e4c310d5eb664526934b36cd7a92b236907c0e4e
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-02-28 14:34:02 +0100

    remove useless preg_replace : now it is done in the sql script when a new project activate the mediawiki plugin

--- src/www/include/service.php
+++ src/www/include/service.php
@@ -49,7 +49,6 @@ function service_create_service($arr, $group_id, $template, $force_enable = fals
 function service_replace_template_name_in_link($link, array $template , Project $project) {
     $link = preg_replace('#(/www/|/projects/|group=)'.$template['name'].'(/|&|$)#','$1'.$project->getUnixName().'$2',$link);
     $link = preg_replace('/group_id='. $template['id'] .'([^\d]|$)/', 'group_id='. $project->getGroupId() .'$1', $link);
-    $link = preg_replace('/\/plugins\/mediawiki\/wiki\/(.*)/', '/plugins/mediawiki/wiki/'. $project->getUnixName(), $link);
     return $link;
 }
 ?>
