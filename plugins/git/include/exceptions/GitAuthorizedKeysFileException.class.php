0b251f00c869fa50650044a3a28ae1c002201cf0
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-02-28 10:14:43 +0100

    move sh file into git plugin repositories

--- plugins/git/include/exceptions/GitAuthorizedKeysFileException.class.php
+++ plugins/git/include/exceptions/GitAuthorizedKeysFileException.class.php
@@ -25,7 +25,7 @@ class GitAuthorizedKeysFileException  extends Exception {
     public function __construct($authorized_keys_file) {
         parent::__construct($authorized_keys_file.' is empty'.
             '. In order to retrieve a file with the gitolite admin public key, run the script : '
-            .Config::get('codendi_dir').'/tools/utils/recreate_authorized_keys.sh');
+            .Config::get('codendi_dir').'/plugins/git/bin/recreate_authorized_keys.sh');
     }
 }
 
