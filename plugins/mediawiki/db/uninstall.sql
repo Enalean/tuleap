fbb1ee37a222a92a80043fe2c3cd26496415e196
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-03-01 08:54:35 +0100

    add lines in uninstall.sql

--- plugins/mediawiki/db/uninstall.sql
+++ plugins/mediawiki/db/uninstall.sql
@@ -4,3 +4,6 @@ DROP DATABASE 'plugin_mediawiki_baah';
 USE codendi;
 DELETE FROM codendi.service
     WHERE short_name = 'plugin_mediawiki';
+
+DELETE FROM codendi.plugin
+    WHERE name = 'mediawiki';
