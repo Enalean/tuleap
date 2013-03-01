a334aa83aef583a0dee2ea5c3ad75e9651c280f3
Ahmed HOSNI <ahmed.hosni@st.com>
2013-03-01 16:03:21 +0100

    Bypass html purifier when formatting changeset values for CSV export

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.class.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.class.php
@@ -122,7 +122,8 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
         if ($value['id'] == 100 || ! array_key_exists($value['id'], $this->values)) {
             return '';
         } else {
-            return $this->format($this->values[$value['id']]);
+            //return $this->format($this->values[$value['id']]);
+            return $this->values[$value['id']]->getLabel();
         }
     }
     
