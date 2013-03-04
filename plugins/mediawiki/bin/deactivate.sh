c7b90996a1e429c05f2a7d6113394500084f89bf
Nicolas Terray <nicolas.terray@enalean.com>
2013-03-04 18:15:50 +0100

    replace debug script by instructions in the readme

--- plugins/mediawiki/bin/deactivate.sh
+++
@@ -1,5 +0,0 @@
-#
-# Sample script to remove a mediawiki for a project named 'baah'
-#
-
-rm -rf /var/lib/codendi/plugins/mediawiki/projects/CommeTuVeux/
--- plugins/mediawiki/db/deactivate.sql
+++
@@ -1,8 +0,0 @@
-/*
-* sample script to remove service from a project with short_name 'commetuveux' and id 104
-*/
-
-USE codendi;
-UPDATE codendi.service SET is_active = 1, is_used = 0 WHERE group_id = 104 AND short_name = 'plugin_mediawiki';
-
-DROP DATABASE `plugin_mediawiki_commetuveux`;
