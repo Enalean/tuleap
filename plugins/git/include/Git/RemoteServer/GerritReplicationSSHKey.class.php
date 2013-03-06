8c05f49c24bb5b5451cc03edd2a8a525ca8dc18a
Dylan Bowden <dylan.bowden@enalean.com>
2013-03-06 12:27:39 +0100

    creating new replication key class

---
+++ plugins/git/include/Git/RemoteServer/GerritReplicationSSHKey.class.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2013. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
+
+class Git_RemoteServer_GerritReplicationSSHKey {
+
+    /** @var string */
+    private $value;
+
+    /** @var string */
+    private $user_name;
+
+    /** @var int */
+    private $host_id;
+
+    /**
+     * @param string $value
+     * @return \Git_RemoteServer_GerritReplicationSSHKey
+     */
+    public function setValue($value) {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @param string $userName
+     * @return \Git_RemoteServer_GerritReplicationSSHKey
+     */
+    public function setUserName($user_name) {
+        $this->user_name = $user_name;
+        return $this;
+    }
+
+    /**
+     * @param int $id
+     * @return \Git_RemoteServer_GerritReplicationSSHKey
+     */
+    public function setGerritHostId($id) {
+        $this->host_id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue() {
+        return $this->value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserName() {
+        return $this->user_name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGerritHostId() {
+        return $this->host_id;
+    }
+}
+?>
