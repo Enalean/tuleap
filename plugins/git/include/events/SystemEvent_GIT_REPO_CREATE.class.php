1fe4bb6f5f9ae63699344109d91943c27596b64d
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-04-08 16:53:49 +0200

    Convert old git shell create repo event to new/gitolite one

--- plugins/git/include/events/SystemEvent_GIT_REPO_CREATE.class.php
+++ plugins/git/include/events/SystemEvent_GIT_REPO_CREATE.class.php
@@ -1,110 +1,55 @@
 <?php
 /**
- * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Enalean, 2013. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
 require_once('common/system_event/SystemEvent.class.php');
-require_once('common/backend/Backend.class.php');
 require_once('common/user/UserManager.class.php');
 
-/**
- * Description of SystemEvent_DVCS_REPO_CREATE
- *
- * @author gstorchi
- */
 class SystemEvent_GIT_REPO_CREATE extends SystemEvent {
 
+    /** @var GitRepositoryFactory */
+    private $repository_factory;
+
+    public function injectDependencies(GitRepositoryFactory $repository_factory) {
+        $this->repository_factory = $repository_factory;
+    }
+
+    private function getRepositoryFromParameters() {
+        $parameters = $this->getParametersAsArray();
+        return $this->repository_factory->getRepositoryById(intval($parameters[0]));
+    }
+
     public function process() {
-
-        global $sys_allow_restricted_users;
-
-        $parameters  = $this->getParametersAsArray();
-        $project     = null;
-        if ( !empty($parameters[0]) ) {
-            $project = $this->getProject( $parameters[0] );
-        }
-        else {
-            $this->error('Missing argument project id');
-            return false;
-        }
-
-        $repositoryName = '';
-        if ( !empty($parameters[1]) ) {
-            $repositoryName = $parameters[1];
-        }
-        else {
-            $this->error('Missing argument repository name');
-            return false;
-        }
-
-        $userId = 0;
-        if ( !empty($parameters[2]) ) {
-            $userId = $parameters[2];
-        }
-        else {
-            $this->error('Missing argument user id');
-            return false;
-        }
-
-        try {
-            $repository = new GitRepository();
-            $repository->setBackend(Backend::instance('Git','GitBackend'));
-            $repository->setDescription(GitRepository::DEFAULT_DESCRIPTION);
-            //default access is private when restricted users are allowed
-            if ( $sys_allow_restricted_users == 1) {
-                $repository->setAccess( GitRepository::PRIVATE_ACCESS );
-            } else {
-                $repository->setAccess( GitRepository::PUBLIC_ACCESS );
-            }
-            $user = null;
-            if ( !empty($userId) ) {
-                $user = UserManager::instance()->getUserById($userId);
-            }
-            if ( !empty($user) ) {
-                $repository->setCreator($user);
-            }
-            $repository->setProject($project);
-            $repository->setName($repositoryName);            
-            $repository->create();
-            $this->done();
-        }
-        catch (GitDaoException $e) {
-            $this->error( $e->getMessage() );
-            return false;
-        }
-        catch (GitDriverException $e) {
-            $this->error( $e->getMessage() );
-            return false;
-        }
-        catch (GitBackendException $e) {
-            $this->error( $e->getMessage() );
-            return false;
-        }
-        catch (Exception $e) {
-            $this->error( $e->getMessage() );
-            return false;
-        }
-
+        $repository = $this->getRepositoryFromParameters();
+        $repository->getBackend()->createReference($repository);
+        $this->done();
     }
 
     public function verbalizeParameters($with_link) {
-
-        return  $this->parameters;
+        if ($with_link) {
+            $repository = $this->getRepositoryFromParameters();
+            return '<a href="/plugins/git/?action=repo_management&group_id='.$repository->getProjectId().'&repo_id='.$repository->getId().'">'.$repository->getName().'</a>';
+        } else {
+            return $parameters;
+        }
     }
+
 
 }
 
---
+++ plugins/git/tests/events/SystemEvent_GIT_REPO_CREATETest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright Enalean (c) 2011, 2012, 2013. All rights reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once dirname(__FILE__).'/../bootstrap.php';
+
+class SystemEvent_GIT_REPO_CREATETest extends TuleapTestCase {
+    private $repository_id = 115;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->backend    = mock('Git_Backend_Gitolite');
+
+        $this->repository = mock('GitRepository');
+        stub($this->repository)->getBackend()->returns($this->backend);
+        
+        $this->repository_factory = mock('GitRepositoryFactory');
+        stub($this->repository_factory)->getRepositoryById()->returns($this->repository);
+
+        $this->event = partial_mock('SystemEvent_GIT_REPO_CREATE', array('done', 'error'));
+        $this->event->setParameters("$this->repository_id");
+        $this->event->injectDependencies($this->repository_factory);
+    }
+
+    public function itGetsTheRepositoryFromTheFactory() {
+        expect($this->repository_factory)->getRepositoryById($this->repository_id)->once();
+        $this->event->process();
+    }
+
+    public function itDelegatesToBackendRepositoryCreation() {
+        expect($this->backend)->createReference()->once();
+        $this->event->process();
+    }
+
+    public function itMarksTheEventAsDone() {
+        expect($this->event)->done()->once();
+        $this->event->process();
+    }
+}
+
+?>
