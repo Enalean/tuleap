158072486ad4457609b048b0e11a59e38c314751
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-04-11 14:13:05 +0200

    Persist ssh key

--- plugins/git/include/Git/RemoteServer/Dao.class.php
+++ plugins/git/include/Git/RemoteServer/Dao.class.php
@@ -56,15 +56,16 @@ class Git_RemoteServer_Dao extends DataAccessObject {
         return $this->retrieve($sql);
     }
 
-    public function save($id, $host, $ssh_port, $http_port, $login, $identity_file) {
-        $id            = $this->da->escapeInt($id);
-        $host          = $this->da->quoteSmart($host);
-        $ssh_port      = $this->da->escapeInt($ssh_port);
-        $http_port     = $this->da->escapeInt($http_port);
-        $login         = $this->da->quoteSmart($login);
-        $identity_file = $this->da->quoteSmart($identity_file);
-        $sql = "REPLACE INTO plugin_git_remote_servers (id, host, ssh_port, http_port, login, identity_file)
-                VALUES ($id, $host, $ssh_port, $http_port, $login, $identity_file)";
+    public function save($id, $host, $ssh_port, $http_port, $login, $identity_file, $replication_key) {
+        $id              = $this->da->escapeInt($id);
+        $host            = $this->da->quoteSmart($host);
+        $ssh_port        = $this->da->escapeInt($ssh_port);
+        $http_port       = $this->da->escapeInt($http_port);
+        $login           = $this->da->quoteSmart($login);
+        $identity_file   = $this->da->quoteSmart($identity_file);
+        $replication_key = $this->da->quoteSmart($replication_key);
+        $sql = "REPLACE INTO plugin_git_remote_servers (id, host, ssh_port, http_port, login, identity_file, ssh_key)
+                VALUES ($id, $host, $ssh_port, $http_port, $login, $identity_file, $replication_key)";
 
         return $this->updateAndGetLastId($sql);
     }
