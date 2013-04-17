c9cf6c78ad080b69dd28ba02a8d6d961daffa73b
Dylan Bowden <dylan.bowden@enalean.com>
2013-04-17 15:48:44 +0200

    adding toArray() method for xml importer

--- plugins/agiledashboard/include/AgileDashboard/XMLImporter.class.php
+++ plugins/agiledashboard/include/AgileDashboard/XMLImporter.class.php
@@ -19,7 +19,38 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/
  */
 
+/**
+ * Transforms imported xml into php values
+ */
 class AgileDashboard_XMLImporter {
 
+    /**
+     *
+     * @param SimpleXMLElement $xml_object
+     * @return array
+     */
+    public function toArray(SimpleXMLElement $xml_object) {
+        $plannings = array();
+
+        $plannings_node_name = AgileDashboard_XMLExporter::NODE_PLANNINGS;
+
+        if (! $xml_object->$plannings_node_name) {
+            return $plannings;
+        }
+
+        foreach ($xml_object->$plannings_node_name->children() as $planning) {
+            $attributes = $planning->attributes();
+
+            $plannings[] = array(
+                PlanningParameters::NAME                => (string) $attributes[PlanningParameters::NAME],
+                PlanningParameters::BACKLOG_TITLE       => (string) $attributes[PlanningParameters::BACKLOG_TITLE],
+                PlanningParameters::PLANNING_TITLE      => (string) $attributes[PlanningParameters::PLANNING_TITLE],
+                PlanningParameters::BACKLOG_TRACKER_ID  => (string) $attributes[PlanningParameters::BACKLOG_TRACKER_ID],
+                PlanningParameters::PLANNING_TRACKER_ID => (string) $attributes[PlanningParameters::BACKLOG_TRACKER_ID],
+            );
+        }
+
+        return $plannings;
+    }
 }
 ?>
--- plugins/agiledashboard/tests/include/AgileDashboard/XMLImporterTest.php
+++ plugins/agiledashboard/tests/include/AgileDashboard/XMLImporterTest.php
@@ -21,5 +21,76 @@ require_once dirname(__FILE__).'/../../bootstrap.php';
 
 class AgileDashboard_XMLimporterTest extends TuleapTestCase {
 
+    /**
+     *
+     * @var SimpleXMLElement
+     */
+    private $xml_object;
+
+    /**
+     *
+     * @var AgileDashboard_XMLImporter
+     */
+    private $importer;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->importer = new AgileDashboard_XMLImporter();
+
+        $name                 = AgileDashboard_XMLExporter::ATTRIBUTE_PLANNING_NAME;
+        $plan_title           = AgileDashboard_XMLExporter::ATTRIBUTE_PLANNING_TITLE;
+        $backlog_title        = AgileDashboard_XMLExporter::ATTRIBUTE_PLANNING_BACKLOG_TITLE;
+        $item_tracker_id      = AgileDashboard_XMLExporter::ATTRIBUTE_PLANNING_ITEM_TRACKER_ID;
+        $milestone_tracker_id = AgileDashboard_XMLExporter::ATTRIBUTE_PLANNING_MILESTONE_TRACKER_ID;
+
+        $default_xml = '<?xml version="1.0" encoding="UTF-8"?>
+                 <agiledashboard>
+                    <plannings>
+                        <planning '.
+                            $name.'="Sprint Planning" '.
+                            $plan_title.'="Sprint Plan" '.
+                            $item_tracker_id.'="T11" '.
+                            $backlog_title.'="Release Backlog" '.
+                            $milestone_tracker_id.'="T11"/>
+                        <planning '.
+                            $name.'="Sprint Planning" '.
+                            $plan_title.'="Sprint Plan" '.
+                            $item_tracker_id.'="T11" '.
+                            $backlog_title.'="Release Backlog" '.
+                            $milestone_tracker_id.'="T11"/>
+                    </plannings>
+                 </agiledashboard>';
+        $this->xml_object = new SimpleXMLElement($default_xml);
+    }
+
+    public function itReturnsAnEmptyArrayIfNoPlanningsExist() {
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>
+                 <agiledashboard />';
+        $xml_object = new SimpleXMLElement($xml);
+
+        $data = $this->importer->toArray($xml_object);
+
+        $this->assertTrue(is_array($data));
+    }
+
+    public function itReturnsAnArrayForEachPlanning() {
+        $data = $this->importer->toArray($this->xml_object);
+
+        $this->assertTrue(is_array($data));
+        $this->assertCount($data, 2);
+    }
+
+    public function itReturnsAnArrayOfPlanningParameterValuesForAPlanning() {
+        $data = $this->importer->toArray($this->xml_object);
+
+        $a_planning = $data[0];
+
+        $this->assertTrue(array_key_exists(PlanningParameters::NAME, $a_planning));
+        $this->assertTrue(array_key_exists(PlanningParameters::BACKLOG_TITLE, $a_planning));
+        $this->assertTrue(array_key_exists(PlanningParameters::PLANNING_TITLE, $a_planning));
+        $this->assertTrue(array_key_exists(PlanningParameters::BACKLOG_TRACKER_ID, $a_planning));
+        $this->assertTrue(array_key_exists(PlanningParameters::PLANNING_TRACKER_ID, $a_planning));
+    }
 }
 ?>
