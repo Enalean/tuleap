3637f30626f9d39a21a27a9137d303a2eca15407
Dylan Bowden <dylan.bowden@enalean.com>
2013-04-18 15:38:20 +0200

    iadding script to import project in xml import

---
+++ src/utils/import_project_xml.php
@@ -0,0 +1,47 @@
+#!/usr/share/codendi/src/utils/php-launcher.sh
+<?php
+/**
+ * Copyright (c) Enalean, 2012. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'pre.php';
+require_once 'common/project/ProjectXMLImporter.class.php';
+
+if ($argc < 3) {
+    echo <<< EOT
+Usage: $argv[0] project_id xml
+Create a project trackers, agiledashbiard and cardwall from XML format
+
+EOT;
+    exit(1);
+}
+
+$xml = file_get_contents($argv[2], "r");
+
+$project = ProjectManager::instance()->getProject($argv[1]);
+
+if ($project && !$project->isError() && !$project->isDeleted()) {
+    $xml_element = new SimpleXMLElement($xml);
+
+    $xml_importer = new ProjectXMLImporter(EventManager::instance());
+    $xml_importer->import($project, $xml_element);
+} else {
+    echo "*** ERROR: Invalid project_id\n";
+    exit(1);
+}
+?>
