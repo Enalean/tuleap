183904c11a0017463758da4c0682912aa4ecff51
dylan_bowden <dylan.bowden@enalean.com>
2013-05-16 18:02:05 +0200

    removing unique on 3 columns

---
+++ src/db/mysql/updates/2013/201316051743_remove_unique_from_svn_checkins.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) Enalean SAS 2013. All rights reserved
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ */
+class b201303121639_add_owner_to_system_events extends ForgeUpgrade_Bucket {
+    public function description() {
+        return <<<EOT
+Remove unique from svn checkins
+EOT;
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $sql = "ALTER TABLE svn_checkins DROP KEY 'uniq_checkins_idx'";
+        if ($this->db->tableNameExists('svn_checkins')) {
+            $res = $this->db->dbh->exec($sql);
+            if ($res === false) {
+                throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occured while removing key uniq_checkins_idx from table svn_checkins');
+            }
+        }
+    }
+
+    public function postUp() {
+    }
+}
+
+?>
