c344306d6bbfde21887e8feeed5fc42fc4b0729b
Nicolas Terray <nicolas.terray@enalean.com>
2013-05-17 18:19:35 +0200

    Dedicated files

---
+++ plugins/agiledashboard/include/AgileDashboard/Milestone/Pane/ContentBacklogStrategy.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright Enalean (c) 2013. All rights reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * I retrieve the content of the backlog
+ */
+abstract class AgileDashboard_Milestone_Pane_ContentBacklogStrategy {
+
+    /** @var Tracker_Artifact[] */
+    protected $milestone_backlog_artifacts;
+
+    public function __construct($milestone_backlog_artifacts, $item_name) {
+        $this->milestone_backlog_artifacts = $milestone_backlog_artifacts;
+        $this->item_name                   = $item_name;
+    }
+
+    /** @return string */
+    public function getItemName() {
+        return $this->item_name;
+    }
+
+    /** @return Tracker_Artifact[] */
+    public abstract function getArtifacts(PFUser $user);
+}
+?>
