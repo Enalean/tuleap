c61d86b27c905ba5cfbdd727bfd0dd1cea6f2889
dylan_bowden <dylan.bowden@enalean.com>
2013-05-30 14:33:15 +0200

    Revert "THIS IS WHERE restriction was commented out for new plannings"
    This reverts commit b572ecaa1726e4f130e75e20e43add4f7b988f79.

--- plugins/agiledashboard/include/Planning/PlanningRequestValidator.class.php
+++ plugins/agiledashboard/include/Planning/PlanningRequestValidator.class.php
@@ -64,8 +64,7 @@ class Planning_RequestValidator {
         return $this->nameIsPresent($planning_parameters)
             && $this->backlogTrackerIdIsPresentAndIsAPositiveIntegers($planning_parameters)
             && $this->planningTrackerIdIsPresentAndIsAPositiveInteger($planning_parameters)
-           // && $this->planningTrackerIsNotThePlanningTrackerOfAnotherPlanningInTheSameProject($group_id, $planning_id, $planning_parameters)
-            ;
+            && $this->planningTrackerIsNotThePlanningTrackerOfAnotherPlanningInTheSameProject($group_id, $planning_id, $planning_parameters);
     }
     
     /**
