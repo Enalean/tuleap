93c453e1b67073f05ee29d5c82313adcddd428c0
Nicolas Terray <nicolas.terray@enalean.com>
2013-05-31 11:30:38 +0200

    Better wording to ease debugging

---
+++ plugins/tracker/include/Tracker/UgroupPermissionsConsistencyMessenger.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2013. All Rights Reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * I provide feedback to user about ugroup permissions consistency
+ */
+class Tracker_UgroupPermissionsConsistencyMessenger {
+
+    public function ugroupsAreTheSame() {
+        echo '<div class="alert alert-info">This template use ugroups that seems to be defined in your project.</div>';
+    }
+
+    public function ugroupsMissing() {
+        echo '<div class="alert alert-warning">This template use ugroups that are not defined in your project. Perhaps it will make some chocapics.</div>';
+    }
+
+    public function allIsWell() {
+    }
+}
+?>
