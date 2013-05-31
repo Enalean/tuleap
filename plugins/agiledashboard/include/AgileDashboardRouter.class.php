7f4a5ac878d6a40d626d8405274cd17d6180d6a8
dylan_bowden <dylan.bowden@enalean.com>
2013-05-31 09:07:20 +0200

    no change- odd merge

--- plugins/agiledashboard/include/AgileDashboardRouter.class.php
+++ plugins/agiledashboard/include/AgileDashboardRouter.class.php
@@ -23,18 +23,18 @@ require_once 'common/plugin/Plugin.class.php';
 /**
  * Routes HTTP (and maybe SOAP ?) requests to the appropriate controllers
  * (e.g. PlanningController, MilestoneController...).
- * 
+ *
  * See AgileDashboardRouter::route()
- * 
+ *
  * TODO: Layout management should be extracted and moved to controllers or views.
  */
 class AgileDashboardRouter {
-    
+
     /**
      * @var Plugin
      */
     private $plugin;
-    
+
     /**
      * @param Service
      */
@@ -68,15 +68,15 @@ class AgileDashboardRouter {
         $this->planning_shortaccess_factory  = $planning_shortaccess_factory;
         $this->milestone_controller_factory  = $milestone_controller_factory;
     }
-    
+
     /**
      * Routes the given request to the appropriate controller.
-     * 
+     *
      * TODO:
      *   - Use a 'resource' parameter to deduce the controller (e.g. someurl/?resource=planning&id=2 )
      *   - Pass $request to action methods
-     * 
-     * @param Codendi_Request $request 
+     *
+     * @param Codendi_Request $request
      */
     public function route(Codendi_Request $request) {
         $controller                     = $this->buildController($request);
@@ -129,15 +129,15 @@ class AgileDashboardRouter {
                 $this->renderAction($controller, 'index', $request);
         }
     }
-    
+
     /**
      * Returns the page title according to the current controller action name.
-     * 
+     *
      * TODO:
      *   - Use a layout template, and move title retrieval to the appropriate presenters.
-     * 
+     *
      * @param string $action_name The controller action name (e.g. index, show...).
-     * 
+     *
      * @return string
      */
     private function getHeaderTitle($action_name) {
@@ -149,16 +149,16 @@ class AgileDashboardRouter {
             'show'  => $GLOBALS['Language']->getText('plugin_agiledashboard', 'planning_show'),
             'showTop'  => $GLOBALS['Language']->getText('plugin_agiledashboard', 'planning_show')
         );
-        
+
         return $header_title[$action_name];
     }
-    
+
     /**
      * Retrieves the Agile Dashboard Service instance matching the request group id.
-     * 
+     *
      * @param Codendi_Request $request
-     * 
-     * @return Service 
+     *
+     * @return Service
      */
     private function getService(Codendi_Request $request) {
         if ($this->service == null) {
@@ -167,10 +167,10 @@ class AgileDashboardRouter {
         }
         return $this->service;
     }
-    
+
     /**
      * Renders the top banner + navigation for all Agile Dashboard pages.
-     * 
+     *
      * @param MVC2_Controller $controller The controller instance
      * @param Codendi_Request $request    The request
      * @param string          $title      The page title
@@ -202,26 +202,26 @@ class AgileDashboardRouter {
         }
         $service->displayHeader($title, $breadcrumbs->getCrumbs(), $toolbar);
     }
-    
+
     private function userIsAdmin(Codendi_Request $request) {
         return $request->getProject()->userIsAdmin($request->getCurrentUser());
     }
-    
+
     /**
      * Renders the bottom footer for all Agile Dashboard pages.
-     * 
-     * @param Codendi_Request $request 
+     *
+     * @param Codendi_Request $request
      */
     private function displayFooter(Codendi_Request $request) {
         $this->getService($request)->displayFooter();
     }
-    
+
     /**
      * Builds a new Planning_Controller instance.
-     * 
+     *
      * @param Codendi_Request $request
-     * 
-     * @return Planning_Controller 
+     *
+     * @return Planning_Controller
      */
     protected function buildController(Codendi_Request $request) {
         return new Planning_Controller(
@@ -232,10 +232,10 @@ class AgileDashboardRouter {
             $this->plugin->getThemePath()
         );
     }
-    
+
     /**
      * Renders the given controller action, with page header/footer.
-     * 
+     *
      * @param MVC2_Controller $controller  The controller instance.
      * @param string          $action_name The controller action name (e.g. index, show...).
      * @param Codendi_Request $request     The request
@@ -249,11 +249,11 @@ class AgileDashboardRouter {
         $this->executeAction($controller, $action_name, $args);
         $this->displayFooter($request);
     }
-    
+
     /**
      * Executes the given controller action, without rendering page header/footer.
      * Useful for actions ending with a redirection instead of page rendering.
-     * 
+     *
      * @param MVC2_Controller $controller  The controller instance.
      * @param string          $action_name The controller action name (e.g. index, show...).
      * @param array           $args        Arguments to pass to the controller action method.
@@ -261,17 +261,17 @@ class AgileDashboardRouter {
     protected function executeAction(MVC2_Controller $controller,
                                                      $action_name,
                                      array           $args = array()) {
-        
+
         call_user_func_array(array($controller, $action_name), $args);
     }
-    
+
     /**
      * Routes some milestone-related requests.
-     * 
+     *
      * TODO:
      *   - merge into AgileDashboardRouter::route()
-     * 
-     * @param Codendi_Request $request 
+     *
+     * @param Codendi_Request $request
      */
     public function routeShowPlanning(Codendi_Request $request) {
         $aid = $request->getValidated('aid', 'int', 0);
