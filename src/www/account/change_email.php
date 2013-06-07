a4350c28cb4e16d6e461658446ad8e0f88eb3863
Ahmed HOSNI <ahmed.hosni@st.com>
2013-06-07 17:40:52 +0100

    Fix CSRF when change email's account

--- src/www/account/change_email.php
+++ src/www/account/change_email.php
@@ -7,8 +7,9 @@
 // 
 
 require_once('pre.php');
-
+require_once('common/include/CSRFSynchronizerToken.class.php');
 require_once('common/event/EventManager.class.php');
+
 $em =& EventManager::instance();
 $em->processEvent('before_change_email', array());
 
@@ -16,6 +17,7 @@ session_require(array('isloggedin'=>1));
 
 
 $HTML->header(array('title'=> $Language->getText('account_change_email', 'title')));
+$csrf = new CSRFSynchronizerToken('/account/change_email.php');
 ?>
 
 <P><B><?php echo $Language->getText('account_change_email', 'title'); ?></B>
@@ -23,6 +25,9 @@ $HTML->header(array('title'=> $Language->getText('account_change_email', 'title'
 <?php echo $Language->getText('account_change_email', 'message'); ?>
 
 <FORM action="change_email-confirm.php" method="post">
+<?php
+echo $csrf->fetchHTMLInput();
+?>
 <P><INPUT type="hidden" name="form_user" value="<?php print user_getid(); ?>">
 <?php echo $Language->getText('account_change_email', 'label_new'); ?>:
 <INPUT type="text" size="30" name="form_newemail">
