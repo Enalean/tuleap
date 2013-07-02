4cba237897c2c4ee0969944de2e7e9b46e455f27
Thomas Cottier <thomas.cottier@enalean.com>
2013-07-02 12:18:55 +0200

    Fixing forgeupgrade bug on debian (path and version check)
    Change-Id: I9a0f73fe21a5c9db726060bc458e16083db6f2ef

--- tools/setup.sh
+++ tools/setup.sh
@@ -1390,12 +1390,13 @@ fi
 
 $MYSQL -u$PROJECT_ADMIN -p$codendiadm_passwd $PROJECT_NAME < /usr/share/forgeupgrade/db/install-mysql.sql
 $INSTALL --group=$PROJECT_ADMIN --owner=$PROJECT_ADMIN --mode=0755 --directory /etc/$PROJECT_NAME/forgeupgrade
-if [ $INSTALL_PROFILE = "rhel" -a "z$RH_MAJOR_VERSION" = "z6" ]; then
-    forge_upgrade_config_dist=$INSTALL_DIR/src/etc/forgeupgrade-config.ini.rhel6.dist
-else
+if [ $INSTALL_PROFILE = "rhel" -a "z$RH_MAJOR_VERSION" = "z5" ]; then
     forge_upgrade_config_dist=$INSTALL_DIR/src/etc/forgeupgrade-config.ini.dist
+else
+    forge_upgrade_config_dist=$INSTALL_DIR/src/etc/forgeupgrade-config.ini.rhel6.dist
 fi
 $INSTALL --group=$PROJECT_ADMIN --owner=$PROJECT_ADMIN --mode=0644 $forge_upgrade_config_dist /etc/$PROJECT_NAME/forgeupgrade/config.ini
+substitute /etc/$PROJECT_NAME/forgeupgrade/config.ini "%project_name%" "$PROJECT_NAME"
 
 ##############################################
 # *Last* step: install plugins
