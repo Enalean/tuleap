989359fe5b4fc29750fc65375a7a4e0c5d6f47d8
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-07-02 11:12:17 +0200

    Remove register globals usage
    Change-Id: I2bef8a6836c2e25ff8d07575437d7bfa3c43ebe4

--- src/www/admin/trove/trove_cat_list.php
+++ src/www/admin/trove/trove_cat_list.php
@@ -14,31 +14,32 @@ session_require(array('group'=>'1','admin_flags'=>'A'));
 
 // #######################################################
 
-function printnode ($nodeid,$text,$delete_ok=false) {
+function printnode($nodeid, $text, $depth = 0, $delete_ok = false) {
     global $Language;
 
-	// print current node, then all subnodes
-	print ('<BR>');
-	for ($i=0;$i<$GLOBALS['depth'];$i++) { print "&nbsp; &nbsp; "; }
-	html_image('ic/cfolder15.png',array());
-	print ('&nbsp; '.$text." ");
-	if ($nodeid != 0) {
-	  print ('&nbsp; <A href="trove_cat_edit.php?trove_cat_id='.$nodeid.'">['.$Language->getText('admin_trove_cat_list','edit').']</A> ');
-	}
-	if ($delete_ok) {
-	    print ('&nbsp; <A href="trove_cat_delete.php?trove_cat_id='.$nodeid.'">['.$Language->getText('admin_trove_cat_list','delete').']</A> ');
-	}
-	if ($nodeid != 0) {
-	  print ('&nbsp;'.help_button('trove_cat',$nodeid)."\n");
-	}
-	$GLOBALS["depth"]++;
-	$res_child = db_query("SELECT trove_cat_id,fullname,parent FROM trove_cat "
-		."WHERE parent='$nodeid' ORDER BY fullpath");
-	while ($row_child = db_fetch_array($res_child)) {
-	    $delete_ok = ($row_child["parent"] != 0);
-	    printnode($row_child["trove_cat_id"],$row_child["fullname"],$delete_ok);
-	}
-	$GLOBALS["depth"]--;
+    // print current node, then all subnodes
+    print ('<BR>');
+    for ($i = 0; $i < $depth; $i++) {
+        print "&nbsp; &nbsp; ";
+    }
+    html_image('ic/cfolder15.png', array());
+    print ('&nbsp; ' . $text . " ");
+    if ($nodeid != 0) {
+        print ('&nbsp; <A href="trove_cat_edit.php?trove_cat_id=' . $nodeid . '">[' . $Language->getText('admin_trove_cat_list', 'edit') . ']</A> ');
+    }
+    if ($delete_ok) {
+        print ('&nbsp; <A href="trove_cat_delete.php?trove_cat_id=' . $nodeid . '">[' . $Language->getText('admin_trove_cat_list', 'delete') . ']</A> ');
+    }
+    if ($nodeid != 0) {
+        print ('&nbsp;' . help_button('trove_cat', $nodeid) . "\n");
+    }
+
+    $res_child = db_query("SELECT trove_cat_id,fullname,parent FROM trove_cat "
+            . "WHERE parent='" . db_ei($nodeid) . "' ORDER BY fullpath");
+    while ($row_child = db_fetch_array($res_child)) {
+        $delete_ok = ($row_child["parent"] != 0);
+        printnode($row_child["trove_cat_id"], $row_child["fullname"], $depth+1, $delete_ok);
+    }
 }
 
 // ########################################################
@@ -47,10 +48,7 @@ $HTML->header(array('title'=>$Language->getText('admin_trove_cat_list','title'))
 
 echo "<H2>".$Language->getText('admin_trove_cat_list','header')."</H2>";
 
-$depth = 0;
-printnode(0,$Language->getText('admin_trove_cat_edit','root'));
-
-echo "<p>";
+printnode(0, $Language->getText('admin_trove_cat_edit','root'));
 
 $HTML->footer(array());
 
