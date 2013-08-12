67ef9e03699df273bdf8d4326a7e0e03fce48f8b
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-08-12 12:31:05 +0200

    Split tests in corresponding classes
    Change-Id: Ic3c8f4f43cfc0e318af59242f3d493c6ba7657a1

--- plugins/tracker/tests/workflow/Trigger/RulesProcessorTest.php
+++ plugins/tracker/tests/workflow/Trigger/RulesProcessorTest.php
@@ -20,64 +20,6 @@
 
 require_once TRACKER_BASE_DIR . '/../tests/bootstrap.php';
 
-abstract class Tracker_Workflow_Trigger_RulesProcessor_BaseTest  extends TuleapTestCase {
-
-    protected $parent;
-    protected $artifact;
-    protected $rules_processor;
-    protected $target_field_id;
-    protected $target_value_id;
-    protected $rule;
-    protected $target_field;
-    protected $target_value;
-    protected $trigger_field;
-    protected $trigger_value;
-    protected $task_tracker;
-    protected $story_tracker;
-
-    public function setUp() {
-        parent::setUp();
-
-        $this->story_tracker = aTracker()->withId(888)->build();
-        $this->task_tracker = aTracker()->withId(899)->build();
-
-        $this->parent = mock('Tracker_Artifact');
-        stub($this->parent)->getTracker()->returns($this->story_tracker);
-
-        $this->artifact = anArtifact()->withChangesets(array(mock('Tracker_Artifact_Changeset')))->withParentWithoutPermissionChecking($this->parent)->withTracker($this->task_tracker)->build();
-
-        $this->rules_processor = new Tracker_Workflow_Trigger_RulesProcessor(
-            new Tracker_Workflow_WorkflowUser(),
-            mock('WorkflowBackendLogger')
-        );
-
-        $this->target_field_id = 569;
-        $this->target_field    = aSelectBoxField()->withId($this->target_field_id)->withTracker($this->story_tracker)->build();
-        $this->target_value_id = 7;
-        $this->target_value    = aBindStaticValue()->withId($this->target_value_id)->build();
-
-        $this->trigger_field = aSelectBoxField()->withId(965)->withTracker($this->task_tracker)->build();
-        $this->trigger_value = aBindStaticValue()->withId(14)->build();
-
-        $this->rule = aTriggerRule()
-            ->applyValue(
-                new Tracker_Workflow_Trigger_FieldValue(
-                    $this->target_field,
-                    $this->target_value
-                )
-            )
-            ->whenAllOf()
-            ->childHas(
-                new Tracker_Workflow_Trigger_FieldValue(
-                    $this->trigger_field,
-                    $this->trigger_value
-                )
-            )
-            ->build();
-    }
-
-}
-
 class Tracker_Workflow_Trigger_RulesProcessor_GeneralTest extends TuleapTestCase {
 
     private $parent;
@@ -96,46 +38,13 @@ class Tracker_Workflow_Trigger_RulesProcessor_GeneralTest extends TuleapTestCase
         $this->task_tracker = aTracker()->withId(899)->build();
         $this->artifact = anArtifact()->withChangesets(array(mock('Tracker_Artifact_Changeset')))->withParentWithoutPermissionChecking($this->parent)->withTracker($this->task_tracker)->build();
         $this->user = aUser()->build();
-        $this->rules_processor = new Tracker_Workflow_Trigger_RulesProcessor(
-            new Tracker_Workflow_WorkflowUser(),
-            mock('WorkflowBackendLogger')
-        );
-
-        $this->target_field_id = 569;
-        $this->target_field    = aSelectBoxField()->withId($this->target_field_id)->build();
-        $this->target_value_id = 7;
-        $this->target_value    = aBindStaticValue()->withId($this->target_value_id)->build();
-        $this->rule = mock('Tracker_Workflow_Trigger_TriggerRule');
-    }
-
-    public function itDoesNothingWhenArtifactHasNoParents() {
-        $artifact = anArtifact()->withTracker($this->task_tracker)->withoutParentWithoutPermissionChecking()->build();
-
-        $this->rules_processor->process($artifact, $this->rule);
-    }
-}
-
-class Tracker_Workflow_Trigger_RulesProcessorTest extends TuleapTestCase {
-
-    private $parent;
-    private $artifact;
-    private $user;
-    private $rules_processor;
-    private $target_field_id;
-    private $target_value_id;
-    private $rule;
-    private $target_field;
-    private $target_value;
-
-    public function setUp() {
-        parent::setUp();
-        $this->parent = mock('Tracker_Artifact');
-        $this->task_tracker = aTracker()->withId(899)->build();
-        $this->artifact = anArtifact()->withChangesets(array(mock('Tracker_Artifact_Changeset')))->withParentWithoutPermissionChecking($this->parent)->withTracker($this->task_tracker)->build();
-        $this->user = aUser()->build();
-        $this->rules_processor = new Tracker_Workflow_Trigger_RulesProcessor(
-            new Tracker_Workflow_WorkflowUser(),
-            mock('WorkflowBackendLogger')
+        $this->rules_processor = partial_mock(
+            'Tracker_Workflow_Trigger_RulesProcessor',
+            array('getRuleStrategy'),
+            array(
+                new Tracker_Workflow_WorkflowUser(),
+                mock('WorkflowBackendLogger')
+            )
         );
 
         $this->target_field_id = 569;
@@ -154,6 +63,13 @@ class Tracker_Workflow_Trigger_RulesProcessorTest extends TuleapTestCase {
             ->build();
     }
 
+    public function itDoesNothingWhenArtifactHasNoParents() {
+        $artifact = anArtifact()->withTracker($this->task_tracker)->withoutParentWithoutPermissionChecking()->build();
+
+        // expect no errors
+        $this->rules_processor->process($artifact, $this->rule);
+    }
+
     public function itAlwaysApplyRuleWhenAtLeastOneValueIsSet() {
         $fields_data = array(
             $this->target_field_id => $this->target_value_id
@@ -166,7 +82,7 @@ class Tracker_Workflow_Trigger_RulesProcessorTest extends TuleapTestCase {
         $this->rules_processor->process($this->artifact, $this->rule);
     }
 
-    public function itDoesntSetTargetValueIfAlreadySet() {
+     public function itDoesntSetTargetValueIfAlreadySet() {
         stub($this->parent)->getValue($this->target_field)->returns(aChangesetValueList()->withValues(array($this->target_value))->build());
         expect($this->parent)->createNewChangeset()->never();
         $this->rules_processor->process($this->artifact, $this->rule);
@@ -180,142 +96,6 @@ class IsAWorkflowUserExpectation extends SimpleExpectation {
 
     public function testMessage($compare) {
         return 'Tracker_Workflow_WorkflowUser expected, '.get_class($compare).' given';
-    }
-}
-
-class Tracker_Workflow_Trigger_RulesProcessor_AllOfTest extends Tracker_Workflow_Trigger_RulesProcessor_BaseTest {
-
-    private $bug_tracker;
-
-    public function setUp() {
-        parent::setUp();
-
-        $this->bug_tracker  = aTracker()->withId(901)->build();
-
-    }
-
-    public function itDoesntSetTargetValueIfAlreadySet() {
-        stub($this->parent)->getValue($this->target_field)->returns(aChangesetValueList()->withValues(array($this->target_value))->build());
-        expect($this->parent)->createNewChangeset()->never();
-        $this->rules_processor->process($this->artifact, $this->rule);
-    }
-
-    public function itSetTheValueIfArtifactHasNoSiblings() {
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator());
-        expect($this->parent)->createNewChangeset()->once();
-
-        $this->rules_processor->process($this->artifact, $this->rule);
-    }
-
-    public function itDoesntSetTheValueIfOneSiblingHasNoValue() {
-        $sibling = aMockArtifact()->withTracker($this->task_tracker)->build();
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator(array($sibling)));
-
-        expect($this->parent)->createNewChangeset()->never();
-
-        $this->rules_processor->process($this->artifact, $this->rule);
-    }
-
-    public function itSetTheValueIfOneSameTypeSiblingHasCorrectValue() {
-        $sibling = mock('Tracker_Artifact');
-        stub($sibling)->getId()->returns(112);
-        stub($sibling)->getTracker()->returns($this->task_tracker);
-        stub($sibling)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array($this->trigger_value))->build());
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator(array($sibling)));
-
-        expect($this->parent)->createNewChangeset()->once();
-
-        $this->rules_processor->process($this->artifact, $this->rule);
-    }
-
-    public function itDoesntSetTheValueIfOneSameTypeSiblingHasIncorrectValue() {
-        $sibling_1 = mock('Tracker_Artifact');
-        stub($sibling_1)->getId()->returns(112);
-        stub($sibling_1)->getTracker()->returns($this->task_tracker);
-        stub($sibling_1)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array($this->trigger_value))->build());
-
-        $sibling_2 = mock('Tracker_Artifact');
-        stub($sibling_2)->getId()->returns(113);
-        stub($sibling_2)->getTracker()->returns($this->task_tracker);
-        stub($sibling_2)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array(aBindStaticValue()->withId('whatever')->build()))->build());
-
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator(array($sibling_1, $sibling_2)));
-
-        expect($this->parent)->createNewChangeset()->never();
-
-        $this->rules_processor->process($this->artifact, $this->rule);
-    }
-}
-
-class Tracker_Workflow_Trigger_RulesProcessor_AllOfWithSeveralRulesTest extends Tracker_Workflow_Trigger_RulesProcessor_BaseTest {
-
-    private $bug_tracker;
-
-    public function setUp() {
-        parent::setUp();
-
-        $this->bug_tracker  = aTracker()->withId(901)->build();
-
-        $this->trigger_field_2 = aSelectBoxField()->withId(236)->withTracker($this->bug_tracker)->build();
-        $this->trigger_value_2 = aBindStaticValue()->withId(28)->build();
-
-        $this->complex_rule = aTriggerRule()
-            ->applyValue(
-                new Tracker_Workflow_Trigger_FieldValue(
-                    $this->target_field,
-                    $this->target_value
-                )
-            )
-            ->whenAllOf()
-            ->childHas(
-                new Tracker_Workflow_Trigger_FieldValue(
-                    $this->trigger_field,
-                    $this->trigger_value
-                )
-            )
-            ->childHas(
-                new Tracker_Workflow_Trigger_FieldValue(
-                    $this->trigger_field_2,
-                    $this->trigger_value_2
-                )
-            )
-            ->build();
-    }
-
-    public function itSetTheValueIfDifferentTypeSiblingHaveLegitValue() {
-        $sibling_1 = mock('Tracker_Artifact');
-        stub($sibling_1)->getId()->returns(112);
-        stub($sibling_1)->getTracker()->returns($this->task_tracker);
-        stub($sibling_1)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array($this->trigger_value))->build());
-
-        $sibling_2 = mock('Tracker_Artifact');
-        stub($sibling_2)->getId()->returns(113);
-        stub($sibling_1)->getTracker()->returns($this->bug_tracker);
-        stub($sibling_2)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array($this->trigger_value_2))->build());
-
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator(array($sibling_1, $sibling_2)));
-
-        expect($this->parent)->createNewChangeset()->once();
-
-        $this->rules_processor->process($this->artifact, $this->complex_rule);
-    }
-
-    public function itDoesntSetTheValueIfOneOfTheChildDoesntApply() {
-        $sibling_1 = mock('Tracker_Artifact');
-        stub($sibling_1)->getId()->returns(112);
-        stub($sibling_1)->getTracker()->returns($this->task_tracker);
-        stub($sibling_1)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array($this->trigger_value))->build());
-
-        $sibling_2 = mock('Tracker_Artifact');
-        stub($sibling_2)->getId()->returns(113);
-        stub($sibling_2)->getTracker()->returns($this->bug_tracker);
-        stub($sibling_2)->getValue($this->trigger_field)->returns(aChangesetValueList()->withValues(array(aBindStaticValue()->withId('whatever')->build()))->build());
-
-        $this->artifact->setSiblingsWithoutPermissionChecking(new ArrayIterator(array($sibling_1, $sibling_2)));
-
-        expect($this->parent)->createNewChangeset()->never();
-
-        $this->rules_processor->process($this->artifact, $this->complex_rule);
     }
 }
 
