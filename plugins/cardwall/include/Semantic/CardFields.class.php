f69392458084b8b9d4e23e2caf00ac7ba06400ff
Martin GOYOT <martin.goyot@enalean.com>
2013-08-27 12:44:24 +0200

    Adding admin section of Card Fields Semantic
    Change-Id: I677056c4827aaafdc967dac96eba19f9f9ba3d7a

--- plugins/cardwall/include/Semantic/CardFields.class.php
+++ plugins/cardwall/include/Semantic/CardFields.class.php
@@ -24,13 +24,16 @@
 class Cardwall_Semantic_CardFields extends Tracker_Semantic {
     const NAME = 'plugin_cardwall_card_fields';
 
-    /** @var array */
+    /** @var Tracker_FormElement_Field[] */
     private $card_fields = array();
 
     /** @var array
      * instances of this semantic
      */
     protected static $_instances;
+
+    /** @var Cardwall_Semantic_Dao_CardFieldsDao */
+    private $dao;
 
     public function __construct(Tracker $tracker) {
         parent::__construct($tracker);
@@ -73,7 +76,51 @@ class Cardwall_Semantic_CardFields extends Tracker_Semantic {
         $this->card_fields = $fields;
     }
 
-    public function displayAdmin(Tracker_SemanticManager $sm, TrackerManager $tracker_manager, Codendi_Request $request, PFUser $current_user) {
+    public function displayAdmin(Tracker_SemanticManager $semantic_manager, TrackerManager $tracker_manager, Codendi_Request $request, PFUser $current_user) {
+        $html_purifier = Codendi_HTMLPurifier::instance();
+        $semantic_manager->displaySemanticHeader($this, $tracker_manager);
+
+
+        $html = '';
+        $fields = $this->getFields();
+        if (!count($fields)) {
+            $html .= $GLOBALS['Language']->getText('plugin_cardwall','semantic_cardFields_no_fields_defined');
+        } else {
+            $html .= $GLOBALS['Language']->getText('plugin_cardwall','semantic_cardFields_fields');
+            $html .= '<blockquote>';
+            $html .= '<table>';
+            foreach($fields as $field) {
+                $html .= '<tr><td>';
+                $html .=  $html_purifier->purify($field->getLabel(), CODENDI_PURIFIER_CONVERT_HTML) ;
+                $html .= '</td><td>';
+                $html .= '<a href="'. $this->getUrl() .'&amp;remove='. (int)$field->id .'" title="' . $GLOBALS['Language']->getText('plugin_cardwall','semantic_cardFields_fields') . '">';
+                $html .= $GLOBALS['HTML']->getimage('ic/cross.png', array('alt' => $GLOBALS['Language']->getText('plugin_cardwall','semantic_cardFields_fields')));
+                $html .= '</a>';
+                $html .= '</td></tr>';
+            }
+            $html .= '</table>';
+            $html .= '</blockquote>';
+        }
+        $options = '';
+        foreach($this->tracker->getFormElements() as $formElement) {
+            $options .= $formElement->fetchAddTooltip($fields);
+        }
+        if ($options) {
+            $html .= '<form action="'. $this->getUrl() .'" method="POST">';
+            $html .= '<p>'. $GLOBALS['Language']->getText('plugin_cardwall','semantic_card_fields_add_field');
+            $html .= '<select name="field">';
+            $html .= $options;
+            $html .= '</select>';
+            $html .= '<input type="submit" name="add" value="'. $GLOBALS['Language']->getText('global', 'btn_submit') .'" />';
+            $html .= '</p>';
+            $html .= '</form>';
+        } else {
+            $html .= '<em>' . $GLOBALS['Language']->getText('plugin_cardwall','semantic_card_fields_no_more_field') . '</em>';
+        }
+
+        $html .= '<p><a href="'.TRACKER_BASE_URL.'/?tracker='. $this->tracker->getId() .'&amp;func=admin-semantic">&laquo; ' . $GLOBALS['Language']->getText('plugin_tracker_admin_semantic','go_back_overview') . '</a></p>';
+        echo $html;
+        $semantic_manager->displaySemanticFooter($this, $tracker_manager);
     }
 
     /**
@@ -116,8 +163,15 @@ class Cardwall_Semantic_CardFields extends Tracker_Semantic {
         return false;
     }
 
-    public function process(Tracker_SemanticManager $sm, TrackerManager $tracker_manager, Codendi_Request $request, PFUser $current_user) {
-
+    public function process(Tracker_SemanticManager $semantic_manager, TrackerManager $tracker_manager, Codendi_Request $request, PFUser $current_user) {
+        if ( $request->get('add') && (int) $request->get('field')) {
+            $field = Tracker_FormElementFactory::instance()->getUsedFormElementById($request->get('field'));
+            $this->getDao()->add($this->tracker->id, $field->id);
+        } else if ( (int) $request->get('remove') ) {
+            $field = Tracker_FormElementFactory::instance()->getUsedFormElementById($request->get('remove'));
+            $this->getDao()->remove($this->tracker->id, $field->id);
+        }
+        $this->displayAdmin($semantic_manager, $tracker_manager, $request, $current_user);
     }
 
     public function save() {
@@ -126,6 +180,13 @@ class Cardwall_Semantic_CardFields extends Tracker_Semantic {
             $dao->add($this->tracker->getId(), $field->getId(), 'end');
         }
         $this->card_fields = array();
+    }
+
+    private function getDao() {
+        if (! $this->dao) {
+            $this->dao = new Cardwall_Semantic_Dao_CardFieldsDao();
+        }
+        return $this->dao;
     }
 
     /**
@@ -142,13 +203,15 @@ class Cardwall_Semantic_CardFields extends Tracker_Semantic {
     }
 
     private function loadFieldsFromTracker(Tracker $tracker) {
-        $this->card_fields = $this->getDao()
-            ->searchByTrackerId($tracker->getId())
-            ->instanciateWith(array($this, 'instantiateFieldFromRow'));
-    }
+        $dao                  = $this->getDao();
+        $rows                 = $dao->searchByTrackerId($tracker->getId());
+        $form_element_factory = Tracker_FormElementFactory::instance();
+        $this->card_fields    = array();
 
-    private function getDao() {
-        return new Cardwall_Semantic_Dao_CardFieldsDao();
+        foreach ($rows as $row) {
+            $field = $form_element_factory->getFieldById($row['field_id']);
+            $this->card_fields[$field->getId()] = $field;
+        }
     }
 
     /**
