c44db7f0dbd32db949f3f27edd65de34646fc47d
Nicolas Terray <nicolas.terray@enalean.com>
2013-08-27 17:44:02 +0200

    Migrate the implicit default behavior to the database
    Change-Id: Ib152c8867962e8a3f25acbf0f2af5795a0103f0d

---
+++ plugins/cardwall/db/mysql/updates/2013/201308271726_set_default_semantic.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2013. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201308271726_set_default_semantic extends ForgeUpgrade_Bucket {
+
+    public function description() {
+        return <<<EOT
+Add the default field for the semantic card_fields
+EOT;
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $this->addCardField('remaining_effort', 0);
+        $this->addCardField('assigned_to', 1);
+        $this->addCardField('impediment', 2);
+    }
+
+    private function addCardField($name, $rank) {
+        $sql = "INSERT INTO plugin_cardwall_semantic_cardfields(tracker_id, field_id, rank)
+                SELECT tracker_id, id, $rank
+                FROM tracker_field
+                WHERE use_it = 1
+                    AND name = '$name'";
+        $this->executeSql($sql);
+    }
+
+    private function executeSql($sql) {
+        $result = $this->db->dbh->exec($sql);
+        if ($result === false) {
+            $error_message = implode(', ', $this->db->dbh->errorInfo());
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete($error_message);
+        }
+    }
+}
+?>
