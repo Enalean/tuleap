1eac832ce4ae0b886ff00070966bed12804208f9
dylan_bowden <dylan.bowden@enalean.com>
2013-09-02 16:29:37 +0200

    UI fonctionality to add/edit comment on a file
    Change-Id: I509fdb6f1e46ffbfc21e5a42d4a9001128474385

--- src/common/dao/FRSFileDao.class.php
+++ src/common/dao/FRSFileDao.class.php
@@ -199,7 +199,7 @@ class FRSFileDao extends DataAccessObject {
     function createFromArray($data_array) {
         $arg    = array();
         $values = array();
-        $cols   = array('filename', 'filepath', 'release_id', 'type_id', 'processor_id', 'file_size', 'status', 'computed_md5', 'reference_md5', 'user_id');
+        $cols   = array('filename', 'filepath', 'release_id', 'type_id', 'processor_id', 'file_size', 'status', 'computed_md5', 'reference_md5', 'user_id', 'comment');
         foreach ($data_array as $key => $value) {
             if (in_array($key, $cols)) {
                 $arg[]    = $key;
