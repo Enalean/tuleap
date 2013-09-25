cb69e4bf87f16a31daabcb2b57e34ed3f0bf1b57
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-09-25 10:30:20 +0200

    Add forgeupgrade
    * Create plugin_agiledashboard_criteria table
    
    Change-Id: Id1bddb8be7a9cb00bf3e5fdbe22079e8b7bf2639

--- plugins/agiledashboard/db/uninstall.sql
+++ plugins/agiledashboard/db/uninstall.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS plugin_agiledashboard_planning;
 DROP TABLE IF EXISTS plugin_agiledashboard_planning_backlog_tracker;
 DROP TABLE IF EXISTS plugin_agiledashboard_semantic_initial_effort;
+DROP TABLE IF EXISTS plugin_agiledashboard_criteria;
 
 DELETE FROM service WHERE short_name='plugin_agiledashboard';
