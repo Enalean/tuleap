0bc08604e35e5b5a43109734c39b770a07336e39
Manuel Vacelet <manuel.vacelet@enalean.com>
2013-10-23 11:43:58 +0200

    Procedure and tools to upgrade to gitolite3
    Change-Id: Ie19a907de74b8cbf9ec549572d0e258c3538ccee

---
+++ plugins/git/bin/gitolite3-suexec-wrapper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Suexec wrapper for gitolite-shell
+#
+
+export GIT_PROJECT_ROOT="/var/lib/codendi/gitolite/repositories"
+export GITOLITE_HTTP_HOME="/usr/com/gitolite"
+
+exec sudo -E -u gitolite /usr/share/gitolite3/gitolite-shell
