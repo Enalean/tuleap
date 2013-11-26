ce072e629a861c13cd06a4592041f20a97f7032d
Martin GOYOT <martin.goyot@enalean.com>
2013-11-26 17:26:11 +0100

    fix javascript errors on plugin git
    Change-Id: Iec9da54ccf26da91a9c817538e787fc15fe0357a

--- plugins/git/www/git.js
+++ plugins/git/www/git.js
@@ -330,9 +330,20 @@ document.observe('dom:loaded', function () {
         });
     }
 
-        $('gerrit_past_project_delete').hide();
-        $('gerrit_past_project_delete_plugin_diasabled').hide();
-        $('gerrit_url').observe('change', toggleMigrateDeleteRemote);
+    (function hideIrrevelantGerritMessages() {
+        if ($('gerrit_past_project_delete')) {
+            $('gerrit_past_project_delete').hide();
+        }
+
+        if ($('gerrit_past_project_delete_plugin_diasabled')) {
+            $('gerrit_past_project_delete_plugin_diasabled').hide();
+        }
+
+        if ($('gerrit_url')) {
+            $('gerrit_url').observe('change', toggleMigrateDeleteRemote);
+        }
+    }
+    )();
 
     function toggleMigrateDeleteRemote() {
         var should_delete  = $('gerrit_url').options[$('gerrit_url').selectedIndex].readAttribute('data-repo-delete'),
