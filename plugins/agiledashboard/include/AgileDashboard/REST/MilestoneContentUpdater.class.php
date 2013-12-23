a6e76a296d37020177f51a97d5f1e1edab32fc21
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2013-12-23 14:47:16 +0100

    Fix request #5751 : PUT /milestones/123/content does not remove anymore links from other trackers than content trackers.
    Change-Id: I011f37731c6580b9c2bad35678eb393507a04b73

--- plugins/agiledashboard/include/AgileDashboard/REST/MilestoneContentUpdater.class.php
+++ plugins/agiledashboard/include/AgileDashboard/REST/MilestoneContentUpdater.class.php
@@ -24,27 +24,33 @@ class MilestoneContentUpdater {
     /** @var Tracker_FormElementFactory */
     private $form_element_factory;
 
+    /** @var Tracker_ArtifactFactory */
+    private $artifact_factory;
+
     public function __construct(Tracker_FormElementFactory $form_element_factory) {
         $this->form_element_factory = $form_element_factory;
+        $this->artifact_factory     = Tracker_ArtifactFactory::instance();
     }
 
     /**
      * User want to update the content of a given milestone
      *
-     * @param array            $linked_artifact_ids  The ids of the artifacts to link
-     * @param PFUser           $current_user         The user who made the link
-     * @param Tracker_Artifact $artifact             The milestone
+     * @param array              $linked_artifact_ids  The ids of the artifacts to link
+     * @param PFUser             $current_user         The user who made the link
+     * @param Planning_Milestone $milestone            The milestone
      *
      */
-    public function updateMilestoneContent(array $linked_artifact_ids, PFUser $current_user, Tracker_Artifact $artifact) {
+    public function updateMilestoneContent(array $linked_artifact_ids, PFUser $current_user, Planning_Milestone $milestone) {
+        $artifact       = $milestone->getArtifact();
         $artlink_fields = $this->form_element_factory->getUsedArtifactLinkFields($artifact->getTracker());
+
         if (! count($artlink_fields)) {
             return;
         }
 
         $fields_data = $this->getFieldsDataForNewChangeset(
             $artlink_fields[0],
-            $artifact,
+            $milestone,
             $current_user,
             $linked_artifact_ids
         );
@@ -54,16 +60,18 @@ class MilestoneContentUpdater {
 
     private function getFieldsDataForNewChangeset(
         Tracker_FormElement_Field_ArtifactLink $artlink_field,
-        Tracker_Artifact $artifact,
+        Planning_Milestone $milestone,
         PFUser $current_user,
         array $linked_artifact_ids
     ) {
+        $artifact                = $milestone->getArtifact();
         $fields_data             = array();
         $elements_already_linked = $this->getMilestoneContentItemsAlreadyLinked(
             $artifact->getLinkedArtifacts($current_user)
         );
-        $unlinked_elements       = $this->unlinkedMilestoneContentItems(
-            $artifact,
+
+        $unlinked_elements = $this->getMilestoneContentItemsToUnlink(
+            $milestone,
             $elements_already_linked,
             $linked_artifact_ids
         );
@@ -123,25 +131,60 @@ class MilestoneContentUpdater {
     /**
      * Returns the list of content items which will be unlinked to the milestone
      *
-     * @param Tracker_Artifact $artifact              The milestone
-     * @param array            $unliked_artifact_ids  The ids of the artifacts to unlink
-     * @param array            $linked_artifact_ids   The ids of the artifacts which will be linked
+     * @param Planning_Milestone $milestone               The milestone
+     * @param array              $elements_already_linked The ids of the artifacts already linked to the milestone
+     * @param array              $linked_artifact_ids     The ids of the artifacts which will be linked
      *
      * @return bool true if success false otherwise
      */
-    private function unlinkedMilestoneContentItems(Tracker_Artifact $artifact, array $unliked_artifact_ids, array $linked_artifact_ids) {
+    private function getMilestoneContentItemsToUnlink(
+        Planning_Milestone $milestone,
+        array $elements_already_linked,
+        array $linked_artifact_ids
+    ) {
+        $artifact       = $milestone->getArtifact();
         $artlink_fields = $this->form_element_factory->getUsedArtifactLinkFields($artifact->getTracker());
         $removed_values = array();
 
+        $content_trackers_ids = $milestone->getPlanning()->getBacklogTrackersIds();
+
         if (count($artlink_fields)) {
-            foreach($unliked_artifact_ids as $unliked_artifact_id) {
-                if (! in_array($unliked_artifact_id, $linked_artifact_ids)) {
-                    $removed_values[$unliked_artifact_id] = 1;
+            foreach($elements_already_linked as $artifact_already_linked_id) {
+                $unlinked_artifact = $this->artifact_factory->getArtifactById($artifact_already_linked_id);
+
+                if ($this->artifactAlreadyLinkedIsNotInTheNewListSentByRESTAndIsInBacklogTracker(
+                        $linked_artifact_ids,
+                        $artifact_already_linked_id,
+                        $unlinked_artifact,
+                        $content_trackers_ids
+                    )
+                ) {
+                    $removed_values[$artifact_already_linked_id] = 1;
                 }
             }
         }
 
         return $removed_values;
+    }
+
+    private function artifactAlreadyLinkedIsNotInTheNewListSentByRESTAndIsInBacklogTracker(
+        array $linked_artifact_ids,
+        $artifact_already_linked_id,
+        $unlinked_artifact,
+        array $content_trackers_ids
+    ) {
+        return ! in_array($artifact_already_linked_id, $linked_artifact_ids)
+               && $this->artifactIsInBacklogTracker($unlinked_artifact, $content_trackers_ids);
+    }
+
+    private function artifactIsInBacklogTracker(Tracker_Artifact $artifact, $backlog_tracker_ids) {
+        foreach ($backlog_tracker_ids as $backlog_tracker_id) {
+            if ($artifact->getTrackerId() == $backlog_tracker_id) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
