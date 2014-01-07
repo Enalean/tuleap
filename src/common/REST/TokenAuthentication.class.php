743e1d7eea3c0060c579c5e4dedd2136b32ff179
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-01-07 18:19:48 +0100

    Basic CSRF protection
    It's by no mean a bullet proof protection against CSRF but it
    provides a first protection.
    Waiting for a better alternative, it's better than stay naked
    
    Change-Id: Ibf666557659295d427ffb43657d3e61b88b5fb41

--- src/common/REST/TokenAuthentication.class.php
+++ src/common/REST/TokenAuthentication.class.php
@@ -59,10 +59,46 @@ class TokenAuthentication implements iAuthenticate {
      */
     private function cookieBasedAuthentication() {
         $current_user = $this->user_manager->getCurrentUser();
-        if (! $current_user->isAnonymous()) {
+        if ($this->isCsrfSafe() && ! $current_user->isAnonymous()) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * @todo We should really check based on a csrf token but no way to get it done yet
+     * @return boolean
+     */
+    private function isCsrfSafe() {
+        if ($this->isRequestFromSelf()) {
+            return true;
+        }
+        return false;
+    }
+
+    private function isRequestFromSelf() {
+        return $this->getQueryHost() === $this->getRefererHost();
+    }
+
+    private function getQueryHost() {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $scheme = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+            return $this->getUrlBase($scheme.$_SERVER['HTTP_HOST']);
+        }
+        return $this->getUrlBase(get_server_url());
+    }
+
+    private function getRefererHost() {
+        return $this->getUrlBase($_SERVER['HTTP_REFERER']);
+    }
+
+    private function getUrlBase($url) {
+        $parsed_url = parse_url($url);
+        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+        $host   = isset($parsed_url['host'])   ? $parsed_url['host'] : '';
+        $port   = isset($parsed_url['port'])   ? ':' . $parsed_url['port'] : '';
+
+        return "$scheme$host$port";
     }
 
     private function tokenBasedAuthentication() {
