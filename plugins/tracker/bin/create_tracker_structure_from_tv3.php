4b751b86f513573ed51f97d7a55611542317df8b
Nicolas Terray <nicolas.terray@enalean.com>
2014-01-16 14:41:22 +0100

    Add cli script to create tv5 structure from a tv3
    Change-Id: I3fed9f137af32d5429d036e1f7e03b2983db4646

---
+++ plugins/tracker/bin/create_tracker_structure_from_tv3.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2014. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'pre.php';
+
+if ($argc !== 6) {
+    fwrite(STDERR, "Usage: {$argv[0]} project_id tv3_id name description itemname". PHP_EOL);
+    exit(1);
+}
+$project_id     = $argv[1];
+$tv3_tracker_id = $argv[2];
+$name           = $argv[3];
+$description    = $argv[4];
+$itemname       = $argv[5];
+
+$project = ProjectManager::instance()->getProject($project_id);
+if ($project->isError() || ! $project->isActive()) {
+    fwrite(STDERR, 'ERROR: Project does not exist or is inactive'. PHP_EOL);
+    exit(1);
+}
+$tv3 = new ArtifactType($project, $tv3_tracker_id);
+if (!$tv3 || !is_object($tv3) || $tv3->isError() ||  !$tv3->isValid() ) {
+    fwrite(STDERR, 'ERROR: Given tracker v3 does not exist or is invalid'. PHP_EOL);
+    exit(1);
+}
+
+$new_tracker = TrackerFactory::instance()->createFromTV3($tv3_tracker_id, $project, $name, $description, $itemname);
+if (! $new_tracker) {
+    fwrite(STDERR, $GLOBALS['Response']->getRawFeedback());
+    fwrite(STDERR, 'ERROR: Unable to migrate the tracker structure'. PHP_EOL);
+    exit(1);
+}
+
+echo $new_tracker->getId();
