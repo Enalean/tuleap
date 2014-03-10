4fea31df0f367274c621a43581aa65612dff56a0
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-03-10 15:34:51 +0100

    Fix request #6290: invert order in follow ups comments is not saved as preferences
    Use 'tracker' as it's the standard way to route toward a tracker.
    So we leverage on Tracker_URL to check that users are allowed to see the tracker
    they want to put preferences on.
    
    Change-Id: Ic87c7572bf5c3648183a7caa7e5ed34f1043eb69

--- plugins/tracker/www/invert_comments_order.php
+++ plugins/tracker/www/invert_comments_order.php
@@ -1,13 +1,13 @@
 <?php
 require_once('pre.php');
 
-$valid = new Valid_UInt('tracker_id');
+$valid = new Valid_UInt('tracker');
 $valid->required();
 if($request->valid($valid)) {
-    if (user_get_preference('tracker_comment_invertorder_'.$request->get('tracker_id'))) {
-        user_del_preference('tracker_comment_invertorder_'.$request->get('tracker_id'));
+    if (user_get_preference('tracker_comment_invertorder_'.$request->get('tracker'))) {
+        user_del_preference('tracker_comment_invertorder_'.$request->get('tracker'));
     } else {
-        user_set_preference('tracker_comment_invertorder_'.$request->get('tracker_id'), '1');
+        user_set_preference('tracker_comment_invertorder_'.$request->get('tracker'), '1');
     }
 }
 
