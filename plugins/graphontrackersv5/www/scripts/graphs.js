bf4362f39b300945232ed6cf3efc5429ffc01208
Hugo <hugo.kelfani@enalean.com>
2014-04-22 17:49:18 +0200

    task #6722 : Better pie chart rendering
    Change-Id: Ie90a2fb9f6815919345d67673583b6f02f2edd4c

--- plugins/graphontrackersv5/www/scripts/graphs.js
+++ plugins/graphontrackersv5/www/scripts/graphs.js
@@ -23,12 +23,13 @@ tuleap.graphontrackersv5.graphs = tuleap.graphontrackersv5.graphs || { };
 
 // Inspired from http://bl.ocks.org/mbostock/3887193
 tuleap.graphontrackersv5.drawDonut = function (id, graph) {
-    var width        = graph.width,
-        height       = graph.height,
-        radius       = Math.min(width, height) / 2,
-        data         = [],
-        total_values = 0,
-        color        = d3.scale.category20();
+    var width             = graph.width,
+        height            = graph.height,
+        radius            = Math.min(width, height) / 2,
+        inner_radius_coef = width/550,
+        data              = [],
+        total_values      = 0,
+        color             = d3.scale.category20c();
 
     for (var i = 0; i < graph.data.length; ++i) {
         total_values += parseFloat(graph.data[i]);
@@ -39,12 +40,16 @@ tuleap.graphontrackersv5.drawDonut = function (id, graph) {
         if (c === null) {
             c = color(i);
         }
-        data.push({
+
+        var line = {
             "label": graph.legend[i],
             "value": graph.data[i],
-            "percentage": ((graph.data[i] / total_values) * 100).toFixed(1),
+            "percentage": ((graph.data[i] / total_values) * 100).toFixed(0),
             "color": c
-        });
+        };
+
+        data.push(line);
+        graph.colors[i] = c;
     }
 
     var pie = d3.layout.pie()
@@ -52,32 +57,163 @@ tuleap.graphontrackersv5.drawDonut = function (id, graph) {
         .sort(null);
 
     var arc = d3.svg.arc()
-        .innerRadius(radius - 125)
+        .innerRadius((radius - 50) / 2)
         .outerRadius(radius - 50);
 
     var svg = d3.selectAll(".plugin_graphontrackersv5_chart[data-graph-id="+id+']').append("svg")
         .attr("width", width)
-        .attr("height", height)
-      .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+        .attr("height", height);
 
-    var g = svg.selectAll(".arc")
+    var grads = svg.append("defs").selectAll("linearGradient")
+            .data(pie(data))
+        .enter()
+            .append("linearGradient")
+            .attr("x1", 0)
+            .attr("y1", 0)
+            .attr("x2", 0)
+            .attr("y2", 1)
+            .attr("id", function(d, i) { return getGradientId(i); });
+    grads.append("stop").attr("offset", "0%").style("stop-color", function(d, i) { return d3.rgb(d.data.color).brighter(0.5); });
+    grads.append("stop").attr("offset", "100%").style("stop-color", function(d, i) { return d.data.color; });
+
+    var chart = svg.append("g")
+        .attr("transform", "translate(" + width / 3 + "," + height / 2 + ")");
+
+    var g = chart.selectAll(".arc")
           .data(pie(data))
         .enter().append("g")
           .attr("class", "arc");
 
-      g.append("path")
-          .attr("d", arc)
-          .style("fill", function(d) { return d.data.color; });
+    g.append("line")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", -radius+50)
+        .attr("y2", function(d, i) {
+            if(i % 2 === 0) {
+              return -radius+40;
+            } else {
+              return -radius+25;
+            }
+        })
+        .attr("stroke", "#DDD")
+        .attr("transform", function(d) {
+            radius;
+          return "rotate(" + (d.startAngle+d.endAngle)/2 * (180/Math.PI) + ")";
+        });
+
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d, i) { return "url(#" + getGradientId(i) + ")"; })
+        .attr("class", function (d, i) {
+            return getDonutSliceClass(i);
+        })
+        .on("mouseover", function(d, i) {
+            svg.select("." + getLegendClass(i)).style("font-weight", "bold");
+        })
+        .on("mouseout", function(d, i) {
+            svg.select("." + getLegendClass(i)).style("font-weight", "normal");
+        })
+        .transition()
+            .duration(750)
+            .attrTween('d', function (b) {
+                var i = d3.interpolate(
+                {
+                    startAngle: 0,
+                    endAngle: 0
+                }, b);
+
+                return function(t) { return arc(i(t)); };
+            });
 
       g.append("text")
           .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("transform", function(d, i) {
+             var dist;
+             if(i % 2 === 0) {
+               dist = radius - 34;
+             } else {
+               dist = radius - 19;
+             }
+             var angle  = (d.startAngle + d.endAngle) / 2, // Middle of wedge
+                 x      = dist * Math.sin(angle),
+                 y      = -dist * Math.cos(angle);
+
+             return "translate(" + x + "," + y + ")";
+           })
           .attr("dy", ".35em")
-          .style("text-anchor", "middle")
-          .text(function(d) { return d.data.label+' ('+d.data.percentage+'%)'; });
+          .style("text-anchor", function (d) {
+              var angle  = (d.startAngle + d.endAngle) / 2;
+
+              if (angle > Math.PI) {
+                  return "end";
+              }
+              return "start";
+           })
+          .text(function(d) { return d.data.percentage+'%'; });
+
+    var legend_x = 2 * width / 3 + 20;
+    var legend_y = Math.max(0, height / 2 - 20 / 2 * data.length);
+    var legend_group = svg.append("g")
+        .attr("transform", "translate(" + legend_x + ", " + legend_y + ")");
+
+    var legend = legend_group.selectAll(".legend")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0, " + i * 20 + ")"; })
+        .on("mouseover", function(d, i) {
+            svg.select("." + getDonutSliceClass(i))
+                .transition()
+                .attr("transform", "scale(1.05)")
+        })
+        .on("mouseout", function(d, i) {
+            svg.select("." + getDonutSliceClass(i))
+                .transition()
+                .attr("transform", "scale(1)");
+        });
+
+    var colors_range = d3.scale.ordinal().range(graph.colors);
+
+    legend.append("rect")
+        .attr("x", 0)
+        .attr("rx", 3)
+        .attr("ry", 3)
+        .attr("width", 16)
+        .attr("height", 16)
+        .style("fill", function (d) { return d.color; });
+
+    legend.append("text")
+        .attr("class", function (d, i) { return getLegendClass(i); })
+        .attr("x", 22)
+        .attr("y", 8)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d) {
+            var legend = d.label,
+                length = legend.length;
+
+            if (length > 25) {
+                return legend.substr(0, 15) + '…' + legend.substr(length - 10, length);
+            }
+            return legend;
+        });
+
+
+    function getGradientId(value_index) {
+        return 'grad_' + id + '_' + value_index;
+    }
+
+    function getLegendClass(value_index) {
+        return 'legend_' + id + '_' + value_index;
+    }
+
+    function getDonutSliceClass(value_index) {
+        return 'slice_' + id + '_' + value_index;
+    }
+
 };
 
-! function ($) {
+!function ($) {
     $(document).ready(function () {
         $.each(tuleap.graphontrackersv5.graphs, function (id, graph) {
             if (graph.type === 'pie') {
@@ -85,4 +221,4 @@ tuleap.graphontrackersv5.drawDonut = function (id, graph) {
             }
         });
     });
-} (jQuery);
+}(window.jQuery);
