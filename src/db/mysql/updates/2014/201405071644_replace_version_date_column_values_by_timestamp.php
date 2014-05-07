cf5051c07797295147cbdf4eadb1412270be3616
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2014-05-07 17:02:03 +0200

    Forgeupgrade for create_date
    Use right timestamp format
    
    Change-Id: Iee57bd627ea69a4a145498b75c443539853feab8

---
+++ src/db/mysql/updates/2014/201405071644_replace_version_date_column_values_by_timestamp.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright (c) Enalean SAS 2014. All rights reserved
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Replace version date column values by timestamp
+ */
+class b201405071644_replace_version_date_column_values_by_timestamp extends ForgeUpgrade_Bucket {
+    public function description() {
+        return "Replace version date column values by timestamp";
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $sql = "UPDATE svn_accessfile_history
+                SET version_date = UNIX_TIMESTAMP()";
+
+        $res = $this->db->dbh->exec($sql);
+
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occured while replacing version date column values by timestamp.');
+        }
+    }
+}
