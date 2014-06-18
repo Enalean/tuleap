ce1b654c1f6ea126afa8b1a2ed5370729efeb9fd
Nicolas Terray <nicolas.terray@enalean.com>
2014-06-18 16:07:22 +0200

    scrollable backlog and submilestones
    Change-Id: I1b319520ec02672ebe19a160a29c321eef64a469

--- plugins/agiledashboard/www/angular/src/app/app.js
+++ plugins/agiledashboard/www/angular/src/app/app.js
@@ -4,4 +4,13 @@ angular.module('tuleap.planningApp', [
     controllers.name,
     services.name,
     'tuleap.planning.submilestone'
-]);
+]).directive('scrollable', function() {
+    return {
+        link: function (scope, element, attrs) {
+            element.jScrollPane({
+                autoReinitialise: true,
+                verticalGutter: 0
+            });
+        }
+    };
+});
