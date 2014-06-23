c0c3b94b8507577e0596927c532c9ca8a25d0b36
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-06-23 16:30:02 +0200

    Fix request #7104 Subversion access control history doesnt manage uppercase repo
    Change-Id: Ic31e86e49767885f67f00cd69e2c403e7705db96

---
+++ src/common/svn/SVN_AccessFile_NoFilePresenter.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2014. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+class SVN_AccessFile_NoFilePresenter {
+
+    public function access_form_title() {
+        return $GLOBALS['Language']->getText('svn_admin_access_control','access_ctrl');
+    }
+
+    public function repo_not_created_label() {
+        return $GLOBALS['Language']->getText('svn_admin_access_control','not_created');
+    }
+}
