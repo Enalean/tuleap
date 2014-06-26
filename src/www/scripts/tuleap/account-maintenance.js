4432a1aead47503d01965a7a345a3338e4d256c2
Hugo <hugo.kelfani@enalean.com>
2014-06-26 17:51:34 +0200

    tasks #7123: Select theme variant in user preferences
    This is a task from story #6003: be able to change the Experimental
    theme color:
    
    * User can choose her variant in user preference page
    * Variant is stored in User Preferences
    * Variant is previsualized on the fly if user is currently using the theme
    
    Change-Id: I25337bdaeba5f898c051ceda7f7554fcb624de2e

--- src/www/scripts/tuleap/account-maintenance.js
+++ src/www/scripts/tuleap/account-maintenance.js
@@ -19,6 +19,9 @@
 
 !(function ($) {
 
+    var themes_list,
+        popover_is_displayed = false;
+
     $(document).ready(function(){
         var $delete_button = $('#button-delete-keys');
         var $checkboxs     = $('input[type="checkbox"][name="ssh_key_selected[]"]');
@@ -32,6 +35,8 @@
 
         $('[data-ssh_key_value]').one('click', displayFullSSHKey);
         $(window).resize(updateHeightValue);
+
+        initThemeVariantSelection();
     });
 
     function displayFullSSHKey() {
@@ -63,5 +68,108 @@
 
         $delete_button.removeAttr('disabled');
     };
+
+    function initThemeVariantSelection() {
+        if ($('.select-user-preferences[name="user_theme"]').length > 0) {
+            bindThemeSelect();
+            fetchThemeVariants();
+            $('.navbar-inner').attr('data-content',codendi.locales.account.theme_variant_preview);
+            $('.navbar-inner').popover({
+                placement: 'bottom',
+                trigger: 'manual'
+            });
+        }
+    }
+
+    function bindThemeSelect() {
+        var theme_selector = $('.select-user-preferences[name="user_theme"]');
+
+        theme_selector.change(function() {
+            fetchThemeVariants();
+            $('.navbar-inner').popover('hide');
+            popover_is_displayed = false;
+        });
+    }
+
+    function fetchThemeVariants() {
+        var theme_selector = $('.select-user-preferences[name="user_theme"]');
+
+        $.ajax({
+            url: '/account/get_available_theme_variants.php',
+            data: {
+                theme: theme_selector.val()
+            },
+            cache: false,
+            dataType: 'json',
+            success: listThemeVariantsIfExist,
+            error: function() {
+                listThemeVariantsIfExist([]);
+            }
+        });
+    }
+
+    function listThemeVariantsIfExist(themes) {
+        themes_list = themes.values;
+        var i,
+            selected_theme_variant       = themes.selected,
+            themes_length        = themes_list.length,
+            theme_variant_group = $('#theme_variant_group'),
+            theme_variant_list  = $('#theme_variant_list'),
+            theme_picker,
+            theme_picker_container;
+
+        theme_variant_list.empty();
+        theme_variant_group.css('display', 'none');
+
+        if (themes_length > 0) {
+            theme_variant_group.css('display', 'block');
+            for (i = 0; i < themes_length; ++i) {
+                theme_picker_container = $('<span></span>')
+                    .addClass('theme_picker_container')
+                    .val(themes_list[i])
+                    .click(selectThemeVariant);
+
+                if (themes_list[i] === selected_theme_variant) {
+                    theme_picker_container.addClass('checked');
+                    $('#current_theme_variant').val(themes_list[i]);
+                    applyThemeVariantToBody(themes_list[i]);
+                }
+
+                theme_picker = $('<span></span>')
+                    .addClass('theme_variant')
+                    .addClass(themes_list[i]);
+
+                theme_variant_list.append(
+                    theme_picker_container.append(
+                        theme_picker
+                    )
+                );
+            }
+        }
+    }
+
+    function selectThemeVariant() {
+        var current_theme_variant = $('#current_theme_variant'),
+            theme_variant_list    = $('#theme_variant_list');
+
+        theme_variant_list.children().each(function(i, element) {
+            $(element).removeClass('checked');
+        });
+        $(this).addClass('checked');
+        current_theme_variant.val(this.value);
+
+        applyThemeVariantToBody(this.value);
+        if (!popover_is_displayed) {
+            $('.navbar-inner').popover('show');
+            popover_is_displayed = true;
+        }
+    }
+
+    function applyThemeVariantToBody(theme_variant) {
+        for (var i = 0, themes_length = themes_list.length ; i < themes_length; ++i) {
+            $(document.body).removeClass(themes_list[i]);
+        }
+        $(document.body).addClass(theme_variant);
+    }
 
 })(window.jQuery);
