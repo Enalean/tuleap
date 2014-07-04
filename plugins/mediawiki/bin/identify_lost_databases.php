9c6e68598810414750d8a6925728ba34b58c0530
dylan_bowden <dylan.bowden@enalean.com>
2014-07-04 11:24:43 +0200

    bugtasks #7175: script to identify broken mw in projects
    Change-Id: Id9b0cece13f656cd0b8822812a5c377025ae27e2

---
+++ plugins/mediawiki/bin/identify_lost_databases.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright (c) Enalean, 2012. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
+
+/**
+ * Execute with php launcher:
+ *
+ * sh /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/plugins/mediawiki/bin/identify_lost_databases.php
+ */
+
+require_once 'pre.php';
+$bad_projects_with_mw_sql = "SELECT g.group_id, g.group_name
+    FROM groups g, group_plugin gp, plugins p
+    WHERE g.group_id = gp.group_id
+    AND gp.plugin_id = p.plugin_id
+    AND p.plugin_name = 'mediawiki'
+    AND g.group_id NOT IN (
+        SELECT project_id FROM plugin_mediawiki_database
+    )";
+
+
+$projects = mysql_query($bad_projects_with_mw_sql);
+if (! $projects) {
+    echo mysql_error();
+}
+
+while ($row = mysql_fetch_row($projects)) {
+    $id   = $row[0];
+    $name = $row[1];
+
+    echo "Unable to find mediawiki for project $name ($id)" . PHP_EOL;
+}
+?>
