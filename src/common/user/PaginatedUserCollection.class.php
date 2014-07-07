2c49c09066629df1f911d492c25e1c65eff67893
Nicolas Terray <nicolas.terray@enalean.com>
2014-07-07 14:23:20 +0200

    Add REST route to be able to search for users
    This reuses getPaginatedUsersByUsernameOrRealname introduced for global
    search.
    
    Some cleanup: logical order for parameters, dead code removal.
    
    Change-Id: Ief60c87c1d50f909a1b46187a554ce0abee389c8

---
+++ src/common/user/PaginatedUserCollection.class.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2014. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class PaginatedUserCollection implements Countable {
+
+    /** @var PFUser[] */
+    private $users;
+
+    /** @var int */
+    private $total_count;
+
+    public function __construct(array $users, $total_count) {
+        $this->users       = $users;
+        $this->total_count = $total_count;
+    }
+
+    /**
+     * @return PFUsers[]
+     */
+    public function getUsers() {
+        return $this->users;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalCount() {
+        return $this->total_count;
+    }
+
+    /**
+     * @see Countable
+     *
+     * @return int
+     */
+    public function count() {
+        return count($this->users);
+    }
+}
