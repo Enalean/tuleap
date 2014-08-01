Enalean/tuleap

commit cc1aa3af1bbfbc1623a2a7fec858e1d4847df34c

request #7291 Private project results with FTS throw an uncatched exception
Change-Id: Ibce3504c2d4ad9b2586c185d4f84d6eef0eae791

plugins/fulltextsearch/tests/ElasticSearch/ResultFactoryTest.php
@@ -28,9 +28,25 @@ class ResultFactoryTest extends TuleapTestCase {
     /** @var array */
     private $serach_data;
 
+    /** @var URLVerification */
+    private $url_verification;
+
+    /** @var ProjectManager */
+    private $project_manager;
+
     public function setUp() {
         parent::setUp();
-        $this->result_factory = new ElasticSearch_1_2_ResultFactory(mock('ProjectManager'));
+
+        $user_manager = stub('UserManager')->getCurrentUser()->returns(mock('PFUser'));
+        UserManager::setInstance($user_manager);
+
+        $this->project_manager  = mock('ProjectManager');
+        $this->url_verification = mock('URLVerification');
+
+        $this->result_factory = new ElasticSearch_1_2_ResultFactory(
+            $this->project_manager,
+            $this->url_verification
+        );
 
         $this->serach_data = array(
             'took' => 2,
@@ -108,5 +124,22 @@ class ResultFactoryTest extends TuleapTestCase {
 
     public function itReturns0IfNoTimeInSearchData() {
         $this->assertEqual($this->result_factory->getQueryTime(array()), 0);
+    }
+
+    public function itSkipsResultIfUserCannotAccessProject() {
+        $query_result = json_decode(
+            file_get_contents(dirname(__FILE__) . '/../_fixtures/ES_query_result_01.txt'),
+            true
+        );
+
+        $project_01 = mock('Project');
+        $project_02 = mock('Project');
+
+        stub($this->project_manager)->getProject(116)->returns($project_01);
+        stub($this->project_manager)->getProject(168)->returns($project_02);
+        stub($this->url_verification)->userCanAccessProject('*', $project_01)->returns(true);
+        stub($this->url_verification)->userCanAccessProject('*', $project_02)->throws(new Project_AccessPrivateException());
+
+        $this->assertCount($this->result_factory->getSearchResults($query_result), 2);
     }
 }

plugins/fulltextsearch/include/ElasticSearch/ClientFactory.class.php
@@ -59,7 +59,10 @@ class ElasticSearch_ClientFactory {
             $client,
             $index,
             $project_manager,
-            new ElasticSearch_1_2_ResultFactory($project_manager)
+            new ElasticSearch_1_2_ResultFactory(
+                $project_manager,
+                new URLVerification()
+            )
         );
     }
 
@@ -83,7 +86,10 @@ class ElasticSearch_ClientFactory {
             $client,
             $index,
             $project_manager,
-            new ElasticSearch_1_2_ResultFactory($project_manager)
+            new ElasticSearch_1_2_ResultFactory(
+                $project_manager,
+                new URLVerification()
+            )
         );
     }
 

plugins/fulltextsearch/include/ElasticSearch/ResultFactory.class.php
@@ -29,8 +29,15 @@ class ElasticSearch_1_2_ResultFactory {
     /** @var ProjectManager */
     private $project_manager;
 
-    public function __construct(ProjectManager $project_manager) {
-        $this->project_manager = $project_manager;
+    /** @var URLVerification */
+    private $url_verification;
+
+    public function __construct(
+        ProjectManager $project_manager,
+        URLVerification $url_verification
+    ) {
+        $this->project_manager  = $project_manager;
+        $this->url_verification = $url_verification;
     }
 
     public function getChangesetIds(array $data) {
@@ -84,11 +91,16 @@ class ElasticSearch_1_2_ResultFactory {
         $user         = $user_manager->getCurrentUser();
 
         foreach ($result['hits']['hits'] as $hit) {
-            $project          = $this->project_manager->getProject($this->extractGroupIdFromHit($hit));
-            $index            = $this->extractIndexFromHit($hit);
-            $url_verification = new URLVerification();
+            $project = $this->project_manager->getProject($this->extractGroupIdFromHit($hit));
+            $index   = $this->extractIndexFromHit($hit);
 
-            if ($project->isError() || ! $url_verification->userCanAccessProject($user, $project)) {
+            if ($project->isError()) {
+                continue;
+            }
+
+            try{
+                $this->url_verification->userCanAccessProject($user, $project);
+            } catch (Project_AccessPrivateException $exception) {
                 continue;
             }
 

plugins/fulltextsearch/tests/ElasticSearch/SearchClientFacadeTest.php
@@ -28,7 +28,10 @@ class ElasticSearch_SearchClientFacadeTest extends TuleapTestCase {
         $this->user                = mock('PFUser');
         $project_manager           = mock('ProjectManager');
         $this->elasticsearchclient = mock('ElasticSearchClient');
-        $result_factory            = new ElasticSearch_1_2_ResultFactory($project_manager);
+        $result_factory            = new ElasticSearch_1_2_ResultFactory(
+            $project_manager,
+            new URLVerification()
+        );
 
         $this->client = new ElasticSearch_SearchClientFacade(
             $this->elasticsearchclient,

commit 25031525a8323f24fa74dee28f99f6eda2709233

Fix regression on test suite introduced by commit cc1aa3af1
When setUp, tearDown...

Change-Id: Iddab5cf2d022112cce938662ca93cbbfdcc3ebc3

src/common/permission/PermissionsManager.class.php
@@ -73,7 +73,7 @@ class PermissionsManager {
     * @param  array   $ugroups         The user's ugroups
     * @return boolean 
     */
-    public function userHasPermission($object_id, $permission_type, $ugroups) {
+    public function userHasPermission($object_id, $permission_type, array $ugroups) {
         if (!isset($this->_permissions[$object_id])) {
             $this->_permissions[$object_id] = array();
         }

plugins/fulltextsearch/tests/ElasticSearch/ResultFactoryTest.php
@@ -90,6 +90,11 @@ class ResultFactoryTest extends TuleapTestCase {
         );
     }
 
+    public function tearDown() {
+        parent::tearDown();
+        UserManager::clearInstance();
+    }
+
     public function itExtractChangesetIdsPerHits() {
         $expected_result = array(
             22 => 3923,