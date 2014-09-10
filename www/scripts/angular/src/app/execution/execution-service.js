b607b95dd1a311524a13c228b81b0dd790885222
Nicolas Terray <nicolas.terray@enalean.com>
2014-09-10 17:19:26 +0200

    task #12427: Fix issue when new line is inserted in comments field
    Change-Id: Iba5d718b8d6750663de61111c296e3a8f5c8d3b1

--- www/scripts/angular/src/app/execution/execution-service.js
+++ www/scripts/angular/src/app/execution/execution-service.js
@@ -44,7 +44,8 @@ function ExecutionService(Restangular, $q) {
         // the nested resources in restangular use nested uri. This means that
         // execution.put() will call /campaigns/:id/executions/:id instead of
         // /executions/:id
-        return rest.oneUrl('testing_executions', baseurl + '/' + execution.uri)
-            .put(execution.plain());
+        return rest
+            .restangularizeElement(null, execution, 'testing_executions')
+            .put();
     }
 }
