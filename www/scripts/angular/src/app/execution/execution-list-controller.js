0ec563173a1c74a3877e0e94af99e5dbd1b10ead
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2014-10-08 13:44:40 +0200

    bugs #13225 Hide categories in list execution when they have no test
    Change-Id: I2047714153ac9c17597250580ffe4a22f08a0f89

--- www/scripts/angular/src/app/execution/execution-list-controller.js
+++ www/scripts/angular/src/app/execution/execution-list-controller.js
@@ -2,9 +2,9 @@ angular
     .module('execution')
     .controller('ExecutionListCtrl', ExecutionListCtrl);
 
-ExecutionListCtrl.$inject = ['$scope', '$state', 'ExecutionService', 'CampaignService', 'SocketService'];
+ExecutionListCtrl.$inject = ['$scope', '$state', '$filter', 'ExecutionService', 'CampaignService', 'SocketService'];
 
-function ExecutionListCtrl($scope, $state, ExecutionService, CampaignService, SocketService) {
+function ExecutionListCtrl($scope, $state, $filter, ExecutionService, CampaignService, SocketService) {
     var campaign_id = $state.params.id;
 
     ExecutionService.loadExecutions(campaign_id);
@@ -23,6 +23,7 @@ function ExecutionListCtrl($scope, $state, ExecutionService, CampaignService, So
         blocked: false,
         notrun:  false
     };
+    $scope.canCategoryBeDisplayed = canCategoryBeDisplayed;
 
     getAssignees(campaign_id, 50, 0);
     getEnvironments(campaign_id, 50, 0);
@@ -52,5 +53,15 @@ function ExecutionListCtrl($scope, $state, ExecutionService, CampaignService, So
 
     function loading() {
         return ExecutionService.loading[campaign_id] === true;
+    }
+
+    function canCategoryBeDisplayed(category) {
+        return $filter('ExecutionListFilter')(
+            category.executions,
+            $scope.search,
+            $scope.status,
+            $scope.selected_assignee,
+            $scope.selected_environment
+        ).length > 0;
     }
 }
