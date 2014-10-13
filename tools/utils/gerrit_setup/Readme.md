45ac40afebaae4f7fb4c98230ee2e908dd4e24d1
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-10-13 09:12:23 +0200

    Automatic setup of gerrit
    Change-Id: I076249517f02cac9399a7d29fd56419e437f6930

---
+++ tools/utils/gerrit_setup/Readme.md
@@ -0,0 +1,53 @@
+Create a gerrit admin account
+-----------------------------
+
+On Tuleap:
+* As site admin create account for gerrit: gerrit-admin-28 with email address (eg. gerrit-admin-28@example.com)
+
+Setup gerrit admin account
+--------------------------
+you@workstation $> make start-gerrit
+
+* Go on gerrit web interface http://tuleap_gerrit_1.gerrit-tuleap.docker:8080
+* Sign-in with  gerrit-admin-28
+* Generate an HTTP password (Settings > HTTP password)
+  -> be careful, generate a password with only with alphanum [A-z0-9]+
+
+Setup gerrit
+------------
+
+you@workstation $> docker exec -ti tuleap_web_1 bash
+root@tuleap_web_1 $> su - codendiadm
+codendiadm@tuleap_web_1 $> cd /usr/share/tuleap/tools/utils/gerrit_setup
+codendiadm@tuleap_web_1 $> ./setup_gerrit.sh --password=<generated password in gerrit interface> --useremail=<gerrit-admin-28@example.com>
+# password is the HTTP password you generated on gerrit
+
+-> you might need to install php-guzzle: yum -y install php-guzzle
+
+Create gerrit reference on Tuleap
+---------------------------------
+
+Go on Tuleap as site admin: Admin > Git > Gerrit
+
+And create a new server with the SSH key you get with:
+
+* Host: tuleap_gerrit_1.gerrit-tuleap.docker
+* HTTP port: 8080
+* ssh port: 29418
+* login: gerrit-admin-28
+* Identity file: /home/codendiadm/.ssh/id_rsa-gerrit28
+* Replication ssh key
+  you@workstation $> docker run -ti --rm --volumes-from tuleap_gerrit_data busybox cat /data/.ssh/id_rsa.pub
+* Use ssl: no
+* Version: 2.8+
+* HTTP password: the one generated in interface at step 2
+
+Initialize replication
+----------------------
+
+you@workstation $> make start-gerrit
+
+Pitfalls
+--------
+
+As of today, replication is broken
---
+++ tools/utils/gerrit_setup/setup_gerrit.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -ex
+
+USER="gerrit-admin-28"
+
+options=$(getopt -o h -l user:,useremail:,password: -- "$@")
+
+eval set -- "$options"
+while true
+do
+    case "$1" in
+    --user)
+        USER=$2;
+        shift 2;;
+    --useremail)
+        USEREMAIL=$2;
+        shift 2;;
+    --password)
+        PASSWORD=$2;
+        shift 2;;
+    --)
+        shift 1; break ;;
+    *)
+        break ;;
+    esac
+done
+
+if [ -z "$USER" ]; then
+    echo "You must specify --user argument";
+    exit 1;
+fi
+
+if [ -z "$USEREMAIL" ] || [ -z "$PASSWORD" ]; then
+    echo "You must specify --useremail and --password arguments";
+    exit 1;
+fi
+
+# Should be good by default
+GERRIT_SERVER="tuleap_gerrit_1.gerrit-tuleap.docker"
+TULEAP_SERVER="tuleap_web_1.tuleap-aio-dev.docker"
+GERRIT_PORT=8080
+
+# Hardly need to modify
+SSH_KEY=/home/codendiadm/.ssh/id_rsa-gerrit28
+GIT=/opt/rh/git19/root/usr/bin/git
+
+setup_gerrit_permissions() {
+    /bin/rm -rf /tmp/gerrit_all_projects
+    $GIT clone http://$GERRIT_SERVER:$GERRIT_PORT/All-Projects /tmp/gerrit_all_projects
+    $GIT -C /tmp/gerrit_all_projects config user.email "$USEREMAIL"
+    $GIT -C /tmp/gerrit_all_projects config user.name "Tuleap gerrit admin"
+    cp project.config.tuleap /tmp/gerrit_all_projects/project.config
+    $GIT -C /tmp/gerrit_all_projects add project.config
+    $GIT -C /tmp/gerrit_all_projects commit -m "Add access control to operate with Tuleap"
+    $GIT -C /tmp/gerrit_all_projects push http://$USER:$PASSWORD@$GERRIT_SERVER:$GERRIT_PORT/All-Projects HEAD:refs/meta/config
+}
+
+setup_user_and_groups() {
+    ssh-keygen -P "" -f $SSH_KEY
+
+    cat $SSH_KEY.pub | curl -XPOST --digest -u $USER:$PASSWORD -H 'Content-type: text/plain' $GERRIT_SERVER:$GERRIT_PORT/a/accounts/self/sshkeys -d @-
+
+    curl --digest --user $USER:$PASSWORD -X PUT $GERRIT_SERVER:$GERRIT_PORT/a/accounts/self/emails/codendiadm@$TULEAP_SERVER -H "Content-Type: application/json;charset=UTF-8" -d'{"no_confirmation": true}'
+
+    curl -H "Content-Type: application/json;charset=UTF-8" --digest --user $USER:$PASSWORD -X PUT $GERRIT_SERVER:$GERRIT_PORT/a/groups/$TULEAP_SERVER-replication -d '{"visible_to_all": true}'
+
+    ssh -i $SSH_KEY -oStrictHostKeyChecking=no -p 29418 $USER@$GERRIT_SERVER gerrit version
+}
+
+setup_gerrit_permissions
+setup_user_and_groups
