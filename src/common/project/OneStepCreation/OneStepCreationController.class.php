ffa950a79d5739662c46ad497556ef3ed315c113
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-11-05 11:57:05 +0100

    Fix request #7601 Security issue with legacy project creation page and IM plugin
    - Remove the legacy project creation
    - Slightly rewrite the "end of creation"
    
    Change-Id: Ic26b345f26a02bc725fd577ba8290ae14ddbb5bf

--- src/common/project/OneStepCreation/OneStepCreationController.class.php
+++ src/common/project/OneStepCreation/OneStepCreationController.class.php
@@ -29,6 +29,11 @@ require_once 'common/project/CustomDescription/CustomDescriptionPresenter.class.
  */
 class Project_OneStepCreation_OneStepCreationController extends MVC2_Controller {
 
+    /**
+     * @var ProjectManager
+     */
+    private $project_manager;
+
     /** @var Project_OneStepCreation_OneStepCreationRequest */
     private $creation_request;
 
@@ -44,6 +49,7 @@ class Project_OneStepCreation_OneStepCreationController extends MVC2_Controller 
         Project_CustomDescription_CustomDescriptionFactory $custom_description_factory
     ) {
         parent::__construct('project', $request);
+        $this->project_manager              = $project_manager;
         $this->required_custom_descriptions = $custom_description_factory->getRequiredCustomDescriptions();
 
         $this->creation_request = new Project_OneStepCreation_OneStepCreationRequest($request, $project_manager);
@@ -70,14 +76,79 @@ class Project_OneStepCreation_OneStepCreationController extends MVC2_Controller 
      * Create the project if request is valid
      */
     public function create() {
+        $this->validate();
+        $project = $this->doCreate();
+        $this->notifySiteAdmin($project);
+        $this->postCreate($project);
+    }
+
+    private function validate() {
         $validator = new Project_OneStepCreation_OneStepCreationValidator($this->creation_request, $this->required_custom_descriptions);
         if (! $validator->validateAndGenerateErrors()) {
             $this->index();
         }
+    }
 
+    private function doCreate() {
         $data = $this->creation_request->getProjectValues();
         require_once 'www/project/create_project.php';
-        create_project($data);
+        $group_id = create_project($data);
+        return $this->project_manager->getProject($group_id);
+    }
+
+    private function notifySiteAdmin(Project $project) {
+        $mail = new Mail();
+        $mail->setTo(Config::get('sys_email_admin'));
+        $mail->setFrom(Config::get('sys_noreply'));
+        $mail->setSubject($GLOBALS['Language']->getText('register_project_one_step', 'complete_mail_subject', array($project->getPublicName())));
+        if ($this->projectsMustBeApprovedByAdmin()) {
+            $mail->setBody(
+                $GLOBALS['Language']->getText(
+                    'register_project_one_step',
+                    'complete_mail_body_approve',
+                    array(
+                        Config::get('sys_name'),
+                        $project->getPublicName(),
+                        get_server_url().'/admin/approve-pending.php'
+                    )
+                )
+            );
+        } else {
+            $mail->setBody(
+                $GLOBALS['Language']->getText(
+                    'register_project_one_step',
+                    'complete_mail_body_auto',
+                    array(
+                        Config::get('sys_name'),
+                        $project->getPublicName(),
+                        get_server_url().'/admin/groupedit.php?group_id='.$project->getID()
+                    )
+                )
+            );
+        }
+
+        if (! $mail->send()) {
+            $GLOBALS['Response']->addFeedback(Feedback::WARN, $GLOBALS['Language']->getText('global', 'mail_failed', array($GLOBALS['sys_email_admin'])));
+        }
+    }
+
+    private function postCreate(Project $project) {
+        if ($this->projectsMustBeApprovedByAdmin()) {
+            $this->renderWait();
+        } else {
+            $GLOBALS['Response']->addFeedback(Feedback::INFO, $GLOBALS['Language']->getText('register_confirmation', 'registration_approved'));
+            $GLOBALS['Response']->redirect('/project/admin/?group_id='.$project->getID());
+        }
+    }
+
+    private function renderWait() {
+        $GLOBALS['HTML']->header(array('title'=> $GLOBALS['Language']->getText('register_confirmation', 'registration_complete')));
+        $this->render('register-complete', new Project_OneStepCreation_OneStepCreationCompletePresenter());
+        $GLOBALS['HTML']->footer(array());
+        exit;
+    }
+
+    private function projectsMustBeApprovedByAdmin() {
+        return Config::get('sys_project_approval', 1) == 1;
     }
 }
-?>
