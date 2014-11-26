a565dc05da8eca5009c7f8e558b9c80b28fbeff4
Nicolas Terray <nicolas.terray@enalean.com>
2014-11-26 11:08:56 +0100

    jQuerify motd resizing
    This decrease dependency on prototypejs.
    
    Part of story story #7511: Drag'nDrop backlog items on Planning v2
    
    Change-Id: Ia82d7bd390f973d124f276aa67241c4ac96c9472

--- src/www/themes/FlamingParrot/js/motd.js
+++ src/www/themes/FlamingParrot/js/motd.js
@@ -21,20 +21,23 @@
  * I make sure that the sidebar and the main content are not hidden under the
  * top navbar whent the motd is displayed
  */
-document.observe('dom:loaded', function () {
+!function ($) {
 
-    $$('.navbar > .motd').each(function (motd_element) {
-        var main_content_element = motd_element.up('.wrapper').down('.main'),
-            initial_margin_top   = parseInt(main_content_element.getStyle('marginTop'), 10);
+    $(document).ready(function () {
+        $('.navbar > .motd').each(updateTopMarginAccordinglyToMOTDHeight);
+    });
+
+    function updateTopMarginAccordinglyToMOTDHeight() {
+        var motd_element = this,
+            main_content_element = $(motd_element).parents('.wrapper').find('.main'),
+            initial_margin_top   = parseInt($(main_content_element).css('margin-top'), 10);
 
         function motdResized() {
-            var height_of_motd = motd_element.getHeight();
-            main_content_element.setStyle({
-                marginTop: (initial_margin_top + height_of_motd) + 'px'
-            });
+            var height_of_motd = $(motd_element).outerHeight();
+            main_content_element.css('margin-top', (initial_margin_top + height_of_motd) + 'px');
         }
 
-        Event.observe(window, 'resize', motdResized);
+        $(window).resize(motdResized);
         motdResized();
-    });
-});
+    }
+}(jQuery);
