303283424894202d27047a136521ae7ea9d2ebc1
Nicolas Terray <nicolas.terray@enalean.com>
2014-12-04 23:11:19 +0100

    Render kanban items as cards
    Part of story #7577 display a Kanban cardwall
    
    Change-Id: I878441cb0d4f4c334e0e5118a0ea3755efb0150b

--- plugins/agiledashboard/www/js/kanban/src/app/app-kanban-controller.js
+++ plugins/agiledashboard/www/js/kanban/src/app/app-kanban-controller.js
@@ -6,7 +6,9 @@
     KanbanCtrl.$inject = ['SharedPropertiesService', 'KanbanService'];
 
     function KanbanCtrl(SharedPropertiesService, KanbanService) {
-        var self = this,
+        var self   = this,
+            limit  = 10,
+            offset = 0,
             kanban = SharedPropertiesService.getKanban();
 
         self.name      = kanban.name;
@@ -25,12 +27,26 @@
             label: 'Closed',
             is_open: false
         };
+        self.loading_backlog = true;
 
         loadColumns();
+        loadBacklog(limit, offset);
 
         function loadColumns() {
             KanbanService.getKanban(kanban.tracker_id).then(function (kanban) {
                 self.board.columns = kanban.columns;
+            });
+        }
+
+        function loadBacklog(limit, offset) {
+            return KanbanService.getBacklog(kanban.tracker_id, limit, offset).then(function(data) {
+                self.backlog.content = self.backlog.content.concat(data.results);
+
+                if (self.backlog.content.length < data.total) {
+                    loadBacklog(limit, offset + limit);
+                } else {
+                    self.loading_backlog = false;
+                }
             });
         }
     }
