450fa612260b908af8ee5b07b477249b648dce37
Sandra Echinard <sandra.echinard@enalean.com>
2015-01-14 10:34:46 +0100

    Fix request #7761: Fatal error when accessing default trackers templates
    Change-Id: I37e48dfc194aabb92820ea236c846ac85a9faf14

--- plugins/tracker/www/resources/templates/index.php
+++ plugins/tracker/www/resources/templates/index.php
@@ -17,7 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+require_once('common/autoload.php');
+
 if ($files = glob('*.xml')) {
     echo '<ul>';
     foreach ($files as $filename) {
