2a1f4ff20fe56cd07be1d1d230cc773df43230e9
Thomas Gerbet <thomas.gerbet@enalean.com>
2015-01-23 17:22:38 +0100

    request #7790: Add missing anti-CSRF token in SSH keys edit form and bookmarks management
    Change-Id: Icf6f62c19adc89f13e1c8e2ce35c71cef1cc2757

--- src/www/my/bookmark_delete.php
+++ src/www/my/bookmark_delete.php
@@ -8,7 +8,7 @@
 require_once('pre.php');
 require_once('bookmarks.php');
 
-$request =& HTTPRequest::instance();
+$request = HTTPRequest::instance();
 
 
 $HTML->header(array("title"=>$Language->getText('bookmark_delete', 'title')));
@@ -18,11 +18,24 @@ $vId = new Valid_UInt('bookmark_id');
 $vId->required();
 if ($request->valid($vId)) {
     $bookmark_id = (int) $request->get('bookmark_id');
+    $csrf_token = new CSRFSynchronizerToken('bookmark_delete');
+
+    if ($request->isPost()) {
+        $csrf_token->check('/my/bookmark_delete.php?bookmark_id='.$bookmark_id);
 	bookmark_delete ($bookmark_id);
-	print "<p>".$Language->getText('bookmark_delete', 'deleted').
-	    "<P><A HREF=\"/my/\">[".$Language->getText('global', 'back_home')."]</A>";
+	print '<p>'.$Language->getText('bookmark_delete', 'deleted').'</p>';
+    } else {
+        print '<form method="post">';
+        print '<p>' . $Language->getText('my_index', 'del_bookmark') . '</p>';
+        print '<input type="hidden" name="bookmark_id" value="'. $bookmark_id .'"/>';
+        print $csrf_token->fetchHTMLInput();
+        print '<input type="submit" value="'. $Language->getText('global', 'btn_submit') .'">';
+        print '</form>';
+    }
+    print "<p><a href=\"/my/\">[".$Language->getText('global', 'back_home')."]</a></p>";
+} else {
+    $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('global', 'missing_parameters'));
+    $GLOBALS['Response']->redirect('/my');
 }
 
 $HTML->footer(array());
-
-?>
