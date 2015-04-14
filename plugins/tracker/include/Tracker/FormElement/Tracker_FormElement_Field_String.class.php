Enalean/tuleap

commit 6e40cb46276cc35df7f5bfc45762b62ed3a6d0ec

request #7945: Fix XSS in tracker form elements
Change-Id: I9532df54dd1568ff76a221c243aea5d82b051f18

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_String.class.php
@@ -129,7 +129,9 @@ class Tracker_FormElement_Field_String extends Tracker_FormElement_Field_Text {
      * @return string
      */
     public function fetchArtifactValueReadOnly(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null) {
-        $value = $value ? $value->getValue() : '';
+        $value    = $value ? $value->getValue() : '';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $value    = $purifier->purify($value);
 
         if (! $value) {
             return $this->getNoValueLabel();

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Container.class.php
@@ -130,8 +130,9 @@ abstract class Tracker_FormElement_Container extends Tracker_FormElement {
      * @return string
      */
     protected function fetchOptgroup($method, $id_prefix, $used, $prefix) {
-        $prefix .= $this->getLabel();
-        $html = '<optgroup id="'. $id_prefix . $this->id .'" label="'. $prefix .'">';
+        $purifier  = Codendi_HTMLPurifier::instance();
+        $prefix   .= $purifier->purify($this->getLabel());
+        $html      = '<optgroup id="'. $id_prefix . $this->id .'" label="'. $prefix .'">';
         $optgroups = '';
         foreach($this->getFormElements() as $formElement) {
             if ($formElement->userCanRead()) {

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Container_Column_Group.class.php
@@ -81,7 +81,7 @@ class Tracker_FormElement_Container_Column_Group {
             foreach ($columns as $c) {
                 if ($content = call_user_func_array(array($c, $method), $params)) {
                     if ($format == 'html') {
-                        $output .= $content ;
+                        $output .= $content;
                     } else {
                         $output .= $content . PHP_EOL;
                     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Container_Fieldset.class.php
@@ -92,11 +92,12 @@ class Tracker_FormElement_Container_Fieldset extends Tracker_FormElement_Contain
         if ($format == 'text') {
             return $label . PHP_EOL . str_pad('', strlen($label), '-') . PHP_EOL;
         } else {
+            $purifier = Codendi_HTMLPurifier::instance();
             return '
                 <tr><td colspan="2">&nbsp;</td></tr>
                 <tr style="color: #444444; background-color: #F6F6F6;">
                     <td align="left" colspan="2">
-                        <h3>'. $label .'</h3>
+                        <h3>'. $purifier->purify($label) .'</h3>
                     </td>
                 </tr>';
         }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.class.php
@@ -736,12 +736,13 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
      * @return string
      */
     public function fetchAddCriteria($used, $prefix = '') {
-        $html = '';
-        $class = 'tracker_report_add_criteria_unused';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
+        $class    = 'tracker_report_add_criteria_unused';
         if (isset($used[$this->id])) {
             $class = 'tracker_report_add_criteria_used';
         }
-        $html .= '<option value="'. $this->id .'" class="'. $class .'">'. $this->getLabel() .'</option>';
+        $html .= '<option value="'. $this->id .'" class="'. $class .'">'. $purifier->purify($this->getLabel()) .'</option>';
         return $html;
     }
 
@@ -754,12 +755,13 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
      * @return string
      */
     public function fetchAddColumn($used, $prefix = '') {
-        $html  = '';
-        $class = 'tracker_report_table_add_column_unused';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
+        $class    = 'tracker_report_table_add_column_unused';
         if (isset($used[$this->id])) {
             $class = 'tracker_report_table_add_column_used';
         }
-        $html .= '<option value="'. $this->id .'" class="'. $class .'">'. $this->getLabel() .'</option>';
+        $html .= '<option value="'. $this->id .'" class="'. $class .'">'. $purifier->purify($this->getLabel()) .'</option>';
         return $html;
     }
 
@@ -772,9 +774,10 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
      * @return string
      */
     public function fetchAddTooltip($used, $prefix = '') {
-        $html = '';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
         if (!isset($used[$this->id])) {
-            $html .= '<option value="'. $this->id .'">'. $this->getLabel() .'</option>';
+            $html .= '<option value="'. $this->id .'">'. $purifier->purify($this->getLabel()) .'</option>';
         }
         return $html;
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -347,13 +347,14 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field {
     }
 
     private function fetchParentSelector($prefill_parent, $name, Tracker $parent_tracker, PFUser $user, $can_create) {
-        $html  = '';
-        $html .= '<p>';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
+        $html    .= '<p>';
         list($label, $possible_parents, $display_selector) = $this->getPossibleArtifactParents($parent_tracker, $user);
         if ($display_selector) {
             $html .= '<label>';
-            $html .= $GLOBALS['Language']->getText('plugin_tracker_artifact', 'formelement_artifactlink_choose_parent', $parent_tracker->getItemName());
-            $html .= '<select name="'. $name .'[parent]">';
+            $html .= $GLOBALS['Language']->getText('plugin_tracker_artifact', 'formelement_artifactlink_choose_parent', $purifier->purify($parent_tracker->getItemName()));
+            $html .= '<select name="'. $purifier->purify($name) .'[parent]">';
             $html .= '<option value="">'. $GLOBALS['Language']->getText('global', 'please_choose_dashed') .'</option>';
             if ($can_create) {
                 $html .= '<option value="'.self::CREATE_NEW_PARENT_VALUE.'">'. $GLOBALS['Language']->getText('plugin_tracker_artifact', 'formelement_artifactlink_create_new_parent') .'</option>';
@@ -369,15 +370,16 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field {
     }
 
     private function fetchArtifactParentsOptions($prefill_parent, $label, array $possible_parents) {
-        $html  = '';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
         if ($possible_parents) {
-            $html .= '<optgroup label="'. $label .'">';
+            $html .= '<optgroup label="'. $purifier->purify($label) .'">';
             foreach ($possible_parents as $possible_parent) {
                 $selected = '';
                 if ($possible_parent->getId() == $prefill_parent) {
                     $selected = ' selected="selected"';
                 }
-                $html .= '<option value="'. $possible_parent->getId() .'"'.$selected.'>'. $possible_parent->getXRefAndTitle() .'</option>';
+                $html .= '<option value="'. $possible_parent->getId() .'"'.$selected.'>'. $purifier->purify($possible_parent->getXRefAndTitle()) .'</option>';
             }
             $html .= '</optgroup>';
         }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.class.php
@@ -110,14 +110,16 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
      * @return string
      */
     public function fetchArtifactValueReadOnly(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null) {
-        $html = '';
-        $html .= '<img src="'.$this->getBurndownImageUrl($artifact).'" alt="'.$this->getLabel().'" width="640" height="480" />';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
+        $html    .= '<img src="'.$this->getBurndownImageUrl($artifact).'" alt="'.$purifier->purify($this->getLabel()).'" width="640" height="480" />';
         return $html;
     }
 
     public function fetchArtifactForOverlay(Tracker_Artifact $artifact, $submitted_values = array()) {
-        $html = '';
-        $html .= '<img src="'.$this->getBurndownImageUrl($artifact).'" alt="'.$this->getLabel().'" width="390" height="400" />';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $html     = '';
+        $html    .= '<img src="'.$this->getBurndownImageUrl($artifact).'" alt="'.$purifier->purify($this->getLabel()).'" width="390" height="400" />';
         return $html;
     }
 
@@ -322,9 +324,10 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
      * @return string
      */
     public function fetchMailArtifactValue(Tracker_Artifact $artifact, PFUser $user, Tracker_Artifact_ChangesetValue $value = null, $format='text') {
-        $output = '';
+        $purifier = Codendi_HTMLPurifier::instance();
+        $output   = '';
         if ($format == Codendi_Mail::FORMAT_HTML) {
-            $output .= '<img src="'.get_server_url().$this->getBurndownImageUrl($artifact).'" alt="'.$this->getLabel().'" width="640" height="480" />';
+            $output .= '<img src="'.get_server_url().$this->getBurndownImageUrl($artifact).'" alt="'.$purifier->purify($this->getLabel()).'" width="640" height="480" />';
             $output .= '<p><em>'.$GLOBALS['Language']->getText('plugin_tracker', 'burndown_email_as_of_today').'</em></p>';
         }
         return $output;

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_File.class.php
@@ -395,20 +395,21 @@ class Tracker_FormElement_Field_File extends Tracker_FormElement_Field {
     protected function fetchAllAttachmentTitleAndDescription($values) {
         $html = '';
         if($values) {
-            $html .= '<div class="tracker-artifact-attachement-title-list tracker_artifact_field">';
-            $html .= '<div class="disabled_field">' . $GLOBALS['Language']->getText('plugin_tracker_artifact', 'formelement_attachment_copy') . '</div>';
-            $html .= '<ul>';
+            $purifier = Codendi_HTMLPurifier::instance();
+            $html    .= '<div class="tracker-artifact-attachement-title-list tracker_artifact_field">';
+            $html    .= '<div class="disabled_field">' . $GLOBALS['Language']->getText('plugin_tracker_artifact', 'formelement_attachment_copy') . '</div>';
+            $html    .= '<ul>';
             foreach($values as $value) {
                 $description = $value->getDescription();
 
                 $html .= '<li>';
                 $html .= '<span class="file-title">';
-                $html .= $value->getFileName();
+                $html .= $purifier->purify($value->getFileName());
                 $html .= '</span>';
 
                 if($description) {
                     $html .= '<span class="file-description">';
-                    $html .= ' - ' . $description;
+                    $html .= ' - ' . $purifier->purify($description);
                     $html .= '</span>';
                 }
                 $html .= '</li>';

commit d6df1e953dc48c9548e96e3f6463a125d66f1d30

request #7998: Fix display of tracker form element string field
The regression was introduced by the commit 6e40cb46276cc35df.

Change-Id: I2568586888324007bf47df94a895ec6344a23c99

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_String.class.php
@@ -129,9 +129,7 @@ class Tracker_FormElement_Field_String extends Tracker_FormElement_Field_Text {
      * @return string
      */
     public function fetchArtifactValueReadOnly(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null) {
-        $value    = $value ? $value->getValue() : '';
-        $purifier = Codendi_HTMLPurifier::instance();
-        $value    = $purifier->purify($value);
+        $value = $value ? $value->getValue() : '';
 
         if (! $value) {
             return $this->getNoValueLabel();