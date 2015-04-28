Enalean/tuleap

commit 52f9c852da1b31db00d0f21ad117dcf257b9376e

request #8015: Fix reflected XSS in the user group creation
Change-Id: Ie3f8b68323d3d25f51bce57d5a082ea8d3ddcca4

src/www/project/admin/ugroup.php
@@ -30,7 +30,8 @@ function format_html_row($row, &$row_num) {
         } else {
             $value = $cell;
         }
-        echo '  <td>'.$value."</td>\n";
+        $purifier = Codendi_HTMLPurifier::instance();
+        echo '  <td>'.$purifier->purify($value)."</td>\n";
     }
     echo "</tr>\n";
 }

src/www/project/admin/editugroup.php
@@ -18,19 +18,20 @@ $request = HTTPRequest::instance();
 
 function get_name_and_desc_form_content($ugroup_name, $ugroup_description) {
     global $Language;
+    $purifier = Codendi_HTMLPurifier::instance();
 
     return ' <table width="100%" border="0" cellpadding="5">
     <tr>
       <td width="21%"><b>'.$Language->getText('project_admin_editugroup', 'name').'</b>:</td>
       <td width="79%"> 
-        <input type="text" name="ugroup_name" value="'.$ugroup_name.'">
+        <input type="text" name="ugroup_name" value="'.$purifier->purify($ugroup_name).'">
       </td>
     </tr>
         <tr><td colspan=2><i>'.$Language->getText('project_admin_editugroup', 'avoid_special_ch').'</td></tr>
     <tr> 
       <td width="21%"><b>'.$Language->getText('project_admin_editugroup', 'desc').'</b>:</td>
       <td width="79%"> 
-      <textarea name="ugroup_description" rows="3" cols="50">'.$ugroup_description.'</textarea>
+      <textarea name="ugroup_description" rows="3" cols="50">'.$purifier->purify($ugroup_description).'</textarea>
       </td>
     </tr>';
 }

src/www/project/admin/ugroup_utils.php
@@ -382,19 +382,20 @@ function ugroup_delete_user_from_project_ugroups($group_id,$user_id) {
  */
 function ugroup_create($group_id, $ugroup_name, $ugroup_description, $group_templates) {
     global $Language;
+    $purifier = Codendi_HTMLPurifier::instance();
 
     // Sanity check
     if (!$ugroup_name) { 
         exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug_name_missed'));
     }
     if (!eregi("^[a-zA-Z0-9_\-]+$",$ugroup_name)) {
-        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','invalid_ug_name',$ugroup_name));
+        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','invalid_ug_name', $purifier->purify($ugroup_name)));
     }
     // Check that there is no ugroup with the same name in this project
     $sql = "SELECT * FROM ugroup WHERE name='$ugroup_name' AND group_id='$group_id'";
     $result=db_query($sql);
     if (db_numrows($result)>0) {
-        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug__exist',$ugroup_name)); 
+        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug__exist', $purifier->purify($ugroup_name)));
     }
     
     
@@ -472,13 +473,14 @@ function ugroup_create($group_id, $ugroup_name, $ugroup_description, $group_temp
  */
 function ugroup_update($group_id, $ugroup_id, $ugroup_name, $ugroup_description) {
     global $Language;
+    $purifier = Codendi_HTMLPurifier::instance();
 
     // Sanity check
     if (!$ugroup_name) { 
         exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug_name_missed'));
     }
     if (!eregi("^[a-zA-Z0-9_\-]+$",$ugroup_name)) {
-        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','invalid_ug_name',$ugroup_name));
+        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','invalid_ug_name', $purifier->purify($ugroup_name)));
     }
     if (!$ugroup_id) {
         exit_error($Language->getText('global','error'),$Language->getText('project_admin_editugroup','ug_id_missed'));
@@ -495,7 +497,7 @@ function ugroup_update($group_id, $ugroup_id, $ugroup_name, $ugroup_description)
     $sql = "SELECT * FROM ugroup WHERE name='$ugroup_name' AND group_id='$group_id' AND ugroup_id!='$ugroup_id'";
     $result=db_query($sql);
     if (db_numrows($result)>0) {
-        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug__exist',$ugroup_name)); 
+        exit_error($Language->getText('global','error'),$Language->getText('project_admin_ugroup_utils','ug__exist', $purifier->purify($ugroup_name)));
     }
 
     // Update

commit f2db5297c738ba13d9539003f3bfdc451a388724

request #8039: Fix the purification in user group permissions
Regression introduced by the commit 52f9c852da1b31

Change-Id: I7b0dc50ede127ce75ee37ab0edcfa72901e9dde3

src/www/project/admin/ugroup.php
@@ -30,8 +30,8 @@ function format_html_row($row, &$row_num) {
         } else {
             $value = $cell;
         }
-        $purifier = Codendi_HTMLPurifier::instance();
-        echo '  <td>'.$purifier->purify($value)."</td>\n";
+
+        echo '  <td>'.$value."</td>\n";
     }
     echo "</tr>\n";
 }
@@ -101,13 +101,15 @@ foreach($title_arr as $title) {
 }
 echo "</tr>\n";
 
+$purifier = Codendi_HTMLPurifier::instance();
+
 $ugroupRow = array();
 $row_num   = 0;
 $result    = db_query("SELECT * FROM ugroup WHERE group_id=100 ORDER BY ugroup_id");
 while ($row = db_fetch_array($result)) {
     if ($project->usesDocman() || ($row['name'] != 'ugroup_document_tech_name_key' && $row['name'] != 'ugroup_document_admin_name_key')) {
-        $ugroupRow[100] = util_translate_name_ugroup($row['name']).' *';
-        $ugroupRow[200] = util_translate_desc_ugroup($row['description']);
+        $ugroupRow[100] = $purifier->purify(util_translate_name_ugroup($row['name']).' *');
+        $ugroupRow[200] = $purifier->purify(util_translate_desc_ugroup($row['description']));
         $ugroupRow[300] = array('value' => '-', 'html_attrs' => 'align="center"');
         $ugroupRow[400] = array('value' => '-', 'html_attrs' => 'align="center"');
         ksort($ugroupRow);
@@ -122,8 +124,9 @@ if ($group_id != 100) {
   if (db_numrows($result) > 0) {
     $ugroupUserDao = new UGroupUserDao();
     while ($row = db_fetch_array($result)) {
-        $ugroupRow[100] = '<a href="/project/admin/editugroup.php?group_id='.$group_id.'&ugroup_id='.$row['ugroup_id'].'&func=edit">'.util_translate_name_ugroup($row['name']).'</a>';
-        $ugroupRow[200] = util_translate_desc_ugroup($row['description']);
+        $ugroupRow[100] = '<a href="/project/admin/editugroup.php?group_id='.$group_id.'&ugroup_id='.$row['ugroup_id'].'&func=edit">'.
+            $purifier->purify(util_translate_name_ugroup($row['name'])).'</a>';
+        $ugroupRow[200] = $purifier->purify(util_translate_desc_ugroup($row['description']));
         $res2=db_query("SELECT count(*) FROM ugroup_user WHERE ugroup_id=".$row['ugroup_id']);
         $nb_members=db_result($res2,0,0);
         if ($nb_members) {