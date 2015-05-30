b8a2bf1b34452400acac15953a688647e2dee710
Thomas Gerbet <thomas.gerbet@enalean.com>
2015-05-30 23:45:54 +0200

    request #5726: Remove the Twitter widget
    Since Twitter shuts down the REST API v1 in 2013 the widget does not work.
    It does not seem to be missed and can certainly be removed.
    
    Change-Id: I72585b56f1045c42eecfdb1b2c09fb4b919e404c

--- src/common/widget/Widget.class.php
+++ src/common/widget/Widget.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2015. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -153,9 +154,6 @@
             case 'myrss':
                 $o = new Widget_MyRss();
                 break;
-            case 'mytwitterfollow':
-                $o = new Widget_MyTwitterFollow();
-                break;
             //case 'mywikipage':                   //not yet
             //    $o = new Widget_MyWikiPage();
             //    break;
@@ -205,9 +203,6 @@
             case 'projectrss':
                 $o = new Widget_ProjectRss();
                 break;
-            case 'projecttwitterfollow':
-                $o = new Widget_ProjectTwitterFollow();
-                break;
             case 'projectsvnstats':
                 $o = new Widget_ProjectSvnStats();
                 break;
@@ -242,15 +237,14 @@
                 $widgets = array('myadmin', 'mysurveys', 'myprojects', 'mybookmarks', 
                     'mymonitoredforums', 'mymonitoredfp', 'myartifacts', 'mybugs', //'mywikipage' //not yet
                     'mytasks', 'mysrs', 'myimageviewer', 
-                    'mylatestsvncommits', 'mytwitterfollow',
-                    'mysystemevent', 'myrss',
+                    'mylatestsvncommits', 'mysystemevent', 'myrss',
                 );
                 break;
             case WidgetLayoutManager::OWNER_TYPE_GROUP:
                 $widgets = array('projectdescription', 'projectmembers', 
                     'projectlatestfilereleases', 'projectlatestnews', 'projectpublicareas', //'projectwikipage' //not yet
-                    'projectlatestsvncommits', 'projectlatestcvscommits', 'projecttwitterfollow', 
-                    'projectsvnstats', 'projectrss', 'projectimageviewer', 'projectcontacts'
+                    'projectlatestsvncommits', 'projectlatestcvscommits', 'projectsvnstats',
+                    'projectrss', 'projectimageviewer', 'projectcontacts'
                 );
                 if ($GLOBALS['sys_use_trove'] != 0) {
                     $widgets[] = 'projectclassification';
@@ -326,4 +320,3 @@
         return '/widgets/widget.php?owner='. $owner_type.$owner_id .'&action=iframe&name['. $this->id .']='. $this->getInstanceId();
     }
 }
-?>
