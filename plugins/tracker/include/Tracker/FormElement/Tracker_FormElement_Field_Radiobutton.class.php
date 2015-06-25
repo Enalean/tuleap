6ca19dd780db325b38337248b3034230e24c25aa
Nicolas Terray <nicolas.terray@enalean.com>
2015-06-25 17:15:06 +0200

    request #8179: Comments display issue
    When "radio button" field is used in the tracker, the comments are
    displayed under the sidebar
    
    Introduced in git #98b3961b76d98cf7ebd02a69fe264cf649734b56
    
    Change-Id: I9a4b2953c1ee1aea24f4a8d5e7316bc3b821b8f8

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Radiobutton.class.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Radiobutton.class.php
@@ -55,7 +55,7 @@ class Tracker_FormElement_Field_Radiobutton extends Tracker_FormElement_Field_Se
     }
 
     protected function fetchFieldContainerEnd() {
-        return '</div>';
+        return '';
     }
 
 
