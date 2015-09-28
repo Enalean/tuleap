31d45d6a552606b2095f9981dd02bce332960ff4
Thomas Gerbet <thomas.gerbet@enalean.com>
2015-09-28 14:41:29 +0200

    request #8447: README files can be rendered in subdirectories
    Change-Id: Iba86b7694b11f04db04ac019d92f6470c5477571

--- plugins/git/include/Markdown/GitMarkdownFile.class.php
+++ plugins/git/include/Markdown/GitMarkdownFile.class.php
@@ -30,22 +30,21 @@ class GitMarkdownFile {
     /**
      * Get the first markdown file name/content for a folder in a Git repository.
      *
-     * @param string $path Path to the repository
      * @param string $node Folder path from repository
      * @param string $commit_sha1 Sha-1 of the comit you want to inspect
      *
      * @return array
      */
-    public function getReadmeFileContent($path, $node, $commit_sha1) {
-        $readme_file = $this->getReadmeFile($path, $node, $commit_sha1);
+    public function getReadmeFileContent($node, $commit_sha1) {
+        $readme_file = $this->getReadmeFile($node, $commit_sha1);
         if ($readme_file && version_compare(phpversion(), '5.3', '>=')){
-            return $this->getFormatedMarkdown($readme_file, $path, $commit_sha1);
+            return $this->getFormatedMarkdown($readme_file, $commit_sha1);
         }
 
         return false;
     }
 
-    private function getFormatedMarkdown($file_name, $path, $commit_sha1) {
+    private function getFormatedMarkdown($file_name, $commit_sha1) {
         $content         = $this->git_exec->getFileContent($commit_sha1, $file_name);
         $content_in_form = GitMarkdownFileContentInForm::getMarkdownContentInForm($content);
 
@@ -55,9 +54,12 @@ class GitMarkdownFile {
         );
     }
 
-    private function getReadmeFile($path, $node, $commit_sha1) {
+    private function getReadmeFile($node, $commit_sha1) {
         $files_list          = $this->git_exec->lsTree($commit_sha1, $node);
-        $markdown_files_list = array_values(preg_grep('/^readme\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)$/i', $files_list));
+        $path                = preg_quote($node, DIRECTORY_SEPARATOR);
+        $markdown_files_list = array_values(
+            preg_grep('/^'. $path .'readme\.(markdown|mdown|mkdn|md|mkd|mdwn|mdtxt|mdtext|text)$/i', $files_list)
+        );
 
         if (isset($markdown_files_list[0])) {
             return $markdown_files_list[0];
