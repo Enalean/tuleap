2b3cdca789e3ba784722051f5aafe6a36d427f12
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2015-10-29 12:26:14 +0100

    Take into account file system references
    This task is part of request #8443: System keywords don't generate the right cross reference when cross projects
    
    The file system keyword is now available cross-project.
    You can test it for instance with an SVN commit message.
    The link to the file works now cross projects.
    
    Change-Id: I9dd5087ca7edd859f135c228a7889a0418f7e53a

--- src/common/frs/FRSFile.class.php
+++ src/common/frs/FRSFile.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Enalean, 2015. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once('common/include/Error.class.php');
@@ -43,6 +44,8 @@ class FRSFile extends Error {
      * A 8 kb buffer size
      */
     const PEAR_BUFFER_SIZE = 8192;
+
+    const REFERENCE_NATURE = 'file';
 
 	/**
      * @var int $file_id the ID of this FRSFile
