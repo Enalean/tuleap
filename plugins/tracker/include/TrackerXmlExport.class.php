0ee1dba3e00bf3f1ee3e4ea823e4604a276baf6a
Nicolas Terray <nicolas.terray@enalean.com>
2015-11-05 15:23:02 +0100

    Revert "request #8535 There should be only one validation of the whole XML import file"
    This reverts commit ad4c8ab2501fa6ec9d540dda0d6efc5cb7c6a96c /
    gerrit Ia1363bbbbbe8d12b4ba7366ab70f4b239ae770db.
    
    Few things have not been seen during the integration of this commit:
    - artifact-definition.rnc has been updated but corresponding .rng is not
      updated (in plugins/tracker).
    - tracker- and artifact-definition.rng have appeared in core, without any
      corresponding .rnc files. I suspect that those files have been
      copied / moved from plugins/tracker.
    - core should not include directly .rng from plugins.
    
    I'm affraid that we cannot integrate this as of today and we will
    have to do more than one validation: at least one fore core + one for
    each plugin to import.
    
    Impacts are:
    - export / import tracker structure
    - migrate tv3 to tv5
    - export / import project (+ tracker and artifact)
    
    Change-Id: I8158975c1dac8b3235ceeb9176bdad86bb962ae2

--- plugins/tracker/include/TrackerXmlExport.class.php
+++ plugins/tracker/include/TrackerXmlExport.class.php
@@ -86,6 +86,7 @@ class TrackerXmlExport {
             $this->artifact_xml_xport->export($tracker, $tracker_xml, $user, $archive);
         }
 
+        $this->rng_validator->validate($xml_trackers, dirname(TRACKER_BASE_DIR).'/www/resources/trackers.rng');
         return $xml_trackers;
     }
 }
