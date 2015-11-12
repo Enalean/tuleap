1de843037d8dd39de37e20968239ea98c802cf4e
Manuel Vacelet <manuel.vacelet@enalean.com>
2015-11-12 11:43:34 +0100

    request #8589 Apache logs filled with No entry for terminal type "unknown";
    - Deploy the right gitolite-suexec-wrapper.sh when using gitolite3
    - Update git apache conf to make use of it
    - Fix gitolite-suexec-wrapper.sh to set a TERM (need by git backend)
    
    Change-Id: I85546e64e25b1d5508384b97dd982992b5d21e52

--- plugins/git/bin/gitolite3-suexec-wrapper.sh
+++ plugins/git/bin/gitolite3-suexec-wrapper.sh
@@ -5,5 +5,6 @@
 
 export GIT_PROJECT_ROOT="/var/lib/codendi/gitolite/repositories"
 export GITOLITE_HTTP_HOME="/var/lib/gitolite"
+export TERM=linux
 
 exec sudo -E -u gitolite /usr/share/gitolite3/gitolite-shell
