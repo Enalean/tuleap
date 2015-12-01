c03486cdef9cc4550c42e375b7ad71618c1b86ba
Joris Masson <joris.masson@enalean.com>
2015-12-01 16:18:54 +0100

    request #8517 Stories created under Sprint Milestone on V2 Planning are not linked to their Release
    Given the following AD plannings:
    
    Release ------------ Sprints
    |                       |
    |                       |
    `Epics  ----------------`Stories
    
    If I create a User story in a Sprint with an Epic parent that is not
    linked to any Release, then the story will be automatically linked
    To the parent Release of the Sprint.
    
    If the Epic is already linked to a Release or if the Story has no
    parent, the actual behaviour does not change.
    
    This link are also propagated to parent plannings.
    
    Change-Id: I0b8fdfd1b41362999ae080ddf33db4a0931a8a35

---
+++ plugins/agiledashboard/tests/include/MilestoneParentLinkerTest.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2015. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once dirname(__FILE__).'/../bootstrap.php';
+
+class MilestoneParentLinkerTest extends TuleapTestCase {
+
+    /**
+     * @var Planning_MilestoneFactory
+     */
+    private $milestone_factory;
+
+    /**
+     * @var MilestoneParentLinker
+     */
+    private $milestone_parent_linker;
+
+    /**
+     * @var Planning_Milestone
+     */
+    private $milestone;
+
+    /**
+     * @var PFUser
+     */
+    private $user;
+
+    /**
+     * @var AgileDashboard_Milestone_Backlog_BacklogStrategyFactory
+     */
+    private $backlog_strategy_factory;
+
+    /**
+     * @var AgileDashboard_Milestone_Backlog_DescendantBacklogStrategy
+     */
+    private $strategy;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->milestone_factory        = mock('Planning_MilestoneFactory');
+        $this->backlog_strategy_factory = mock('AgileDashboard_Milestone_Backlog_BacklogStrategyFactory');
+        $this->milestone_parent_linker  = new MilestoneParentLinker(
+            $this->milestone_factory,
+            $this->backlog_strategy_factory
+        );
+
+        $this->milestone = mock('Planning_Milestone');
+        $this->user      = mock('PFUser');
+        $this->strategy  = mock('AgileDashboard_Milestone_Backlog_DescendantBacklogStrategy');
+
+        stub($this->backlog_strategy_factory)->getBacklogStrategy()->returns($this->strategy);
+    }
+
+    public function itDoesNothingIfTheMilestoneHasNoParent() {
+        $artifact_added            = anArtifact()->withId(101)->build();
+        $parent_milestone_artifact = mock('Tracker_Artifact');
+
+        stub($this->milestone)->getParent()->returns(null);
+
+        expect($parent_milestone_artifact)->linkArtifact()->never();
+
+        $this->milestone_parent_linker->linkToMilestoneParent(
+            $this->milestone,
+            $this->user,
+            $artifact_added
+        );
+    }
+
+    public function itDoesNothingIfTheArtifactTrackerIsNotInParentMilestoneBacklogTrackers() {
+        $artifact_added            = anArtifact()->withId(101)->withTrackerId(201)->build();
+        $parent_milestone_artifact = mock('Tracker_Artifact');
+        $parent_milestone          = stub('Planning_Milestone')->getArtifact()->returns($parent_milestone_artifact);
+        $descendant_tracker        = aTracker()->withId(202)->build();
+
+        stub($this->strategy)->getDescendantTrackers()->returns(array($descendant_tracker));
+        stub($this->milestone)->getParent()->returns($parent_milestone);
+
+        expect($parent_milestone_artifact)->linkArtifact()->never();
+
+        $this->milestone_parent_linker->linkToMilestoneParent(
+            $this->milestone,
+            $this->user,
+            $artifact_added
+        );
+    }
+
+    public function itDoesNothingIfTheParentIsLinkedToParentMilestone() {
+        $artifact_added            = stub('Tracker_Artifact')->getTrackerId()->returns(201);
+        $parent_milestone_artifact = mock('Tracker_Artifact');
+        $parent_milestone          = stub('Planning_Milestone')->getArtifact()->returns($parent_milestone_artifact);
+        $parent_linked_artifact    = stub('Tracker_Artifact')->getId()->returns(102);
+        $descendant_tracker        = aTracker()->withId(201)->build();
+
+        stub($this->strategy)->getDescendantTrackers()->returns(array($descendant_tracker));
+        stub($artifact_added)->getParent()->returns($parent_linked_artifact);
+        stub($parent_milestone_artifact)->getLinkedArtifacts($this->user)->returns(array($parent_linked_artifact));
+        stub($this->milestone)->getParent()->returns($parent_milestone);
+
+        expect($parent_milestone_artifact)->linkArtifact()->never();
+
+        $this->milestone_parent_linker->linkToMilestoneParent(
+            $this->milestone,
+            $this->user,
+            $artifact_added
+        );
+    }
+
+    public function itLinksTheItemToParentMilestone() {
+        $added_artifact            = stub('Tracker_Artifact')->getTrackerId()->returns(201);
+        $parent_milestone_artifact = mock('Tracker_Artifact');
+        $parent_milestone          = stub('Planning_Milestone')->getArtifact()->returns($parent_milestone_artifact);
+        $parent_linked_artifact    = stub('Tracker_Artifact')->getId()->returns(102);
+        $parent                    = stub('Tracker_Artifact')->getId()->returns(103);
+        $descendant_tracker        = aTracker()->withId(201)->build();
+
+        stub($this->strategy)->getDescendantTrackers()->returns(array($descendant_tracker));
+
+        stub($added_artifact)->getId()->returns(101);
+        stub($added_artifact)->getParent()->returns($parent);
+        stub($parent_milestone_artifact)->getLinkedArtifacts($this->user)->returns(array($parent_linked_artifact));
+        stub($this->milestone)->getParent()->returns($parent_milestone);
+
+        expect($parent_milestone_artifact)->linkArtifact(101, $this->user)->once();
+
+        $this->milestone_parent_linker->linkToMilestoneParent(
+            $this->milestone,
+            $this->user,
+            $added_artifact
+        );
+    }
+
+    public function itLinksTheItemToParentMilestoneIfTheItemHasNoParent() {
+        $added_artifact            = stub('Tracker_Artifact')->getTrackerId()->returns(201);
+        $parent_milestone_artifact = mock('Tracker_Artifact');
+        $parent_milestone          = stub('Planning_Milestone')->getArtifact()->returns($parent_milestone_artifact);
+        $parent_linked_artifact    = stub('Tracker_Artifact')->getId()->returns(102);
+        $descendant_tracker        = aTracker()->withId(201)->build();
+
+        stub($added_artifact)->getId()->returns(101);
+        stub($added_artifact)->getParent()->returns(null);
+        stub($this->strategy)->getDescendantTrackers()->returns(array($descendant_tracker));
+
+        stub($parent_milestone_artifact)->getLinkedArtifacts($this->user)->returns(array($parent_linked_artifact));
+        stub($this->milestone)->getParent()->returns($parent_milestone);
+
+        expect($parent_milestone_artifact)->linkArtifact(101, $this->user)->once();
+
+        $this->milestone_parent_linker->linkToMilestoneParent(
+            $this->milestone,
+            $this->user,
+            $added_artifact
+        );
+    }
+}
