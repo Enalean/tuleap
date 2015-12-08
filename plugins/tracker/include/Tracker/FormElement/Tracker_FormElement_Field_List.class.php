Enalean/tuleap

commit 731a8828b1354ca755a328545f51ae30c6f4e3cd

request #8464: Check if values set for field list are possible
Change-Id: Ifddf7eb9e3eaa204f4e806a97a1c756623ba6792

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List.class.php
@@ -1301,9 +1301,34 @@ abstract class Tracker_FormElement_Field_List extends Tracker_FormElement_Field 
      * @return bool true if the value is considered ok
      */
     public function isValid(Tracker_Artifact $artifact, $value) {
-        $this->has_errors = !$this->validate($artifact, $value);
+        $this->has_errors = $this->isPossibleValue($value) && $this->validate($artifact, $value);
 
-        return !$this->has_errors;
+        return $this->has_errors;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isPossibleValue($value) {
+        $is_possible_value   = true;
+        $all_possible_values = $this->getBind()->getAllValues();
+
+        if (is_array($value)) {
+            foreach ($value as $id) {
+                $is_possible_value = $is_possible_value && $this->checkValueExists($id, $all_possible_values);
+            }
+        } else {
+            $is_possible_value = $this->checkValueExists($value, $all_possible_values);
+        }
+
+        return $is_possible_value;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function checkValueExists($value, $all_possible_values) {
+        return array_key_exists($value, $all_possible_values) || $value == Tracker_FormElement_Field_List::NONE_VALUE;
     }
 
     /**

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_OpenList.class.php
@@ -922,4 +922,34 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
 
         return parent::getRESTAvailableValues();
     }
+
+    /**
+     * @return bool
+     */
+    protected function checkValueExists($value, $all_possible_values) {
+        return $this->isValueEmpty($value) || $this->isValueABindNoneValue($value) ||
+            $this->isValueABindValue($value, $all_possible_values);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isValueEmpty($value) {
+        return $value === '';
+    }
+
+    /**
+     * @return bool
+     */
+    private function isValueABindValue($value, $all_possible_values) {
+        return substr($value, 0, 1) === Tracker_FormElement_Field_OpenList::BIND_PREFIX &&
+            array_key_exists(substr($value, 1), $all_possible_values);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isValueABindNoneValue($value) {
+        return $value === Tracker_FormElement_Field_OpenList::BIND_PREFIX . Tracker_FormElement_Field_OpenList::NONE_VALUE;
+    }
 }

plugins/tracker/tests/Tracker_FormElement_Field_ListTest.php
@@ -242,6 +242,12 @@ class Tracker_FormElement_Field_ListTest extends UnitTestCase {
         $submitted_value_1 = '123'; // $v1
         $submitted_value_2 = '456'; // $v2
         $submitted_value_3 = '789'; // $v3
+        stub($bind)->getAllValues()->returns(array(
+                $submitted_value_1 => null,
+                $submitted_value_2 => null,
+                $submitted_value_3 => null
+            )
+        );
         
         $artifact->setReturnReference('getLastChangeset', $changeset);
         
@@ -295,6 +301,10 @@ class Tracker_FormElement_Field_ListTest extends UnitTestCase {
         $v1->setReturnValue('__toString', '# 123');
         $v1->setReturnValue('getLabel','label1');
         $submitted_value_1 = '123'; // $v1
+        stub($bind)->getAllValues()->returns(array(
+                $submitted_value_1 => null
+            )
+        );
         
         $artifact->setReturnReference('getLastChangeset', $changeset);
         
@@ -337,6 +347,12 @@ class Tracker_FormElement_Field_ListTest extends UnitTestCase {
         $v2->setReturnValue('__toString', '# 456');
         $v2->setReturnValue('getLabel','label2');
         $submitted_value_2 = '456'; // $v2
+
+        stub($bind)->getAllValues()->returns(array(
+                $submitted_value_1 => null,
+                $submitted_value_2 => null
+            )
+        );
         
         $artifact->setReturnReference('getLastChangeset', $changeset);
         
@@ -423,14 +439,25 @@ class Tracker_FormElement_Field_ListTest extends UnitTestCase {
     }
     
     public function testIsValidRequired() {
-        $artifact  = new MockTracker_Artifact();
+        $artifact   = new MockTracker_Artifact();
+        $bind       = new MockTracker_FormElement_Field_List_Bind_Static();
         $field_list = new $this->field_class();
+        $field_list->setReturnReference('getBind', $bind);
         $field_list->setReturnValue('isRequired', true);
         
         $value1 = '';
         $value2 = null;
         $value3 = '100';
         $value4 = 'value';
+
+        stub($bind)->getAllValues()->returns(array(
+                $value1 => null,
+                $value2 => null,
+                $value3 => null,
+                $value4 => null
+
+            )
+        );
         
         $field_list->setReturnValue('isNone', true, array($value1));
         $field_list->setReturnValue('isNone', true, array($value2));
@@ -598,5 +625,40 @@ class Tracker_FormElement_Field_List_RESTTests extends TuleapTestCase {
         $value = 'some_value';
 
         $field->getFieldDataFromRESTValueByField($value);
+    }
+}
+
+class Tracker_FormElement_Field_List_Validate_Values extends TuleapTestCase {
+
+    private $artifact;
+    private $bind;
+    private $list;
+
+    public function setUp() {
+        parent::setUp();
+        $this->bind     = mock('Tracker_FormElement_Field_List_Bind_Static');
+        $this->list     = new Tracker_FormElement_Field_ListTestVersion();
+        $this->artifact = new MockTracker_Artifact();
+        stub($this->list)->getBind()->returns($this->bind);
+        stub($this->bind)->getAllValues()->returns(array(
+                101 => null,
+                102 => null,
+                103 => null
+            )
+        );
+    }
+
+    public function itAcceptsValidValues() {
+        $this->assertTrue($this->list->isValid($this->artifact, 101));
+        $this->assertTrue($this->list->isValid($this->artifact, Tracker_FormElement_Field_List::NONE_VALUE));
+        $this->assertTrue($this->list->isValid($this->artifact, strval(Tracker_FormElement_Field_List::NONE_VALUE)));
+        $this->assertTrue($this->list->isValid($this->artifact, array(101, 103)));
+    }
+
+    public function itDoesNotAcceptIncorrectValues() {
+        $this->assertFalse($this->list->isValid($this->artifact, 9999));
+        $this->assertFalse($this->list->isValid($this->artifact, array(9998, 9999)));
+        $this->assertFalse($this->list->isValid($this->artifact, array(101, 9999)));
+
     }
 }

plugins/tracker/tests/Tracker_FormElement_Field_OpenListTest.php
@@ -104,7 +104,7 @@ class Tracker_FormElement_Field_OpenListTest extends TuleapTestCase {
         $dar->setReturnValueAt(11, 'valid', false);
         $value_dao->setReturnReference('searchById', $dar);
         
-        $bind = new MockTracker_FormElement_Field_List_Bind_Static();
+        $bind = mock('Tracker_FormElement_Field_List_Bind_Static');
         $bind_values = array(
             1000 => mock('Tracker_FormElement_Field_List_BindValue'),
             1001 => mock('Tracker_FormElement_Field_List_BindValue'),
@@ -391,5 +391,34 @@ class Tracker_FormElement_Field_OpenList_RESTTests extends TuleapTestCase {
         $value = 'some_value';
 
         $field->getFieldDataFromRESTValueByField($value);
+    }
+}
+
+class Tracker_FormElement_Field_OpenList_Validate_Values extends TuleapTestCase {
+    private $artifact;
+    private $bind;
+    private $field;
+
+    public function setUp() {
+        parent::setUp();
+        $this->artifact = mock('Tracker_Artifact');
+        $this->bind     = mock('Tracker_FormElement_Field_List_Bind_Static');
+        $this->field    = partial_mock('Tracker_FormElement_Field_OpenList', array('getBind', 'validate'));
+        stub($this->field)->getBind()->returns($this->bind);
+        stub($this->field)->validate()->returns(true);
+        stub($this->bind)->getAllValues()->returns(array(
+                101 => null,
+                102 => null,
+                103 => null
+            )
+        );
+    }
+
+    public function itAcceptsValidValues() {
+        $this->assertTrue($this->field->isValid($this->artifact, ''));
+        $this->assertTrue($this->field->isValid($this->artifact, 'b101'));
+        $this->assertTrue($this->field->isValid($this->artifact, Tracker_FormElement_Field_OpenList::BIND_PREFIX .
+            Tracker_FormElement_Field_OpenList::NONE_VALUE));
+        $this->assertTrue($this->field->isValid($this->artifact, array('b101', 'b102')));
     }
 }

tests/lib/rest/TestDataBuilder.php
@@ -275,6 +275,9 @@ class REST_TestDataBuilder extends TestDataBuilder {
     public function generateMilestones() {
         echo "Create milestones\n";
 
+        $this->tracker_formelement_factory->clearInstance();
+        $this->tracker_formelement_factory->instance();
+
         $user = $this->user_manager->getUserByUserName(self::ADMIN_USER_NAME);
 
         $this->createRelease($user, 'Release 1.0', '126');
@@ -291,6 +294,11 @@ class REST_TestDataBuilder extends TestDataBuilder {
         $release->linkArtifact(self::SPRINT_ARTIFACT_ID, $user);
 
         return $this;
+    }
+
+    private function clearFormElementCache() {
+        $this->tracker_formelement_factory->clearInstance();
+        $this->tracker_formelement_factory->instance();
     }
 
     public function generateContentItems() {

tests/rest/ArtifactsTest.php
@@ -52,7 +52,7 @@ class ArtifactsTest extends RestBase {
             ),
             'values' => array(
                $this->getSubmitTextValue(REST_TestDataBuilder::EPICS_TRACKER_ID, $summary_field_label, $summary_field_value),
-               $this->getSubmitListValue(REST_TestDataBuilder::EPICS_TRACKER_ID, 'Status', 205)
+               $this->getSubmitListValue(REST_TestDataBuilder::EPICS_TRACKER_ID, 'Status', 103)
             ),
         ));
 

commit 4e570111b5a3b6253f95dae91f0473b9c837efc8

request #8650: Errors feedback is not relevant
When there are error in your submission form, list fields are
highlighted as in error whereas it is not the case.

Issue introduced by commit 731a8828b1354ca755a328545f51ae30c6f4e3cd
(Ifddf7eb9e3eaa204f4e806a97a1c756623ba6792)
See request #8464 for details.

Change-Id: I64037941a323145848e1d7ea65b12e415c5a5ee5

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List.class.php
@@ -1308,9 +1308,9 @@ abstract class Tracker_FormElement_Field_List extends Tracker_FormElement_Field 
      * @return bool true if the value is considered ok
      */
     public function isValid(Tracker_Artifact $artifact, $value) {
-        $this->has_errors = $this->isPossibleValue($value) && $this->validate($artifact, $value);
+        $this->has_errors = ! ($this->isPossibleValue($value) && $this->validate($artifact, $value));
 
-        return $this->has_errors;
+        return ! $this->has_errors;
     }
 
     /**