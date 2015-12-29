de1828cfa8d426d1d07a8e92338da4e1aaf58584
Thomas Gerbet <thomas.gerbet@enalean.com>
2015-12-29 17:17:25 +0100

    request #8715: SVN authentication with modmysql does not work with a database located on another server than Tuleap
    To reproduce the issue, you need to:
      - have your MySQL DB on a different server than your frontend
      - use modmysql for SVN authentication
    
    Change-Id: I7335e5125000965ebd2300a35cfb0cb9382962a8

--- tests/simpletest/common/svn/SVN_Apache_ModMysqlTest.php
+++ tests/simpletest/common/svn/SVN_Apache_ModMysqlTest.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012. All Rights Reserved.
- * 
+ * Copyright (c) Enalean, 2012-2015. All Rights Reserved.
+ *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Tuleap; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -21,33 +21,31 @@ require_once 'common/svn/SVN_Apache_ModMysql.class.php';
 
 
 class SVN_Apache_ModMysqlTest extends UnitTestCase {
-    
-    function setUp() {
-        $GLOBALS['sys_dbhost'] = 'db_server';
-        $GLOBALS['sys_dbname'] = 'db';
-        $GLOBALS['svn_prefix'] = '/svnroot';
-        $GLOBALS['sys_dbauth_user']   = 'dbauth_user';
-        $GLOBALS['sys_dbauth_passwd'] = 'dbauth_passwd';
+
+    public function setUp() {
+        parent::setUp();
+        ForgeConfig::store();
+        ForgeConfig::set('sys_dbhost', 'db_server');
+        ForgeConfig::set('sys_dbname', 'db');
+        ForgeConfig::set('svn_prefix', '/svnroot');
+        ForgeConfig::set('sys_dbauth_user', 'dbauth_user');
+        ForgeConfig::set('sys_dbauth_passwd', 'dbauth_passwd');
     }
-    
-    function tearDown() {
-        unset($GLOBALS['sys_dbname']);
-        unset($GLOBALS['sys_dbhost']);
-        unset($GLOBALS['svn_prefix']);
-        unset($GLOBALS['sys_dbauth_user']);
-        unset($GLOBALS['sys_dbauth_passwd']);
+
+    public function tearDown() {
+        ForgeConfig::restore();
+        parent::tearDown();
     }
-    
+
     private function GivenAnApacheAuthenticationConfForGuineaPigProject() {
         $project_db_row = array('unix_group_name' => 'GPig',
-                                'group_name'      => 'Guinea Pig',
-                                'group_id'        => 101);
+            'group_name' => 'Guinea Pig',
+            'group_id' => 101);
         $apacheConf = new SVN_Apache_ModMysql($project_db_row);
         return $apacheConf->getConf();
     }
-    
-    
-    function testGetApacheAuthShouldContainsDefaultValues() {
+
+    public function testGetApacheAuthShouldContainsDefaultValues() {
         $conf = $this->GivenAnApacheAuthenticationConfForGuineaPigProject();
 
         $this->assertPattern('/Location \/svnroot\/GPig>/', $conf);
@@ -55,25 +53,24 @@ class SVN_Apache_ModMysqlTest extends UnitTestCase {
         $this->assertPattern('/AuthType Basic/', $conf);
         $this->assertPattern('/AuthName "Subversion Authorization \(Guinea Pig\)"/', $conf);
     }
-    
-    function testGetApacheAuthShouldSetupMysqlAccess() {
+
+    public function testGetApacheAuthShouldSetupMysqlAccess() {
         $conf = $this->GivenAnApacheAuthenticationConfForGuineaPigProject();
-        
+
         $this->assertPattern('/AuthMYSQLEnable/', $conf);
         $this->assertPattern('/AuthMySQLUser/', $conf);
         $this->assertPattern('/AuthMySQLPassword/', $conf);
+        $this->assertPattern('/AuthMySQLHost/', $conf);
         $this->assertPattern('/AuthMySQLDB/', $conf);
         $this->assertPattern('/AuthMySQLUserTable/', $conf);
         $this->assertPattern('/AuthMySQLNameField/', $conf);
         $this->assertPattern('/AuthMySQLPasswordField/', $conf);
         $this->assertPattern('/AuthMySQLUserCondition/', $conf);
     }
-    
-    function testGetApacheAuthShouldNotReferenceAuthPerl() {
+
+    public function testGetApacheAuthShouldNotReferenceAuthPerl() {
         $conf = $this->GivenAnApacheAuthenticationConfForGuineaPigProject();
-        
+
         $this->assertNoPattern('/PerlAccessHandler/', $conf);
     }
 }
-
-?>
