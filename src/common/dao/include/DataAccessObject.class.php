4e9188057e2e5ed17d2e05ff0e4754a6b079e81a
Thomas Gerbet <thomas.gerbet@enalean.com>
2015-12-31 10:09:53 +0100

    request #8732: LDAP write does not properly create LDAP account from a new Tuleap account
    Informations in the DB table plugin_ldap_user are not created
    
    Change-Id: Ie8f075f9a2d26142502ca675c3677d08f4e7a400

--- src/common/dao/include/DataAccessObject.class.php
+++ src/common/dao/include/DataAccessObject.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012-2015. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once 'DataAccess.class.php';
@@ -42,9 +43,9 @@ class DataAccessObject {
     //! A constructor
     /**
     * Constructs the Dao
-    * @param $da instance of the DataAccess class
+    * @param $da DataAccess
     */
-    public function __construct($da = null) {
+    public function __construct(DataAccess $da = null) {
         $this->table_name = 'CLASSNAME_MUST_BE_DEFINE_FOR_EACH_CLASS';
         $this->da = $da ? $da : CodendiDataAccess::instance();
     }
