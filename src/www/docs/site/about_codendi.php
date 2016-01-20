74d3f117c7f2783780faaac8373a4a60d70e5480
lincoln <lincoln@56jin.com>
2016-01-20 15:04:19 +0800

    修改注释。

--- src/www/docs/site/about_codendi.php
+++ src/www/docs/site/about_codendi.php
@@ -4,15 +4,17 @@
 // Copyright 1999-2000 (c) The SourceForge Crew
 // http://sourceforge.net
 //
-// 
+//
+require_once ('pre.php');
 
-require_once('pre.php');
+$HTML->header ( array (
+		title => $Language->getText ( 'docs_site_about', 'title', array (
+				$GLOBALS ['sys_name'] 
+		) ) 
+) );
 
+include ($Language->getContent ( 'docman/about_codendi' ));
 
-$HTML->header(array(title=>$Language->getText('docs_site_about','title', array($GLOBALS['sys_name']))));
-
-include($Language->getContent('docman/about_codendi'));
-
-$HTML->footer(array());
+$HTML->footer ( array () );
 
 ?>
--- src/www/docs/site/index.php
+++ src/www/docs/site/index.php
@@ -8,23 +8,22 @@
 //
 
 /*
-        Docmentation Manager
-        by Quentin Cregan, SourceForge 06/2000
-*/
-
-require_once('pre.php');
-require_once('www/docman/doc_utils.php');
-
+ * Docmentation Manager
+ * by Quentin Cregan, SourceForge 06/2000
+ */
+require_once ('pre.php');
+require_once ('www/docman/doc_utils.php');
 
 $group_id = 1;
 
-echo $HTML->header(array('title'=> $Language->getText('docs_site_index','title')));
+echo $HTML->header ( array (
+		'title' => $Language->getText ( 'docs_site_index', 'title' ) 
+) );
 
-echo '<H2>'.$Language->getText('docs_site_index','title').'</H2>';
+echo '<H2>' . $Language->getText ( 'docs_site_index', 'title' ) . '</H2>';
 
+display_doc_list ( $group_id );
 
-display_doc_list($group_id);
-
-$HTML->footer(array());
+$HTML->footer ( array () );
 
 ?>
