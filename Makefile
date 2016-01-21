Enalean/tuleap

commit 6329bc1922e77c5ff2c6989a6f1da3525c2cd2f8

request #8779: Passwords contain invalid characters for the creation of a new dev instance
We have less entropy when the password is generated this way but it is good enough
especially for dev tools

Change-Id: I3af7d8d9f55597cc1819365624b37245332e6695

Makefile
@@ -227,9 +227,9 @@ rest_docker_snap_run:
 #
 
 .env:
-	@MYSQL_ROOT_PASSWORD=`head -c 32 /dev/urandom | base64` && echo "MYSQL_ROOT_PASSWORD=$$MYSQL_ROOT_PASSWORD" > .env
-	@LDAP_ROOT_PASSWORD=`head -c 32 /dev/urandom | base64` && echo "LDAP_ROOT_PASSWORD=$$LDAP_ROOT_PASSWORD" >> .env
-	@LDAP_MANAGER_PASSWORD=`head -c 32 /dev/urandom | base64` && echo "LDAP_MANAGER_PASSWORD=$$LDAP_MANAGER_PASSWORD" >> .env
+	@MYSQL_ROOT_PASSWORD=$(env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "MYSQL_ROOT_PASSWORD=$$MYSQL_ROOT_PASSWORD" > .env
+	@LDAP_ROOT_PASSWORD=$(env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "LDAP_ROOT_PASSWORD=$$LDAP_ROOT_PASSWORD" >> .env
+	@LDAP_MANAGER_PASSWORD=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "LDAP_MANAGER_PASSWORD=$$LDAP_MANAGER_PASSWORD" >> .env
 	@echo VIRTUAL_HOST=tuleap_web_1.tuleap-aio-dev.docker >> .env
 
 dev-setup: .env

commit 8b40072b5bb5fd6c6ead6a569b9a5f80aa339c3f

request #8795: Mac OS X users can setup a new dev environment
Fix regression introduced by 6329bc1922e77c5ff2c6989a6f1da3525c2cd2f8

Change-Id: I6c782d437114b0679010ef512bf437238bc8c8eb

Makefile
@@ -227,9 +227,9 @@ rest_docker_snap_run:
 #
 
 .env:
-	@MYSQL_ROOT_PASSWORD=$(env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "MYSQL_ROOT_PASSWORD=$$MYSQL_ROOT_PASSWORD" > .env
-	@LDAP_ROOT_PASSWORD=$(env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "LDAP_ROOT_PASSWORD=$$LDAP_ROOT_PASSWORD" >> .env
-	@LDAP_MANAGER_PASSWORD=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32) && echo "LDAP_MANAGER_PASSWORD=$$LDAP_MANAGER_PASSWORD" >> .env
+	@echo "MYSQL_ROOT_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" > .env
+	@echo "LDAP_ROOT_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" >> .env
+	@echo "LDAP_MANAGER_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" >> .env
 	@echo VIRTUAL_HOST=tuleap_web_1.tuleap-aio-dev.docker >> .env
 
 dev-setup: .env