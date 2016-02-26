ac9ed3169a3a4c76ee9d8479ce0233d9ed9ddd98
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2016-02-26 21:46:52 +0100

    request #8309 Get rid of PrototypeJS - hudson_tab.js
    Change-Id: I6dac401767c01e24973cf938b742286f1c74408b

--- plugins/hudson/www/js/hudson_tab.js
+++ plugins/hudson/www/js/hudson_tab.js
@@ -1,11 +1,35 @@
-function toggle_addurlform() {
-    Effect.toggle('hudson_add_job', 'slide', { duration: 0.3 });
-}
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-function toggle_iframe(joburl) {
-    if ( ! $('hudson_iframe_div').visible() ) {
-        Effect.toggle('hudson_iframe_div', 'appear', { duration: 0.3 });
+(function($) {
+    toggle_addurlform = function() {
+        $('#hudson_add_job').slideToggle(300);
     }
-    $('hudson_iframe').src = joburl;
-    $('link_show_only').href = joburl;
-}
+
+    toggle_iframe = function(joburl) {
+        var hudson_iframe_div = $('#hudson_iframe_div');
+
+        if (hudson_iframe_div.is(":visible") === false) {
+            hudson_iframe_div.show();
+        }
+
+        $('#hudson_iframe').attr('src', joburl);
+        $('#link_show_only').attr('href', joburl);
+    }
+})(jQuery);
