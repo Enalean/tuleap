4f48e05db820954ae7c428e754c1f6ebfb26515f
Thomas Gerbet <thomas.gerbet@enalean.com>
2016-03-02 17:44:55 +0100

    request #8918: LDAP directory bindings to a user group are kept after the ugroup removal
    Change-Id: I6ff2d6b625a85688efe1e0cb313887a61922640b

---
+++ plugins/ldap/db/mysql/updates/2016/201603031145_clean_ldap_ugroup_removed.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+class b201603031145_clean_ldap_ugroup_removed extends ForgeUpgrade_Bucket {
+    public function description() {
+        return 'Clean LDAP bindings to removed user groups';
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $sql = 'DELETE plugin_ldap_ugroup.* FROM plugin_ldap_ugroup LEFT JOIN ugroup USING(ugroup_id) WHERE ugroup.ugroup_id IS NULL;';
+
+        $res = $this->db->dbh->exec($sql);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occurred while cleaning the plugin_ldap_ugroup table.');
+        }
+    }
+}
