57fb11cde1926ca6464ce4048633de3130d44afb
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2016-03-09 11:41:00 +0100

    request #8948 Too many messages are displayed for a chart in error
    Change-Id: I4ab7fbee2be84be99ab706521183cb99924442be

--- plugins/graphontrackersv5/include/graphic-library/GraphOnTrackersV5_Engine_Pie.class.php
+++ plugins/graphontrackersv5/include/graphic-library/GraphOnTrackersV5_Engine_Pie.class.php
@@ -1,26 +1,27 @@
 <?php
 /* 
  * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
  *
  * Originally written by Mahmoud MAALEJ, 2006. STMicroelectronics.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
 require_once('common/chart/Chart_Pie.class.php');
-require_once('GraphOnTrackersV5_Engine.class.php');
 require_once('common/layout/ColorHelper.class.php');
 
 class GraphOnTrackersV5_Engine_Pie extends GraphOnTrackersV5_Engine {
@@ -33,11 +34,15 @@ class GraphOnTrackersV5_Engine_Pie extends GraphOnTrackersV5_Engine {
     var $legend;
     
     
-    function validData(){
+    public function validData(){
         if ((is_array($this->data)) && (array_sum($this->data) > 0)){
             return true;
-        }else{
-            echo " <p class='feedback_info'>".$GLOBALS['Language']->getText('plugin_graphontrackersv5_engine','no_datas',array($this->title))."</p>";                
+        } else {
+            $GLOBALS['Response']->addFeedback(
+                'error',
+                $GLOBALS['Language']->getText('plugin_graphontrackersv5_engine','no_datas',array($this->title))
+            );
+
             return false;
         }
     }
