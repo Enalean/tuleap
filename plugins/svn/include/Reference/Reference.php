d7bc58813c66952143e095f3d0dc245bcee17892
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2016-03-09 12:20:44 +0100

    Create plugin reference
    This task is part of story #8815 Manage cross references between svn
    revisions & the world
    
    This commit allows Tuleap to parse Subversion references
    for the new Subversion plugin.
    This kind of reference are like : svn #repo01/1
    
    If you add such a reference in an Artifact comment for instance,
    the reference will be clickable.
    
    For now, it does not work cross-project.
    Old Subersion reference must work.
    
    If you use the Docker stuff for you development setup,
    please use that trick to have working references:
    https://gerrit.tuleap.net/#/c/5046/1/src/common/include/Codendi_HTMLPurifier.class.php
    
    Change-Id: If8ecaa4051a94971cd8c7d92b7949b02550d9b74

---
+++ plugins/svn/include/Reference/Reference.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace Tuleap\Svn\Reference;
+
+use Tuleap\Svn\Repository\Repository;
+use Project;
+use SvnPlugin;
+use BackendLogger;
+
+class Reference extends \Reference {
+
+    public function __construct(Project $project, Repository $repository, $keyword, $revision_id) {
+        $base_id         = 0;
+        $visibility      = 'P';
+        $is_used         = 1;
+        $project_id      = $project->getId();
+        $repository_path = urlencode($repository->getFullName());
+
+        parent::__construct(
+            $base_id,
+            $keyword,
+            '',
+            SVN_BASE_URL . "/index.php?roottype=svn&view=rev&root=$repository_path&revision=$revision_id",
+            $visibility,
+            SvnPlugin::SERVICE_SHORTNAME,
+            SvnPlugin::SYSTEM_NATURE_NAME,
+            $is_used,
+            $project_id
+        );
+    }
+
+}
---
+++ plugins/svn/tests/Reference/ExtractorTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace Tuleap\Svn\Reference;
+
+use Tuleap\Svn\Repository\RepositoryManager;
+use Tuleap\Svn\Repository\CannotFindRepositoryException;
+use TuleapTestCase;
+
+require_once __DIR__ .'/../bootstrap.php';
+
+class ExtractorTest extends TuleapTestCase {
+
+    /**
+     * @var Extractor
+     */
+    private $extractor;
+
+    /**
+     * @var Project
+     */
+    private $project;
+
+    /**
+     * @var RepositoryManager
+     */
+    private $repository_manager;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->project            = stub('Project')->getID()->returns(101);
+        $this->repository_manager = mock('Tuleap\Svn\Repository\RepositoryManager');
+        $this->extractor          = new Extractor($this->repository_manager);
+    }
+
+    public function itReturnsFalseIfReferenceDoesNotProvideRepositoryName() {
+        $keyword = 'svn';
+        $value   = '1';
+
+        stub($this->project)->usesService('plugin_svn')->returns(true);
+
+        $this->assertFalse($this->extractor->getReference($this->project, $keyword, $value));
+    }
+
+    public function itReturnsFalseIfTheProjectDoesNotUseTheSubversionPlugin() {
+        $keyword = 'svn';
+        $value   = 'repo01/1';
+
+        stub($this->project)->usesService('plugin_svn')->returns(false);
+
+        $this->assertFalse($this->extractor->getReference($this->project, $keyword, $value));
+    }
+
+    public function itReturnsFalseIfTheProvidedRepositoryIsNotInTheCurrentProject() {
+        $keyword = 'svn';
+        $value   = 'repo02/1';
+
+        stub($this->project)->usesService('plugin_svn')->returns(true);
+        stub($this->repository_manager)
+            ->getRepositoryByName($this->project, 'repo02')
+            ->throws(new CannotFindRepositoryException());
+
+        $this->assertFalse($this->extractor->getReference($this->project, $keyword, $value));
+    }
+
+    public function itBuildsASubversionPluginReference() {
+        $keyword    = 'svn';
+        $value      = 'repo01/1';
+        $repository = stub('Tuleap\Svn\Repository\Repository')->getFullName()->returns('project01/repo01');
+
+        stub($this->project)->usesService('plugin_svn')->returns(true);
+        stub($this->repository_manager)
+            ->getRepositoryByName($this->project, 'repo01')
+            ->returns($repository);
+
+        $reference = $this->extractor->getReference($this->project, $keyword, $value);
+
+        $this->assertIsA(
+            $reference,
+            'Tuleap\Svn\Reference\Reference'
+        );
+
+        $this->assertEqual($reference->getGroupId(), 101);
+        $this->assertEqual($reference->getKeyword(), 'svn');
+    }
+
+}
