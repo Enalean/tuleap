906f269acc4ca591926a916955b64238163aa075
hosniah <ahmed.hosni@st.com>
2016-03-29 16:01:28 +0100

    request #7263: Display, clean and restore TV5 pending for deletion
    Change-Id: Id89837817091e9117bc01cf6b0f9b80df3137566

--- plugins/tracker/include/trackerPlugin.class.php
+++ plugins/tracker/include/trackerPlugin.class.php
@@ -120,6 +120,8 @@ class trackerPlugin extends Plugin {
             $this->_addHook(FULLTEXTSEARCH_EVENT_DOES_TRACKER_SERVICE_USE_UGROUP);
         }
 
+        $this->_addHook(Event::LIST_DELETED_TRACKERS, 'displayDeletedTrackers');
+
         return parent::getHooksAndCallbacks();
     }
 
@@ -206,7 +208,7 @@ class trackerPlugin extends Plugin {
     public function getSystemEventClass($params) {
         switch($params['type']) {
             case SystemEvent_TRACKER_V3_MIGRATION::NAME:
-                $params['class'] = 'SystemEvent_TRACKER_V3_MIGRATION';
+                $params['class']        = 'SystemEvent_TRACKER_V3_MIGRATION';
                 $params['dependencies'] = array(
                     $this->getMigrationManager(),
                 );
@@ -672,6 +674,16 @@ class trackerPlugin extends Plugin {
             $trackerManager = new TrackerManager();
             $trackerManager->deleteProjectTrackers($groupId);
         }
+    }
+
+    /**
+     * Display the list of trackers marked as deleted with the possibility to completely purge or restore a given deleted tracker.
+     *
+     * @return Void
+     */
+    function displayDeletedTrackers() {
+         $trackerManager = new TrackerManager();
+         $trackerManager->displayDeletedTrackers();
     }
 
    /**
