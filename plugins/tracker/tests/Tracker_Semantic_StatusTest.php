70536664ae5717f9541a0d5c9f87acd9444dc3a5
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2016-04-07 14:42:27 +0200

    request #9016 Tracker structure incomplete export in XML format
    If a semantic is bound to a deleted field, the XML export
    fails.
    
    Now, if you are in this case, the semantic is no more exported.
    
    Change-Id: Ide0dcf101fbc1f9933d8e8ba87d588cadd50dead

--- plugins/tracker/tests/Tracker_Semantic_StatusTest.php
+++ plugins/tracker/tests/Tracker_Semantic_StatusTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Enalean, 2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2016. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,13 +18,11 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once('bootstrap.php');
-Mock::generate('Tracker');
-Mock::generate('Tracker_FormElement_Field_List');
-require_once('common/language/BaseLanguage.class.php');
-Mock::generate('BaseLanguage');
 
-class Tracker_Semantic_StatusTest extends UnitTestCase {
+require_once('bootstrap.php');
+require_once('common/language/BaseLanguage.class.php');
+
+class Tracker_Semantic_StatusTest extends TuleapTestCase {
     private $xml_security;
 
     public function setUp() {
@@ -32,34 +30,39 @@ class Tracker_Semantic_StatusTest extends UnitTestCase {
 
         $this->xml_security = new XML_Security();
         $this->xml_security->enableExternalLoadOfEntities();
+
+        $this->tracker = mock('Tracker');
+        $this->field   = stub('Tracker_FormElement_Field_List')->getId()->returns(103);
+
+        $GLOBALS['Language'] = mock('BaseLanguage');
+        $GLOBALS['Language']->setReturnValue('getText','Status', array('plugin_tracker_admin_semantic','status_label'));
+        $GLOBALS['Language']->setReturnValue('getText','Define the status of an artifact', array('plugin_tracker_admin_semantic','status_description'));
     }
 
     public function tearDown() {
         $this->xml_security->disableExternalLoadOfEntities();
 
+        unset($GLOBALS['Language']);
+
         parent::tearDown();
     }
 
     public function testExport() {
-        $GLOBALS['Language'] = new MockBaseLanguage($this);
-        $GLOBALS['Language']->setReturnValue('getText','Status', array('plugin_tracker_admin_semantic','status_label'));
-        $GLOBALS['Language']->setReturnValue('getText','Define the status of an artifact', array('plugin_tracker_admin_semantic','status_description'));
+        $xml      = simplexml_load_file(dirname(__FILE__) . '/_fixtures/ImportTrackerSemanticStatusTest.xml');
+        $semantic = new Tracker_Semantic_Status($this->tracker, $this->field, array(806, 807, 808, 809));
+        $root     = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><tracker />');
 
-        $xml = simplexml_load_file(dirname(__FILE__) . '/_fixtures/ImportTrackerSemanticStatusTest.xml');
+        $array_xml_mapping = array(
+            'F14' => 103,
+            'values' => array(
+                'F14-V66' => 806,
+                'F14-V67' => 807,
+                'F14-V68' => 808,
+                'F14-V69' => 809,
+            )
+        );
 
-        $tracker = new MockTracker();
-        $f = new MockTracker_FormElement_Field_List();
-        $f->setReturnValue('getId', 103);
-        $tst = new Tracker_Semantic_Status($tracker, $f, array(806, 807, 808, 809));
-        $root = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><tracker />');
-        $array_xml_mapping = array('F14' => 103,
-                                   'values' => array(
-                                       'F14-V66' => 806,
-                                       'F14-V67' => 807,
-                                       'F14-V68' => 808,
-                                       'F14-V69' => 809,
-                                   ));
-        $tst->exportToXML($root, $array_xml_mapping);
+        $semantic->exportToXML($root, $array_xml_mapping);
 
         $this->assertEqual((string)$xml->shortname, (string)$root->semantic->shortname);
         $this->assertEqual((string)$xml->label, (string)$root->semantic->label);
@@ -68,6 +71,14 @@ class Tracker_Semantic_StatusTest extends UnitTestCase {
         $this->assertEqual(count($xml->open_values), count($root->semantic->open_values));
     }
 
-}
+    public function itDoesNotExportIfFieldIsNotExported() {
+        $semantic = new Tracker_Semantic_Status($this->tracker, $this->field);
+        $root     = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><tracker />');
 
-?>
+        $array_xml_mapping = array();
+
+        $semantic->exportToXML($root, $array_xml_mapping);
+
+        $this->assertEqual($root->count(), 0);
+    }
+}
