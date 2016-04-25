05ee23361d5ea80d022b7ca20c9a4e4efdbf4f6f
Nicolas Terray <nicolas.terray@enalean.com>
2016-04-25 16:15:43 +0200

    Spring Housekeeping
    Delete stuff that are not used anymore:
    
    * reviewboard conf
    * cucumber conf
    * svn-based upgrade system
    * lxc stuff (YAY docker)
    * old Docker stuff, we use Compose now (dev_start.sh)
    * migration scripts
    * old packaging scripts
    * various plugins
    * old phpcs scripts
    * …
    
    Change-Id: I4356a89043e7a0209150f79d22bf4198573893f8

--- tools/migration/orange/domain_name.sql
+++
@@ -1,3 +0,0 @@
-UPDATE groups SET http_domain = REPLACE(http_domain, 'yourdev.rd.francetelecom.fr', 'orangeforge.rd.francetelecom.fr');
-UPDATE service SET link = REPLACE(link,'yourdev.rd.francetelecom.fr','orangeforge.rd.francetelecom.fr') WHERE short_name = 'homepage';
-UPDATE service SET link = REPLACE(link,'https://', 'http://') WHERE short_name = 'homepage' AND link LIKE '%orangeforge.rd.francetelecom.fr%';
