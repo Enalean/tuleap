8b438b82b052c5eaad25b98bc48bf46dec66ba29
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2016-07-13 10:13:26 +0200

    Modal for adding task and adding new item
    This is part of story #9090 have computed fields compatible with artifact links v2
    
    Change-Id: I963164b811d69252d9684d0a80bc1c6d34b5f854

--- plugins/tracker/www/scripts/modal-in-place.js
+++ plugins/tracker/www/scripts/modal-in-place.js
@@ -84,6 +84,38 @@ tuleap.textarea = tuleap.textarea || { };
             }
         },
 
+        displayAutocomputed : function (element) {
+            element.find('.tracker_formelement_label').hide();
+            element.find('.tracker_formelement_edit').show();
+            element.find('.auto-computed').show();
+            element.find('.add-field').hide();
+            element.removeClass('in-edition');
+        },
+
+        displayInEdition : function (element) {
+            element.find('.tracker_formelement_edit').hide();
+            element.find('.tracker_formelement_label').show();
+            element.find('.auto-computed').hide();
+            element.find('.add-field').show();
+            element.addClass('in-edition');
+        },
+
+        switchValueToManualMode: function (field_id) {
+            var field_computed_is_autocomputed = document.getElementsByName("artifact[" + field_id + "][is_autocomputed]");
+            if (field_computed_is_autocomputed[0] !== undefined && field_computed_is_autocomputed[0] !== undefined) {
+                field_computed_is_autocomputed[0].value = '0';
+            }
+        },
+
+        switchValueToAutoComputedMode: function (field_id) {
+            var field_computed_manual_value    = document.getElementsByName("artifact[" + field_id + "][manual_value]");
+            var field_computed_is_autocomputed = document.getElementsByName("artifact[" + field_id + "][is_autocomputed]");
+            if (field_computed_manual_value[0] !== undefined && field_computed_is_autocomputed[0] !== undefined) {
+                field_computed_manual_value[0].value    = null;
+                field_computed_is_autocomputed[0].value = '1';
+            }
+        },
+
         loadCreateArtifactModal : function(tracker_id, artifact_link_id, callback) {
             var self = this;
 
@@ -103,6 +135,25 @@ tuleap.textarea = tuleap.textarea || { };
                 $('.tuleap-modal-main-panel form textarea').each( function(){
                     var element = $(this).get(0); //transform to prototype
                     self.enableRichTextArea(element);
+                });
+
+                $(".tracker_artifact_field-computed").each( function() {
+                    var $element                        = $(this);
+                    var $field_id                       = $(this).find('.add-field').data('field-id');
+                    var field_computed_manual_value    = document.getElementsByName("artifact[" + $field_id + "][manual_value]");
+                    var field_computed_is_autocomputed = document.getElementsByName("artifact[" + $field_id + "][is_autocomputed]");
+
+                    self.displayAutocomputed($element);
+                    $element.find('.tracker_formelement_edit').on('click', function () {
+                        self.displayInEdition($element);
+                        $element.off('click');
+                        self.switchValueToManualMode($field_id);
+                    });
+
+                    $element.find('.auto-compute').on('click', function () {
+                        self.displayAutocomputed($element);
+                        self.switchValueToAutoComputedMode($field_id);
+                    });
                 });
 
             }).fail(function() {
@@ -138,64 +189,31 @@ tuleap.textarea = tuleap.textarea || { };
                     }
                 });
 
-                function displayAutocomputed (element) {
-                    element.find('.tracker_formelement_label').hide();
-                    element.find('.tracker_formelement_edit').show();
-                    element.find('.auto-computed').show();
-                    element.find('.add-field').hide();
-                    element.removeClass('in-edition');
-                };
-
-                function displayInEdition (element) {
-                    element.find('.tracker_formelement_edit').hide();
-                    element.find('.tracker_formelement_label').show();
-                    element.find('.auto-computed').hide();
-                    element.find('.add-field').show();
-                    element.addClass('in-edition');
-                };
-
-                function switchValueToManualMode (field_id) {
-                    var field_computed_is_autocomputed = document.getElementsByName("artifact[" + field_id + "][is_autocomputed]");
-                    if (field_computed_is_autocomputed[0] !== undefined && field_computed_is_autocomputed[0] !== undefined) {
-                        field_computed_is_autocomputed[0].value = '0';
-                    }
-                };
-
-                function switchValueToAutoComputedMode (field_id) {
-                    var field_computed_manual_value    = document.getElementsByName("artifact[" + field_id + "][manual_value]");
-                    var field_computed_is_autocomputed = document.getElementsByName("artifact[" + field_id + "][is_autocomputed]");
-                    if (field_computed_manual_value[0] !== undefined && field_computed_is_autocomputed[0] !== undefined) {
-                        field_computed_manual_value[0].value    = null;
-                        field_computed_is_autocomputed[0].value = '1';
-                    }
-                };
-
                 $(".tracker_artifact_field").each( function() {
-                    var $element                       = $(this);
+                    var $element                        = $(this);
                     var $field_id                       = $(this).find('.add-field').data('field-id');
                     var field_computed_manual_value    = document.getElementsByName("artifact[" + $field_id + "][manual_value]");
                     var field_computed_is_autocomputed = document.getElementsByName("artifact[" + $field_id + "][is_autocomputed]");
 
                     if (field_computed_manual_value[0] !== undefined && field_computed_is_autocomputed[0] !== undefined) {
                         if (field_computed_manual_value[0].value) {
-                            displayInEdition($element);
+                            self.displayInEdition($element);
                         } else {
-                            displayAutocomputed($element);
+                            self.displayAutocomputed($element);
                         }
                     }
 
                     $element.find('.tracker_formelement_edit').on('click', function () {
-                        displayInEdition($element);
+                        self.displayInEdition($element);
                         $element.off('click');
-                        switchValueToManualMode($field_id);
+                        self.switchValueToManualMode($field_id);
                     });
 
                     $element.find('.auto-compute').on('click', function () {
-                        displayAutocomputed($element);
-                        switchValueToAutoComputedMode($field_id);
+                        self.displayAutocomputed($element);
+                        self.switchValueToAutoComputedMode($field_id);
                     });
                 });
-
             }).fail(function() {
                 tuleap.modal.hideLoad();
                 codendi.feedback.log('error', codendi.locales['tracker_modal_errors'].bad_request);
