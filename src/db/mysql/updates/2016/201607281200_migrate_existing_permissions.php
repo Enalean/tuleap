96f18a62af9e72f28cde527b616411d44f653833
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2016-08-10 17:36:24 +0200

    Do not add members to already existing FRS_Admin ugroups
    part of story #9281 define FRS global permissions
    
    To test you must:
    
    1. Erase existing frs admin perms:
    
      DELETE FROM frs_global_permissions WHERE permission_type = 'FRS_ADMIN';
    
    2. Erase existing FRS_Admin ugroups:
    
      DELETE ugroup_user.*
      FROM ugroup_user
       INNER JOIN ugroup USING (ugroup_id)
      WHERE ugroup.name = 'FRS_Admin';
    
      DELETE FROM ugroup WHERE name = 'FRS_Admin';
    
    3. Revert the execution of the current bucket:
    
      DELETE FROM forge_upgrade_bucket
      WHERE script LIKE '%/201607281200_migrate_existing_permissions.php';
    
    4. Create a ugroup named 'FRS_Admin' in a project with frs and add a
    user that was not a legacy file administrator (file_flags != 2 in
    user_group table for the current project)
    
    5. Run forgeupgrade command.
    
    Expected result:
    ----------------
    * There are two FRS_Admin ugroup in the project
    * The firt one (before forgeupgrade) does not change
    * The second one has only file administrators
    
    Change-Id: I5b522d42f92c9b94863cbf88ef150cc86445b6e6

--- src/db/mysql/updates/2016/201607281200_migrate_existing_permissions.php
+++ src/db/mysql/updates/2016/201607281200_migrate_existing_permissions.php
@@ -38,13 +38,15 @@ class b201607281200_migrate_existing_permissions extends ForgeUpgrade_Bucket
 
         $this->db->dbh->beginTransaction();
 
+        $last_ugroup_id = $this->getLastUGroupId();
+
         $sql = "INSERT INTO ugroup (name, description, group_id)
                     SELECT DISTINCT '$group_frs_admin', '$frs_admin_description', groups.group_id
-                        FROM groups
+                    FROM groups
                         INNER JOIN user_group ON user_group.group_id = groups.group_id
                     WHERE status <> 'D'
-                    AND groups.group_id <> 100
-                    AND file_flags = 2";
+                      AND groups.group_id <> 100
+                      AND file_flags = 2";
 
         if ($this->db->dbh->exec($sql) === false) {
             $this->rollBackOnError('An error occured while migrating admin frs permissions for ugroup'.$sql);
@@ -55,16 +57,21 @@ class b201607281200_migrate_existing_permissions extends ForgeUpgrade_Bucket
                         ugroup.ugroup_id,
                         user_id
                     FROM ugroup
-                    INNER JOIN user_group ON ugroup.group_id = user_group.group_id
+                      INNER JOIN user_group ON ugroup.group_id = user_group.group_id
                     WHERE file_flags = 2
-                    AND name = '$group_frs_admin'";
+                      AND name = '$group_frs_admin'
+                      AND ugroup_id > $last_ugroup_id";
 
         if ($this->db->dbh->exec($sql) === false) {
             $this->rollBackOnError('An error occured while migrating admin frs permissions for ugroup_user'.$sql);
         }
 
         $sql = "INSERT INTO frs_global_permissions (project_id, permission_type, ugroup_id)
-                    SELECT group_id, '$permission_frs_admin', ugroup_id FROM ugroup WHERE name = '$group_frs_admin'";
+                SELECT group_id, '$permission_frs_admin', ugroup_id
+                FROM ugroup
+                WHERE name = '$group_frs_admin'
+                  AND ugroup_id > $last_ugroup_id";
+
         if ($this->db->dbh->exec($sql) === false) {
             $this->rollBackOnError('An error occured while add admin frs permissions in new table'.$sql);
         }
@@ -76,5 +83,14 @@ class b201607281200_migrate_existing_permissions extends ForgeUpgrade_Bucket
     {
         $this->db->dbh->rollBack();
         throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete($message);
+    }
+
+    private function getLastUGroupId()
+    {
+        $sql = "SELECT MAX(ugroup_id) AS last_ugroup_id FROM ugroup";
+
+        $res = $this->db->dbh->query($sql)->fetch();
+
+        return $res['last_ugroup_id'];
     }
 }
