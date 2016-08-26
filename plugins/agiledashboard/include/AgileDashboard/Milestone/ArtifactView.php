6ed4aa08a8cfc6dfad60ce541905a0451cac0b4f
Nicolas Terray <nicolas.terray@enalean.com>
2016-08-26 09:52:07 +0200

    story #9427: Quickly switch between FRS/Scrum/Artifact release
    In new FRS view:
    [×] Add a tab leading to the linked artifact
    [×] Add a tab leading to the scrum milestones
    
    In Scrum milestone view:
    [×] Add a tab leading to the artifact
    [×] Add a tab leading to the FRS view
    
    In Artifact view:
    [×] Add a tab leading to the FRS view
    [×] Add a tab leading to the scrum milestones
    
    Change-Id: Ic550de008dee861f68ea7be8552918b486727eed

---
+++ plugins/agiledashboard/include/AgileDashboard/Milestone/ArtifactView.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\Milestone;
+
+use Planning_Milestone;
+use Codendi_Request;
+use PFUser;
+use Tracker_Artifact_View_View;
+
+class ArtifactView extends Tracker_Artifact_View_View
+{
+
+    /**
+     * @var Planning_Milestone
+     */
+    private $milestone;
+
+    public function __construct(Planning_Milestone $milestone, Codendi_Request $request, PFUser $user)
+    {
+        parent::__construct($milestone->getArtifact(), $request, $user);
+
+        $this->milestone = $milestone;
+    }
+
+    /** @see Tracker_Artifact_View_View::getTitle() */
+    public function getTitle()
+    {
+        return $GLOBALS['Language']->getText('plugin_agiledashboard', 'Milestone')
+            . ' <i class="icon-external-link"></i>';
+    }
+
+    /** @see Tracker_Artifact_View_View::getIdentifier() */
+    public function getIdentifier()
+    {
+        return "milestone";
+    }
+
+    /** @see Tracker_Artifact_View_View::fetch() */
+    public function fetch()
+    {
+        // Nothing to fetch as the tab is a redirect to the milestone
+    }
+
+    public function getURL()
+    {
+        return AGILEDASHBOARD_BASE_URL .'/?'. http_build_query(
+            array(
+                'group_id'    => $this->milestone->getGroupId(),
+                'planning_id' => $this->milestone->getPlanningId(),
+                'action'      => 'show',
+                'aid'         => $this->milestone->getArtifactId(),
+            )
+        );
+    }
+}
---
+++ plugins/frs/include/FRS/ArtifactView.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\FRS;
+
+use Codendi_Request;
+use PFUser;
+use Tracker_Artifact_View_View;
+use Tracker_Artifact;
+
+class ArtifactView extends Tracker_Artifact_View_View
+{
+    /**
+     * @var Tracker_Artifact
+     */
+    private $release_id;
+
+    public function __construct($release_id, Tracker_Artifact $artifact, Codendi_Request $request, PFUser $user)
+    {
+        parent::__construct($artifact, $request, $user);
+
+        $this->release_id = $release_id;
+    }
+
+    /** @see Tracker_Artifact_View_View::getTitle() */
+    public function getTitle()
+    {
+        return $GLOBALS['Language']->getText('plugin_frs', 'File_release')
+        . ' <i class="icon-external-link"></i>';
+    }
+
+    /** @see Tracker_Artifact_View_View::getIdentifier() */
+    public function getIdentifier()
+    {
+        return "frs";
+    }
+
+    /** @see Tracker_Artifact_View_View::fetch() */
+    public function fetch()
+    {
+        // Nothing to fetch as the tab is a redirect to the frs
+    }
+
+    public function getURL()
+    {
+        return '/file/shownotes.php?release_id=' . (int)$this->release_id;
+    }
+}
