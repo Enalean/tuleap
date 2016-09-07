ce1ee6a0825b628fb37c4f6bc44bbe3bc3d19fc6
Thomas Gerbet <thomas.gerbet@enalean.com>
2016-09-07 11:37:40 +0200

    Be able to use EPEL's ViewVC with SVN integrated in the Tuleap core
    This is part of story #8597 have a recent version of ViewVC
    
    For now, to be able to test you must remove viewvc-tuleap by hand
    (rpm -e --nodeps viewvc-tuleap) and then install the viewvc package.
    
    Change-Id: Ia669e02690f93c3590ef686701e178887ac1cf4c

--- src/www/svn/viewvc.php
+++ src/www/svn/viewvc.php
@@ -1,19 +1,33 @@
 <?php
-//
-// SourceForge: Breaking Down the Barriers to Open Source Development
-// Copyright 1999-2000 (c) The SourceForge Crew
-// http://sourceforge.net
-//
-// 
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright 1999-2000 (c) The SourceForge Crew
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Tuleap\SvnCore\ViewVC\ViewVCProxyFactory;
+use Tuleap\ViewVCVersionChecker;
 
 require_once('pre.php');
-require_once('viewvc_utils.php');
-require_once('www/svn/svn_utils.php');
 
 if (user_isloggedin()) {
     $vRoot = new Valid_String('root');
     $vRoot->required();
-    if(!$request->valid($vRoot)) {
+    if(! $request->valid($vRoot)) {
         exit_no_group();
     }
     $root = $request->get('root');
@@ -24,43 +38,10 @@ if (user_isloggedin()) {
     }
     $group_id = $project->getID();
 
-    $vRootType = new Valid_WhiteList('roottype', array('svn'));
-    $vRootType->setErrorMessage($Language->getText('svn_viewvc','bad_roottype'));
-    $vRootType->required();
-    if($request->valid($vRootType)) {
-
-  if (!svn_utils_check_access(user_getname(), $project->getSVNRootPath(), viewvc_utils_getfile("/svn/viewvc.php"))) {
-    exit_error($Language->getText('svn_viewvc','access_denied'), 
-	       $Language->getText('svn_viewvc','acc_den_comment',session_make_url("/project/memberlist.php?group_id=$group_id")));
-  }
-
-  viewvc_utils_track_browsing($group_id,'svn');
-
-  $display_header_footer = viewvc_utils_display_header();
-
-  if ($display_header_footer) {
-    $prefix_title = '';
-    if ($path = viewvc_utils_getfile("/svn/viewvc.php")) {
-        $prefix_title = basename($path) .' - ';
-    }
-    $GLOBALS['HTML']->addStylesheet('/viewvc-static/styles.css');
-    svn_header(array(
-        'title' => $prefix_title . $Language->getText('svn_utils','browse_tree'),
-        'path'  => '/'.urlencode(viewvc_utils_getfile("/svn/viewvc.php"))
-    ));
-  }
-
-  viewvc_utils_passcommand();
-
-  if ($display_header_footer) {
-    site_footer(array());
-  }
-    } else {
-        svn_header(array ('title'=>$Language->getText('svn_utils','browse_tree')));
-        site_footer(array());
-    }
+    $viewvc_version_checker = new ViewVCVersionChecker();
+    $viewvc_proxy_factory   = new ViewVCProxyFactory($viewvc_version_checker);
+    $viewvc_proxy           = $viewvc_proxy_factory->getViewVCProxy();
+    $viewvc_proxy->displayContent($project, $request);
 } else {
-  exit_not_logged_in();
+    exit_not_logged_in();
 }
-
-?>
