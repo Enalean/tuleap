Enalean/tuleap

commit 0c529cf27bfd3b7cafd3b95b3f2605f271b5530c

request #9407 HTTPS is not properly detected when behind a reverse proxy
Change-Id: I377c04775556b539b8afba5b32321c35885d4891

plugins/webdav/include/Webdav_URLVerification.class.php
@@ -64,16 +64,16 @@ class Webdav_URLVerification extends URLVerification {
      *
      * @return void
      */
-    public function assertValidUrl($server) {
+    public function assertValidUrl($server, HTTPRequest $request) {
         if (strcmp($server['HTTP_HOST'], $this->getWebDAVHost()) == 0 
             && strcmp($this->getWebDAVHost(), $GLOBALS['sys_default_domain']) != 0
             && strcmp($this->getWebDAVHost(), $GLOBALS['sys_https_host']) != 0
             ) {
-            if (!$this->isUsingSSL($server) && $GLOBALS['sys_force_ssl'] == 1) {
+            if (! $request->isSecure() && $GLOBALS['sys_force_ssl'] == 1) {
                 $this->forbiddenError();
             }
         } else {
-            parent::assertValidUrl($server);
+            parent::assertValidUrl($server, $request);
         }
     }
 

plugins/statistics/include/statisticsPlugin.class.php
@@ -237,12 +237,7 @@ class StatisticsPlugin extends Plugin {
     }
     
     private function getSoapUri() {
-        if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || $GLOBALS['sys_force_ssl'] == 1) {
-            $protocol = "https";
-        } else {
-            $protocol = "http";
-        }
-        return $protocol.'://'.$GLOBALS['sys_default_domain'].'/plugins/statistics/soap';
+        return HTTPRequest::instance()->getServerUrl().'/plugins/statistics/soap';
     }
     
     public function renderWSDL() {

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_ChangesetValue_Text.class.php
@@ -183,20 +183,11 @@ class Tracker_Artifact_ChangesetValue_Text extends Tracker_Artifact_ChangesetVal
      * @return string text to be displayed in mail notifications when the text has been changed
      */
     protected function fetchHtmlMailDiff($formated_diff, $artifact_id, $changeset_id) {
-        $protocol = $this->getServerProtocol();
-        $url      = $protocol.'://'.$GLOBALS['sys_default_domain'].TRACKER_BASE_URL.'/?aid='.$artifact_id.'#followup_'.$changeset_id;
+        $url      = HTTPRequest::instance()->getServerUrl().TRACKER_BASE_URL.'/?aid='.$artifact_id.'#followup_'.$changeset_id;
 
         return '<a href="'.$url.'">' . $GLOBALS['Language']->getText('plugin_tracker_include_artifact', 'goto_diff') . '</a>';
     }
 
-    private function getServerProtocol() {
-        if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || $GLOBALS['sys_force_ssl'] == 1) {
-            return 'https';
-        }
-
-        return 'http';
-    }
-    
     /**
      * Returns the "set to" for field added later
      *

plugins/tracker/www/soap/index.php
@@ -25,16 +25,7 @@
 require_once 'pre.php';
 require_once dirname(__FILE__).'/../../include/constants.php';
 
-// Check if we the server is in secure mode or not.
-if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || $GLOBALS['sys_force_ssl'] == 1) {
-    $protocol = "https";
-} else {
-    $protocol = "http";
-}
-
-$server_uri  = $protocol .'://'. ForgeConfig::get('sys_default_domain');
-$uri         = $server_uri . TRACKER_BASE_URL .'/soap';
-
+$request = HTTPRequest::instance();
 if ($request->exist('wsdl')) {
     // Use a static wsdl file
     //$wsdl = file_get_contents(TRACKER_BASE_DIR .'/tracker.wsdl');
@@ -45,6 +36,8 @@ if ($request->exist('wsdl')) {
     // Use nusoap to generate the wsdl
     require_once 'nusoap.php';
     require_once 'utils_soap.php';
+
+    $uri    = $request->getServerUrl() . TRACKER_BASE_URL .'/soap';
 
     $server = new soap_server();
     $server->configureWSDL('TuleapTrackerV5API',$uri,false,'rpc','http://schemas.xmlsoap.org/soap/http',$uri);

plugins/webdav/tests/Webdav_URLVerificationTest.php
@@ -20,19 +20,23 @@
 
 require_once 'bootstrap.php';
 
-Mock::generatePartial(
-    'Webdav_URLVerification',
-    'Webdav_URLVerificationTestVersion',
-    array('getWebDAVHost', 'forbiddenError', 'isException')
-);
+class Webdav_URLVerificationTest extends TuleapTestCase {
 
-class Webdav_URLVerificationTest extends UnitTestCase {
+    private $request;
+    private $webdavURLVerification;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->request = mock('HTTPRequest');
+        $this->webdavURLVerification = partial_mock('Webdav_URLVerification', array('getWebDAVHost', 'forbiddenError', 'isException'));
+    }
 
     public function tearDown() {
-        parent::tearDown();
         unset($GLOBALS['sys_force_ssl']);
         unset($GLOBALS['sys_default_domain']);
         unset($GLOBALS['sys_https_host']);
+        parent::tearDown();
     }
 
     function testAssertValidUrlHTTPAndForceSslEquals0() {
@@ -42,30 +46,28 @@ class Webdav_URLVerificationTest extends UnitTestCase {
         $GLOBALS['sys_https_host']     = 'example.com';
         $GLOBALS['sys_force_ssl'] = 0;
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectNever('isException'); // no parent call
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectNever('isException'); // no parent call
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 
     function testAssertValidUrlHTTPSAndForceSslEquals0() {
-        $server = array('HTTP_HOST' => 'webdav.codendi.org',
-                        'HTTPS'     => 'on');
+        $server = array('HTTP_HOST' => 'webdav.codendi.org');
+        stub($this->request)->isSecure()->returns(true);
 
         $GLOBALS['sys_default_domain'] = 'example.com';
         $GLOBALS['sys_https_host']     = 'example.com';
         $GLOBALS['sys_force_ssl']      = 0;
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectNever('isException'); // no parent call
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectNever('isException'); // no parent call
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 
     function testAssertValidUrlHTTPAndForceSslEquals1() {
@@ -75,59 +77,54 @@ class Webdav_URLVerificationTest extends UnitTestCase {
         $GLOBALS['sys_https_host']     = 'example.com';
         $GLOBALS['sys_force_ssl']      = 1;
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
 
-        $WebdavURLVerification->expectOnce('forbiddenError');
-        $WebdavURLVerification->expectNever('isException'); // no parent call
+        $this->webdavURLVerification->expectOnce('forbiddenError');
+        $this->webdavURLVerification->expectNever('isException'); // no parent call
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 
-    function testAssertValidUrlHTTPSAndForceSslEquals1() {
-        $server = array('HTTP_HOST' => 'webdav.codendi.org',
-                        'HTTPS'     => 'on');
+    function _testAssertValidUrlHTTPSAndForceSslEquals1() {
+        $server = array('HTTP_HOST' => 'webdav.codendi.org');
+        stub($this->request)->isSecure()->returns(true);
 
         $GLOBALS['sys_default_domain'] = 'example.com';
         $GLOBALS['sys_https_host']     = 'example.com';
         $GLOBALS['sys_force_ssl'] = 1;
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectNever('isException'); // no parent call
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectNever('isException'); // no parent call
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 
     function testAssertValidUrlNotPluginHost() {
         $server = array('HTTP_HOST' => 'codendi.org');
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'webdav.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectOnce('isException'); // parent call
-        $WebdavURLVerification->setReturnValue('isException', true);
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectOnce('isException'); // parent call
+        $this->webdavURLVerification->setReturnValue('isException', true);
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
-
 
     function testAssertValidUrlButWebdavHostIsDefaultDomain() {
         $server = array('HTTP_HOST' => 'a.codendi.org');
 
         $GLOBALS['sys_default_domain'] = 'a.codendi.org';
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'a.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'a.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectOnce('isException'); // parent call
-        $WebdavURLVerification->setReturnValue('isException', true);
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectOnce('isException'); // parent call
+        $this->webdavURLVerification->setReturnValue('isException', true);
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 
     function testAssertValidUrlButWebdavHostIsHttpsHost() {
@@ -136,14 +133,12 @@ class Webdav_URLVerificationTest extends UnitTestCase {
         $GLOBALS['sys_default_domain'] = 'example.com';
         $GLOBALS['sys_https_host']     = 'b.codendi.org';
 
-        $WebdavURLVerification = new Webdav_URLVerificationTestVersion($this);
-        $WebdavURLVerification->setReturnValue('getWebDAVHost', 'b.codendi.org');
+        $this->webdavURLVerification->setReturnValue('getWebDAVHost', 'b.codendi.org');
 
-        $WebdavURLVerification->expectNever('forbiddenError');
-        $WebdavURLVerification->expectOnce('isException'); // parent call
-        $WebdavURLVerification->setReturnValue('isException', true);
+        $this->webdavURLVerification->expectNever('forbiddenError');
+        $this->webdavURLVerification->expectOnce('isException'); // parent call
+        $this->webdavURLVerification->setReturnValue('isException', true);
 
-        $WebdavURLVerification->assertValidUrl($server);
+        $this->webdavURLVerification->assertValidUrl($server, $this->request);
     }
 }
-?>

src/common/REST/GateKeeper.class.php
@@ -19,23 +19,24 @@
 
 namespace Tuleap\REST;
 
-use \PFUser;
-use \Exception;
-use \ForgeConfig;
+use PFUser;
+use Exception;
+use ForgeConfig;
+use HTTPRequest;
 
 /**
  * Ensure that request has the right properties before going RESTFul
  */
 class GateKeeper {
 
-    public function assertAccess(PFUser $user) {
+    public function assertAccess(PFUser $user, HTTPRequest $request) {
         if ($this->isTokenBasedAuthentication($user)) {
-            if ($this->isHTTPS() || $this->canReachApiWithoutHTTPS()) {
+            if ($request->isSecure() || $this->canReachApiWithoutHTTPS()) {
                 return true;
             }
             throw new Exception('The API is only accessible over HTTPS');
         } else {
-            if ($this->isCSRFSafe()) {
+            if ($this->isCSRFSafe($request)) {
                 return true;
             }
             throw new Exception('Referer doesn\'t match host. CSRF tentative ?');
@@ -44,10 +45,6 @@ class GateKeeper {
 
     private function isTokenBasedAuthentication(PFUser $user) {
         return $user->isAnonymous();
-    }
-
-    private function isHTTPS() {
-        return isset($_SERVER['HTTPS']);
     }
 
     private function canReachApiWithoutHTTPS() {
@@ -62,23 +59,19 @@ class GateKeeper {
      * @todo We should really check based on a csrf token but no way to get it done yet
      * @return boolean
      */
-    private function isCSRFSafe() {
-        if ($this->isRequestFromSelf()) {
+    private function isCSRFSafe(HTTPRequest $request) {
+        if ($this->isRequestFromSelf($request)) {
             return true;
         }
         return false;
     }
 
-    private function isRequestFromSelf() {
-        return $this->getQueryHost() === $this->getRefererHost();
+    private function isRequestFromSelf(HTTPRequest $request) {
+        return $this->getQueryHost($request) === $this->getRefererHost();
     }
 
-    private function getQueryHost() {
-        if (isset($_SERVER['HTTP_HOST'])) {
-            $scheme = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
-            return $this->getUrlBase($scheme.$_SERVER['HTTP_HOST']);
-        }
-        return $this->getUrlBase(get_server_url());
+    private function getQueryHost(HTTPRequest $request) {
+        return $this->getUrlBase($request->getServerUrl());
     }
 
     private function getRefererHost() {

src/common/include/URLVerification.class.php
@@ -28,14 +28,6 @@ class URLVerification {
     protected $urlChunks = null;
 
     /**
-     * Constructor of the class
-     *
-     * @return void
-     */
-    function __construct() {
-    }
-
-    /**
      * Returns an array containing data for the redirection URL
      *
      * @return Array
@@ -136,17 +128,6 @@ class URLVerification {
     }
 
     /**
-     * Return true if given request is using SSL
-     *
-     * @param Array $server
-     *
-     * @return Boolean
-     */
-    public function isUsingSSL($server) {
-        return (isset($server['HTTPS']) && $server['HTTPS'] == 'on');
-    }
-
-    /**
      * Should we treat current request as an exception
      *
      * @param array $server
@@ -197,10 +178,10 @@ class URLVerification {
      *
      * @return String
      */
-    function getRedirectionURL($server) {
+    function getRedirectionURL(HTTPRequest $request, $server) {
         $chunks   = $this->getUrlChunks($server);
 
-        $location = $this->getRedirectLocation($server, $chunks);
+        $location = $this->getRedirectLocation($request, $server, $chunks);
 
         if (isset($chunks['script'])) {
             $location .= $chunks['script'];
@@ -210,18 +191,18 @@ class URLVerification {
         return $location;
     }
 
-    private function getRedirectLocation(array $server, array $chunks) {
+    private function getRedirectLocation(HTTPRequest $request, array $server, array $chunks) {
         if (isset($chunks['protocol']) || isset($chunks['host'])) {
-            return $this->rewriteProtocol($server, $chunks);
+            return $this->rewriteProtocol($request, $server, $chunks);
         }
         return '';
     }
 
-    private function rewriteProtocol(array $server, array $chunks) {
+    private function rewriteProtocol(HTTPRequest $request, array $server, array $chunks) {
         if (isset($chunks['protocol'])) {
             $location = $chunks['protocol']."://";
         } else {
-            if ($this->isUsingSSL($server)) {
+            if ($request->isSecure()) {
                 $location = "https://";
             } else {
                 $location = "http://";
@@ -244,25 +225,10 @@ class URLVerification {
      *
      * @return void
      */
-    public function verifyProtocol($server) {
-        if (!$this->isUsingSSL($server)) {
-            if ($GLOBALS['sys_force_ssl'] == 1) {
-                $this->urlChunks['protocol'] = 'https';
-            }
-        }
-    }
-
-    /**
-     * Modify the host name if needed
-     *
-     * @param Array $server
-     *
-     * @return void
-     *
-     */
-    public function verifyHost($server) {
-        if (!$this->isUsingSSL($server) && $GLOBALS['sys_force_ssl'] == 1) {
-            $this->urlChunks['host'] = $GLOBALS['sys_https_host'];
+    public function verifyProtocol(HTTPRequest $request) {
+        if (! $request->isSecure() && $GLOBALS['sys_force_ssl'] == 1) {
+            $this->urlChunks['protocol'] = 'https';
+            $this->urlChunks['host']     = $GLOBALS['sys_https_host'];
         }
     }
 
@@ -533,14 +499,13 @@ class URLVerification {
      *
      * @return void
      */
-    public function assertValidUrl($server) {
+    public function assertValidUrl($server, HTTPRequest $request) {
         if (!$this->isException($server)) {
-            $this->verifyProtocol($server);
-            $this->verifyHost($server);
+            $this->verifyProtocol($request);
             $this->verifyRequest($server);
             $chunks = $this->getUrlChunks();
             if (isset($chunks)) {
-                $location = $this->getRedirectionURL($server);
+                $location = $this->getRedirectionURL($request, $server);
                 $this->header($location);
             }
 

src/www/api/index.php
@@ -30,7 +30,7 @@ use Luracast\Restler\Defaults;
 
 try {
     $gate_keeper = new GateKeeper();
-    $gate_keeper->assertAccess(UserManager::instance()->getCurrentUser());
+    $gate_keeper->assertAccess(UserManager::instance()->getCurrentUser(), HTTPRequest::instance());
 } catch (Exception $exception) {
     header("HTTP/1.0 403 Forbidden");
     $GLOBALS['Response']->sendJSON(array(

commit 14d4e692b8afa1ef867c855a0d4450a75f07a4a2

request #9485: Webdav is broken due to a fatal error
Issue has been introduced by commit 0c529cf27bfd3b7cafd3b95b3f2605f271b5530c

Change-Id: Ifeff2af4485ba967433c59f6d278200d12d77a45

plugins/webdav/include/Webdav_URLVerification.class.php
@@ -29,11 +29,9 @@ class Webdav_URLVerification extends URLVerification {
      * Constructor of the class
      *
      * @param String $host
-     *
-     * @return void
      */
-    function __construct($host) {
-        parent::__construct();
+    public function __construct($host)
+    {
         $this->webdavHost = $host;
     }
 