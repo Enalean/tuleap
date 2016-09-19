faada082b2707051214e21fbbe794d4d8533c5bb
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2016-09-19 14:25:00 +0200

    New "Folders" plugin
    part of story #9468 import folders
    
    Its creates an empty plugins which depends on tracker.
    The plugin doesn't do anything for now
    
    Change-Id:I5dde224ee72349531c61c9f9260911f8bd3ef966

---
+++ plugins/artifactsfolders/include/artifactsfoldersPlugin.class.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Tuleap\ArtifactsFolders\ArtifactsFoldersPluginInfo;
+
+require_once 'autoload.php';
+
+class ArtifactsFoldersPlugin extends Plugin
+{
+
+    public function __construct($id)
+    {
+        parent::__construct($id);
+        $this->setScope(self::SCOPE_PROJECT);
+    }
+
+    public function getHooksAndCallbacks()
+    {
+        return parent::getHooksAndCallbacks();
+    }
+
+    /**
+     * @see Plugin::getDependencies()
+     */
+    public function getDependencies()
+    {
+        return array('tracker');
+    }
+
+    /**
+     * @return PluginInfo
+     */
+    public function getPluginInfo()
+    {
+        if (! $this->pluginInfo) {
+            $this->pluginInfo = new ArtifactsFoldersPluginInfo($this);
+        }
+        return $this->pluginInfo;
+    }
+}
