916eec6d90cc21915a599213dad1562e2cd032b2
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2016-09-30 17:50:49 +0200

    Warn that project admins are not removed during daily synchronization
    This is part of story #9396 have project members automatically synchronized with LDAP
    
    During the daily synchronization or when we bound manually project
    members to an LDAP group, the users are now removed if necessary from
    project members.
    
    Project administrators are kept and an entry is added in the project history
    to say that this user is not removed.
    
    Change-Id: I7cf818204dbdcccd0a4de520d348ab35c625adc1

---
+++ plugins/ldap/db/mysql/updates/2016/201609301657_add_bind_column.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201609301657_add_bind_column extends ForgeUpgrade_Bucket {
+    public function description() {
+        return 'Add bind option for project members';
+    }
+
+    public function preUp() {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up() {
+        $sql = "ALTER TABLE plugin_ldap_project_group ADD COLUMN bind_option varchar(255) NOT NULL default 'bind'";
+
+        $res = $this->db->dbh->exec($sql);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occurred while adding column in the plugin_ldap_project_group table.');
+        }
+    }
+}
