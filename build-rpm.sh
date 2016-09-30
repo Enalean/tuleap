49418203e49df4f51f27833e957a4a6ba48c66a3
Manuel VACELET <manuel.vacelet@enalean.com>
2016-09-30 11:32:49 +0200

    Fix script for release generation
    When we change the version number, we must first commit with VERSION updated
    but the script won't properly generate the package number because the tag is
    not available yet.
    
    Then if we detect that the last tag is different from the VERSION file, we
    decide it's a "-1" release.
    
    + Minor fix, as when there is no commit after the version, the package is -1,
    then when there is one commit it must be -2.
    
    Change-Id: I7838d70652b81e3451f4cda46e309ca6147eb3ca

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -d "$TULEAP_PATH" ]; then
     echo "*** ERROR: TULEAP_PATH is missing"
     exit 1
 fi
 
+if [ ! -d "$WORKSPACE" ]; then
+    echo "*** ERROR: WORKSPACE is missing"
+    exit 1
+fi
+
 DOCKERIMAGE=build-plugin-pullrequest-rpm
+
+PACKAGE_VERSION=$(cat VERSION | tr -d '[[:space:]]')
 
 RELEASE=1
 LAST_TAG=$(git describe --abbrev=0 --tags)
-NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
-if [ $NB_COMMITS -gt 0 ]; then
-    RELEASE=$NB_COMMITS
+if [ $LAST_TAG == $PACKAGE_VERSION ]; then
+    NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
+    if [ $NB_COMMITS -gt 0 ]; then
+	RELEASE=$NB_COMMITS
+    fi
 fi
 
 docker build -t $DOCKERIMAGE rpm
