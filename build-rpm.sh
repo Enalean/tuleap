f4c382f82b7152353f5f16cc7e73ca4d102d1436
Manuel VACELET <manuel.vacelet@enalean.com>
2016-09-30 17:42:30 +0200

    Backport version management from pullrequest
    Plus bump to 0.6 to have a proper versionnage
    
    Change-Id: I1752e9eb6873f03be1ca5b166afde65bac816602

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
+set -e
+
 if [ ! -d "$TULEAP_PATH" ]; then
     echo "*** ERROR: TULEAP_PATH is missing"
     exit 1
 fi
 
+if [ ! -d "$WORKSPACE" ]; then
+    echo "*** ERROR: WORKSPACE is missing"
+    exit 1
+fi
+
 DOCKERIMAGE=build-plugin-trafficlights-rpm
+
+PACKAGE_VERSION=$(cat VERSION | tr -d '[[:space:]]')
 
 RELEASE=1
 LAST_TAG=$(git describe --abbrev=0 --tags)
-NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
-if [ $NB_COMMITS -gt 0 ]; then
-    RELEASE=$NB_COMMITS
+if [ $LAST_TAG == $PACKAGE_VERSION ]; then
+    NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
+    if [ $NB_COMMITS -gt 0 ]; then
+	RELEASE=$(($NB_COMMITS + 1))
+    fi
 fi
 
 docker build -t $DOCKERIMAGE rpm
-docker run --rm -v $TULEAP_PATH:/tuleap  -v $PWD:/tuleap/plugins/trafficlights -v $WORKSPACE:/output -e UID=`id -u` -e GID=`id -g` -e RELEASE=$RELEASE $DOCKERIMAGE
+docker run --rm -v $TULEAP_PATH:/tuleap -v $PWD:/tuleap/plugins/trafficlights -v $WORKSPACE:/output -e UID=`id -u` -e GID=`id -g` -e RELEASE=$RELEASE $DOCKERIMAGE
