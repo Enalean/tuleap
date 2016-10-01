b16cf77c60a7cca14e7d2bad8c24cc2dedd31519
Manuel VACELET <manuel.vacelet@enalean.com>
2016-10-01 18:28:20 +0200

    Version 1.0.0
    Need a stable version to be tagged
    
    Change-Id: Iaa947c2291186d7252c4953bd68c37ddbc4a5716

--- packaging/build-rpm.sh
+++ packaging/build-rpm.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 # Find the path of this directory
 if [ -f "$0" ]; then
@@ -7,11 +9,15 @@ else
     mydir=$(dirname $(readlink -f $(which $0)))
 fi
 
+PACKAGE_VERSION=$(cat $mydir/../package.json | grep version | cut -d: -f 2 | tr -d ',' | tr -d '"' | tr -d " ")
+
 RELEASE=1
 LAST_TAG=$(git describe --abbrev=0 --tags)
-NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
-if [ $NB_COMMITS -gt 0 ]; then
-    RELEASE=$(($NB_COMMITS+1))
+if [ "$LAST_TAG" == "$PACKAGE_VERSION" ]; then
+    NB_COMMITS=$(git log --oneline $LAST_TAG..HEAD | wc -l)
+    if [ $NB_COMMITS -gt 0 ]; then
+	RELEASE=$(($NB_COMMITS+1))
+    fi
 fi
 
 docker run --rm -e UID=`id -u` -e GID=`id -g` -e RELEASE=$RELEASE -v $mydir/..:/realtime enalean/build-tuleap-realtime
