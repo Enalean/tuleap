Enalean/tuleap

commit ad980c116c1edd9dad8d56a7f73dca59c7aa2b9a

This is Tuleap 8.19.99.84
Change-Id: I0cf7022ef5c35e7dc52d79dd35db022dc57e2dee

plugins/git/include/Git/RemoteServer/Dao.class.php
@@ -176,6 +176,7 @@ class Git_RemoteServer_Dao extends DataAccessObject {
             WHERE id = $id";
         }
 
+        echo $sql; die();
         return $this->updateAndGetLastId($sql);
     }
 

VERSION
@@ -1,1 +1,1 @@
-8.19.99.83
+8.19.99.84

plugins/statistics/VERSION
@@ -1,1 +1,1 @@
-0.65
+0.66

commit 20d5a05993f2ebd5ef35e40088e3e4b1407bc9b1

request #9557: Gerrit remote server can not be saved anymore
Regression introduced by ad980c116c1edd9dad8d56a7f73dca59c7aa2b9a

Change-Id: I2cf713fb2ebd077785d9e09e6aecf7cb97aac2e6

plugins/git/include/Git/RemoteServer/Dao.class.php
@@ -176,7 +176,6 @@ class Git_RemoteServer_Dao extends DataAccessObject {
             WHERE id = $id";
         }
 
-        echo $sql; die();
         return $this->updateAndGetLastId($sql);
     }
 