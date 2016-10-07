c20d9d99de5f1e2f1394b9fb0fa22e25e6f335f9
Juliana Leclaire <juliana.leclaire@enalean.com>
2016-10-07 17:36:38 +0200

    Fancy cumulative
    This is a part of story #9463 see cumulative flow diagram
    
    To test: Run 'bower install' in kanban folder and 'npm run build'.
    
    Change-Id: I1ad7e9c64e971f54b16449da63580248ab9a4c9b

--- plugins/agiledashboard/www/js/kanban/src/app/reports-modal/reports-modal-controller.js
+++ plugins/agiledashboard/www/js/kanban/src/app/reports-modal/reports-modal-controller.js
@@ -20,131 +20,12 @@ function ReportsModalController(
     DiagramRestService
 ) {
     var self = this;
-    self.loading = true;
-    self.chartjs_options = {
-        responsive         : true,
-        maintainAspectRatio: false,
-        hover: {
-            onHover: function() {
-                angular.element(this.chart.canvas).removeClass('pointer-cursor');
-            }
-        },
-        legend: {
-            display : true,
-            position: 'right',
-            reverse : true,
-            onHover : function(event) {
-                angular.element(event.target).addClass('pointer-cursor');
-            }
-        },
-        tooltips: {
-            // This reverses the order
-            itemSort: function(a, b) {
-                if (a.datasetIndex < b.datasetIndex) {
-                    return 1;
-                } else if (a.datasetIndex > b.datasetIndex) {
-                    return -1;
-                }
-                return 0;
-            }
-        },
-        scales: {
-            xAxes: [{
-                type : 'time',
-                ticks: {
-                    fontColor: '#9c9c9c'
-                },
-                time: {
-                    displayFormats: {
-                        /// moment.js Date format
-                        day: gettextCatalog.getString('MM/DD')
-                    },
-                    minUnit: 'day'
-                }
-            }],
-            yAxes: [{
-                position: 'left',
-                stacked : true,
-                ticks   : {
-                    fontColor  : '#9c9c9c',
-                    beginAtZero: true
-                },
-                scaleLabel: {
-                    display    : true,
-                    labelString: gettextCatalog.getString('Nb. of cards'),
-                    fontColor  : '#9c9c9c'
-                }
-            }]
-        }
-    };
-    self.chartjs_labels = [];
-    self.chartjs_data   = [];
-    self.chartjs_series = [];
-    self.chartjs_colors = [];
-    self.kanban_label   = "";
-    self.params         = {
-        last_seven_days: {
-            number: 7,
-            time_unit: 'day',
-            interval_between_points: 1
-        },
-        last_month: {
-            number: 1,
-            time_unit: 'month',
-            interval_between_points: 1
-        },
-        last_three_months: {
-            number: 3,
-            time_unit: 'month',
-            interval_between_points: 7
-        },
-        last_six_months: {
-            number: 6,
-            time_unit: 'month',
-            interval_between_points: 7
-        },
-        last_year: {
-            number: 1,
-            time_unit: 'year',
-            interval_between_points: 7
-        }
-    };
-    self.value_last_data = self.params.last_seven_days;
-    self.key_last_data   = 'last_seven_days';
 
-    self.cancel         = function() { $modalInstance.dismiss('cancel'); };
-    self.init           = init;
-    self.loadData       = loadData;
+    /// moment.js Date format
+    var localized_format = gettextCatalog.getString('MM/DD');
+    var ISO_DATE_FORMAT  = 'YYYY-MM-DD';
 
-    self.init();
-
-    function init() {
-        self.loadData();
-    }
-    function setChartjsData(cumulative_flow_data) {
-        var first_column    = _.first(cumulative_flow_data.columns).values;
-        self.chartjs_labels = _.map(first_column, 'start_date');
-        self.chartjs_series = _.map(cumulative_flow_data.columns, 'label');
-        self.chartjs_data   = _.map(cumulative_flow_data.columns, function(column) {
-            return _.map(column.values, 'kanban_items_count');
-        });
-
-        var colors = computeGradientColors(self.chartjs_series.length);
-        self.chartjs_colors = _(colors).reverse()
-            .map(function(color) {
-                return {
-                    backgroundColor          : color.toString(),
-                    pointBackgroundColor     : color.toString(),
-                    pointHoverBackgroundColor: color.toString(),
-                    borderColor              : color.toString(),
-                    pointBorderColor         : color.toString(),
-                    pointHoverBorderColor    : color.toString(),
-                    pointHitRadius           : 20
-                };
-            }).value();
-    }
-
-    self.schemeCategory20cWithoutLightest = [
+    var schemeCategory20cWithoutLightest = [
         '#3182bd',
         '#6baed6',
         '#9ecae1',
@@ -163,19 +44,108 @@ function ReportsModalController(
         // '#dadaeb',
         '#636363',
         '#969696',
-        '#bdbdbd',
+        '#bdbdbd'
         // '#d9d9d9'
     ];
 
-    function computeGradientColors(nb_columns) {
-        var color_scale = d3.scaleOrdinal(self.schemeCategory20cWithoutLightest);
+    var color_scale = d3.scale.ordinal()
+        .range(schemeCategory20cWithoutLightest);
 
-        var colors = [];
-        for (var i = 0; i < nb_columns; i++) {
-            colors.push(color_scale(i));
+    self.options = {
+        chart: {
+            type: 'stackedAreaChart',
+            margin: {
+                top: 20,
+                right: 20,
+                bottom: 30,
+                left: 40
+            },
+            x: function (d) {
+                return moment(d.start_date).valueOf();
+            },
+            y: function (d) {
+                return d.kanban_items_count;
+            },
+            useVoronoi: false,
+            clipEdge: true,
+            duration: 100,
+            showControls: false,
+            useInteractiveGuideline: true,
+            xScale: d3.time.scale(),
+            xAxis: {
+                showMaxMin: false,
+                tickFormat: function (d) {
+                    return moment(d).format(localized_format);
+                }
+            },
+            yAxis: {
+                axisLabel: gettextCatalog.getString('Nb. of cards'),
+                axisLabelDistance: -20,
+                tickFormat: d3.format('d')
+            },
+            color: function(data, index) {
+                return color_scale(index);
+            },
+            legend: {
+                rightAlign: false,
+                margin: {
+                    bottom: 20
+                },
+                dispatch: {
+                    legendMouseover: function(d3_legend_data) {
+                        triggerAreaHighlight(d3_legend_data, 'mouseover');
+                    },
+                    legendMouseout: function(d3_legend_data) {
+                        triggerAreaHighlight(d3_legend_data, 'mouseout');
+                    }
+                }
+            }
         }
+    };
 
-        return colors;
+    self.loading      = true;
+    self.data         = [];
+    self.kanban_label = "";
+
+    self.params  = {
+        last_seven_days: {
+            number                 : 7,
+            time_unit              : 'day',
+            interval_between_points: 1
+        },
+        last_month: {
+            number                 : 1,
+            time_unit              : 'month',
+            interval_between_points: 1
+        },
+        last_three_months: {
+            number                 : 3,
+            time_unit              : 'month',
+            interval_between_points: 7
+        },
+        last_six_months: {
+            number                 : 6,
+            time_unit              : 'month',
+            interval_between_points: 7
+        },
+        last_year: {
+            number                 : 1,
+            time_unit              : 'year',
+            interval_between_points: 7
+        }
+    };
+    self.value_last_data = self.params.last_seven_days;
+    self.key_last_data   = 'last_seven_days';
+
+    self.cancel   = function() { $modalInstance.dismiss('cancel'); };
+    self.init     = init;
+    self.loadData = loadData;
+    self.onReady  = onReady;
+
+    self.init();
+
+    function init() {
+        self.loadData();
     }
 
     function loadData() {
@@ -183,17 +153,72 @@ function ReportsModalController(
         self.value_last_data = self.params[self.key_last_data];
         self.kanban_label    = SharedPropertiesService.getKanban().label;
         var kanban_id        = SharedPropertiesService.getKanban().id;
-        var start_date       = moment().subtract(self.value_last_data.number, self.value_last_data.time_unit).format('YYYY-MM-DD');
-        var end_date         = moment().format('YYYY-MM-DD');
+        var start_date       = moment().subtract(self.value_last_data.number, self.value_last_data.time_unit).format(ISO_DATE_FORMAT);
+        var end_date         = moment().add(1, 'days').format(ISO_DATE_FORMAT);
 
         DiagramRestService.getCumulativeFlowDiagram(
             kanban_id,
             start_date,
             end_date,
             self.value_last_data.interval_between_points
-        ).then(setChartjsData)
-         .finally(function() {
-             self.loading = false;
-         });
+        ).then(setGraphData)
+        .finally(function() {
+            self.loading = false;
+        });
+    }
+
+    function setGraphData(cumulative_flow_data) {
+        var color_domain = _.map(cumulative_flow_data.columns, function (data, index, columns) {
+            return (columns.length - 1) - index;
+        });
+        color_scale.domain(color_domain);
+
+        _.forEach(cumulative_flow_data.columns, function (column) {
+            column.key = column.label;
+
+            var data_for_today        = _.last(column.values);
+            data_for_today.start_date = moment(data_for_today.start_date).subtract(12, 'hours').format();
+        });
+
+        self.data = cumulative_flow_data.columns;
+    }
+
+    function onReady() {
+        var closed_columns_id = getCollapsedKanbanColumnsIds();
+
+        d3.selectAll('.nv-series')
+            .each(function (d3_legend_data) {
+                if (_.contains(closed_columns_id, d3_legend_data.id)) {
+                    var click_event = new MouseEvent('click');
+                    this.dispatchEvent(click_event);
+                }
+            });
+    }
+
+    function triggerAreaHighlight(d3_legend_data, event_type) {
+        d3.selectAll('.nv-area')
+            .each(function(d3_path_data) {
+                if (d3_legend_data.id === d3_path_data.id) {
+                    var event = new MouseEvent(event_type);
+                    this.dispatchEvent(event);
+
+                    return false;
+                }
+            });
+    }
+
+    function getCollapsedKanbanColumnsIds() {
+        var kanban = SharedPropertiesService.getKanban();
+
+        var all_columns = [].concat(kanban.columns);
+        all_columns.push(kanban.backlog);
+        all_columns.push(kanban.archive);
+
+        return _(all_columns)
+            .filter({ is_open: false })
+            .map(function (column) {
+                return column.id.toString();
+            })
+            .value();
     }
 }
