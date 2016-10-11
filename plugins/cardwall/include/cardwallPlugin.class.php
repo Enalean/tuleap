Enalean/tuleap

commit 8b814c271d9ed516d5f9abbff0331a19a1b08979

Plugins no longer append javascript to core Combined
=> Combined is a performance killer on slow iops servers, we need to ship
pre-built versions.

In order to build the assets run `npm i && npm run build` at the root of
tuleap sources.

Part of request #9299: Get rid of combined

Change-Id: I7abc7dd9c1aaa1a87e037f615030be5fa1fc6944

plugins/cardwall/include/cardwallPlugin.class.php
@@ -180,14 +180,12 @@ class cardwallPlugin extends Plugin {
         // Only show the js if we're actually in the Cardwall pages.
         // This stops styles inadvertently clashing with the main site.
         if ($this->isAgileDashboardOrTrackerUrl() && $this->canUseStandardJavsacript()) {
-            echo $this->getJavascriptIncludesForScripts(array(
-                'ajaxInPlaceEditorExtensions.js',
-                'cardwall.js',
-                'script.js',
-                'custom-mapping.js',
-                'CardsEditInPlace.js',
-                'fullscreen.js',
-            ));
+            $agiledashboard_plugin = PluginManager::instance()->getPluginByName('agiledashboard');
+            if ($agiledashboard_plugin->currentRequestIsForPlugin()) {
+                $tracker_plugin = PluginManager::instance()->getPluginByName('tracker');
+                echo $tracker_plugin->getMinifiedAssetHTML()."\n";
+            }
+            echo $this->getMinifiedAssetHTML()."\n";
         }
     }
 

.gitignore
@@ -48,3 +48,5 @@ npm-debug.log
 .DS_Store
 .sass-cache/
 *.css.map
+node_modules/
+plugins/*/www/assets/*.js

gulpfile.js
@@ -1,0 +1,48 @@
+'use strict';
+
+var gulp    = require('gulp'),
+    exec    = require('gulp-exec'),
+    install = require('gulp-install'),
+    fs      = require('fs'),
+    path    = require('path'),
+    plugins = getAllPluginsWithGulpfile();
+
+
+
+gulp.task('default', ['build']);
+
+gulp.task('install', plugins.map(function (plugin) { return 'install-'+ plugin; }));
+gulp.task('build', plugins.map(function (plugin) { return 'build-'+ plugin; }));
+
+plugins.forEach(function (plugin) {
+    gulp.task('install-'+ plugin, function () {
+        return installInPlugin(plugin);
+    });
+
+    gulp.task('build-'+ plugin, ['install-'+ plugin], function () {
+        return buildInPlugin(plugin);
+    });
+})
+
+function installInPlugin(plugin) {
+    return gulp.src('./plugins/'+ plugin +'/package.json')
+        .pipe(gulp.dest('./plugins/'+ plugin +'/'))
+        .pipe(install());
+}
+
+function buildInPlugin(plugin) {
+    return gulp.src('plugins/'+ plugin +'/gulpfile.js')
+        .pipe(exec('node_modules/.bin/gulp --gulpfile=<%= file.path %>'));
+}
+
+function getAllPluginsWithGulpfile() {
+    var plugins_path = './plugins';
+
+    return fs.readdirSync(plugins_path).filter(function (file) {
+            try {
+                return fs.statSync(path.join(plugins_path, file, 'gulpfile.js')).isFile();
+            } catch (e) {
+                return false;
+            }
+        });
+}

plugins/agiledashboard/gulpfile.js
@@ -1,0 +1,26 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+
+var version = require("fs").readFileSync("VERSION", "utf8").trim();
+
+var jsFiles = [
+    'www/js/display-angular-feedback.js',
+    'www/js/MilestoneContent.js',
+    'www/js/planning-view.js',
+    'www/js/ContentFilter.js',
+    'www/js/home.js'
+],
+jsDest = 'www/assets/';
+
+gulp.task('concat', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('agiledashboard.' + version + '.js'))
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(jsFiles, ['concat']);
+});
+
+gulp.task('build',  ['concat']);
+gulp.task('default', ['build']);

plugins/agiledashboard/include/agiledashboardPlugin.class.php
@@ -49,7 +49,6 @@ class AgileDashboardPlugin extends Plugin {
             require_once dirname(__FILE__) .'/../../tracker/include/autoload.php';
             $this->_addHook('cssfile', 'cssfile', false);
             $this->_addHook('javascript_file');
-            $this->_addHook(Event::COMBINED_SCRIPTS, 'combined_scripts', false);
             $this->_addHook(TRACKER_EVENT_INCLUDE_CSS_FILE, 'tracker_event_include_css_file', false);
             $this->_addHook(TRACKER_EVENT_TRACKERS_DUPLICATED, 'tracker_event_trackers_duplicated', false);
             $this->_addHook(TRACKER_EVENT_BUILD_ARTIFACT_FORM_ACTION, 'tracker_event_build_artifact_form_action', false);
@@ -359,15 +358,17 @@ class AgileDashboardPlugin extends Plugin {
     public function javascript_file() {
         if ($this->isAnAgiledashboardRequest()) {
             if ($this->isPlanningV2URL()) {
-                echo '<script type="text/javascript" src="' . $this->getPluginPath() . '/js/planning-v2/bin/assets/planning-v2.js"></script>';
+                echo '<script type="text/javascript" src="' . $this->getPluginPath() . '/js/planning-v2/bin/assets/planning-v2.js"></script>'."\n";
             } elseif ($this->isKanbanURL()) {
                 echo '<script type="text/javascript" src="js/resize-content.js"></script>'."\n";
+            } else {
+                echo $this->getMinifiedAssetHTML()."\n";
             }
         }
     }
 
     private function isAnAgiledashboardRequest() {
-        return strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0;
+        return $this->currentRequestIsForPlugin();
     }
 
     private function isKanbanURL() {
@@ -381,19 +382,6 @@ class AgileDashboardPlugin extends Plugin {
         $pane_info_identifier = new AgileDashboard_PaneInfoIdentifier();
 
         return $pane_info_identifier->isPaneAPlanningV2($request->get('pane'));
-    }
-
-    public function combined_scripts($params) {
-        $params['scripts'] = array_merge(
-            $params['scripts'],
-            array(
-                $this->getPluginPath().'/js/display-angular-feedback.js',
-                $this->getPluginPath().'/js/MilestoneContent.js',
-                $this->getPluginPath().'/js/planning-view.js',
-                $this->getPluginPath().'/js/ContentFilter.js',
-                $this->getPluginPath().'/js/home.js',
-            )
-        );
     }
 
     public function process(Codendi_Request $request) {

plugins/cardwall/gulpfile.js
@@ -1,0 +1,27 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+
+var version = require("fs").readFileSync("VERSION", "utf8").trim();
+
+var jsFiles = [
+    'www/js/ajaxInPlaceEditorExtensions.js',
+    'www/js/cardwall.js',
+    'www/js/script.js',
+    'www/js/custom-mapping.js',
+    'www/js/CardsEditInPlace.js',
+    'www/js/fullscreen.js'
+],
+jsDest = 'www/assets/';
+
+gulp.task('concat', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('cardwall.' + version + '.js'))
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(jsFiles, ['concat']);
+});
+
+gulp.task('build',  ['concat']);
+gulp.task('default', ['build']);

plugins/docman/gulpfile.js
@@ -1,0 +1,24 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+var version = require("fs").readFileSync("VERSION", "utf8").trim();
+
+var jsFiles = [
+    '../../src/www/scripts/behaviour/behaviour.js',
+    'www/scripts/docman.js',
+    'www/scripts/embedded_file.js',
+    'www/scripts/ApprovalTableReminder.js'
+],
+jsDest = 'www/assets/';
+
+gulp.task('concat', function() {
+    return gulp.src(jsFiles)
+        .pipe(concat('docman.' + version + '.js'))
+        .pipe(gulp.dest(jsDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(jsFiles, ['concat']);
+});
+
+gulp.task('build',  ['concat']);
+gulp.task('default', ['build']);

plugins/docman/include/docmanPlugin.class.php
@@ -51,7 +51,7 @@ class DocmanPlugin extends Plugin {
         parent::__construct($id);
 
         $this->_addHook('cssfile',                           'cssFile',                           false);
-        $this->_addHook('javascript_file',                   'jsFile',                            false);
+        $this->_addHook('javascript_file');
         $this->_addHook('logs_daily',                        'logsDaily',                         false);
         $this->_addHook('permission_get_name',               'permission_get_name',               false);
         $this->_addHook('permission_get_object_type',        'permission_get_object_type',        false);
@@ -98,7 +98,6 @@ class DocmanPlugin extends Plugin {
 
         $this->_addHook('fill_project_history_sub_events', 'fillProjectHistorySubEvents', false);
         $this->_addHook('project_is_deleted',              'project_is_deleted',          false);
-        $this->_addHook(Event::COMBINED_SCRIPTS,           'combinedScripts',             false);
         $this->addHook(Event::PROCCESS_SYSTEM_CHECK);
         $this->addHook(Event::SERVICES_TRUNCATED_EMAILS);
 
@@ -251,21 +250,18 @@ class DocmanPlugin extends Plugin {
     function cssFile($params) {
         // Only show the stylesheet if we're actually in the Docman pages.
         // This stops styles inadvertently clashing with the main site.
-        if (strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0 ||
+        if ($this->currentRequestIsForPlugin() ||
             strpos($_SERVER['REQUEST_URI'], '/widgets/') === 0 
         ) {
             echo '<link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/style.css" />'."\n";
         }
     }
     
-    function jsFile($params) {
+    function javascript_file($params) {
         // Only show the stylesheet if we're actually in the Docman pages.
         // This stops styles inadvertently clashing with the main site.
-        if (strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0) {
-            echo '<script type="text/javascript" src="/scripts/behaviour/behaviour.js"></script>'."\n";
-            echo '<script type="text/javascript" src="/scripts/scriptaculous/scriptaculous.js"></script>'."\n";
-            echo '<script type="text/javascript" src="'.$this->getPluginPath().'/scripts/docman.js"></script>'."\n";
-            echo '<script type="text/javascript" src="'.$this->getPluginPath().'/scripts/embedded_file.js"></script>'."\n";
+        if ($this->currentRequestIsForPlugin()) {
+            echo $this->getMinifiedAssetHTML()."\n";
         }
     }
 
@@ -859,17 +855,6 @@ class DocmanPlugin extends Plugin {
             $this->controller[$controller]->setRequest($request);
         }
         return $this->controller[$controller];
-    }
-
-    /**
-     * Append scripts to the combined JS file
-     *
-     * @param Array $params parameters of the hook
-     *
-     * @return Void
-     */
-    public function combinedScripts($params) {
-        $params['scripts'] = array_merge($params['scripts'], array($this->getPluginPath().'/scripts/ApprovalTableReminder.js'));
     }
 
     public function fulltextsearch_event_fetch_all_document_search_types($params) {

commit 99bff76bac0d30f3b2c79c16afffa5635e0f7c82

request #9568 Access to a tracker page will throw a fatal error if the cardwall is enabled but not the agiledashboard
Regression has been introduced by commit 8b814c271d9ed516d5f9abbff0331a19a1b08979

Change-Id: Ie671d352508302c943339fca1b05d658dc61c8c1

plugins/cardwall/include/cardwallPlugin.class.php
@@ -203,8 +203,8 @@ class cardwallPlugin extends Plugin {
         // Only show the js if we're actually in the Cardwall pages.
         // This stops styles inadvertently clashing with the main site.
         if ($this->isAgileDashboardOrTrackerUrl() && $this->canUseStandardJavsacript()) {
-            $agiledashboard_plugin = PluginManager::instance()->getPluginByName('agiledashboard');
-            if ($agiledashboard_plugin->currentRequestIsForPlugin()) {
+            $agiledashboard_plugin = PluginManager::instance()->getAvailablePluginByName('agiledashboard');
+            if ($agiledashboard_plugin && $agiledashboard_plugin->currentRequestIsForPlugin()) {
                 $tracker_plugin = PluginManager::instance()->getPluginByName('tracker');
                 echo $tracker_plugin->getMinifiedAssetHTML()."\n";
             }