22b36884d0f89a1d0148a75d47a12dfade0d7970
Humbert <humbert.moreaux@enalean.com>
2016-10-13 09:57:29 +0200

    Plugin BotMattermost - Sender Services
    part of the story #9129 Define bots to send notification to Mattermost
    
    Add Sender Services to allow additional plugin of botmattermost to send messages using this service.
    Will be used by BotMattermost-Git and BotMattermost-AgileDashboard
    
    Change-Id: I1377849b908e2b00f2b2d3a30b00aabd06d07b2c

---
+++ include/SenderServices/Sender.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\BotMattermost\SenderServices;
+
+use Exception;
+use Tuleap\BotMattermost\Bot\Bot;
+
+class Sender
+{
+
+    private $encoder_message;
+    private $notification_maker;
+    private $client;
+
+    public function __construct(
+        EncoderMessage $encoder_message,
+        NotificationBuilder $notification_maker,
+        ClientBotMattermost $client
+    ) {
+        $this->encoder_message    = $encoder_message;
+        $this->notification_maker = $notification_maker;
+        $this->client             = $client;
+    }
+
+    /**
+     * @param Bot[] $bots
+     */
+    public function pushNotifications(array $bots)
+    {
+        $text = $this->notification_maker->buildNotificationText();
+        foreach ($bots as $bot) {
+            $this->pushNotificationsForEachChannels($bot, $text);
+        }
+    }
+
+    public function send($post_string, $url)
+    {
+        try {
+            $this->client->sendMessage($post_string, $url);
+        } catch (Exception $ex) {
+            //Do nothing
+        }
+    }
+
+    private function pushNotificationsForEachChannels(Bot $bot, $text)
+    {
+        $channels_names = $bot->getChannelsNames();
+        if (count($channels_names) > 0) {
+            foreach ($channels_names as $channel) {
+                $message = $this->encoder_message->generateMessage(
+                    $bot,
+                    $text,
+                    $channel
+                );
+                $this->send($message, $bot->getWebhookUrl());
+            }
+        } else {
+            $message = $this->encoder_message->generateMessage($bot, $text);
+            $this->send($message, $bot->getWebhookUrl());
+        }
+    }
+}
