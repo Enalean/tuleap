d4234cf3012a0889a908214427a23a029995a066
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2016-11-08 11:04:17 +0100

    Modal for TV3 deletion
    part of story #9586 Get rid of Bootstrap in site admin / configuration
    
    Change-Id: Idf32b3c57f09bd759cad479540730d95b3182753

--- src/www/tracker/admin/restore.php
+++ src/www/tracker/admin/restore.php
@@ -25,69 +25,63 @@ use Tuleap\Tracker\ArtifactPendingDeletionPresenter;
 
 require_once('pre.php');
 
-require_once('common/include/GroupFactory.class.php');
-require_once('common/tracker/ArtifactTypeFactory.class.php');
-require_once('common/tracker/ArtifactType.class.php');
-require_once('common/tracker/ArtifactCanned.class.php');
-require_once('common/tracker/ArtifactFieldFactory.class.php');
-require_once('common/tracker/ArtifactField.class.php');
-require_once('common/tracker/ArtifactFieldSetFactory.class.php');
-require_once('common/tracker/ArtifactFieldSet.class.php');
-require_once('common/tracker/ArtifactReport.class.php');
-require_once('common/tracker/ArtifactReportFactory.class.php');
-require_once('common/tracker/ArtifactReportField.class.php');
-require_once('common/tracker/Artifact.class.php');
-require_once('common/tracker/ArtifactRuleFactory.class.php');
-require_once('common/tracker/ArtifactPendingDeletionPresenter.php');
-require_once('../include/ArtifactTypeHtml.class.php');
-require_once('../include/ArtifactCannedHtml.class.php');
-require_once('../include/ArtifactReportHtml.class.php');
-require_once('../include/ArtifactHtml.class.php');
+session_require(array('group' => '1', 'admin_flags' => 'A'));
 
-session_require(array('group'=>'1','admin_flags'=>'A'));
+$GLOBALS['HTML']->includeFooterJavascriptFile('/scripts/admin/trackers-pending-removal.js');
 
 $pm      = ProjectManager::instance();
 $request = HTTPRequest::instance();
-$func = $request->get('func');
-	switch ( $func ) {
-	case 'restore':
-	    if ($group = $pm->getProject($request->getValidated('group_id', 'GroupId'))) {
-            $ath =  new ArtifactType($group, $atid);
-            if (!$ath->restore()) {
-              $feedback = $Language->getText('tracker_admin_restore','restore_failed');
+$func    = $request->get('func');
+switch ($func) {
+    case 'restore':
+        if ($group = $pm->getProject($request->getValidated('group_id', 'GroupId'))) {
+            $atid =  $request->get('atid');
+            $ath  = new ArtifactType($group, $atid);
+            if (! $ath->restore()) {
+                $feedback = $GLOBALS['Language']->getText('tracker_admin_restore', 'restore_failed');
             } else {
-              $feedback = $Language->getText('tracker_admin_restore','tracker_restored');
+                $feedback = $GLOBALS['Language']->getText('tracker_admin_restore', 'tracker_restored');
             }
         }
-		break;
+        break;
 
-	case 'delay':
-	    if ($group = $pm->getProject($request->getValidated('group_id', 'GroupId'))) {
-            $ath =  new ArtifactType($group, $request->getValidated('atid', 'uint'));
+    case 'delay':
+        if ($group = $pm->getProject($request->getValidated('group_id', 'GroupId'))) {
+            $ath = new ArtifactType($group, $request->getValidated('atid', 'uint'));
             // just check date >= today
 
-            if (!$ath->delay($delay_date)) {
-              if ($ath->isError())
-                exit_error($Language->getText('global','error'),$ath->getErrorMessage()." | ".$Language->getText('tracker_admin_restore','delay_failed'));
-              exit_error($Language->getText('global','error'),$Language->getText('tracker_admin_restore','delay_failed'));
+            if (! $ath->delay($delay_date)) {
+                if ($ath->isError()) {
+                    exit_error(
+                        $GLOBALS['Language']->getText('global', 'error'),
+                        $ath->getErrorMessage() . " | " . $GLOBALS['Language']->getText(
+                            'tracker_admin_restore', 'delay_failed'
+                        )
+                    );
+                }
+                exit_error(
+                    $GLOBALS['Language']->getText('global', 'error'),
+                    $GLOBALS['Language']->getText('tracker_admin_restore', 'delay_failed')
+                );
             } else {
-              $feedback = $Language->getText('tracker_admin_restore','delayed_deletion');
+                $feedback = $GLOBALS['Language']->getText('tracker_admin_restore', 'delayed_deletion');
             }
         }
-		break;
+        break;
 
 
-	case 'delete':
+    case 'delete':
         // Create field factory
         if ($group = $pm->getProject($request->getValidated('group_id', 'GroupId'))) {
-            $atid = $request->getValidated('atid', 'uint');
-            $ath =  new ArtifactType($group, $atid);
-            $atf = new ArtifactTypeFactory($group);
+            $atid           = $request->getValidated('atid', 'uint');
+            $ath            = new ArtifactType($group, $atid);
+            $atf            = new ArtifactTypeFactory($group);
             $art_field_fact = new ArtifactFieldFactory($ath);
 
             // Then delete all the fields informations
-            if ( !$art_field_fact->deleteFields($atid) ) {
-                exit_error($Language->getText('global','error'),$art_field_fact->getErrorMessage());
+            if (! $art_field_fact->deleteFields($atid)) {
+                exit_error($GLOBALS['Language']->getText('global', 'error'), $art_field_fact->getErrorMessage());
+
                 return false;
             }
 
@@ -95,24 +89,25 @@ $func = $request->get('func');
             // Create field factory
             $art_report_fact = new ArtifactReportFactory();
 
-            if ( !$art_report_fact->deleteReports($atid) ) {
-                exit_error($Language->getText('global','error'),$art_report_fact->getErrorMessage());
+            if (! $art_report_fact->deleteReports($atid)) {
+                exit_error($GLOBALS['Language']->getText('global', 'error'), $art_report_fact->getErrorMessage());
+
                 return false;
             }
 
             // Delete the artifact type itself
-            if ( !$atf->deleteArtifactType($atid) ) {
-                exit_error($Language->getText('global','error'),$atf->getErrorMessage());
+            if (! $atf->deleteArtifactType($atid)) {
+                exit_error($GLOBALS['Language']->getText('global', 'error'), $atf->getErrorMessage());
             }
-            $feedback = $Language->getText('tracker_admin_restore','tracker_deleted');
+            $feedback = $GLOBALS['Language']->getText('tracker_admin_restore', 'tracker_deleted');
         }
-		break;
+        break;
 
 
-	default:
-	  break;
-	} // switch
-$group  = $pm->getProject(1);
+    default:
+        break;
+} // switch
+$group = $pm->getProject(1);
 
 $renderer = new AdminPageRenderer();
 $renderer->header($GLOBALS['Language']->getText('tracker_admin_restore', 'pending_deletions'));
@@ -129,11 +124,11 @@ if (TrackerV3::instance()->available()) {
 
     while ($tracker = db_fetch_array($trackers)) {
         $tracker_list[] = array(
-           'group_artifact_id' => $tracker['group_artifact_id'],
-           'project_name'      => $tracker['project_name'],
-           'name'              => $tracker['name'],
-           'deletion_date'     => date("Y-m-d", $tracker['deletion_date']),
-           'group_id'          => $tracker['group_id'],
+            'group_artifact_id' => $tracker['group_artifact_id'],
+            'project_name' => $tracker['project_name'],
+            'name' => $tracker['name'],
+            'deletion_date' => date("Y-m-d", $tracker['deletion_date']),
+            'group_id' => $tracker['group_id'],
         );
     }
 
