9b018e80d941fa0a9f38654aaab59ebbf79dec25
Benjamin Dauton <benjamin.dauton@enalean.com>
2016-11-18 15:04:49 +0100

    story #9587: Most recent logins
    Change-Id: Icc8025e58b94bc4c58a57dfc65b1927524be7812

--- src/www/admin/lastlogins.php
+++ src/www/admin/lastlogins.php
@@ -2,38 +2,45 @@
 //
 // SourceForge: Breaking Down the Barriers to Open Source Development
 // Copyright 1999-2000 (c) The SourceForge Crew
+// Copyright 2016 Enalean SAS
 // http://sourceforge.net
 //
 //
 
 require_once('pre.php');
 
+use Tuleap\Admin\AdminPageRenderer;
+use Tuleap\Admin\MostRecentLoginsPresenter;
+use Tuleap\Admin\MostRecentLoginPresenter;
+
+$GLOBALS['HTML']->includeFooterJavascriptFile('/scripts/admin/most-recent-logins.js');
 
 session_require(array('group'=>'1','admin_flags'=>'A'));
 
 $res_logins = db_query("SELECT session.user_id AS user_id,"
-	. "session.ip_addr AS ip_addr,"
-	. "session.time AS time,"
-	. "user.user_name AS user_name FROM session,user "
-	. "WHERE session.user_id=user.user_id AND "
-	. "session.user_id>0 AND session.time>0 ORDER BY session.time DESC LIMIT 5000");
-if (db_numrows($res_logins) < 1) exit_error("No records found","There must be an error somewhere.");
+    . "session.ip_addr AS ip_addr,"
+    . "session.time AS time,"
+    . "user.user_name AS user_name FROM session,user "
+    . "WHERE session.user_id=user.user_id AND "
+    . "session.user_id>0 AND session.time>0 ORDER BY session.time DESC LIMIT 5000"
+);
 
-$HTML->header(array('title'=>$Language->getText('admin_lastlogins','title'), 'main_classes' => array('tlp-framed')));
+$most_recent_login_presenters = array();
 
-print '<h1>'.$Language->getText('admin_lastlogins','recent').'</h1>';
-print '<table class="tlp-table">';
-print '<tbody>';
 while ($row_logins = db_fetch_array($res_logins)) {
-	print '<tr>';
-	print "<td>$row_logins[user_name]</td>";
-	print "<td>$row_logins[ip_addr]</td>";
-	print "<td>" . date($Language->getText('system','datefmt'),$row_logins['time']) . "</td>";
-	print '</tr>';
+    $most_recent_login_presenters[] = new MostRecentLoginPresenter(
+        $row_logins['user_name'],
+        $row_logins['ip_addr'],
+        $row_logins['time']
+    );
 }
-print '</tbody>';
-print '</table>';
 
-$HTML->footer(array());
+$most_recent_logins_presenter = new MostRecentLoginsPresenter($most_recent_login_presenters);
 
-?>
+$admin_page = new AdminPageRenderer();
+$admin_page->renderAPresenter(
+    $Language->getText('admin_lastlogins', 'title'),
+    ForgeConfig::get('codendi_dir') . '/src/templates/admin/most_recent_logins/',
+    'most-recent-logins',
+    $most_recent_logins_presenter
+);
