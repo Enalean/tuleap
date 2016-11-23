Enalean/tuleap

commit 833d40a9c4c437c49ad76e8462fb8a81aa9b0908

request #9403: User info endpoint should not be mandatory
This endpoint is not mandatory as we can see in the specification [1] [2]
so it should not be mandatory on our side.

For example, CoreOS dex does not support it [3] or Yahoo does not gives it
in the discovery file they provide [4].

[1] https://openid.net/specs/openid-connect-core-1_0.html#CodeFlowSteps
[2] https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata
[3] https://github.com/coreos/dex/blob/master/Documentation/oidc-notes.md
[4] https://login.yahoo.com/.well-known/openid-configuration

Change-Id: I636a8d3c782d6f0daa919466190b73eb91cd7333

plugins/openidconnectclient/include/OpenIDConnectClient/Authentication/Flow.php
@@ -173,4 +173,20 @@ class Flow extends Basic {
         $this->clientInfo->fromArray($this->options->get(self::OPT_CLIENT_INFO, array()));
         return $this->clientInfo;
     }
+
+    /**
+     * @return array
+     */
+    public function getUserInfo($access_token)
+    {
+        $user_info_request  = $this->createUserInfoRequest($access_token);
+        $user_info_endpoint = $user_info_request->getUserInfoEndpoint();
+
+        if (empty($user_info_endpoint)) {
+            return array();
+        }
+
+        $user_info_response = $this->getUserInfoDispatcher()->sendUserInfoRequest($user_info_request);
+        return $user_info_response->getClaims();
+    }
 }

plugins/openidconnectclient/include/OpenIDConnectClient/Administration/Controller.php
@@ -97,7 +97,7 @@ class Controller {
         $name                   = $request->get('name');
         $authorization_endpoint = $request->get('authorization_endpoint');
         $token_endpoint         = $request->get('token_endpoint');
-        $userinfo_endpoint      = $request->get('userinfo_endpoint');
+        $userinfo_endpoint      = $request->get('userinfo_endpoint') ? $request->get('userinfo_endpoint') : '';
         $client_id              = $request->get('client_id');
         $client_secret          = $request->get('client_secret');
         $icon                   = $request->get('icon');
@@ -141,7 +141,7 @@ class Controller {
         $name                   = $request->get('name');
         $authorization_endpoint = $request->get('authorization_endpoint');
         $token_endpoint         = $request->get('token_endpoint');
-        $userinfo_endpoint      = $request->get('userinfo_endpoint');
+        $userinfo_endpoint      = $request->get('userinfo_endpoint') ? $request->get('userinfo_endpoint') : '';
         $client_id              = $request->get('client_id');
         $client_secret          = $request->get('client_secret');
         $icon                   = $request->get('icon');

plugins/openidconnectclient/include/OpenIDConnectClient/Provider/ProviderManager.php
@@ -215,13 +215,15 @@ class ProviderManager {
         $icon,
         $color
     ) {
-        $string_validator   = new Valid_String();
-        $http_uri_validator = new Valid_HTTPSURI();
+        $string_validator            = new Valid_String();
+        $http_uri_validator          = new Valid_HTTPSURI();
         $http_uri_validator->required();
+        $userinfo_endpoint_validator = new Valid_HTTPSURI();
 
         return $string_validator->validate($name) && $string_validator->validate($client_id) &&
             $string_validator->validate($client_secret) && $http_uri_validator->validate($authorization_endpoint) &&
-            $http_uri_validator->validate($token_endpoint) && $http_uri_validator->validate($userinfo_endpoint) &&
+            $http_uri_validator->validate($token_endpoint) &&
+            $userinfo_endpoint_validator->validate($userinfo_endpoint) &&
             $string_validator->validate($icon) && $string_validator->validate($color);
     }
 

plugins/openidconnectclient/templates/administration-providers-modal-create.mustache
@@ -16,7 +16,7 @@
         <div class="modal-body">
             <div class="row-fluid">
                 <div class="span8">
-                    <label for="name">{{ name }}</label>
+                    <label for="name">{{ name }}<span class="highlight"> *</span></label>
                     <input class="provider-name" type="text" name="name" id="name" required>
                 </div>
 
@@ -41,22 +41,22 @@
                 </div>
             </div>
 
-            <label for="authorization_endpoint">{{ authorization_endpoint }}</label>
+            <label for="authorization_endpoint">{{ authorization_endpoint }}<span class="highlight"> *</span></label>
             <input type="url" pattern="https://.+" name="authorization_endpoint" id="authorization_endpoint" placeholder="{{ https_placeholder }}" required>
 
-            <label for="token_endpoint">{{ token_endpoint }}</label>
+            <label for="token_endpoint">{{ token_endpoint }}<span class="highlight"> *</span></label>
             <input type="url" pattern="https://.+"name="token_endpoint" id="token_endpoint" placeholder="{{ https_placeholder }}" required>
 
             <label for="userinfo_endpoint">{{ userinfo_endpoint }}</label>
-            <input class="provider-user-info-endpoint" type="url" pattern="https://.+" name="userinfo_endpoint" id="userinfo_endpoint" placeholder="{{ https_placeholder }}" required>
+            <input class="provider-user-info-endpoint" type="url" pattern="https://.+" name="userinfo_endpoint" id="userinfo_endpoint" placeholder="{{ https_placeholder }}">
 
             <p class="info"><i class="icon-info-sign"></i> {{ client_help }}</p>
             <p class="info provider-callback-url">{{ callback_url }}</p>
 
-            <label for="client_id">{{ client_id }}</label>
+            <label for="client_id">{{ client_id }}<span class="highlight"> *</span></label>
             <input type="text" name="client_id" id="client_id" required>
 
-            <label for="client_secret">{{ client_secret }}</label>
+            <label for="client_secret">{{ client_secret }}<span class="highlight"> *</span></label>
             <input type="text" name="client_secret" id="client_secret" required>
 
             <p class="preview-label">{{ preview }}</p>

plugins/openidconnectclient/templates/administration-providers-modal-update.mustache
@@ -17,7 +17,7 @@
         <div class="modal-body">
             <div class="row-fluid">
                 <div class="span8">
-                    <label for="name-{{ getId }}">{{ name }}</label>
+                    <label for="name-{{ getId }}">{{ name }}<span class="highlight"> *</span></label>
                     <input class="provider-name" type="text" name="name" id="name-{{ getId }}" value="{{ getName }}" required>
                 </div>
 
@@ -42,22 +42,22 @@
                 </div>
             </div>
 
-            <label for="authorization_endpoint-{{ getId }}">{{ authorization_endpoint }}</label>
+            <label for="authorization_endpoint-{{ getId }}">{{ authorization_endpoint }}<span class="highlight"> *</span></label>
             <input type="url" pattern="https://.+" name="authorization_endpoint" id="authorization_endpoint-{{ getId }}" placeholder="{{ https_placeholder }}" value="{{ getAuthorizationEndpoint }}" required>
 
-            <label for="token_endpoint-{{ getId }}">{{ token_endpoint }}</label>
+            <label for="token_endpoint-{{ getId }}">{{ token_endpoint }}<span class="highlight"> *</span></label>
             <input type="url" pattern="https://.+"name="token_endpoint" id="token_endpoint-{{ getId }}" placeholder="{{ https_placeholder }}" value="{{ getTokenEndpoint }}" required>
 
             <label for="userinfo_endpoint-{{ getId }}">{{ userinfo_endpoint }}</label>
-            <input class="provider-user-info-endpoint" type="url" pattern="https://.+" name="userinfo_endpoint" id="userinfo_endpoint-{{ getId }}" placeholder="{{ https_placeholder }}" value="{{ getUserInfoEndpoint }}" required>
+            <input class="provider-user-info-endpoint" type="url" pattern="https://.+" name="userinfo_endpoint" id="userinfo_endpoint-{{ getId }}" placeholder="{{ https_placeholder }}" value="{{ getUserInfoEndpoint }}">
 
             <p class="info"><i class="icon-info-sign"></i> {{ client_help }}</p>
             <p class="info provider-callback-url">{{ callback_url }}</p>
 
-            <label for="client_id-{{ getId }}">{{ client_id }}</label>
+            <label for="client_id-{{ getId }}">{{ client_id }}<span class="highlight"> *</span></label>
             <input type="text" name="client_id" id="client_id-{{ getId }}" value="{{ getClientId }}" required>
 
-            <label for="client_secret-{{ getId }}">{{ client_secret }}</label>
+            <label for="client_secret-{{ getId }}">{{ client_secret }}<span class="highlight"> *</span></label>
             <input type="text" name="client_secret" id="client_secret-{{ getId }}" value="{{ getClientSecret }}" required>
 
             <p class="preview-label">{{ preview }}</p>

plugins/openidconnectclient/templates/administration-providers.mustache
@@ -21,8 +21,10 @@
                 <span class="provider-label">{{ token_endpoint }}</span>
                 <span>{{ getTokenEndpoint }}</span>
 
+                {{# getUserInfoEndpoint }}
                 <span class="provider-label">{{ userinfo_endpoint }}</span>
                 <span>{{ getUserInfoEndpoint }}</span>
+                {{/ getUserInfoEndpoint }}
             </div>
             <div class="provider-client">
                 <span class="provider-label">{{ client_id }}</span>

plugins/openidconnectclient/tests/Provider/ProviderManagerTest.php
@@ -43,6 +43,25 @@ class ProviderManagerTest extends TuleapTestCase {
         );
     }
 
+    public function itCreatesNewProviderWithAnEmptyUserInfoEndpoint()
+    {
+        $provider_dao     = mock('Tuleap\OpenIDConnectClient\Provider\ProviderDao');
+        $provider_manager = new ProviderManager($provider_dao);
+
+        $provider_dao->expectOnce('create');
+
+        $provider_manager->create(
+            'Provider',
+            'https://example.com/auth',
+            'https://example.com/token',
+            '',
+            'ID',
+            'Secret',
+            'github',
+            'fiesta_red'
+        );
+    }
+
     public function itUpdatesProvider() {
         $provider_dao     = mock('Tuleap\OpenIDConnectClient\Provider\ProviderDao');
         $provider_dao->setReturnValue('save', true);

plugins/openidconnectclient/www/themes/default/css/style.scss
@@ -102,6 +102,10 @@ body.openid-connect-link {
                     }
                 }
             }
+
+            .highlight {
+                color: red;
+            }
         }
     }
 

commit 47fc6c7b04269b8092b3219117d47b3e541840ce

Request the scopes email and profile when the provider is the unique authentication endpoint
This contribution also fix an issue introduced by commit 833d40a9c4c437c49ad76e8462fb8a81aa9b0908:
the previous patch allowed to use a provider without user info endpoint but the information
was not retrieved anymore in any cases.

This is part of story #9658: log in into a Tuleap instance using only an OpenID Connect provider

Change-Id: I89de0926dfaaf0f3a418dcff484f013ba1c9ab44

plugins/openidconnectclient/include/OpenIDConnectClient/Authentication/Flow.php
@@ -103,7 +103,11 @@ class Flow extends Basic {
      */
     public function getAuthorizationRequestUri(Provider $provider, $return_to) {
         $this->setOptions($provider);
-        $authorization_request = $this->createAuthorizationRequest();
+        $scope = 'openid';
+        if ($provider->isUniqueAuthenticationEndpoint()) {
+            $scope = 'openid profile email';
+        }
+        $authorization_request = $this->createAuthorizationRequest($scope);
         return $this->getAuthorizationDispatcher()->createAuthorizationRequestUri(
             $authorization_request,
             $provider,
@@ -179,13 +183,12 @@ class Flow extends Basic {
      */
     public function getUserInfo($access_token)
     {
-        $user_info_request  = $this->createUserInfoRequest($access_token);
-        $user_info_endpoint = $user_info_request->getUserInfoEndpoint();
-
+        $user_info_endpoint = $this->getClientInfo()->getUserInfoEndpoint();
         if (empty($user_info_endpoint)) {
             return array();
         }
 
+        $user_info_request  = $this->createUserInfoRequest($access_token);
         $user_info_response = $this->getUserInfoDispatcher()->sendUserInfoRequest($user_info_request);
         return $user_info_response->getClaims();
     }