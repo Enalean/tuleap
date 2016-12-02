e483f45b1ddb4fbf38260cdd35c22d1537abcb2b
Thomas Gerbet <thomas.gerbet@enalean.com>
2016-12-02 15:24:11 +0100

    Do not redirect immediatly if the platform is restricted to the anonymous
    This is part of story #9658: log in into a Tuleap instance using only an OpenID Connect provider
    
    Change-Id: I1a95e613dbee37c1d7ae489852cca05e07070a59

---
+++ plugins/openidconnectclient/include/OpenIDConnectClient/Login/SpecificLoginPresenter.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\OpenIDConnectClient\Login;
+
+use ForgeConfig;
+
+class SpecificLoginPresenter
+{
+    /**
+     * @var array
+     */
+    public $providers;
+
+    public function __construct(array $providers)
+    {
+        $this->providers = $providers;
+    }
+
+    public function help_email()
+    {
+        return ForgeConfig::get('sys_email_admin');
+    }
+
+    public function need_help()
+    {
+        return $GLOBALS['Language']->getText('account_login', 'need_help');
+    }
+
+    public function help_subject()
+    {
+        return $GLOBALS['Language']->getText('account_login', 'help_subject', array(ForgeConfig::get('sys_name')));
+    }
+
+    public function account_login_login_with_tuleap()
+    {
+        return $GLOBALS['Language']->getText('account_login', 'login_with_tuleap', array(ForgeConfig::get('sys_name')));
+    }
+
+    public function login_intro()
+    {
+        return file_get_contents($GLOBALS['Language']->getContent('account/login_intro', null, null, '.html'));
+    }
+}
