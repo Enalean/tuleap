Enalean/tuleap

commit 9ff88e32e4eb139b1669beb58c483ade2d4140ef

request #9202 Make phpwiki compatible with php 5.6
I tried to remove all LongArray usage but I kept the following snippet in case
of

 /** PHP5 deprecated old-style globals if !(bool)ini_get('register_long_arrays').
  *  See Bug #1180115
  * We want to work with those old ones instead of the new superglobals,
  * for easier coding.
  */
foreach (array('SERVER','REQUEST','GET','POST','SESSION','ENV','COOKIE') as $k) {
    if (!isset($GLOBALS['HTTP_'.$k.'_VARS']) and isset($GLOBALS['_'.$k]))
        $GLOBALS['HTTP_'.$k.'_VARS'] =& $GLOBALS['_'.$k];
}
unset($k);

Change-Id: I92eefe7c4e277cf1e404d0602457ac0600932c24

src/common/wiki/phpwiki/lib/Request.php
@@ -37,10 +37,10 @@ class Request {
         switch($this->get('REQUEST_METHOD')) {
         case 'GET':
         case 'HEAD':
-            $this->args = &$GLOBALS['HTTP_GET_VARS'];
+            $this->args = &$_GET;
             break;
         case 'POST':
-            $this->args = &$GLOBALS['HTTP_POST_VARS'];
+            $this->args = &$_POST;
             break;
         default:
             $this->args = array();
@@ -58,10 +58,10 @@ class Request {
     }
 
     function get($key) {
-        if (!empty($GLOBALS['HTTP_SERVER_VARS']))
-            $vars = &$GLOBALS['HTTP_SERVER_VARS'];
+        if (!empty($_SERVER))
+            $vars = &$_SERVER;
         else // cgi or other servers than Apache
-            $vars = &$GLOBALS['HTTP_ENV_VARS'];
+            $vars = &$_ENV;
 
         if (isset($vars[$key]))
             return $vars[$key];
@@ -541,7 +541,7 @@ class Request {
     
     function _fix_multipart_form_data () {
         if (preg_match('|^multipart/form-data|', $this->get('CONTENT_TYPE')))
-            $this->_strip_leading_nl($GLOBALS['HTTP_POST_VARS']);
+            $this->_strip_leading_nl($_POST);
     }
     
     function _strip_leading_nl(&$var) {
@@ -590,7 +590,7 @@ class Request_SessionVars {
 class Request_CookieVars {
     
     function get($key) {
-        $vars = &$GLOBALS['HTTP_COOKIE_VARS'];
+        $vars = &$_COOKIE;
         if (isset($vars[$key])) {
             @$val = unserialize(base64_decode($vars[$key]));
             if (!empty($val))
@@ -603,7 +603,7 @@ class Request_CookieVars {
     }
 
     function get_old($key) {
-        $vars = &$GLOBALS['HTTP_COOKIE_VARS'];
+        $vars = &$_COOKIE;
         if (isset($vars[$key])) {
             @$val = unserialize(base64_decode($vars[$key]));
             if (!empty($val))
@@ -623,7 +623,7 @@ class Request_CookieVars {
     	if (defined('MAIN_setUser') and $key = 'WIKI_ID') return;
         if (defined('WIKI_XMLRPC') and WIKI_XMLRPC) return;
 
-        $vars = &$GLOBALS['HTTP_COOKIE_VARS'];
+        $vars = &$_COOKIE;
         if (is_numeric($persist_days)) {
             $expires = time() + (24 * 3600) * $persist_days;
         }
@@ -647,12 +647,12 @@ class Request_CookieVars {
         if (isset($deleted[$key])) return;
         if (defined('WIKI_XMLRPC') and WIKI_XMLRPC) return;
         
-        $vars = &$GLOBALS['HTTP_COOKIE_VARS'];
+        $vars = &$_COOKIE;
         if (!defined('COOKIE_DOMAIN'))
             @setcookie($key, '', 0);
         else    
             @setcookie($key, '', 0, COOKIE_DOMAIN);
-        unset($GLOBALS['HTTP_COOKIE_VARS'][$key]);
+        unset($_COOKIE[$key]);
         unset($_COOKIE[$key]);
         $deleted[$key] = 1;
     }
@@ -667,15 +667,12 @@ class Request_CookieVars {
 */
 class Request_UploadedFile {
     function getUploadedFile($postname) {
-        global $HTTP_POST_FILES;
 
         // Against php5 with !ini_get('register-long-arrays'). See Bug #1180115
-        if (empty($HTTP_POST_FILES) and !empty($_FILES))
-            $HTTP_POST_FILES =& $_FILES;
-        if (!isset($HTTP_POST_FILES[$postname]))
+        if (!isset($_FILES[$postname]))
             return false;
         
-        $fileinfo =& $HTTP_POST_FILES[$postname];
+        $fileinfo =& $_FILES[$postname];
         if ($fileinfo['error']) {
             // See https://sourceforge.net/forum/message.php?msg_id=3093651
             $err = (int) $fileinfo['error'];
@@ -1108,11 +1105,8 @@ class Request_AccessLogEntry
         if ($dbh and $dbh->isOpen() and $this->_accesslog->logtable) {
             $log_tbl =& $this->_accesslog->logtable;
             if ($request->get('REQUEST_METHOD') == "POST") {
-                // strangely HTTP_POST_VARS doesn't contain all posted vars.
-          	if (check_php_version(4,2))
-                    $args = $_POST; // copy not ref. clone not needed on hashes
-                else
-                    $args = $GLOBALS['HTTP_POST_VARS'];
+                $args = $_POST;
+
                 // garble passwords
                 if (!empty($args['auth']['passwd']))    $args['auth']['passwd'] = '<not displayed>';
                 if (!empty($args['dbadmin']['passwd'])) $args['dbadmin']['passwd'] = '<not displayed>';

src/common/wiki/phpwiki/RPC2.php
@@ -12,7 +12,7 @@
 // Intercept GET requests from confused users.  Only POST is allowed here!
 // There is some indication that $HTTP_SERVER_VARS is deprecated in php > 4.1.0
 // in favour of $_Server, but as far as I know, it still works.
-if ($HTTP_SERVER_VARS['REQUEST_METHOD'] != "POST")  
+if ($_SERVER['REQUEST_METHOD'] != "POST")
 {
     die('This is the address of the XML-RPC interface.' .
         '  You must use XML-RPC calls to access information here.');

src/common/wiki/phpwiki/SOAP.php
@@ -42,8 +42,6 @@ function checkCredentials(&$server, &$credentials, $access, $pagename) {
         $line = base64_decode(str_replace("Basic ","",trim($server->header['Authorization'])));
         list($credentials['username'],$credentials['password']) = explode(':',$line);
     } else {
-        if (!isset($_SERVER))
-            $_SERVER =& $GLOBALS['HTTP_SERVER_VARS'];
         // TODO: where in the header is the client IP
         if (!isset($credentials['username'])) {
             if (isset($_SERVER['REMOTE_ADDR']))

src/common/wiki/phpwiki/configurator.php
@@ -52,33 +52,30 @@
  * subsequent requests will fail. (POST to save the INI)
  */
 
-global $HTTP_SERVER_VARS, $HTTP_POST_VARS, $tdwidth;
-if (empty($_GET)) $_GET =& $GLOBALS['HTTP_GET_VARS'];
-if (empty($_ENV)) $_ENV =& $GLOBALS['HTTP_ENV_VARS'];
+global $tdwidth;
 
 if (empty($configurator))
     $configurator = "configurator.php";
-if (!strstr($HTTP_SERVER_VARS["SCRIPT_NAME"], $configurator) and defined('DATA_PATH'))
+if (!strstr($_SERVER["SCRIPT_NAME"], $configurator) and defined('DATA_PATH'))
     $configurator = DATA_PATH . "/" . $configurator;
-$scriptname = str_replace('configurator.php', 'index.php', $HTTP_SERVER_VARS["SCRIPT_NAME"]);
+$scriptname = str_replace('configurator.php', 'index.php', $_SERVER["SCRIPT_NAME"]);
 
 $tdwidth = 700;
 $config_file = (substr(PHP_OS,0,3) == 'WIN') ? 'config\\config.ini' : 'config/config.ini';
 $fs_config_file = dirname(__FILE__) . (substr(PHP_OS,0,3) == 'WIN' ? '\\' : '/') . $config_file;
-if (isset($HTTP_POST_VARS['create']))  header('Location: '.$configurator.'?show=_part1&create=1#create');
+if (isset($_POST['create']))  header('Location: '.$configurator.'?show=_part1&create=1#create');
 
 // helpers from lib/WikiUser/HttpAuth.php
 if (!function_exists('_http_user')) {
     function _http_user() {
-        if (!isset($_SERVER))
-            $_SERVER = $GLOBALS['HTTP_SERVER_VARS'];
+
 	if (!empty($_SERVER['PHP_AUTH_USER']))
 	    return array($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
 	if (!empty($_SERVER['REMOTE_USER']))
 	    return array($_SERVER['REMOTE_USER'], $_SERVER['PHP_AUTH_PW']);
-        if (!empty($GLOBALS['HTTP_ENV_VARS']['REMOTE_USER']))
-	    return array($GLOBALS['HTTP_ENV_VARS']['REMOTE_USER'], 
-	                 $GLOBALS['HTTP_ENV_VARS']['PHP_AUTH_PW']);
+        if (!empty($_ENV['REMOTE_USER']))
+	    return array($_ENV['REMOTE_USER'],
+            $_ENV['PHP_AUTH_PW']);
 	if (!empty($GLOBALS['REMOTE_USER']))
 	    return array($GLOBALS['REMOTE_USER'], $GLOBALS['PHP_AUTH_PW']);
 	    
@@ -90,8 +87,6 @@ if (!function_exists('_http_user')) {
 	return array('','');
     }
     function _http_logout() {
-        if (!isset($_SERVER))
-            $_SERVER =& $GLOBALS['HTTP_SERVER_VARS'];
         // maybe we should random the realm to really force a logout. but the next login will fail.
         // better_srand(); $realm = microtime().rand();
         header('WWW-Authenticate: Basic realm="'.WIKI_NAME.'"');
@@ -1491,13 +1486,12 @@ URL options -- you can probably skip this section.
 
 For a pretty wiki (no index.php in the url) set a seperate DATA_PATH.");
 
-global $HTTP_SERVER_VARS;
 $properties["Server Name"] =
-new _define_commented_optional('SERVER_NAME', $HTTP_SERVER_VARS['SERVER_NAME'], "
+new _define_commented_optional('SERVER_NAME', $_SERVER['SERVER_NAME'], "
 Canonical name of the server on which this PhpWiki resides.");
 
 $properties["Server Port"] =
-new numeric_define_commented('SERVER_PORT', $HTTP_SERVER_VARS['SERVER_PORT'], "
+new numeric_define_commented('SERVER_PORT', $_SERVER['SERVER_PORT'], "
 Canonical httpd port of the server on which this PhpWiki resides.",
 "onchange=\"validate_ereg('Sorry, \'%s\' is no valid port number.', '^[0-9]+$', 'SERVER_PORT', this);\"");
 
@@ -1764,9 +1758,8 @@ class _variable {
     }
 
     function value() {
-      global $HTTP_POST_VARS;
-      if (isset($HTTP_POST_VARS[$this->config_item_name]))
-          return $HTTP_POST_VARS[$this->config_item_name];
+      if (isset($_POST[$this->config_item_name]))
+          return $_POST[$this->config_item_name];
       else 
           return $this->default_value;
     }
@@ -1879,9 +1872,8 @@ extends unchangeable_variable {
 class _variable_selection
 extends _variable {
     function value() {
-        global $HTTP_POST_VARS;
-        if (!empty($HTTP_POST_VARS[$this->config_item_name]))
-            return $HTTP_POST_VARS[$this->config_item_name];
+        if (!empty($_POST[$this->config_item_name]))
+            return $_POST[$this->config_item_name];
         else {
 	    list($option, $label) = each($this->default_value);
             return $option;
@@ -2130,9 +2122,8 @@ extends _variable {
             $this->jscheck = "onchange=\"validate_ereg('Sorry, \'%s\' cannot be empty.', '^.+$', '" . $this->get_config_item_name() . "', this);\"";
     }
     function get_html() {
-	global $HTTP_POST_VARS, $HTTP_GET_VARS;
 	$s = $this->get_config_item_header();
-        if (isset($HTTP_POST_VARS['create']) or isset($HTTP_GET_VARS['create'])) {
+        if (isset($_POST['create']) or isset($_GET['create'])) {
 	    $new_password = random_good_password();
 	    $this->default_value = $new_password;
 	    $s .= "Created password: <strong>$new_password</strong><br />&nbsp;<br />";
@@ -2253,29 +2244,6 @@ extends _define {
         return $ta;
     }
 }
-
-/*
-class _ini_set
-extends _variable {
-    function value() {
-        global $HTTP_POST_VARS;
-        if ($v = $HTTP_POST_VARS[$this->config_item_name])
-            return $v;
-        else {
-	    return ini_get($this->get_config_item_name);
-        }
-    }
-    function _config_format($value) {
-        return sprintf("ini_set('%s', '%s');", $this->get_config_item_name(), $value);
-    }
-    function _get_config_line($posted_value) {
-        if ($posted_value && ! $posted_value == $this->default_value)
-            return "\n" . $this->_config_format($posted_value);
-        else
-            return "\n;" . $this->_config_format($this->default_value);
-    }
-}
-*/
 
 class boolean_define
 extends _define {
@@ -2472,10 +2440,10 @@ if (!function_exists('is_a')) {
 }
 
 
-if (!empty($HTTP_POST_VARS['action']) 
-    and $HTTP_POST_VARS['action'] == 'make_config'
-    and !empty($HTTP_POST_VARS['ADMIN_USER'])
-    and !empty($HTTP_POST_VARS['ADMIN_PASSWD'])
+if (!empty($_POST['action'])
+    and $_POST['action'] == 'make_config'
+    and !empty($_POST['ADMIN_USER'])
+    and !empty($_POST['ADMIN_PASSWD'])
     )
 {
 
@@ -2489,9 +2457,7 @@ if (!empty($HTTP_POST_VARS['action'])
 ; $preamble
 ";
 
-    $posted = $GLOBALS['HTTP_POST_VARS'];
-    /*if (defined('DEBUG'))
-     printArray($GLOBALS['HTTP_POST_VARS']);*/
+    $posted = $_POST;
 
     foreach ($properties as $option_name => $a) {
         $posted_value = stripslashes($posted[$a->config_item_name]);
@@ -2541,11 +2507,11 @@ if (!empty($HTTP_POST_VARS['action'])
     echo "<p>To make any corrections, <a href=\"configurator.php\">edit the settings again</a>.</p>\n";
 
 } else { // first time or create password
-    $posted = $GLOBALS['HTTP_POST_VARS'];
+    $posted = $_POST;
     // No action has been specified - we make a form.
 
-    if (!empty($GLOBALS['HTTP_GET_VARS']['start_debug'])) 
-    	$configurator .= ("?start_debug=" . $GLOBALS['HTTP_GET_VARS']['start_debug']);
+    if (!empty($_GET['start_debug']))
+    	$configurator .= ("?start_debug=" . $_GET['start_debug']);
     echo '
 <form action="',$configurator,'" method="post">
 <input type="hidden" name="action" value="make_config" />

src/common/wiki/phpwiki/lib/BlockParser.php
@@ -353,7 +353,7 @@ class ParsedBlock extends Block_HtmlElement {
         // nextBlock == block, both are the same objects. So we have to clone it.
         for ($block = $this->_getBlock($input); 
              $block; 
-             $block = (is_object($nextBlock) ? clone($nextBlock) : $nextBlock))
+             $block = (is_object($nextBlock) ? clone $nextBlock : $nextBlock))
         {
             while ($nextBlock = $this->_getBlock($input)) {
                 // Attempt to merge current with following block.
@@ -401,7 +401,7 @@ class ParsedBlock extends Block_HtmlElement {
         $re_set = &$this->_regexpset;
         //FIXME: php5 fails to advance here!
         for ($m = $re_set->match($line); $m; $m = $re_set->nextMatch($line, $m)) {
-            $block = clone($this->_block_types[$m->regexp_ind]);
+            $block = clone $this->_block_types[$m->regexp_ind];
             if (DEBUG & _DEBUG_PARSER)
                 $input->_debug('>', get_class($block));
             

src/common/wiki/phpwiki/lib/ErrorManager.php
@@ -509,8 +509,8 @@ class PhpError {
     }
 
     function printSimpleTrace($bt) {
-        global $HTTP_SERVER_VARS;
-        $nl = isset($HTTP_SERVER_VARS['REQUEST_METHOD']) ? "<br />" : "\n";
+
+        $nl = isset($_SERVER['REQUEST_METHOD']) ? "<br />" : "\n";
         echo $nl."Traceback:".$nl;
         foreach ($bt as $i => $elem) {
             if (!array_key_exists('file', $elem)) {

src/common/wiki/phpwiki/lib/IniConfig.php
@@ -512,7 +512,7 @@ function IniConfig($file) {
 // moved from lib/config.php [1ms]
 function fixup_static_configs($file) {
     global $FieldSeparator, $charset, $WikiNameRegexp, $AllActionPages;
-    global $HTTP_SERVER_VARS, $DBParams, $LANG;
+    global $DBParams, $LANG;
     // init FileFinder to add proper include paths
     FindFile("lib/interwiki.map",true);
     
@@ -599,8 +599,8 @@ function fixup_static_configs($file) {
             run_install("_part1");
             trigger_error($error, E_USER_ERROR);
             exit();
-        } elseif ($HTTP_SERVER_VARS["REQUEST_METHOD"] == "POST") {
-            $GLOBALS['HTTP_GET_VARS']['show'] = '_part1';
+        } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $_GET['show'] = '_part1';
             trigger_error($error, E_USER_WARNING);
         }
     }
@@ -615,8 +615,8 @@ function fixup_static_configs($file) {
             run_install("_part1");
             trigger_error($error, E_USER_ERROR);
             exit();
-        } elseif ($HTTP_SERVER_VARS["REQUEST_METHOD"] == "POST") {
-            $GLOBALS['HTTP_GET_VARS']['show'] = '_part1';
+        } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $_GET['show'] = '_part1';
             trigger_error($error, E_USER_WARNING);
         }
     }
@@ -659,7 +659,7 @@ function fixup_static_configs($file) {
  */
 function fixup_dynamic_configs($file) {
     global $WikiNameRegexp;
-    global $HTTP_SERVER_VARS, $DBParams, $LANG;
+    global $DBParams, $LANG;
 
     if (defined('INCLUDE_PATH') and INCLUDE_PATH)
         @ini_set('include_path', INCLUDE_PATH);
@@ -738,8 +738,8 @@ function fixup_dynamic_configs($file) {
     //
     foreach (array('SERVER_NAME','SERVER_PORT') as $var) {
         //FIXME: for CGI without _SERVER
-        if (!defined($var) and !empty($HTTP_SERVER_VARS[$var]))
-            define($var, $HTTP_SERVER_VARS[$var]);
+        if (!defined($var) and !empty($_SERVER[$var]))
+            define($var, $_SERVER[$var]);
     }
     $tuleap_request = HTTPRequest::instance();
     if (!defined('SERVER_NAME')) define('SERVER_NAME', '127.0.0.1');
@@ -807,7 +807,7 @@ function fixup_dynamic_configs($file) {
         // pages will appear at e.g. '/wikidir/index.php/HomePage'.
         //
 
-        $REDIRECT_URL = &$HTTP_SERVER_VARS['REDIRECT_URL'];
+        $REDIRECT_URL = &$_SERVER['REDIRECT_URL'];
         if (USE_PATH_INFO and isset($REDIRECT_URL)
             and ! IsProbablyRedirectToIndex()) {
             // FIXME: This is a hack, and won't work if the requested
@@ -834,9 +834,9 @@ function fixup_dynamic_configs($file) {
     // Detect PrettyWiki setup (not loading index.php directly)
     // $SCRIPT_FILENAME should be the same as __FILE__ in index.php
     if (!isset($SCRIPT_FILENAME))
-        $SCRIPT_FILENAME = @$HTTP_SERVER_VARS['SCRIPT_FILENAME'];
+        $SCRIPT_FILENAME = @$_SERVER['SCRIPT_FILENAME'];
     if (!isset($SCRIPT_FILENAME))
-        $SCRIPT_FILENAME = @$HTTP_ENV_VARS['SCRIPT_FILENAME'];
+        $SCRIPT_FILENAME = @$_ENV['SCRIPT_FILENAME'];
     if (!isset($SCRIPT_FILENAME))
         $SCRIPT_FILENAME = dirname(__FILE__.'/../') . '/index.php';
     if (isWindows())
@@ -844,10 +844,10 @@ function fixup_dynamic_configs($file) {
     define('SCRIPT_FILENAME', $SCRIPT_FILENAME);
 
     // Get remote host name, if apache hasn't done it for us
-    if (empty($HTTP_SERVER_VARS['REMOTE_HOST'])
-        and !empty($HTTP_SERVER_VARS['REMOTE_ADDR'])
+    if (empty($_SERVER['REMOTE_HOST'])
+        and !empty($_SERVER['REMOTE_ADDR'])
         and ENABLE_REVERSE_DNS)
-        $HTTP_SERVER_VARS['REMOTE_HOST'] = gethostbyaddr($HTTP_SERVER_VARS['REMOTE_ADDR']);
+        $_SERVER['REMOTE_HOST'] = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 
 }
 

src/common/wiki/phpwiki/lib/PageList.php
@@ -366,11 +366,10 @@ class _PageList_Column_content extends _PageList_Column {
             $this->_heading .= sprintf(_(" ... first %d bytes"),
                                        $this->bytes);
         } elseif ($field == 'hi_content') {
-            global $HTTP_POST_VARS;
-            if (!empty($HTTP_POST_VARS['admin_replace'])) {
-                $search = $HTTP_POST_VARS['admin_replace']['from'];
+            if (!empty($_POST['admin_replace'])) {
+                $search = $_POST['admin_replace']['from'];
                 $this->_heading .= sprintf(_(" ... around %s"),
-                                           '»'.$search.'«');
+                                           'Â»'.$search.'Â«');
             }
         }
     }
@@ -386,9 +385,8 @@ class _PageList_Column_content extends _PageList_Column {
         if (empty($pagelist->_sortby[$this->_field]))
             unset($revision_handle->_data['%content']);
         if ($this->_field == 'hi_content') {
-            global $HTTP_POST_VARS;
             unset($revision_handle->_data['%pagedata']['_cached_html']);
-            $search = $HTTP_POST_VARS['admin_replace']['from'];
+            $search = $_POST['admin_replace']['from'];
             if ($search and ($i = strpos($c,$search))) {
                 $l = strlen($search);
                 $j = max(0,$i - ($this->bytes / 2));
@@ -400,7 +398,7 @@ class _PageList_Column_content extends _PageList_Column {
                                  );
             } else {
                 $c = sprintf(_("%s not found"),
-                             '»'.$search.'«');
+                             'Â»'.$search.'Â«');
                 return HTML::div(array('style' => 'font-size:x-small','align'=>'center'),
                                  $c);
             }

commit 98df3ca735f2dc56e427b7e1f6820d57a7ae4467

request #9765: Characters ', ", \ and NUL are backslashed in PHPWiki
This issue has been introduced by commit 9ff88e32e4eb139b1669beb58c483ade2d4140ef.

This contribution also disable magic quotes on new Tuleap installation.
This "feature" is something from the past and it should definitively
stay in the past. Also, this option does not exist since PHP 5.4.0, we
should drop it as soon as possible. FYI, it is disabled since several
weeks on my development instance and I was not able to detect particular
issues.

This contribution feels appropriate to disable magic quotes on new
installations since the issue we fix here does not exist if you have
disabled the magic quotes. I have only fix the PHPWiki code to not force
existing instances to edit their /etc/httpd/conf.d/php.ini file.

Change-Id: I8428e4643efea3a97908f00997386489d8966b48

src/common/wiki/phpwiki/lib/Request.php
@@ -517,11 +517,11 @@ class Request {
     
 
     function _fix_magic_quotes_gpc() {
-        $needs_fix = array('HTTP_POST_VARS',
-                           'HTTP_GET_VARS',
-                           'HTTP_COOKIE_VARS',
-                           'HTTP_SERVER_VARS',
-                           'HTTP_POST_FILES');
+        $needs_fix = array('_POST',
+                           '_GET',
+                           '_COOKIE',
+                           '_SERVER',
+                           '_FILES');
         
         // Fix magic quotes.
         if (get_magic_quotes_gpc()) {

src/etc/php.conf.dist
@@ -27,7 +27,7 @@ AddType application/x-httpd-php .php
 
 # PHP variables
 php_flag register_globals on
-php_flag magic_quotes_gpc on
+php_flag magic_quotes_gpc off
 php_flag short_open_tag on
 # Needed by trackers and phpWiki
 php_flag register_long_arrays on