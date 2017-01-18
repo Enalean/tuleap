7dec5ea46b83616271dd9b23f12d4b67838db183
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-01-18 14:23:30 +0100

    New project visibility menu bar
    part of story #9756 better control project visibility change by project admins
    
    This commit is a first step to new page.
    It add a new link in bar menu, it only introduce new objects for next
    commit and leads to a under construction page
    
    Change-Id: I0549bfdf573f056dccf5b65ba6d75a520833f4ab

---
+++ src/www/project/admin/project_visibility.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright Enalean (c) 2017. All rights reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Tuealp\project\Admin\ProjectVisibilityController;
+use Tuealp\project\Admin\ProjectVisibilityRouter;
+
+require_once('pre.php');
+
+$group_id = $request->get('group_id');
+session_require(array('group' => $group_id, 'admin_flags' => 'A'));
+
+if (! $request->getProject()) {
+    exit_no_group();
+}
+
+$project_visibility_router = new ProjectVisibilityRouter(
+    new ProjectVisibilityController()
+);
+$project_visibility_router->route($request);
+
+project_admin_footer(array());
