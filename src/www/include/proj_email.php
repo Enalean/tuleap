Enalean/tuleap

commit 732165fea7b7d2dc39d681b2ea0bf5129c42cc42

request #9801: Do not send full credentials in clear to the user when an user account is created by a site administrator
Change-Id: I4525d110eeb1abcf29a26e222ce2c98ed0cedf51

src/www/include/proj_email.php
@@ -36,14 +36,15 @@ function send_new_user_email($to, $login, $confirm_hash) {
     return $mail->send();
 }
 
-function send_admin_new_user_email($to, $login, $password) {
+function send_admin_new_user_email($to, $login)
+{
     //needed by new_user_email.txt
     $base_url  = get_server_url();
     $presenter = new MailPresenterFactory();
 
     $renderer  = TemplateRendererFactory::build()->getRenderer(ForgeConfig::get('codendi_dir') .'/src/templates/mail/');
     $mail = new TuleapRegisterMail($presenter, $renderer, "mail-admin");
-    $mail = $mail->getMail($login, $password, '', $base_url, $GLOBALS['sys_noreply'], $to, "admin");
+    $mail = $mail->getMail($login, '', $base_url, $GLOBALS['sys_noreply'], $to, "admin");
     return $mail->send();
 }
 
@@ -54,7 +55,7 @@ function send_new_user_email_notification($to, $login) {
 
     $renderer  = TemplateRendererFactory::build()->getRenderer(ForgeConfig::get('codendi_dir') .'/src/templates/mail/');
     $mail = new TuleapRegisterMail($presenter, $renderer, "mail-notification");
-    $mail = $mail->getMail($login, '', '', $base_url, $GLOBALS['sys_noreply'], $to, "admin-notification");
+    $mail = $mail->getMail($login, '', $base_url, $GLOBALS['sys_noreply'], $to, "admin-notification");
     return $mail->send();
 }
 
@@ -65,6 +66,6 @@ function send_approval_new_user_email($to, $login) {
 
     $renderer  = TemplateRendererFactory::build()->getRenderer(ForgeConfig::get('codendi_dir') .'/src/templates/mail/');
     $mail = new TuleapRegisterMail($presenter, $renderer, "mail-admin-approval");
-    $mail = $mail->getMail($login, '', '', $base_url, $GLOBALS['sys_noreply'], $to, "admin-approval");
+    $mail = $mail->getMail($login, '', $base_url, $GLOBALS['sys_noreply'], $to, "admin-approval");
     return $mail->send();
 }

src/common/mail/MailPresenterFactory.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2015-2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -27,7 +27,7 @@ class MailPresenterFactory {
      *
      * @return MailRegisterPresenter
      */
-    public function createMailAccountPresenter($login, $password, $confirm_hash, $presenter_role, $logo_url) {
+    public function createMailAccountPresenter($login, $confirm_hash, $presenter_role, $logo_url) {
         $color_logo = "#000";
         $color_button = "#347DBA";
 
@@ -36,7 +36,6 @@ class MailPresenterFactory {
 
         $attributes_presenter = array(
             "login"         => $login,
-            "password"      => $password,
             "color_logo"    => $color_logo,
             "color_button"  => $color_button,
             "confirm_hash"  => $confirm_hash,
@@ -95,7 +94,6 @@ class MailPresenterFactory {
      */
     private function createAdminEmailPresenter(array $attributes_presenter) {
         $login      = $attributes_presenter["login"];
-        $password   = $attributes_presenter["password"];
 
         include($GLOBALS['Language']->getContent('account/new_account_email'));
         $presenter = new MailRegisterByAdminPresenter(
@@ -109,9 +107,7 @@ class MailPresenterFactory {
             $help,
             $attributes_presenter["color_logo"],
             $login,
-            $section_three,
-            $section_after_password,
-            $password
+            $section_three
         );
         return $presenter;
     }

src/common/mail/MailRegisterByAdminPresenter.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2015-2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -21,7 +21,6 @@
 class MailRegisterByAdminPresenter extends MailRegisterPresenter {
 
     public $section_three;
-    public $section_after_password;
     public $password;
 
     public function __construct(
@@ -35,14 +34,9 @@ class MailRegisterByAdminPresenter extends MailRegisterPresenter {
         $help,
         $color_logo,
         $login,
-        $section_three,
-        $section_after_password,
-        $password
+        $section_three
     ) {
         parent::__construct($logo_url, $title, $section_one, $section_two, $section_after_login, $thanks, $signature, $help, $color_logo, $login);
         $this->section_three          = $section_three;
-        $this->section_after_password = $section_after_password;
-        $this->password               = $password;
     }
 }
-?>

src/common/mail/TuleapRegisterMail.class.php
@@ -40,7 +40,8 @@ class TuleapRegisterMail {
      *
      * @return Codendi_Mail
      */
-    public function getMail($login, $password, $confirm_hash, $base_url, $from, $to, $presenter_role) {
+    public function getMail($login, $confirm_hash, $base_url, $from, $to, $presenter_role)
+    {
         if ($presenter_role === "user") {
             $subject = $GLOBALS['Language']->getText('include_proj_email', 'account_register', $GLOBALS['sys_name']);
             include($GLOBALS['Language']->getContent('include/new_user_email'));
@@ -65,7 +66,6 @@ class TuleapRegisterMail {
                 $this->template,
                 $this->mail_presenter_factory->createMailAccountPresenter(
                     $login,
-                    $password,
                     $confirm_hash,
                     $presenter_role,
                     $cid_logo

src/templates/mail/mail-admin.mustache
@@ -73,7 +73,7 @@
                                                                     <tbody>
                                                                     <tr>
                                                                         <td class="kmTextContent" valign="top" style='border-collapse:collapse;mso-table-lspace:0;mso-table-rspace:0;color:#222;font-family:arial,sans-serif;font-size:14px;line-height:130%;text-align:left;padding-top:20px;padding-bottom:15px;padding-left:18px;padding-right:18px;'>
-                                                                            <span style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;">{{ section_one }}<b>{{ login }}</b>{{ section_after_login }}<b>{{ password }}</b>{{ section_after_password }}</span><span style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;"></span>
+                                                                            <span style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;">{{ section_one }}<b>{{ login }}</b>. {{ section_after_login }}</span><span style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;"></span>
                                                                             <br style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;" />
                                                                             <br style="font-family: arial, sans-serif; font-size: 14px; line-height: normal;" />
                                                                             <span style="font-family: arial, sans-serif; font-size: 14px; color: rgb(55, 55, 55); line-height: 24px;">{{ section_two }}</span>

src/www/account/register.php
@@ -266,8 +266,7 @@ if ($request->isPost() && $request->exist('Register')) {
                 $from      = $GLOBALS['sys_noreply'];
                 $is_sent = send_admin_new_user_email(
                     $request->get('form_email'),
-                    $request->get('form_loginname'),
-                    $request->get('form_pw')
+                    $request->get('form_loginname')
                 );
 
                 if (! $is_sent) {
@@ -290,7 +289,7 @@ if ($request->isPost() && $request->exist('Register')) {
                     );
                 }
                 $presenter = new MailPresenterFactory();
-                $email_presenter = $presenter->createMailAccountPresenter($user_name, '', $mail_confirm_code, "user", $logo_retriever->getUrl());
+                $email_presenter = $presenter->createMailAccountPresenter($user_name, $mail_confirm_code, "user", $logo_retriever->getUrl());
             }
 
             $title  = $Language->getText('account_register', 'title_confirm');
@@ -326,7 +325,7 @@ if ($request->isPost() && $request->exist('Register')) {
             $redirect_url       = '/';
             $redirect_content   = $Language->getText('account_register', 'msg_redirect');
             $presenter          = new MailPresenterFactory();
-            $email_presenter    = $presenter->createMailAccountPresenter($user_name, '', $mail_confirm_code, "user", $logo_retriever->getUrl());
+            $email_presenter    = $presenter->createMailAccountPresenter($user_name, $mail_confirm_code, "user", $logo_retriever->getUrl());
         }
         $presenter = new Account_ConfirmationPresenter(
             $title,

commit 67fe78db997c8517208779573c4893575448a550

request #9904: Generation of the registration confirmation message when user can register themselves results in a fatal error
To reproduce the issue you must have sys_user_approval set to 0 and create
a user account without being site administrator.

The issue has been introduced by commit 732165fea7b7d2dc39d681b2ea0bf5129c42cc42

Change-Id: I017b44c20f74605e4bb51dc75f6d5afe1bfdece5

src/www/include/proj_email.php
@@ -32,7 +32,7 @@ function send_new_user_email($to, $login, $confirm_hash) {
 
     $renderer  = TemplateRendererFactory::build()->getRenderer(ForgeConfig::get('codendi_dir') .'/src/templates/mail/');
     $mail = new TuleapRegisterMail($presenter, $renderer, "mail");
-    $mail = $mail->getMail($login, '', $confirm_hash, $base_url, $GLOBALS['sys_noreply'], $to, "user");
+    $mail = $mail->getMail($login, $confirm_hash, $base_url, $GLOBALS['sys_noreply'], $to, "user");
     return $mail->send();
 }
 