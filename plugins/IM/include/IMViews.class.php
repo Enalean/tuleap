Enalean/tuleap

commit 5bb5dc862d424b6b6c1133c6d62722128aaf7d24

request #9578: Session tokens are stored in clear in the database
The goal of this contribution is to:
 * limit the impact of a leak of the session table
 * prevent timing attack existing with the current implementation

In order to do that we hash the session token before storing it. Unlike
passwords, we use a fast cryptographic hash function to limit the impact
on performance. Also, the token is a large cryptographic random value so
even with a fast hash function the bruteforce is not easy. You basically
have to bruteforce the whole keyspace of the random value or find a collision
in the hash function.
Since we are not able to compare the token provided by the user and the one
stored in constant time using the database engine, we need something to select
the proper session before checking the token. This implies that all existing
sessions will be invalidated by this change.

Change-Id: Ic3ee00c9f4277aae57aa4f41f85dd201da61bac7

src/common/user/SessionManager.php
@@ -1,0 +1,147 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\User;
+
+use HTTPRequest;
+use PFUser;
+use RandomNumberGenerator;
+use SessionDao;
+use UserManager;
+
+class SessionManager
+{
+    const HASH_ALGORITHM               = 'sha256';
+    const SESSION_IDENTIFIER_SEPARATOR = '.';
+
+    /**
+     * @var SessionDao
+     */
+    private $session_dao;
+    /**
+     * @var RandomNumberGenerator
+     */
+    private $random_number_generator;
+    /**
+     * @var UserManager
+     */
+    private $user_manager;
+
+    public function __construct(
+        UserManager $user_manager,
+        SessionDao $session_dao,
+        RandomNumberGenerator $random_number_generator
+    ) {
+        $this->session_dao             = $session_dao;
+        $this->random_number_generator = $random_number_generator;
+        $this->user_manager            = $user_manager;
+    }
+
+    /**
+     * @return PFUser
+     * @throws InvalidSessionException
+     */
+    public function getUser($session_identifier, $current_time, $session_lifetime)
+    {
+        list($session_id, $session_token) = $this->getSessionIdentifierParts($session_identifier);
+
+        $session = $this->session_dao->searchById($session_id, $current_time, $session_lifetime);
+        if ($session === false) {
+            throw new InvalidSessionException();
+        }
+
+        $hashed_session_token = hash(self::HASH_ALGORITHM, $session_token);
+        if (! hash_equals($session['session_hash'], $hashed_session_token)) {
+            throw new InvalidSessionException();
+        }
+
+        $user = $this->user_manager->getUserById($session['user_id']);
+
+        if ($user === null) {
+            throw new InvalidSessionException();
+        }
+
+        $user->setSessionHash($session_identifier);
+        return $user;
+    }
+
+    /**
+     * @return string
+     * @throws SessionNotCreatedException
+     */
+    public function createSession(PFUser $user, HTTPRequest $request, $current_time)
+    {
+        $token        = $this->random_number_generator->getNumber();
+        $hashed_token = hash(self::HASH_ALGORITHM, $token);
+        $session_id   = $this->session_dao->create(
+            $user->getId(),
+            $hashed_token,
+            $request->getIPAddress(),
+            $current_time
+        );
+
+        if ($session_id === null) {
+            throw new SessionNotCreatedException();
+        }
+
+        $session_identifier = $session_id . self::SESSION_IDENTIFIER_SEPARATOR . $token;
+        $user->setSessionHash($session_identifier);
+
+        return $session_identifier;
+    }
+
+    /**
+     * @throws SessionDataAccessException
+     */
+    public function destroyCurrentSession(PFUser $user)
+    {
+        list($session_id) = $this->getSessionIdentifierParts($user->getSessionHash());
+        $is_deleted = $this->session_dao->deleteSessionById($session_id);
+        if ($is_deleted === false) {
+            throw new SessionDataAccessException();
+        }
+        $user->setSessionHash(false);
+    }
+
+    /**
+     * @throws SessionDataAccessException
+     */
+    public function destroyAllSessions(PFUser $user)
+    {
+        $is_deleted = $this->session_dao->deleteSessionByUserId($user->getId());
+        if ($is_deleted === false) {
+            throw new SessionDataAccessException();
+        }
+        $user->setSessionHash(false);
+    }
+
+    /**
+     * @return array
+     * @throws InvalidSessionException
+     */
+    private function getSessionIdentifierParts($session_identifier)
+    {
+        $session_identifier_parts = explode(self::SESSION_IDENTIFIER_SEPARATOR, $session_identifier);
+        if (count($session_identifier_parts) !== 2) {
+            throw new InvalidSessionException();
+        }
+        return $session_identifier_parts;
+    }
+}

src/db/mysql/database_structure.sql
@@ -713,15 +713,14 @@ CREATE TABLE project_weekly_metric (
 #
 
 CREATE TABLE session (
-  user_id int(11) NOT NULL default '0',
-  session_hash char(32) NOT NULL default '',
+  id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INT(11) NOT NULL,
+  session_hash CHAR(64) NOT NULL,
   ip_addr varchar(45) NOT NULL default '',
   time int(11) NOT NULL default '0',
-  PRIMARY KEY  (session_hash),
   KEY idx_session_user_id (user_id),
-  KEY time_idx (time),
   KEY idx_session_time (time)
-);
+) ENGINE=InnoDB;
 
 #
 # Table structure for table 'snippet'

tests/simpletest/common/user/SessionManagerTest.php
@@ -1,0 +1,160 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2016. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\User;
+
+use TuleapTestCase;
+
+class SessionManagerTest extends TuleapTestCase
+{
+    const SESSION_LIFETIME_2_WEEKS = 1209600;
+    const CURRENT_TIME             = 1481202269;
+
+    /**
+     * @var \UserManager
+     */
+    private $user_manager;
+    /**
+     * @var \RandomNumberGenerator
+     */
+    private $random_number_generator;
+    /**
+     * @var \SessionDao
+     */
+    private $session_dao;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->user_manager            = mock('UserManager');
+        $this->random_number_generator = mock('RandomNumberGenerator');
+        $this->session_dao             = mock('SessionDao');
+    }
+
+    public function itThrowsAnExceptionWhenTheSessionIdentifierIsMalformed()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_identifier = 'malformed_session_identifier';
+
+        $this->expectException('Tuleap\User\InvalidSessionException');
+        $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
+    }
+
+    public function itThrowsAnExceptionWhenTheSessionIsNotFound()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_identifier = '1.random_string';
+
+        stub($this->session_dao)->searchById('1', self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS)->returns(false);
+
+        $this->expectException('Tuleap\User\InvalidSessionException');
+        $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
+    }
+
+    public function itThrowsAnExceptionWhenTokenDoesNotMatch()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_id         = '1';
+        $session_token      = 'token';
+        $session_identifier = "$session_id.$session_token";
+
+        stub($this->session_dao)->searchById($session_id, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS)->returns(
+            array('session_hash' => 'expected_token')
+        );
+
+        $this->expectException('Tuleap\User\InvalidSessionException');
+        $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
+    }
+
+    public function itThrowsAnExceptionWhenTheUserCanNotBeRetrieved()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_id           = '1';
+        $session_token        = 'token';
+        $hashed_session_token = hash(SessionManager::HASH_ALGORITHM, $session_token);
+        $session_identifier   = "$session_id.$session_token";
+
+        stub($this->session_dao)->searchById($session_id, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS)->returns(
+            array(
+                'session_hash' => $hashed_session_token,
+                'user_id'      => '101'
+            )
+        );
+
+        $this->expectException('Tuleap\User\InvalidSessionException');
+        $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
+    }
+
+    public function itGetsTheUserFromTheSessionIdentifier()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_id           = '1';
+        $session_token        = 'token';
+        $hashed_session_token = hash(SessionManager::HASH_ALGORITHM, $session_token);
+        $session_identifier   = "$session_id.$session_token";
+        $user_id              = '101';
+
+        stub($this->session_dao)->searchById($session_id, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS)->returns(
+            array(
+                'session_hash' => $hashed_session_token,
+                'user_id'      => $user_id
+            )
+        );
+        $user = mock('PFUser');
+        stub($this->user_manager)->getUserById($user_id)->returns($user);
+
+        $user->expectOnce('setSessionHash', array("$session_id.$session_token"));
+        $session_user = $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
+        $this->assertIdentical($user, $session_user);
+    }
+
+    public function itThrowsAnExceptionWhenTheSessionCanNotBeCreated()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        stub($this->session_dao)->create()->returns(null);
+        $user    = mock('PFUser');
+        $request = mock('HTTPRequest');
+
+        $this->expectException('Tuleap\User\SessionNotCreatedException');
+        $session_manager->createSession($user, $request, self::CURRENT_TIME);
+    }
+
+    public function itCreatesANewSession()
+    {
+        $session_manager = new SessionManager($this->user_manager, $this->session_dao, $this->random_number_generator);
+
+        $session_id   = '1';
+        $user         = mock('PFUser');
+        $request      = mock('HTTPRequest');
+        $random_token = 'random_token';
+        stub($this->session_dao)->create()->returns($session_id);
+        stub($this->random_number_generator)->getNumber()->returns($random_token);
+
+        $user->expectOnce('setSessionHash', array("$session_id.$random_token"));
+        $session_identifier = $session_manager->createSession($user, $request, self::CURRENT_TIME);
+        $this->assertEqual("$session_id.$random_token", $session_identifier);
+    }
+}

src/common/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoloade60c5e02540651bafbd65f6221f10c12($class) {
+function autoloade0e37a7739af975a3904d2cea1137b00($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -468,7 +468,6 @@ function autoloade60c5e02540651bafbd65f6221f10c12($class) {
             'servicenotallowedforprojectexception' => '/project/ServiceNotAllowedForProjectException.class.php',
             'servicesvn' => '/svn/ServiceSVN.class.php',
             'sessiondao' => '/dao/SessionDao.class.php',
-            'sessionnotcreatedexception' => '/user/SessionNotCreatedException.class.php',
             'simplesanitizer' => '/include/SimpleSanitizer.class.php',
             'sitecache' => '/include/SiteCache.php',
             'snippetdao' => '/dao/SnippetDao.class.php',
@@ -795,6 +794,7 @@ function autoloade60c5e02540651bafbd65f6221f10c12($class) {
             'tuleap\\user\\admin\\userlistresultspresenter' => '/user/Admin/UserListResultsPresenter.php',
             'tuleap\\user\\admin\\userlistresultsuserpresenter' => '/user/Admin/UserListResultsUserPresenter.php',
             'tuleap\\user\\admin\\userlistsearchfieldspresenter' => '/user/Admin/UserListSearchFieldsPresenter.php',
+            'tuleap\\user\\invalidsessionexception' => '/user/InvalidSessionException.php',
             'tuleap\\user\\password\\passwordvalidatorpresenter' => '/user/Password/PasswordValidatorPresenter.php',
             'tuleap\\user\\password\\reset\\creator' => '/user/Password/Reset/Creator.php',
             'tuleap\\user\\password\\reset\\dataaccessobject' => '/user/Password/Reset/DataAccessObject.php',
@@ -813,6 +813,9 @@ function autoloade60c5e02540651bafbd65f6221f10c12($class) {
             'tuleap\\user\\rest\\v1\\usermembershipresource' => '/user/REST/v1/UserMembershipResource.php',
             'tuleap\\user\\rest\\v1\\userpreferencerepresentation' => '/user/REST/v1/UserPreferenceRepresentation.class.php',
             'tuleap\\user\\rest\\v1\\userresource' => '/user/REST/v1/UserResource.php',
+            'tuleap\\user\\sessiondataaccessexception' => '/user/SessionDataAccessException.php',
+            'tuleap\\user\\sessionmanager' => '/user/SessionManager.php',
+            'tuleap\\user\\sessionnotcreatedexception' => '/user/SessionNotCreatedException.class.php',
             'tuleap\\user\\useractionpresenter' => '/user/UserActionPresenter.php',
             'tuleap\\viewvcversionchecker' => '/include/ViewVCVersionChecker.php',
             'tuleap\\xml\\mappingsregistry' => '/xml/MappingsRegistry.php',
@@ -1012,5 +1015,5 @@ function autoloade60c5e02540651bafbd65f6221f10c12($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoloade60c5e02540651bafbd65f6221f10c12');
+spl_autoload_register('autoloade0e37a7739af975a3904d2cea1137b00');
 // @codeCoverageIgnoreEnd

commit 022163ceb7299d149be5b115d3a049f43d90a3a6

request #9919: Feedbacks are sometimes not displayed
The size of the new session identifier is too big to be stored
in the current column.
Also, the non hashed session identifier should not be stored in the database.

This fix the regression introduced by the commit 5bb5dc862d424b6b6c1133c6d62722128aaf7d24

Change-Id: Iab0bdae6b08e79b0fd34911f0bc6cb74a9c82c49

plugins/IM/include/IMViews.class.php
@@ -103,7 +103,7 @@ class IMViews extends Views {
 
         $jabberConf = $im_object->get_server_conf();
 
-    	$sessionId = session_hash();
+        $sessionId = UserManager::instance()->getCurrentUser()->getSessionHash();
 		$server_dns = $jabberConf['server_dns'];
 		$conference_service = $jabberConf['conference_service'];
 

plugins/IM/include/JabbexFactory.class.php
@@ -14,7 +14,7 @@ class JabbexFactory {
         if ( ! self::$_jabbex_instance) {
             try {
                 require_once("jabbex_api/Jabbex.php");
-                self::$_jabbex_instance = new Jabbex(session_hash());
+                self::$_jabbex_instance = new Jabbex(UserManager::instance()->getCurrentUser()->getSessionHash());
             } catch (Exception $e) {
                 $GLOBALS['Response']->addFeedback('error', 'Jabbex require_once error:'.$e->getMessage());
                 return null;

src/common/dao/FeedbackDao.class.php
@@ -1,36 +1,40 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('include/DataAccessObject.class.php');
 
 /**
  *  Data Access Object for Feedback 
  */
-class FeedbackDao extends DataAccessObject {
+class FeedbackDao extends DataAccessObject
+{
     /**
     * Searches Feedback 
     * @return DataAccessResult
     */
-    function search($session_hash) {
-        $sql = sprintf("SELECT * FROM feedback WHERE session_hash = %s",
-				$this->da->quoteSmart($session_hash));
+    public function search($session_id)
+    {
+        $session_id = $this->da->escapeInt($session_id);
+
+        $sql = "SELECT * FROM feedback WHERE session_id = $session_id";
+
         return $this->retrieve($sql);
     }
 
@@ -39,10 +43,13 @@ class FeedbackDao extends DataAccessObject {
     * create a row in the table Feedback 
     * @return true if there is no error
     */
-    function create($session_hash, $serialized_feedback) {
-        $sql = sprintf("REPLACE INTO feedback (session_hash, feedback, created_at) VALUES (%s, %s, NOW())",
-				$this->da->quoteSmart($session_hash),
-				$this->da->quoteSmart($serialized_feedback));
+    public function create($session_id, $serialized_feedback)
+    {
+        $session_id          = $this->da->escapeInt($session_id);
+        $serialized_feedback = $this->da->quoteSmart($serialized_feedback);
+
+        $sql = "REPLACE INTO feedback (session_id, feedback, created_at) VALUES ($session_id, $serialized_feedback, NOW())";
+
         return $this->update($sql);
     }
     
@@ -50,12 +57,12 @@ class FeedbackDao extends DataAccessObject {
     * delete a row in the table Feedback 
     * @return true if there is no error
     */
-    function delete($session_hash) {
-		$sql = sprintf("DELETE FROM feedback WHERE session_hash = %s",
-				$this->da->quoteSmart($session_hash));
+    public function delete($session_id)
+    {
+        $session_id = $this->da->escapeInt($session_id);
+
+        $sql = "DELETE FROM feedback WHERE session_id = $session_id";
+
         return $this->update($sql);
     }
 }
-
-
-?>

src/common/include/Response.class.php
@@ -44,13 +44,14 @@ class Response {
     * Constructor
     */
     public function Response() {
-        if (session_hash()) {
+        $session_id = UserManager::instance()->getCurrentUser()->getSessionId();
+        if ($session_id) {
             $dao = $this->getFeedbackDao();
-            $dar = $dao->search(session_hash());
+            $dar = $dao->search($session_id);
             if ($dar && $dar->valid()) {
                 $row = $dar->current();
                 $this->_feedback = unserialize($row['feedback']);
-                $dao->delete(session_hash());
+                $dao->delete($session_id);
             }
         }
         if (!$this->_feedback) {
@@ -118,8 +119,9 @@ class Response {
     }
 
     function _serializeFeedback() {
-        $dao = $this->getFeedbackDao();
-        $dao->create(session_hash(), serialize($this->_feedback));
+        $dao        = $this->getFeedbackDao();
+        $session_id = UserManager::instance()->getCurrentUser()->getSessionId();
+        $dao->create($session_id, serialize($this->_feedback));
     }
 
     function setCookie($name, $value, $expire = 0) {

src/common/layout/BaseLayout.php
@@ -21,6 +21,7 @@
 namespace Tuleap\Layout;
 
 use Tuleap\Layout\IncludeAssets;
+use UserManager;
 use Widget_Static;
 use Response;
 use ForgeConfig;
@@ -129,7 +130,7 @@ abstract class BaseLayout extends Response
 
     public function redirect($url)
     {
-        $is_anon = session_hash() ? false : true;
+        $is_anon = UserManager::instance()->getCurrentUser()->isAnonymous();
         $has_feedback = $GLOBALS['feedback'] || count($this->_feedback->logs);
         if (($is_anon && (headers_sent() || $has_feedback)) || (!$is_anon && headers_sent())) {
             $this->header(array('title' => 'Redirection'));

src/common/user/SessionManager.php
@@ -78,6 +78,7 @@ class SessionManager
             throw new InvalidSessionException();
         }
 
+        $user->setSessionId($session_id);
         $user->setSessionHash($session_identifier);
         return $user;
     }
@@ -102,6 +103,7 @@ class SessionManager
         }
 
         $session_identifier = $session_id . self::SESSION_IDENTIFIER_SEPARATOR . $token;
+        $user->setSessionId($session_id);
         $user->setSessionHash($session_identifier);
 
         return $session_identifier;
@@ -112,11 +114,12 @@ class SessionManager
      */
     public function destroyCurrentSession(PFUser $user)
     {
-        list($session_id) = $this->getSessionIdentifierParts($user->getSessionHash());
+        $session_id = $user->getSessionId();
         $is_deleted = $this->session_dao->deleteSessionById($session_id);
         if ($is_deleted === false) {
             throw new SessionDataAccessException();
         }
+        $user->setSessionId(false);
         $user->setSessionHash(false);
     }
 
@@ -129,6 +132,7 @@ class SessionManager
         if ($is_deleted === false) {
             throw new SessionDataAccessException();
         }
+        $user->setSessionId(false);
         $user->setSessionHash(false);
     }
 

src/common/user/User.class.php
@@ -166,6 +166,11 @@ class PFUser implements PFO_User, IHaveAnSSHKey {
     var $_usergroupdao;
 
     /**
+     * @var int|false
+     */
+    private $session_id;
+
+    /**
      * session hash
      * By default it is false. Use explicitly setSessionHash()
      * @see setSessionHash
@@ -242,6 +247,7 @@ class PFUser implements PFO_User, IHaveAnSSHKey {
         }
 
         $this->session_hash = false;
+        $this->session_id   = false;
     }
 
 
@@ -1266,6 +1272,19 @@ class PFUser implements PFO_User, IHaveAnSSHKey {
       */
      function getSessionHash() {
          return $this->session_hash;
+     }
+
+     public function setSessionId($session_id)
+     {
+         $this->session_id = $session_id;
+     }
+
+    /**
+     * @return int|false
+     */
+     public function getSessionId()
+     {
+         return $this->session_id;
      }
 
      /**

src/db/mysql/database_structure.sql
@@ -1524,11 +1524,11 @@ CREATE TABLE ugroup_mapping (
 );
 
 CREATE TABLE feedback (
-  session_hash CHAR( 32 ) NOT NULL ,
-  feedback TEXT NOT NULL ,
-  created_at DATETIME NOT NULL ,
-  PRIMARY KEY ( session_hash )
-);
+  session_id INT(11) UNSIGNED,
+  feedback TEXT NOT NULL,
+  created_at DATETIME NOT NULL,
+  PRIMARY KEY (session_id)
+) ENGINE=InnoDB;
 
 CREATE TABLE notifications (
   user_id int(11) NOT NULL default '0',

src/db/mysql/updates/2017/201701311615_feedback_supports_new_session_identifier.php
@@ -1,0 +1,59 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201701311615_feedback_supports_new_session_identifier extends ForgeUpgrade_Bucket
+{
+    public function description()
+    {
+        return 'Update feedback management to use the new session management';
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $sql_drop_session_table = 'DROP TABLE IF EXISTS feedback';
+        $res                    = $this->db->dbh->exec($sql_drop_session_table);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occurred while dropping the table feedback');
+        }
+
+        $sql_recreate_table = 'CREATE TABLE feedback (
+                                  session_id INT(11) UNSIGNED,
+                                  feedback TEXT NOT NULL,
+                                  created_at DATETIME NOT NULL,
+                                  PRIMARY KEY (session_id)
+                               ) ENGINE=InnoDB';
+        $res                = $this->db->dbh->exec($sql_recreate_table);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occurred while creating the table session');
+        }
+    }
+
+    public function postUp()
+    {
+        if (! $this->db->tableNameExists('feedback')) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('feedback table is missing');
+        }
+    }
+}

src/www/include/session.php
@@ -84,8 +84,4 @@ function session_continue($sessionKey) {
     return $user->isLoggedIn();
 }
 
-function session_hash() {
-    return UserManager::instance()->getCurrentUser()->getSessionHash();
-}
-
 ?>

tests/simpletest/common/user/SessionManagerTest.php
@@ -125,6 +125,7 @@ class SessionManagerTest extends TuleapTestCase
         $user = mock('PFUser');
         stub($this->user_manager)->getUserById($user_id)->returns($user);
 
+        $user->expectOnce('setSessionId', array($session_id));
         $user->expectOnce('setSessionHash', array("$session_id.$session_token"));
         $session_user = $session_manager->getUser($session_identifier, self::CURRENT_TIME, self::SESSION_LIFETIME_2_WEEKS);
         $this->assertIdentical($user, $session_user);
@@ -153,6 +154,7 @@ class SessionManagerTest extends TuleapTestCase
         stub($this->session_dao)->create()->returns($session_id);
         stub($this->random_number_generator)->getNumber()->returns($random_token);
 
+        $user->expectOnce('setSessionId', array($session_id));
         $user->expectOnce('setSessionHash', array("$session_id.$random_token"));
         $session_identifier = $session_manager->createSession($user, $request, self::CURRENT_TIME);
         $this->assertEqual("$session_id.$random_token", $session_identifier);