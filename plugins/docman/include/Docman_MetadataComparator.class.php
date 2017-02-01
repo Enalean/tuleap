Enalean/tuleap

commit b6edb116f574d9c3b22dad37614f3b1c985b915a

request #9803 Docman - <> are not well displayed form property name in import configuration recap page
To reproduce
In docman in a project A create a property My<Property>,
In docman in project B, Import prpoerty from project A,

The property only display My

Change-Id: I7718cff45d20a53421b8e436402114cd9897a2f6

plugins/docman/include/Docman_MetadataComparator.class.php
@@ -89,7 +89,7 @@ class Docman_MetadataComparator {
         $dstLoveIter = $dstLoveFactory->getIteratorByFieldId($dstMd->getId(), $dstMd->getLabel(), true);
         $dstLoveArray = $this->getArrayFromIterator($dstLoveIter, 'getId');
 
-        $maxRow = max($srcLoveIter->count(), $dstLoveIter->count());
+        $purifier = Codendi_HTMLPurifier::instance();
 
         // Keep a trace of matching love
         $matchingLove = array();
@@ -137,7 +137,7 @@ class Docman_MetadataComparator {
             $html .= "<td>";
             switch($rowStyle) {
             case 'missing':
-                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_import_love', array($srcLove->getName()));
+                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_import_love', array($purifier->purify($srcLove->getName())));
             }
             $html .= "</td\n>";
 
@@ -153,7 +153,7 @@ class Docman_MetadataComparator {
                 $html .= "<tr>\n";
                 // Name
                 $html .= "<td>&nbsp;</td>\n";
-                $html .= "<td>".$love->getName()."</td>\n";
+                $html .= "<td>".$purifier->purify($love->getName())."</td>\n";
                 // Presence in source project
                 $html .= "<td></td>\n";
                 // Presence in destination project
@@ -201,6 +201,8 @@ class Docman_MetadataComparator {
         $html .= "<th>".$GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_action', array($this->dstGo->getPublicName()))."</th>\n";
         $html .= "</tr>\n";
 
+        $purifier = Codendi_HTMLPurifier::instance();
+
         // Keep a trace of metadata that matched in the dst metadata list.
         $matchingMd = array();
         $srcMdIter->rewind();
@@ -245,6 +247,7 @@ class Docman_MetadataComparator {
                 }
             }
 
+            $purified_property_name = $purifier->purify($srcMd->getName());
 
             //
             // Display result
@@ -253,7 +256,7 @@ class Docman_MetadataComparator {
 
             // Property
             $html .= "<td colspan=\"2\" style=\"font-weight: bold;\">";
-            $html .= $srcMd->getName();
+            $html .= $purified_property_name;
             $html .= "</td>";
 
             // Presence in source project
@@ -291,13 +294,13 @@ class Docman_MetadataComparator {
             case 'equivalent':
                 $diffArray = $this->checkMdDifferences($srcMd, $dstMd, $mdMap['love']);
                 $diffStr = '<ul style="padding:0;padding-left:1.5em;margin:0;"><li>';
-                $diffStr .= implode('</li><li>', $diffArray);
+                $diffStr .= implode('</li><li>', $purifier->purify($diffArray));
                 $diffStr .= '</li></ul>';
 
-                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_update_md', array($srcMd->getName(), $this->dstGo->getPublicName(), $diffStr));                
+                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_update_md', array($purified_property_name, $this->dstGo->getPublicName(), $diffStr));
                 break;
             case 'missing':
-                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_import_md', array($srcMd->getName()));
+                $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_import_md', array($purified_property_name));
                 break;
             case 'conflict':
                 $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_conflict');
@@ -328,7 +331,8 @@ class Docman_MetadataComparator {
 
                 // Name
                 $html .= "<td colspan=\"2\" style=\"font-weight: bold;\">";
-                $html .= $md->getName();
+                $purified_name = $purifier->purify($md->getName());
+                $html .= $purified_name;
                 $html .= "</td>";
 
                 // Presence in source project

commit 5d73fa86176e236c6d9acccd534df463e8c061ae

request #9920: Warnings are raised for copy/paste folder in two differents projects
This issue has been introduced by commit b6edb116f574d9c3b22dad37614f3b1c985b915a

Change-Id: Ia5fd8289e59f5289bbc0501a852ff5d079ac965e

plugins/docman/include/Docman_MetadataComparator.class.php
@@ -293,9 +293,12 @@ class Docman_MetadataComparator {
                 break;
             case 'equivalent':
                 $diffArray = $this->checkMdDifferences($srcMd, $dstMd, $mdMap['love']);
-                $diffStr = '<ul style="padding:0;padding-left:1.5em;margin:0;"><li>';
-                $diffStr .= implode('</li><li>', $purifier->purify($diffArray));
-                $diffStr .= '</li></ul>';
+                $diffStr = '<ul style="padding:0;padding-left:1.5em;margin:0;">';
+                foreach ($diffArray as $diff) {
+                    $diff_purified = $purifier->purify($diff, CODENDI_PURIFIER_FULL);
+                    $diffStr      .= "<li>$diff_purified</li>";
+                }
+                $diffStr .= '</ul>';
 
                 $html .= $GLOBALS['Language']->getText('plugin_docman', 'admin_md_import_tbl_act_update_md', array($purified_property_name, $this->dstGo->getPublicName(), $diffStr));
                 break;