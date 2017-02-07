3556b5fb4bc59b6cb729d6efabf5a31e91b6a01d
Juliana Leclaire <juliana.leclaire@enalean.com>
2017-02-07 16:57:20 +0100

    Check if value exists in list before building query
    This is a part of story #8860 TQL for select box
    
    To test: If you search on field list with a non
    existent value you should have an error.
    
    Change-Id: I59230443148ee57a963a68a1da4dcfdfbb96cb22

--- plugins/tracker/include/Tracker/Report/Query/Advanced/InvalidFields/NotEqualComparisonVisitor.php
+++ plugins/tracker/include/Tracker/Report/Query/Advanced/InvalidFields/NotEqualComparisonVisitor.php
@@ -139,7 +139,10 @@ class NotEqualComparisonVisitor implements Tracker_FormElement_FieldVisitor, IPr
 
     private function visitList()
     {
-        return new ListFieldChecker(new EmptyStringAllowed(), new CollectionOfListValuesExtractor());
+        return new ListFieldChecker(
+            new EmptyStringAllowed(),
+            new CollectionOfListValuesExtractor()
+        );
     }
 
     public function visitSubmittedBy(Tracker_FormElement_Field_SubmittedBy $field)
---
+++ plugins/tracker/tests/Tracker/Report/Query/Advanced/InvalidFields/ListFieldCheckerTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+namespace Tuleap\Tracker\Report\Query\Advanced\InvalidFields;
+
+use Tracker_FormElement_Field_List;
+use Tracker_FormElement_Field_List_Bind_Static;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\Comparison;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\ListFields\ListFieldChecker;
+use TuleapTestCase;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\SimpleValueWrapper;
+
+require_once TRACKER_BASE_DIR . '/../tests/bootstrap.php';
+
+class ListFieldCheckerTest extends TuleapTestCase
+{
+    /** @var ListFieldChecker */
+    private $list_field_checker;
+    /** @var Tracker_FormElement_Field_List */
+    private $field;
+    /** @var Comparison */
+    private $comparison;
+    /** @var Tracker_FormElement_Field_List_Bind_Static */
+    private $bind;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->list_field_checker = new ListFieldChecker(
+            new EmptyStringAllowed(),
+            new CollectionOfListValuesExtractor()
+        );
+        $this->comparison = mock('Tuleap\Tracker\Report\Query\Advanced\Grammar\Comparison');
+        $this->bind       = mock('Tracker_FormElement_Field_List_Bind_Static');
+        $this->field      = aCheckboxField()->withBind($this->bind)->build();
+        $list_values      = array(
+            100 => aFieldListStaticValue()->withId(100)->withLabel('a')->build(),
+            101 => aFieldListStaticValue()->withId(101)->withLabel('b')->build()
+        );
+        stub($this->bind)->getAllValues()->returns($list_values);
+    }
+
+    public function itDoesNotThrowWhenEmptyValueIsAllowed()
+    {
+        $value_wrapper = new SimpleValueWrapper('');
+
+        stub($this->comparison)->getValueWrapper()->returns($value_wrapper);
+
+        $this->list_field_checker->checkFieldIsValidForComparison($this->comparison, $this->field);
+        $this->pass();
+    }
+
+    public function itDoesNotThrowWhenValueExists()
+    {
+        $value_wrapper = new SimpleValueWrapper('a');
+
+        stub($this->comparison)->getValueWrapper()->returns($value_wrapper);
+
+        $this->list_field_checker->checkFieldIsValidForComparison($this->comparison, $this->field);
+        $this->pass();
+    }
+
+    public function itThrowsWhenValueDoNotExist()
+    {
+        $value_wrapper = new SimpleValueWrapper('c');
+
+        stub($this->comparison)->getValueWrapper()->returns($value_wrapper);
+
+        $this->expectException('Tuleap\Tracker\Report\Query\Advanced\InvalidFields\ListFields\ListValueDoNotExistComparisonException');
+
+        $this->list_field_checker->checkFieldIsValidForComparison($this->comparison, $this->field);
+    }
+}
