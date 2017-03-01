72b51b2a8162ce4023c3ced03506e6f5a27c759b
Manuel VACELET <manuel.vacelet@enalean.com>
2017-03-01 17:04:19 +0100

    Cache permissions to reduce the amout of SQL queries at Start Scrum
    First step, reduce the number of SQL requests by caching permissions.
    This removes 2004 queries on the 5016 (!) and reduce wall time by 15%
    according to blackfire.
    
    It should also improve anything that check permissions.
    
    Part of request #10030 start scrum timeout with slow database writes
    
    Change-Id: Idd624b2e4897d32047868e2dc055f1f06352039b

---
+++ src/common/project/UgroupsPermissionsCache.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\Project;
+
+class UgroupsPermissionsCache
+{
+    private static $instance;
+
+    private $cache;
+
+    public static function instance()
+    {
+        if (! self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function get($group_id, $object_id, array $permission_types, $use_default_permissions)
+    {
+        $key = $this->getKey($group_id, $object_id, $permission_types, $use_default_permissions);
+        if (isset($this->cache[$key])) {
+            return $this->cache[$key];
+        }
+        return null;
+    }
+
+    public function set($group_id, $object_id, array $permission_types, $use_default_permissions, $value)
+    {
+        $this->cache[$this->getKey($group_id, $object_id, $permission_types, $use_default_permissions)] = $value;
+    }
+
+    private function getKey($group_id, $object_id, array $permission_types, $use_default_permissions)
+    {
+        $use = $use_default_permissions ? 'true' : 'false';
+        return $group_id.'-'.$object_id.'-'.implode('-', $permission_types).'-'.$use;
+    }
+}
