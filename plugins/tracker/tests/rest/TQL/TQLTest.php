608b4826d9061b2dff09cdd437808c4a1f65b639
Nicolas Terray <nicolas.terray@enalean.com>
2017-03-01 18:50:55 +0100

    request #10031: REST tests for TQL
    In order to decrease time spent to manually test TQL, we should automate 
    the tests.
    
    Change-Id: Ia25a0b4614f1f27da5a499a7088631e02461a614

---
+++ plugins/tracker/tests/rest/TQL/TQLTest.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
+
+namespace Tuleap\Tracker\Tests\REST\TQL;
+
+use Guzzle\Http\Exception\ClientErrorResponseException;
+use REST_TestDataBuilder;
+use RestBase;
+
+require_once dirname(__FILE__).'/../bootstrap.php';
+
+/**
+ * @group TrackerTests
+ */
+class TQLTest extends RestBase
+{
+    const PROJECT_NAME = 'tql';
+
+    private $tracker_id;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->tracker_id = $this->getTrackerId();
+    }
+
+    public function testTQLQueries()
+    {
+        $tests = array(
+            ''                                                             => array('bug1', 'bug2', 'bug3'),
+            'summary = "bug1"'                                             => array('bug1'),
+            'summary = "bug"'                                              => array('bug1', 'bug2', 'bug3'),
+            'summary = "bug" and details = "original2"'                    => array('bug2'),
+            'remaining_effort between(1, 42)'                              => array('bug1'),
+            'remaining_effort > 3.14'                                      => array('bug2'),
+            'story_points <= 21'                                           => array('bug1', 'bug2'),
+            'story_points = ""'                                            => array('bug3'),
+            'story_points != ""'                                           => array('bug1', 'bug2'),
+            'due_date = "2017-01-10"'                                      => array('bug2'),
+            'timesheeting < "2017-01-18 14:36"'                            => array('bug1'),
+            'last_update_date between(now() - 1w, now())'                  => array('bug1', 'bug2', 'bug3'),
+            'submitted_by = MYSELF()'                                      => array('bug1', 'bug2', 'bug3'),
+            'submitted_by != MYSELF()'                                     => array(),
+            'submitted_by IN (MYSELF())'                                   => array('bug1', 'bug2', 'bug3'),
+            'submitted_by NOT IN (MYSELF())'                               => array(),
+            'status IN ("todo", "doing") OR ugroups = "Membres du projet"' => array('bug1', 'bug2'),
+            'status = ""'                                                  => array('bug2', 'bug3'),
+            'ugroups = "Contractors"'                                      => array('bug1'),
+        );
+        foreach ($tests as $query => $expectation) {
+            $message = "Query $query should returns ". implode(', ', $expectation);
+
+            $response = $this->performExpertQuery($query);
+            $this->assertEquals($response->getStatusCode(), 200, $message);
+
+            $artifacts = $response->json();
+            $this->assertCount(count($expectation), $artifacts, $message);
+            foreach ($expectation as $index => $title) {
+                $this->assertEquals($title, $artifacts[$index]['title'], $message);
+            }
+        }
+    }
+
+    public function testInvalidQuery()
+    {
+        $exception_thrown = false;
+        try {
+            $this->performExpertQuery('summary="bug1');
+        } catch (ClientErrorResponseException $exception) {
+            $response = $exception->getResponse();
+            $this->assertEquals(400, $response->getStatusCode());
+
+            $body = $response->json();
+            $this->assertContains(
+                "Error during parsing expert query",
+                $body['error']['message']
+            );
+
+            $exception_thrown = true;
+        }
+        $this->assertTrue($exception_thrown);
+    }
+
+    public function testUnknownValueForListFields()
+    {
+        $exception_thrown = false;
+        try {
+            $this->performExpertQuery('status = "pouet"');
+        } catch (ClientErrorResponseException $exception) {
+            $response = $exception->getResponse();
+            $this->assertEquals(400, $response->getStatusCode());
+
+            $body = $response->json();
+            $this->assertContains(
+                "The value 'pouet' doesn't exist for the list field 'status'",
+                $body['error']['message']
+            );
+
+            $exception_thrown = true;
+        }
+        $this->assertTrue($exception_thrown);
+    }
+
+    public function testInvalidDateTime()
+    {
+        $exception_thrown = false;
+        try {
+            $this->performExpertQuery('due_date = "2017-01-10 12:12"');
+        } catch (ClientErrorResponseException $exception) {
+            $response = $exception->getResponse();
+            $this->assertEquals(400, $response->getStatusCode());
+
+            $body = $response->json();
+            $this->assertContains(
+                "The date field 'due_date' cannot be compared to the string value '2017-01-10 12:12'",
+                $body['error']['message']
+            );
+
+            $exception_thrown = true;
+        }
+        $this->assertTrue($exception_thrown);
+    }
+
+    public function testUnknownField()
+    {
+        $exception_thrown = false;
+        try {
+            $this->performExpertQuery('test = "bug1"');
+        } catch (ClientErrorResponseException $exception) {
+            $response = $exception->getResponse();
+            $this->assertEquals(400, $response->getStatusCode());
+
+            $body = $response->json();
+            $this->assertContains(
+                "The field 'test' doesn't exist",
+                $body['error']['message']
+            );
+
+            $exception_thrown = true;
+        }
+        $this->assertTrue($exception_thrown);
+    }
+
+    private function getResponse($request)
+    {
+        return $this->getResponseByToken(
+            $this->getTokenForUserName(REST_TestDataBuilder::TEST_USER_1_NAME),
+            $request
+        );
+    }
+
+    private function getProjectId()
+    {
+        $query = http_build_query(
+            array(
+                'limit' => 1,
+                'query' => json_encode(
+                    array(
+                        'shortname' => self::PROJECT_NAME
+                    )
+                )
+            )
+        );
+        $response = $this->getResponse($this->client->get("projects/?$query"))->json();
+
+        return $response[0]['id'];
+    }
+
+    private function getTrackerId()
+    {
+        $project_id = $this->getProjectId();
+
+        $response = $this->getResponse($this->client->get("projects/$project_id/trackers"))->json();
+
+        return $response[0]['id'];
+    }
+
+    private function performExpertQuery($query)
+    {
+        $query = urlencode($query);
+        $url   = "trackers/$this->tracker_id/artifacts?expert_query=$query";
+
+        return $this->getResponse(
+            $this->client->get($url)
+        );
+    }
+}
