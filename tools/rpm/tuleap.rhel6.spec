Enalean/tuleap

commit f561a86c796896f3caaa710318bbf60634f4066e

Be able to switch to the Tuleap SSH key management from the UI
This is part of request #9970: Directly manage SSH keys instead of delegating it to gitolite

Change-Id: If3d20ce60e9c674f5e629649d70f1979c5cedb6d

plugins/git/etc/gitolite3.rc.dist
@@ -86,8 +86,8 @@
 
         # These FEATURES are enabled by default.
 
-            # essential (unless you're using smart-http mode)
-            'ssh-authkeys',
+            # SSH keys deployment is managed by Tuleap
+            # 'ssh-authkeys',
 
             # creates git-config enties from gitolite.conf file entries like 'config foo.bar = baz'
             'git-config',

plugins/git/include/Git/AdminGitoliteConfig.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -20,10 +20,12 @@
  */
 
 use Tuleap\Admin\AdminPageRenderer;
+use Tuleap\Git\Gitolite\SSHKey\ManagementDetector;
 
 class Git_AdminGitoliteConfig {
 
-    const ACTION = 'update_config';
+    const ACTION_UPDATE_CONFIG              = 'update_config';
+    const ACTION_MIGRATE_SSH_KEY_MANAGEMENT = 'migrate_to_tuleap_ssh_keys_management';
 
     /**
      * @var Git_SystemEventManager
@@ -42,17 +44,23 @@ class Git_AdminGitoliteConfig {
 
     /** @var AdminPageRenderer */
     private $admin_page_renderer;
+    /**
+     * @var ManagementDetector
+     */
+    private $management_detector;
 
     public function __construct(
         CSRFSynchronizerToken $csrf,
         ProjectManager $project_manager,
         Git_SystemEventManager $system_event_manager,
-        AdminPageRenderer $admin_page_renderer
+        AdminPageRenderer $admin_page_renderer,
+        ManagementDetector $management_detector
     ) {
         $this->csrf                 = $csrf;
         $this->project_manager      = $project_manager;
         $this->system_event_manager = $system_event_manager;
         $this->admin_page_renderer  = $admin_page_renderer;
+        $this->management_detector  = $management_detector;
     }
 
     public function process(Codendi_Request $request) {
@@ -62,15 +70,27 @@ class Git_AdminGitoliteConfig {
             return;
         }
 
-        if ($action !== self::ACTION) {
-            $GLOBALS['Response']->addFeedback(
-                'error',
-                $GLOBALS['Language']->getText('plugin_git', 'regenerate_config_bad_request')
-            );
-            return;
+        switch ($action) {
+            case self::ACTION_UPDATE_CONFIG:
+                $this->csrf->check();
+                $this->regenerateGitoliteConfigForAProject($request);
+                break;
+            case self::ACTION_MIGRATE_SSH_KEY_MANAGEMENT:
+                $this->csrf->check();
+                $this->migrateToTuleapSSHKeyManagement();
+                break;
+            default:
+                $GLOBALS['Response']->addFeedback(
+                    'error',
+                    $GLOBALS['Language']->getText('plugin_git', 'regenerate_config_bad_request')
+                );
         }
 
-        $this->csrf->check();
+        return true;
+    }
+
+    private function regenerateGitoliteConfigForAProject(Codendi_Request $request)
+    {
         $project = $this->getProject($request->get('gitolite_config_project'));
 
         if (! $project) {
@@ -87,7 +107,6 @@ class Git_AdminGitoliteConfig {
             'info',
             $GLOBALS['Language']->getText('plugin_git', 'regenerate_config_waiting', array($project->getPublicName()))
         );
-        return true;
     }
 
     /**
@@ -97,13 +116,26 @@ class Git_AdminGitoliteConfig {
         return $this->project_manager->getProjectFromAutocompleter($project_name_from_autocomplete);
     }
 
+    private function migrateToTuleapSSHKeyManagement()
+    {
+        if (! $this->management_detector->canRequestAuthorizedKeysFileManagementByTuleap()) {
+            return;
+        }
+        $this->system_event_manager->queueMigrateToTuleapSSHKeyManagement();
+        $GLOBALS['Response']->addFeedback(
+            Feedback::INFO,
+            $GLOBALS['Language']->getText('plugin_git', 'migrate_to_tuleap_ssh_keys_management_feedback')
+        );
+    }
+
     public function display(Codendi_Request $request) {
         $title    = $GLOBALS['Language']->getText('plugin_git', 'descriptor_name');
         $template_path = dirname(GIT_BASE_DIR).'/templates';
 
         $admin_presenter = new Git_AdminGitoliteConfigPresenter(
             $title,
-            $this->csrf
+            $this->csrf,
+            $this->management_detector->canRequestAuthorizedKeysFileManagementByTuleap()
         );
 
         $this->admin_page_renderer->renderANoFramedPresenter(

plugins/git/include/Git/AdminGitoliteConfigPresenter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -27,10 +27,11 @@ class Git_AdminGitoliteConfigPresenter  extends Git_AdminPresenter {
 
 
 
-    public function __construct($title, CSRFSynchronizerToken $csrf_token) {
+    public function __construct($title, CSRFSynchronizerToken $csrf_token, $migrate_to_tuleap_ssh_keys_management) {
         parent::__construct($title, $csrf_token);
 
-        $this->regenerate_gitolite_configuration = $GLOBALS['Language']->getText('plugin_git', 'regenerate_gitolite_configuration');
+        $this->migrate_to_tuleap_ssh_keys_management = $migrate_to_tuleap_ssh_keys_management;
+        $this->regenerate_gitolite_configuration     = $GLOBALS['Language']->getText('plugin_git', 'regenerate_gitolite_configuration');
     }
 
     public function gitolite_config_title() {
@@ -43,5 +44,20 @@ class Git_AdminGitoliteConfigPresenter  extends Git_AdminPresenter {
 
     public function submit() {
         return $GLOBALS['Language']->getText('plugin_git', 'view_admin_git_admins_submit_button');
+    }
+
+    public function migrate_to_tuleap_ssh_keys_management_title()
+    {
+        return $GLOBALS['Language']->getText('plugin_git', 'migrate_to_tuleap_ssh_keys_management_title');
+    }
+
+    public function migrate_to_tuleap_ssh_keys_management_description()
+    {
+        return $GLOBALS['Language']->getText('plugin_git', 'migrate_to_tuleap_ssh_keys_management_description');
+    }
+
+    public function migrate_to_tuleap_ssh_keys_management_button()
+    {
+        return $GLOBALS['Language']->getText('plugin_git', 'migrate_to_tuleap_ssh_keys_management_button');
     }
 }

plugins/git/include/Git/AdminPresenter.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -24,10 +24,11 @@ class Git_AdminPresenter {
 
     public $csrf_token;
 
-    public $manage_gerrit           = false;
-    public $manage_mirrors          = false;
-    public $manage_gitolite_config  = false;
-    public $manage_general_settings = false;
+    public $manage_gerrit                         = false;
+    public $manage_mirrors                        = false;
+    public $manage_gitolite_config                = false;
+    public $migrate_to_tuleap_ssh_keys_management = false;
+    public $manage_general_settings               = false;
 
     public $mirrors_active          = '';
     public $general_settings_active = '';

plugins/git/include/Git/AdminRouter.class.php
@@ -20,6 +20,7 @@
 
 use Tuleap\Git\AdminGerritBuilder;
 use Tuleap\Git\GeneralSettingsController;
+use Tuleap\Git\Gitolite\SSHKey\ManagementDetector;
 use Tuleap\Git\Permissions\RegexpFineGrainedDisabler;
 use Tuleap\Git\Permissions\RegexpFineGrainedRetriever;
 use Tuleap\Git\Permissions\RegexpFineGrainedEnabler;
@@ -78,6 +79,10 @@ class Git_AdminRouter {
      * @var Restrictor
      */
     private $gerrit_restrictor;
+    /**
+     * @var ManagementDetector
+     */
+    private $management_detector;
 
     public function __construct(
         Git_RemoteServer_GerritServerFactory $gerrit_server_factory,
@@ -92,7 +97,8 @@ class Git_AdminRouter {
         AdminPageRenderer                    $admin_page_renderer,
         RegexpFineGrainedDisabler            $regexp_disabler,
         GerritServerResourceRestrictor       $gerrit_ressource_restrictor,
-        Restrictor                           $gerrit_restrictor
+        Restrictor                           $gerrit_restrictor,
+        ManagementDetector                   $management_detector
     ) {
         $this->gerrit_server_factory          = $gerrit_server_factory;
         $this->csrf                           = $csrf;
@@ -107,6 +113,7 @@ class Git_AdminRouter {
         $this->regexp_disabler                = $regexp_disabler;
         $this->gerrit_ressource_restrictor    = $gerrit_ressource_restrictor;
         $this->gerrit_restrictor              = $gerrit_restrictor;
+        $this->management_detector            = $management_detector;
     }
 
     public function process(Codendi_Request $request) {
@@ -136,7 +143,8 @@ class Git_AdminRouter {
                 $this->csrf,
                 $this->project_manager,
                 $this->git_system_event_manager,
-                $this->admin_page_renderer
+                $this->admin_page_renderer,
+                $this->management_detector
             );
         } elseif ($request->get('pane') === 'mirrors_admin' || $request->get('view') === 'mirrors_restriction'){
             return new Git_AdminMirrorController(

plugins/git/include/Git/Gitolite/SSHKey/ManagementDetector.php
@@ -20,6 +20,8 @@
 
 namespace Tuleap\Git\Gitolite\SSHKey;
 
+use SystemEventManager;
+use Tuleap\Git\Gitolite\SSHKey\SystemEvent\MigrateToTuleapSSHKeyManagement;
 use Tuleap\Git\Gitolite\VersionDetector;
 use Tuleap\Git\GlobalParameterDao;
 
@@ -33,13 +35,24 @@ class ManagementDetector
      * @var GlobalParameterDao
      */
     private $global_parameter_dao;
+    /**
+     * @var SystemEventManager
+     */
+    private $system_event_manager;
 
-    public function __construct(VersionDetector $version_detector, GlobalParameterDao $global_parameter_dao)
-    {
+    public function __construct(
+        VersionDetector $version_detector,
+        GlobalParameterDao $global_parameter_dao,
+        SystemEventManager $system_event_manager
+    ) {
         $this->version_detector     = $version_detector;
         $this->global_parameter_dao = $global_parameter_dao;
+        $this->system_event_manager = $system_event_manager;
     }
 
+    /**
+     * @return bool
+     */
     public function isAuthorizedKeysFileManagedByTuleap()
     {
         if (! $this->version_detector->isGitolite3()) {
@@ -47,5 +60,23 @@ class ManagementDetector
         }
 
         return $this->global_parameter_dao->isAuthorizedKeysFileManagedByTuleap();
+    }
+
+    /**
+     * @return bool
+     */
+    public function canRequestAuthorizedKeysFileManagementByTuleap()
+    {
+        if (! $this->version_detector->isGitolite3()) {
+            return false;
+        }
+
+        if ($this->global_parameter_dao->isAuthorizedKeysFileManagedByTuleap()) {
+            return false;
+        }
+
+        return ! $this->system_event_manager->isThereAnEventAlreadyOnGoing(
+            MigrateToTuleapSSHKeyManagement::NAME
+        );
     }
 }

plugins/git/include/Git/Gitolite/SSHKey/SystemEvent/MigrateToTuleapSSHKeyManagement.php
@@ -1,0 +1,71 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\Gitolite\SSHKey\SystemEvent;
+
+use System_Command;
+use System_Command_CommandException;
+use Tuleap\Git\GlobalParameterDao;
+
+class MigrateToTuleapSSHKeyManagement extends \SystemEvent
+{
+    const NAME              = 'MIGRATE_TO_TULEAP_SSH_KEY_MANAGEMENT';
+    const GITOLITE3_RC_PATH = '/var/lib/gitolite/.gitolite.rc';
+
+    /**
+     * @var GlobalParameterDao
+     */
+    private $global_parameter_dao;
+    /**
+     * @var System_Command
+     */
+    private $system_command;
+
+    public function injectDependencies(
+        GlobalParameterDao $global_parameter_dao,
+        System_Command $system_command
+    ) {
+        $this->global_parameter_dao = $global_parameter_dao;
+        $this->system_command       = $system_command;
+    }
+
+    public function verbalizeParameters($with_link)
+    {
+        return '';
+    }
+
+    public function process()
+    {
+        try {
+            $this->system_command->exec('sed -i "s/\'ssh-authkeys\',/#\'ssh-authkeys\',/" ' . escapeshellarg(self::GITOLITE3_RC_PATH));
+        } catch (System_Command_CommandException $ex) {
+            $this->error($ex->getMessage());
+            return;
+        }
+
+        $has_been_enabled = $this->global_parameter_dao->enableAuthorizedKeysFileManagementByTuleap();
+        if ($has_been_enabled === false) {
+            $this->error('The database has not been able to save the new manager of the authorized keys file');
+            return;
+        }
+
+        $this->done();
+    }
+}

plugins/git/include/Git/SystemEventManager.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright Enalean (c) 2011-2016. All rights reserved.
+ * Copyright Enalean (c) 2011-2017. All rights reserved.
  *
  * Tuleap and Enalean names and logos are registrated trademarks owned by
  * Enalean SAS. All other trademarks or names are properties of their respective
@@ -23,6 +23,7 @@
  */
 
 use Tuleap\Git\Events\ParseGitolite3Logs;
+use Tuleap\Git\Gitolite\SSHKey\SystemEvent\MigrateToTuleapSSHKeyManagement;
 
 /**
  * I'm responsible to create system events with the right parameters
@@ -248,6 +249,17 @@ class Git_SystemEventManager {
         );
     }
 
+    public function queueMigrateToTuleapSSHKeyManagement()
+    {
+        $this->system_event_manager->createEvent(
+            MigrateToTuleapSSHKeyManagement::NAME,
+            null,
+            SystemEvent::PRIORITY_MEDIUM,
+            SystemEvent::OWNER_ROOT,
+            'Tuleap\\Git\\Gitolite\\SSHKey\\SystemEvent\\MigrateToTuleapSSHKeyManagement'
+        );
+    }
+
     public function isRepositoryUpdateAlreadyQueued(GitRepository $repository) {
         return $this->system_event_manager->areThereMultipleEventsQueuedMatchingFirstParameter(
             SystemEvent_GIT_REPO_UPDATE::NAME,
@@ -307,7 +319,8 @@ class Git_SystemEventManager {
      */
     public function getTypesForDefaultQueue() {
         $types = array(
-            ParseGitolite3Logs::NAME
+            ParseGitolite3Logs::NAME,
+            MigrateToTuleapSSHKeyManagement::NAME
         );
 
         if ($this->repository_factory->hasGitShellRepositories()) {

commit 836803a9d69fd46c9cef725117bf546fd00e62ac

Deploy the Tuleap/gitolite admin key during the initial setup
Without it, a fresh install can not use the Git plugin

Issue has been introduced by commit f561a86c796896f3caaa710318bbf60634f4066e.

This is part of request #9970: Directly manage SSH keys instead of delegating it to gitolite

Change-Id: I9a93b8ea44d3f1123ac4de3974c1186ea22139b7

plugins/git/etc/gitolite3.rc.dist
@@ -86,8 +86,8 @@
 
         # These FEATURES are enabled by default.
 
-            # SSH keys deployment is managed by Tuleap
-            # 'ssh-authkeys',
+            # SSH keys deployment is managed by Tuleap after the initial installation
+            'ssh-authkeys',
 
             # creates git-config enties from gitolite.conf file entries like 'config foo.bar = baz'
             'git-config',

tools/rpm/tuleap.rhel6.spec
@@ -866,6 +866,9 @@ if [ ! -d "%{APP_DATA_DIR}/gitolite/admin" ]; then
     # doesn't have DB access .
     perl -pi -e 's/# GROUPLIST_PGM/GROUPLIST_PGM/' /var/lib/gitolite/.gitolite.rc
 
+    # SSH keys are managed by Tuleap
+    sed -i "s/'ssh-authkeys',/#'ssh-authkeys',/" /var/lib/gitolite/.gitolite.rc
+
     # add codendiadm to gitolite group
     if ! groups codendiadm | grep -q gitolite 2> /dev/null ; then
 	usermod -a -G gitolite codendiadm