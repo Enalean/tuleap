Enalean/tuleap

commit 9030d2e569931239c90e1c94b5bbff9d3a559e20

Complete Nginx configuration
What should work:
- Web browse SVN core & SVN plugin (+all admin tasks, were already possible)
- Do regular SVN operations (managed by proxy)
- Web browse git repositories (+ all admin tasks, were already possible)
- Do git over https
- REST API explorer
- PHPWiki

Please have a look at src/etc/nginx18/README.mkd to do your setup.

Part of request #9202 Make Tuleap compatible with PHP 5.6

Change-Id: Idd31b4c225f53d8c00d00cbbe4ef876ec9d4bff7

src/common/include/HTTPRequest.class.php
@@ -283,4 +283,41 @@ class HTTPRequest extends Codendi_Request {
             return $_SERVER[self::HEADER_REMOTE_ADDR];
         }
     }
+
+    /**
+     * Return PATH_INFO content from the env. variables available in FastCGI mode
+     *
+     * In FastCGI we don't have PATH_INFO set automatically. However this value can be computed based on other
+     * variables in the environment. Useful when running the application with php-fpm
+     *
+     * @return string
+     */
+    public function getPathInfoFromFCGI()
+    {
+        $path = $this->stripQueryString($this->getFromServer('REQUEST_URI'));
+        $path = $this->stripScriptNameBase($path, $this->getFromServer('SCRIPT_NAME'));
+        return $this->stripScriptName($path, $this->getFromServer('SCRIPT_NAME'));
+    }
+
+    private function stripQueryString($request_uri)
+    {
+        $query_string_start = strpos($request_uri, '?');
+        return substr($request_uri, 0, $query_string_start);
+    }
+
+    private function stripScriptNameBase($request_uri, $script_name)
+    {
+        $script_name_base_end = strlen(dirname($script_name));
+        return substr($request_uri, $script_name_base_end);
+    }
+
+    private function stripScriptName($request_uri, $script_name)
+    {
+        $script_name       = basename($script_name);
+        $script_name_start = strpos($request_uri, '/'.$script_name);
+        if ($script_name_start !== false) {
+            return substr($request_uri, strlen($script_name) + 1);
+        }
+        return $request_uri;
+    }
 }

plugins/agiledashboard/include/AgileDashboard/REST/v1/BacklogItemResource.php
@@ -165,14 +165,15 @@ class BacklogItemResource extends AuthenticatedResource {
         if ($initial_effort_field && $initial_effort_field->userCanRead($current_user)) {
 
             $rest_value = $initial_effort_field->getFullRESTValue($current_user, $artifact->getLastChangeset());
+            if ($rest_value) {
+                if (is_a($initial_effort_field, 'Tracker_FormElement_Field_List')) {
+                    $value = $this->getBacklogItemInitialEffortFromList($rest_value);
+                } else {
+                    $value = $rest_value->value;
+                }
 
-            if (is_a($initial_effort_field, 'Tracker_FormElement_Field_List')) {
-                $value = $this->getBacklogItemInitialEffortFromList($rest_value);
-            } else {
-                $value = $rest_value->value;
+                $backlog_item->setInitialEffort($value);
             }
-
-            $backlog_item->setInitialEffort($value);
         }
 
         return $backlog_item;

plugins/git/etc/gitphp.conf.php
@@ -58,20 +58,6 @@ $gitphp_conf['gittmp'] = "/tmp/";
 $gitphp_conf['title'] = "Tuleap";
 
 /*
- * self
- * This is the path to the script that will be inserted
- * in urls.  If you leave this blank/commented the script
- * will try to guess the correct URL, but you can override
- * it here if it's not being guessed correctly.
- *
- * If only specifying the directory path (and omitting the
- * index.php filename), make sure to include the trailing
- * slash!
- */
-$gitphp_conf['self'] = "http://{$_SERVER['SCRIPT_URL']}/";
-$_SERVER['SCRIPT_NAME'] = $_SERVER['SCRIPT_URL'];
-
-/*
  * compressformat
  * Indicates what kind of compression will be done on the
  * snapshot archive.  Recognized settings are:

plugins/svn/etc/nginx18/svn.conf
@@ -1,0 +1,33 @@
+location ^~ /plugins/svn {
+    alias /usr/share/tuleap/plugins/svn/www;
+
+    if (!-f $request_filename) {
+        rewrite ^ /plugins/svn/index.php last;
+    }
+
+    location ~ \.php(/|$) {
+        if (!-f $request_filename) {
+            rewrite ^ /plugins/svn/index.php last;
+        }
+        fastcgi_pass 127.0.0.1:9000;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $request_filename;
+    }
+}
+
+location ^~ /svnplugin {
+    proxy_pass http://tuleap;
+    proxy_set_header X-Real-IP         $remote_addr;
+    # Allow to know what is the original IP address (esp. for logging purpose as well as session management)
+    proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+    # Allow to know what is the original protocol (so Tuleap knows if things were in HTTPS)
+    proxy_set_header X-Forwarded-Proto $scheme;
+    # What is the name of the platform to the end users
+    proxy_set_header Host              $host;
+    # Write Destination header for Subversion COPY and MOVE operations
+    set $fixed_destination $http_destination;
+    if ( $http_destination ~* ^https(.*)$ ) {
+        set $fixed_destination http$1;
+    }
+    proxy_set_header Destination $fixed_destination;
+}

plugins/svn/include/Svn/ViewVC/EPELViewVCProxy.php
@@ -167,16 +167,7 @@ class EPELViewVCProxy implements ViewVCProxy
         $this->access_history_saver->saveAccess($user, $repository);
 
         //this is very important. default path must be /
-        $path = "/";
-
-        if ($request->getFromServer('PATH_INFO') != "") {
-            $path = $request->getFromServer('PATH_INFO');
-
-            // hack: path must always end with /
-            if (strrpos($path, "/") != (strlen($path) - 1)) {
-                $path .= "/";
-            }
-        }
+        $path = $request->getPathInfoFromFCGI();
 
         $command = 'HTTP_COOKIE='.$this->escapeStringFromServer($request, 'HTTP_COOKIE').' '.
             'REMOTE_USER=' . escapeshellarg($user->getUserName()) . ' '.

src/etc/fpm.conf.dist
@@ -10,5 +10,11 @@ php_value[memory_limit] = 196M
 php_admin_flag[short_open_tag] = on
 php_value[date.timezone] = Europe/Paris
 
-# For "/users" or "/projects"
-security.limit_extensions =
+; For development, catch worker output
+catch_workers_output = yes
+php_flag[display_errors] = on
+php_flag[html_errors] = on
+
+; For git over http
+;php_value[post_max_size] = 256M
+;php_value[upload_max_filesize] = 256M

src/etc/nginx18/README.mkd
@@ -1,0 +1,89 @@
+Canonical nginx configuration to be used with php fpm
+=====================================================
+
+How to use it ?
+---------------
+
+### Prepare the ground
+
+You need to adapt apache configuration to run it on localhost:8080. Moreover you no longer need to run ssl on it as
+it will be handled by nginx.
+
+Edit `/etc/httpd/conf/httpd.conf` and change the values for:
+
+* Listen 127.0.0.1:8080
+* NameVirtualHost *:8080
+* <VirtualHost *:8080>
+* #Include conf/ssl.conf
+
+### Deploy fpm conf
+
+In `/etc/opt/rh/rh-php56/php-fpm.d/www.conf`
+* Remove php_value[session.save_path]
+* Remove php_value[soap.wsdl_cache_dir]
+* Append the content of src/etc/fpm.conf.dist (cat /usr/share/tuleap/src/etc/fpm.conf.dist >> /etc/opt/rh/rh-php56/php-fpm.d/www.conf)
+
+start fpm:
+
+    service rh-php56-php-fpm restart
+
+### Deploy nginx
+
+Deploy `src/etc/nginx18/tuleap.conf.dist` as `/etc/opt/rh/rh-nginx18/nginx/conf.d/tuleap.conf` if it doesn't exist yet
+adapt to your context (name, ssl, etc)
+
+Then run
+
+    /usr/share/tuleap/src/etc/nginx18/redeploy.sh
+
+The configuration is meant to be redeployable at anytime.
+
+Note: if you ever need to go back to apache mode, you can by editing `/etc/opt/rh/rh-nginx18/nginx/conf.d/tuleap.conf`
+and swaping the includes to "promote" `conf.d/tuleap-apache.proxy`
+
+Organisation
+============
+
+We assume that:
+
+* nginx.conf file only has "include conf.d/*.conf"
+* conf.d/tuleap.conf is under admin responsability (esp. SSL config)
+* all the other parts are managed by Tuleap itself or the base nginx package.
+
+Files organization
+
+    nginx.conf
+    conf.d/
+    |-- tuleap.d/
+    |      Core configuration / Should not be modified by hand
+    |-- tuleap-plugins/
+    |      Plugin configuration  / Should not be modified by hand
+    `-- tuleap.conf / modified by admin
+
+Pitfalls and issues
+-------------------
+
+### PATH_INFO & try_files
+
+To make it simple: do not try to mix try_files and PATH_INFO.
+Worst, try_files will only reliably work if you have a full fledged "front-controller pattern. Otherwise your are
+pretty much doomed.
+
+### Multiviews
+
+Apache multiviews allow some URL simplifications:
+/help/api -> /help/api.php
+
+To make it simple, this will not work with nginx and should be avoided & replaced.
+
+Try to fix access to /help/api that doesn't work
+https://forum.nginx.org/read.php?9,235994
+http://serverfault.com/questions/211515/apache-style-multiviews-with-nginx
+http://serverfault.com/questions/277768/nginx-equivalent-to-apache-multiviews
+https://www.digitalocean.com/community/tutorials/understanding-nginx-server-and-location-block-selection-algorithms
+https://www.nginx.com/resources/wiki/start/topics/tutorials/config_pitfalls/
+https://www.digitalocean.com/community/tutorials/understanding-and-implementing-fastcgi-proxying-in-nginx
+
+#location / {
+#    try_files $uri $uri/ /index.php?$args;
+#}

src/etc/nginx18/plugin.conf.dist
@@ -1,0 +1,21 @@
+# See the how and the why here: http://serverfault.com/a/828579/169219
+location ^~ /plugins/%name% {
+    alias /usr/share/tuleap/plugins/%name%/www;
+
+    if (!-f $request_filename) {
+        rewrite ^ /plugins/%name%/index.php last;
+    }
+
+    location ~ \.php$ {
+        if (!-f $request_filename) {
+            rewrite ^ /plugins/%name%/index.php last;
+        }
+        fastcgi_pass 127.0.0.1:9000;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $request_filename;
+    }
+
+    location ~ /assets {
+        expires 12h;
+    }
+}

commit e375ac31ac0d8ba30f499b91a4924b4a59e9f32b

request #10074: ViewVC doesn't deal properly with folders with spaces
Issue has been introduced by commit 9030d2e569931239c90e1c94b5bbff9d3a559e20

Change-Id: Iec43fd5a426a92e03df760cc24997316d906ff52

src/common/include/HTTPRequest.class.php
@@ -304,7 +304,7 @@ class HTTPRequest extends Codendi_Request {
      */
     public function getPathInfoFromFCGI()
     {
-        $path = $this->stripQueryString($this->getFromServer('REQUEST_URI'));
+        $path = $this->stripQueryString(urldecode($this->getFromServer('REQUEST_URI')));
         $path = $this->stripScriptNameBase($path, $this->getFromServer('SCRIPT_NAME'));
         return $this->stripScriptName($path, $this->getFromServer('SCRIPT_NAME'));
     }