8f07fbc9f35c98cbb8d35bb88bd5f24b77b24711
Thomas Gerbet <thomas.gerbet@enalean.com>
2017-03-21 13:58:24 +0100

    A project created webhook can be added
    This is part of story #9962: trigger a webhook when a project is created
    
    Change-Id: I6b9ad75b387aa74a79784dfff47c17271470f26b

--- src/common/project/Admin/WebhooksPresenter.php
+++ src/common/project/Admin/WebhooksPresenter.php
@@ -31,6 +31,10 @@ class WebhooksPresenter
      * @var WebhookPresenter[]
      */
     public $webhooks;
+    /**
+     * @var \CSRFSynchronizerToken
+     */
+    public $csrf_token;
     public $tab_title;
     public $pane_title;
     public $project_creation_description;
@@ -43,25 +47,35 @@ class WebhooksPresenter
     public $close_modal;
     public $date_modal;
     public $status_modal;
+    public $add_webhook;
+    public $add_webhook_modal;
+    public $add_webhook_description_modal;
+    public $name_placeholder_modal;
 
-    public function __construct($title, array $webhooks)
+    public function __construct($title, array $webhooks, \CSRFSynchronizerToken $csrf_token)
     {
         $this->title = $title;
 
         $this->webhooks     = $webhooks;
         $this->has_webhooks = !empty($webhooks);
 
-        $this->tab_title                    = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_tab_title');
-        $this->pane_title                   = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_pane_title');
-        $this->project_creation_description = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_project_creation_description');
-        $this->name                         = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_name');
-        $this->url                          = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_url');
-        $this->last_push_date               = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_last_push_date');
-        $this->last_push_status             = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_last_push_status');
-        $this->no_webhooks                  = $GLOBALS['Language']->getText('admin_project_configuration', 'no_webhooks');
-        $this->button_show_logs             = $GLOBALS['Language']->getText('admin_project_configuration', 'show_logs');
-        $this->close_modal                  = $GLOBALS['Language']->getText('admin_project_configuration', 'close_modal');
-        $this->date_modal                   = $GLOBALS['Language']->getText('admin_project_configuration', 'date_modal');
-        $this->status_modal                 = $GLOBALS['Language']->getText('admin_project_configuration', 'status_modal');
+        $this->csrf_token = $csrf_token;
+
+        $this->tab_title                     = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_tab_title');
+        $this->pane_title                    = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_pane_title');
+        $this->project_creation_description  = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_project_creation_description');
+        $this->name                          = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_name');
+        $this->url                           = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_url');
+        $this->last_push_date                = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_last_push_date');
+        $this->last_push_status              = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_last_push_status');
+        $this->no_webhooks                   = $GLOBALS['Language']->getText('admin_project_configuration', 'no_webhooks');
+        $this->button_show_logs              = $GLOBALS['Language']->getText('admin_project_configuration', 'show_logs');
+        $this->close_modal                   = $GLOBALS['Language']->getText('admin_project_configuration', 'close_modal');
+        $this->date_modal                    = $GLOBALS['Language']->getText('admin_project_configuration', 'date_modal');
+        $this->status_modal                  = $GLOBALS['Language']->getText('admin_project_configuration', 'status_modal');
+        $this->add_webhook                   = $GLOBALS['Language']->getText('admin_project_configuration', 'add_new_webhook');
+        $this->add_webhook_modal             = $GLOBALS['Language']->getText('admin_project_configuration', 'add_webhook_modal');
+        $this->add_webhook_description_modal = $GLOBALS['Language']->getText('admin_project_configuration', 'add_webhook_description_modal');
+        $this->name_placeholder_modal        = $GLOBALS['Language']->getText('admin_project_configuration', 'webhook_name_placeholder_modal');
     }
 }
