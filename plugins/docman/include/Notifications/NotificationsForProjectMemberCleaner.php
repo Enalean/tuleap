4dd84c12cf0f6b95f6afffcb0bf30c6de369e1ca
Joris Masson <joris.masson@enalean.com>
2017-03-23 11:36:35 +0100

    Deal with user removed from private project
    This is a part of story #9892: notify group of people in addition to
    individuals
    
    In a private project, add notifications for a user. Then remove this
    user from the project. All her notifications should be removed.
    In a public project, notifications should not be removed.
    
    Change-Id: I40b8156bf5a89338536eb9a4b883ce3299714050

---
+++ plugins/docman/include/Notifications/NotificationsForProjectMemberCleaner.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Docman\Notifications;
+
+use Docman_ItemFactory;
+use Docman_NotificationsManager;
+use PFUser;
+use Project;
+
+class NotificationsForProjectMemberCleaner
+{
+    /**
+     * @var Docman_ItemFactory
+     */
+    private $item_factory;
+    /**
+     * @var Dao
+     */
+    private $users_to_notify_dao;
+    /**
+     * @var Docman_NotificationsManager
+     */
+    private $notifications_manager;
+
+    public function __construct(
+        Docman_ItemFactory $item_factory,
+        Docman_NotificationsManager $notifications_manager,
+        Dao $users_to_notify_dao
+    ) {
+        $this->item_factory          = $item_factory;
+        $this->users_to_notify_dao   = $users_to_notify_dao;
+        $this->notifications_manager = $notifications_manager;
+    }
+
+    public function cleanNotificationsAfterUserRemoval(Project $project, PFUser $user)
+    {
+        if ($project->isPublic()) {
+            return;
+        }
+
+        if ($user->isMember($project->getID())) {
+            return;
+        }
+
+        $root_item = $this->item_factory->getRoot($project->getID());
+        if (! $root_item) {
+            return;
+        }
+
+        $dar = $this->notifications_manager->listAllMonitoredItems($project->getID(), $user->getId());
+        if ($dar && ! $dar->isError()) {
+            foreach ($dar as $row) {
+                $this->notifications_manager->remove($row['user_id'], $row['item_id'], $row['type']);
+            }
+        }
+    }
+}
