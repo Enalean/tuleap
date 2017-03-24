4939e6ba5cea0d127321a25e58f7857197602dae
Manuel VACELET <manuel.vacelet@enalean.com>
2017-03-24 14:51:05 +0100

    request #10071 Mediawiki 1.23 migration page links are not correct
    Link to the right page
    
    Change-Id: I8bc75eedcb821d9b046a82313b7162b3e791cef5

--- plugins/mediawiki/include/MediawikiAdminPermissionsPanePresenter.class.php
+++ plugins/mediawiki/include/MediawikiAdminPermissionsPanePresenter.class.php
@@ -79,7 +79,7 @@ class MediawikiAdminPermissionsPanePresenter extends MediawikiAdminPanePresenter
     }
 
     public function route() {
-        return MEDIAWIKI_BASE_URL . '/forge_admin?' . http_build_query(array(
+        return MEDIAWIKI_BASE_URL . '/forge_admin.php?' . http_build_query(array(
             'group_id' => $this->project->getID(),
             'action'   => 'save_permissions'
         ));
