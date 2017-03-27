c91affd5ee1f72d17fc67639043a560f7d96c725
Thomas Gerbet <thomas.gerbet@enalean.com>
2017-03-27 17:58:41 +0200

    request #10094: XSS when displaying the administration or a transition of a list field
    Change-Id: Ie37e5583f0c648e1b9978ee9cfb6acf2f97518c5

--- plugins/tracker/www/scripts/TrackerBinds.js
+++ plugins/tracker/www/scripts/TrackerBinds.js
@@ -1,20 +1,21 @@
 /**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 var codendi = codendi || { };
@@ -56,13 +57,18 @@ codendi.tracker.bind.Editor = Class.create({
         });
         return s.join(' ');
     },
+    nl2br: function(str) {
+        return str.replace('/\n/g', '<br />');
+    },
     //hide the textarea and textfield which update description and label of the value
     //replace them by a link. If the user click on the link, hide the link and show the fields
     editStaticValues: function (element) {
         var tf_label = element;
         var ta_description = element.up().down('textarea');
-        var link = new Element('a', { href: '#', title: 'Edit ' + tf_label.value }).update(tf_label.value);
-        var descr = new Element('div').addClassName('tracker-admin-bindvalue_description').update(this.wordwrap(ta_description.value, 80, '<br />'));
+        var link = new Element('a', { href: '#', title: 'Edit ' + tf_label.value }).update(tuleap.escaper.html(tf_label.value));
+        var descr = new Element('div').addClassName('tracker-admin-bindvalue_description').update(
+            this.nl2br(tuleap.escaper.html(this.wordwrap(ta_description.value, 80, '\n')))
+        );
         tf_label.insert({before: link});
         link.insert({after: descr});
         link.observe('click', function (evt) {
