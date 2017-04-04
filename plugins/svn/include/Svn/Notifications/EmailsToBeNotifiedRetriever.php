4f0af67fa5443c77cb7e061890689129e2839d9e
Nicolas Terray <nicolas.terray@enalean.com>
2017-04-04 16:01:21 +0200

    Send notifications to ugroup in addition to emails
    Part of story #9897: notify group in SVN
    
    If you enter manually in the db a ugroup for a svn notification, then
    the members of this ugroup will receive an email each time the commit is
    done on the given path.
    
    Change-Id: I61614bad8584000001db6766e60be655a70dc1ab

--- plugins/svn/include/Svn/Notifications/EmailsToBeNotifiedRetriever.php
+++ plugins/svn/include/Svn/Notifications/EmailsToBeNotifiedRetriever.php
@@ -20,9 +20,11 @@
 
 namespace Tuleap\Svn\Notifications;
 
+use ProjectUGroup;
 use Tuleap\Svn\Admin\MailNotification;
 use Tuleap\Svn\Admin\MailNotificationManager;
 use Tuleap\Svn\Repository\Repository;
+use UGroupManager;
 
 class EmailsToBeNotifiedRetriever
 {
@@ -34,13 +36,25 @@ class EmailsToBeNotifiedRetriever
      * @var UsersToNotifyDao
      */
     private $user_dao;
+    /**
+     * @var UgroupsToNotifyDao
+     */
+    private $ugroup_dao;
+    /**
+     * @var UGroupManager
+     */
+    private $ugroup_manager;
 
     public function __construct(
         MailNotificationManager $notification_manager,
-        UsersToNotifyDao $user_dao
+        UsersToNotifyDao $user_dao,
+        UgroupsToNotifyDao $ugroup_dao,
+        UGroupManager $ugroup_manager
     ) {
         $this->notification_manager = $notification_manager;
         $this->user_dao             = $user_dao;
+        $this->ugroup_dao           = $ugroup_dao;
+        $this->ugroup_manager       = $ugroup_manager;
     }
 
     public function getEmailsToBeNotifiedForPath(Repository $repository, $path)
@@ -49,19 +63,45 @@ class EmailsToBeNotifiedRetriever
 
         $notifications = $this->notification_manager->getByPath($repository, $path);
         foreach ($notifications as $notification) {
-            $mail_list       = explode(",", $notification->getNotifiedMails());
-            $mail_list       = array_map('trim', $mail_list);
-            $notified_emails = array_merge($mail_list, $notified_emails);
+            $this->addEmails($notification, $notified_emails);
             $this->addUsers($notification, $notified_emails);
+            $this->addUgroups($notification, $notified_emails);
         }
 
-        return array_values(array_filter($notified_emails));
+        return array_unique(array_values(array_filter($notified_emails)));
+    }
+
+    private function addEmails($notification, &$emails)
+    {
+        $mail_list = explode(",", $notification->getNotifiedMails());
+        $mail_list = array_map('trim', $mail_list);
+        $emails    = array_merge($mail_list, $emails);
     }
 
     private function addUsers(MailNotification $notification, array &$emails)
     {
         foreach ($this->user_dao->searchUsersByNotificationId($notification->getId()) as $row) {
             $emails[] = $row['email'];
+        }
+    }
+
+    private function addUgroups(MailNotification $notification, array &$emails)
+    {
+        $project = $notification->getRepository()->getProject();
+        foreach ($this->ugroup_dao->searchUgroupsByNotificationId($notification->getId()) as $row) {
+            $ugroup = $this->ugroup_manager->getUGroup($project, $row['ugroup_id']);
+            if ($ugroup) {
+                $this->addUgroupMembers($ugroup, $emails);
+            }
+        }
+    }
+
+    private function addUgroupMembers(ProjectUGroup $ugroup, array &$emails)
+    {
+        foreach ($ugroup->getMembers() as $user) {
+            if ($user->isAlive()) {
+                $emails[] = $user->getEmail();
+            }
         }
     }
 }
