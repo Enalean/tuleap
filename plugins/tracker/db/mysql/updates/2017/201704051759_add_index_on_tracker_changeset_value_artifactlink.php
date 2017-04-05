5d5d6e1ba1ae932af6694dd72052db0a2ba57bb1
Manuel VACELET <manuel.vacelet@enalean.com>
2017-04-05 18:01:42 +0200

    request #10127 Missing index related to artifact links type
    Change-Id: I52e6f778e8263e668aa6b9118d21fa824a18d7eb

---
+++ plugins/tracker/db/mysql/updates/2017/201704051759_add_index_on_tracker_changeset_value_artifactlink.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @codingStandardsIgnoreFile
+ */
+
+class b201704051759_add_index_on_tracker_changeset_value_artifactlink extends ForgeUpgrade_Bucket
+{
+    public function description()
+    {
+        return "Add index to on tracker_changeset_value_artifactlink";
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $this->updateTable();
+    }
+
+    private function updateTable()
+    {
+        $sql = "ALTER TABLE tracker_changeset_value_artifactlink ADD INDEX idx_group_id_keyword (group_id, keyword)";
+
+        $this->addIndex('tracker_changeset_value_artifactlink', 'idx_group_id_keyword', $sql);
+    }
+
+    private function indexNameExists($table_name, $index) {
+        $sql = 'SHOW INDEX FROM '.$table_name.' WHERE Key_name LIKE '.$this->db->dbh->quote($index);
+        $res = $this->db->dbh->query($sql);
+        if ($res && $res->fetch() !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function addIndex($table_name, $index, $sql) {
+        $this->log->info('Add index '.$table_name);
+        if (!$this->indexNameExists($table_name, $index)) {
+            $res = $this->db->dbh->exec($sql);
+            if ($res === false) {
+                $info = $this->db->dbh->errorInfo();
+                $msg  = 'An error occured adding index to '.$table_name.': '.$info[2].' ('.$info[1].' - '.$info[0].')';
+                $this->log->error($msg);
+                throw new ForgeUpgrade_Bucket_Db_Exception($msg);
+            }
+            $this->log->info($index.' successfully added index');
+        } else {
+            $this->log->info($index.' already exists');
+        }
+    }
+}
