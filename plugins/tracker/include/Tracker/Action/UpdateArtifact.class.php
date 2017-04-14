Enalean/tuleap

commit f140d26eb5bba49bfdb898c4ae3d6587097d7197

request #10118: Remote code execution through object unserialization of a user's recent elements
The CVE-ID CVE-2017-7411 has been assigned to this issue.

Change-Id: Ia6bed7dda9fb4e0f07dad4ec5de0e4341b9da064

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact.class.php
@@ -31,6 +31,8 @@ use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureIsChildLinkRetrie
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationCollectionBuilder;
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationDetector;
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\SubmittedValueConvertor;
+use Tuleap\Tracker\RecentlyVisited\RecentlyVisitedDao;
+use Tuleap\Tracker\RecentlyVisited\VisitRecorder;
 
 class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable_Interface {
     const REST_ROUTE        = 'artifacts';
@@ -805,7 +807,7 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
                 $this->getPriorityManager()->moveArtifactAfterWithHistoryChangeLogging($this->getId(), $target_id, $milestone_id, $project_id);
                 break;
             case 'show-in-overlay':
-                $renderer = new Tracker_Artifact_EditOverlayRenderer($this, $this->getEventManager());
+                $renderer = new Tracker_Artifact_EditOverlayRenderer($this, $this->getEventManager(), $this->getVisitRecorder());
                 $renderer->display($request, $current_user);
                 break;
             case 'get-new-changesets':
@@ -840,7 +842,8 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
                     $this,
                     $this->getFormElementFactory(),
                     $layout,
-                    $this->getNatureIsChildLinkRetriever()
+                    $this->getNatureIsChildLinkRetriever(),
+                    $this->getVisitRecorder()
                 );
 
                 $renderer->display($request, $current_user);
@@ -865,7 +868,8 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
                         $this->getEventManager(),
                         $this, $this->getFormElementFactory(),
                         $layout,
-                        $this->getNatureIsChildLinkRetriever()
+                        $this->getNatureIsChildLinkRetriever(),
+                        $this->getVisitRecorder()
                     );
                     $renderer->display($request, $current_user);
                 }
@@ -880,6 +884,14 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
 
     private function getArtifactlinkDao() {
         return new Tracker_FormElement_Field_Value_ArtifactLinkDao();
+    }
+
+    /**
+     * @return VisitRecorder
+     */
+    private function getVisitRecorder()
+    {
+        return new VisitRecorder(new RecentlyVisitedDao());
     }
 
     private function sendUserDoesNotHavePermissionsErrorCode() {

plugins/tracker/db/install.sql
@@ -801,6 +801,14 @@ CREATE TABLE plugin_tracker_notification_assigned_to (
     project_id INT(11) NOT NULL PRIMARY KEY
 ) ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS plugin_tracker_recently_visited;
+CREATE TABLE plugin_tracker_recently_visited (
+    user_id INT(11) NOT NULL,
+    artifact_id INT(11) NOT NULL,
+    created_on INT(11) UNSIGNED NOT NULL,
+    PRIMARY KEY(user_id, artifact_id)
+) ENGINE=InnoDB;
+
 -- Enable service for project 100
 INSERT INTO service(group_id, label, description, short_name, link, is_active, is_used, scope, rank)
        VALUES      ( 100, 'plugin_tracker:service_lbl_key', 'plugin_tracker:service_desc_key', 'plugin_tracker', '/plugins/tracker/?group_id=$group_id', 1, 1, 'system', 151);

plugins/tracker/db/mysql/updates/2017/201704041400_add_recently_visited_table.php
@@ -1,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201704041400_add_recently_visited_table extends ForgeUpgrade_Bucket
+{
+    public function description()
+    {
+        return 'Add table to store the the recently visited artifacts';
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $sql = 'CREATE TABLE plugin_tracker_recently_visited (
+                  user_id INT(11) NOT NULL,
+                  artifact_id INT(11) NOT NULL,
+                  created_on INT(11) UNSIGNED NOT NULL,
+                  PRIMARY KEY(user_id, artifact_id)
+                ) ENGINE=InnoDB';
+
+        $this->db->createTable('plugin_tracker_recently_visited', $sql);
+    }
+
+    public function postUp()
+    {
+        if (! $this->db->tableNameExists('plugin_tracker_recently_visited')) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('plugin_tracker_recently_visited table is missing');
+        }
+    }
+}

plugins/tracker/db/uninstall.sql
@@ -92,6 +92,8 @@ DROP TABLE IF EXISTS plugin_tracker_artifactlink_natures_allowed_projects;
 
 DROP TABLE IF EXISTS plugin_tracker_notification_assigned_to;
 
+DROP TABLE IF EXISTS plugin_tracker_recently_visited;
+
 DELETE FROM permissions WHERE permission_type LIKE 'PLUGIN_TRACKER_%';
 DELETE FROM permissions_values WHERE permission_type LIKE 'PLUGIN_TRACKER_%';
 

plugins/tracker/include/Tracker/Artifact/RecentlyVisited/RecentlyVisitedDao.php
@@ -1,0 +1,79 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\RecentlyVisited;
+
+use DataAccessObject;
+
+class RecentlyVisitedDao extends DataAccessObject
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->enableExceptionsOnError();
+    }
+
+    /**
+     * @return bool
+     */
+    public function save($user_id, $artifact_id, $created_on)
+    {
+        $user_id     = $this->da->escapeInt($user_id);
+        $artifact_id = $this->da->escapeInt($artifact_id);
+        $created_on  = $this->da->escapeInt($created_on);
+
+        $this->startTransaction();
+        $sql_update     = "INSERT INTO plugin_tracker_recently_visited(user_id, artifact_id, created_on)
+                VALUES ($user_id, $artifact_id, $created_on)
+                ON DUPLICATE KEY UPDATE created_on=$created_on";
+        $has_been_saved = $this->update($sql_update);
+        if (! $has_been_saved) {
+            $this->rollBack();
+            return false;
+        }
+
+        $sql_clean_history     = "DELETE FROM plugin_tracker_recently_visited WHERE user_id = $user_id AND created_on <= (
+                                    SELECT created_on FROM (
+                                      SELECT created_on FROM plugin_tracker_recently_visited WHERE user_id = $user_id ORDER BY created_on DESC LIMIT 1 OFFSET 6
+                                    ) oldest_entry_to_keep
+                                  )";
+        $has_history_been_cleaned = $this->update($sql_clean_history);
+
+        if (! $has_history_been_cleaned) {
+            $this->rollBack();
+            return false;
+        }
+
+        $this->commit();
+        return true;
+    }
+
+    /**
+     * @return \DataAccessResult|false
+     */
+    public function searchVisitByUserId($user_id)
+    {
+        $user_id = $this->da->escapeInt($user_id);
+
+        $sql = "SELECT artifact_id FROM plugin_tracker_recently_visited WHERE user_id = $user_id";
+
+        return $this->retrieve($sql);
+    }
+}

plugins/tracker/include/Tracker/Artifact/RecentlyVisited/VisitRecorder.php
@@ -1,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\RecentlyVisited;
+
+class VisitRecorder
+{
+    /**
+     * @var RecentlyVisitedDao
+     */
+    private $dao;
+
+    public function __construct(RecentlyVisitedDao $dao)
+    {
+        $this->dao = $dao;
+    }
+
+    /**
+     * @throws \DataAccessException
+     */
+    public function record(\PFUser $user, \Tracker_Artifact $artifact)
+    {
+        $this->dao->save($user->getId(), $artifact->getId(), $_SERVER['REQUEST_TIME']);
+    }
+}

plugins/tracker/include/Tracker/Artifact/RecentlyVisited/VisitRetriever.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\RecentlyVisited;
+
+class VisitRetriever
+{
+    /**
+     * @var RecentlyVisitedDao
+     */
+    private $dao;
+    /**
+     * @var \Tracker_ArtifactFactory
+     */
+    private $artifact_factory;
+
+    public function __construct(RecentlyVisitedDao $dao, \Tracker_ArtifactFactory $artifact_factory)
+    {
+        $this->dao              = $dao;
+        $this->artifact_factory = $artifact_factory;
+    }
+
+    /**
+     * @return \Tracker_Artifact[]
+     * @throws \DataAccessException
+     */
+    public function getMostRecentlySeenArtifacts(\PFUser $user)
+    {
+        $recently_visited_rows = $this->dao->searchVisitByUserId($user->getId());
+        $artifacts_id = array();
+        foreach ($recently_visited_rows as $recently_visited_row) {
+            $artifacts_id[] = $recently_visited_row['artifact_id'];
+        }
+
+        return $this->artifact_factory->getArtifactsByArtifactIdList($artifacts_id);
+    }
+}

plugins/tracker/include/Tracker/Artifact/Renderer/CopyRenderer.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright Enalean (c) 2014 - 2016. All rights reserved.
+ * Copyright Enalean (c) 2014 - 2017. All rights reserved.
  *
  * Tuleap and Enalean names and logos are registrated trademarks owned by
  * Enalean SAS. All other trademarks or names are properties of their respective
@@ -23,6 +23,7 @@
  */
 
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureIsChildLinkRetriever;
+use Tuleap\Tracker\RecentlyVisited\VisitRecorder;
 
 class Tracker_Artifact_CopyRenderer extends Tracker_Artifact_ReadOnlyRenderer {
 
@@ -31,9 +32,10 @@ class Tracker_Artifact_CopyRenderer extends Tracker_Artifact_ReadOnlyRenderer {
         Tracker_Artifact $artifact,
         Tracker_FormElementFactory $formelement_factory,
         Tracker_IDisplayTrackerLayout $layout,
-        NatureIsChildLinkRetriever $retriever
+        NatureIsChildLinkRetriever $retriever,
+        VisitRecorder $visit_recorder
     ) {
-        parent::__construct($event_manager, $artifact, $formelement_factory, $layout, $retriever);
+        parent::__construct($event_manager, $artifact, $formelement_factory, $layout, $retriever, $visit_recorder);
         $this->redirect->query_parameters = array(
             'tracker' => $artifact->getTrackerId(),
             'func'    => 'submit-copy-artifact',

commit 56874d7797f786beff3ac765e9d31146edf40050

request #10149: Modifying an artifact without doing any changes throws a fatal error
Issue has been introduced by commit f140d26eb5bba49bfdb898c4ae3d6587097d7197

Change-Id: If419731d95dad4c9ae3643fe712a659fa7d28a8e

plugins/tracker/include/Tracker/Action/UpdateArtifact.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013-2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,6 +19,7 @@
  */
 
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureIsChildLinkRetriever;
+use Tuleap\Tracker\RecentlyVisited\VisitRecorder;
 
 class Tracker_Action_UpdateArtifact {
 
@@ -31,17 +32,23 @@ class Tracker_Action_UpdateArtifact {
     /** @var EventManager */
     private $event_manager;
     private $artifact_retriever;
+    /**
+     * @var VisitRecorder
+     */
+    private $visit_recorder;
 
     public function __construct(
         Tracker_Artifact $artifact,
         Tracker_FormElementFactory $form_element_factory,
         EventManager $event_manager,
-        NatureIsChildLinkRetriever $artifact_retriever
+        NatureIsChildLinkRetriever $artifact_retriever,
+        VisitRecorder $visit_recorder
     ) {
         $this->artifact             = $artifact;
         $this->form_element_factory = $form_element_factory;
         $this->event_manager        = $event_manager;
         $this->artifact_retriever   = $artifact_retriever;
+        $this->visit_recorder       = $visit_recorder;
     }
 
     public function process(Tracker_IDisplayTrackerLayout $layout, Codendi_Request $request, PFUser $current_user) {
@@ -84,7 +91,9 @@ class Tracker_Action_UpdateArtifact {
                     $this->artifact,
                     $this->form_element_factory,
                     $layout,
-                    $this->artifact_retriever);
+                    $this->artifact_retriever,
+                    $this->visit_recorder
+                );
                 $render->display($request, $current_user);
             }
         } catch (Tracker_Exception $e) {
@@ -97,7 +106,9 @@ class Tracker_Action_UpdateArtifact {
                     $this->artifact,
                     $this->form_element_factory,
                     $layout,
-                    $this->artifact_retriever);
+                    $this->artifact_retriever,
+                    $this->visit_recorder
+                );
                 $render->display($request, $current_user);
             }
         }

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact.class.php
@@ -728,7 +728,8 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
                     $this,
                     $this->getFormElementFactory(),
                     $this->getEventManager(),
-                    $this->getNatureIsChildLinkRetriever()
+                    $this->getNatureIsChildLinkRetriever(),
+                    $this->getVisitRecorder()
                 );
                 $action->process($layout, $request, $current_user);
                 break;

plugins/tracker/tests/Tracker/Action/UpdateArtifactTest.php
@@ -108,12 +108,14 @@ class Tracker_Artifact_Update_BaseTest extends TuleapTestCase
 
         $this->event_manager      = mock('EventManager');
         $this->artifact_retriever = mock('Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureIsChildLinkRetriever');
+        $visit_recorder           = mock('Tuleap\\Tracker\\RecentlyVisited\\VisitRecorder');
 
         $this->action = new Tracker_Action_UpdateArtifact(
             $this->task,
             $this->formelement_factory,
             $this->event_manager,
-            $this->artifact_retriever
+            $this->artifact_retriever,
+            $visit_recorder
         );
 
     }
@@ -199,14 +201,16 @@ class Tracker_Artifact_SendCardInfoOnUpdate_WithRemainingEffortTest extends Trac
 
     public function itSendTheAutocomputedValueOfTheArtifact()
     {
-        $tracker = aMockTracker()->withId($this->tracker_id)->build();
-        $task    = aMockArtifact()->withId($this->artifact_id)->withTracker($tracker)->build();
+        $tracker        = aMockTracker()->withId($this->tracker_id)->build();
+        $task           = aMockArtifact()->withId($this->artifact_id)->withTracker($tracker)->build();
+        $visit_recorder = mock('Tuleap\\Tracker\\RecentlyVisited\\VisitRecorder');
 
         $action = new Tracker_Action_UpdateArtifact(
             $task,
             $this->formelement_factory,
             $this->event_manager,
-            $this->artifact_retriever
+            $this->artifact_retriever,
+            $visit_recorder
         );
 
         stub($GLOBALS['Language'])->getText('plugin_tracker', 'autocomputed_field')->returns('autocomputed');
@@ -350,12 +354,14 @@ class Tracker_Artifact_RedirectUrlTest extends Tracker_Artifact_Update_BaseTest 
     }
 
     private function getRedirectUrlFor($request_data) {
-        $request  = new Codendi_Request($request_data);
-        $action   = new Tracker_Artifact_RedirectUrlTestVersion(
+        $request        = new Codendi_Request($request_data);
+        $visit_recorder = mock('Tuleap\\Tracker\\RecentlyVisited\\VisitRecorder');
+        $action         = new Tracker_Artifact_RedirectUrlTestVersion(
             $this->task,
             $this->formelement_factory,
             $this->event_manager,
-            $this->artifact_retriever
+            $this->artifact_retriever,
+            $visit_recorder
         );
         return $action->getRedirectUrlAfterArtifactUpdate($request);
 