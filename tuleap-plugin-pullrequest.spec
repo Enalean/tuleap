Enalean/tuleap

commit f944d133718f297ece2b52c13745272fad9896d0

request #10148: Fine grained permissions are not taken into account when merging a pull request from the web UI
Change-Id: I5b9568ed42720de14ac6abf697c83fb24fbbb200

tuleap-plugin-pullrequest.spec
@@ -47,6 +47,8 @@ npm run build
 %{__install} -m 755 -d $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/pullrequest
 %{__cp} -ar include db templates README.mkd VERSION ChangeLog site-content $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/pullrequest
 
+%{__install} etc/sudoers.d/gitolite-access-command $RPM_BUILD_ROOT/etc/sudoers.d/gitolite-access-command
+
 # logrotate
 %{__mkdir} -p $RPM_BUILD_ROOT/etc/logrotate.d
 %{__install} etc/logrotate.syslog.dist $RPM_BUILD_ROOT/etc/logrotate.d/tuleap_pullrequest

etc/sudoers.d/gitolite-access-command
@@ -1,0 +1,3 @@
+Defaults:codendiadm !requiretty
+Defaults:codendiadm !env_reset
+codendiadm ALL= (gitolite) SETENV: NOPASSWD: /usr/bin/gitolite access -q *

include/PullRequest/Authorization/AccessControlVerifier.php
@@ -1,0 +1,76 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\PullRequest\Authorization;
+
+use GitRepository;
+use PFUser;
+use System_Command;
+use Tuleap\Git\Permissions\FineGrainedRetriever;
+
+class AccessControlVerifier
+{
+    /**
+     * @var FineGrainedRetriever
+     */
+    private $fine_grained_retriever;
+    /**
+     * @var System_Command
+     */
+    private $system_command;
+
+    public function __construct(FineGrainedRetriever $fine_grained_retriever, System_Command $system_command)
+    {
+        $this->fine_grained_retriever = $fine_grained_retriever;
+        $this->system_command         = $system_command;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canWrite(PFUser $user, GitRepository $repository, $reference)
+    {
+        if ($this->fine_grained_retriever->doesRepositoryUseFineGrainedPermissions($repository)) {
+            return $this->canWriteAccordingToGitolite($user, $repository, $reference);
+        }
+
+        return $user->hasPermission(\Git::PERM_WRITE, $repository->getId(), $repository->getProjectId()) ||
+            $user->hasPermission(\Git::PERM_WPLUS, $repository->getId(), $repository->getProjectId());
+    }
+
+    /**
+     * @return bool
+     */
+    private function canWriteAccordingToGitolite(PFUser $user, GitRepository $repository, $reference)
+    {
+        require_once GIT_BASE_DIR.'/PathJoinUtil.php';
+        $repository_name = escapeshellarg(unixPathJoin(array($repository->getProject()->getUnixName(), $repository->getFullName())));
+        $user_name       = escapeshellarg($user->getUserName());
+        $reference       = escapeshellarg($reference);
+
+        try {
+            $this->system_command->exec("sudo -u gitolite gitolite access -q $repository_name $user_name 'W' $reference");
+        } catch (\System_Command_CommandException $ex) {
+            return false;
+        }
+
+        return true;
+    }
+}

include/PullRequest/REST/v1/PullRequestRepresentationFactory.php
@@ -20,6 +20,7 @@
 
 namespace Tuleap\PullRequest\REST\v1;
 
+use Tuleap\PullRequest\Authorization\AccessControlVerifier;
 use Tuleap\PullRequest\GitExec;
 use Tuleap\PullRequest\PullRequest;
 
@@ -30,10 +31,15 @@ class PullRequestRepresentationFactory
      * @var GitExec;
      */
     private $executor;
+    /**
+     * @var AccessControlVerifier
+     */
+    private $access_control_verifier;
 
-    public function __construct(GitExec $executor)
+    public function __construct(GitExec $executor, AccessControlVerifier $access_control_verifier)
     {
-        $this->executor = $executor;
+        $this->executor                = $executor;
+        $this->access_control_verifier = $access_control_verifier;
     }
 
     public function getPullRequestRepresentation($pull_request, $repository_src, $repository_dest, $user)
@@ -42,8 +48,9 @@ class PullRequestRepresentationFactory
         $short_stat_repres = new PullRequestShortStatRepresentation();
         $short_stat_repres->build($short_stat);
 
-        $user_can_merge   = $repository_dest->userCanWrite($user);
-        $user_can_abandon = ($repository_dest->userCanWrite($user) || $repository_src->userCanWrite($user));
+        $user_can_merge   = $this->access_control_verifier->canWrite($user, $repository_dest, $pull_request->getBranchDest());
+        $user_can_abandon = $user_can_merge ||
+            $this->access_control_verifier->canWrite($user, $repository_src, $pull_request->getBranchSrc());
 
         $pull_request_representation = new PullRequestRepresentation();
         $pull_request_representation->build(

include/PullRequest/REST/v1/PullRequestsResource.php
@@ -21,6 +21,9 @@
 namespace Tuleap\PullRequest\REST\v1;
 
 use Luracast\Restler\RestException;
+use Tuleap\Git\Permissions\FineGrainedDao;
+use Tuleap\Git\Permissions\FineGrainedRetriever;
+use Tuleap\PullRequest\Authorization\AccessControlVerifier;
 use Tuleap\PullRequest\Comment\Comment;
 use Tuleap\PullRequest\Comment\Dao as CommentDao;
 use Tuleap\PullRequest\Comment\Factory as CommentFactory;
@@ -106,6 +109,11 @@ class PullRequestsResource extends AuthenticatedResource
      */
     private $inline_comment_creator;
 
+    /**
+     * @var AccessControlVerifier
+     */
+    private $access_control_verifier;
+
     public function __construct()
     {
         $this->git_repository_factory = new GitRepositoryFactory(
@@ -146,6 +154,11 @@ class PullRequestsResource extends AuthenticatedResource
 
         $dao = new \Tuleap\PullRequest\InlineComment\Dao();
         $this->inline_comment_creator = new InlineCommentCreator($dao, $reference_manager);
+
+        $this->access_control_verifier = new AccessControlVerifier(
+            new FineGrainedRetriever(new FineGrainedDao()),
+            new \System_Command()
+        );
     }
 
     /**
@@ -190,7 +203,7 @@ class PullRequestsResource extends AuthenticatedResource
         $this->checkUserCanReadRepository($user, $repository_src);
 
         $executor                  = $this->getExecutor($repository_src);
-        $pr_representation_factory = new PullRequestRepresentationFactory($executor);
+        $pr_representation_factory = new PullRequestRepresentationFactory($executor, $this->access_control_verifier);
 
         return $pr_representation_factory->getPullRequestRepresentation($pull_request, $repository_src, $repository_dest, $user);
     }
@@ -526,7 +539,7 @@ class PullRequestsResource extends AuthenticatedResource
         $repository_src  = $this->getRepository($pull_request->getRepositoryId());
         $repository_dest = $this->getRepository($pull_request->getRepoDestId());
 
-        $this->checkUserCanWriteRepository($user, $repository_src);
+        $this->checkUserCanWrite($user, $repository_dest, $pull_request->getBranchDest());
         $this->checkUserCanReadRepository($user, $repository_src);
 
         $status = $body->status;
@@ -543,7 +556,7 @@ class PullRequestsResource extends AuthenticatedResource
         $updated_pull_request        = $this->getPullRequest($id);
 
         $executor                  = $this->getExecutor($repository_src);
-        $pr_representation_factory = new PullRequestRepresentationFactory($executor);
+        $pr_representation_factory = new PullRequestRepresentationFactory($executor, $this->access_control_verifier);
 
         return $pr_representation_factory->getPullRequestRepresentation($updated_pull_request, $repository_src, $repository_dest, $user);
     }
@@ -790,11 +803,11 @@ class PullRequestsResource extends AuthenticatedResource
         }
     }
 
-    private function checkUserCanWriteRepository(PFUser $user, GitRepository $repository)
+    private function checkUserCanWrite(PFUser $user, GitRepository $repository, $reference)
     {
         ProjectAuthorization::userCanAccessProject($user, $repository->getProject(), new URLVerification());
 
-        if (! $repository->userCanWrite($user)) {
+        if (! $this->access_control_verifier->canWrite($user, $repository, $reference)) {
             throw new RestException(403, 'User is not able to WRITE the git repository');
         }
     }

include/PullRequest/REST/v1/RepositoryResource.php
@@ -20,6 +20,9 @@
 
 namespace Tuleap\PullRequest\REST\v1;
 
+use Tuleap\Git\Permissions\FineGrainedDao;
+use Tuleap\Git\Permissions\FineGrainedRetriever;
+use Tuleap\PullRequest\Authorization\AccessControlVerifier;
 use Tuleap\PullRequest\Dao as PullRequestDao;
 use Tuleap\PullRequest\Factory as PullRequestFactory;
 use Tuleap\PullRequest\GitExec;
@@ -68,6 +71,11 @@ class RepositoryResource
         $this->user_manager                 = UserManager::instance();
         $this->query_to_criterion_converter = new QueryToCriterionConverter();
 
+        $this->access_control_verifier = new AccessControlVerifier(
+            new FineGrainedRetriever(new FineGrainedDao()),
+            new \System_Command()
+        );
+
         $this->logger = new Logger();
     }
 
@@ -91,7 +99,7 @@ class RepositoryResource
             $repository_dest = $this->git_repository_factory->getRepositoryById($pull_request->getRepoDestId());
 
             $executor                  = new GitExec($repository_src->getFullPath(), $repository_src->getFullPath());
-            $pr_representation_factory = new PullRequestRepresentationFactory($executor);
+            $pr_representation_factory = new PullRequestRepresentationFactory($executor, $this->access_control_verifier);
 
             try {
                 $pull_request_representation = $pr_representation_factory->getPullRequestRepresentation(

include/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoload8e232909a8546f9d403ae3b2b2354472($class) {
+function autoload14f2ecc85616216eb1a386a492fa9a1d($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -10,6 +10,7 @@ function autoload8e232909a8546f9d403ae3b2b2354472($class) {
             'tuleap\\pullrequest\\additionalactionspresenter' => '/PullRequest/AdditionalActionsPresenter.php',
             'tuleap\\pullrequest\\additionalhelptextpresenter' => '/PullRequest/AdditionalHelpTextPresenter.php',
             'tuleap\\pullrequest\\additionalinfopresenter' => '/PullRequest/AdditionalInfoPresenter.php',
+            'tuleap\\pullrequest\\authorization\\accesscontrolverifier' => '/PullRequest/Authorization/AccessControlVerifier.php',
             'tuleap\\pullrequest\\comment\\comment' => '/PullRequest/Comment/Comment.php',
             'tuleap\\pullrequest\\comment\\dao' => '/PullRequest/Comment/Dao.php',
             'tuleap\\pullrequest\\comment\\factory' => '/PullRequest/Comment/Factory.php',
@@ -97,5 +98,5 @@ function autoload8e232909a8546f9d403ae3b2b2354472($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoload8e232909a8546f9d403ae3b2b2354472');
+spl_autoload_register('autoload14f2ecc85616216eb1a386a492fa9a1d');
 // @codeCoverageIgnoreEnd

tests/Authorization/AccessControlVerifierTest.php
@@ -1,0 +1,95 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\PullRequest\Authorization;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+class AccessControlVerifierTest extends \TuleapTestCase
+{
+    /**
+     * @var \Tuleap\Git\Permissions\FineGrainedRetriever
+     */
+    private $fine_grained_permissions;
+    /**
+     * @var \System_Command
+     */
+    private $system_command;
+    /**
+     * @var \PFUser
+     */
+    private $user;
+    /**
+     * @var \GitRepository
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->fine_grained_permissions = mock('Tuleap\\Git\\Permissions\\FineGrainedRetriever');
+        $this->system_command           = mock('System_Command');
+        $this->user                     = mock('PFUser');
+        $this->repository               = mock('GitRepository');
+    }
+
+    public function itVerifiesThatAUserCanWriteWhenFineGrainedPermissionsAreNotEnabled()
+    {
+        stub($this->fine_grained_permissions)->doesRepositoryUseFineGrainedPermissions()->returns(false);
+
+        $access_control_verifier  = new AccessControlVerifier($this->fine_grained_permissions, $this->system_command);
+
+        stub($this->user)->hasPermission()->returns(true);
+
+        $this->system_command->expectNever('exec');
+
+        $can_write = $access_control_verifier->canWrite($this->user, $this->repository, 'master');
+        $this->assertTrue($can_write);
+    }
+
+    public function itVerifiesThatAUserCanWriteWhenFineGrainedPermissionsAreEnabled()
+    {
+        stub($this->fine_grained_permissions)->doesRepositoryUseFineGrainedPermissions()->returns(true);
+
+        $access_control_verifier  = new AccessControlVerifier($this->fine_grained_permissions, $this->system_command);
+
+        stub($this->user)->expectNever('hasPermission');
+        $project = mock('Project');
+        stub($this->repository)->getProject()->returns($project);
+
+        $can_write = $access_control_verifier->canWrite($this->user, $this->repository, 'master');
+        $this->assertTrue($can_write);
+    }
+
+    public function itVerifiesThatAUserCanNotWriteWhenFineGrainedPermissionsAreEnabledAndHeDoesNotHaveAccess()
+    {
+        stub($this->fine_grained_permissions)->doesRepositoryUseFineGrainedPermissions()->returns(true);
+        stub($this->system_command)->exec()->throws(new \System_Command_CommandException('', array(), 1));
+
+        $access_control_verifier  = new AccessControlVerifier($this->fine_grained_permissions, $this->system_command);
+
+        stub($this->user)->expectNever('hasPermission');
+        $project = mock('Project');
+        stub($this->repository)->getProject()->returns($project);
+
+        $can_write = $access_control_verifier->canWrite($this->user, $this->repository, 'master');
+        $this->assertFalse($can_write);
+    }
+}

commit 0cdaf5a8609bdf06625b8173fb42bdc26fe99aea

Fix the packaging issue due to the addition of a new sudoers file
Issue has been introduced by the commit f944d133718f297ece2b52c13745272fad9896d0.

This is a part of request #10148: Fine grained permissions are not taken into
account when merging a pull request from the web UI

Change-Id: I90dbc90f423b0e58aed6c38a41277aec1f9f5f0b

tuleap-plugin-pullrequest.spec
@@ -47,7 +47,7 @@ npm run build
 %{__install} -m 755 -d $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/pullrequest
 %{__cp} -ar include db templates README.mkd VERSION ChangeLog site-content $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/pullrequest
 
-%{__install} etc/sudoers.d/gitolite-access-command $RPM_BUILD_ROOT/etc/sudoers.d/gitolite-access-command
+%{__install} -D etc/sudoers.d/gitolite-access-command $RPM_BUILD_ROOT/etc/sudoers.d/gitolite-access-command
 
 # logrotate
 %{__mkdir} -p $RPM_BUILD_ROOT/etc/logrotate.d
@@ -79,6 +79,7 @@ fi
 %defattr(-,root,root,-)
 %{_datadir}/tuleap/plugins/pullrequest
 %config(noreplace) /etc/logrotate.d/tuleap_pullrequest
+%attr(00600,root,root) /etc/sudoers.d/gitolite-access-command
 
 %changelog
 * Tue Feb 9 2016 Manuel VACELET <manuel.vacelet@enalean.com> -