e0fb3e3fc4863079634be49a14f808988c8d6479
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-04-19 17:14:26 +0200

    request #10163: Packaging exit with error
    Change-Id: Icec0d969d1c2682f5efb7d8f9adabfba096be8b8

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,15 @@ module.exports = function (grunt) {
                 options: {
                     sourcemap: 'none',
                     style: 'compressed',
-                    loadPath: ['/tuleap/plugins/botmattermost/www/themes/FlamingParrot/css']
+                    loadPath: ['/tuleap/plugins/botmattermost/www/themes/BurningParrot/css']
                 },
                 files: {
-                    "www/themes/default/css/style.css": "www/themes/default/css/style.scss",
-                    "www/themes/FlamingParrot/css/style.css": "www/themes/FlamingParrot/css/style.scss"
+                    "www/themes/BurningParrot/css/style-blue.css": "www/themes/BurningParrot/css/style-blue.scss",
+                    "www/themes/BurningParrot/css/style-green.css": "www/themes/BurningParrot/css/style-green.scss",
+                    "www/themes/BurningParrot/css/style-grey.css": "www/themes/BurningParrot/css/style-grey.scss",
+                    "www/themes/BurningParrot/css/style-orange.css": "www/themes/BurningParrot/css/style-orange.scss",
+                    "www/themes/BurningParrot/css/style-purple.css": "www/themes/BurningParrot/css/style-purple.scss",
+                    "www/themes/BurningParrot/css/style-red.css": "www/themes/BurningParrot/css/style-red.scss"
                 }
             }
         }
