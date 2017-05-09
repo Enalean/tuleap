74ea82540b715ed8f82d3e6af059094ee0c931e7
Nicolas Terray <nicolas.terray@enalean.com>
2017-05-09 17:46:25 +0200

    Display empty state when no dashboard on /my/
    Part of story #10173: Have multiple dashboards
    
    When a user hasn't any dashboards, then we should display an empty
    state.
    
    Change-Id: I5b45b377566c484f0fe353698e0b98ece2022a56

--- src/common/Dashboard/User/MyPresenter.php
+++ src/common/Dashboard/User/MyPresenter.php
@@ -1,22 +1,22 @@
 <?php
 /**
-* Copyright (c) Enalean, 2017. All rights reserved
-*
-* This file is a part of Tuleap.
-*
-* Tuleap is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* Tuleap is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Tuleap. If not, see <http://www.gnu.org/licenses/
-*/
+ * Copyright (c) Enalean, 2017. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
 
 namespace Tuleap\Dashboard\User;
 
@@ -32,9 +32,11 @@ class MyPresenter
      * @var DashboardPresenter[]
      */
     public $user_dashboards;
+    public $has_dashboard;
 
     public $add_dashboard_label;
     public $dashboard_name_label;
+    public $no_dashboard_label;
 
     public $cancel;
     public $close;
@@ -52,42 +54,20 @@ class MyPresenter
         UserPresenter $user_presenter,
         array $user_dashboards
     ) {
-        $this->csrf_token       = $csrf;
-        $this->user_presenter   = $user_presenter;
-        $this->user_dashboards  = $user_dashboards;
+        $this->csrf_token      = $csrf;
+        $this->user_presenter  = $user_presenter;
+        $this->user_dashboards = $user_dashboards;
+        $this->has_dashboard   = count($user_dashboards) > 0;
 
-        $this->add_dashboard_label  = dgettext(
-            'tuleap-core',
-            'Add dashboard'
-        );
-        $this->delete_dashboard_title  = dgettext(
-            'tuleap-core',
-            'Delete dashboard'
-        );
-        $this->delete_dashboard_label  = dgettext(
-            'tuleap-core',
-            'Delete dashboard'
-        );
-        $this->edit_dashboard_title  = dgettext(
-            'tuleap-core',
-            'Edit dashboard'
-        );
-        $this->edit_dashboard_label = dgettext(
-            'tuleap-core',
-            'Edit dashboard'
-        );
-        $this->dashboard_name_label = dgettext(
-            'tuleap-core',
-            'Dashboard name'
-        );
+        $this->add_dashboard_label    = _('Add dashboard');
+        $this->delete_dashboard_title = _('Delete dashboard');
+        $this->delete_dashboard_label = _('Delete dashboard');
+        $this->edit_dashboard_title   = _('Edit dashboard');
+        $this->edit_dashboard_label   = _('Edit dashboard');
+        $this->dashboard_name_label   = _('Dashboard name');
+        $this->no_dashboard_label     = _("You don't have any dashboards.");
 
-        $this->cancel = dgettext(
-            'tuleap-core',
-            'Cancel'
-        );
-        $this->close = dgettext(
-            'tuleap-core',
-            'Close'
-        );
+        $this->cancel = _('Cancel');
+        $this->close  = _('Close');
     }
 }
