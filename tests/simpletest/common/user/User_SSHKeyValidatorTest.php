Enalean/tuleap

commit 9f43853b3cf26c991c301f6fa60cec66f15d0dcb

Harden the generation of the Gitolite authorized_keys file
It should not be possible to get an authorized_keys file with invalid entries

This is part of request #9970: Directly manage SSH keys instead of delegating it to gitolite

Change-Id: I59a7f0e24911e9338aaa5a6fc45562fcb81f5f2f

plugins/git/include/Git/Gitolite/SSHKey/AuthorizedKeysFileCreator.php
@@ -20,6 +20,9 @@
 
 namespace Tuleap\Git\Gitolite\SSHKey;
 
+use Logger;
+use System_Command;
+use System_Command_CommandException;
 use Tuleap\Git\Gitolite\SSHKey\Provider\IProvideKey;
 
 class AuthorizedKeysFileCreator
@@ -28,10 +31,20 @@ class AuthorizedKeysFileCreator
      * @var IProvideKey
      */
     private $keys;
+    /**
+     * @var System_Command
+     */
+    private $system_command;
+    /**
+     * @var Logger
+     */
+    private $logger;
 
-    public function __construct(IProvideKey $keys)
+    public function __construct(IProvideKey $keys, System_Command $system_command, Logger $logger)
     {
-        $this->keys = $keys;
+        $this->keys           = $keys;
+        $this->system_command = $system_command;
+        $this->logger         = $logger;
     }
 
     /**
@@ -39,15 +52,71 @@ class AuthorizedKeysFileCreator
      */
     public function dump($file_path, $shell, $authentication_options)
     {
-        $file = @fopen($file_path, 'wb');
-        if ($file === false) {
-            throw new DumpKeyException('Could not open the authorized keys file');
+        try {
+            $this->dumpAllKeysAtOnce($file_path, $shell, $authentication_options);
+        } catch (MalformedAuthorizedKeysFileException $ex) {
+            $this->dumpKeysFilteringInvalidOnes($file_path, $shell, $authentication_options);
         }
+    }
+
+    /**
+     * @throws MalformedAuthorizedKeysFileException
+     * @throws DumpKeyException
+     */
+    private function dumpAllKeysAtOnce($file_path, $shell, $authentication_options)
+    {
+        $file = $this->openAuthorizedKeysFile($file_path);
 
         foreach ($this->keys as $key) {
             $this->dumpKey($file, $key, $shell, $authentication_options);
         }
 
+        $this->closeAuthorizedKeysFile($file);
+
+        if (! $this->isAuthorizedKeysValid($file_path)) {
+            throw new MalformedAuthorizedKeysFileException('The generated authorized keys file contains invalid entries');
+        }
+    }
+
+    /**
+     * @throws DumpKeyException
+     */
+    private function dumpKeysFilteringInvalidOnes($file_path, $shell, $authentication_options)
+    {
+        $file = $this->openAuthorizedKeysFile($file_path);
+
+        $temporary_authorized_key_line_file = tempnam(\ForgeConfig::get('tmp_dir'), 'gitolite3-authorized-key-line');
+        if ($temporary_authorized_key_line_file === false) {
+            @unlink($temporary_authorized_key_line_file);
+            throw new DumpKeyException('Could not create a temporary file to verify an authorized key line');
+        }
+
+        foreach ($this->keys as $key) {
+            $this->dumpKeyIfValid($file, $temporary_authorized_key_line_file, $key, $shell, $authentication_options);
+        }
+
+        @unlink($temporary_authorized_key_line_file);
+        $this->closeAuthorizedKeysFile($file);
+    }
+
+    /**
+     * @return resource
+     * @throws DumpKeyException
+     */
+    private function openAuthorizedKeysFile($file_path)
+    {
+        $file = @fopen($file_path, 'wb');
+        if ($file === false) {
+            throw new DumpKeyException('Could not open the authorized keys file');
+        }
+        return $file;
+    }
+
+    /**
+     * @throws DumpKeyException
+     */
+    private function closeAuthorizedKeysFile($file)
+    {
         if (fflush($file) === false) {
             throw new DumpKeyException('Could not flush content to the authorized keys file');
         }
@@ -61,13 +130,62 @@ class AuthorizedKeysFileCreator
      */
     private function dumpKey($file, Key $key, $shell, $authentication_options)
     {
-        $result = fwrite(
-            $file,
-            'command="' . $shell . ' ' . $key->getUsername() . '",' . $authentication_options . ' ' .
-            $key->getKey() . PHP_EOL
-        );
+        $result = fwrite($file, $this->getAuthorizedKeyLine($key, $shell, $authentication_options));
         if ($result === null) {
             throw new DumpKeyException('Could not write to the authorized keys file');
         }
+    }
+
+    /**
+     * @throws DumpKeyException
+     */
+    private function dumpKeyIfValid($file, $temporary_authorized_key_line_file, Key $key, $shell, $authentication_options)
+    {
+        $authorized_key_line = $this->getAuthorizedKeyLine($key, $shell, $authentication_options);
+
+        $is_temporary_authorized_key_line_file_written = file_put_contents(
+            $temporary_authorized_key_line_file,
+            $authorized_key_line
+        );
+
+        if ($is_temporary_authorized_key_line_file_written === false) {
+            @unlink($temporary_authorized_key_line_file);
+            throw new DumpKeyException('Could not write to the temporary file to verify an authorized key line');
+        }
+
+        if (! $this->isAuthorizedKeysValid($temporary_authorized_key_line_file)) {
+            $this->logger->warn('The key ' . $key->getKey() . ' of the user ' . $key->getUsername() . ' is malformed');
+            return;
+        }
+
+        $result = fwrite($file, $this->getAuthorizedKeyLine($key, $shell, $authentication_options));
+        if ($result === null) {
+            throw new DumpKeyException('Could not write to the authorized keys file');
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getAuthorizedKeyLine(Key $key, $shell, $authentication_options)
+    {
+        return 'command="' . $shell . ' ' . $key->getUsername() . '",' . $authentication_options . ' ' .
+            $key->getKey() . PHP_EOL;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAuthorizedKeysValid($file_path)
+    {
+        try {
+            $this->system_command->exec(
+                '/usr/share/tuleap/src/utils/ssh-keys-validity-checker.sh ' . escapeshellarg($file_path)
+            );
+        } catch (System_Command_CommandException $ex) {
+            return false;
+        }
+
+        return true;
     }
 }

src/common/user/User_SSHKeyValidator.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012 - 2014. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with Tuleap; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-require_once 'UserManager.class.php';
 
 /**
  * Ensure SSH key is valid
@@ -54,11 +53,14 @@ class User_SSHKeyValidator {
     }
 
     private function isValid($key_file, $key) {
+        if ($key === '') {
+            return false;
+        }
         $written = file_put_contents($key_file, $key);
         if ($written === strlen($key)) {
             $return = 1;
             $output = array();
-            exec("ssh-keygen -l -f $key_file 2>&1", $output, $return);
+            exec('/usr/share/tuleap/src/utils/ssh-keys-validity-checker.sh ' . escapeshellarg($key_file), $output, $return);
             if ($return === 0) {
                 return true;
             }
@@ -66,5 +68,3 @@ class User_SSHKeyValidator {
         return false;
     }
 }
-
-?>

plugins/git/include/Git/Gitolite/SSHKey/Key.php
@@ -23,6 +23,10 @@ namespace Tuleap\Git\Gitolite\SSHKey;
 class Key
 {
     /**
+     * @var \Rule_UserName
+     */
+    private static $username_rule;
+    /**
      * @var string
      */
     private $username;
@@ -31,10 +35,19 @@ class Key
      */
     private $key;
 
+    /**
+     * @throws \InvalidArgumentException
+     */
     public function __construct($username, $key)
     {
+        if (! $this->isUsernameValid($username)) {
+            throw new \InvalidArgumentException('The username must be UNIX valid');
+        }
         $this->username = $username;
-        $this->key      = $key;
+        $this->key      = trim($key);
+        if (mb_strpos($this->key, "\n") !== false) {
+            throw new \InvalidArgumentException('Only one key is expected');
+        }
     }
 
     /**
@@ -51,5 +64,16 @@ class Key
     public function getKey()
     {
         return $this->key;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isUsernameValid($username)
+    {
+        if (self::$username_rule === null) {
+            self::$username_rule = new \Rule_UserName();
+        }
+        return self::$username_rule->isUnixValid($username);
     }
 }

plugins/git/include/Git/Gitolite/SSHKey/MalformedAuthorizedKeysFileException.php
@@ -1,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\Gitolite\SSHKey;
+
+class MalformedAuthorizedKeysFileException extends DumpKeyException
+{
+}

plugins/git/include/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoloadbe6cd522b19debf93f4b9f2be59a87c4($class) {
+function autoload102149debee6f09050d5afd05f57c473($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -249,6 +249,7 @@ function autoloadbe6cd522b19debf93f4b9f2be59a87c4($class) {
             'tuleap\\git\\gitolite\\sshkey\\gitolite3dumper' => '/Git/Gitolite/SSHKey/Gitolite3Dumper.php',
             'tuleap\\git\\gitolite\\sshkey\\gitolite3massdumper' => '/Git/Gitolite/SSHKey/Gitolite3MassDumper.php',
             'tuleap\\git\\gitolite\\sshkey\\key' => '/Git/Gitolite/SSHKey/Key.php',
+            'tuleap\\git\\gitolite\\sshkey\\malformedauthorizedkeysfileexception' => '/Git/Gitolite/SSHKey/MalformedAuthorizedKeysFileException.php',
             'tuleap\\git\\gitolite\\sshkey\\managementdetector' => '/Git/Gitolite/SSHKey/ManagementDetector.php',
             'tuleap\\git\\gitolite\\sshkey\\massdumper' => '/Git/Gitolite/SSHKey/MassDumper.php',
             'tuleap\\git\\gitolite\\sshkey\\provider\\accessexception' => '/Git/Gitolite/SSHKey/Provider/AccessException.php',
@@ -348,5 +349,5 @@ function autoloadbe6cd522b19debf93f4b9f2be59a87c4($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoloadbe6cd522b19debf93f4b9f2be59a87c4');
+spl_autoload_register('autoload102149debee6f09050d5afd05f57c473');
 // @codeCoverageIgnoreEnd

plugins/git/include/gitPlugin.class.php
@@ -1930,10 +1930,12 @@ class GitPlugin extends Plugin {
         $gitolite_admin_path = $this->getGitoliteAdminPath();
         $git_exec            = new Git_Exec($gitolite_admin_path);
 
+        $system_command = new System_Command();
+
         return new DumperFactory(
             $management_detector,
-            new AuthorizedKeysFileCreator($whole_instance_keys),
-            new System_Command(),
+            new AuthorizedKeysFileCreator($whole_instance_keys, $system_command, $this->getLogger()),
+            $system_command,
             $git_exec,
             $gitolite_admin_path,
             $user_manager

plugins/git/tests/Git/Gitolite/SSHKey/AuthorizedKeysFileCreatorTest.php
@@ -36,16 +36,48 @@ class AuthorizedKeysFileCreatorTest extends TuleapTestCase
         stub($keys)->valid()->returnsAt(0, true);
         stub($keys)->current()->returnsAt(1, $key2);
         stub($keys)->valid()->returnsAt(1, true);
+        $system_command = mock('System_Command');
+        $logger         = mock('Logger');
+        stub($logger)->expectNever('warn');
 
         $temporary_file = tempnam(sys_get_temp_dir(), 'AuthorizedKeysFileCreatorUnitTests');
 
-        $authorized_keys_file_creator = new AuthorizedKeysFileCreator($keys);
+        $authorized_keys_file_creator = new AuthorizedKeysFileCreator($keys, $system_command, $logger);
         $authorized_keys_file_creator->dump($temporary_file, '/bin/false', 'some-options');
         $generated_authorized_keys = file_get_contents($temporary_file);
         @unlink($temporary_file);
 
         $expected_authorized_keys = 'command="/bin/false user1",some-options ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDgTGQXojsjAemABiCqPS9k7h5VLigeNhfJFc1Xx3DRZ0B1+eCAI7IT65VzYEHlkW8pTK9IZO6yFLM5aYiLF5GD1VoDxP7zuslCU5gTIl1eWJzMQY/5mc4IP+8dk+p4CoTlXwU5xnZatUWwiF8PnaM2evga4sAwLHBZ8QqiNIaHEQ==
 command="/bin/false user2",some-options ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDF8IKZurK86EWGW2Q8jYkiXmkWZfEQ2SJlYnIylWMey0tRB5pr9G9oKbKt25RHigfeFJXgKIvPhAku5R08ejfoAG+/V3H8cXqf0zk0VxuIuTZk7OJ+8ll0i8x52Daepr102i7agnNk2c7CQ9Tz2+sXgYrMVPK4QroEOXY1rFCbHQ== user2@example.com
+';
+
+        $this->assertEqual($expected_authorized_keys, $generated_authorized_keys);
+    }
+
+    public function itRejectsInvalidGeneratedKeysFile()
+    {
+        $key1 = new Key('invalid_user', 'ssh-rsa not_valid');
+        $key2 = new Key('valid_user', 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDF8IKZurK86EWGW2Q8jYkiXmkWZfEQ2SJlYnIylWMey0tRB5pr9G9oKbKt25RHigfeFJXgKIvPhAku5R08ejfoAG+/V3H8cXqf0zk0VxuIuTZk7OJ+8ll0i8x52Daepr102i7agnNk2c7CQ9Tz2+sXgYrMVPK4QroEOXY1rFCbHQ== user2@example.com');
+
+        $keys = mock('Tuleap\Git\Gitolite\SSHKey\Provider\IProvideKey');
+        stub($keys)->current()->returnsAt(0, $key1);
+        stub($keys)->valid()->returnsAt(0, true);
+        stub($keys)->current()->returnsAt(1, $key2);
+        stub($keys)->valid()->returnsAt(1, true);
+        $system_command = mock('System_Command');
+        stub($system_command)->throwAt(0, 'exec', new \System_Command_CommandException('', array(), 1));
+        stub($system_command)->throwAt(1, 'exec', new \System_Command_CommandException('', array(), 1));
+        $logger = mock('Logger');
+        stub($logger)->expectOnce('warn');
+        $temporary_file = tempnam(sys_get_temp_dir(), 'AuthorizedKeysFileCreatorUnitTests');
+
+        $authorized_keys_file_creator = new AuthorizedKeysFileCreator($keys, $system_command, $logger);
+        $authorized_keys_file_creator->dump($temporary_file, '/bin/false', 'some-options');
+        $generated_authorized_keys = file_get_contents($temporary_file);
+        @unlink($temporary_file);
+
+        $expected_authorized_keys = 'command="/bin/false invalid_user",some-options ssh-rsa not_valid
+command="/bin/false valid_user",some-options ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDF8IKZurK86EWGW2Q8jYkiXmkWZfEQ2SJlYnIylWMey0tRB5pr9G9oKbKt25RHigfeFJXgKIvPhAku5R08ejfoAG+/V3H8cXqf0zk0VxuIuTZk7OJ+8ll0i8x52Daepr102i7agnNk2c7CQ9Tz2+sXgYrMVPK4QroEOXY1rFCbHQ== user2@example.com
 ';
 
         $this->assertEqual($expected_authorized_keys, $generated_authorized_keys);

plugins/git/tests/Git/Gitolite/SSHKey/KeyTest.php
@@ -1,0 +1,62 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\Gitolite\SSHKey;
+
+use TuleapTestCase;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+class KeyTest extends TuleapTestCase
+{
+    public function itChecksUsernameValidity()
+    {
+        $this->expectException('\InvalidArgumentException');
+        new Key(
+            'This is not a valid " username',
+            'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDgTGQXojsjAemABiCqPS9k7h5VLigeNhfJFc1Xx3DRZ0B1+eCAI7IT65VzYEHlkW8pTK9IZO6yFLM5aYiLF5GD1VoDxP7zuslCU5gTIl1eWJzMQY/5mc4IP+8dk+p4CoTlXwU5xnZatUWwiF8PnaM2evga4sAwLHBZ8QqiNIaHEQ=='
+        );
+    }
+
+    public function itCleansKey()
+    {
+        $key = new Key(
+            'user',
+            '
+            ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDgTGQXojsjAemABiCqPS9k7h5VLigeNhfJFc1Xx3DRZ0B1+eCAI7IT65VzYEHlkW8pTK9IZO6yFLM5aYiLF5GD1VoDxP7zuslCU5gTIl1eWJzMQY/5mc4IP+8dk+p4CoTlXwU5xnZatUWwiF8PnaM2evga4sAwLHBZ8QqiNIaHEQ==
+            '
+        );
+
+        $this->assertEqual(
+            $key->getKey(),
+            'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDgTGQXojsjAemABiCqPS9k7h5VLigeNhfJFc1Xx3DRZ0B1+eCAI7IT65VzYEHlkW8pTK9IZO6yFLM5aYiLF5GD1VoDxP7zuslCU5gTIl1eWJzMQY/5mc4IP+8dk+p4CoTlXwU5xnZatUWwiF8PnaM2evga4sAwLHBZ8QqiNIaHEQ=='
+        );
+    }
+
+    public function itAcceptsOnlyOneKey()
+    {
+        $this->expectException('\InvalidArgumentException');
+        new Key(
+            'user',
+            'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDgTGQXojsjAemABiCqPS9k7h5VLigeNhfJFc1Xx3DRZ0B1+eCAI7IT65VzYEHlkW8pTK9IZO6yFLM5aYiLF5GD1VoDxP7zuslCU5gTIl1eWJzMQY/5mc4IP+8dk+p4CoTlXwU5xnZatUWwiF8PnaM2evga4sAwLHBZ8QqiNIaHEQ==
+            ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDF8IKZurK86EWGW2Q8jYkiXmkWZfEQ2SJlYnIylWMey0tRB5pr9G9oKbKt25RHigfeFJXgKIvPhAku5R08ejfoAG+/V3H8cXqf0zk0VxuIuTZk7OJ+8ll0i8x52Daepr102i7agnNk2c7CQ9Tz2+sXgYrMVPK4QroEOXY1rFCbHQ=='
+        );
+    }
+}

commit 185f4a142e1064a1de97126aa7ff9f9bbf64d782

request #10242: SSH keys validation accepts keys that are not valid
ssh-keygen has an undocummented feature (at least in the version used in CentOS6):
when it encounters a newline in a public key file it will extract the fingerprint
like it was an authorized_keys file. This allow users to upload public key with extra
parameters that have nothing to do in a public key file.

This issue has been introduced by commit 9f43853b3cf26c991c301f6fa60cec66f15d0dcb.

Change-Id: I87aa56e70dafc41303aaba3c27b79b8e69e17b79

plugins/git/include/Git/Gitolite/SSHKey/AuthorizedKeysFileCreator.php
@@ -191,7 +191,7 @@ class AuthorizedKeysFileCreator
     {
         try {
             $this->system_command->exec(
-                '/usr/share/tuleap/src/utils/ssh-keys-validity-checker.sh ' . escapeshellarg($file_path)
+                '/usr/share/tuleap/src/utils/authorized-keys-validity-checker.sh ' . escapeshellarg($file_path)
             );
         } catch (System_Command_CommandException $ex) {
             return false;

src/common/user/User_SSHKeyValidator.class.php
@@ -60,7 +60,7 @@ class User_SSHKeyValidator {
         if ($written === strlen($key)) {
             $return = 1;
             $output = array();
-            exec('/usr/share/tuleap/src/utils/ssh-keys-validity-checker.sh ' . escapeshellarg($key_file), $output, $return);
+            exec('ssh-keygen -l -f ' . escapeshellarg($key_file) . ' > /dev/null 2>&1', $output, $return);
             if ($return === 0) {
                 return true;
             }

src/utils/authorized-keys-validity-checker.sh
@@ -21,7 +21,7 @@
 set -e
 
 if [ -z "$1" ]; then
-    (>&2 echo "Usage: $0 path_to_existing_ssh_keys_file")
+    (>&2 echo "Usage: $0 path_to_existing_authorized_keys_file")
     exit 1
 fi
 

tests/simpletest/common/user/User_SSHKeyValidatorTest.php
@@ -65,6 +65,17 @@ class User_SSHKeyValidator_KeyValidationTest extends User_SSHKeyValidatorTest {
         $this->assertCount($this->validator->validateAllKeys($keys), 0);
     }
 
+    public function itRaisesAWarningWhenTheKeyIsNotValidOutsideAnAuthorizedKeysFile()
+    {
+        $keys = array(
+            'tuleap.example.com,192.0.2.1 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNpemQvp5G/ldgg5diu/OZdNVV3mHqsHmTBcJKiFnfwxNxzZDdTb7hXQKEd6akU6qbmlGPr8AYMBEfII/C47o/B93y2trghS1dVYKyEq7Md/uZx+NFnGysNiMeWr1jPWHWEiNfKgbZPW6OMY200fNGXROmxvp4BQLID7bPLXVLctvCRO4uD2KlK66uWaql7QuGWxzY2C09d15Q/84oVwcIVook/luP1ieHg6syS9FutO+j0//Hfg2Cze/JrrxIZT2XUUAVeyM9uSwW2bBprmDI8rq3UXUotcJws9Pc4PgK7U5P4w1qBQFRonJSjYbK2+1EXLPvV5S60E2mwu6Ta513'
+        );
+
+        stub($GLOBALS['Response'])->addFeedback('warning', '*')->once();
+
+        $this->assertCount($this->validator->validateAllKeys($keys), 0);
+    }
+
     public function itRaisesAWarningWhenTheKeyIsInvalidAmongValidKeys() {
         $keys = array(
             $this->key1,