Enalean/tuleap

commit 7f4acef33f2a8c078d6f13830cc1216b9cac1e90

request #10219: XSS through the Transclude plugin of PHPWiki
Change-Id: I693dab7ad2c371c60d9710d9524d4906ae6f6002

src/common/wiki/phpwiki/lib/stdlib.php
@@ -342,7 +342,8 @@ function PossiblyGlueIconToText($proto_or_url, $text) {
  * @return boolean True if same, false else.
  */
 function IsSafeURL($url) {
-    return !preg_match('/([<>"])|(%3C)|(%3E)|(%22)/', $url);
+    $valid_uri = new Valid_LocalURI();
+    return !preg_match('/([<>"])|(%3C)|(%3E)|(%22)/', $url) && $valid_uri->validate($url);
 }
 
 /**

src/common/wiki/phpwiki/lib/plugin/Transclude.php
@@ -85,8 +85,11 @@ extends WikiPlugin
             return $this->error(_("Bad url in src: remove all of <, >, \""));
         }
 
+        $uri_sanitizer = new \Tuleap\Sanitizer\URISanitizer(new Valid_LocalURI());
+        $sanitized_src = $uri_sanitizer->sanitizeForHTMLAttribute($src);
+
         $params = array('title' => _("Transcluded page"),
-                        'src' => $src,
+                        'src' => $sanitized_src,
                         'width' => "100%",
                         'height' => $height,
                         'marginwidth' => 0,
@@ -94,7 +97,7 @@ extends WikiPlugin
                         'class' => 'transclude',
                         "onload" => "adjust_iframe_height(this);");
 
-        $noframe_msg[] = fmt("See: %s", HTML::a(array('href' => $src), $src));
+        $noframe_msg[] = fmt("See: %s", HTML::a(array('href' => $sanitized_src), $src));
 
         $noframe_msg = HTML::div(array('class' => 'transclusion'),
                                  HTML::p(array(), $noframe_msg));
@@ -106,7 +109,7 @@ extends WikiPlugin
         */
 
         return HTML(HTML::p(array('class' => 'transclusion-title'),
-                            fmt("Transcluded from %s", LinkURL($src))),
+                            fmt("Transcluded from %s", LinkURL($sanitized_src))),
                     $this->_js(), $iframe);
     }
 

commit 976cedfd7a70ea9d2144008493700a1adbbe57c0

request #10286: PHPWiki does not accept FTP URI as links anymore
Issue has been introduced by commit 7f4acef33f2a8c078d6f13830cc1216b9cac1e90.
Allowing mailto and FTP URIs should be enough for the majority of the use cases.

Change-Id: Ifddaa2281510c48357c77d6b973f933617571721

src/common/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoloade91df67b499edc345530caa40a281768($class) {
+function autoload5c20cbf5522706f990909e95af5d9546($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -1028,12 +1028,14 @@ function autoloade91df67b499edc345530caa40a281768($class) {
             'valid_array' => '/valid/ValidFactory.class.php',
             'valid_email' => '/valid/ValidFactory.class.php',
             'valid_file' => '/valid/ValidFactory.class.php',
+            'valid_ftpuri' => '/valid/ValidFactory.class.php',
             'valid_genericusernamesuffix' => '/valid/ValidFactory.class.php',
             'valid_groupid' => '/valid/ValidFactory.class.php',
             'valid_httpsuri' => '/valid/ValidFactory.class.php',
             'valid_httpuri' => '/valid/ValidFactory.class.php',
             'valid_int' => '/valid/ValidFactory.class.php',
             'valid_localuri' => '/valid/ValidFactory.class.php',
+            'valid_mailtouri' => '/valid/ValidFactory.class.php',
             'valid_numeric' => '/valid/ValidFactory.class.php',
             'valid_pv' => '/valid/ValidFactory.class.php',
             'valid_realnameformat' => '/valid/ValidFactory.class.php',
@@ -1114,5 +1116,5 @@ function autoloade91df67b499edc345530caa40a281768($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoloade91df67b499edc345530caa40a281768');
+spl_autoload_register('autoload5c20cbf5522706f990909e95af5d9546');
 // @codeCoverageIgnoreEnd

src/common/valid/ValidFactory.class.php
@@ -1,23 +1,24 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2007. All Rights Reserved.
  *
  * Originally written by Manuel VACELET, 2007.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -133,6 +134,28 @@ class Valid_LocalURI extends Valid_String {
     public function validate($value)
     {
         $this->addRule(new Rule_Regexp('/^(http:\/\/|https:\/\/|#|\/|\?)/i'));
+        return parent::validate($value);
+    }
+}
+
+/**
+ * Check that value is a possible FTP(S) URI
+ */
+class Valid_FTPURI extends Valid_String {
+    public function validate($value)
+    {
+        $this->addRule(new Rule_Regexp('/^ftps?:\/\/.+/i'));
+        return parent::validate($value);
+    }
+}
+
+/**
+ * Check that value is a possible mail URI
+ */
+class Valid_MailtoURI extends Valid_String {
+    public function validate($value)
+    {
+        $this->addRule(new Rule_Regexp('/^mailto:[^[:space:]].*/i'));
         return parent::validate($value);
     }
 }

src/common/wiki/phpwiki/lib/stdlib.php
@@ -342,8 +342,13 @@ function PossiblyGlueIconToText($proto_or_url, $text) {
  * @return boolean True if same, false else.
  */
 function IsSafeURL($url) {
-    $valid_uri = new Valid_LocalURI();
-    return !preg_match('/([<>"])|(%3C)|(%3E)|(%22)/', $url) && $valid_uri->validate($url);
+    $valid_local_uri  = new Valid_LocalURI();
+    $valid_ftp_uri    = new Valid_FTPURI();
+    $valid_mailto_uri = new Valid_MailtoURI();
+
+    return !preg_match('/([<>"])|(%3C)|(%3E)|(%22)/', $url) && (
+            $valid_local_uri->validate($url) || $valid_ftp_uri->validate($url) || $valid_mailto_uri->validate($url)
+        );
 }
 
 /**

tests/simpletest/common/valid/ValidHelpersTest.php
@@ -1,23 +1,24 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2007. All Rights Reserved.
  *
  * Originally written by Manuel VACELET, 2007.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
+ * along with Tuleap; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
@@ -100,5 +101,27 @@ class ValidHelperTest extends TuleapTestCase {
         $this->assertFalse($validator->validate('gopher://example.com'));
         $this->assertFalse($validator->validate('javascript:alert(1);'));
         $this->assertFalse($validator->validate('Stringhttp://'));
+    }
+
+    public function itValidatesFTPURI()
+    {
+        $validator = new Valid_FTPURI();
+
+        $this->assertTrue($validator->validate('ftp://example.com'));
+        $this->assertTrue($validator->validate('FTP://example.com'));
+        $this->assertTrue($validator->validate('ftps://example.com'));
+        $this->assertTrue($validator->validate('FTPS://example.com'));
+        $this->assertFalse($validator->validate('https://notaftp.example.com/'));
+        $this->assertFalse($validator->validate('ftp://'));
+    }
+
+    public function itValidatesMailtoURI()
+    {
+        $validator = new Valid_MailtoURI();
+
+        $this->assertTrue($validator->validate('mailto:tuleap@example.com'));
+        $this->assertTrue($validator->validate('mailto:tuleap@example.com?subject=Tuleap%20Unit%20Tests'));
+        $this->assertFalse($validator->validate('mailto: tuleap@example.com'));
+        $this->assertFalse($validator->validate('mailto:'));
     }
 }
976cedfd7a70ea9d2144008493700a1adbbe57c0
Thomas Gerbet <thomas.gerbet@enalean.com>
2017-06-05 16:18:13 +0200

    request #10286: PHPWiki does not accept FTP URI as links anymore
    Issue has been introduced by commit 7f4acef33f2a8c078d6f13830cc1216b9cac1e90.
    Allowing mailto and FTP URIs should be enough for the majority of the use cases.
    
    Change-Id: Ifddaa2281510c48357c77d6b973f933617571721

--- src/common/valid/ValidFactory.class.php
+++ src/common/valid/ValidFactory.class.php
@@ -1,23 +1,24 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2007. All Rights Reserved.
  *
  * Originally written by Manuel VACELET, 2007.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -133,6 +134,28 @@ class Valid_LocalURI extends Valid_String {
     public function validate($value)
     {
         $this->addRule(new Rule_Regexp('/^(http:\/\/|https:\/\/|#|\/|\?)/i'));
+        return parent::validate($value);
+    }
+}
+
+/**
+ * Check that value is a possible FTP(S) URI
+ */
+class Valid_FTPURI extends Valid_String {
+    public function validate($value)
+    {
+        $this->addRule(new Rule_Regexp('/^ftps?:\/\/.+/i'));
+        return parent::validate($value);
+    }
+}
+
+/**
+ * Check that value is a possible mail URI
+ */
+class Valid_MailtoURI extends Valid_String {
+    public function validate($value)
+    {
+        $this->addRule(new Rule_Regexp('/^mailto:[^[:space:]].*/i'));
         return parent::validate($value);
     }
 }
