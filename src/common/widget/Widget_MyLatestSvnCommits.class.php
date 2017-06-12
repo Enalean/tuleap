b74841082d5a651abff70d7fb1722aafbf77ced2
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-06-12 15:05:50 +0200

    Correct style for project latest svn et CVS commit
    This is a part of story #10173 Have multiple dashboards
    
    Change-Id: I0ae4df17f420651b21fb34355c3a20906eadbd78

--- src/common/widget/Widget_MyLatestSvnCommits.class.php
+++ src/common/widget/Widget_MyLatestSvnCommits.class.php
@@ -66,20 +66,24 @@ class Widget_MyLatestSvnCommits extends Widget {
         $user        = UserManager::instance()->getCurrentUser();
         $pm          = ProjectManager::instance();
         $project_ids = $user->getProjects();
+
+        $revision_total = 0;
         foreach ($project_ids as $project_id) {
             $project = $pm->getProject($project_id);
             if ($project->usesSVN()) {
+                $html .= '<div class="project-last-commit-project-title">';
                 list($hide_now,$count_diff,$hide_url) = my_hide_url('my_svn_group', $project_id, $request->get('hide_item_id'), count($project_ids), $request->get('hide_my_svn_group'), $request->get('dashboard_id'));
                 $html .= $hide_url;
 
                 $html .= '<strong>' . $project->getPublicName() . '</strong>';
                 if (!$hide_now) {
                     list($latest_revisions, $nb_revisions) = svn_get_revisions($project, 0, $this->_nb_svn_commits, '', $user->getUserName(), '', '', 0, false);
+                    $revision_total += $nb_revisions;
                     if (db_numrows($latest_revisions) > 0) {
                         $i = 0;
                         while ($data = db_fetch_array($latest_revisions)) {
                             $html .= '<div class="'. util_get_alt_row_color($i++) .'" style="border-bottom:1px solid #ddd">';
-                            $html .= '<div style="font-size:0.98em;">';
+                            $html .= '<div style="font-size:0.98em;" class="project-last-commit-text">';
                             $html .= '<a href="'. $this->_getLinkToCommit($project->getGroupId(), $data['revision']) .'">rev #'.$data['revision'].'</a>';
                             $html .= ' '.$GLOBALS['Language']->getText('my_index','my_latest_svn_commit_on').' ';
                             //In the db, svn dates are stored as int whereas cvs dates are stored as timestamp
@@ -100,16 +104,24 @@ class Widget_MyLatestSvnCommits extends Widget {
                             $html .= '</div>';
                             $html .= '</div>';
                         }
-                        $html .= '<div style="text-align:center" class="'. util_get_alt_row_color($i++) .'">';
+                        $html .= '<div style="text-align:center" class="'. util_get_alt_row_color($i++) .' project-last-commit-text-more">';
                         $html .= '<a href="'. $this->_getLinkToMore($project->getGroupId(), $user->getUserName()) .'">[ More ]</a>';
                         $html .= '</div>';
                     } else {
-                        $html .= '<div></div>';
+                        $html .= '<div>' .
+                            $GLOBALS['Language']->getText('my_index', 'my_latest_commit_empty') . '</div>';
                     }
+
                 } else {
                     $html .= '<div></div>';
                 }
+
+                $html .=  '</div>';
             }
+        }
+
+        if ($revision_total === 0) {
+            $html .= $GLOBALS['Language']->getText('my_index', 'my_latest_commit_empty');
         }
         return $html;
     }
--- src/common/widget/Widget_ProjectLatestCommits.class.php
+++ src/common/widget/Widget_ProjectLatestCommits.class.php
@@ -25,7 +25,8 @@ require_once('common/rss/RSS.class.php');
 /**
 * Widget_ProjectLatestCommits
 */
-class Widget_ProjectLatestCommits extends Widget {
+class Widget_ProjectLatestCommits extends Widget
+{
     var $latest_revisions = null;
     var $group_id;
     var $commits_callback;
@@ -38,45 +39,53 @@ class Widget_ProjectLatestCommits extends Widget {
         $this->group_id         = $request->get('group_id');
         $this->commits_callback = $get_commits_callback;
     }
+
     /* protected */ function _getLinkToCommit($data) { }
     /* protected */ function _getLinkToMore() { }
 
-    function getLatestRevisions() {
+    public function getLatestRevisions()
+    {
         if (! $this->latest_revisions) {
-            $pm = ProjectManager::instance();
+            $pm      = ProjectManager::instance();
             $project = $pm->getProject($this->group_id);
             if ($project && $this->canBeUsedByProject($project)) {
                 $get_commits_callback = $this->commits_callback;
                 list($this->latest_revisions,) = $get_commits_callback($project, 0, 5);
             }
         }
+
         return $this->latest_revisions;
     }
 
-    function getContent() {
+    public function getContent()
+    {
         $html = '';
-        $i = 1;
-        $UH = UserHelper::instance();
-        $hp = Codendi_HTMLPurifier::instance();
+        $i    = 1;
+        $UH   = UserHelper::instance();
+        $hp   = Codendi_HTMLPurifier::instance();
 
         $latest_revisions = $this->getLatestRevisions();
         if (! $latest_revisions) {
             return $html;
         }
 
-        while($data = db_fetch_array($latest_revisions)) {
-            $html .= '<div class="'. util_get_alt_row_color($i++) .'" style="border-bottom:1px solid #ddd">';
-            $html .= '<div style="font-size:0.98em;">';
-            $html .= '<a href="'. $this->_getLinkToCommit($data) .'">#'.$data['revision'].'</a>';
+        $number_of_commit = 0;
+        while ($data = db_fetch_array($latest_revisions)) {
+            $html .= '<div class="' . util_get_alt_row_color($i++) . '" style="border-bottom:1px solid #ddd">';
+            $html .= '<div style="font-size:0.98em;" class="project-last-commit-text">';
+            $html .= '<a href="' . $this->_getLinkToCommit($data) . '">#' . $data['revision'] . '</a>';
             $html .= ' by ';
             if (isset($data['whoid'])) {
                 $name = $UH->getDisplayNameFromUserId($data['whoid']);
             } else {
                 $name = $UH->getDisplayNameFromUserName($data['who']);
             }
-            $html .= $hp->purify($name, CODENDI_PURIFIER_CONVERT_HTML).' on ';
+            $html .= $hp->purify($name, CODENDI_PURIFIER_CONVERT_HTML) . ' on ';
             //In the db, svn dates are stored as int whereas cvs dates are stored as timestamp
-            $html .= format_date($GLOBALS['Language']->getText('system', 'datefmt'), (is_numeric($data['date']) ? $data['date'] : strtotime($data['date'])));
+            $html .= format_date(
+                $GLOBALS['Language']->getText('system', 'datefmt'),
+                (is_numeric($data['date']) ? $data['date'] : strtotime($data['date']))
+            );
             $html .= '</div>';
             $html .= '<div style="padding-left:20px; padding-bottom:4px; color:#555">';
             $html .= util_make_links(substr($data['description'], 0, 255), $this->group_id);
@@ -85,50 +94,40 @@ class Widget_ProjectLatestCommits extends Widget {
             }
             $html .= '</div>';
             $html .= '</div>';
+
+            $number_of_commit++;
         }
-        $html .= '<div style="text-align:center" class="'. util_get_alt_row_color($i++) .'">';
-        $html .= '<a href="'. $this->_getLinkToMore() .'">[ More ]</a>';
+
+        if ($number_of_commit === 0) {
+            $html .= $GLOBALS['Language']->getText('my_index','my_latest_commit_empty');
+
+            return $html;
+        }
+
+        $html .= '<div class="' . util_get_alt_row_color($i++) . ' project-last-commit-text-more">';
+        $html .= '<a href="' . $this->_getLinkToMore() . '">[ More ]</a>';
         $html .= '</div>';
+
         return $html;
     }
-    function isAvailable() {
+
+    public function isAvailable()
+    {
         return user_isloggedin() ? true : false;
     }
-    function hasRss() {
+
+    public function hasRss()
+    {
         return false;
     }
 
-    function getCategory() {
+    public function getCategory()
+    {
         return 'scm';
     }
 
-    function isAjax() {
+    public function isAjax()
+    {
         return true;
     }
-
-    /*
-    Do not use rss for this widget because we don't resolved authentification issue
-    function displayRss() {
-        $pm = ProjectManager::instance();
-        $project = $pm->getProject($this->group_id);
-        $rss = new RSS(array(
-            'title'       => $project->getPublicName() .' - '. $this->getTitle(),
-            'description' => '',
-            'link'        => get_server_url(),
-            'language'    => 'en-us',
-            'copyright'   => $GLOBALS['Language']->getText('rss','copyright',array($GLOBALS['sys_long_org_name'],$GLOBALS['sys_name'],date('Y',time()))),
-            'pubDate'     => gmdate('D, d M Y G:i:s',time()).' GMT',
-        ));
-        while($data = db_fetch_array($this->latest_revisions)) {
-            $rss->addItem(array(
-                'title'       => '#'.$data['revision'] .' by '. $data['who'] .' on '. format_date($GLOBALS['Language']->getText('system', 'datefmt'), $data['date']),
-                'description' => util_make_links(nl2br($data['description'])),
-                'link'        => '/svn/?func=detailrevision&amp;group_id='.$this->group_id.'&amp;commit_id='.$data['commit_id']
-            ));
-        }
-        $rss->display();
-        exit;
-
-    }*/
 }
-?>
