2d482557d204845f9b6898075f4cf3b3a44e5f78
Nicolas Terray <nicolas.terray@enalean.com>
2017-06-29 13:25:30 +0200

    Group activities by day
    Part of story #10334: Widget Heartbeat - artifacts
    
    There are three groups of activities: today, yesterday, and recently. If
    no activities in one group, then the corresponding label is not displayed.
    
    Change-Id: I75b63a50a90e82411375c265021ca3a5e62e40a5

--- src/common/widget/ProjectHeartbeatPresenter.php
+++ src/common/widget/ProjectHeartbeatPresenter.php
@@ -30,6 +30,9 @@ class ProjectHeartbeatPresenter
     public $purified_empty_state;
     public $error_message;
     public $locale;
+    public $today;
+    public $yesterday;
+    public $recently;
 
     public function __construct(Project $project, PFUser $user)
     {
@@ -37,6 +40,9 @@ class ProjectHeartbeatPresenter
         $this->locale     = $user->getShortLocale();
 
         $this->error_message = _('Unable to fetch the latest activities of the project');
+        $this->today         = _('Today');
+        $this->yesterday     = _('Yesterday');
+        $this->recently      = _('Recently');
 
         $this->purified_empty_state = Codendi_HTMLPurifier::instance()->purify(
             _('There are no items <br> you can see'),
