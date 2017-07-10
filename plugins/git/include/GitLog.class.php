Enalean/tuleap

commit 045fc38c415b3ea04cdcbdc7bae87dc293b70249

request #10106 Parse of gitlolite logs takes too much time and space
Introduce a table to aggregate data per day (model of svn_full_history)

ATM this doesn't manage the import from previous storage model.
However you can test the import of logs with import_all_giotlite3_logs.php
tool but you will loose the access done via gitphp

Change-Id: Id58c247dc9458bf75427862003021b2aa11107b8

plugins/git/include/GitLog.class.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2012. All Rights Reserved.
  *
  * This file is a part of Tuleap.
@@ -42,10 +43,50 @@ class GitLog {
      *
      * @return Void
      */
-    function logsDaily($params) {
-        $params['logs'][] = array('sql'   => $this->_dao->getSqlStatementForLogsDaily($params['group_id'], $params['logs_cond']),
-                                  'field' => $GLOBALS['Language']->getText('plugin_git', 'logsdaily_field'),
-                                  'title' => $GLOBALS['Language']->getText('plugin_git', 'logsdaily_title'));
+    function logsDaily($params)
+    {
+        $params['logs'][] = array(
+            'sql'   => $this->_dao->getSqlStatementForLogsDaily(
+                $params['group_id'],
+                $params['logs_cond'],
+                $this->getGitReadLogFilter($params['group_id'], $params['who'], $params['span'])
+            ),
+            'field' => $GLOBALS['Language']->getText('plugin_git', 'logsdaily_field'),
+            'title' => $GLOBALS['Language']->getText('plugin_git', 'logsdaily_title')
+        );
+    }
+
+    /**
+     * @see logs_cond in source_code_access_utils.php
+     *
+     * @param $group_id
+     * @return string
+     */
+    private function getGitReadLogFilter($group_id, $who, $span)
+    {
+        return $this->getWhoFilter($group_id, $who).
+            $this->getDateFilter($span);
+    }
+
+    private function getWhoFilter($group_id, $who)
+    {
+        $project = ProjectManager::instance()->getProject($group_id);
+        if ($who == "allusers") {
+            return "";
+        } else {
+            $users = $this->_dao->da->escapeIntImplode($project->getMembersId());
+            if ($who == "members") {
+                return " AND user.user_id IN ($users) ";
+            }
+            return " AND user.user_id NOT IN ($users) ";
+        }
+    }
+
+    private function getDateFilter($span)
+    {
+        $start_date = new DateTime();
+        $start_date->sub(new DateInterval('P'.$span.'D'));
+
+        return 'log.day >= '.$this->_dao->da->quoteSmart($start_date->format('Ymd'));
     }
 }
-?>

plugins/git/bin/convert_gitolite_full_logs.php
@@ -33,23 +33,10 @@ $console    = new Log_ConsoleLogger();
 $logger     = new GitBackendLogger();
 $broker_log = new BrokerLogger(array($logger, $console));
 
-$detector = new VersionDetector();
-if (! $detector->isGitolite3()) {
-    $broker_log->error("You are currently using Gitolite2. Parsing logs only works for Gitolite3.");
-    exit(1);
-}
+$broker_log->info("Starting transfer from plugin_git_full_history to plugin_git_log_read_daily");
 
-$broker_log->info("Starting parse gitolite3 logs.");
-$gitolite_parser = new Gitolite3LogParser(
-    $broker_log,
-    new System_Command(),
-    new HttpUserValidator(),
-    new Dao(),
-    new GitRepositoryFactory(new GitDao(), ProjectManager::instance()),
-    UserManager::instance(),
-    new GitoliteFileLogsDao()
-);
-$gitolite_parser->parseAllLogs(GITOLITE3_LOGS_PATH);
+$transfer = new \Tuleap\Git\Gitolite\GitoliteFullLogsToAggregatedLogs($broker_log);
+$transfer->convert();
 
 $broker_log->info("Logs parsed with success.");
 

plugins/git/db/install.sql
@@ -252,6 +252,19 @@ CREATE TABLE plugin_git_full_history (
   INDEX time_idx(time, repository_id)
 );
 
+CREATE TABLE IF NOT EXISTS plugin_git_full_history_checkpoint (
+  last_timestamp int(11) UNSIGNED NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS plugin_git_log_read_daily (
+  repository_id int(10) unsigned NOT NULL,
+  user_id int(11) NOT NULL,
+  day int(11) UNSIGNED NOT NULL,
+  git_read int(11) UNSIGNED NOT NULL default 0,
+  PRIMARY KEY (repository_id, user_id, day),
+  INDEX time_idx(day, repository_id)
+);
+
 CREATE TABLE plugin_git_file_logs_parse (
   file_name VARCHAR(255) NOT NULL,
   end_line INT(11),

plugins/git/db/mysql/updates/2017/201703301111_create_table_plugin_git_log_read.php
@@ -1,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @codingStandardsIgnoreFile
+ */
+
+class b201703301111_create_table_plugin_git_log_read extends ForgeUpgrade_Bucket
+{
+
+    public function description()
+    {
+        return "Create table plugin_git_log_read_daily";
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $sql = "CREATE TABLE plugin_git_log_read_daily (
+                  repository_id int(10) unsigned NOT NULL,
+                  user_id int(11) NOT NULL,
+                  day int(11) UNSIGNED NOT NULL,
+                  git_read int(11) UNSIGNED NOT NULL default 0,
+                  PRIMARY KEY (repository_id, user_id, day),
+                  INDEX time_idx(day, repository_id)
+                );";
+
+        $this->execDB($sql, 'An error occured while creating table plugin_git_log_read_daily');
+    }
+
+    protected function execDB($sql, $message)
+    {
+        $res = $this->db->dbh->exec($sql);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete($message.implode(', ', $this->db->dbh->errorInfo()));
+        }
+    }
+}

plugins/git/db/mysql/updates/2017/201704031442_create_table_plugin_git_full_history_checkpoint.php
@@ -1,0 +1,55 @@
+    <?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @codingStandardsIgnoreFile
+ */
+
+class b201704031442_create_table_plugin_git_full_history_checkpoint extends ForgeUpgrade_Bucket
+{
+
+    public function description()
+    {
+        return "Create table plugin_git_full_history_checkpoint";
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    /**
+     * This table is only needed for existing platforms that convert from plugin_git_full_history
+     */
+    public function up()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS plugin_git_full_history_checkpoint (
+                  last_timestamp int(11) UNSIGNED NOT NULL
+                )";
+
+        $this->execDB($sql, 'An error occured while creating table plugin_git_full_history_checkpoint');
+    }
+
+    protected function execDB($sql, $message)
+    {
+        $res = $this->db->dbh->exec($sql);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete($message.implode(', ', $this->db->dbh->errorInfo()));
+        }
+    }
+}

plugins/git/db/uninstall.sql
@@ -17,6 +17,8 @@ DROP TABLE IF EXISTS plugin_git_webhook_url;
 DROP TABLE IF EXISTS plugin_git_webhook_log;
 DROP TABLE IF EXISTS plugin_git_repository_fine_grained_permissions_enabled;
 DROP TABLE IF EXISTS plugin_git_full_history;
+DROP TABLE IF EXISTS plugin_git_full_history_checkpoint;
+DROP TABLE IF EXISTS plugin_git_log_read_daily;
 DROP TABLE IF EXISTS plugin_git_file_logs_parse;
 DROP TABLE IF EXISTS plugin_git_fine_grained_regexp_enabled;
 DROP TABLE IF EXISTS plugin_git_repository_fine_grained_regexp_enabled;

plugins/git/include/Git/Gitolite/Gitolite3LogParser.php
@@ -30,6 +30,7 @@ use System_Command;
 use Tuleap\Git\History\Dao;
 use Tuleap\Git\RemoteServer\Gerrit\HttpUserValidator;
 use UserManager;
+use GitRepository;
 
 class Gitolite3LogParser
 {
@@ -69,6 +70,16 @@ class Gitolite3LogParser
      * @var GitoliteFileLogsDao
      */
     private $file_logs_dao;
+
+    /**
+     * @var array
+     */
+    private $access_cache = array();
+
+    /**
+     * @var array
+     */
+    private $day_accesses_cache = array();
 
     public function __construct(
         Logger $logger,
@@ -130,6 +141,7 @@ class Gitolite3LogParser
                     }
                 }
 
+                $this->storeCacheInDb();
                 $this->file_logs_dao->storeLastLine($log, ftell($log_file));
                 fclose($log_file);
             }
@@ -164,7 +176,56 @@ class Gitolite3LogParser
                 $user_id = 0;
             }
 
-            $this->history_dao->insertGitReadAccess($repository->getId(), $user_id, $day->getTimestamp());
+            $this->cacheAccess($repository, $user_id, $day);
+        }
+    }
+
+    private function resetCaches()
+    {
+        $this->access_cache = array();
+        $this->day_accesses_cache = array();
+    }
+
+    private function cacheAccess(GitRepository $repository, $user_id, DateTime $day)
+    {
+        $day_key = $day->format('Ymd');
+        if (! isset($this->access_cache[$day_key][$repository->getId()][$user_id])) {
+            $this->access_cache[$day_key][$repository->getId()][$user_id] = 0;
+        }
+        $this->access_cache[$day_key][$repository->getId()][$user_id]++;
+    }
+
+    private function storeCacheInDb()
+    {
+        $this->history_dao->startTransaction();
+        foreach ($this->access_cache as $day => $repositories) {
+            foreach ($repositories as $repository_id => $users) {
+                foreach ($users as $user_id => $count) {
+                    if ($this->hasRecord($repository_id, $user_id, $day)) {
+                        $this->history_dao->addGitReadAccess($day, $repository_id, $user_id, $count);
+                    } else {
+                        $this->history_dao->insertGitReadAccess($day, $repository_id, $user_id, $count);
+                    }
+                }
+            }
+        }
+        $this->history_dao->commit();
+        $this->resetCaches();
+    }
+
+    private function hasRecord($repository_id, $user_id, $day)
+    {
+        $this->cacheAccessPerDay($day);
+        return isset($this->day_accesses_cache[$day][$repository_id][$user_id]);
+    }
+
+    private function cacheAccessPerDay($day)
+    {
+        if (! isset($this->day_accesses_cache[$day])) {
+            $dar = $this->history_dao->searchAccessPerDay($day);
+            foreach ($dar as $row) {
+                $this->day_accesses_cache[$day][$row['repository_id']][$row['user_id']] = 1;
+            }
         }
     }
 

plugins/git/include/Git/Gitolite/GitoliteFullLogsToAggregatedLogs.php
@@ -1,0 +1,104 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\Gitolite;
+
+use DataAccessObject;
+use DateTime;
+use DateInterval;
+use Logger;
+use Exception;
+
+class GitoliteFullLogsToAggregatedLogs extends \DataAccessObject
+{
+
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        parent::__construct();
+        $this->logger = $logger;
+        $this->enableExceptionsOnError();
+    }
+
+    public function convert()
+    {
+        $start_timestamp = $this->getLastImportedTimestamp();
+        do {
+            $end_timestamp   = $this->getEndDate($start_timestamp);
+            $this->logger->info(sprintf("Import between %s and %s", date('c', $start_timestamp), date('c', $end_timestamp)));
+            $start_timestamp = $this->updateBetween($start_timestamp, $end_timestamp);
+            $this->logger->info('Done, wait for 1 sec');
+            sleep(1);
+        } while ($end_timestamp < time());
+    }
+
+    private function updateBetween($start_timestamp, $end_timestamp)
+    {
+        $start_timestamp = $this->da->escapeInt($start_timestamp);
+        $end_timestamp   = $this->da->escapeInt($end_timestamp);
+
+        $this->da->startTransaction();
+
+        $sql = "INSERT INTO plugin_git_log_read_daily(repository_id, user_id, day, git_read)
+          SELECT repository_id, user_id, DATE_FORMAT(FROM_UNIXTIME(time), '%Y%m%d') as day, count(*)
+          FROM plugin_git_full_history
+          WHERE time > $start_timestamp AND time <= $end_timestamp
+          GROUP BY repository_id, user_id, day";
+        $this->update($sql);
+
+        $sql = "TRUNCATE TABLE plugin_git_full_history_checkpoint";
+        $this->update($sql);
+
+        $sql = "INSERT INTO plugin_git_full_history_checkpoint(last_timestamp)
+                VALUES ($end_timestamp)";
+        $this->update($sql);
+
+        $this->da->commit();
+        return $end_timestamp;
+    }
+
+    private function getLastImportedTimestamp()
+    {
+        $sql = 'SELECT last_timestamp FROM plugin_git_full_history_checkpoint';
+        $dar = $this->retrieve($sql);
+        if ($dar && $dar->rowCount() == 1) {
+            $row = $dar->getRow();
+            return $row['last_timestamp'];
+        }
+        $sql = 'SELECT MIN(time) as time FROM plugin_git_full_history';
+        $dar = $this->retrieve($sql);
+        if ($dar && $dar->rowCount() == 1) {
+            $row = $dar->getRow();
+            return $row['time'];
+        }
+        throw new Exception('Nothing to import');
+    }
+
+    private function getEndDate($start_timestamp)
+    {
+        $date_time = new DateTime("@$start_timestamp");
+        $date_time->add(new DateInterval('P7D'));
+        return $date_time->format('U');
+    }
+}

commit 87796fb2d13ec66a52be7a66ba305b72d30e15f2

request #10432: Git project access logs are only accessible when the filter "All users" is used
The issue has been introduced by commit 045fc38c415b3ea04cdcbdc7bae87dc293b70249.

Change-Id: Ic7caaf14dbfb753e3cd9df7bdd33bfc6dcbe5009

plugins/git/include/GitLog.class.php
@@ -64,24 +64,30 @@ class GitLog {
      */
     private function getGitReadLogFilter($group_id, $who, $span)
     {
-        return $this->getWhoFilter($group_id, $who).
-            $this->getDateFilter($span);
+        $filters = array($this->getWhoFilter($group_id, $who), $this->getDateFilter($span));
+        return implode(' AND ', array_filter($filters));
     }
 
+    /**
+     * @return string
+     */
     private function getWhoFilter($group_id, $who)
     {
-        $project = ProjectManager::instance()->getProject($group_id);
-        if ($who == "allusers") {
-            return "";
-        } else {
-            $users = $this->_dao->da->escapeIntImplode($project->getMembersId());
-            if ($who == "members") {
-                return " AND user.user_id IN ($users) ";
-            }
-            return " AND user.user_id NOT IN ($users) ";
+        if ($who === 'allusers') {
+            return '';
         }
+
+        $project = ProjectManager::instance()->getProject($group_id);
+        $users   = $this->_dao->da->escapeIntImplode($project->getMembersId());
+        if ($who === 'members') {
+            return "user.user_id IN ($users)";
+        }
+        return "user.user_id NOT IN ($users)";
     }
 
+    /**
+     * @return string
+     */
     private function getDateFilter($span)
     {
         $start_date = new DateTime();