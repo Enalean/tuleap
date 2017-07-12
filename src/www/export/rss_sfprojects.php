Enalean/tuleap

commit a746e73aeedc5eb350c4a8227887f3fc99081bcf

request #9097: Notices are raised when RSS feeds are accessed
To trigger the notices go to:
https://<tuleap_instance>/export/rss_sfnews.php
https://<tuleap_instance>/export/rss_sfnewreleases.php
https://<tuleap_instance>/export/rss_sfprojects.php

Change-Id: I0e3ebb3b064351b35d71cf32be81ffff789c6f39

src/www/export/rss_sfprojects.php
@@ -4,9 +4,8 @@ require_once('pre.php');
 require('./rss_utils.inc');
 require_once('www/new/new_utils.php');
 
-$server = get_server_url();
-
-$request =& HTTPRequest::instance();
+$request = HTTPRequest::instance();
+$server  = $request->getServerUrl();
 
 if ($request->get('option') == "newest") {
   if (!$request->get('limit')) {
@@ -18,7 +17,7 @@ if ($request->get('option') == "newest") {
   $res = db_query($query);
 } else {
   $res = db_query("SELECT group_id,unix_group_name,group_name,short_description, xrx_export_ettm FROM groups '
-		  .'WHERE access != '".db_es(Project::ACCESS_PRIVATE)."' AND status='A' AND type=1 ORDER BY group_id".($request->get('limit')?" LIMIT ".$request->get('limit'):""));
+		  .'WHERE access != '".db_es(Project::ACCESS_PRIVATE)."' AND status='A' AND type=1 ORDER BY group_id".($request->get('limit')?" LIMIT ".db_ei($request->get('limit')):""));
 }
 
 if ($request->get('type') == "rss") {
@@ -31,24 +30,28 @@ print '<?xml version="1.0"  encoding="UTF-8" ?>
 
 // ## one time output
 print " <channel>\n";
-print "  <copyright>".$Language->getText('export_rss_sfnewreleases','copyright',array($GLOBALS['sys_long_org_name'],$GLOBALS['sys_name'],date('Y',time())))."</copyright>\n";
+print "  <copyright>".$Language->getText('export_rss_sfnewreleases', 'copyright', array(
+		ForgeConfig::get('sys_long_org_name'),
+		ForgeConfig::get('sys_name'),
+		date('Y',time())
+	))."</copyright>\n";
 print "  <pubDate>".gmdate('D, d M Y g:i:s',time())." GMT</pubDate>\n";
-print "  <description>".$GLOBALS['sys_name']." ".$Language->getText('export_index','full_proj_listing')."</description>\n";
+print "  <description>". ForgeConfig::get('sys_name') ." ".$Language->getText('export_index','full_proj_listing')."</description>\n";
 print "  <link>$server</link>\n";
-if ($option == "newest") {
-print "  <title>".$GLOBALS['sys_name']." ".$Language->getText('export_rss_sfprojects','new_proj')."</title>\n";
+if ($request->get('option') == "newest") {
+	print "  <title>". ForgeConfig::get('sys_name') ." ".$Language->getText('export_rss_sfprojects','new_proj')."</title>\n";
 } else {
-print "  <title>".$GLOBALS['sys_name']." ".$Language->getText('export_index','full_proj_listing')."</title>\n";
+	print "  <title>". ForgeConfig::get('sys_name') ." ".$Language->getText('export_index','full_proj_listing')."</title>\n";
 }
-print "  <webMaster>".$GLOBALS['sys_email_contact']."</webMaster>\n";
+print "  <webMaster>". ForgeConfig::get('sys_email_contact') ."</webMaster>\n";
 print "  <language>en-us</language>\n";
 // ## item outputs
 while ($row = db_fetch_array($res)) {
 	print "  <item>\n";
 	print "   <title>".htmlspecialchars($row['group_name'])."</title>\n";
-	print "   <link>$server/project/?group_id=$row[group_id]</link>\n";
+	print "   <link>$server/project/?group_id=". urlencode($row['group_id']) . "</link>\n";
 	print "   <description>";
-	print ereg_replace(" *\r*\n *"," ",rss_description($row['short_description']));
+	print preg_replace(" *\r*\n *"," ",rss_description($row['short_description']));
 	print "</description>\n";
 	print "  </item>\n";
 }
@@ -59,7 +62,7 @@ print " </channel>\n";
 } else if ($request->get('type') == "csv") {
 
     header('Content-Type: text/csv');
-    if ($option == "newest") {
+    if ($request->get('option') == "newest") {
       header ('Content-Disposition: filename=new_projects.csv');
     } else {
       header ('Content-Disposition: filename=all_projects.csv');
@@ -99,7 +102,7 @@ print " </channel>\n";
 	// Get Project admin as contacts
 	$res_admin = db_query("SELECT user.user_id AS user_id,user.user_name AS user_name, user.realname AS realname, user.email AS email "
 			      . "FROM user,user_group "
-			      . "WHERE user_group.user_id=user.user_id AND user_group.group_id=".$row['group_id']." AND "
+			      . "WHERE user_group.user_id=user.user_id AND user_group.group_id=". db_ei($row['group_id']) ." AND "
 			      . "user_group.admin_flags = 'A'");
 
 	$admins = array();
@@ -107,25 +110,24 @@ print " </channel>\n";
 	    $admins[] = $row_admin['realname'].' <'.$row_admin['email'].'>';
 	}
 	
-	print "$row[group_id],";
-	print "$row[unix_group_name],";
-	print '"$row[group_name]",';
-	print '"'.ereg_replace(" *\r*\n *"," ",$row['short_description']).'",';
+	print $row['group_id']. ",";
+	print $row['unix_group_name'] . ",";
+	print $row['group_name'] . ',';
+	print '"'.preg_replace(" *\r*\n *"," ",$row['short_description']).'",';
 	print '"'.join(',',$lang).'",';
 	print '"'.join(',',$os).'",';
 	print '"'.join(',',$devstate).'",';
 	
-	print "$server/file/showfiles.php?group_id=$row[group_id],";
-	print "$server/projects/$row[unix_group_name],";
+	print "$server/file/showfiles.php?group_id=" . $row['group_id'] . ",";
+	print "$server/projects/". $row['unix_group_name'] . ",";
 	print '"'.join(',',$admins).'",';
 	
 	// Finally print whether this file is visisble in the Xerox eTTM Software Inventory
 	// Xerox specific
-	print "$row[xrx_export_ettm]";
+	print $row['xrx_export_ettm'];
 	print "\n";
 	
 }
 // ## end output
 print "\n";
 }
-?>

src/www/export/rss_sfnewreleases.php
@@ -1,5 +1,5 @@
 <?php
-  //
+  // Copyright (c) Enalean, 2016. All Rights Reserved.
   // SourceForge: Breaking Down the Barriers to Open Source Development
   // Copyright 1999-2000 (c) The SourceForge Crew
   // http://sourceforge.net
@@ -13,41 +13,55 @@ require_once('www/new/new_utils.php');
 require_once('common/frs/FRSReleaseFactory.class.php');
 require('./rss_utils.inc');
 
+$request = HTTPRequest::instance();
+
 header("Content-Type: text/xml");
 
 print '<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="0.91">
 ';
 // ## default limit
-if (!$limit) $limit = 10;
-if ($limit > 100) $limit = 100;
+$limit          = 10;
+$validator_uint = new Valid_UInt('limit');
+if ($request->valid($validator_uint)) {
+  $limit = $request->get('limit');
+}
+if ($limit > 100) {
+  $limit = 100;
+}
 
-$query = new_utils_get_new_releases_long(0,0,$limit*3);
-$res=db_query($query);
-
+$query = new_utils_get_new_releases_long(0, 0, $limit*3);
+$res   = db_query($query);
 
 // ## one time output
 print " <channel>\n";
-print "  <copyright>".$Language->getText('export_rss_sfnewreleases','copyright',array($GLOBALS['sys_long_org_name'],$GLOBALS['sys_name'],date('Y',time())))."</copyright>\n";
+print "  <copyright>".$Language->getText(
+        'export_rss_sfnewreleases',
+        'copyright',
+        array(ForgeConfig::get('sys_long_org_name'),ForgeConfig::get('sys_name'),date('Y',time()))
+    )."</copyright>\n";
 print "  <pubDate>".gmdate('D, d M Y g:i:s',time())." GMT</pubDate>\n";
-print "  <description>".$Language->getText('export_rss_sfnewreleases','new_releases',$GLOBALS['sys_name'])."</description>\n";
-print "  <link>".get_server_url()."</link>\n";
-print "  <title>".$Language->getText('export_rss_sfnewreleases','new_releases',$GLOBALS['sys_name'])."</title>\n";
-list($host,$port) = explode(':',$GLOBALS['sys_default_domain']);		
-print "  <webMaster>webmaster@".$host."</webMaster>\n";
+print "  <description>".$Language->getText(
+        'export_rss_sfnewreleases',
+        'new_releases',
+        ForgeConfig::get('sys_name')
+    ) ."</description>\n";
+print "  <link>". $request->getServerUrl() ."</link>\n";
+print "  <title>".$Language->getText('export_rss_sfnewreleases','new_releases',ForgeConfig::get('sys_name'))."</title>\n";
 print "  <language>en-us</language>\n";
 // ## item outputs
 $outputtotal = 0;
-$frspf = new FRSPackageFactory();
-$frsrf = new FRSReleaseFactory();
+$frspf       = new FRSPackageFactory();
+$frsrf       = new FRSReleaseFactory();
+$G_RELEASE   = array();
 
 while ($row = db_fetch_array($res)) {
-  if (!$G_RELEASE["$row[group_id]"]) {
+  if (!$G_RELEASE[$row['group_id']]) {
     if ($frspf->userCanRead($row['group_id'], $row['package_id'], 100) &&
-        $frsrf->userCanRead($row['group_id'], $row['package_id'], $row_newrel['release_id'], 100)) {
+        $frsrf->userCanRead($row['group_id'], $row['package_id'], $row['release_id'], 100)) {
       print "  <item>\n";
       print "   <title>".htmlspecialchars($row['group_name']." ". $row['release_version'])."</title>\n";
-      print "   <link>".get_server_url()."/file/showfiles.php?group_id=$row[group_id]</link>\n";
+      print "   <link>". $request->getServerUrl() ."/file/showfiles.php?group_id=" . urlencode($row['group_id']) . "</link>\n";
       print "   <description>".rss_description($row['short_description'])."</description>\n";
       print "  </item>\n";
       $outputtotal++;
@@ -60,4 +74,3 @@ while ($row = db_fetch_array($res)) {
  }
 // ## end output
 print " </channel>\n</rss>";
-?>

src/www/export/rss_sfnews.php
@@ -7,23 +7,27 @@ require_once('common/rss/RSS.class.php');
 require('rss_utils.inc');
 
 //First, check for valid group_id
-$request =& HTTPRequest::instance();
+$request = HTTPRequest::instance();
 if ($request->exist('group_id')) {
     $pm = ProjectManager::instance();
     $project = $pm->getProject($request->get('group_id'));
     if (!$project) {
         $rss = new RSS(array(
-            'title'       => $Language->getText('export_rss_sfnews','news',$GLOBALS['sys_name']),
-            'description' => $Language->getText('export_rss_sfnews','highlights',$GLOBALS['sys_name']),
-            'link'        => get_server_url(),
+            'title'       => $Language->getText('export_rss_sfnews', 'news', ForgeConfig::get('sys_name')),
+            'description' => $Language->getText('export_rss_sfnews', 'highlights', ForgeConfig::get('sys_name')),
+            'link'        => $request->getServerUrl(),
             'language'    => 'en-us',
-            'copyright'   => $Language->getText('export_rss_sfnewreleases','copyright',array($GLOBALS['sys_long_org_name'],$GLOBALS['sys_name'],date('Y',time()))),
+            'copyright'   => $Language->getText(
+                'export_rss_sfnewreleases',
+                'copyright',
+                array(ForgeConfig::get('sys_long_org_name'), ForgeConfig::get('sys_name'),date('Y',time()))
+            ),
             'pubDate'     => gmdate('D, d M Y G:i:s',time()).' GMT',
         ));
         $rss->addItem(array(
             'title'       => 'Error',
             'description' => 'Project not found',
-            'link'        => get_server_url()
+            'link'        => $request->getServerUrl()
         ));
         $rss->display();
         exit;
@@ -36,8 +40,14 @@ print '<?xml version="1.0"  encoding="UTF-8" ?>
 <rss version="0.91">
 ';
 // ## default limit
-if (!isset($limit) || !$limit) $limit = 10;
-if ($limit > 100) $limit = 100;
+$limit          = 10;
+$validator_uint = new Valid_UInt('limit');
+if ($request->valid($validator_uint)) {
+    $limit = $request->get('limit');
+}
+if ($limit > 100) {
+    $limit = 100;
+}
 
 //
 // Database news_bytes table:
@@ -50,46 +60,50 @@ if ($limit > 100) $limit = 100;
 if (isset($group_id) && $group_id) {
     $project = new Project($group_id);
     // We want only project news, not deleted
-    $where_clause = " is_approved<>4 AND group_id=$group_id ";
+    $where_clause = " is_approved<>4 AND group_id=" . db_ei($group_id) . " ";
 } else {
     // We want only approved news (=1 => automatically <>4)
     $where_clause = " is_approved = 1 ";
 }
 
 $res = db_query('SELECT forum_id,summary,date,details,group_id FROM news_bytes '
-	.'WHERE '.$where_clause.' ORDER BY date DESC LIMIT '.$limit);
+	.'WHERE '.$where_clause.' ORDER BY date DESC LIMIT '. db_ei($limit));
 
 // ## one time output
 
 print " <channel>\n";
-print "  <copyright>".$Language->getText('export_rss_sfnewreleases','copyright',array($GLOBALS['sys_long_org_name'],$GLOBALS['sys_name'],date('Y',time())))."</copyright>\n";
+print "  <copyright>".$Language->getText(
+        'export_rss_sfnewreleases',
+        'copyright',
+        array(ForgeConfig::get('sys_long_org_name'),ForgeConfig::get('sys_name'),date('Y',time())))."</copyright>\n";
 print "  <pubDate>".gmdate('D, d M Y G:i:s',time())." GMT</pubDate>\n";
 
 if (isset($group_id) && $group_id) {
-  print "  <description>".$Language->getText('export_rss_sfnews','highlights',$GLOBALS['sys_name'])." - ".$project->getPublicName()."</description>\n";
-    print "  <link>".get_server_url()."/project/?group_id=$group_id</link>\n";
-    print "  <title> ".$Language->getText('export_rss_sfnews','news',$GLOBALS['sys_name'])." - ".$project->getPublicName()."</title>\n";
+  print "  <description>".$Language->getText('export_rss_sfnews',
+          'highlights',
+          ForgeConfig::get('sys_name'))." - ".$project->getPublicName()."</description>\n";
+    print "  <link>". $request->getServerUrl() ."/project/?group_id=". urlencode($group_id) . "</link>\n";
+    print "  <title> ".$Language->getText('export_rss_sfnews', 'news', ForgeConfig::get('sys_name'))." - ".$project->getPublicName()."</title>\n";
 } else {
-    print "  <description>".$Language->getText('export_rss_sfnews','highlights',$GLOBALS['sys_name'])."</description>\n";
-    print "  <link>".get_server_url()."</link>\n";
-    print "  <title> ".$Language->getText('export_rss_sfnews','news',$GLOBALS['sys_name'])."</title>\n";
+    print "  <description>".$Language->getText('export_rss_sfnews', 'highlights', ForgeConfig::get('sys_name'))."</description>\n";
+    print "  <link>". $request->getServerUrl() ."</link>\n";
+    print "  <title> ".$Language->getText('export_rss_sfnews','news', ForgeConfig::get('sys_name')
+        )."</title>\n";
 }
-list($host,$port) = explode(':',$GLOBALS['sys_default_domain']);		
-print "  <webMaster>webmaster@".$host."</webMaster>\n";
 print "  <language>en-us</language>\n";
 // ## item outputs
 
 while ($row = db_fetch_array($res)) {
-	$forum_id=$row['forum_id'];
-    $group_id = isset($group_id)?$group_id:$GLOBALS['sys_news_group'];
+	$forum_id = $row['forum_id'];
+    $group_id = isset($group_id) ? $group_id:ForgeConfig::get('sys_news_group');
 	if (news_check_permission($forum_id,$group_id)) {
 	    print "  <item>\n";
 	    print "   <title>".htmlspecialchars($row['summary'])."</title>\n";
 	    // if news group, link is main page
-	    if ($row['group_id'] != $GLOBALS['sys_news_group']) {
-		print "   <link>".get_server_url()."/forum/forum.php?forum_id=$row[forum_id]</link>\n";
+	    if ($row['group_id'] != ForgeConfig::get('sys_news_group')) {
+		    print "   <link>". $request->getServerUrl() ."/forum/forum.php?forum_id=". urlencode($forum_id) . "</link>\n";
 	    } else {
-		print "   <link>".get_server_url()."/</link>\n";
+		    print "   <link>". $request->getServerUrl() ."/</link>\n";
 	    }
 	    print "   <description>".rss_description($row['details'])."</description>\n";
 	    print "  </item>\n";
@@ -97,4 +111,3 @@ while ($row = db_fetch_array($res)) {
 }
 // ## end output
 print " </channel>\n</rss>";
-?>

commit ee27a5e2ffea5baf299793d5446d0b746832053f

request #10445: A warning is raised when the RSS feed of the newest projects is accessed
You can trigger the warning by accessing the page
https://<tuleap_instance>/export/rss_sfprojects.php?type=rss&option=newest

This issue has been introduced by commit
a746e73aeedc5eb350c4a8227887f3fc99081bcf

Change-Id: If48622e1d6b5fa9f713b54a859c63cc72eecc59d

src/www/export/rss_sfprojects.php
@@ -51,7 +51,7 @@ while ($row = db_fetch_array($res)) {
 	print "   <title>".htmlspecialchars($row['group_name'])."</title>\n";
 	print "   <link>$server/project/?group_id=". urlencode($row['group_id']) . "</link>\n";
 	print "   <description>";
-	print preg_replace(" *\r*\n *"," ",rss_description($row['short_description']));
+	print preg_replace("/ *\r*\n */"," ",rss_description($row['short_description']));
 	print "</description>\n";
 	print "  </item>\n";
 }
@@ -113,7 +113,7 @@ print " </channel>\n";
 	print $row['group_id']. ",";
 	print $row['unix_group_name'] . ",";
 	print $row['group_name'] . ',';
-	print '"'.preg_replace(" *\r*\n *"," ",$row['short_description']).'",';
+	print '"'.preg_replace("/ *\r*\n */"," ",$row['short_description']).'",';
 	print '"'.join(',',$lang).'",';
 	print '"'.join(',',$os).'",';
 	print '"'.join(',',$devstate).'",';