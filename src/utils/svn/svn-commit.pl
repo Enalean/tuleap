Enalean/tuleap

commit 10be1c2e2a87625f8cf23b3d0817f0d842d944be

request #10372: SQL injection in Subversion post commit script
Change-Id: Id4ceb70d8d003ea9c9ff5d7ef032dc4d1377b828

src/utils/svn/svn-commit.pl
@@ -1,21 +1,19 @@
 #!/usr/bin/perl
-#
+# Copyright (c) Enalean, 2017. All Rights Reserved.
 # Copyright (c) Xerox Corporation, Codendi Team, 2001-2010. All rights reserved
 #
-# This file is a part of Codendi.
-#
-# Codendi is free software; you can redistribute it and/or modify
+# Tuleap is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# Codendi is distributed in the hope that it will be useful,
+# Tuleap is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+# along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
 #
 
 #use strict;
@@ -60,8 +58,9 @@ if ( !$from_rev ) {
     $gname =~ s|.*/||; # Remove everything until the last slash
     my $group_id = &set_group_info_from_name($gname);
 
-    my $q_max_rev = "SELECT MAX(revision) AS r FROM svn_commits c WHERE group_id=$group_id";
+    my $q_max_rev = "SELECT MAX(revision) AS r FROM svn_commits c WHERE group_id=?";
     my $c_max_rev = $dbh->prepare($q_max_rev);
+    $c_max_rev->bind_param(1, $group_id, SQL_INTEGER);
     my $r_max_rev = $c_max_rev->execute();
     if ($r_max_rev && ($c_max_rev->rows eq 1)) {
 	my $row = $c_max_rev->fetchrow_hashref;

src/utils/group.pl
@@ -1,18 +1,22 @@
 #
-# Codendi
+# Copyright (c) Enalean, 2017. All Rights Reserved.
 # Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
-# http://www.codendi.com
 #
-# 
+# Tuleap is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#  License:
-#    This file is subject to the terms and conditions of the GNU General Public
-#    license. See the file COPYING in the main directory of this archive for
-#    more details.
+# Tuleap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Purpose:
-#    This Perl include file mimics some of the fucntion in common/project/Group.class.php
-#    to allow Perl scripts to handle group information
+# You should have received a copy of the GNU General Public License
+# along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+#
+
+use DBI qw(:sql_types);
 
 my ($GROUP_INFO);
 
@@ -21,8 +25,9 @@ sub set_group_info_from_name {
   my ($gname) = @_;
   my ($query, $c, $res);
 
-  $query = "SELECT * FROM groups WHERE unix_group_name='$gname'";
+  $query = "SELECT * FROM groups WHERE unix_group_name=?";
   $c = $dbh->prepare($query);
+  $c->bind_param(1, $gname, SQL_VARCHAR);
   $res = $c->execute();
 
   if (!$res || ($c->rows < 1)) {
@@ -116,9 +121,10 @@ sub get_email_from_login {
     my ($query, $c, $res);
 
     if ($username ne '') {
-        $query = "SELECT email FROM user WHERE user_name='$username' AND (status='A' OR status='R') ";
+        $query = "SELECT email FROM user WHERE user_name=? AND (status='A' OR status='R') ";
         
         $c = $dbh->prepare($query);
+        $c->bind_param(1, $username, SQL_VARCHAR);
         $res = $c->execute();
     
         if (!$res || ($c->rows < 1)) {
@@ -218,9 +224,10 @@ sub is_valid_email {
     my ($query, $c, $res);
 
     if ($email ne '') {
-        $query = "SELECT * FROM user WHERE email='$email' ";
+        $query = "SELECT * FROM user WHERE email=? ";
         
         $c = $dbh->prepare($query);
+        $c->bind_param(1, $email, SQL_VARCHAR);
         $res = $c->execute();
         
         if (!$res || ($c->rows < 1)) {

src/utils/ldap.pl
@@ -8,6 +8,9 @@
 #############################
 #
 #############################
+
+use DBI qw(:sql_types);
+
 sub ldap_connect {
     &load_local_config($db_config_file);
 
@@ -139,8 +142,9 @@ sub ldap_connect {
             "SELECT NULL"
           . " FROM groups g JOIN plugin_ldap_svn_repository svnrep USING (group_id)"
           . " WHERE svnrep.ldap_auth = 1"
-          . " AND g.group_id = $group_id";
+          . " AND g.group_id = ?";
         my $c = $dbh->prepare($query);
+        $c->bind_param(1, $group_id, SQL_INTEGER);
         $c->execute();
         return ( $c->rows == 1 );
     }

src/utils/svn/svn-checkins.pl
@@ -1,28 +1,31 @@
 #
-# Codendi
+# Copyright (c) Enalean, 2017. All Rights Reserved.
 # Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
-# http://www.codendi.com
 #
-# 
+# Tuleap is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#  License:
-#    This file is subject to the terms and conditions of the GNU General Public
-#    license. See the file COPYING in the main directory of this archive for
-#    more details.
+# Tuleap is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Purpose:
-#    This Perl include file stores Subversion commit information into the 
-#   Codendi Database
+# You should have received a copy of the GNU General Public License
+# along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
 #
 
+use DBI qw(:sql_types);
 
 require $utils_path."/session.pl";
 
 sub db_get_field {
   my ($table, $fieldname, $value, $retfieldname) = @_;
   my ($query, $res);
-  $query = "SELECT $retfieldname  FROM $table WHERE $fieldname='$value'";
+  $query = "SELECT $retfieldname  FROM $table WHERE $fieldname=?";
   $sth = $dbh->prepare($query);
+  $sth->bind_param(1, $value, SQL_VARCHAR);
   $res = $sth->execute();
   if ($sth->rows >= 1) {
     $hash_ref = $sth->fetchrow_hashref;
@@ -38,20 +41,21 @@ sub db_get_index {
   my ($table, $fieldname, $value) = @_;
   my ($query, $res);
   $debug = 0;
-  $query = "SELECT id  FROM $table WHERE $fieldname=\"$value\"";
+  $query = "SELECT id  FROM $table WHERE $fieldname=?";
   if ($debug) {
     print STDERR $query, "\n";
   }
   $sth = $dbh->prepare($query);
+  $sth->bind_param(1, $value, SQL_VARCHAR);
   $res = $sth->execute();
   if ($sth->rows >= 1) {
     $hash_ref = $sth->fetchrow_hashref;
     $res = $hash_ref->{'id'};
   } else {
     ## new repository to create
-    $query = sprintf "INSERT INTO $table (id, $fieldname) VALUES ('', %s)",
-                $dbh->quote($value);
+    $query = "INSERT INTO $table (id, $fieldname) VALUES ('', ?)";
     $sth = $dbh->prepare($query);
+    $sth->bind_param(1, $value, SQL_VARCHAR);
     $res = $sth->execute();
     if (!$res) {
       $res = 0;
@@ -80,11 +84,17 @@ sub db_add_record {
   }
 
   $query = "INSERT INTO svn_checkins (type, commitid, dirid, fileid, addedlines, removedlines)".
-    "VALUES ('$type', '$commit_id','$dir_id','$file_id', '$added', '$removed')";
+    "VALUES (?, ?, ?, ?, ?, ?)";
   if ($debug) {
     print STDERR $query, "\n";
   }
   $sth = $dbh->prepare($query);
+  $sth->bind_param(1, $type, SQL_VARCHAR);
+  $sth->bind_param(2, $commit_id, SQL_INTEGER);
+  $sth->bind_param(3, $dir_id, SQL_INTEGER);
+  $sth->bind_param(4, $file_id, SQL_INTEGER);
+  $sth->bind_param(5, $added, SQL_INTEGER);
+  $sth->bind_param(6, $removed, SQL_INTEGER);
   $res = $sth->execute();
 
   return $res;
@@ -113,11 +123,17 @@ sub db_get_commit {
   $repo_id = db_get_index('svn_repositories','repository', $repo);
 
   $query = "INSERT INTO svn_commits (group_id,repositoryid,revision,date,whoid,description) ".
-    "VALUES ('$group_id','$repo_id','$revision','$date','$uid','$fulldesc')";
+    "VALUES (?, ?, ?, ?, ?, ?)";
   if ($debug) {
     print STDERR $query, "\n";
   }
   $sth = $dbh->prepare($query);
+  $sth->bind_param(1, $group_id, SQL_INTEGER);
+  $sth->bind_param(2, $repo_id, SQL_INTEGER);
+  $sth->bind_param(3, $revision, SQL_INTEGER);
+  $sth->bind_param(4, $date, SQL_INTEGER);
+  $sth->bind_param(5, $uid, SQL_INTEGER);
+  $sth->bind_param(6, $fulldesc, SQL_VARCHAR);
   $res = $sth->execute();
   if (!$res) {
     if ($debug) {

commit 953327791453731360222d94f26e22158bf264ce

request #10471: Script used to reimport SVN commits into the database after a manual import is broken
You should not get an error about an attribute parameter not being a hash ref anymore.

The issue has been introduced by commit 10be1c2e2a87625f8cf23b3d0817f0d842d944be

Change-Id: I8d5ca0af0c607e8162beccdc361e0add213f4453

src/utils/svn/svn-commit.pl
@@ -21,6 +21,7 @@
 use Getopt::Std;
 use Carp;
 use DBI;
+use DBI qw(:sql_types);
 
 # Svnlook path.
 my $svnlook = "/usr/bin/svnlook";