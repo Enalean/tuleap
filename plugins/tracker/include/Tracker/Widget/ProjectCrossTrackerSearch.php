b7640e4b55b04fafbec8c8814f4aed5580928e3f
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-08-01 14:37:50 +0200

    Create widget for projects
    this is part of story #10495 have base cross tracker search widget (step 1)
    
    This commit allows user to add a new widget in project section.
    The new widget only displays an empty state for now.
    
    Change-Id: If78c9bb144385aa8e58cc751b217dee48b9f682d

---
+++ plugins/tracker/include/Tracker/Widget/ProjectCrossTrackerSearch.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+namespace Tuleap\Tracker\Widget;
+
+use TemplateRendererFactory;
+use Widget;
+
+class ProjectCrossTrackerSearch extends Widget
+{
+    const NAME = 'crosstrackersearch';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME);
+    }
+
+    public function getContent()
+    {
+        $renderer = TemplateRendererFactory::build()->getRenderer(
+            TRACKER_TEMPLATE_DIR . '/widgets'
+        );
+
+        return $renderer->renderToString(
+            'project-cross-tracker-search',
+            new ProjectCrossTrackerSearchPresenter()
+        );
+    }
+
+    public function getDescription()
+    {
+        return dgettext('tuleap-tracker', 'Search into multiple trackers and multiple projects.');
+    }
+
+    public function getIcon()
+    {
+        return "fa-list";
+    }
+
+    public function getTitle()
+    {
+        return dgettext('tuleap-tracker', 'Cross tracker search');
+    }
+
+    public function getCategory()
+    {
+        return 'trackers';
+    }
+
+    public function isUnique()
+    {
+        return false;
+    }
+}
