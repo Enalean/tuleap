954abe81b72aff1dd730111d440314ea93fa7a5d
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-08-09 18:04:13 +0200

    request #10509 On subversion, Rename userGroup name is not considered on Access control
    Change-Id: Ib535787f0b7a4ef7090f5cb4bf60ec8cf4a2491c

--- tests/simpletest/common/svn/SVNAccessFileTest.php
+++ tests/simpletest/common/svn/SVNAccessFileTest.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Copyright (c) STMicroelectronics 2011. All rights reserved
+ * Copyright (c) Enalean 2017. All rights reserved
  *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,10 +17,12 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class SVNAccessFileTest extends TuleapTestCase {
+class SVNAccessFileTest extends TuleapTestCase
+{
 
-    function testisGroupDefinedInvalidSyntax() {
-        $saf = new SVNAccessFile();
+    public function testisGroupDefinedInvalidSyntax()
+    {
+        $saf    = new SVNAccessFile();
         $groups = array();
         $this->assertFalse($saf->isGroupDefined($groups, 'uGroup1 = rw'));
         $this->assertFalse($saf->isGroupDefined($groups, '@uGroup1  rw'));
@@ -28,21 +31,25 @@ class SVNAccessFileTest extends TuleapTestCase {
         $this->assertFalse($saf->isGroupDefined($groups, '@@uGroup1 = rw'));
     }
 
-    function testisGroupDefinedNoUGroup() {
+    public function testisGroupDefinedNoUGroup()
+    {
         $groups = array();
-        $saf = new SVNAccessFile();
+        $saf    = new SVNAccessFile();
         $this->assertFalse($saf->isGroupDefined($groups, '@uGroup3 = rw'));
     }
 
-    function testisGroupDefined() {
+    public function testisGroupDefined()
+    {
         $groups = array('ugroup2' => true, 'a' => true);
-        $saf = new SVNAccessFile();
-        $this->assertTrue($saf->isGroupDefined($groups, '@uGroup2=rw'));
+        $saf    = new SVNAccessFile();
+        $this->assertTrue($saf->isGroupDefined($groups, '@ugroup2=rw'));
+        $this->assertFalse($saf->isGroupDefined($groups, '@uGroup2=rw'));
         $this->assertFalse($saf->isGroupDefined($groups, '@uGroup3 = rw'));
         $this->assertTrue($saf->isGroupDefined($groups, '@a=rw'));
     }
 
-    function testValidateUGroupLine() {
+    public function testValidateUGroupLine()
+    {
         $saf = partial_mock('SVNAccessFile', array('isGroupDefined'));
         $saf->setReturnValue('isGroupDefined', true);
         $groups = array('uGroup1' => false, 'uGroup2' => false, 'uGroup3' => true, 'uGroup33' => true);
@@ -56,9 +63,10 @@ class SVNAccessFileTest extends TuleapTestCase {
         $this->assertEqual('@uGroup33	= rw', $saf->validateUGroupLine($groups, '@uGroup33	= rw', null));
     }
 
-    function testRenameGroup() {
+    public function testRenameGroup()
+    {
         $groups = array('ugroup1' => SVNAccessFile::UGROUP_DEFAULT, 'ugroup2' => SVNAccessFile::UGROUP_DEFAULT, 'ugroup3' => SVNAccessFile::UGROUP_REDEFINED);
-        $saf = new SVNAccessFile();
+        $saf    = new SVNAccessFile();
         $saf->setRenamedGroup('ugroup11', 'ugroup1');
         $this->assertEqual('@ugroup11 = rw', $saf->renameGroup($groups, '@ugroup1 = rw'));
         $this->assertEqual('@ugroup2 = rw', $saf->renameGroup($groups, '@ugroup2 = rw'));
@@ -68,25 +76,28 @@ class SVNAccessFileTest extends TuleapTestCase {
         $this->assertEqual('@ugroup2 = rw', $saf->renameGroup($groups, '@ugroup2 = rw'));
     }
 
-    function testCommentInvalidLine() {
+    public function testCommentInvalidLine()
+    {
         $groups = array('ugroup1' => SVNAccessFile::UGROUP_DEFAULT, 'ugroup2' => SVNAccessFile::UGROUP_DEFAULT, 'ugroup3' => SVNAccessFile::UGROUP_REDEFINED);
-        $saf = new SVNAccessFile();
+        $saf    = new SVNAccessFile();
         $this->assertEqual('@ugroup1 = rw', $saf->commentInvalidLine($groups, '@ugroup1 = rw'));
         $this->assertEqual('# @ugroup2', $saf->commentInvalidLine($groups, '@ugroup2'));
     }
 
-    function testParseGroupLines() {
+    public function testParseGroupLines()
+    {
         $project = aMockProject()->build();
-        
+
         $saf = partial_mock('SVNAccessFile', array('getPlatformBlock'));
         $saf->setReturnValue('getPlatformBlock', "[groups]\nmembers = user1, user2\nuGroup1 = user3\n\n[/]\n*=\n@members=rw\n");
-        
+
         $this->assertEqual("[/]\n@members=rw\n# @group1 = r", $saf->parseGroupLines($project, "[/]\n@members=rw\n@group1 = r"));
         $this->assertEqual("[/]\n@members=rw\n# @group1 = r\n[Groups]\ngroup1=user1, user2\n[/trunk]\n@group1=r\nuser1=rw", $saf->parseGroupLines($project, "[/]\n@members=rw\n@group1 = r\n[Groups]\ngroup1=user1, user2\n[/trunk]\n@group1=r\nuser1=rw"));
         $this->assertEqual("[/]\n@members=rw\n# @group1 = r\n[Groups]\ngroup1=user1, user2\n[groups]\ngroup2=user3\n[/trunk]\n@group1=r\nuser1=rw\n@group2=rw", $saf->parseGroupLines($project, "[/]\n@members=rw\n@group1 = r\n[Groups]\ngroup1=user1, user2\n[groups]\ngroup2=user3\n[/trunk]\n@group1=r\nuser1=rw\n@group2=rw"));
     }
 
-    function testAccumulateDefinedGroupsFromDeFaultGroupsSection() {
+    public function testAccumulateDefinedGroupsFromDeFaultGroupsSection()
+    {
         $saf = new SVNAccessFile();
         $this->assertEqual(array(), $saf->accumulateDefinedGroups(array(), '', true));
 
@@ -97,7 +108,8 @@ class SVNAccessFileTest extends TuleapTestCase {
         $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED, 'group2' => SVNAccessFile::UGROUP_DEFAULT), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_REDEFINED), 'group2 = user11, user22', true));
     }
 
-    function testAccumulateDefinedGroups() {
+    public function testAccumulateDefinedGroups()
+    {
         $saf = new SVNAccessFile();
         $this->assertEqual(array(), $saf->accumulateDefinedGroups(array(), ''));
 
@@ -107,14 +119,18 @@ class SVNAccessFileTest extends TuleapTestCase {
 
         $this->assertEqual(array(), $saf->accumulateDefinedGroups(array(), '[/]'));
 
-        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array(), 'Group1 = user1, user2', false));
+        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array(), 'group1 = user1, user2', false));
+        $this->assertNotEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array(), 'Group1 = user1, user2', false));
 
-        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'Group1 = user1, user2', false));
+        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'group1 = user1, user2', false));
+        $this->assertNotEqual(array('group1' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'Group1 = user1, user2', false));
 
-        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_DEFAULT, 'group2' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'Group2 = user1, user2', false));
+        $this->assertEqual(array('group1' => SVNAccessFile::UGROUP_DEFAULT, 'group2' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'group2 = user1, user2', false));
+        $this->assertNotEqual(array('group1' => SVNAccessFile::UGROUP_DEFAULT, 'group2' => SVNAccessFile::UGROUP_REDEFINED), $saf->accumulateDefinedGroups(array('group1' => SVNAccessFile::UGROUP_DEFAULT), 'Group2 = user1, user2', false));
     }
 
-    function testGetCurrentSection() {
+    public function testGetCurrentSection()
+    {
         $saf = new SVNAccessFile();
         $this->assertEqual(-1, $saf->getCurrentSection('', -1));
         $this->assertEqual(-1, $saf->getCurrentSection('blah', -1));
@@ -126,13 +142,14 @@ class SVNAccessFileTest extends TuleapTestCase {
         $this->assertEqual(-1, $saf->getCurrentSection('Group1 = user1, user2', -1));
     }
 
-    function testSvnAccessFileShouldCallsvn_utils_read_svn_access_file_defaultsWithCaseSensitiveRepositoryName() {
+    public function testSvnAccessFileShouldCallsvn_utils_read_svn_access_file_defaultsWithCaseSensitiveRepositoryName()
+    {
         $project = aMockProject()->build();
         stub($project)->getSVNRootPath()->returns('/svnroot/mytestproject');
-        
+
         $saf = partial_mock('SVNAccessFile', array('getPlatformBlock'));
         expect($saf)->getPlatformBlock('/svnroot/mytestproject')->once();
-        
+
         $saf->parseGroupLines($project, '');
     }
 }
