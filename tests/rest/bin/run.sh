4b39285ae3b8a3645a65012db8260d51adc996c3
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-08-18 11:21:26 +0200

    request #10575 OSX users should be able to run rest tests locally
    There are two issues:
    
    * the uid / gid not correctly retrieved (due to docker for mac)
    * the folders rest and REST and the fact that the OSX file system is case insensitive
    
    Change-Id: Id1a0978f6875956ff6d6b17d2fbeaff873470bd5

--- tests/rest/bin/run.sh
+++ tests/rest/bin/run.sh
@@ -6,6 +6,11 @@ setup_runner_account() {
     USER_ID=$(stat -c '%u' /usr/share/tuleap)
     GROUP_ID=$(stat -c '%g' /usr/share/tuleap)
 
+    if [[ "$USER_ID" -eq 0 && "$GROUP_ID" -eq 0 ]]; then
+        USER_ID=1000
+        GROUP_ID=1000
+    fi
+
     groupadd -g $GROUP_ID runner
     useradd -u $USER_ID -g $GROUP_ID runner
     echo "runner soft nproc unlimited" >> /etc/security/limits.d/90-nproc.conf
