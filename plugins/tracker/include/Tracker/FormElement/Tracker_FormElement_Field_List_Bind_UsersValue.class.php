Enalean/tuleap

commit 1df47b2d193bac323df78269253c7e37c33c136d

request #10615: Add tooltip back to dashboards
Since new dashboards, the tooltip on cross references are not anymore
displayed.

To fix this ASAP, the proposal is to bring back the old mechanism as is.
This has major drawbacks:
* Tooltip look 'n feel is ugly.
* Loading of tooltips in recent ES7 code is dirty

However this allows to remove a regression and decrease the frustration
of users in a minimal amount of time, instead of investigating about a
rewrite of our Tooltip library (get rid of jQuery, use dedicated
foundation like popper.js, …).

Change-Id: Ibc25e0001a80733c9086c5030e7cc1bc0684470a

plugins/tracker/www/scripts/TrackerArtifact.js
@@ -369,5 +369,3 @@ document.observe('dom:loaded', function () {
     window.onbeforeunload = codendi.tracker.artifact.editor.warnOnPageLeave;
 
 });
-
-codendi.Tooltip.selectors.push('a[class=direct-link-to-artifact]');

plugins/agiledashboard/include/AgileDashboard/KanbanJavascriptDependenciesProvider.php
@@ -36,8 +36,6 @@ class KanbanJavascriptDependenciesProvider
             array('file' => $kanban_include_assets->getFileURL('angular.js'), 'unique-name' => 'angular'),
             array('snippet' => 'window.CKEDITOR_BASEPATH = "' . $ckeditor_path . '";'),
             array('file' => $ckeditor_path . 'ckeditor.js'),
-            array('file' => '/scripts/codendi/Tooltip.js'),
-            array('file' => '/scripts/codendi/Tooltip-loader.js'),
             array('file' => $kanban_include_assets->getFileURL('kanban.js')),
         );
     }

plugins/agiledashboard/include/agiledashboardPlugin.class.php
@@ -492,6 +492,8 @@ class AgileDashboardPlugin extends Plugin {
     {
         if ($this->isKanbanURL()) {
             $provider = new KanbanJavascriptDependenciesProvider();
+            $params['javascript_files'][] = '/scripts/codendi/Tooltip.js';
+            $params['javascript_files'][] = '/scripts/codendi/Tooltip-loader.js';
             foreach ($provider->getDependencies() as $javascript) {
                 if (isset($javascript['snippet'])) {
                     $GLOBALS['HTML']->includeFooterJavascriptSnippet($javascript['snippet']);

src/common/Dashboard/JavascriptFilesIncluder.php
@@ -40,6 +40,8 @@ class JavascriptFilesIncluder
      */
     public function includeJavascriptFiles(array $dashboards_presenter)
     {
+        $GLOBALS['Response']->includeFooterJavascriptFile('/scripts/codendi/Tooltip.js');
+        $GLOBALS['Response']->includeFooterJavascriptFile('/scripts/codendi/Tooltip-loader.js');
         $GLOBALS['Response']->includeFooterJavascriptFile($this->include_assets->getFileURL('dashboard.js'));
         $this->includeJavascriptFilesNeededByWidgets($dashboards_presenter);
     }

src/www/scripts/codendi/Tooltip.js
@@ -116,7 +116,7 @@ var codendi = codendi || { };
         }
     };
 
-    codendi.Tooltip.selectors = ['a.cross-reference'];
+    codendi.Tooltip.selectors = ['a.cross-reference', 'a[class^=direct-link-to]'];
 
     codendi.Tooltip.load = function (element, at_cursor_position) {
         var sparkline_hrefs = {};

src/www/scripts/dashboards/dashboard-async-widget.js
@@ -32,6 +32,7 @@ function init() {
             .then(function (html) {
                 widget.innerHTML = sanitize(html);
                 widget.classList.remove('dashboard-widget-asynchronous-loading');
+                window.codendi.Tooltip.load(widget);
                 togglerInit(widget);
             });
     });

src/www/scripts/dashboards/widgets/project-heartbeat/project-heartbeat.js
@@ -51,6 +51,7 @@ function displayActivities(widget_content, entries) {
     const rendered_activities = render(template, getGroupedEntries(widget_content, entries));
     insertRenderedActivitiesInDOM(rendered_activities, activities);
 
+    window.codendi.Tooltip.load(activities);
     activities.classList.add('shown');
 }
 

tests/simpletest/common/Dashboard/JavascriptFilesIncluderTest.php
@@ -44,9 +44,12 @@ class JavascriptFilesIncluderTest extends TuleapTestCase
         );
     }
 
-    public function itAlwaysIncludesDashboardJs()
+    public function itAlwaysIncludesTooltipAndDashboardJs()
     {
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->once();
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile()->count(3);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('/scripts/codendi/Tooltip.js')->at(0);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('/scripts/codendi/Tooltip-loader.js')->at(1);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->at(2);
 
         $this->includer->includeJavascriptFiles(array());
     }
@@ -85,24 +88,24 @@ class JavascriptFilesIncluderTest extends TuleapTestCase
     {
         $dashboard = $this->getDashboardWithWidgets(array('first_widget', 'second_widget'));
 
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile()->count(5);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile()->count(7);
         expect($GLOBALS['Response'])->includeFooterJavascriptSnippet()->count(1);
         //first widget
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->at(0);
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_one')->at(1);
-        expect($GLOBALS['Response'])->includeFooterJavascriptSnippet('dependency_two')->at(0);
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_three')->at(2);
-        // second widget
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->at(2);
         expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_one')->at(3);
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_four')->at(4);
+        expect($GLOBALS['Response'])->includeFooterJavascriptSnippet('dependency_two')->at(0);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_three')->at(4);
+        // second widget
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_one')->at(5);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dependency_four')->at(6);
 
         $this->includer->includeJavascriptFiles(array($dashboard));
     }
 
     private function expectDependenciesScriptsWillNOTBeIncluded()
     {
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile()->count(1);
-        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->at(0);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile()->count(3);
+        expect($GLOBALS['Response'])->includeFooterJavascriptFile('dashboard.js')->at(2);
 
         expect($GLOBALS['Response'])->includeFooterJavascriptSnippet()->never();
     }

commit 327b672f9f66ca704989c965a8cd9e89a2875f43

request #10627: Whole tracker report page is displayed in the tooltip when a tracker name is hover on the trackers homepage
Issue has been introduced by commit 1df47b2d193bac323df78269253c7e37c33c136d

Change-Id: I1d357779dd9e57cd0d9c1938d0d9569eff84688b

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_UsersValue.class.php
@@ -59,7 +59,7 @@ class Tracker_FormElement_Field_List_Bind_UsersValue extends Tracker_FormElement
     protected function getLink() {
         $display_name = $this->getLabel();
 
-        return '<a class="direct-link-to-user" href="'. $this->getUserUrl() .'">'.
+        return '<a class="link-to-user" href="'. $this->getUserUrl() .'">'.
                $this->hp->purify($display_name, CODENDI_PURIFIER_CONVERT_HTML) .
                '</a>';
     }

plugins/tracker/include/Tracker/TrackerManager.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012-2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 use Tuleap\Admin\AdminPageRenderer;
@@ -707,7 +708,7 @@ class TrackerManager implements Tracker_IFetchTrackerSwitcher {
                         }
                     }
                     $html .= '<div class="tracker_homepage_info">';
-                    $html .= '<a class="direct-link-to-tracker" href="'.TRACKER_BASE_URL.'/?tracker='. $tracker->id .'">';
+                    $html .= '<a class="link-to-tracker" href="'.TRACKER_BASE_URL.'/?tracker='. $tracker->id .'">';
                     $html .= '<i class="icon-circle tracker_color_info '. $tracker->getColor() .'"></i>';
                     $html .= $hp->purify($tracker->name, CODENDI_PURIFIER_CONVERT_HTML);
                     $html .= '</a>';

plugins/tracker/www/scripts/TrackerArtifact.js
@@ -1,6 +1,6 @@
 /**
 * Copyright (c) Xerox Corporation, Codendi Team, 2001-2008. All rights reserved
-* Copyright (c) Enalean, 2016. All Rights Reserved.
+* Copyright (c) Enalean, 2016-2017. All Rights Reserved.
 *
 * Originally written by Nicolas Terray, 2008
 *
@@ -59,7 +59,7 @@ document.observe('dom:loaded', function () {
             new codendi.Tooltip(
                 div.up()
                    .previous()
-                   .down('a.direct-link-to-tracker'),
+                   .down('a.link-to-tracker'),
                 '')
            .createTooltip(
                div.remove()

plugins/tracker/www/scripts/TrackerArtifactLink.js
@@ -1,23 +1,23 @@
 /*
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2008. All rights reserved
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-2017. All Rights Reserved.
  *
  * Originally written by Nicolas Terray, 2008
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
+ * along with Tuleap; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *
@@ -579,7 +579,7 @@ document.observe('dom:loaded', function () {
         //links to artifacts load in a new browser tab/window
         $$('#tracker-link-artifact-slow-way-content a.cross-reference',
             '#tracker-link-artifact-slow-way-content a.direct-link-to-artifact',
-            '#tracker-link-artifact-slow-way-content a.direct-link-to-user').each(function (a) {
+            '#tracker-link-artifact-slow-way-content a.link-to-user').each(function (a) {
             a.target = '_blank';
             a.rel = 'noreferrer';
         });
@@ -874,7 +874,7 @@ document.observe('dom:loaded', function () {
                     //links to artifacts load in a new browser tab/window
                     $$('#tracker-link-artifact-fast-ways a.cross-reference',
                         '#tracker-link-artifact-fast-ways a.direct-link-to-artifact',
-                        '#tracker-link-artifact-fast-ways a.direct-link-to-user').each(function (a) {
+                        '#tracker-link-artifact-fast-ways a.link-to-user').each(function (a) {
                         a.target = '_blank';
                         a.rel = 'noreferrer';
                     });

plugins/tracker/www/themes/default/css/style.scss
@@ -1226,7 +1226,7 @@ ul.tracker-form-element-checkbox {
     font-size: 1.2em;
 }
 
-.tracker_homepage_info > .direct-link-to-tracker > .tracker_color_info {
+.tracker_homepage_info > .link-to-tracker > .tracker_color_info {
     display: inline-block;
     margin-right: 5px;
     vertical-align: middle;