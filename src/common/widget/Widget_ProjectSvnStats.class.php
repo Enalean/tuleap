Enalean/tuleap

commit ce7c8b06706f84d868c953c4cea990916f833097

Layout class is deprecated
This is a part of story #10353 display a kanban as a widget

BurningParrotTheme should not extend Layout class. This is a preliminary
step to display the toolbar in burning parrot.  This forces us to move
the color management for charts in a dedicated object.

Change-Id: Ia2cc1e5056f5f68d312b2c98f558a145217c7483

src/common/widget/Widget_ProjectSvnStats.class.php
@@ -54,17 +54,20 @@ class Widget_ProjectSvnStats extends Widget
             '&name[' . $this->id . ']=' . $this->getInstanceId() . '" />
                 </div>';
     }
-    
+
     protected $tmp_nb_of_commit;
-    public function process($owner_type, $owner_id) {
+    public function process($owner_type, $owner_id)
+    {
         $dao = new SvnCommitsDao(CodendiDataAccess::instance());
+        $colors_for_charts = new ColorsForCharts();
+
         //The default duration is 3 months back
         $nb_weeks = 4 * 3;
         $duration = 7 * $nb_weeks;
-        
+
         $day = 24 * 3600;
         $week = 7 * $day;
-        
+
         //compute the stats
         $stats = array();
         $nb_of_commits = array();
@@ -76,10 +79,10 @@ class Widget_ProjectSvnStats extends Widget
         if (count($stats)) {
             //sort the results
             uksort($stats, array($this, 'sortByTop'));
-            
+
             $today           = $_SERVER['REQUEST_TIME'];
             $start_of_period = strtotime("-$nb_weeks weeks");
-            
+
             //fill-in the holes
             $tmp_stats = array();
             foreach($stats as $whoid => $stat) {
@@ -90,14 +93,14 @@ class Widget_ProjectSvnStats extends Widget
                 }
                 $stats[$whoid]['by_week'] = $tmp_stats;
             }
-            
-            
+
+
             //fill-in the labels
             $dates = array();
             for($i = $start_of_period ; $i <= $today ; $i += $week) {
                 $dates[] = date('M d', $i);
             }
-            
+
             $nb_commiters    = count($stats);
             $widgetFormatter = new Widget_ProjectSvnStats_Layout($nb_commiters);
 
@@ -120,7 +123,7 @@ class Widget_ProjectSvnStats extends Widget
                 $c->legend->setColumns(2);
             }
 
-            $colors = array_reverse(array_slice($GLOBALS['HTML']->getChartColors(), 0, $nb_commiters));
+            $colors = array_reverse(array_slice($colors_for_charts->getChartColors(), 0, $nb_commiters));
             $nb_colors = count($colors);
             $bars = array();
             $i = 0;
@@ -136,7 +139,7 @@ class Widget_ProjectSvnStats extends Widget
                 }
                 $bars[] = $l;
             }
-            
+
             $gbplot = new AccBarPlot($bars);
             $c->Add($gbplot);
         } else {
@@ -145,7 +148,7 @@ class Widget_ProjectSvnStats extends Widget
         }
         echo $c->stroke();
     }
-    
+
     protected function sortByTop($a, $b) {
         return strnatcasecmp($this->tmp_nb_of_commit[$a], $this->tmp_nb_of_commit[$b]);
     }

plugins/IM/include/IMViews.class.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Copyright (c) Enalean, 2011 - 2017. All Rights Reserved.
+ * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Tuleap\Chart\ColorsForCharts;
+
 require_once('pre.php');
 require_once('common/mvc/Views.class.php');
 require_once('common/include/HTTPRequest.class.php');
@@ -222,8 +244,10 @@ class IMViews extends Views {
             $purifier = Codendi_HTMLPurifier::instance();
             $uh = new UserHelper();
 
+            $colors_for_charts = new ColorsForCharts();
+
             $nick_color_arr = array();  // association array nickname => color
-            $available_colors = $GLOBALS['HTML']->getTextColors();
+            $available_colors = $colors_for_charts->getTextColors();
 
             echo '<table class="logs">';
             echo ' <tr>';
@@ -252,10 +276,10 @@ class IMViews extends Views {
                 if ( ! array_key_exists($conv->getNickname(), $nick_color_arr)) {
                     // if all the colors have been used, we start again with the same colors
                     if (sizeof($available_colors) == 0) {
-                        $available_colors = $GLOBALS['HTML']->getChartColors();
+                        $available_colors = $colors_for_charts->getChartColors();
                     }
                     $current_color = array_pop($available_colors);  // remove a color from the array, and set it to current color
-                    $nick_color_arr[$conv->getNickname()] = $GLOBALS['HTML']->getColorCodeFromColorName($current_color);
+                    $nick_color_arr[$conv->getNickname()] = $colors_for_charts->getColorCodeFromColorName($current_color);
                 }
 
                 echo ' <tr class="'.get_class($conv).'">';

plugins/git/include/Git_LastPushesGraph.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013-2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2013 - 2017. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2012. All Rights Reserved.
  *
  * This file is a part of Tuleap.
@@ -18,6 +18,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
+use Tuleap\Chart\ColorsForCharts;
 
 require_once 'common/chart/Chart.class.php';
 
@@ -61,7 +63,7 @@ class Git_LastPushesGraph {
      * @var Array
      */
     protected $year    = array();
-    
+
     /**
      * Constructor.
      *
@@ -85,7 +87,7 @@ class Git_LastPushesGraph {
         }
         $this->displayChart = false;
         $this->weeksNumber  = min($weeksNumber, self::MAX_WEEKSNUMBER);
-        // Init some class properties according to 'weeks number' parameter         
+        // Init some class properties according to 'weeks number' parameter
         $today              = $_SERVER['REQUEST_TIME'];
         $startPeriod        = strtotime("-$this->weeksNumber weeks");
         $weekInSeconds      = self::WEEKS_IN_SECONDS ;
@@ -158,8 +160,9 @@ class Git_LastPushesGraph {
      * @return BarPlot
      */
     private function displayRepositoryPushesByWeek() {
+        $colors_for_charts = new ColorsForCharts();
         $nbRepo   = count($this->repoList);
-        $colors   = array_slice($GLOBALS['HTML']->getChartColors(), 0, $nbRepo);
+        $colors   = array_slice($colors_for_charts->getChartColors(), 0, $nbRepo);
         $nbColors = count($colors);
         $i        = 0;
         $bplot    = array();
@@ -201,12 +204,12 @@ class Git_LastPushesGraph {
                 }
             }
             $pushes = array_pad($pushes, $this->weeksNumber, 0);
-        }  
-        return $pushes;      
+        }
+        return $pushes;
     }
-    
+
     /**
-     * Create a JpGraph accumulated barPlot chart 
+     * Create a JpGraph accumulated barPlot chart
      *
      * @param Array $bplot Array of JpGraph barPlot objects
      * @param Chart $graph The output graph that will contains accumulated barPlots

plugins/statistics/include/Statistics_DiskUsageHtml.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) STMicroelectronics, 2009. All Rights Reserved.
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  *
  * Originally written by Manuel Vacelet, 2009
  *
@@ -20,6 +20,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
+use Tuleap\Chart\ColorsForCharts;
 
 require_once 'Statistics_DiskUsageOutput.class.php';
 require_once 'ProjectQuotaManager.class.php';
@@ -68,6 +70,8 @@ class Statistics_DiskUsageHtml extends Statistics_DiskUsageOutput {
         if ($res) {
             $services = $this->_dum->getProjectServices();
 
+            $colors_for_charts = new ColorsForCharts();
+
             $titles = array('Service', 'Start size', 'End size', 'Size evolution', 'Rate evolution');
 
             echo html_build_list_table_top($titles);
@@ -79,7 +83,7 @@ class Statistics_DiskUsageHtml extends Statistics_DiskUsageOutput {
                 echo '<tr class="'. util_get_alt_row_color($i++) .'">';
                 echo '<td>';
                 if ($colored) {
-                    $color = $GLOBALS['HTML']->getColorCodeFromColorName($this->_dum->getServiceColor($row['service']));
+                    $color = $colors_for_charts->getColorCodeFromColorName($this->_dum->getServiceColor($row['service']));
                     $color = $this->applyColorModifier($color.':1.5');
                     echo '<span class="plugin_statistics_table_legend" style="background-color:'.$color.';">&nbsp;</span>';
                 }

src/common/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoload9598b6f46b74709635e7479e1c0598f1($class) {
+function autoload116a1f7b1f934295bd09c031a3821c94($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -612,6 +612,7 @@ function autoload9598b6f46b74709635e7479e1c0598f1($class) {
             'tuleap\\admin\\systemevents\\homepagepanesectionpresenter' => '/admin/SystemEventsPaneSectionPresenter.php',
             'tuleap\\backend\\fileextensionfilteriterator' => '/backend/FileExtensionFilterIterator.php',
             'tuleap\\burningparrotcompatiblepagedetector' => '/include/BurningParrotCompatiblePageDetector.php',
+            'tuleap\\chart\\colorsforcharts' => '/chart/ColorsForCharts.php',
             'tuleap\\cvs\\diskusage\\collector' => '/cvs/DiskUsage/Collector.php',
             'tuleap\\cvs\\diskusage\\fullhistorydao' => '/cvs/DiskUsage/FullHistoryDao.php',
             'tuleap\\cvs\\diskusage\\retriever' => '/cvs/DiskUsage/Retriever.php',
@@ -1143,5 +1144,5 @@ function autoload9598b6f46b74709635e7479e1c0598f1($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoload9598b6f46b74709635e7479e1c0598f1');
+spl_autoload_register('autoload116a1f7b1f934295bd09c031a3821c94');
 // @codeCoverageIgnoreEnd

src/common/chart/Chart.class.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Codendi.
@@ -18,44 +19,53 @@
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Chart\ColorsForCharts;
+
 require_once('jpgraph.php');
 require_once('jpgraph_gantt.php');
 require_once('jpgraph_line.php');
 require_once('jpgraph_bar.php');
-require_once('jpgraph_date.php'); 
+require_once('jpgraph_date.php');
 
 /**
 * Chart
-* 
+*
 * Facade for jpgraph Graph
-* 
+*
 * @see jpgraph documentation for usage
 */
-class Chart {
-    
+class Chart
+{
+    /**
+     * @var ColorsForCharts
+     */
+    protected $colors_for_charts;
+
     private $width  = null;
     private $height = null;
-    
+
     protected $jpgraph_instance;
-    
+
     /**
     * Constructor
-    * 
+    *
     * @param int    $aWidth      Default is 600
     * @param int    $aHeight     Default is 400
     * @param string $aCachedName Default is ""
     * @param int    $aTimeOut    Default is 0
     * @param bool   $aInline     Default is true
-    * 
+    *
     * @return void
     */
     public function __construct($aWidth = 600, $aHeight = 400, $aCachedName = "", $aTimeOut = 0, $aInline = true) {
         $this->width  = $aWidth;
         $this->height = $aHeight;
-        
+
+        $this->colors_for_charts = new ColorsForCharts();
+
         $classname = $this->getGraphClass();
         $this->jpgraph_instance = new $classname($aWidth,$aHeight,$aCachedName,$aTimeOut,$aInline);
-        $this->jpgraph_instance->SetMarginColor($GLOBALS['HTML']->getChartBackgroundColor());
+        $this->jpgraph_instance->SetMarginColor($this->getChartBackgroundColor());
         $this->jpgraph_instance->SetFrame(true, $this->getMainColor(), 0);
         if ($aWidth && $aHeight) {
             $this->jpgraph_instance->img->SetAntiAliasing();
@@ -72,22 +82,22 @@ class Chart {
         $this->jpgraph_instance->legend->SetShadow(false);
         $this->jpgraph_instance->legend->SetColor($this->getMainColor());
         $this->jpgraph_instance->legend->SetFrameWeight(0);
-        $this->jpgraph_instance->legend->SetFillColor($GLOBALS['HTML']->getChartBackgroundColor());
+        $this->jpgraph_instance->legend->SetFillColor($this->getChartBackgroundColor());
         $this->jpgraph_instance->legend->SetFont($this->getFont(), FS_NORMAL, 8);
         $this->jpgraph_instance->legend->SetVColMargin(5);
         $this->jpgraph_instance->legend->SetPos(0.05, 0.5, 'right', 'center');
         $this->jpgraph_instance->legend->SetLineSpacing(10);
-        
+
         $this->jpgraph_instance->title->SetFont($this->getFont(), FS_BOLD, 12);
         $this->jpgraph_instance->title->SetColor($this->getMainColor());
         $this->jpgraph_instance->title->SetMargin(15);
-        
+
         $this->jpgraph_instance->subtitle->SetFont($this->getFont(), FS_NORMAL, 9);
         $this->jpgraph_instance->subtitle->SetColor($this->getMainColor());
         $this->jpgraph_instance->subtitle->SetAlign('left', 'top', 'left');
         $this->jpgraph_instance->subtitle->SetMargin(20);
     }
-    
+
     /**
      * Get the name of the jpgraph class to instantiate
      *
@@ -96,7 +106,7 @@ class Chart {
     protected function getGraphClass() {
         return 'Graph';
     }
-    
+
     /**
      * Use magic method to retrieve property of a jpgraph instance
      * /!\ Do not call it directly
@@ -108,7 +118,7 @@ class Chart {
     public function __get($name) {
         return $this->jpgraph_instance->$name;
     }
-    
+
     /**
      * Use magic method to set property of a jpgraph instance
      * /!\ Do not call it directly
@@ -121,7 +131,7 @@ class Chart {
     public function __set($name, $value) {
         return $this->jpgraph_instance->$name = $value;
     }
-    
+
     /**
      * Use magic method to know if a property of a jpgraph instance exists
      * /!\ Do not call it directly
@@ -133,7 +143,7 @@ class Chart {
     public function __isset($name) {
         return isset($this->jpgraph_instance->$name);
     }
-    
+
     /**
      * Use magic method to unset a property of a jpgraph instance
      * /!\ Do not call it directly
@@ -145,7 +155,7 @@ class Chart {
     public function __unset($name) {
         unset($this->jpgraph_instance->$name);
     }
-    
+
     /**
      * Use magic method to call a method of a jpgraph instance
      * /!\ Do not call it directly
@@ -170,7 +180,7 @@ class Chart {
             $this->jpgraph_instance->xaxis->SetFont($this->getFont(), FS_NORMAL, 8);
             $this->jpgraph_instance->xaxis->SetLabelAngle(45);
             $this->jpgraph_instance->xaxis->title->SetFont($this->getFont(), FS_BOLD, 8);
-            
+
             $this->jpgraph_instance->yaxis->SetColor($this->getMainColor(), $this->getMainColor());
             $this->jpgraph_instance->yaxis->SetFont($this->getFont(), FS_NORMAL, 8);
             $this->jpgraph_instance->xaxis->title->SetFont($this->getFont(), FS_BOLD, 8);
@@ -178,7 +188,7 @@ class Chart {
         }
         return $result;
     }
-    
+
     /**
      * Return the font used by the chart
      *
@@ -187,7 +197,7 @@ class Chart {
     public function getFont() {
         return FF_USERFONT;
     }
-    
+
     /**
      * Return the main color used by the chart (axis, text, ...)
      *
@@ -195,19 +205,24 @@ class Chart {
      * @see Layout->getChartMainColor
      */
     public function getMainColor() {
-        return $GLOBALS['HTML']->getChartMainColor();
+        return $this->colors_for_charts->getChartMainColor();
     }
-    
+
     /**
      * Return the colors used by the chart to draw data (part of pies, bars, ...)
      *
-     * @return string
+     * @return array
      * @see Layout->getChartColors
      */
     public function getThemedColors() {
-        return $GLOBALS['HTML']->getChartColors();
+        return $this->colors_for_charts->getChartColors();
     }
-    
+
+    private function getChartBackgroundColor()
+    {
+        return $this->colors_for_charts->getChartBackgroundColor();
+    }
+
     /**
      * Compute the height of the top margin
      *
@@ -218,7 +233,7 @@ class Chart {
     }
 
     /**
-     * Diplay a given message as png image 
+     * Diplay a given message as png image
      *
      * @param String $msg Message to display
      *
@@ -250,4 +265,3 @@ class Chart {
         }
     }
 }
-?>

src/common/chart/Chart_Gantt.class.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2013 - 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Codendi.
@@ -23,51 +24,51 @@ require_once($GLOBALS['jpgraph_dir'].'/jpgraph_gantt.php');
 
 /**
 * PieChart
-* 
+*
 * Facade for jpgraph GanttGraph
-* 
+*
 * @see jpgraph documentation for usage
 */
 class Chart_Gantt extends Chart{
 
     /**
     * Constructor
-    * 
+    *
     * @param int    $aWidth      Default is 0
     * @param int    $aHeight     Default is 0
     * @param string $aCachedName Default is ""
     * @param int    $aTimeOut    Default is 0
     * @param bool   $aInline     Default is true
-    * 
+    *
     * @return void
     */
     public function __construct($aWidth = 0, $aHeight = 0, $aCachedName = "", $aTimeOut = 0, $aInline = true) {
         parent::__construct($aWidth, $aHeight, $aCachedName, $aTimeOut, $aInline);
-        
-        $header_color = $GLOBALS['HTML']->getGanttHeaderColor();
-        
+
+        $header_color = $this->colors_for_charts->getGanttHeaderColor();
+
         $this->scale->year->grid->SetColor($this->getMainColor());
         $this->scale->year->grid->Show(true);
         $this->scale->year->SetBackgroundColor($header_color);
         $this->scale->year->SetFont($this->getFont(), FS_NORMAL, 8);
-        
+
         $this->scale->month->grid->SetColor($this->getMainColor());
         $this->scale->month->grid->Show(true);
         $this->scale->month->SetBackgroundColor($header_color);
         $this->scale->month->SetFont($this->getFont(), FS_NORMAL, 8);
-        
+
         $this->scale->week->grid->SetColor($this->getMainColor());
         $this->scale->week->SetFont($this->getFont(), FS_NORMAL, 8);
-        
+
         $this->scale->day->grid->SetColor($this->getMainColor());
         $this->scale->day->SetFont($this->getFont(), FS_NORMAL, 6);
-        
+
         $this->scale->actinfo->SetBackgroundColor($header_color);
         $this->scale->actinfo->SetFont($this->getFont(), FS_NORMAL, 8);
-        
+
         $this->scale->actinfo->vgrid->SetColor($header_color);
     }
-    
+
     /**
      * Get the name of the jpgraph class to instantiate
      *
@@ -76,7 +77,7 @@ class Chart_Gantt extends Chart{
     protected function getGraphClass() {
         return 'GanttGraph';
     }
-    
+
     /**
      * Return the color used to draw a gantt bar when the task is late
      *
@@ -84,9 +85,9 @@ class Chart_Gantt extends Chart{
      * @see Layout->getGanttLateBarColor
      */
     public function getLateBarColor() {
-        return $GLOBALS['HTML']->getGanttLateBarColor();
+        return $this->colors_for_charts->getGanttLateBarColor();
     }
-    
+
     /**
      * Return the color used to draw a gantt bar when there is an error (mainly in dates)
      *
@@ -94,9 +95,9 @@ class Chart_Gantt extends Chart{
      * @see Layout->getGanttErrorBarColor
      */
     public function getErrorBarColor() {
-        return $GLOBALS['HTML']->getGanttErrorBarColor();
+        return $this->colors_for_charts->getGanttErrorBarColor();
     }
-    
+
     /**
      * Return the color used to draw a gantt bar when the task is green
      *
@@ -104,9 +105,9 @@ class Chart_Gantt extends Chart{
      * @see Layout->getGanttGreenBarColor
      */
     public function getGreenBarColor() {
-        return $GLOBALS['HTML']->getGanttGreenBarColor();
+        return $this->colors_for_charts->getGanttGreenBarColor();
     }
-    
+
     /**
      * Return the color used to draw the "today" vertical line
      *
@@ -114,8 +115,8 @@ class Chart_Gantt extends Chart{
      * @see Layout->getGanttTodayLineColor
      */
     public function getTodayLineColor() {
-        return $GLOBALS['HTML']->getGanttTodayLineColor();
+        return $this->colors_for_charts->getGanttTodayLineColor();
     }
-    
+
 }
 ?>

src/common/chart/Chart_GanttBar.class.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Codendi.
@@ -18,43 +19,47 @@
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Chart\ColorsForCharts;
+
 /**
 * Chart_GanttBar
-* 
+*
 * Facade for jpgraph GanttBar
-* 
+*
 * @see jpgraph documentation for usage
 */
 class Chart_GanttBar {
-    
+
     protected $jpgraph_instance;
-    
+
     /**
     * Constructor
-    * 
+    *
     * @param int    $aPos          Vertical position (row)
     * @param string $aLabel        Text label
     * @param int    $aStart        Start date
     * @param int    $aEnd          End date
     * @param string $aCaption      Caption string for bar. Default is ""
     * @param float  $aHeightFactor Height factor. Default is 0.6
-    * 
+    *
     * @return void
     */
     public function __construct($aPos, $aLabel, $aStart, $aEnd, $aCaption="", $aHeightFactor=0.6) {
         $this->jpgraph_instance = new GanttBar($aPos, $aLabel, $aStart, $aEnd, $aCaption, $aHeightFactor);
-        $color      = $GLOBALS['HTML']->getGanttBarColor();
+
+        $colors_for_charts = new ColorsForCharts();
+
+        $color      = $colors_for_charts->getGanttBarColor();
         $color_dark = $color .':0.65';
         $this->jpgraph_instance->progress->SetPattern(BAND_SOLID, $color_dark);
         $this->jpgraph_instance->setColor($color_dark);
         $this->jpgraph_instance->setPattern(GANTT_SOLID, $color);
-        $this->jpgraph_instance->title->setColor($GLOBALS['HTML']->getChartMainColor());
+        $this->jpgraph_instance->title->setColor($colors_for_charts->getChartMainColor());
         $this->jpgraph_instance->title->setFont($this->getFont(), FS_NORMAL, 8);
-        $this->jpgraph_instance->caption->setColor($GLOBALS['HTML']->getChartMainColor());
+        $this->jpgraph_instance->caption->setColor($colors_for_charts->getChartMainColor());
         $this->jpgraph_instance->caption->setFont($this->getFont(), FS_NORMAL, 7);
-                        
     }
-    
+
     /**
      * Return the font used by the bar
      *
@@ -63,7 +68,7 @@ class Chart_GanttBar {
     public function getFont() {
         return FF_USERFONT;
     }
-    
+
     /**
      * Use magic method to retrieve property of a jpgraph instance
      * /!\ Do not call it directly
@@ -75,7 +80,7 @@ class Chart_GanttBar {
     public function __get($name) {
         return $this->jpgraph_instance->$name;
     }
-    
+
     /**
      * Use magic method to set property of a jpgraph instance
      * /!\ Do not call it directly
@@ -88,7 +93,7 @@ class Chart_GanttBar {
     public function __set($name, $value) {
         return $this->jpgraph_instance->$name = $value;
     }
-    
+
     /**
      * Use magic method to know if a property of a jpgraph instance exists
      * /!\ Do not call it directly
@@ -100,7 +105,7 @@ class Chart_GanttBar {
     public function __isset($name) {
         return isset($this->jpgraph_instance->$name);
     }
-    
+
     /**
      * Use magic method to unset a property of a jpgraph instance
      * /!\ Do not call it directly
@@ -112,7 +117,7 @@ class Chart_GanttBar {
     public function __unset($name) {
         unset($this->jpgraph_instance->$name);
     }
-    
+
     /**
      * Use magic method to call a method of a jpgraph instance
      * /!\ Do not call it directly
@@ -126,20 +131,19 @@ class Chart_GanttBar {
         $result = call_user_func_array(array($this->jpgraph_instance, $method), $args);
         return $result;
     }
-    
+
     /**
      * Set CSIM target and alt for the gantt bar
-     * 
+     *
      * @param string $link the target
      * @param string $alt  the alt of the target
      *
      * @return void
      */
     public function setCSIM($link, $alt) {
-        $this->jpgraph_instance->SetCSIMTarget($link); 
+        $this->jpgraph_instance->SetCSIMTarget($link);
         $this->jpgraph_instance->SetCSIMAlt($alt);
-        $this->jpgraph_instance->title->SetCSIMTarget(array($link, $link)); 
+        $this->jpgraph_instance->title->SetCSIMTarget(array($link, $link));
         $this->jpgraph_instance->title->SetCSIMAlt(array($alt));
     }
 }
-?>

commit 45490d13da23309eb0532243ed0d05af758e1df5

request #10643: Project SVN statistics widget throws a fatal error
The issue has been introduced by commit ce7c8b06706f84d868c953c4cea990916f833097

Change-Id: Ie728092165acaff5eab577c1c3cfe44964a7ac3a

src/common/widget/Widget_ProjectSvnStats.class.php
@@ -21,6 +21,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Chart\ColorsForCharts;
 use Tuleap\Dashboard\Project\ProjectDashboardController;
 
 require_once('common/dao/SvnCommitsDao.class.php');