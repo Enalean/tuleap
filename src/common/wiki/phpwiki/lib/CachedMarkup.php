Enalean/tuleap

commit ad28ecb3920cee73b426847b0c2ea960763d4f26

request #10628: Detection and transformation to <a> tag of URLs in text can lead to XSS
This contribution also removes the linkification in text of the specific
subdomain www as it is very specific to the www subdomain and we can not
determine the proper URI scheme to use. Always using the http:// scheme
is not really future-proof as httpsis becoming the default.
This means if we find in a text "www.example.com" is will not be transformed
anymore to '<a href="http://www.example.com">http://www.example.com</a>'.
If the URL scheme is specified the transformation still happen, e.g.
a text with "https://www.example.com" is still transformed to
'<a href="https://www.example.com">https://www.example.com</a>'.

The target _blank as also be removed from the generated links as it is
considered a bad practice to modify the default browser behavior expected
by the user.

HTMLPurifier does not know the SSH scheme so to be able to accept
SSH URIs we need to define the SSH scheme in a way HTMLPurifier
is capable to deal with and to register the scheme we have defined.

Change-Id: I7424f672979d50e492b605be0f9a70c5da45b9f3

src/common/wiki/phpwiki/lib/CachedMarkup.php
@@ -123,7 +123,8 @@ class CacheableMarkup extends XmlContent {
 		$this->_append($subitem);
 	}
 	elseif (!is_object($item)) {
-	    $this->_buf .= util_make_links($this->_quote((string) $item), GROUP_ID);
+	    $purifier   = Codendi_HTMLPurifier::instance();
+	    $this->_buf = $purifier->purify((string) $item, CODENDI_PURIFIER_BASIC_NOBR, GROUP_ID);
 	}
 	elseif (isa($item, 'Cached_DynamicContent')) {
 	    if ($this->_buf) {

src/common/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoloadbfb1030336409e84ecd5c5a3bdb97bfc($class) {
+function autoload14576d70cdd0da5a8e624990eb14076a($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -699,6 +699,7 @@ function autoloadbfb1030336409e84ecd5c5a3bdb97bfc($class) {
             'tuleap\\glyph\\glyph' => '/Glyph/Glyph.php',
             'tuleap\\glyph\\glyphfinder' => '/Glyph/GlyphFinder.php',
             'tuleap\\glyph\\glyphnotfoundexception' => '/Glyph/GlyphNotFoundException.php',
+            'tuleap\\htmlpurifiersshscheme' => '/include/HTMLPurifierSSHScheme.php',
             'tuleap\\httpd\\postrotateevent' => '/Httpd/PostRotateEvent.php',
             'tuleap\\instrument\\collect' => '/Instrument/Collect.php',
             'tuleap\\instrument\\noopstatsd' => '/Instrument/NoopStatsd.php',
@@ -1169,5 +1170,5 @@ function autoloadbfb1030336409e84ecd5c5a3bdb97bfc($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoloadbfb1030336409e84ecd5c5a3bdb97bfc');
+spl_autoload_register('autoload14576d70cdd0da5a8e624990eb14076a');
 // @codeCoverageIgnoreEnd

src/common/include/Codendi_HTMLPurifier.class.php
@@ -39,6 +39,16 @@
 require_once('common/constants.php');
 
 class Codendi_HTMLPurifier {
+    private static $allowed_schemes = array(
+        'http'   => true,
+        'https'  => true,
+        'mailto' => true,
+        'ftp'    => true,
+        'nntp'   => true,
+        'news'   => true,
+        'tel'    => true
+    );
+
     /**
      * Hold an instance of the class
      */
@@ -79,6 +89,7 @@ class Codendi_HTMLPurifier {
         $config = HTMLPurifier_Config::createDefault();
         $this->setConfigAttribute($config, 'Core', 'Encoding', 'UTF-8');
         $this->setConfigAttribute($config, 'Cache', 'SerializerPath', $GLOBALS['codendi_cache_dir']);
+        $this->setConfigAttribute($config, 'URI', 'AllowedSchemes', self::$allowed_schemes);
         return $config;
     }
 
@@ -91,6 +102,10 @@ class Codendi_HTMLPurifier {
         $config = $this->getCodendiConfig();
         $this->setConfigAttribute($config, 'HTML', 'Allowed', $this->getLightConfigMarkups());
         $this->setConfigAttribute($config, 'AutoFormat', 'Linkify', true);
+        HTMLPurifier_URISchemeRegistry::instance()->register('ssh', new \Tuleap\HTMLPurifierSSHScheme());
+        $allowed_schemes        = self::$allowed_schemes;
+        $allowed_schemes['ssh'] = true;
+        $this->setConfigAttribute($config, 'URI', 'AllowedSchemes', $allowed_schemes);
         return $config;
     }
 
@@ -147,15 +162,10 @@ class Codendi_HTMLPurifier {
     }
 
     /**
-     * Transform links and emails from text to html links
-     *
-     * @param String $data
-     *
-     * @return String
+     * @return string
      */
-    function makeLinks($data = '', $group_id = 0) {
-        if(empty($data)) { return $data; }
-
+    private function linkifyMails($data)
+    {
         // john.doe@yahoo.com => <a href="mailto:...">...</a>
         $mailto_pattern = '
           (?<=\W|^)  # email must be at the beginning of the string or be preceded by a non word
@@ -163,27 +173,23 @@ class Codendi_HTMLPurifier {
           (
             ([a-z0-9_]|\-|\.)+@([^[:space:]<&>]*)([[:alnum:]-])   # really basic email pattern
           )';
-        $data = preg_replace("`$mailto_pattern`ix", "<a href=\"mailto:\\1\" target=\"_blank\" rel=\"noreferrer\">\\1</a>", $data);
+        return preg_replace("`$mailto_pattern`ix", "<a href=\"mailto:\\1\">\\1</a>", $data);
+    }
 
-        // www.yahoo.com => http://www.yahoo.com
-        $data = preg_replace("/([ \t\n])www\./i","\\1http://www.",$data);
-
-        // http://www.yahoo.com => <a href="...">...</a>
-
+    /**
+     * @return string
+     */
+    private function dealWithSpecialCasesWithFramingURLCharacters($data)
+    {
         // Special case for urls between brackets or double quotes
-        // e.g. <http://www.google.com> or "http://www.google.com"
+        // e.g. <https://www.example.com> or "https://www.example.com"
         // In some places (e.g. tracker follow-ups) the text is already encoded, so the brackets are replaced by &lt; and &gt; See SR #652.
         $url_pattern = '([[:alnum:]]+)://([^[:space:]<]*)([[:alnum:]#?/&=])';
         $matching    = '\1://\2\3';
+
         $data = preg_replace("`$url_pattern&quot;`i", "$matching\"", $data);
         $data = preg_replace("`$url_pattern&#039;`i", "$matching'",  $data);
-        $data = preg_replace("`$url_pattern&gt;`i",   "$matching>",  $data);
-        // Now, replace
-        $data = preg_replace("`$url_pattern`i", "<a href=\"$matching\" target=\"_blank\" rel=\"noreferrer\">$matching</a>", $data);
-
-        $this->insertReferences($data, $group_id);
-
-        return $data;
+        return preg_replace("`$url_pattern&gt;`i",   "$matching>",  $data);
     }
 
     /**
@@ -241,12 +247,15 @@ class Codendi_HTMLPurifier {
             break;
 
         case CODENDI_PURIFIER_BASIC:
-            $clean = nl2br($this->makeLinks(htmlentities($html, ENT_QUOTES, 'UTF-8'), $groupId));
+            $data  = $this->linkifyMails(htmlentities($html, ENT_QUOTES, 'UTF-8'));
+            $data  = $this->dealWithSpecialCasesWithFramingURLCharacters($data);
+            $clean = $this->purify(nl2br($data), CODENDI_PURIFIER_LIGHT, $groupId);
             break;
         case CODENDI_PURIFIER_BASIC_NOBR:
-            $clean = $this->makeLinks(htmlentities($html, ENT_QUOTES, 'UTF-8'), $groupId);
+            $data  = $this->linkifyMails(htmlentities($html, ENT_QUOTES, 'UTF-8'));
+            $data  = $this->dealWithSpecialCasesWithFramingURLCharacters($data);
+            $clean = $this->purify($data, CODENDI_PURIFIER_LIGHT, $groupId);
             break;
-
         case CODENDI_PURIFIER_JS_QUOTE:
             $clean = $this->js_string_purifier($html, JSON_HEX_APOS);
             break;

src/common/include/HTMLPurifierSSHScheme.php
@@ -1,0 +1,34 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap;
+
+class HTMLPurifierSSHScheme extends \HTMLPurifier_URIScheme
+{
+    public $default_port = 22;
+    public $browsable = false;
+    public $hierarchical = true;
+    public $secure = true;
+
+    public function doValidate(&$uri, $config, $context)
+    {
+        return true;
+    }
+}

src/www/forum/forum.php
@@ -35,8 +35,9 @@ function forum_show_a_nested_message ($result,$row=0) {
 	  $g_id = db_result($gr,0,'group_id');
 	}
 
-	$poster = UserManager::instance()->getUserByUserName(db_result($result, $row, 'user_name'));
-	$ret_val = '
+	$purifier = Codendi_HTMLPurifier::instance();
+	$poster   = UserManager::instance()->getUserByUserName(db_result($result, $row, 'user_name'));
+	$ret_val  = '
 		<TABLE BORDER="0" WIDTH="100%">
 			<TR>                  
               <TD class="thread" NOWRAP>'.$Language->getText('forum_forum','by').': '.UserHelper::instance()->getLinkOnUser($poster).
@@ -50,7 +51,7 @@ function forum_show_a_nested_message ($result,$row=0) {
 			</TR>
 			<TR>
 				<TD>
-					'. util_make_links(nl2br(db_result($result,$row,'body')), $g_id ) .'
+					'. $purifier->purify(db_result($result,$row,'body'), CODENDI_PURIFIER_BASIC, $g_id) .'
 				</TD>
 			</TR>';
     

src/www/forum/forum_utils.php
@@ -246,11 +246,6 @@ function forum_create_forum($group_id,$forum_name,$is_public=1,$create_default_m
 	}
 }
 
-function make_links ($data="") {
-	//moved make links to /include/utils.php
-	util_make_links($data);
-}
-
 function get_forum_name($id) {
   global $Language;
 	/*

src/www/forum/message.php
@@ -73,12 +73,13 @@ if ($request->valid($vMsg)) {
 
 	echo html_build_list_table_top ($title_arr);
 
-	$poster = UserManager::instance()->getUserByUserName(db_result($result, 0, "user_name"));
+	$purifier = Codendi_HTMLPurifier::instance();
+	$poster   = UserManager::instance()->getUserByUserName(db_result($result, 0, "user_name"));
 	echo "<TR><TD class=\"threadmsg\">\n";
 	echo $Language->getText('forum_message','by').": ".UserHelper::instance()->getLinkOnUser($poster)."<BR>";
 	echo $Language->getText('forum_message','date').": ".format_date($GLOBALS['Language']->getText('system', 'datefmt'),db_result($result,0, "date"))."<BR>";
 	echo $Language->getText('forum_message','subject').": ". db_result($result,0, "subject")."<P>";
-	echo util_make_links(nl2br(db_result($result,0, 'body')), $group_id);
+    echo $purifier->purify(db_result($result,0, 'body'), CODENDI_PURIFIER_BASIC, $group_id);
 	echo "</TD></TR>";
 	
     $crossref_fact= new CrossReferenceFactory($msg_id, ReferenceManager::REFERENCE_NATURE_FORUMMESSAGE, $group_id);

src/www/include/utils.php
@@ -415,10 +415,12 @@ function util_line_wrap ($text, $wrap = 80, $break = "\n") {
 	return implode($break, $result);
 }
 
+/**
+ * @deprecated
+ */
 function util_make_links ($data='',$group_id = 0) {
-    // creation of links has moved to Codendi_HTMLPurifier::makeLinks()
     $hp = Codendi_HTMLPurifier::instance();
-    return $hp->makeLinks($data, $group_id);
+    return $hp->purify($data, CODENDI_PURIFIER_BASIC_NOBR, $group_id);
 }
 
 function util_make_reference_links ($data,$group_id) {

commit 000f21ec020df42214e80a762cf2e065bf5579e2

request #10647: Generation of PHPWiki pages is partial
Only the last section is cached and then displayed.

Issue has been introduced by commit ad28ecb3920cee73b426847b0c2ea960763d4f26.

Change-Id: Ib76ee4c319c95b1e2f0bb42be8cfad2f865dc1c5

src/common/wiki/phpwiki/lib/CachedMarkup.php
@@ -123,8 +123,8 @@ class CacheableMarkup extends XmlContent {
 		$this->_append($subitem);
 	}
 	elseif (!is_object($item)) {
-	    $purifier   = Codendi_HTMLPurifier::instance();
-	    $this->_buf = $purifier->purify((string) $item, CODENDI_PURIFIER_BASIC_NOBR, GROUP_ID);
+	    $purifier    = Codendi_HTMLPurifier::instance();
+	    $this->_buf .= $purifier->purify((string) $item, CODENDI_PURIFIER_BASIC_NOBR, GROUP_ID);
 	}
 	elseif (isa($item, 'Cached_DynamicContent')) {
 	    if ($this->_buf) {