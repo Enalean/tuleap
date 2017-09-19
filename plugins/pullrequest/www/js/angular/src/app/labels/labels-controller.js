0d558417a81f5b582c581f2306f9d0de3ad45411
Thomas Gorka <thomas.gorka@enalean.com>
2017-09-19 09:42:56 +0200

    Labels directive should have a dedicated controller
    How to test:
    - Labels should be visible from the pullrequest homepage and
      from the detailled view.
    
    Change-Id: Iaf2699e1f58d826da45ca6edfafa1fa3a1e8b2c4

---
+++ plugins/pullrequest/www/js/angular/src/app/labels/labels-controller.js
@@ -0,0 +1,45 @@
+angular
+    .module('tuleap.pull-request')
+    .controller("LabelsController", LabelsController);
+
+LabelsController.$inject = [
+    '$window',
+    '$element',
+    'SharedPropertiesService'
+];
+
+function LabelsController(
+    $window,
+    $element,
+    SharedPropertiesService
+) {
+    var self = this;
+
+    initLabels();
+
+    function initLabels() {
+        if (self.pullRequestId && self.projectId) {
+            createLabelsBox(self.pullRequestId, self.projectId, 0);
+            return;
+        }
+
+        SharedPropertiesService.whenReady().then(function() {
+            var pull_request = SharedPropertiesService.getPullRequest();
+
+            createLabelsBox(
+                pull_request.id,
+                pull_request.repository_dest.project.id,
+                pull_request.user_can_update_labels
+            );
+        });
+    }
+
+    function createLabelsBox(pull_request_id, project_id, user_can_update_labels) {
+        $window.LabelsBox.create(
+            $element[0],
+            '/api/v1/pull_requests/' + pull_request_id + '/labels',
+            '/api/v1/projects/' + project_id + '/labels',
+            user_can_update_labels
+        );
+    }
+}
