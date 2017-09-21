16611a6b620bd75c1f2aa1a0546597d214b047da
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-09-21 14:56:23 +0200

    Be able to display existing links in update release form
    this is part of story #9692 Have a field in the FRS where I can add a link
    
    In edit release form, we should be able to see existing links.
    It's not possible to edit or delete them.
    
    Edition will never be allowed, deletion will be done in a dedicated commit.
    
    Change-Id: Ia09ad51ef040a908571d2458fdd11c2e96b9ff31

---
+++ src/common/frs/UploadedLinkUpdateTablePresenter.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *  Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ *   This file is a part of Tuleap.
+ *
+ *   Tuleap is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tuleap is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\FRS;
+
+class UploadedLinkUpdateTablePresenter
+{
+    public $link_label;
+    public $name_label;
+    public $uploaded_links_label;
+
+    /**
+     * @var UploadedLinkPresenter[]
+     */
+    public $existing_links;
+
+    /**
+     * @param UploadedLinkPresenter[] $existing_links
+     */
+    public function __construct(array $existing_links)
+    {
+        $this->uploaded_links_label = _('Links');
+        $this->link_label           = _('Link');
+        $this->name_label           = _('Name');
+
+        $this->existing_links = $existing_links;
+    }
+}
