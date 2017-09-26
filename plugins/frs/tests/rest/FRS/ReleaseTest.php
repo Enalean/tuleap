79305244ff5d23252fdbe90bcc741ddc62af56ba
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-09-26 16:00:35 +0200

    Add REST tests on frs_release
    this is part of story #9692 Have a field in the FRS where I can add a link
    
    Change-Id: Ic255b22af179056fe08dab5687c27e1a5176ffc7

---
+++ plugins/frs/tests/rest/FRS/ReleaseTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\FRS\Tests\REST;
+
+require_once dirname(__FILE__).'/../bootstrap.php';
+
+use REST_TestDataBuilder;
+use RestBase;
+
+class ReleaseTest extends RestBase
+{
+    const PROJECT_NAME = 'frs-test';
+
+    private $project_id;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->project_id = $this->getProjectId(self::PROJECT_NAME);
+    }
+
+    protected function getResponse($request)
+    {
+        return $this->getResponseByToken(
+            $this->getTokenForUserName(REST_TestDataBuilder::TEST_USER_1_NAME),
+            $request
+        );
+    }
+
+    public function testGETPackage()
+    {
+        $response = $this->getResponse($this->client->get('frs_release/1'));
+        $release  = $response->json();
+
+        var_dump($release['links']);
+
+        $this->assertEquals($release['id'], 1);
+        $this->assertEquals($release['name'], 'release1');
+        $this->assertEquals($release['links'][0]["link"], 'http://example.fr');
+        $this->assertEquals($release['links'][0]["release_time"], '2015-12-08T16:55:00+01:00');
+    }
+}
