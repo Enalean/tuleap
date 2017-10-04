b632f4b21d2f4b443617e59847e3c24634c66b6a
Manuel VACELET <manuel.vacelet@enalean.com>
2017-10-04 14:56:13 +0200

    Remove database init dead code
    Part of request #10713 REST test should not depend on Tuleap internal API
    
    Change-Id: I9f1cff8dc18584ba4b5656b03bbbae08d2659960

--- tests/lib/DatabaseInitialisation.class.php
+++ tests/lib/DatabaseInitialisation.class.php
@@ -1,11 +1,12 @@
 <?php
 
-class DatabaseInitialization {
-
+class DatabaseInitialization
+{
     /** @var mysqli */
     protected $mysqli;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->loadConfiguration();
         $this->mysqli = mysqli_init();
         if (! $this->mysqli->real_connect($GLOBALS['sys_dbhost'], $GLOBALS['sys_dbuser'], $GLOBALS['sys_dbpasswd'])) {
@@ -13,55 +14,13 @@ class DatabaseInitialization {
         }
     }
 
-    public function setUp() {
-        $this->initDb();
-        $this->mysqli->select_db(ForgeConfig::get('sys_dbname'));
-    }
+    private function loadConfiguration()
+    {
+        ForgeConfig::loadFromFile('/etc/tuleap/conf/database.inc');
 
-    /**
-     * Execute all statements of given file (bulk imports)
-     *
-     * @param String $file
-     */
-    protected function mysqlLoadFile($file) {
-        echo "Load $file\n";
-        $mysql_cmd = 'mysql -h '.$GLOBALS['sys_dbhost'].' -u'.$GLOBALS['sys_dbuser'].' -p'.$GLOBALS['sys_dbpasswd'].' '.$GLOBALS['sys_dbname'];
-        $cmd = $mysql_cmd.' < '.$file;
-        system($cmd);
-    }
-
-    protected function loadConfiguration() {
-        if (is_file('/etc/tuleap/conf/database.inc')) {
-            ForgeConfig::loadFromFile('/etc/tuleap/conf/database.inc');
-        } else {
-            $config_file = 'tests.inc';
-            ForgeConfig::loadFromFile(dirname(__FILE__)."/../../src/etc/$config_file.dist");
-            ForgeConfig::loadFromFile(dirname($this->getLocalIncPath())."/$config_file");
-        }
         $GLOBALS['sys_dbhost']   = ForgeConfig::get('sys_dbhost');
         $GLOBALS['sys_dbuser']   = ForgeConfig::get('sys_dbuser');
         $GLOBALS['sys_dbpasswd'] = ForgeConfig::get('sys_dbpasswd');
         $GLOBALS['sys_dbname']   = ForgeConfig::get('sys_dbname');
-    }
-
-    protected function getLocalIncPath() {
-        return getenv('CODENDI_LOCAL_INC') ? getenv('CODENDI_LOCAL_INC') : '/etc/codendi/conf/local.inc';
-    }
-
-    protected function initDb() {
-        echo "Create database structure \n";
-
-        $this->forceCreateDatabase();
-        $this->mysqlLoadFile('src/db/mysql/database_structure.sql');
-        $this->mysqlLoadFile('src/db/mysql/database_initvalues.sql');
-        $this->mysqlLoadFile('plugins/tracker/db/install.sql');
-        $this->mysqlLoadFile('plugins/graphontrackersv5/db/install.sql');
-        $this->mysqlLoadFile('plugins/agiledashboard/db/install.sql');
-        $this->mysqlLoadFile('plugins/cardwall/db/install.sql');
-    }
-
-    protected function forceCreateDatabase() {
-        $this->mysqli->query("DROP DATABASE IF EXISTS ".$GLOBALS['sys_dbname']);
-        $this->mysqli->query("CREATE DATABASE ".$GLOBALS['sys_dbname']." CHARACTER SET utf8");
     }
 }
--- tests/lib/rest/init_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once dirname(__FILE__).'/../../../src/common/autoload.php';
-require_once dirname(__FILE__).'/../autoload.php';
-
-$db_init = new REST_DatabaseInitialization();
-$db_init->setUp();
