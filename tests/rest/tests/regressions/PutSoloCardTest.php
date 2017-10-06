97f00e97b9574501c5005f7e67eb3fc19f3b81ee
Manuel VACELET <manuel.vacelet@enalean.com>
2017-10-06 13:29:37 +0200

    REST test speed-up: PutSoloCardTest
    Creating stuff by REST is slow while it's fast with XML import
    
    Part of request #10713 REST test should not depend on Tuleap internal API
    
    Change-Id: Iab742f33c080a121a2fddcb210181986312b8c5c

--- tests/rest/tests/regressions/PutSoloCardTest.php
+++ tests/rest/tests/regressions/PutSoloCardTest.php
@@ -18,7 +18,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/
  */
 
-require_once dirname(__FILE__).'/../../lib/autoload.php';
+require_once __DIR__.'/../../lib/autoload.php';
 
 /**
  * PUT /cards/:id cannot update solo card
@@ -30,85 +30,26 @@ require_once dirname(__FILE__).'/../../lib/autoload.php';
  * @see https://tuleap.net/plugins/tracker/?aid=6430
  * @group Regressions
  */
-class Regressions_PutSoloCardTest extends RestBase {
+class Regressions_PutSoloCardTest extends RestBase
+{
+    public function testItEditSoloCardLabel()
+    {
+        $stories     = $this->getArtifactIdsIndexedByTitle('private-member', 'story');
+        $planning_id = $this->getSprintPlanningId();
 
-    /** @var Test_Rest_TrackerFactory */
-    private $tracker_test_helper;
-    private $project_id;
-    private $story;
-    private $task;
-    private $sprint;
-    private $planning_id;
-
-    public function testItEditSoloCardLabel() {
-        $put = json_encode(array(
-            "label"     => "Whatever",
-            "column_id" => null,
-            "values"    => array()
-        ));
-        try {
-        $response = $this->getResponse($this->client->put('cards/'.$this->planning_id.'_'.$this->story['id'], null, $put));
+        $put = json_encode(
+            array(
+                "label"     => "Whatever",
+                "column_id" => null,
+                "values"    => array()
+            )
+        );
+        $response = $this->getResponse($this->client->put('cards/'.$planning_id.'_'.$stories['Story 1'], null, $put));
         $this->assertEquals($response->getStatusCode(), 200);
-        } catch (Guzzle\Http\Exception\BadResponseException $exception) {
-            echo $exception->getRequest();
-            echo $exception->getResponse()->getBody(true);
-            die(PHP_EOL);
-        }
     }
 
-    public function setUp() {
-        parent::setUp();
-        $this->tracker_test_helper = new Test\Rest\Tracker\TrackerFactory(
-            $this->client,
-            $this->rest_request,
-            $this->project_private_member_id,
-            REST_TestDataBuilder::TEST_USER_1_NAME
-        );
-
-        try {
-            $this->story       = $this->createStory('Story 1');
-            $this->task        = $this->createTask('Task 1');
-            $this->sprint      = $this->createSprint("Sprint 1");
-            $this->planning_id = $this->getSprintPlanningId();
-        } catch (Guzzle\Http\Exception\BadResponseException $exception) {
-            echo $exception->getRequest();
-            echo $exception->getResponse()->getBody(true);
-            die(PHP_EOL);
-        }
-    }
-
-    private function createStory($summary) {
-        $tracker = $this->tracker_test_helper->getTrackerRest('story');
-        return $tracker->createArtifact(
-            array(
-                $tracker->getSubmitTextValue('I want to', $summary),
-                $tracker->getSubmitListValue('Status', 'To be done'),
-            )
-        );
-    }
-
-    private function createTask($summary) {
-        $tracker = $this->tracker_test_helper->getTrackerRest('task');
-        return $tracker->createArtifact(
-            array(
-                $tracker->getSubmitTextValue('Summary', $summary),
-                $tracker->getSubmitListValue('Status', 'To be done'),
-            )
-        );
-    }
-
-
-    private function createSprint($name) {
-        $tracker = $this->tracker_test_helper->getTrackerRest('sprint');
-        return $tracker->createArtifact(
-            array(
-                $tracker->getSubmitTextValue('Name', $name),
-                $tracker->getSubmitListValue('Status', 'Current'),
-            )
-        );
-    }
-
-    private function getSprintPlanningId() {
+    private function getSprintPlanningId()
+        {
         $project_plannings = $this->getResponse($this->client->get("projects/$this->project_private_member_id/plannings"))->json();
         foreach ($project_plannings as $planning) {
             if ($planning['label'] == 'Sprint Planning') {
