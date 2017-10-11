c7321de62cb5b6cc51487c01ceffbe2eed153872
Nicolas Terray <nicolas.terray@enalean.com>
2017-10-11 14:45:18 +0200

    request #10739: Bad behavior of field dependency in tv3
    The request #10628 and request #10655 introduced a regression in field dependency behavior:
    
    Setup
    -----
    
    As tracker admin you should:
    
    * Define a select box as source field (ie S: source_field_1, source_field_2)
    * Define a multi select box as target field.(ie T: target_field_1 ,target_field_2,
      target_field_3)
    * Define a certain dependency between S and T:
      * source_field_1 => target_field_1, target_field_2
      * source_field_2 => target_field_1, target_field_3
    * Set the permission to the source field as ReadOnly for a given group (ie for
      project members)
    * Set the permission to the target field as update for the same group.
    
    Expected behaviour
    ------------------
    
    * As tracker admin, submit new artifact with value on source  S == source_field_1
    * As project member (not tracker admin nor project admin) try to update artifact.
      * The value of source field (source_field_1) will be displayed in readonly
      * You should have the choice in the target field T between just (target_field_1,
        target_field_2); target_field_3 should not be displayed in the target field.
    
    Change-Id: I239a35d9f81a41d2e499b873e30bb48ee4fe2601

--- src/www/tracker/include/ArtifactHtml.class.php
+++ src/www/tracker/include/ArtifactHtml.class.php
@@ -471,7 +471,7 @@ class ArtifactHtml extends Artifact {
                 } else if ($field->getName() == 'last_update_date') {
                 	$value = format_date($GLOBALS['Language']->getText('system', 'datefmt'),$field_value);    
                 } else {
-                    $value = $field_html->display($this->ArtifactType->getID(), $field_value, false, false, $read_only, false, false, 0, false, 0, false, 0, false, $this->ArtifactType->getGroupID());
+                    $value = $field_html->display($this->ArtifactType->getID(), $field_value, false, false, $read_only, false, false, 0, false, 0, false, 0, true, $this->ArtifactType->getGroupID());
                 }
                 
                 $html = array('label' => $label, 'value' => $value);
