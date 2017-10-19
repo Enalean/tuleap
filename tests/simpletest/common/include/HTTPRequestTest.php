e40d8020cdffacc32fe9adc541a89f9a545873ea
Manuel VACELET <manuel.vacelet@enalean.com>
2017-10-19 09:38:34 +0200

    request #10757 PHP notices when no user agent
    Change-Id: I98dd5d398b5aca1fc40ef140fbdaab542e9d8954

--- tests/simpletest/common/include/HTTPRequestTest.php
+++ tests/simpletest/common/include/HTTPRequestTest.php
@@ -368,6 +368,11 @@ class HTTPRequest_BrowserTests extends TuleapTestCase {
         parent::tearDown();
     }
 
+    public function testNoNoticesWhenNoUserAgent() {
+        unset($_SERVER['HTTP_USER_AGENT']);
+        $this->request->getBrowser();
+    }
+
     public function testIE9CompatibilityModeIsDeprected() {
         $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)';
         $browser = $this->request->getBrowser();
