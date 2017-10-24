c5c10f48184390a5a43b245194d27da38de5ae4d
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2017-10-24 17:14:55 +0200

    request #10770 Gitolite log parse can exceed mysql int limit
    Change-Id: I8a16322a9266210c1d39cdda19199900738928ce

---
+++ plugins/git/db/mysql/updates/2017/201710241704_update_git_file_logs_parse_to_bigint.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201710241704_update_git_file_logs_parse_to_bigint extends ForgeUpgrade_Bucket
+{
+    public function description()
+    {
+        return "change end_line of plugin_git_file_logs_parse from int to bigint";
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $sql = "ALTER TABLE plugin_git_file_logs_parse
+                  MODIFY end_line BIGINT UNSIGNED";
+
+        $res = $this->db->dbh->exec($sql);
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('An error occurred while altering the plugin_git_file_log_parse table.');
+        }
+    }
+}
