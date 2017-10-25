2cd92f6230a7b15e28bc68f01b8c093948d581aa
Juliana Leclaire <juliana.leclaire@enalean.com>
2017-10-25 16:31:30 +0200

    Query with != prevent @comment to be search with other things than a string (NOW/MYSELF/int/float/date/...)
    This is a part of story #9882 TQL in follow-up comments
    
    Change-Id: Ia2e0250f4bf3c163c0e896a8ee9860bcc723793c

--- plugins/tracker/include/Tracker/Report/Query/Advanced/InvalidMetadata/NotEqualComparisonChecker.php
+++ plugins/tracker/include/Tracker/Report/Query/Advanced/InvalidMetadata/NotEqualComparisonChecker.php
@@ -20,14 +20,91 @@
 
 namespace Tuleap\Tracker\Report\Query\Advanced\InvalidMetadata;
 
+use DateTime;
+use Tuleap\Tracker\Report\Query\Advanced\DateFormat;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\BetweenValueWrapper;
 use Tuleap\Tracker\Report\Query\Advanced\Grammar\Comparison;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\CurrentDateTimeValueWrapper;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\CurrentUserValueWrapper;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\InValueWrapper;
 use Tuleap\Tracker\Report\Query\Advanced\Grammar\Metadata;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\MetadataValueWrapperParameters;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\SimpleValueWrapper;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\ValueWrapperParameters;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\ValueWrapperVisitor;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\Integer\CommentToDateComparisonException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\Integer\CommentToFloatComparisonException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\Integer\CommentToIntComparisonException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\Integer\CommentToMySelfComparisonException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\Integer\CommentToNowComparisonException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\MySelfIsNotSupportedException;
+use Tuleap\Tracker\Report\Query\Advanced\InvalidFields\NowIsNotSupportedException;
 
-class NotEqualComparisonChecker implements ICheckMetadataForAComparison
+class NotEqualComparisonChecker implements ICheckMetadataForAComparison, ValueWrapperVisitor
 {
     const OPERATOR = '!=';
 
     public function checkMetaDataIsValid(Metadata $metadata, Comparison $comparison)
     {
+        try {
+            $value = $comparison->getValueWrapper()->accept($this, new MetadataValueWrapperParameters());
+        } catch (NowIsNotSupportedException $exception) {
+            throw new CommentToNowComparisonException();
+        } catch (MySelfIsNotSupportedException $exception) {
+            throw new CommentToMySelfComparisonException();
+        }
+
+        if (is_float($value + 0)) {
+            throw new CommentToFloatComparisonException($value);
+        }
+
+        if (is_numeric($value)) {
+            throw new CommentToIntComparisonException($value);
+        }
+
+        if ($this->getDateTimeFromValue($value) !== false && $value !== '') {
+            throw new CommentToDateComparisonException();
+        }
+    }
+
+    public function visitCurrentDateTimeValueWrapper(
+        CurrentDateTimeValueWrapper $value_wrapper,
+        ValueWrapperParameters $parameters
+    ) {
+        throw new NowIsNotSupportedException();
+    }
+
+    public function visitSimpleValueWrapper(SimpleValueWrapper $value_wrapper, ValueWrapperParameters $parameters)
+    {
+        return $value_wrapper->getValue();
+    }
+
+    public function visitBetweenValueWrapper(BetweenValueWrapper $value_wrapper, ValueWrapperParameters $parameters)
+    {
+    }
+
+    public function visitInValueWrapper(
+        InValueWrapper $collection_of_value_wrappers,
+        ValueWrapperParameters $parameters
+    ) {
+    }
+
+    public function visitCurrentUserValueWrapper(
+        CurrentUserValueWrapper $value_wrapper,
+        ValueWrapperParameters $parameters
+    ) {
+        throw new MySelfIsNotSupportedException();
+    }
+
+    private function getDateTimeFromValue($value)
+    {
+        $date_value = DateTime::createFromFormat(DateFormat::DATETIME, $value);
+        if ($date_value !== false) {
+            return $date_value;
+        }
+
+        $date_value = DateTime::createFromFormat(DateFormat::DATE, $value);
+
+        return $date_value;
     }
 }
