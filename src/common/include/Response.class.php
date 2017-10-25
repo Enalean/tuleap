82261539cb85d1247cf73618dea8d5d15e9e174d
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-10-25 13:46:55 +0200

    Display only one feedback on field validation error
    This is part of story #10736 administrate artifact links type at project level
    
    This behaviour has been introduced by a6fceea74d7f1c1ab94e7ca1088bf89d78627fba
    
    On field validation, errors are printed twice in the error box.
    
    To fix this, we let the exception print all the messages in the Web UI
    and we clear them from the global feedback.
    
    Change-Id: Ife6d6167d41055954efada8f1575116cd868cb61

--- src/common/include/Response.class.php
+++ src/common/include/Response.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Tuleap.
@@ -18,10 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
-
-require_once('common/include/Feedback.class.php');
-require_once('common/dao/FeedbackDao.class.php');
-require_once('common/include/CookieManager.class.php');
 
 /**
  * Response
@@ -116,6 +112,11 @@ class Response {
 
     public function clearFeedback() {
         $this->_feedback = new Feedback();
+    }
+
+    public function clearFeedbackErrors()
+    {
+        return $this->_feedback->clearErrors();
     }
 
     private function getFeedbackDao() {
