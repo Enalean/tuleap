b529e0f5e4dfc229fcc6373c7d381fcf14556aac
Nicolas Terray <nicolas.terray@enalean.com>
2017-10-31 11:52:55 +0100

    Rework project hierarchy pane UX
    Part of story #10061 new project admin homepage and navigation
    
    After UXD review, here are the required changes:
    - Description paragraph should come first
    - There should be only one paragraph, no bold
    - There should not be a checkbox to remove the parent
    - The parent project should be displayed as a property
    - Once there is a parent, to change it we must remove/add
    - On remove, there is a confirmation modal
    
    Change-Id: I2dbfbbda417a776a812f89b9011b8afe425b1c83

--- src/common/project/Admin/ProjectDetails/ProjectHierarchyPresenter.php
+++ src/common/project/Admin/ProjectDetails/ProjectHierarchyPresenter.php
@@ -20,21 +20,22 @@
 
 namespace Tuleap\Project\Admin\ProjectDetails;
 
-use Codendi_HTMLPurifier;
-
 class ProjectHierarchyPresenter
 {
     public $parent_project_info;
     public $delete_button;
     public $purified_project_children;
+    public $parent_project_label;
     public $child_projects_label;
     public $add_parent_button;
-    public $update_parent_button;
     public $empty_children_label;
     public $project_name_label;
     public $project_hierarchy_title;
-    public $purified_project_hierarchy_desc;
+    public $project_hierarchy_desc;
     public $is_hierarchy_shown;
+    public $delete_modal_title;
+    public $delete_modal_desc;
+    public $cancel_button;
 
     public function __construct(
         array $parent_project_info,
@@ -48,18 +49,17 @@ class ProjectHierarchyPresenter
         $this->project_name_label      = _('Project name');
         $this->empty_children_label    = _('No children projects');
         $this->delete_button           = _('Delete');
+        $this->delete_modal_title      = _('Delete parent');
+        $this->delete_modal_desc       = _('Wow, wait a minute. You are about to remove the parent. Please confirm your action.');
+        $this->cancel_button           = _('Cancel');
+        $this->parent_project_label    = _('Parent project');
         $this->child_projects_label    = _('Child projects');
         $this->add_parent_button       = _('Add parent project');
-        $this->update_parent_button    = _('Update parent project');
         $this->project_hierarchy_title = _('Project hierarchy');
-
-        $this->purified_project_hierarchy_desc = Codendi_HTMLPurifier::instance()->purify(
-            _(
-                "<p>This feature aims to provide a way to structure projects organizations from permissions, user groups and control point of view.</p>
-                <p>As of today, it's only used by Git/ Gerrit for umbrella projects (if relevant to your platform/ project).</p>
-                <p><strong>However, setting a hierarchy between projects might have future impacts (e.g. members of the parent project could possibly have access to all sub-projects even without explicit permissions granted).</strong></p>"
-            ),
-            CODENDI_PURIFIER_LIGHT
-        );
+        $this->project_hierarchy_desc  = _("This feature aims to provide a way to structure projects organizations from
+            permissions, user groups and control point of view. As of today, it's only used by Git/Gerrit for umbrella
+            projects (if relevant to your platform/project). However, setting a hierarchy between projects might have
+            future impacts (e.g. members of the parent project could possibly have access to all sub-projects even
+            without explicit permissions granted).");
     }
 }
