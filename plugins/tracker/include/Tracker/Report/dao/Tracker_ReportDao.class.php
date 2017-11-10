48dce15862758abd643854b919b1fe64a3a76230
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-11-10 15:34:32 +0100

    request #10815 Each access to a report log information not needed
    Each time a user access to a report, some old and unused debug information
    are added into /var/log/tuleap/debug.log
    
    These are not needed, so we remove this (crappy) logging.
    
    Change-Id: I1c0995e76163cdbf12188b8edf45efbf9a11e2cc

--- plugins/tracker/include/Tracker/Report/dao/Tracker_ReportDao.class.php
+++ plugins/tracker/include/Tracker/Report/dao/Tracker_ReportDao.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 class Tracker_ReportDao extends DataAccessObject {
@@ -424,24 +425,4 @@ class Tracker_ReportDao extends DataAccessObject {
         return in_array($ugroupId, $dynamic_ugroups) &&
                in_array($ugroupId, $allowed_groups);
     }
-    
-    
-    // {{{ Those logging methods are here only for debugging purpose, to have 
-    //     metrics about execution time of the search requests.
-    //     Would be nice to have a real logging strategy in Tuleap.
-    private $time_start;
-    public function logStart($method, $query) {
-        $this->log($method, "searching for ". $query);
-        $this->time_start = microtime(1);
-    }
-    public function logEnd($method, $nb_matching) {
-        $time_taken = microtime(1) - $this->time_start;
-        $this->log($method, "\tNb matching: $nb_matching");
-        $this->log($method, "\tTime taken: $time_taken seconds");
-    }
-    private function log($method, $message) {
-        error_log(date('c') ." $method -> $message\n", 3, $GLOBALS['codendi_log'] .'/debug.log');
-    }
-    // }}}
 }
-?>
