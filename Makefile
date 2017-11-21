Enalean/tuleap

commit fd81f655a04dcb2f5d3225ac9a9a402db224a513

request #10820 Tuleap development environment should leverage docker-compose >2
How to test: stop and start your development environment, everything should
work seamlessly. You will probably need to upgrade docker-compose as v2 format
is available since docker-compose 1.9

Change-Id: Ib28ffd2c5d8e68a94df72a08871b4542dcd24d71

Makefile
@@ -172,20 +172,13 @@ deploy-githooks:
 # Start development enviromnent with Docker Compose
 #
 
+dev-setup: .env deploy-githooks ## Setup environment for Docker Compose (should only be run once)
+
 .env:
 	@echo "MYSQL_ROOT_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" > .env
 	@echo "LDAP_ROOT_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" >> .env
 	@echo "LDAP_MANAGER_PASSWORD=`env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 32`" >> .env
 	@echo VIRTUAL_HOST=tuleap-web.tuleap-aio-dev.docker >> .env
-
-dev-setup: .env deploy-githooks ## Setup environment for Docker Compose (should only be run once)
-	@echo "Create all data containers"
-	@$(DOCKER) inspect tuleap_ldap_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_ldap_data -v /data busybox true
-	@$(DOCKER) inspect tuleap_db_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_db_data -v /var/lib/mysql busybox true
-	@$(DOCKER) inspect tuleap_es_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_es_data -v /data busybox true
-	@$(DOCKER) inspect tuleap_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_data -v /data busybox true
-	@$(DOCKER) inspect tuleap_reverseproxy_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_reverseproxy_data -v /reverseproxy_data busybox true
-	@$(DOCKER) inspect tuleap_gerrit_data > /dev/null 2>&1 || $(DOCKER) run -t --name=tuleap_gerrit_data -v /home/gerrit busybox true
 
 show-passwords: ## Display passwords generated for Docker Compose environment
 	@$(DOCKER) run --rm --volumes-from tuleap_data busybox cat /data/root/.tuleap_passwd
@@ -196,30 +189,28 @@ dev-forgeupgrade: ## Run forgeupgrade in Docker Compose environment
 dev-clear-cache: ## Clear caches in Docker Compose environment
 	@$(DOCKER) exec tuleap-web /usr/share/tuleap/src/utils/tuleap --clear-caches
 
-start-dns: ## Start dnsdock to be able to reach Docker Compose environment without having to touch /etc/hosts file
-	@$(DOCKER) stop dnsdock || true
-	@$(DOCKER) rm dnsdock || true
-	@$(DOCKER) run -d -v /var/run/docker.sock:/var/run/docker.sock --name dnsdock -p 172.17.42.1:53:53/udp tonistiigi/dnsdock
-
-start-rp:
-	@echo "Start reverse proxy"
-	@$(DOCKER_COMPOSE) up -d rp
-
 start: ## Start Tuleap Web + LDAP + DB in Docker Compose environment
 	@echo "Start Tuleap Web + LDAP + DB"
+	@./tools/docker/migrate_to_volume.sh
 	@$(DOCKER_COMPOSE) up -d web
 	@echo -n "Your instance will be soon available: http://"
 	@grep VIRTUAL_HOST .env | cut -d= -f2
 	@echo "You might want to type 'make show-passwords' to see site default passwords"
+	@echo -n "tuleap-web ip address: "
+	@$(DOCKER) inspect -f '{{.NetworkSettings.Networks.tuleap_default.IPAddress}}' tuleap-web
 
-start-php56: ## Start Tuleap web with php56 & nginx18 support - EXPERIMENTAL
+start-php56: ## Start Tuleap web with php56 & nginx
 	@echo "Start Tuleap in PHP 5.6"
+	@./tools/docker/migrate_to_volume.sh
 	@$(DOCKER_COMPOSE) -f docker-compose-php56.yml up -d web
+	@echo -n "tuleap-web ip address: "
+	@$(DOCKER) inspect -f '{{.NetworkSettings.Networks.tuleap_default.IPAddress}}' tuleap-web
 
 start-distlp:
 	@echo "Start Tuleap with reverse-proxy, backend web and backend svn"
+	@./tools/docker/migrate_to_volume.sh
 	-@$(DOCKER_COMPOSE) stop
-	@$(SUDO) docker-compose -f docker-compose-distlp.yml up -d reverse-proxy
+	@$(DOCKER_COMPOSE) -f docker-compose-distlp.yml up -d reverse-proxy
 	@ip=`$(DOCKER) inspect -f '{{.NetworkSettings.Networks.tuleap_default.IPAddress}}' tuleap_reverse-proxy_1`; \
 		echo "Add '$$ip tuleap-web.tuleap-aio-dev.docker' to /etc/hosts"; \
 		echo "Ensure $$ip is configured as sys_trusted_proxies in /etc/tuleap/conf/local.inc"
@@ -230,9 +221,6 @@ start-distlp:
 
 stop-distlp:
 	@$(SUDO) docker-compose -f docker-compose-distlp.yml stop
-
-start-es:
-	@$(DOCKER_COMPOSE) up -d es
 
 env-gerrit: .env
 	@grep --quiet GERRIT_SERVER_NAME .env || echo 'GERRIT_SERVER_NAME=tuleap-gerrit.gerrit-tuleap.docker' >> .env

docker-compose-distlp.yml
@@ -4,10 +4,9 @@ services:
   reverse-proxy:
     image: enalean/tuleap-reverseproxy:distlp-1.1
     command: /tuleap/tools/distlp/reverse-proxy/run.sh
-    volumes_from:
-          - container:tuleap_data # Needed for configuration (platform name)
     volumes:
       - .:/tuleap
+      - tuleap-data:/data # Needed for configuration (platform name)
     depends_on:
       - backend-web
       - backend-svn
@@ -19,10 +18,9 @@ services:
     volumes:
       - .:/usr/share/tuleap
       - ../tuleap-plugins:/usr/share/tuleap-plugins
+      - tuleap-data:/data
     env_file:
       - .env
-    volumes_from:
-      - container:tuleap_data
     depends_on:
       - ldap
       - db
@@ -31,30 +29,15 @@ services:
   backend-svn:
     image: enalean/tuleap-dev:1.5
     command: /usr/share/tuleap/tools/distlp/backend-svn/backend-svn.sh
-    volumes_from:
-      - container:tuleap_data
     volumes:
       - .:/usr/share/tuleap
+      - tuleap-data:/data
     env_file:
       - .env
     depends_on:
       - ldap
       - db
       - rabbitmq
-
-  ldap:
-    image: enalean/ldap
-    env_file:
-      - .env
-    volumes_from:
-      - container:tuleap_ldap_data
-
-  db:
-    image: mysql:5.5
-    env_file:
-      - .env
-    volumes_from:
-      - container:tuleap_db_data
 
   rabbitmq:
     image: rabbitmq:3.6

docker-compose-mac.yml
@@ -1,7 +1,10 @@
-web:
-  environment:
-    - DO_NOT_LAUNCH_FORGEUPGRADE=true
-  ports:
-   - "80:80"
-   - "443:443"
-   - "2222:22"
+version: "2"
+
+services:
+  web:
+    environment:
+      - DO_NOT_LAUNCH_FORGEUPGRADE=true
+    ports:
+     - "80:80"
+     - "443:443"
+     - "2222:22"

docker-compose-php56.yml
@@ -1,2 +1,5 @@
-web:
-  image: enalean/tuleap-aio-dev:c6-php56-nginx
+version: "2"
+
+services:
+  web:
+    image: enalean/tuleap-aio-dev:c6-php56-nginx

docker-compose.yml
@@ -1,57 +1,53 @@
-rp:
-  image: enalean/tuleap-reverseproxy
-  links:
-   - web:tuleap-web
-  volumes_from:
-   - tuleap_reverseproxy_data
+version: "2"
 
-web:
-  container_name: tuleap-web
-  image: enalean/tuleap-aio-dev:nodb
-  links:
-   - ldap:ldap
-   - db:db
-  volumes:
-   - .:/usr/share/tuleap
-   - ../tuleap-plugins:/usr/share/tuleap-plugins
-  env_file:
-   - .env
-  volumes_from:
-   - tuleap_data
+services:
+  web:
+    container_name: tuleap-web
+    image: enalean/tuleap-aio-dev:nodb
+    volumes:
+    - .:/usr/share/tuleap
+    - ../tuleap-plugins:/usr/share/tuleap-plugins
+    - tuleap-data:/data
+    env_file:
+    - .env
+    depends_on:
+    - ldap
+    - db
 
-ldap:
-  image: enalean/ldap
-  env_file:
-   - .env
-  volumes_from:
-   - tuleap_ldap_data
+  ldap:
+    image: enalean/ldap
+    env_file:
+    - .env
+    volumes:
+    - ldap-data:/data
 
-es:
-  image: enalean/elasticsearch-tuleap
-  volumes_from:
-   - tuleap_es_data
-  command: -Des.script.disable_dynamic=false
+  gerrit:
+    container_name: tuleap-gerrit
+    image: enalean/gerrit-tuleap:2.12
+    volumes:
+    - gerrit-data:/home/gerrit
+    env_file:
+    - .env
+    depends_on:
+    - ldap
+    - db
 
-gerrit:
-  container_name: tuleap-gerrit
-  image: enalean/gerrit-tuleap:2.12
-  volumes_from:
-   - tuleap_gerrit_data
-  env_file:
-   - .env
-  links:
-   - ldap:ldap
-   - web:tuleap
+  db:
+    image: mysql:5.5
+    env_file:
+    - .env
+    volumes:
+    - db-data:/var/lib/mysql
 
-db:
-  image: mysql:5.5
-  env_file:
-   - .env
-  volumes_from:
-   - tuleap_db_data
+  graphite:
+    container_name: tuleap-graphite
+    image: hopsoft/graphite-statsd
+    volumes:
+    - graphite-data:/opt/graphite/storage
 
-graphite:
-  container_name: tuleap-graphite
-  image: hopsoft/graphite-statsd
-  volumes:
-   - tuleap-graphite-graphite-storage:/opt/graphite/storage
+volumes:
+  tuleap-data:
+  ldap-data:
+  gerrit-data:
+  db-data:
+  graphite-data:

tools/docker/migrate_to_volume.sh
@@ -1,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+COMPOSE_PROJECT_NAME="$(basename $(pwd))"
+
+if docker inspect tuleap_data > /dev/null 2>&1; then
+    if ! docker run --rm -v tuleap_db-data:/stuff centos:6 test -d /stuff/mysql; then
+        echo "Migrate legacy data containers to volumes"
+        echo "Copy DB"
+        docker run --rm -ti --volumes-from tuleap_db_data -v "${COMPOSE_PROJECT_NAME}_db-data":/copy/mysql centos:6 cp -ar /var/lib/mysql /copy
+        echo "Copy LDAP"
+        docker run --rm -ti --volumes-from tuleap_ldap_data -v "${COMPOSE_PROJECT_NAME}_ldap-data":/copy/data centos:6 cp -ar /data /copy
+        echo "Copy Gerrit"
+        docker run --rm -ti --volumes-from tuleap_gerrit_data -v "${COMPOSE_PROJECT_NAME}_gerrit-data":/copy/gerrit centos:6 cp -ar /home/gerrit /copy
+        echo "Copy Tuleap"
+        docker run --rm -ti --volumes-from tuleap_data -v "${COMPOSE_PROJECT_NAME}_tuleap-data":/copy/data centos:6 cp -ar /data /copy
+    fi
+fi

commit c35638200d9afbbf6ccbcb0b7ac095e8d957e79c

request #10834: Unable to display passwords of the dev environnement
Issue has been introduced by fd81f655a04dcb2f5d3225ac9a9a402db224a513

Change-Id: I6614e5ea8f14ab32cf40f25db9bcedf17f5a99b3

Makefile
@@ -181,7 +181,7 @@ dev-setup: .env deploy-githooks ## Setup environment for Docker Compose (should 
 	@echo VIRTUAL_HOST=tuleap-web.tuleap-aio-dev.docker >> .env
 
 show-passwords: ## Display passwords generated for Docker Compose environment
-	@$(DOCKER) run --rm --volumes-from tuleap_data busybox cat /data/root/.tuleap_passwd
+	@$(DOCKER_COMPOSE) exec web cat /data/root/.tuleap_passwd
 
 dev-forgeupgrade: ## Run forgeupgrade in Docker Compose environment
 	@$(DOCKER) exec tuleap-web /usr/lib/forgeupgrade/bin/forgeupgrade --config=/etc/tuleap/forgeupgrade/config.ini update