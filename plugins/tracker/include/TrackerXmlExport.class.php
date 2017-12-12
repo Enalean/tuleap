Enalean/tuleap

commit e828d421eda9761b5ba26f20e7bfc108158e4801

XML export deals with disabled type in project
This is part of story #10736 administrate artifact links type at project level

Now, the natures tag contains all the editable types in the project.
In addition, the disabled types have the attribute is_used set to 0.

Change-Id: Iff4e180a7290fb9619d45ae2e80a835a637930d3

plugins/tracker/include/TrackerXmlExport.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,7 +19,9 @@
  */
 
 use Tuleap\Project\XML\Export\ArchiveInterface;
-use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureDao;
+use Tuleap\Tracker\Admin\ArtifactLinksUsageDao;
+use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenter;
+use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenterFactory;
 
 class TrackerXmlExport
 {
@@ -49,8 +51,13 @@ class TrackerXmlExport
     /** @var EventManager */
     private $event_manager;
 
-    /** @var  NatureDao */
-    private $nature_dao;
+    /** @var  NaturePresenterFactory */
+    private $nature_presenter_factory;
+
+    /**
+     * @var ArtifactLinksUsageDao
+     */
+    private $artifact_links_usage_dao;
 
     public function __construct(
         TrackerFactory $tracker_factory,
@@ -59,19 +66,21 @@ class TrackerXmlExport
         Tracker_Artifact_XMLExport $artifact_xml_export,
         UserXMLExporter $user_xml_exporter,
         EventManager $event_manager,
-        NatureDao $nature_dao
+        NaturePresenterFactory $nature_presenter_factory,
+        ArtifactLinksUsageDao $artifact_links_usage_dao
     ) {
-        $this->tracker_factory         = $tracker_factory;
-        $this->trigger_rules_manager   = $trigger_rules_manager;
-        $this->rng_validator           = $rng_validator;
-        $this->artifact_xml_export     = $artifact_xml_export;
-        $this->user_xml_exporter       = $user_xml_exporter;
-        $this->event_manager           = $event_manager;
-        $this->nature_dao              = $nature_dao;
+        $this->tracker_factory          = $tracker_factory;
+        $this->trigger_rules_manager    = $trigger_rules_manager;
+        $this->rng_validator            = $rng_validator;
+        $this->artifact_xml_export      = $artifact_xml_export;
+        $this->user_xml_exporter        = $user_xml_exporter;
+        $this->event_manager            = $event_manager;
+        $this->nature_presenter_factory = $nature_presenter_factory;
+        $this->artifact_links_usage_dao = $artifact_links_usage_dao;
     }
 
     public function exportToXmlFull(
-        $group_id,
+        Project $project,
         SimpleXMLElement $xml_content,
         PFUser $user,
         ArchiveInterface $archive
@@ -81,11 +90,11 @@ class TrackerXmlExport
 
         $xml_trackers = $xml_content->addChild('trackers');
 
-        $this->addUsedNature($xml_content, $group_id);
-        foreach ($this->tracker_factory->getTrackersByGroupId($group_id) as $tracker) {
+        $this->addUsedNature($xml_content, $project);
+        foreach ($this->tracker_factory->getTrackersByGroupId($project->getID()) as $tracker) {
             if ($tracker->isActive()) {
                 $exported_trackers[] = $tracker;
-                $artifacts = $this->exportTracker($xml_trackers, $tracker, $xml_field_mapping, $group_id);
+                $artifacts = $this->exportTracker($xml_trackers, $tracker, $xml_field_mapping);
                 $this->artifact_xml_export->export($tracker, $artifacts, $user, $archive);
             }
         }
@@ -93,7 +102,7 @@ class TrackerXmlExport
         $params = array(
             'user'        => $user,
             'xml_content' => &$xml_content,
-            'group_id'    => $group_id
+            'group_id'    => $project->getID()
         );
         $this->event_manager->processEvent(TRACKER_EVENT_EXPORT_FULL_XML, $params);
 
@@ -101,15 +110,21 @@ class TrackerXmlExport
         $this->validateTrackerExport($xml_trackers);
     }
 
-    private function addUsedNature(SimpleXMLElement $xml, $project_id)
+    private function addUsedNature(SimpleXMLElement $xml, Project $project)
     {
-        $natures      = $xml->addChild('natures');
-        $used_natures = $this->nature_dao->searchAllUsedNatureByProject($project_id);
+        $natures    = $xml->addChild('natures');
+        $used_types = $this->nature_presenter_factory->getAllTypesEditableInProject($project);
 
-        foreach ($used_natures as $nature) {
-            if ($nature['nature']) {
-                $natures->addChild('nature', $nature['nature']);
-            }
+        foreach ($used_types as $type) {
+            $this->addTypeChild($natures, $type, $project->getID());
+        }
+    }
+
+    private function addTypeChild(SimpleXMLElement $natures, NaturePresenter $type, $project_id)
+    {
+        $type_child = $natures->addChild('nature', $type->shortname);
+        if ($this->artifact_links_usage_dao->isTypeDisabledInProject($project_id, $type->shortname)) {
+            $type_child->addAttribute('is_used', 0);
         }
     }
 
@@ -127,7 +142,7 @@ class TrackerXmlExport
         foreach ($this->tracker_factory->getTrackersByGroupId($group_id) as $tracker) {
             if ($tracker->isActive()) {
                 $exported_trackers[] = $tracker;
-                $this->exportTracker($xml_trackers, $tracker, $xml_field_mapping, $group_id);
+                $this->exportTracker($xml_trackers, $tracker, $xml_field_mapping);
             }
         }
 
@@ -135,7 +150,7 @@ class TrackerXmlExport
         $this->validateTrackerExport($xml_trackers);
     }
 
-    private function exportTracker(SimpleXMLElement $xml_trackers, Tracker $tracker, &$xml_field_mapping, $project_id)
+    private function exportTracker(SimpleXMLElement $xml_trackers, Tracker $tracker, &$xml_field_mapping)
     {
         $child = null;
 

plugins/tracker/include/trackerPlugin.class.php
@@ -37,6 +37,7 @@ use Tuleap\Tracker\FormElement\BurndownCacheDateRetriever;
 use Tuleap\Tracker\FormElement\BurndownCalculator;
 use Tuleap\Tracker\FormElement\ComputedFieldCalculator;
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureDao;
+use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenterFactory;
 use Tuleap\Tracker\FormElement\SystemEvent\SystemEvent_BURNDOWN_DAILY;
 use Tuleap\Tracker\FormElement\SystemEvent\SystemEvent_BURNDOWN_GENERATE;
 use Tuleap\Tracker\Import\Spotter;
@@ -906,7 +907,8 @@ class trackerPlugin extends Plugin {
      */
     private function getTrackerXmlExport(UserXMLExporter $user_xml_exporter, $can_bypass_threshold)
     {
-        $rng_validator = new XML_RNGValidator();
+        $rng_validator           = new XML_RNGValidator();
+        $artifact_link_usage_dao = new ArtifactLinksUsageDao();
 
         return new TrackerXmlExport(
             $this->getTrackerFactory(),
@@ -920,7 +922,8 @@ class trackerPlugin extends Plugin {
             ),
             $user_xml_exporter,
             EventManager::instance(),
-            new NatureDao()
+            new NaturePresenterFactory(new NatureDao(), $artifact_link_usage_dao),
+            $artifact_link_usage_dao
         );
     }
 
@@ -1254,7 +1257,7 @@ class trackerPlugin extends Plugin {
 
         if ($params['options']['all'] === true) {
             $this->getTrackerXmlExport($user_xml_exporter, $can_bypass_threshold)
-            ->exportToXmlFull($project->getID(), $params['into_xml'], $user, $params['archive']);
+            ->exportToXmlFull($project, $params['into_xml'], $user, $params['archive']);
 
         } else if (isset($params['options']['tracker_id'])) {
             $this->exportSingleTracker($params, $project, $user_xml_exporter, $user, $can_bypass_threshold);

plugins/tracker/tests/TrackerXmlExportTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenter;
+
 require_once 'bootstrap.php';
 
 class TrackerXmlExportTest extends TuleapTestCase {
@@ -26,8 +28,8 @@ class TrackerXmlExportTest extends TuleapTestCase {
     private $tracker2;
     private $xml_export;
 
-    /** @var  Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureDao */
-    private $nature_dao;
+    /** @var  Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenterFactory */
+    private $nature_presenter_factory;
 
     public function setUp() {
         parent::setUp();
@@ -37,7 +39,8 @@ class TrackerXmlExportTest extends TuleapTestCase {
         $tracker_factory = stub('TrackerFactory')->getTrackersByGroupId()->returns(array($this->tracker1, $this->tracker2));
         stub($tracker_factory)->getTrackerById(456)->returns($this->tracker1);
 
-        $this->nature_dao = mock('Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureDao');
+        $this->nature_presenter_factory = mock('Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NaturePresenterFactory');
+        $this->artifact_link_dao        = mock('Tuleap\Tracker\Admin\ArtifactLinksUsageDao');
 
         $this->xml_export = new TrackerXmlExport(
             $tracker_factory,
@@ -46,13 +49,14 @@ class TrackerXmlExportTest extends TuleapTestCase {
             mock('Tracker_Artifact_XMLExport'),
             mock('UserXMLExporter'),
             mock('EventManager'),
-            $this->nature_dao
+            $this->nature_presenter_factory,
+            $this->artifact_link_dao
         );
     }
 
     public function testExportToXml() {
         $xml_content = new SimpleXMLElement('<project/>');
-        $group_id    = 123;
+        $project     = aMockProject()->withId(123)->build();
 
         stub($this->tracker1)->isActive()->returns(true);
         stub($this->tracker2)->isActive()->returns(true);
@@ -60,14 +64,14 @@ class TrackerXmlExportTest extends TuleapTestCase {
         expect($this->tracker1)->exportToXML()->once();
         expect($this->tracker2)->exportToXML()->once();
 
-        stub($this->nature_dao)->searchAllUsedNatureByProject()->returnsDar(
-            array(
-                'nature' => 'fixed in'
-            )
-        );
+        $type = new NaturePresenter('fixed_in', '', '', true);
+
+        stub($this->nature_presenter_factory)->getAllTypesEditableInProject()->returns(array(
+            $type
+        ));
 
         $this->xml_export->exportToXMl(
-            $group_id,
+            $project,
             $xml_content,
             mock('PFUser')
         );
@@ -75,7 +79,7 @@ class TrackerXmlExportTest extends TuleapTestCase {
 
     public function testExportToXmlDoNotIncludeDeletedTrackers() {
         $xml_content = new SimpleXMLElement('<project/>');
-        $group_id    = 123;
+        $project     = aMockProject()->withId(123)->build();
 
         stub($this->tracker1)->isActive()->returns(true);
         stub($this->tracker2)->isActive()->returns(false);
@@ -83,14 +87,14 @@ class TrackerXmlExportTest extends TuleapTestCase {
         expect($this->tracker1)->exportToXML()->once();
         expect($this->tracker2)->exportToXML()->never();
 
-        stub($this->nature_dao)->searchAllUsedNatureByProject()->returnsDar(
-            array(
-                'nature' => 'fixed in'
-            )
-        );
+        $type = new NaturePresenter('fixed_in', '', '', true);
+
+        stub($this->nature_presenter_factory)->getAllTypesEditableInProject()->returns(array(
+            $type
+        ));
 
         $this->xml_export->exportToXMl(
-            $group_id,
+            $project,
             $xml_content,
             mock('PFUser')
         );

commit 85c707d6b11078acb70cb070bd5f3e81e72b57eb

request #10920: Agile Dashboard > Admin > "Export the configuration" not working
Issue has been introduced by commit e828d421eda9761b5ba26f20e7bfc108158e4801.

Change-Id: I5713e301005aa6a2d0f7a9746d93deb105831012

plugins/tracker/include/TrackerXmlExport.class.php
@@ -129,7 +129,7 @@ class TrackerXmlExport
     }
 
     public function exportToXml(
-        $group_id,
+        Project $project,
         SimpleXMLElement $xml_content,
         PFUser $user
     ) {
@@ -138,8 +138,8 @@ class TrackerXmlExport
 
         $xml_trackers = $xml_content->addChild('trackers');
 
-        $this->addUsedNature($xml_content, $group_id);
-        foreach ($this->tracker_factory->getTrackersByGroupId($group_id) as $tracker) {
+        $this->addUsedNature($xml_content, $project);
+        foreach ($this->tracker_factory->getTrackersByGroupId($project->getID()) as $tracker) {
             if ($tracker->isActive()) {
                 $exported_trackers[] = $tracker;
                 $this->exportTracker($xml_trackers, $tracker, $xml_field_mapping);

plugins/tracker/include/trackerPlugin.class.php
@@ -909,7 +909,7 @@ class trackerPlugin extends Plugin {
         $archive = new NoArchive();
 
         $this->getTrackerXmlExport($user_xml_exporter, $can_bypass_threshold)
-            ->exportToXml($params['project']->getID(), $params['into_xml'], $user);
+            ->exportToXml($params['project'], $params['into_xml'], $user);
     }
 
     /**