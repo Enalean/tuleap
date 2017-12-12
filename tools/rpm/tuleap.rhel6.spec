Enalean/tuleap

commit 64cba489d6753fc2f7c6a96dbe07a0877a29150d

Use latest version of the explorer
Part of request #10893: Update Restler library

Now we use composer instead of rpm to depend on Restler.

The API explorer is directly included (and patched) in the sources. No
moar crappy symbolic link \o/

Change-Id: I424f8bf2dc0b096cc2d5bcbdda48576849798c1e

tools/rpm/tuleap.rhel6.spec
@@ -82,8 +82,6 @@ Requires: forgeupgrade >= 1.2
 Requires: shared-mime-info
 # Documentation
 Requires: tuleap-documentation
-# Rest API
-Requires: %{php_base}-restler >= 3.0-0.7
 # SELinux policy tools
 Requires(post): policycoreutils-python
 # Bind utils
@@ -473,7 +471,7 @@ Summary: Web API Explorer
 Group: Development/Tools
 Version: 1.0
 Release: @@VERSION@@_@@RELEASE@@%{?dist}
-Requires: %{php_base}-restler
+Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}
 Obsoletes: restler-api-explorer
 %description api-explorer
 Web API Explorer for Restler. Based on Swagger UI, it dynamically generates beautiful documentation.
@@ -705,9 +703,6 @@ touch $RPM_BUILD_ROOT/%{APP_DATA_DIR}/gitolite/projects.list
 %{__ln_s} %{APP_LOG_DIR} $RPM_BUILD_ROOT/%{OLD_APP_LOG_DIR}
 %{__ln_s} /etc/rc.d/init.d/%{APP_NAME} $RPM_BUILD_ROOT/etc/rc.d/init.d/codendi
 %{__ln_s} /etc/%{APP_NAME} $RPM_BUILD_ROOT/etc/%{OLD_APP_NAME}
-
-# Symlink for the API Explorer
-%{__ln_s} /usr/share/restler/vendor/Luracast/Restler/explorer/ $RPM_BUILD_ROOT/%{APP_DIR}/src/www/api/explorer
 
 ##
 ## On package install

.gitignore
@@ -40,7 +40,6 @@ plugins/maillog
 !tools/plugins/languages/*.css
 !tools/plugins/plugincreationwizard/*.css
 src/vendor/
-src/www/api/explorer
 /.idea/
 npm-debug.log
 yarn-error.log

plugins/tracker/include/REST/v1/ArtifactsResource.class.php
@@ -61,7 +61,7 @@ class ArtifactsResource extends AuthenticatedResource {
     const VALUES_FORMAT_COLLECTION = 'collection';
     const VALUES_FORMAT_BY_FIELD   = 'by_field';
     const VALUES_FORMAT_ALL        = 'all';
-    const VALUES_DEFAULT           = '';
+    const VALUES_DEFAULT           = null;
 
     const EMPTY_TYPE = '';
 

plugins/tracker/include/REST/v1/ReportsResource.class.php
@@ -43,7 +43,7 @@ class ReportsResource extends AuthenticatedResource
     const MAX_LIMIT      = 50;
     const DEFAULT_LIMIT  = 10;
     const DEFAULT_OFFSET = 0;
-    const DEFAULT_VALUES = '';
+    const DEFAULT_VALUES = null;
     const ALL_VALUES     = 'all';
 
     /** @var Tracker_REST_Artifact_ArtifactRepresentationBuilder */

plugins/tracker/include/REST/v1/TrackersResource.class.php
@@ -57,7 +57,7 @@ class TrackersResource extends AuthenticatedResource {
     const MAX_LIMIT            = 1000;
     const DEFAULT_LIMIT        = 100;
     const DEFAULT_OFFSET       = 0;
-    const DEFAULT_VALUES       = '';
+    const DEFAULT_VALUES       = null;
     const ALL_VALUES           = 'all';
     const DEFAULT_CRITERIA     = '';
     const ORDER_ASC            = 'asc';

src/common/REST/BasicAuthentication.class.php
@@ -20,7 +20,7 @@
 namespace Tuleap\REST;
 
 use \Luracast\Restler\iAuthenticate;
-use \Luracast\Restler\RestException;
+use \Luracast\Restler\InvalidAuthCredentials;
 
 class BasicAuthentication implements iAuthenticate {
 
@@ -32,7 +32,7 @@ class BasicAuthentication implements iAuthenticate {
                 return true;
             }
 
-            throw new RestException(401, 'Basic Authentication Required');
+            throw new InvalidAuthCredentials(401, 'Basic Authentication Required');
         }
     }
 

src/common/REST/TokenIsAllowed.class.php
@@ -19,7 +19,7 @@
 
 namespace Tuleap\REST;
 
-use \Luracast\Restler\RestException;
+use \Luracast\Restler\InvalidAuthCredentials;
 
 use Rest_Exception_InvalidTokenException;
 use User_LoginException;
@@ -43,9 +43,9 @@ class TokenIsAllowed {
                 return true;
             }
         } catch (User_LoginException $exception) {
-            throw new RestException(403, $exception->getMessage());
+            throw new InvalidAuthCredentials(403, $exception->getMessage());
         } catch (Rest_Exception_InvalidTokenException $exception) {
-            throw new RestException(401, $exception->getMessage());
+            throw new InvalidAuthCredentials(401, $exception->getMessage());
         }
 
         return false;

src/etc/nginx18/tuleap.d/03-locations.conf
@@ -26,7 +26,6 @@ location /doc {
 }
 
 location /api {
-    disable_symlinks off;
     try_files $uri $uri/ /api/index.php?$args;
 }
 

commit 6acf84ceff3d452a374e6306467713b26dc1d7f0

Existing Tuleap instances can not be upgraded due to the presence of php-restler package
Restler dependency is now retrieved through Composer so the package is not needed.
On existing Tuleap instances trying to upgrade and having the API Explorer installed
you will get an error because /usr/share/tuleap/src/www/api/explorer is already
existing on the filesystem. php-restler should be removed beofre trying to upgrade.

Issue has been introduced by commit 64cba489d6753fc2f7c6a96dbe07a0877a29150d.

This is part of request #10893: Update Restler library

Change-Id: Idfb86a4f23d5f1b7c8b10088d96250bfaa74a7ec

tools/rpm/tuleap.rhel6.spec
@@ -87,6 +87,8 @@ Requires(post): policycoreutils-python
 # Bind utils
 Requires: bind-utils
 
+Obsoletes: %{php_base}-restler
+
 %description
 Tuleap is a web based application that address all the aspects of product development.
 