14f5d25a2f0ff634624e1ba8655c6c9309b87124
Joris Masson <joris.masson@enalean.com>
2017-12-18 14:05:24 +0100

    Build plugin component dependencies in order
    This is part of story #10678 define a query for title and description
    semantics
    
    How to test:
    Clear all node_modules (del tuleap/**/*/node_modules, using del-cli)
    Build tuleap
    - Kanban and cross-tracker should be built after tracker is finished.
    
    It lets us also deal with the artifact-modal dependency in a more
    standard way.
    
    Change-Id: Iead7a1d2c96ebeb1b84647262bdf5a85421f4e5b

--- gulpfile.js
+++ gulpfile.js
@@ -195,20 +195,12 @@ var fat_combined_files = [
     bower_app_paths = [
         'plugins/agiledashboard/www/js/planning-v2/',
     ],
-    artifact_modal_path = [
-        'plugins/tracker/www/scripts/angular-artifact-modal/'
-    ],
-    angular_app_paths = [
-        'plugins/agiledashboard/www/js/kanban/'
-    ],
     asset_dir = 'www/assets';
 
 tuleap.declare_plugin_tasks(asset_dir);
 component_builder.installAndBuildNpmComponents(components_paths, 'components-core', ['clean-js-core']);
-component_builder.installAndBuildNpmComponents(artifact_modal_path, 'artifact-modal');
 component_builder.installAndBuildBowerComponents(bower_app_paths, 'bower-apps');
-component_builder.installAndBuildNpmComponents(angular_app_paths, 'angular-apps', ['artifact-modal']);
-var base_dir = '.'
+var base_dir = '.';
 sass_builder.cleanAndBuildSass('sass-core-common', base_dir, common_scss);
 sass_builder.cleanAndBuildSass('sass-core-select2', base_dir, select2_scss);
 sass_builder.cleanAndBuildSass('sass-core-themes', base_dir, core_scss);
@@ -285,7 +277,7 @@ gulp.task('watch', function() {
 
 gulp.task('core', ['js-core', 'sass-core']);
 
-gulp.task('build', ['components-core', 'bower-apps', 'angular-apps'], function(callback) {
+gulp.task('build', ['components-core', 'bower-apps'], function(callback) {
     runSequence('core', 'plugins', callback);
 });
 
