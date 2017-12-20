Enalean/tuleap

commit 4d360105da6584ebc04f05bf8af1e70194f8827c

request #10861 Autocomplete must returns LDAP and local users
When adding a project member for instance, all the users (LDAP and local users)
must be returned by the autocomplete script and not only LDAP users
when we use Tuleap with LDAP.

Autocomplete for users is now able to retrieve LDAP entries and local users.

Some information:

* If a user as an LDAP entry and a local account, the ldap entry is
displayed (no double entries)
* The global limit for content (ldap + local) is still 15 elements

Change-Id: I80c45dd7267d7bf1e4797ea367515ebe533ff519

plugins/ldap/include/ldapPlugin.class.php
@@ -26,6 +26,8 @@ require_once 'autoload.php';
 require_once 'constants.php';
 
 use Tuleap\Layout\IncludeAssets;
+use Tuleap\LDAP\Exception\IdentifierTypeNotFoundException;
+use Tuleap\LDAP\Exception\IdentifierTypeNotRecognizedException;
 use Tuleap\LDAP\LinkModalContentPresenter;
 use Tuleap\LDAP\NonUniqueUidRetriever;
 use Tuleap\Project\Admin\ProjectMembers\MembersEditProcessAction;
@@ -288,9 +290,22 @@ class LdapPlugin extends Plugin {
             $sync = LDAP_UserSync::instance();
             foreach($lri as $lr) {
                 if ($lr->exist() && $lr->valid()) {
+                    try {
+                        $tuleap_user = $this->getLdapUserManager()->getUserByIdentifier("ldapuid:" . $lr->getLogin());
+
+                        if ($tuleap_user) {
+                            $tuleap_user_id = $tuleap_user->getId();
+                        }
+                    } catch (IdentifierTypeNotFoundException $e) {
+                        $tuleap_user_id = null;
+                    } catch (IdentifierTypeNotRecognizedException $e) {
+                        $tuleap_user_id = null;
+                    }
+
                     $params['userList'][] = array(
                         'display_name' => $sync->getCommonName($lr).' ('.$lr->getLogin().')',
                         'login'        => $lr->getLogin(),
+                        'user_id'      => $tuleap_user_id,
                         'has_avatar'   => false
                     );
                 }
@@ -413,21 +428,6 @@ class LdapPlugin extends Plugin {
     }
 
     /**
-     * Get a User object from an LDAP iterator
-     *
-     * @param LDAPResultIterator $lri An LDAP result iterator
-     *
-     * @return PFUser
-     */
-    protected function getUserFromLdapIterator($lri) {
-        if($lri && count($lri) === 1) {
-            $ldapUm = $this->getLdapUserManager();
-            return $ldapUm->getUserFromLdap($lri->current());
-        }
-        return null;
-    }
-
-    /**
      * Hook
      * Params:
      *  IN  $params['ident']
@@ -449,41 +449,23 @@ class LdapPlugin extends Plugin {
                 // (uid, email, common name)
                 $lri  = $ldap->searchUser($params['ident']);
             }
-            $params['user'] = $this->getUserFromLdapIterator($lri);
+            $params['user'] = $this->getLdapUserManager()->getUserFromLdapIterator($lri);
         }
     }
 
-    /**
-     * $params['identifier'] IN
-     * $params['user'] OUT
-     * $params['tokenFound'] OUT
-     *
-     * @param unknown_type $params
-     */
-    function user_manager_get_user_by_identifier($params) {
+    public function user_manager_get_user_by_identifier($params)
+    {
         if ($this->isLdapAuthType() && $this->isLDAPUserManagementEnabled()) {
-            // identifier = type:value
-            $separatorPosition = strpos($params['identifier'], ':');
-            $type = strtolower(substr($params['identifier'], 0, $separatorPosition));
-            $value = strtolower(substr($params['identifier'], $separatorPosition + 1));
+            try {
+                $tuleap_user = $this->getLdapUserManager()->getUserByIdentifier($params['identifier']);
 
-            $ldap = $this->getLdap();
-            $lri = null;
-            switch ($type) {
-                case 'ldapid':
-                    $params['tokenFound'] = true;
-                    $lri = $ldap->searchEdUid($value);
-                    break;
-                case 'ldapdn':
-                    $params['tokenFound'] = true;
-                    $lri = $ldap->searchDn($value);
-                    break;
-                case 'ldapuid':
-                    $params['tokenFound'] = true;
-                    $lri = $ldap->searchLogin($value);
-                    break;
+                $params['tokenFound'] = true;
+                $params['user']       = $tuleap_user;
+            } catch (IdentifierTypeNotFoundException $e) {
+                // Do nothing
+            } catch (IdentifierTypeNotRecognizedException $e) {
+                // Do nothing
             }
-            $params['user'] = $this->getUserFromLdapIterator($lri);
         }
     }
 

plugins/ldap/include/LDAP_UserManager.class.php
@@ -1,33 +1,28 @@
 <?php
 /**
  * Copyright (c) STMicroelectronics, 2008. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
  *
  * Originally written by Manuel Vacelet, 2008
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once 'LDAP_UserDao.class.php';
-require_once 'LDAP.class.php';
-require_once 'LDAP_UserSync.class.php';
-require_once 'LDAP_User.class.php';
-require_once 'common/user/UserManager.class.php';
-require_once 'common/system_event/SystemEventManager.class.php';
-require_once 'system_event/SystemEvent_PLUGIN_LDAP_UPDATE_LOGIN.class.php';
+use Tuleap\LDAP\Exception\IdentifierTypeNotFoundException;
+use Tuleap\LDAP\Exception\IdentifierTypeNotRecognizedException;
 
 /**
  * Manage interaction between an LDAP group and Codendi user_group.
@@ -579,6 +574,53 @@ class LDAP_UserManager {
     {
         return SystemEventManager::instance();
     }
-}
 
-?>
+    /**
+     * @return PFUser|null
+     */
+    public function getUserByIdentifier($identifier)
+    {
+        $separator_position = strpos($identifier, ':');
+        $type               = strtolower(substr($identifier, 0, $separator_position));
+        $value              = strtolower(substr($identifier, $separator_position + 1));
+
+        if (! $type) {
+            throw new IdentifierTypeNotFoundException();
+        }
+
+        $ldap = $this->getLdap();
+        $lri = null;
+        switch ($type) {
+            case 'ldapid':
+                $lri = $ldap->searchEdUid($value);
+                break;
+            case 'ldapdn':
+                $lri = $ldap->searchDn($value);
+                break;
+            case 'ldapuid':
+                $lri = $ldap->searchLogin($value);
+                break;
+            default:
+                throw new IdentifierTypeNotRecognizedException();
+                break;
+        }
+
+        return $this->getUserFromLdapIterator($lri);
+    }
+
+    /**
+     * Get a User object from an LDAP iterator
+     *
+     * @param LDAPResultIterator $lri An LDAP result iterator
+     *
+     * @return PFUser|null
+     */
+    public function getUserFromLdapIterator(LDAPResultIterator $lri = null)
+    {
+        if ($lri && count($lri) === 1) {
+            return $this->getUserFromLdap($lri->current());
+        }
+
+        return null;
+    }
+}

plugins/ldap/include/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoloadec995e90cf0afb4b7890a936aa7c48e2($class) {
+function autoloadd3b5d9a8c78e74a7fe53907ad99175c1($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -48,6 +48,8 @@ function autoloadec995e90cf0afb4b7890a936aa7c48e2($class) {
             'ldapresult' => '/LDAPResult.class.php',
             'ldapresultiterator' => '/LDAPResult.class.php',
             'systemevent_plugin_ldap_update_login' => '/system_event/SystemEvent_PLUGIN_LDAP_UPDATE_LOGIN.class.php',
+            'tuleap\\ldap\\exception\\identifiertypenotfoundexception' => '/exception/IdentifierTypeNotFoundException.php',
+            'tuleap\\ldap\\exception\\identifiertypenotrecognizedexception' => '/exception/IdentifierTypeNotRecognizedException.php',
             'tuleap\\ldap\\linkmodalcontentpresenter' => '/LinkModalContentPresenter.php',
             'tuleap\\ldap\\nonuniqueuidretriever' => '/NonUniqueUidRetriever.php',
             'tuleap\\ldap\\project\\ugroup\\binding\\additionalmodalpresenterbuilder' => '/LDAP/Project/UGroup/Binding/AdditionalModalPresenterBuilder.php'
@@ -58,5 +60,5 @@ function autoloadec995e90cf0afb4b7890a936aa7c48e2($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoloadec995e90cf0afb4b7890a936aa7c48e2');
+spl_autoload_register('autoloadd3b5d9a8c78e74a7fe53907ad99175c1');
 // @codeCoverageIgnoreEnd

plugins/ldap/include/exception/IdentifierTypeNotFoundException.php
@@ -1,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\LDAP\Exception;
+
+use Exception;
+
+class IdentifierTypeNotFoundException extends Exception
+{
+
+}

plugins/ldap/include/exception/IdentifierTypeNotRecognizedException.php
@@ -1,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\LDAP\Exception;
+
+use Exception;
+
+class IdentifierTypeNotRecognizedException extends Exception
+{
+
+}

src/common/dao/UserDao.class.php
@@ -393,7 +393,7 @@ class UserDao extends DataAccessObject {
     {
         $name = $this->getDa()->quoteLikeValueSurround($name);
 
-        $sql = "SELECT SQL_CALC_FOUND_ROWS realname, user_name, has_avatar".
+        $sql = "SELECT SQL_CALC_FOUND_ROWS user_id, realname, user_name, has_avatar".
             " FROM user".
             " WHERE (realname LIKE $name".
             " OR user_name LIKE $name)".

src/www/user/autocomplete.php
@@ -64,25 +64,31 @@ $evParams = array('searchToken'     => $userName,
                   'json_format'     => $json_format,
                   'userList'        => &$userList,
                   'has_more'        => &$has_more,
-                  'pluginAnswered'  => &$pluginAnswered);
+                  'pluginAnswered'  => &$pluginAnswered
+);
+
 $em = EventManager::instance();
 $em->processEvent("ajax_search_user", $evParams);
 
-// If no plugin answered, search in DB.
-if(!$pluginAnswered) {
+if (count($userList) < $limit) {
     // search user dao
-    $userDao = new UserDao(CodendiDataAccess::instance());
-    $dar = $userDao->searchUserNameLike($userName, $limit);
+    $userDao   = new UserDao(CodendiDataAccess::instance());
+    $sql_limit = (int) ($limit - count($userList));
+
+    $dar = $userDao->searchUserNameLike($userName, $sql_limit);
     while($dar->valid()) {
         $row = $dar->current();
         $userList[] = array(
             'display_name' => $row['realname']." (".$row['user_name'].")",
             'login'        => $row['user_name'],
-            'has_avatar'   => $row['has_avatar']
+            'has_avatar'   => $row['has_avatar'],
+            'user_id'      => $row['user_id'],
         );
+
         $dar->next();
     }
-    $has_more = $userDao->foundRows() > $limit;
+
+    $has_more = $has_more || ($userDao->foundRows() > $limit);
 }
 
 //
@@ -127,18 +133,28 @@ if ($json_format) {
         }
     }
 
-    foreach ($userList as $user) {
+    $users_already_seen = array();
+    foreach ($userList as $user_info) {
+        $user_id = $user_info['user_id'];
+        if ($user_id && in_array($user_id, $users_already_seen)) {
+            continue;
+        }
+        $users_already_seen[] = $user_id;
+
+        $display_name   = $user_info['display_name'];
+        $login          = $user_info['login'];
+
         $json_entries[] = array(
             'type'       => 'user',
-            'id'         => $user['display_name'],
-            'text'       => $user['display_name'],
-            'avatar_url' => '/users/' . $user['login'] . '/avatar.png',
-            'has_avatar' => (bool)$user['has_avatar']
+            'id'         => $display_name,
+            'text'       => $display_name,
+            'avatar_url' => '/users/' . urlencode($login) . '/avatar.png',
+            'has_avatar' => (bool)$user_info['has_avatar']
         );
     }
 
     $output = array(
-        'results' => $json_entries,
+        'results' => array_values($json_entries),
         'pagination' => array(
             'more' => $has_more
         )

commit 0877ea11676cd701f47049a6fa3d81c6205bad6f

request #10939: LDAP users are automatically created in Tuleap when displayed in a select box autocompleted
LDAP users should be created on Tuleap until really necessary. Creating the user on Tuleap side
when the user is displayed in an autocompleter is overkill.

Issue has been introduced by commit 4d360105da6584ebc04f05bf8af1e70194f8827c.

Change-Id: Iffce32ad0ee996fab0e93460d750fc6733d0d5ef

plugins/ldap/include/ldapPlugin.class.php
@@ -287,21 +287,16 @@ class LdapPlugin extends Plugin {
 
             $validEmail = isset($params['validEmail']) ? $params['validEmail'] : false;
 
-            $ldap = $this->getLdap();
-            $lri  = $ldap->searchUserAsYouType($params['searchToken'], $params['limit'], $validEmail);
-            $sync = LDAP_UserSync::instance();
+            $ldap         = $this->getLdap();
+            $lri          = $ldap->searchUserAsYouType($params['searchToken'], $params['limit'], $validEmail);
+            $user_manager = UserManager::instance();
+            $sync         = LDAP_UserSync::instance();
             foreach($lri as $lr) {
                 if ($lr->exist() && $lr->valid()) {
-                    try {
-                        $tuleap_user = $this->getLdapUserManager()->getUserByIdentifier("ldapuid:" . $lr->getLogin());
-
-                        if ($tuleap_user) {
-                            $tuleap_user_id = $tuleap_user->getId();
-                        }
-                    } catch (IdentifierTypeNotFoundException $e) {
-                        $tuleap_user_id = null;
-                    } catch (IdentifierTypeNotRecognizedException $e) {
-                        $tuleap_user_id = null;
+                    $tuleap_user_id = null;
+                    $tuleap_user    = $user_manager->getUserByLdapId($lr->getEdUid());
+                    if ($tuleap_user !== null) {
+                        $tuleap_user_id = $tuleap_user->getId();
                     }
 
                     $params['userList'][] = array(