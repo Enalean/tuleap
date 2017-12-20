Enalean/tuleap

commit 8151ef2a6a1a4551e440959e93f8c14862c073ca

Have babel-polyfill loaded by FlamingParrot
This is part of story #10678 define a query for title and description
semantics

How to test:
Build tuleap. In FlamingParrot pages, the following functionality should
work:
- Navbar search
- Navbar history
- Sidebar
- Keyboard shortcuts
- MOTD
- There should not be errors in PullRequest plugin pages (with
LabelsBox)

FlamingParrot code is now entirely managed by webpack.
In order to not repeat ourselves for CodeMirror-related code in Tracker
and Cross-tracker plugins, we want to extract common code as ES modules.
We need to load polyfills and babel-polyfill (rightly) disallows
including it more than once per page. Thus, Flaming Parrot code will
include babel-polyfill, will be built once and loaded on all Flaming
Parrot pages. This way, like Burning Parrot pages, we can rely on the
fact that runtime polyfills are already there.

Change-Id: Ibb327fcc058a89c2a604482832c4b8fdaa6d07e3

gulpfile.js
@@ -101,14 +101,6 @@ var fat_combined_files = [
         'src/www/scripts/tuleap/listFilter.js',
         'src/www/scripts/codendi/Tooltip.js'
     ],
-    flaming_parrot_files = [
-        'src/www/themes/FlamingParrot/js/navbar.js',
-        'src/www/themes/FlamingParrot/js/sidebar.js',
-        'src/www/themes/FlamingParrot/js/motd.js',
-        'src/www/themes/FlamingParrot/js/keymaster/keymaster.js',
-        'src/www/themes/FlamingParrot/js/keymaster-sequence/keymaster.sequence.min.js',
-        'src/www/themes/FlamingParrot/js/keyboard-navigation.js'
-    ],
     burning_parrot_files = [
         'src/www/scripts/tuleap/escaper.js',
         'src/www/themes/BurningParrot/vendor/smooth-scrollbar/smooth-scrollbar.min.js',
@@ -230,7 +222,6 @@ gulp.task('js-core', function() {
         tuleap                     : fat_combined_files,
         tuleap_subset              : subset_combined_files,
         tuleap_subset_flamingparrot: subset_combined_files.concat(subset_combined_flamingparrot_files),
-        flamingparrot              : flaming_parrot_files,
         burningparrot              : burning_parrot_files
     };
 

src/common/layout/Layout.class.php
@@ -215,6 +215,9 @@ abstract class Layout extends Tuleap\Layout\BaseLayout
         return $this;
     }
 
+    protected function includeJavascriptPolyfills() {
+    }
+
     /**
      * @return PFUser
      */
@@ -455,7 +458,8 @@ abstract class Layout extends Tuleap\Layout\BaseLayout
      * @see includeJavascriptFile
      * @see includeJavascriptSnippet
      */
-    public function displayJavascriptElements($params) {
+    public function displayJavascriptElements($params)
+    {
         if ($this->shouldIncludeFatCombined($params)) {
             echo $this->include_asset->getHTMLSnippet('tuleap.js');
         } else {
@@ -477,6 +481,7 @@ abstract class Layout extends Tuleap\Layout\BaseLayout
         if (ForgeConfig::get('DEBUG_MODE') && (ForgeConfig::get('DEBUG_DISPLAY_FOR_ALL') || user_ismember(1, 'A')) ) {
             echo '<script type="text/javascript" src="/scripts/codendi/debug_reserved_names.js"></script>'."\n";
         }
+        $this->includeJavascriptPolyfills();
 
         $em = EventManager::instance();
         $em->processEvent("javascript_file", null);

src/www/scripts/FlamingParrot/index.js
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2017, Enalean. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
+
+import '../navbar-history/index-flamingparrot.js';
+import './navbar.js';
+import './sidebar.js';
+import './motd.js';
+import './keyboard-navigation.js';

src/www/scripts/FlamingParrot/keyboard-navigation.js
@@ -1,0 +1,152 @@
+/**
+ * Copyright (c) Enalean, 2014 - 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import $   from 'jquery';
+import key from 'keymaster';
+import 'keymaster-sequence';
+
+function deployKeyboardShortcuts() {
+    var my                       = $('#navbar-my'),
+        logo                     = $('#navbar-logo'),
+        project                  = $('#navbar-project'),
+        project_dropdown         = $('#dropdown-project'),
+        user_navigation          = $('#navbar-user-navigation'),
+        user_navigation_dropdown = $('#dropdown-user-navigation'),
+        sidebar_collapse         = $('#sidebar-collapser'),
+        sidebar_git              = $('a#sidebar-plugin_git'),
+        sidebar_project_home     = $('a#sidebar-homepage'),
+        sidebar_wiki             = $('a#sidebar-wiki'),
+        sidebar_svn              = $('a#sidebar-svn'),
+        sidebar_tracker          = $('a#sidebar-plugin_tracker'),
+        sidebar_dashboard        = $('a#sidebar-dashboard'),
+        sidebar_agiledashboard   = $('a#sidebar-plugin_agiledashboard'),
+        sidebar_docman           = $('a#sidebar-docman'),
+        sidebar_cvs              = $('a#sidebar-cvs'),
+        sidebar_file             = $('a#sidebar-file'),
+        sidebar_mediawiki        = $('a#sidebar-plugin_mediawiki'),
+        sidebar_im               = $('a#sidebar-IM'),
+        sidebar_forum            = $('a#sidebar-forum'),
+        sidebar_mail             = $('a#sidebar-mail'),
+        sidebar_news             = $('a#sidebar-news');
+
+    setShortcutOnLink(["g","m"], my);
+    setShortcutOnLink(["g","h"], logo);
+    setShortcutOnProjectDropDown(["g","p"], project, project_dropdown);
+    setShortcutOnUserDropDown(["g","a"], user_navigation, user_navigation_dropdown);
+    setShortcutOnSidebar("s", sidebar_collapse);
+    setShortcutExitInput(27);
+    setHelpModalShortcut(63);
+    setShortcutOnSidebarLink(["p","h"], sidebar_dashboard);
+    setShortcutOnSidebarLink(["p","a"], sidebar_agiledashboard);
+    setShortcutOnSidebarLink(["p","p"], sidebar_project_home);
+    setShortcutOnSidebarLink(["p","d"], sidebar_docman);
+    setShortcutOnSidebarLink(["p","m"], sidebar_mediawiki);
+    setShortcutOnSidebarLink(["p","w"], sidebar_wiki);
+    setShortcutOnSidebarLink(["p","t"], sidebar_tracker);
+    setShortcutOnSidebarLink(["p","g"], sidebar_git);
+    setShortcutOnSidebarLink(["p","s"], sidebar_svn);
+    setShortcutOnSidebarLink(["p","c"], sidebar_cvs);
+    setShortcutOnSidebarLink(["p","f"], sidebar_file);
+    setShortcutOnSidebarLink(["p","b"], sidebar_forum);
+    setShortcutOnSidebarLink(["p","n"], sidebar_news);
+    setShortcutOnSidebarLink(["p","l"], sidebar_mail);
+    setShortcutOnSidebarLink(["p","i"], sidebar_im);
+}
+
+function setShortcutOnProjectDropDown(shortCut, element, dropdown) {
+    key.sequence(shortCut, function () {
+        if (element.length > 0) {
+            element.click();
+            if (dropdown !== undefined) {
+                dropdown.find("input").first().focus();
+                return false;
+            }
+        }
+    });
+}
+
+function setShortcutOnUserDropDown(shortCut, element, dropdown) {
+    key.sequence(shortCut, function () {
+        if (element.length > 0) {
+            element.click();
+            if (dropdown !== undefined) {
+                dropdown.find("a")[1].focus();
+                return false;
+            }
+        }
+    });
+}
+
+function setShortcutOnSidebar(shortCut, element) {
+    key(shortCut, function () {
+        if (element.length > 0) {
+            element.click();
+            return false;
+        };
+    });
+}
+
+function setShortcutOnSidebarLink(shortCut, element) {
+    key.sequence(shortCut, function () {
+        if (element.length > 0) {
+            window.location.href = element.attr('href');
+            return false;
+        }
+    });
+}
+
+function setShortcutOnLink(shortCut, element) {
+    key.sequence(shortCut, function () {
+        if (element.length > 0) {
+            window.location.href = element.attr('href');
+            return false;
+        }
+    });
+}
+
+function showOrHideHelpModal(help_modal) {
+    help_modal.modal('toggle');
+}
+
+function setShortcutExitInput(shortCut) {
+    $(document).keyup(function(event) {
+        if (event.which === shortCut) {
+            var help_modal = $('#keyboard-navigation-help-modal');
+            if (help_modal.hasClass('in')) {
+                showOrHideHelpModal(help_modal);
+                return false;
+            } else if ($(document.activeElement).is('input')) {
+                $(document.activeElement).blur();
+            }
+            $("body").click();
+            return false;
+        }
+    });
+}
+
+function setHelpModalShortcut(shortCut) {
+    var help_modal = $('#keyboard-navigation-help-modal');
+    $(document).keypress(function(event) {
+        if (event.which === shortCut && !$(document.activeElement).is('textarea, input')) {
+            showOrHideHelpModal(help_modal);
+        }
+    });
+}
+
+$(document).ready(deployKeyboardShortcuts);

commit 4d673235585246d06cdd45414a094ec327c786d4

request #10940 Npm run watch raises errors
Introduced by 8151ef2a6a1a4551e440959e93f8c14862c073ca

- 'npm run watch' should run without error

Change-Id: I9d37be4a9e4169e532b40a38cac47e9ea4e52342

gulpfile.js
@@ -248,7 +248,6 @@ gulp.task('watch', function() {
         fat_combined_files
             .concat(subset_combined_files)
             .concat(subset_combined_flamingparrot_files)
-            .concat(flaming_parrot_files)
             .concat(burning_parrot_files),
         ['js-core']
     );