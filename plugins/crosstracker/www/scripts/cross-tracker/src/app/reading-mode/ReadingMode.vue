cdc19ed37e47cbe9f5a8e68efd73e33541e4725e
Joris Masson <joris.masson@enalean.com>
2017-12-22 12:22:38 +0100

    Display query in reading mode
    Part of story #10678 define a query for title and description semantics
    
    Change-Id: I1f8544fd0f2a4157b1d344361304084bc29c9ae8

--- plugins/crosstracker/www/scripts/cross-tracker/src/app/reading-mode/ReadingMode.vue
+++ plugins/crosstracker/www/scripts/cross-tracker/src/app/reading-mode/ReadingMode.vue
@@ -1,18 +1,21 @@
 (<template>
     <div class="cross-tracker-reading-mode">
-        <div class="cross-tracker-reading-mode-fields"
-            v-bind:class="{'reading-mode-disabled': ! is_report_loaded, 'disabled' : is_user_anonymous}"
+        <div class="reading-mode-report"
+            v-bind:class="{'disabled' : is_switch_disabled}"
             v-on:click="switchToWritingMode"
         >
             <tracker-list-reading-mode
                 v-bind:reading-cross-tracker-report="readingCrossTrackerReport"
             ></tracker-list-reading-mode>
+            <div class="reading-mode-query"
+                v-if="is_expert_query_not_empty"
+            >{{ readingCrossTrackerReport.expert_query }}</div>
         </div>
         <div class="dashboard-widget-content-cross-tracker-reading-mode-actions"
             v-bind:class="{'cross-tracker-hide': are_actions_hidden }"
         >
-            <button class="tlp-button-primary tlp-button-small tlp-button-outline dashboard-widget-content-cross-tracker-reading-mode-actions-cancel">{{ cancel }}</button>
-            <button class="tlp-button-primary tlp-button-small dashboard-widget-content-cross-tracker-reading-mode-actions-save">
+            <button class="tlp-button-primary tlp-button-outline dashboard-widget-content-cross-tracker-reading-mode-actions-cancel">{{ cancel }}</button>
+            <button class="tlp-button-primary dashboard-widget-content-cross-tracker-reading-mode-actions-save">
                 <span class="cross-tracker-loader"><i class="tlp-button-icon fa fa-spinner fa-spin"></i></span>
                 {{ save_report }}
             </button>
@@ -44,12 +47,15 @@
         computed: {
             save_report:() => gettext_provider.gettext("Save report"),
             cancel:()      => gettext_provider.gettext("Cancel"),
-            is_report_loaded() {
-                return this.backendCrossTrackerReport.loaded;
+            is_switch_disabled() {
+                return (! this.backendCrossTrackerReport.loaded || this.is_user_anonymous);
             },
             is_user_anonymous() {
                 return isAnonymous();
             },
+            is_expert_query_not_empty() {
+                return this.readingCrossTrackerReport.expert_query !== '';
+            }
         },
         methods: {
             switchToWritingMode() {
