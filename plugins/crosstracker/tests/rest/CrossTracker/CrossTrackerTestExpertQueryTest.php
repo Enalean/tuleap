768f82270eb94479ee1247fa85ee71abca665a72
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2017-12-26 12:12:34 +0100

    Add REST tests
    This is part of story #10678 define a query for title and description semantics
    
    Change-Id: I3e9de5a51fa0bf7d418ddf068dab77322223ec05

--- plugins/crosstracker/tests/rest/CrossTracker/CrossTrackerTestExpertQueryTest.php
+++ plugins/crosstracker/tests/rest/CrossTracker/CrossTrackerTestExpertQueryTest.php
@@ -53,25 +53,131 @@ class CrossTrackerTestExpertQueryTest extends RestBase
         $this->allEpicArtifactsMustBeRetrievedByQuery();
     }
 
-    private function allEpicArtifactsMustBeRetrievedByQuery()
+    /**
+     * @depends testNonEmptyTitle
+     */
+    public function testMultipleTitles()
+    {
+        $params = array(
+            "trackers_id"  => array($this->epic_tracker_id),
+            "expert_query" => ' @title = "first" OR @title = "third" '
+        );
+
+        $response = $this->getResponse($this->client->put('cross_tracker_reports/1', null, $params));
+        $this->assertEquals($response->getStatusCode(), 201);
+
+        $cross_tracker_report = $response->json();
+
+        $this->assertEquals(
+            $cross_tracker_report["expert_query"],
+            '@title = "first" OR @title = "third"'
+        );
+
+        $this->getMatchingEpicArtifactByIds(array(
+            $this->epic_artifact_ids[3],
+            $this->epic_artifact_ids[1],
+        ));
+    }
+
+    /**
+     * @depends testMultipleTitles
+     */
+    public function testEmptyDescription()
+    {
+        $params = array(
+            "trackers_id"  => array($this->epic_tracker_id),
+            "expert_query" => ' @description = "" '
+        );
+
+        $response = $this->getResponse($this->client->put('cross_tracker_reports/1', null, $params));
+        $this->assertEquals($response->getStatusCode(), 201);
+
+        $cross_tracker_report = $response->json();
+
+        $this->assertEquals(
+            $cross_tracker_report["expert_query"],
+            '@description = ""'
+        );
+
+        $this->getMatchingEpicArtifactByIds(array(
+            $this->epic_artifact_ids[8]
+        ));
+    }
+
+    /**
+     * @depends testEmptyDescription
+     */
+    public function testEmptyDescriptionWithNotEmptyTitle()
+    {
+        $params = array(
+            "trackers_id"  => array($this->epic_tracker_id),
+            "expert_query" => ' @description = "" AND @title != "" '
+        );
+
+        $response = $this->getResponse($this->client->put('cross_tracker_reports/1', null, $params));
+        $this->assertEquals($response->getStatusCode(), 201);
+
+        $cross_tracker_report = $response->json();
+
+        $this->assertEquals(
+            $cross_tracker_report["expert_query"],
+            '@description = "" AND @title != ""'
+        );
+
+        $this->getMatchingEpicArtifactByIds(array(
+            $this->epic_artifact_ids[8]
+        ));
+    }
+
+    private function getMatchingArtifactsFromJson()
     {
         $response = $this->getResponse($this->client->get('cross_tracker_reports/1/content?limit=50&offset=0'));
 
         $this->assertEquals($response->getStatusCode(), 200);
         $cross_tracker_artifacts = $response->json();
 
+        return $cross_tracker_artifacts['artifacts'];
+    }
+
+    private function allEpicArtifactsMustBeRetrievedByQuery()
+    {
+        $cross_tracker_artifacts = $this->getMatchingArtifactsFromJson();
+
         $this->assertEquals(
-            count($cross_tracker_artifacts['artifacts']),
+            count($cross_tracker_artifacts),
             8
         );
 
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][0]['id'], $this->epic_artifact_ids[8]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][1]['id'], $this->epic_artifact_ids[7]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][2]['id'], $this->epic_artifact_ids[6]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][3]['id'], $this->epic_artifact_ids[5]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][4]['id'], $this->epic_artifact_ids[4]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][5]['id'], $this->epic_artifact_ids[3]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][6]['id'], $this->epic_artifact_ids[2]);
-        $this->assertEquals($cross_tracker_artifacts['artifacts'][7]['id'], $this->epic_artifact_ids[1]);
+        $this->assertEquals($cross_tracker_artifacts[0]['id'], $this->epic_artifact_ids[8]);
+        $this->assertEquals($cross_tracker_artifacts[1]['id'], $this->epic_artifact_ids[7]);
+        $this->assertEquals($cross_tracker_artifacts[2]['id'], $this->epic_artifact_ids[6]);
+        $this->assertEquals($cross_tracker_artifacts[3]['id'], $this->epic_artifact_ids[5]);
+        $this->assertEquals($cross_tracker_artifacts[4]['id'], $this->epic_artifact_ids[4]);
+        $this->assertEquals($cross_tracker_artifacts[5]['id'], $this->epic_artifact_ids[3]);
+        $this->assertEquals($cross_tracker_artifacts[6]['id'], $this->epic_artifact_ids[2]);
+        $this->assertEquals($cross_tracker_artifacts[7]['id'], $this->epic_artifact_ids[1]);
+
+        $this->assertNotEmpty($cross_tracker_artifacts[0]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[1]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[2]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[3]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[4]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[5]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[6]['title']);
+        $this->assertNotEmpty($cross_tracker_artifacts[7]['title']);
+    }
+
+    private function getMatchingEpicArtifactByIds(array $artifact_ids)
+    {
+        $cross_tracker_artifacts = $this->getMatchingArtifactsFromJson();
+
+        $this->assertEquals(
+            count($cross_tracker_artifacts),
+            count($artifact_ids)
+        );
+
+        foreach ($artifact_ids as $key => $artifact_id) {
+            $this->assertEquals($cross_tracker_artifacts[$key]['id'], $artifact_id);
+        }
     }
 }
