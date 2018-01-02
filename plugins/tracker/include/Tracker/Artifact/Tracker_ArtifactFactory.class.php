Enalean/tuleap

commit ea314d88d53c415451b79a36103a24a7a7d8fca6

story #10771: get several artifacts at once from the REST API
This contribution introduce a new REST route GET /artifacts

Change-Id: I0ce7e6dc7a7da9d8a35a4644b20337a9aefd6972

plugins/tracker/include/Tracker/Artifact/Tracker_ArtifactFactory.class.php
@@ -144,15 +144,29 @@ class Tracker_ArtifactFactory {
      * 
      * @param array $artifact_ids
      * 
-     * @return array of Tracker_Artifact
+     * @return Tracker_Artifact[]
      */
-    public function getArtifactsByArtifactIdList(array $artifact_ids) {
-        $artifacts = array();
+    public function getArtifactsByArtifactIdList(array $artifact_ids)
+    {
+        $artifact_ids            = array_unique($artifact_ids);
+        $artifacts               = array();
+        $not_cached_artifact_ids = array();
+
         foreach ($artifact_ids as $artifact_id) {
-            if ($artifact = $this->getArtifactById($artifact_id)) {
-                $artifacts[$artifact_id] = $artifact;
+            if (isset($this->artifacts[$artifact_id])) {
+                $artifacts[] = $this->artifacts[$artifact_id];
+            } else {
+                $not_cached_artifact_ids[] = $artifact_id;
             }
         }
+
+        $rows = $this->getDao()->searchByIds($not_cached_artifact_ids);
+        foreach ($rows as $row) {
+            $artifact                            = $this->getInstanceFromRow($row);
+            $this->artifacts[$artifact->getId()] = $artifact;
+            $artifacts[]                         = $artifact;
+        }
+
         return $artifacts;
     }
     

plugins/tracker/include/REST/v1/ArtifactsResource.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2013 - 2017. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -21,13 +21,18 @@
 namespace Tuleap\Tracker\REST\v1;
 
 use Tracker;
+use Tuleap\REST\JsonDecoder;
 use \Tuleap\REST\ProjectAuthorization;
 use \Tuleap\REST\Header;
 use Tuleap\REST\AuthenticatedResource;
 use \Luracast\Restler\RestException;
 use \Tracker_ArtifactFactory;
 use \Tracker_Artifact;
+use Tuleap\REST\QueryParameterException;
+use Tuleap\REST\QueryParameterParser;
 use Tuleap\Tracker\Exception\SemanticTitleNotDefinedException;
+use Tuleap\Tracker\REST\Artifact\ArtifactBatchQueryConverter;
+use Tuleap\Tracker\REST\Artifact\MalformedArtifactBatchQueryConverterException;
 use Tuleap\Tracker\REST\Artifact\MovedArtifactValueBuilder;
 use \UserManager;
 use \PFUser;
@@ -52,11 +57,12 @@ use Tuleap\Tracker\FormElement\Field\ArtifactLink\Nature\NatureDao;
 use Tracker_Artifact_Attachment_AlreadyLinkedToAnotherArtifactException;
 
 class ArtifactsResource extends AuthenticatedResource {
-    const MAX_LIMIT      = 50;
-    const DEFAULT_LIMIT  = 10;
-    const DEFAULT_OFFSET = 0;
-    const ORDER_ASC      = 'asc';
-    const ORDER_DESC     = 'desc';
+    const MAX_LIMIT          = 50;
+    const DEFAULT_LIMIT      = 10;
+    const DEFAULT_OFFSET     = 0;
+    const MAX_ARTIFACT_BATCH = 100;
+    const ORDER_ASC          = 'asc';
+    const ORDER_DESC         = 'desc';
 
     const VALUES_FORMAT_COLLECTION = 'collection';
     const VALUES_FORMAT_BY_FIELD   = 'by_field';
@@ -91,6 +97,59 @@ class ArtifactsResource extends AuthenticatedResource {
             new NatureDao()
         );
         $this->moved_value_builder = new MovedArtifactValueBuilder();
+    }
+
+    /**
+     * Get artifacts
+     *
+     *
+     * <p>
+     * $query parameter expects following format <code>{"id":[x,y,z]}</code> where x, y and z are artifact ID.
+     * No more than 100 artifacts can be requested at once.
+     * </p>
+     *
+     *
+     * @url GET
+     * @access hybrid
+     *
+     * @param string $query JSON object of search criteria properties {@from query}
+     * @param int    $limit     Number of elements displayed per page {@from path}{@min 1}{@max 100}
+     * @param int    $offset    Position of the first element to display {@from path}{@min 0}
+     *
+     * @return array
+     */
+    public function getArtifacts($query, $limit = self::MAX_ARTIFACT_BATCH,  $offset = self::DEFAULT_OFFSET)
+    {
+        $this->checkAccess();
+
+        $this->options();
+
+        $query_parameter_parser = new QueryParameterParser(new JsonDecoder());
+
+        try {
+            $requested_artifact_ids = $query_parameter_parser->getArrayOfInt($query, 'id');
+        } catch (QueryParameterException $ex) {
+            throw new RestException(400, $ex->getMessage());
+        }
+        if (count($requested_artifact_ids) > 100) {
+            throw new RestException(403, 'No more than '. self::MAX_ARTIFACT_BATCH .' artifacts can be requested at once.');
+        }
+
+        $user                     = UserManager::instance()->getCurrentUser();
+        $artifact_representations = array();
+
+        $artifacts = $this->artifact_factory->getArtifactsByArtifactIdList(
+            array_slice($requested_artifact_ids, $offset, $limit)
+        );
+        foreach ($artifacts as $artifact) {
+            if ($artifact->userCanView($user)) {
+                $artifact_representations[] = $this->builder->getArtifactRepresentationWithFieldValuesInBothFormat($user, $artifact);
+            }
+        }
+
+        Header::sendPaginationHeaders($limit, $offset, count($requested_artifact_ids), self::MAX_ARTIFACT_BATCH);
+
+        return array(self::VALUES_FORMAT_COLLECTION => $artifact_representations);
     }
 
     /**
@@ -376,7 +435,7 @@ class ArtifactsResource extends AuthenticatedResource {
      * @url OPTIONS
      */
     public function options() {
-        Header::allowOptionsPost();
+        Header::allowOptionsGetPost();
     }
 
     /**

plugins/tracker/include/Tracker/Artifact/dao/Tracker_ArtifactDao.class.php
@@ -38,6 +38,15 @@ class Tracker_ArtifactDao extends DataAccessObject {
         return $this->retrieve($sql);
     }
 
+    public function searchByIds(array $ids)
+    {
+        $ids = $this->da->escapeIntImplode($ids);
+
+        $sql = "SELECT * FROM tracker_artifact WHERE id IN ($ids)";
+
+        return $this->retrieve($sql);
+    }
+
     public function searchByTrackerId($tracker_id) {
         $tracker_id = $this->da->escapeInt($tracker_id);
         $sql = "SELECT A.*, CVT.value AS title, CVT.body_format AS title_format

tests/rest/tests/ArtifactsTest.php
@@ -34,7 +34,7 @@ class ArtifactsTest extends ArtifactBase
 
     public function testOptionsArtifacts() {
         $response = $this->getResponse($this->client->options('artifacts'));
-        $this->assertEquals(array('OPTIONS', 'POST'), $response->getHeader('Allow')->normalize()->toArray());
+        $this->assertEquals(array('OPTIONS', 'GET', 'POST'), $response->getHeader('Allow')->normalize()->toArray());
     }
 
     public function testPostArtifact() {
@@ -326,6 +326,33 @@ class ArtifactsTest extends ArtifactBase
                     throw new Exception('You need to update this test for the field: '.print_r($field, true));
             }
         }
+    }
+
+    /**
+     * @depends testPostArtifact
+     */
+    public function testGetArtifacts()
+    {
+        $do_not_exist_artifact_id = 999999999999999999;
+        $existing_artifact_ids    = array_merge($this->level_one_artifact_ids, $this->level_two_artifact_ids);
+        $wanted_artifacts_ids     = $existing_artifact_ids;
+        $wanted_artifacts_ids[]   = $do_not_exist_artifact_id;
+
+        $query     = json_encode(array('id' => $wanted_artifacts_ids));
+        $response  = $this->getResponse($this->client->get('artifacts?query=' . urlencode($query)));
+        $artifacts = $response->json();
+
+        $this->assertCount(count($existing_artifact_ids), $artifacts['collection']);
+    }
+
+    public function testGetTooManyArtifacts()
+    {
+        $too_many_artifacts_id = array_fill(0, 10000, 1);
+        $query                 = json_encode(array('id' => $too_many_artifacts_id));
+
+        $this->setExpectedException('Guzzle\Http\Exception\ClientErrorResponseException');
+        $response = $this->getResponse($this->client->get('artifacts?query=' . urlencode($query)));
+        $this->assertEquals($response->getStatusCode(), 403);
     }
 
 

commit e0caa0bcf77a982bb771c1a8f674ced73a919515

request #10957: Calling the REST route PATCH kanban/{id}/items leads to a DB error
Issue has been introduced by commit ea314d88d53c415451b79a36103a24a7a7d8fca6.

Change-Id: I3e9a97ebdfa298d8b5b1f001ea88f0b5a5b2357b

plugins/tracker/include/Tracker/Artifact/Tracker_ArtifactFactory.class.php
@@ -160,6 +160,10 @@ class Tracker_ArtifactFactory {
             }
         }
 
+        if (empty($not_cached_artifact_ids)) {
+            return $artifacts;
+        }
+
         $rows = $this->getDao()->searchByIds($not_cached_artifact_ids);
         foreach ($rows as $row) {
             $artifact                            = $this->getInstanceFromRow($row);