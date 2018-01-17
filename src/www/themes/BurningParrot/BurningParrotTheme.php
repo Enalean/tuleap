afddbd0f6006c33b78771f30a6fef540a4a3a9b3
Benjamin Dauton <benjamin.dauton@enalean.com>
2018-01-17 11:03:33 +0100

    request #11024: Give the ability to plugins to append content to help page
    Change-Id: Idb08cd970adbfa78eb8aee8478f7a63a3edebd64

--- src/www/themes/BurningParrot/BurningParrotTheme.php
+++ src/www/themes/BurningParrot/BurningParrotTheme.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - 2018. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -120,8 +120,15 @@ class BurningParrotTheme extends BaseLayout
 
     public function displayHelpPage()
     {
-        $this->event_manager->processEvent('site_help', null);
+        $extra_content = '';
+
+        $this->event_manager->processEvent('site_help', array(
+            'extra_content' => &$extra_content
+        ));
+
         include($GLOBALS['Language']->getContent('help/site'));
+
+        echo $extra_content;
     }
 
     private function getArrayOfClassnamesForBodyTag($params, $sidebar)
