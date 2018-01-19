e2357eac90ee4299c45ebc44a10d842ab6785bff
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2018-01-19 13:54:40 +0100

    Rename "points" into "points_with_date" in BurnupRepresentation
    this is part of request #11031 - Migrate burndown in D3 JS
    
    Now burndown returns two values:
     - "points" used by jpGraph
     - "point_with_date" used by D3
    
    Burnup "points" should be aligned with D3 and should be renamed in "points_with_date"
    
    How to test:
     - launch npm run build
     - access to a burnup, graph should be displayed without error
    
    Change-Id: I0559410c7296eb7b1e38614780f27ba1c84760bb

--- plugins/agiledashboard/tests/rest/ArtifactsTest.php
+++ plugins/agiledashboard/tests/rest/ArtifactsTest.php
@@ -47,7 +47,7 @@ class ArtifactsTest extends ArtifactBase
 
         foreach ($burnup['values'] as $field) {
             if ($field['label'] === ArtifactBase::BURNUP_FIELD_SHORTNAME) {
-                $this->assertEquals($field['value']['points'], $expected_burnup_chart);
+                $this->assertEquals($field['value']['points_with_date'], $expected_burnup_chart);
             }
         }
     }
