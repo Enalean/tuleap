88b8707db82d124af458046c4d319b8717e877bb
Manuel VACELET <manuel.vacelet@enalean.com>
2018-01-22 11:52:04 +0100

    Make search for group scope configurable
    So one can trade for performance or for completness
    
    Part of request #10890 LDAP Support for nested groups
    
    Change-Id: If2fedc3ddd63fe9d5569d6ed88a5491e5fdc3b5b

--- plugins/ldap/include/LDAP.class.php
+++ plugins/ldap/include/LDAP.class.php
@@ -1,12 +1,27 @@
 <?php
-// Codendi
-// Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
-// http://www.codendi.com
-// Copyright (c) cjt Systemsoftware AG, 2017. All Rights Reserved.
+/**
+ * Copyright (c) Enalean, 2011 - 2018. All Rights Reserved.
+ * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * LDAP class definition
- * Provides LDAP facilities to Codendi:
+ * Provides LDAP facilities:
  * - directory search
  * - user authentication
  * The ldap object is initialized with global parameters (from local.inc):
@@ -527,13 +542,33 @@ class LDAP {
             // Catch errors to detect if there are more results available than
             // the list actually returned (helps to refine the search)
             $this->trapErrors();
-            // Use SCOPE_SUBTREE to get all groups from different subbranches of "sys_ldap_grp_dn"
-            $lri = $this->search($this->ldapParams['grp_dn'], $filter, self::SCOPE_SUBTREE, $attrs, $attrsOnly, $sizeLimit);
+            $lri = $this->search($this->ldapParams['grp_dn'], $filter, $this->getSearchGroupScope(), $attrs, $attrsOnly, $sizeLimit);
         }
         if ($lri === false) {
             return new LDAPResultIterator(array(), array());
         } else {
             return $lri;
+        }
+    }
+
+    private function getSearchGroupScope()
+    {
+        $scope = '';
+        if (isset($this->ldapParams['grp_search_scope'])) {
+            $scope = $this->ldapParams['grp_search_scope'];
+        }
+        return $this->getScopeFromConf($scope);
+    }
+
+    private function getScopeFromConf($scope)
+    {
+        switch (strtolower($scope)) {
+            case self::SCOPE_SUBTREE_TEXT:
+                return self::SCOPE_SUBTREE;
+
+            case self::SCOPE_ONELEVEL_TEXT:
+            default:
+                return self::SCOPE_ONELEVEL;
         }
     }
 
