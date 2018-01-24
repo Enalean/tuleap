Enalean/tuleap

commit 895532a6830fba8a613190e7668c013ccb516340

request #10557 Seconds are displayed in artifact modal
To reproduce:
 - In kanabn modal have a date field with "Display time" checked
 - Date should be displayed as 2017-08-14 16:14 instead of 2017-08-14 16:14:14

Change-Id: If2163f8fe363d11f746d0c17d0e38da09ad3fa9f

plugins/tracker/www/scripts/angular-artifact-modal/src/tuleap-artifact-modal-fields/date-field/date-field-controller.js
@@ -25,6 +25,7 @@ function DateFieldController(
 
         if (self.field.is_time_displayed) {
             date_picker.setAttribute('data-enabletime', true);
+            removeSecondsToDate();
         }
 
         var options = {
@@ -38,6 +39,10 @@ function DateFieldController(
 
     function getFieldSize() {
         return (self.field.is_time_displayed) ? DATETIME_PICKER_SIZE : DATE_PICKER_SIZE;
+    }
+
+    function removeSecondsToDate() {
+        self.value_model.value = self.value_model.value.slice(0, -3);
     }
 
     function isRequiredAndEmpty() {

commit 4131999d834e6309c50ba2a71cee7b538152ea99

request #11044 Empty Datetime fields cause a javascript error in artifact modal
Bug introduced in 895532a6830fba8a613190e7668c013ccb516340

How to test:
- In a kanban tracker, set up a date field to use time (check the
checkbox)
- Edit a kanban artifact which has no value for this field (empty value,
or default value empty). There should not be a JS error, the datepicker
should show up when you click.

- Edit an artifact with a value. Seconds shouldn't be displayed.

Change-Id: Ie4f0f0a7ff6d70b13937a31548052075995c0958

plugins/tracker/www/scripts/angular-artifact-modal/src/model/field-values-service.js
@@ -1,10 +1,12 @@
 import { copy }              from 'angular';
 import _                     from 'lodash';
-import moment                from 'moment';
 import AwkwardCreationFields from './awkward-creation-fields-constant.js';
 import {
     formatExistingValue as formatForLinkField
 } from '../tuleap-artifact-modal-fields/link-field/link-field-initializer.js';
+import {
+    formatExistingValue as formatForDateField
+} from '../tuleap-artifact-modal-fields/date-field/date-field-initializer.js';
 
 export default TuleapArtifactFieldValuesService;
 
@@ -54,15 +56,7 @@ function TuleapArtifactFieldValuesService(
 
         switch (field.type) {
             case 'date':
-                if (field.is_time_displayed) {
-                    if (artifact_value.value) {
-                        value_obj.value = moment(artifact_value.value, moment.ISO_8601).format('YYYY-MM-DD HH:mm:ss');
-                    }
-                } else {
-                    if (artifact_value.value) {
-                        value_obj.value = moment(artifact_value.value, moment.ISO_8601).format('YYYY-MM-DD');
-                    }
-                }
+                value_obj = formatForDateField(field, artifact_value);
                 break;
             case 'cb':
                 value_obj.bind_value_ids = mapCheckboxValues(field, artifact_value.bind_value_ids);

plugins/tracker/www/scripts/angular-artifact-modal/src/tuleap-artifact-modal-fields/date-field/date-field-controller.js
@@ -25,7 +25,6 @@ function DateFieldController(
 
         if (self.field.is_time_displayed) {
             date_picker.setAttribute('data-enabletime', true);
-            removeSecondsToDate();
         }
 
         var options = {
@@ -39,10 +38,6 @@ function DateFieldController(
 
     function getFieldSize() {
         return (self.field.is_time_displayed) ? DATETIME_PICKER_SIZE : DATE_PICKER_SIZE;
-    }
-
-    function removeSecondsToDate() {
-        self.value_model.value = self.value_model.value.slice(0, -3);
     }
 
     function isRequiredAndEmpty() {

plugins/tracker/www/scripts/angular-artifact-modal/src/tuleap-artifact-modal-fields/date-field/date-field-initializer.js
@@ -1,0 +1,33 @@
+import moment from 'moment';
+
+export {
+    formatExistingValue
+};
+
+function formatExistingValue(field, artifact_value) {
+    const {
+        field_id,
+        type,
+        permissions,
+        is_time_displayed
+    } = field;
+
+    return {
+        field_id,
+        type,
+        permissions,
+        value: getValue(artifact_value.value, is_time_displayed)
+    };
+}
+
+function getValue(value, is_time_displayed) {
+    if (value === null) {
+        return '';
+    }
+
+    if (is_time_displayed) {
+        return moment(value, moment.ISO_8601).format('YYYY-MM-DD HH:mm');
+    }
+
+    return moment(value, moment.ISO_8601).format('YYYY-MM-DD');
+}

plugins/tracker/www/scripts/angular-artifact-modal/src/model/field-values-service.spec.js
@@ -401,7 +401,7 @@ describe("TuleapArtifactFieldValuesService", () => {
                     ]
                 };
                 var output = FieldValuesService.getSelectedValues(artifact_values, tracker);
-                expect(moment.fn.format).toHaveBeenCalledWith("YYYY-MM-DD HH:mm:ss");
+                expect(moment.fn.format).toHaveBeenCalledWith("YYYY-MM-DD HH:mm");
                 expect(output[609].field_id).toEqual(609);
                 expect(output[609].permissions).toEqual(["read", "update", "create"]);
             });