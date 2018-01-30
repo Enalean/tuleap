7bdaac6ed9962eae2c5fc9f5c0a1e6e95cbade87
Nicolas Terray <nicolas.terray@enalean.com>
2018-01-30 09:52:36 +0100

    Display relevant tooltip for LUD field
    request #10919: Artifact field "Last Update Date" has wrong blue tooltip
    
    In tracker » admin » field usage, the blue tooltip on mouse hover over
    "Last Update Date" field should shows "The artifact is automatically set
    to artifact update date" instead of "[…] submission date".
    
    Change-Id: I4c5a6c3c5b690d03998122aa62979836c5a076cd

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_LastUpdateDate.class.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_LastUpdateDate.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2011 - 2018. All rights reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
  */
 
 require_once('common/date/DateHelper.class.php');
@@ -317,7 +318,7 @@ class Tracker_FormElement_Field_LastUpdateDate extends Tracker_FormElement_Field
         $html = '';
         $html .= '<div>'. $this->formatDateTime(time()) . '</div>';
         $html .= '<span class="tracker-admin-form-element-help">';
-        $html .= $GLOBALS['Language']->getText('plugin_tracker_formelement_admin', 'submittedon_help');
+        $html .= $GLOBALS['Language']->getText('plugin_tracker_formelement_admin', 'lastupdatedate_help');
         $html .= '</span>';
         return $html;
     }
