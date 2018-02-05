Enalean/tuleap

commit a32033c4866b53ea4259af583e3e01a20f849c6f

Update parent on backlog item card
Part of story #10738 have the list of parents when I edit a backlog item

Test with https://gerrit.tuleap.net/#/c/10324/

How to test:
- Checkout the artifact-modal repo
- Build the artifact modal (npm run build)
- Make a symbolic link from the modal to
planning-v2/vendor/artifact-modal
- Build PV2 (npm run build)

- When you create / edit an artifact and choose a parent, it should
appear on the card

Change-Id: I84524f876b123c98e3122fdb3d350a09b0f26b9a

plugins/agiledashboard/www/js/planning-v2/src/app/backlog-item-collection/backlog-item-collection-service.js
@@ -1,4 +1,8 @@
-import _ from 'lodash';
+import {
+    remove,
+    some,
+    find
+} from 'lodash';
 
 export default BacklogItemCollectionService;
 
@@ -9,34 +13,30 @@ BacklogItemCollectionService.$inject = [
 function BacklogItemCollectionService(
     BacklogItemService
 ) {
-    var self = this;
-    _.extend(self, {
-        items                               : {},
-        refreshBacklogItem                  : refreshBacklogItem,
-        removeBacklogItemsFromCollection    : removeBacklogItemsFromCollection,
-        addOrReorderBacklogItemsInCollection: addOrReorderBacklogItemsInCollection
+    const self = this;
+    Object.assign(self, {
+        items: {},
+        refreshBacklogItem,
+        removeBacklogItemsFromCollection,
+        addOrReorderBacklogItemsInCollection
     });
 
     function refreshBacklogItem(backlog_item_id) {
         self.items[backlog_item_id].updating = true;
 
-        return BacklogItemService.getBacklogItem(backlog_item_id).then(function(data) {
-            self.items[backlog_item_id].label          = data.backlog_item.label;
-            self.items[backlog_item_id].initial_effort = data.backlog_item.initial_effort;
-            self.items[backlog_item_id].card_fields    = data.backlog_item.card_fields;
-            self.items[backlog_item_id].updating       = false;
-            self.items[backlog_item_id].status         = data.backlog_item.status;
-            self.items[backlog_item_id].has_children   = data.backlog_item.has_children;
+        return BacklogItemService.getBacklogItem(backlog_item_id).then(({ backlog_item }) => {
+            Object.assign(self.items[backlog_item_id], backlog_item);
+            self.items[backlog_item_id].updating = false;
 
-            if (! data.backlog_item.has_children) {
+            if (! backlog_item.has_children) {
                 self.items[backlog_item_id].children.collapsed = true;
             }
         });
     }
 
     function removeBacklogItemsFromCollection(backlog_items_collection, backlog_items_to_remove) {
-        _.remove(backlog_items_collection, function(item) {
-            return _.some(backlog_items_to_remove, item);
+        remove(backlog_items_collection, function(item) {
+            return some(backlog_items_to_remove, item);
         });
     }
 
@@ -47,7 +47,7 @@ function BacklogItemCollectionService(
 
         if (compared_to) {
             index = backlog_items_collection.indexOf(
-                _.find(backlog_items_collection, { id: compared_to.item_id })
+                find(backlog_items_collection, { id: compared_to.item_id })
             );
 
             if (compared_to.direction === 'after') {

plugins/agiledashboard/include/AgileDashboard/Milestone/Backlog/IBacklogItem.class.php
@@ -22,17 +22,21 @@
 /**
  * I am a Backlog Item
  */
-interface AgileDashboard_Milestone_Backlog_IBacklogItem {
+interface AgileDashboard_Milestone_Backlog_IBacklogItem
+{
+    public function setInitialEffort($value);
 
-    function setInitialEffort($value);
+    public function setStatus($status);
 
-    function setStatus($status);
+    public function setHasChildren($has_children);
 
-    function setHasChildren($has_children);
+    public function id();
 
-    function id();
+    public function hasChildren();
 
-    function hasChildren();
+    public function xRef();
 
-    function xRef();
+    public function getParent();
+
+    public function setParent(Tracker_Artifact $parent);
 }

plugins/agiledashboard/include/AgileDashboard/REST/v1/BacklogItemResource.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -45,8 +45,8 @@ use Tuleap\Tracker\REST\v1\ArtifactLinkUpdater;
 /**
  * Wrapper for Backlog_Items related REST methods
  */
-class BacklogItemResource extends AuthenticatedResource {
-
+class BacklogItemResource extends AuthenticatedResource
+{
     const MAX_LIMIT = 100;
 
     /** @var Tracker_ArtifactFactory */
@@ -123,14 +123,24 @@ class BacklogItemResource extends AuthenticatedResource {
         return $backlog_item_representation;
     }
 
-    private function getBacklogItem(PFUser $current_user, Tracker_Artifact $artifact) {
+    private function getBacklogItem(PFUser $current_user, Tracker_Artifact $artifact)
+    {
         $semantic_manager = new Tracker_SemanticManager($artifact->getTracker());
         $semantics        = $semantic_manager->getSemantics();
 
         $artifact     = $this->updateArtifactTitleSemantic($current_user, $artifact, $semantics);
         $backlog_item = new AgileDashboard_Milestone_Backlog_BacklogItem($artifact);
         $backlog_item = $this->updateBacklogItemStatusSemantic($current_user, $artifact, $backlog_item, $semantics);
-        $backlog_item = $this->updateBacklogItemInitialEffortSemantic($current_user, $artifact, $backlog_item, $semantics);
+        $backlog_item = $this->updateBacklogItemInitialEffortSemantic(
+            $current_user,
+            $artifact,
+            $backlog_item,
+            $semantics
+        );
+        $parent_artifact = $artifact->getParent($current_user);
+        if ($parent_artifact !== null) {
+            $backlog_item->setParent($parent_artifact);
+        }
 
         return $backlog_item;
     }

plugins/agiledashboard/www/js/planning-v2/src/app/app-planning-controller.js
@@ -253,8 +253,7 @@ function PlanningController(
             return addSubmilestoneToSubmilestone(submilestone_id);
         }
 
-        var parent_item = (! _.isEmpty(self.current_milestone)) ? self.current_milestone : undefined;
-        NewTuleapArtifactModalService.showCreation(submilestone_type.id, parent_item, callback);
+        NewTuleapArtifactModalService.showCreation(submilestone_type.id, self.milestone_id, callback);
     }
 
     function addSubmilestoneToSubmilestone(submilestone_id) {
@@ -280,7 +279,7 @@ function PlanningController(
     }
 
     function showAddItemToSubMilestoneModal(item_type, parent_item) {
-        var compared_to;
+        let compared_to;
         if (! _.isEmpty(parent_item.content)) {
             compared_to = {
                 direction: "before",
@@ -289,21 +288,19 @@ function PlanningController(
         }
 
         function callback(item_id) {
-            var promise;
+            let promise;
             if (compared_to) {
                 promise = MilestoneService.addReorderToContent(parent_item.id, [item_id], compared_to);
             } else {
                 promise = MilestoneService.addToContent(parent_item.id, [item_id]);
             }
 
-            promise.then(function() {
+            return promise.then(() => {
                 return prependItemToSubmilestone(item_id, parent_item);
             });
-
-            return promise;
         }
 
-        NewTuleapArtifactModalService.showCreation(item_type.id, parent_item, callback);
+        NewTuleapArtifactModalService.showCreation(item_type.id, null, callback);
     }
 
     function prependItemToSubmilestone(child_item_id, parent_item) {

plugins/agiledashboard/www/js/planning-v2/src/app/app-planning-controller.spec.js
@@ -545,37 +545,23 @@ describe("PlanningController - ", function() {
         });
     });
 
-    describe("showAddSubmilestoneModal() -", function() {
-        var event, submilestone_type;
-        beforeEach(function() {
+    describe("showAddSubmilestoneModal() -", () => {
+        let event, submilestone_type;
+        beforeEach(() => {
             submilestone_type = { id: 82 };
             event = jasmine.createSpyObj("Click event", ["preventDefault"]);
-            NewTuleapArtifactModalService.showCreation.and.callFake(function(a, b, callback) {
-                callback(1668);
-            });
+            NewTuleapArtifactModalService.showCreation.and.callFake((a, b, callback) => callback(1668));
         });
 
-        it("Given any click event and a submilestone_type object, when I show the artifact modal, then the event's default action will be prevented and the NewTuleapArtifactModalService will be called with a callback", function() {
+        it("Given any click event and a submilestone_type object, then the event's default action will be prevented and the NewTuleapArtifactModalService will be called with a callback", () => {
             PlanningController.showAddSubmilestoneModal(event, submilestone_type);
 
-            expect(NewTuleapArtifactModalService.showCreation).toHaveBeenCalledWith(82, undefined, jasmine.any(Function));
+            expect(NewTuleapArtifactModalService.showCreation).toHaveBeenCalledWith(82, PlanningController.milestone_id, jasmine.any(Function));
         });
 
-        describe("callback -", function() {
-            var get_request;
-            beforeEach(function() {
-                get_request = $q.defer();
-                MilestoneService.getMilestone.and.returnValue(get_request.promise);
-            });
-
-            describe("Given that we were in a milestone context", function() {
-                var patch_request;
-                beforeEach(function() {
-                    patch_request = $q.defer();
-                    MilestoneService.patchSubMilestones.and.returnValue(patch_request.promise);
-                });
-
-                it(", when the artifact modal calls its callback, then the milestones collection will be updated", function() {
+        describe("callback -", () => {
+            describe("Given that we were in a milestone context", () => {
+                it(", when the artifact modal calls its callback, then the milestones collection will be updated", () => {
                     PlanningController.backlog.rest_route_id = 736;
                     PlanningController.milestones.content    = [
                         {
@@ -584,16 +570,16 @@ describe("PlanningController - ", function() {
                             semantic_status: "open"
                         }
                     ];
-
-                    PlanningController.showAddSubmilestoneModal(event, submilestone_type);
-                    patch_request.resolve();
-                    get_request.resolve({
+                    MilestoneService.patchSubMilestones.and.returnValue($q.when());
+                    MilestoneService.getMilestone.and.returnValue($q.when({
                         results: {
                             id             : 1668,
                             label          : "Sprint 2015-20",
                             semantic_status: "open"
                         }
-                    });
+                    }));
+
+                    PlanningController.showAddSubmilestoneModal(event, submilestone_type);
                     $scope.$apply();
 
                     expect(MilestoneService.patchSubMilestones).toHaveBeenCalledWith(736, [1668]);
@@ -613,31 +599,31 @@ describe("PlanningController - ", function() {
                 });
             });
 
-            it("Given that we were in a project context (Top Backlog), when the artifact modal calls its callback, then the MilestoneService will be called and the milestones collection will be updated", function() {
+            it("Given that we were in a project context (Top Backlog), when the artifact modal calls its callback, then the MilestoneService will be called and the milestones collection will be updated", () => {
                 spyOn(PlanningController, "isMilestoneContext").and.returnValue(false);
                 PlanningController.milestones.content = [
                     {
-                        id: 3118,
+                        id   : 3118,
                         label: "Sprint 2015-38"
                     }
                 ];
-
-                PlanningController.showAddSubmilestoneModal(event, submilestone_type);
-                get_request.resolve({
+                MilestoneService.getMilestone.and.returnValue($q.when({
                     results: {
-                        id: 1668,
+                        id   : 1668,
                         label: "Sprint 2015-20"
                     }
-                });
+                }));
+
+                PlanningController.showAddSubmilestoneModal(event, submilestone_type);
                 $scope.$apply();
 
                 expect(MilestoneService.getMilestone).toHaveBeenCalledWith(1668, jasmine.any(Object));
                 expect(PlanningController.milestones.content).toEqual([
                     {
-                        id: 1668,
+                        id   : 1668,
                         label: "Sprint 2015-20"
                     }, {
-                        id: 3118,
+                        id   : 3118,
                         label: "Sprint 2015-38"
                     }
                 ]);
@@ -645,16 +631,12 @@ describe("PlanningController - ", function() {
         });
     });
 
-    describe("showAddItemToSubMilestoneModal() -", function() {
-        var item_type, artifact, submilestone, get_backlog_item_request;
+    describe("showAddItemToSubMilestoneModal() -", () => {
+        let item_type, artifact, submilestone;
 
-        beforeEach(function() {
-            get_backlog_item_request = $q.defer();
+        beforeEach(() => {
             MilestoneService.updateInitialEffort.and.callThrough();
-            BacklogItemService.getBacklogItem.and.returnValue(get_backlog_item_request.promise);
-            NewTuleapArtifactModalService.showCreation.and.callFake(function(a, b, callback) {
-                callback(7488);
-            });
+            NewTuleapArtifactModalService.showCreation.and.callFake((a, b, callback) => callback(7488));
             artifact = {
                 backlog_item: {
                     id: 7488
@@ -662,41 +644,37 @@ describe("PlanningController - ", function() {
             };
         });
 
-        it("Given an item_type object and a milestone object, when I show the new artifact modal, then the NewTuleapArtifactModalService will be called with a callback", function() {
-            item_type = { id: 94 };
+        it("Given an item_type object and a milestone object, then the NewTuleapArtifactModalService will be called with a callback", () => {
+            item_type    = { id: 94 };
             submilestone = { id: 196 };
 
             PlanningController.showAddItemToSubMilestoneModal(item_type, submilestone);
 
-            expect(NewTuleapArtifactModalService.showCreation).toHaveBeenCalledWith(94, submilestone, jasmine.any(Function));
+            expect(NewTuleapArtifactModalService.showCreation).toHaveBeenCalledWith(94, null, jasmine.any(Function));
         });
 
-        describe("callback - Given a submilestone object and an item id,", function() {
-            var add_to_content_request;
-
-            beforeEach(function() {
-                add_to_content_request = $q.defer();
-                item_type              = { id: 413 };
+        describe("callback - Given a submilestone object and an item id,", () => {
+            beforeEach(() => {
+                item_type = { id: 413 };
                 submilestone = {
-                    id: 92,
+                    id     : 92,
                     content: []
                 };
             });
 
-            it("when the new artifact modal calls its callback, then the artifact will be prepended to the submilestone using the REST route and will be prepended to its content attribute", function() {
+            it("when the artifact modal calls its callback, then the artifact will be prepended to the submilestone using the REST route and will be prepended to its content attribute", () => {
                 submilestone.content = [
                     { id: 9402 }
                 ];
-                MilestoneService.addReorderToContent.and.returnValue(add_to_content_request.promise);
+                BacklogItemService.getBacklogItem.and.returnValue($q.when(artifact));
+                MilestoneService.addReorderToContent.and.returnValue($q.when());
 
                 PlanningController.showAddItemToSubMilestoneModal(item_type, submilestone);
-                get_backlog_item_request.resolve(artifact);
-                add_to_content_request.resolve();
                 $scope.$apply();
 
                 expect(MilestoneService.addReorderToContent).toHaveBeenCalledWith(92, [7488], {
                     direction: "before",
-                    item_id: 9402
+                    item_id  : 9402
                 });
                 expect(BacklogItemService.getBacklogItem).toHaveBeenCalledWith(7488);
                 expect(submilestone.content).toEqual([
@@ -705,12 +683,11 @@ describe("PlanningController - ", function() {
                 ]);
             });
 
-            it("and given that the submilestone's content was empty, when the new artifact modal calls its callback, then the artifact will be prepended to the submilestone using the REST route and will be prepended to its content attribute", function() {
-                MilestoneService.addToContent.and.returnValue(add_to_content_request.promise);
+            it("and given that the submilestone's content was empty, when the artifact modal calls its callback, then the artifact will be prepended to the submilestone using the REST route and will be prepended to its content attribute", () => {
+                BacklogItemService.getBacklogItem.and.returnValue($q.when(artifact));
+                MilestoneService.addToContent.and.returnValue($q.when());
 
                 PlanningController.showAddItemToSubMilestoneModal(item_type, submilestone);
-                get_backlog_item_request.resolve(artifact);
-                add_to_content_request.resolve();
                 $scope.$apply();
 
                 expect(MilestoneService.addToContent).toHaveBeenCalledWith(92, [7488]);

plugins/agiledashboard/www/js/planning-v2/src/app/backlog-item/backlog-item-details/backlog-item-details-controller.js
@@ -1,5 +1,4 @@
-import _ from 'lodash';
-
+import _                  from 'lodash';
 import BacklogFilterValue from '../../backlog-filter-terms.js';
 
 export default BacklogItemDetailsController;
@@ -55,33 +54,30 @@ function BacklogItemDetailsController(
     function showAddChildModal($event, item_type) {
         $event.preventDefault();
 
-        var callback = function(item_id) {
-            var promise = BacklogItemService.removeAddBacklogItemChildren(
+        function callback(item_id) {
+            return BacklogItemService.removeAddBacklogItemChildren(
                 undefined,
                 self.backlog_item.id,
                 [item_id]
-            );
-
-            promise.then(function() {
+            ).then(() => {
                 return appendItemToChildren(item_id, self.backlog_item);
             });
-
-            return promise;
-        };
+        }
 
         NewTuleapArtifactModalService.showCreation(
             item_type.id,
-            self.backlog_item,
+            self.backlog_item.id,
             callback
         );
     }
 
     function appendItemToChildren(child_item_id) {
-        return BacklogItemService.getBacklogItem(child_item_id).then(function(data) {
-            BacklogItemCollectionService.items[child_item_id] = data.backlog_item;
+        return BacklogItemService.getBacklogItem(child_item_id).then(({ backlog_item: child_item }) => {
+            child_item.parent = self.backlog_item;
+            BacklogItemCollectionService.items[child_item_id] = child_item;
 
             if (canBeAddedToChildren(child_item_id)) {
-                self.backlog_item.children.data.push(data.backlog_item);
+                self.backlog_item.children.data.push(child_item);
                 if (! self.backlog_item.has_children) {
                     self.backlog_item.children.loaded = true;
                 }

plugins/agiledashboard/www/js/planning-v2/src/app/backlog-item/backlog-item-details/backlog-item-details-controller.spec.js
@@ -45,9 +45,9 @@ describe("BacklogItemDetailsController -", function() {
         });
     });
 
-    describe("showAddChildModal() -", function() {
-        var event, item_type;
-        beforeEach(function() {
+    describe("showAddChildModal() -", () => {
+        let event, item_type;
+        beforeEach(() => {
             event       = jasmine.createSpyObj("Click event", ["preventDefault"]);
             item_type   = { id: 7 };
             BacklogItemDetailsController.backlog_item = {
@@ -64,38 +64,33 @@ describe("BacklogItemDetailsController -", function() {
             BacklogItemCollectionService.items[53] = BacklogItemDetailsController.backlog_item;
         });
 
-        it("Given an event and an item type, when I show the modal to add a child to the current backlog item, then the event's default action will be prevented and the NewTuleapArtifactModalService will be called with a callback", function() {
+        it("Given an event and an item type, then the event's default action will be prevented and the NewTuleapArtifactModalService will be called with a callback", () => {
             BacklogItemDetailsController.showAddChildModal(event, item_type);
 
             expect(event.preventDefault).toHaveBeenCalled();
             expect(NewTuleapArtifactModalService.showCreation).toHaveBeenCalledWith(
                 7,
-                BacklogItemDetailsController.backlog_item,
+                BacklogItemDetailsController.backlog_item.id,
                 jasmine.any(Function)
             );
         });
 
-        describe("callback -", function() {
-            var artifact, get_backlog_item_request, remove_add_backlog_item_children_request;
-            beforeEach(function() {
-                get_backlog_item_request                 = $q.defer();
-                remove_add_backlog_item_children_request = $q.defer();
-                NewTuleapArtifactModalService.showCreation.and.callFake(function(a, b, callback) {
-                    callback(207);
-                });
-                BacklogItemService.getBacklogItem.and.returnValue(get_backlog_item_request.promise);
+        describe("callback -", () => {
+            let artifact;
+            beforeEach(() => {
+                NewTuleapArtifactModalService.showCreation.and.callFake((a, b, callback) => callback(207));
                 artifact = {
                     backlog_item: {
                         id: 207
                     }
                 };
-                BacklogItemService.removeAddBacklogItemChildren.and.returnValue(remove_add_backlog_item_children_request.promise);
             });
 
-            it("When the new artifact modal calls its callback, then the artifact will be appended to the current backlog item's children using REST, it will be retrieved from the server, added to the items collection and appended to the current backlog item's children array", function() {
+            it("When the artifact modal calls its callback, then the artifact will be appended to the current backlog item's children using REST, it will be retrieved from the server, added to the items collection and appended to the current backlog item's children array", () => {
+                BacklogItemService.getBacklogItem.and.returnValue($q.when(artifact));
+                BacklogItemService.removeAddBacklogItemChildren.and.returnValue($q.when());
+
                 BacklogItemDetailsController.showAddChildModal(event, item_type);
-                get_backlog_item_request.resolve(artifact);
-                remove_add_backlog_item_children_request.resolve();
                 $scope.$apply();
 
                 expect(BacklogItemService.removeAddBacklogItemChildren).toHaveBeenCalledWith(
@@ -104,29 +99,27 @@ describe("BacklogItemDetailsController -", function() {
                     [207]
                 );
                 expect(BacklogItemService.getBacklogItem).toHaveBeenCalledWith(207);
-                expect(BacklogItemCollectionService.items[207]).toEqual({ id: 207 });
+                expect(BacklogItemCollectionService.items[207].id).toEqual(207);
+                expect(BacklogItemCollectionService.items[207].parent.id).toEqual(53);
                 expect(BacklogItemCollectionService.refreshBacklogItem).toHaveBeenCalledWith(53);
-                expect(BacklogItemDetailsController.backlog_item.children.data).toEqual([
-                    { id: 352 },
-                    { id: 207 }
-                ]);
+                const children_ids = BacklogItemDetailsController.backlog_item.children.data.map(({ id }) => id);
+                expect(children_ids).toEqual([352, 207]);
             });
 
-            it("Given that the current backlog item did not have children, when the new artifact modal calls its callback, then the artifact will be appended to the current backlog item's children and the children will be marked as loaded", function() {
+            it("Given that the current backlog item did not have children, when the new artifact modal calls its callback, then the artifact will be appended to the current backlog item's children and the children will be marked as loaded", () => {
                 BacklogItemDetailsController.backlog_item.children = {
                     loaded: false,
                     data  : []
                 };
                 BacklogItemDetailsController.backlog_item.has_children = false;
+                BacklogItemService.getBacklogItem.and.returnValue($q.when(artifact));
+                BacklogItemService.removeAddBacklogItemChildren.and.returnValue($q.when());
 
                 BacklogItemDetailsController.showAddChildModal(event, item_type);
-                get_backlog_item_request.resolve(artifact);
-                remove_add_backlog_item_children_request.resolve();
                 $scope.$apply();
 
-                expect(BacklogItemDetailsController.backlog_item.children.data).toEqual([
-                    { id: 207 }
-                ]);
+                const children_ids = BacklogItemDetailsController.backlog_item.children.data.map(({ id }) => id);
+                expect(children_ids).toEqual([207]);
                 expect(BacklogItemDetailsController.backlog_item.children.loaded).toBeTruthy();
             });
         });

commit 68c52c8b956c1f8df391153cbb0831cbb8d4b0dd

request #11098 Closing children when adding a new one in planning-v2
Bug introduced by commit a32033c4866b53ea4259af583e3e01a20f849c6f

How to test:
- Given a User story with Task children and given it is "expanded" to
show children in planning-v2. When you add a new Task child, using the
modal, then the User story won't "collapse" back. You won't have to
reload the children list.

Change-Id: Ia2c86d5600d4c4d23f6477d27511cbad8ce855e7

plugins/agiledashboard/www/js/planning-v2/src/app/backlog-item-collection/backlog-item-collection-service.js
@@ -25,8 +25,16 @@ function BacklogItemCollectionService(
         self.items[backlog_item_id].updating = true;
 
         return BacklogItemService.getBacklogItem(backlog_item_id).then(({ backlog_item }) => {
-            Object.assign(self.items[backlog_item_id], backlog_item);
-            self.items[backlog_item_id].updating = false;
+            const { label, initial_effort, card_fields, status, has_children } = backlog_item;
+
+            Object.assign(self.items[backlog_item_id], {
+                label,
+                initial_effort,
+                card_fields,
+                updating: false,
+                status,
+                has_children
+            });
 
             if (! backlog_item.has_children) {
                 self.items[backlog_item_id].children.collapsed = true;