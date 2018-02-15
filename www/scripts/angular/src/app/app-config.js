57fd71b6a1c59c2a63daa616c60d894183d3752e
Joris Masson <joris.masson@enalean.com>
2018-02-15 13:32:29 +0100

    request #11132 Open List field causes a javascript error in artifact modal
    Introduced by 21165919a8c551c560f8357e72f8b3f80e7b318b
    
    How to test:
    - When you create a bug using the modal, open list and date fields
    should init correctly. There should not be a javascript error
    - Same when you edit a test definition
    
    Change-Id: I3afe341673cbf90645638f9633d3b194d4f8eb50

--- www/scripts/angular/src/app/app-config.js
+++ www/scripts/angular/src/app/app-config.js
@@ -2,12 +2,24 @@ import './breadcrumb.tpl.html';
 
 export default TestManagementConfig;
 
-TestManagementConfig.$inject = ['$urlRouterProvider', '$breadcrumbProvider'];
+TestManagementConfig.$inject = [
+    '$urlRouterProvider',
+    '$breadcrumbProvider',
+    '$compileProvider'
+];
 
-function TestManagementConfig($urlRouterProvider, $breadcrumbProvider) {
+function TestManagementConfig(
+    $urlRouterProvider,
+    $breadcrumbProvider,
+    $compileProvider
+) {
     $urlRouterProvider.otherwise('/campaigns');
     $breadcrumbProvider.setOptions({
         prefixStateName: 'campaigns.milestone',
         templateUrl: 'breadcrumb.tpl.html'
     });
+
+    // To remove this setting, move all init() code
+    // of directive controllers to $onInit
+    $compileProvider.preAssignBindingsEnabled(true);
 }
