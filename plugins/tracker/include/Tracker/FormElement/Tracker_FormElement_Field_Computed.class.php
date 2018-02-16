Enalean/tuleap

commit d8049941e90be557f0bb16f2b40301aac9c94334

Total team effort is computed based on Agiledashboard planning definition
this is part of story #10670 a BurnUp chart based on Done stories

Computation is now related to agiledashboard planning.
Example of computation:

02/01/2018
R1 > US1
   > Bug1

Total initial effort is US1 + Bug1

05/01/2018
R1 > US1
   > US2
   > Bug2

Total initial effort is US1 + US2 + Bug1

Change-Id: I8bfbdc130b5e85c7479fb608afb528f7c4ee5998

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Computed.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -399,29 +399,30 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
      *
      * @return string
      */
-    public function fetchArtifactValueReadOnly(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null)
-    {
-        $purifier       = Codendi_HTMLPurifier::instance();
-        $computed_value = $this->getComputedValueWithNoStopOnManualValue($artifact);
+    public function fetchArtifactValueReadOnly(
+        Tracker_Artifact $artifact,
+        Tracker_Artifact_ChangesetValue $changeset_value = null
+    ) {
+        $value    = null;
+        $purifier = Codendi_HTMLPurifier::instance();
 
-        if ($value !== null) {
-            $value = $value->getValue();
+        if ($changeset_value && $changeset_value->isManualValue()) {
+            $value = $changeset_value->getValue();
         }
 
+        $computed_value = $this->getComputedValueWithNoStopOnManualValue($artifact);
         if ($computed_value === null) {
             $computed_value = $this->getFieldEmptyMessage();
         }
+
+
         $html_computed_value = '<span class="auto-computed">'. $purifier->purify($computed_value) .' (' .
             $GLOBALS['Language']->getText('plugin_tracker', 'autocomputed_field').')</span>';
 
-        if ($value === null) {
+        if ($changeset_value && ! $changeset_value->isManualValue()) {
             $value = $html_computed_value;
         }
 
-        return $this->fetchFieldReadOnly($value, $html_computed_value);
-    }
-
-    private function fetchFieldReadOnly($value, $html_computed_value) {
         return '<div class="auto-computed-label">'. $value. '</div>'.
             '<div class="back-to-autocompute">'.$html_computed_value.'</div>';
     }
@@ -444,9 +445,8 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
         Tracker_Artifact_ChangesetValue $value = null,
         $format = 'text'
     ) {
-        $current_user = UserManager::instance()->getCurrentUser();
-        $changeset    = $artifact->getLastChangesetWithFieldValue($this);
-        $value        = $this->getValueForChangeset($changeset, $current_user);
+        $changeset = $artifact->getLastChangesetWithFieldValue($this);
+        $value     = $this->getComputedValue($user, $changeset->getArtifact(), $changeset->getSubmittedOn());
 
         return ($value !== null) ? $value : "-";
     }
@@ -461,7 +461,7 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
     protected function fetchTooltipValue(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null) {
         $current_user = UserManager::instance()->getCurrentUser();
         $changeset    = $artifact->getLastChangesetWithFieldValue($this);
-        $value        = $this->getValueForChangeset($changeset, $current_user);
+        $value        = $this->getComputedValue($current_user, $changeset->getArtifact(), $changeset->getSubmittedOn());
 
         return ($value !== null) ? $value : "-";
     }
@@ -472,7 +472,8 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
         $artifact     = Tracker_ArtifactFactory::instance()->getArtifactById($artifact_id);
 
         $changeset = $this->getTrackerChangesetFactory()->getChangeset($artifact, $changeset_id);
-        return $this->getValueForChangeset($changeset, $current_user);
+
+        return $this->getComputedValue($current_user, $changeset->getArtifact(), $changeset->getSubmittedOn());
     }
 
     public function getSoapValue(PFUser $user, Tracker_Artifact_Changeset $changeset)
@@ -512,24 +513,13 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
         return $artifact_field_value_full_representation;
     }
 
-    private function getValueForChangeset(Tracker_Artifact_Changeset $artifact_changeset, PFUser $user)
-    {
-        $changeset = $artifact_changeset->getValue($this);
-        if ($changeset && $changeset->getNumeric() !== null) {
-            return $changeset->getNumeric();
-        } else {
-            $empty_array = array();
-            return $this->getComputedValue($user, $artifact_changeset->getArtifact(), null, $empty_array);
-        }
-    }
-
     /**
      * @return int|float|null
      */
     private function getManualValueForChangeset(Tracker_Artifact_Changeset $artifact_changeset)
     {
         $changeset_value = $artifact_changeset->getValue($this);
-        if ($changeset_value && $changeset_value->getNumeric() !== null) {
+        if ($changeset_value && $changeset_value->isManualValue()) {
             return $changeset_value->getNumeric();
         }
 
@@ -834,12 +824,27 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
 
     public function getChangesetValue($changeset, $value_id, $has_changed)
     {
-        $changeset_value = null;
-        if ($row = $this->getValueDao()->searchById($value_id, $this->id)->getRow()) {
-            $int_row_value   = $row['value'];
-            $changeset_value = new ChangesetValueComputed($value_id, $changeset, $this, $has_changed, $int_row_value);
+        $row = $this->getValueDao()->searchById($value_id, $this->id)->getRow();
+
+        if ($row && $row['value'] !== null) {
+            $is_manual_value = true;
+
+            return new ChangesetValueComputed(
+                $value_id,
+                $changeset,
+                $this,
+                $has_changed,
+                $row['value'],
+                $is_manual_value
+            );
         }
-        return $changeset_value;
+
+        $user  = $this->getCurrentUser();
+        $value = $this->getComputedValue($user, $changeset->getArtifact(), $changeset->getSubmittedOn());
+
+        $is_manual_value = false;
+
+        return new ChangesetValueComputed($value_id, $changeset, $this, $has_changed, $value, $is_manual_value);
     }
 
     private function getTrackerChangesetFactory()

plugins/agiledashboard/include/AgileDashboard/FormElement/Burnup.php
@@ -20,6 +20,7 @@
 
 namespace Tuleap\AgileDashboard\FormElement;
 
+use AgileDashboard_Semantic_InitialEffortFactory;
 use Codendi_HTMLPurifier;
 use EventManager;
 use PFUser;
@@ -27,7 +28,9 @@ use SystemEventManager;
 use TemplateRendererFactory;
 use Tracker_Artifact;
 use Tracker_Artifact_Changeset;
+use Tracker_Artifact_ChangesetFactoryBuilder;
 use Tracker_Artifact_ChangesetValue;
+use Tracker_ArtifactFactory;
 use Tracker_FormElement_Chart_Field_Exception;
 use Tracker_FormElement_Field;
 use Tracker_FormElement_Field_ReadOnly;
@@ -138,7 +141,7 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
             $artifact_value .= '<thead><tr><th></th><th>Team</th><th>Total</th></tr></thead><tbody>';
             foreach ($burnup_data->getEfforts() as $timestamp => $burnup_effort) {
                 $artifact_value .= '<tr>';
-                $date = new \DateTime("@$timestamp");
+                $date           = new \DateTime("@$timestamp");
                 $artifact_value .= '<td>' . $purifier->purify($date->format('d-m-Y')) . '</td>';
                 $artifact_value .= '<td>' . $purifier->purify($burnup_effort->getTeamEffort()) . '</td>';
                 $artifact_value .= '<td>' . $purifier->purify($burnup_effort->getTotalEffort()) . '</td>';
@@ -165,6 +168,7 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
             return '';
         }
         $renderer = TemplateRendererFactory::build()->getRenderer(AGILEDASHBOARD_TEMPLATE_DIR);
+
         return $renderer->renderToString(
             'formelement/burnup-cache-generation-modal',
             array('project_id' => $artifact->getTracker()->getGroupId(), 'artifact_id' => $artifact->getId())
@@ -293,7 +297,7 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
 
         $burnup_representation = new BurnupRepresentation($capacity, $burnup_data);
 
-        $field_representation  = new ArtifactFieldValueRepresentation();
+        $field_representation = new ArtifactFieldValueRepresentation();
         $field_representation->build($this->getId(), $this->getLabel(), $burnup_representation);
 
         return $field_representation;
@@ -384,23 +388,12 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
     }
 
     /**
-     * @return FieldCalculator
-     */
-    private function getTotalEffortCalculator()
-    {
-        return new FieldCalculator(
-            new BurnupTotalEffortCalculator(
-                new BurnupManualValuesAndChildrenListRetriever(new BurnupDao())
-            )
-        );
-    }
-
-    /**
      * @return BurnupDataBuilder
      */
     private function getBurnupDataBuilder()
     {
         $burnup_cache_dao = new BurnupCacheDao;
+
         return new BurnupDataBuilder(
             $this->getLogger(),
             new BurnupCacheChecker(
@@ -417,7 +410,22 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
             $this->getConfigurationValueRetriever(),
             $burnup_cache_dao,
             $this->getTeamEffortCalculator(),
-            $this->getTotalEffortCalculator()
+            $this->getBurnupCalculator()
+        );
+    }
+
+    /**
+     * @return BurnupCalculator
+     */
+    private function getBurnupCalculator()
+    {
+        $changeset_factory = Tracker_Artifact_ChangesetFactoryBuilder::build();
+
+        return new BurnupCalculator(
+            $changeset_factory,
+            Tracker_ArtifactFactory::instance(),
+            new BurnupDao(),
+            AgileDashboard_Semantic_InitialEffortFactory::instance()
         );
     }
 

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupCalculator.php
@@ -1,0 +1,109 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\FormElement;
+
+use AgileDashboard_Semantic_InitialEffortFactory;
+use Tracker_Artifact;
+use Tracker_Artifact_ChangesetFactory;
+use Tracker_ArtifactFactory;
+
+class BurnupCalculator
+{
+    /**
+     * @var Tracker_Artifact_ChangesetFactory
+     */
+    private $changeset_factory;
+    /**
+     * @var BurnupDao
+     */
+    private $burnup_dao;
+    /**
+     * @var Tracker_ArtifactFactory
+     */
+    private $artifact_factory;
+    /**
+     * @var AgileDashboard_Semantic_InitialEffortFactory
+     */
+    private $initial_effort_factory;
+
+
+    public function __construct(
+        Tracker_Artifact_ChangesetFactory $changeset_factory,
+        Tracker_ArtifactFactory $artifact_factory,
+        BurnupDao $burnup_dao,
+        AgileDashboard_Semantic_InitialEffortFactory $initial_effort_factory
+    ) {
+        $this->changeset_factory      = $changeset_factory;
+        $this->burnup_dao             = $burnup_dao;
+        $this->artifact_factory       = $artifact_factory;
+        $this->initial_effort_factory = $initial_effort_factory;
+    }
+
+    public function getValue($artifact_id, $timestamp)
+    {
+        $backlog_items = $this->getPlanningLinkedArtifactAtTimestamp(
+            $artifact_id,
+            $timestamp
+        );
+
+        $total_effort = 0;
+        /**
+         * @var $item \AgileDashboard_Milestone_Backlog_BacklogItem
+         */
+        foreach ($backlog_items as $item) {
+            $artifact     = $this->artifact_factory->getArtifactById($item['id']);
+            $total_effort += $this->getEffort($artifact, $timestamp);
+        }
+
+        return $total_effort;
+    }
+
+    private function getPlanningLinkedArtifactAtTimestamp(
+        $artifact_id,
+        $timestamp
+    ) {
+        return $this->burnup_dao->searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp);
+    }
+
+    /**
+     * @return float
+     */
+    private function getEffort(
+        Tracker_Artifact $artifact,
+        $timestamp
+    ) {
+        $semantic_initial_effort = $this->initial_effort_factory->getByTracker($artifact->getTracker());
+        /**
+         * @var $initial_effort_field \Tracker_FormElement_Field
+         */
+        $initial_effort_field = $semantic_initial_effort->getField();
+
+        $effort = 0;
+        if ($initial_effort_field) {
+            $changeset = $this->changeset_factory->getChangesetAtTimestamp($artifact, $timestamp);
+            if ($artifact->getValue($initial_effort_field, $changeset)) {
+                $effort = $artifact->getValue($initial_effort_field, $changeset)->getValue();
+            }
+        }
+
+        return (float) $effort;
+    }
+}

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupDao.php
@@ -257,4 +257,40 @@ class BurnupDao extends DataAccessObject
 
         return $this->retrieveFirstRow($sql);
     }
+
+    public function searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp)
+    {
+        $artifact_id = $this->da->escapeInt($artifact_id);
+        $timestamp   = $this->da->escapeInt($timestamp);
+
+        $sql = "SELECT linked_art.id AS id
+                FROM tracker_artifact AS parent_art
+                    INNER JOIN tracker ON parent_art.tracker_id = tracker.id
+                    INNER JOIN tracker_changeset AS changeset1 ON changeset1.artifact_id = parent_art.id
+                    LEFT JOIN  tracker_changeset AS changeset2
+                        ON (
+                            changeset2.artifact_id = parent_art.id
+                            AND changeset1.id < changeset2.id
+                            AND changeset2.submitted_on <= $timestamp
+                        )
+                    INNER JOIN tracker_field AS f
+                        ON (f.tracker_id = parent_art.tracker_id AND f.formElement_type = 'art_link' AND use_it = 1)
+                    INNER JOIN tracker_changeset_value AS cv
+                        ON (cv.changeset_id = changeset1.id AND cv.field_id = f.id)
+                    INNER JOIN tracker_changeset_value_artifactlink artlink
+                        ON (artlink.changeset_value_id = cv.id)
+                    INNER JOIN tracker_artifact AS linked_art
+                        ON (linked_art.id = artlink.artifact_id)
+                    INNER JOIN plugin_agiledashboard_planning
+                        ON plugin_agiledashboard_planning.planning_tracker_id = parent_art.tracker_id
+                    INNER JOIN plugin_agiledashboard_planning_backlog_tracker
+                        ON plugin_agiledashboard_planning_backlog_tracker.planning_id = plugin_agiledashboard_planning.id
+                        AND linked_art.tracker_id = plugin_agiledashboard_planning_backlog_tracker.tracker_id
+                WHERE parent_art.id = $artifact_id
+                    AND tracker.deletion_date IS NULL
+                    AND changeset2.id IS NULL
+                    AND changeset1.submitted_on <= $timestamp";
+
+        return $this->retrieve($sql);
+    }
 }

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupDataBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -51,10 +51,11 @@ class BurnupDataBuilder
      * @var FieldCalculator
      */
     private $team_effort_team_calculator;
+
     /**
-     * @var FieldCalculator
+     * @var BurnupCalculator
      */
-    private $total_effort_team_calculator;
+    private $burnup_calculator;
 
     public function __construct(
         Logger $logger,
@@ -62,14 +63,14 @@ class BurnupDataBuilder
         ChartConfigurationValueRetriever $field_retriever,
         BurnupCacheDao $burnup_cache_dao,
         FieldCalculator $team_effort_team_calculator,
-        FieldCalculator $total_effort_team_calculator
+        BurnupCalculator $burnup_calculator
     ) {
-        $this->logger                       = $logger;
-        $this->cache_checker                = $cache_checker;
-        $this->field_retriever              = $field_retriever;
-        $this->burnup_cache_dao             = $burnup_cache_dao;
-        $this->team_effort_team_calculator  = $team_effort_team_calculator;
-        $this->total_effort_team_calculator = $total_effort_team_calculator;
+        $this->logger                      = $logger;
+        $this->cache_checker               = $cache_checker;
+        $this->field_retriever             = $field_retriever;
+        $this->burnup_cache_dao            = $burnup_cache_dao;
+        $this->team_effort_team_calculator = $team_effort_team_calculator;
+        $this->burnup_calculator           = $burnup_calculator;
     }
 
     /**
@@ -127,32 +128,26 @@ class BurnupDataBuilder
 
         $now = time();
         if ($now >= $burnup_data->getTimePeriod()->getStartDate() && $now <= $burnup_data->getTimePeriod()->getEndDate()) {
-            $burnup_data->addEffort($this->getCurrentEffort($artifact), $now);
+            $burnup_data->addEffort($this->getCurrentEffort($artifact, $now), $now);
         }
     }
 
     /**
      * @return BurnupEffort
      */
-    private function getCurrentEffort(Tracker_Artifact $artifact)
+    private function getCurrentEffort(Tracker_Artifact $artifact, $now)
     {
         $stop_on_manual_value = true;
 
         $team_effort = $this->team_effort_team_calculator->calculate(
             array($artifact->getId()),
-            time(),
+            $now,
             $stop_on_manual_value,
             null,
             null
         );
 
-        $total_effort = $this->total_effort_team_calculator->calculate(
-            array($artifact->getId()),
-            time(),
-            $stop_on_manual_value,
-            null,
-            null
-        );
+        $total_effort = $this->burnup_calculator->getValue($artifact->getId(), $now);
 
         return new BurnupEffort($team_effort, $total_effort);
     }

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupTotalEffortCalculator.php
@@ -1,52 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-namespace Tuleap\AgileDashboard\FormElement;
-
-use Logger;
-use Tuleap\Tracker\FormElement\IProvideArtifactChildrenForComputedCalculation;
-
-class BurnupTotalEffortCalculator implements IProvideArtifactChildrenForComputedCalculation
-{
-    /**
-     * @var BurnupManualValuesAndChildrenListRetriever
-     */
-    private $burnup_calculator;
-
-    public function __construct(
-        BurnupManualValuesAndChildrenListRetriever $burnup_calculator
-    ) {
-        $this->burnup_calculator = $burnup_calculator;
-    }
-
-    public function fetchChildrenAndManualValuesOfArtifacts(
-        array $artifact_ids_to_fetch,
-        $timestamp,
-        $stop_on_manual_value,
-        $target_field_name,
-        $computed_field_id
-    ) {
-        return $this->burnup_calculator->getChildrenForBurnupWithComputedValuesAtGivenDate(
-            $artifact_ids_to_fetch,
-            $timestamp,
-            false
-        );
-    }
-}

plugins/agiledashboard/include/AgileDashboard/FormElement/SystemEvent/SystemEvent_BURNUP_DAILY.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -25,10 +25,11 @@ use SystemEvent;
 use TimePeriodWithoutWeekEnd;
 use Tuleap\Agiledashboard\FormElement\BurnupCacheDao;
 use Tuleap\Agiledashboard\FormElement\BurnupCacheDateRetriever;
+use Tuleap\AgileDashboard\FormElement\BurnupCalculator;
 use Tuleap\AgileDashboard\FormElement\BurnupDao;
 use Tuleap\Tracker\FormElement\FieldCalculator;
 
-class SystemEvent_BURNUP_DAILY extends SystemEvent
+class SystemEvent_BURNUP_DAILY extends SystemEvent // @codingStandardsIgnoreLine
 {
     const NAME = 'SystemEvent_BURNUP_DAILY';
 
@@ -43,9 +44,9 @@ class SystemEvent_BURNUP_DAILY extends SystemEvent
     private $logger;
 
     /**
-     * @var  FieldCalculator
+     * @var  BurnupCalculator
      */
-    private $total_effort_calculator;
+    private $burnup_calculator;
 
     /**
      * @var FieldCalculator
@@ -69,18 +70,18 @@ class SystemEvent_BURNUP_DAILY extends SystemEvent
 
     public function injectDependencies(
         BurnupDao $burnup_dao,
-        FieldCalculator $total_effort_calculator,
+        BurnupCalculator $burnup_calculator,
         FieldCalculator $team_effort_calculator,
         BurnupCacheDao $cache_dao,
         BackendLogger $logger,
         BurnupCacheDateRetriever $date_retriever
     ) {
-        $this->burnup_dao              = $burnup_dao;
-        $this->logger                  = $logger;
-        $this->total_effort_calculator = $total_effort_calculator;
-        $this->team_effort_calculator  = $team_effort_calculator;
-        $this->cache_dao               = $cache_dao;
-        $this->date_retriever          = $date_retriever;
+        $this->burnup_dao             = $burnup_dao;
+        $this->logger                 = $logger;
+        $this->burnup_calculator      = $burnup_calculator;
+        $this->team_effort_calculator = $team_effort_calculator;
+        $this->cache_dao              = $cache_dao;
+        $this->date_retriever         = $date_retriever;
     }
 
     public function process()
@@ -108,12 +109,9 @@ class SystemEvent_BURNUP_DAILY extends SystemEvent
                     "Calculating burnup for artifact #" . $burnup['id'] . ' at ' . date('Y-m-d H:i:s', $yesterday)
                 );
 
-                $total_effort = $this->total_effort_calculator->calculate(
-                    array($burnup['id']),
-                    $yesterday,
-                    true,
-                    null,
-                    null
+                $total_effort = $this->burnup_calculator->getValue(
+                    $burnup['id'],
+                    $yesterday
                 );
 
                 $team_effort = $this->team_effort_calculator->calculate(

plugins/agiledashboard/include/AgileDashboard/FormElement/SystemEvent/SystemEvent_BURNUP_GENERATE.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -26,17 +26,18 @@ use SystemEvent;
 use TimePeriodWithoutWeekEnd;
 use Tuleap\Agiledashboard\FormElement\BurnupCacheDao;
 use Tuleap\Agiledashboard\FormElement\BurnupCacheDateRetriever;
+use Tuleap\AgileDashboard\FormElement\BurnupCalculator;
 use Tuleap\AgileDashboard\FormElement\BurnupDao;
 use Tuleap\Tracker\FormElement\FieldCalculator;
 
-class SystemEvent_BURNUP_GENERATE extends SystemEvent
+class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreLine
 {
     const NAME = 'SystemEvent_BURNUP_GENERATE';
 
     /**
-     * @var FieldCalculator
+     * @var BurnupCalculator
      */
-    public $total_effort_calculator;
+    public $burnup_calculator;
     /**
      * @var FieldCalculator
      */
@@ -64,18 +65,18 @@ class SystemEvent_BURNUP_GENERATE extends SystemEvent
 
     public function injectDependencies(
         BurnupDao $burnup_dao,
-        FieldCalculator $total_effort_calculator,
+        BurnupCalculator $burnup_calculator,
         FieldCalculator $team_effort_calculator,
         BurnupCacheDao $cache_dao,
         BackendLogger $logger,
         BurnupCacheDateRetriever $date_retriever
     ) {
-        $this->burnup_dao              = $burnup_dao;
-        $this->logger                  = $logger;
-        $this->total_effort_calculator = $total_effort_calculator;
-        $this->team_effort_calculator  = $team_effort_calculator;
-        $this->cache_dao               = $cache_dao;
-        $this->date_retriever          = $date_retriever;
+        $this->burnup_dao             = $burnup_dao;
+        $this->logger                 = $logger;
+        $this->burnup_calculator      = $burnup_calculator;
+        $this->team_effort_calculator = $team_effort_calculator;
+        $this->cache_dao              = $cache_dao;
+        $this->date_retriever         = $date_retriever;
     }
 
     private function getArtifactIdFromParameters()
@@ -119,12 +120,9 @@ class SystemEvent_BURNUP_GENERATE extends SystemEvent
         foreach ($this->date_retriever->getWorkedDaysToCacheForPeriod($burnup, $yesterday) as $worked_day) {
             $this->logger->debug("Day " . date("Y-m-d H:i:s", $worked_day));
 
-            $total_effort = $this->total_effort_calculator->calculate(
-                array($burnup_information['id']),
-                $worked_day,
-                true,
-                null,
-                null
+            $total_effort = $this->burnup_calculator->getValue(
+                $burnup_information['id'],
+                $worked_day
             );
 
             $team_effort = $this->team_effort_calculator->calculate(

commit 4b7878e5a5528d4b088dab3b7de6c758d2f80787

request #11158: Computed field value might not be visible in the tracker artifact view
The value of a computed field is not visible in the tracker artifact view
if the field not editable at the artifact submission and is not edited afterwards.
Said otherwise, no value is displayed if the Tracker_Artifact_ChangesetValue is null.

This issue has been introduced by commit d8049941e90be557f0bb16f2b40301aac9c94334.

Change-Id: I2bdc7b2d844bf931798205213b11fa7c0d064db0

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Computed.class.php
@@ -419,7 +419,7 @@ class Tracker_FormElement_Field_Computed extends Tracker_FormElement_Field_Float
         $html_computed_value = '<span class="auto-computed">'. $purifier->purify($computed_value) .' (' .
             $GLOBALS['Language']->getText('plugin_tracker', 'autocomputed_field').')</span>';
 
-        if ($changeset_value && ! $changeset_value->isManualValue()) {
+        if ($value === null) {
             $value = $html_computed_value;
         }
 