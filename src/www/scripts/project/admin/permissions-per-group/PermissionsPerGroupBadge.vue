Enalean/tuleap

commit a98518b5c4e4d93decb26db27ff2a9a5921b9ab5

Introduce Load all button for AD planning
this is part of story #10064 Display permissions per group

Introduce a new button to be able to load all permissions of
agieldashboard plannings.

How to test:
 - click on button:
 - No planning is defined: global empty state
 - All groups: all ugroups of all planning are displayed
 - Search a ugroup without permission: ugroup empty state
 - Search a ugroup with permissions: corresponding ugroups should be displayed

Badge styling will be done in a dedicated commit

Change-Id: I94857c96a085fb2e2b940b58e4bc54f0519bcbc7

plugins/agiledashboard/www/js/permissions-per-group/po/fr.po
@@ -1,0 +1,28 @@
+msgid ""
+msgstr ""
+"Project-Id-Version: \n"
+"POT-Creation-Date: \n"
+"PO-Revision-Date: \n"
+"Last-Translator: Marie Ange GARNIER <marie-ange.garnier@enalean.com>\n"
+"Language-Team: \n"
+"Language: fr\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"X-Generator: Poedit 1.8.7.1\n"
+"Plural-Forms: nplurals=2; plural=(n > 1);\n"
+
+msgid "%s has no permission for agiledashboard plannings"
+msgstr "%s n'a pas de permission sur les plannings du tableau de bord agile"
+
+msgid "Agiledashboard has no planning defined"
+msgstr "Le tableau de bord agile n'a pas de planning défini"
+
+msgid "Planning"
+msgstr "Planning"
+
+msgid "View planning permissions"
+msgstr "Voir les permissions du planning"
+
+msgid "Who can prioritize?"
+msgstr "Qui peut prioriser ?"

src/www/scripts/project/admin/permissions-per-group/PermissionsPerGroupBadge.vue
@@ -1,0 +1,39 @@
+/**
+* Copyright (c) Enalean, 2018. All Rights Reserved.
+*
+* This file is a part of Tuleap.
+*
+* Tuleap is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* Tuleap is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+(<template>
+    <span v-bind:class="{
+            'tlp-badge-danger tlp-badge-outline': isProjectAdmin,
+            'tlp-badge-primary tlp-badge-outline': isStatic,
+            'tlp-badge-secondary': isCustom
+        }"
+    >{{ groupName }}</span>
+</template>)
+
+(<script>
+    export default {
+        name: 'PermissionsPerGroupBadge',
+        props: {
+            isProjectAdmin: Boolean,
+            isStatic: Boolean,
+            isCustom: Boolean,
+            groupName: String
+        },
+    };
+</script>)

plugins/agiledashboard/include/AgileDashboardRouter.class.php
@@ -22,6 +22,7 @@ use Tuleap\Agiledashboard\FormElement\BurnupCacheGenerator;
 use Tuleap\AgileDashboard\FormElement\FormElementController;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneChecker;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneDao;
+use Tuleap\AgileDashboard\PerGroup\AgileDashboardJSONPermissionsRetriever;
 use Tuleap\AgileDashboard\Planning\ScrumPlanningFilter;
 
 require_once 'common/plugin/Plugin.class.php';
@@ -95,6 +96,10 @@ class AgileDashboardRouter {
      * @var ScrumPlanningFilter
      */
     private $planning_filter;
+    /**
+     * @var AgileDashboardJSONPermissionsRetriever
+     */
+    private $permissions_retriever;
 
     public function __construct(
         Plugin $plugin,
@@ -109,7 +114,8 @@ class AgileDashboardRouter {
         PlanningPermissionsManager $planning_permissions_manager,
         AgileDashboard_HierarchyChecker $hierarchy_checker,
         ScrumForMonoMilestoneChecker $scrum_mono_milestone_checker,
-        ScrumPlanningFilter $planning_filter
+        ScrumPlanningFilter $planning_filter,
+        AgileDashboardJSONPermissionsRetriever $permissions_retriever
     ) {
         $this->plugin                       = $plugin;
         $this->milestone_factory            = $milestone_factory;
@@ -124,6 +130,7 @@ class AgileDashboardRouter {
         $this->hierarchy_checker            = $hierarchy_checker;
         $this->scrum_mono_milestone_checker = $scrum_mono_milestone_checker;
         $this->planning_filter              = $planning_filter;
+        $this->permissions_retriever        = $permissions_retriever;
     }
 
     /**
@@ -212,6 +219,20 @@ class AgileDashboardRouter {
                 break;
             case 'burnup-cache-generate':
                 $this->buildFormElementController()->forceBurnupCacheGeneration($request);
+                break;
+            case 'permission-per-group':
+                if (! $request->getCurrentUser()->isAdmin($request->getProject()->getID())) {
+                    $GLOBALS['Response']->send400JSONErrors(
+                        array(
+                            'error' => dgettext(
+                                'tuleap-agiledashboard',
+                                "You don't have permissions to see user groups."
+                            )
+                        )
+                    );
+                }
+
+                $this->permissions_retriever->retrieve($request->getProject(), $request->getCurrentUser(), $request->get('selected_ugroup_id'));
                 break;
             case 'index':
             default:

plugins/agiledashboard/include/AgileDashboardRouterBuilder.php
@@ -24,8 +24,12 @@ use Tuleap\AgileDashboard\MonoMilestone\MonoMilestoneBacklogItemDao;
 use Tuleap\AgileDashboard\MonoMilestone\MonoMilestoneItemsFinder;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneChecker;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneDao;
+use Tuleap\AgileDashboard\PerGroup\AgileDashboardJSONPermissionsRetriever;
+use Tuleap\AgileDashboard\PerGroup\AgileDashboardPermissionsRepresentationBuilder;
+use Tuleap\AgileDashboard\PerGroup\PlanningPermissionsRepresentationBuilder;
 use Tuleap\AgileDashboard\Planning\ScrumPlanningFilter;
 use Tuleap\AgileDashboard\REST\v1\BacklogItemRepresentationFactory;
+use Tuleap\Project\Admin\Permission\PermissionPerGroupUGroupRepresentationBuilder;
 
 class AgileDashboardRouterBuilder {
 
@@ -98,6 +102,7 @@ class AgileDashboardRouterBuilder {
 
         $mono_milestone_checker = new ScrumForMonoMilestoneChecker(new ScrumForMonoMilestoneDao(), $planning_factory);
 
+        $ugroup_manager = new UGroupManager();
         return new AgileDashboardRouter(
             $plugin,
             $milestone_factory,
@@ -114,7 +119,19 @@ class AgileDashboardRouterBuilder {
             new PlanningPermissionsManager(),
             $this->getHierarchyChecker(),
             $mono_milestone_checker,
-            new ScrumPlanningFilter($mono_milestone_checker, $planning_factory)
+            new ScrumPlanningFilter($mono_milestone_checker, $planning_factory),
+            new AgileDashboardJSONPermissionsRetriever(
+                new AgileDashboardPermissionsRepresentationBuilder(
+                    $ugroup_manager, $planning_factory,
+                    new PlanningPermissionsRepresentationBuilder(
+                        new PlanningPermissionsManager(),
+                        PermissionsManager::instance(),
+                        new PermissionPerGroupUGroupRepresentationBuilder(
+                            $ugroup_manager
+                        )
+                    )
+                )
+            )
         );
     }
 

plugins/agiledashboard/include/PerGroup/AgileDashboardJSONPermissionsRetriever.php
@@ -1,0 +1,62 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\PerGroup;
+
+use AgileDashboardPlugin;
+use PFUser;
+use Project;
+
+class AgileDashboardJSONPermissionsRetriever
+{
+    /**
+     * @var AgileDashboardPermissionsRepresentationBuilder
+     */
+    private $representation_builder;
+
+    public function __construct(AgileDashboardPermissionsRepresentationBuilder $representation_builder)
+    {
+        $this->representation_builder = $representation_builder;
+    }
+
+    public function retrieve(Project $project, PFUser $user, $selected_ugroup_id = null)
+    {
+        if (! $project->usesService(AgileDashboardPlugin::PLUGIN_SHORTNAME)) {
+            $GLOBALS['Response']->send400JSONErrors(
+                array(
+                    'error' => dgettext(
+                        'tuleap-agiledashboard',
+                        "Service Agiledashboard is disabled."
+                    )
+                )
+            );
+        }
+
+        $permission_representation = $this->representation_builder->build(
+            $project,
+            $user,
+            $selected_ugroup_id
+        );
+
+        $GLOBALS['Response']->sendJSON(
+            $permission_representation
+        );
+    }
+}

plugins/agiledashboard/include/PerGroup/AgileDashboardPermissionsRepresentation.php
@@ -1,0 +1,39 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\PerGroup;
+
+class AgileDashboardPermissionsRepresentation
+{
+    /**
+     * @var PlanningPermissionRepresentation[]
+     */
+    public $plannings_permissions;
+
+    /**
+     * AgileDashboardPermissionRepresentation constructor.
+     *
+     * @param PlanningPermissionRepresentation[] $permissions
+     */
+    public function __construct(array $permissions)
+    {
+        $this->plannings_permissions = $permissions;
+    }
+}

plugins/agiledashboard/include/PerGroup/AgileDashboardPermissionsRepresentationBuilder.php
@@ -1,0 +1,88 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\PerGroup;
+
+use PFUser;
+use PlanningFactory;
+use Project;
+use ProjectUGroup;
+use UGroupManager;
+
+class AgileDashboardPermissionsRepresentationBuilder
+{
+    /**
+     * @var UGroupManager
+     */
+    private $ugroup_manager;
+
+    /**
+     * @var PlanningFactory
+     */
+    private $planning_factory;
+    /**
+     * @var PlanningPermissionsRepresentationBuilder
+     */
+    private $planning_builder;
+
+    public function __construct(
+        UGroupManager $ugroup_manager,
+        PlanningFactory $planning_factory,
+        PlanningPermissionsRepresentationBuilder $planning_builder
+    ) {
+        $this->ugroup_manager   = $ugroup_manager;
+        $this->planning_factory = $planning_factory;
+        $this->planning_builder = $planning_builder;
+    }
+
+    public function build(
+        Project $project,
+        PFUser $user,
+        $ugroup_id = null
+    ) {
+        $ugroup = $this->ugroup_manager->getUGroup($project, $ugroup_id);
+
+        return new AgileDashboardPermissionsRepresentation(
+            $this->getPlanningPermissionsRepresentation(
+                $project,
+                $user,
+                $ugroup
+            )
+        );
+    }
+
+    private function getPlanningPermissionsRepresentation(
+        Project $project,
+        PFUser $user,
+        ProjectUGroup $user_group = null
+    ) {
+        $plannings      = $this->planning_factory->getPlannings($user, $project->getID());
+        $planning_names = array();
+
+        foreach ($plannings as $project_planning) {
+            $representation   = $this->planning_builder->build($project, $project_planning, $user_group);
+            if ($representation) {
+                $planning_names[] = $representation;
+            }
+        }
+
+        return $planning_names;
+    }
+}

plugins/agiledashboard/include/PerGroup/PlanningPermissionRepresentation.php
@@ -1,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\AgileDashboard\PerGroup;
+
+use Tuleap\Project\Admin\Permission\PermissionPerGroupUGroupRepresentation;
+
+class PlanningPermissionRepresentation
+{
+    /**
+     * @var String
+     */
+    public $quick_link;
+    /**
+     * @var String
+     */
+    public $name;
+    /**
+     * @var PermissionPerGroupUGroupRepresentation[]
+     */
+    public $ugroups;
+
+    public function __construct($quick_link, $name, array $ugroups)
+    {
+        $this->quick_link = $quick_link;
+        $this->name       = $name;
+        $this->ugroups    = $ugroups;
+    }
+}

commit 6d92fe44d5818fee39357437134b5c7ef7be26e9

Fix generate-po.sh broken by PermissionsPerGroupBadge vue component
Part of story #10064 Display permissions per group

Problem introduced in a98518b5c4e4d93decb26db27ff2a9a5921b9ab5

Generate po is broken because it doesn't support multi-line in vue
templates even though we put braces around <template> in the vue file.

How to test:
- Run 'make generate-po'
--> It works

Change-Id: I86f226b7ad5905c4ceec2311aa84fed500721c74

plugins/agiledashboard/www/js/permissions-per-group/po/fr.po
@@ -12,6 +12,7 @@ msgstr ""
 "X-Generator: Poedit 1.8.7.1\n"
 "Plural-Forms: nplurals=2; plural=(n > 1);\n"
 
+#, javascript-format
 msgid "%s has no permission for agiledashboard plannings"
 msgstr "%s n'a pas de permission sur les plannings du tableau de bord agile"
 

src/www/scripts/project/admin/permissions-per-group/PermissionsPerGroupBadge.vue
@@ -18,12 +18,9 @@
 */
 
 (<template>
-    <span v-bind:class="{
-            'tlp-badge-danger tlp-badge-outline': isProjectAdmin,
-            'tlp-badge-primary tlp-badge-outline': isStatic,
-            'tlp-badge-secondary': isCustom
-        }"
-    >{{ groupName }}</span>
+    <span v-bind:class="{ 'tlp-badge-danger tlp-badge-outline': isProjectAdmin, 'tlp-badge-primary tlp-badge-outline': isStatic, 'tlp-badge-secondary': isCustom }">
+        {{ groupName }}
+    </span>
 </template>)
 
 (<script>