Enalean/tuleap

commit 711deb26bf16190f16fad4d70bfcd618618f4445

Manage list of supported widgets
Need a big refactoring because we actually had to delegate the management
of the configuration to plugins themselves.

Basically there are 2 kind of supported widgets so far:

- Core widgets
- Kanban

Each subsequent plugin will have to add a specific code to grant the import.

Part of request #11076 Project dashboards should be part of xml import.

Change-Id: I290be43ec6d61f2b5704ef3046ae6c2ba36a49fd

plugins/agiledashboard/include/AgileDashboard/AgileDashboardController.class.php
@@ -336,7 +336,8 @@ class AgileDashboard_Controller extends MVC2_PluginController {
                                     )
                                 )
                             ),
-                            $logger
+                            $logger,
+                            $this->event_manager
                         )
                     )
                 ),

plugins/agiledashboard/include/AgileDashboard/Kanban/KanbanXmlImporter.php
@@ -99,12 +99,12 @@ class KanbanXmlImporter
                 (string) $attrs["name"],
                 $tracker_mapping[(string) $attrs["tracker_id"]]
             );
-            $mappings_registry->addWidget((string)$attrs['ID'], $kanban_id);
 
-            $kanban    = $this->dashboard_kanban_factory->getKanban(
+            $kanban = $this->dashboard_kanban_factory->getKanban(
                 $user,
                 $kanban_id
             );
+            $mappings_registry->addReference((string)$attrs['ID'], $kanban);
 
             foreach ($xml_configuration as $xml_columns) {
                 $columns_attrs = $xml_columns->attributes();

plugins/agiledashboard/include/AgileDashboard/Widget/WidgetKanbanCreator.php
@@ -44,15 +44,6 @@ class WidgetKanbanCreator
     public function create(Codendi_Request $request, $owner_id, $owner_type)
     {
         $kanban         = $request->get('kanban');
-        if ($kanban === false) {
-            throw new \RuntimeException('Missing parameter: kanban');
-        }
-        if (! isset($kanban['id'])) {
-            throw new \RuntimeException('Missing parameter: kanban[id]');
-        }
-        if (! isset($kanban['title'])) {
-            throw new \RuntimeException('Missing parameter: kanban[title]');
-        }
         $kanban_id      = $kanban['id'];
         $kanban_title   = $kanban['title'];
 

plugins/agiledashboard/include/AgileDashboard/Widget/WidgetKanbanXMLImporter.php
@@ -1,0 +1,97 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\AgileDashboard\Widget;
+
+use Tuleap\Widget\Event\ConfigureAtXMLImport;
+use Tuleap\XML\MappingsRegistry;
+
+class WidgetKanbanXMLImporter
+{
+    /**
+     * @param ConfigureAtXMLImport $event
+     * @throws \RuntimeException
+     */
+    public function configureWidget(ConfigureAtXMLImport $event)
+    {
+        $content_id = $event->getWidget()->create($this->getRequest($event));
+        $event->setContentId($content_id);
+        $event->setWidgetIsConfigured();
+    }
+
+    /**
+     * @param ConfigureAtXMLImport $event
+     * @throws \RuntimeException
+     * @return \Codendi_Request
+     */
+    private function getRequest(ConfigureAtXMLImport $event)
+    {
+        return new \Codendi_Request(
+            $this->getParametersFromXML($event->getXML(), $event->getMappingsRegistry())
+        );
+    }
+
+    /**
+     * @param \SimpleXMLElement $xml
+     * @param MappingsRegistry $mapping_registry
+     * @throws \RuntimeException
+     * @return string[][]
+     */
+    private function getParametersFromXML(\SimpleXMLElement $xml, MappingsRegistry $mapping_registry)
+    {
+        $params = [
+            'kanban' => [
+                'title' => '',
+            ],
+        ];
+        if (isset($xml->preference)) {
+            foreach ($xml->preference as $preference) {
+                $preference_name = trim((string)$preference['name']);
+                $this->setNameReferenceParameters($preference, $mapping_registry, $preference_name, $params);
+            }
+        }
+        if (! isset($params['kanban']['id'])) {
+            throw new \RuntimeException("Kanban id is missing");
+        }
+        return $params;
+    }
+
+    /**
+     * @param \SimpleXMLElement $preference
+     * @param MappingsRegistry $mapping_registry
+     * @param string $preference_name
+     * @param array $params
+     *
+     * @throws \RuntimeException
+     */
+    private function setNameReferenceParameters(\SimpleXMLElement $preference, MappingsRegistry $mapping_registry, $preference_name, array &$params)
+    {
+        foreach ($preference->reference as $reference) {
+            $key = trim((string)$reference['name']);
+            $ref = trim((string)$reference['REF']);
+            $kanban = $mapping_registry->getReference($ref);
+            if ($kanban === null) {
+                throw new \RuntimeException("Reference $ref for kanban widget was not found");
+            }
+            $params[$preference_name][$key] = $kanban->getId();
+        }
+    }
+}

plugins/agiledashboard/include/Planning/PlanningController.class.php
@@ -488,7 +488,8 @@ class Planning_Controller extends MVC2_PluginController {
                         )
                     )
                 ),
-                $logger
+                $logger,
+                $event_manager
             )
         );
 

plugins/agiledashboard/include/agiledashboardPlugin.class.php
@@ -99,6 +99,7 @@ class AgileDashboardPlugin extends Plugin {
             $this->addHook(\Tuleap\Widget\Event\GetWidget::NAME);
             $this->addHook(\Tuleap\Widget\Event\GetUserWidgetList::NAME);
             $this->addHook(\Tuleap\Widget\Event\GetProjectWidgetList::NAME);
+            $this->addHook(\Tuleap\Widget\Event\ConfigureAtXMLImport::NAME);
             $this->addHook(TRACKER_EVENT_INCLUDE_CSS_FILE);
             $this->addHook(TRACKER_EVENT_TRACKERS_DUPLICATED, 'tracker_event_trackers_duplicated', false);
             $this->addHook(TRACKER_EVENT_BUILD_ARTIFACT_FORM_ACTION, 'tracker_event_build_artifact_form_action', false);
@@ -462,6 +463,14 @@ class AgileDashboardPlugin extends Plugin {
     public function getProjectWidgetList(\Tuleap\Widget\Event\GetProjectWidgetList $event)
     {
         $event->addWidget(ProjectKanban::NAME);
+    }
+
+    public function configureAtXMLImport(\Tuleap\Widget\Event\ConfigureAtXMLImport $event)
+    {
+        if ($event->getWidget()->getId() === ProjectKanban::NAME) {
+            $xml_import = new \Tuleap\AgileDashboard\Widget\WidgetKanbanXMLImporter();
+            $xml_import->configureWidget($event);
+        }
     }
 
     private function redirectOrAppend(Codendi_Request $request, Tracker_Artifact $artifact, Tracker_Artifact_Redirect $redirect, $requested_planning, Tracker_Artifact $last_milestone_artifact = null) {

plugins/agiledashboard/include/autoload.php
@@ -2,7 +2,7 @@
 // @codingStandardsIgnoreFile
 // @codeCoverageIgnoreStart
 // this is an autogenerated file - do not edit
-function autoload5049622fd7ba544e2fcb74af4ce06026($class) {
+function autoload78b08491754a38195fed43e6f2b8fd3a($class) {
     static $classes = null;
     if ($classes === null) {
         $classes = array(
@@ -325,7 +325,8 @@ function autoload5049622fd7ba544e2fcb74af4ce06026($class) {
             'tuleap\\agiledashboard\\widget\\widgetkanbandao' => '/AgileDashboard/Widget/WidgetKanbanDao.php',
             'tuleap\\agiledashboard\\widget\\widgetkanbandeletor' => '/AgileDashboard/Widget/WidgetKanbanDeletor.php',
             'tuleap\\agiledashboard\\widget\\widgetkanbanpresenter' => '/AgileDashboard/Widget/WidgetKanbanPresenter.php',
-            'tuleap\\agiledashboard\\widget\\widgetkanbanretriever' => '/AgileDashboard/Widget/WidgetKanbanRetriever.php'
+            'tuleap\\agiledashboard\\widget\\widgetkanbanretriever' => '/AgileDashboard/Widget/WidgetKanbanRetriever.php',
+            'tuleap\\agiledashboard\\widget\\widgetkanbanxmlimporter' => '/AgileDashboard/Widget/WidgetKanbanXMLImporter.php'
         );
     }
     $cn = strtolower($class);
@@ -333,5 +334,5 @@ function autoload5049622fd7ba544e2fcb74af4ce06026($class) {
         require dirname(__FILE__) . $classes[$cn];
     }
 }
-spl_autoload_register('autoload5049622fd7ba544e2fcb74af4ce06026');
+spl_autoload_register('autoload78b08491754a38195fed43e6f2b8fd3a');
 // @codeCoverageIgnoreEnd

plugins/agiledashboard/phpunit/AgileDashboard/Widget/WidgetKanbanXMLImporterTest.php
@@ -1,0 +1,154 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\AgileDashboard\Widget;
+
+require_once __DIR__.'/../../boostrap.php';
+
+use PHPUnit\Framework\TestCase;
+use Tuleap\Widget\Event\ConfigureAtXMLImport;
+use Mockery;
+use Tuleap\XML\MappingsRegistry;
+
+class WidgetKanbanXMLImporterTest extends TestCase
+{
+    /**
+     * @var MappingsRegistry
+     */
+    private $registry;
+    /**
+     * @var Mockery
+     */
+    private $widget;
+
+    public function setUp()
+    {
+        $kanban = new \AgileDashboard_Kanban(
+            20001,
+            101,
+            'Kanban name'
+        );
+
+        $this->registry = new MappingsRegistry();
+        $this->registry->addReference('K123', $kanban);
+
+        $this->widget = Mockery::mock(\Widget::class);
+    }
+
+    public function tearDown()
+    {
+        if ($container = Mockery::getContainer()) {
+            $this->addToAssertionCount($container->mockery_getExpectationCount());
+        }
+
+        Mockery::close();
+        parent::tearDown();
+    }
+
+    public function testItGetReferenceFromRegistry()
+    {
+        $xml = new \SimpleXMLElement(
+            '<?xml version="1.0" encoding="UTF-8"?>
+             <widget name="plugin_agiledashboard_projects_kanban">
+                <preference name="kanban">
+                    <reference name="id" REF="K123"/>
+                </preference>
+              </widget>'
+        );
+
+        $this->widget->shouldReceive('create')->with(\Mockery::on(function (\Codendi_Request $request) {
+            if ($request->get('kanban') &&
+                $request->getInArray('kanban', 'id') === 20001 &&
+                $request->getInArray('kanban', 'title') === '') {
+                return true;
+            }
+            return false;
+        }))->once()->andReturn(30003);
+
+        $event = new ConfigureAtXMLImport(
+            $this->widget,
+            $xml,
+            $this->registry
+        );
+
+        $importer = new WidgetKanbanXMLImporter();
+        $importer->configureWidget($event);
+
+        $this->assertTrue($event->isWidgetConfigured());
+        $this->assertEquals(30003, $event->getContentId());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testItShouldThrowExceptionsWhenIDIsNotPresent()
+    {
+        $xml = new \SimpleXMLElement(
+            '<?xml version="1.0" encoding="UTF-8"?>
+             <widget name="plugin_agiledashboard_projects_kanban">
+                <preference name="kanban">
+                    <value name="title">Da kanban</value>
+                </preference>
+              </widget>'
+        );
+
+        $this->widget->shouldNotReceive('create');
+
+        $event = new ConfigureAtXMLImport(
+            $this->widget,
+            $xml,
+            $this->registry
+        );
+
+        $importer = new WidgetKanbanXMLImporter();
+        $importer->configureWidget($event);
+
+        $this->assertFalse($event->isWidgetConfigured());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testItShouldThrowExceptionsWhenKanbanIsNotReferenced()
+    {
+        $xml = new \SimpleXMLElement(
+            '<?xml version="1.0" encoding="UTF-8"?>
+             <widget name="plugin_agiledashboard_projects_kanban">
+                <preference name="kanban">
+                    <reference name="id" REF="K2222"/>
+                </preference>
+              </widget>'
+        );
+
+        $this->widget->shouldNotReceive('create');
+
+        $event = new ConfigureAtXMLImport(
+            $this->widget,
+            $xml,
+            $this->registry
+        );
+
+        $importer = new WidgetKanbanXMLImporter();
+        $importer->configureWidget($event);
+
+        $this->assertFalse($event->isWidgetConfigured());
+    }
+}

commit 7fa72aa17a3d5909cccf1f73093cfbc63cf5bef2

request #11191 Start scrum broken
Fix regression introduced by 711deb26bf16190f16fad4d70bfcd618618f4445

Change-Id: Id52188c63b0fb4425f0850be1e26ae45a2f89743

plugins/agiledashboard/include/AgileDashboard/AgileDashboardController.class.php
@@ -324,18 +324,10 @@ class AgileDashboard_Controller extends MVC2_PluginController {
                         new UploadedLinksUpdater(new UploadedLinksDao(), FRSLog::instance()),
                         new ProjectDashboardXMLImporter(
                             new ProjectDashboardSaver(
-                                new ProjectDashboardDao(
-                                    new DashboardWidgetDao(
-                                        new WidgetFactory(
-                                            $user_manager,
-                                            new User_ForgeUserGroupPermissionsManager(
-                                                new User_ForgeUserGroupPermissionsDao()
-                                            ),
-                                            $this->event_manager
-                                        )
-                                    )
-                                )
+                                $project_dao
                             ),
+                            $widget_factory,
+                            $widget_dao,
                             $logger,
                             $this->event_manager
                         )