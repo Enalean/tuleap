1183c6986b99c7cf3b59b524abcd0c23f8c09c20
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-02-26 16:46:44 +0100

    request #11193: Resending activation emails does not work when users also need to be approved by a site administrator
    Even if sys_user_approval is set to 1, resending activation emails should work.
    
    Change-Id: I6880717380d70196b05a20b9dddcba4f523af5c0

--- src/www/admin/approve_pending_users.php
+++ src/www/admin/approve_pending_users.php
@@ -159,31 +159,30 @@ $expiry_date = 0;
             }
         } else if ($action_select === 'resend_email') {
             $csrf_token->check();
-            if (ForgeConfig::get('sys_user_approval') == 0) {
-                $user_manager = UserManager::instance();
-                foreach ($users_array as $user_id) {
-                    $user = $user_manager->getUserById($user_id);
-                    if ($user === null) {
-                        continue;
-                    }
-                    if ($user->getStatus() !== PFUser::STATUS_PENDING && $user->getStatus() !== PFUser::STATUS_VALIDATED &&
-                        $user->getStatus() !== PFUser::STATUS_VALIDATED_RESTRICTED) {
-                        continue;
-                    }
+            $user_manager = UserManager::instance();
+            foreach ($users_array as $user_id) {
+                $user = $user_manager->getUserById($user_id);
+                if ($user === null) {
+                    continue;
+                }
+                if ($user->getStatus() !== PFUser::STATUS_PENDING && $user->getStatus() !== PFUser::STATUS_VALIDATED &&
+                    $user->getStatus() !== PFUser::STATUS_VALIDATED_RESTRICTED) {
+                    continue;
+                }
 
-                    $is_mail_sent = send_new_user_email($user->getEmail(), $user->getUserName(), $user->getConfirmHash());
+                $is_mail_sent = send_new_user_email($user->getEmail(), $user->getUserName(), $user->getConfirmHash());
 
-                    if ($is_mail_sent) {
-                        $GLOBALS['Response']->addFeedback(
-                            Feedback::INFO,
-                            $Language->getText('admin_approve_pending_users', 'resend_mail_success', array($user->getEmail()))
-                        );
-                    } else {
-                        $GLOBALS['Response']->addFeedback(
-                            Feedback::INFO,
-                            $Language->getText('admin_approve_pending_users', 'resend_mail_error', array($user->getEmail()))
-                        );
-                    }
+                if ($is_mail_sent) {
+                    $GLOBALS['Response']->addFeedback(
+                        Feedback::INFO,
+                        $Language->getText('admin_approve_pending_users', 'resend_mail_success',
+                            array($user->getEmail()))
+                    );
+                } else {
+                    $GLOBALS['Response']->addFeedback(
+                        Feedback::INFO,
+                        $Language->getText('admin_approve_pending_users', 'resend_mail_error', array($user->getEmail()))
+                    );
                 }
             }
         }
