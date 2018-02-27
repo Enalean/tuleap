cd6e0dff3bfe684038995b624cf1d670a78464d1
Nicolas Terray <nicolas.terray@enalean.com>
2018-02-27 14:19:10 +0100

    Do not duplicate results with = OPEN()
    Part of story #10702: define a query with status semantic
    
    Take the same strategy used for commit #0d3514d8e682c1940aedbac37bcc81791c343db3
    so that we do not have duplicates when we search for example `@status =
    OPEN() or @title = 'typo'`
    
    Change-Id: Ieac5699b6b2d4e43ec85522d5efc476d7c6a1682

--- plugins/crosstracker/include/CrossTracker/Report/Query/Advanced/QueryBuilder/Metadata/Semantic/Status/EqualComparisonFromWhereBuilder.php
+++ plugins/crosstracker/include/CrossTracker/Report/Query/Advanced/QueryBuilder/Metadata/Semantic/Status/EqualComparisonFromWhereBuilder.php
@@ -20,6 +20,8 @@
 
 namespace Tuleap\CrossTracker\Report\Query\Advanced\QueryBuilder\Metadata\Semantic\Status;
 
+use ParagonIE\EasyDB\EasyStatement;
+use Tracker;
 use Tuleap\CrossTracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\Report\Query\Advanced\Grammar\Comparison;
 use Tuleap\Tracker\Report\Query\Advanced\Grammar\Metadata;
@@ -28,24 +30,36 @@ class EqualComparisonFromWhereBuilder implements FromWhereBuilder
 {
     public function getFromWhere(Metadata $metadata, Comparison $comparison, array $trackers)
     {
+        $tracker_ids = array_map(
+            function (Tracker $tracker) {
+                return $tracker->getId();
+            },
+            $trackers
+        );
+        $tracker_ids_statement = EasyStatement::open()->in('field.tracker_id IN (?*)', $tracker_ids);
+
         $from = "LEFT JOIN (
             tracker_changeset_value AS changeset_value_status
-            INNER JOIN tracker_semantic_status
-                ON (
-                    tracker_semantic_status.field_id = changeset_value_status.field_id
-                )
+            INNER JOIN (
+                SELECT DISTINCT field_id
+                FROM tracker_semantic_status AS field
+                WHERE $tracker_ids_statement
+            ) AS equal_status_field ON (equal_status_field.field_id = changeset_value_status.field_id)
             INNER JOIN tracker_changeset_value_list AS tracker_changeset_value_status
                 ON (
                     tracker_changeset_value_status.changeset_value_id = changeset_value_status.id
                 )
         ) ON (
-            tracker_semantic_status.tracker_id = tracker_artifact.tracker_id
-            AND changeset_value_status.changeset_id = tracker_artifact.last_changeset_id
+            changeset_value_status.changeset_id = tracker_artifact.last_changeset_id
         )";
 
         $where = "changeset_value_status.changeset_id IS NOT NULL
-            AND tracker_changeset_value_status.bindvalue_id = tracker_semantic_status.open_value_id";
+            AND tracker_changeset_value_status.bindvalue_id IN (
+                SELECT open_value_id
+                FROM tracker_semantic_status AS field
+                WHERE $tracker_ids_statement
+            )";
 
-        return new ParametrizedFromWhere($from, $where, [], []);
+        return new ParametrizedFromWhere($from, $where, $tracker_ids, $tracker_ids);
     }
 }
