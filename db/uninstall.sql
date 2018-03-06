9d173c92f4ccb25412ed112813dcd0dfa779499b
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-03-06 14:08:35 +0100

    Add missing entry in uninstall.sql
    Change-Id: I985211f3464dcfcc68c3289b5fc15fa606b8e39e

--- db/uninstall.sql
+++ db/uninstall.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS plugin_timesheeting_enabled_trackers;
 DROP TABLE IF EXISTS plugin_timesheeting_writers;
 DROP TABLE IF EXISTS plugin_timesheeting_readers;
+DROP TABLE IF EXISTS plugin_timesheeting_times;
