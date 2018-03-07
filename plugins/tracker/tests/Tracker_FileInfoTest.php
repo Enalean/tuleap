88335628912412006023f67d4fbc9ed01fe5b648
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-03-07 18:37:50 +0100

    request #11257 XML project import must not create tracker folder and content as root
    When creating a project with the XML project import script,
    if there is any attachment in the XML content,
    then the folder /var/lib/tuleap/tracker will be owned by root and some content
    will also be owned by root, which is wrong.
    
    The whole folder with its content must be owned by the http user.
    
    Change-Id: If20d9c4b1fdc092373d468eb630f31ebb9e90ca2

--- plugins/tracker/tests/Tracker_FileInfoTest.php
+++ plugins/tracker/tests/Tracker_FileInfoTest.php
@@ -1,26 +1,25 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2018. All Rights Reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once('bootstrap.php');
-Mock::generatePartial('Tracker_FileInfo', 'Tracker_FileInfoTestVersion', array('getFiletype'));
 
-Mock::generate('Tracker_FormElement_Field_File');
+require_once('bootstrap.php');
 
 class Tracker_FileInfo_CommonTest extends TuleapTestCase {
     protected $fixture_data_dir;
@@ -81,7 +80,7 @@ class Tracker_FileInfoTest extends Tracker_FileInfo_CommonTest {
     }
 
     function testIsImage() {
-        $fi = new Tracker_FileInfoTestVersion();
+        $fi = partial_mock(Tracker_FileInfo::class, array('getFiletype'));
         $fi->setReturnValueAt(0, 'getFiletype', 'image/png');
         $fi->setReturnValueAt(1, 'getFiletype', 'image/gif');
         $fi->setReturnValueAt(2, 'getFiletype', 'image/jpg');
@@ -157,10 +156,25 @@ class Tracker_FileInfo_PostUploadActionsTest extends Tracker_FileInfo_CommonTest
 
     private $thumbnails_dir;
 
-    public function setUp() {
+    public function setUp()
+    {
         parent::setUp();
+
         $this->thumbnails_dir = $this->working_directory.'/thumbnails';
         mkdir($this->thumbnails_dir);
+
+        $this->backend = mock(Backend::class);
+        Backend::setInstance(Backend::BACKEND, $this->backend);
+
+        Forgeconfig::store();
+    }
+
+    public function tearDown()
+    {
+        Backend::clearInstances();
+        Forgeconfig::restore();
+
+        parent::tearDown();
     }
 
     public function itCreatesThumbnailForPng() {
@@ -219,7 +233,16 @@ class Tracker_FileInfo_PostUploadActionsTest extends Tracker_FileInfo_CommonTest
         ));
 
     }
+
+    public function itEnsuresFilesIsOwnedByHttpUser()
+    {
+        copy($this->fixture_data_dir.'/logo.jpg', $this->working_directory.'/421');
+
+        $file_info_1 = new Tracker_FileInfo(421, $this->field, 0, '', '', '', 'image/jpg');
+        ForgeConfig::set('sys_http_user', 'user');
+
+        expect($this->backend)->changeOwnerGroupMode($this->working_directory.'/421', 'user', 'user', 0644)->once();
+
+        $file_info_1->postUploadActions();
+    }
 }
-
-
-?>
