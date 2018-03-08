c7eded49efe1cc61ba177103d0caca1f970f75c1
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-03-08 11:51:43 +0100

    Create script to extract statistics
    This is part of request #11258 Add cli script to extract stats
    
    This script can generate the CSV file through the cli.
    It must be run by either root or codendiadm.
    
    The generated CSV will always uses "," as separator.
    (there is no way for now to force the login in a script and get the
    csv separator preference properly.)
    
    Change-Id: I18e5c224b732a1331e88616a73c0105217fc2823

---
+++ plugins/statistics/bin/extract_service_usage.php
@@ -0,0 +1,109 @@
+#!/usr/share/tuleap/src/utils/php-launcher.sh
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Tuleap\SVN\DiskUsage\Collector as SVNCollector;
+use Tuleap\SVN\DiskUsage\Retriever as SVNRetriever;
+use Tuleap\CVS\DiskUsage\Retriever as CVSRetriever;
+use Tuleap\CVS\DiskUsage\Collector as CVSCollector;
+use Tuleap\CVS\DiskUsage\FullHistoryDao;
+
+require_once 'pre.php';
+require_once 'www/project/export/project_export_utils.php';
+
+if ($argc !== 4) {
+    fwrite(STDERR, "Usage: {$argv[0]} start_date end_date output". PHP_EOL);
+    exit(1);
+}
+
+$posix_user = posix_getpwuid(posix_geteuid());
+$sys_user   = $posix_user['name'];
+if ($sys_user !== 'root' && $sys_user !== 'codendiadm') {
+    fwrite(STDERR, 'Unsufficient privileges for user '.$sys_user.PHP_EOL);
+    exit(1);
+}
+
+$start_date = $argv[1];
+$end_date   = $argv[2];
+$output     = $argv[3];
+
+$rule_valid_date = new Rule_Date();
+
+if (! $rule_valid_date->isValid($start_date)) {
+    fwrite(STDERR, 'Start date is not valid. Expected format is yyyy-mm-dd.'. PHP_EOL);
+    exit(1);
+}
+
+if (! $rule_valid_date->isValid($end_date)) {
+    fwrite(STDERR, 'End date is not valid. Expected format is yyyy-mm-dd.'. PHP_EOL);
+    exit(1);
+}
+
+if ($start_date > $end_date) {
+    fwrite(STDERR, 'Start date must be lesser or equal than end date.'. PHP_EOL);
+    exit(1);
+}
+
+$content = '';
+$content .= "Start date : $start_date \n";
+$content .= "End date : $end_date \n\n";
+
+$dao          = new Statistics_ServicesUsageDao(CodendiDataAccess::instance(), $start_date, $end_date);
+$csv_exporter = new Statistics_Services_UsageFormatter(new Statistics_Formatter($start_date, $end_date, get_csv_separator()));
+
+$custom_description_factory = new Project_CustomDescription_CustomDescriptionFactory(
+    new Project_CustomDescription_CustomDescriptionDao()
+);
+
+$custom_description_value_dao = new Project_CustomDescription_CustomDescriptionValueDao();
+
+$trove_cat_dao     = new TroveCatDao();
+$trove_cat_factory = new TroveCatFactory($trove_cat_dao);
+$disk_usage_dao    = new Statistics_DiskUsageDao();
+$svn_log_dao       = new SVN_LogDao();
+$svn_retriever     = new SVNRetriever($disk_usage_dao);
+$svn_collector     = new SVNCollector($svn_log_dao, $svn_retriever);
+$cvs_history_dao   = new FullHistoryDao();
+$cvs_retriever     = new CVSRetriever($disk_usage_dao);
+$cvs_collector     = new CVSCollector($cvs_history_dao, $cvs_retriever);
+$event_manager     = EventManager::instance();
+
+$disk_usage_manager = new Statistics_DiskUsageManager(
+    $disk_usage_dao,
+    $svn_collector,
+    $cvs_collector,
+    $event_manager
+);
+
+$csv_builder = new \Tuleap\Statistics\CSV\CSVBuilder(
+    $dao,
+    $csv_exporter,
+    $custom_description_factory,
+    $custom_description_value_dao,
+    $trove_cat_dao,
+    $trove_cat_factory,
+    $disk_usage_manager,
+    PluginManager::instance(),
+    $event_manager
+);
+
+$content .= $csv_builder->buildServiceUsageCSVContent($start_date, $end_date);
+
+file_put_contents($output, $content);
