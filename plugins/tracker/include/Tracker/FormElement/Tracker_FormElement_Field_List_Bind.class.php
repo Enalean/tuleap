Enalean/tuleap

commit 098b5b68dcb645db8a446d4c274157880208657f

request #11192: Filters set in tracker reports are vulnerable to SQL injections
Change-Id: Ie7051dff5460c67be7932eac77143555428c946d

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Enalean, 2015 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -288,14 +288,18 @@ abstract class Tracker_FormElement_Field_List_Bind implements
             $a = 'A_'. $this->field->id;
             $b = 'B_'. $this->field->id;
             if ($this->isSearchingNone($criteria_value)) {
-                $values_id = array_values($criteria_value);
+                $values_id = $this->getDao()->getDa()->escapeIntImplode(array_values($criteria_value));
 
-                return " $b.bindvalue_id IN (". implode(',', $values_id) .") OR $b.bindvalue_id IS NULL ";
+                return " $b.bindvalue_id IN (". $values_id .") OR $b.bindvalue_id IS NULL ";
             }
 
-            $ids_to_search = $this->getIdsToSearch($criteria_value);
+            $ids_to_search = $this->getDao()->getDa()->escapeIntImplode($this->getIdsToSearch($criteria_value));
 
-            return " $b.bindvalue_id IN(". implode(',', $ids_to_search) .") ";
+            if ($ids_to_search === '') {
+                return '';
+            }
+
+            return " $b.bindvalue_id IN(". $ids_to_search .") ";
         }
         return '';
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Date.class.php
@@ -365,49 +365,28 @@ class Tracker_FormElement_Field_Date extends Tracker_FormElement_Field {
                 $to = $_SERVER['REQUEST_TIME'];
             }
             if ( empty($from) ) {
+                $to               = $this->getDao()->getDa()->escapeInt($to);
                 $and_compare_date = "$column <=  $to + $seconds_in_a_day - 1 ";
             } else {
+                $from             = $this->getDao()->getDa()->escapeInt($from);
+                $to               = $this->getDao()->getDa()->escapeInt($to);
                 $and_compare_date = "$column BETWEEN $from
                                              AND $to + $seconds_in_a_day - 1";
             }
         } else {
             switch ($op) {
                 case '<':
+                    $to               = $this->getDao()->getDa()->escapeInt($to);
                     $and_compare_date = "$column < $to";
                     break;
                 case '=':
+                    $to               = $this->getDao()->getDa()->escapeInt($to);
                     $and_compare_date = "$column BETWEEN $to
                                                  AND $to + $seconds_in_a_day - 1";
                     break;
                 default:
+                    $to               = $this->getDao()->getDa()->escapeInt($to);
                     $and_compare_date = "$column > $to + $seconds_in_a_day";
-                    break;
-            }
-        }
-
-        return $and_compare_date;
-    }
-
-    private function getSQLCompareDateTime($is_advanced, $op, $from, $to, $column) {
-        if ($is_advanced) {
-            if ( ! $to ) {
-                $to = $_SERVER['REQUEST_TIME'];
-            }
-            if ( empty($from) ) {
-                $and_compare_date = "$column <= ". $to;
-            } else {
-                $and_compare_date = "$column BETWEEN $from AND $to";
-            }
-        } else {
-            switch ($op) {
-                case '<':
-                    $and_compare_date = "$column < ". $to;
-                    break;
-                case '=':
-                    $and_compare_date = "$column = $to";
-                    break;
-                default:
-                    $and_compare_date = "$column > $to";
                     break;
             }
         }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_LastModifiedBy.class.php
@@ -1,5 +1,5 @@
 <?php
-/** Copyright (c) Enalean, 2015. All Rights Reserved.
+/** Copyright (c) Enalean, 2015-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -50,9 +50,10 @@ class Tracker_FormElement_Field_LastModifiedBy extends Tracker_FormElement_Field
                                array_values($criteria_value),
                                array_merge(array(100),array_keys($this->getBind()->getAllValues())));
             if (count($ids_to_search) > 1) {
-                return " c.submitted_by IN(". implode(',', $ids_to_search) .") ";
+
+                return " c.submitted_by IN(". $this->getDao()->getDa()->escapeIntImplode($ids_to_search) .") ";
             } else if (count($ids_to_search)) {
-                return " c.submitted_by = ". implode('', $ids_to_search) ." ";
+                return " c.submitted_by = ". $this->getDao()->getDa()->escapeInt($ids_to_search) ." ";
             }
         }
         return '';

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_OpenList.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001 - 2009. All rights reserved
- * Copyright (c) Enalean, 2015 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -595,21 +595,19 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
                     $bindvalues[] = $v->getId();
                 }
             }
-            $openvalues = implode(',', $openvalues);
-            $bindvalues = implode(',', $bindvalues);
             //Only filter query if criteria is valuated
             if ($openvalues || $bindvalues) {
                 $a = 'A_'. $this->id;
                 $b = 'B_'. $this->id;
                 $statement = '';
                 if ($openvalues) {
-                    $statement .= "$b.openvalue_id IN ($openvalues)";
+                    $statement .= "$b.openvalue_id IN (". $this->getDao()->getDa()->escapeIntImplode($openvalues).")";
                 }
                 if ($bindvalues) {
                     if ($statement) {
                         $statement .= ' OR ';
                     }
-                    $statement .= "$b.bindvalue_id IN ($bindvalues)";
+                    $statement .= "$b.bindvalue_id IN (". $this->getDao()->getDa()->escapeIntImplode($bindvalues) . ")";
                 }
                 return " INNER JOIN tracker_changeset_value AS $a 
                          ON ($a.changeset_id = c.id 

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_SubmittedBy.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2013-2018. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -48,9 +49,9 @@ class Tracker_FormElement_Field_SubmittedBy extends Tracker_FormElement_Field_Li
                                array_values($criteria_value),
                                array_merge(array(100),array_keys($this->getBind()->getAllValues())));
             if (count($ids_to_search) > 1) {
-                return " artifact.submitted_by IN(". implode(',', $ids_to_search) .") ";
+                return " artifact.submitted_by IN(". $this->getDao()->getDa()->escapeIntImplode($ids_to_search) .") ";
             } else if (count($ids_to_search)) {
-                return " artifact.submitted_by = ". implode('', $ids_to_search) ." ";
+                return " artifact.submitted_by = ". $this->getDao()->getDa()->escapeInt($ids_to_search) ." ";
             }
         }
         return '';

commit 78943240ecb23d8440facbfe6cd8da88a447f817

request #11264: Fatal error when a tracker report filters a list bound to ugroups
Issue has been introduced by commit 098b5b68dcb645db8a446d4c274157880208657f.

Change-Id: Iaf7d63de8a15f5f6d4a0fccc338e58de34662c9e

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind.class.php
@@ -287,13 +287,16 @@ abstract class Tracker_FormElement_Field_List_Bind implements
         if ($criteria_value) {
             $a = 'A_'. $this->field->id;
             $b = 'B_'. $this->field->id;
+
+            $data_access = CodendiDataAccess::instance();
+
             if ($this->isSearchingNone($criteria_value)) {
-                $values_id = $this->getDao()->getDa()->escapeIntImplode(array_values($criteria_value));
+                $values_id = $data_access->escapeIntImplode(array_values($criteria_value));
 
                 return " $b.bindvalue_id IN (". $values_id .") OR $b.bindvalue_id IS NULL ";
             }
 
-            $ids_to_search = $this->getDao()->getDa()->escapeIntImplode($this->getIdsToSearch($criteria_value));
+            $ids_to_search = $data_access->escapeIntImplode($this->getIdsToSearch($criteria_value));
 
             if ($ids_to_search === '') {
                 return '';