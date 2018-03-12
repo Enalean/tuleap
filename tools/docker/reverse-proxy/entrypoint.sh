c3070cf1ff84c0d79be5e951abbe7d1dec8e56ab
Nicolas Terray <nicolas.terray@gmail.com>
2018-03-12 16:34:49 +0100

    request #11270: Add realtime server to dev stack
    > As a developer,
    > In order to ease testing of TTM contributions,
    > I would like to have a realtime server up and running with `make start`
    > command.
    
    To test, issue the following command:
    echo "REALTIME_KEY=$(head -c 64 /dev/urandom | base64 --wrap=88)" >> .env
    
    Then stop your environment:
    docker-compose down
    
    And start all the things:
    make start
    
    This will build a tuleap-realtime image, and generate/deploy certificate.
    
    Then go to a TTM campaign with many users. You should see their avatars
    switching from tests to tests in realtime.
    
    Note: as of today the realtime image is based on a Dockerfile since this is a
    temporary solution, waiting for a full dev environment (we should be able to
    hack tuleap-realtime). When we will provide a full dev environment, the
    Dockerfile will be replaced by a standard docker image.
    
    Change-Id: I572b4a00f6bd5c79e8706f3d7608a5df55623d10

---
+++ tools/docker/reverse-proxy/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p /etc/pki/tls/private/ /etc/pki/tls/certs
+if [ ! -f /etc/pki/tls/certs/localhost.cert.pem ]; then
+    openssl req \
+        -batch \
+        -nodes \
+        -x509 \
+        -newkey rsa:4096 \
+        -keyout /etc/pki/tls/private/localhost.key.pem \
+        -out /etc/pki/tls/certs/localhost.cert.pem \
+        -days 365 \
+        -subj "/C=XX/ST=SomeState/L=SomeCity/O=SomeOrganization/OU=SomeDepartment/CN=tuleap-web.tuleap-aio-dev.docker"
+fi
+
+exec "$@"
