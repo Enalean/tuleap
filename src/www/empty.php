d495a98f0d25bc30e739f7faee62f0750acd9ee8
Nicolas Terray <nicolas.terray@gmail.com>
2018-03-12 10:37:55 +0100

    empty.php should stay empty (again)
    Revert modifications done to test previous integration.
    
    Part of story #11239: generate dynamic credentials from HashiCorp Vault into a Tuleap instance
    
    Change-Id: Ie326643b8f262f7eba3e6c63e6703c217f74ac98

--- src/www/empty.php
+++ src/www/empty.php
@@ -1,14 +1,3 @@
 <?php
 
 require_once 'pre.php';
-
-$public_key = 'fDLMRGYuFzWweXRc9NZkclGwXPRl2+jqN16/ZEEBIBI=';
-$private_key = '/PYrnpyejn6lt9nuQNeMnBKMz5cJQlKN2mTBnXASwmh8MsxEZi4XNbB5dFz01mRyUbBc9GXb6Oo3Xr9kQQEgEg==';
-
-$secret_key = base64_decode($private_key);
-$domain = 'tuleap-web.tuleap-aio-dev.docker';
-$username = 'forge__dynamic_credential-identifier';
-$password = 'password';
-$expiration = '2018-03-08T14:32:26+01:00';
-var_dump(base64_encode(sodium_crypto_sign_detached($domain . $username . $password . $expiration, $secret_key)));
-var_dump(base64_encode(sodium_crypto_sign_detached($domain . $username, $secret_key)));
