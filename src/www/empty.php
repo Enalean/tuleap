d85faec65079683d549d6199614d2c2e03901b0b
Nicolas Terray <nicolas.terray@enalean.com>
2018-03-20 18:10:09 +0100

    This is Tuleap 9.18.99.84
    Change-Id: I96259b5d0fafcc00ec9974d9fa49670830735b53

--- src/www/empty.php
+++ src/www/empty.php
@@ -1,3 +1,13 @@
 <?php
 
 require_once 'pre.php';
+
+$public_key = '6EvO0rQQU0ujwRg9xQDJb3X55RpMk1bRX3qfmhgxzZg=';
+$secret_key = '8siQhRBt7mxZ4dYVWuHsuu/W8bRbdRayl3kMrMfLv6roS87StBBTS6PBGD3FAMlvdfnlGkyTVtFfep+aGDHNmA==';
+
+$secret_key = base64_decode($secret_key);
+$domain     = 'tuleap-web.tuleap-aio-dev.docker';
+$username   = 'forge__dynamic_credential-identifier';
+$password   = 'password';
+$expiration = '2018-03-28T14:32:26+01:00';
+var_dump(base64_encode(sodium_crypto_sign_detached($domain . $username . $password . $expiration, $secret_key)));
