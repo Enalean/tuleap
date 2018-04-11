Enalean/tuleap

commit ae0e9dcb75a024301c9c48dc3532a9862ebad8b0

Move Git permissions DAO to PDO and prepared statements
This is part of story #11261: convert Git plugin to SQL prepared statements

Change-Id: I92f2f1d3ef2a04c935adb4cca4b7458b5d52cac3

plugins/git/include/Git/PermissionsDao.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,86 +19,93 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-class Git_PermissionsDao extends DataAccessObject
+use ParagonIE\EasyDB\EasyStatement;
+
+class Git_PermissionsDao extends \Tuleap\DB\DataAccessObject
 {
 
     public function disableAnonymousRegisteredAuthenticated($project_id)
     {
-        $this->da->startTransaction();
+        $this->getDB()->beginTransaction();
 
-        if (! $this->updatePermissionsForGitRepositories(
-            $project_id,
-            array(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED, ProjectUGroup::AUTHENTICATED),
-            ProjectUGroup::PROJECT_MEMBERS
-        )) {
-            $this->da->rollback();
-            return false;
+        try {
+            $this->updatePermissionsForGitRepositories(
+                $project_id,
+                array(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED, ProjectUGroup::AUTHENTICATED),
+                ProjectUGroup::PROJECT_MEMBERS
+            );
+            $this->updatePermissionsForDefaultGitAccessRights(
+                $project_id,
+                array(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED, ProjectUGroup::AUTHENTICATED),
+                ProjectUGroup::PROJECT_MEMBERS
+            );
+        } catch (PDOException $ex) {
+            $this->getDB()->rollBack();
+            throw $ex;
         }
 
-        if (! $this->updatePermissionsForDefaultGitAccessRights(
-            $project_id,
-            array(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED, ProjectUGroup::AUTHENTICATED),
-            ProjectUGroup::PROJECT_MEMBERS
-        )) {
-            $this->da->rollback();
-            return false;
-        }
-
-        return $this->da->commit();
+        $this->getDB()->commit();
     }
 
     public function disableAuthenticated($project_id)
     {
-        $this->da->startTransaction();
+        $this->getDB()->beginTransaction();
 
-        if (! $this->updatePermissionsForGitRepositories(
-            $project_id,
-            array(ProjectUGroup::AUTHENTICATED),
-            ProjectUGroup::REGISTERED
-        )) {
-            $this->da->rollback();
-            return false;
+        try {
+            $this->updatePermissionsForGitRepositories(
+                $project_id,
+                array(ProjectUGroup::AUTHENTICATED),
+                ProjectUGroup::REGISTERED
+            );
+            $this->updatePermissionsForDefaultGitAccessRights(
+                $project_id,
+                array(ProjectUGroup::AUTHENTICATED),
+                ProjectUGroup::REGISTERED
+            );
+        }  catch (PDOException $ex) {
+            $this->getDB()->rollBack();
+            throw $ex;
         }
 
-        if (! $this->updatePermissionsForDefaultGitAccessRights(
-            $project_id,
-            array(ProjectUGroup::AUTHENTICATED),
-            ProjectUGroup::REGISTERED
-        )) {
-            $this->da->rollback();
-            return false;
-        }
-
-        return $this->da->commit();
+        $this->getDB()->commit();
     }
 
     private function updatePermissionsForDefaultGitAccessRights($project_id, array $old_ugroup_ids, $new_ugroup_id)
     {
-        $project_id          = $this->da->escapeInt($project_id);
-        $old_ugroup_ids      = $this->da->escapeIntImplode($old_ugroup_ids);
-        $git_permission_type = $this->da->quoteSmartImplode(',', Git::allDefaultPermissionTypes());
+        $git_permission_type_in_condition = EasyStatement::open()->in('?*', Git::allPermissionTypes());
+        $old_ugroup_ids_in_condition      = EasyStatement::open()->in('?*', $old_ugroup_ids);
 
         $sql = "UPDATE permissions
-                SET ugroup_id = $new_ugroup_id
-                WHERE ugroup_id IN ($old_ugroup_ids)
-                  AND object_id = $project_id
-                  AND permission_type IN ($git_permission_type)";
+                SET ugroup_id = ?
+                WHERE ugroup_id IN ($old_ugroup_ids_in_condition)
+                  AND object_id = ?
+                  AND permission_type IN ($git_permission_type_in_condition)";
 
-        return $this->update($sql);
+        $update_params   = [$new_ugroup_id];
+        $update_params   = array_merge($update_params, $old_ugroup_ids_in_condition->values());
+        $update_params[] = $project_id;
+        $update_params   = array_merge($update_params, $git_permission_type_in_condition->values());
+
+        $this->getDB()->safeQuery($sql, $update_params);
     }
 
     private function updatePermissionsForGitRepositories($project_id, array $old_ugroup_ids, $new_ugroup_id)
     {
-        $project_id          = $this->da->escapeInt($project_id);
-        $old_ugroup_ids      = $this->da->escapeIntImplode($old_ugroup_ids);
-        $git_permission_type = $this->da->quoteSmartImplode(',', Git::allPermissionTypes());
+        $git_permission_type_in_condition = EasyStatement::open()->in('?*', Git::allPermissionTypes());
+        $old_ugroup_ids_in_condition      = EasyStatement::open()->in('?*', $old_ugroup_ids);
+
         $sql = "UPDATE permissions perms
-                  JOIN plugin_git git ON (perms.object_id = CAST(git.repository_id AS CHAR) AND perms.permission_type IN ($git_permission_type))
-                SET perms.ugroup_id = $new_ugroup_id
-                WHERE perms.ugroup_id IN ($old_ugroup_ids)
-                  AND git.project_id = $project_id
-                ";
-        return $this->update($sql);
+                  JOIN plugin_git git ON (perms.object_id = CAST(git.repository_id AS CHAR) AND perms.permission_type IN ($git_permission_type_in_condition))
+                SET perms.ugroup_id = ?
+                WHERE perms.ugroup_id IN ($old_ugroup_ids_in_condition)
+                  AND git.project_id = ?";
+
+        $update_params   = $git_permission_type_in_condition->values();
+        $update_params[] = $new_ugroup_id;
+        $update_params   = array_merge($update_params, $old_ugroup_ids_in_condition->values());
+        $update_params[] = $project_id;
+
+        $this->getDB()->safeQuery($sql, $update_params);
     }
 
     public function getAllProjectsWithAnonymousRepositories()
@@ -113,40 +120,47 @@ class Git_PermissionsDao extends DataAccessObject
 
     private function getAllProjectsWithPermissionGroup($ugroup_id)
     {
-        $git_permission_type = $this->da->quoteSmartImplode(',', Git::allPermissionTypes());
-        $not_deleted_date    = $this->da->quoteSmart(GitDao::NOT_DELETED_DATE);
+        $git_permission_type_in_condition = EasyStatement::open()->in('?*', Git::allPermissionTypes());
 
         $sql = "SELECT DISTINCT groups.group_id
                 FROM groups
                   JOIN plugin_git AS git ON (git.project_id = groups.group_id)
-                  JOIN permissions ON (permissions.object_id = CAST(git.repository_id AS CHAR) AND permissions.permission_type IN ($git_permission_type))
+                  JOIN permissions ON (permissions.object_id = CAST(git.repository_id AS CHAR) AND permissions.permission_type IN ($git_permission_type_in_condition))
                 WHERE groups.status = 'A'
-                  AND git.repository_deletion_date = $not_deleted_date
-                  AND permissions.ugroup_id = $ugroup_id
+                  AND git.repository_deletion_date = ?
+                  AND permissions.ugroup_id = ?
                 ";
-        return $this->retrieve($sql);
+
+        $params = $git_permission_type_in_condition->values();
+        $params[] = GitDao::NOT_DELETED_DATE;
+        $params[] = $ugroup_id;
+
+        return $this->getDB()->safeQuery($sql, $params);
     }
 
     public function updateAllAnonymousAccessToRegistered()
     {
-        return $this->updateAllPermissions(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED);
+        $this->updateAllPermissions(ProjectUGroup::ANONYMOUS, ProjectUGroup::REGISTERED);
     }
 
     public function updateAllAuthenticatedAccessToRegistered()
     {
-        return $this->updateAllPermissions(ProjectUGroup::AUTHENTICATED, ProjectUGroup::REGISTERED);
+        $this->updateAllPermissions(ProjectUGroup::AUTHENTICATED, ProjectUGroup::REGISTERED);
     }
 
     private function updateAllPermissions($old_ugroup_id, $new_ugroup_id)
     {
-        $all_permission_types = array_merge(Git::allPermissionTypes(), Git::allDefaultPermissionTypes());
-        $git_permission_type  = $this->da->quoteSmartImplode(',', $all_permission_types);
+        $all_permission_types             = array_merge(Git::allPermissionTypes(), Git::allDefaultPermissionTypes());
+        $git_permission_type_in_condition = EasyStatement::open()->in('?*', $all_permission_types);
 
         $sql = "UPDATE permissions
-                SET ugroup_id = $new_ugroup_id
-                WHERE ugroup_id = $old_ugroup_id
-                    AND permission_type IN ($git_permission_type)
-                ";
-        return $this->update($sql);
+                SET ugroup_id = ?
+                WHERE ugroup_id = ?
+                    AND permission_type IN ($git_permission_type_in_condition)";
+
+        $update_params = [$new_ugroup_id, $old_ugroup_id];
+        $update_params = array_merge($update_params, $git_permission_type_in_condition->values());
+
+        $this->getDB()->safeQuery($sql, $update_params);
     }
 }

plugins/git/include/GitPermissionsManager.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -127,7 +127,7 @@ class GitPermissionsManager
         }
     }
 
-    private function queueProjectsConfigurationUpdate(DataAccessResult $dar) {
+    private function queueProjectsConfigurationUpdate(array $dar) {
         $projects_ids = array();
         if (count($dar) > 0) {
             foreach ($dar as $row) {

plugins/git/tests/GitPermissionsManagerTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -49,7 +49,7 @@ abstract class GitPermissionsManagerTest extends TuleapTestCase {
 class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTest {
 
     public function testWhenSwitchingFromAnonymousToRegularItUpdatesAllProjectsThatWereUsingAnonymous() {
-        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returnsDar(array('group_id' => 101), array('group_id' => 104));
+        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returns([['group_id' => 101], ['group_id' => 104]]);
 
         expect($this->git_permissions_dao)->updateAllAnonymousAccessToRegistered()->once();
 
@@ -59,7 +59,7 @@ class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTe
     }
 
     public function testWhenSwitchingFromAnonymousToRegularItDoesNothingWhenNoProjectsWereUsingAnonymous() {
-        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returnsEmptyDar();
+        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returns([]);
 
         expect($this->git_system_event_manager)->queueProjectsConfigurationUpdate()->never();
         expect($this->git_permissions_dao)->updateAllAnonymousAccessToRegistered()->never();
@@ -78,7 +78,7 @@ class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTe
     }
 
     public function testWhenSwitchingFromAnonymousToRestrictedItUpdatesAllProjectsThatWereUsingAnonymous() {
-        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returnsDar(array('group_id' => 101), array('group_id' => 104));
+        stub($this->git_permissions_dao)->getAllProjectsWithAnonymousRepositories()->returns([['group_id' => 101], ['group_id' => 104]]);
 
         expect($this->git_system_event_manager)->queueProjectsConfigurationUpdate(array(101, 104))->once();
         expect($this->git_permissions_dao)->updateAllAnonymousAccessToRegistered()->once();
@@ -87,7 +87,7 @@ class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTe
     }
 
     public function testWhenSwitchingFromRestrictedToAnonymousItUpdatesAllProjectThatWereUsingUnRestricted() {
-        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returnsDar(array('group_id' => 102), array('group_id' => 107));
+        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returns([['group_id' => 102], ['group_id' => 107]]);
 
         expect($this->git_system_event_manager)->queueProjectsConfigurationUpdate(array(102, 107))->once();
         expect($this->git_permissions_dao)->updateAllAuthenticatedAccessToRegistered()->once();
@@ -96,7 +96,7 @@ class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTe
     }
 
     public function testWhenSwitchingFromRestrictedToRegularItUpdatesAllProjectThatWereUsingUnRestricted() {
-        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returnsDar(array('group_id' => 102), array('group_id' => 107));
+        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returns([['group_id' => 102], ['group_id' => 107]]);
 
         expect($this->git_system_event_manager)->queueProjectsConfigurationUpdate(array(102, 107))->once();
         expect($this->git_permissions_dao)->updateAllAuthenticatedAccessToRegistered()->once();
@@ -105,7 +105,7 @@ class GitPermissionsManager_SiteAccessUpdateTest extends GitPermissionsManagerTe
     }
 
     public function testWhenSwitchingFromRestrictedToRegularItDoesNothingWhenNoProjectsWereUsingAuthenticated() {
-        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returnsEmptyDar();
+        stub($this->git_permissions_dao)->getAllProjectsWithUnrestrictedRepositories()->returns([]);
 
         expect($this->git_system_event_manager)->queueProjectsConfigurationUpdate()->never();
         expect($this->git_permissions_dao)->updateAllAuthenticatedAccessToRegistered()->never();

commit 54e3d9a03316b1c4aa36202b0c8e3a5c58c57e53

Changing platform access level should only impact Git default permissions
Issue introduced by commit ae0e9dcb75a024301c9c48dc3532a9862ebad8b0.

This is part of story #11261: convert Git plugin to SQL prepared statements

Change-Id: I26c4544e19e0a93bf4bec9edda899a1bafa00783

plugins/git/include/Git/PermissionsDao.php
@@ -72,7 +72,7 @@ class Git_PermissionsDao extends \Tuleap\DB\DataAccessObject
 
     private function updatePermissionsForDefaultGitAccessRights($project_id, array $old_ugroup_ids, $new_ugroup_id)
     {
-        $git_permission_type_in_condition = EasyStatement::open()->in('?*', Git::allPermissionTypes());
+        $git_permission_type_in_condition = EasyStatement::open()->in('?*', Git::allDefaultPermissionTypes());
         $old_ugroup_ids_in_condition      = EasyStatement::open()->in('?*', $old_ugroup_ids);
 
         $sql = "UPDATE permissions