a97a1653d1fefea3ded42a0e72d52311a427a5d8
Thomas Gorka <thomas.gorka@enalean.com>
2018-04-12 16:05:50 +0200

    Check that all trackers use a subby field (readable)
    Part of story #10703 - define a query with submitted_by fields
    
    How to test:
    - Remove the "submitted by" from a tracker
    - In the widget, select this tracker, setup a query, then validate
    --> An error is show because one of the selected trackers does not have
        a submitted by field
    
    - Take another one tracker, remove its "submitted by" field
    - In the widget, select the 2 trackers, setup a query, then validate
    --> An error is show because 2 of the selected trackers do not have
        a submitted by field
    
    Change-Id: I7ed225bcf886e2f221fc2d26ed9028559ded38d2

---
+++ plugins/crosstracker/include/CrossTracker/Report/Query/Advanced/QueryValidation/Metadata/SubmittedByIsMissingInAtLeastOneTrackerException.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright Enalean (c) 2018. All rights reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\CrossTracker\Report\Query\Advanced\QueryValidation\Metadata;
+
+use Tuleap\CrossTracker\Report\Query\Advanced\QueryValidation\InvalidQueryException;
+
+class SubmittedByIsMissingInAtLeastOneTrackerException extends InvalidQueryException
+{
+
+    public function __construct($count)
+    {
+        parent::__construct(
+            sprintf(
+                dngettext(
+                    'tuleap-crosstracker',
+                    'One of the trackers involved in the query does not expose a submitted by field. Please refine
+                    your query or check the configuration of the trackers.',
+                    '%d of the trackers involved in the query do not expose a submitted by field. Please refine
+                    your query or check the configuration of the trackers.',
+                    $count
+                ),
+                $count
+            )
+        );
+    }
+}
