130b076c232eab06a89735bb8f965bc826b6215d
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2018-04-26 15:50:45 +0200

    execution of cypress ans distlp should be merged
    this is part of request #11328 move some TTM tests to cypress
    
    Change-Id: Ic1cf51bf803bdf9f10b209e91dbbe92e92775b7b

--- tests/e2e/distlp/wrap.sh
+++ tests/e2e/distlp/wrap.sh
@@ -9,7 +9,7 @@ set -ex
 MAX_TEST_EXECUTION_TIME='30m'
 DOCKERCOMPOSE="docker-compose -f docker-compose-distlp-tests.yml -p distlp-tests-${BUILD_TAG}"
 
-test_results_folder='./test_results_e2e_distlp'
+test_results_folder='./test_results_distlp'
 if [ -n "$1" ]; then
     test_results_folder="$1"
 fi
@@ -19,8 +19,10 @@ clean_env() {
 }
 
 wait_until_tests_are_executed() {
-    local test_container_id="$($DOCKERCOMPOSE ps -q test)"
-    timeout "$MAX_TEST_EXECUTION_TIME" docker wait "$test_container_id" || \
+    local test_cli_container_id="$($DOCKERCOMPOSE ps -q test-cli)"
+    local test_cypress_container_id="$($DOCKERCOMPOSE ps -q test-cypress)"
+
+    timeout "$MAX_TEST_EXECUTION_TIME" docker wait "$test_cli_container_id" "$test_cypress_container_id" || \
         echo 'Tests take to much time to execute. End of execution will not be waited for!'
 }
 
@@ -34,6 +36,7 @@ wait_until_tests_are_executed
 
 $DOCKERCOMPOSE logs backend-web > "$test_results_folder/backend-web.log"
 $DOCKERCOMPOSE logs backend-svn > "$test_results_folder/backend-svn.log"
-$DOCKERCOMPOSE logs test > "$test_results_folder/test.log"
+$DOCKERCOMPOSE logs test-cli > "$test_results_folder/test-cli.log"
+$DOCKERCOMPOSE logs test-cypress > "$test_results_folder/test-cypress.log"
 
 clean_env
