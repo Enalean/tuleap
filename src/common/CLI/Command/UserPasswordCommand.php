272d6f279f88c52615c6afc47d6172c3f820f327
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-05-25 16:49:23 +0200

    Be able to set a user password from the CLI tool
    This is part of request #11508 tuleap cli tools can reset a user password
    
    Change-Id: I65cdb6481e4fc792fa85c796cd3f419f27da2d04

---
+++ src/common/CLI/Command/UserPasswordCommand.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\CLI\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Tuleap\Password\PasswordSanityChecker;
+use UserManager;
+
+class UserPasswordCommand extends Command
+{
+    const NAME = 'set-user-password';
+
+    /**
+     * @var UserManager
+     */
+    private $user_manager;
+    /**
+     * @var PasswordSanityChecker
+     */
+    private $password_sanity_checker;
+
+    public function __construct(UserManager $user_manager, PasswordSanityChecker $password_sanity_checker)
+    {
+        parent::__construct(self::NAME);
+
+        $this->user_manager            = $user_manager;
+        $this->password_sanity_checker = $password_sanity_checker;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Set a user password')
+            ->addArgument('user_name', InputArgument::REQUIRED, 'Tuleap username')
+            ->addArgument('password', InputArgument::OPTIONAL, 'New password for user');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $user_name = $input->getArgument('user_name');
+        $user      = $this->user_manager->getUserByLoginName($user_name);
+
+        if (! $user) {
+            throw new InvalidArgumentException("User $user_name not found.");
+        }
+
+        $password = $input->getArgument('password');
+        if (! $password) {
+            $helper = $this->getHelper('question');
+
+            $question = new Question('New password? ');
+            $question->setHidden(true);
+            $question->setHiddenFallback(false);
+
+            $password = $helper->ask($input, $output, $question);
+        }
+
+        if (! $this->password_sanity_checker->check($password)) {
+            throw new InvalidArgumentException("The provided password does not match the expected password policy.");
+        }
+
+        $user->setPassword($password);
+        if (! $this->user_manager->updateDb($user)) {
+            return 1;
+        };
+    }
+}
