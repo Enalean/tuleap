56910ea436dafda294c20b9aa8941683de3c1198
Benjamin Dauton <benjamin.dauton@enalean.com>
2018-06-15 16:29:22 +0200

    Display a form on click on Call Me Back button in FP pages.
    - This form doesn't do anything for now.
    - There's no datepicker for now (maybe for ever…)
    
    Part of the story #11593 Have a Call Me Back button and form on every page
    
    Change-Id: I5c90928cf9d61468d3f1c056c16e8b35ea523a05

--- plugins/create_test_env/scripts/call-me-back-flaming-parrot/src/CallMeBack.vue
+++ plugins/create_test_env/scripts/call-me-back-flaming-parrot/src/CallMeBack.vue
@@ -17,10 +17,36 @@
   - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
   -->
 <template>
-    <div class="call-me-back">
-        <button class="call-me-back-button">
+    <div class="call-me-back dropup">
+        <button class="call-me-back-button dropdown-toggle" data-toggle="dropdown">
             <i class="icon-coffee"></i>
         </button>
+        <form class="call-me-back-form dropdown-menu pull-right dropup" v-on:click.stop>
+            <p class="call-me-back-form-intro">
+                <translate>Fill this form and we'll call you back.</translate>
+            </p>
+            <label for="call-me-back-phone-number">
+                <translate>Phone number</translate> <i class="icon-asterisk"></i>
+            </label>
+            <div class="input-prepend">
+                <span class="add-on">
+                    <i class="icon-phone"></i>
+                </span>
+                <input type="text" id="call-me-back-phone-number" name="phone" placeholder="+33 0 00 00 00 00">
+            </div>
+            <label for="call-me-back-date">
+                <translate>Call me back on</translate> <i class="icon-asterisk"></i>
+            </label>
+            <div class="input-prepend">
+                <span class="add-on">
+                    <i class="icon-calendar"></i>
+                </span>
+                <input type="text" id="call-me-back-date" name="date" placeholder="yyyy-mm-dd">
+            </div>
+            <button type="button" class="btn btn-primary btn-large">
+                <translate>Call me back</translate>
+            </button>
+        </form>
     </div>
 </template>
 
