Enalean/tuleap

commit ae5b42899a061b6a505e76daa9d9577ff4ba5e69

Git smart http routed by FrontRouter
Take the opportunity to go a little bit deeper in the FrontRouting with more
complex routes (complex regexp to indentify Smart HTTP requests) in addition
to low level permission handling.

For permission handling, we introduce a new Interface that completly disable
the existing mechanism "URLVerification". It's the common protection that
forbid access to private projects, restricted users & co.

It was previously managed by a dedicated hook in URLVerification that
"whitelisted" URLs so permissions were handled later in the stack.
With this new approach we make clear the requests that implement the new
interface are "on their own" and developer has to deal with all permission
and authentication checking manually.

Part of request #11450 FrontRouter for git plugin

Change-Id: Icb2473a5fe5241e16ec828a68c44dd50ff383d80

plugins/git/include/Git/HTTP/HTTPAccessControl.php
@@ -1,0 +1,141 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\Git\HTTP;
+
+use Logger;
+use User_LoginManager;
+use Tuleap\Git\Gerrit\ReplicationHTTPUserAuthenticator;
+
+class HTTPAccessControl
+{
+    /**
+     * @var Logger
+     */
+    private $logger;
+    /**
+     * @var User_LoginManager
+     */
+    private $login_manager;
+    /**
+     * @var ReplicationHTTPUserAuthenticator
+     */
+    private $authenticator;
+    /**
+     * @var \PermissionsManager
+     */
+    private $permissions_manager;
+
+    public function __construct(Logger $logger, User_LoginManager $login_manager, ReplicationHTTPUserAuthenticator $authenticator, \PermissionsManager $permissions_manager)
+    {
+        $this->logger              = $logger;
+        $this->login_manager       = $login_manager;
+        $this->authenticator       = $authenticator;
+        $this->permissions_manager = $permissions_manager;
+    }
+
+    public function getUser(\URLVerification $url_verification, \GitRepository $repository, \Git_URL $url)
+    {
+        $user = null;
+        if ($this->needAuthentication($url_verification, $repository, $url)) {
+            $this->logger->debug('Repository '.$repository->getFullName().' need authentication');
+            $user = $this->authenticate($repository);
+        }
+        return $user;
+    }
+
+    private function needAuthentication(\URLVerification $url_verification, \GitRepository $repository, \Git_URL $url)
+    {
+        return $url_verification->doesPlatformRequireLogin() ||
+            $this->isGitPush($url) ||
+            ! $this->canBeReadByAnonymous($repository) ||
+            $this->isInPrivateProject($repository);
+    }
+
+    private function isGitPush(\Git_URL $url)
+    {
+        return $url->isGitPush();
+    }
+
+    private function isInPrivateProject(\GitRepository $repository)
+    {
+        return $repository->getProject()->isPublic() === false;
+    }
+
+    private function canBeReadByAnonymous(\GitRepository $repository)
+    {
+        $ugroup_ids = $this->permissions_manager->getAuthorizedUgroupIds($repository->getId(), \Git::PERM_READ);
+        foreach ($ugroup_ids as $ugroup_id) {
+            if ($ugroup_id == \ProjectUGroup::ANONYMOUS) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function basicAuthenticationChallenge()
+    {
+        header('WWW-Authenticate: Basic realm="'.\ForgeConfig::get('sys_name').' git authentication"');
+        header('HTTP/1.0 401 Unauthorized');
+        exit;
+    }
+
+    /**
+     * @param \GitRepository $repository
+     * @return null|false|\PFO_User
+     */
+    private function authenticate(\GitRepository $repository)
+    {
+        if (! isset($_SERVER['PHP_AUTH_USER']) ||
+            $_SERVER['PHP_AUTH_USER'] == '' ||
+            ! isset($_SERVER['PHP_AUTH_PW']) ||
+            $_SERVER['PHP_AUTH_PW'] == ''
+        ) {
+            $this->basicAuthenticationChallenge();
+        } else {
+            $user = null;
+            try {
+                $user = $this->authenticator->authenticate(
+                    $repository,
+                    $_SERVER['PHP_AUTH_USER'],
+                    $_SERVER['PHP_AUTH_PW']
+                );
+
+                $this->logger->debug('LOGGED AS ' . $user->getUnixName());
+            } catch (\Exception $exception) {
+                $this->logger->debug('Replication user not recognized ' . $exception->getMessage());
+            }
+
+            if ($user === null) {
+                try {
+                    $user = $this->login_manager->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+                    $this->logger->debug("LOGGED AS ".$user->getUnixName());
+                    return $user;
+                } catch (\Exception $exception) {
+                    $this->logger->debug('LOGIN ERROR ' . $exception->getMessage());
+                    $this->basicAuthenticationChallenge();
+                }
+            }
+
+            return false;
+        }
+    }
+}

tests/phpunit/phpunit.xml
@@ -3,6 +3,7 @@
          failOnRisky="true" beStrictAboutChangesToGlobalState="true" beStrictAboutTestsThatDoNotTestAnything="true">
     <php>
         <ini name="date.timezone" value="Europe/Paris"/>
+        <ini name="memory_limit" value="256M" /><!-- Needed for code coverage generation in PHP 5.6 -->
     </php>
     <filter>
         <whitelist processUncoveredFilesFromWhitelist="true">

plugins/git/include/Git.class.php
@@ -335,7 +335,6 @@ class Git extends PluginController {
             $this->factory,
             $_SERVER['REQUEST_URI']
         );
-        $this->routeGitSmartHTTP($url);
         $this->routeUsingFriendlyURLs($url);
         $this->routeUsingStandardURLs($url);
 
@@ -402,45 +401,6 @@ class Git extends PluginController {
             $this->regexp_retriever,
             $this->gerrit_server_factory
         );
-    }
-
-    private function routeGitSmartHTTP(Git_URL $url)
-    {
-        if (! $url->isSmartHTTP()) {
-            return;
-        }
-
-        $repository = $url->getRepository();
-        if (! $repository) {
-            return;
-        }
-
-        $logger = new WrapperLogger($this->logger, 'http');
-
-        $password_handler = PasswordHandlerFactory::getPasswordHandler();
-        $command_factory  = new Git_HTTP_CommandFactory(
-            $this->factory,
-            new User_LoginManager(
-                EventManager::instance(),
-                UserManager::instance(),
-                new User_PasswordExpirationChecker(),
-                $password_handler
-            ),
-            PermissionsManager::instance(),
-            new URLVerification(),
-            $logger,
-            $this->gerrit_server_factory,
-            new ReplicationHTTPUserAuthenticator(
-                $password_handler,
-                $this->gerrit_server_factory,
-                new HttpUserValidator()
-            ),
-            $this->detector
-        );
-
-        $http_wrapper = new Git_HTTP_Wrapper($logger);
-        $http_wrapper->stream($command_factory->getCommandForRepository($repository, $url));
-        exit;
     }
 
     private function routeUsingFriendlyURLs(Git_URL $url) {

plugins/git/include/Git/HTTP/CommandFactory.class.php
@@ -21,79 +21,28 @@
 use Tuleap\Git\Gerrit\ReplicationHTTPUserAuthenticator;
 use Tuleap\Git\Gitolite\VersionDetector;
 
-class Git_HTTP_CommandFactory {
-
-    /**
-     * @var ReplicationHTTPUserAuthenticator
-     */
-    private $authenticator;
-
-    /**
-     * @var Logger
-     */
-    private $logger;
-
-    /**
-     * @var PermissionsManager
-     */
-    private $permissions_manager;
-
-    /**
-     * @var User_LoginManager
-     */
-    private $login_manager;
-
-    /**
-     * @var GitRepositoryFactory
-     */
-    private $repository_factory;
-
-    /**
-     * @var URLVerification
-     */
-    private $url_verification;
-
-    /**
-     * @var Git_RemoteServer_GerritServerFactory
-     */
-    private $gerrit_server_factory;
-
+class Git_HTTP_CommandFactory
+{
     /**
      * @var VersionDetector
      */
     private $detector;
 
-    public function __construct(
-        GitRepositoryFactory $repository_factory,
-        User_LoginManager $login_manager,
-        PermissionsManager $permissions_manager,
-        URLVerification $url_verification,
-        Logger $logger,
-        Git_RemoteServer_GerritServerFactory $gerrit_server_factory,
-        ReplicationHTTPUserAuthenticator $authenticator,
-        VersionDetector $detector
-    ) {
-        $this->repository_factory    = $repository_factory;
-        $this->login_manager         = $login_manager;
-        $this->permissions_manager   = $permissions_manager;
-        $this->logger                = $logger;
-        $this->url_verification      = $url_verification;
-        $this->gerrit_server_factory = $gerrit_server_factory;
-        $this->authenticator         = $authenticator;
-        $this->detector              = $detector;
+    public function __construct(VersionDetector $detector)
+    {
+        $this->detector = $detector;
     }
 
-    public function getCommandForRepository(GitRepository $repository, Git_URL $url) {
+    public function getCommandForUser(Git_URL $url, PFO_User $user = null)
+    {
         $command = $this->getGitHttpBackendCommand();
-        if ($this->needAuthentication($repository, $url)) {
-            $this->logger->debug('Repository '.$repository->getFullName().' need authentication');
-            $command = $this->authenticate($repository, $command);
+        if ($user !== null) {
+            $command = $this->getGitoliteCommand($user, $command);
         }
         $command->setPathInfo($url->getPathInfo());
         $command->setQueryString($url->getQueryString());
         return $command;
     }
-
 
     private function getGitHttpBackendCommand() {
         $command = new Git_HTTP_CommandCentos6GitHttpBackend();
@@ -101,73 +50,6 @@ class Git_HTTP_CommandFactory {
             $command = new \Tuleap\Git\HTTP\CommandSCL29GitHttpBackend();
         }
         return $command;
-    }
-
-    private function needAuthentication(GitRepository $repository, Git_URL $url)
-    {
-        return $this->url_verification->doesPlatformRequireLogin() ||
-            $this->isGitPush($url) ||
-            ! $this->canBeReadByAnonymous($repository) ||
-            $this->isInPrivateProject($repository);
-    }
-
-    private function isGitPush(Git_URL $url) {
-        return $url->isGitPush();
-    }
-
-    private function isInPrivateProject(GitRepository $repository) {
-        return $repository->getProject()->isPublic() == false;
-    }
-
-    private function canBeReadByAnonymous(GitRepository $repository) {
-        $ugroup_ids = $this->permissions_manager->getAuthorizedUgroupIds($repository->getId(), Git::PERM_READ);
-        foreach ($ugroup_ids as $ugroup_id) {
-            if ($ugroup_id == ProjectUGroup::ANONYMOUS) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private function basicAuthenticationChallenge() {
-        header('WWW-Authenticate: Basic realm="'.ForgeConfig::get('sys_name').' git authentication"');
-        header('HTTP/1.0 401 Unauthorized');
-        exit;
-    }
-
-    private function authenticate(GitRepository $repository, Git_HTTP_Command $command)
-    {
-        if (! isset($_SERVER['PHP_AUTH_USER']) ||
-            $_SERVER['PHP_AUTH_USER'] == '' ||
-            ! isset($_SERVER['PHP_AUTH_PW']) ||
-            $_SERVER['PHP_AUTH_PW'] == ''
-        ) {
-            $this->basicAuthenticationChallenge();
-        } else {
-            $user = null;
-            try {
-                $user = $this->authenticator->authenticate(
-                    $repository,
-                    $_SERVER['PHP_AUTH_USER'],
-                    $_SERVER['PHP_AUTH_PW']
-                );
-
-                $this->logger->debug('LOGGED AS ' . $user->getUnixName());
-            } catch (Exception $exception) {
-                $this->logger->debug('Replication user not recognized ' . $exception->getMessage());
-            }
-
-            if ($user === null) {
-                try {
-                    $user = $this->login_manager->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
-                } catch (Exception $exception) {
-                    $this->logger->debug('LOGIN ERROR ' . $exception->getMessage());
-                    $this->basicAuthenticationChallenge();
-                }
-            }
-
-            return $this->getGitoliteCommand($user, $command);
-        }
     }
 
     private function getGitoliteCommand(PFO_User $user, Git_HTTP_Command $command) {

commit 202c75e4d2b5550eac780336636753f59fe5232c

request #11627: Authentication of replication HTTP users used by Gerrit is always denied
Even when the given credentials are valid the access was denied but the logfile
was correctly indicating that the user were properly identified.

The issue has been introduced by commit ae5b42899a061b6a505e76daa9d9577ff4ba5e69.

Change-Id: If0925261850794a7d2d1ddfebb0418f208d72e4e

plugins/git/include/Git/HTTP/HTTPAccessControl.php
@@ -43,7 +43,7 @@ class HTTPAccessControl
     /**
      * @var ReplicationHTTPUserAuthenticator
      */
-    private $authenticator;
+    private $replication_http_user_authenticator;
 
     /**
      * @var PermissionsManager
@@ -58,17 +58,20 @@ class HTTPAccessControl
     public function __construct(
         Logger $logger,
         User_LoginManager $login_manager,
-        ReplicationHTTPUserAuthenticator $authenticator,
+        ReplicationHTTPUserAuthenticator $replication_http_user_authenticator,
         PermissionsManager $permissions_manager,
         UserDao $user_dao
     ) {
-        $this->logger              = $logger;
-        $this->login_manager       = $login_manager;
-        $this->authenticator       = $authenticator;
-        $this->permissions_manager = $permissions_manager;
-        $this->user_dao            = $user_dao;
+        $this->logger                              = $logger;
+        $this->login_manager                       = $login_manager;
+        $this->replication_http_user_authenticator = $replication_http_user_authenticator;
+        $this->permissions_manager                 = $permissions_manager;
+        $this->user_dao                            = $user_dao;
     }
 
+    /**
+     * @return null|false|\PFO_User
+     */
     public function getUser(\URLVerification $url_verification, \GitRepository $repository, \Git_URL $url)
     {
         $user = null;
@@ -117,7 +120,7 @@ class HTTPAccessControl
 
     /**
      * @param \GitRepository $repository
-     * @return null|false|\PFO_User
+     * @return false|\PFO_User
      */
     private function authenticate(\GitRepository $repository)
     {
@@ -127,34 +130,34 @@ class HTTPAccessControl
             $_SERVER['PHP_AUTH_PW'] == ''
         ) {
             $this->basicAuthenticationChallenge();
-        } else {
-            $user = null;
-            try {
-                $user = $this->authenticator->authenticate(
-                    $repository,
-                    $_SERVER['PHP_AUTH_USER'],
-                    $_SERVER['PHP_AUTH_PW']
-                );
-
-                $this->logger->debug('LOGGED AS ' . $user->getUnixName());
-            } catch (\Exception $exception) {
-                $this->logger->debug('Replication user not recognized ' . $exception->getMessage());
-            }
-
-            if ($user === null) {
-                try {
-                    $user = $this->login_manager->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
-                    $this->logger->debug("LOGGED AS ".$user->getUnixName());
-                    $this->updateLastAccessDateForUser($user);
-                    return $user;
-                } catch (\Exception $exception) {
-                    $this->logger->debug('LOGIN ERROR ' . $exception->getMessage());
-                    $this->basicAuthenticationChallenge();
-                }
-            }
-
-            return false;
         }
+
+        try {
+            $user = $this->replication_http_user_authenticator->authenticate(
+                $repository,
+                $_SERVER['PHP_AUTH_USER'],
+                $_SERVER['PHP_AUTH_PW']
+            );
+
+            $this->logger->debug('LOGGED AS ' . $user->getUnixName());
+            return $user;
+        } catch (\User_InvalidPasswordException $exception) {
+            $this->logger->debug('Replication user not recognized ' . $exception->getMessage());
+        } catch (\Git_RemoteServer_NotFoundException $exception) {
+            $this->logger->debug($exception->getMessage());
+        }
+
+        try {
+            $user = $this->login_manager->authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+            $this->logger->debug('LOGGED AS ' . $user->getUnixName());
+            $this->updateLastAccessDateForUser($user);
+            return $user;
+        } catch (\User_LoginException $exception) {
+            $this->logger->debug('LOGIN ERROR ' . $exception->getMessage());
+            $this->basicAuthenticationChallenge();
+        }
+
+        return false;
     }
 
     private function updateLastAccessDateForUser(PFUser $user)

plugins/git/phpunit/HTTP/HTTPAccessControlTest.php
@@ -1,0 +1,213 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\HTTP;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+use PermissionsManager;
+use PHPUnit\Framework\TestCase;
+use Tuleap\Git\Gerrit\ReplicationHTTPUserAuthenticator;
+
+class HTTPAccessControlTest extends TestCase
+{
+    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    protected function tearDown()
+    {
+        unset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+    }
+
+    public function testHTTPReplicationUserCanBeAuthenticated()
+    {
+        $logger                              = \Mockery::mock(\Logger::class);
+        $user_login_manager                  = \Mockery::mock(\User_LoginManager::class);
+        $replication_http_user_authenticator = \Mockery::mock(ReplicationHTTPUserAuthenticator::class);
+        $permissions_manager                 = \Mockery::mock(PermissionsManager::class);
+        $user_dao                            = \Mockery::mock(\UserDao::class);
+
+        $http_access_control = new HTTPAccessControl(
+            $logger,
+            $user_login_manager,
+            $replication_http_user_authenticator,
+            $permissions_manager,
+            $user_dao
+        );
+
+        $url_verification = \Mockery::mock(\URLVerification::class);
+        $git_repository   = \Mockery::mock(\GitRepository::class);
+        $git_url          = \Mockery::mock(\Git_URL::class);
+
+        $logger->shouldReceive('debug');
+        $url_verification->shouldReceive('doesPlatformRequireLogin')->andReturns(true);
+        $git_repository->shouldReceive('getFullName');
+        $_SERVER['PHP_AUTH_USER'] = 'forge__gerrit_1';
+        $_SERVER['PHP_AUTH_PW']   = 'password';
+        $expected_user = \Mockery::mock(\PFO_User::class);
+        $expected_user->shouldReceive('getUnixName');
+        $replication_http_user_authenticator->shouldReceive('authenticate')->andReturns($expected_user);
+
+        $authenticated_user = $http_access_control->getUser($url_verification, $git_repository, $git_url);
+
+        $this->assertSame($expected_user, $authenticated_user);
+    }
+
+    public function testTuleapUserCanBeAuthenticated()
+    {
+        $logger                              = \Mockery::mock(\Logger::class);
+        $user_login_manager                  = \Mockery::mock(\User_LoginManager::class);
+        $replication_http_user_authenticator = \Mockery::mock(ReplicationHTTPUserAuthenticator::class);
+        $permissions_manager                 = \Mockery::mock(PermissionsManager::class);
+        $user_dao                            = \Mockery::mock(\UserDao::class);
+
+        $http_access_control = new HTTPAccessControl(
+            $logger,
+            $user_login_manager,
+            $replication_http_user_authenticator,
+            $permissions_manager,
+            $user_dao
+        );
+
+        $url_verification = \Mockery::mock(\URLVerification::class);
+        $git_repository   = \Mockery::mock(\GitRepository::class);
+        $git_url          = \Mockery::mock(\Git_URL::class);
+
+        $logger->shouldReceive('debug');
+        $url_verification->shouldReceive('doesPlatformRequireLogin')->andReturns(true);
+        $git_repository->shouldReceive('getFullName');
+        $_SERVER['PHP_AUTH_USER'] = 'user1';
+        $_SERVER['PHP_AUTH_PW']   = 'password';
+        $replication_http_user_authenticator->shouldReceive('authenticate')->
+            andThrows(\Mockery::spy(\Git_RemoteServer_NotFoundException::class));
+        $expected_user = \Mockery::mock(\PFUser::class);
+        $expected_user->shouldReceive('getUnixName');
+        $expected_user->shouldReceive('getId');
+        $user_login_manager->shouldReceive('authenticate')->andReturns($expected_user);
+        $user_dao->shouldReceive('storeLastAccessDate')->once();
+
+        $authenticated_user = $http_access_control->getUser($url_verification, $git_repository, $git_url);
+
+        $this->assertSame($expected_user, $authenticated_user);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testAuthenticationIsDeniedWhenNoValidUserIsFound()
+    {
+        $logger                              = \Mockery::mock(\Logger::class);
+        $user_login_manager                  = \Mockery::mock(\User_LoginManager::class);
+        $replication_http_user_authenticator = \Mockery::mock(ReplicationHTTPUserAuthenticator::class);
+        $permissions_manager                 = \Mockery::mock(PermissionsManager::class);
+        $user_dao                            = \Mockery::mock(\UserDao::class);
+
+        $http_access_control = new HTTPAccessControl(
+            $logger,
+            $user_login_manager,
+            $replication_http_user_authenticator,
+            $permissions_manager,
+            $user_dao
+        );
+
+        $url_verification = \Mockery::mock(\URLVerification::class);
+        $git_repository   = \Mockery::mock(\GitRepository::class);
+        $git_url          = \Mockery::mock(\Git_URL::class);
+
+        $logger->shouldReceive('debug');
+        $url_verification->shouldReceive('doesPlatformRequireLogin')->andReturns(true);
+        $git_repository->shouldReceive('getFullName');
+        $_SERVER['PHP_AUTH_USER'] = 'user1';
+        $_SERVER['PHP_AUTH_PW']   = 'invalid_password';
+        $replication_http_user_authenticator->shouldReceive('authenticate')->
+            andThrows(\Mockery::spy(\Git_RemoteServer_NotFoundException::class));
+        $user_login_manager->shouldReceive('authenticate')->
+            andThrows(\Mockery::spy(\User_LoginException::class));
+
+        $http_access_control->getUser($url_verification, $git_repository, $git_url);
+
+        $this->fail('The test should have exited to request a valid basic authentication');
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testAuthenticationIsRequestedWhenEmptyCredentialIsGiven()
+    {
+        $logger                              = \Mockery::mock(\Logger::class);
+        $user_login_manager                  = \Mockery::mock(\User_LoginManager::class);
+        $replication_http_user_authenticator = \Mockery::mock(ReplicationHTTPUserAuthenticator::class);
+        $permissions_manager                 = \Mockery::mock(PermissionsManager::class);
+        $user_dao                            = \Mockery::mock(\UserDao::class);
+
+        $http_access_control = new HTTPAccessControl(
+            $logger,
+            $user_login_manager,
+            $replication_http_user_authenticator,
+            $permissions_manager,
+            $user_dao
+        );
+
+        $url_verification = \Mockery::mock(\URLVerification::class);
+        $git_repository   = \Mockery::mock(\GitRepository::class);
+        $git_url          = \Mockery::mock(\Git_URL::class);
+
+        $logger->shouldReceive('debug');
+        $url_verification->shouldReceive('doesPlatformRequireLogin')->andReturns(true);
+        $git_repository->shouldReceive('getFullName');
+        $_SERVER['PHP_AUTH_USER'] = '';
+        $_SERVER['PHP_AUTH_PW']   = '';
+
+        $http_access_control->getUser($url_verification, $git_repository, $git_url);
+
+        $this->fail('The test should have exited to request a valid basic authentication');
+    }
+
+    public function testNoAuthenticationIsRequiredForAReadAccessOfPublicRepoOnAnInstanceAccessibleToAnonymous()
+    {
+        $logger                              = \Mockery::mock(\Logger::class);
+        $user_login_manager                  = \Mockery::mock(\User_LoginManager::class);
+        $replication_http_user_authenticator = \Mockery::mock(ReplicationHTTPUserAuthenticator::class);
+        $permissions_manager                 = \Mockery::mock(PermissionsManager::class);
+        $user_dao                            = \Mockery::mock(\UserDao::class);
+
+        $http_access_control = new HTTPAccessControl(
+            $logger,
+            $user_login_manager,
+            $replication_http_user_authenticator,
+            $permissions_manager,
+            $user_dao
+        );
+
+        $url_verification = \Mockery::mock(\URLVerification::class);
+        $git_repository   = \Mockery::mock(\GitRepository::class);
+        $git_url          = \Mockery::mock(\Git_URL::class);
+
+        $logger->shouldReceive('debug');
+        $url_verification->shouldReceive('doesPlatformRequireLogin')->andReturns(false);
+        $git_url->shouldReceive('isGitPush')->andReturns(false);
+        $project = \Mockery::mock(\Project::class);
+        $project->shouldReceive('isPublic')->andReturns(true);
+        $git_repository->shouldReceive('getProject')->andReturns($project);
+        $git_repository->shouldReceive('getId')->andReturns(1);
+        $permissions_manager->shouldReceive('getAuthorizedUgroupIds')->andReturns([\ProjectUGroup::ANONYMOUS]);
+
+        $this->assertNull($http_access_control->getUser($url_verification, $git_repository, $git_url));
+    }
+}

tests/phpunit/phpunit.xml
@@ -8,6 +8,7 @@
     <filter>
         <whitelist processUncoveredFilesFromWhitelist="true">
             <file>../../src/common/Dashboard/Dashboard.php</file>
+            <file>../../plugins/git/include/Git/HTTP/HTTPAccessControl.php</file>
             <directory suffix=".php">../../src/common/user</directory>
             <directory suffix=".php">../../src/common/frs/PermissionsPerGroup</directory>
             <directory suffix=".php">../../src/common/Password/</directory>