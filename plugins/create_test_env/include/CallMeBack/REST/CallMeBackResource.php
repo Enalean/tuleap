bc1ec69789074c401bcee309d0b48ee2fd57dd19
Benjamin Dauton <benjamin.dauton@enalean.com>
2018-06-18 13:41:18 +0200

    Add REST route POST /api/call_me_back
    This route send an email (defined in plugin configuration in the site admin
    part of Tuleap) and set a user preference so we will be able to not display
    the CTA button again.
    
    Part of the story #11593 Have a Call Me Back button and form on every page
    
    Change-Id: Ib8409cfefac71f5fd2309841a658c120da2f700e

---
+++ plugins/create_test_env/include/CallMeBack/REST/CallMeBackResource.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\CallMeBack\REST;
+
+use PFUser;
+use EventManager;
+use StandardPasswordHandler;
+use User_LoginManager;
+use User_PasswordExpirationChecker;
+use Tuleap\REST\Header;
+use Tuleap\REST\UserManager;
+use Tuleap\REST\AuthenticatedResource;
+use Luracast\Restler\RestException;
+use Tuleap\CallMeBack\CallMeBackEmailNotifier;
+use Tuleap\CallMeBack\CallMeBackEmailDao;
+use Tuleap\CallMeBack\Exception\NotifyException;
+
+class CallMeBackResource extends AuthenticatedResource
+{
+    /**
+     * @var UserManager
+     */
+    private $rest_user_manager;
+
+    /**
+     * @var CallMeBackEmailNotifier
+     */
+    private $notifier;
+
+    public function __construct()
+    {
+        $this->rest_user_manager = UserManager::build();
+        $this->notifier          = new CallMeBackEmailNotifier(
+            new CallMeBackEmailDao()
+        );
+    }
+
+    /**
+     * @url OPTIONS
+     */
+    public function options()
+    {
+        $this->sendAllowHeaders();
+    }
+
+    /**
+     * Send an email with call me back information
+     *
+     * @param string $phone {@from body} User phone number
+     * @param string $date {@from body} Date when user want to be called back
+     *
+     * @access protected
+     *
+     * @url POST
+     * @status 201
+     *
+     * @throws 403
+     * @throws 500
+     */
+    public function post($phone, $date)
+    {
+        try {
+            $this->checkAccess();
+
+            $current_user = $this->rest_user_manager->getCurrentUser();
+
+            $this->notifier->notify($current_user, $phone, $date);
+
+            $current_user->setPreference('plugin_call_me_back_asked_to_be_called_back', '1');
+        } catch (NotifyException $exception) {
+            throw new RestException(500, "Unable to notify the people who will call back");
+        } finally {
+            $this->sendAllowHeaders();
+        }
+    }
+
+    private function sendAllowHeaders()
+    {
+        Header::allowOptionsPost();
+    }
+}
