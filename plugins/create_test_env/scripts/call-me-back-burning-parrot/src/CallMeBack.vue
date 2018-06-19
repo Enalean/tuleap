84751d1025551ef1dbd04fccff8872a134e24c5a
Benjamin Dauton <benjamin.dauton@enalean.com>
2018-06-19 11:57:02 +0200

    On form submit in BP pages, send an email
    Part of the story #11593 Have a Call Me Back button and form on every page
    
    Change-Id: Ie718c0ac60fe72629fc3964a10a500646a6c51dd

--- plugins/create_test_env/scripts/call-me-back-burning-parrot/src/CallMeBack.vue
+++ plugins/create_test_env/scripts/call-me-back-burning-parrot/src/CallMeBack.vue
@@ -21,51 +21,85 @@
         <button class="call-me-back-button tlp-button-primary tlp-button-large" ref="call_me_back_button">
             <i class="fa fa-coffee"></i>
         </button>
-        <form class="call-me-back-form tlp-dropdown-menu tlp-dropdown-menu-top tlp-dropdown-menu-right">
-            <p class="call-me-back-form-intro">
-                <translate>Fill this form and we'll call you back.</translate>
-            </p>
-            <div class="tlp-form-element">
-                <label class="tlp-label" for="call-me-back-phone-number">
-                    <translate>Phone number</translate> <i class="fa fa-asterisk"></i>
-                </label>
-                <div class="tlp-form-element tlp-form-element-prepend">
-                    <span class="tlp-prepend"><i class="fa fa-phone"></i></span>
-                    <input type="text"
-                        class="tlp-input"
-                        id="call-me-back-phone-number"
-                        name="phone"
-                        placeholder="+33 0 00 00 00 00"
-                    >
-                </div>
+        <div class="call-me-back-form tlp-dropdown-menu tlp-dropdown-menu-top tlp-dropdown-menu-right">
+            <div class="call-me-back-save-the-date" v-if="save_the_date">
+                <i class="fa fa-thumbs-o-up"></i>
+                <p v-translate>We will call you back on</p>
+                <span class="tlp-badge-success tlp-badge-outline">{{ call_me_back_formatted_date }}</span>
             </div>
-            <div class="tlp-form-element">
-                <label class="tlp-label" for="call-me-back-date">
-                    <translate>Call me back on</translate> <i class="fa fa-asterisk"></i>
-                </label>
-                <div class="tlp-form-element tlp-form-element-prepend">
-                    <span class="tlp-prepend"><i class="fa fa-calendar"></i></span>
-                    <input type="text"
-                        ref="call_me_back_date"
-                        class="tlp-input tlp-input-date"
-                        id="call-me-back-date"
-                        name="date"
-                        placeholder="yyyy-mm-dd"
-                    >
+
+            <form v-else>
+                <p class="call-me-back-form-intro" v-translate>
+                    Fill this form and we'll call you back.
+                </p>
+                <div class="tlp-alert-danger" v-if="error" v-translate>
+                    Ooops something went wrong. Please try again later.
                 </div>
-            </div>
-            <button type="button" class="tlp-button-primary tlp-button-wide">
-                <translate>Call me back</translate>
-            </button>
-        </form>
+                <div class="tlp-form-element">
+                    <label class="tlp-label" for="call-me-back-phone-number">
+                        <translate>Phone number</translate> <i class="fa fa-asterisk"></i>
+                    </label>
+                    <div class="tlp-form-element tlp-form-element-prepend">
+                        <span class="tlp-prepend"><i class="fa fa-phone"></i></span>
+                        <input type="text"
+                            class="tlp-input"
+                            id="call-me-back-phone-number"
+                            name="call_me_back_phone"
+                            placeholder="+33000000000"
+                            v-model="call_me_back_phone"
+                        >
+                    </div>
+                </div>
+                <div class="tlp-form-element">
+                    <label class="tlp-label" for="call-me-back-date">
+                        <translate>Call me back on</translate> <i class="fa fa-asterisk"></i>
+                    </label>
+                    <div class="tlp-form-element tlp-form-element-prepend">
+                        <span class="tlp-prepend"><i class="fa fa-calendar"></i></span>
+                        <input type="text"
+                            ref="call_me_back_date"
+                            class="tlp-input tlp-input-date"
+                            id="call-me-back-date"
+                            name="call_me_back_date"
+                            placeholder="yyyy-mm-dd"
+                            v-model="call_me_back_date"
+                        >
+                    </div>
+                </div>
+                <button type="button"
+                    class="tlp-button-primary tlp-button-wide"
+                    v-on:click="callMeBack"
+                    v-bind:disabled="loading"
+                >
+                    <i class="tlp-button-icon fa fa-spinner fa-spin" v-if="loading"></i>
+                    <translate>Call me back</translate>
+                </button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
 import { dropdown, datePicker } from 'tlp';
+import { askToBeCalledBack }    from './rest-querier.js';
+import { DateTime }             from 'luxon';
 
 export default {
     name: 'CallMeBack',
+    data() {
+        return {
+            loading           : false,
+            error             : false,
+            save_the_date     : false,
+            call_me_back_phone: '',
+            call_me_back_date : ''
+        };
+    },
+    computed: {
+        call_me_back_formatted_date() {
+            return DateTime.fromISO(this.call_me_back_date).toLocaleString(DateTime.DATE_FULL);
+        }
+    },
     mounted() {
         this.bindDropdown();
         this.bindCalendar();
@@ -78,9 +112,29 @@ export default {
             datePicker(
                 this.$refs.call_me_back_date,
                 {
-                    static: true
+                    static: true,
+                    onValueUpdate: (date, string_value) => {
+                        this.call_me_back_date = string_value;
+                    }
                 }
             );
+        },
+        async callMeBack() {
+            this.loading = true;
+
+            try {
+                await askToBeCalledBack(
+                    this.call_me_back_phone,
+                    this.call_me_back_date
+                );
+
+                this.save_the_date = true;
+            } catch (e) {
+                const { error } = await e.response.json();
+                this.error      = error.code + ' ' + error.message;
+            } finally {
+                this.loading = false;
+            }
         }
     }
 };
