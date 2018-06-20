Enalean/tuleap

commit 045da64aa4c1e88e589ebc31bd552b768c714458

Vue-ify old palette
Part of story #11542 - Color on cards

How to test:
- No functional changes on old palette

Change-Id: Ibc7a600bf9cd708af459df78cff81775af7f9ccb

plugins/tracker/www/scripts/TrackerAdminFields.js
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Tuleap, 2017. All rights reserved
+ * Copyright (c) Tuleap, 2017 - 2018. All rights reserved
  *
  * This file is a part of Codendi.
  *
@@ -17,6 +17,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
+
+import { createOldColorPicker } from './colorpicker/index.js';
 
 document.observe('dom:loaded', function () {
 
@@ -238,8 +240,8 @@ document.observe('dom:loaded', function () {
                             //Put here the javascript stuff you need to call once the content of the modal dialog is loaded
 
                             //Color picker
-                            selected_element.select('.colorpicker').each(function (element) {
-                                    codendi.colorpicker.colorpickers[element.identify()] = new codendi.colorpicker.Colorpicker(element);
+                            selected_element.select('.vue-mount-point').each(function (element) {
+                                    createOldColorPicker(element);
                                 }
                             );
 

gulpfile.js
@@ -54,7 +54,6 @@ const fat_combined_files = [
         'src/www/scripts/codendi/Tooltip-loader.js',
         'src/www/scripts/codendi/Toggler.js',
         'src/www/scripts/codendi/DropDownPanel.js',
-        'src/www/scripts/codendi/colorpicker.js',
         'src/www/scripts/autocomplete.js',
         'src/www/scripts/textboxlist/multiselect.js',
         'src/www/scripts/tablekit/tablekit.js',

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_BindDecorator.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved.
+ * Copyright (c) Enalean, 2011 - 2018. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once 'common/layout/ColorHelper.class.php';
@@ -33,10 +34,10 @@ class Tracker_FormElement_Field_List_BindDecorator {
         $this->g        = $g;
         $this->b        = $b;
     }
-    
+
     /**
      * Decorate a value.
-     * @param string $value The value to decorate Don't forget to html-purify. 
+     * @param string $value The value to decorate Don't forget to html-purify.
      * @param boolean $full false if you want only the decoration
      * @return string html
      */
@@ -51,14 +52,14 @@ class Tracker_FormElement_Field_List_BindDecorator {
         }
         return $html;
     }
-    
+
     public function decorateSelectOption() {
         return 'border-left: 16px solid '. ColorHelper::RGBToHexa($this->r, $this->g, $this->b) .';';
     }
-    
+
     /**
      * Display the color and allow the user to edit it
-     * @param string $value The value to decorate Don't forget to html-purify. 
+     * @param string $value The value to decorate Don't forget to html-purify.
      * @param boolean $full false if you want only the decoration
      * @return string html
      */
@@ -66,25 +67,35 @@ class Tracker_FormElement_Field_List_BindDecorator {
         $html = '';
         $hexa = ColorHelper::RGBToHexa($this->r, $this->g, $this->b);
         $id   = 'decorator_'. $this->field_id .'_'. $this->value_id;
-        $html .= self::fetchSquareColor($id, $hexa, 'colorpicker', $this->r, $this->g, $this->b);
-        $html .= '<input id="'. $id .'_field" type="text" size="6" autocomplete="off" name="bind[decorator]['. $this->value_id .']" value="'. $hexa .'" />';
+        $html .= self::getColorPickerMountPoint($id, $this->value_id, $hexa);
+
         return $html;
     }
-    
+
+    private static function getColorPickerMountPoint($decorator_id, $value_id, $hexa_color)
+    {
+        return '
+            <div class="vue-mount-point"
+                data-decorator-id="'. $decorator_id .'"
+                data-value-id="'. $value_id .'"
+                data-color-hexa="'. $hexa_color . '"
+            ></div>
+        ';
+    }
+
     /**
      * Display the transparent color and allow the user to edit it
-     * @param string $value The value to decorate Don't forget to html-purify. 
+     * @param string $value The value to decorate Don't forget to html-purify.
      * @param boolean $full false if you want only the decoration
      * @return string html
      */
     public static function noDecoratorEdit($field_id, $value_id) {
         $html = '';
         $id   = 'decorator_'. $field_id .'_'. $value_id;
-        $html .= self::fetchSquareColor($id, 'transparent', 'colorpicker', null, null, null, 'ic/layer-transparent.png');
-        $html .= '<input id="'. $id .'_field" type="text" size="6" autocomplete="off" name="bind[decorator]['. $value_id .']" value="" />';
+        $html .= self::getColorPickerMountPoint($id, $value_id, null);
         return $html;
     }
-    
+
     protected static function fetchSquareColor($id, $title, $classname, $r, $g, $b, $img = 'blank16x16.png') {
         $html = '';
         $bgcolor = '';
@@ -101,7 +112,7 @@ class Tracker_FormElement_Field_List_BindDecorator {
         ));
         return $html;
     }
-    
+
     /**
      * @return string the internal structure of  the decorator as JSON
      */
@@ -116,7 +127,7 @@ class Tracker_FormElement_Field_List_BindDecorator {
             )
         );
     }
-    
+
     /**
      * Save a decorator
      */
@@ -125,7 +136,7 @@ class Tracker_FormElement_Field_List_BindDecorator {
         list($r, $g, $b) = ColorHelper::HexaToRGB($hexacolor);
         $dao->save($field_id, $value_id, $r, $g, $b);
     }
-    
+
     /**
      * Delete a decorator
      */
@@ -136,7 +147,7 @@ class Tracker_FormElement_Field_List_BindDecorator {
 
     /**
      * Transforms Bind into a SimpleXMLElement
-     * 
+     *
      * @param SimpleXMLElement $root the node to which the Bind is attached (passed by reference)
      * @param int $val the id indentifing the value in the XML (different form $this->value_id)
      */

commit edbba3eb6143aee65834c5e1ec87672fbfba6849

request #11645: Tracker administration of triggers is not usable
The triggers tab always show an empty, non deletable, trigger
The global rules tab is also partially broken, the fields to add a new
rule are always displayed.

While being there, we also took the opportunity to fix the selectable value
that must be set on the field impacted by the trigger.

Issue has been introduced by commit 045da64aa4c1e88e589ebc31bd552b768c714458.

Change-Id: Id8790cafe032423c88aa153dd9aeb1cccaa7d304

plugins/tracker/www/scripts/TrackerAdminFieldWorkflow.js
@@ -1,0 +1,214 @@
+/**
+ * Copyright (c) Tuleap, 2017 - 2018. All rights reserved
+ * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+document.observe('dom:loaded', function () {
+    $$(".tracker-field-richtext").each(function define_rich_text(elem) {
+        var r = new codendi.RTE(elem);
+    });
+
+    $$("input[type=checkbox][name^=remove_postaction]", "input[type=checkbox][name^=remove_rule]").each(function (elem) {
+        elem.observe('click', function (evt) {
+            if (elem.checked) {
+                elem.up('tr').down('div').addClassName('deleted');
+                elem.up('tr').select('select').each(function (e) { e.disabled = true; e.readOnly = true; });
+                elem.up('tr').select('input').each(function (e) { e.required = false; });
+            } else {
+                elem.up('tr').down('div').removeClassName('deleted');
+                elem.up('tr').select('select').each(function (e) { e.disabled = false; e.readOnly = false; });
+                elem.up('tr').select('input').each(function (e) { if (e.hasClassName('required')) e.required = true; });
+            }
+        });
+    });
+
+    $$('.add_new_rule_title').each(function (add) {
+        var link = new Element('a', { href: '#add_new_rule' })
+            .update(add.innerHTML)
+            .observe('click', function (evt) {
+                add.next().toggle();
+                Event.stop(evt);
+                return false;
+            });
+        add.update(link);
+        add.next().hide();
+    });
+
+    /*
+     * Trigger event handling
+     */
+    (function trigger_init() {
+        if (! $('add_new_trigger_title')) {
+            return;
+        }
+
+        var trigger                   = new tuleap.trackers.trigger(),
+            existing_triggers_table   = $('triggers_existing').down('tbody'),
+            triggering_field_template = existing_triggers_table.down('.trigger_description_triggering_field').remove(),
+            trigger_template          = existing_triggers_table.down('tr').remove();
+
+        function displayExistingTriggers() {
+            if (tuleap.trackers.trigger.existing.size() == 0) {
+                return;
+            }
+
+            tuleap.trackers.trigger.existing.each(function(trigger) {
+                displayTrigger(trigger);
+            });
+        }
+
+        function displayTrigger(trigger_as_JSON) {
+            var trigger_id      = trigger_as_JSON.id,
+                trigger_element = addTriggerContainer(trigger_template, trigger_id);
+
+            trigger_element.down('.trigger_description_target_field_name').update(trigger_as_JSON.target.field_label);
+            trigger_element.down('.trigger_description_target_field_value').update(trigger_as_JSON.target.field_value_label);
+
+            trigger_as_JSON.triggering_fields.each(function(triggering_field) {
+                addTriggeringField(triggering_field, trigger_element);
+            });
+            removeFirstOperator(trigger_element);
+            bindRemove(trigger_element, trigger_id);
+
+            function addTriggerContainer(trigger_template, trigger_id) {
+                var trigger_element;
+
+                existing_triggers_table.insert(trigger_template.cloneNode(true));
+                trigger_element = existing_triggers_table.childElements().last();
+                trigger_element.writeAttribute('data-trigger-id', trigger_id);
+
+                return trigger_element;
+            }
+
+            function addTriggeringField(triggering_field, trigger_element) {
+                var triggering_fields_list     = trigger_element.down('.trigger_description_triggering_fields'),
+                    condition                  = codendi.locales.tracker_trigger[trigger_as_JSON.condition].name,
+                    operator                   = codendi.locales.tracker_trigger[trigger_as_JSON.condition].operator,
+                    have_field                 = codendi.locales.tracker_trigger[trigger_as_JSON.condition].have_field,
+                    triggering_field_element;
+
+                triggering_fields_list.insert(triggering_field_template.cloneNode(true));
+                triggering_field_element = triggering_fields_list.childElements().last();
+
+                triggering_field_element.down('.trigger_description_triggering_field_operator')
+                    .update(operator);
+                triggering_field_element.down('.trigger_description_triggering_field_quantity')
+                    .update(condition);
+                triggering_field_element.down('.trigger_description_triggering_have_field')
+                    .update(have_field);
+                triggering_field_element.down('.trigger_description_triggering_field_tracker')
+                    .update(triggering_field.tracker_name);
+                triggering_field_element.down('.trigger_description_triggering_field_field_name')
+                    .update(triggering_field.field_label);
+                triggering_field_element.down('.trigger_description_triggering_field_field_value')
+                    .update(triggering_field.field_value_label);
+
+                if (triggering_fields_list.childElements().size() > 1) {
+                    triggering_field_element.down('.trigger_description_triggering_field_when').hide();
+                }
+            }
+
+            function removeFirstOperator(trigger_element) {
+                trigger_element.down('.trigger_description_triggering_field_operator').update('');
+            }
+
+            function bindRemove(trigger_element, trigger_id) {
+                Event.observe(trigger_element.down('.trigger_remove'), 'click', function() {
+                    var query_params = window.location.href.toQueryParams();
+                    new Ajax.Request(
+                        codendi.tracker.base_url+'?tracker='+query_params['tracker']+'&id='+trigger_id+'&func=admin-workflow-delete-trigger',
+                        {
+                            'method' : "POST",
+                            'onSuccess': function () {
+                                trigger_element.remove();
+                            },
+                            'onFailure' : function(response) {
+                                alert(response.responseText);
+                            }
+                        }
+                    );
+                });
+            }
+        }
+
+        function reset() {
+            trigger.getTriggeringFields().each(function(triggering_field) {
+                if (triggering_field.getContainer().readAttribute('data-trigger-condition-initial') !== 'true') {
+                    trigger.removeTriggeringField(triggering_field);
+                } else {
+                    triggering_field.removeAllOptions();
+                }
+
+                $('triggers_form').reset();
+            });
+        }
+
+        (function display() {
+            $('trigger_create_new').hide();
+
+            (function bindAddNewTrigger() {
+                Event.observe($('add_new_trigger_title'), 'click', function(evt) {
+                    $('add_new_trigger_title').hide();
+                    $('trigger_create_new').show();
+                    Event.stop(evt);
+                });
+            })();
+
+            displayExistingTriggers();
+        })();
+
+        (function bindAddExtraTriggeringField() {
+            Event.observe($('trigger_add_condition'), 'click', function(evt) {
+                var triggering_field = trigger.addTriggeringField();
+
+                triggering_field.activateDeleteButton(trigger);
+                triggering_field.makeOperatorDynamic();
+                Event.stop(evt);
+            });
+        })();
+
+        (function bindCancelAddNewTrigger(){
+            Event.observe($('trigger_add_cancel'), 'click', function(evt) {
+                $('trigger_create_new').hide();
+                $('add_new_trigger_title').show();
+                reset();
+                Event.stop(evt);
+            });
+        })();
+
+        (function bindSubmitNewTrigger(){
+            var callback = function() {
+                $('trigger_create_new').hide();
+                $('add_new_trigger_title').show();
+                displayNewTrigger(trigger);
+            };
+
+            Event.observe($('trigger_submit_new'), 'click', function() {
+                trigger.save(callback);
+            });
+
+            function displayNewTrigger(trigger) {
+                var trigger_as_JSON = trigger.toJSON();
+
+                trigger_as_JSON.id = trigger.getId();
+                displayTrigger(trigger_as_JSON);
+                reset();
+            }
+        })();
+    })();
+});

plugins/tracker/www/scripts/TrackerAdminFields.js
@@ -2,20 +2,20 @@
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  * Copyright (c) Tuleap, 2017 - 2018. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import { createColorPicker } from './colorpicker/index.js';
@@ -275,248 +275,6 @@ document.observe('dom:loaded', function () {
             });
         });
     }
-
-
-    // {{{ Drag 'n Drop
-    /*
-    $$('.tracker-admin-field', '.tracker-admin-container').each(function (field) {
-        var d = new Draggable(field, {
-                handle: field.down('label'),
-                revert: true,
-                ghosting: true,
-                scroll: window
-            }
-        );
-    });
-
-    $$('.tracker-admin-container').each(function (container) {
-        Droppables.add(container, {
-            accept: ['tracker-admin-container', 'tracker-admin-field'],
-            hoverclass: 'tracker-admin-container_drop',
-            onDrop: function (drag, drop, evt) {
-                if (drop !== drag.up('.tracker-admin-container')) {
-                    drop.insert({bottom: drag.remove()}).setStyle({
-                        left: 'auto',
-                        top: 'auto'
-                    });
-                }
-            }
-        });
-    });
-    // }}} */
-
-    var button = $('button_preview_xml');
-    if (button) {
-        var iframe = new Element('iframe', {
-                src: 'about:blank',
-                name:  'preview_xml_dialog',
-                style: 'display: none; width:100%; height: 300px;'
-            }
-        );
-        var td = new Element('td');
-        td.colSpan = 4;
-        button.up('tbody').appendChild(new Element('tr')).appendChild(td);
-        td.appendChild(iframe);
-        button.observe('click', function (evt) {
-            button.form.target = 'preview_xml_dialog';
-            iframe.show();
-        });
-    }
-
-    $$(".tracker-field-richtext").each(function define_rich_text(elem) {
-        var r = new codendi.RTE(elem);
-    });
-
-    $$("input[type=checkbox][name^=remove_postaction]", "input[type=checkbox][name^=remove_rule]").each(function (elem) {
-        elem.observe('click', function (evt) {
-            if (elem.checked) {
-                elem.up('tr').down('div').addClassName('deleted');
-                elem.up('tr').select('select').each(function (e) { e.disabled = true; e.readOnly = true; });
-                elem.up('tr').select('input').each(function (e) { e.required = false; });
-            } else {
-                elem.up('tr').down('div').removeClassName('deleted');
-                elem.up('tr').select('select').each(function (e) { e.disabled = false; e.readOnly = false; });
-                elem.up('tr').select('input').each(function (e) { if (e.hasClassName('required')) e.required = true; });
-            }
-        });
-    });
-
-    $$('.add_new_rule_title').each(function (add) {
-        var link = new Element('a', { href: '#add_new_rule' })
-            .update(add.innerHTML)
-            .observe('click', function (evt) {
-                add.next().toggle();
-                Event.stop(evt);
-                return false;
-            });
-        add.update(link);
-        add.next().hide();
-    });
-
-    /*
-     * Trigger event handling
-     */
-
-
-    (function trigger_init() {
-        if (! $('add_new_trigger_title')) {
-            return;
-        }
-
-        var trigger                   = new tuleap.trackers.trigger(),
-            existing_triggers_table   = $('triggers_existing').down('tbody'),
-            triggering_field_template = existing_triggers_table.down('.trigger_description_triggering_field').remove(),
-            trigger_template          = existing_triggers_table.down('tr').remove();
-
-        function displayExistingTriggers() {
-            if (tuleap.trackers.trigger.existing.size() == 0) {
-                return;
-            }
-
-            tuleap.trackers.trigger.existing.each(function(trigger) {
-                displayTrigger(trigger);
-            });
-        }
-
-        function displayTrigger(trigger_as_JSON) {
-            var trigger_id      = trigger_as_JSON.id,
-                trigger_element = addTriggerContainer(trigger_template, trigger_id);
-
-            trigger_element.down('.trigger_description_target_field_name').update(trigger_as_JSON.target.field_label);
-            trigger_element.down('.trigger_description_target_field_value').update(trigger_as_JSON.target.field_value_label);
-
-            trigger_as_JSON.triggering_fields.each(function(triggering_field) {
-                addTriggeringField(triggering_field, trigger_element);
-            });
-            removeFirstOperator(trigger_element);
-            bindRemove(trigger_element, trigger_id);
-
-            function addTriggerContainer(trigger_template, trigger_id) {
-                var trigger_element;
-
-                existing_triggers_table.insert(trigger_template.cloneNode(true));
-                trigger_element = existing_triggers_table.childElements().last();
-                trigger_element.writeAttribute('data-trigger-id', trigger_id);
-
-                return trigger_element;
-            }
-
-            function addTriggeringField(triggering_field, trigger_element) {
-                var triggering_fields_list     = trigger_element.down('.trigger_description_triggering_fields'),
-                    condition                  = codendi.locales.tracker_trigger[trigger_as_JSON.condition].name,
-                    operator                   = codendi.locales.tracker_trigger[trigger_as_JSON.condition].operator,
-                    have_field                 = codendi.locales.tracker_trigger[trigger_as_JSON.condition].have_field,
-                    triggering_field_element;
-
-                triggering_fields_list.insert(triggering_field_template.cloneNode(true));
-                triggering_field_element = triggering_fields_list.childElements().last();
-
-                triggering_field_element.down('.trigger_description_triggering_field_operator')
-                        .update(operator);
-                triggering_field_element.down('.trigger_description_triggering_field_quantity')
-                        .update(condition);
-                triggering_field_element.down('.trigger_description_triggering_have_field')
-                        .update(have_field);
-                triggering_field_element.down('.trigger_description_triggering_field_tracker')
-                        .update(triggering_field.tracker_name);
-                triggering_field_element.down('.trigger_description_triggering_field_field_name')
-                        .update(triggering_field.field_label);
-                triggering_field_element.down('.trigger_description_triggering_field_field_value')
-                        .update(triggering_field.field_value_label);
-
-                if (triggering_fields_list.childElements().size() > 1) {
-                    triggering_field_element.down('.trigger_description_triggering_field_when').hide();
-                }
-            }
-
-            function removeFirstOperator(trigger_element) {
-                trigger_element.down('.trigger_description_triggering_field_operator').update('');
-            }
-
-            function bindRemove(trigger_element, trigger_id) {
-                Event.observe(trigger_element.down('.trigger_remove'), 'click', function() {
-                    var query_params = window.location.href.toQueryParams();
-                    new Ajax.Request(
-                        codendi.tracker.base_url+'?tracker='+query_params['tracker']+'&id='+trigger_id+'&func=admin-workflow-delete-trigger',
-                        {
-                            'method' : "POST",
-                            'onSuccess': function () {
-                                trigger_element.remove();
-                            },
-                            'onFailure' : function(response) {
-                                alert(response.responseText);
-                            }
-                        }
-                    );
-                });
-            }
-        }
-
-        function reset() {
-            trigger.getTriggeringFields().each(function(triggering_field) {
-                if (triggering_field.getContainer().readAttribute('data-trigger-condition-initial') !== 'true') {
-                    trigger.removeTriggeringField(triggering_field);
-                } else {
-                    triggering_field.removeAllOptions();
-                }
-
-                $('triggers_form').reset();
-            });
-        }
-
-        (function display() {
-            $('trigger_create_new').hide();
-
-            (function bindAddNewTrigger() {
-                Event.observe($('add_new_trigger_title'), 'click', function(evt) {
-                    $('add_new_trigger_title').hide();
-                    $('trigger_create_new').show();
-                    Event.stop(evt);
-                });
-            })();
-
-            displayExistingTriggers();
-        })();
-
-        (function bindAddExtraTriggeringField() {
-            Event.observe($('trigger_add_condition'), 'click', function(evt) {
-                var triggering_field = trigger.addTriggeringField();
-
-                triggering_field.activateDeleteButton(trigger);
-                triggering_field.makeOperatorDynamic();
-                Event.stop(evt);
-           });
-        })();
-
-        (function bindCancelAddNewTrigger(){
-            Event.observe($('trigger_add_cancel'), 'click', function(evt) {
-                $('trigger_create_new').hide();
-                $('add_new_trigger_title').show();
-                reset();
-                Event.stop(evt);
-            });
-        })();
-
-        (function bindSubmitNewTrigger(){
-            var callback = function() {
-                $('trigger_create_new').hide();
-                $('add_new_trigger_title').show();
-                displayNewTrigger(trigger);
-            };
-
-            Event.observe($('trigger_submit_new'), 'click', function() {
-                trigger.save(callback);
-            });
-
-            function displayNewTrigger(trigger) {
-                var trigger_as_JSON = trigger.toJSON();
-
-                trigger_as_JSON.id = trigger.getId();
-                displayTrigger(trigger_as_JSON);
-                reset();
-            }
-        })();
-    })();
 });
 
 

plugins/tracker/www/scripts/TrackerTriggers.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Enalean, 2013-2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -150,10 +150,11 @@ tuleap.trackers.trigger = Class.create({
         }
 
         function populateTargetFieldValues(field_values) {
-            $H(field_values.values).each(function(field_value) {
-                var option = createOption(field_value.value, "trigger-target-field-value");
-                $('trigger_target_field_value').appendChild(option);
-            })
+            field_values.values.forEach(function (field_value) {
+                $('trigger_target_field_value').appendChild(
+                    createOption(field_value, "trigger-target-field-value")
+                );
+            });
         }
     },
 