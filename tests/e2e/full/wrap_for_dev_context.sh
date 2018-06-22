4de754a47bc8b7a360584c4a9a5aea9988faa55d
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2018-06-22 11:32:40 +0200

    Have a user friendly way to write cypress tests
    part of request #11328 move some TTM tests to cypress
    
    We have 2 different commands to write/maintain REST tests:
     - tests_cypress: Run Cypress tests locally
     - tests_cypress_dev: start a new container with Cypress projects to write/debugg tests
    
    How to test:
     - make tests_cypress
       => test should be executed, results are stored under test_results_e2e_full
    
     - make tests_cypress_dev
    => you should have a new docker container: tuleap_runtests_backend-web-e2e
       inspect container and retrieve its IP,
       in /etc/hosts add a new  line
       172.19.0.2   tuleap
     - go under tests/e2e/full
       npx cypress open
    => the electron app should open, you should be able to launch and write new tests
       (if https://tuleap is not found wait 1 or 2 minutes that the container has full started)
    
    If you run again make tests_cypress_dev the container will be recreated from scratch
    and you should have only _fixture projects
    
    Change-Id: I04e89b4da0e7859dce1cc651ee9679ea06b7d7a2

---
+++ tests/e2e/full/wrap_for_dev_context.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is the central script to execute in order to execute "e2e tests"
+# This will bring-up the platform, open cypress
+
+set -euxo pipefail
+DOCKERCOMPOSE="docker-compose -f docker-compose-e2e-full-tests-with-app.yml -p e2e-tests"
+
+test_results_folder='./test_results_e2e_full'
+if [ "$#" -eq "1" ]; then
+    test_results_folder="$1"
+fi
+
+clean_env() {
+    $DOCKERCOMPOSE down --remove-orphans --volumes || true
+}
+
+mkdir -p "$test_results_folder" || true
+rm -rf "$test_results_folder/*" || true
+
+clean_env
+
+echo "$DOCKERCOMPOSE up -d --build"
+
+TEST_RESULT_OUTPUT="$test_results_folder" $DOCKERCOMPOSE up -d --build
