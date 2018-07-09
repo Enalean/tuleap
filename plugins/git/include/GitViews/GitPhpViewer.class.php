cc22752c4d9bc2eea133a265f05c45d9d9943d80
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-07-09 10:54:47 +0200

    Remove "pre-sanitization" of requests processed by GitPHP
    It is a bad practice to "pre-sanitize" request parameters since
    it is not possible to know how these variables are going to be used.
    Without the context it only gives an impression of security.
    
    The needed escaping as been added in the templates a while ago,
    the "pre-sanitization" can safely be dropped.
    
    This is part of story #10411: cleanly integrate gitphp into tuleap
    
    Change-Id: If50cf3a97eb49a88ee9bd858393ca4126773295c

--- plugins/git/include/GitViews/GitPhpViewer.class.php
+++ plugins/git/include/GitViews/GitPhpViewer.class.php
@@ -47,7 +47,6 @@ class GitViews_GitPhpViewer {
     public function getContent($is_download)
     {
         set_time_limit(300);
-        $this->preSanitizeRequestForGitphp();
         if (! $is_download) {
             echo '<div id="gitphp" class="plugin_git_gitphp">';
         }
@@ -56,15 +55,6 @@ class GitViews_GitPhpViewer {
 
         if (! $is_download) {
             echo '</div>';
-        }
-    }
-
-    private function preSanitizeRequestForGitphp() {
-        $hp = Codendi_HTMLPurifier::instance();
-        foreach(array('h', 'hb', 'hp') as $parameter) {
-            if (isset($_REQUEST[$parameter])) {
-                $_GET[$parameter] = $hp->purify($_REQUEST[$parameter]);
-            }
         }
     }
 
