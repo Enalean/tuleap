Enalean/tuleap

commit 16e168eceee5c20be71a188de6d92899f2317b4b

Prepare changeset post creation to deal with other task than mail notifications
End goal will be to deal with webhooks in addition to mail notifications.

To achieve that, some renaming has been done to make more obvious
that post creation tasks can be something else than mails and the usage
internal Tuleap deprecated APIs has been abandonned.

This is part of story #11035: trigger webhook on artifact update

Change-Id: If4cbb9b59f47d86eef4af0fcb9baca01daf1d005

plugins/tracker/include/Tracker/Artifact/Changeset/PostCreation/ActionsRunnerDao.php
@@ -1,0 +1,70 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017-2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\Artifact\Changeset\PostCreation;
+
+use Tuleap\DB\DataAccessObject;
+
+class ActionsRunnerDao extends DataAccessObject
+{
+    public function addNewPostCreationEvent($changeset_id)
+    {
+        $sql = 'INSERT INTO tracker_post_creation_event_log(changeset_id, create_date)
+                VALUES(?, UNIX_TIMESTAMP())';
+
+        $this->getDB()->run($sql, $changeset_id);
+    }
+
+    public function addStartDate($changeset_id)
+    {
+        $sql = 'UPDATE tracker_post_creation_event_log SET start_date = UNIX_TIMESTAMP() WHERE changeset_id = ?';
+
+        $this->getDB()->run($sql, $changeset_id);
+    }
+
+    public function addEndDate($changeset_id)
+    {
+        $sql = 'UPDATE tracker_post_creation_event_log SET end_date = UNIX_TIMESTAMP() WHERE changeset_id = ?';
+
+        $this->getDB()->run($sql, $changeset_id);
+    }
+
+    public function getLastEndDate()
+    {
+        $sql = 'SELECT MAX(end_date) AS max
+          FROM tracker_post_creation_event_log';
+
+        return $this->getDB()->single($sql);
+    }
+
+    public function searchPostCreationEventsAfter($create_date)
+    {
+        $sql = 'SELECT count(*) as nb FROM tracker_post_creation_event_log WHERE start_date IS NULL AND create_date > ?';
+
+        return $this->getDB()->single($sql, $create_date);
+    }
+
+    public function deleteLogsOlderThan($delay)
+    {
+        $sql = 'DELETE FROM tracker_post_creation_event_log WHERE end_date IS NOT NULL AND end_date < UNIX_TIMESTAMP() - ?';
+
+        return $this->getDB()->run($sql, $delay);
+    }
+}

plugins/tracker/db/install.sql
@@ -854,8 +854,8 @@ CREATE TABLE plugin_tracker_projects_unused_artifactlink_types (
     INDEX idx_artifactlink_types_unused_project_id(project_id)
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS tracker_email_notification_log;
-CREATE TABLE IF NOT EXISTS tracker_email_notification_log (
+DROP TABLE IF EXISTS tracker_post_creation_event_log;
+CREATE TABLE IF NOT EXISTS tracker_post_creation_event_log (
     changeset_id INT(11) NOT NULL PRIMARY KEY,
     create_date int(11) NOT NULL,
     start_date int(11) NULL,

plugins/tracker/db/mysql/updates/2018/201807131145_rename_tracker_email_notification_log.php
@@ -1,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class b201807131145_rename_tracker_email_notification_log extends ForgeUpgrade_Bucket // phpcs:ignore
+{
+    public function description()
+    {
+        return 'Rename tracker_email_notification_log table';
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        if ($this->db->tableNameExists('tracker_post_creation_event_log')) {
+            return;
+        }
+
+        $res = $this->db->dbh->query('ALTER TABLE tracker_email_notification_log RENAME tracker_post_creation_event_log');
+
+        if ($res === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('Renaming of the table tracker_email_notification_log failed');
+        }
+    }
+}

plugins/tracker/include/Tracker/Artifact/Changeset/NewChangesetCreatorBase.class.php
@@ -166,7 +166,7 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         }
 
         if ($send_notification) {
-            $artifact->getChangeset($changeset_id)->notify();
+            $artifact->getChangeset($changeset_id)->executePostCreationActions();
             $this->emitWebhooks($artifact, $submitter, 'update');
         }
 

plugins/tracker/include/Tracker/Artifact/Changeset/Notification/NotifierDao.php
@@ -1,80 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-namespace Tuleap\Tracker\Artifact\Changeset\Notification;
-
-use DataAccessObject;
-
-class NotifierDao extends DataAccessObject
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->enableExceptionsOnError();
-    }
-
-    public function addNewNotification($changeset_id)
-    {
-        $changeset_id = (int) $changeset_id;
-
-        $sql = "INSERT INTO tracker_email_notification_log(changeset_id, create_date)
-                VALUES($changeset_id, UNIX_TIMESTAMP())";
-        $this->update($sql);
-    }
-
-    public function addStartDate($changeset_id)
-    {
-        $changeset_id = (int) $changeset_id;
-
-        $sql = "UPDATE tracker_email_notification_log SET start_date = UNIX_TIMESTAMP() WHERE changeset_id = $changeset_id";
-        $this->update($sql);
-    }
-
-    public function addEndDate($changeset_id)
-    {
-        $changeset_id = (int) $changeset_id;
-
-        $sql = "UPDATE tracker_email_notification_log SET end_date = UNIX_TIMESTAMP() WHERE changeset_id = $changeset_id";
-        $this->update($sql);
-    }
-
-    public function getLastEndDate()
-    {
-        $sql = "SELECT MAX(end_date) AS max
-          FROM tracker_email_notification_log";
-        $row = $this->retrieveFirstRow($sql);
-        return (int) $row['max'];
-    }
-
-    public function searchPendingNotificationsAfter($create_date)
-    {
-        $create_date = (int) $create_date;
-        $sql = "SELECT count(*) as nb FROM tracker_email_notification_log WHERE start_date IS NULL AND create_date > $create_date";
-        $row = $this->retrieveFirstRow($sql);
-        return (int) $row['nb'];
-    }
-
-    public function deleteLogsOlderThan($delay)
-    {
-        $delay = (int) $delay;
-        $sql = "DELETE FROM tracker_email_notification_log WHERE end_date IS NOT NULL AND end_date < UNIX_TIMESTAMP() - $delay";
-        $this->update($sql);
-    }
-}

plugins/tracker/include/Tracker/Artifact/Changeset/PostCreation/ActionsRunner.php
@@ -19,7 +19,7 @@
  *
  */
 
-namespace Tuleap\Tracker\Artifact\Changeset\Notification;
+namespace Tuleap\Tracker\Artifact\Changeset\PostCreation;
 
 use BaseLanguage;
 use Codendi_HTMLPurifier;
@@ -46,6 +46,7 @@ use Tuleap\Queue\QueueFactory;
 use Tuleap\Queue\Worker;
 use Tuleap\Tracker\Artifact\MailGateway\MailGatewayConfig;
 use Tuleap\Tracker\Artifact\MailGateway\MailGatewayConfigDao;
+use Tuleap\Tracker\Notifications\RecipientsManager;
 use Tuleap\Tracker\Notifications\Settings\UserNotificationSettingsRetriever;
 use Tuleap\Tracker\Notifications\UnsubscribersNotificationDAO;
 use Tuleap\Tracker\Notifications\UserNotificationOnlyStatusChangeDAO;
@@ -53,7 +54,7 @@ use UserHelper;
 use UserManager;
 use WrapperLogger;
 
-class Notifier
+class ActionsRunner
 {
     const DEFAULT_MAIL_SENDER = 'forge__artifacts';
     const DEFAULT_SENDER_EXPOSED_FIELDS = [
@@ -91,11 +92,11 @@ class Notifier
      */
     private $mail_sender;
     /**
-     * @var NotifierDao
+     * @var ActionsRunnerDao
      */
     private $notifier_dao;
     /**
-     * @var \ConfigNotificationEmailCustomSender
+     * @var ConfigNotificationEmailCustomSender
      */
     private $config_notification_custom_sender;
 
@@ -107,7 +108,7 @@ class Notifier
         UserHelper $user_helper,
         RecipientsManager $recipients_manager,
         MailSender $mail_sender,
-        NotifierDao $notifier_dao,
+        ActionsRunnerDao $notifier_dao,
         ConfigNotificationEmailCustomSender $config_notification_custom_sender
     ) {
         $this->logger                                   = new WrapperLogger($logger, __CLASS__);
@@ -123,7 +124,7 @@ class Notifier
 
     public static function build(Logger $logger)
     {
-        return new Notifier(
+        return new ActionsRunner(
             $logger,
             new MailGatewayConfig(
                 new MailGatewayConfigDao()
@@ -145,7 +146,7 @@ class Notifier
                 new UserNotificationOnlyStatusChangeDAO()
             ),
             new MailSender(),
-            new NotifierDao(),
+            new ActionsRunnerDao(),
             new ConfigNotificationEmailCustomSender(new ConfigNotificationEmailCustomSenderDao())
         );
     }
@@ -155,12 +156,12 @@ class Notifier
      *
      * @param Tracker_Artifact_Changeset $changeset
      */
-    public function notify(Tracker_Artifact_Changeset $changeset)
+    public function executePostCreationActions(Tracker_Artifact_Changeset $changeset)
     {
         if ($this->useAsyncEmails($changeset)) {
-            $this->queueNotification($changeset);
+            $this->queuePostCreationEvent($changeset);
         } else {
-            $this->processNotify($changeset);
+            $this->processPostCreationActions($changeset);
         }
     }
 
@@ -169,10 +170,10 @@ class Notifier
      *
      * @param Tracker_Artifact_Changeset $changeset
      */
-    public function processAsyncNotify(Tracker_Artifact_Changeset $changeset)
+    public function processAsyncPostCreationActions(Tracker_Artifact_Changeset $changeset)
     {
         $this->notifier_dao->addStartDate($changeset->getId());
-        $this->processNotify($changeset);
+        $this->processPostCreationActions($changeset);
         $this->notifier_dao->addEndDate($changeset->getId());
     }
 
@@ -198,13 +199,13 @@ class Notifier
         return false;
     }
 
-    private function queueNotification(Tracker_Artifact_Changeset $changeset)
+    private function queuePostCreationEvent(Tracker_Artifact_Changeset $changeset)
     {
         try {
-            $this->notifier_dao->addNewNotification($changeset->getId());
+            $this->notifier_dao->addNewPostCreationEvent($changeset->getId());
             $queue = QueueFactory::getPersistentQueue($this->logger, Worker::EVENT_QUEUE_NAME, QueueFactory::REDIS);
             $queue->pushSinglePersistentMessage(
-                AsynchronousNotifier::TOPIC,
+                AsynchronousActionsRunner::TOPIC,
                 [
                     'artifact_id'  => (int) $changeset->getArtifact()->getId(),
                     'changeset_id' => (int) $changeset->getId(),
@@ -212,12 +213,12 @@ class Notifier
             );
         } catch (Exception $exception) {
             $this->logger->error("Unable to queue notification for {$changeset->getId()}, fallback to online notif");
-            $this->processNotify($changeset);
+            $this->processPostCreationActions($changeset);
             $this->notifier_dao->addEndDate($changeset->getId());
         }
     }
 
-    private function processNotify(Tracker_Artifact_Changeset $changeset)
+    private function processPostCreationActions(Tracker_Artifact_Changeset $changeset)
     {
         $tracker = $changeset->getTracker();
         if (! $tracker->isNotificationStopped()) {

plugins/tracker/include/Tracker/Artifact/Changeset/PostCreation/AsynchronousActionsRunner.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,12 +19,12 @@
  *
  */
 
-namespace Tuleap\Tracker\Artifact\Changeset\Notification;
+namespace Tuleap\Tracker\Artifact\Changeset\PostCreation;
 
 use Tracker_ArtifactFactory;
 use Tuleap\Queue\WorkerEvent;
 
-class AsynchronousNotifier
+class AsynchronousActionsRunner
 {
     const TOPIC = 'tuleap.tracker.artifact';
 
@@ -32,11 +32,11 @@ class AsynchronousNotifier
     {
         if ($event->getEventName() === self::TOPIC) {
             $message = $event->getPayload();
-            $notifier = Notifier::build($event->getLogger());
+            $notifier = ActionsRunner::build($event->getLogger());
             $artifact = Tracker_ArtifactFactory::instance()->getArtifactById($message['artifact_id']);
             $changeset = $artifact->getChangeset($message['changeset_id']);
 
-            $notifier->processAsyncNotify($changeset);
+            $notifier->processAsyncPostCreationActions($changeset);
         }
     }
 }

plugins/tracker/include/Tracker/Artifact/Changeset/PostCreation/AsynchronousSupervisor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,12 +19,11 @@
  *
  */
 
-namespace Tuleap\Tracker\Artifact\Changeset\Notification;
+namespace Tuleap\Tracker\Artifact\Changeset\PostCreation;
 
 use Logger;
 use WrapperLogger;
 use ForgeConfig;
-use System_Command;
 
 class AsynchronousSupervisor
 {
@@ -33,7 +32,7 @@ class AsynchronousSupervisor
     const ONE_WEEK_IN_SECONDS = 604800;
 
     /**
-     * @var NotifierDao
+     * @var ActionsRunnerDao
      */
     private $dao;
     /**
@@ -41,7 +40,7 @@ class AsynchronousSupervisor
      */
     private $logger;
 
-    public function __construct(Logger $logger, NotifierDao $dao)
+    public function __construct(Logger $logger, ActionsRunnerDao $dao)
     {
         $this->logger = new WrapperLogger($logger, __CLASS__);
         $this->dao    = $dao;
@@ -57,10 +56,10 @@ class AsynchronousSupervisor
 
     private function warnWhenToMuchDelay()
     {
-        $last_end_date = $this->dao->getLastEndDate();
-        $nb_pending_notifications = $this->dao->searchPendingNotificationsAfter($last_end_date + self::ACCEPTABLE_PROCESS_DELAY);
-        if ($nb_pending_notifications > 0) {
-            $this->logger->warn("There are ".$nb_pending_notifications." notifications pending, you should check '/usr/share/tuleap/src/utils/worker.php' and it's log file to ensure it's still running.");
+        $last_end_date     = $this->dao->getLastEndDate();
+        $nb_pending_events = $this->dao->searchPostCreationEventsAfter($last_end_date + self::ACCEPTABLE_PROCESS_DELAY);
+        if ($nb_pending_events > 0) {
+            $this->logger->warn('There are ' . $nb_pending_events . " post creation events waiting to be processed, you should check '/usr/share/tuleap/src/utils/worker.php' and it's log file to ensure it's still running.");
         }
     }
 

commit 15f16649bdd583761eed83011d69e4c2ef4f3b6f

Retrieval of not processed post changeset creation events should be possible
Currently, it crashes and blocks SYSTEM_CHECK executions.

Issue has been introduced by 16e168eceee5c20be71a188de6d92899f2317b4b.

This is part of story #11035: trigger webhook on artifact update

Change-Id: Idab6f48be9dc52a04976ce91ec8e673092c50c66

plugins/tracker/include/Tracker/Artifact/Changeset/PostCreation/ActionsRunnerDao.php
@@ -58,7 +58,7 @@ class ActionsRunnerDao extends DataAccessObject
     {
         $sql = 'SELECT count(*) as nb FROM tracker_post_creation_event_log WHERE start_date IS NULL AND create_date > ?';
 
-        return $this->getDB()->single($sql, $create_date);
+        return $this->getDB()->single($sql, [$create_date]);
     }
 
     public function deleteLogsOlderThan($delay)