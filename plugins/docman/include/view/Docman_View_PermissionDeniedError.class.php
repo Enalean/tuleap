40f13bec22065c5d278ff71c728ceee5d7b5b343
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-07-17 09:49:46 +0200

    request #11836: Accessing a docman element without enough permissions raises a fatal error
    Issue has been itnroduced by commit c3c7446be2bffc1104e675e9d6007b5884bacc77.
    
    Change-Id: I2b96cefaf50bf65a3e0552c69c99451f2c830236

--- plugins/docman/include/view/Docman_View_PermissionDeniedError.class.php
+++ plugins/docman/include/view/Docman_View_PermissionDeniedError.class.php
@@ -1,39 +1,30 @@
 <?php
-
-/* 
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2010. All Rights Reserved.
  *
+ * This file is a part of Tuleap.
  *
- * This file is a part of Codendi.
- *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * 
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('Docman_View_DocmanError.class.php');
-require_once(dirname(__FILE__).'/../Docman_Error_PermissionDenied.class.php');
-
-class Docman_View_PermissionDeniedError extends Docman_View_DocmanError {
-    function display($params = array()) {
+class Docman_View_PermissionDeniedError extends Docman_View_DocmanError
+{
+    function display($params = array())
+    {
         $sendMail = new Docman_Error_PermissionDenied();
-        $sendMail->buildInterface();
+        $sendMail->buildInterface(UserManager::instance()->getCurrentUser());
         exit;
-        
     }
-
 }
-
-?>
