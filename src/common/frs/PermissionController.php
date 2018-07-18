Enalean/tuleap

commit 7e2617d8f7ae4e5d8d2f79b32201bbe5c9af01b3

REST tests project imports is done in PHP 7.2
All fatal errors, warnings and notices has been solved.

This is part of story #11144: Run a first REST test in PHP7

Change-Id: I63981cf1495e074a7874d53792bdfbda7cd4b38c

src/common/frs/PermissionController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -33,6 +33,7 @@ use User_ForgeUserGroupFactory;
 use ProjectUGroup;
 use ServiceFile;
 use TemplateRenderer;
+use UserManager;
 
 class PermissionController extends BaseFrsPresenter
 {
@@ -147,7 +148,25 @@ class PermissionController extends BaseFrsPresenter
         $toolbar_presenter->setPermissionIsActive();
         $toolbar_presenter->displaySectionNavigation();
 
-        $service->displayHeader($project, $title);
+        $user = UserManager::instance()->getCurrentUser();
+        if ($this->permission_manager->isAdmin($project, $user)) {
+            $toolbar[] = array(
+                'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_admin'),
+                'url'   => '/file/admin/?'. http_build_query(array(
+                        'group_id' => $project->getID(),
+                        'action'   => 'edit-permissions'
+                    ))
+            );
+        }
+
+        $toolbar[] = array(
+            'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_help'),
+            'url'   => "javascript:help_window('/doc/".$user->getShortLocale()."/user-guide/frs.html')"
+        );
+
+        $breadcrumbs = [];
+
+        $service->displayHeader($title, $toolbar, $breadcrumbs);
         $renderer->renderToPage('toolbar-presenter', $toolbar_presenter);
     }
 

src/common/frs/ServiceFile.class.php
@@ -1,28 +1,23 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Enalean, 2016 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - 2018. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
-
-
-use Tuleap\FRS\FRSPermissionDao;
-use Tuleap\FRS\FRSPermissionFactory;
-use Tuleap\FRS\FRSPermissionManager;
 
 /**
  * ServiceFile
@@ -176,37 +171,11 @@ class ServiceFile extends Service
         ));
     }
 
-    private function getFrsPermissionManager()
-    {
-        return new FRSPermissionManager(
-            new FRSPermissionDao(),
-            new FRSPermissionFactory(new FRSPermissionDao())
-        );
-    }
-
-    public function displayHeader(Project $project, $title)
+    public function displayHeader($title, $breadcrumbs, $toolbar, $params = array())
     {
         $GLOBALS['HTML']->includeJavascriptSnippet(
             file_get_contents($GLOBALS['Language']->getContent('script_locale', null, 'svn', '.js'))
         );
-
-        $user = UserManager::instance()->getCurrentUser();
-        if ($this->getFrsPermissionManager()->isAdmin($project, $user)) {
-            $toolbar[]   = array(
-                'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_admin'),
-                'url'   => '/file/admin/?'. http_build_query(array(
-                    'group_id' => $project->getID(),
-                    'action'   => 'edit-permissions'
-                ))
-            );
-        }
-
-        $toolbar[] = array(
-            'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_help'),
-            'url'   => "javascript:help_window('/doc/".$user->getShortLocale()."/user-guide/frs.html')"
-        );
-
-        $breadcrumbs = array();
 
         parent::displayHeader($title, $breadcrumbs, $toolbar);
     }

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_Changeset_Null.class.php
@@ -1,21 +1,22 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012-2018. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -84,7 +85,8 @@ class Tracker_Artifact_Changeset_Null extends Tracker_Artifact_Changeset {
      *
      * @return void
      */
-    public function updateComment($body, $user) {
+    public function updateComment($body, $user, $comment_format, $timestamp)
+    {
     }
 
     /**

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactId.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean 2017. All rights reserved
+ * Copyright (c) Enalean 2017-2018. All rights reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2010. All rights reserved
  *
  * This file is a part of Tuleap.
@@ -144,7 +144,8 @@ class Tracker_FormElement_Field_ArtifactId extends Tracker_FormElement_Field_Int
         return $output;
     }
 
-    public function fetchArtifactValueWithEditionFormIfEditable(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null) {
+    public function fetchArtifactValueWithEditionFormIfEditable(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null, $submitted_values = [])
+    {
         return $this->fetchArtifactValueReadOnly($artifact, $value);
     }
 
@@ -227,7 +228,8 @@ class Tracker_FormElement_Field_ArtifactId extends Tracker_FormElement_Field_Int
      *
      * @return string html
      */
-     public function fetchSubmit() {
+     public function fetchSubmit($submitted_values = [])
+     {
          return '';
      }
      

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -1339,7 +1339,12 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
     /**
      * @see Tracker_FormElement_Field::hasChanges()
      */
-    public function hasChanges(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue_ArtifactLink $old_value, $new_value) {
+    public function hasChanges(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $old_value, $new_value)
+    {
+        if (! $old_value instanceof Tracker_Artifact_ChangesetValue_ArtifactLink) {
+            return false;
+        }
+
         $source_of_association_collection_dev_null = new SourceOfAssociationCollection();
         $submitted_value = $this->getSubmittedValueConvertor()->convert(
             $new_value,

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.class.php
@@ -31,10 +31,6 @@ use Tuleap\Tracker\FormElement\ChartFieldUsage;
 use Tuleap\Tracker\FormElement\ChartMessageFetcher;
 use Tuleap\Tracker\FormElement\SystemEvent\SystemEvent_BURNDOWN_GENERATE;
 
-require_once 'common/chart/ErrorChart.class.php';
-require_once 'common/date/TimePeriodWithWeekEnd.class.php';
-require_once 'common/date/TimePeriodWithoutWeekEnd.class.php';
-
 class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field implements Tracker_FormElement_Field_ReadOnly
 {
 
@@ -607,7 +603,8 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
      *
      * @return string html
      */
-    public function fetchSubmit() {
+    public function fetchSubmit($submitted_values = [])
+    {
         return '';
     }
 

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_CrossReferences.class.php
@@ -140,7 +140,8 @@ class Tracker_FormElement_Field_CrossReferences extends Tracker_FormElement_Fiel
         return $crossref_factory;
     }
 
-    public function fetchCSVChangesetValue($artifact_id, $changeset_id, $value) {
+    public function fetchCSVChangesetValue($artifact_id, $changeset_id, $value, $report = null)
+    {
         $html = '';
         $crossref_fact = $this->getCrossReferencesFactory($artifact_id);
 
@@ -482,7 +483,12 @@ class Tracker_FormElement_Field_CrossReferences extends Tracker_FormElement_Fiel
      *
      * @return boolean true on success or false on failure
      */
-    public function validateFieldWithPermissionsAndRequiredStatus(Tracker_Artifact $artifact, $submitted_value, Tracker_Artifact_ChangesetValue $last_changeset_value = null) {
+    public function validateFieldWithPermissionsAndRequiredStatus(
+        Tracker_Artifact $artifact,
+        $submitted_value,
+        Tracker_Artifact_ChangesetValue $last_changeset_value = null,
+        $is_submission = null
+    ) {
         return true;
     }
     
@@ -504,7 +510,8 @@ class Tracker_FormElement_Field_CrossReferences extends Tracker_FormElement_Fiel
      *
      * @return string html
      */
-     public function fetchSubmit() {
+     public function fetchSubmit($submitted_values = [])
+     {
          return '';
      }
 

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_File.class.php
@@ -166,7 +166,8 @@ class Tracker_FormElement_Field_File extends Tracker_FormElement_Field {
         return '';
     }
 
-    public function fetchArtifactCopyMode(Tracker_Artifact $artifact) {
+    public function fetchArtifactCopyMode(Tracker_Artifact $artifact, $submitted_values = [])
+    {
         $last_changeset = $artifact->getLastChangeset();
         if ($last_changeset) {
             $value = $last_changeset->getValue($this);

commit a18c90d52db9213efa606a7ede7c7e63f7b3cb36

request #11844: FRS can not be accessed
Issue has been introduced by 7e2617d8f7ae4e5d8d2f79b32201bbe5c9af01b3.

Change-Id: Ica7fde5d5fbd83df6a3adf6563f85593f0b46e56

src/common/frs/PermissionController.php
@@ -148,25 +148,7 @@ class PermissionController extends BaseFrsPresenter
         $toolbar_presenter->setPermissionIsActive();
         $toolbar_presenter->displaySectionNavigation();
 
-        $user = UserManager::instance()->getCurrentUser();
-        if ($this->permission_manager->isAdmin($project, $user)) {
-            $toolbar[] = array(
-                'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_admin'),
-                'url'   => '/file/admin/?'. http_build_query(array(
-                        'group_id' => $project->getID(),
-                        'action'   => 'edit-permissions'
-                    ))
-            );
-        }
-
-        $toolbar[] = array(
-            'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_help'),
-            'url'   => "javascript:help_window('/doc/".$user->getShortLocale()."/user-guide/frs.html')"
-        );
-
-        $breadcrumbs = [];
-
-        $service->displayHeader($title, $toolbar, $breadcrumbs);
+        $service->displayFRSHeader($project, $title);
         $renderer->renderToPage('toolbar-presenter', $toolbar_presenter);
     }
 

src/common/frs/ServiceFile.class.php
@@ -19,6 +19,10 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\FRS\FRSPermissionDao;
+use Tuleap\FRS\FRSPermissionFactory;
+use Tuleap\FRS\FRSPermissionManager;
+
 /**
  * ServiceFile
  */
@@ -171,12 +175,38 @@ class ServiceFile extends Service
         ));
     }
 
-    public function displayHeader($title, $breadcrumbs, $toolbar, $params = array())
+    public function displayFRSHeader(Project $project, $title)
     {
         $GLOBALS['HTML']->includeJavascriptSnippet(
             file_get_contents($GLOBALS['Language']->getContent('script_locale', null, 'svn', '.js'))
         );
 
-        parent::displayHeader($title, $breadcrumbs, $toolbar);
+        $user = UserManager::instance()->getCurrentUser();
+        if ($this->getFrsPermissionManager()->isAdmin($project, $user)) {
+            $toolbar[]   = array(
+                'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_admin'),
+                'url'   => '/file/admin/?'. http_build_query(array(
+                        'group_id' => $project->getID(),
+                        'action'   => 'edit-permissions'
+                    ))
+            );
+        }
+
+        $toolbar[] = array(
+            'title' => $GLOBALS['Language']->getText('file_file_utils', 'toolbar_help'),
+            'url'   => "javascript:help_window('/doc/".$user->getShortLocale()."/user-guide/frs.html')"
+        );
+
+        $breadcrumbs = array();
+
+        $this->displayHeader($title, $breadcrumbs, $toolbar);
+    }
+
+    private function getFrsPermissionManager()
+    {
+        return new FRSPermissionManager(
+            new FRSPermissionDao(),
+            new FRSPermissionFactory(new FRSPermissionDao())
+        );
     }
 }

src/www/file/admin/editproc.php
@@ -67,7 +67,7 @@ $presenter = new ToolbarPresenter($project, $title);
 $presenter->setProcessorsIsActive();
 $presenter->displaySectionNavigation();
 
-$project->getService(Service::FILE)->displayHeader($project, $title);
+$project->getService(Service::FILE)->displayFRSHeader($project, $title);
 $renderer->renderToPage('toolbar-presenter', $presenter);
 
 $sql = "SELECT name,rank FROM frs_processor WHERE group_id=".db_ei($group_id)." AND processor_id=".db_ei($proc_id);

src/www/file/admin/manageprocessors.php
@@ -67,7 +67,7 @@ $presenter = new ToolbarPresenter($project, $title);
 $presenter->setProcessorsIsActive();
 $presenter->displaySectionNavigation();
 
-$project->getService(Service::FILE)->displayHeader($project, $title);
+$project->getService(Service::FILE)->displayFRSHeader($project, $title);
 $renderer->renderToPage('toolbar-presenter', $presenter);
 
 $vAdd      = new Valid_String('add');

src/www/file/file_utils.php
@@ -39,7 +39,7 @@ function file_utils_header($params) {
         $project_manager = ProjectManager::instance();
         $project         = $project_manager->getProject($group_id);
 
-        $project->getService(Service::FILE)->displayHeader($project, $params['title']);
+        $project->getService(Service::FILE)->displayFRSHeader($project, $params['title']);
     }
 }
 

src/www/file/showfiles.php
@@ -117,7 +117,7 @@ $params = array (
     'file_p_for',
     $hp->purify($project_manager->getProject($group_id)->getPublicName())
 ), 'pv' => $pv);
-$project->getService(Service::FILE)->displayHeader($project, $params['title']);
+$project->getService(Service::FILE)->displayFRSHeader($project, $params['title']);
 
 if ($num_packages < 1) {
     echo '<h3>' . $Language->getText('file_showfiles', 'no_file_p') . '</h3><p>' . $Language->getText('file_showfiles', 'no_p_available');