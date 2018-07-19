Enalean/tuleap

commit 1980be1d25a630c2c7096a1eda7226f9af0ab938

request #11660: Replace the deprecated PHP4-style constructors
This contribution has mostly been generated automatically with
PHP-CS-Fixer and the rule no_php4_constructor.

Only manual changes in this contribution are in tests were
constructors are directly called.

Change-Id: I517413efe6c1374afeb8e9dd501e09c5bd0a68cc

plugins/docman/include/Docman_Filter.class.php
@@ -24,7 +24,7 @@ class Docman_Filter {
     var $value;
     var $md;
 
-    function Docman_Filter($md) {
+    function __construct($md) {
         $this->value = null;
         $this->md = $md;
     }
@@ -102,8 +102,8 @@ class Docman_FilterDate extends Docman_Filter {
     var $field_operator_name;
     var $field_value_name;
 
-    function Docman_FilterDate($md) {
-        parent::Docman_Filter($md);
+    function __construct($md) {
+        parent::__construct($md);
         $this->operator = null;
         if($md !== null) {
             $this->field_operator_name  = $md->getLabel().'_operator';
@@ -211,8 +211,8 @@ extends Docman_FilterDate {
     var $valueStart;
     var $valueEnd;
 
-    function Docman_FilterDateAdvanced($md) {
-        parent::Docman_FilterDate($md);
+    function __construct($md) {
+        parent::__construct($md);
 
         $base = $md->getLabel().'_value';
         $this->fieldNameStart = $base.'_start';
@@ -302,10 +302,10 @@ extends Docman_FilterDate {
  */
 class Docman_FilterList extends Docman_Filter {
 
-    function Docman_FilterList($md) {
+    function __construct($md) {
         $mdFactory = new Docman_MetadataFactory($md->getGroupId());
         $mdFactory->appendMetadataValueList($md, false);
-        parent::Docman_Filter($md);
+        parent::__construct($md);
         $this->setValue(0);
     }
 
@@ -356,8 +356,8 @@ class Docman_FilterList extends Docman_Filter {
 class Docman_FilterListAdvanced 
 extends Docman_FilterList {
 
-    function Docman_FilterListAdvanced($md) {
-        parent::Docman_FilterList($md);
+    function __construct($md) {
+        parent::__construct($md);
         $this->setValue(array());
     }
 
@@ -421,15 +421,15 @@ extends Docman_FilterList {
 * Item type filters
 */
 class Docman_FilterItemTypeAdvanced extends Docman_FilterListAdvanced {
-    function Docman_FilterItemTypeAdvanced($md) {
-        parent::Docman_Filter($md);
+    function __construct($md) {
+        parent::__construct($md);
         $this->setValue(array());
     }
 }
 
 class Docman_FilterItemType extends Docman_FilterList {
-    function Docman_FilterItemType($md) {
-        parent::Docman_Filter($md);
+    function __construct($md) {
+        parent::__construct($md);
         $this->setValue(0);
     }
 }
@@ -439,8 +439,8 @@ class Docman_FilterItemType extends Docman_FilterList {
  */
 class Docman_FilterText extends Docman_Filter {
 
-    function Docman_FilterText($md) {
-        parent::Docman_Filter($md);
+    function __construct($md) {
+        parent::__construct($md);
     }
 
     function initFromRow($row) {
@@ -460,8 +460,8 @@ class Docman_FilterText extends Docman_Filter {
 class Docman_FilterGlobalText extends Docman_FilterText {
     var $dynTextFields;
 
-    function Docman_FilterGlobalText($md, $dynTextFields) {
-        parent::Docman_FilterText($md);
+    function __construct($md, $dynTextFields) {
+        parent::__construct($md);
         $this->dynTextFields = $dynTextFields;
     }
 
@@ -472,8 +472,8 @@ class Docman_FilterGlobalText extends Docman_FilterText {
 
 class Docman_FilterOwner extends Docman_Filter {
 
-    function Docman_FilterOwner($md) {
-        parent::Docman_Filter($md);
+    function __construct($md) {
+        parent::__construct($md);
     }
 
     function initFromRow($row) {

commit 13325062356fad6bcbbbc965fb9ddd9a78232222

request #11861 Docman search filters do not list item types anymore
Regression introduced by 1980be1d25a630c2c7096a1eda7226f9af0ab938

The parent constructor was not directly called. It was the parent of
parent constructor that was previously called in the two modified
classes.

Change-Id: I6933313ff36643febe2cdae1c4dd659d03406649

plugins/docman/include/Docman_Filter.class.php
@@ -422,14 +422,14 @@ extends Docman_FilterList {
 */
 class Docman_FilterItemTypeAdvanced extends Docman_FilterListAdvanced {
     function __construct($md) {
-        parent::__construct($md);
+        Docman_Filter::__construct($md);
         $this->setValue(array());
     }
 }
 
 class Docman_FilterItemType extends Docman_FilterList {
     function __construct($md) {
-        parent::__construct($md);
+        Docman_Filter::__construct($md);
         $this->setValue(0);
     }
 }