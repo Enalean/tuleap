bce34cfd2c72b4964348304d5fa8190b96802369
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-07-21 12:44:31 +0200

    request #11887 Move BugzillaReference plugin tests to PHPUnit
    The use Reference; has been removed because it generates a conflicts
    with PHPUnit code coverage ...
    
    Change-Id: I5a2555aca858b2f8a5e063ea7d2741ccb5d5e113

---
+++ plugins/bugzilla_reference/phpunit/Bugzilla/CrossReferenceCreatorTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2017 - 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Bugzilla;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+class CrossReferenceCreatorTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->cross_reference_dao = \Mockery::spy(\CrossReferenceDao::class);
+        $this->rest_reference_dao  = \Mockery::spy(\Tuleap\Bugzilla\Reference\RESTReferenceCreator::class);
+
+        $this->cross_reference_creator = new CrossReferenceCreator(
+            $this->cross_reference_dao,
+            $this->rest_reference_dao
+        );
+    }
+
+    public function testItCreatesReference()
+    {
+        $this->cross_reference_dao->shouldReceive('fullReferenceExistInDb')->andReturn(false);
+
+        $this->cross_reference_dao->shouldReceive('createDbCrossRef')->once();
+        $this->rest_reference_dao->shouldReceive('create')->once();
+
+        $cross_reference    = \Mockery::spy(\CrossReference::class);
+        $bugzilla_reference = \Mockery::spy(\Tuleap\Bugzilla\Reference\Reference::class);
+
+        $this->cross_reference_creator->create($cross_reference, $bugzilla_reference);
+    }
+
+    public function testItDoesNotCreateReferenceIfAlreadyExisting()
+    {
+        $this->cross_reference_dao->shouldReceive('fullReferenceExistInDb')->andReturn(true);
+
+        $this->cross_reference_dao->shouldReceive('createDbCrossRef')->never();
+        $this->rest_reference_dao->shouldReceive('create')->never();
+
+        $cross_reference    = \Mockery::spy(\CrossReference::class);
+        $bugzilla_reference = \Mockery::spy(\Tuleap\Bugzilla\Reference\Reference::class);
+
+        $this->cross_reference_creator->create($cross_reference, $bugzilla_reference);
+    }
+}
--- plugins/bugzilla_reference/phpunit/bootstrap.php
+++ plugins/bugzilla_reference/phpunit/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -16,6 +16,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
-require_once __DIR__ . '/../include/autoload.php';
+require_once __DIR__ . '/../include/bugzilla_referencePlugin.class.php';
