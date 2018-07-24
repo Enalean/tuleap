Enalean/tuleap

commit 35146c02a538fd6ad11015bc020fe259843d2970

request #11210 In milestone tracker report filter must not check in parent milestone
Given the following planning:

Epic --------------> Release
|                    |
US and Bugs -------> Sprints

If I filter in the US tracker report with "In sprint" and
if my US is directly linked to a release, the tracker report filter
must not retrieve all the release content.

It must retrieve only the sprint (and potential sub milestones) content.

Change-Id: Ib8b51698fbb976c342adf1922b563e7cd3b53dee

plugins/agiledashboard/include/AgileDashboard/BacklogItem/SubBacklogItemProvider.class.php
@@ -45,13 +45,20 @@ class AgileDashboard_BacklogItem_SubBacklogItemProvider {
     /** @var AgileDashboard_Milestone_Backlog_BacklogFactory */
     private $backlog_factory;
 
+    /**
+     * @var PlanningFactory
+     */
+    private $planning_factory;
+
     public function __construct(Tracker_ArtifactDao $dao,
         AgileDashboard_Milestone_Backlog_BacklogFactory $backlog_factory,
-        AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory $backlog_item_collection_factory
+        AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory $backlog_item_collection_factory,
+        PlanningFactory $planning_factory
     ) {
         $this->backlog_item_collection_factory = $backlog_item_collection_factory;
         $this->backlog_factory                 = $backlog_factory;
         $this->dao                             = $dao;
+        $this->planning_factory                = $planning_factory;
     }
 
     /**
@@ -67,14 +74,16 @@ class AgileDashboard_BacklogItem_SubBacklogItemProvider {
             return $this->getMatchingIdsForTopBacklog($milestone, $backlog_tracker, $user);
         }
 
-        return $this->getMatchingIdsForMilestone($milestone, $backlog_tracker);
+        return $this->getMatchingIdsForMilestone($milestone, $backlog_tracker, $user);
     }
 
-    private function getMatchingIdsForMilestone(Planning_Milestone $milestone, Tracker $backlog_tracker) {
+    private function getMatchingIdsForMilestone(Planning_Milestone $milestone, Tracker $backlog_tracker, PFUser $user)
+    {
         $milestone_id_seed = array($milestone->getArtifactId());
-
         $this->inspected_ids = $milestone_id_seed;
-        $this->filterBacklogIds($backlog_tracker->getId(), $milestone_id_seed);
+
+        $filtrable_backlog_tracker_ids = $this->getSubPlanningTrackerIds($milestone, $user);
+        $this->filterBacklogIds($backlog_tracker->getId(), $milestone_id_seed, $filtrable_backlog_tracker_ids);
 
         return $this->backlog_ids;
     }
@@ -96,21 +105,37 @@ class AgileDashboard_BacklogItem_SubBacklogItemProvider {
      * Retrieve all linked artifacts and keep only those that belong to backlog tracker
      *
      * We need to keep list of ids we already looked at so we avoid cycles.
-     *
-     * @param int $backlog_tracker_id
-     * @param array $artifacts
      */
-    private function filterBacklogIds($backlog_tracker_id, array $artifacts) {
+    private function filterBacklogIds($backlog_tracker_id, array $artifacts, array $filtrable_planning_tracker_ids)
+    {
         $artifacts_to_inspect = array();
         foreach ($this->dao->getLinkedArtifactsByIds($artifacts, $this->inspected_ids) as $artifact_row) {
-            $artifacts_to_inspect[] = $artifact_row['id'];
+            if (in_array($artifact_row['tracker_id'], $filtrable_planning_tracker_ids)) {
+                $artifacts_to_inspect[] = $artifact_row['id'];
+            }
+
             if ($artifact_row['tracker_id'] == $backlog_tracker_id) {
                 $this->backlog_ids[$artifact_row['id']] = true;
             }
+
             $this->inspected_ids[] = $artifact_row['id'];
         }
+
         if (count($artifacts_to_inspect) > 0) {
-            $this->filterBacklogIds($backlog_tracker_id, $artifacts_to_inspect);
+            $this->filterBacklogIds($backlog_tracker_id, $artifacts_to_inspect, $filtrable_planning_tracker_ids);
         }
+    }
+
+    /**
+     * @return int[]
+     */
+    private function getSubPlanningTrackerIds(Planning_Milestone $milestone, PFUser $user)
+    {
+        $planning_tracker_ids = [];
+        foreach ($this->planning_factory->getSubPlannings($milestone->getPlanning(), $user) as $sub_planning) {
+            $planning_tracker_ids[] = $sub_planning->getPlanningTrackerId();
+        }
+
+        return $planning_tracker_ids;
     }
 }

plugins/agiledashboard/include/Planning/PlanningFactory.class.php
@@ -747,4 +747,21 @@ class PlanningFactory {
             return $this->getPlanningByPlanningTracker($planning_tracker);
         }
     }
+
+    /**
+     * @return Planning[]
+     */
+    public function getSubPlannings(Planning $base_planning, PFUser $user)
+    {
+        $all_plannings = $this->getOrderedPlanningsWithBacklogTracker($user, $base_planning->getGroupId());
+        $sub_plannings = [];
+        foreach ($all_plannings as $key => $planning) {
+            if ($planning->getId() == $base_planning->getId()) {
+                $sub_plannings = array_slice($all_plannings, $key+1);
+                break;
+            }
+        }
+
+        return $sub_plannings;
+    }
 }

plugins/agiledashboard/tests/include/AgileDashboard/BacklogItem/SubBacklogItemProviderTest.php
@@ -29,15 +29,29 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
 
     public function setUp() {
         parent::setUp();
-        $this->milestone       = aMilestone()->withArtifact(anArtifact()->withId(3)->build())->build();
+
+        $release_tracker  = aMockTracker()->withId(105)->build();
+        $sprint_tracker   = aMockTracker()->withId(106)->build();
+        $release_planning = aPlanning()->withId(1)->withPlanningTracker($release_tracker)->build();
+        $sprint_planning  = aPlanning()->withId(2)->withPlanningTracker($sprint_tracker)->build();
+
+        $this->milestone       = aMilestone()->withArtifact(anArtifact()->withId(3)->build())->withPlanning($sprint_planning)->build();
         $this->backlog_tracker = aTracker()->withId(35)->build();
         $this->dao             = mock('Tracker_ArtifactDao');
 
         $this->user                            = aUser()->build();
         $this->backlog_factory                 = mock('AgileDashboard_Milestone_Backlog_BacklogFactory');
         $this->backlog_item_collection_factory = mock('AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory');
+        $this->planning_factory                = stub(PlanningFactory::class)->getSubPlannings()->returns(array(
+            $sprint_planning
+        ));
 
-        $this->provider = new AgileDashboard_BacklogItem_SubBacklogItemProvider($this->dao, $this->backlog_factory, $this->backlog_item_collection_factory);
+        $this->provider = new AgileDashboard_BacklogItem_SubBacklogItemProvider(
+            $this->dao,
+            $this->backlog_factory,
+            $this->backlog_item_collection_factory,
+            $this->planning_factory
+        );
     }
 
     public function itReturnsTheMatchingIds() {
@@ -57,5 +71,20 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
 
         $result = $this->provider->getMatchingIds($this->milestone, $this->backlog_tracker, $this->user);
         $this->assertEqual($result, array());
+    }
+
+    public function itDoesNotFilterFromArtifactsThatAreNotContentOfSubOrCurrentPlanning()
+    {
+        stub($this->dao)->getLinkedArtifactsByIds(array(3), array(3))->returnsDar(
+            array('id' => 7,  'tracker_id' => 35),
+            array('id' => 8,  'tracker_id' => 35),
+            array('id' => 11, 'tracker_id' => 35),
+            array('id' => 158, 'tracker_id' => 105)
+        );
+
+        expect($this->dao)->getLinkedArtifactsByIds()->once();
+
+        $result = $this->provider->getMatchingIds($this->milestone, $this->backlog_tracker, $this->user);
+        $this->assertEqual(array_keys($result), array(7, 8, 11));
     }
 }

plugins/agiledashboard/include/AgileDashboard/Milestone/ParentTrackerRetriever.php
@@ -55,17 +55,8 @@ class ParentTrackerRetriever
             }
         }
 
-        $all_plannings = $this->planning_factory->getOrderedPlanningsWithBacklogTracker($user, $milestone->getGroupId());
-        $sub_plannings = [];
-        foreach ($all_plannings as $key => $planning) {
-            if ($planning->getId() == $milestone->getPlanning()->getId()) {
-                $sub_plannings = array_slice($all_plannings, $key+1);
-                break;
-            }
-        }
-
         $sub_backlog_tracker_ids = [];
-        foreach ($sub_plannings as $sub_planning) {
+        foreach ($this->planning_factory->getSubPlannings($milestone->getPlanning(), $user) as $sub_planning) {
             $sub_backlog_tracker_ids = array_merge($sub_backlog_tracker_ids, $sub_planning->getBacklogTrackersIds());
         }
 

plugins/agiledashboard/include/agiledashboardPlugin.class.php
@@ -279,7 +279,13 @@ class AgileDashboardPlugin extends Plugin {
         $milestone          = $milestone_provider->getMilestone();
 
         if ($milestone) {
-            $provider = new AgileDashboard_BacklogItem_SubBacklogItemProvider(new Tracker_ArtifactDao(), $this->getBacklogFactory(), $this->getBacklogItemCollectionFactory());
+            $provider = new AgileDashboard_BacklogItem_SubBacklogItemProvider(
+                new Tracker_ArtifactDao(),
+                $this->getBacklogFactory(),
+                $this->getBacklogItemCollectionFactory(),
+                $this->getPlanningFactory()
+            );
+
             $params['result'][]         = $provider->getMatchingIds($milestone, $backlog_tracker, $user);
             $params['search_performed'] = true;
         }

plugins/agiledashboard/tests/include/AgileDashboard/Milestone/ParentTrackerRetrieverTest.php
@@ -52,10 +52,7 @@ class ParentTrackerRetrieverTest extends TuleapTestCase
         $user                        = mock(PFUser::class);
         $descendant_backlog_trackers = [$user_story_tracker, $bug_tracker];
 
-        stub($planning_factory)->getOrderedPlanningsWithBacklogTracker()->returns([
-            $release_planning,
-            $sprint_planning
-        ]);
+        stub($planning_factory)->getSubPlannings($sprint_planning, $user)->returns([]);
 
         $trackers = $retriever->getCreatableParentTrackers($milestone, $user, $descendant_backlog_trackers);
 
@@ -88,9 +85,7 @@ class ParentTrackerRetrieverTest extends TuleapTestCase
         $user                        = mock(PFUser::class);
         $descendant_backlog_trackers = [$user_story_tracker, $bug_tracker];
 
-        stub($planning_factory)->getOrderedPlanningsWithBacklogTracker($user, '*')->returns([
-            $product_planning,
-            $release_planning,
+        stub($planning_factory)->getSubPlannings($release_planning, $user)->returns([
             $sprint_planning
         ]);
 
@@ -122,8 +117,7 @@ class ParentTrackerRetrieverTest extends TuleapTestCase
         $user                        = mock(PFUser::class);
         $descendant_backlog_trackers = [$user_story_tracker, $bug_tracker];
 
-        stub($planning_factory)->getOrderedPlanningsWithBacklogTracker($user, '*')->returns([
-            $release_planning,
+        stub($planning_factory)->getSubPlannings($release_planning, $user)->returns([
             $sprint_planning
         ]);
 
@@ -162,8 +156,7 @@ class ParentTrackerRetrieverTest extends TuleapTestCase
         $user                        = mock(PFUser::class);
         $descendant_backlog_trackers = [$user_story_tracker, $bug_tracker];
 
-        stub($planning_factory)->getOrderedPlanningsWithBacklogTracker($user, '*')->returns([
-            $release_planning,
+        stub($planning_factory)->getSubPlannings($release_planning, $user)->returns([
             $sprint_planning,
             $sub_sprint_planning
         ]);
@@ -196,9 +189,7 @@ class ParentTrackerRetrieverTest extends TuleapTestCase
         $user                        = mock(PFUser::class);
         $descendant_backlog_trackers = [$user_story_tracker, $bug_tracker, $epic_tracker];
 
-        stub($planning_factory)->getOrderedPlanningsWithBacklogTracker($user, '*')->returns([
-            $sprint_planning,
-        ]);
+        stub($planning_factory)->getSubPlannings($sprint_planning, $user)->returns([]);
 
         $trackers = $retriever->getCreatableParentTrackers($milestone, $user, $descendant_backlog_trackers);
 

commit acb01d84ac380e66fb8f0d1ea869ee936a61ff7f

request #11785 Cannot filter children artifacts planned in sprints
This bug has been introduced by 35146c02a538fd6ad11015bc020fe259843d2970

All the filter by milestone must be OK, without searching in neasted
links (bug that was fixed by previous request #11210)

Change-Id: Ic14ad49c1352e4fec6ffaed350e7f61a9ac929be

plugins/agiledashboard/include/AgileDashboard/BacklogItem/SubBacklogItemProvider.class.php
@@ -50,7 +50,8 @@ class AgileDashboard_BacklogItem_SubBacklogItemProvider {
      */
     private $planning_factory;
 
-    public function __construct(Tracker_ArtifactDao $dao,
+    public function __construct(
+        Tracker_ArtifactDao $dao,
         AgileDashboard_Milestone_Backlog_BacklogFactory $backlog_factory,
         AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory $backlog_item_collection_factory,
         PlanningFactory $planning_factory
@@ -110,7 +111,11 @@ class AgileDashboard_BacklogItem_SubBacklogItemProvider {
     {
         $artifacts_to_inspect = array();
         foreach ($this->dao->getLinkedArtifactsByIds($artifacts, $this->inspected_ids) as $artifact_row) {
-            if (in_array($artifact_row['tracker_id'], $filtrable_planning_tracker_ids)) {
+            $artifact_row_tracker_id = $artifact_row['tracker_id'];
+
+            if (! $this->planning_factory->isTrackerIdUsedInAPlanning($artifact_row['tracker_id']) ||
+                in_array($artifact_row_tracker_id, $filtrable_planning_tracker_ids)
+            ) {
                 $artifacts_to_inspect[] = $artifact_row['id'];
             }
 

plugins/agiledashboard/include/Planning/PlanningFactory.class.php
@@ -438,6 +438,8 @@ class PlanningFactory {
             $p->setBacklogTrackers($this->getBacklogTrackers($p));
             return $p;
         }
+
+        return null;
     }
 
     public function isTrackerIdUsedInAPlanning($tracker_id) {

plugins/agiledashboard/tests/include/AgileDashboard/BacklogItem/SubBacklogItemProviderTest.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-require_once dirname(__FILE__).'/../../../bootstrap.php';
+require_once __DIR__.'/../../../bootstrap.php';
 
 class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCase {
 
@@ -29,22 +29,22 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
 
     public function setUp() {
         parent::setUp();
+        $this->setUpGlobalsMockery();
 
-        $release_tracker  = aMockTracker()->withId(105)->build();
-        $sprint_tracker   = aMockTracker()->withId(106)->build();
-        $release_planning = aPlanning()->withId(1)->withPlanningTracker($release_tracker)->build();
-        $sprint_planning  = aPlanning()->withId(2)->withPlanningTracker($sprint_tracker)->build();
+        $this->backlog_tracker = aTracker()->withId(35)->build();
+        $this->task_tracker    = aMockeryTracker()->withId(36)->withParent($this->backlog_tracker)->build();
+        $release_tracker       = aMockeryTracker()->withId(105)->build();
+        $sprint_tracker        = aMockeryTracker()->withId(106)->build();
+        $release_planning      = aPlanning()->withId(1)->withPlanningTracker($release_tracker)->build();
+        $sprint_planning       = aPlanning()->withId(2)->withPlanningTracker($sprint_tracker)->withBacklogTracker($this->backlog_tracker)->build();
 
         $this->milestone       = aMilestone()->withArtifact(anArtifact()->withId(3)->build())->withPlanning($sprint_planning)->build();
-        $this->backlog_tracker = aTracker()->withId(35)->build();
-        $this->dao             = mock('Tracker_ArtifactDao');
+        $this->dao             = \Mockery::spy(\Tracker_ArtifactDao::class);
 
         $this->user                            = aUser()->build();
-        $this->backlog_factory                 = mock('AgileDashboard_Milestone_Backlog_BacklogFactory');
-        $this->backlog_item_collection_factory = mock('AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory');
-        $this->planning_factory                = stub(PlanningFactory::class)->getSubPlannings()->returns(array(
-            $sprint_planning
-        ));
+        $this->backlog_factory                 = \Mockery::spy(\AgileDashboard_Milestone_Backlog_BacklogFactory::class);
+        $this->backlog_item_collection_factory = \Mockery::spy(\AgileDashboard_Milestone_Backlog_BacklogItemCollectionFactory::class);
+        $this->planning_factory                = \Mockery::spy(PlanningFactory::class);
 
         $this->provider = new AgileDashboard_BacklogItem_SubBacklogItemProvider(
             $this->dao,
@@ -52,6 +52,11 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
             $this->backlog_item_collection_factory,
             $this->planning_factory
         );
+
+        $this->planning_factory->shouldReceive('getSubPlannings')->andReturn($sprint_planning);
+        $this->planning_factory->shouldReceive('isTrackerIdUsedInAPlanning')->with(35)->andReturn(false);
+        $this->planning_factory->shouldReceive('isTrackerIdUsedInAPlanning')->with(36)->andReturn(false);
+        $this->planning_factory->shouldReceive('isTrackerIdUsedInAPlanning')->with(105)->andReturn(true);
     }
 
     public function itReturnsTheMatchingIds() {
@@ -63,6 +68,7 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
         stub($this->dao)->getLinkedArtifactsByIds(array(7, 8, 11), array(3, 7, 8, 11))->returnsEmptyDar();
 
         $result = $this->provider->getMatchingIds($this->milestone, $this->backlog_tracker, $this->user);
+
         $this->assertEqual(array_keys($result), array(7, 8, 11));
     }
 
@@ -82,9 +88,30 @@ class AgileDashboard_BacklogItem_SubBacklogItemProviderTest extends TuleapTestCa
             array('id' => 158, 'tracker_id' => 105)
         );
 
-        expect($this->dao)->getLinkedArtifactsByIds()->once();
+        stub($this->dao)->getLinkedArtifactsByIds(array(7, 8, 11), array(3, 7, 8, 11, 158))->returnsEmptyDar();
 
         $result = $this->provider->getMatchingIds($this->milestone, $this->backlog_tracker, $this->user);
         $this->assertEqual(array_keys($result), array(7, 8, 11));
+    }
+
+    public function itFiltersFromArtifactsThatAreChildOfContentOfSubOrCurrentPlanning()
+    {
+        stub($this->dao)->getLinkedArtifactsByIds(array(3), array(3))->returnsDar(
+            array('id' => 7,  'tracker_id' => 35),
+            array('id' => 8,  'tracker_id' => 35),
+            array('id' => 11, 'tracker_id' => 35),
+            array('id' => 158, 'tracker_id' => 105)
+        );
+
+        stub($this->dao)->getLinkedArtifactsByIds(array(7, 8, 11), array(3, 7, 8, 11, 158))->returnsDar(
+            array('id' => 200,  'tracker_id' => 36),
+            array('id' => 201,  'tracker_id' => 36),
+            array('id' => 159, 'tracker_id' => 105)
+        );
+
+        stub($this->dao)->getLinkedArtifactsByIds(array(200, 201), array(3, 7, 8, 11, 158, 200, 201, 159))->returnsEmptyDar();
+
+        $result = $this->provider->getMatchingIds($this->milestone, $this->task_tracker, $this->user);
+        $this->assertEqual(array_keys($result), array(200, 201));
     }
 }