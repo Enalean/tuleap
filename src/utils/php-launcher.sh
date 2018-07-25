Enalean/tuleap

commit 6a7c9569b53846e350dcd42313fadafd027dc064

request #11897: Stack traces can be displayed to end users when CLI command potentially leaking sensitive information
Change-Id: I4dfa7834fe18e701f1104c2bc557342056e74379

src/utils/php-launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) Enalean, 2011-2016. All Rights Reserved.
+# Copyright (c) Enalean, 2011-2018. All Rights Reserved.
 # Copyright (c) STMicroelectronics, 2005. All Rights Reserved.
 #
 # Originally written by Manuel Vacelet, 2005
@@ -45,10 +45,10 @@ PEAR_PATH="/usr/share/pear"
 # Include path is only defined in php.conf (and not php.ini).
 # It was also reported that 8MB (default memory limit) is not sufficient in some cases.
 if [ -z "$PHP_PARAMS" ]; then
-    PHP_PARAMS="-q -d include_path=/usr/share/php:/usr/share/pear:/usr/share/tuleap/src/www/include:/usr/share/tuleap/src:/usr/share/codendi/src/www/include:/usr/share/codendi/src:/usr/share/jpgraph:. -d memory_limit=256M -d display_errors=On"
+    PHP_PARAMS="-q -d include_path=/usr/share/php:/usr/share/pear:/usr/share/tuleap/src/www/include:/usr/share/tuleap/src:/usr/share/codendi/src/www/include:/usr/share/codendi/src:/usr/share/jpgraph:. -d memory_limit=256M"
 fi
 
 # Finally runs php interpretor
 phpscript=$1;
 shift;
-exec "${PHP}" ${PHP_PARAMS} $phpscript "$@"
+exec "${PHP}" ${PHP_PARAMS} $phpscript "$@" 2> /dev/null

plugins/git/bin/gl-membership.pl
@@ -43,4 +43,4 @@ while (<PHP_LAUNCHER>) {
 }
 close(PHP_LAUNCHER);
 
-exec "/opt/rh/rh-php56/root/usr/bin/php $PHP_PARAMS /usr/share/tuleap/plugins/git/bin/gl-membership.php $username";
+exec "/opt/rh/rh-php56/root/usr/bin/php -d error_reporting=0 $PHP_PARAMS /usr/share/tuleap/plugins/git/bin/gl-membership.php $username";

commit dae9460031a54ccd114456f7b7f9ab8642ba6693

request #11916: Tuleap CLI tool does not display any errors anymore
The change introduced in commit 6a7c9569b53846e350dcd42313fadafd027dc064
is a bit aggressive and even remove expected error output from the CLI
tool.

This contribution try to compromise between the data leaks and usability.
Calls made through /usr/bin/tuleap displays errors again.

Testers, /usr/bin/tuleap is deployed by the packaging so either think
of redeploying it yourself or directly use src/utils/tuleap.

Change-Id: I1358b904dee14bed89f5f9c4725a2b51cfbfb550

src/utils/php-launcher.sh
@@ -48,7 +48,12 @@ if [ -z "$PHP_PARAMS" ]; then
     PHP_PARAMS="-q -d include_path=/usr/share/php:/usr/share/pear:/usr/share/tuleap/src/www/include:/usr/share/tuleap/src:/usr/share/codendi/src/www/include:/usr/share/codendi/src:/usr/share/jpgraph:. -d memory_limit=256M"
 fi
 
+php_display_errors="-d error_reporting=0"
+if [ "$DISPLAY_ERRORS" = true ]; then
+    php_display_errors=""
+fi
+
 # Finally runs php interpretor
 phpscript=$1;
 shift;
-exec "${PHP}" ${PHP_PARAMS} $phpscript "$@" 2> /dev/null
+exec "${PHP}" ${php_display_errors} ${PHP_PARAMS} $phpscript "$@"

src/utils/tuleap
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d '/usr/share/tuleap' ]; then
-    /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/src/utils/tuleap.php $@
+    DISPLAY_ERRORS=true /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/src/utils/tuleap.php $@
 else
-    /usr/share/codendi/src/utils/php-launcher.sh /usr/share/codendi/src/utils/tuleap.php $@
+    DISPLAY_ERRORS=true /usr/share/codendi/src/utils/php-launcher.sh /usr/share/codendi/src/utils/tuleap.php $@
 fi