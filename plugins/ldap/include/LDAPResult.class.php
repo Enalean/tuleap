2952c79db2a4767de02012b65e84f5b6fb6eeb34
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2018-08-10 14:50:02 +0200

    request #12103 LDAP group found by displayname must deal with an empty displayname
    If the displayname is NULL or empty, then the common name is returned.
    
    Change-Id: I5e3b18eea4689a26b2b0144fb8de8bd57ae628ba

--- plugins/ldap/include/LDAPResult.class.php
+++ plugins/ldap/include/LDAPResult.class.php
@@ -1,26 +1,25 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012-2018. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2005. All Rights Reserved.
  *
  * Originally written by Manuel Vacelet, 2005
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi; if not, write to the Free Software
+ * along with Tuleap; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * 
  */
 
 
@@ -210,11 +209,15 @@ class LDAPResult implements Iterator, Countable {
     /**
      * @return string
      * */
-    function getGroupDisplayName()
+    public function getGroupDisplayName()
     {
         if (isset($this->ldapParams['grp_display_name'])) {
-            return $this->get($this->ldapParams['grp_display_name']);
+            $display_name = $this->get($this->ldapParams['grp_display_name']);
+            if ($display_name) {
+                return $display_name;
+            }
         }
+
         return $this->getCommonName();
     }
 
