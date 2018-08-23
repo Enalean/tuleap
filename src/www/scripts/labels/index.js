9e05ada68202f33d3c630415114b053429bc4082
Joris Masson <joris.masson@enalean.com>
2018-08-23 10:51:01 +0200

    Migrate labels box to Burning Parrot
    Part of story #10410 have burning parrot pull requests
    
    How to test:
    - The labels are properly displayed in the dashboard (list of all pull
    rrequests). They are not editable
    - The labels are properly displayed in the pull request details pane.
    You can add, remove and search labels. Labels are persisted on the
    backend properly.
    
    - No functional changes on the Project admin labels or the Labeled items
    widget configuration
    
    Change-Id: I300937089850375b3ce7b39e6eedf3395f5eea91

--- src/www/scripts/labels/index.js
+++
@@ -1,7 +0,0 @@
-import { create } from "./labels-box";
-
-document.addEventListener("DOMContentLoaded", function() {
-    [].forEach.call(document.querySelectorAll(".item-labels-box"), function(element) {
-        create(element, element.dataset.labelsEndpoint);
-    });
-});
