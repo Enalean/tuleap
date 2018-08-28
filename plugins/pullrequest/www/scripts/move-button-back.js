84a3cc37e182760e9694ff84f043fa52bd508f84
Thomas Gorka <thomas.gorka@enalean.com>
2018-08-28 10:31:20 +0200

    Migrate open pull requests badge to tlp
    Part of story #10410 have burning parrot pull requests
    
    How to test:
    --> The badge is properly displayed
    
    Change-Id: Ie0becae7fca6c8735102534a86af010279962636

--- plugins/pullrequest/www/scripts/move-button-back.js
+++ plugins/pullrequest/www/scripts/move-button-back.js
@@ -16,22 +16,18 @@
  */
 
 document.addEventListener("DOMContentLoaded", () => {
-    var plugin_git_reference = document.getElementById("plugin_git_reference");
+    var plugin_git_reference = document.getElementsByClassName("git-repository-name");
 
-    if (!plugin_git_reference) {
+    if (!plugin_git_reference.length) {
         return;
     }
 
-    var plugin_git_reference_title = plugin_git_reference.getElementsByTagName("h1")[0];
+    var plugin_git_reference_title = plugin_git_reference[0];
     var button_back = document.getElementsByClassName("pull-request-button-back")[0];
 
     if (!button_back) {
         return;
     }
 
-    var nb_pull_request_badge = plugin_git_reference_title.getElementsByClassName(
-        "nb-pull-request-badge"
-    )[0];
-    plugin_git_reference_title.removeChild(nb_pull_request_badge);
     plugin_git_reference_title.appendChild(button_back);
 });
