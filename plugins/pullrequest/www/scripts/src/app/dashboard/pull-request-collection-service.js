1e35bb9ef6abbc0fc2d4f25dfe3eff41d45dc556
Thomas Gorka <thomas.gorka@enalean.com>
2018-08-28 13:35:26 +0200

    Remove the last lodash dependencies
    This patch reduces the technical debt.
    
    Part of story #10410 have burning parrot pull requests
    
    How to test:
    --> open pull requests are loaded
    --> closed pull requests are loaded when the "load closed PR" is clicked
    --> pull requests are displayed in the right order (from the newer to
       the latest)
    
    Change-Id: I066617db47fe6546488fda0a7f20f2209e3d03a5

--- plugins/pullrequest/www/scripts/src/app/dashboard/pull-request-collection-service.js
+++ plugins/pullrequest/www/scripts/src/app/dashboard/pull-request-collection-service.js
@@ -1,6 +1,4 @@
 import { noop } from "angular";
-import partition from "lodash.partition";
-import forEachRight from "lodash.foreachright";
 
 export default PullRequestCollectionService;
 
@@ -39,14 +37,19 @@ function PullRequestCollectionService(
     function loadAllPullRequests() {
         const repository_id = SharedPropertiesService.getRepositoryId();
 
-        const promise = PullRequestCollectionRestService.getAllPullRequests(repository_id).then(
+        return PullRequestCollectionRestService.getAllPullRequests(repository_id).then(
             pull_requests => {
-                const all_pull_requests = partition(
-                    pull_requests,
-                    PullRequestService.isPullRequestClosed
-                );
-                const closed_pull_requests = all_pull_requests[0];
-                const open_pull_requests = all_pull_requests[1];
+                const closed_pull_requests = [];
+                const open_pull_requests = [];
+
+                pull_requests.forEach(pull_request => {
+                    if (PullRequestService.isPullRequestClosed(pull_request)) {
+                        closed_pull_requests.push(pull_request);
+                        return;
+                    }
+
+                    open_pull_requests.push(pull_request);
+                });
 
                 there_is_at_least_one_open_pull_request = open_pull_requests.length > 0;
                 there_is_at_least_one_closed_pull_request = closed_pull_requests.length > 0;
@@ -57,8 +60,6 @@ function PullRequestCollectionService(
                 closed_pull_requests_loaded = true;
             }
         );
-
-        return promise;
     }
 
     function loadOpenPullRequests() {
@@ -69,7 +70,7 @@ function PullRequestCollectionService(
             callback = noop;
         }
 
-        const promise = PullRequestCollectionRestService.getAllOpenPullRequests(
+        return PullRequestCollectionRestService.getAllOpenPullRequests(
             repository_id,
             callback
         ).then(function(open_pull_requests) {
@@ -80,40 +81,28 @@ function PullRequestCollectionService(
             there_is_at_least_one_open_pull_request = open_pull_requests.length > 0;
             open_pull_requests_loaded = true;
         });
-
-        return promise;
     }
 
     function loadClosedPullRequests() {
-        var repository_id = SharedPropertiesService.getRepositoryId();
+        const repository_id = SharedPropertiesService.getRepositoryId();
 
-        var promise = PullRequestCollectionRestService.getAllClosedPullRequests(
+        return PullRequestCollectionRestService.getAllClosedPullRequests(
             repository_id,
             progressivelyLoadCallback
         ).then(function(closed_pull_requests) {
             there_is_at_least_one_closed_pull_request = closed_pull_requests.length > 0;
             closed_pull_requests_loaded = true;
         });
-
-        return promise;
     }
 
     function progressivelyLoadCallback(pull_requests) {
-        forEachRight(pull_requests, function(pull_request) {
-            self.all_pull_requests.push(pull_request);
-        });
+        const pull_request_copy = [...pull_requests];
+        self.all_pull_requests.push(...pull_request_copy.reverse());
     }
 
     function resetAllPullRequests(pull_requests) {
-        emptyArray(self.all_pull_requests);
-
-        forEachRight(pull_requests, function(pull_request) {
-            self.all_pull_requests.push(pull_request);
-        });
-    }
-
-    function emptyArray(array) {
-        array.length = 0;
+        self.all_pull_requests.length = 0;
+        self.all_pull_requests.push(...pull_requests.reverse());
     }
 
     function search(pull_request_id) {
