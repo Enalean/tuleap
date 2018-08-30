bd04e18b62c606189d5673ba1c672f1eafadb087
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2018-08-30 16:59:37 +0200

    Fix warnings & errors in login page
    this is part of request #12147 Fix PHP 7.2 warnings and errors
    
    Change-Id: I524dda78f49cafac0585323f4e3b751f2d0ad46e

--- src/www/account/login.php
+++ src/www/account/login.php
@@ -1,11 +1,23 @@
 <?php
-// Copyright (c) Enalean, 2015-2017. All Rights Reserved.
-//
-// SourceForge: Breaking Down the Barriers to Open Source Development
-// Copyright 1999-2000 (c) The SourceForge Crew
-// http://sourceforge.net
-//
-//
+/**
+ * Copyright (c) Enalean, 2015-2018. All rights reserved
+ * Copyright 1999-2000 (c) The SourceForge Crew
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/
+ */
 
 header("Expires: Wed, 11 Nov 1998 11:11:11 GMT");
 header("Cache-Control: no-cache, no-store, must-revalidate");
@@ -21,7 +33,7 @@ if ($request->get('confirm_hash')) {
     $login_controller->confirmHash();
 }
 
-$em =& EventManager::instance();
+$em = EventManager::instance();
 
 //
 // Validate input
@@ -31,7 +43,7 @@ $em =& EventManager::instance();
 $_cVar = array();
 // Raw variables
 $_rVar = array();
-$request =& HTTPRequest::instance();
+$request = HTTPRequest::instance();
 
 $_rVar['form_loginname'] = null;
 if($request->valid(new Valid_String('form_loginname'))) {
