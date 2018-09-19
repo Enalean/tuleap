da067090a0f3a4dc907b7bbddce292f4a91d983b
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-09-19 18:37:59 +0200

    Authentication response are processed without the help of an external lib
    Tuleap now manages directly the authentication response from the
    authentication endpoint.
    This contribution reduces the dependency to an external library,
    the goal is to remove it completly to be able to drop completly
    the non compatible with PHP 7.2 code that is currently used.
    
    request #12235: Drop usage of InoOicClient library in the OpenIDConnect client plugin
    
    Change-Id: I77441b2e50cf074c80a3a60ce939a788f127df42

---
+++ plugins/openidconnectclient/phpunit/Authentication/Authorization/AuthorizationResponseTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\OpenIDConnectClient\Authentication\Authorization;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class AuthorizationResponseTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testResponseIsBuiltWhenAllParametersAreAvailable()
+    {
+        $request = \Mockery::mock(\HTTPRequest::class);
+        $request->shouldReceive('get')->with('code')->andReturns('code');
+        $request->shouldReceive('get')->with('state')->andReturns('state');
+
+        $response = AuthorizationResponse::buildFromHTTPRequest($request);
+        $this->assertSame('code', $response->getCode());
+        $this->assertSame('state', $response->getState());
+    }
+
+    /**
+     * @expectedException \Tuleap\OpenIDConnectClient\Authentication\Authorization\MissingParameterAuthorizationResponseException
+     * @expectedExceptionMessage code
+     */
+    public function testResponseConstructionIsRejectedWhenCodeIsMissing()
+    {
+        $request = \Mockery::mock(\HTTPRequest::class);
+        $request->shouldReceive('get')->with('code')->andReturns(false);
+
+        AuthorizationResponse::buildFromHTTPRequest($request);
+    }
+
+    /**
+     * @expectedException \Tuleap\OpenIDConnectClient\Authentication\Authorization\MissingParameterAuthorizationResponseException
+     * @expectedExceptionMessage state
+     */
+    public function testResponseConstructionIsRejectedWhenStateIsMissing()
+    {
+        $request = \Mockery::mock(\HTTPRequest::class);
+        $request->shouldReceive('get')->with('code')->andReturns('code');
+        $request->shouldReceive('get')->with('state')->andReturns(false);
+
+        AuthorizationResponse::buildFromHTTPRequest($request);
+    }
+}
