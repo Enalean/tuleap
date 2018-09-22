f814e6bbd123d8363c966c6862f4048b52dc1973
Nicolas Terray <nicolas.terray@enalean.com>
2018-09-22 08:26:43 +0200

    Use a button instead of a badge for selector
    Part of story #10414: have a branch/tag selector
    
    No functional change, only styling and refactoring.
    
    You still need the $git_repository_bp = 1; in your local.inc to see the button.
    
    Now the main index.js for repository view does not contain anymore specific
    code, the remaining clone bar code is moved in its own file in order to
    prepare the field for next commit when we will introduce the branch/tag
    selector dropdown.
    
    Change-Id: I3779104206fb199ae5c0f0f97a9eb127bfd51000

--- plugins/git/www/scripts/repository/index.js
+++ plugins/git/www/scripts/repository/index.js
@@ -17,47 +17,12 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { dropdown as createDropdown } from "tlp";
 import initAlreadyForkedModal from "./already-forked-modal.js";
 import initCopyButton from "./copy-button.js";
+import initCloneBarDropdown from "./clone-bar-dropdown.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initAlreadyForkedModal();
     initCopyButton();
-
-    const button = document.getElementById("git-repository-clone-dropdown-button");
-    const transport_buttons = document.getElementsByClassName("git-repository-clone-transport");
-    const input = document.getElementById("git-repository-clone-input");
-    const button_text = document.getElementById("git-repository-clone-button-text");
-    const selected_icon = document.getElementById("git-repository-clone-selected-icon");
-
-    const read_only_badge = document.getElementById("git-repository-clone-read-only");
-    if (
-        !button ||
-        transport_buttons.length === 0 ||
-        !input ||
-        !button_text ||
-        !selected_icon ||
-        !read_only_badge
-    ) {
-        return;
-    }
-
-    const dropdown = createDropdown(button);
-
-    input.addEventListener("click", function() {
-        this.select();
-    });
-
-    for (const transport_button of transport_buttons) {
-        transport_button.addEventListener("click", function() {
-            const { url, isReadOnly } = this.dataset;
-            input.value = url;
-            button_text.textContent = this.text;
-            this.insertBefore(selected_icon, this.firstChild);
-            read_only_badge.classList.toggle("git-repository-clone-hidden", !isReadOnly);
-
-            dropdown.hide();
-        });
-    }
+    initCloneBarDropdown();
 });
