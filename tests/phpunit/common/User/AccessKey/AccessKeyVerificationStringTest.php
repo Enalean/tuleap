593b7674691cd26cba2cc258f07375e389c7c506
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-09-24 18:12:51 +0200

    API access keys can be generated
    For now the generation of the access key is not saved,
    so the generation is not really useful for an end user.
    
    This is part of story #12266: use an API key rather than a login / password
    
    Change-Id: I255b740fd00ac49f31d8643301fdbada437caa69

---
+++ tests/phpunit/common/User/AccessKey/AccessKeyVerificationStringTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\user\AccessKey;
+
+use PHPUnit\Framework\TestCase;
+use Tuleap\Cryptography\ConcealedString;
+
+class AccessKeyVerificationStringTest extends TestCase
+{
+    /**
+     * @expectedException \Tuleap\user\AccessKey\IncorrectSizeVerificationStringException
+     */
+    public function testIncorrectlySizedVerificationStringAreRejected()
+    {
+        new AccessKeyVerificationString(new ConcealedString('too_short'));
+    }
+
+    public function testGeneratedKeyIsValid()
+    {
+        $generated_access_key = AccessKeyVerificationString::generateNewAccessKeyVerificationString();
+        $access_key           = new AccessKeyVerificationString($generated_access_key->getString());
+
+        $this->assertSame($generated_access_key->getString(), $access_key->getString());
+    }
+}
