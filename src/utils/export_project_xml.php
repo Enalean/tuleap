Enalean/tuleap

commit fa66b55bd1aa09d94aaeb2ca0444ccdace232fdc

request #12300: Git post receive actions are not entirely processed when using RHEL/CentOS7
Similar issues has also been in other plugins and tools.

Change-Id: Ia8baa77e670bf90db45cb80075f3983eb17591fa

src/utils/export_project_xml.php
@@ -1,7 +1,7 @@
-#!/usr/share/codendi/src/utils/php-launcher.sh
+#!/usr/share/tuleaps/src/utils/php-launcher.sh
 <?php
 /**
- * Copyright (c) Enalean, 2012 - 2015. All Rights Reserved.
+ * Copyright (c) Enalean, 2012 - 2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *

plugins/git/etc/sudoers.d/tuleap-git-postreceive
@@ -1,3 +1,3 @@
 Defaults:gitolite !requiretty
 Defaults:gitolite !env_reset
-gitolite ALL= (codendiadm) SETENV: NOPASSWD: /usr/share/codendi/src/utils/php-launcher.sh /usr/share/codendi/plugins/git/hooks/post-receive.php*
+gitolite ALL= (codendiadm) SETENV: NOPASSWD: /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/plugins/git/hooks/post-receive.php*

plugins/git/hooks/post-receive-gitolite
@@ -20,5 +20,5 @@
 git_dir_path=$(git rev-parse --git-dir 2>/dev/null)
 repository_path=$(cd "$git_dir_path"; pwd)
 
-sudo -u codendiadm /usr/share/codendi/src/utils/php-launcher.sh /usr/share/codendi/plugins/git/hooks/post-receive.php "$repository_path" 2> /dev/null
+sudo -u codendiadm /usr/share/tuleap/src/utils/php-launcher.sh /usr/share/tuleap/plugins/git/hooks/post-receive.php "$repository_path" 2> /dev/null
 # END OF NEEDED CODENDI BLOCK

plugins/git/hooks/post-receive.php
@@ -1,4 +1,4 @@
-#!/usr/share/codendi/src/utils/php-launcher.sh
+#!/usr/share/tuleap/src/utils/php-launcher.sh
 <?php
 /**
  * Copyright Enalean (c) 2011-2018. All rights reserved.

plugins/git/include/Git/GitoliteHousekeeping/GitoliteHousekeepingGitGc.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -51,8 +51,8 @@ class Git_GitoliteHousekeeping_GitoliteHousekeepingGitGc {
         } else {
             $this->logger->warn(
                 'Cannot run git gc on gitolite admin working copy. '.
-                'Please run as root: /usr/share/codendi/src/utils/php-launcher.sh '.
-                '/usr/share/codendi/plugins/git/bin/gl-admin-housekeeping.php'
+                'Please run as root: /usr/share/tuleap/src/utils/php-launcher.sh '.
+                '/usr/share/tuleap/plugins/git/bin/gl-admin-housekeeping.php'
             );
         }
     }

plugins/git/include/Git/Hook/PostReceiveMailSender.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright Enalean (c) 2013-2017. All rights reserved.
+ * Copyright Enalean (c) 2013-2018. All rights reserved.
  *
- * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Tuleap and Enalean names and logos are registered trademarks owned by
  * Enalean SAS. All other trademarks or names are properties of their respective
  * owners.
  *
@@ -77,7 +77,7 @@ class PostReceiveMailSender
         $mail_raw_output  = array();
         $exit_status_code = 0;
         exec('GIT_DIR=' . escapeshellarg($repository->getFullPath()) .
-            ' /usr/share/codendi/plugins/git/hooks/post-receive-email ' . escapeshellarg($oldrev) . ' ' .
+            ' /usr/share/tuleap/plugins/git/hooks/post-receive-email ' . escapeshellarg($oldrev) . ' ' .
             escapeshellarg($newrev) . ' ' . escapeshellarg($refname), $mail_raw_output, $exit_status_code);
 
         $subject       = isset($mail_raw_output[0]) ? $mail_raw_output[0] : self::DEFAULT_MAIL_SUBJECT;

plugins/git/tests/Git/GitoliteHousekeeping/GitoliteHousekeepingGitGcTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -52,8 +52,8 @@ class Git_GitoliteHousekeeping_GitoliteHousekeepingGitGcTest extends TuleapTestC
 
         expect($this->logger)->warn(
             'Cannot run git gc on gitolite admin working copy. '.
-            'Please run as root: /usr/share/codendi/src/utils/php-launcher.sh '.
-            '/usr/share/codendi/plugins/git/bin/gl-admin-housekeeping.php'
+            'Please run as root: /usr/share/tuleap/src/utils/php-launcher.sh '.
+            '/usr/share/tuleap/plugins/git/bin/gl-admin-housekeeping.php'
         )->once();
         expect($this->gitgc)->execGitGcAsAppAdm()->never();
 

plugins/svn/bin/migrate_to_plugin.php
@@ -1,4 +1,4 @@
-#!/usr/share/codendi/src/utils/php-launcher.sh
+#!/usr/share/tuleap/src/utils/php-launcher.sh
 <?php
 /**
  * Copyright Enalean (c) 2018. All rights reserved.

commit 3af7d22beb19cac786e64d1e33dd739f19a92f41

request #12316: Wrong shebang on project export script
Calling /usr/share/tuleap/src/utils/export_project_xml.php directly
should work.

Issue has been introduced by fa66b55bd1aa09d94aaeb2ca0444ccdace232fdc.

Change-Id: I6405ba1019bcb46b3e57a03b2df45c1f935c30e6

src/utils/export_project_xml.php
@@ -1,4 +1,4 @@
-#!/usr/share/tuleaps/src/utils/php-launcher.sh
+#!/usr/share/tuleap/src/utils/php-launcher.sh
 <?php
 /**
  * Copyright (c) Enalean, 2012 - 2018. All Rights Reserved.