Enalean/tuleap

commit 1a7eb79d32d4fb1b2afc8eb5c8c3289440f2225b

Retrieving a set of commits via the REST API is fast again
The addition of the status and of the author as a Tuleap user
has slow down tremendously the retrieval of commit representations.
Consequently, the REST routes allowing to retrieve tags, branches or
the commits of a pull request can be too slow to be usable in the web UI.

This contribution solves the issue on working on the two root causes:
 * an index has been added on the user table to be able to quickly
find users matching an email.
 * the number of SQL queries has been reduced. For getting the representations
of a set of n commits, 2*n queries were necessary now only 2 queries are needed.

This is part of:
 - story #10414: have a branch/tag selector
 - story #11664: display list of commits of a pull request

Change-Id: I36e1b3ed36545772f43edc8daf6ebeccc6fc438b

plugins/git/include/CommitStatus/CommitStatusRetriever.php
@@ -37,13 +37,32 @@ class CommitStatusRetriever
      */
     public function getLastCommitStatus(\GitRepository $repository, $commit_reference)
     {
-        $row = $this->dao->getLastCommitStatusByRepositoryIdAndCommitReference($repository->getId(), $commit_reference);
+        return $this->getLastCommitStatuses($repository, [$commit_reference])[0];
+    }
 
-        if (empty($row)) {
-            return new CommitStatusUnknown;
+    /**
+     * @return CommitStatus[]
+     */
+    public function getLastCommitStatuses(\GitRepository $repository, array $commit_references)
+    {
+        $statuses           = [];
+        $commit_status_rows = $this->dao->getLastCommitStatusByRepositoryIdAndCommitReferences(
+            $repository->getId(),
+            $commit_references
+        );
+        $commit_status_rows_indexed_by_reference = [];
+        foreach ($commit_status_rows as $commit_status_row) {
+            $commit_status_rows_indexed_by_reference[$commit_status_row['commit_reference']] = $commit_status_row;
+        }
+        foreach ($commit_references as $commit_reference) {
+            if (! isset($commit_status_rows_indexed_by_reference[$commit_reference])) {
+                $statuses[] = new CommitStatusUnknown;
+                continue;
+            }
+            $date = new \DateTimeImmutable('@' . $commit_status_row['date']);
+            $statuses[] = new CommitStatusWithKnownStatus($commit_status_row['status'], $date);
         }
 
-        $date = new \DateTimeImmutable('@' . $row['date']);
-        return new CommitStatusWithKnownStatus($row['status'], $date);
+        return $statuses;
     }
 }

plugins/git/phpunit/CommitStatus/CommitStatusRetrieverTest.php
@@ -36,9 +36,13 @@ class CommitStatusRetrieverTest extends TestCase
 
         $repository = \Mockery::mock(\GitRepository::class);
 
-        $dao->shouldReceive('getLastCommitStatusByRepositoryIdAndCommitReference')->andReturns(
-            ['status' => CommitStatusWithKnownStatus::STATUS_SUCCESS, 'date' => 1528892466]
-        );
+        $dao->shouldReceive('getLastCommitStatusByRepositoryIdAndCommitReferences')->andReturns([
+            [
+                'commit_reference' => '38762cf7f55934b34d179ae6a4c80cadccbb7f0a',
+                'status'           => CommitStatusWithKnownStatus::STATUS_SUCCESS,
+                'date'             => 1528892466
+            ]
+        ]);
         $repository->shouldReceive('getId');
 
         $commit_status = $commit_status_retriever->getLastCommitStatus(
@@ -57,7 +61,7 @@ class CommitStatusRetrieverTest extends TestCase
 
         $repository = \Mockery::mock(\GitRepository::class);
 
-        $dao->shouldReceive('getLastCommitStatusByRepositoryIdAndCommitReference')->andReturns([]);
+        $dao->shouldReceive('getLastCommitStatusByRepositoryIdAndCommitReferences')->andReturns([]);
         $repository->shouldReceive('getId');
 
         $commit_status = $commit_status_retriever->getLastCommitStatus(
@@ -66,5 +70,35 @@ class CommitStatusRetrieverTest extends TestCase
         );
 
         $this->assertInstanceOf(CommitStatusUnknown::class, $commit_status);
+    }
+
+    public function testASetOfCommitStatusCanBeRetrieved()
+    {
+        $dao = \Mockery::mock(CommitStatusDAO::class);
+
+        $commit_status_retriever = new CommitStatusRetriever($dao);
+
+        $repository = \Mockery::mock(\GitRepository::class);
+
+        $dao->shouldReceive('getLastCommitStatusByRepositoryIdAndCommitReferences')->andReturns([
+            [
+                'commit_reference' => '38762cf7f55934b34d179ae6a4c80cadccbb7f0a',
+                'status'           => CommitStatusWithKnownStatus::STATUS_SUCCESS,
+                'date'             => 1528892466
+            ],
+            [
+                'commit_reference' => '23badb142cabe3e604ceb5fd5d243354e8e9f491',
+                'status'           => CommitStatusWithKnownStatus::STATUS_SUCCESS,
+                'date'             => 1528898888
+            ]
+        ]);
+        $repository->shouldReceive('getId');
+
+        $commit_statuses = $commit_status_retriever->getLastCommitStatuses(
+            $repository,
+            ['38762cf7f55934b34d179ae6a4c80cadccbb7f0a', '23badb142cabe3e604ceb5fd5d243354e8e9f491']
+        );
+
+        $this->assertCount(2, $commit_statuses);
     }
 }

plugins/git/include/CommitMetadata/CommitMetadata.php
@@ -18,41 +18,40 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Tuleap\PullRequest\REST\v1;
+namespace Tuleap\Git\CommitMetadata;
 
-use Tuleap\Git\REST\v1\GitCommitRepresentation;
+use Tuleap\Git\CommitStatus\CommitStatus;
 
-class GitCommitRepresentationCollection
+class CommitMetadata
 {
     /**
-     * @var GitCommitRepresentation[]
+     * @var CommitStatus
      */
-    private $commits_collection = [];
+    private $last_commit_status;
     /**
-     * @var int
+     * @var null|\PFUser
      */
-    private $total_size;
+    private $author;
 
-    /**
-     * @param GitCommitRepresentation[]
-     * @param int      $total_size
-     */
-    public function __construct(array $commits_representations, $total_size)
+    public function __construct(CommitStatus $last_commit_status, \PFUser $author = null)
     {
-        $this->commits_collection = $commits_representations;
-        $this->total_size = $total_size;
+        $this->last_commit_status = $last_commit_status;
+        $this->author             = $author;
     }
 
     /**
-     * @return GitCommitRepresentation[]
+     * @return CommitStatus
      */
-    public function getCommitsCollection()
+    public function getCommitStatus()
     {
-        return $this->commits_collection;
+        return $this->last_commit_status;
     }
 
-    public function getSize()
+    /**
+     * @return null|\PFUser
+     */
+    public function getAuthor()
     {
-        return $this->total_size;
+        return $this->author;
     }
 }

plugins/git/include/CommitMetadata/CommitMetadataRetriever.php
@@ -1,0 +1,68 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\CommitMetadata;
+
+use Tuleap\Git\CommitStatus\CommitStatusRetriever;
+use Tuleap\Git\GitPHP\Commit;
+
+class CommitMetadataRetriever
+{
+    /**
+     * @var CommitStatusRetriever
+     */
+    private $status_retriever;
+    /**
+     * @var \UserManager
+     */
+    private $user_manager;
+
+    public function __construct(CommitStatusRetriever $status_retriever, \UserManager $user_manager)
+    {
+        $this->status_retriever = $status_retriever;
+        $this->user_manager     = $user_manager;
+    }
+
+    public function getMetadataByRepositoryAndCommits(\GitRepository $repository, Commit ...$commits)
+    {
+        $commit_references = [];
+        $author_emails     = [];
+        foreach ($commits as $commit) {
+            $commit_references[] = $commit->GetHash();
+            $author_emails[]     = $commit->getAuthorEmail();
+        }
+        $statuses         = $this->status_retriever->getLastCommitStatuses($repository, $commit_references);
+        $authors_by_email = $this->user_manager->getUserCollectionByEmails($author_emails);
+
+        $metadata = [];
+
+        $commit_metadata_iterator = new \MultipleIterator(\MultipleIterator::MIT_NEED_ALL);
+        $commit_metadata_iterator->attachIterator(new \ArrayIterator($commits));
+        $commit_metadata_iterator->attachIterator(new \ArrayIterator($statuses));
+
+        foreach ($commit_metadata_iterator as list($commit, $status)) {
+            $author_email = $commit->getAuthorEmail();
+            $author       = $authors_by_email->getUserByEmail($author_email);
+            $metadata[]   = new CommitMetadata($status, $author);
+        }
+
+        return $metadata;
+    }
+}

plugins/git/include/CommitStatus/CommitStatusDAO.php
@@ -20,6 +20,7 @@
 
 namespace Tuleap\Git\CommitStatus;
 
+use ParagonIE\EasyDB\EasyStatement;
 use Tuleap\DB\DataAccessObject;
 
 class CommitStatusDAO extends DataAccessObject
@@ -37,14 +38,25 @@ class CommitStatusDAO extends DataAccessObject
         );
     }
 
-    public function getLastCommitStatusByRepositoryIdAndCommitReference($repository_id, $commit_reference)
+    public function getLastCommitStatusByRepositoryIdAndCommitReferences($repository_id, array $commit_references)
     {
-        $sql = 'SELECT status, date
-                FROM plugin_git_commit_status
-                WHERE repository_id = ? AND commit_reference = ?
-                ORDER BY id DESC
-                LIMIT 1';
+        if (empty($commit_references)) {
+            return [];
+        }
+        $commit_references_in_condition = EasyStatement::open()->in('?*', $commit_references);
 
-        return $this->getDB()->row($sql, $repository_id, $commit_reference);
+        $sql = "SELECT commit_status1.commit_reference, commit_status1.status, commit_status1.date
+                FROM plugin_git_commit_status AS commit_status1
+                LEFT JOIN plugin_git_commit_status AS commit_status2 ON (
+                  commit_status1.repository_id = commit_status2.repository_id AND
+                  commit_status1.commit_reference = commit_status2.commit_reference AND
+                  commit_status1.id < commit_status2.id
+                )
+                WHERE
+                  commit_status1.repository_id = ? AND
+                  commit_status1.commit_reference IN ($commit_references_in_condition) AND
+                  commit_status2.id IS NULL";
+
+        return $this->getDB()->safeQuery($sql, array_merge([$repository_id], $commit_references_in_condition->values()));
     }
 }

plugins/git/include/REST/v1/GitCommitRepresentation.php
@@ -21,6 +21,7 @@
 
 namespace Tuleap\Git\REST\v1;
 
+use Tuleap\Git\CommitMetadata\CommitMetadata;
 use Tuleap\Git\CommitStatus\CommitStatus;
 use Tuleap\Git\CommitStatus\CommitStatusUnknown;
 use Tuleap\Git\GitPHP\Commit;
@@ -66,7 +67,7 @@ class GitCommitRepresentation
      */
     public $commit_status;
 
-    public function build($repository_path, Commit $commit, CommitStatus $commit_status)
+    public function build($repository_path, Commit $commit, CommitMetadata $commit_metadata)
     {
         $this->id            = $commit->GetHash();
         $this->title         = $commit->GetTitle();
@@ -82,15 +83,14 @@ class GitCommitRepresentation
             ]
         );
 
-        $user_manager = \UserManager::instance();
-
-        $author = $user_manager->getUserByEmail($this->author_email);
+        $author = $commit_metadata->getAuthor();
         if ($author !== null) {
             $author_representation = new MinimalUserRepresentation();
             $this->author = $author_representation->build($author);
         }
 
         $this->commit_status = null;
+        $commit_status       = $commit_metadata->getCommitStatus();
         if ($commit_status->getStatusName() !== CommitStatusUnknown::NAME) {
             $this->commit_status = new GitCommitStatusRepresentation();
             $this->commit_status->build($commit_status);

plugins/git/include/REST/v1/GitCommitRepresentationBuilder.php
@@ -23,35 +23,46 @@ namespace Tuleap\Git\REST\v1;
 
 use Git_GitRepositoryUrlManager;
 use GitRepository;
-use Tuleap\Git\CommitStatus\CommitStatusRetriever;
+use Tuleap\Git\CommitMetadata\CommitMetadataRetriever;
 use Tuleap\Git\GitPHP\Commit;
 
 class GitCommitRepresentationBuilder
 {
     /**
-     * @var CommitStatusRetriever
+     * @var CommitMetadataRetriever
      */
-    private $status_retriever;
+    private $metadata_retriever;
     /**
      * @var Git_GitRepositoryUrlManager
      */
     private $url_manager;
 
-    public function __construct(CommitStatusRetriever $status_retriever, Git_GitRepositoryUrlManager $url_manager)
+    public function __construct(CommitMetadataRetriever $metadata_retriever, Git_GitRepositoryUrlManager $url_manager)
     {
-        $this->status_retriever = $status_retriever;
-        $this->url_manager      = $url_manager;
+        $this->metadata_retriever = $metadata_retriever;
+        $this->url_manager        = $url_manager;
     }
 
-    public function build(GitRepository $repository, Commit $commit)
+    /**
+     * @return GitCommitRepresentationCollection
+     */
+    public function build(GitRepository $repository, Commit ...$commits)
     {
-        $commit_status = $this->status_retriever->getLastCommitStatus($repository, $commit->GetHash());
+        $metadata = $this->metadata_retriever->getMetadataByRepositoryAndCommits($repository, ...$commits);
 
+        $commit_representation_iterator = new \MultipleIterator(\MultipleIterator::MIT_NEED_ALL);
+        $commit_representation_iterator->attachIterator(new \ArrayIterator($commits));
+        $commit_representation_iterator->attachIterator(new \ArrayIterator($metadata));
+
+        $representations = [];
         $repository_path = $this->url_manager->getRepositoryBaseUrl($repository);
 
-        $commit_representation = new GitCommitRepresentation();
-        $commit_representation->build($repository_path, $commit, $commit_status);
+        foreach ($commit_representation_iterator as list($commit, $metadata)) {
+            $commit_representation = new GitCommitRepresentation();
+            $commit_representation->build($repository_path, $commit, $metadata);
+            $representations[] = $commit_representation;
+        }
 
-        return $commit_representation;
+        return new GitCommitRepresentationCollection(...$representations);
     }
 }

plugins/git/include/REST/v1/GitCommitRepresentationCollection.php
@@ -1,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Git\REST\v1;
+
+use Tuleap\Git\GitPHP\Commit;
+
+class GitCommitRepresentationCollection
+{
+    /**
+     * @var GitCommitRepresentation[]
+     */
+    private $representations_indexed_by_commit_reference = [];
+
+    public function __construct(GitCommitRepresentation ...$representations)
+    {
+        foreach ($representations as $representation) {
+            $this->representations_indexed_by_commit_reference[$representation->id] = $representation;
+        }
+    }
+
+    /**
+     * @return GitCommitRepresentation
+     */
+    public function getRepresentation(Commit $commit)
+    {
+        if (! isset($this->representations_indexed_by_commit_reference[$commit->GetHash()])) {
+            throw new \UnexpectedValueException('Commit not found in the set of representations');
+        }
+        return $this->representations_indexed_by_commit_reference[$commit->GetHash()];
+    }
+
+    /**
+     * @return GitCommitRepresentation[]
+     */
+    public function getWholeCollectionAsArray()
+    {
+        return array_values($this->representations_indexed_by_commit_reference);
+    }
+}

commit fbe16868239038fdeb5c64e35a1ef8ecce2f8486

Set of commit statuses is retrieved with the expected values
This fix an issue introduced by 1a7eb79d32d4fb1b2afc8eb5c8c3289440f2225b,
only the commit status values retrieved in the database are used for the
set of commits.

This is part of:
 - story #10414: have a branch/tag selector
 - story #11664: display list of commits of a pull request

Change-Id: Ifda2ef1e8a0d66c457b77c77c9eaee4e30419bed

plugins/git/include/CommitStatus/CommitStatusRetriever.php
@@ -59,8 +59,9 @@ class CommitStatusRetriever
                 $statuses[] = new CommitStatusUnknown;
                 continue;
             }
-            $date = new \DateTimeImmutable('@' . $commit_status_row['date']);
-            $statuses[] = new CommitStatusWithKnownStatus($commit_status_row['status'], $date);
+            $commit_status_row = $commit_status_rows_indexed_by_reference[$commit_reference];
+            $date              = new \DateTimeImmutable('@' . $commit_status_row['date']);
+            $statuses[]        = new CommitStatusWithKnownStatus($commit_status_row['status'], $date);
         }
 
         return $statuses;

plugins/git/phpunit/CommitStatus/CommitStatusRetrieverTest.php
@@ -88,7 +88,7 @@ class CommitStatusRetrieverTest extends TestCase
             ],
             [
                 'commit_reference' => '23badb142cabe3e604ceb5fd5d243354e8e9f491',
-                'status'           => CommitStatusWithKnownStatus::STATUS_SUCCESS,
+                'status'           => CommitStatusWithKnownStatus::STATUS_FAILURE,
                 'date'             => 1528898888
             ]
         ]);
@@ -100,5 +100,9 @@ class CommitStatusRetrieverTest extends TestCase
         );
 
         $this->assertCount(2, $commit_statuses);
+        $this->assertSame(1528892466, $commit_statuses[0]->getDate()->getTimestamp());
+        $this->assertSame(CommitStatusWithKnownStatus::STATUS_SUCCESS_NAME, $commit_statuses[0]->getStatusName());
+        $this->assertSame(1528898888, $commit_statuses[1]->getDate()->getTimestamp());
+        $this->assertSame(CommitStatusWithKnownStatus::STATUS_FAILURE_NAME, $commit_statuses[1]->getStatusName());
     }
 }