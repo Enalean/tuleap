ed112c6bd033c4dafa53323aa6d271f18aecdd88
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-10-03 08:56:01 +0200

    request #12349: Use an autoload generated by Composer for the archivedeleteditems plugin
    Change-Id: I1236f7ca4cd4db16c187be01f4044df5ce038c92

--- plugins/archivedeleteditems/include/archivedeleteditemsPlugin.class.php
+++ plugins/archivedeleteditems/include/archivedeleteditemsPlugin.class.php
@@ -19,7 +19,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Tuleap\ArchiveDeletedItems\ArchiveLogger;
 use Tuleap\ArchiveDeletedItems\FileCopier;
