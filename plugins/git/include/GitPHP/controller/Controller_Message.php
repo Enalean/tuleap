2c55b9994cdfdd13acb4b569960d64a456950b72
Nicolas Terray <nicolas.terray@enalean.com>
2018-10-04 12:03:44 +0200

    Convert Message to burning parrot
    Part of story #10416: burning parrot gloss on gitphp views
    
    Search againt commits.
    -> if no commit matches, then you have an empty state
    -> if you hack the url and submit and invalid search:
    --> s=a => error message "You must enter search text of at least 2 characters"
    --> st=toto => error message "Invalid search type"
    
    Change-Id: I60f2d8025ea5becb55a17b50f7c783435dae9579

--- plugins/git/include/GitPHP/controller/Controller_Message.php
+++ plugins/git/include/GitPHP/controller/Controller_Message.php
@@ -1,18 +1,26 @@
 <?php
-
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (C) 2010 Christopher Han <xiphux@gmail.com>
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace Tuleap\Git\GitPHP;
 
-/**
- * GitPHP Controller Message
- *
- * Controller for displaying a message page
- *
- * @author Christopher Han <xiphux@gmail.com>
- * @copyright Copyright (c) 2010 Christopher Han
- * @package GitPHP
- * @subpackage Controller
- */
 /**
  * Message controller class
  *
@@ -47,6 +55,9 @@ class Controller_Message extends ControllerBase // @codingStandardsIgnoreLine
      */
     protected function GetTemplate() // @codingStandardsIgnoreLine
     {
+        if (\ForgeConfig::get('git_repository_bp')) {
+            return 'tuleap/message.tpl';
+        }
         return 'message.tpl';
     }
 
