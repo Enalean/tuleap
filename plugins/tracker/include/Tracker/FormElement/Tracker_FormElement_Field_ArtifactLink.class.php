Enalean/tuleap

commit d61948c7fce4229105b1a74791aae5b756a1d22c

Render only once the artifact renderer table in an editable artifact
Currently the table is rendered twice: once for the read only section
and once for the editable section. Rendering twice this table cost
resources and it makes the DOM heavier.

This is part of request #12245: Displaying an artifact with a lot of artifact links is slow

Change-Id: I846489a2878b20181ecb7b73a54a1117d776281a

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -509,6 +509,10 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
     ) {
         $current_user = $this->getCurrentUser();
         $html = '';
+        if (! $read_only) {
+            $html = '<div class="tracker_formelement_read_and_edit">';
+        }
+
 
         if ($reverse_artifact_links) {
             $html .= '<div class="artifact-link-value-reverse">';
@@ -532,11 +536,9 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
         $html .= '<h5 class="artifack_link_subtitle">'.$this->getWidgetTitle($reverse_artifact_links).'</h5>';
 
         $html_name_new = '';
-        $html_name_del = '';
 
         if ($name) {
             $html_name_new = 'name="'. $name .'[new_values]"';
-            $html_name_del = 'name="'. $name .'[removed_values]';
         }
 
         $hp              = Codendi_HTMLPurifier::instance();
@@ -544,6 +546,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
 
         if (! $read_only) {
             $read_only_class = '';
+            $html .= '<div class="tracker_formelement_read_and_edit_edition_section">';
             $html .= '<div><span class="input-append" style="display:inline;"><input type="text"
                              '. $html_name_new .'
                              class="tracker-form-element-artifactlink-new"
@@ -580,6 +583,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
                 $can_create   = true;
                 $html .= $this->fetchParentSelector($prefill_parent, $name, $parent_tracker, $current_user, $can_create);
             }
+            $html .= '</div>';
         }
 
         $html .= '<div class="tracker-form-element-artifactlink-list '.$read_only_class.'">';
@@ -978,12 +982,8 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
 
     public function fetchArtifactValueWithEditionFormIfEditable(Tracker_Artifact $artifact, Tracker_Artifact_ChangesetValue $value = null, $submitted_values = array())
     {
-        $artifact_links_to_render = $this->getArtifactLinksToRenderFromChangesetValue($value);
-
-        $hidden_artifact_value = "<div class='tracker_hidden_edition_field' data-field-id=" . $this->getId() . ">" .
-            $this->fetchLinks($artifact, $artifact_links_to_render, $submitted_values) .
-            "</div>";
-        return $hidden_artifact_value . $this->fetchArtifactValueReadOnly($artifact, $value, $artifact_links_to_render) ;
+        return $this->fetchArtifactValue($artifact, $value, $submitted_values) .
+            "<div class='tracker_hidden_edition_field' data-field-id=" . $this->getId() . '></div>';
     }
 
     private function fetchLinksReadOnly(Tracker_Artifact $artifact, ArtifactLinksToRender $artifact_links_to_render)

plugins/artifactsfolders/include/Folder/ArtifactLinkInformationPrepender.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -50,30 +50,28 @@ class ArtifactLinkInformationPrepender
         $read_only
     ) {
         if ($reverse_artifact_links) {
-            return;
+            return '';
         }
 
         $value            = false;
         $folder_hierarchy = $this->hierarchy_builder->getHierarchyOfFolderForArtifact($artifact);
-        if ($read_only) {
-            if ($folder_hierarchy) {
-                $value = $this->fetchLinkToFolder($folder_hierarchy);
-            }
-        } else {
+
+        if (empty($folder_hierarchy)) {
+            return '';
+        }
+        if ($folder_hierarchy) {
+            $value = '<div class="tracker_formelement_read_and_edit_read_section">' . $this->fetchLinkToFolder($folder_hierarchy) . '</div>';
+        }
+        if (! $read_only) {
             $current_folder = null;
             if ($folder_hierarchy) {
                 $current_folder = end($folder_hierarchy);
             }
-            $value = $this->fetchSelectBox($artifact, $current_user, $current_folder);
+            $value .= '<div class="tracker_formelement_read_and_edit_edition_section">' .
+                $this->fetchSelectBox($artifact, $current_user, $current_folder) . '</div>';
         }
 
-        if (! $value) {
-            return;
-        }
-
-        $current_folder = $GLOBALS['Language']->getText('plugin_folders', 'current_folder');
-
-        return '<p>' . $current_folder . ' ' . $value . '</p>';
+        return '<p>' . $GLOBALS['Language']->getText('plugin_folders', 'current_folder') . ' ' . $value . '</p>';
     }
 
     private function fetchLinkToFolder(array $folder_hierarchy)

plugins/tracker/include/Tracker/Report/Tracker_Report_Renderer_Table.class.php
@@ -1152,12 +1152,11 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                         }
                     }
                     if (isset($matching_ids['nature'])) {
-                        $artifact_id = $row['id'];
-                        $nature = $matching_ids['nature'][$artifact_id];
-                        if ($read_only) {
-                            $forward_label = Codendi_HTMLPurifier::instance()->purify($nature->forward_label);
-                            $html .= "<td>$forward_label</td>";
-                        } else {
+                        $artifact_id   = $row['id'];
+                        $nature        = $matching_ids['nature'][$artifact_id];
+                        $forward_label = Codendi_HTMLPurifier::instance()->purify($nature->forward_label);
+                        $html         .= '<td class="tracker_formelement_read_and_edit_read_section">' . $forward_label . '</td>';
+                        if (! $read_only) {
                             $project = $this->report->getTracker()->getProject();
                             $natures = $this->getAllUsableTypesInProjectWithCache($project);
                             $natures_presenter = array();
@@ -1172,8 +1171,9 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                                     'is_selected'   => ($selected_nature == $nature_i->shortname)
                                 );
                             }
+
                             $name = "artifact[{$artifactlink_field_id}][natures][{$row['id']}]";
-                            $html .= '<td>';
+                            $html .= '<td class="tracker_formelement_read_and_edit_edition_section">';
                             $html .= $renderer->renderToString(
                                 'artifactlink-nature-selector',
                                 new NatureSelectorPresenter($natures_presenter, $name, '')

plugins/tracker/www/scripts/TrackerArtifactEditionSwitcher.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Enalean, 2014-2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2014-2018. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -298,6 +298,9 @@ tuleap.tracker.artifact = tuleap.tracker.artifact || {};
                 .find(".tracker_hidden_edition_field")
                 .show();
             $(element)
+                .find(".tracker_formelement_read_and_edit_edition_section")
+                .show();
+            $(element)
                 .find(".auto-computed-label")
                 .hide();
             $(element)
@@ -358,9 +361,12 @@ tuleap.tracker.artifact = tuleap.tracker.artifact || {};
         var removeReadOnlyElements = function(element) {
             $(element)
                 .children(
-                    ":not(.tracker_formelement_label,.tracker_hidden_edition_field, .artifact-link-value-reverse, .tracker_formelement_edit, .auto-computed-label,.back-to-autocompute)"
+                    ":not(.tracker_formelement_label,.tracker_hidden_edition_field, .tracker_formelement_read_and_edit, .artifact-link-value-reverse, .tracker_formelement_edit, .auto-computed-label,.back-to-autocompute)"
                 )
                 .remove();
+            $(element)
+                .find(".tracker_formelement_read_and_edit_read_section")
+                .hide();
         };
 
         var removeUnwrappedText = function(element) {

plugins/tracker/www/themes/default/css/style.scss
@@ -1402,6 +1402,10 @@ div.tracker-admin-field-controls {
         }
     }
 
+    .tracker_formelement_read_and_edit_edition_section {
+        display: none;
+    }
+
     &.in-edition {
         .tracker_formelement_edit {
             display: none;

commit 7c50f5a82716d1fe3575311116320d5effa274c4

Fields after an artifact link field in a fieldset are not editable anymore
Issue has been introduced by d61948c7fce4229105b1a74791aae5b756a1d22c.

This is part of request #12245: Displaying an artifact with a lot of artifact links is slow

Change-Id: Id7aea6adabedc44ba94e54b044eba768ed8b207e

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -583,7 +583,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
                 $can_create   = true;
                 $html .= $this->fetchParentSelector($prefill_parent, $name, $parent_tracker, $current_user, $can_create);
             }
-            $html .= '</div>';
+            $html .= '</div></div>';
         }
 
         $html .= '<div class="tracker-form-element-artifactlink-list '.$read_only_class.'">';