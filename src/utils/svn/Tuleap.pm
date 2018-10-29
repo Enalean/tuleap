Enalean/tuleap

commit 26cbef9a769815c23a2503826e137a1ef5067cb9

Block acces to SVN plugin and core when project is suspended
part of story #12160 have a clean way to suspend a project

How to test:
 - redeploy Tuleap.pm and reload httpd
 - you can't perform any action on SVN plugin and core (checkout/commit) for suspended project

 - Change project status
   => new system event are created
   => httpd is reloaded at system event execution
   => svn core and plugin avaible depending on project status

This patch does not deal with redis cache, if redis is started changes won't apply.

Change-Id: I3d1e3ed7db7a8b0fd7d8dcaf97855f6d77d72336

src/utils/svn/Tuleap.pm
@@ -236,7 +236,7 @@ sub apr_is_user_allowed {
     my $tuleap_username = get_tuleap_username($cfg, $dbh, $username);
 
     my $is_user_authenticated = 0;
-    if (user_authorization($dbh, $project_id, $tuleap_username) &&
+    if (user_authorization($r, $dbh, $project_id, $tuleap_username) &&
         user_authentication($r, $cfg, $dbh, $username, $user_secret, $tuleap_username)) {
         apr_add_user_to_cache($cfg, $username, $user_secret);
         $is_user_authenticated = 1;
@@ -354,7 +354,7 @@ sub redis_user_authorization() {
     my $dbh = DBI->connect($cfg->{TuleapDSN}, $cfg->{TuleapDbUser}, $cfg->{TuleapDbPass}, { AutoCommit => 0 });
     my $tuleap_username = get_tuleap_username($cfg, $dbh, $username);
 
-    my $user_is_authorized_for_project = user_authorization($dbh, $project_id, $tuleap_username);
+    my $user_is_authorized_for_project = user_authorization($log, $dbh, $project_id, $tuleap_username);
     $redis->setex($cache_key, $cfg->{TuleapCacheLifetime}, $user_is_authorized_for_project);
 
     $dbh->disconnect();
@@ -404,9 +404,9 @@ sub get_tuleap_username() {
 }
 
 sub user_authorization() {
-    my ($dbh, $project_id, $tuleap_username) = @_;
+    my ($r, $dbh, $project_id, $tuleap_username) = @_;
 
-    if (! $tuleap_username || ! can_user_access_project($dbh, $project_id, $tuleap_username)) {
+    if (! $tuleap_username || ! can_user_access_project($r, $dbh, $project_id, $tuleap_username)) {
         return 0;
     }
 
@@ -485,7 +485,7 @@ sub update_user_token_usage {
 }
 
 sub can_user_access_project {
-    my ($dbh, $project_id, $username) = @_;
+    my ($r, $dbh, $project_id, $username) = @_;
 
     my $query = << 'EOF';
     SELECT NULL
@@ -495,8 +495,12 @@ sub can_user_access_project {
     SELECT NULL
     FROM user
     JOIN user_group ON user_group.user_id=user.user_id
+    JOIN groups ON user_group.group_id = groups.group_id
+        AND groups.status = 'A'
     WHERE user.status='R' AND user_name=? AND user_group.group_id=?;
 EOF
+
+    $r->log->notice("[Tuleap.pm][apr] Operation aborted, project is not active");
 
     my $statement = $dbh->prepare($query);
     $statement->bind_param(1, $username, SQL_VARCHAR);

src/common/system_event/SystemEvent.class.php
@@ -41,6 +41,9 @@ abstract class SystemEvent {
     const TYPE_EDIT_SSH_KEYS         = "EDIT_SSH_KEYS";
     const TYPE_PROJECT_CREATE        = "PROJECT_CREATE";
     const TYPE_PROJECT_DELETE        = "PROJECT_DELETE";
+    const TYPE_PROJECT_ACTIVE        = "PROJECT_ACTIVE";
+    const TYPE_PROJECT_SUSPENDED     = "PROJECT_SUSPENDED";
+    const TYPE_PROJECT_PENDING       = "PROJECT_PENDING  ";
     const TYPE_PROJECT_RENAME        = "PROJECT_RENAME";
     const TYPE_UGROUP_MODIFY         = "UGROUP_MODIFY";
     const TYPE_USER_CREATE           = "USER_CREATE";
@@ -54,7 +57,7 @@ abstract class SystemEvent {
     const TYPE_PROJECT_IS_PRIVATE    = "PROJECT_IS_PRIVATE";
     const TYPE_MAILING_LIST_CREATE   = "MAILING_LIST_CREATE";
     const TYPE_MAILING_LIST_DELETE   = "MAILING_LIST_DELETE";
-    const TYPE_SERVICE_USAGE_SWITCH  = "SERVICE_USAGE_SWITCH";    
+    const TYPE_SERVICE_USAGE_SWITCH  = "SERVICE_USAGE_SWITCH";
     const TYPE_ROOT_DAILY            = "ROOT_DAILY";
     const TYPE_COMPUTE_MD5SUM        = "COMPUTE_MD5SUM";
     const TYPE_MASSMAIL              = "MASSMAIL";

src/common/system_event/SystemEventManager.class.php
@@ -19,6 +19,11 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Svn\ApacheConfGenerator;
+use Tuleap\System\ApacheServiceControl;
+use Tuleap\System\ServiceControl;
+use Tuleap\SystemEvent\SystemEvent_PROJECT_ACTIVE;
+
 /**
 * Manager of system events
 *
@@ -49,6 +54,9 @@ class SystemEventManager {
             Event::UPDATE_ALIASES,
             'approve_pending_project',
             'project_is_deleted',
+            'project_is_active',
+            'project_is_suspended',
+            'project_is_pending',
             'project_admin_add_user',
             'project_admin_remove_user',
             'project_admin_activate_user',
@@ -165,6 +173,27 @@ class SystemEventManager {
             $this->createEvent(SystemEvent::TYPE_PROJECT_DELETE,
                                $params['group_id'],
                                SystemEvent::PRIORITY_LOW);
+            break;
+        case 'project_is_active':
+            $this->createEvent(
+                SystemEvent::TYPE_PROJECT_ACTIVE,
+                $params['group_id'],
+                SystemEvent::PRIORITY_LOW
+            );
+            break;
+        case 'project_is_suspended':
+            $this->createEvent(
+                SystemEvent::TYPE_PROJECT_SUSPENDED,
+                $params['group_id'],
+                SystemEvent::PRIORITY_LOW
+            );
+            break;
+        case 'project_is_pending':
+            $this->createEvent(
+                SystemEvent::TYPE_PROJECT_PENDING,
+                $params['group_id'],
+                SystemEvent::PRIORITY_LOW
+            );
             break;
         case Event::PROJECT_RENAME:
             $this->createEvent(SystemEvent::TYPE_PROJECT_RENAME,
@@ -351,6 +380,18 @@ class SystemEventManager {
             return 'Tuleap\SystemEvent\Massmail';
         }
 
+        if ($type === SystemEvent::TYPE_PROJECT_ACTIVE) {
+            return SystemEvent_PROJECT_ACTIVE::class;
+        }
+
+        if ($type === SystemEvent::TYPE_PROJECT_SUSPENDED) {
+            return \Tuleap\SystemEvent\SystemEvent_PROJECT_SUSPENDED::class;
+        }
+
+        if ($type === SystemEvent::TYPE_PROJECT_PENDING) {
+            return \Tuleap\SystemEvent\SystemEvent_PROJECT_PENDING::class;
+        }
+
         return 'SystemEvent_' . $type;
     }
 
@@ -392,7 +433,8 @@ class SystemEventManager {
      *
      * @return SystemEvent
      */
-    public function getInstanceFromRow($row) {
+    public function getInstanceFromRow($row)
+    {
         $em           = EventManager::instance();
         $sysevent     = null;
         $klass        = null;
@@ -419,13 +461,23 @@ class SystemEventManager {
         case SystemEvent::TYPE_MASSMAIL:
             $klass = $this->getClassForType($row['type']);
             break;
-
         case SystemEvent::TYPE_SVN_UPDATE_HOOKS:
         case SystemEvent::TYPE_SVN_AUTHORIZE_TOKENS:
         case SystemEvent::TYPE_SVN_REVOKE_TOKENS:
         case SystemEvent::TYPE_SVN_AUTH_CACHE_CHANGE:
             $klass = $this->getClassForType($row['type']);
             $klass_params = array(Backend::instance(Backend::SVN));
+            break;
+        case SystemEvent::TYPE_PROJECT_ACTIVE:
+        case SystemEvent::TYPE_PROJECT_SUSPENDED:
+        case SystemEvent::TYPE_PROJECT_PENDING:
+            $klass        = $this->getClassForType($row['type']);
+            $klass_params = [
+                new ApacheConfGenerator(
+                    new ApacheServiceControl(new ServiceControl()),
+                    Backend::instance(Backend::SVN)
+                )
+            ];
             break;
 
         default:

src/common/system_event/include/SystemEvent_PROJECT_ACTIVE.php
@@ -1,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\SystemEvent;
+
+use System_Command_CommandException;
+use SystemEvent;
+use Tuleap\Svn\ApacheConfGenerator;
+
+class SystemEvent_PROJECT_ACTIVE extends SystemEvent // phpcs:ignore
+{
+    /**
+     * @var ApacheConfGenerator
+     */
+    private $apache_conf_generator;
+
+    public function injectDependencies(
+        ApacheConfGenerator $apache_conf_generator
+    ) {
+        $this->apache_conf_generator  = $apache_conf_generator;
+    }
+
+    /**
+     * Verbalize the parameters so they are readable and much user friendly in
+     * notifications
+     *
+     * @param bool $with_link true if you want links to entities. The returned
+     *                        string will be html instead of plain/text
+     *
+     * @return string
+     */
+    public function verbalizeParameters($with_link)
+    {
+        return 'project: ' . $this->verbalizeProjectId($this->getIdFromParam($this->parameters), $with_link);
+    }
+
+    public function process()
+    {
+        try {
+            $this->apache_conf_generator->generate();
+            $this->done();
+        } catch (System_Command_CommandException $e) {
+            $this->error($e->getMessage());
+        }
+    }
+}

src/common/system_event/include/SystemEvent_PROJECT_DELETE.class.php
@@ -1,29 +1,25 @@
 <?php
 /**
+ * Copyright (c) Tuleap 2018. All rights reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  *
  * 
  */
-
-require_once 'common/project/Project.class.php';
-require_once 'common/system_event/SystemEvent.class.php';
-require_once 'common/tracker/ArtifactTypeFactory.class.php';
-require_once('common/project/UGroupBinding.class.php');
 
 /**
 * System Event classes
@@ -51,7 +47,7 @@ class SystemEvent_PROJECT_DELETE extends SystemEvent {
      *
      * @return Boolean
      */
-    function process() {
+    public function process() {
         // Check parameters
         $groupId=$this->getIdFromParam($this->parameters);
 

src/common/system_event/include/SystemEvent_PROJECT_PENDING.php
@@ -1,0 +1,64 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\SystemEvent;
+
+use System_Command_CommandException;
+use SystemEvent;
+use Tuleap\Svn\ApacheConfGenerator;
+use Tuleap\System\ApacheServiceControl;
+
+class SystemEvent_PROJECT_PENDING extends SystemEvent // phpcs:ignore
+{
+    /**
+     * @var ApacheConfGenerator
+     */
+    private $apache_conf_generator;
+
+    public function injectDependencies(
+        ApacheConfGenerator $apache_conf_generator
+    ) {
+        $this->apache_conf_generator  = $apache_conf_generator;
+    }
+
+    /**
+     * Verbalize the parameters so they are readable and much user friendly in
+     * notifications
+     *
+     * @param bool $with_link true if you want links to entities. The returned
+     *                        string will be html instead of plain/text
+     *
+     * @return string
+     */
+    public function verbalizeParameters($with_link)
+    {
+        return 'project: ' . $this->verbalizeProjectId($this->getIdFromParam($this->parameters), $with_link);
+    }
+
+    public function process()
+    {
+        try {
+            $this->apache_conf_generator->generate();
+            $this->done();
+        } catch (System_Command_CommandException $e) {
+            $this->error($e->getMessage());
+        }
+    }
+}

src/common/system_event/include/SystemEvent_PROJECT_SUSPENDED.php
@@ -1,0 +1,64 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\SystemEvent;
+
+use System_Command_CommandException;
+use SystemEvent;
+use Tuleap\Svn\ApacheConfGenerator;
+use Tuleap\System\ApacheServiceControl;
+
+class SystemEvent_PROJECT_SUSPENDED extends SystemEvent // phpcs:ignore
+{
+    /**
+     * @var ApacheConfGenerator
+     */
+    private $apache_conf_generator;
+
+    public function injectDependencies(
+        ApacheConfGenerator $apache_conf_generator
+    ) {
+        $this->apache_conf_generator  = $apache_conf_generator;
+    }
+
+    /**
+     * Verbalize the parameters so they are readable and much user friendly in
+     * notifications
+     *
+     * @param bool $with_link true if you want links to entities. The returned
+     *                        string will be html instead of plain/text
+     *
+     * @return string
+     */
+    public function verbalizeParameters($with_link)
+    {
+        return 'project: ' . $this->verbalizeProjectId($this->getIdFromParam($this->parameters), $with_link);
+    }
+
+    public function process()
+    {
+        try {
+            $this->apache_conf_generator->generate();
+            $this->done();
+        } catch (System_Command_CommandException $e) {
+            $this->error($e->getMessage());
+        }
+    }
+}

commit 937cabf79a8b86e4749a1ba2de3d9d0d7306cde6

Reorganize SVN Redis cache to be able to revoke cache per project
User seen per project are now stored in a zset so that the cache of
a project can be invalidated. The implementation is based upon the
recommendation/trick mentionned here [0] since it is not possible
to set an expiration on a member of a set.

No functionnal changes are expected from this contribution, the
invalidation of the cache when a project has a status change will
done in a future contribution. You can however check it works as
expected by dropping the set of seen username per project and check
that you get a cache miss:

   DEL apache_svn_project_set_<project_id>

While this contribution does not add a new feature, the error introduced
by commit 26cbef9a769815c23a2503826e137a1ef5067cb9
("Can't locate object method 'log' via package 'Apache2::Log::Request'")
is fixed.

To test, you need to redeploy the Tuleap.pm file to
/usr/share/perl5/vendor_perl/Apache/Tuleap.pm and restart Apache.

This is part of story #12160: have a clean way to suspend a project

[0] https://github.com/antirez/redis/issues/135#issuecomment-2361996

Change-Id: Ib056f7c3ca1c37bdf39de8c1106ff78e248c9970

src/utils/svn/Tuleap.pm
@@ -343,10 +343,18 @@ sub redis_is_user_allowed {
 sub redis_user_authorization() {
     my ($log, $cfg, $redis, $username, $project_id) = @_;
 
-    my $cache_key = "apache_svn_".$username."_".$project_id;
-    my $cache     = $redis->get($cache_key);
-    if (defined $cache) {
-        return $cache;
+    my $current_timestamp = time();
+
+    my $cache_key             = "apache_svn_".$username. "_" . $project_id;
+    my $cache_set_key         = "apache_svn_project_set_" . $project_id;
+    my $cache_timestamp_added = $redis->zscore($cache_set_key, $username);
+    if (defined $cache_timestamp_added && ($cache_timestamp_added + $cfg->{TuleapCacheLifetime}) > $current_timestamp) {
+        my $cache = $redis->get($cache_key);
+        if (defined $cache) {
+            return $cache;
+        }
+    } elsif (defined $cache_timestamp_added) {
+        $redis->zremrangebyscore($cache_set_key, "-inf", $current_timestamp - $cfg->{TuleapCacheLifetime});
     }
 
     $log->notice("[Tuleap.pm][redis] cache miss authorization for $username in $project_id");
@@ -356,6 +364,7 @@ sub redis_user_authorization() {
 
     my $user_is_authorized_for_project = user_authorization($log, $dbh, $project_id, $tuleap_username);
     $redis->setex($cache_key, $cfg->{TuleapCacheLifetime}, $user_is_authorized_for_project);
+    $redis->zadd($cache_set_key, $current_timestamp, $username);
 
     $dbh->disconnect();
 
@@ -404,9 +413,9 @@ sub get_tuleap_username() {
 }
 
 sub user_authorization() {
-    my ($r, $dbh, $project_id, $tuleap_username) = @_;
+    my ($log, $dbh, $project_id, $tuleap_username) = @_;
 
-    if (! $tuleap_username || ! can_user_access_project($r, $dbh, $project_id, $tuleap_username)) {
+    if (! $tuleap_username || ! can_user_access_project($log, $dbh, $project_id, $tuleap_username)) {
         return 0;
     }
 
@@ -485,7 +494,7 @@ sub update_user_token_usage {
 }
 
 sub can_user_access_project {
-    my ($r, $dbh, $project_id, $username) = @_;
+    my ($log, $dbh, $project_id, $username) = @_;
 
     my $query = << 'EOF';
     SELECT NULL
@@ -499,9 +508,6 @@ sub can_user_access_project {
         AND groups.status = 'A'
     WHERE user.status='R' AND user_name=? AND user_group.group_id=?;
 EOF
-
-    $r->log->notice("[Tuleap.pm][apr] Operation aborted, project is not active");
-
     my $statement = $dbh->prepare($query);
     $statement->bind_param(1, $username, SQL_VARCHAR);
     $statement->bind_param(2, $username, SQL_VARCHAR);
@@ -512,6 +518,10 @@ EOF
 
     $statement->finish();
     undef $statement;
+
+    if (! $can_access) {
+        $log->debug("[Tuleap.pm] Access to project #$project_id has been denied to $username");
+    }
 
     return $can_access;
 }