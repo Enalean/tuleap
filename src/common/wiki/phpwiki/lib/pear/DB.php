Enalean/tuleap

commit 1980be1d25a630c2c7096a1eda7226f9af0ab938

request #11660: Replace the deprecated PHP4-style constructors
This contribution has mostly been generated automatically with
PHP-CS-Fixer and the rule no_php4_constructor.

Only manual changes in this contribution are in tests were
constructors are directly called.

Change-Id: I517413efe6c1374afeb8e9dd501e09c5bd0a68cc

src/common/wiki/phpwiki/lib/pear/DB.php
@@ -741,11 +741,11 @@ class DB_Error extends PEAR_Error
      *
      * @see PEAR_Error
      */
-    function DB_Error($code = DB_ERROR, $mode = PEAR_ERROR_RETURN,
+    function __construct($code = DB_ERROR, $mode = PEAR_ERROR_RETURN,
               $level = E_USER_NOTICE, $debuginfo = null)
     {
         if (is_int($code)) {
-            $this->PEAR_Error('DB Error: ' . DB::errorMessage($code), $code, $mode, $level, $debuginfo);
+            parent::__construct('DB Error: ' . DB::errorMessage($code), $code, $mode, $level, $debuginfo);
         } else {
             $this->PEAR_Error("DB Error: $code", DB_ERROR, $mode, $level, $debuginfo);
         }
@@ -793,7 +793,7 @@ class DB_result
      * @param resource $result  result resource id
      * @param array    $options assoc array with optional result options
      */
-    function DB_result(&$dbh, $result, $options = array())
+    function __construct(&$dbh, $result, $options = array())
     {
         $this->dbh = &$dbh;
         $this->result = $result;
@@ -1092,7 +1092,7 @@ class DB_row
      *
      * @param resource row data as array
      */
-    function DB_row(&$arr)
+    function __construct(&$arr)
     {
         foreach ($arr as $key => $value) {
             $this->$key = &$arr[$key];

commit 2967c39b9c6e5d6829397609b5e37c85174b7b6a

request #12468: PHPWiki DB errors should be creatable without PHP fatal errors
DB_Error object instantiation should work when $code is not an int.

Issue has been introduced by 1980be1d25a630c2c7096a1eda7226f9af0ab938.

Change-Id: I72093215d3789597eb7ac60e31c845d9c851aa8d

src/common/wiki/phpwiki/lib/pear/DB.php
@@ -460,7 +460,7 @@ class DB_Error extends PEAR_Error
         if (is_int($code)) {
             parent::__construct('DB Error: ' . DB::errorMessage($code), $code, $mode, $level, $debuginfo);
         } else {
-            $this->PEAR_Error("DB Error: $code", DB_ERROR, $mode, $level, $debuginfo);
+            parent::__construct("DB Error: $code", DB_ERROR, $mode, $level, $debuginfo);
         }
     }
     // }}}