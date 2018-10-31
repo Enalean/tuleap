Enalean/tuleap

commit 937cabf79a8b86e4749a1ba2de3d9d0d7306cde6

Reorganize SVN Redis cache to be able to revoke cache per project
User seen per project are now stored in a zset so that the cache of
a project can be invalidated. The implementation is based upon the
recommendation/trick mentionned here [0] since it is not possible
to set an expiration on a member of a set.

No functionnal changes are expected from this contribution, the
invalidation of the cache when a project has a status change will
done in a future contribution. You can however check it works as
expected by dropping the set of seen username per project and check
that you get a cache miss:

   DEL apache_svn_project_set_<project_id>

While this contribution does not add a new feature, the error introduced
by commit 26cbef9a769815c23a2503826e137a1ef5067cb9
("Can't locate object method 'log' via package 'Apache2::Log::Request'")
is fixed.

To test, you need to redeploy the Tuleap.pm file to
/usr/share/perl5/vendor_perl/Apache/Tuleap.pm and restart Apache.

This is part of story #12160: have a clean way to suspend a project

[0] https://github.com/antirez/redis/issues/135#issuecomment-2361996

Change-Id: Ib056f7c3ca1c37bdf39de8c1106ff78e248c9970

src/utils/svn/Tuleap.pm
@@ -343,10 +343,18 @@ sub redis_is_user_allowed {
 sub redis_user_authorization() {
     my ($log, $cfg, $redis, $username, $project_id) = @_;
 
-    my $cache_key = "apache_svn_".$username."_".$project_id;
-    my $cache     = $redis->get($cache_key);
-    if (defined $cache) {
-        return $cache;
+    my $current_timestamp = time();
+
+    my $cache_key             = "apache_svn_".$username. "_" . $project_id;
+    my $cache_set_key         = "apache_svn_project_set_" . $project_id;
+    my $cache_timestamp_added = $redis->zscore($cache_set_key, $username);
+    if (defined $cache_timestamp_added && ($cache_timestamp_added + $cfg->{TuleapCacheLifetime}) > $current_timestamp) {
+        my $cache = $redis->get($cache_key);
+        if (defined $cache) {
+            return $cache;
+        }
+    } elsif (defined $cache_timestamp_added) {
+        $redis->zremrangebyscore($cache_set_key, "-inf", $current_timestamp - $cfg->{TuleapCacheLifetime});
     }
 
     $log->notice("[Tuleap.pm][redis] cache miss authorization for $username in $project_id");
@@ -356,6 +364,7 @@ sub redis_user_authorization() {
 
     my $user_is_authorized_for_project = user_authorization($log, $dbh, $project_id, $tuleap_username);
     $redis->setex($cache_key, $cfg->{TuleapCacheLifetime}, $user_is_authorized_for_project);
+    $redis->zadd($cache_set_key, $current_timestamp, $username);
 
     $dbh->disconnect();
 
@@ -404,9 +413,9 @@ sub get_tuleap_username() {
 }
 
 sub user_authorization() {
-    my ($r, $dbh, $project_id, $tuleap_username) = @_;
+    my ($log, $dbh, $project_id, $tuleap_username) = @_;
 
-    if (! $tuleap_username || ! can_user_access_project($r, $dbh, $project_id, $tuleap_username)) {
+    if (! $tuleap_username || ! can_user_access_project($log, $dbh, $project_id, $tuleap_username)) {
         return 0;
     }
 
@@ -485,7 +494,7 @@ sub update_user_token_usage {
 }
 
 sub can_user_access_project {
-    my ($r, $dbh, $project_id, $username) = @_;
+    my ($log, $dbh, $project_id, $username) = @_;
 
     my $query = << 'EOF';
     SELECT NULL
@@ -499,9 +508,6 @@ sub can_user_access_project {
         AND groups.status = 'A'
     WHERE user.status='R' AND user_name=? AND user_group.group_id=?;
 EOF
-
-    $r->log->notice("[Tuleap.pm][apr] Operation aborted, project is not active");
-
     my $statement = $dbh->prepare($query);
     $statement->bind_param(1, $username, SQL_VARCHAR);
     $statement->bind_param(2, $username, SQL_VARCHAR);
@@ -512,6 +518,10 @@ EOF
 
     $statement->finish();
     undef $statement;
+
+    if (! $can_access) {
+        $log->debug("[Tuleap.pm] Access to project #$project_id has been denied to $username");
+    }
 
     return $can_access;
 }

commit ace68fd002dcdc6db5aa478d3d97523b2885be9c

Server errors when doing SVN operations and using the APR authentication cache
All SVN accesses ends up with a server error when the APR cache is used.
The issue has been caused by commit introduced by
937cabf79a8b86e4749a1ba2de3d9d0d7306cde6 while fixing the issue introduced
by 26cbef9a769815c23a2503826e137a1ef5067cb9.

To test, you need to redeploy the Tuleap.pm file to
/usr/share/perl5/vendor_perl/Apache/Tuleap.pm and restart Apache.

This is part of story #12160: have a clean way to suspend a project

Change-Id: I65044fb977466abb994445864946648288d904eb

src/utils/svn/Tuleap.pm
@@ -236,7 +236,7 @@ sub apr_is_user_allowed {
     my $tuleap_username = get_tuleap_username($cfg, $dbh, $username);
 
     my $is_user_authenticated = 0;
-    if (user_authorization($r, $dbh, $project_id, $tuleap_username) &&
+    if (user_authorization($r->log, $dbh, $project_id, $tuleap_username) &&
         user_authentication($r, $cfg, $dbh, $username, $user_secret, $tuleap_username)) {
         apr_add_user_to_cache($cfg, $username, $user_secret);
         $is_user_authenticated = 1;