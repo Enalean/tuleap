ac8bd3738c2634a72764150aa904755840af0984
Thomas Gerbet <thomas.gerbet@enalean.com>
2018-10-31 16:12:21 +0100

    request #12477: Use an autoload generated by Composer for the Bugzilla reference plugin
    Change-Id: I7e004a8772e4d73741f7d01c4f01c5e4969d2752

--- plugins/bugzilla_reference/include/bugzilla_referencePlugin.class.php
+++ plugins/bugzilla_reference/include/bugzilla_referencePlugin.class.php
@@ -34,7 +34,7 @@ use Tuleap\reference\ReferenceValidator;
 use Tuleap\reference\ReservedKeywordsRetriever;
 
 require_once __DIR__ . '/constants.php';
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 class bugzilla_referencePlugin extends Plugin
 {
