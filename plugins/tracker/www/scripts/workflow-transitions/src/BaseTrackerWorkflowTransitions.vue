02722358cb3b67654af246251039ac993a525b32
Thi Mai <thimai@sogilis.com>
2018-11-14 11:08:39 +0100

    Have a minimal vue empty state
    Part of story #12187 configure workflow pre & post actions at target
    state level
    
    How to test: go to '/plugins/tracker/workflow/{id}/transitions'.
    Stands for: an empty page with title and message text "Page under construction"
    
    Change-Id: Iadd8dc7cadff2542f790d1aaa5029e0c828bddca

---
+++ plugins/tracker/www/scripts/workflow-transitions/src/BaseTrackerWorkflowTransitions.vue
@@ -0,0 +1,36 @@
+<!--
+  - Copyright (c) Enalean, 2018. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <section class="tlp-pane">
+        <div class="tlp-pane-container">
+            <div class="tlp-pane-header">
+                <h1 class="tlp-pane-title" v-translate>Transitions rules configuration</h1>
+            </div>
+            <section class="tlp-pane-section empty-page-text">
+                <span v-translate>Page under construction</span>
+            </section>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    name: "BaseTrackerWorflowTransitions"
+};
+</script>
---
+++ plugins/tracker/www/scripts/workflow-transitions/src/index.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright Enalean (c) 2018. All rights reserved.
+ *
+ * Tuleap and Enalean names and logos are registrated trademarks owned by
+ * Enalean SAS. All other trademarks or names are properties of their respective
+ * owners.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Vue from "vue";
+import GettextPlugin from "vue-gettext";
+import french_translations from "../po/fr.po";
+import BaseTrackerWorkflowTransitions from "./BaseTrackerWorkflowTransitions.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const vue_mount_point = document.getElementById("tracker-workflow-transitions");
+
+    if (!vue_mount_point) {
+        return;
+    }
+
+    Vue.use(GettextPlugin, {
+        translations: {
+            fr: french_translations.messages
+        },
+        silent: true
+    });
+
+    const locale = document.body.dataset.userLocale;
+    Vue.config.language = locale;
+
+    const RootComponent = Vue.extend(BaseTrackerWorkflowTransitions);
+    new RootComponent().$mount(vue_mount_point);
+});
