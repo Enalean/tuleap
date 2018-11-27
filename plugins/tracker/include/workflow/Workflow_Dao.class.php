013f36ecfa47bbccda4aaaf57c554b29eb25763c
Jibidus <jean-baptiste@sogilis.com>
2018-11-27 14:36:38 +0100

    Fix Workflow_Dao styling
    Part of story #12187 configure workflow pre & post actions at target
    state level
    
    Change-Id: I518582a5fc4ecacb0e95c37bcb404ab0b6100fcf

--- plugins/tracker/include/workflow/Workflow_Dao.class.php
+++ plugins/tracker/include/workflow/Workflow_Dao.class.php
@@ -17,68 +17,78 @@
  * You should have received a copy of the GNU General Public License
  * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 require_once('common/dao/include/DataAccessObject.class.php');
 
-class Workflow_Dao extends DataAccessObject {
-    public function __construct($da = null) {
+class Workflow_Dao extends DataAccessObject
+{
+    public function __construct($da = null)
+    {
         parent::__construct($da);
         $this->table_name = 'tracker_workflow';
     }
-    
-    public function create($tracker_id, $field_id) {
+
+    public function create($tracker_id, $field_id)
+    {
         $tracker_id = $this->da->escapeInt($tracker_id);
-        $field_id    = $this->da->escapeInt($field_id);
+        $field_id = $this->da->escapeInt($field_id);
         $sql = "INSERT INTO $this->table_name (tracker_id, field_id)
                 VALUES ($tracker_id, $field_id)";
         return $this->updateAndGetLastId($sql);
     }
-    
-    public function searchById($workflow_id) {
+
+    public function searchById($workflow_id)
+    {
         $workflow_id = $this->da->escapeInt($workflow_id);
         $sql = "SELECT * 
                 FROM $this->table_name
                 WHERE workflow_id = $workflow_id";
         return $this->retrieve($sql);
     }
-    
-    public function searchByTrackerId($tracker_id) {
+
+    public function searchByTrackerId($tracker_id)
+    {
         $tracker_id = $this->da->escapeInt($tracker_id);
         $sql = "SELECT * 
                 FROM $this->table_name
                 WHERE tracker_id = $tracker_id";
         return $this->retrieve($sql);
     }
-    
-    public function updateActivation($workflow_id, $is_used) {
+
+    public function updateActivation($workflow_id, $is_used)
+    {
         $workflow_id = $this->da->escapeInt($workflow_id);
         $is_used = $this->da->escapeInt($is_used);
         $sql = " UPDATE $this->table_name SET is_used=$is_used WHERE workflow_id=$workflow_id";
-        return $this->update($sql);        
+        return $this->update($sql);
     }
-    
-    public function delete($workflow_id) {
+
+    public function delete($workflow_id)
+    {
         $workflow_id = $this->da->escapeInt($workflow_id);
         $sql = " DELETE FROM $this->table_name WHERE workflow_id=$workflow_id";
         return $this->update($sql);
     }
-    
-    public function duplicate($to_tracker_id, $from_id, $to_id, $values, $is_used) {
+
+    public function duplicate($to_tracker_id, $from_id, $to_id, $values, $is_used)
+    {
         $to_tracker_id = $this->da->escapeInt($to_tracker_id);
-        $to_id    = $this->da->escapeInt($to_id);
+        $to_id = $this->da->escapeInt($to_id);
         $is_used = $this->da->escapeInt($is_used);
         $sql = "INSERT INTO $this->table_name (tracker_id, field_id, is_used)
                 VALUES ($to_tracker_id, $to_id, $is_used)";
         return $this->updateAndGetLastId($sql);
     }
-    
-    public function save($tracker_id, $field_id, $is_used) {
+
+    public function save($tracker_id, $field_id, $is_used)
+    {
         $tracker_id = $this->da->escapeInt($tracker_id);
-        $field_id    = $this->da->escapeInt($field_id);
-        $is_used    = $this->da->escapeInt($is_used);
+        $field_id = $this->da->escapeInt($field_id);
+        $is_used = $this->da->escapeInt($is_used);
         $sql = "INSERT INTO $this->table_name (tracker_id, field_id, is_used)
                 VALUES ($tracker_id, $field_id, $is_used)";
         return $this->updateAndGetLastId($sql);
     }
 }
+
 ?>
