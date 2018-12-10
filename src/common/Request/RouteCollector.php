Enalean/tuleap

commit 6931b41970868cfb9289b25257eedf24a069a758

request #12542 Change avatar mgmt lib + make them available everywhere
This avoids depending on a lib that is only shipped out of git master branch.

Change-Id: Ie7baed15f001a4a1b0c58cea3e7f69d4c09beb15

src/common/Request/RouteCollector.php
@@ -48,6 +48,7 @@ use Tuleap\Password\Configuration\PasswordConfigurationSaver;
 use Tuleap\Trove\TroveCatListController;
 use Tuleap\User\AccessKey\AccessKeyCreationController;
 use Tuleap\User\AccessKey\AccessKeyRevocationController;
+use Tuleap\User\Account\ChangeAvatarController;
 use Tuleap\User\Profile\AvatarController;
 use Tuleap\User\Profile\ProfileController;
 use Tuleap\User\Profile\ProfilePresenterBuilder;
@@ -134,6 +135,9 @@ class RouteCollector
             $r->post('/access_key/revoke', function () {
                 return new AccessKeyRevocationController();
             });
+        });
+        $r->post('/account/avatar', function () {
+            return new ChangeAvatarController(\UserManager::instance());
         });
 
         $r->addRoute(['GET'], '/users/{name}[/]', function () {

src/common/User/Account/ChangeAvatarController.php
@@ -1,0 +1,111 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Tuleap\User\Account;
+
+use CSRFSynchronizerToken;
+use Feedback;
+use Gumlet\ImageResize;
+use Gumlet\ImageResizeException;
+use HTTPRequest;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\ForbiddenException;
+use Tuleap\Request\NotFoundException;
+
+class ChangeAvatarController implements DispatchableWithRequest
+{
+    const AVATAR_MAX_SIZE = 100;
+
+    /**
+     * @var \UserManager
+     */
+    private $user_manager;
+
+    public function __construct(\UserManager $user_manager)
+    {
+        $this->user_manager = $user_manager;
+    }
+
+    /**
+     * Is able to process a request routed by FrontRouter
+     *
+     * @param HTTPRequest $request
+     * @param BaseLayout $layout
+     * @param array $variables
+     * @throws NotFoundException
+     * @throws ForbiddenException
+     * @return void
+     */
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    {
+        $user = $request->getCurrentUser();
+        if ($user->isAnonymous()) {
+            $layout->redirect('/');
+        }
+
+        $csrf = new CSRFSynchronizerToken('/account/index.php');
+        $csrf->check();
+
+        if ($request->get('use-default-avatar')) {
+            $user->setHasAvatar(false);
+            if ($this->user_manager->updateDb($user)) {
+                $layout->addFeedback(Feedback::INFO, $GLOBALS['Language']->getText('account_change_avatar', 'success'));
+            } else {
+                $layout->addFeedback(Feedback::ERROR, $GLOBALS['Language']->getText('account_change_avatar', 'error'));
+            }
+        } elseif (isset($_FILES['avatar'])) {
+            if ($_FILES['avatar']['error']) {
+                $GLOBALS['Response']->addFeedback(
+                    Feedback::ERROR,
+                    _('An error occured with your upload. Please try again or choose another image.')
+                );
+                $layout->redirect('/account');
+            }
+            try {
+                $image = new ImageResize($_FILES['avatar']['tmp_name']);
+                $image->quality_truecolor = false;
+                $image->crop(self::AVATAR_MAX_SIZE, self::AVATAR_MAX_SIZE);
+                // Replace transparent background by white color to avoid strange rendering in Tuleap.
+                $image->addFilter(function ($imageDesc) {
+                    $x = imagesx($imageDesc);
+                    $y = imagesy($imageDesc);
+                    $dst_im = imagecreatetruecolor($x, $y);
+                    $background_color = imagecolorallocate($dst_im, 255, 255, 255);
+                    imagefilledrectangle($dst_im, 0, 0, $x, $y, $background_color);
+
+                    imagecopy($dst_im, $imageDesc, 0, 0, 0, 0, $x, $y);
+
+                    imagecopy($imageDesc, $dst_im, 0, 0, 0, 0, $x, $y);
+
+                    imagedestroy($dst_im);
+                });
+                $image->save($user->getAvatarFilePath(), IMAGETYPE_PNG, 9, 0640);
+                $user->setHasAvatar();
+                $this->user_manager->updateDb($user);
+                $layout->addFeedback(Feedback::INFO, $GLOBALS['Language']->getText('account_change_avatar', 'success'));
+            } catch (ImageResizeException $exception) {
+                $layout->addFeedback(Feedback::ERROR, $exception->getMessage());
+            }
+        }
+        $layout->redirect('/account');
+    }
+}

plugins/tracker/include/Tracker/Artifact/Presenter/FollowUpCommentsPresenter.php
@@ -47,7 +47,7 @@ class Tracker_Artifact_Presenter_FollowUpCommentsPresenter {
             $diff_to_previous = $followup->diffToPrevious();
             $presenters[] = array(
                 'getId'              => $followup->getId(),
-                'getAvatarIfEnabled' => $followup->getAvatarIfEnabled(),
+                'getAvatar'          => $followup->getAvatar(),
                 'getUserLink'        => $followup->getUserLink(),
                 'getTimeAgo'         => $followup->getTimeAgo(),
                 'getFollowupContent' => $followup->getFollowupContent($diff_to_previous)

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_Changeset.class.php
@@ -224,7 +224,7 @@ class Tracker_Artifact_Changeset extends Tracker_Artifact_Followup_Item {
     public function fetchFollowUp($diff_to_previous) {
         $html = '';
 
-        $html .= $this->getAvatarIfEnabled();
+        $html .= $this->getAvatar();
 
         $html .= '<div class="tracker_artifact_followup_header">';
         $html .= $this->getPermalink();

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_Changeset_Comment.class.php
@@ -200,7 +200,7 @@ class Tracker_Artifact_Changeset_Comment {
         }
 
         $user     = UserManager::instance()->getUserById($this->submitted_by);
-        $avatar   = (ForgeConfig::get('sys_enable_avatars')) ? $user->fetchHtmlAvatar() : '';
+        $avatar   = $user->fetchHtmlAvatar();
         $timezone = ($user->getId() != 0) ? ' ('.$user->getTimezone().')' : '';
 
         $html =

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_Followup_Item.class.php
@@ -54,12 +54,9 @@ abstract class Tracker_Artifact_Followup_Item {
 
     abstract public function canHoldValue();
 
-    public function getAvatarIfEnabled() {
-        if (ForgeConfig::get('sys_enable_avatars')) {
-            return '<div class="tracker_artifact_followup_avatar">' . $this->getHTMLAvatar() . '</div>';
-        }
-
-        return '';
+    public function getAvatar()
+    {
+        return '<div class="tracker_artifact_followup_avatar">' . $this->getHTMLAvatar() . '</div>';
     }
 
     public function getPermalink() {

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_PriorityHistoryChange.class.php
@@ -152,7 +152,7 @@ class Tracker_Artifact_PriorityHistoryChange extends Tracker_Artifact_Followup_I
 
     public function fetchFollowUp($diff_to_previous) {
         $html  = '';
-        $html .= $this->getAvatarIfEnabled();
+        $html .= $this->getAvatar();
 
         $html .= '<div class="tracker_artifact_followup_header">';
         $html .= $this->getPermalink();

plugins/tracker/templates/follow-ups.mustache
@@ -2,7 +2,7 @@
     {{# followups }}
         <li id="followup_{{ getId }}">
             <div class="tracker_artifact_followup_header">
-                {{{ getAvatarIfEnabled }}}
+                {{{ getAvatar }}}
 
                 <div>
                     {{{ getUserLink }}}

commit d4621013a1a1ef3807b764844219ee0882345f9a

request #12608: Impossible to choose an avatar for a new user
The issue has been introduced by 6931b41970868cfb9289b25257eedf24a069a758.
The previous used to deal with the image resizing was creating the whole
folder hierarchy to save the outputted file, the new one does not.

Change-Id: I0fa0d6a5d0e90bab004665fd0b433ed0cb4d1965

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -146,12 +146,6 @@ msgstr ""
 "définitivement le changement."
 
 msgid ""
-"An error occured with your upload. Please try again or choose another image."
-msgstr ""
-"Une erreur est survenue lors de votre téléversement. Merci d'essayer à "
-"nouveau ou de choisir une autre image."
-
-msgid ""
 "An error occurred in form submission, a link is not valid. Please retry."
 msgstr ""
 "Une erreur est survenue, un lien n'est pas valide. Merci de recommencer."
@@ -182,6 +176,12 @@ msgid "An error occurred while trying to update permissions delegations."
 msgstr ""
 "Une erreur est survenue lors de la mise à jour des délégations de "
 "permissions."
+
+msgid ""
+"An error occurred with your upload. Please try again or choose another image."
+msgstr ""
+"Une erreur est survenue lors de votre téléversement. Merci d'essayer à "
+"nouveau ou de choisir une autre image."
 
 msgid "And more..."
 msgstr "Et plus ..."

site-content/tuleap-core.pot
@@ -118,10 +118,6 @@ msgid ""
 msgstr ""
 
 msgid ""
-"An error occured with your upload. Please try again or choose another image."
-msgstr ""
-
-msgid ""
 "An error occurred in form submission, a link is not valid. Please retry."
 msgstr ""
 
@@ -147,6 +143,10 @@ msgid "An error occurred while trying to remove the label."
 msgstr ""
 
 msgid "An error occurred while trying to update permissions delegations."
+msgstr ""
+
+msgid ""
+"An error occurred with your upload. Please try again or choose another image."
 msgstr ""
 
 msgid "And more..."

src/common/Request/RouteCollector.php
@@ -49,6 +49,7 @@ use Tuleap\Trove\TroveCatListController;
 use Tuleap\User\AccessKey\AccessKeyCreationController;
 use Tuleap\User\AccessKey\AccessKeyRevocationController;
 use Tuleap\User\Account\ChangeAvatarController;
+use Tuleap\User\Account\UserAvatarSaver;
 use Tuleap\User\Profile\AvatarController;
 use Tuleap\User\Profile\ProfileController;
 use Tuleap\User\Profile\ProfilePresenterBuilder;
@@ -137,7 +138,8 @@ class RouteCollector
             });
         });
         $r->post('/account/avatar', function () {
-            return new ChangeAvatarController(\UserManager::instance());
+            $user_manager = \UserManager::instance();
+            return new ChangeAvatarController($user_manager, new UserAvatarSaver($user_manager));
         });
 
         $r->addRoute(['GET'], '/users/{name}[/]', function () {

src/common/User/Account/ChangeAvatarController.php
@@ -23,7 +23,6 @@ namespace Tuleap\User\Account;
 
 use CSRFSynchronizerToken;
 use Feedback;
-use Gumlet\ImageResize;
 use Gumlet\ImageResizeException;
 use HTTPRequest;
 use Tuleap\Layout\BaseLayout;
@@ -33,16 +32,19 @@ use Tuleap\Request\NotFoundException;
 
 class ChangeAvatarController implements DispatchableWithRequest
 {
-    const AVATAR_MAX_SIZE = 100;
-
     /**
      * @var \UserManager
      */
     private $user_manager;
+    /**
+     * @var UserAvatarSaver
+     */
+    private $user_avatar_saver;
 
-    public function __construct(\UserManager $user_manager)
+    public function __construct(\UserManager $user_manager, UserAvatarSaver $user_avatar_saver)
     {
-        $this->user_manager = $user_manager;
+        $this->user_manager      = $user_manager;
+        $this->user_avatar_saver = $user_avatar_saver;
     }
 
     /**
@@ -76,31 +78,13 @@ class ChangeAvatarController implements DispatchableWithRequest
             if ($_FILES['avatar']['error']) {
                 $GLOBALS['Response']->addFeedback(
                     Feedback::ERROR,
-                    _('An error occured with your upload. Please try again or choose another image.')
+                    _('An error occurred with your upload. Please try again or choose another image.')
                 );
                 $layout->redirect('/account');
             }
             try {
-                $image = new ImageResize($_FILES['avatar']['tmp_name']);
-                $image->quality_truecolor = false;
-                $image->crop(self::AVATAR_MAX_SIZE, self::AVATAR_MAX_SIZE);
-                // Replace transparent background by white color to avoid strange rendering in Tuleap.
-                $image->addFilter(function ($imageDesc) {
-                    $x = imagesx($imageDesc);
-                    $y = imagesy($imageDesc);
-                    $dst_im = imagecreatetruecolor($x, $y);
-                    $background_color = imagecolorallocate($dst_im, 255, 255, 255);
-                    imagefilledrectangle($dst_im, 0, 0, $x, $y, $background_color);
+                $this->user_avatar_saver->saveAvatar($user, $_FILES['avatar']['tmp_name']);
 
-                    imagecopy($dst_im, $imageDesc, 0, 0, 0, 0, $x, $y);
-
-                    imagecopy($imageDesc, $dst_im, 0, 0, 0, 0, $x, $y);
-
-                    imagedestroy($dst_im);
-                });
-                $image->save($user->getAvatarFilePath(), IMAGETYPE_PNG, 9, 0640);
-                $user->setHasAvatar();
-                $this->user_manager->updateDb($user);
                 $layout->addFeedback(Feedback::INFO, $GLOBALS['Language']->getText('account_change_avatar', 'success'));
             } catch (ImageResizeException $exception) {
                 $layout->addFeedback(Feedback::ERROR, $exception->getMessage());

src/common/User/Account/UserAvatarSaver.php
@@ -1,0 +1,71 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\User\Account;
+
+use Gumlet\ImageResize;
+use Gumlet\ImageResizeException;
+
+class UserAvatarSaver
+{
+    const AVATAR_MAX_SIZE = 100;
+
+    /**
+     * @var \UserManager
+     */
+    private $user_manager;
+
+    public function __construct(\UserManager $user_manager)
+    {
+        $this->user_manager = $user_manager;
+    }
+
+    /**
+     * @throws ImageResizeException
+     */
+    public function saveAvatar(\PFUser $user, $temporary_path_avatar)
+    {
+        $image = new ImageResize($temporary_path_avatar);
+        $image->quality_truecolor = false;
+        $image->crop(self::AVATAR_MAX_SIZE, self::AVATAR_MAX_SIZE);
+        // Replace transparent background by white color to avoid strange rendering in Tuleap.
+        $image->addFilter(function ($imageDesc) {
+            $x                = imagesx($imageDesc);
+            $y                = imagesy($imageDesc);
+            $dst_im           = imagecreatetruecolor($x, $y);
+            $background_color = imagecolorallocate($dst_im, 255, 255, 255);
+            imagefilledrectangle($dst_im, 0, 0, $x, $y, $background_color);
+
+            imagecopy($dst_im, $imageDesc, 0, 0, 0, 0, $x, $y);
+
+            imagecopy($imageDesc, $dst_im, 0, 0, 0, 0, $x, $y);
+
+            imagedestroy($dst_im);
+        });
+        $avatar_path   = $user->getAvatarFilePath();
+        $avatar_folder = dirname($avatar_path);
+        if (! is_dir($avatar_folder) && ! mkdir($avatar_folder, 0777, true) && ! is_dir($avatar_folder)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $avatar_folder));
+        }
+        $image->save($user->getAvatarFilePath(), IMAGETYPE_PNG, 9, 0640);
+        $user->setHasAvatar();
+        $this->user_manager->updateDb($user);
+    }
+}

tests/phpunit/common/User/Account/UserAvatarSaverTest.php
@@ -1,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\User\Account;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class UserAvatarSaverTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    const MINIMAL_PNG_BASE64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==';
+
+    public function testANewAvatarCanBeUploaded()
+    {
+        $filesystem   = vfsStream::setup();
+        $user_manager = \Mockery::mock(\UserManager::class);
+
+        $user_avatar_saver = new UserAvatarSaver($user_manager);
+
+        $user             = \Mockery::mock(\PFUser::class);
+        $avatar_file_path = $filesystem->url() . '/folder/user/avatar';
+        $user->shouldReceive('getAvatarFilePath')->andReturns($avatar_file_path);
+
+
+        $user->shouldReceive('setHasAvatar')->once();
+        $user_manager->shouldReceive('updateDb')->once();
+
+        $avatar_temporary_path = $filesystem->url() . '/avatar_tmp_upload';
+        file_put_contents($avatar_temporary_path, base64_decode(self::MINIMAL_PNG_BASE64));
+
+        $user_avatar_saver->saveAvatar($user, $avatar_temporary_path);
+
+        $this->assertFileExists($avatar_file_path);
+    }
+}