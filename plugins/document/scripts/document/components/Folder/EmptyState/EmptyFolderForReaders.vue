f76643b9f192ba530ba7c7cbc5a430c9939d78af
Nicolas Terray <nicolas.terray@enalean.com>
2018-12-13 11:24:38 +0100

    Different folder empty states for readers and writers
    Part of story #12447 Modal new document for empty/wiki/link
    
    Readers and writers should not have the same empty state on a folder.
    
    Styling is missing, a dedicated changeset will fix margins and stuff.
    
    Change-Id: Iacea59423d0a0fe356cc09281a116c849b4232bd

---
+++ plugins/document/scripts/document/components/Folder/EmptyState/EmptyFolderForReaders.vue
@@ -0,0 +1,68 @@
+<!--
+  - Copyright (c) Enalean, 2018. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="empty-pane">
+        <div class="empty-page-illustration">
+            <empty-folder-for-readers-svg/>
+        </div>
+        <div class="empty-page-text-with-small-text">
+            <translate>This folder is empty</translate>
+            <div class="empty-page-small-text" v-translate>or you don't have permissions to see its items</div>
+        </div>
+        <router-link v-bind:to="route_to"
+                     class="tlp-button-primary tlp-button-large"
+                     v-if="can_go_to_parent"
+        >
+            <i class="fa fa-reply tlp-button-icon"></i>
+            <translate>Go to parent folder</translate>
+        </router-link>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import EmptyFolderForReadersSvg from "./EmptyFolderForReadersSvg.vue";
+
+export default {
+    name: "EmptyFolderForReaders",
+    components: { EmptyFolderForReadersSvg },
+    computed: {
+        ...mapState(["current_folder", "current_folder_ascendant_hierarchy"]),
+        index_of_parent() {
+            return this.current_folder_ascendant_hierarchy.length - 2;
+        },
+        parent() {
+            if (this.index_of_parent > 0) {
+                return this.current_folder_ascendant_hierarchy[this.index_of_parent];
+            }
+
+            return null;
+        },
+        route_to() {
+            return this.parent !== null
+                ? { name: "folder", params: { item_id: this.parent.id } }
+                : { name: "root_folder" };
+        },
+        can_go_to_parent() {
+            return this.index_of_parent >= -1;
+        }
+    }
+};
+</script>
