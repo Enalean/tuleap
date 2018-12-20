45337a51e2067ce6210c476b716d64649c846854
Nicolas Terray <nicolas.terray@enalean.com>
2018-12-20 20:52:37 +0100

    Refactoring: extract small components
    Part of story #12447: Modal new document for empty/wiki/link
    
    In order to ease the introduction of a new type in the creation modale, we need
    to extract smaller components first so that the modal component does not become
    too fat.
    
    Small functional changes:
    - when the modal is open, title input has focus
    - when the modal is dismissed (e.g cancel button) and reopen, the previous
      entered title/description are lost.
    
    Change-Id: I22647d4cd9da0dd9bbd85554e34c810324408780

---
+++ plugins/document/scripts/document/components/Folder/NewItem/Property/PropertyDescription.vue
@@ -0,0 +1,54 @@
+<!--
+  - Copyright (c) Enalean, 2018. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+    <div class="tlp-form-element">
+        <div class="tlp-form-element">
+            <label
+                class="tlp-label"
+                for="document-new-item-description"
+                v-translate
+            >
+                Description
+            </label>
+            <textarea
+                type="text"
+                class="tlp-textarea"
+                id="document-new-item-description"
+                name="description"
+                v-bind:placeholder="placeholder"
+                v-bind:value="value"
+                v-on:input="$emit('input', $event.target.value)"
+            ></textarea>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        value: String
+    },
+    computed: {
+        placeholder() {
+            return this.$gettext("My useful document description");
+        }
+    }
+};
+</script>
