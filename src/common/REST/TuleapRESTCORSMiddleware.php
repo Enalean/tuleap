Enalean/tuleap

commit 90adb7cf29e7a22523796c29d48dcbfd0a34e8bb

Document manager upload endpoint can be used cross-origin like the REST API
This endpoint will be used after a call to the Tuleap REST API so
it should as accessible as the REST API otherwise cross origin client
will be broken.

To keep the Tus server as interoperable as possible, PSR-15 middlewares
are used to set the necessary headers. Sadly, Tuleap still supports PHP 5.6
so look alike interfaces are introduced in the meantime. Once the PHP 5.6
support is dropped, these interfaces should be swapped out for the PSR
interfaces.

How to access the test upload endpoint is described in the introductory
commit 426ca739d33e52787ad1439f52a5c8d658d56c8d.

This is part of story #12612: upload document via REST route

Change-Id: I47fc0e04c90a81f6fba039200d6c80c3f666b8ce

src/common/REST/TuleapRESTCORSMiddleware.php
@@ -1,0 +1,49 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\REST;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Tuleap\Http\Server\MiddlewareInterface;
+use Tuleap\Http\Server\RequestHandlerInterface;
+
+final class TuleapRESTCORSMiddleware implements MiddlewareInterface
+{
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler)
+    {
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+        $response = $response->withAddedHeader(
+            'Access-Control-Allow-Headers',
+            ['Authorization', 'Origin', 'X-Auth-UserId', 'X-Auth-Token', 'X-Client-Uuid', 'X-Auth-AccessKey']
+        );
+        $response = $response->withAddedHeader(
+            'Access-Control-Expose-Headers',
+            ['X-PAGINATION-SIZE', 'X-PAGINATION-LIMIT-MAX', 'X-PAGINATION-LIMIT']
+        );
+
+        return $response;
+    }
+}

plugins/docman/include/Tus/TusCORSMiddleware.php
@@ -1,0 +1,48 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Docman\Tus;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Tuleap\Http\Server\MiddlewareInterface;
+use Tuleap\Http\Server\RequestHandlerInterface;
+
+final class TusCORSMiddleware implements MiddlewareInterface
+{
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler)
+    {
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Methods', ['OPTIONS', 'HEAD', 'PATCH', 'POST', 'DELETE']);
+        $response = $response->withAddedHeader(
+            'Access-Control-Allow-Headers',
+            ['Tus-Resumable', 'Upload-Offset', 'Upload-Length', 'X-HTTP-Method-Override']
+        );
+        $response = $response->withAddedHeader(
+            'Access-Control-Expose-Headers',
+            ['Tus-Version', 'Tus-Resumable', 'Upload-Offset', 'Upload-Length', 'Tus-Extension', 'Tus-Max-Size']
+        );
+
+        return $response;
+    }
+}

plugins/docman/include/Tus/TusFileProvider.php
@@ -1,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Docman\Tus;
+
+interface TusFileProvider
+{
+    /**
+     * @return TusFile|null
+     */
+    public function getFile(\Psr\Http\Message\ServerRequestInterface $request);
+}

plugins/docman/include/Tus/TusServer.php
@@ -21,8 +21,9 @@
 namespace Tuleap\Docman\Tus;
 
 use Http\Message\ResponseFactory;
+use Tuleap\Http\Server\RequestHandlerInterface;
 
-final class TusServer
+final class TusServer implements RequestHandlerInterface
 {
     const TUS_VERSION = '1.0.0';
 
@@ -30,17 +31,23 @@ final class TusServer
      * @var ResponseFactory
      */
     private $response_factory;
+    /**
+     * @var TusFileProvider
+     */
+    private $file_provider;
 
-    public function __construct(ResponseFactory $response_factory)
+    public function __construct(ResponseFactory $response_factory, TusFileProvider $file_provider)
     {
         $this->response_factory = $response_factory;
+        $this->file_provider = $file_provider;
     }
 
     /**
      * @return \Psr\Http\Message\ResponseInterface
      */
-    public function serve(\Psr\Http\Message\RequestInterface $request, TusFile $file)
+    public function handle(\Psr\Http\Message\ServerRequestInterface $request)
     {
+        $response = null;
         try {
             switch ($request->getMethod()) {
                 case 'OPTIONS':
@@ -48,11 +55,17 @@ final class TusServer
                     break;
                 case 'HEAD':
                     $this->checkProtocolVersionIsSupported($request);
-                    $response = $this->processHead($file);
+                    $file = $this->file_provider->getFile($request);
+                    if ($file !== null) {
+                        $response = $this->processHead($file);
+                    }
                     break;
                 case 'PATCH':
                     $this->checkProtocolVersionIsSupported($request);
-                    $response = $this->processPatch($request, $file);
+                    $file = $this->file_provider->getFile($request);
+                    if ($file !== null) {
+                        $response = $this->processPatch($request, $file);
+                    }
                     break;
                 default:
                     return $this->response_factory->createResponse(405);
@@ -69,6 +82,10 @@ final class TusServer
                 $exception->getMessage(),
                 ['Tus-Resumable' => self::TUS_VERSION]
             );
+        }
+
+        if ($response === null) {
+            $response = $this->response_factory->createResponse(404);
         }
 
         if ($request->getMethod() === 'OPTIONS') {

plugins/docman/include/Upload/DocumentToUploadProvider.php
@@ -1,0 +1,49 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Docman\Upload;
+
+use Tuleap\Docman\Tus\TusFileProvider;
+
+final class DocumentToUploadProvider implements TusFileProvider
+{
+    const ONE_MB_FILE = 1048576;
+
+    /**
+     * @var string
+     */
+    private $root_path_storage;
+
+    public function __construct($root_path_storage)
+    {
+        $this->root_path_storage = $root_path_storage;
+    }
+
+    public function getFile(\Psr\Http\Message\ServerRequestInterface $request)
+    {
+        $test_file = $this->root_path_storage . '/test_upload';
+        if (! file_exists($test_file)) {
+            touch($test_file);
+        }
+        $handle = fopen($test_file, 'ab');
+
+        return new DocumentToUpload($handle, self::ONE_MB_FILE, filesize($test_file));
+    }
+}

plugins/docman/include/Upload/FileUploadController.php
@@ -22,20 +22,20 @@ namespace Tuleap\Docman\Upload;
 
 use GuzzleHttp\Psr7\ServerRequest;
 use HTTPRequest;
+use Tuleap\Docman\Tus\TusCORSMiddleware;
 use Tuleap\Docman\Tus\TusServer;
-use Tuleap\Http\MessageFactoryBuilder;
 use Tuleap\Layout\BaseLayout;
 use Tuleap\Request\DispatchableWithRequestNoAuthz;
 use Tuleap\REST\BasicAuthentication;
+use Tuleap\REST\TuleapRESTCORSMiddleware;
 use Tuleap\REST\UserManager;
 
 class FileUploadController implements DispatchableWithRequestNoAuthz
 {
-    const ONE_MB_FILE = 1048576;
     /**
-     * @var string
+     * @var TusServer
      */
-    private $root_path_storage;
+    private $tus_server;
     /**
      * @var UserManager
      */
@@ -44,31 +44,35 @@ class FileUploadController implements DispatchableWithRequestNoAuthz
      * @var BasicAuthentication
      */
     private $basic_rest_authentication;
+    /**
+     * @var TusCORSMiddleware
+     */
+    private $tus_cors_middleware;
+    /**
+     * @var TuleapRESTCORSMiddleware
+     */
+    private $rest_cors_middleware;
 
     public function __construct(
-        $root_path_storage,
+        TusServer $tus_server,
+        TusCORSMiddleware $tus_cors_middleware,
+        TuleapRESTCORSMiddleware $rest_cors_middleware,
         UserManager $rest_user_manager,
         BasicAuthentication $basic_rest_authentication
     ) {
-        $this->root_path_storage         = $root_path_storage;
+        $this->tus_server                = $tus_server;
+        $this->tus_cors_middleware       = $tus_cors_middleware;
+        $this->rest_cors_middleware      = $rest_cors_middleware;
         $this->rest_user_manager         = $rest_user_manager;
         $this->basic_rest_authentication = $basic_rest_authentication;
     }
 
     public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
     {
-        $test_file = $this->root_path_storage . '/test_upload';
-        if (! file_exists($test_file)) {
-            touch($test_file);
-        }
-        $handle = fopen($test_file, 'ab');
+        $server_request = ServerRequest::fromGlobals();
 
-        $document = new DocumentToUpload($handle, self::ONE_MB_FILE, filesize($test_file));
-
-        $tus_server = new TusServer(MessageFactoryBuilder::build());
-        $response   = $tus_server->serve(ServerRequest::fromGlobals(), $document);
-
-        fclose($handle);
+        $dispatcher = new FileUploadDispatcher($this->tus_server, $this->tus_cors_middleware, $this->rest_cors_middleware);
+        $response   = $dispatcher->handle($server_request);
 
         http_response_code($response->getStatusCode());
         foreach ($response->getHeaders() as $header => $values) {

plugins/docman/include/Upload/FileUploadDispatcher.php
@@ -1,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Docman\Upload;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Tuleap\Docman\Tus\TusServer;
+use Tuleap\Http\Server\MiddlewareInterface;
+use Tuleap\Http\Server\RequestHandlerInterface;
+
+final class FileUploadDispatcher implements RequestHandlerInterface
+{
+    /**
+     * @var TusServer
+     */
+    private $server;
+    /**
+     * @var MiddlewareInterface[]
+     */
+    private $middlewares;
+
+    public function __construct(TusServer $server, MiddlewareInterface ...$middlewares)
+    {
+        $this->server      = $server;
+        $this->middlewares = $middlewares;
+    }
+
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request)
+    {
+        $middleware = current($this->middlewares);
+        if ($middleware === false) {
+            return $this->server->handle($request);
+        }
+        next($this->middlewares);
+        return $middleware->process($request, $this);
+    }
+}

plugins/docman/include/docmanPlugin.class.php
@@ -36,6 +36,9 @@ use Tuleap\Docman\Notifications\UsersUpdater;
 use Tuleap\Docman\PermissionsPerGroup\PermissionPerGroupDocmanServicePaneBuilder;
 use Tuleap\Docman\REST\ResourcesInjector;
 use Tuleap\Docman\REST\v1\ItemRepresentationBuilder;
+use Tuleap\Docman\Tus\TusServer;
+use Tuleap\Docman\Upload\DocumentToUploadProvider;
+use Tuleap\Http\MessageFactoryBuilder;
 use Tuleap\Layout\PaginationPresenter;
 use Tuleap\Mail\MailFilter;
 use Tuleap\Mail\MailLogger;
@@ -1253,7 +1256,9 @@ class DocmanPlugin extends Plugin
         if (ForgeConfig::get('enable_tus_test_endpoint')) {
             $event->getRouteCollector()->addRoute(['OPTIONS', 'HEAD', 'PATCH'], '/uploads/docman/file', function () {
                 return new \Tuleap\Docman\Upload\FileUploadController(
-                    '/tmp',
+                    new TusServer(MessageFactoryBuilder::build(), new DocumentToUploadProvider('/tmp')),
+                    new \Tuleap\Docman\Tus\TusCORSMiddleware(),
+                    new \Tuleap\REST\TuleapRESTCORSMiddleware(),
                     \Tuleap\REST\UserManager::build(),
                     new \Tuleap\REST\BasicAuthentication()
                 );

commit 4365e5deaa623bb7ee5fcb17811a25c2b8023a1c

request #12647: Cross origin requests to the REST API are rejected
Issue has been introduced by 90adb7cf29e7a22523796c29d48dcbfd0a34e8bb.

The Content-Type header needs to be on the list of allowed headers otherwise
only a few values are accepted and these values does not include the JSON
or XML content types.

Change-Id: I0509d9918a06887290a45042858738a67be44a5a

src/common/REST/TuleapRESTCORSMiddleware.php
@@ -37,7 +37,7 @@ final class TuleapRESTCORSMiddleware implements MiddlewareInterface
         $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
         $response = $response->withAddedHeader(
             'Access-Control-Allow-Headers',
-            ['Authorization', 'Origin', 'X-Auth-UserId', 'X-Auth-Token', 'X-Client-Uuid', 'X-Auth-AccessKey']
+            ['Authorization', 'Content-Type', 'Origin', 'X-Auth-UserId', 'X-Auth-Token', 'X-Client-Uuid', 'X-Auth-AccessKey']
         );
         $response = $response->withAddedHeader(
             'Access-Control-Expose-Headers',