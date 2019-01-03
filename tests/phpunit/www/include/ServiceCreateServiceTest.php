4cdda25c366edb56b371018c9b0524985c535f85
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2019-01-03 14:55:13 +0100

    Move www/include tests to PHPUnit
    This is part of request #12688 Move core tests to PHPUnit
    
    Change-Id: I7e5b0140e48a8f7c816e8b7a338f1d54c0af6b31

---
+++ tests/phpunit/www/include/ServiceCreateServiceTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2011 - 2019. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Project;
+
+require_once __DIR__.'/../../../../src/www/include/service.php';
+
+class ServiceCreateServiceTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    protected function setUp()
+    {
+        $this->template = [
+            'name' => 'template-name',
+            'id'   => 120
+        ];
+        $this->project = Mockery::mock(Project::class);
+        $this->project->shouldReceive('getGroupId')->andReturn(101);
+        $this->project->shouldReceive('getUnixName')->andReturn('h1tst');
+    }
+
+    private function assertLinkEquals($link, $expected)
+    {
+        $result = service_replace_template_name_in_link($link, $this->template, $this->project);
+        $this->assertSame($expected, $result);
+    }
+
+    public function testItReplacesNameIfLinkIsDashboard()
+    {
+        $link     = '/projects/template-name/';
+        $expected = '/projects/h1tst/';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItReplacesNameIfLinkContainesAmpersand()
+    {
+        $link     = 'test=template-name&group=template-name';
+        $expected = 'test=template-name&group=h1tst';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItReplacesGroupId()
+    {
+        $link     = '/www/?group_id=120';
+        $expected = '/www/?group_id=101';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItDoesntReplaceGroupIdIfNoMatch()
+    {
+        $link     = '/www/?group_id=1204'; //template id is 120
+        $expected = '/www/?group_id=1204';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItReplacesWebroot()
+    {
+        $link     = '/www/template-name/';
+        $expected = '/www/h1tst/';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItReplacesWhenUsedAsQueryParameter()
+    {
+        $link     = 'group=template-name';
+        $expected = 'group=h1tst';
+        $this->assertLinkEquals($link, $expected);
+    }
+
+    public function testItDoesntReplaceWhenNameIsPartOfAPluginName()
+    {
+        $this->template['name'] = 'agile';
+        $link     = '/plugins/agiledashboard/';
+        $expected = '/plugins/agiledashboard/';
+        $this->assertLinkEquals($link, $expected);
+    }
+}
---
+++ tests/phpunit/www/include/UtilsHTTPTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2011 - 2019. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap;
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__.'/../../../../src/www/include/utils.php';
+
+class UtilsHTTPTest extends TestCase
+{
+
+    public function testItExtractBody()
+    {
+        $string = "Content-type: sdfsdf\r\n\r\nThe body";
+        list($headers, $body) = http_split_header_body($string);
+
+        $this->assertSame("The body", $body);
+    }
+
+    public function testItExtractBodyThatStartsWithNul()
+    {
+        $string = "Content-type: sdfsdf\r\n\r\n".(0x00)."The body";
+        list($headers, $body) = http_split_header_body($string);
+
+        $this->assertSame((0x00)."The body", $body);
+    }
+
+    public function testItExtractBodyThatStartsWithLN()
+    {
+        list($headers, $body) = http_split_header_body("Content-type: sdfsdf\r\n\r\n
+The body");
+        $this->assertSame("\nThe body", $body);
+    }
+
+    public function testItExtractHeaders()
+    {
+        list($headers, $body) = http_split_header_body("Content-disposition: anefe
+Content-type: sdfsdf\r\n\r\nThe body");
+        $this->assertSame("Content-disposition: anefe\nContent-type: sdfsdf", $headers);
+    }
+
+    /**
+     * @see https://tuleap.net/plugins/tracker/?aid=5604&group_id=101 ViewVC download broken when file start with 0x00
+     */
+    public function testItExtractsBodyWithBinaryData()
+    {
+        list($headers, $body) = http_split_header_body(file_get_contents(dirname(__FILE__).'/_fixtures/svn_bin_data'));
+        $this->assertSame("Content-Type: text/plain", $headers);
+    }
+}
