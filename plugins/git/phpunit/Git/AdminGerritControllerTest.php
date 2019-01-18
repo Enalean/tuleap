ef05e85f5fa3ebe28d17e9ce5ca132fd2e3feb1c
Manuel VACELET <manuel.vacelet@enalean.com>
2019-01-18 11:35:09 +0100

    Forbid to create new gerrit server with Digest Auth
    Digest Auth is no longer supported by newer version of gerrit and basic
    auth is superior in any possible ways (esp. because it avoids to have
    a dedicated HTTP_PASSWORD for ldap users)
    
    Part of story #12784 use gerrit 2.16 with tuleap
    
    Change-Id: I9baa0f503e99e987eea4b6d56d605ebd0e2b8166

--- plugins/git/phpunit/Git/AdminGerritControllerTest.php
+++ plugins/git/phpunit/Git/AdminGerritControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2019. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -16,28 +16,49 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
-use Tuleap\Git\AdminGerritBuilder;
+namespace Tuleap\Git;
 
-require_once dirname(__FILE__).'/../bootstrap.php';
+use CSRFSynchronizerToken;
+use Git_RemoteServer_GerritServer;
+use Git_RemoteServer_GerritServerFactory;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+use Response;
+use User_SSHKeyValidator;
 
-class Git_Admin_process_Test extends TuleapTestCase {
+class GitAdminProcessTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
 
-    public function setUp() {
-        parent::setUp();
-        $this->request             = aRequest()->build();
-        $this->csrf                = mock('CSRFSynchronizerToken');
-        $this->factory             = mock('Git_RemoteServer_GerritServerFactory');
-        $this->admin_page_renderer = mock('Tuleap\Admin\AdminPageRenderer');
-        $this->admin               = new Git_AdminGerritController(
-            $this->csrf,
-            $this->factory,
-            $this->admin_page_renderer,
-            mock('Tuleap\Git\GerritServerResourceRestrictor'),
-            mock('Tuleap\Git\RemoteServer\Gerrit\Restrictor'),
-            new AdminGerritBuilder(mock('User_SSHKeyValidator'))
-        );
+    /**
+     * @var \Codendi_Request
+     */
+    private $request;
+    private $csrf;
+    /**
+     * @var MockInterface|Git_RemoteServer_GerritServerFactory
+     */
+    private $factory;
+    private $admin_page_renderer;
+    private $admin;
+    private $request_update_existing_server;
+    private $a_brand_new_server;
+    private $an_existing_server;
+
+    public function setUp()
+    {
+        $GLOBALS['Response']       = \Mockery::spy(Response::class);
+        $this->csrf                = \Mockery::spy(CSRFSynchronizerToken::class);
+
+        $this->request             = new \Codendi_Request([], \Mockery::spy(\ProjectManager::class));
+        $this->request->set($this->csrf->getTokenName(), $this->csrf->getToken());
+        $this->request->set('action', 'add-gerrit-server');
+
+        $this->admin_page_renderer = \Mockery::spy(\Tuleap\Admin\AdminPageRenderer::class);
 
         $this->request_update_existing_server = array(
             'host'                 => 'g.example.com',
@@ -50,7 +71,7 @@ class Git_Admin_process_Test extends TuleapTestCase {
             'gerrit_version'       => '2.5',
             'http_password'        => 'azerty',
             'replication_password' => 'replication_password',
-            'auth_type'            => 'Digest'
+            'auth_type'            => 'Basic'
         );
 
         $this->a_brand_new_server = new Git_RemoteServer_GerritServer(
@@ -65,7 +86,7 @@ class Git_Admin_process_Test extends TuleapTestCase {
             '2.5',
             'azerty',
             '',
-            'Digest'
+            'Basic'
         );
 
         $this->an_existing_server = new Git_RemoteServer_GerritServer(
@@ -80,27 +101,44 @@ class Git_Admin_process_Test extends TuleapTestCase {
             '2.5',
             'azerty',
             'azerty',
-            'Digest'
+            'Basic'
         );
 
-        stub($this->factory)->getServers()->returns(array(
-            1 => $this->an_existing_server
-        ));
+        $this->factory             = \Mockery::spy(
+            Git_RemoteServer_GerritServerFactory::class,
+            [
+                'getServers'    => [1 => $this->an_existing_server],
+                'getServerById' => $this->an_existing_server,
+            ]
+        );
 
-        stub($this->factory)->getServerById()->returns($this->an_existing_server);
 
-        $this->request->set($this->csrf->getTokenName(), $this->csrf->getToken());
-        $this->request->set('action', 'add-gerrit-server');
+        $this->admin               = new \Git_AdminGerritController(
+            $this->csrf,
+            $this->factory,
+            $this->admin_page_renderer,
+            \Mockery::spy(GerritServerResourceRestrictor::class),
+            \Mockery::spy(RemoteServer\Gerrit\Restrictor::class),
+            new AdminGerritBuilder(\Mockery::spy(User_SSHKeyValidator::class))
+        );
     }
 
-    public function itDoesNotSaveAnythingIfTheRequestIsNotValid() {
+    /**
+     * @test
+     */
+    public function itDoesNotSaveAnythingIfTheRequestIsNotValid()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('server', false);
-        expect($this->factory)->save()->never();
+        $this->factory->shouldReceive('save')->never();
         $this->admin->process($this->request);
     }
 
-    public function itDoesNotSaveAServerIfNoDataIsGiven() {
+    /**
+     * @test
+     */
+    public function itDoesNotSaveAServerIfNoDataIsGiven()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('host', '');
         $this->request->set('ssh_port', '');
@@ -112,12 +150,16 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('gerrit_version', '2.5');
         $this->request->set('http_password', '');
         $this->request->set('replication_password', '');
-        $this->request->set('auth_type', 'Digest');
-        expect($this->factory)->save()->never();
+        $this->request->set('auth_type', 'Basic');
+        $this->factory->shouldReceive('save')->never();
         $this->admin->process($this->request);
     }
 
-    public function itDoesNotSaveAServerIfItsHostIsEmpty() {
+    /**
+     * @test
+     */
+    public function itDoesNotSaveAServerIfItsHostIsEmpty()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('host', '');
         $this->request->set('ssh_port', '1234');
@@ -129,12 +171,16 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('gerrit_version', '2.5');
         $this->request->set('http_password', 'azerty');
         $this->request->set('replication_password', 'azerty');
-        $this->request->set('auth_type', 'Digest');
-        expect($this->factory)->save()->never();
+        $this->request->set('auth_type', 'Basic');
+        $this->factory->shouldReceive('save')->never();
         $this->admin->process($this->request);
     }
 
-    public function itNotSavesAServerIfItsHostIsNotEmptyAndAllOtherDataAreEmpty() {
+    /**
+     * @test
+     */
+    public function itNotSavesAServerIfItsHostIsNotEmptyAndAllOtherDataAreEmpty()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('host', 'awesome_host');
         $this->request->set('ssh_port', '');
@@ -147,11 +193,15 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('http_password', '');
         $this->request->set('replication_password', '');
         $this->request->set('auth_type', '');
-        expect($this->factory)->save()->never();
+        $this->factory->shouldReceive('save')->never();
         $this->admin->process($this->request);
     }
 
-    public function itCheckWithCSRFIfTheRequestIsForged() {
+    /**
+     * @test
+     */
+    public function itCheckWithCSRFIfTheRequestIsForged()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('host', 'host');
         $this->request->set('ssh_port', '1234');
@@ -163,12 +213,16 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('gerrit_version', '2.5');
         $this->request->set('http_password', 'azerty');
         $this->request->set('replication_password', 'azerty');
-        $this->request->set('auth_type', 'Digest');
-        expect($this->csrf)->check()->once();
+        $this->request->set('auth_type', 'Basic');
+        $this->csrf->shouldReceive('check')->once();
         $this->admin->process($this->request);
     }
 
-    public function itSavesNewGerritServer() {
+    /**
+     * @test
+     */
+    public function itSavesNewGerritServer()
+    {
         $this->request->set('action', 'add-gerrit-server');
         $this->request->set('host', 'host');
         $this->request->set('ssh_port', '1234');
@@ -180,18 +234,29 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('gerrit_version', '2.5');
         $this->request->set('http_password', 'azerty');
         $this->request->set('replication_password', 'azerty');
-        $this->request->set('auth_type', 'Digest');
-        expect($this->factory)->save($this->a_brand_new_server)->once();
+        $this->request->set('auth_type', 'Basic');
+        $s = $this->a_brand_new_server;
+        $this->factory->shouldReceive('save')->with(\Mockery::on(function (Git_RemoteServer_GerritServer $param) use ($s) {
+            return $s == $param;
+        }))->once();
         $this->admin->process($this->request);
     }
 
-    public function itRedirectsAfterSave() {
+    /**
+     * @test
+     */
+    public function itRedirectsAfterSave()
+    {
         $this->request->set('action', 'add-gerrit-server');
-        expect($GLOBALS['Response'])->redirect()->once();
+        $GLOBALS['Response']->shouldReceive('redirect')->once();
         $this->admin->process($this->request);
     }
 
-    public function itUpdatesExistingGerritServer() {
+    /**
+     * @test
+     */
+    public function itUpdatesExistingGerritServer()
+    {
         $this->request->set('action', 'edit-gerrit-server');
         $this->request->set('gerrit_server_id', 1);
         $this->request->set('host', 'g.example.com');
@@ -205,15 +270,19 @@ class Git_Admin_process_Test extends TuleapTestCase {
         $this->request->set('http_password', 'azerty');
         $this->request->set('replication_password', 'azerty');
         $this->request->set('auth_type', 'Digest');
-        expect($this->factory)->save($this->an_existing_server)->once();
+        $this->factory->shouldReceive('save')->with($this->an_existing_server)->once();
         $this->admin->process($this->request);
     }
 
-    public function itDeletesGerritServer() {
+    /**
+     * @test
+     */
+    public function itDeletesGerritServer()
+    {
         $this->request->set('action', 'delete-gerrit-server');
         $this->request->set('gerrit_server_id', 1);
-        expect($this->factory)->delete($this->an_existing_server)->once();
-        expect($this->factory)->save($this->an_existing_server)->never();
+        $this->factory->shouldReceive('delete')->with($this->an_existing_server)->once();
+        $this->factory->shouldReceive('save')->with($this->an_existing_server)->never();
         $this->admin->process($this->request);
     }
 }
