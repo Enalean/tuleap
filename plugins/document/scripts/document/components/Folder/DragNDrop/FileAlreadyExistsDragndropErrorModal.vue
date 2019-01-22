Enalean/tuleap

commit 0fbda56fa47ef4303b9d190648cacb0315353eb0

Prevent users to drop files bigger than sys_max_size_upload
Part of story #10721: upload documents with drag'n drop

Try to drop files bigger than sys_max_size_upload. An error modal is
displayed.

Change-Id: I749b036d452bf4fbb9a226eaa27569cbca6e48d4

plugins/document/scripts/document/components/Folder/DragNDrop/MaxSizeDragndropErrorModal.vue
@@ -21,14 +21,9 @@
     <error-modal>
         <translate
             tag="p"
-            v-bind:translate-params="{nb: max_files_dragndrop}"
-            v-bind:translate-n="max_files_dragndrop"
-            translate-plural="You are not allowed to drag 'n drop more than %{ nb } files at once."
+            v-bind:translate-params="{size: max_size_upload_in_mb}"
         >
-            You are not allowed to drag 'n drop more than %{ nb } file at once.
-        </translate>
-        <translate tag="p">
-            Please start again.
+            You are not allowed to upload files bigger than %{ size }.
         </translate>
     </error-modal>
 </template>
@@ -36,11 +31,15 @@
 <script>
 import { mapState } from "vuex";
 import ErrorModal from "./ErrorModal.vue";
+import prettyKibibytes from "pretty-kibibytes";
 
 export default {
     components: { ErrorModal },
     computed: {
-        ...mapState(["max_files_dragndrop"])
+        ...mapState(["max_size_upload"]),
+        max_size_upload_in_mb() {
+            return prettyKibibytes(this.max_size_upload);
+        }
     }
 };
 </script>

plugins/document/include/Tree/DocumentTreePresenter.php
@@ -34,11 +34,16 @@ class DocumentTreePresenter
      * @var bool
      */
     public $user_is_admin;
+    /**
+     * @var int
+     */
+    public $max_size_upload;
 
     public function __construct(\Project $project, \PFUser $user)
     {
-        $this->project_id    = $project->getID();
-        $this->project_name  = $project->getUnixNameLowerCase();
-        $this->user_is_admin = $user->isAdmin($project->getID());
+        $this->project_id      = $project->getID();
+        $this->project_name    = $project->getUnixNameLowerCase();
+        $this->user_is_admin   = $user->isAdmin($project->getID());
+        $this->max_size_upload = \ForgeConfig::get("sys_max_size_upload");
     }
 }

plugins/document/scripts/document/components/App.vue
@@ -41,7 +41,8 @@ export default {
         project_id: Number,
         user_is_admin: Boolean,
         date_time_format: String,
-        max_files_dragndrop: Number
+        max_files_dragndrop: Number,
+        max_size_upload: Number
     },
     computed: {
         ...mapState(["has_folder_permission_error", "has_folder_loading_error"])
@@ -65,7 +66,8 @@ export default {
             this.user_is_admin,
             this.date_time_format,
             this.$gettext("Documents"),
-            this.max_files_dragndrop
+            this.max_files_dragndrop,
+            this.max_size_upload
         ]);
     }
 };

plugins/document/scripts/document/components/Folder/DragNDrop/CurrentFolderDropZone.vue
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import prettyKibibytes from "pretty-kibibytes";
 import { mapState, mapGetters } from "vuex";
 import { sprintf } from "sprintf-js";
 
@@ -41,17 +42,18 @@ export default {
     },
     computed: {
         ...mapGetters(["current_folder_title"]),
-        ...mapState(["max_files_dragndrop"]),
+        ...mapState(["max_files_dragndrop", "max_size_upload"]),
         message_success() {
             return sprintf(
                 this.$ngettext(
-                    "Drop one file to upload it to %(folder)s.",
-                    "Drop up to %(nb_files)s files to upload them to %(folder)s.",
+                    "Drop one file to upload it to %(folder)s (max size is %(size)s).",
+                    "Drop up to %(nb_files)s files to upload them to %(folder)s (max size is %(size)s).",
                     this.max_files_dragndrop
                 ),
                 {
                     nb_files: this.max_files_dragndrop,
-                    folder: this.current_folder_title
+                    folder: this.current_folder_title,
+                    size: prettyKibibytes(this.max_size_upload)
                 }
             );
         },

plugins/document/scripts/document/components/Folder/DragNDrop/DragNDropInCurrentFolder.vue
@@ -41,12 +41,14 @@ export default {
         return {
             main: null,
             error_modal_shown: false,
-            is_dropzone_highlighted: false
+            is_dropzone_highlighted: false,
+            MAX_FILES_ERROR: "max_files",
+            MAX_SIZE_ERROR: "max_size"
         };
     },
     computed: {
         ...mapGetters(["user_can_dragndrop"]),
-        ...mapState(["current_folder", "max_files_dragndrop"]),
+        ...mapState(["current_folder", "max_files_dragndrop", "max_size_upload"]),
         user_can_dragndrop_in_current_folder() {
             return (
                 this.user_can_dragndrop && this.current_folder && this.current_folder.user_can_write
@@ -55,6 +57,11 @@ export default {
         error_modal_name() {
             if (!this.error_modal_shown) {
                 return null;
+            }
+
+            if (this.error_modal_shown === this.MAX_SIZE_ERROR) {
+                return () =>
+                    import(/* webpackChunkName: "document-max-size-dragndrop-error-modal" */ "./MaxSizeDragndropErrorModal.vue");
             }
 
             return () =>
@@ -95,12 +102,21 @@ export default {
                 return;
             }
 
-            if (event.dataTransfer.files.length > this.max_files_dragndrop) {
-                this.error_modal_shown = true;
+            const files = event.dataTransfer.files;
+
+            if (files.length > this.max_files_dragndrop) {
+                this.error_modal_shown = this.MAX_FILES_ERROR;
                 return;
             }
 
-            for (const file of event.dataTransfer.files) {
+            for (const file of files) {
+                if (file.size > this.max_size_upload) {
+                    this.error_modal_shown = this.MAX_SIZE_ERROR;
+                    return;
+                }
+            }
+
+            for (const file of files) {
                 this.$store.dispatch("addNewUploadFile", [file, this.current_folder]);
             }
         },

plugins/document/scripts/document/components/Folder/DragNDrop/ErrorModal.vue
@@ -1,0 +1,66 @@
+<!--
+  - Copyright (c) Enalean, 2019. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="tlp-modal tlp-modal-danger"
+         role="dialog"
+         aria-labelledby="document-dragndrop-error-modal-title"
+    >
+        <div class="tlp-modal-header">
+            <h1 class="tlp-modal-title" id="document-dragndrop-error-modal-title">
+                <i class="fa fa-frown-o tlp-modal-title-icon"></i>
+                <translate>Oops</translate>
+            </h1>
+            <div class="tlp-modal-close" data-dismiss="modal" v-bind:aria-label="close">
+                &times;
+            </div>
+        </div>
+        <div class="tlp-modal-body">
+            <slot></slot>
+        </div>
+        <div class="tlp-modal-footer tlp-modal-footer-large">
+            <button type="submit"
+                    class="tlp-button-danger tlp-button-outline tlp-modal-action"
+                    data-dismiss="modal"
+                    v-translate
+            >
+                Close
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { modal as createModal } from "tlp";
+
+export default {
+    computed: {
+        close() {
+            return this.$gettext("Close");
+        }
+    },
+    mounted() {
+        const modal = createModal(this.$el);
+        modal.addEventListener("tlp-modal-hidden", () => {
+            this.$emit("error-modal-hidden");
+        });
+        modal.show();
+    }
+};
+</script>

plugins/document/scripts/document/index.js
@@ -52,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const date_time_format = document.body.dataset.dateTimeFormat;
     const user_id = Number.parseInt(document.body.dataset.userId, 10);
     const max_files_dragndrop = Number.parseInt(vue_mount_point.dataset.maxFilesDragndrop, 10);
+    const max_size_upload = Number.parseInt(vue_mount_point.dataset.maxSizeUpload, 10);
 
     moment.tz(user_timezone).locale(user_locale);
 
@@ -66,7 +67,8 @@ document.addEventListener("DOMContentLoaded", () => {
             project_id,
             user_is_admin,
             date_time_format,
-            max_files_dragndrop
+            max_files_dragndrop,
+            max_size_upload
         }
     }).$mount(vue_mount_point);
 });

plugins/document/scripts/document/po/fr.po
@@ -9,7 +9,7 @@ msgstr ""
 "MIME-Version: 1.0\n"
 "Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: 8bit\n"
-"X-Generator: Poedit 2.2\n"
+"X-Generator: Poedit 2.0.6\n"
 "Plural-Forms: nplurals=2; plural=(n > 1);\n"
 
 msgid "Administration"
@@ -36,11 +36,16 @@ msgstr "Le document a été créé avec succès."
 msgid "Documents"
 msgstr "Documents"
 
-msgid "Drop one file to upload it to %(folder)s."
-msgid_plural "Drop up to %(nb_files)s files to upload them to %(folder)s."
-msgstr[0] "Déposez un fichier pour le téléverser dans %(folder)s."
+msgid "Drop one file to upload it to %(folder)s (max size is %(size)s)."
+msgid_plural ""
+"Drop up to %(nb_files)s files to upload them to %(folder)s (max size is "
+"%(size)s)."
+msgstr[0] ""
+"Déposez un fichier pour le téléverser dans %(folder)s (la taille max est "
+"%(size)s)."
 msgstr[1] ""
-"Déposez jusqu'à %(nb_files)s fichiers pour les téléverser dans %(folder)s."
+"Déposez jusqu'à %(nb_files)s fichiers pour les téléverser dans %(folder)s "
+"(la taille max est %(size)s)."
 
 msgid "Dropping files in %s is forbidden."
 msgstr "Déposer des fichiers dans %s n'est pas permis."
@@ -132,6 +137,11 @@ msgstr[0] ""
 msgstr[1] ""
 "Vous n'êtes pas autorisés à glisser-déposer plus de %{ nb } fichiers à la "
 "fois."
+
+msgid "You are not allowed to upload files bigger than %{ size }."
+msgstr ""
+"Vous n'êtes pas autorisés à téléverser des fichiers avec une taille excédant "
+"%{ size }."
 
 msgid "You may only access documents you are granted read permission on."
 msgstr ""

commit e0e528572715a2e981224464718bf32bfb449233

Do not display only once the error modal on drop
Part of story #10721: upload documents with drag'n drop

Try to drop a too big file or a file that already exists: an error modal
is displayed. Close the modal and retry immediately with the same file:
the error modal is displayed again.

Bug introduced by commit 0fbda56fa47ef4303b9d190648cacb0315353eb0.

In VueJS, $emit does not bubble the event, you have to manually do it
(or use an external lib but it is not worth it).

Change-Id: Ib9ca64237cb2fd8f3168a8a3b109a5817eb9ed06

plugins/document/scripts/document/components/Folder/DragNDrop/FileAlreadyExistsDragndropErrorModal.vue
@@ -18,7 +18,7 @@
   -->
 
 <template>
-    <error-modal>
+    <error-modal v-on:error-modal-hidden="bubbleErrorModalHidden">
         <translate tag="p">
             A document already exists with the same title.
         </translate>
@@ -28,6 +28,11 @@
 <script>
 import ErrorModal from "./ErrorModal.vue";
 export default {
-    components: { ErrorModal }
+    components: { ErrorModal },
+    methods: {
+        bubbleErrorModalHidden() {
+            this.$emit("error-modal-hidden");
+        }
+    }
 };
 </script>

plugins/document/scripts/document/components/Folder/DragNDrop/MaxFilesDragndropErrorModal.vue
@@ -18,7 +18,7 @@
   -->
 
 <template>
-    <error-modal>
+    <error-modal v-on:error-modal-hidden="bubbleErrorModalHidden">
         <translate
             tag="p"
             v-bind:translate-params="{nb: max_files_dragndrop}"
@@ -41,6 +41,11 @@ export default {
     components: { ErrorModal },
     computed: {
         ...mapState(["max_files_dragndrop"])
+    },
+    methods: {
+        bubbleErrorModalHidden() {
+            this.$emit("error-modal-hidden");
+        }
     }
 };
 </script>

plugins/document/scripts/document/components/Folder/DragNDrop/MaxSizeDragndropErrorModal.vue
@@ -18,7 +18,7 @@
   -->
 
 <template>
-    <error-modal>
+    <error-modal v-on:error-modal-hidden="bubbleErrorModalHidden">
         <translate
             tag="p"
             v-bind:translate-params="{size: max_size_upload_in_mb}"
@@ -39,6 +39,11 @@ export default {
         ...mapState(["max_size_upload"]),
         max_size_upload_in_mb() {
             return prettyKibibytes(this.max_size_upload);
+        }
+    },
+    methods: {
+        bubbleErrorModalHidden() {
+            this.$emit("error-modal-hidden");
         }
     }
 };