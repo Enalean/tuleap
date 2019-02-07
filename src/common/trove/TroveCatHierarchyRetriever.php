7d07bc42ea8b6de70b16914a85eda4ff0fb9f1a0
Nicolas Terray <nicolas.terray@enalean.com>
2019-02-07 17:25:37 +0100

    Indicates that a category is marked as project flag
    Part of story #12553: see flags that categorize my project in all project pages
    
    Hack the db to flag some top categories as project flag (in trove_cat
    table).
    Go to siteadmin » project settings » categories.
    You should see in the list a new column with a check icon next to
    categories marked as project flag.
    
    Change-Id: I5326f613482c0dee01bc279dcd21435d608ff32c

--- src/common/trove/TroveCatHierarchyRetriever.php
+++ src/common/trove/TroveCatHierarchyRetriever.php
@@ -98,6 +98,7 @@ class TroveCatHierarchyRetriever
                 'is_top_level_id'                 => (int) $row_child['parent'] === 0,
                 'is_parent_mandatory'             => (int) $row_child['parent_mandatory'] === 1 && $row_child['parent_mandatory'] !== null,
                 'nb_max_values'                   => (int) $row_child['nb_max_values'],
+                'is_project_flag'                 => (boolean) $row_child['is_project_flag'],
                 'purified_delete_message'         => Codendi_HTMLPurifier::instance()->purify(
                     $GLOBALS['Language']->getText(
                         'admin_trove_cat_delete',
