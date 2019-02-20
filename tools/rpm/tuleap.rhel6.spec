Enalean/tuleap

commit 744819e41b089e0d80229d407e05fad306733113

Stop loading widget's assets on all dashboards
Part of request #12590 Widgets should not include their CSS files on
empty dashboards

This patch aims to improve performances on all dashboards by removing
unused assets.

How to test:
For each of the following widgets:
- Cross Tracker Search
- Document Manager: Documents under review (User Dashboard)
- Continuous Integration: Test Results
- Labeled Items
- Project Links
- Timetracking Overview
- Personal time tracking
The following is true:
- Their CSS / JS files are NOT loaded on an empty User / Project
dashboard
- When added to a User / Project dashboard, their CSS / JS files are
loaded

Performance notes:

Before this patch, on an empty project dashboard, without any extension,
Lighthouse on Chromium 70 reports a Performance Audit score of 52 with a
Time to Interactive of 6930ms (with simulated 3G and 4x CPU slowdown).
We load 12 CSS files and 5 JS files.

After this patch, same settings: Performance score is 63, Time to
Interactive is 6,370ms.
We load 6 CSS files and 4 JS files.

Will be done later:
- Renderer Widget makes us load Tracker / Cardwall / Graph on trackers
v5 CSS files on every dashboard. It will be dealt with separately.
- Provide a way for widgets to declare javascript dependencies for
adding widgets (for Labeled items to load "configure-widget.js")

Change-Id: Ia66c58147eab7c92b71303ae81164c8041fa992a

tools/rpm/tuleap.rhel6.spec
@@ -535,9 +535,10 @@ done
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/prometheus_metrics
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/project_ownership
-%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/crosstracker
+%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/project_ownership
+%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/projectlinks
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/velocity
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/composer.json

tools/rpm/tuleap.rhel7.spec
@@ -449,9 +449,10 @@ done
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/fulltextsearch
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/plugins/project_ownership
-%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/crosstracker
+%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/project_ownership
+%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/projectlinks
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/velocity
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/composer.json

plugins/crosstracker/include/CrossTracker/Widget/ProjectCrossTrackerSearch.php
@@ -24,8 +24,10 @@ use Codendi_Request;
 use HTTPRequest;
 use Project;
 use TemplateRendererFactory;
-use Tuleap\Layout\IncludeAssets;
 use Tuleap\CrossTracker\CrossTrackerReportDao;
+use Tuleap\Layout\CssAsset;
+use Tuleap\Layout\CssAssetCollection;
+use Tuleap\Layout\IncludeAssets;
 use Widget;
 
 class ProjectCrossTrackerSearch extends Widget
@@ -164,6 +166,15 @@ class ProjectCrossTrackerSearch extends Widget
         return array(
             array('file' => $cross_tracker_include_assets->getFileURL('cross-tracker.js'))
         );
+    }
+
+    public function getStylesheetDependencies()
+    {
+        $include_assets = new IncludeAssets(
+            __DIR__ . '/../../../../../src/www/assets/crosstracker/BurningParrot',
+            '/assets/crosstracker/BurningParrot'
+        );
+        return new CssAssetCollection([new CssAsset($include_assets, 'style')]);
     }
 
     /**

plugins/crosstracker/include/crosstrackerPlugin.class.php
@@ -108,7 +108,6 @@ class crosstrackerPlugin extends Plugin // phpcs:ignore
             $this->addHook(\Tuleap\Widget\Event\GetUserWidgetList::NAME);
             $this->addHook(\Tuleap\Widget\Event\GetProjectWidgetList::NAME);
             $this->addHook(Event::REST_RESOURCES);
-            $this->addHook(Event::BURNING_PARROT_GET_STYLESHEETS);
             $this->addHook(TRACKER_EVENT_PROJECT_CREATION_TRACKERS_REQUIRED);
             $this->addHook(CollectRoutesEvent::NAME);
         }
@@ -172,27 +171,6 @@ class crosstrackerPlugin extends Plugin // phpcs:ignore
         foreach ($dao->searchTrackersIdUsedByCrossTrackerByProjectId($params['project_id']) as $row) {
             $params['tracker_ids_list'][] = $row['id'];
         }
-    }
-
-    /** @see \Event::BURNING_PARROT_GET_STYLESHEETS */
-    public function burningParrotGetStylesheets(array $params)
-    {
-        $current_page = new CurrentPage();
-        $current_user = HTTPRequest::instance()->getCurrentUser();
-
-        if (! $current_page->isDashboard()) {
-            return;
-        }
-
-        $css_assets              = new CssAsset(
-            new IncludeAssets(
-                __DIR__ . '/../../../src/www/assets/crosstracker/BurningParrot',
-                '/assets/crosstracker/BurningParrot'
-            ),
-            'style'
-        );
-        $theme_variation         = new ThemeVariation($params['variant'], $current_user);
-        $params['stylesheets'][] = $css_assets->getFileURL($theme_variation);
     }
 
     public function collectRoutesEvent(CollectRoutesEvent $event)

plugins/docman/include/Docman_Widget_MyDocman.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2017 - 2018. All rights reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Enalean, 2017. All rights reserved
  *
  * This file is a part of Tuleap.
  *
@@ -18,6 +18,9 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/
  */
+use Tuleap\Layout\CssAsset;
+use Tuleap\Layout\CssAssetCollection;
+use Tuleap\Layout\IncludeAssets;
 
 /**
  * Docman_Widget_MyDocman
@@ -189,6 +192,13 @@ class Docman_Widget_MyDocman extends Widget
 
         return $ajax_url;
     }
-}
 
-?>
+    public function getStylesheetDependencies()
+    {
+        $theme_include_assets = new IncludeAssets(
+            __DIR__ . '/../www/themes/BurningParrot/assets',
+            DOCMAN_BASE_URL . '/themes/BurningParrot/assets'
+        );
+        return new CssAssetCollection([new CssAsset($theme_include_assets, 'style')]);
+    }
+}

plugins/docman/include/docmanPlugin.class.php
@@ -126,7 +126,6 @@ class DocmanPlugin extends Plugin
         $this->addHook(Event::PROJECT_ACCESS_CHANGE);
         $this->addHook(Event::SITE_ACCESS_CHANGE);
         $this->addHook(Event::SERVICE_CLASSNAMES);
-        $this->addHook(Event::BURNING_PARROT_GET_STYLESHEETS);
         $this->addHook(NavigationDropdownQuickLinksCollector::NAME);
         $this->addHook(PermissionPerGroupPaneCollector::NAME);
     }
@@ -1140,21 +1139,6 @@ class DocmanPlugin extends Plugin
     private function getUsersUpdater()
     {
         return new UsersUpdater($this->getUsersToNotifyDao());
-    }
-
-    public function burning_parrot_get_stylesheets(array $params)
-    {
-        if ($this->canIncludeStylsheets()) {
-            $variant = $params['variant'];
-            $params['stylesheets'][] = $this->getThemePath() . '/css/style-' . $variant->getName() . '.css';
-        }
-    }
-
-    private function canIncludeStylsheets()
-    {
-        return strpos($_SERVER['REQUEST_URI'], '/plugins/docman/') === 0 ||
-            strpos($_SERVER['REQUEST_URI'], '/my/') === 0 ||
-            strpos($_SERVER['REQUEST_URI'], '/projects/') === 0;
     }
 
     public function collectProjectAdminNavigationPermissionDropdownQuickLinks(NavigationDropdownQuickLinksCollector $quick_links_collector)

plugins/hudson/include/hudsonPlugin.class.php
@@ -55,41 +55,11 @@ class hudsonPlugin extends Plugin
         $this->addHook(\Tuleap\Reference\ReferenceGetTooltipContentEvent::NAME);
         $this->addHook(Event::AJAX_REFERENCE_SPARKLINE, 'ajax_reference_sparkline', false);
         $this->addHook('statistics_collector',          'statistics_collector',       false);
-
-        $this->addHook(Event::BURNING_PARROT_GET_STYLESHEETS);
-        $this->addHook(Event::BURNING_PARROT_GET_JAVASCRIPT_FILES);
     }
 
-    public function burning_parrot_get_stylesheets($params)
+    private function canIncludeStylesheets()
     {
-        if ($this->canIncludeStylsheets()) {
-            $variant = $params['variant'];
-            $params['stylesheets'][] = $this->getThemePath() .'/css/style-'. $variant->getName() .'.css';
-        }
-    }
-
-    public function burningParrotGetJavascriptFiles(array $params)
-    {
-        if ($this->isInDashboard()) {
-            $include_assets = new IncludeAssets(
-                __DIR__ . '/../../../src/www/assets/hudson/scripts',
-                '/assets/hudson/scripts'
-            );
-
-            $params['javascript_files'][] = $include_assets->getFileURL('test-results-pie.js');
-        }
-    }
-    private function isInDashboard()
-    {
-        $current_page = new CurrentPage();
-        return $current_page->isDashboard();
-    }
-
-    private function canIncludeStylsheets()
-    {
-        return strpos($_SERVER['REQUEST_URI'], HUDSON_BASE_URL . '/') === 0 ||
-            strpos($_SERVER['REQUEST_URI'], '/my/') === 0 ||
-            strpos($_SERVER['REQUEST_URI'], '/projects/') === 0;
+        return strpos($_SERVER['REQUEST_URI'], HUDSON_BASE_URL . '/') === 0;
     }
 
     public function getPluginInfo() {
@@ -111,7 +81,7 @@ class hudsonPlugin extends Plugin
     function cssFile($params) {
         // Only show the stylesheet if we're actually in the hudson pages.
         // This stops styles inadvertently clashing with the main site.
-        if ($this->canIncludeStylsheets() ||
+        if ($this->canIncludeStylesheets() ||
             strpos($_SERVER['REQUEST_URI'], '/widgets/') === 0
         ) {
             echo '<link rel="stylesheet" type="text/css" href="'.$this->getThemePath().'/css/style.css" />';

plugins/hudson/include/hudson_Widget_JobTestResults.class.php
@@ -22,6 +22,9 @@
 use Tuleap\Dashboard\User\UserDashboardController;
 use Tuleap\Hudson\HudsonJobBuilder;
 use Tuleap\Hudson\TestResultPieChart\TestResultsPieChartDisplayer;
+use Tuleap\Layout\CssAsset;
+use Tuleap\Layout\CssAssetCollection;
+use Tuleap\Layout\IncludeAssets;
 
 class hudson_Widget_JobTestResults extends HudsonJobWidget
 {
@@ -134,5 +137,26 @@ class hudson_Widget_JobTestResults extends HudsonJobWidget
         }
 
         return $html;
+    }
+
+    public function getJavascriptDependencies()
+    {
+        $include_assets = new IncludeAssets(
+            __DIR__ . '/../../../src/www/assets/hudson/scripts',
+            '/assets/hudson/scripts'
+        );
+
+        return [
+            ['file' => $include_assets->getFileURL('test-results-pie.js')]
+        ];
+    }
+
+    public function getStylesheetDependencies()
+    {
+        $theme_include_assets = new IncludeAssets(
+            __DIR__ . '/../www/themes/BurningParrot/assets',
+            HUDSON_BASE_URL . '/themes/BurningParrot/assets'
+        );
+        return new CssAssetCollection([new CssAsset($theme_include_assets, 'style')]);
     }
 }

commit d8e4ddb2490eec0ccfe5df57b319cfcb819765c6

request #13010: Project links widget can not be displayed in a project dashboard
Ressources should be delivered by their respective packages:
- the main Tuleap packages must have all the resources needed for the project links plugin
- the Hudson/Jenkins must take care of its own resources

Issue has been introduced by 744819e41b089e0d80229d407e05fad306733113.

This contribution can be tested by building the packages and installing
with them. The script tools/rpm/build_and_run_packages.sh can help.

Change-Id: I639a51bb3ca167c735aa434b65da82038cb2e994

tools/rpm/tuleap.rhel6.spec
@@ -535,7 +535,6 @@ done
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/crosstracker
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/project_ownership
-%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/projectlinks
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/velocity
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/composer.json
@@ -1026,7 +1025,7 @@ fi
 %{APP_DIR}/src/www/assets/manifest.json
 %{APP_DIR}/src/www/assets/admindelegation
 %{APP_DIR}/src/www/assets/statistics
-%{APP_DIR}/src/www/assets/hudson
+%{APP_DIR}/src/www/assets/projectlinks
 %{APP_DIR}/src/www/assets/ckeditor-*
 %{APP_DIR}/src/www/codendi.css
 %{APP_DIR}/src/www/cvs
@@ -1218,6 +1217,7 @@ fi
 %files plugin-hudson
 %defattr(-,%{APP_USER},%{APP_USER},-)
 %{APP_DIR}/plugins/hudson
+%{APP_DIR}/src/www/assets/hudson
 
 %files plugin-hudson-svn
 %defattr(-,%{APP_USER},%{APP_USER},-)

tools/rpm/tuleap.rhel7.spec
@@ -454,7 +454,6 @@ done
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/crosstracker
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/document
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/project_ownership
-%{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/projectlinks
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/tuleap_synchro
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/www/assets/velocity
 %{__rm} -rf $RPM_BUILD_ROOT/%{APP_DIR}/src/composer.json
@@ -817,7 +816,7 @@ fi
 %{APP_DIR}/src/www/assets/manifest.json
 %{APP_DIR}/src/www/assets/admindelegation
 %{APP_DIR}/src/www/assets/statistics
-%{APP_DIR}/src/www/assets/hudson
+%{APP_DIR}/src/www/assets/projectlinks
 %{APP_DIR}/src/www/assets/ckeditor-*
 %{APP_DIR}/src/www/codendi.css
 %{APP_DIR}/src/www/cvs
@@ -973,6 +972,7 @@ fi
 %files plugin-hudson
 %defattr(-,root,root,-)
 %{APP_DIR}/plugins/hudson
+%{APP_DIR}/src/www/assets/hudson
 
 %files plugin-hudson-svn
 %defattr(-,root,root,-)