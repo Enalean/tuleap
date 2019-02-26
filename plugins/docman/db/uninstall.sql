06d8fbc0cf81b7950d606dcef0ecef253896fe79
Thomas Gorka <thomas.gorka@enalean.com>
2019-02-26 10:12:39 +0100

    Purge docman settings from forconfig table on plugin uninstall
    part of: story #11297 configure what docman accept for upload
    
    How to test:
    - uninstall the docman plugin
    --> The table forgeconfig should not have:
        - a row with name = 'max_number_of_files'
        - a row with name = 'max_file_size'
    
    Change-Id: Ie6f26d0bcb56267ba0138030625e4ab6975378dc

--- plugins/docman/db/uninstall.sql
+++ plugins/docman/db/uninstall.sql
@@ -26,3 +26,6 @@ DELETE FROM permissions WHERE permission_type LIKE 'PLUGIN_DOCMAN_%';
 DELETE FROM user_preferences WHERE preference_name LIKE 'plugin_docman%';
 
 DROP TABLE IF EXISTS plugin_docman_widget_embedded;
+
+DELETE FROM forgeconfig WHERE name = 'max_number_of_files';
+DELETE FROM forgeconfig WHERE name = 'max_file_size';
