Enalean/tuleap

commit 3a5535e58351cfe4f70b51ab2bb4ced6cb836927

Use PSR-17 [0] HTTP factories
This replace the existing factories that had the same role.

With this contribution and the one introducing PSR-18 HTTP clients
(see d489d91ef0ab38a2610c1eab55a2351f9465453d) Tuleap has within its
internal API a modern way to interact with HTTP services. The use
of the PSR interfaces should be future proof and at least guarantee us
some interoperability with the rest of the ecosystem.

No changes are expected at the functional level.

This is part of request #12996: Move to PSR-18 HTTP client and PSR-17 HTTP factories

[0] https://www.php-fig.org/psr/psr-17/

Change-Id: Iaa70046a7559339f1ffec0e99a6247e66dbc1873

plugins/docman/include/Tus/TusServer.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Docman\Tus;
 
-use Http\Message\ResponseFactory;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
@@ -31,7 +31,7 @@ final class TusServer implements RequestHandlerInterface
     private const TUS_VERSION = '1.0.0';
 
     /**
-     * @var ResponseFactory
+     * @var ResponseFactoryInterface
      */
     private $response_factory;
     /**
@@ -40,7 +40,7 @@ final class TusServer implements RequestHandlerInterface
     private $data_store;
 
     public function __construct(
-        ResponseFactory $response_factory,
+        ResponseFactoryInterface $response_factory,
         TusDataStore $data_store
     ) {
         $this->response_factory = $response_factory;

plugins/docman/phpunit/Tus/TusServerTest.php
@@ -20,27 +20,27 @@
 
 namespace Tuleap\Docman\Tus;
 
-use Http\Message\MessageFactory;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 
 class TusServerTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
 
     /**
-     * @var MessageFactory
+     * @var ResponseFactoryInterface
      */
-    private $message_factory;
+    private $response_factory;
     private $data_store;
     private $file_information_provider;
 
     protected function setUp() : void
     {
-        $this->message_factory           = MessageFactoryBuilder::build();
+        $this->response_factory          = HTTPFactoryBuilder::responseFactory();
         $this->data_store                = \Mockery::mock(TusDataStore::class);
         $this->file_information_provider = \Mockery::mock(TusFileInformationProvider::class);
         $this->data_store->shouldReceive('getFileInformationProvider')->andReturns($this->file_information_provider);
@@ -49,7 +49,7 @@ class TusServerTest extends TestCase
     public function testInformationAboutTheServerCanBeGathered() : void
     {
         $this->data_store->shouldReceive('getTerminater')->andReturns(null);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('OPTIONS');
@@ -64,7 +64,7 @@ class TusServerTest extends TestCase
     public function testInformationAboutExtensionsAreGivenIfThereIsAnAvailableExtension() : void
     {
         $this->data_store->shouldReceive('getTerminater')->andReturns(\Mockery::mock(TusTerminaterDataStore::class));
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('OPTIONS');
@@ -77,7 +77,7 @@ class TusServerTest extends TestCase
 
     public function testInformationAboutTheFileBeingUploadedCanBeGathered() : void
     {
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('HEAD');
@@ -112,7 +112,7 @@ class TusServerTest extends TestCase
         }
         $locker = \Mockery::mock(TusLocker::class);
         $this->data_store->shouldReceive('getLocker')->andReturns($locker);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $upload_request = \Mockery::mock(ServerRequestInterface::class);
         $upload_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -170,7 +170,7 @@ class TusServerTest extends TestCase
 
     public function testRequestWithANonSupportedVersionOfTheProtocolIsRejected() : void
     {
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('HEAD');
@@ -184,7 +184,7 @@ class TusServerTest extends TestCase
     public function testAnUploadRequestWithAnIncorrectOffsetIsRejected() : void
     {
         $this->data_store->shouldReceive('getLocker')->andReturns(null);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -204,7 +204,7 @@ class TusServerTest extends TestCase
 
     public function testAnUploadRequestWithoutTheOffsetIsRejected() : void
     {
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -221,7 +221,7 @@ class TusServerTest extends TestCase
 
     public function testAnUploadRequestWithAnIncorrectContentTypeIsRejected() : void
     {
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -240,7 +240,7 @@ class TusServerTest extends TestCase
         $data_writer = \Mockery::mock(TusWriter::class);
         $this->data_store->shouldReceive('getWriter')->andReturns($data_writer);
         $this->data_store->shouldReceive('getLocker')->andReturns(null);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -267,7 +267,7 @@ class TusServerTest extends TestCase
 
     public function testANotFoundErrorIsGivenWhenTheFileCanNotBeProvided() : void
     {
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -285,7 +285,7 @@ class TusServerTest extends TestCase
     {
         $terminater = \Mockery::mock(TusTerminaterDataStore::class);
         $this->data_store->shouldReceive('getTerminater')->andReturns($terminater);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('DELETE');
@@ -303,7 +303,7 @@ class TusServerTest extends TestCase
     public function testAnUploadCanNotBeTerminatedWhenTheTerminationExtensionIsNotEnabled() : void
     {
         $this->data_store->shouldReceive('getTerminater')->andReturns(null);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('DELETE');
@@ -321,7 +321,7 @@ class TusServerTest extends TestCase
         $finisher = \Mockery::mock(TusFinisherDataStore::class);
         $this->data_store->shouldReceive('getFinisher')->andReturns($finisher);
         $this->data_store->shouldReceive('getLocker')->andReturns(null);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incomplete_upload_request = \Mockery::mock(ServerRequestInterface::class);
         $incomplete_upload_request->shouldReceive('getMethod')->andReturns('PATCH');
@@ -350,7 +350,7 @@ class TusServerTest extends TestCase
     {
         $locker = \Mockery::mock(TusLocker::class);
         $this->data_store->shouldReceive('getLocker')->andReturns($locker);
-        $server = new TusServer($this->message_factory, $this->data_store);
+        $server = new TusServer($this->response_factory, $this->data_store);
 
         $incoming_request = \Mockery::mock(ServerRequestInterface::class);
         $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');

plugins/docman/include/docmanPlugin.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
- * Copyright (c) Enalean, 2015 - 2018.
+ * Copyright (c) Enalean, 2015 - 2019.
  *
  * Originally written by Manuel Vacelet, 2006
  *
@@ -45,7 +45,7 @@ use Tuleap\Docman\REST\v1\ItemRepresentationBuilder;
 use Tuleap\Docman\REST\v1\MetadataRepresentationBuilder;
 use Tuleap\Docman\Tus\TusServer;
 use Tuleap\Docman\Upload\FileUploadController;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Layout\PaginationPresenter;
 use Tuleap\Mail\MailFilter;
 use Tuleap\Mail\MailLogger;
@@ -1270,7 +1270,7 @@ class DocmanPlugin extends Plugin
         $path_allocator              = new \Tuleap\Docman\Upload\DocumentUploadPathAllocator();
         return new FileUploadController(
             new TusServer(
-                MessageFactoryBuilder::build(),
+                HTTPFactoryBuilder::responseFactory(),
                 new \Tuleap\Docman\Upload\DocumentDataStore(
                     new \Tuleap\Docman\Upload\DocumentBeingUploadedInformationProvider(
                         $path_allocator,

plugins/docman/phpunit/Tus/TusCORSMiddlewareTest.php
@@ -26,19 +26,19 @@ use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 
 class TusCORSMiddlewareTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
 
-    private $message_factory;
+    private $response_factory;
 
     public function testTusInformationIsAdded()
     {
-        $this->message_factory = MessageFactoryBuilder::build();
+        $this->response_factory = HTTPFactoryBuilder::responseFactory();
 
-        $request_handler_response = $this->message_factory->createResponse(200);
+        $request_handler_response = $this->response_factory->createResponse(200);
 
         $request_handler = \Mockery::mock(RequestHandlerInterface::class);
         $request_handler->shouldReceive('handle')->andReturns($request_handler_response);
@@ -54,9 +54,9 @@ class TusCORSMiddlewareTest extends TestCase
 
     public function testAllowedAndExposedHeadersAreNotOverwritten()
     {
-        $this->message_factory = MessageFactoryBuilder::build();
+        $this->response_factory = HTTPFactoryBuilder::responseFactory();
 
-        $request_handler_response = $this->message_factory->createResponse(200)
+        $request_handler_response = $this->response_factory->createResponse(200)
             ->withHeader('Access-Control-Allow-Headers', 'MyAllowedHeader')
             ->withHeader('Access-Control-Expose-Headers', 'MyExposedHeader');
 

plugins/git/hooks/post-receive.php
@@ -25,7 +25,7 @@
 
 use Tuleap\Git\Webhook\GitWebhookStatusLogger;
 use Tuleap\Http\HttpClientFactory;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Jenkins\JenkinsCSRFCrumbRetriever;
 use Tuleap\Mail\MailFilter;
 use Tuleap\Mail\MailLogger;
@@ -95,7 +95,12 @@ $post_receive = new Git_Hook_PostReceive(
     $system_event_manager,
     EventManager::instance(),
     new \Tuleap\Git\Webhook\WebhookRequestSender(
-        new \Tuleap\Webhook\Emitter(MessageFactoryBuilder::build(), HttpClientFactory::createAsyncClient(), new GitWebhookStatusLogger($webhook_dao)),
+        new \Tuleap\Webhook\Emitter(
+                HTTPFactoryBuilder::requestFactory(),
+                HTTPFactoryBuilder::streamFactory(),
+                HttpClientFactory::createAsyncClient(),
+                new GitWebhookStatusLogger($webhook_dao)
+        ),
         new \Tuleap\Git\Webhook\WebhookFactory($webhook_dao),
         $logger
     ),

plugins/hudson/include/HudsonJobBuilder.php
@@ -21,16 +21,16 @@
 namespace Tuleap\Hudson;
 
 use Http\Client\HttpAsyncClient;
-use Http\Message\RequestFactory;
 use HudsonJobURLFileException;
 use HudsonJobURLFileNotFoundException;
+use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use SimpleXMLElement;
 
 class HudsonJobBuilder
 {
     /**
-     * @var RequestFactory
+     * @var RequestFactoryInterface
      */
     private $http_request_factory;
     /**
@@ -39,7 +39,7 @@ class HudsonJobBuilder
     private $http_client;
 
 
-    public function __construct(RequestFactory $http_request_factory, HttpAsyncClient $http_client)
+    public function __construct(RequestFactoryInterface $http_request_factory, HttpAsyncClient $http_client)
     {
         $this->http_request_factory = $http_request_factory;
         $this->http_client          = $http_client;

plugins/hudson/include/hudsonActions.class.php
@@ -20,7 +20,7 @@
  */
 
 use Tuleap\Http\HttpClientFactory;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Hudson\HudsonJobBuilder;
 
 /**
@@ -42,7 +42,7 @@ class hudsonActions extends Actions {
         $job_url = $request->get('hudson_job_url');
         try {
             $minimal_job_factory = new MinimalHudsonJobFactory();
-            $job_builder         = new HudsonJobBuilder(MessageFactoryBuilder::build(), HttpClientFactory::createAsyncClient());
+            $job_builder         = new HudsonJobBuilder(HTTPFactoryBuilder::requestFactory(), HttpClientFactory::createAsyncClient());
             $job                 = $job_builder->getHudsonJob(
                 $minimal_job_factory->getMinimalHudsonJob($job_url, '')
             );

plugins/hudson/include/hudsonPlugin.class.php
@@ -22,7 +22,7 @@
 use Tuleap\Dashboard\Project\ProjectDashboardController;
 use Tuleap\Dashboard\User\UserDashboardController;
 use Tuleap\Http\HttpClientFactory;
-use Tuleap\Http\MessageFactoryBuilder;
+use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Hudson\HudsonJobBuilder;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\Request\CurrentPage;
@@ -125,7 +125,7 @@ class hudsonPlugin extends Plugin
 
         $user        = UserManager::instance()->getCurrentUser();
         $hf          = $this->getMinimalHudsonJobFactory();
-        $job_builder = new HudsonJobBuilder(MessageFactoryBuilder::build(), HttpClientFactory::createAsyncClient());
+        $job_builder = new HudsonJobBuilder(HTTPFactoryBuilder::requestFactory(), HttpClientFactory::createAsyncClient());
 
         switch($get_widget_event->getName()) {
             // MY
@@ -269,7 +269,7 @@ class hudsonPlugin extends Plugin
                     $row = $dar->current();
                     try {
                         $minimal_job_factory = $this->getMinimalHudsonJobFactory();
-                        $job_builder         = new HudsonJobBuilder(MessageFactoryBuilder::build(), HttpClientFactory::createAsyncClient());
+                        $job_builder         = new HudsonJobBuilder(HTTPFactoryBuilder::requestFactory(), HttpClientFactory::createAsyncClient());
                         $job                 = $job_builder->getHudsonJob(
                             $minimal_job_factory->getMinimalHudsonJob($row['job_url'], '')
                         );
@@ -351,7 +351,7 @@ class hudsonPlugin extends Plugin
                     $row = $dar->current();
                     try {
                         $minimal_job_factory = $this->getMinimalHudsonJobFactory();
-                        $job_builder         = new HudsonJobBuilder(MessageFactoryBuilder::build(), HttpClientFactory::createAsyncClient());
+                        $job_builder         = new HudsonJobBuilder(HTTPFactoryBuilder::requestFactory(), HttpClientFactory::createAsyncClient());
                         $job                 = $job_builder->getHudsonJob(
                             $minimal_job_factory->getMinimalHudsonJob($row['job_url'], '')
                         );

commit bd81b7a34967cbcd15dbbdb44c6a618bd02390f9

request #13081: Tus specification is not respected when trying to use a non supported version of the protocol
The header Tus-Version is not present when responding to client using a
non supported version of the protocol, it should [0]. The issue has been
introduced by 3a5535e58351cfe4f70b51ab2bb4ced6cb836927.

Th opportunity has also been took to fix a few edge cases spotted by Psalm.

[0] https://tus.io/protocols/resumable-upload.html#tus-version

Change-Id: Id97a804cb6a214564c304081d50917b1b502ef3c

src/common/Tus/CannotReadIncomingFileException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018 - Present. All Rights Reserved.
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -22,10 +22,10 @@ declare(strict_types=1);
 
 namespace Tuleap\Tus;
 
-abstract class TusServerException extends \RuntimeException
+final class CannotReadIncomingFileException extends TusServerException
 {
-    final public function getHTTPStatusCode() : int
+    public function __construct()
     {
-        return (int) $this->getCode();
+        parent::__construct('Cannot access to the content of incoming request', 500);
     }
 }

src/common/Tus/TusServer.php
@@ -86,15 +86,14 @@ final class TusServer implements RequestHandlerInterface
             }
         } catch (TusServerIncompatibleVersionException $exception) {
             $response = $this->response_factory->createResponse(
-                $exception->getCode(),
-                $exception->getMessage(),
-                ['Tus-Version' => self::TUS_VERSION]
+                $exception->getHTTPStatusCode(),
+                $exception->getMessage()
             );
+            $response = $response->withHeader('Tus-Version', self::TUS_VERSION);
         } catch (TusServerException $exception) {
             $response = $this->response_factory->createResponse(
-                $exception->getCode(),
-                $exception->getMessage(),
-                ['Tus-Resumable' => self::TUS_VERSION]
+                $exception->getHTTPStatusCode(),
+                $exception->getMessage()
             );
         }
 
@@ -133,8 +132,8 @@ final class TusServer implements RequestHandlerInterface
     private function processHead(TusFileInformation $file_information) : ResponseInterface
     {
         return $this->response_factory->createResponse(204)
-            ->withHeader('Upload-Length', $file_information->getLength())
-            ->withHeader('Upload-Offset', $file_information->getOffset())
+            ->withHeader('Upload-Length', (string) $file_information->getLength())
+            ->withHeader('Upload-Offset', (string) $file_information->getOffset())
             ->withHeader('Cache-Control', 'no-cache');
     }
 
@@ -156,6 +155,11 @@ final class TusServer implements RequestHandlerInterface
             return $this->response_factory->createResponse(400);
         }
 
+        $incoming_stream = $request->getBody()->detach();
+        if ($incoming_stream === null) {
+            throw new CannotReadIncomingFileException();
+        }
+
         $copied_size = 0;
         $locker      = $this->data_store->getLocker();
         try {
@@ -170,7 +174,7 @@ final class TusServer implements RequestHandlerInterface
             $copied_size = $this->data_store->getWriter()->writeChunk(
                 $file_information,
                 $file_information->getOffset(),
-                $request->getBody()->detach()
+                $incoming_stream
             );
         } finally {
             if ($locker !== null) {
@@ -185,7 +189,7 @@ final class TusServer implements RequestHandlerInterface
         }
 
         return $this->response_factory->createResponse(204)
-            ->withHeader('Upload-Offset', $file_information->getOffset() + $copied_size);
+            ->withHeader('Upload-Offset', (string) ($file_information->getOffset() + $copied_size));
     }
 
     /**

tests/phpunit/common/Tus/TusServerTest.php
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 namespace Tuleap\Tus;
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
@@ -27,7 +29,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
 use Tuleap\Http\HTTPFactoryBuilder;
 
-class TusServerTest extends TestCase
+final class TusServerTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
 
@@ -179,6 +181,7 @@ class TusServerTest extends TestCase
         $response = $server->handle($incoming_request);
 
         $this->assertEquals(412, $response->getStatusCode());
+        $this->assertTrue($response->hasHeader('Tus-Version'));
     }
 
     public function testAnUploadRequestWithAnIncorrectOffsetIsRejected() : void
@@ -192,6 +195,10 @@ class TusServerTest extends TestCase
         $incoming_request->shouldReceive('getHeaderLine')->with('Content-Type')->andReturns('application/offset+octet-stream');
         $incoming_request->shouldReceive('hasHeader')->with('Upload-Offset')->andReturns(true);
         $incoming_request->shouldReceive('getHeaderLine')->with('Upload-Offset')->andReturns(10);
+        $request_body = \Mockery::mock(StreamInterface::class);
+        $request_body_stream = fopen('php://memory', 'rb+');
+        $request_body->shouldReceive('detach')->andReturns($request_body_stream);
+        $incoming_request->shouldReceive('getBody')->andReturns($request_body);
 
         $file_information = \Mockery::mock(TusFileInformation::class);
         $file_information->shouldReceive('getOffset')->andReturns(20);
@@ -233,6 +240,30 @@ class TusServerTest extends TestCase
         $response = $server->handle($incoming_request);
 
         $this->assertEquals(415, $response->getStatusCode());
+    }
+
+    public function testAnErrorIsGivenWhenTheIncomingRequestBodyCannotBeRead() : void
+    {
+        $data_writer = \Mockery::mock(TusWriter::class);
+        $this->data_store->shouldReceive('getWriter')->andReturns($data_writer);
+        $this->data_store->shouldReceive('getLocker')->andReturns(null);
+        $server = new TusServer($this->response_factory, $this->data_store);
+
+        $incoming_request = \Mockery::mock(ServerRequestInterface::class);
+        $incoming_request->shouldReceive('getMethod')->andReturns('PATCH');
+        $incoming_request->shouldReceive('getHeaderLine')->with('Tus-Resumable')->andReturns('1.0.0');
+        $incoming_request->shouldReceive('getHeaderLine')->with('Content-Type')->andReturns('application/offset+octet-stream');
+        $incoming_request->shouldReceive('hasHeader')->with('Upload-Offset')->andReturns(true);
+        $incoming_request->shouldReceive('getHeaderLine')->with('Upload-Offset')->andReturns(0);
+        $request_body = \Mockery::mock(StreamInterface::class);
+        $request_body->shouldReceive('detach')->andReturns(null);
+        $incoming_request->shouldReceive('getBody')->andReturns($request_body);
+
+        $file_information = \Mockery::mock(TusFileInformation::class);
+        $this->file_information_provider->shouldReceive('getFileInformation')->andReturns($file_information);
+
+        $response = $server->handle($incoming_request);
+        $this->assertEquals(500, $response->getStatusCode());
     }
 
     public function testAnErrorIsGivenWhenTheFileCanNotBeSaved() : void
@@ -358,6 +389,10 @@ class TusServerTest extends TestCase
         $incoming_request->shouldReceive('getHeaderLine')->with('Content-Type')->andReturns('application/offset+octet-stream');
         $incoming_request->shouldReceive('hasHeader')->with('Upload-Offset')->andReturns(true);
         $incoming_request->shouldReceive('getHeaderLine')->with('Upload-Offset')->andReturns(0);
+        $request_body = \Mockery::mock(StreamInterface::class);
+        $request_body_stream = fopen('php://memory', 'rb+');
+        $request_body->shouldReceive('detach')->andReturns($request_body_stream);
+        $incoming_request->shouldReceive('getBody')->andReturns($request_body);
 
         $file_information = \Mockery::mock(TusFileInformation::class);
         $file_information->shouldReceive('getLength')->andReturns(123456);