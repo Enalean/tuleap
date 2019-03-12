25295cd3c9040083e669f16b6ded7c6dbe2054fe
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-03-12 17:19:53 +0100

    request #13108 Drag and drop is not supported by IE 11
    This patch must be tested on:
     - Microsoft edge/firefox/IE11/Chrome
    
    How to test:
     - Try to drag and drop a file in docman
     -> This should not be allowed for IE11
     -> should works as usual for other browsers
    
    Change-Id: I378bfd69b0d9a3f5de0f7339935d350a2923bafe

---
+++ plugins/document/scripts/document/components/Folder/DragNDrop/BrowserNotSupported.vue
@@ -0,0 +1,39 @@
+<!--
+  - Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <error-modal v-on:error-modal-hidden="bubbleErrorModalHidden">
+        <p v-translate>
+            Drag and drop is not supported under IE11. Please use a more recent browser.
+        </p>
+    </error-modal>
+</template>
+
+<script>
+import ErrorModal from "./ErrorModal.vue";
+export default {
+    name: "BrowserNotSupported",
+    components: { ErrorModal },
+    methods: {
+        bubbleErrorModalHidden() {
+            this.$emit("error-modal-hidden");
+        }
+    }
+};
+</script>
