7011e5b1feee6c1670cab61c42af591bb5ccf5f0
Jibidus <jean-baptiste@sogilis.com>
2019-03-13 14:37:56 +0100

    Remove duplicate strict_types
    Part of epic #12142 Baseline v1
    
    Some php files contains following line multiple times:
    declare(strict_types=1);
    
    Only one occurence of this line is required.
    
    Change-Id: I7f7574be2d650f74922d6045c6f6a1620e5dd5cc

--- include/ServiceController.php
+++ include/ServiceController.php
@@ -21,8 +21,6 @@
 
 declare(strict_types=1);
 
-declare(strict_types=1);
-
 namespace Tuleap\Baseline;
 
 use HTTPRequest;
--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -19,6 +19,8 @@
  *
  */
 
+declare(strict_types=1);
+
 require_once __DIR__ . '/../../../tests/phpunit/bootstrap.php';
 require_once __DIR__ . '/../include/baselinePlugin.class.php';
 
