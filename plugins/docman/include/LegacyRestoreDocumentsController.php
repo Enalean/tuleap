edbfef1d8bc312f5ae9856f0af42d3c96d56ed98
Thomas Gerbet <thomas.gerbet@enalean.com>
2019-03-21 09:02:25 +0100

    Use front router for the docman plugin
    This move will allow us to re-use the same approach
    than the one used when uploading file from the REST API
    but for downloading file.
    
    No functionnal changes.
    
    Part of request #13133: A REST API user can not download a document manager file
    
    Change-Id: I49af185df6314b188f5aaf6cb1a5084dd3493191

---
+++ plugins/docman/include/LegacyRestoreDocumentsController.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
+ * Copyright (c) STMicroelectronics, 2010. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Docman;
+
+use CSRFSynchronizerToken;
+use Docman_Controller;
+use Docman_ItemFactory;
+use Docman_VersionFactory;
+use HTTPRequest;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchableWithRequest;
+use Valid_WhiteList;
+
+final class LegacyRestoreDocumentsController implements DispatchableWithRequest
+{
+    /**
+     * @var \DocmanPlugin
+     */
+    private $plugin;
+
+    public function __construct(\DocmanPlugin $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables) : void
+    {
+        \Tuleap\Project\ServiceInstrumentation::increment('docman');
+        // Need to setup the controller so the notification & logging works (setup in controller constructor)
+        new Docman_Controller($this->plugin, $this->plugin->getPluginPath(), $this->plugin->getThemePath(), $request);
+
+        $current_user = $request->getCurrentUser();
+        if (! $current_user->isSuperUser()) {
+            $layout->redirect('/');
+        }
+
+        $func    = $request->getValidated('func', new Valid_WhiteList('func', array('confirm_restore_item', 'confirm_restore_version')));
+        $groupId = $request->getValidated('group_id', 'uint', 0);
+        $id      = $request->getValidated('id', 'uint', 0);
+
+        $csrf_token = new CSRFSynchronizerToken('/admin/show_pending_documents.php?group_id=' . urlencode((string) $groupId));
+        $csrf_token->check();
+
+        if ($request->existAndNonEmpty('func')) {
+            switch ($func) {
+                case 'confirm_restore_item':
+                    $itemFactory = new Docman_ItemFactory($groupId);
+                    $item = $itemFactory->getItemFromDb($id, array('ignore_deleted' => true));
+                    if ($itemFactory->restore($item)) {
+                        $url = $this->plugin->getPluginPath().'/?group_id='.$groupId.'&action=details&id='.$id.'&section=properties';
+                        $layout->addFeedback('info', $GLOBALS['Language']->getText('plugin_docman', 'item_restored', array($url)), CODENDI_PURIFIER_DISABLED);
+                        $layout->redirect('/admin/show_pending_documents.php?group_id='.$groupId.'&focus=item');
+                    } else {
+                        exit_error($GLOBALS['Language']->getText('plugin_docman', 'error'), $GLOBALS['Language']->getText('plugin_docman', 'item_not_restored'));
+                    }
+                    break;
+                case 'confirm_restore_version':
+                    $versionFactory = new Docman_VersionFactory();
+                    $version = $versionFactory->getSpecificVersionById($id);
+                    if ($version !== null && $versionFactory->restore($version)) {
+                        $url = $this->plugin->getPluginPath().'/?group_id='.$groupId.'&action=details&id='.$version->getItemId().'&section=history';
+                        $layout->addFeedback('info', $GLOBALS['Language']->getText('plugin_docman', 'version_restored', array($url)), CODENDI_PURIFIER_DISABLED);
+                        $layout->redirect('/admin/show_pending_documents.php?group_id='.$groupId.'&focus=version');
+                    } else {
+                        exit_error($GLOBALS['Language']->getText('plugin_docman', 'error'), $GLOBALS['Language']->getText('plugin_docman', 'version_not_restored'));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
