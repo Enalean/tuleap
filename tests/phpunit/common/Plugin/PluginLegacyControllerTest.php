Enalean/tuleap

commit ec5adc5f325e6bd02370609a0633ef9ede97b733

Move hudson plugin to front controller
Introduce an automated way to move to front controller with the a few
line of code. That doesn't clean the code base of all pseudo routers
but it introduce the route & front controller so future work will
be easier anyway.

Part of request #13030 Use front router for all plugins

Change-Id: I333ca138b7477c145020c9b91746d9a8ed4945cf

plugins/hudson/include/hudsonPlugin.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2011 - 2019. All Rights Reserved.
+ * Copyright (c) Enalean, 2011 - Present. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Tuleap.
@@ -24,8 +24,6 @@ use Tuleap\Dashboard\User\UserDashboardController;
 use Tuleap\Http\HttpClientFactory;
 use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Hudson\HudsonJobBuilder;
-use Tuleap\Layout\IncludeAssets;
-use Tuleap\Request\CurrentPage;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/constants.php';
@@ -55,6 +53,8 @@ class hudsonPlugin extends Plugin
         $this->addHook(\Tuleap\Reference\ReferenceGetTooltipContentEvent::NAME);
         $this->addHook(Event::AJAX_REFERENCE_SPARKLINE, 'ajax_reference_sparkline', false);
         $this->addHook('statistics_collector',          'statistics_collector',       false);
+
+        $this->listenToCollectRouteEventWithDefaultController();
     }
 
     private function canIncludeStylesheets()

src/common/plugin/Plugin.class.php
@@ -19,6 +19,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use FastRoute\RouteCollector;
 use Tuleap\Layout\IncludeAssets;
 
 /**
@@ -395,5 +396,25 @@ class Plugin implements PFO_Plugin {
             'plugin'  => $this->getName(),
             'handler' => $handler,
         ];
+    }
+
+    protected function listenToCollectRouteEventWithDefaultController() : void
+    {
+        $this->addHook(\Tuleap\Request\CollectRoutesEvent::NAME, 'defaultCollectRoutesEvent');
+    }
+
+    public function defaultCollectRoutesEvent(\Tuleap\Request\CollectRoutesEvent $event)
+    {
+        $event->getRouteCollector()->addGroup($this->getPluginPath(), function(FastRoute\RouteCollector $r) {
+            $r->addRoute(['GET', 'POST'], '[/[index.php]]', $this->getRouteHandler('routePluginLegacyController'));
+        });
+    }
+
+    /**
+     * @return \Tuleap\Plugin\PluginLegacyController
+     */
+    public function routePluginLegacyController()
+    {
+        return new \Tuleap\Plugin\PluginLegacyController($this);
     }
 }

src/common/plugin/PluginLegacyController.php
@@ -1,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Plugin;
+
+use HTTPRequest;
+use Plugin;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\ForbiddenException;
+use Tuleap\Request\NotFoundException;
+
+class PluginLegacyController implements DispatchableWithRequest
+{
+
+    /**
+     * @var Plugin
+     */
+    private $plugin;
+
+    public function __construct(Plugin $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * Is able to process a request routed by FrontRouter
+     *
+     * @param HTTPRequest $request
+     * @param BaseLayout  $layout
+     * @param array       $variables
+     * @return void
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    {
+        ServiceInstrumentation::increment($this->plugin->getName());
+        $this->plugin->process();
+    }
+}

plugins/agiledashboard/etc/nginx/agiledashboard.conf
@@ -1,12 +1,0 @@
-# See the how and the why here: http://serverfault.com/a/828579/169219
-location ^~ /plugins/agiledashboard/ {
-    alias /usr/share/tuleap/plugins/agiledashboard/www/;
-
-    if (!-f $request_filename) {
-        rewrite ^ /index.php last;
-    }
-
-    location ^~ /plugins/agiledashboard/assets/ {
-        expires max;
-    }
-}

plugins/cardwall/etc/nginx/cardwall.conf
@@ -1,12 +1,0 @@
-# See the how and the why here: http://serverfault.com/a/828579/169219
-location ^~ /plugins/cardwall/ {
-    alias /usr/share/tuleap/plugins/cardwall/www/;
-
-    if (!-f $request_filename) {
-        rewrite ^ /index.php last;
-    }
-
-    location ^~ /plugins/cardwall/assets/ {
-        expires max;
-    }
-}

plugins/hudson/www/index.php
@@ -1,32 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean 2011 - 2018. All rights reserved
- * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-require_once('pre.php');
-require_once('common/plugin/PluginManager.class.php');
-
-$plugin_manager = PluginManager::instance();
-$p = $plugin_manager->getPluginByName('hudson');
-
-if ($p && $plugin_manager->isPluginAvailable($p)) {
-    $p->process();
-} else {
-    header('Location: '.get_server_url());
-}

plugins/tracker/etc/nginx/tracker.conf
@@ -1,12 +1,0 @@
-# See the how and the why here: http://serverfault.com/a/828579/169219
-location ^~ /plugins/tracker/ {
-    alias /usr/share/tuleap/plugins/tracker/www/;
-
-    if (!-f $request_filename) {
-        rewrite ^ /index.php last;
-    }
-
-    location ^~ /plugins/tracker/assets/ {
-        expires max;
-    }
-}

plugins/userlog/etc/nginx/userlog.conf
@@ -1,11 +1,0 @@
-location ^~ /plugins/userlog/ {
-    alias /usr/share/tuleap/plugins/userlog/www/;
-
-    if (!-f $request_filename) {
-        rewrite ^ /index.php last;
-    }
-
-    location ^~ /plugins/userlog/assets/ {
-        expires 12h;
-    }
-}

commit 9d9768e3ceb20d868505164423a6fc0ff59202d4

hudson/Jenkins plugin is not accessible anymore
Issue has been introduced by ec5adc5f325e6bd02370609a0633ef9ede97b733.

This contribution also harden a bit the usage of the "automated way" to
move to the front controller pattern for existing plugins.

This is part of request #13030: Use front router for all plugins

Change-Id: I639a5c4c30ec93b6e682c47f9e158c40099ce7e1

plugins/hudson/include/hudsonPlugin.class.php
@@ -24,11 +24,12 @@ use Tuleap\Dashboard\User\UserDashboardController;
 use Tuleap\Http\HttpClientFactory;
 use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Hudson\HudsonJobBuilder;
+use Tuleap\Plugin\PluginWithLegacyInternalRouting;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/constants.php';
 
-class hudsonPlugin extends Plugin
+class hudsonPlugin extends PluginWithLegacyInternalRouting
 {
     const ICONS_PATH = '/plugins/hudson/themes/default/images/ic/';
 
@@ -363,7 +364,8 @@ class hudsonPlugin extends Plugin
         }
     }
 
-    function process() {
+    public function process() : void
+    {
         require_once('hudson.class.php');
         $controler = new hudson();
         $controler->process();

src/common/plugin/Plugin.class.php
@@ -397,24 +397,4 @@ class Plugin implements PFO_Plugin {
             'handler' => $handler,
         ];
     }
-
-    protected function listenToCollectRouteEventWithDefaultController() : void
-    {
-        $this->addHook(\Tuleap\Request\CollectRoutesEvent::NAME, 'defaultCollectRoutesEvent');
-    }
-
-    public function defaultCollectRoutesEvent(\Tuleap\Request\CollectRoutesEvent $event)
-    {
-        $event->getRouteCollector()->addGroup($this->getPluginPath(), function(FastRoute\RouteCollector $r) {
-            $r->addRoute(['GET', 'POST'], '[/[index.php]]', $this->getRouteHandler('routePluginLegacyController'));
-        });
-    }
-
-    /**
-     * @return \Tuleap\Plugin\PluginLegacyController
-     */
-    public function routePluginLegacyController()
-    {
-        return new \Tuleap\Plugin\PluginLegacyController($this);
-    }
 }

src/common/plugin/PluginLegacyController.php
@@ -24,21 +24,20 @@ declare(strict_types=1);
 namespace Tuleap\Plugin;
 
 use HTTPRequest;
-use Plugin;
 use Tuleap\Layout\BaseLayout;
+use Tuleap\Project\ServiceInstrumentation;
 use Tuleap\Request\DispatchableWithRequest;
 use Tuleap\Request\ForbiddenException;
 use Tuleap\Request\NotFoundException;
 
-class PluginLegacyController implements DispatchableWithRequest
+final class PluginLegacyController implements DispatchableWithRequest
 {
-
     /**
-     * @var Plugin
+     * @var PluginWithLegacyInternalRouting
      */
     private $plugin;
 
-    public function __construct(Plugin $plugin)
+    public function __construct(PluginWithLegacyInternalRouting $plugin)
     {
         $this->plugin = $plugin;
     }
@@ -53,7 +52,7 @@ class PluginLegacyController implements DispatchableWithRequest
      * @throws ForbiddenException
      * @throws NotFoundException
      */
-    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables) : void
     {
         ServiceInstrumentation::increment($this->plugin->getName());
         $this->plugin->process();

src/common/plugin/PluginWithLegacyInternalRouting.php
@@ -1,0 +1,49 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Plugin;
+
+use FastRoute\RouteCollector;
+use Plugin;
+use Tuleap\Request\CollectRoutesEvent;
+
+abstract class PluginWithLegacyInternalRouting extends Plugin
+{
+    abstract public function process() : void;
+
+    final protected function listenToCollectRouteEventWithDefaultController() : void
+    {
+        $this->addHook(CollectRoutesEvent::NAME, 'defaultCollectRoutesEvent');
+    }
+
+    final public function defaultCollectRoutesEvent(CollectRoutesEvent $event) : void
+    {
+        $event->getRouteCollector()->addGroup($this->getPluginPath(), function (RouteCollector $r) {
+            $r->addRoute(['GET', 'POST'], '[/[index.php]]', $this->getRouteHandler('routePluginLegacyController'));
+        });
+    }
+
+    final public function routePluginLegacyController() : PluginLegacyController
+    {
+        return new PluginLegacyController($this);
+    }
+}

tests/phpunit/common/Plugin/PluginLegacyControllerTest.php
@@ -1,0 +1,48 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Plugin;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Tuleap\Instrument\Prometheus\Prometheus;
+use Tuleap\Layout\BaseLayout;
+
+final class PluginLegacyControllerTest extends TestCase
+{
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    public function testLegacyPluginCanBeAccessed() : void
+    {
+        $plugin = Mockery::mock(PluginWithLegacyInternalRouting::class);
+        $plugin->shouldReceive('getName')->andReturn('test');
+        $controller = new PluginLegacyController($plugin);
+
+        $plugin->shouldReceive('process')->once();
+
+        $controller->process(Mockery::mock(\HTTPRequest::class), Mockery::mock(BaseLayout::class), []);
+        $this->assertStringContainsString(
+            'tuleap_project_service_access_total{service="test"} 1',
+            Prometheus::instance()->renderText()
+        );
+    }
+}