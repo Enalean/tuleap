Enalean/tuleap

commit ff418fb4c30a42d49326ea538ba584f6b14441ea

HTTP requests can be processed through a PSR-15 [0] middleware pipeline
This contribution introduce a way to process a PSR-7 [1] server request
using only PSR-15 middlewares. This approach helps the developer to separate
the different layers of the request processing and as a consequence it makes
the request processing easier to test.
This approach is also the one taken in the modern PHP ecosystem so it should
not surprise new developers deep diving into the codebase.

For now, this contribution makes the usage of a PSR-15 middleware pipeline opt-in.
The routes available to upload files in the FRS and document manager has been converted
to it as an example and because they were already using PSR-7 and PSR-15 interfaces.

No functionnal change is expected.

Part of request #13133: A REST API user can not download a document manager file

[0] https://www.php-fig.org/psr/psr-15/
[1] https://www.php-fig.org/psr/psr-7/

Change-Id: I28e173b3ca99331d674d1ae8972e616d9cc029aa

plugins/document/include/Tree/DocumentTreeUnderConstructionController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2019. All Rights Reserved.
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -23,6 +23,7 @@ declare(strict_types = 1);
 namespace Tuleap\Document\Tree;
 
 use HTTPRequest;
+use Project;
 use Tuleap\Layout\BaseLayout;
 use Tuleap\Request\DispatchableWithProject;
 use Tuleap\Request\DispatchableWithRequest;
@@ -51,14 +52,12 @@ class DocumentTreeUnderConstructionController implements DispatchableWithRequest
     }
 
     /**
-     * @param HTTPRequest $request
      * @param array       $variables
      *
-     * @return \Project
      * @throws NotFoundException
      */
-    public function getProject(\HTTPRequest $request, array $variables)
+    public function getProject(array $variables) : Project
     {
-        return $this->project_extractor->getProject($request, $variables);
+        return $this->project_extractor->getProject($variables);
     }
 }

plugins/git/include/Git/GitRepositoryBrowserController.php
@@ -27,6 +27,7 @@ use GitRepository;
 use GitViews_GitPhpViewer;
 use GitViews_ShowRepo_Content;
 use HTTPRequest;
+use Project;
 use TemplateRendererFactory;
 use Tuleap\Git\Repository\GitRepositoryHeaderDisplayer;
 use Tuleap\Git\Repository\View\FilesHeaderPresenterBuilder;
@@ -87,13 +88,11 @@ class GitRepositoryBrowserController implements DispatchableWithRequest, Dispatc
     }
 
     /**
-     * @param HTTPRequest $request
      * @param array       $variables
      *
-     * @return \Project
      * @throws NotFoundException
      */
-    public function getProject(HTTPRequest $request, array $variables)
+    public function getProject(array $variables) : Project
     {
         $project = $this->project_manager->getProjectByCaseInsensitiveUnixName($variables['project_name']);
         if (! $project || $project->isError()) {

plugins/docman/include/Download/DocmanFileDownloadController.php
@@ -30,11 +30,16 @@ use Logger;
 use LogicException;
 use Project;
 use ProjectManager;
-use function session_write_close;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchablePSR15Compatible;
 use Tuleap\Request\DispatchableWithProject;
 use Tuleap\Request\DispatchableWithRequest;
 use Tuleap\Request\NotFoundException;
+use UserManager;
+use Zend\HttpHandlerRunner\Emitter\EmitterInterface;
 
 final class DocmanFileDownloadController implements DispatchableWithRequest, DispatchableWithProject
 {
@@ -101,7 +106,7 @@ final class DocmanFileDownloadController implements DispatchableWithRequest, Dis
         $file_response->send();
     }
 
-    public function getProject(\HTTPRequest $request, array $variables) : Project
+    public function getProject(array $variables) : Project
     {
         if ($this->item !== null) {
             throw new LogicException(

plugins/docman/include/Upload/Document/DocumentBeingUploadedInformationProvider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2019. All Rights Reserved.
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -22,6 +22,8 @@ declare(strict_types=1);
 
 namespace Tuleap\Docman\Upload\Document;
 
+use PFUser;
+use Tuleap\REST\RESTCurrentUserMiddleware;
 use Tuleap\Tus\TusFileInformation;
 use Tuleap\Tus\TusFileInformationProvider;
 use Tuleap\Upload\FileAlreadyUploadedInformation;
@@ -55,17 +57,18 @@ final class DocumentBeingUploadedInformationProvider implements TusFileInformati
     public function getFileInformation(\Psr\Http\Message\ServerRequestInterface $request) : ?TusFileInformation
     {
         $item_id = $request->getAttribute('id');
-        $user_id = $request->getAttribute('user_id');
 
-        if ($item_id === null || $user_id === null) {
+        if ($item_id === null) {
             return null;
         }
 
         $item_id = (int) $item_id;
+        /** @var PFUser $current_user */
+        $current_user = $request->getAttribute(RESTCurrentUserMiddleware::class);
 
         $document_row = $this->dao->searchDocumentOngoingUploadByItemIDUserIDAndExpirationDate(
             $item_id,
-            $user_id,
+            (int) $current_user->getId(),
             (new \DateTimeImmutable())->getTimestamp()
         );
         if (empty($document_row)) {

plugins/docman/include/Upload/Version/VersionBeingUploadedInformationProvider.php
@@ -22,13 +22,14 @@ declare(strict_types = 1);
 
 namespace Tuleap\Docman\Upload\Version;
 
+use PFUser;
+use Tuleap\REST\RESTCurrentUserMiddleware;
 use Tuleap\Tus\TusFileInformation;
 use Tuleap\Tus\TusFileInformationProvider;
 use Tuleap\Upload\FileBeingUploadedInformation;
 
 class VersionBeingUploadedInformationProvider implements TusFileInformationProvider
 {
-
     /**
      * @var DocumentOnGoingVersionToUploadDAO
      */
@@ -47,7 +48,6 @@ class VersionBeingUploadedInformationProvider implements TusFileInformationProvi
         \Docman_ItemFactory $item_factory,
         VersionUploadPathAllocator $path_allocator
     ) {
-
         $this->dao            = $dao;
         $this->item_factory   = $item_factory;
         $this->path_allocator = $path_allocator;
@@ -56,17 +56,18 @@ class VersionBeingUploadedInformationProvider implements TusFileInformationProvi
     public function getFileInformation(\Psr\Http\Message\ServerRequestInterface $request): ?TusFileInformation
     {
         $version_id = $request->getAttribute('id');
-        $user_id    = $request->getAttribute('user_id');
 
-        if ($version_id === null || $user_id === null) {
+        if ($version_id === null) {
             return null;
         }
 
         $version_id = (int) $version_id;
+        /** @var PFUser $current_user */
+        $current_user = $request->getAttribute(RESTCurrentUserMiddleware::class);
 
         $document_row = $this->dao->searchDocumentVersionOngoingUploadByVersionIDUserIDAndExpirationDate(
             $version_id,
-            (int) $user_id,
+            (int) $current_user->getId(),
             (new \DateTimeImmutable())->getTimestamp()
         );
 

plugins/docman/include/docmanPlugin.class.php
@@ -1273,10 +1273,10 @@ class DocmanPlugin extends Plugin
 
     public function routeUploadsDocmanFile(): FileUploadController
     {
-
         $document_ongoing_upload_dao = new \Tuleap\Docman\Upload\Document\DocumentOngoingUploadDAO();
         $root_path                   = $this->getPluginInfo()->getPropertyValueForName('docman_root');
         $path_allocator              = new \Tuleap\Docman\Upload\Document\DocumentUploadPathAllocator();
+        $user_manager                = UserManager::instance();
         return FileUploadController::build(
             new \Tuleap\Docman\Upload\Document\DocumentDataStore(
                 new \Tuleap\Docman\Upload\Document\DocumentBeingUploadedInformationProvider(
@@ -1302,7 +1302,7 @@ class DocmanPlugin extends Plugin
                     new Docman_ItemDao(),
                     new Docman_FileStorage($root_path),
                     new Docman_MIMETypeDetector(),
-                    UserManager::instance(),
+                    $user_manager,
                     new DBTransactionExecutorWithConnection(DBFactory::getMainTuleapDBConnection())
                 ),
                 new \Tuleap\Docman\Upload\Document\DocumentUploadCanceler(
@@ -1315,10 +1315,10 @@ class DocmanPlugin extends Plugin
 
     public function routeUploadsVersionFile(): FileUploadController
     {
-        $root_path      = $this->getPluginInfo()->getPropertyValueForName('docman_root');
-        $path_allocator = new VersionUploadPathAllocator();
-        $version_to_upload_dao = new DocumentOnGoingVersionToUploadDAO();
-        $event_manager = EventManager::instance();
+        $root_path                = $this->getPluginInfo()->getPropertyValueForName('docman_root');
+        $path_allocator           = new VersionUploadPathAllocator();
+        $version_to_upload_dao    = new DocumentOnGoingVersionToUploadDAO();
+        $event_manager            = EventManager::instance();
         $approval_table_retriever = new ApprovalTableRetriever(new Docman_ApprovalTableFactoriesFactory(), new Docman_VersionFactory());
         return FileUploadController::build(
             new VersionDataStore(

plugins/docman/phpunit/Download/DocmanFileDownloadControllerTest.php
@@ -74,10 +74,9 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $this->item_factory->shouldReceive('getItemFromDb')->andReturn($docman_file);
         $this->project_manager->shouldReceive('getProject')->andReturn(Mockery::mock(Project::class));
 
-        $http_request = Mockery::mock(HTTPRequest::class);
-        $controller->getProject($http_request, ['file_id' => '1']);
+        $controller->getProject(['file_id' => '1']);
         $this->expectException(LogicException::class);
-        $controller->getProject($http_request, ['file_id' => '2']);
+        $controller->getProject(['file_id' => '2']);
     }
 
     public function testProjectCanNotBeFoundWhenItemDoesNotExist() : void
@@ -92,7 +91,7 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $this->item_factory->shouldReceive('getItemFromDb')->andReturn(null);
 
         $this->expectException(NotFoundException::class);
-        $controller->getProject(Mockery::mock(HTTPRequest::class), ['file_id' => '1']);
+        $controller->getProject(['file_id' => '1']);
     }
 
     public function testProjectCanNotBeFoundWhenItemIsLinkedToANonExistingProject() : void
@@ -110,7 +109,7 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $this->project_manager->shouldReceive('getProject')->andReturn(null);
 
         $this->expectException(NotFoundException::class);
-        $controller->getProject(Mockery::mock(HTTPRequest::class), ['file_id' => '1']);
+        $controller->getProject(['file_id' => '1']);
     }
 
     public function testItemMustHaveBeenIdentifiedBeforeProcessingTheRequest() : void
@@ -141,10 +140,9 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $this->project_manager->shouldReceive('getProject')->andReturn(Mockery::mock(Project::class));
 
         $uri_variables =  ['file_id' => '1'];
-        $http_request  = Mockery::mock(HTTPRequest::class);
-        $controller->getProject($http_request, $uri_variables);
+        $controller->getProject($uri_variables);
         $this->expectException(NotFoundException::class);
-        $controller->process($http_request, Mockery::mock(BaseLayout::class), $uri_variables);
+        $controller->process(Mockery::mock(HTTPRequest::class), Mockery::mock(BaseLayout::class), $uri_variables);
     }
 
     public function testDownloadFailsWhenRequestedVersionCannotBeFound() : void
@@ -165,7 +163,7 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $http_request  = Mockery::mock(HTTPRequest::class);
         $http_request->shouldReceive('getCurrentUser')->andReturn(Mockery::mock(PFUser::class));
         $uri_variables =  ['file_id' => '1', 'version_id' => '1'];
-        $controller->getProject($http_request, $uri_variables);
+        $controller->getProject($uri_variables);
 
         $this->response_generator->shouldReceive('generateResponse')->andThrow(new VersionNotFoundException($docman_file, 1));
 
@@ -191,7 +189,7 @@ final class DocmanFileDownloadControllerTest extends TestCase
         $http_request  = Mockery::mock(HTTPRequest::class);
         $http_request->shouldReceive('getCurrentUser')->andReturn(Mockery::mock(PFUser::class));
         $uri_variables =  ['file_id' => '1'];
-        $controller->getProject($http_request, $uri_variables);
+        $controller->getProject($uri_variables);
 
         $this->response_generator->shouldReceive('generateResponse')->andThrow(
             Mockery::mock(FileDownloadException::class)

plugins/docman/phpunit/Upload/Document/DocumentBeingUploadedInformationProviderTest.php
@@ -25,6 +25,9 @@ namespace Tuleap\Docman\Upload\Document;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
+use Tuleap\Http\Server\NullServerRequest;
+use Tuleap\REST\RESTCurrentUserMiddleware;
+use UserManager;
 
 class DocumentBeingUploadedInformationProviderTest extends TestCase
 {
@@ -35,6 +38,9 @@ class DocumentBeingUploadedInformationProviderTest extends TestCase
         $path_allocator = new DocumentUploadPathAllocator();
         $dao            = \Mockery::mock(DocumentOngoingUploadDAO::class);
         $item_factory   = \Mockery::mock(\Docman_ItemFactory::class);
+        $user_manager   = \Mockery::mock(UserManager::class);
+        $current_user   = \Mockery::mock(\PFUser::class);
+        $user_manager->shouldReceive('getCurrentUser')->andReturn($current_user);
         $data_store     = new DocumentBeingUploadedInformationProvider($path_allocator, $dao, $item_factory);
 
         $dao->shouldReceive('searchDocumentOngoingUploadByItemIDUserIDAndExpirationDate')->andReturns([
@@ -43,12 +49,13 @@ class DocumentBeingUploadedInformationProviderTest extends TestCase
         ]);
         $item_factory->shouldReceive('getItemFromDb')->andReturns(null);
 
-        $request = \Mockery::mock(ServerRequestInterface::class);
-        $item_id = 12;
-        $request->shouldReceive('getAttribute')->with('id')->andReturns((string) $item_id);
-        $request->shouldReceive('getAttribute')->with('user_id')->andReturns('102');
+        $item_id      = 12;
+        $current_user = \Mockery::mock(\PFUser::class);
+        $current_user->shouldReceive('getID')->andReturn('102');
+        $server_request = (new NullServerRequest())->withAttribute('id', (string) $item_id)
+            ->withAttribute(RESTCurrentUserMiddleware::class, $current_user);
 
-        $file_information = $data_store->getFileInformation($request);
+        $file_information = $data_store->getFileInformation($server_request);
 
         $this->assertSame($item_id, $file_information->getID());
         $this->assertSame(123456, $file_information->getLength());
@@ -68,12 +75,13 @@ class DocumentBeingUploadedInformationProviderTest extends TestCase
         ]);
         $item_factory->shouldReceive('getItemFromDb')->andReturns(\Mockery::mock(\Docman_Item::class));
 
-        $request = \Mockery::mock(ServerRequestInterface::class);
-        $item_id = 12;
-        $request->shouldReceive('getAttribute')->with('id')->andReturns((string) $item_id);
-        $request->shouldReceive('getAttribute')->with('user_id')->andReturns('102');
+        $item_id      = 12;
+        $current_user = \Mockery::mock(\PFUser::class);
+        $current_user->shouldReceive('getID')->andReturn('102');
+        $server_request = (new NullServerRequest())->withAttribute('id', (string) $item_id)
+            ->withAttribute(RESTCurrentUserMiddleware::class, $current_user);
 
-        $file_information = $data_store->getFileInformation($request);
+        $file_information = $data_store->getFileInformation($server_request);
 
         $this->assertSame($item_id, $file_information->getID());
         $this->assertSame(123456, $file_information->getLength());
@@ -98,7 +106,7 @@ class DocumentBeingUploadedInformationProviderTest extends TestCase
     {
         $dao           = \Mockery::mock(DocumentOngoingUploadDAO::class);
         $item_factory  = \Mockery::mock(\Docman_ItemFactory::class);
-        $data_store = new DocumentBeingUploadedInformationProvider(
+        $data_store    = new DocumentBeingUploadedInformationProvider(
             new DocumentUploadPathAllocator(),
             $dao,
             $item_factory
@@ -107,11 +115,12 @@ class DocumentBeingUploadedInformationProviderTest extends TestCase
         $dao->shouldReceive('searchDocumentOngoingUploadByItemIDUserIDAndExpirationDate')->andReturns([]);
         $item_factory->shouldReceive('getItemFromDb')->andReturns(null);
 
-        $request = \Mockery::mock(ServerRequestInterface::class);
-        $request->shouldReceive('getAttribute')->with('id')->andReturns('12');
-        $request->shouldReceive('getAttribute')->with('user_id')->andReturns('102');
+        $current_user = \Mockery::mock(\PFUser::class);
+        $current_user->shouldReceive('getID')->andReturn('102');
+        $server_request = (new NullServerRequest())->withAttribute('id', '12')
+            ->withAttribute(RESTCurrentUserMiddleware::class, $current_user);
 
 
-        $this->assertNull($data_store->getFileInformation($request));
+        $this->assertNull($data_store->getFileInformation($server_request));
     }
 }

commit 307882213c46eb1ffb154dd08a237e5e81952649

Git repositories can not be browsed from the web UI
Issue introduced by commit ff418fb4c30a42d49326ea538ba584f6b14441ea.

Part of request #13133: A REST API user can not download a document manager file

Change-Id: I75454603d253145ba4e86e46e00e034ad8f39ebf

plugins/document/include/Tree/DocumentTreeUnderConstructionController.php
@@ -43,7 +43,7 @@ class DocumentTreeUnderConstructionController implements DispatchableWithRequest
 
     public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
     {
-        $project = $this->getProject($request, $variables);
+        $project = $this->getProject($variables);
 
         $user = $request->getCurrentUser();
         $user->setPreference("plugin_document_set_display_under_construction_modal_" . $project->getID(), true);

plugins/git/include/Git/GitRepositoryBrowserController.php
@@ -115,7 +115,7 @@ class GitRepositoryBrowserController implements DispatchableWithRequest, Dispatc
      */
     public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
     {
-        $project = $this->getProject($request, $variables);
+        $project = $this->getProject($variables);
         if (! $project->usesService(gitPlugin::SERVICE_SHORTNAME)) {
             throw new NotFoundException(dgettext("tuleap-git", "Git service is disabled."));
         }