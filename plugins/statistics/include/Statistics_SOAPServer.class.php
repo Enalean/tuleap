Enalean/tuleap

commit dd1bfc200e29db80eb740e0df68620c08bfd743b

SOAP API can handle accesses to resources in private projects without restricted users
Restricted users that are members of a private project that do not include
restricted users should be able to access resources of this project through
the SOAP API.

This contribution harden the existing behavior and add missing checks.

This is part of story #13237: Prevent restricted to be added to private projects

Change-Id: I46ae5389a911d02724778938ff5ff45f74ed776e

plugins/statistics/include/Statistics_SOAPServer.class.php
@@ -24,7 +24,7 @@
 class Statistics_SOAPServer {
     
     /**
-     * @var SOAP_RequestValidator
+     * @var SOAPRequestValidator
      */
     private $soap_request_validator;
 
@@ -38,7 +38,7 @@ class Statistics_SOAPServer {
      */
     private $project_quota_manager;
 
-    public function __construct(SOAP_RequestValidator $soap_request_validator, Statistics_DiskUsageManager $disk_usage_manager, ProjectQuotaManager $project_quota_manager) {
+    public function __construct(SOAPRequestValidator $soap_request_validator, Statistics_DiskUsageManager $disk_usage_manager, ProjectQuotaManager $project_quota_manager) {
         $this->soap_request_validator = $soap_request_validator;
         $this->disk_usage_manager     = $disk_usage_manager;
         $this->project_quota_manager  = $project_quota_manager;

plugins/statistics/include/statisticsPlugin.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - Present. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2008. All Rights Reserved.
  *
  * Originally written by Manuel VACELET, 2008
@@ -27,8 +27,11 @@ use Tuleap\Project\Admin\Navigation\NavigationDropdownItemPresenter;
 use Tuleap\Project\Admin\Navigation\NavigationPresenter;
 use Tuleap\Project\Admin\Navigation\NavigationPresenterBuilder;
 use Tuleap\Project\Admin\ProjectDetailsPresenter;
+use Tuleap\Project\ProjectAccessChecker;
 use Tuleap\Project\Quota\ProjectQuotaInformation;
 use Tuleap\Project\Quota\ProjectQuotaRequester;
+use Tuleap\Project\RestrictedUserCanAccessProjectVerifier;
+use Tuleap\SOAP\SOAPRequestValidatorImplementation;
 use Tuleap\SVN\DiskUsage\Collector as SVNCollector;
 use Tuleap\SVN\DiskUsage\Retriever as SVNRetriever;
 use Tuleap\CVS\DiskUsage\Retriever as CVSRetriever;
@@ -263,11 +266,17 @@ class StatisticsPlugin extends Plugin
     }
 
     private function instantiateSOAPServer($uri, $service_class) {
-        require_once 'common/soap/SOAP_RequestValidator.class.php';
         require_once 'Statistics_DiskUsageManager.class.php';
         $user_manager           = UserManager::instance();
         $project_manager        = ProjectManager::instance();
-        $soap_request_validator = new SOAP_RequestValidator($project_manager, $user_manager);
+        $soap_request_validator = new SOAPRequestValidatorImplementation(
+            $project_manager,
+            $user_manager,
+            new ProjectAccessChecker(
+                new PermissionsOverrider_PermissionsOverriderManager(),
+                new RestrictedUserCanAccessProjectVerifier()
+            )
+        );
         $disk_usage_manager     = $this->getDiskUsageManager();
         $project_quota_manager  = new ProjectQuotaManager();
 

src/common/Project/ProjectManager.class.php
@@ -628,7 +628,7 @@ class ProjectManager
      *
      * @param Integer $groupId    Id of the project
      * @param String  $method     Name of the callback method
-     * @param Boolean $byUnixName Optional, Search the project by its unix name instead of its id
+     * @param bool $byUnixName Optional, Search the project by its unix name instead of its id
      *
      * @return Project or SoapFault
      */

src/common/frs/FRSReleaseFactory.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean 2017 - 2019. All rights reserved
+ * Copyright (c) Enalean 2017 - Present. All rights reserved
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Tuleap.
@@ -25,6 +25,8 @@ use Tuleap\FRS\UploadedLinkDeletor;
 use Tuleap\FRS\UploadedLinksDao;
 use Tuleap\Mail\MailFilter;
 use Tuleap\Mail\MailLogger;
+use Tuleap\Project\ProjectAccessChecker;
+use Tuleap\Project\RestrictedUserCanAccessProjectVerifier;
 
 require_once ('FRSRelease.class.php');
 require_once ('common/dao/FRSReleaseDao.class.php');
@@ -351,7 +353,10 @@ class FRSReleaseFactory {
         return $this->_getFRSPackageFactory()->userCanAdmin($user, $project_id);
     }
 
-    function userCanRead($group_id, $package_id, $release_id, $user_id = false)
+    /**
+     * @return bool
+     */
+    public function userCanRead($group_id, $package_id, $release_id, $user_id = false)
     {
         $um = $this->getUserManager();
         if (! $user_id) {
@@ -360,19 +365,41 @@ class FRSReleaseFactory {
             $user = $um->getUserById($user_id);    
         }
 
+        if ($user === null) {
+            return false;
+        }
+
         if ($this->userCanAdmin($user, $group_id)) {
             return true;
-        } else {
-            $pm = $this->getPermissionsManager();
-            if ($pm->isPermissionExist($release_id, FRSRelease::PERM_READ)) {
-                $ok = $pm->userHasPermission($release_id, FRSRelease::PERM_READ, $user->getUgroups($group_id, array()));
-            } else {
-                $frspf = $this->_getFRSPackageFactory();
-                $ok    = $frspf->userCanRead($group_id, $package_id, $user->getId());
-            }
-
-            return $ok;
         }
+
+        $project = ProjectManager::instance()->getProject($group_id);
+        if ($project === null) {
+            return false;
+        }
+
+        $project_access_checker = new ProjectAccessChecker(
+            PermissionsOverrider_PermissionsOverriderManager::instance(),
+            new RestrictedUserCanAccessProjectVerifier()
+        );
+
+        try {
+            $project_access_checker->checkUserCanAccessProject($user, $project);
+        } catch (Project_AccessException $exception) {
+            return false;
+        }
+
+        $pm = $this->getPermissionsManager();
+        if ($pm->isPermissionExist($release_id, FRSRelease::PERM_READ)) {
+            return $pm->userHasPermission(
+                $release_id,
+                FRSRelease::PERM_READ,
+                $user->getUgroups($project->getID(), array())
+            );
+        }
+
+        $frspf = $this->_getFRSPackageFactory();
+        return $frspf->userCanRead($project->getID(), $package_id, $user->getId());
     }
 
     /** 

src/common/soap/SOAPRequestValidator.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -37,16 +37,11 @@ use Project;
 interface SOAPRequestValidator
 {
     /**
-     *
      * @see session_continue
-     *
-     * @param String $session_key
-     *
-     * @return PFUser
      */
-    public function continueSession($session_key);
+    public function continueSession(string $session_key) : PFUser;
 
-    public function assertUserCanAccessProject(PFUser $user, Project $project);
+    public function assertUserCanAccessProject(PFUser $user, Project $project) : void;
 
     public function getProjectById($project_id, $method_name);
 

src/common/soap/SOAPRequestValidatorImplementation.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,18 +17,28 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+namespace Tuleap\SOAP;
+
+use Exception;
+use PFUser;
+use Project;
+use Project_AccessException;
+use ProjectManager;
+use Tuleap\Project\ProjectAccessChecker;
+use UserManager;
+
 /**
  * This class is a convenient proxy for parameters checking when doing SOAP server
- * 
+ *
  * It's meant to assert that given parameters are valid and returns corresponding
  * objects.
  * If there are errors, exception are thrown.
- * 
+ *
  * It manipulates:
  * - user session
  * - project
  */
-class SOAP_RequestValidator implements \Tuleap\SOAP\SOAPRequestValidator
+class SOAPRequestValidatorImplementation implements SOAPRequestValidator
 {
     /**
      * @var ProjectManager
@@ -39,43 +49,46 @@ class SOAP_RequestValidator implements \Tuleap\SOAP\SOAPRequestValidator
      * @var UserManager
      */
     private $user_manager;
-
-    public function __construct(ProjectManager $project_manager, UserManager $user_manager) {
-        $this->project_manager = $project_manager;
-        $this->user_manager    = $user_manager;
-    }
-    
     /**
-     *
-     * @see session_continue
-     * 
-     * @param String $session_key
-     * 
-     * @return PFUser
+     * @var ProjectAccessChecker
      */
-    public function continueSession($session_key) {
+    private $project_access_checker;
+
+    public function __construct(
+        ProjectManager $project_manager,
+        UserManager $user_manager,
+        ProjectAccessChecker $project_access_checker
+    ) {
+        $this->project_manager        = $project_manager;
+        $this->user_manager           = $user_manager;
+        $this->project_access_checker = $project_access_checker;
+    }
+
+    public function continueSession(string $session_key) : PFUser
+    {
         $user = $this->user_manager->getCurrentUser($session_key);
         if ($user->isLoggedIn()) {
             return $user;
         }
-        throw new Exception('Invalid session', '3001');
+        throw new Exception('Invalid session', 3001);
     }
 
-    public function assertUserCanAccessProject(PFUser $user, Project $project) {
-        if (($project->isPublic() && $user->isRestricted() && ! $user->isMember($project->getGroupId())) ||
-            (! $project->isPublic() && ! $user->isMember($project->getGroupId()))) {
-            throw new Exception('User do not have access to the project', '3002');
+    public function assertUserCanAccessProject(PFUser $user, Project $project) : void
+    {
+        try {
+            $this->project_access_checker->checkUserCanAccessProject($user, $project);
+        } catch (Project_AccessException $exception) {
+            throw new Exception('User do not have access to the project', 3002);
         }
     }
-    
-    public function getProjectById($project_id, $method_name) {
+
+    public function getProjectById($project_id, $method_name)
+    {
         return $this->project_manager->getGroupByIdForSoap($project_id, $method_name);
     }
-    
-    public function getProjectByName($project_name, $method_name) {
+
+    public function getProjectByName($project_name, $method_name)
+    {
         return $this->project_manager->getGroupByIdForSoap($project_name, $method_name, true);
     }
-
 }
-
-?>

src/common/svn/SVN_SOAPServer.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,7 +21,6 @@ use Tuleap\SOAP\SOAPRequestValidator;
 
 require_once 'SVN_RepositoryListing.class.php';
 require_once 'SVN_LogFactory.class.php';
-require_once 'common/soap/SOAP_RequestValidator.class.php';
 require_once 'common/date/TimeInterval.class.php';
 
 /**
@@ -29,7 +28,7 @@ require_once 'common/date/TimeInterval.class.php';
  */
 class SVN_SOAPServer {
     /**
-     * @var SOAP_RequestValidator 
+     * @var SOAPRequestValidator
      */
     private $soap_request_validator;
     

src/www/soap/common/group.php
@@ -1,5 +1,9 @@
 <?php
 
+use Tuleap\Project\ProjectAccessChecker;
+use Tuleap\Project\RestrictedUserCanAccessProjectVerifier;
+use Tuleap\SOAP\SOAPRequestValidatorImplementation;
+
 require_once('user.php');
 require_once('common/include/GroupFactory.class.php');
 
@@ -287,11 +291,16 @@ function getGroupUgroups($sessionKey, $group_id) {
 
 function getProjectGroupsAndUsers($session_key, $group_id) {
     try {
-        require_once 'common/soap/SOAP_RequestValidator.class.php';
-
         $project_manager        = ProjectManager::instance();
         $user_manager           = UserManager::instance();
-        $soap_request_validator = new SOAP_RequestValidator($project_manager, $user_manager);
+        $soap_request_validator = new SOAPRequestValidatorImplementation(
+            $project_manager,
+            $user_manager,
+            new ProjectAccessChecker(
+                new PermissionsOverrider_PermissionsOverriderManager(),
+                new RestrictedUserCanAccessProjectVerifier()
+            )
+        );
 
         $user    = $soap_request_validator->continueSession($session_key);
         $project = $soap_request_validator->getProjectById($group_id, 'getProjectGroupsAndUsers');

commit f8644ef48973e9ce700097ebbad9b9c031ba030d

Crash when accessing statistics plugin data via the SOAP API
Issue introduced by dd1bfc200e29db80eb740e0df68620c08bfd743b.

This is part of story #13237: Prevent restricted to be added to private projects

Change-Id: I67b08351ebad0fca9488869a284ccc1f273c0706

plugins/statistics/include/Statistics_SOAPServer.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -17,7 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
+use Tuleap\SOAP\SOAPRequestValidator;
+
 /**
  * Wrapper for statistics related SOAP methods
  */
@@ -34,7 +36,7 @@ class Statistics_SOAPServer {
     private $disk_usage_manager;
     
     /**
-     * @var Statistics_ProjectQuotaManager
+     * @var ProjectQuotaManager
      */
     private $project_quota_manager;
 
@@ -108,7 +110,7 @@ class Statistics_SOAPServer {
     }
     
     private function assertUserCanAccessProject(PFUser $user, $group_id) {
-        $project = $this->soap_request_validator->getProjectById($group_id);
+        $project = $this->soap_request_validator->getProjectById($group_id, 'statistics');
         $this->soap_request_validator->assertUserCanAccessProject($user, $project);
     }
     
@@ -128,4 +130,3 @@ class Statistics_SOAPServer {
         return ($user->isSuperUser() || $user->isMember($group_id, 'A'));
     }
 }
-?>