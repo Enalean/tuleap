Enalean/tuleap

commit d656fc41ed6d91dc42d3fd0387ac1ba3597c2942

request #12898 Permissions screen in docman not accurate after modification
Change-Id: Ie8bd841dc9c00a899f47f55fe3c3b92c3eac3f4a

plugins/docman/include/Docman_Actions.class.php
@@ -1042,7 +1042,8 @@ class Docman_Actions extends Actions {
     * top item. And for each child node, there is a callback to
     * Docman_Actions::recursivePermission (see each method for details).
     */
-    public function permissions($params) {
+    public function permissions($params)
+    {
         $id    = isset($params['id'])    ? $params['id']    : $this->_controler->request->get('id');
         $force = isset($params['force']) ? $params['force'] : false;
         if ($id && $this->_controler->request->exist('permissions')) {
@@ -1057,6 +1058,11 @@ class Docman_Actions extends Actions {
                     'user'     => $user,
                 )
             );
+
+            $project_id = (int) $this->_controler->request->get('group_id');
+            $url        = DOCMAN_BASE_URL . "/?group_id=".urlencode($project_id)."&action=details&id=".urlencode($id)."&section=permissions";
+
+            $GLOBALS['Response']->redirect($url);
         }
     }
 

commit 0ad36b9318f376547141d03995c8f3f52c44b5d4

request #13261: SOAP endpoint updateDocmanFile incorrectly redirect to the login page when updating the permissions
The user should only be redirected when he/she uses the web UI.

Issue has been introduced by d656fc41ed6d91dc42d3fd0387ac1ba3597c2942.

Change-Id: I2387ee0c0ff653ee8304385c6c95e2f735e25d0e

plugins/docman/include/Docman_Actions.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © Enalean, 2011 - 2018. All Rights Reserved.
+ * Copyright © Enalean, 2011 - Present. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
  *
  * Originally written by Manuel Vacelet, 2006
@@ -1055,10 +1055,12 @@ class Docman_Actions extends Actions {
                 )
             );
 
-            $project_id = (int) $this->_controler->request->get('group_id');
-            $url        = DOCMAN_BASE_URL . "/?group_id=".urlencode($project_id)."&action=details&id=".urlencode($id)."&section=permissions";
-
-            $GLOBALS['Response']->redirect($url);
+            $this->_controler->view = 'RedirectAfterCrud';
+            $this->_controler->_viewParams['default_url_params'] = [
+                'action'  => 'details',
+                'section' => 'permissions',
+                'id'      => $id,
+            ];
         }
     }
 

plugins/docman/include/view/soap/Docman_View_SOAP_RedirectAfterCrud.class.php
@@ -1,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+final class Docman_View_SOAP_RedirectAfterCrud extends Docman_View_View // phpcs:ignore
+{
+}