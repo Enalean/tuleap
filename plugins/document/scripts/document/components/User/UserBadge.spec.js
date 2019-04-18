46d8ebec3e52b5b070b76cee5a19c2093bf4f466
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-04-18 17:31:57 +0200

    request #13282 add missing tests for userBadge component
    functionnal changes.
    
    Change-Id: I3756a325b70365fe40e4db3a54acebf2497a04dd

---
+++ plugins/document/scripts/document/components/User/UserBadge.spec.js
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import UserBadge from "./UserBadge.vue";
+
+import localVue from "../../helpers/local-vue.js";
+import { createStoreMock } from "../../helpers/store-wrapper.spec-helper.js";
+
+describe("UserBadge", () => {
+    let user_badge_factory;
+    beforeEach(() => {
+        const store_options = {};
+
+        const store = createStoreMock(store_options);
+
+        user_badge_factory = (props = {}) => {
+            return shallowMount(UserBadge, {
+                localVue,
+                propsData: { ...props },
+                mocks: { $store: store }
+            });
+        };
+    });
+
+    it(`Given user has avatar
+        When we display the user badge
+        Then its avatar is displayed`, () => {
+        const wrapper = user_badge_factory({
+            user: {
+                id: 1,
+                has_avatar: true,
+                user_url: "https://example.com/avatar",
+                is_anonymous: false
+            }
+        });
+
+        expect(wrapper.find("[data-test=document-user-avatar]").exists()).toBeTruthy();
+    });
+
+    it(`Given user hasn't an avatar
+        When we display the user badge
+        Then whe should not display it`, () => {
+        const wrapper = user_badge_factory({
+            user: {
+                id: 1,
+                has_avatar: false,
+                user_url: "https://example.com/avatar",
+                is_anonymous: false
+            }
+        });
+
+        expect(wrapper.find("[data-test=document-user-avatar]").exists()).toBeFalsy();
+    });
+
+    it(`Given user is connected
+        When we display the user badge
+        Then we should be able to click on its name`, () => {
+        const wrapper = user_badge_factory({
+            user: {
+                id: 1,
+                has_avatar: false,
+                user_url: "https://example.com/avatar",
+                is_anonymous: false
+            }
+        });
+
+        expect(wrapper.find("[data-test=document-user-profile-link]").exists()).toBeTruthy();
+    });
+
+    it(`Given user is annonymous
+        When we display the user badge
+        Then we should be able to click on its name`, () => {
+        const wrapper = user_badge_factory({
+            user: {
+                id: 1,
+                has_avatar: false,
+                user_url: "https://example.com/avatar",
+                is_anonymous: true
+            }
+        });
+
+        expect(wrapper.find("[data-test=document-user-profile-link]").exists()).toBeFalsy();
+    });
+});
