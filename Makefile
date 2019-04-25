Enalean/tuleap

commit 47107bee7ca7045b9f6f566252acf2f2f6bdd8b8

Move to Webpack
Everything is supposed to work as before.

Part of request #13299: Move all SCSS/CSS build process to Webpack

Change-Id: Idc58d5f547e72d197f50d18f3f7517111842e126

Makefile
@@ -29,6 +29,8 @@ $(RPM_TMP)/SPECS/%.spec: $(BASE_DIR)/%.spec
 
 $(RPM_TMP)/SOURCES/$(NAME_VERSION).tar.gz: $(RPM_TMP)
 	[ -h $(RPM_TMP)/SOURCES/$(NAME_VERSION) ] || ln -s $(BASE_DIR) $(RPM_TMP)/SOURCES/$(NAME_VERSION)
+	[ ! -d $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets ] || rm -rf $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets
+	cp -ar $(BASE_DIR)/../../src/www/assets/botmattermost $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets
 	cd $(RPM_TMP)/SOURCES && \
 		find $(NAME_VERSION)/ \(\
 		-path $(NAME_VERSION)/tests -o\

tuleap-plugin-botmattermost.spec
@@ -28,8 +28,10 @@ find www/themes -name '*.scss' | xargs rm -f
 %install
 %{__rm} -rf $RPM_BUILD_ROOT
 
+%{__install} -m 755 -d $RPM_BUILD_ROOT/%{_datadir}/tuleap/src/www/assets
 %{__install} -m 755 -d $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost
 %{__cp} -ar db include site-content template README.mkd VERSION $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost
+%{__cp} -ar assets $RPM_BUILD_ROOT/%{_datadir}/tuleap/src/www/assets/botmattermost
 
 # www
 %{__mkdir} -p $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost/www

webpack.common.js
@@ -1,0 +1,50 @@
+/*
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const path = require("path");
+const webpack_configurator = require("../../tools/utils/scripts/webpack-configurator.js");
+
+let entry_points = {
+    'modals': "./scripts/modals.js"
+};
+
+const colors = ["blue", "green", "grey", "orange", "purple", "red"];
+for (const color of colors) {
+    entry_points[`style-${color}`] = `./themes/BurningParrot/css/style-${color}.scss`;
+}
+
+module.exports = [
+    {
+        entry: entry_points,
+        context: path.resolve(__dirname),
+        output: webpack_configurator.configureOutput(path.resolve(__dirname, "../../src/www/assets/botmattermost/")),
+        module: {
+            rules: [
+                webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+                webpack_configurator.rule_scss_loader,
+                webpack_configurator.rule_css_assets
+            ]
+        },
+        plugins: [
+            webpack_configurator.getCleanWebpackPlugin(),
+            webpack_configurator.getManifestPlugin(),
+            ...webpack_configurator.getCSSExtractionPlugins()
+        ]
+    }
+];

Gruntfile.js
@@ -1,34 +1,0 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        sass: {
-            dist: {
-                options: {
-                    sourcemap: "none",
-                    style: "compressed",
-                    loadPath: ["/tuleap/plugins/botmattermost/www/themes/BurningParrot/css"]
-                },
-                files: {
-                    "www/themes/BurningParrot/css/style-blue.css":
-                        "www/themes/BurningParrot/css/style-blue.scss",
-                    "www/themes/BurningParrot/css/style-green.css":
-                        "www/themes/BurningParrot/css/style-green.scss",
-                    "www/themes/BurningParrot/css/style-grey.css":
-                        "www/themes/BurningParrot/css/style-grey.scss",
-                    "www/themes/BurningParrot/css/style-orange.css":
-                        "www/themes/BurningParrot/css/style-orange.scss",
-                    "www/themes/BurningParrot/css/style-purple.css":
-                        "www/themes/BurningParrot/css/style-purple.scss",
-                    "www/themes/BurningParrot/css/style-red.css":
-                        "www/themes/BurningParrot/css/style-red.scss"
-                }
-            }
-        }
-    });
-
-    grunt.loadNpmTasks("grunt-contrib-sass");
-
-    grunt.registerTask("build", ["sass"]);
-
-    grunt.registerTask("default", ["build"]);
-};

include/botmattermostPlugin.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016-2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -22,6 +22,8 @@ use Tuleap\BotMattermost\Bot\BotDao;
 use Tuleap\BotMattermost\Bot\BotFactory;
 use Tuleap\BotMattermost\Controller\AdminController;
 use Tuleap\BurningParrotCompatiblePageEvent;
+use Tuleap\Layout\CssAssetWithoutVariantDeclinaisons;
+use Tuleap\Layout\IncludeAssets;
 
 require_once 'constants.php';
 require_once 'autoload.php';
@@ -61,7 +63,11 @@ class BotMattermostPlugin extends Plugin
     public function burning_parrot_get_javascript_files($params)
     {
         if (strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0) {
-            $params['javascript_files'][] = $this->getThemePath() .'/js/modals.js';
+            $asset   = new IncludeAssets(
+                __DIR__ . '/../../../src/www/assets/botmattermost/',
+                '/assets/botmattermost'
+            );
+            $params['javascript_files'][] = $asset->getFileURL('modals.js');
         }
     }
 
@@ -69,7 +75,11 @@ class BotMattermostPlugin extends Plugin
     {
         if (strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0) {
             $variant = $params['variant'];
-            $params['stylesheets'][] = $this->getThemePath() .'/css/style-'.$variant->getName().'.css';
+            $asset   = new IncludeAssets(
+                __DIR__ . '/../../../src/www/assets/botmattermost/',
+                '/assets/botmattermost'
+            );
+            $params['stylesheets'][] = $asset->getFileURL('style-'.$variant->getName().'.css');
         }
     }
 

themes/BurningParrot/css/style-blue.scss
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Enalean, 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2017-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -17,6 +17,6 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-@import '../../../../../../src/www/themes/common/tlp/src/scss/variables/variables-red';
+@import '../../src/www/themes/common/tlp/src/scss/variables/variables-blue';
 
 @import 'bot_configuration';

commit 1d5d5bfe52c2b031aef86534e2ab81210736af63

Static assets must be included in the RPM package
Issue introduced by 47107bee7ca7045b9f6f566252acf2f2f6bdd8b8.

Part of request #13299: Move all SCSS/CSS build process to Webpack

Change-Id: I95db3cf4ddbcc5f75aabcc0436ffce9dc3ae8b1b

Makefile
@@ -1,3 +1,4 @@
+SHELL := /bin/bash
 RPM_TMP=${HOME}/rpmbuild
 PKG_NAME=tuleap-plugin-botmattermost
 VERSION=$(shell LANG=C cat VERSION)
@@ -27,13 +28,19 @@ $(RPM_TMP)/SPECS/%.spec: $(BASE_DIR)/%.spec
 		sed -e 's/@@RELEASE@@/$(RELEASE)/g' \
 		> $@
 
-$(RPM_TMP)/SOURCES/$(NAME_VERSION).tar.gz: $(RPM_TMP)
+.PHONY: build
+build:
+	cd /build/src && npm install && \
+    cd /build/src/plugins/botmattermost && npm install && npm run build
+
+$(RPM_TMP)/SOURCES/$(NAME_VERSION).tar.gz: build $(RPM_TMP)
 	[ -h $(RPM_TMP)/SOURCES/$(NAME_VERSION) ] || ln -s $(BASE_DIR) $(RPM_TMP)/SOURCES/$(NAME_VERSION)
 	[ ! -d $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets ] || rm -rf $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets
 	cp -ar $(BASE_DIR)/../../src/www/assets/botmattermost $(RPM_TMP)/SOURCES/$(NAME_VERSION)/assets
 	cd $(RPM_TMP)/SOURCES && \
 		find $(NAME_VERSION)/ \(\
 		-path $(NAME_VERSION)/tests -o\
+		-name 'node_modules' -o\
 		-name '*.spec' -o\
 		-name 'Makefile' -o\
 		-name ".git" -o\
@@ -53,5 +60,7 @@ $(RPM_TMP):
 docker-run:
 	@[ -n "$(GID)" -a -n "$(UID)" ] || (echo "*** ERROR: UID or GID are missing" && false)
 	useradd -d /build -m build
-	su --login --command "make -C /tuleap/plugins/botmattermost all RELEASE=$(RELEASE) DIST=.$(DIST)" build
+	pushd /tuleap && git checkout-index -a --prefix=/build/src/ && popd
+	cp -Rf /plugin/ /build/src/plugins/botmattermost && chown -R build /build/src
+	su --login --command "make -C /build/src/plugins/botmattermost all RELEASE=$(RELEASE) DIST=.$(DIST)" build
 	install -o $(UID) -g $(GID) -m 0644 /build/rpmbuild/RPMS/noarch/*.rpm /output

build-rpm.sh
@@ -30,4 +30,4 @@ if [[ "$OS" = 'centos:7' ]]; then
 fi
 
 docker build -t $DOCKERIMAGE rpm
-docker run --rm -v $TULEAP_PATH:/tuleap -v $PWD:/tuleap/plugins/botmattermost -v $WORKSPACE:/output -e UID=`id -u` -e GID=`id -g` -e RELEASE=$RELEASE -e DIST=$DIST $DOCKERIMAGE
+docker run --rm -v "$TULEAP_PATH":/tuleap:ro -v $PWD:/plugin:ro -v "$WORKSPACE":/output -e UID=`id -u` -e GID=`id -g` -e RELEASE=$RELEASE -e DIST=$DIST $DOCKERIMAGE

rpm/Dockerfile
@@ -1,8 +1,7 @@
 FROM centos:7
 
 RUN yum install -y make rpm-build tar git fontconfig epel-release && \
-    yum install -y nodejs npm rubygem-sass
+    yum install -y nodejs npm
 
-WORKDIR /tuleap/plugins/botmattermost
-VOLUME ["/tuleap/plugins/botmattermost"]
+WORKDIR /plugin
 CMD make docker-run

tuleap-plugin-botmattermost.spec
@@ -21,9 +21,6 @@ Bot Mattermost management for Tuleap
 
 
 %build
-npm install
-npm run build
-find www/themes -name '*.scss' | xargs rm -f
 
 %install
 %{__rm} -rf $RPM_BUILD_ROOT
@@ -36,7 +33,7 @@ find www/themes -name '*.scss' | xargs rm -f
 # www
 %{__mkdir} -p $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost/www
 %{__mkdir} -p $RPM_BUILD_ROOT/%{_sysconfdir}/logrotate.d
-%{__cp} -ar www/index.php www/themes www/admin $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost/www
+%{__cp} -ar www/index.php www/admin $RPM_BUILD_ROOT/%{_datadir}/tuleap/plugins/botmattermost/www
 %{__cp} -ar %{name}.conf $RPM_BUILD_ROOT/%{_sysconfdir}/logrotate.d
 
 %pre
@@ -48,6 +45,7 @@ find www/themes -name '*.scss' | xargs rm -f
 %files
 %defattr(-,root,root,-)
 %{_datadir}/tuleap/plugins/botmattermost
+%{_datadir}/tuleap/src/www/assets/botmattermost
 %config(noreplace) %{_sysconfdir}/logrotate.d/%{name}.conf
 
 

webpack.common.js
@@ -37,8 +37,7 @@ module.exports = [
         module: {
             rules: [
                 webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
-                webpack_configurator.rule_scss_loader,
-                webpack_configurator.rule_css_assets
+                webpack_configurator.rule_scss_loader
             ]
         },
         plugins: [