3773724e7277ed0d249b2b2c6278ecf893ef8b74
Manuel VACELET <manuel.vacelet@enalean.com>
2019-04-26 11:04:01 +0200

    Convert EventManager tests to phpunit
    Part of request #13314 Tuleap EventManager should be PSR-14 compliant
    
    Change-Id: Idfe412469f63c0d84451dab1386236fc598cde0a

--- tests/phpunit/common/event/EventManagerTest.php
+++ tests/phpunit/common/event/EventManagerTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -16,14 +16,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- *
- * @codingStandardsIgnoreFile
+
  */
 
 use PHPUnit\Framework\TestCase;
 
-class EventManagerTest extends TestCase
+class EventManagerTest extends TestCase // phpcs:ignore
 {
+    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
     /**
      * @test
@@ -42,5 +42,86 @@ class EventManagerTest extends TestCase
         );
 
         $event_manager->processEvent('foo', ['stuff' => 'bar']);
+    }
+
+    public function testSingleton()
+    {
+        $this->assertEquals(
+            EventManager::instance(),
+            EventManager::instance()
+        );
+        $this->assertInstanceOf(EventManager::class, EventManager::instance());
+    }
+
+    public function testProcessEvent1()
+    {
+        $params = [ 'foo' => 'bar' ];
+
+        //The listeners
+        $l1 = Mockery::mock();
+        $l1->shouldReceive('doSomething')->with($params)->once();
+        $l2 = Mockery::mock();
+        $l2->shouldReceive('doSomething')->with($params)->once();
+        $l3 = Mockery::mock();
+        $l3->shouldNotReceive('doSomethingElse');
+
+
+        //The events
+        $e1 = 'event1';
+        $e2 = 'event2';
+
+        //The event Manager
+        $m = new EventManager();
+
+        //We register the listeners for the type
+        $m->addListener($e1, $l1, 'doSomething', false);
+        $m->addListener($e1, $l2, 'doSomething', false);
+        $m->addListener($e2, $l3, 'doSomethingElse', true);
+
+        //We process event
+        $m->processEvent($e1, $params);
+    }
+
+    public function testProcessEvent2()
+    {
+        $params = [ 'foo' => 'bar' ];
+
+        //The listeners
+        $l1 = Mockery::mock();
+        $l1->shouldNotReceive('doSomething');
+        $l2 = Mockery::mock();
+        $l2->shouldNotReceive('doSomething');
+        $l3 = Mockery::mock();
+        $l3->shouldReceive('doSomethingElse')->with('event2', $params)->once();
+
+        //The events
+        $e1 = 'event1';
+        $e2 = 'event2';
+
+        //The event Manager
+        $m = new EventManager();
+
+        //We register the listeners for the type
+        $m->addListener($e1, $l1, 'doSomething', false);
+        $m->addListener($e1, $l2, 'doSomething', false);
+        $m->addListener($e2, $l3, 'doSomethingElse', true);
+
+        //We process event
+        $m->processEvent($e2, $params);
+    }
+
+    public function testItCanSendAnEventObjectInsteadOfStringPlusParams()
+    {
+        $event = new class {
+            public const NAME = 'doSomething';
+        };
+
+        $listener = Mockery::mock();
+        $listener->shouldReceive('doSomething')->with($event)->once();
+
+        $event_manager = new EventManager();
+        $event_manager->addListener($event::NAME, $listener, 'doSomething', false);
+
+        $event_manager->processEvent($event);
     }
 }
