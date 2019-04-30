Enalean/tuleap

commit 0a33f623f7220da9256bb1c94481642cdbd35df6

Add a content hash on the ressources of the TLP UI framework
Resources of the framework are now built exclusively with Webpack
and each generated asset has a content hash. This make sure the assets
retrieved by the browsers will be up to date and can be cached aggressively.
nginx configuration has been updated to leverage that.

The stylesheets of the FlamingParrot theme are also built with Webpack due
to a direct dependency to the TLP font. The build with the proposed approach
also make sure that all resources linked by the stylesheets exist (the build
crashing otherwise). The FP theme was referencing non existing resources until
this contribution.

The assets built in dev/watch mode now have an inlined source map and are not
minified to ease debug sessions.

No functionnal change is expected.

Part of story #13237: Prevent restricted to be added to private projects

Change-Id: Id62c07ab193a46c8693ecbcaec05e9eab8463233

tools/utils/scripts/webpack-rule-configs.js
@@ -17,10 +17,11 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-const BabelPresetEnv = require("@babel/preset-env");
-const BabelPluginSyntaxDynamicImport = require("@babel/plugin-syntax-dynamic-import");
+const BabelPresetEnv = require("@babel/preset-env").default;
+const BabelPluginSyntaxDynamicImport = require("@babel/plugin-syntax-dynamic-import").default;
 const BabelPluginRewireExports = require("babel-plugin-rewire-exports").default;
 const BabelPluginIstanbul = require("babel-plugin-istanbul").default;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const babel_preset_env_ie_config = [
     BabelPresetEnv,
@@ -145,6 +146,23 @@ const rule_easygettext_loader = {
     use: [{ loader: "json-loader" }, { loader: "easygettext-loader" }]
 };
 
+const rule_scss_loader = {
+    test: /\.scss$/,
+    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+};
+
+const rule_css_assets = {
+    test: /(\.(png|gif|eot|ttf|woff|woff2))|(font\.svg)$/,
+    use: [
+        {
+            loader: "file-loader",
+            options: {
+                name: "css-assets/[name]-[sha256:hash].[ext]"
+            }
+        }
+    ]
+};
+
 module.exports = {
     configureBabelRule,
     babel_options_ie11,
@@ -155,5 +173,7 @@ module.exports = {
     rule_ng_cache_loader,
     rule_angular_gettext_loader,
     rule_angular_gettext_extract_loader,
-    rule_easygettext_loader
+    rule_easygettext_loader,
+    rule_scss_loader,
+    rule_css_assets
 };

.eslintrc.js
@@ -125,7 +125,10 @@ module.exports = {
                 "karma.conf.js",
                 "plugins/tracker/grammar/",
                 "tools/**/*.js",
-                "webpack.config.js"
+                "webpack.config.js",
+                "webpack.common.js",
+                "webpack.dev.js",
+                "webpack.prod.js"
             ],
             env: {
                 node: true

gulpfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Enalean, 2016 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -178,8 +178,6 @@ gulp.task("watch", function() {
     gulp.watch(
         core_build_manifest.themes.common.files
             .concat(select2_scss.themes.common.files)
-            .concat(core_build_manifest.themes.FlamingParrot.files)
-            .concat(core_build_manifest.themes.FlamingParrot.watched_includes)
             .concat(core_build_manifest.themes.BurningParrot.files)
             .concat(core_build_manifest.themes.BurningParrot.watched_includes),
         ["sass-core"]

plugins/textualreport/include/TextualReport/SinglePagePresenterBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -20,12 +20,12 @@
 
 namespace Tuleap\TextualReport;
 
-use ForgeConfig;
-use PDOStatement;
 use PFUser;
 use ThemeVariant;
 use ThemeVariantColor;
+use Tuleap\Layout\CssAssetWithoutVariantDeclinaisons;
 use Tuleap\Layout\IncludeAssets;
+use Tuleap\Layout\ThemeVariation;
 
 class SinglePagePresenterBuilder
 {
@@ -80,19 +80,30 @@ class SinglePagePresenterBuilder
      */
     private function getStylesheetsToEmbed(PFUser $current_user)
     {
-        $theme_variant = new ThemeVariant();
-        $color         = ThemeVariantColor::buildFromVariant($theme_variant->getVariantForUser($current_user));
+        $theme_variant   = new ThemeVariant();
+        $color           = ThemeVariantColor::buildFromVariant($theme_variant->getVariantForUser($current_user));
+        $theme_variation = new ThemeVariation($color, $current_user);
+
+        $root_core_themes_directory = __DIR__ . '/../../../../src/www/themes/';
+
+        $tlp_framework_base_css = new CssAssetWithoutVariantDeclinaisons(
+            new IncludeAssets(
+                $root_core_themes_directory . '/common/tlp/dist/',
+                $root_core_themes_directory . '/common/tlp/dist/'
+            ),
+            'tlp' . $theme_variation->getFileColorCondensedSuffix()
+        );
 
         $core_burning_parrot_include_assets = new IncludeAssets(
-            ForgeConfig::get('tuleap_dir') . '/src/www/themes/BurningParrot/assets',
-            '/themes/BurningParrot/assets'
+            $root_core_themes_directory . '/BurningParrot/assets',
+            $root_core_themes_directory . '/BurningParrot/assets'
         );
 
         $stylesheets = file_get_contents(
-            ForgeConfig::get('sys_urlroot') . '/themes/common/tlp/dist/tlp-' . $color->getName() . '.min.css'
+            $tlp_framework_base_css->getFileURL($theme_variation)
         );
         $stylesheets .= file_get_contents(
-            ForgeConfig::get('sys_urlroot') . $core_burning_parrot_include_assets->getFileURL(
+            $core_burning_parrot_include_assets->getFileURL(
                 'burning-parrot-' . $color->getName() . '.css'
             )
         );

src/etc/nginx/tuleap.d/03-locations.conf
@@ -10,11 +10,11 @@ location /assets/ {
     expires 1y;
 }
 
-location ~ ^/themes/common/tlp/dist/tlp-(.*).min.js$ {
+location /themes/common/tlp/dist/ {
     expires 1y;
 }
 
-location /themes/common/tlp/dist/fonts/font-awesome-4.7.0 {
+location /themes/FlamingParrot/assets/ {
     expires 1y;
 }
 

src/www/themes/BurningParrot/BurningParrotTheme.php
@@ -76,7 +76,7 @@ class BurningParrotTheme extends BaseLayout
             ForgeConfig::get('tuleap_dir') . '/src/www/themes/common/tlp/dist',
             '/themes/common/tlp/dist'
         );
-        $this->includeFooterJavascriptFile($tlp_include_assets->getFileURL('tlp.' . $user->getLocale() . '.min.js'));
+        $this->includeFooterJavascriptFile($tlp_include_assets->getFileURL('tlp-' . $user->getLocale() . '.js'));
         $this->includeFooterJavascriptFile($this->include_asset->getFileURL('burning-parrot.js'));
     }
 
@@ -241,7 +241,7 @@ class BurningParrotTheme extends BaseLayout
         if (isset($params['sidebar'])) {
             $this->show_sidebar = true;
             return $params['sidebar'];
-        } else if (! empty($params['group'])) {
+        } elseif (! empty($params['group'])) {
             $project = $this->project_manager->getProject($params['group']);
             $this->show_sidebar = true;
             return $this->getSidebarPresenterForProject($project, $params);

src/www/themes/BurningParrot/HeaderPresenterBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -29,6 +29,7 @@ use ThemeVariant;
 use ThemeVariantColor;
 use Tuleap\Layout\CssAsset;
 use Tuleap\Layout\CssAssetCollection;
+use Tuleap\Layout\CssAssetWithoutVariantDeclinaisons;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\Layout\SidebarPresenter;
 use Tuleap\Layout\ThemeVariation;
@@ -180,9 +181,17 @@ class HeaderPresenterBuilder
 
     private function getStylesheets(ThemeVariation $theme_variation)
     {
-        $stylesheets = array(
-            '/themes/common/tlp/dist/tlp' . $theme_variation->getFileColorCondensedSuffix() . '.min.css',
+        $tlp_framework_base_css = new CssAssetWithoutVariantDeclinaisons(
+            new IncludeAssets(
+                __DIR__ . '/../../themes/common/tlp/dist/',
+                '/themes/common/tlp/dist/'
+            ),
+            'tlp' . $theme_variation->getFileColorCondensedSuffix()
         );
+
+        $stylesheets = [
+            $tlp_framework_base_css->getFileURL($theme_variation)
+        ];
 
         $core_burning_parrot_css = new CssAsset(
             new IncludeAssets(

src/www/themes/FlamingParrot/css/print.scss
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Enalean, 2013-2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -16,6 +16,9 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
+$tuleap-common-theme-basepath: '../../common/';
+
 @import '../../common/css/utils/utils';
 
 body {

commit 1a96398ee7b82ac9cd11636b741a4247cd19cf91

request #13351: Customized organization logos are not visible on FlamingParrot pages
Organization logos are no more resolved in the stylesheets so they still be
customized by administrators.
A proper solution would be to let administrators customize the logos directly
from the site administration and manage them in a similar way than users' avatars.

Issue introduced by 0a33f623f7220da9256bb1c94481642cdbd35df6.

Change-Id: I980e5636d223e6308ffe2e1f83f57b0cd9dc2624

tools/utils/scripts/webpack-rule-configs.js
@@ -148,7 +148,22 @@ const rule_easygettext_loader = {
 
 const rule_scss_loader = {
     test: /\.scss$/,
-    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+    use: [
+        MiniCssExtractPlugin.loader,
+        {
+            loader: "css-loader",
+            options: {
+                url: (url, resourcePath) => {
+                    // Organization logos might be customized by administrators, let's exclude them for now
+                    return (
+                        url.endsWith("organization_logo.png") ||
+                        url.endsWith("organization_logo_small.png")
+                    );
+                }
+            }
+        },
+        "sass-loader"
+    ]
 };
 
 const rule_css_assets = {