Enalean/tuleap

commit a2b4ad31c586e22b9feae7bf5ac86fc80fddb1e5

Fix recent issues spotted by vimeo/psalm in the tracker plugin
Most of these issues has been introduced by recent developments for
story #12189 (prevent field modification according to state),
story #13243 (import/export workflow simple mode via XML) and
story #13012 (remove trailing 0 of tracker float field).

The goal here is to clean errors that has been introduced since the baseline
creation before recreating a new baseline with the update of vimeo/psalm.

Part of request #13292: Integrate vimeo/psalm to the test pipeline

Change-Id: Ie28a862142d1f0ab3417c6fe9af1be886005bd00

plugins/tracker/include/REST/v1/ArtifactsResource.class.php
@@ -465,8 +465,20 @@ class ArtifactsResource extends AuthenticatedResource {
         $this->sendAllowHeadersForLinkedArtifacts();
         Header::sendPaginationHeaders($limit, $offset, $linked_artifacts->getTotalSize(), self::MAX_LIMIT);
 
+        $artifact_representations = [];
+        foreach ($linked_artifacts->getArtifacts() as $linked_artifact) {
+            $tracker_representation = new MinimalTrackerRepresentation();
+            $tracker_representation->build($linked_artifact->getTracker());
+
+            $artifact_representations[] = $this->builder->getArtifactRepresentationWithFieldValuesInBothFormat(
+                $user,
+                $artifact,
+                $tracker_representation
+            );
+        }
+
         return array(
-            self::VALUES_FORMAT_COLLECTION => $linked_artifacts->getArtifacts()
+            self::VALUES_FORMAT_COLLECTION => $artifact_representations
         );
     }
 

plugins/tracker/include/Tracker/Artifact/Tracker_ArtifactLinkInfo.class.php
@@ -159,13 +159,17 @@ class Tracker_ArtifactLinkInfo
     public function userCanView(PFUser $user) {
         $artifact = $this->getArtifact();
 
-        return $artifact->userCanView($user);
+        return $artifact !== null && $artifact->userCanView($user);
     }
 
-    public function getArtifact() : Tracker_Artifact
+    public function getArtifact() : ?Tracker_Artifact
     {
         if (! $this->artifact) {
-            $this->artifact = Tracker_ArtifactFactory::instance()->getArtifactById($this->artifact_id);
+            $artifact = Tracker_ArtifactFactory::instance()->getArtifactById($this->artifact_id);
+            if ($artifact === null) {
+                return null;
+            }
+            $this->artifact = $artifact;
         }
         return $this->artifact;
     }

plugins/tracker/include/Tracker/FormElement/Field/ArtifactLink/Nature/CustomColumn/ValueFormatter.php
@@ -81,7 +81,11 @@ class ValueFormatter
             return '';
         }
 
-        $changeset = $artifact_link_info->getArtifact()->getLastChangeset();
+        $artifact  = $artifact_link_info->getArtifact();
+        if ($artifact === null) {
+            return '';
+        }
+        $changeset = $artifact->getLastChangeset();
         if (! $changeset) {
             return '';
         }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015-2019. All Rights Reserved.
+ * Copyright (c) Enalean, 2015-Present. All Rights Reserved.
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
  *
  * This file is a part of Tuleap.
@@ -1124,7 +1124,6 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
         $read_only              = false;
         $name                   = 'artifact['. $this->id .']';
         $prefill_removed_values = array();
-        $artifact_links         = array();
 
         // Well, shouldn't be here but API doesn't provide a Null Artifact on creation yet
         // Here to avoid having to pass null arg for fetchHtmlWidget
@@ -1135,8 +1134,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
             $this,
             $this->getTrackerFactory(),
             Tracker_ReportFactory::instance(),
-            $this->getNaturePresenterFactory(),
-            ...$artifact_links
+            $this->getNaturePresenterFactory()
         );
 
         return $this->fetchHtmlWidget(

plugins/tracker/include/REST/Artifact/ArtifactRepresentationBuilder.php
@@ -278,24 +278,17 @@ class ArtifactRepresentationBuilder
             );
         }
 
-        $total_size               = (int) $this->nature_dao->foundRows();
-        $artifact_representations = [];
-        foreach ($linked_artifacts_ids as $artifact_id) {
-            $artifact = $this->artifact_factory->getArtifactByIdUserCanView($user, $artifact_id);
-            if ($artifact) {
-                $tracker_representation = new MinimalTrackerRepresentation();
-                $tracker_representation->build($artifact->getTracker());
-
-                $artifact_representations[] = $this->getArtifactRepresentationWithFieldValuesInBothFormat(
-                    $user,
-                    $artifact,
-                    $tracker_representation
-                );
+        $total_size = (int) $this->nature_dao->foundRows();
+        $artifacts  = [];
+        foreach ($linked_artifacts_ids as $linked_artifact_id) {
+            $artifact = $this->artifact_factory->getArtifactByIdUserCanView($user, $linked_artifact_id);
+            if ($artifact !== null) {
+                $artifacts[] = $artifact;
             }
         }
 
         return new Tracker_Artifact_PaginatedArtifacts(
-            $artifact_representations,
+            $artifacts,
             $total_size
         );
     }

plugins/tracker/include/REST/CompleteTrackerRepresentation.class.php
@@ -45,7 +45,7 @@ class CompleteTrackerRepresentation implements TrackerRepresentation
     public $html_url;
 
     /**
-     * @var Tuleap\REST\ResourceReference
+     * @var ProjectReference
      */
     public $project;
 
@@ -80,12 +80,12 @@ class CompleteTrackerRepresentation implements TrackerRepresentation
     public $semantics = array();
 
     /**
-     * @var Tuleap\Tracker\REST\WorkflowRepresentation
+     * @var WorkflowRepresentation | null
      */
     public $workflow;
 
     /**
-     * @var Tuleap\Tracker\REST\TrackerReference
+     * @var TrackerReference
      */
 
     public $parent;

plugins/tracker/include/REST/v1/TrackersResource.class.php
@@ -51,8 +51,10 @@ use Tuleap\Tracker\Report\Query\Advanced\Grammar\SyntaxError;
 use Tuleap\Tracker\Report\Query\Advanced\LimitSizeIsExceededException;
 use Tuleap\Tracker\Report\Query\Advanced\SearchablesAreInvalidException;
 use Tuleap\Tracker\Report\Query\Advanced\SearchablesDoNotExistException;
+use Tuleap\Tracker\REST\Artifact\ArtifactRepresentation;
 use Tuleap\Tracker\REST\Artifact\ArtifactRepresentationBuilder;
 use Tuleap\Tracker\REST\Artifact\ParentArtifactReference;
+use Tuleap\Tracker\REST\CompleteTrackerRepresentation;
 use Tuleap\Tracker\REST\MinimalTrackerRepresentation;
 use Tuleap\Tracker\REST\PermissionsExporter;
 use Tuleap\Tracker\REST\ReportRepresentation;
@@ -146,7 +148,7 @@ class TrackersResource extends AuthenticatedResource
      *
      * @param int $id Id of the tracker
      *
-     * @return Tuleap\Tracker\REST\CompleteTrackerRepresentation
+     * @return CompleteTrackerRepresentation
      * @throws RestException 403
      * @throws RestException 404
      */
@@ -412,7 +414,7 @@ class TrackersResource extends AuthenticatedResource
     }
 
     /**
-     * @return Tuleap\Tracker\REST\Artifact\ArtifactRepresentation[]
+     * @return ArtifactRepresentation[]
      */
     private function getListOfArtifactRepresentation(PFUser $user, $artifacts, $with_all_field_values)
     {
@@ -596,7 +598,7 @@ class TrackersResource extends AuthenticatedResource
      * @param int    $id    Id of the tracker.
      * @param string $query JSON object of search criteria properties {@from query}
      *
-     * @return Tuleap\Tracker\REST\CompleteTrackerRepresentation
+     * @return CompleteTrackerRepresentation
      *
      * @throws I18NRestException 500
      * @throws I18NRestException 400

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_ChangesetValue.class.php
@@ -157,7 +157,7 @@ abstract class Tracker_Artifact_ChangesetValue
     /**
      * Returns the value of this changeset value
      *
-     * @return string|array The value of this artifact changeset value
+     * @return mixed The value of this artifact changeset value
      */
     public abstract function getValue();
 

commit 8e36844b3f8dab47b2bad99f2818f618e95fd7e2

request #13374: REST API GET /artifacts/{id}/linked_artifacts does not return the representation of the linked artifacts
The REST route returns a collection of representations of the current artifact
instead of the representation of the artifacts linked to the current artifact.

Issue has been introduced by a2b4ad31c586e22b9feae7bf5ac86fc80fddb1e5.

Change-Id: I32610d207740fecd54b4b9c71b4a1763a6ce7cdd

plugins/tracker/include/REST/v1/ArtifactsResource.class.php
@@ -472,7 +472,7 @@ class ArtifactsResource extends AuthenticatedResource {
 
             $artifact_representations[] = $this->builder->getArtifactRepresentationWithFieldValuesInBothFormat(
                 $user,
-                $artifact,
+                $linked_artifact,
                 $tracker_representation
             );
         }

plugins/tracker/include/Tracker/Artifact/Tracker_ArtifactLinkInfo.class.php
@@ -165,11 +165,7 @@ class Tracker_ArtifactLinkInfo
     public function getArtifact() : ?Tracker_Artifact
     {
         if (! $this->artifact) {
-            $artifact = Tracker_ArtifactFactory::instance()->getArtifactById($this->artifact_id);
-            if ($artifact === null) {
-                return null;
-            }
-            $this->artifact = $artifact;
+            $this->artifact = Tracker_ArtifactFactory::instance()->getArtifactById($this->artifact_id);
         }
         return $this->artifact;
     }

plugins/tracker/include/Tracker/FormElement/Field/ArtifactLink/Nature/CustomColumn/ValueFormatter.php
@@ -81,11 +81,7 @@ class ValueFormatter
             return '';
         }
 
-        $artifact  = $artifact_link_info->getArtifact();
-        if ($artifact === null) {
-            return '';
-        }
-        $changeset = $artifact->getLastChangeset();
+        $changeset = $artifact_link_info->getArtifact()->getLastChangeset();
         if (! $changeset) {
             return '';
         }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.class.php
@@ -1124,6 +1124,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
         $read_only              = false;
         $name                   = 'artifact['. $this->id .']';
         $prefill_removed_values = array();
+        $artifact_links         = array();
 
         // Well, shouldn't be here but API doesn't provide a Null Artifact on creation yet
         // Here to avoid having to pass null arg for fetchHtmlWidget
@@ -1134,7 +1135,8 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
             $this,
             $this->getTrackerFactory(),
             Tracker_ReportFactory::instance(),
-            $this->getNaturePresenterFactory()
+            $this->getNaturePresenterFactory(),
+            ...$artifact_links
         );
 
         return $this->fetchHtmlWidget(

plugins/tracker/tests/rest/Artifacts/LinkedArtifactsTest.php
@@ -1,0 +1,90 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\Tests\REST\Artifacts;
+
+use Tuleap\Tracker\Tests\REST\TrackerBase;
+
+require_once __DIR__.'/../bootstrap.php';
+
+final class LinkedArtifactsTest extends TrackerBase
+{
+    private const PROJECT_SHORTNAME = 'linked-artifacts';
+    private const TRACKER_SHORTNAME = 'linked_artifacts';
+    private const FIELD_SHORTNAME   = 'link';
+
+    public function testLinkedArtifacts() : void
+    {
+        $project_id = $this->getProjectId(self::PROJECT_SHORTNAME);
+        $tracker_id = $this->tracker_ids[$project_id][self::TRACKER_SHORTNAME];
+        $field_id   = $this->getAUsedField($tracker_id, self::FIELD_SHORTNAME);
+
+        $artifact_id_1 = $this->createArtifact($tracker_id, $field_id);
+        $artifact_id_2 = $this->createArtifact($tracker_id, $field_id);
+        $artifact_id_3 = $this->createArtifact($tracker_id, $field_id, $artifact_id_1, $artifact_id_2);
+
+        $response = $this->getResponse(
+            $this->client->get('artifacts/' . urlencode((string) $artifact_id_3) . '/linked_artifacts?direction=forward')
+        );
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $linked_artifacts_collection = $response->json();
+
+        $linked_artifacts_id = [];
+        foreach ($linked_artifacts_collection['collection'] as $linked_artifact) {
+            $linked_artifacts_id[] = $linked_artifact['id'];
+        }
+
+        $this->assertEqualsCanonicalizing([$artifact_id_1, $artifact_id_2], $linked_artifacts_id);
+    }
+
+    private function createArtifact(int $tracker_id, int $art_link_field_id, int ...$linked_artifacts) : int
+    {
+        $payload = [
+            'tracker' => ['id' => $tracker_id],
+            'values'  => [
+                [
+                    'field_id' => $art_link_field_id,
+                    'links' => (static function (int ...$linked_artifacts) : array {
+                        $representations = [];
+
+                        foreach ($linked_artifacts as $linked_artifact) {
+                            $representations[] = ['id' => $linked_artifact];
+                        }
+
+                        return $representations;
+                    })(...$linked_artifacts)
+                ]
+            ]
+        ];
+
+        $response = $this->getResponse(
+            $this->client->post('artifacts', null, json_encode($payload))
+        );
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        $json = $response->json();
+
+        return $json['id'];
+    }
+}

plugins/tracker/tests/rest/_fixtures/LinkedArtifacts/project.xml
@@ -1,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+  ~ Copyright Enalean (c) 2019-Present. All rights reserved.
+  ~
+  ~ Tuleap and Enalean names and logos are registered trademarks owned by
+  ~ Enalean SAS. All other trademarks or names are properties of their respective
+  ~ owners.
+  ~
+  ~ This file is a part of Tuleap.
+  ~
+  ~ Tuleap is free software; you can redistribute it and/or modify
+  ~ it under the terms of the GNU General Public License as published by
+  ~ the Free Software Foundation; either version 2 of the License, or
+  ~ (at your option) any later version.
+  ~
+  ~ Tuleap is distributed in the hope that it will be useful,
+  ~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ~ GNU General Public License for more details.
+  ~
+  ~ You should have received a copy of the GNU General Public License
+  ~ along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<project
+    unix-name="linked-artifacts" full-name="linked-artifacts" description="linked-artifacts" access="public">
+    <long-description>Project for tracker fields actions</long-description>
+    <services>
+        <service shortname="plugin_tracker" enabled="true" />
+    </services>
+    <ugroups>
+        <ugroup name="project_members" description="">
+            <members>
+                <member format="username">rest_api_tester_1</member>
+            </members>
+        </ugroup>
+        <ugroup name="project_admins" description="">
+            <members>
+                <member format="username">rest_api_tester_1</member>
+            </members>
+        </ugroup>
+    </ugroups>
+    <trackers>
+        <tracker id="T1424" parent_id="0" instantiate_for_new_projects="1">
+            <name>
+                <![CDATA[ linked_artifacts ]]>
+            </name>
+            <item_name>linked_artifacts</item_name>
+            <description>
+                <![CDATA[ linked_artifacts ]]>
+            </description>
+            <color>inca-silver</color>
+            <cannedResponses/>
+            <formElements>
+                <formElement type="art_link" ID="F34791" rank="0">
+                    <name>link</name>
+                    <label>
+                        <![CDATA[ link ]]>
+                    </label>
+                </formElement>
+            </formElements>
+            <permissions>
+                <permission scope="tracker" ugroup="UGROUP_ANONYMOUS" type="PLUGIN_TRACKER_ACCESS_FULL"/>
+                <permission scope="field" REF="F34791" ugroup="UGROUP_ANONYMOUS" type="PLUGIN_TRACKER_FIELD_READ"/>
+                <permission scope="field" REF="F34791" ugroup="UGROUP_REGISTERED" type="PLUGIN_TRACKER_FIELD_SUBMIT"/>
+                <permission scope="field" REF="F34791" ugroup="UGROUP_PROJECT_MEMBERS" type="PLUGIN_TRACKER_FIELD_UPDATE"/>
+            </permissions>
+        </tracker>
+    </trackers>
+</project>

plugins/tracker/tests/rest/_fixtures/LinkedArtifacts/users.xml
@@ -1,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<users>
+ <user>
+  <id>102</id>
+  <username>rest_api_tester_1</username>
+  <realname><![CDATA[Test User 1]]></realname>
+  <email><![CDATA[test_user_1@example.com]]></email>
+  <ldapid>tester1</ldapid>
+ </user>
+</users>