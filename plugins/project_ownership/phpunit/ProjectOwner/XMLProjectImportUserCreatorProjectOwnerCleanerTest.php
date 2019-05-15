Enalean/tuleap

commit dda39bbbb1b3a94e6b3537444a11176eccab0cbe

Restricted user cannot be added to project in the 'private without restricted mode'
Both the project members and the user groups are impacted by this change.

To test using the web UI, you can select a non restricted user in the autocompleter,
mark this user as restricted and then try to add the previously selected user.

To test, you will also need to enable the "private without restricted" mode by setting
the global parameter feature_flag_project_without_restricted to 1.

This is part of story #13237: Prevent restricted to be added to private projects

Change-Id: I202e9a2878de13a074154f64322dc6db31a978e4

tests/simpletest/common/Project/UGroupTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  *
  * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,10 +27,23 @@ class UGroup_AddUserTest extends TuleapTestCase {
         $this->user_id = 400;
         $this->user    = stub('PFUser')->getId()->returns($this->user_id);
     }
-    
-    function itAddUserIntoStaticGroup() {
+
+    public function tearDown() : void
+    {
+        parent::tearDown();
+        ProjectManager::clearInstance();
+    }
+
+    public function itAddUserIntoStaticGroup() : void
+    {
         $ugroup_id = 200;
         $group_id  = 300;
+
+        $project_manager = Mockery::mock(ProjectManager::class);
+        ProjectManager::setInstance($project_manager);
+        $project = Mockery::mock(Project::class);
+        $project_manager->shouldReceive('getProject')->with($group_id)->andReturn($project);
+        $project->shouldReceive('getAccess')->andReturn(Project::ACCESS_PUBLIC);
         
         $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('addUserToStaticGroup', 'exists'));
         stub($ugroup)->exists()->returns(true);
@@ -44,6 +57,12 @@ class UGroup_AddUserTest extends TuleapTestCase {
     function itThrowAnExceptionIfStaticUGroupDoesntExist() {
         $ugroup_id = 200;
         $group_id  = 300;
+
+        $project_manager = Mockery::mock(ProjectManager::class);
+        ProjectManager::setInstance($project_manager);
+        $project = Mockery::mock(Project::class);
+        $project_manager->shouldReceive('getProject')->with($group_id)->andReturn($project);
+        $project->shouldReceive('getAccess')->andReturn(Project::ACCESS_PUBLIC);
         
         $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('exists'));
         stub($ugroup)->exists()->returns(false);
@@ -57,7 +76,13 @@ class UGroup_AddUserTest extends TuleapTestCase {
     function itAddUserIntoDynamicGroup() {
         $ugroup_id = $GLOBALS['UGROUP_WIKI_ADMIN'];
         $group_id  = 300;
-        
+
+        $project_manager = Mockery::mock(ProjectManager::class);
+        ProjectManager::setInstance($project_manager);
+        $project = Mockery::mock(Project::class);
+        $project_manager->shouldReceive('getProject')->with($group_id)->andReturn($project);
+        $project->shouldReceive('getAccess')->andReturn(Project::ACCESS_PUBLIC);
+
         $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('_getUserGroupDao'));
         
         $dao = mock('UserGroupDao');

commit f33b04de24719d84e603ffb7b5bf341e3fa0fddb

request #13379: Adding a non-project member to the project administrators does not update the project member group
Issue introduced by dda39bbbb1b3a94e6b3537444a11176eccab0cbe.

This contribution also fixes an issue with the project ownership plugin
and the project XML import. A project imported via XML could have no
project owner at the end of the import, we know make sure one the administrator
is chosen as the project owner.

Change-Id: I791f753767f0dc8061b18bd89a588a4ff5e3cc42

plugins/project_ownership/include/ProjectOwner/XMLProjectImportUserCreatorProjectOwnerCleaner.php
@@ -1,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProjectOwnership\ProjectOwner;
+
+use Tuleap\Project\Admin\ProjectUGroup\ProjectImportCleanupUserCreatorFromAdministrators;
+
+final class XMLProjectImportUserCreatorProjectOwnerCleaner
+{
+    /**
+     * @var ProjectOwnerUpdater
+     */
+    private $project_owner_updater;
+
+    public function __construct(ProjectOwnerUpdater $project_owner_updater)
+    {
+        $this->project_owner_updater = $project_owner_updater;
+    }
+
+    public function updateProjectOwnership(
+        ProjectImportCleanupUserCreatorFromAdministrators $cleanup_user_creator_from_administrators
+    ) : void {
+        $ugroup_admin = $cleanup_user_creator_from_administrators->getUGroupAdministrator();
+        $project      = $ugroup_admin->getProject();
+        if ($project === null) {
+            return;
+        }
+        $creator = $cleanup_user_creator_from_administrators->getCreator();
+
+        foreach ($ugroup_admin->getMembers() as $administrator) {
+            if ($administrator->getId() !== $creator->getId()) {
+                $this->project_owner_updater->updateProjectOwner($project, $administrator);
+                return;
+            }
+        }
+    }
+}

plugins/project_ownership/include/project_ownershipPlugin.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,17 +18,22 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\DB\DBFactory;
+use Tuleap\DB\DBTransactionExecutorWithConnection;
 use Tuleap\Project\Admin\Navigation\HeaderNavigationDisplayer;
 use Tuleap\Project\Admin\Navigation\NavigationItemPresenter;
 use Tuleap\Project\Admin\Navigation\NavigationPresenter;
 use Tuleap\Project\Admin\ProjectUGroup\ApproveProjectAdministratorRemoval;
+use Tuleap\Project\Admin\ProjectUGroup\ProjectImportCleanupUserCreatorFromAdministrators;
 use Tuleap\Project\Admin\ProjectUGroup\ProjectUGroupMemberUpdatable;
 use Tuleap\ProjectOwnership\ProjectAdmin\CannotRemoveProjectOwnerFromTheProjectAdministratorsException;
 use Tuleap\ProjectOwnership\ProjectAdmin\IndexController;
 use Tuleap\ProjectOwnership\ProjectAdmin\ProjectOwnerPresenterBuilder;
 use Tuleap\ProjectOwnership\ProjectOwner\ProjectOwnerDAO;
 use Tuleap\ProjectOwnership\ProjectOwner\ProjectOwnerRetriever;
+use Tuleap\ProjectOwnership\ProjectOwner\ProjectOwnerUpdater;
 use Tuleap\ProjectOwnership\ProjectOwner\UserWithStarBadgeFinder;
+use Tuleap\ProjectOwnership\ProjectOwner\XMLProjectImportUserCreatorProjectOwnerCleaner;
 use Tuleap\ProjectOwnership\REST\ProjectOwnershipResource;
 use Tuleap\ProjectOwnership\SystemEvents\ProjectOwnershipSystemEventManager;
 use Tuleap\ProjectOwnership\SystemEvents\ProjectOwnerStatusNotificationSystemEvent;
@@ -64,6 +69,7 @@ class project_ownershipPlugin extends Plugin // phpcs:ignore
         $this->addHook(CollectRoutesEvent::NAME);
         $this->addHook(ProjectUGroupMemberUpdatable::NAME);
         $this->addHook(ApproveProjectAdministratorRemoval::NAME);
+        $this->addHook(ProjectImportCleanupUserCreatorFromAdministrators::NAME);
         $this->addHook(UserWithStarBadgeCollector::NAME);
         $this->addHook('project_is_suspended');
         $this->addHook('project_is_active');
@@ -173,6 +179,19 @@ class project_ownershipPlugin extends Plugin // phpcs:ignore
                 $project_owner
             );
         }
+    }
+
+    public function projectImportCleanupUserCreatorFromAdministrators(
+        ProjectImportCleanupUserCreatorFromAdministrators $cleanup_user_creator_from_administrators
+    ) : void {
+        $xml_project_user_creator_project_owner_updater = new XMLProjectImportUserCreatorProjectOwnerCleaner(
+            new ProjectOwnerUpdater(
+                new ProjectOwnerDAO(),
+                new DBTransactionExecutorWithConnection(DBFactory::getMainTuleapDBConnection())
+            )
+        );
+
+        $xml_project_user_creator_project_owner_updater->updateProjectOwnership($cleanup_user_creator_from_administrators);
     }
 
     public function userWithStarBadgeCollector(UserWithStarBadgeCollector $collector)

src/common/Project/Admin/ProjectUGroup/NotProjectAdministratorUGroup.php
@@ -1,0 +1,34 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Project\Admin\ProjectUGroup;
+
+use ProjectUGroup;
+use UnexpectedValueException;
+
+final class NotProjectAdministratorUGroup extends UnexpectedValueException
+{
+    public function __construct(ProjectUGroup $ugroup)
+    {
+        parent::__construct(sprintf('Expected UGroup %d to be a project administrator ugroup', $ugroup->getId()));
+    }
+}

src/common/Project/Admin/ProjectUGroup/ProjectImportCleanupUserCreatorFromAdministrators.php
@@ -1,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Project\Admin\ProjectUGroup;
+
+use PFUser;
+use ProjectUGroup;
+use Tuleap\Event\Dispatchable;
+
+final class ProjectImportCleanupUserCreatorFromAdministrators implements Dispatchable
+{
+    public const NAME = 'projectImportCleanupUserCreatorFromAdministrators';
+    /**
+     * @var PFUser
+     */
+    private $creator;
+    /**
+     * @var ProjectUGroup
+     */
+    private $ugroup_administrator;
+
+    public function __construct(PFUser $creator, ProjectUGroup $ugroup_administrator)
+    {
+        $this->creator              = $creator;
+        if ($ugroup_administrator->getId() !== ProjectUGroup::PROJECT_ADMIN) {
+            throw new NotProjectAdministratorUGroup($ugroup_administrator);
+        }
+        $this->ugroup_administrator = $ugroup_administrator;
+    }
+
+    /**
+     * @return PFUser
+     */
+    public function getCreator() : PFUser
+    {
+        return $this->creator;
+    }
+
+    /**
+     * @return ProjectUGroup
+     */
+    public function getUGroupAdministrator() : ProjectUGroup
+    {
+        return $this->ugroup_administrator;
+    }
+}

src/common/Project/ProjectUGroup.php
@@ -19,7 +19,11 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\DB\DBFactory;
+use Tuleap\DB\DBTransactionExecutorWithConnection;
 use Tuleap\Project\Admin\ProjectUGroup\CannotAddRestrictedUserToProjectNotAllowingRestricted;
+use Tuleap\Project\Admin\ProjectUGroup\DynamicUGroupMembersUpdater;
+use Tuleap\Project\UserPermissionsDao;
 use Tuleap\User\UserGroup\NameTranslator;
 
 /**
@@ -169,19 +173,6 @@ class ProjectUGroup implements User_UGroup // phpcs:ignore PSR1.Classes.ClassDec
     public function setProjectId($project_id)
     {
         $this->group_id = $project_id;
-    }
-
-    /**
-     * Get instance of UserGroupDao
-     *
-     * @return UserGroupDao
-     */
-    protected function getUserGroupDao()
-    {
-        if (!$this->user_group_dao) {
-            $this->user_group_dao = new UserGroupDao();
-        }
-        return $this->user_group_dao;
     }
 
     /**
@@ -506,47 +497,25 @@ class ProjectUGroup implements User_UGroup // phpcs:ignore PSR1.Classes.ClassDec
 
     /**
      * Add user to a dynamic ugroup
-     *
-     * @param PFUser $user User to add
-     *
-     * @return Void
      */
-    protected function addUserToDynamicGroup(PFUser $user)
+    protected function addUserToDynamicGroup(PFUser $user) : void
     {
-        $dao  = $this->getUserGroupDao();
-        $flag = $this->getAddFlagForUGroupId($this->id);
-        $dao->updateUserGroupFlags($user->getId(), $this->group_id, $flag);
+        $project = $this->getProject();
+        if ($project === null) {
+            return;
+        }
+
+        $this->getDynamicUGroupMembersUpdater()->addUser($project, $this, $user);
     }
 
-    /**
-     * Convert a dynamic ugroup_id into it's DB table update to add someone to a given group
-     *
-     * @param int $id Id of the ugroup
-     *
-     * @throws UGroup_Invalid_Exception
-     *
-     * @return string
-     */
-    public function getAddFlagForUGroupId($id)
+    private function getDynamicUGroupMembersUpdater() : DynamicUGroupMembersUpdater
     {
-        switch ($id) {
-            case self::PROJECT_ADMIN:
-                return "admin_flags = 'A'";
-            case self::FILE_MANAGER_ADMIN:
-                return 'file_flags = 2';
-            case self::WIKI_ADMIN:
-                return 'wiki_flags = 2';
-            case self::SVN_ADMIN:
-                return 'svn_flags = 2';
-            case self::FORUM_ADMIN:
-                return 'forum_flags = 2';
-            case self::NEWS_ADMIN:
-                return 'news_flags = 2';
-            case self::NEWS_WRITER:
-                return 'news_flags = 1';
-            default:
-                throw new UGroup_Invalid_Exception();
-        }
+        return new DynamicUGroupMembersUpdater(
+            new UserPermissionsDao(),
+            new DBTransactionExecutorWithConnection(DBFactory::getMainTuleapDBConnection()),
+            new UGroupBinding($this->getUGroupUserDao(), new UGroupManager()),
+            EventManager::instance()
+        );
     }
 
     /**
@@ -588,52 +557,14 @@ class ProjectUGroup implements User_UGroup // phpcs:ignore PSR1.Classes.ClassDec
         ugroup_remove_user_from_ugroup($group_id, $ugroup_id, $user_id);
     }
 
-    /**
-     * Remove user from dynamic ugroup
-     *
-     * @param PFUser $user User to remove
-     *
-     * @return bool
-     */
-    protected function removeUserFromDynamicGroup(PFUser $user)
+    protected function removeUserFromDynamicGroup(PFUser $user) : void
     {
-        $dao  = $this->getUserGroupDao();
-        if ($this->id == self::PROJECT_ADMIN &&
-            $dao->searchProjectAdminsByProjectIdExcludingOneUserId($this->group_id, $user->getId())->rowCount() === 0) {
-            throw new Exception('Impossible to remove last admin of the project');
+        $project = $this->getProject();
+        if ($project === null) {
+            return;
         }
-        $flag = $this->getRemoveFlagForUGroupId($this->id);
-        return $dao->updateUserGroupFlags($user->getId(), $this->group_id, $flag);
-    }
 
-    /**
-     * Convert a dynamic ugroup_id into it's DB table update to remove someone from given group
-     *
-     * @param int $id Id of the ugroup
-     *
-     * @throws UGroup_Invalid_Exception
-     *
-     * @return string
-     */
-    public function getRemoveFlagForUGroupId($id)
-    {
-        switch ($id) {
-            case self::PROJECT_ADMIN:
-                return "admin_flags = ''";
-            case self::FILE_MANAGER_ADMIN:
-                return 'file_flags = 0';
-            case self::WIKI_ADMIN:
-                return 'wiki_flags = 0';
-            case self::SVN_ADMIN:
-                return 'svn_flags = 0';
-            case self::FORUM_ADMIN:
-                return 'forum_flags = 0';
-            case self::NEWS_ADMIN:
-            case self::NEWS_WRITER:
-                return 'news_flags = 0';
-            default:
-                throw new UGroup_Invalid_Exception();
-        }
+        $this->getDynamicUGroupMembersUpdater()->removeUser($project, $this, $user);
     }
 
     /**

src/common/Project/ProjectXMLImporter.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013 - 2017. All Rights Reserved.
+ * Copyright (c) Enalean, 2013 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -24,6 +24,7 @@ use Tuleap\FRS\FRSPermissionCreator;
 use Tuleap\FRS\UploadedLinksUpdater;
 use Tuleap\Project\Admin\ProjectUGroup\CannotCreateUGroupException;
 use Tuleap\Dashboard\Project\ProjectDashboardXMLImporter;
+use Tuleap\Project\Admin\ProjectUGroup\ProjectImportCleanupUserCreatorFromAdministrators;
 use Tuleap\Project\UgroupDuplicator;
 use Tuleap\Project\UserRemover;
 use Tuleap\Project\XML\Import\ArchiveInterface;
@@ -371,9 +372,12 @@ class ProjectXMLImporter {
         }
     }
 
-    private function cleanProjectAdminsFromUserCreator(ProjectUGroup $ugroup, array $users, PFUser $user_creator)
+    private function cleanProjectAdminsFromUserCreator(ProjectUGroup $ugroup, array $users, PFUser $user_creator) : void
     {
         if (! empty($users) && ! in_array($user_creator, $users)) {
+            $this->event_manager->processEvent(
+                new ProjectImportCleanupUserCreatorFromAdministrators($user_creator, $ugroup)
+            );
             $ugroup->removeUser($user_creator);
         }
     }

plugins/project_ownership/phpunit/ProjectOwner/XMLProjectImportUserCreatorProjectOwnerCleanerTest.php
@@ -1,0 +1,89 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProjectOwnership\ProjectOwner;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+use Project;
+use ProjectUGroup;
+use Tuleap\Project\Admin\ProjectUGroup\ProjectImportCleanupUserCreatorFromAdministrators;
+
+final class XMLProjectImportUserCreatorProjectOwnerCleanerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testFirstProjectAdministratorFoundNotBeingTheCreatorIsSetAsProjectOwner() : void
+    {
+        $updater = Mockery::mock(ProjectOwnerUpdater::class);
+        $cleaner = new XMLProjectImportUserCreatorProjectOwnerCleaner($updater);
+
+        $creator = Mockery::mock(PFUser::class);
+        $creator->shouldReceive('getId')->andReturn(101);
+        $other_admin_1 = Mockery::mock(PFUser::class);
+        $other_admin_1->shouldReceive('getId')->andReturn(102);
+        $other_admin_2 = Mockery::mock(PFUser::class);
+
+        $ugroup = $this->createUGroupAdmin();
+        $project = Mockery::mock(Project::class);
+        $ugroup->shouldReceive('getProject')->andReturn($project);
+        $ugroup->shouldReceive('getMembers')->andReturn([$creator, $other_admin_1, $other_admin_2]);
+
+        $updater->shouldReceive('updateProjectOwner')->with($project, $other_admin_1);
+
+        $cleaner->updateProjectOwnership($this->createEvent($creator, $ugroup));
+    }
+
+    public function testNothingIsDoneWhenTheUGroupIsCorruptedAndCannotFindTheProjec() : void
+    {
+        $updater = Mockery::mock(ProjectOwnerUpdater::class);
+        $cleaner = new XMLProjectImportUserCreatorProjectOwnerCleaner($updater);
+
+        $creator = Mockery::mock(PFUser::class);
+
+        $ugroup = $this->createUGroupAdmin();
+        $ugroup->shouldReceive('getProject')->andReturn(null);
+
+        $updater->shouldNotReceive('updateProjectOwner');
+
+        $cleaner->updateProjectOwnership($this->createEvent($creator, $ugroup));
+    }
+
+
+    private function createEvent(PFUser $creator, ProjectUGroup $ugroup) : ProjectImportCleanupUserCreatorFromAdministrators
+    {
+        return new ProjectImportCleanupUserCreatorFromAdministrators($creator, $ugroup);
+    }
+
+    /**
+     * @return ProjectUGroup|Mockery\MockInterface
+     */
+    private function createUGroupAdmin() : ProjectUGroup
+    {
+        $ugroup = Mockery::mock(ProjectUGroup::class);
+        $ugroup->shouldReceive('getId')->andReturn(ProjectUGroup::PROJECT_ADMIN);
+
+        return $ugroup;
+    }
+}

tests/phpunit/common/Project/Admin/ProjectUGroup/ProjectImportCleanupUserCreatorFromAdministratorsTest.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Project\Admin\ProjectUGroup;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+use ProjectUGroup;
+
+final class ProjectImportCleanupUserCreatorFromAdministratorsTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testValuesHoldByTheEventCanBeAccessed() : void
+    {
+        $creator = Mockery::mock(PFUser::class);
+        $ugroup  = Mockery::mock(ProjectUGroup::class);
+        $ugroup->shouldReceive('getId')->andReturn(ProjectUGroup::PROJECT_ADMIN);
+        $event = new ProjectImportCleanupUserCreatorFromAdministrators($creator, $ugroup);
+
+        $this->assertSame($creator, $event->getCreator());
+        $this->assertSame($ugroup, $event->getUGroupAdministrator());
+    }
+
+    public function testOnlyProjectAdminUGroupAreAccepted() : void
+    {
+        $ugroup  = Mockery::mock(ProjectUGroup::class);
+        $ugroup->shouldReceive('getId')->andReturn(ProjectUGroup::PROJECT_MEMBERS);
+
+        $this->expectException(NotProjectAdministratorUGroup::class);
+        new ProjectImportCleanupUserCreatorFromAdministrators(Mockery::mock(PFUser::class), $ugroup);
+    }
+}

tests/simpletest/common/Project/UGroupTest.php
@@ -73,7 +73,8 @@ class UGroup_AddUserTest extends TuleapTestCase {
         $ugroup->addUser($this->user);
     }
     
-    function itAddUserIntoDynamicGroup() {
+    public function itAddsUserIntoDynamicGroup() : void
+    {
         $ugroup_id = $GLOBALS['UGROUP_WIKI_ADMIN'];
         $group_id  = 300;
 
@@ -83,16 +84,11 @@ class UGroup_AddUserTest extends TuleapTestCase {
         $project_manager->shouldReceive('getProject')->with($group_id)->andReturn($project);
         $project->shouldReceive('getAccess')->andReturn(Project::ACCESS_PUBLIC);
 
-        $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('getUserGroupDao'));
+        $ugroup = Mockery::mock(ProjectUGroup::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $ugroup->shouldReceive('addUserToDynamicGroup')->with($this->user)->once();
         
-        $dao = mock('UserGroupDao');
-        stub($ugroup)->getUserGroupDao()->returns($dao);
-        
-        $ugroup->__construct(array('ugroup_id' => $ugroup_id, 'group_id' => $group_id));
-        
-        
-        $dao->expectOnce('updateUserGroupFlags', array($this->user_id, $group_id, 'wiki_flags = 2'));
-        
+        $ugroup->__construct(['ugroup_id' => $ugroup_id, 'group_id' => $group_id]);
+
         $ugroup->addUser($this->user);
     }
     
@@ -164,37 +160,15 @@ class UGroup_RemoveUserTest extends TuleapTestCase {
         $ugroup->removeUser($this->user);
     }
     
-    function itRemoveUserFromDynamicGroup() {
+    public function itRemovesUserFromDynamicGroup() : void
+    {
         $ugroup_id = $GLOBALS['UGROUP_WIKI_ADMIN'];
         $group_id  = 300;
         
-        $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('getUserGroupDao'));
+        $ugroup = Mockery::mock(ProjectUGroup::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $ugroup->shouldReceive('removeUserFromDynamicGroup')->with($this->user)->once();
         
-        $dao = mock('UserGroupDao');
-        stub($ugroup)->getUserGroupDao()->returns($dao);
-        
-        $ugroup->__construct(array('ugroup_id' => $ugroup_id, 'group_id' => $group_id));
-        
-        $dao->expectOnce('updateUserGroupFlags', array($this->user_id, $group_id, 'wiki_flags = 0'));
-        
-        $ugroup->removeUser($this->user);
-    }
-    
-    function itIsNotPossibleToRemoveAllAdminsOfAProject() {
-        $ugroup_id = $GLOBALS['UGROUP_PROJECT_ADMIN'];
-        $group_id  = 300;
-        
-        $ugroup = TestHelper::getPartialMock('ProjectUGroup', array('getUserGroupDao'));
-        
-        $project_admin_dar = TestHelper::emptyDar();
-
-        $dao = stub('UserGroupDao')->searchProjectAdminsByProjectIdExcludingOneUserId()->returns($project_admin_dar);
-        stub($ugroup)->getUserGroupDao()->returns($dao);
-        
-        $ugroup->__construct(array('ugroup_id' => $ugroup_id, 'group_id' => $group_id));
-        
-        $dao->expectNever('updateUserGroupFlags');
-        $this->expectException();
+        $ugroup->__construct(['ugroup_id' => $ugroup_id, 'group_id' => $group_id]);
         
         $ugroup->removeUser($this->user);
     }
@@ -321,27 +295,6 @@ class UGroup_getMembersTest extends UGroup_getUsersBaseTest {
         
         $this->assertArrayNotEmpty($ugroup->getMembersUserName());
         $this->assertArrayNotEmpty($ugroup->getMembersUserName(), array('garfiel', $this->goofy_incomplete_row));
-    }
-}
-
-class UGroup_DynamicGroupTest extends TuleapTestCase {
-    
-    function itConvertDynamicGroupIdToCorrespondingDatabaseFieldUpdateForAdd() {
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId($GLOBALS['UGROUP_PROJECT_ADMIN']),      "admin_flags = 'A'");
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId($GLOBALS['UGROUP_WIKI_ADMIN']),         'wiki_flags = 2');
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId(ProjectUGroup::FORUM_ADMIN),                   "forum_flags = 2");
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId(ProjectUGroup::SVN_ADMIN),                     "svn_flags = 2");
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId(ProjectUGroup::NEWS_ADMIN),                    "news_flags = 2");
-        $this->assertEqual(ProjectUGroup::getAddFlagForUGroupId(ProjectUGroup::NEWS_WRITER), "news_flags = 1");
-    }
-    
-    function itConvertDynamicGroupIdToCorrespondingDatabaseFieldUpdateForRemove() {
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId($GLOBALS['UGROUP_PROJECT_ADMIN']),      "admin_flags = ''");
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId($GLOBALS['UGROUP_WIKI_ADMIN']),         'wiki_flags = 0');
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId(ProjectUGroup::FORUM_ADMIN),                   "forum_flags = 0");
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId(ProjectUGroup::SVN_ADMIN),                     "svn_flags = 0");
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId(ProjectUGroup::NEWS_ADMIN),                    "news_flags = 0");
-        $this->assertEqual(ProjectUGroup::getRemoveFlagForUGroupId(ProjectUGroup::NEWS_WRITER), "news_flags = 0");
     }
 }
 