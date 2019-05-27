2293e3eebc4e16544c3a51e821f2d2a2b82826b8
Thomas Gerbet <thomas.gerbet@enalean.com>
2019-05-27 14:48:24 +0200

    Prevent potential infinite redirection loop when saving the deactivation of deprecated browser warning
    This contribution makes sure the page saving the deactivation of the banner can only
    be called with a POST request as all state changing actions should.
    It also makes sure that if the CSRf verification fails, the user is redirected to an
    existing page.
    
    Fix request #13428: Make sure the page /account/disable_ie7_warning.php does not go in a infinite
    redirection loop when the CSRF verification fails
    
    Change-Id: I9126b21724760e243adec3ef6719f3471c47aa1f

---
+++ src/common/User/Account/DisableLegacyBrowsersWarningMessageController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2014-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\User\Account;
+
+use CSRFSynchronizerToken;
+use Feedback;
+use HTTPRequest;
+use PFUser;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchableWithRequestNoAuthz;
+
+final class DisableLegacyBrowsersWarningMessageController implements DispatchableWithRequestNoAuthz
+{
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables) : void
+    {
+        $csrf = new CSRFSynchronizerToken('/account/disable_legacy_browser_warning');
+        $csrf->check('/my/', $request);
+
+        $request->getCurrentUser()->setPreference(PFUser::PREFERENCE_DISABLE_IE7_WARNING, '1');
+
+        $layout->addFeedback(Feedback::INFO, _('We will bother you later with the deprecation of IE < 11.'));
+        $layout->redirect('/my/');
+    }
+}
