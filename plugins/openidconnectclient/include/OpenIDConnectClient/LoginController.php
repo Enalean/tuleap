Enalean/tuleap

commit 7ee306db8390c9a3b478b2414e686e7d2efd93e9

OpenIDConnect Client plugin uses front controller
This is part of request #13030: Use front router for all plugins

Change-Id: I52adb601553876834061c8281d9c47ee8d31ef73

plugins/openidconnectclient/include/OpenIDConnectClient/LoginController.php
@@ -1,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\OpenIDConnectClient;
+
+use ForgeConfig;
+use HTTPRequest;
+use TemplateRendererFactory;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\OpenIDConnectClient\Login\ConnectorPresenterBuilder;
+use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\ForbiddenException;
+use Tuleap\Request\NotFoundException;
+
+class LoginController implements DispatchableWithRequest
+{
+
+    /**
+     * @var ConnectorPresenterBuilder
+     */
+    private $connector_presenter_builder;
+
+    public function __construct(ConnectorPresenterBuilder $connector_presenter_builder)
+    {
+        $this->connector_presenter_builder = $connector_presenter_builder;
+    }
+
+    /**
+     * Is able to process a request routed by FrontRouter
+     *
+     * @param HTTPRequest $request
+     * @param BaseLayout  $layout
+     * @param array       $variables
+     * @return void
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    {
+        $return_to = $request->get('return_to');
+        $presenter = $this->connector_presenter_builder->getLoginSpecificPageConnectorPresenter($return_to);
+
+        $layout->header(array(
+            'title' => $GLOBALS['Language']->getText('account_login', 'page_title',
+                array(ForgeConfig::get('sys_name'))),
+            'body_class' => array('login-page')
+        ));
+        $renderer = TemplateRendererFactory::build()->getRenderer(OPENIDCONNECTCLIENT_TEMPLATE_DIR);
+        $renderer->renderToPage('login-page', $presenter);
+        $layout->footer(array('without_content' => true));
+    }
+}

plugins/openidconnectclient/include/OpenIDConnectClient/Router.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -21,10 +21,12 @@
 namespace Tuleap\OpenIDConnectClient;
 
 use Feedback;
-use ForgeConfig;
 use HTTPRequest;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Request\DispatchableWithRequest;
 
-class Router {
+class Router implements DispatchableWithRequest
+{
 
     /**
      * @var Login\Controller
@@ -51,7 +53,8 @@ class Router {
         $this->user_mapping_controller   = $user_mapping_controller;
     }
 
-    public function route(HTTPRequest $request) {
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    {
         $action = $request->get('action');
 
         if ($action === 'remove-user-mapping') {
@@ -59,7 +62,7 @@ class Router {
             return;
         }
 
-        $this->checkTLSPresence($request);
+        $this->checkTLSPresence($request, $layout);
         switch ($action) {
             case 'link':
                 $this->account_linker_controller->showIndex($request);
@@ -72,14 +75,14 @@ class Router {
         }
     }
 
-    private function checkTLSPresence(HTTPRequest $request) {
+    private function checkTLSPresence(HTTPRequest $request, BaseLayout $layout)
+    {
         if(! $request->isSecure()) {
-            $GLOBALS['Response']->addFeedback(
+            $layout->addFeedback(
                 Feedback::ERROR,
                 dgettext('tuleap-openidconnectclient', 'The OpenID Connect plugin can only be used if the platform is accessible with HTTPS')
             );
-            $GLOBALS['Response']->redirect('/account/login.php');
+            $layout->redirect('/account/login.php');
         }
     }
-
 }

plugins/openidconnectclient/include/OpenIDConnectClient/AdminRouter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -20,14 +20,16 @@
 
 namespace Tuleap\OpenIDConnectClient;
 
-
 use CSRFSynchronizerToken;
 use Feedback;
 use HTTPRequest;
 use PFUser;
+use Tuleap\Layout\BaseLayout;
 use Tuleap\OpenIDConnectClient\Administration\Controller;
+use Tuleap\Request\DispatchableWithRequest;
 
-class AdminRouter {
+class AdminRouter implements DispatchableWithRequest
+{
     /**
      * @var Controller
      */
@@ -42,9 +44,10 @@ class AdminRouter {
         $this->csrf_token = $csrf_token;
     }
 
-    public function route(HTTPRequest $request) {
+    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    {
         $current_user = $request->getCurrentUser();
-        $this->checkUserIsSiteAdmin($current_user);
+        $this->checkUserIsSiteAdmin($current_user, $layout);
 
         $action = $request->get('action');
         switch ($action) {
@@ -62,13 +65,14 @@ class AdminRouter {
         }
     }
 
-    private function checkUserIsSiteAdmin(PFUser $user) {
+    private function checkUserIsSiteAdmin(PFUser $user, BaseLayout $layout)
+    {
         if(! $user->isSuperUser()) {
-            $GLOBALS['Response']->addFeedback(
+            $layout->addFeedback(
                 Feedback::ERROR,
                 $GLOBALS['Language']->getText('global', 'perm_denied')
             );
-            $GLOBALS['Response']->redirect('/');
+            $layout->redirect('/');
         }
     }
 }

plugins/openidconnectclient/include/openidconnectclientPlugin.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,6 +18,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use FastRoute\RouteCollector;
 use Tuleap\BurningParrotCompatiblePageEvent;
 use Tuleap\Http\HttpClientFactory;
 use Tuleap\Http\HTTPFactoryBuilder;
@@ -41,6 +42,7 @@ use Tuleap\OpenIDConnectClient\Authentication\UserInfo\UserInfoRequestSender;
 use Tuleap\OpenIDConnectClient\Login\ConnectorPresenterBuilder;
 use Tuleap\OpenIDConnectClient\Login;
 use Tuleap\OpenIDConnectClient\Login\IncoherentDataUniqueProviderException;
+use Tuleap\OpenIDConnectClient\LoginController;
 use Tuleap\OpenIDConnectClient\OpenIDConnectClientLogger;
 use Tuleap\OpenIDConnectClient\OpenIDConnectClientPluginInfo;
 use Tuleap\OpenIDConnectClient\Provider\EnableUniqueAuthenticationEndpointVerifier;
@@ -53,11 +55,15 @@ use Tuleap\OpenIDConnectClient\UserMapping\UserPreferencesPresenter;
 use Tuleap\OpenIDConnectClient\UserMapping;
 use Tuleap\OpenIDConnectClient\Administration;
 use Tuleap\Admin\AdminPageRenderer;
+use Tuleap\Request\CollectRoutesEvent;
+use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\DispatchTemporaryRedirect;
 
 require_once __DIR__ . '/constants.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-class openidconnectclientPlugin extends Plugin {
+class openidconnectclientPlugin extends Plugin
+{
     public function __construct($id) {
         parent::__construct($id);
         bindtextdomain('tuleap-openidconnectclient', __DIR__ . '/../site-content');
@@ -79,6 +85,7 @@ class openidconnectclientPlugin extends Plugin {
         $this->addHook(Event::IS_OLD_PASSWORD_REQUIRED_FOR_PASSWORD_CHANGE);
         $this->addHook(Event::GET_LOGIN_URL);
         $this->addHook('display_newaccount');
+        $this->addHook(CollectRoutesEvent::NAME);
     }
 
     /**
@@ -354,9 +361,10 @@ class openidconnectclientPlugin extends Plugin {
         }
     }
 
-    public function process(HTTPRequest $request) {
+    public function routeIndex() : DispatchableWithRequest
+    {
         if(! $this->canPluginAuthenticateUser()) {
-            return;
+            return new DispatchTemporaryRedirect('/');
         }
 
         $user_manager                = UserManager::instance();
@@ -392,14 +400,14 @@ class openidconnectclientPlugin extends Plugin {
             $provider_manager,
             $user_mapping_manager
         );
-        $router                    = new Router(
+        return new Router(
             $login_controller,
             $account_linker_controller,
-            $user_mapping_controller);
-        $router->route($request);
+            $user_mapping_controller
+        );
     }
 
-    public function processAdmin(HTTPRequest $request)
+    public function routeAdminIndex() : DispatchableWithRequest
     {
         $provider_manager                               = $this->getProviderManager();
         $user_mapping_manager                           = new UserMappingManager(new UserMappingDao());
@@ -420,32 +428,29 @@ class openidconnectclientPlugin extends Plugin {
             OPENIDCONNECTCLIENT_BASE_URL . '/admin'
         );
 
-        $router = new AdminRouter($controller, $csrf_token);
-        $router->route($request);
+        return new AdminRouter($controller, $csrf_token);
     }
 
-    public function processSpecificLoginPage(HTTPRequest $request)
+    public function routeLogin() : DispatchableWithRequest
     {
-        if (!$this->canPluginAuthenticateUser()) {
-            $GLOBALS['Response']->redirect('/');
+        if (! $this->canPluginAuthenticateUser()) {
+            return new DispatchTemporaryRedirect('/');
         }
 
-        $provider_manager        = $this->getProviderManager();
-        $login_presenter_builder = new ConnectorPresenterBuilder(
-            $provider_manager,
-            $this->getAuthorizationRequestCreator()
+        return new LoginController(
+            new ConnectorPresenterBuilder(
+                $this->getProviderManager(),
+                $this->getAuthorizationRequestCreator()
+            )
         );
+    }
 
-        $return_to = $request->get('return_to');
-        $presenter = $login_presenter_builder->getLoginSpecificPageConnectorPresenter($return_to);
-
-        $GLOBALS['HTML']->header(array(
-            'title' => $GLOBALS['Language']->getText('account_login', 'page_title',
-                array(ForgeConfig::get('sys_name'))),
-            'body_class' => array('login-page')
-        ));
-        $renderer = TemplateRendererFactory::build()->getRenderer(OPENIDCONNECTCLIENT_TEMPLATE_DIR);
-        $renderer->renderToPage('login-page', $presenter);
-        $GLOBALS['HTML']->footer(array('without_content' => true));
+    public function collectRoutesEvent(CollectRoutesEvent $event) : void
+    {
+        $event->getRouteCollector()->addGroup($this->getPluginPath(), function (RouteCollector $r) {
+            $r->addRoute(['GET', 'POST'], '[/[index.php]]', $this->getRouteHandler('routeIndex'));
+            $r->addRoute(['GET', 'POST'], '/admin[/[index.php]]', $this->getRouteHandler('routeAdminIndex'));
+            $r->addRoute(['GET', 'POST'], '/login.php', $this->getRouteHandler('routeLogin'));
+        });
     }
 }

plugins/openidconnectclient/tests/AdminRouterTest.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,25 +18,12 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('bootstrap.php');
+require_once __DIR__.'/bootstrap.php';
 
 use Tuleap\OpenIDConnectClient\AdminRouter;
 
-class AdminRouterTest extends TuleapTestCase {
-
-    private $existing_response;
-
-    public function setUp() {
-        parent::setUp();
-        $this->existing_response = $GLOBALS['Response'];
-        $GLOBALS['Response']     = mock('Layout');
-    }
-
-    public function tearDown() {
-        $GLOBALS['Response'] = $this->existing_response;
-        parent::tearDown();
-    }
-
+class AdminRouterTest extends TuleapTestCase
+{
     public function itIsOnlyAccessibleBySuperUser() {
         $controller = mock('Tuleap\OpenIDConnectClient\Administration\Controller');
         $csrf_token = mock('CSRFSynchronizerToken');
@@ -46,10 +32,11 @@ class AdminRouterTest extends TuleapTestCase {
         $request    = mock('HTTPRequest');
         $request->setReturnValue('getCurrentUser', $user);
 
-        $GLOBALS['Response']->expectOnce('redirect');
+        $response = Mockery::mock(\Tuleap\Layout\BaseLayout::class);
+        $response->shouldReceive('addFeedback')->once();
+        $response->shouldReceive('redirect')->once();
 
         $router = new AdminRouter($controller, $csrf_token);
-        $router->route($request);
+        $router->process($request, $response, []);
     }
-
 }

plugins/openidconnectclient/tests/RouterTest.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2016 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,36 +18,25 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('bootstrap.php');
+require_once __DIR__.'/bootstrap.php';
 
 use Tuleap\OpenIDConnectClient\Router;
 
 class RouterTest extends TuleapTestCase {
 
-    private $existing_response;
-
-    public function setUp() {
-        parent::setUp();
-        $this->existing_response = $GLOBALS['Response'];
-        $GLOBALS['Response']     = mock('Layout');
-    }
-
-    public function tearDown() {
-        $GLOBALS['Response'] = $this->existing_response;
-        parent::tearDown();
-    }
-
-    public function itOnlyAcceptsHTTPS() {
+    public function itOnlyAcceptsHTTPS()
+    {
         $login_controller          = mock('Tuleap\OpenIDConnectClient\Login\Controller');
         $account_linker_controller = mock('Tuleap\OpenIDConnectClient\AccountLinker\Controller');
         $user_mapping_controller   = mock('Tuleap\OpenIDConnectClient\UserMapping\Controller');
         $request                   = mock('HTTPRequest');
         $request->setReturnValue('isSecure', false);
 
-        $GLOBALS['Response']->expectOnce('redirect');
+        $response = Mockery::mock(\Tuleap\Layout\BaseLayout::class);
+        $response->shouldReceive('addFeedback')->once();
+        $response->shouldReceive('redirect')->once();
 
         $router = new Router($login_controller, $account_linker_controller, $user_mapping_controller);
-        $router->route($request);
+        $router->process($request, $response, []);
     }
-
 }

plugins/openidconnectclient/www/.htaccess
@@ -1,1 +1,0 @@
-php_flag register_globals off

plugins/openidconnectclient/www/admin/index.php
@@ -1,30 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-require_once('pre.php');
-
-$plugin_manager = PluginManager::instance();
-$plugin         = $plugin_manager->getPluginByName('openidconnectclient');
-$request        = HTTPRequest::instance();
-if ($plugin && $plugin_manager->isPluginAvailable($plugin)) {
-    $plugin->processAdmin($request);
-} else {
-    header('Location: ' . $request->getServerUrl());
-}

commit ec5d55e861d3e986ac3bb1b55b4c5e82f641aaa5

request #13433: Cannot log in with an OpenID Connect provider when the instance does not allow anonymous user
Issue introduced by 7ee306db8390c9a3b478b2414e686e7d2efd93e9.

Change-Id: I5134b2a3e026d730ad26dc473b222df0bcedbc2a

plugins/openidconnectclient/include/OpenIDConnectClient/LoginController.php
@@ -29,10 +29,11 @@ use TemplateRendererFactory;
 use Tuleap\Layout\BaseLayout;
 use Tuleap\OpenIDConnectClient\Login\ConnectorPresenterBuilder;
 use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\DispatchableWithRequestNoAuthz;
 use Tuleap\Request\ForbiddenException;
 use Tuleap\Request\NotFoundException;
 
-class LoginController implements DispatchableWithRequest
+class LoginController implements DispatchableWithRequestNoAuthz
 {
 
     /**

plugins/openidconnectclient/include/OpenIDConnectClient/Router.php
@@ -23,9 +23,9 @@ namespace Tuleap\OpenIDConnectClient;
 use Feedback;
 use HTTPRequest;
 use Tuleap\Layout\BaseLayout;
-use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\DispatchableWithRequestNoAuthz;
 
-class Router implements DispatchableWithRequest
+class Router implements DispatchableWithRequestNoAuthz
 {
 
     /**