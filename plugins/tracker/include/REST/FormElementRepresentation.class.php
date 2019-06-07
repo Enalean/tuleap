Enalean/tuleap

commit a31b5abfef4d08a957eed302d9e8d6fc472ae14a

Refactoring: form element representations are build in a dedicated object
This is part of story #12188 hide fieldsets according to state

No functional changes here.
We extract the build of FormElement representations into a dedicated
object.

This will ease the code readability and the ability to build
representations based on different contexts.

Change-Id: I5bd2ecd390c07ff577e05a68fdc27f5be6a18b8c

plugins/tracker/include/REST/FormElementRepresentation.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013. All Rights Reserved.
+ * Copyright (c) Enalean, 2013 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -20,7 +20,8 @@
 
 use Tuleap\REST\JsonCast;
 
-class Tracker_REST_FieldRepresentation {
+class Tracker_REST_FormElementRepresentation //phpcs:ignore
+{
 
     public const BIND_TYPE  = 'type';
     public const BIND_LIST  = 'list';
@@ -53,7 +54,7 @@ class Tracker_REST_FieldRepresentation {
     public $type;
 
     /**
-     * @var array {@type Tuleap\Tracker\REST\FieldValueRepresentation }
+     * @var array|null {@type Tuleap\Tracker\REST\FieldValueRepresentation }
      */
     public $values = array();
 
@@ -79,36 +80,42 @@ class Tracker_REST_FieldRepresentation {
      */
     public $permissions = array();
 
-    public function build(Tracker_FormElement $field, $type, array $permissions) {
-        $this->field_id = JsonCast::toInt($field->getId());
-        $this->name     = $field->getName();
-        $this->label    = $field->getLabel();
+    /**
+     * @var mixed
+     */
+    public $default_value;
 
-        if ($field instanceof Tracker_FormElement_Field) {
-            $this->required      = JsonCast::toBoolean($field->isRequired());
-            $this->collapsed     = false;
+    public function build(Tracker_FormElement $form_element, $type, array $permissions)
+    {
+        $this->field_id = JsonCast::toInt($form_element->getId());
+        $this->name     = $form_element->getName();
+        $this->label    = $form_element->getLabel();
 
+        if ($form_element instanceof Tracker_FormElement_Field) {
+            $this->required  = JsonCast::toBoolean($form_element->isRequired());
+            $this->collapsed = false;
         } else {
-            $this->required      = false;
-            $this->collapsed     = (bool) $field->isCollapsed();
+            /** @var Tracker_FormElement_Container $form_element */
+            $this->required  = false;
+            $this->collapsed = (bool) $form_element->isCollapsed();
         }
 
-        $this->default_value = $field->getDefaultRESTValue();
+        $this->default_value = $form_element->getDefaultRESTValue();
         $this->type   = $type;
 
         $this->values = null;
-        if ($field->getRESTAvailableValues()) {
-            $this->values = $field->getRESTAvailableValues();
+        if ($form_element->getRESTAvailableValues()) {
+            $this->values = $form_element->getRESTAvailableValues();
         }
 
-        $bindings = $field->getRESTBindingProperties();
+        $bindings = $form_element->getRESTBindingProperties();
         $this->bindings = array(
             self::BIND_TYPE => $bindings[Tracker_FormElement_Field_List_Bind::REST_TYPE_KEY],
             self::BIND_LIST => array_map(
                 function ($binding) {
                     return array(
-                        Tracker_REST_FieldRepresentation::BIND_ID   => $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_ID],
-                        Tracker_REST_FieldRepresentation::BIND_LABEL=> $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_LABEL]
+                        Tracker_REST_FormElementRepresentation::BIND_ID   => $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_ID],
+                        Tracker_REST_FormElementRepresentation::BIND_LABEL=> $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_LABEL]
                     );
                 },
                 $bindings[Tracker_FormElement_Field_List_Bind::REST_LIST_KEY]
@@ -119,11 +126,11 @@ class Tracker_REST_FieldRepresentation {
             function ($permission) {
                 switch ($permission) {
                     case Tracker_FormElement::REST_PERMISSION_READ:
-                        return Tracker_REST_FieldRepresentation::PERM_READ;
+                        return Tracker_REST_FormElementRepresentation::PERM_READ;
                     case Tracker_FormElement::REST_PERMISSION_UPDATE:
-                        return Tracker_REST_FieldRepresentation::PERM_UPDATE;
+                        return Tracker_REST_FormElementRepresentation::PERM_UPDATE;
                     case Tracker_FormElement::REST_PERMISSION_SUBMIT:
-                        return Tracker_REST_FieldRepresentation::PERM_CREATE;
+                        return Tracker_REST_FormElementRepresentation::PERM_CREATE;
                 }
             },
             $permissions

plugins/timetracking/include/Timetracking/REST/v1/ProjectResource.php
@@ -37,6 +37,7 @@ use Tuleap\Timetracking\Admin\TimetrackingUgroupRetriever;
 use Tuleap\Timetracking\Permissions\PermissionsRetriever;
 use Tuleap\Timetracking\Time\TimeDao;
 use Tuleap\Timetracking\Time\TimeRetriever;
+use Tuleap\Tracker\REST\FormElementRepresentationsBuilder;
 use Tuleap\Tracker\REST\PermissionsExporter;
 use Tuleap\Tracker\Workflow\PostAction\FrozenFields\FrozenFieldsDao;
 use Tuleap\Tracker\Workflow\PostAction\FrozenFields\FrozenFieldDetector;
@@ -84,20 +85,23 @@ class ProjectResource
             TrackerFactory::instance(),
             new Tracker_REST_TrackerRestBuilder(
                 Tracker_FormElementFactory::instance(),
-                new PermissionsExporter(
-                    new FrozenFieldDetector(
-                        new TransitionRetriever(
-                            new StateFactory(
-                                new TransitionFactory(
-                                    Workflow_Transition_ConditionFactory::build()
+                new FormElementRepresentationsBuilder(
+                    Tracker_FormElementFactory::instance(),
+                    new PermissionsExporter(
+                        new FrozenFieldDetector(
+                            new TransitionRetriever(
+                                new StateFactory(
+                                    new TransitionFactory(
+                                        Workflow_Transition_ConditionFactory::build()
+                                    ),
+                                    new SimpleWorkflowDao()
                                 ),
-                                new SimpleWorkflowDao()
+                                new TransitionExtractor()
                             ),
-                            new TransitionExtractor()
-                        ),
-                        new FrozenFieldsRetriever(
-                            new FrozenFieldsDao(),
-                            Tracker_FormElementFactory::instance()
+                            new FrozenFieldsRetriever(
+                                new FrozenFieldsDao(),
+                                Tracker_FormElementFactory::instance()
+                            )
                         )
                     )
                 )

plugins/timetracking/include/Timetracking/REST/v1/TimetrackingOverviewRepresentationsBuilder.php
@@ -117,7 +117,7 @@ class TimetrackingOverviewRepresentationsBuilder
         foreach ($trackers_row as $tracker_row) {
             $tracker = $this->tracker_factory->getTrackerById($tracker_row["tracker_id"]);
             if ($this->checkTrackerAndPermissions($tracker, $user)) {
-                $tracker_representations["trackers"][] = $this->tracker_rest_builder->getTrackerRepresentationWithoutWorkflowComputedPermissions(
+                $tracker_representations["trackers"][] = $this->tracker_rest_builder->getTrackerRepresentationInTrackerContext(
                     $user,
                     $tracker
                 );

plugins/timetracking/phpunit/Time/TimetrackingOverviewRepresentationsBuilderTest.php
@@ -162,7 +162,7 @@ class TimetrackingOverviewRepresentationsBuilderTest extends TestCase
         );
 
         $this->tracker_factory->shouldReceive('getTrackerById')->with(16)->andReturn($this->tracker);
-        $this->tracker_rest_builder->shouldReceive('getTrackerRepresentationWithoutWorkflowComputedPermissions')->with(
+        $this->tracker_rest_builder->shouldReceive('getTrackerRepresentationInTrackerContext')->with(
             $this->user,
             $this->tracker
         )->andReturn($tracker_representation);

plugins/tracker/include/REST/FieldDateRepresentation.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,18 +18,18 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Tracker_REST_FieldDateRepresentation extends Tracker_REST_FieldRepresentation {
+class Tracker_REST_FormElementDateRepresentation extends Tracker_REST_FormElementRepresentation {
 
     /**
      * @var bool
      */
     public $is_time_displayed;
 
-    public function build(Tracker_FormElement $field, $type, array $permissions) {
-        parent::build($field, $type, $permissions);
+    public function build(Tracker_FormElement $form_element, $type, array $permissions) {
+        parent::build($form_element, $type, $permissions);
 
-        if ($field instanceof Tracker_FormElement_Field_Date) {
-            $this->is_time_displayed = $field->isTimeDisplayed();
+        if ($form_element instanceof Tracker_FormElement_Field_Date) {
+            $this->is_time_displayed = $form_element->isTimeDisplayed();
         }
     }
 }

plugins/tracker/include/REST/FieldOpenListRepresentation.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013. All Rights Reserved.
+ * Copyright (c) Enalean, 2013 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,121 +18,20 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Tuleap\REST\JsonCast;
-
-class Tracker_REST_FieldOpenListRepresentation {
-
-    public const BIND_TYPE  = 'type';
-    public const BIND_LIST  = 'list';
-
-    public const BIND_ID    = 'id';
-    public const BIND_LABEL = 'label';
-
-    public const PERM_READ   = 'read';
-    public const PERM_UPDATE = 'update';
-    public const PERM_CREATE = 'create';
-
-    /**
-     * @var int
-     */
-    public $field_id;
-
-    /**
-     * @var string
-     */
-    public $label;
-
-    /**
-     * @var string
-     */
-    public $name;
-
-    /**
-     * @var string (string|text|sb|msb|cb|date|file|int|float|tbl|art_link|perm|shared|aid|atid|lud|subby|subon|cross|burndown|computed|fieldset|column|linebreak|separator|staticrichtext)
-     */
-    public $type;
-
-    /**
-     * @var array {@type Tuleap\Tracker\REST\FieldValueRepresentation }
-     */
-    public $values = array();
-
-    /**
-     *
-     * @var bool
-     */
-    public $required;
-
-    /**
-     *
-     * @var bool
-     */
-    public $collapsed;
-
-    /**
-     * @var array
-     */
-    public $bindings = array();
-
-    /**
-     * @var array {@type string} One of (read, update, submit)
-     */
-    public $permissions = array();
+class Tracker_REST_FieldOpenListRepresentation extends  Tracker_REST_FormElementRepresentation
+{
 
     /**
      * @var string
      */
     public $hint;
 
-    public function build(Tracker_FormElement_Field_OpenList $field, $type, array $permissions) {
-        $this->field_id = JsonCast::toInt($field->getId());
-        $this->name     = $field->getName();
-        $this->label    = $field->getLabel();
-        $this->hint     = $field->getProperty('hint');
+    public function build(Tracker_FormElement $form_element, $type, array $permissions)
+    {
+        parent::build($form_element, $type, $permissions);
 
-        if ($field instanceof Tracker_FormElement_Field) {
-            $this->required      = JsonCast::toBoolean($field->isRequired());
-            $this->collapsed     = false;
-
-        } else {
-            $this->required      = false;
-            $this->collapsed     = (bool) $field->isCollapsed();
+        if ($form_element instanceof Tracker_FormElement_Field_OpenList) {
+            $this->hint = $form_element->getProperty('hint');
         }
-
-        $this->default_value = $field->getDefaultRESTValue();
-        $this->type   = $type;
-
-        $this->values = null;
-        if ($field->getRESTAvailableValues()) {
-            $this->values = $field->getRESTAvailableValues();
-        }
-
-        $bindings = $field->getRESTBindingProperties();
-        $this->bindings = array(
-            self::BIND_TYPE => $bindings[Tracker_FormElement_Field_List_Bind::REST_TYPE_KEY],
-            self::BIND_LIST => array_map(
-                function ($binding) {
-                    return array(
-                        Tracker_REST_FieldRepresentation::BIND_ID   => $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_ID],
-                        Tracker_REST_FieldRepresentation::BIND_LABEL=> $binding[Tracker_FormElement_Field_List_Bind_Users::REST_BINDING_LIST_LABEL]
-                    );
-                },
-                $bindings[Tracker_FormElement_Field_List_Bind::REST_LIST_KEY]
-            )
-        );
-
-        $this->permissions = array_map(
-            function ($permission) {
-                switch ($permission) {
-                    case Tracker_FormElement::REST_PERMISSION_READ:
-                        return Tracker_REST_FieldRepresentation::PERM_READ;
-                    case Tracker_FormElement::REST_PERMISSION_UPDATE:
-                        return Tracker_REST_FieldRepresentation::PERM_UPDATE;
-                    case Tracker_FormElement::REST_PERMISSION_SUBMIT:
-                        return Tracker_REST_FieldRepresentation::PERM_CREATE;
-                }
-            },
-            $permissions
-        );
     }
 }

plugins/tracker/include/REST/FormElementRepresentationsBuilder.php
@@ -1,0 +1,118 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\REST;
+
+use PFUser;
+use Tracker;
+use Tracker_Artifact;
+use Tracker_FormElement;
+use Tracker_FormElement_Field_Date;
+use Tracker_FormElement_Field_OpenList;
+use Tracker_FormElementFactory;
+use Tracker_REST_FieldOpenListRepresentation;
+use Tracker_REST_FormElementDateRepresentation;
+use Tracker_REST_FormElementRepresentation;
+
+class FormElementRepresentationsBuilder
+{
+    /**
+     * @var PermissionsExporter
+     */
+    private $permissions_exporter;
+    /**
+     * @var Tracker_FormElementFactory
+     */
+    private $form_element_factory;
+
+    public function __construct(
+        Tracker_FormElementFactory $form_element_factory,
+        PermissionsExporter $permissions_exporter
+    ) {
+        $this->permissions_exporter = $permissions_exporter;
+        $this->form_element_factory = $form_element_factory;
+    }
+
+    /**
+     * @return Tracker_REST_FormElementRepresentation[]
+     */
+    public function buildRepresentationsInTrackerContext(Tracker $tracker, PFUser $user) : array
+    {
+        return $this->buildRepresentations($tracker, null, $user);
+    }
+
+    /**
+     * @return Tracker_REST_FormElementRepresentation[]
+     */
+    public function buildRepresentationsInArtifactContext(Tracker_Artifact $artifact, PFUser $user) : array
+    {
+        return $this->buildRepresentations($artifact->getTracker(), $artifact, $user);
+    }
+
+    /**
+     * @return Tracker_REST_FormElementRepresentation[]
+     */
+    private function buildRepresentations(Tracker $tracker, ?Tracker_Artifact $artifact, PFUser $user) : array
+    {
+        $representation_collection = [];
+        foreach ($this->form_element_factory->getAllUsedFormElementOfAnyTypesForTracker($tracker) as $form_element) {
+            if (! $form_element->userCanRead($user)) {
+                continue;
+            }
+
+            if ($form_element instanceof Tracker_FormElement_Field_Date) {
+                $form_element_representation = new Tracker_REST_FormElementDateRepresentation();
+            } elseif ($form_element instanceof Tracker_FormElement_Field_OpenList) {
+                $form_element_representation = new Tracker_REST_FieldOpenListRepresentation();
+            } else {
+                $form_element_representation = new Tracker_REST_FormElementRepresentation();
+            }
+
+            $form_element_representation->build(
+                $form_element,
+                $this->form_element_factory->getType($form_element),
+                $this->getPermissionsForFormElement($form_element, $artifact, $user)
+            );
+
+            $representation_collection[] = $form_element_representation;
+        }
+
+        return $representation_collection;
+    }
+
+    private function getPermissionsForFormElement(
+        Tracker_FormElement $form_element,
+        ?Tracker_Artifact $artifact,
+        PFUser $user
+    ) : array {
+        if ($artifact === null) {
+            return $this->permissions_exporter->exportUserPermissionsForFieldWithoutWorkflowComputedPermissions(
+                $user,
+                $form_element
+            );
+        }
+
+        return $this->permissions_exporter->exportUserPermissionsForFieldWithWorkflowComputedPermissions(
+            $user,
+            $form_element,
+            $artifact
+        );
+    }
+}

plugins/tracker/include/REST/PermissionsExporter.php
@@ -38,11 +38,11 @@ class PermissionsExporter
     /**
      * @var FrozenFieldDetector
      */
-    private $read_only_field_detector;
+    private $frozen_field_detector;
 
     public function __construct(FrozenFieldDetector $read_only_field_detector)
     {
-        $this->read_only_field_detector = $read_only_field_detector;
+        $this->frozen_field_detector = $read_only_field_detector;
     }
 
     public function exportUserPermissionsForFieldWithoutWorkflowComputedPermissions(
@@ -63,7 +63,7 @@ class PermissionsExporter
             return $permissions;
         }
 
-        if ($this->read_only_field_detector->isFieldFrozen($artifact, $field)) {
+        if ($this->frozen_field_detector->isFieldFrozen($artifact, $field)) {
             $permissions = $this->removeUpdatePermissionFromField($permissions);
         }
 

commit 474dba3ddcf8fcc3fe407935b4f220acd03f5eb8

The parsing of Tracker_REST_FormElementRepresentation breaks the generation the OpenAPI specification file
Due to an issue in the parsing of union annotations, Restler breaks and generates
a PHP warning when you access the API Explorer for the first time.

This contribution works around the issue.
You can reproduce by clearing Tuleap cache and then accessing to the
API Explorer.

Issue has been introduced by a31b5abfef4d08a957eed302d9e8d6fc472ae14a.

This is part of story #12188 hide fieldsets according to state

Change-Id: I9387a192fc51e857da1a625516cf47f7aedd1b93

plugins/tracker/include/REST/FormElementRepresentation.class.php
@@ -54,7 +54,7 @@ class Tracker_REST_FormElementRepresentation //phpcs:ignore
     public $type;
 
     /**
-     * @var array|null {@type Tuleap\Tracker\REST\FieldValueRepresentation }
+     * @var array | null {@type Tuleap\Tracker\REST\FieldValueRepresentation }
      */
     public $values = array();
 