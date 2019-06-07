e1f30f4675588f901e9062240cbcc210fa24ca39
Joris Masson <joris.masson@enalean.com>
2019-06-07 15:54:42 +0200

    Fix loading of translations for TTM
    Part of request #13473 Artifact modal is never localized
    
    How to test:
    - When you set your user language to "Français" in your profile and you
    open the artifact modal in Test Management, strings are localized in
    French.
    - Strings are still localized in Test Management app.
    
    Change-Id: I63a8176bdcac8b96bee77e98a431026e7e7e93dd

--- scripts/angular/src/app/app.js
+++ scripts/angular/src/app/app.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Enalean, 2017-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import angular from "angular";
 import ngSanitize from "angular-sanitize";
 import ui_router from "angular-ui-router";
@@ -6,7 +25,7 @@ import angular_tlp from "angular-tlp";
 
 import "angular-breadcrumb";
 import "angular-gettext";
-import "../../po/fr.po";
+import translations from "../../po/fr.po";
 import "angular-moment";
 import "d3-selection-multi";
 
@@ -48,6 +67,14 @@ export default angular
         artifact_links_graph
     ])
     .config(TestManagementConfig)
+    .run([
+        "gettextCatalog",
+        function(gettextCatalog) {
+            for (const [language, strings] of Object.entries(translations)) {
+                gettextCatalog.setStrings(language, strings);
+            }
+        }
+    ])
     .directive("autoFocus", AutoFocusDirective)
     .directive("enableTlpTableFilter", EnableTlpTableFilterDirective)
     .directive("openTlpDropdown", OpenTlpDropdownDirective)
