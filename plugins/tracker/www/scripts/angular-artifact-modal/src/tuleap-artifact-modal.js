45b0a8078f6830349f3696fc6e52fe5286c39c29
Joris Masson <joris.masson@enalean.com>
2019-06-07 15:42:40 +0200

    Fix loading of translations for angular apps
    Part of request #13473 Artifact modal is never localized
    
    Other patches will be needed for external angular js apps.
    
    How to test:
    - When you set your user language to "Français" in your profile and you
    open the artifact modal (kanban, scrum planning), strings are localized
    in French.
    - No functional change in other apps. Strings are still localized.
    
    Change-Id: Id4d4c8ea5742ae5784ccec64a9c979c0bf4b27ad

--- plugins/tracker/www/scripts/angular-artifact-modal/src/tuleap-artifact-modal.js
+++ plugins/tracker/www/scripts/angular-artifact-modal/src/tuleap-artifact-modal.js
@@ -1,10 +1,29 @@
+/*
+ * Copyright (c) Enalean, 2017-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import angular from "angular";
 import ngSanitize from "angular-sanitize";
 
 import "imports-loader?CKEDITOR=>window.CKEDITOR!ng-ckeditor";
 import "angular-moment";
 import "angular-gettext";
-import "../po/fr.po";
+import translations from "../po/fr.po";
 
 import fields from "./tuleap-artifact-modal-fields/fields.js";
 import model from "./model/model.js";
@@ -29,6 +48,14 @@ angular
         ngSanitize,
         quota_display,
         tuleap_highlight
+    ])
+    .run([
+        "gettextCatalog",
+        function(gettextCatalog) {
+            for (const [language, strings] of Object.entries(translations)) {
+                gettextCatalog.setStrings(language, strings);
+            }
+        }
     ])
     .component("tuleapArtifactModalNewFollowup", NewFollowupComponent)
     .controller("TuleapArtifactModalController", ArtifactModalController)
