Enalean/tuleap

commit 07f0ec8534779904bcae245c636a932c73463972

Refactoring: extract fields to be saved in method
No functional changes.

Later we will need to save fields in a specific order. To avoid
proposing a too big changeset, this one only deal with moving duplicate
usage of formelement factory in one unique method.

As a bonus, declare return types

Part of story #13453: finish drag'n drop images in tracker text areas

Change-Id: I58637f411f7c8c9edd09b2da467f5627ca5ea16f

plugins/tracker/include/Tracker/Artifact/Changeset/InitialChangesetCreatorBase.class.php
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 use Tuleap\Tracker\Artifact\Event\ArtifactCreated;
 
 /**
@@ -56,20 +58,21 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
      *
      * @return int The Id of the initial changeset, or null if fields were not valid
      */
-    public function create(Tracker_Artifact $artifact, array $fields_data, PFUser $submitter, $submitted_on) {
+    public function create(Tracker_Artifact $artifact, array $fields_data, PFUser $submitter, $submitted_on): ?int
+    {
         if (! $this->doesRequestAppearToBeValid($artifact, $fields_data, $submitter)) {
-            return;
+            return null;
         }
 
         $this->initializeAFakeChangesetSoThatListAndWorkflowEncounterAnEmptyState($artifact);
 
         if (! $this->askWorkflowToUpdateTheRequestAndCheckGlobalRules($artifact, $fields_data, $submitter)) {
-            return;
+            return null;
         }
 
         $changeset_id = $this->createChangesetId($artifact, $submitter, $submitted_on);
         if (! $changeset_id) {
-            return;
+            return null;
         }
 
         $this->storeFieldsValues($artifact, $fields_data, $submitter, $changeset_id);
@@ -88,28 +91,32 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
         return $changeset_id;
     }
 
-    /**
-     * @return void
-     */
     protected abstract function saveNewChangesetForField(
         Tracker_FormElement_Field $field,
         Tracker_Artifact $artifact,
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    );
+    ): void;
 
-    private function storeFieldsValues(Tracker_Artifact $artifact, array $fields_data, PFUser $submitter, $changeset_id) {
-        $used_fields = $this->formelement_factory->getUsedFields($artifact->getTracker());
-        foreach ($used_fields as $field) {
+    private function storeFieldsValues(
+        Tracker_Artifact $artifact,
+        array $fields_data,
+        PFUser $submitter,
+        $changeset_id
+    ): void {
+        foreach ($this->getFieldsToBeSavedInCorrectOrder($artifact) as $field) {
             $this->saveNewChangesetForField($field, $artifact, $fields_data, $submitter, $changeset_id);
         }
     }
 
+    /**
+     * @return int|false
+     */
     private function createChangesetId(
         Tracker_Artifact $artifact,
         PFUser $submitter,
-        $submitted_on
+        int $submitted_on
     ) {
         $email = null;
         if ($submitter->isAnonymous()) {
@@ -123,7 +130,7 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
         Tracker_Artifact $artifact,
         array $fields_data,
         PFUser $submitter
-    ) {
+    ): bool {
         if ($submitter->isAnonymous() && ! trim($submitter->getEmail())) {
             $GLOBALS['Response']->addFeedback('error', $GLOBALS['Language']->getText('plugin_tracker_artifact', 'email_required'));
             return false;
@@ -140,7 +147,7 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
         Tracker_Artifact $artifact,
         array &$fields_data,
         PFUser $submitter
-    ) {
+    ): bool {
         try {
             $workflow = $artifact->getWorkflow();
             $workflow->validate($fields_data, $artifact, "");
@@ -155,7 +162,7 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
         }
     }
 
-    private function initializeAFakeChangesetSoThatListAndWorkflowEncounterAnEmptyState(Tracker_Artifact $artifact) {
+    private function initializeAFakeChangesetSoThatListAndWorkflowEncounterAnEmptyState(Tracker_Artifact $artifact): void {
         $artifact->setChangesets(array(new Tracker_Artifact_Changeset_Null()));
     }
 }

plugins/tracker/include/Tracker/Artifact/Changeset/NewChangesetCreatorBase.class.php
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 use Tuleap\Tracker\Artifact\ArtifactInstrumentation;
 use Tuleap\Tracker\Artifact\Exception\FieldValidationException;
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationCollectionBuilder;
@@ -85,7 +87,7 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         $submitted_on,
         $send_notification,
         $comment_format
-    ) {
+    ): ?Tracker_Artifact_Changeset {
         $comment = trim($comment);
 
         $this->changeset_dao->startTransaction();
@@ -175,9 +177,6 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         return $new_changeset;
     }
 
-    /**
-     * @return void
-     */
     protected abstract function saveNewChangesetForField(
         Tracker_FormElement_Field $field,
         Tracker_Artifact $artifact,
@@ -185,15 +184,19 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    );
+    ): bool;
 
     /**
      * @throws Tracker_FieldValueNotStoredException
      */
-    private function storeFieldsValues(Tracker_Artifact $artifact, $previous_changeset, array $fields_data, PFUser $submitter, $changeset_id) {
-        $used_fields = $this->formelement_factory->getUsedFields($artifact->getTracker());
-
-        foreach ($used_fields as $field) {
+    private function storeFieldsValues(
+        Tracker_Artifact $artifact,
+        $previous_changeset,
+        array $fields_data,
+        PFUser $submitter,
+        $changeset_id
+    ): bool {
+        foreach ($this->getFieldsToBeSavedInCorrectOrder($artifact) as $field) {
             if (! $this->saveNewChangesetForField($field, $artifact, $previous_changeset, $fields_data, $submitter, $changeset_id)) {
                 $this->changeset_dao->rollBack();
                 $purifier = Codendi_HTMLPurifier::instance();
@@ -217,7 +220,7 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         $submitted_on,
         $comment_format,
         $changeset_id
-    ) {
+    ): bool {
         $comment_format = Tracker_Artifact_Changeset_Comment::checkCommentFormat($comment_format);
 
         $comment_added = $this->changeset_comment_dao->createNewVersion($changeset_id, $comment, $submitter->getId(), $submitted_on, 0, $comment_format);
@@ -243,7 +246,7 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
         $comment,
         PFUser $submitter,
         $email
-    ) {
+    ): bool {
         if ($submitter->isAnonymous() && ($email == null || $email == '')) {
             $message = $GLOBALS['Language']->getText('plugin_tracker_artifact', 'email_required');
             throw new Tracker_Exception($message);
@@ -270,7 +273,7 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
          * We need to run the post actions to validate the data
          */
         $workflow->before($fields_data, $submitter, $artifact);
-        $workflow->checkGlobalRules($fields_data, $this->formelement_factory);
+        $workflow->checkGlobalRules($fields_data);
 
         return true;
     }

plugins/tracker/include/Tracker/Artifact/Changeset/ChangesetCreatorBase.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,12 +18,13 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 /**
  * I am a Template Method to create an initial changeset.
  */
-abstract class Tracker_Artifact_Changeset_ChangesetCreatorBase {
-
-
+abstract class Tracker_Artifact_Changeset_ChangesetCreatorBase
+{
     /** @var Tracker_Artifact_Changeset_FieldsValidator */
     protected $fields_validator;
 
@@ -53,10 +54,8 @@ abstract class Tracker_Artifact_Changeset_ChangesetCreatorBase {
         $this->field_initializator = $field_initializator;
     }
 
-    /**
-     * @return bool
-     */
-    protected function isFieldSubmitted(Tracker_FormElement_Field $field, array $fields_data) {
+    protected function isFieldSubmitted(Tracker_FormElement_Field $field, array $fields_data): bool
+    {
         return isset($fields_data[$field->getId()]);
     }
 
@@ -71,10 +70,9 @@ abstract class Tracker_Artifact_Changeset_ChangesetCreatorBase {
         PFUser $submitter,
         Tracker_Artifact_Changeset $new_changeset,
         ?Tracker_Artifact_Changeset $previous_changeset = null
-    ) {
+    ): bool {
         if ($this->artifact_factory->save($artifact)) {
-            $used_fields = $this->formelement_factory->getUsedFields($artifact->getTracker());
-            foreach ($used_fields as $field) {
+            foreach ($this->getFieldsToBeSavedInCorrectOrder($artifact) as $field) {
                 $field->postSaveNewChangeset($artifact, $submitter, $new_changeset, $previous_changeset);
             }
 
@@ -84,5 +82,15 @@ abstract class Tracker_Artifact_Changeset_ChangesetCreatorBase {
         }
 
         return false;
+    }
+
+    /**
+     * @param Tracker_Artifact $artifact
+     *
+     * @return Tracker_FormElement_Field[]
+     */
+    protected function getFieldsToBeSavedInCorrectOrder(Tracker_Artifact $artifact): array
+    {
+        return $this->formelement_factory->getUsedFields($artifact->getTracker());
     }
 }

plugins/tracker/include/Tracker/Artifact/Changeset/InitialChangesetAtGivenDateCreator.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -17,6 +17,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+
+declare(strict_types=1);
 
 /**
  * I create an initial changeset at a given date.
@@ -36,7 +38,7 @@ class Tracker_Artifact_Changeset_InitialChangesetAtGivenDateCreator extends Trac
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    ) {
+    ): void {
         $is_submission = true;
         $bypass_perms  = true;
         if ($this->isFieldSubmitted($field, $fields_data)) {

plugins/tracker/include/Tracker/Artifact/Changeset/InitialChangesetCreator.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 /**
  * I create an initial changeset
  */
@@ -32,7 +34,7 @@ class Tracker_Artifact_Changeset_InitialChangesetCreator extends Tracker_Artifac
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    ) {
+    ): void {
         $is_submission = true;
         $bypass_perms  = true;
         $workflow      = $artifact->getWorkflow();
@@ -53,7 +55,8 @@ class Tracker_Artifact_Changeset_InitialChangesetCreator extends Tracker_Artifac
         }
     }
 
-    private function pushDefaultValueInSubmittedValues(Tracker_FormElement_Field $field, array &$fields_data) {
+    private function pushDefaultValueInSubmittedValues(Tracker_FormElement_Field $field, array &$fields_data): void
+    {
         $fields_data[$field->getId()] = $field->getDefaultValue();
     }
 }

plugins/tracker/include/Tracker/Artifact/Changeset/NewChangesetAtGivenDateCreator.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,7 +18,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationCollectionBuilder;
+declare(strict_types=1);
 
 /**
  * I create a new changeset (update of an artifact) at a given date.
@@ -29,9 +29,6 @@ use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationCollectionB
  */
 class Tracker_Artifact_Changeset_NewChangesetAtGivenDateCreator extends Tracker_Artifact_Changeset_NewChangesetCreatorBase
 {
-    /**
-     * @see Tracker_Artifact_Changeset_NewChangesetCreatorBase::saveNewChangesetForField()
-     */
     protected function saveNewChangesetForField(
         Tracker_FormElement_Field $field,
         Tracker_Artifact $artifact,
@@ -39,7 +36,7 @@ class Tracker_Artifact_Changeset_NewChangesetAtGivenDateCreator extends Tracker_
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    ) {
+    ): bool {
         $is_submission = false;
         $bypass_perms  = true;
 

plugins/tracker/include/Tracker/Artifact/Changeset/NewChangesetCreator.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,7 +18,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Tuleap\Tracker\FormElement\Field\ArtifactLink\SourceOfAssociationCollectionBuilder;
+declare(strict_types=1);
 
 /**
  * I create a new changeset (update of an artifact)
@@ -35,7 +35,7 @@ class Tracker_Artifact_Changeset_NewChangesetCreator extends Tracker_Artifact_Ch
         array $fields_data,
         PFUser $submitter,
         $changeset_id
-    ) {
+    ): bool {
         $is_submission = false;
         $bypass_perms  = true;
         $workflow      = $artifact->getWorkflow();

plugins/tracker/tests/Artifact/Changeset/InitialChangesetCreatorTest.php
@@ -96,7 +96,7 @@ class Tracker_Artifact_Changeset_InitialChangesetCreator_WorkflowTest extends Tr
 
     public function itDoesNotCallTheAfterMethodOnWorkflowWhenSaveOfArtifactFails()
     {
-        stub($this->changeset_dao)->create()->returns(true);
+        stub($this->changeset_dao)->create()->returns(123);
         stub($this->artifact_factory)->save()->returns(false);
         stub($this->workflow)->validate()->returns(true);
 
@@ -134,7 +134,7 @@ class Tracker_Artifact_Changeset_InitialChangesetCreator_DefaultValueTest extend
 
         parent::setUp();
 
-        stub($this->changeset_dao)->create()->returns(true);
+        stub($this->changeset_dao)->create()->returns(123);
     }
 
     protected function getFields() {

commit fff71fd25e921af744a92c1814b5771ef69d5ced

Strengthen typehint in *ChangesetCreaterBase::create
Strict typing introduced at git #tuleap/stable/07f0ec8534779904bcae245c636a932c73463972
broke cardwall where artifact are updated in place, without any comment.

Go to a cardwall, edit in place the remaining effort of a story, refresh
the page. The card has the new value.

Part of story #13453: finish drag'n drop images in tracker text areas

Change-Id: Ib7ee3df14ccaa3b2ab3a7beb6c430b8f59e39c32

plugins/tracker/include/Tracker/Artifact/Changeset/InitialChangesetCreatorBase.class.php
@@ -59,7 +59,7 @@ abstract class Tracker_Artifact_Changeset_InitialChangesetCreatorBase extends Tr
      *
      * @return int The Id of the initial changeset, or null if fields were not valid
      */
-    public function create(Tracker_Artifact $artifact, array $fields_data, PFUser $submitter, $submitted_on): ?int
+    public function create(Tracker_Artifact $artifact, array $fields_data, PFUser $submitter, int $submitted_on): ?int
     {
         if (! $this->doesRequestAppearToBeValid($artifact, $fields_data, $submitter)) {
             return null;

plugins/tracker/include/Tracker/Artifact/Changeset/NewChangesetCreatorBase.class.php
@@ -68,12 +68,6 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
     /**
      * Update an artifact (means create a new changeset)
      *
-     * @param array   $fields_data       Artifact fields values
-     * @param string  $comment           The comment (follow-up) associated with the artifact update
-     * @param PFUser  $submitter         The user who is doing the update
-     * @param bool $send_notification true if a notification must be sent, false otherwise
-     * @param string  $comment_format    The comment (follow-up) type ("text" | "html")
-     *
      * @throws Tracker_Exception In the validation
      * @throws Tracker_NoChangeException In the validation
      * @throws FieldValidationException
@@ -83,11 +77,11 @@ abstract class Tracker_Artifact_Changeset_NewChangesetCreatorBase extends Tracke
     public function create(
         Tracker_Artifact $artifact,
         array $fields_data,
-        $comment,
+        string $comment,
         PFUser $submitter,
-        $submitted_on,
-        $send_notification,
-        $comment_format
+        int $submitted_on,
+        bool $send_notification,
+        string $comment_format
     ): ?Tracker_Artifact_Changeset {
         $comment = trim($comment);
 

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact.class.php
@@ -1061,7 +1061,7 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
         $validator    = new Tracker_Artifact_Changeset_NewChangesetFieldsValidator($this->getFormElementFactory(), $this->getWorkflowUpdateChecker());
         $creator      = $this->getNewChangesetCreator($validator);
 
-        return $creator->create($this, $fields_data, $comment, $submitter, $submitted_on, $send_notification, $comment_format);
+        return $creator->create($this, $fields_data, (string) $comment, $submitter, (int) $submitted_on, (bool) $send_notification, (string) $comment_format);
     }
 
     public function createNewChangesetWhitoutRequiredValidation(
@@ -1076,7 +1076,7 @@ class Tracker_Artifact implements Recent_Element_Interface, Tracker_Dispatchable
         $creator      = $this->getNewChangesetCreator($validator);
 
         return $creator->create(
-            $this, $fields_data, $comment, $submitter, $submitted_on, $send_notification, $comment_format
+            $this, $fields_data, (string) $comment, $submitter, (int) $submitted_on, (bool) $send_notification, (string) $comment_format
         );
     }
 

plugins/tracker/include/Tracker/Artifact/Tracker_ArtifactCreator.class.php
@@ -106,7 +106,7 @@ class Tracker_ArtifactCreator //phpcs:ignore
         $submitted_on,
         $send_notification
     ) {
-        $changeset_id = $this->changeset_creator->create($artifact, $fields_data, $user, $submitted_on);
+        $changeset_id = $this->changeset_creator->create($artifact, $fields_data, $user, (int) $submitted_on);
         if (! $changeset_id) {
             return;
         }