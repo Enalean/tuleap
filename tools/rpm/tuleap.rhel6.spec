Enalean/tuleap

commit ce55c184ccf6f8644453f54ac71e45bab0449dc9

request #13498: Webdav plugin has a dependency to the docman plugin but does not specify it in the RPM package
Change-Id: I92846d723ffbe28f4162fc958dca0762c9d71c55

tools/rpm/tuleap.rhel6.spec
@@ -266,7 +266,7 @@ Summary: WebDAV plugin for Tuleap
 Group: Development/Tools
 Version: @@PLUGIN_WEBDAV_VERSION@@
 Release: @@VERSION@@_@@RELEASE@@%{?dist}
-Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}
+Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}, tuleap-plugin-docman
 Obsoletes: php-sabredav
 %description plugin-webdav
 Plugin to access to file releases & docman though WebDAV

tools/rpm/tuleap.rhel7.spec
@@ -192,7 +192,7 @@ Summary: WebDAV plugin for Tuleap
 Group: Development/Tools
 Version: @@PLUGIN_WEBDAV_VERSION@@
 Release: @@VERSION@@_@@RELEASE@@%{?dist}
-Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}
+Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}, tuleap-plugin-docman
 Obsoletes: php-sabredav
 %description plugin-webdav
 Plugin to access to file releases & docman though WebDAV

commit f718b4a64915eee409dc84d1bad7c2fc164cc285

request #13536: Webdav plugin RPM cannot be installed or updated on RHEL/CentOS6
There is no docman plugin package on RHEL/CentOS6, it is bundled with the core.

Issue has been introduced by ce55c184ccf6f8644453f54ac71e45bab0449dc9.

Change-Id: Ib6ed269ab6e31ed027fd4b7c11cb7225e01180ac

tools/rpm/tuleap.rhel6.spec
@@ -269,7 +269,7 @@ Summary: WebDAV plugin for Tuleap
 Group: Development/Tools
 Version: @@PLUGIN_WEBDAV_VERSION@@
 Release: @@VERSION@@_@@RELEASE@@%{?dist}
-Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}, tuleap-plugin-docman
+Requires: %{name} = @@VERSION@@-@@RELEASE@@%{?dist}
 Obsoletes: php-sabredav
 %description plugin-webdav
 Plugin to access to file releases & docman though WebDAV