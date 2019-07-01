b6c3307c9d3a0a26c23d6056e5623c938024e3f3
Nicolas Terray <nicolas.terray@enalean.com>
2019-07-01 10:21:00 +0200

    Fix E2E tests for tracker global admin page
    This is a follow-up to git #tuleap/stable/0cb320ab454507c6d160d60d8e5eaace69e6dd31
    
    E2E tests were not updated after the move to front router.
    
    Part of request #13561: Move tracker admin to FrontRouter
    
    Change-Id: I6ed31f59af1882731dc28cc55bbb29bad75c5675

--- tests/e2e/full/cypress/integration/check_permissions_spec.js
+++ tests/e2e/full/cypress/integration/check_permissions_spec.js
@@ -72,11 +72,12 @@ describe("Permissions", function() {
     });
 
     it("should raise an error when user try to access to plugin Tracker admin page", function() {
-        cy.visit("/plugins/tracker/?func=global-admin&group_id=" + this.permission_project_id);
-
-        cy.get("[data-test=feedback]").contains(
-            "Access denied. You don't have permissions to perform this action."
-        );
+        cy.request({
+            url: "/plugins/tracker/global-admin/" + this.permission_project_id,
+            failOnStatusCode: false
+        }).then(response => {
+            expect(response.status).to.eq(403);
+        });
     });
 
     it("should raise an error when user try to access to plugin Git admin page", function() {
