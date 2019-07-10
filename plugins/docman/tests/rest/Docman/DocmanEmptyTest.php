9f34d03c0c1ad54c337a471aaab44bca210e2783
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-07-10 09:42:19 +0200

    PUT docman_empty/id/metadata
    part of story #13463 Split PATCH docman_items_type/id route
    
    How to test:
    => If you try to update the obsolescence date or the status and if their metadata are not used, you will have an error
    => If you provide a bad owner_id => 400
    => Else, the empty is updated
    
    Change-Id: I9a7f399b6ea93cb5121bcf355d429a4e388876bc

--- plugins/docman/tests/rest/Docman/DocmanEmptyTest.php
+++ plugins/docman/tests/rest/Docman/DocmanEmptyTest.php
@@ -43,13 +43,15 @@ class DocmanEmptyTest extends DocmanTestExecutionHelper
         $items_id      = $folder['id'];
         $deleted_items = $this->loadFolderContent($items_id, 'DELETE Empty');
         $lock_items    = $this->loadFolderContent($items_id, 'LOCK Empty');
+        $put_items     = $this->loadFolderContent($items_id, 'PUT HM Empty');
 
         return array_merge(
             $root_folder,
             $folder,
             $items,
             $deleted_items,
-            $lock_items
+            $lock_items,
+            $put_items
         );
     }
 
@@ -175,6 +177,76 @@ class DocmanEmptyTest extends DocmanTestExecutionHelper
         $document = $response->json();
         $this->assertEquals($document['lock_info'], null);
     }
+
+    /**
+     * @depends testGetDocumentItemsForAdminUser
+     */
+    public function testPutBasicHardcodedMetadata(array $items): void
+    {
+        $item_name         = 'PUT EM';
+        $item_to_update    = $this->findItemByTitle($items, $item_name);
+        $item_to_update_id = $item_to_update['id'];
+
+        $this->assertEquals($item_name, $item_to_update['title']);
+        $this->assertEquals('', $item_to_update['description']);
+        $this->assertEquals(110, $item_to_update['owner']['id']);
+
+        $date_before_update           = \DateTimeImmutable::createFromFormat(
+            \DateTime::ATOM,
+            $item_to_update['last_update_date']
+        );
+        $date_before_update_timestamp = $date_before_update->getTimestamp();
+
+        $put_resource = [
+            'id'                => $item_to_update_id,
+            'title'             => 'PUT EM New title',
+            'description'       => 'Danger ! Danger !',
+            'owner_id'          => 101,
+            'status'            => 'none'
+        ];
+
+        $updated_metadata_file_response = $this->getResponseByName(
+            DocmanDataBuilder::ADMIN_USER_NAME,
+            $this->client->put('docman_empty_documents/' . $item_to_update_id . '/metadata', null, $put_resource)
+        );
+
+        $this->assertEquals(200, $updated_metadata_file_response->getStatusCode());
+
+        $new_version_response = $this->getResponseByName(
+            DocmanDataBuilder::ADMIN_USER_NAME,
+            $this->client->get('docman_items/' . $item_to_update_id)
+        );
+
+        $this->assertEquals($new_version_response->getStatusCode(), 200);
+
+        $new_version = $new_version_response->json();
+
+        $date_after_update          = \DateTimeImmutable::createFromFormat(
+            \DateTime::ATOM,
+            $new_version['last_update_date']
+        );
+        $last_update_date_timestamp = $date_after_update->getTimestamp();
+        $this->assertGreaterThanOrEqual($date_before_update_timestamp, $last_update_date_timestamp);
+
+        $this->assertEquals('PUT EM New title', $new_version['title']);
+        $this->assertEquals('Danger ! Danger !', $new_version['description']);
+        $this->assertEquals(101, $new_version['owner']['id']);
+    }
+
+    /**
+     * @depends testGetRootId
+     */
+    public function testOptionsMetadata(int $id): void
+    {
+        $response = $this->getResponse(
+            $this->client->options('docman_wikis/' . $id . '/metadata'),
+            REST_TestDataBuilder::ADMIN_USER_NAME
+        );
+
+        $this->assertEquals(['OPTIONS', 'PUT'], $response->getHeader('Allow')->normalize()->toArray());
+        $this->assertEquals($response->getStatusCode(), 200);
+    }
+
 
     /**
      * @depends testGetRootId
