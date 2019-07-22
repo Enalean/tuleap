Enalean/tuleap

commit 868b76795c2e28db8ca944fcf70681b3547a00f7

Replace event management based on CustomEvents by a Vue-based event bus
This approach brings a few advantages:
 * It makes sure that two different instances of the app do
not interact together, the events are kept within the app. This
might becomes handy if the document application needs to be put
in a widget.
 * IE11 is reported by both MDN and Can I Use as not supporting
CustomEvent constructor [0][1].
 * The events can be observed from the Vue dev tools.

It was one of the migration path proposed by the Vue team when
`$dispatch` and `$broadcast` were removed from Vue [2].

No functionnal change.

Part of story #12456: Cut/Copy/paste items

[0] https://github.com/mdn/browser-compat-data/blob/65740c63cb8c861f4511f29b3fa3cb75365f6878/api/CustomEvent.json#L113
[1] https://github.com/Fyrd/caniuse/blob/cdb9d8788bfd147b86bbd1abe3ba3198a2d284ef/features-json/customevent.json#L358
[2] https://vuejs.org/v2/guide/migration.html#dispatch-and-broadcast-replaced

Change-Id: I282157bfb6988d4a4e9afbef22fef0e9fd308cd2

plugins/document/scripts/document/components/Folder/FolderContentRow.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright (c) Enalean, 2018-2019. All Rights Reserved.
+  - Copyright (c) Enalean, 2018-Present. All Rights Reserved.
   -
   - This file is a part of Tuleap.
   -
@@ -94,6 +94,7 @@ import {
 import LockProperty from "./Property/LockProperty.vue";
 import DocumentTitleLockInfo from "./LockInfo/DocumentTitleLockInfo.vue";
 import ApprovalTableBadge from "./ApprovalTables/ApprovalTableBadge.vue";
+import EventBus from "../../helpers/event-bus.js";
 
 export default {
     name: "FolderContentRow",
@@ -199,16 +200,12 @@ export default {
         const is_under_the_fold = position_from_top > viewport_height;
 
         if (is_under_the_fold) {
-            document.dispatchEvent(
-                new CustomEvent("item-has-been-created-under-the-fold", {
-                    detail: { item: this.item }
-                })
-            );
+            EventBus.$emit("item-has-been-created-under-the-fold");
         }
     },
     methods: {
         closeActionMenu() {
-            document.dispatchEvent(new CustomEvent("document-hide-action-menu"));
+            EventBus.$emit("hide-action-menu");
         }
     }
 };

plugins/document/scripts/document/components/Folder/ActionsButton/NewItemButton.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright (c) Enalean, 2018 - 2018. All Rights Reserved.
+  - Copyright (c) Enalean, 2018 - Present. All Rights Reserved.
   -
   - This file is a part of Tuleap.
   -
@@ -30,17 +30,15 @@
 </template>
 
 <script>
+import EventBus from "../../../helpers/event-bus.js";
+
 export default {
     props: {
         item: Object
     },
     methods: {
         showNewDocumentModal() {
-            document.dispatchEvent(
-                new CustomEvent("show-new-document-modal", {
-                    detail: { parent: this.item }
-                })
-            );
+            EventBus.$emit("show-new-document-modal", { detail: { parent: this.item } });
         }
     }
 };

plugins/document/scripts/document/components/Folder/ActionsButton/NewItemVersionButton.spec.js
@@ -23,9 +23,13 @@ import CreateNewItemVersionButton from "./NewItemVersionButton.vue";
 import localVue from "../../../helpers/local-vue.js";
 import { rewire$redirectToUrl, restore } from "../../../helpers/location-helper.js";
 import { createStoreMock } from "@tuleap-vue-components/store-wrapper.js";
+import {
+    rewire as rewireEventBus,
+    restore as restoreEventBus
+} from "../../../helpers/event-bus.js";
 
 describe("CreateNewItemVersionButton", () => {
-    let create_new_item_version_button_factory;
+    let create_new_item_version_button_factory, event_bus;
     beforeEach(() => {
         const state = {
             project_id: 101
@@ -44,17 +48,19 @@ describe("CreateNewItemVersionButton", () => {
                 mocks: { $store: store }
             });
         };
+
+        event_bus = jasmine.createSpyObj("event_bus", ["$emit"]);
+        rewireEventBus(event_bus);
     });
 
     afterEach(() => {
         restore();
+        restoreEventBus();
     });
 
     it(`Given item is a file
         When we click on [create new version]
         Then create-new-item-version event should be dispatched`, () => {
-        spyOn(document, "dispatchEvent");
-
         const wrapper = create_new_item_version_button_factory({
             item: {
                 id: 1,
@@ -66,16 +72,15 @@ describe("CreateNewItemVersionButton", () => {
 
         wrapper.find("[data-test=docman-new-item-version-button]").trigger("click");
 
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("show-create-new-item-version-modal")
+        expect(event_bus.$emit).toHaveBeenCalledWith(
+            "show-create-new-item-version-modal",
+            jasmine.any(Object)
         );
     });
 
     it(`Given item is an embedded file
         When we click on [create new version]
         Then create-new-item-version event should be dispatched`, () => {
-        spyOn(document, "dispatchEvent");
-
         const wrapper = create_new_item_version_button_factory({
             item: {
                 id: 1,
@@ -87,16 +92,15 @@ describe("CreateNewItemVersionButton", () => {
 
         wrapper.find("[data-test=docman-new-item-version-button]").trigger("click");
 
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("show-create-new-item-version-modal")
+        expect(event_bus.$emit).toHaveBeenCalledWith(
+            "show-create-new-item-version-modal",
+            jasmine.any(Object)
         );
     });
 
     it(`Given item is a wiki with no approval table
         When we click on [create new version]
         Then create-new-item-version event should be dispatched`, () => {
-        spyOn(document, "dispatchEvent");
-
         const wrapper = create_new_item_version_button_factory({
             item: {
                 id: 1,
@@ -109,16 +113,15 @@ describe("CreateNewItemVersionButton", () => {
 
         wrapper.find("[data-test=docman-new-item-version-button]").trigger("click");
 
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("show-create-new-item-version-modal")
+        expect(event_bus.$emit).toHaveBeenCalledWith(
+            "show-create-new-item-version-modal",
+            jasmine.any(Object)
         );
     });
 
     it(`Given item is a wiki with an approval table
         When we click on [create new version]
         Then no event should be dispatched`, () => {
-        spyOn(document, "dispatchEvent");
-
         const wrapper = create_new_item_version_button_factory({
             item: {
                 id: 1,
@@ -133,7 +136,7 @@ describe("CreateNewItemVersionButton", () => {
 
         wrapper.find("[data-test=docman-new-item-version-button]").trigger("click");
 
-        expect(document.dispatchEvent).not.toHaveBeenCalled();
+        expect(event_bus.$emit).not.toHaveBeenCalled();
     });
 
     it(`Given item is an empty document
@@ -174,8 +177,9 @@ describe("CreateNewItemVersionButton", () => {
 
         wrapper.find("[data-test=docman-new-item-version-button]").trigger("click");
 
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("show-create-new-item-version-modal")
+        expect(event_bus.$emit).toHaveBeenCalledWith(
+            "show-create-new-item-version-modal",
+            jasmine.any(Object)
         );
     });
 

plugins/document/scripts/document/components/Folder/ActionsButton/NewItemVersionButton.vue
@@ -32,8 +32,9 @@
 <script>
 import { mapState } from "vuex";
 import { TYPE_EMPTY, TYPE_WIKI } from "../../../constants.js";
-
 import { redirectToUrl } from "../../../helpers/location-helper.js";
+import EventBus from "../../../helpers/event-bus.js";
+
 export default {
     name: "UpdateButton",
     props: {
@@ -77,11 +78,9 @@ export default {
             this.showUpdateFileModal();
         },
         showUpdateFileModal() {
-            document.dispatchEvent(
-                new CustomEvent("show-create-new-item-version-modal", {
-                    detail: { current_item: this.item }
-                })
-            );
+            EventBus.$emit("show-create-new-item-version-modal", {
+                detail: { current_item: this.item }
+            });
         }
     }
 };

plugins/document/scripts/document/components/Folder/ActionsDropDown/CopyItem.spec.js
@@ -21,11 +21,18 @@ import { shallowMount } from "@vue/test-utils";
 import localVue from "../../../helpers/local-vue.js";
 import { createStoreMock } from "@tuleap-vue-components/store-wrapper.js";
 import CopyItem from "./CopyItem.vue";
+import {
+    rewire as rewireEventBus,
+    restore as restoreEventBus
+} from "../../../helpers/event-bus.js";
 
 describe("CopyItem", () => {
-    let store, copy_item_factory;
+    let store, event_bus, copy_item_factory;
     beforeEach(() => {
         store = createStoreMock({}, { clipboard: {} });
+
+        event_bus = jasmine.createSpyObj("event_bus", ["$emit"]);
+        rewireEventBus(event_bus);
 
         copy_item_factory = (props = {}) => {
             return shallowMount(CopyItem, {
@@ -36,20 +43,20 @@ describe("CopyItem", () => {
         };
     });
 
+    afterEach(() => {
+        restoreEventBus();
+    });
+
     it(`Given item is copied
         Then the store is updated accordingly
         And the menu closed`, () => {
         const item = { id: 147, type: "item_type", title: "My item" };
         const wrapper = copy_item_factory({ item });
 
-        spyOn(document, "dispatchEvent");
-
         wrapper.trigger("click");
 
         expect(store.commit).toHaveBeenCalledWith("clipboard/copyItem", item);
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("document-hide-action-menu")
-        );
+        expect(event_bus.$emit).toHaveBeenCalledWith("hide-action-menu");
     });
 
     it(`Given an item is being pasted
@@ -62,10 +69,8 @@ describe("CopyItem", () => {
         expect(wrapper.attributes().disabled).toBeTruthy();
         expect(wrapper.classes("tlp-dropdown-menu-item-disabled")).toBe(true);
 
-        spyOn(document, "dispatchEvent");
-
         wrapper.trigger("click");
 
-        expect(document.dispatchEvent).not.toHaveBeenCalled();
+        expect(event_bus.$emit).not.toHaveBeenCalled();
     });
 });

plugins/document/scripts/document/components/Folder/ActionsDropDown/CopyItem.vue
@@ -31,6 +31,7 @@
 </template>
 <script>
 import { mapState } from "vuex";
+import EventBus from "../../../helpers/event-bus.js";
 
 export default {
     name: "CopyItem",
@@ -43,7 +44,7 @@ export default {
     methods: {
         copyItem() {
             if (!this.pasting_in_progress) {
-                document.dispatchEvent(new CustomEvent("document-hide-action-menu"));
+                EventBus.$emit("hide-action-menu");
             }
             this.$store.commit("clipboard/copyItem", this.item);
         }

plugins/document/scripts/document/components/Folder/ActionsDropDown/DropdownButton.vue
@@ -38,6 +38,7 @@
 
 <script>
 import { dropdown as createDropdown } from "tlp";
+import EventBus from "../../../helpers/event-bus.js";
 
 export default {
     name: "DropdownButton",
@@ -55,15 +56,17 @@ export default {
     mounted() {
         this.dropdown = createDropdown(this.$refs.dropdownButton);
 
-        const hideActionMenu = () => {
+        EventBus.$on("hide-action-menu", this.hideActionMenu);
+    },
+    beforeDestroy() {
+        EventBus.$off("hide-action-menu", this.hideActionMenu);
+    },
+    methods: {
+        hideActionMenu() {
             if (this.dropdown && this.dropdown.is_shown) {
                 this.dropdown.hide();
             }
-        };
-        document.addEventListener("document-hide-action-menu", hideActionMenu);
-        this.$once("hook:beforeDestroy", () => {
-            document.removeEventListener("document-hide-action-menu", hideActionMenu);
-        });
+        }
     }
 };
 </script>

plugins/document/scripts/document/components/Folder/ActionsDropDown/DropdownMenu.spec.js
@@ -22,9 +22,13 @@ import { createStoreMock } from "@tuleap-vue-components/store-wrapper.js";
 import localVue from "../../../helpers/local-vue.js";
 import DropdownMenu from "./DropdownMenu.vue";
 import { restore } from "../../../helpers/location-helper";
+import {
+    rewire as rewireEventBus,
+    restore as restoreEventBus
+} from "../../../helpers/event-bus.js";
 
 describe("DropdownMenu", () => {
-    let dropdown_menu_factory, store;
+    let dropdown_menu_factory, store, event_bus;
     beforeEach(() => {
         const state = {
             max_files_dragndrop: 10,
@@ -46,9 +50,13 @@ describe("DropdownMenu", () => {
         };
 
         store.getters.is_item_an_empty_document = () => false;
+
+        event_bus = jasmine.createSpyObj("event_bus", ["$emit"]);
+        rewireEventBus(event_bus);
     });
     afterEach(() => {
         restore();
+        restoreEventBus();
     });
     it(`Given item title should be hidden (button displayed in quick look view)
         When we display the menu
@@ -126,14 +134,14 @@ describe("DropdownMenu", () => {
                 can_user_manage: true
             }
         });
-        spyOn(document, "dispatchEvent");
 
         expect(wrapper.contains("[data-test=docman-dropdown-details]")).toBeTruthy();
 
         wrapper.find("[data-test=docman-dropdown-details]").trigger("click");
 
-        expect(document.dispatchEvent).toHaveBeenCalledWith(
-            new CustomEvent("show-update-item-metadata-modal")
+        expect(event_bus.$emit).toHaveBeenCalledWith(
+            "show-update-item-metadata-modal",
+            jasmine.any(Object)
         );
     });
 

commit df3ea241a9167f153cc0a62e875557f526bce11b

No notification when the item is created outside the viewport
Issue introduced by 868b76795c2e28db8ca944fcf70681b3547a00f7.

Part of story #12456: Cut/Copy/paste items

Change-Id: I8c90b8bbf97534fb7924f9b55cfa5f4f90125f3c

plugins/document/scripts/document/components/Folder/FolderContentRow.vue
@@ -200,7 +200,9 @@ export default {
         const is_under_the_fold = position_from_top > viewport_height;
 
         if (is_under_the_fold) {
-            EventBus.$emit("item-has-been-created-under-the-fold");
+            EventBus.$emit("item-has-been-created-under-the-fold", {
+                detail: { item: this.item }
+            });
         }
     },
     methods: {