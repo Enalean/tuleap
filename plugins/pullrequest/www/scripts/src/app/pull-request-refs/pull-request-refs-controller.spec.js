487d06590d3ab4a3653718f809c27d847df2fa98
Joris Masson <joris.masson@enalean.com>
2019-07-23 14:43:54 +0200

    Fix eslint errors in pullrequest plugin
    part of request #13545: Introduce eslint to the test pipeline
    
    How to test:
    - npm run eslint -- --quiet ./plugins/pullrequest
    - The previous command should return 0 error. "quiet option" disables
    warnings, which are not handled by this commit.
    - No functional change expected
    
    Change-Id: I7c2694d395e30ca40f1a29c7db854b9b7a78d894

--- plugins/pullrequest/www/scripts/src/app/pull-request-refs/pull-request-refs-controller.spec.js
+++ plugins/pullrequest/www/scripts/src/app/pull-request-refs/pull-request-refs-controller.spec.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import angular from "angular";
 import tuleap_pullrequest_module from "tuleap-pullrequest-module";
 import pullrequest_refs_controller from "./pull-request-refs-controller.js";
@@ -12,7 +31,6 @@ describe("PullRequestRefsController -", function() {
 
         angular.mock.module(tuleap_pullrequest_module);
 
-        // eslint-disable-next-line angular/di
         angular.mock.inject(function(_$controller_, _SharedPropertiesService_) {
             $controller = _$controller_;
             SharedPropertiesService = _SharedPropertiesService_;
--- plugins/pullrequest/www/scripts/src/app/pull-request/pull-request-rest-service.spec.js
+++ plugins/pullrequest/www/scripts/src/app/pull-request/pull-request-rest-service.spec.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import angular from "angular";
 import tuleap_pullrequest_module from "tuleap-pullrequest-module";
 
@@ -9,7 +28,6 @@ describe("PullRequestRestService -", function() {
     beforeEach(function() {
         angular.mock.module(tuleap_pullrequest_module);
 
-        // eslint-disable-next-line angular/di
         angular.mock.inject(function(
             _$httpBackend_,
             _ErrorModalService_,
