Enalean/tuleap

commit dd5849727b6c5f046fc02a3857a54e4732949413

Remove \get_server_url() function
The function is marked as deprecated since 2016.
Since the upgrade to Psalm 3.4.5 it is also responsible for
a non negligeable part of the issues found when a subpart of the codebase
is analyzed (see the attached file in the request).

The change affects some site-content files so a note in the deployment
guide will need to be added.

Part of request #13292: Integrate vimeo/psalm to the test pipeline

Change-Id: I3006924885a9950c4952e2a10c3792bf05b8b4a0

src/www/tracker/gotoid.php
@@ -1,4 +1,5 @@
 <?php
+// Copyright (c) Enalean, 2012-Present. All Rights Reserved.
 // Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
 //
 // 
@@ -48,7 +49,7 @@ function generic_redirect($location,$aid,$group_id,$art_group_id,$atid,$atn,$art
 
 // Start of main code
 
-$location = "Location: ".get_server_url();
+$location = 'Location: /';
 
 // $atn is the "artifact type name" i.e. the tracker short name detected in the text
 // Detected: 'xxx #nnn', transformed to  '$atn #$aid'

plugins/docman/bin/DocmanExport/XMLExport.class.php
@@ -34,7 +34,7 @@ class XMLExport {
 
     public function createDomDocument() {
         $impl = new DOMImplementation();
-        $dtd = $impl->createDocumentType('docman', '', get_server_url().'/plugins/docman/docman-1.0.dtd');
+        $dtd = $impl->createDocumentType('docman', '', HTTPRequest::instance()->getServerUrl().'/plugins/docman/docman-1.0.dtd');
         $doc = $impl->createDocument('', '', $dtd);
         $doc->encoding     = 'UTF-8';
         $doc->standalone   = 'no';

plugins/docman/include/ApprovalTable/NotificationCycle.class.php
@@ -210,7 +210,7 @@ class Docman_ApprovalTableNotificationCycle {
     }
 
     function getReviewUrl() {
-        $baseUrl = get_server_url().'/plugins/docman/?group_id='.$this->item->getGroupId();
+        $baseUrl = HTTPRequest::instance()->getServerUrl().'/plugins/docman/?group_id='.$this->item->getGroupId();
         $reviewUrl = $baseUrl .'&action=details&section=approval&id='.$this->item->getId();
         return $reviewUrl;
     }
@@ -315,7 +315,7 @@ class Docman_ApprovalTableNotificationCycle {
         $project         = $project_manager->getProject($this->item->getGroupId());
 
         $reviewUrl = $this->getReviewUrl();
-        $baseUrl   = get_server_url().'/plugins/docman/?group_id='.$this->item->getGroupId();
+        $baseUrl   = HTTPRequest::instance()->getServerUrl().'/plugins/docman/?group_id='.$this->item->getGroupId();
         $propUrl   = $baseUrl .'&action=edit&id='.$this->item->getId();
         $body      = $GLOBALS['Language']->getText(
             'plugin_docman',
@@ -557,7 +557,7 @@ class Docman_ApprovalTableNotificationCycle {
     function getNotificationBodyText() {
         $project_manager = ProjectManager::instance();
         $project         = $project_manager->getProject($this->item->getGroupId());
-        $baseUrl         = get_server_url().'/plugins/docman/?group_id='.$this->item->getGroupId();
+        $baseUrl         = HTTPRequest::instance()->getServerUrl().'/plugins/docman/?group_id='.$this->item->getGroupId();
         $itemUrl         = $baseUrl .'&action=show&id='.$this->item->getId();
         $comment         = '';
         $userComment     = $this->table->getDescription();

plugins/docman/include/ApprovalTable/Reminder.class.php
@@ -208,7 +208,7 @@ class Docman_ApprovalTableReminder {
      * @return String
      */
     private function getReviewUrl(Docman_Item $docmanItem) {
-        $baseUrl   = get_server_url().'/plugins/docman/?group_id='.$docmanItem->getGroupId();
+        $baseUrl   = HTTPRequest::instance()->getServerUrl().'/plugins/docman/?group_id='.$docmanItem->getGroupId();
         $reviewUrl = $baseUrl.'&action=details&section=approval&id='.$docmanItem->getId().'&review=1';
         return $reviewUrl;
     }
@@ -221,7 +221,7 @@ class Docman_ApprovalTableReminder {
      * @return String
      */
     private function getItemUrl(Docman_Item $docmanItem) {
-        $baseUrl   = get_server_url().'/plugins/docman/?group_id='.$docmanItem->getGroupId();
+        $baseUrl   = HTTPRequest::instance()->getServerUrl().'/plugins/docman/?group_id='.$docmanItem->getGroupId();
         $itemUrl   = $baseUrl.'&action=show&id='.$docmanItem->getId();
         return $itemUrl;
     }

plugins/docman/include/ApprovalTable/Reviewer/ReviewerFactory.class.php
@@ -338,7 +338,7 @@ class Docman_ApprovalTableReviewerFactory {
         $reviewsArray = array();
         $dao = Docman_ApprovalTableReviewerFactory::_getDao();
         $dar = $dao->getAllReviewsForUserByState($userId, PLUGIN_DOCMAN_APPROVAL_STATE_NOTYET);
-        $docmanUrl = get_server_url().'/plugins/docman';
+        $docmanUrl = HTTPRequest::instance()->getServerUrl() .'/plugins/docman';
         while($dar->valid()) {
             $row = $dar->current();
             $baseUrl = $docmanUrl.'/?group_id='.$row['group_id'];
@@ -361,7 +361,7 @@ class Docman_ApprovalTableReviewerFactory {
         $reviewsArray = array();
         $dao = Docman_ApprovalTableReviewerFactory::_getDao();
         $dar = $dao->getAllApprovalTableForUser($userId);
-        $docmanUrl = get_server_url().'/plugins/docman';
+        $docmanUrl = HTTPRequest::instance()->getServerUrl().'/plugins/docman';
         while($dar->valid()) {
             $row = $dar->current();
 

plugins/docman/include/Docman_HTTPController.class.php
@@ -108,7 +108,7 @@ class Docman_HTTPController extends Docman_Controller {
             $obsoDate = DateHelper::formatForLanguage($GLOBALS['Language'], $item->getObsolescenceDate(), true);
 
             // Urls
-            $baseUrl = get_server_url().$this->pluginPath.'/index.php?group_id='.$item->getGroupId().'&id='.$item->getId();
+            $baseUrl = HTTPRequest::instance()->getServerUrl().$this->pluginPath.'/index.php?group_id='.$item->getGroupId().'&id='.$item->getId();
             $directUrl = $baseUrl .'&action=show';
             $detailUrl = $baseUrl .'&action=details';
             

plugins/git/include/SystemEvents/SystemEvent_GIT_GERRIT_MIGRATION.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -94,7 +94,7 @@ class SystemEvent_GIT_GERRIT_MIGRATION extends SystemEvent {
         if (! $user->isAnonymous()) {
             $factory = new BaseLanguageFactory();
             $language = $factory->getBaseLanguage($user->getLocale());
-            $url = get_server_url() . GIT_BASE_URL . '/?action=repo_management&group_id='.$repository->getProjectId().'&repo_id='.$repository->getId().'&pane=gerrit';
+            $url = HTTPRequest::instance()->getServerUrl() . GIT_BASE_URL . '/?action=repo_management&group_id='.$repository->getProjectId().'&repo_id='.$repository->getId().'&pane=gerrit';
 
             $notification = new Notification(
                 array($user->getEmail()),

plugins/graphontrackers/www/reportgraphic.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2013-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2013-Present. All Rights Reserved.
  * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
  *
  * Originally written by Mahmoud MAALEJ, 2006. STMicroelectronics.
@@ -58,6 +58,6 @@ if ($p && $plugin_manager->isPluginAvailable($p)) {
         }
     }
 } else {
-    header('Location: '.get_server_url());
+    header('Location: /');
 }
 ?>

commit 00bfe13f1401e0260488d5ed8b6aeba03e1f2eb0

request #13663: "gotoid" links in legacy trackers are broken
Issue introduced by dd5849727b6c5f046fc02a3857a54e4732949413.

Change-Id: I23c7120e4390a43e0a22d5245690ed26ccb923fe

src/www/tracker/gotoid.php
@@ -49,7 +49,7 @@ function generic_redirect($location,$aid,$group_id,$art_group_id,$atid,$atn,$art
 
 // Start of main code
 
-$location = 'Location: /';
+$location = 'Location: ';
 
 // $atn is the "artifact type name" i.e. the tracker short name detected in the text
 // Detected: 'xxx #nnn', transformed to  '$atn #$aid'