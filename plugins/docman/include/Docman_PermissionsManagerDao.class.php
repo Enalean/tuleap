Enalean/tuleap

commit 43fb51f1b67f3d5a6c150524d0e6c6f9713fdfcd

Add the list of permissions in the docman item representation
Part of story #13654: have detailed permissions in REST API
Part of story #12457: New document modal: permissions

Change-Id: I0f36e078213807e84d8f43e2140ee5bd5d7a47cd

plugins/docman/include/Docman_PermissionsManagerDao.class.php
@@ -4,7 +4,7 @@
  * Copyright (c) STMicroelectronics, 2006. All Rights Reserved.
  *
  * Originally written by Manuel Vacelet, 2006
- * 
+ *
  * This file is a part of Tuleap.
  *
  * Tuleap is free software; you can redistribute it and/or modify
@@ -31,12 +31,17 @@ class Docman_PermissionsManagerDao extends DataAccessObject {
         $this->groupId = $groupId;
     }
 
-    function retreivePermissionsForItems($itemsIds, $perms, $ugroupIds) {
+    public function retrievePermissionsForItems($itemsIds, $perms, $ugroupIds)
+    {
+        $itemsIds  = $this->da->quoteSmartImplode(',', $itemsIds);
+        $perms     = $this->da->quoteSmartImplode(',', $perms);
+        $ugroupIds = $this->da->escapeIntImplode($ugroupIds);
+
         $sql = 'SELECT *'.
             ' FROM permissions'.
-            " WHERE object_id IN ('".implode("','", $itemsIds)."')".
-            ' AND permission_type IN ('.implode(',', $perms).')'.
-            ' AND ugroup_id IN ('.implode(',', $ugroupIds).')';
+            " WHERE object_id IN ('.$itemsIds.')".
+            ' AND permission_type IN ('.$perms.')'.
+            ' AND ugroup_id IN ('.$ugroupIds.')';
         return $this->retrieve($sql);
     }
 
@@ -66,21 +71,21 @@ class Docman_PermissionsManagerDao extends DataAccessObject {
             return false;
         }
     }
-    
+
 
     /**
      * Returns project admin members for a given group
-     * 
+     *
      * @param Project $project
      */
     function getProjectAdminMembers($project) {
         $sql = 'SELECT email, language_id FROM user u JOIN user_group ug USING(user_id) WHERE ug.admin_flags="A" AND u.status IN ("A", "R") AND ug.group_id = '.$this->da->escapeInt($project->getId());
-        return $this->retrieve($sql); 
+        return $this->retrieve($sql);
     }
-    
+
     /**
      * Returns ugroup members of ugroups
-     * 
+     *
      * @param Integer $ugroupId
      */
     function getUgroupMembers($ugroupId) {
@@ -90,7 +95,7 @@ class Docman_PermissionsManagerDao extends DataAccessObject {
 
     /**
      * Returns docman admin ugroups
-     * 
+     *
      * @param Project $project
      *
      * @return DataAccessResult

plugins/docman/include/Docman_PermissionsManager.class.php
@@ -23,10 +23,19 @@
 
 //phpcs:ignoreFile
 
-class Docman_PermissionsManager {
-
+class Docman_PermissionsManager
+{
     const PLUGIN_OPTION_DELETE = 'only_siteadmin_can_delete';
     const PLUGIN_DOCMAN_ADMIN  = 'PLUGIN_DOCMAN_ADMIN';
+
+    public const ITEM_PERMISSION_TYPE_READ   = 'PLUGIN_DOCMAN_READ';
+    public const ITEM_PERMISSION_TYPE_WRITE  = 'PLUGIN_DOCMAN_WRITE';
+    public const ITEM_PERMISSION_TYPE_MANAGE = 'PLUGIN_DOCMAN_MANAGE';
+    public const ITEM_PERMISSION_TYPES       = [
+        self::ITEM_PERMISSION_TYPE_READ,
+        self::ITEM_PERMISSION_TYPE_WRITE,
+        self::ITEM_PERMISSION_TYPE_MANAGE
+    ];
 
     /**
      * @var Project
@@ -189,7 +198,7 @@ class Docman_PermissionsManager {
             $pm = $this->_getPermissionManagerInstance();
             $canRead = $user->isSuperUser()
                 || $this->userCanAdmin($user) //There are default perms for admin
-                || $pm->userHasPermission($item_id, 'PLUGIN_DOCMAN_READ', $user->getUgroups($this->getProject()->getID(), array()))
+                || $pm->userHasPermission($item_id, self::ITEM_PERMISSION_TYPE_READ, $user->getUgroups($this->getProject()->getID(), array()))
                 || $this->_userHasWritePermission($user, $item_id);
 
             $this->_setCanRead($user->getId(), $item_id, $canRead);
@@ -269,7 +278,7 @@ class Docman_PermissionsManager {
         $pm = $this->_getPermissionManagerInstance();
         $canWrite = $user->isSuperUser()
                 || $this->userCanAdmin($user) //There are default perms for admin
-                || $pm->userHasPermission($item_id, 'PLUGIN_DOCMAN_WRITE', $user->getUgroups($this->getProject()->getID(), array()))
+                || $pm->userHasPermission($item_id, self::ITEM_PERMISSION_TYPE_WRITE, $user->getUgroups($this->getProject()->getID(), array()))
                 || $this->userCanManage($user, $item_id);
         if($canWrite) {
             $this->_setCanRead($user->getId(), $item_id, true);
@@ -330,7 +339,7 @@ class Docman_PermissionsManager {
             $pm = $this->_getPermissionManagerInstance();
             $canManage = $user->isSuperUser()
                 || $this->userCanAdmin($user) //There are default perms for admin
-                || $pm->userHasPermission($item_id, 'PLUGIN_DOCMAN_MANAGE', $user->getUgroups($this->getProject()->getID(), array())) ;
+                || $pm->userHasPermission($item_id, self::ITEM_PERMISSION_TYPE_MANAGE, $user->getUgroups($this->getProject()->getID(), array())) ;
             $this->_setCanManage($user->getId(), $item_id, $canManage);
         }
         return $this->cache_manage[$user->getId()][$item_id];
@@ -439,9 +448,8 @@ class Docman_PermissionsManager {
     }
 
     function cloneItemPermissions($srcItemId, $dstItemId, $toGroupId) {
-        $perms = array('PLUGIN_DOCMAN_READ', 'PLUGIN_DOCMAN_WRITE', 'PLUGIN_DOCMAN_MANAGE');
         $pm = $this->_getPermissionManagerInstance();
-        $pm->clonePermissions($srcItemId, $dstItemId, $perms, $toGroupId);
+        $pm->clonePermissions($srcItemId, $dstItemId, self::ITEM_PERMISSION_TYPES, $toGroupId);
     }
 
     function cloneDocmanPermissions($srcGroupId, $dstGroupId) {
@@ -453,9 +461,9 @@ class Docman_PermissionsManager {
     function setDefaultItemPermissions($itemId, $force=false) {
         $dao = $this->getDao();
 
-        $dao->setDefaultPermissions($itemId, 'PLUGIN_DOCMAN_READ', $force);
-        $dao->setDefaultPermissions($itemId, 'PLUGIN_DOCMAN_WRITE', $force);
-        $dao->setDefaultPermissions($itemId, 'PLUGIN_DOCMAN_MANAGE', $force);
+        $dao->setDefaultPermissions($itemId, self::ITEM_PERMISSION_TYPE_READ, $force);
+        $dao->setDefaultPermissions($itemId, self::ITEM_PERMISSION_TYPE_WRITE, $force);
+        $dao->setDefaultPermissions($itemId, self::ITEM_PERMISSION_TYPE_MANAGE, $force);
     }
 
     function setDefaultDocmanPermissions($groupId) {
@@ -499,17 +507,16 @@ class Docman_PermissionsManager {
         }
 
         if(count($objIds) > 0) {
-            $perms = array("'PLUGIN_DOCMAN_READ'", "'PLUGIN_DOCMAN_WRITE'", "'PLUGIN_DOCMAN_MANAGE'");
-            $dar = $dao->retreivePermissionsForItems($objIds, $perms, $user->getUgroups($this->getProject()->getID(), array()));
+            $dar = $dao->retrievePermissionsForItems($objIds, self::ITEM_PERMISSION_TYPES, $user->getUgroups($this->getProject()->getID(), array()));
             foreach($dar as $row) {
                 switch($row['permission_type']) {
-                case 'PLUGIN_DOCMAN_MANAGE':
+                case self::ITEM_PERMISSION_TYPE_MANAGE:
                     $this->_setCanManage($userId, $row['object_id'], true);
                     break;
-                case 'PLUGIN_DOCMAN_WRITE':
+                case self::ITEM_PERMISSION_TYPE_WRITE:
                     $this->_setCanWrite($userId, $row['object_id'], true);
                     break;
-                case 'PLUGIN_DOCMAN_READ':
+                case self::ITEM_PERMISSION_TYPE_READ:
                     $this->_setCanRead($userId, $row['object_id'], true);
                     break;
                 }
@@ -618,13 +625,13 @@ class Docman_PermissionsManager {
     */
     public static function getDefinitionIndexForPermission($p) {
         switch ($p) {
-            case 'PLUGIN_DOCMAN_READ':
+            case self::ITEM_PERMISSION_TYPE_READ:
                 return 1;
                 break;
-            case 'PLUGIN_DOCMAN_WRITE':
+            case self::ITEM_PERMISSION_TYPE_WRITE:
                 return 2;
                 break;
-            case 'PLUGIN_DOCMAN_MANAGE':
+            case self::ITEM_PERMISSION_TYPE_MANAGE:
                 return 3;
                 break;
             default:
@@ -648,7 +655,7 @@ class Docman_PermissionsManager {
     function getDocmanManagerUsers($objectId, $project) {
         $userArray = array();
         $dao = $this->getDao();
-        $dar = $this->_getPermissionManagerInstance()->getUgroupIdByObjectIdAndPermissionType($objectId, 'PLUGIN_DOCMAN_MANAGE');
+        $dar = $this->_getPermissionManagerInstance()->getUgroupIdByObjectIdAndPermissionType($objectId, self::ITEM_PERMISSION_TYPE_MANAGE);
         if ($dar) {
             foreach ($dar as $row) {
                 if ($row['ugroup_id'] > 100) {

plugins/docman/include/REST/v1/DocmanItemsResource.php
@@ -31,6 +31,7 @@ use Docman_ItemFactory;
 use Docman_VersionFactory;
 use EventManager;
 use Luracast\Restler\RestException;
+use PermissionsManager;
 use Project;
 use ProjectManager;
 use Tuleap\Docman\ApprovalTable\ApprovalTableRetriever;
@@ -38,10 +39,12 @@ use Tuleap\Docman\ApprovalTable\ApprovalTableStateMapper;
 use Tuleap\Docman\REST\v1\Folders\ItemCanHaveSubItemsChecker;
 use Tuleap\Docman\REST\v1\Metadata\MetadataRepresentationBuilder;
 use Tuleap\Docman\REST\v1\Metadata\UnknownMetadataException;
+use Tuleap\Docman\REST\v1\Permissions\DocmanItemPermissionsForGroupsBuilder;
 use Tuleap\REST\AuthenticatedResource;
 use Tuleap\REST\Header;
 use Tuleap\REST\I18NRestException;
 use Tuleap\REST\UserManager as RestUserManager;
+use UGroupManager;
 use UserHelper;
 
 class DocmanItemsResource extends AuthenticatedResource
@@ -289,15 +292,17 @@ class DocmanItemsResource extends AuthenticatedResource
         );
     }
 
-    private function getItemRepresentationBuilder(Docman_Item $item, Project $project)
+    private function getItemRepresentationBuilder(Docman_Item $item, Project $project) : ItemRepresentationBuilder
     {
         $html_purifier = Codendi_HTMLPurifier::instance();
 
-        $item_representation_builder = new ItemRepresentationBuilder(
+        $permissions_manager = $this->getDocmanPermissionManager($project);
+
+        return new ItemRepresentationBuilder(
             $this->item_dao,
             \UserManager::instance(),
             Docman_ItemFactory::instance($item->getGroupId()),
-            $this->getDocmanPermissionManager($project),
+            $permissions_manager,
             new \Docman_LockFactory(new \Docman_LockDao(), new \Docman_Log()),
             new ApprovalTableStateMapper(),
             new MetadataRepresentationBuilder(
@@ -309,8 +314,13 @@ class DocmanItemsResource extends AuthenticatedResource
                 new \Docman_ApprovalTableFactoriesFactory(),
                 new Docman_VersionFactory()
             ),
+            new DocmanItemPermissionsForGroupsBuilder(
+                $permissions_manager,
+                ProjectManager::instance(),
+                PermissionsManager::instance(),
+                new UGroupManager()
+            ),
             $html_purifier
         );
-        return $item_representation_builder;
     }
 }

plugins/docman/include/REST/v1/ItemRepresentation.php
@@ -24,6 +24,7 @@ use Codendi_HTMLPurifier;
 use Tuleap\Docman\REST\v1\EmbeddedFiles\EmbeddedFilePropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Files\FilePropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Metadata\MetadataRepresentation;
+use Tuleap\Docman\REST\v1\Permissions\DocmanItemPermissionsForGroupsRepresentation;
 use Tuleap\Docman\REST\v1\Wiki\WikiPropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Links\LinkPropertiesRepresentation;
 use Tuleap\REST\JsonCast;
@@ -142,6 +143,12 @@ class ItemRepresentation
      */
     public $approval_table;
 
+    /**
+     * @var DocmanItemPermissionsForGroupsRepresentation {@required false} {@type \Tuleap\Docman\REST\v1\Permissions\DocmanItemPermissionsForGroupsRepresentation}
+     * @psalm-var DocmanItemPermissionsForGroupsRepresentation|null
+     */
+    public $permissions_for_groups;
+
     public function build(
         \Docman_Item $item,
         Codendi_HTMLPurifier $purifier,
@@ -153,12 +160,13 @@ class ItemRepresentation
         array $metadata_representations,
         bool $has_approval_table,
         bool $is_approval_table_enabled,
-        ?ItemApprovalTableRepresentation $approval_table = null,
-        ?ItemLockInfoRepresentation $lock_info = null,
-        ?FilePropertiesRepresentation $file_properties = null,
-        ?EmbeddedFilePropertiesRepresentation $embedded_file_properties = null,
-        ?LinkPropertiesRepresentation $link_properties = null,
-        ?WikiPropertiesRepresentation $wiki_properties = null
+        ?ItemApprovalTableRepresentation $approval_table,
+        ?ItemLockInfoRepresentation $lock_info,
+        ?DocmanItemPermissionsForGroupsRepresentation $permissions_for_groups,
+        ?FilePropertiesRepresentation $file_properties,
+        ?EmbeddedFilePropertiesRepresentation $embedded_file_properties,
+        ?LinkPropertiesRepresentation $link_properties,
+        ?WikiPropertiesRepresentation $wiki_properties
     ) {
         $this->id                         = JsonCast::toInt($item->getId());
         $this->title                      = $item->getTitle();
@@ -180,6 +188,7 @@ class ItemRepresentation
         $this->metadata                   = $metadata_representations;
         $this->has_approval_table         = $has_approval_table;
         $this->is_approval_table_enabled  = $is_approval_table_enabled;
+        $this->permissions_for_groups     = $permissions_for_groups;
 
         $parent_id = JsonCast::toInt($item->getParentId());
 

plugins/docman/include/REST/v1/ItemRepresentationBuilder.php
@@ -30,6 +30,7 @@ use Tuleap\Docman\REST\v1\EmbeddedFiles\EmbeddedFilePropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Files\FilePropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Metadata\MetadataRepresentationBuilder;
 use Tuleap\Docman\REST\v1\Metadata\UnknownMetadataException;
+use Tuleap\Docman\REST\v1\Permissions\DocmanItemPermissionsForGroupsBuilder;
 use Tuleap\Docman\REST\v1\Wiki\WikiPropertiesRepresentation;
 use Tuleap\Docman\REST\v1\Links\LinkPropertiesRepresentation;
 use Tuleap\User\REST\MinimalUserRepresentation;
@@ -69,6 +70,10 @@ class ItemRepresentationBuilder
      */
     private $approval_table_retriever;
     /**
+     * @var DocmanItemPermissionsForGroupsBuilder
+     */
+    private $item_permissions_for_groups_builder;
+    /**
      * @var Codendi_HTMLPurifier
      */
     private $purifier;
@@ -82,17 +87,19 @@ class ItemRepresentationBuilder
         ApprovalTableStateMapper $approval_table_state_mapper,
         MetadataRepresentationBuilder $metadata_representation_builder,
         ApprovalTableRetriever $approval_table_retriever,
+        DocmanItemPermissionsForGroupsBuilder $item_permissions_for_groups_builder,
         Codendi_HTMLPurifier $purifier
     ) {
-        $this->dao                             = $dao;
-        $this->user_manager                    = $user_manager;
-        $this->docman_item_factory             = $docman_item_factory;
-        $this->permissions_manager             = $permissions_manager;
-        $this->lock_factory                    = $lock_factory;
-        $this->approval_table_state_mapper     = $approval_table_state_mapper;
-        $this->metadata_representation_builder = $metadata_representation_builder;
-        $this->approval_table_retriever        = $approval_table_retriever;
-        $this->purifier                        = $purifier;
+        $this->dao                                 = $dao;
+        $this->user_manager                        = $user_manager;
+        $this->docman_item_factory                 = $docman_item_factory;
+        $this->permissions_manager                 = $permissions_manager;
+        $this->lock_factory                        = $lock_factory;
+        $this->approval_table_state_mapper         = $approval_table_state_mapper;
+        $this->metadata_representation_builder     = $metadata_representation_builder;
+        $this->approval_table_retriever            = $approval_table_retriever;
+        $this->purifier                            = $purifier;
+        $this->item_permissions_for_groups_builder = $item_permissions_for_groups_builder;
     }
 
     /**
@@ -166,6 +173,7 @@ class ItemRepresentationBuilder
             $is_approval_table_enabled,
             $approval_table,
             $lock_info,
+            $this->item_permissions_for_groups_builder->getRepresentation($current_user, $item),
             $file_properties,
             $embedded_file_properties,
             $link_properties,

plugins/docman/include/REST/v1/Permissions/DocmanItemPermissionsForGroupsBuilder.php
@@ -1,0 +1,102 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Docman\REST\v1\Permissions;
+
+use Docman_Item;
+use Docman_PermissionsManager;
+use IPermissionsManagerNG;
+use PFUser;
+use Project;
+use ProjectManager;
+use Tuleap\Project\REST\UserGroupRepresentation;
+use UGroupManager;
+
+class DocmanItemPermissionsForGroupsBuilder
+{
+    /**
+     * @var Docman_PermissionsManager
+     */
+    private $docman_permissions_manager;
+    /**
+     * @var ProjectManager
+     */
+    private $project_manager;
+    /**
+     * @var IPermissionsManagerNG
+     */
+    private $permissions_manager;
+    /**
+     * @var UGroupManager
+     */
+    private $ugroup_manager;
+
+    public function __construct(
+        Docman_PermissionsManager $docman_permissions_manager,
+        ProjectManager $project_manager,
+        IPermissionsManagerNG $permissions_manager,
+        UGroupManager $ugroup_manager
+    ) {
+        $this->docman_permissions_manager = $docman_permissions_manager;
+        $this->project_manager            = $project_manager;
+        $this->permissions_manager        = $permissions_manager;
+        $this->ugroup_manager             = $ugroup_manager;
+    }
+
+    public function getRepresentation(PFUser $user, Docman_Item $item) : ?DocmanItemPermissionsForGroupsRepresentation
+    {
+        if (! $this->docman_permissions_manager->userCanManage($user, $item->getId())) {
+            return null;
+        }
+
+        $project = $this->project_manager->getProject($item->getGroupId());
+
+        return DocmanItemPermissionsForGroupsRepresentation::build(
+            $this->buildUGroupsRepresentationForPermission($project, $item, Docman_PermissionsManager::ITEM_PERMISSION_TYPE_READ),
+            $this->buildUGroupsRepresentationForPermission($project, $item, Docman_PermissionsManager::ITEM_PERMISSION_TYPE_WRITE),
+            $this->buildUGroupsRepresentationForPermission($project, $item, Docman_PermissionsManager::ITEM_PERMISSION_TYPE_MANAGE),
+        );
+    }
+
+    /**
+     * @psalm-param value-of<Docman_PermissionsManager::ITEM_PERMISSION_TYPES> $permission_type
+     * @return UserGroupRepresentation[]
+     */
+    private function buildUGroupsRepresentationForPermission(Project $project, Docman_Item $item, string $permission_type) : array
+    {
+        $user_group_representations = [];
+
+        $ugroup_ids = $this->permissions_manager->getAuthorizedUGroupIdsForProject(
+            $project,
+            $item->getId(),
+            $permission_type
+        );
+        foreach ($ugroup_ids as $ugroup_id) {
+            $ugroup = $this->ugroup_manager->getUGroup($project, $ugroup_id);
+            if ($ugroup !== null) {
+                $user_group_representations[] = (new UserGroupRepresentation())->build((int) $project->getID(), $ugroup);
+            }
+        }
+
+        return $user_group_representations;
+    }
+}

plugins/docman/include/REST/v1/Permissions/DocmanItemPermissionsForGroupsRepresentation.php
@@ -1,0 +1,58 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Docman\REST\v1\Permissions;
+
+use Tuleap\Project\REST\UserGroupRepresentation;
+
+final class DocmanItemPermissionsForGroupsRepresentation
+{
+    /**
+     * @var array {@type Tuleap\Project\REST\UserGroupRepresentation}
+     * @psalm-var \Tuleap\Project\REST\UserGroupRepresentation[]
+     */
+    public $can_read = [];
+    /**
+     * @var array {@type Tuleap\Project\REST\UserGroupRepresentation}
+     * @psalm-var \Tuleap\Project\REST\UserGroupRepresentation[]
+     */
+    public $can_write = [];
+    /**
+     * @var array {@type Tuleap\Project\REST\UserGroupRepresentation}
+     * @psalm-var \Tuleap\Project\REST\UserGroupRepresentation[]
+     */
+    public $can_manage = [];
+
+    /**
+     * @param UserGroupRepresentation[] $can_read
+     * @param UserGroupRepresentation[] $can_write
+     * @param UserGroupRepresentation[] $can_manage
+     */
+    public static function build(array $can_read, array $can_write, array $can_manage) : self
+    {
+        $representation             = new self();
+        $representation->can_read   = $can_read;
+        $representation->can_write  = $can_write;
+        $representation->can_manage = $can_manage;
+        return $representation;
+    }
+}

plugins/docman/include/docmanPlugin.class.php
@@ -58,6 +58,7 @@ use Tuleap\Docman\REST\ResourcesInjector;
 use Tuleap\Docman\REST\v1\DocmanItemsEventAdder;
 use Tuleap\Docman\REST\v1\ItemRepresentationBuilder;
 use Tuleap\Docman\REST\v1\Metadata\MetadataRepresentationBuilder;
+use Tuleap\Docman\REST\v1\Permissions\DocmanItemPermissionsForGroupsBuilder;
 use Tuleap\Docman\Upload\UploadPathAllocatorBuilder;
 use Tuleap\Docman\Upload\Version\DocumentOnGoingVersionToUploadDAO;
 use Tuleap\Docman\Upload\Version\VersionBeingUploadedInformationProvider;
@@ -1260,12 +1261,14 @@ class DocmanPlugin extends Plugin
 
         $html_purifier = Codendi_HTMLPurifier::instance();
 
+        $permissions_manager = Docman_PermissionsManager::instance($project->getID());
+
         $metadata_factory            = new Docman_MetadataFactory($project->getID());
         $item_representation_builder = new ItemRepresentationBuilder(
             new Docman_ItemDao(),
             $this->getUserManager(),
             $this->getItemFactory(),
-            Docman_PermissionsManager::instance($project->getID()),
+            $permissions_manager,
             $this->getDocmanLockFactory(),
             new ApprovalTableStateMapper(),
             new MetadataRepresentationBuilder(
@@ -1276,6 +1279,12 @@ class DocmanPlugin extends Plugin
             new ApprovalTableRetriever(
                 new Docman_ApprovalTableFactoriesFactory(),
                 new Docman_VersionFactory()
+            ),
+            new DocmanItemPermissionsForGroupsBuilder(
+                $permissions_manager,
+                ProjectManager::instance(),
+                PermissionsManager::instance(),
+                new UGroupManager()
             ),
             $html_purifier
         );

commit 6711cf4c4ad96e9983715367dbe9d57a4cbdc090

A non admin user can not see documents in the document manager UI
Issue has been introduced by 43fb51f1b67f3d5a6c150524d0e6c6f9713fdfcd.

Part of story #12457: New document modal: permissions

Change-Id: Iffee61bc0fe55c00ddf8c6567b43af35788c3f17

plugins/docman/include/Docman_PermissionsManagerDao.class.php
@@ -39,7 +39,7 @@ class Docman_PermissionsManagerDao extends DataAccessObject {
 
         $sql = 'SELECT *'.
             ' FROM permissions'.
-            " WHERE object_id IN ('.$itemsIds.')".
+            ' WHERE object_id IN ('.$itemsIds.')'.
             ' AND permission_type IN ('.$perms.')'.
             ' AND ugroup_id IN ('.$ugroupIds.')';
         return $this->retrieve($sql);