730c982783a0f791a62d194ce717a9dba58130aa
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2019-08-08 14:22:41 +0200

    Add SQL index on expiration date
    This is part of story #13667 have an expiration date associated to API AccessKey
    
    Given that keys are retrived by expiration_date for the daily deletion,
    then we add an index on this information.
    
    Change-Id: Ia5d29a29fe75421afd4f096e6265ae49f399fe38

---
+++ src/db/mysql/updates/2019/201908081421_add_index_on_user_access_key_expiration_date.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+class b201908081421_add_index_on_user_access_key_expiration_date extends ForgeUpgrade_Bucket // @codingStandardsIgnoreLine
+{
+    public function description()
+    {
+        return 'Add index on expiration date for user API access key';
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $this->db->addIndex(
+            'user_access_key',
+            'idx_expiration_date',
+            'ALTER TABLE user_access_key ADD INDEX idx_expiration_date (expiration_date)'
+        );
+    }
+}
