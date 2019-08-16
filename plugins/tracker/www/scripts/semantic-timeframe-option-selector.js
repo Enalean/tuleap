dfca5c68dab2551e59905936ececa9ee1602eca4
Thomas Gorka <thomas.gorka@enalean.com>
2019-08-16 13:51:14 +0200

    Prevent user to select start date field === end date field
    part of: story #12537 configure my burndown/up & sprint with a start date & end date
    
    How to test:
    - Select a start date field
    - Select the "start date + end date" mode
    - Open the end date selectbox
    --> The start date field is disabled in the selectbox options
    
    - Select an end date field
    - open the start date field
    --> The end date field is disabled in the selectbox options
    
    - Switch to "start date + duration" mode
    --> The end date selectbox is disabled and it displays "Choose a
    field..."
    --> Every fields are selectable in the start date selectbox
    
    - Now switch back to "start date + end date" mode
    --> the curretly selected start date field is disabled in the end date
    field selectbox
    
    Change-Id: I4cb043af73db9e423e9e6dde4131365f1527c2cd

--- plugins/tracker/www/scripts/semantic-timeframe-option-selector.js
+++ plugins/tracker/www/scripts/semantic-timeframe-option-selector.js
@@ -19,11 +19,77 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("semantic-timeframe-options");
+    const start_date_selectbox = document.getElementById("start-date");
+    const end_date_selectbox = document.getElementById("end-date-field");
+    const option_end_date_radio_button = document.getElementById("option-end-date");
+    const option_duration_radio_button = document.getElementById("option-duration");
 
-    container.addEventListener("click", toggleSelectboxes);
+    registerEvents(
+        container,
+        start_date_selectbox,
+        end_date_selectbox,
+        option_end_date_radio_button,
+        option_duration_radio_button
+    );
+
+    initSelectboxBindings(start_date_selectbox, end_date_selectbox, option_end_date_radio_button);
 
     toggleSelectboxes();
 });
+
+function registerEvents(
+    container,
+    start_date_selectbox,
+    end_date_selectbox,
+    option_end_date_radio_button,
+    option_duration_radio_button
+) {
+    container.addEventListener("click", toggleSelectboxes);
+
+    start_date_selectbox.addEventListener("change", () =>
+        disableAlreadySelectedDateFields(
+            start_date_selectbox,
+            end_date_selectbox,
+            option_end_date_radio_button
+        )
+    );
+    end_date_selectbox.addEventListener("change", () =>
+        disableAlreadySelectedDateFields(
+            end_date_selectbox,
+            start_date_selectbox,
+            option_end_date_radio_button
+        )
+    );
+    option_end_date_radio_button.addEventListener("click", () =>
+        initSelectboxBindings(
+            start_date_selectbox,
+            end_date_selectbox,
+            option_end_date_radio_button
+        )
+    );
+
+    option_duration_radio_button.addEventListener("click", () => {
+        enableAllOptions([start_date_selectbox, end_date_selectbox]);
+        end_date_selectbox.children[0].selected = true;
+    });
+}
+
+function initSelectboxBindings(
+    start_date_selectbox,
+    end_date_selectbox,
+    option_end_date_radio_button
+) {
+    disableAlreadySelectedDateFields(
+        start_date_selectbox,
+        end_date_selectbox,
+        option_end_date_radio_button
+    );
+    disableAlreadySelectedDateFields(
+        end_date_selectbox,
+        start_date_selectbox,
+        option_end_date_radio_button
+    );
+}
 
 function toggleSelectboxes() {
     const options = document.querySelectorAll(".semantic-timeframe-option-radio");
@@ -63,4 +129,41 @@ function showAsterix(radio_button) {
     }
 
     asterix.classList.remove("tracker-administration-semantic-timeframe-option-not-required");
+}
+
+function disableAlreadySelectedDateFields(
+    current_selectbox,
+    target_selectbox,
+    option_end_date_radio_button
+) {
+    if (!option_end_date_radio_button.checked) {
+        return;
+    }
+
+    const current_selectbox_value = getNumberValue(current_selectbox);
+
+    if (!current_selectbox_value) {
+        enableAllOptions([target_selectbox]);
+
+        return;
+    }
+
+    for (const option of target_selectbox.children) {
+        const option_value = getNumberValue(option);
+        if (option_value && option_value === current_selectbox_value) {
+            option.disabled = true;
+        }
+    }
+}
+
+function getNumberValue(element) {
+    return Number(element.value);
+}
+
+function enableAllOptions(selectboxes) {
+    selectboxes.forEach(selectbox => {
+        for (const option of selectbox.children) {
+            option.disabled = false;
+        }
+    });
 }
