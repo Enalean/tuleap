a1af13947d21576e6db9acc4b2a3b682bc191ab3
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-08-16 11:12:43 +0200

    New folder should have same behaviour than item creation
    part of story #12634 Add custom metadata in modal
    
    How to test:
    Create a new folder
    Metadata are displayed in read only mode, if you create the folder
    its metadata will be inherited from its parent
    
    Change-Id: I6eead24df77a91a8991dae2dddf5eaa557eae9f1

---
+++ plugins/document/scripts/document/components/Folder/ModalNewItem/NewFolderModal.spec.js
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import localVue from "../../../helpers/local-vue.js";
+import { tlp } from "tlp-mocks";
+
+import NewFolderModal from "./NewFolderModal.vue";
+import { createStoreMock } from "@tuleap-vue-components/store-wrapper.js";
+
+import EventBus from "../../../helpers/event-bus.js";
+
+describe("NewFolderModal", () => {
+    let factory, store;
+
+    beforeEach(() => {
+        const general_store = {
+            state: {
+                current_folder: {
+                    id: 42,
+                    title: "My current folder",
+                    metadata: [
+                        {
+                            short_name: "title",
+                            name: "title",
+                            list_value: "My current folder",
+                            is_multiple_value_allowed: false,
+                            type: "text",
+                            is_required: false
+                        },
+                        {
+                            short_name: "custom metadata",
+                            name: "custom",
+                            value: "value",
+                            is_multiple_value_allowed: false,
+                            type: "text",
+                            is_required: false
+                        }
+                    ]
+                },
+                is_obsolescence_date_metadata_used: true,
+                is_item_status_metadata_used: true,
+                project_id: 102
+            }
+        };
+
+        store = createStoreMock(general_store, { metadata: {} });
+
+        factory = () => {
+            return shallowMount(NewFolderModal, {
+                localVue,
+                mocks: { $store: store }
+            });
+        };
+
+        tlp.modal.and.returnValue({
+            addEventListener: () => {},
+            show: () => {},
+            hide: () => {}
+        });
+    });
+
+    afterEach(() => {});
+
+    it("Does not load project metadata, when they have already been loaded", async () => {
+        store.state.metadata = {
+            has_loaded_metadata: true
+        };
+
+        const wrapper = factory();
+
+        EventBus.$emit("show-new-document-modal", {
+            detail: { parent: store.state.current_folder }
+        });
+        await wrapper.vm.$nextTick().then(() => {});
+
+        expect(store.dispatch).not.toHaveBeenCalledWith("metadata/loadProjectMetadata");
+    });
+
+    it("It inherit default values from parent metadata", () => {
+        const folder_to_create = {
+            metadata: [
+                {
+                    short_name: "custom metadata",
+                    name: "custom",
+                    value: "value",
+                    is_multiple_value_allowed: false,
+                    type: "text",
+                    is_required: false
+                }
+            ]
+        };
+
+        const wrapper = factory();
+
+        EventBus.$emit("show-new-folder-modal", {
+            detail: { parent: store.state.current_folder }
+        });
+        expect(wrapper.vm.item.metadata).toEqual(folder_to_create.metadata);
+    });
+});
