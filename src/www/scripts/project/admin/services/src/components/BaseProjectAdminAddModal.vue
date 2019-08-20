Enalean/tuleap

commit 3f686d9af896e51f6c622cb75f709d5754d04875

Select icon and open in new tab in Add modal
part of story #13566 associate a custom icon to external services
part of story #13567 decide that custom services opens in a new
tab/window by default

How to test:
- As project admin, you can add a new service. The icon selector will let
you choose an icon and there is a switch to "open in a new tab" but those
values are not saved yet (they are ignored). There is a preview of the
label and icon.
- As site admin, you can add a new service and can also set it to
available for new projects.
- As site admin, only in the Default Site Template, you can choose a
short name. However, when you add it, the service is invisible in the UI
of all projects [0].
- You can no longer choose to add a new service that opens in an iframe
(the input is removed).
- No functional change when Editing services.

[0]: see request #13712, that behaviour is the same on master.

Change-Id: I13f5f9678524cb160e77f5115458ff38f784d6a0

src/www/scripts/project/admin/services/src/components/BaseProjectAdminAddModal.vue
@@ -18,7 +18,7 @@
   -->
 
 <template>
-    <edit-modal
+    <add-modal
         v-bind:form_url="form_url"
         ref="modal"
         v-on:reset-modal="resetModal"
@@ -26,34 +26,27 @@
         <template slot="content">
             <input type="hidden" v-bind:name="csrf_token_name" v-bind:value="csrf_token">
             <sidebar-previewer
-                v-bind:label="service.label"
+                v-bind:label="preview_label"
                 v-bind:icon_name="service.icon_name"
             />
-            <in-edition-custom-service
-                v-if="service.is_project_scope && is_shown"
+            <in-creation-custom-service
                 v-bind:minimal_rank="minimal_rank"
                 v-bind:service="service"
                 v-bind:allowed_icons="allowed_icons"
             />
-            <read-only-system-service
-                v-if="!service.is_project_scope && is_shown"
-                v-bind:minimal_rank="minimal_rank"
-                v-bind:service="service"
-            />
         </template>
-    </edit-modal>
+    </add-modal>
 </template>
 <script>
-import InEditionCustomService from "./Service/InEditionCustomService.vue";
+import AddModal from "./AddModal.vue";
 import SidebarPreviewer from "./SidebarPreviewer.vue";
-import EditModal from "./EditModal.vue";
-import ReadOnlySystemService from "./Service/ReadOnlySystemService.vue";
+import InCreationCustomService from "./Service/InCreationCustomService.vue";
 import { service_modal_mixin } from "./service-modal-mixin.js";
-import { edit_modal_mixin } from "./edit-modal-mixin.js";
+import { add_modal_mixin } from "./add-modal-mixin.js";
 
 export default {
-    name: "BaseProjectAdminEditModal",
-    components: { ReadOnlySystemService, EditModal, InEditionCustomService, SidebarPreviewer },
-    mixins: [service_modal_mixin, edit_modal_mixin]
+    name: "BaseProjectAdminAddModal",
+    components: { AddModal, SidebarPreviewer, InCreationCustomService },
+    mixins: [service_modal_mixin, add_modal_mixin]
 };
 </script>

src/www/scripts/project/admin/services/src/components/BaseSiteAdminAddModal.vue
@@ -18,7 +18,7 @@
   -->
 
 <template>
-    <edit-modal
+    <add-modal
         v-bind:form_url="form_url"
         ref="modal"
         v-on:reset-modal="resetModal"
@@ -26,34 +26,48 @@
         <template slot="content">
             <input type="hidden" v-bind:name="csrf_token_name" v-bind:value="csrf_token">
             <sidebar-previewer
-                v-bind:label="service.label"
+                v-bind:label="preview_label"
                 v-bind:icon_name="service.icon_name"
             />
-            <in-edition-custom-service
-                v-if="service.is_project_scope && is_shown"
+            <in-creation-custom-service
                 v-bind:minimal_rank="minimal_rank"
                 v-bind:service="service"
                 v-bind:allowed_icons="allowed_icons"
-            />
-            <read-only-system-service
-                v-if="!service.is_project_scope && is_shown"
-                v-bind:minimal_rank="minimal_rank"
-                v-bind:service="service"
-            />
+            >
+                <template slot="is_active">
+                    <service-is-active id="project-service-add-modal-active" v-bind:value="service.is_active"/>
+                </template>
+                <template slot="shortname" v-if="is_default_template">
+                    <service-shortname id="project-service-add-modal-shortname" v-bind:value="service.short_name"/>
+                </template>
+            </in-creation-custom-service>
         </template>
-    </edit-modal>
+    </add-modal>
 </template>
 <script>
-import InEditionCustomService from "./Service/InEditionCustomService.vue";
+import AddModal from "./AddModal.vue";
 import SidebarPreviewer from "./SidebarPreviewer.vue";
-import EditModal from "./EditModal.vue";
-import ReadOnlySystemService from "./Service/ReadOnlySystemService.vue";
+import InCreationCustomService from "./Service/InCreationCustomService.vue";
+import ServiceIsActive from "./Service/ServiceIsActive.vue";
+import ServiceShortname from "./Service/ServiceShortname.vue";
 import { service_modal_mixin } from "./service-modal-mixin.js";
-import { edit_modal_mixin } from "./edit-modal-mixin.js";
+import { add_modal_mixin } from "./add-modal-mixin.js";
+import { ADMIN_PROJECT_ID } from "../constants.js";
 
 export default {
-    name: "BaseProjectAdminEditModal",
-    components: { ReadOnlySystemService, EditModal, InEditionCustomService, SidebarPreviewer },
-    mixins: [service_modal_mixin, edit_modal_mixin]
+    name: "BaseSiteAdminAddModal",
+    components: {
+        AddModal,
+        SidebarPreviewer,
+        InCreationCustomService,
+        ServiceIsActive,
+        ServiceShortname
+    },
+    mixins: [service_modal_mixin, add_modal_mixin],
+    computed: {
+        is_default_template() {
+            return this.project_id === ADMIN_PROJECT_ID;
+        }
+    }
 };
 </script>

src/www/scripts/project/admin/services/src/components/BaseSiteAdminEditModal.vue
@@ -19,22 +19,22 @@
 
 <template>
     <edit-modal v-bind:form_url="form_url" ref="modal" v-on:reset-modal="resetModal">
-        <template v-slot:content>
+        <template slot="content">
             <input type="hidden" v-bind:name="csrf_token_name" v-bind:value="csrf_token">
             <sidebar-previewer
                 v-bind:label="service.label"
                 v-bind:icon_name="service.icon_name"
             />
-            <project-defined-service
+            <in-edition-custom-service
                 v-if="service.is_project_scope && is_shown"
                 v-bind:minimal_rank="minimal_rank"
                 v-bind:service="service"
                 v-bind:allowed_icons="allowed_icons"
             >
-                <template v-slot:is_active>
-                    <service-is-active v-bind:value="service.is_active"/>
+                <template slot="is_active">
+                    <service-is-active id="project-service-add-modal-active" v-bind:value="service.is_active"/>
                 </template>
-            </project-defined-service>
+            </in-edition-custom-service>
             <editable-system-service
                 v-if="!service.is_project_scope && is_shown"
                 v-bind:minimal_rank="minimal_rank"
@@ -44,22 +44,23 @@
     </edit-modal>
 </template>
 <script>
-import ProjectDefinedService from "./Service/ProjectDefinedService.vue";
+import InEditionCustomService from "./Service/InEditionCustomService.vue";
 import EditModal from "./EditModal.vue";
 import ServiceIsActive from "./Service/ServiceIsActive.vue";
 import SidebarPreviewer from "./SidebarPreviewer.vue";
-import { edit_modal_mixin } from "./edit-modal-mixin.js";
 import EditableSystemService from "./Service/EditableSystemService.vue";
+import { service_modal_mixin } from "./service-modal-mixin.js";
+import { edit_modal_mixin } from "./edit-modal-mixin.js";
 
 export default {
     name: "BaseSiteAdminEditModal",
     components: {
         EditableSystemService,
         EditModal,
-        ProjectDefinedService,
+        InEditionCustomService,
         ServiceIsActive,
         SidebarPreviewer
     },
-    mixins: [edit_modal_mixin]
+    mixins: [service_modal_mixin, edit_modal_mixin]
 };
 </script>

src/www/scripts/project/admin/services/src/components/Service/InCreationCustomService.vue
@@ -1,0 +1,89 @@
+<!--
+  - Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="tlp-modal-body">
+        <div class="project-admin-services-modals-top-fields">
+            <service-label id="project-service-add-modal-label" v-model="service.label"/>
+            <service-is-used
+                id="project-service-add-modal-enabled"
+                v-bind:value="service.is_used"
+            />
+        </div>
+        <div class="project-admin-services-modals-top-fields">
+            <icon-selector
+                id="project-service-add-modal-icon"
+                v-model="service.icon_name"
+                v-bind:allowed_icons="allowed_icons"
+            />
+            <slot name="is_active">
+                <hidden-service-is-active v-bind:value="service.is_active"/>
+            </slot>
+        </div>
+
+        <service-rank
+            id="project-service-add-modal-rank"
+            v-bind:minimal_rank="minimal_rank"
+            v-bind:value="service.rank"
+        />
+        <service-link id="project-service-add-modal-link" v-bind:value="service.link"/>
+        <slot name="shortname"/>
+        <service-description
+            id="project-service-add-modal-description"
+            v-bind:value="service.description"
+        />
+
+        <service-open-in-new-tab
+            id="project-service-add-modal-new-tab"
+            v-bind:value="service.is_in_new_tab"
+        />
+    </div>
+</template>
+<script>
+import ServiceOpenInNewTab from "./ServiceOpenInNewTab.vue";
+import IconSelector from "./IconSelector.vue";
+import ServiceLabel from "./ServiceLabel.vue";
+import ServiceLink from "./ServiceLink.vue";
+import ServiceDescription from "./ServiceDescription.vue";
+import ServiceIsUsed from "./ServiceIsUsed.vue";
+import ServiceRank from "./ServiceRank.vue";
+import HiddenServiceIsActive from "./HiddenServiceIsActive.vue";
+import { service_mixin } from "./service-mixin.js";
+
+export default {
+    name: "InCreationCustomService",
+    components: {
+        ServiceOpenInNewTab,
+        HiddenServiceIsActive,
+        ServiceRank,
+        ServiceIsUsed,
+        ServiceDescription,
+        ServiceLink,
+        ServiceLabel,
+        IconSelector
+    },
+    mixins: [service_mixin],
+    props: {
+        allowed_icons: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>

src/www/scripts/project/admin/services/src/components/Service/ServiceOpenInNewTab.vue
@@ -17,44 +17,26 @@
   - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
   -->
 
-<template>
+<template functional>
     <div class="tlp-form-element">
-        <label
-            class="tlp-label"
-            v-bind:for="id"
-            v-translate
-        >
-            Description
+        <label class="tlp-label" v-bind:for="props.id" v-translate>
+            Open in a new tab
         </label>
-        <input
-            type="text"
-            class="tlp-input"
-            v-bind:id="id"
-            name="description"
-            v-bind:placeholder="description_placeholder"
-            size="70"
-            maxlength="255"
-            v-bind:value="value"
-        >
+        <div class="tlp-switch">
+            <input
+                type="checkbox"
+                class="tlp-switch-checkbox"
+                v-bind:id="props.id"
+                name="is_in_new_tab"
+                value="1"
+                v-bind:checked="props.value"
+                v-on:change="listeners.change || (() => {})"
+            >
+            <label
+                class="tlp-switch-button"
+                v-bind:for="props.id"
+                aria-hidden
+            ></label>
+        </div>
     </div>
 </template>
-<script>
-export default {
-    name: "ServiceDescription",
-    props: {
-        value: {
-            type: String,
-            required: true
-        },
-        id: {
-            type: String,
-            required: true
-        }
-    },
-    computed: {
-        description_placeholder() {
-            return this.$gettext("Awesome service to manage extra stuff");
-        }
-    }
-};
-</script>

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -126,6 +126,9 @@ msgstr "Statut du compte utilisateur"
 msgid "Activate synchronized project membership management"
 msgstr "Activer la gestion synchronisée de l'appartenance au projet"
 
+msgid "Add a new service"
+msgstr "Ajouter un nouveau service"
+
 msgid "Add binding"
 msgstr "Ajouter le lien"
 
@@ -248,12 +251,6 @@ msgstr "Demander à rejoindre"
 
 msgid "Ask to join this project"
 msgstr "Demander à rejoindre ce projet"
-
-msgid "Available (also available for new projects)"
-msgstr "Disponible (également disponible dans les nouveaux projets)"
-
-msgid "Awesome service to manage extra stuff"
-msgstr "Le service qui gère des trucs"
 
 msgid "Beyond 3 columns, each column<br>will have the same size."
 msgstr "Au-delà de 3 colonnes, chaque colonne<br>aura la même taille."
@@ -416,17 +413,11 @@ msgstr "Impossible d'accèder au fichier de contrôle d'accès du dépôt SVN"
 msgid "Could not update hooks of the SVN repository"
 msgstr "Impossible de mettre à jour les hooks du dépôt SVN"
 
-msgid "Create a new service"
-msgstr "Créer un nouveau service"
-
 msgid "Create from"
 msgstr "Créer à partir de"
 
 msgid "Create label"
 msgstr "Créer le label"
-
-msgid "Create service"
-msgstr "Créer le service"
 
 msgid "Create user group"
 msgstr "Ajouter un groupe utilisateur"
@@ -501,9 +492,6 @@ msgstr "Détails"
 msgid "Display density:"
 msgstr "Densité d'affichage :"
 
-msgid "Display in iframe"
-msgstr "Afficher dans une iframe"
-
 msgid "Display platform statistics on home page."
 msgstr "Afficher les statistiques de la plateforme sur la page d'accueil."
 
@@ -557,9 +545,6 @@ msgstr "Groupe vide"
 
 msgid "Enable accessibility mode"
 msgstr "Activer le mode d'accessiblilité"
-
-msgid "Enabled"
-msgstr "Activé"
 
 msgid "Enabled?"
 msgstr "Activé ?"
@@ -824,9 +809,6 @@ msgstr "Mois"
 
 msgid "My personnal page"
 msgstr "Ma page personnelle"
-
-msgid "My service"
-msgstr "Mon service"
 
 msgid "Name"
 msgstr "Nom"
@@ -1272,9 +1254,6 @@ msgstr "Description courte"
 
 msgid "Short description shouldn't exceed 255 characters."
 msgstr "La description courte ne doit pas dépasser 255 caractères."
-
-msgid "Short name"
-msgstr "Nom court"
 
 msgid "Shortcut list"
 msgstr "Liste des raccourcis"
@@ -2032,9 +2011,6 @@ msgstr "Retour à la page d'accueil"
 
 msgid "last<br>month"
 msgstr "le mois<br>dernier"
-
-msgid "my_service"
-msgstr "mon_service"
 
 #, php-format
 msgid "parameter \"query\" syntax error: \"%s\" property not found"

src/templates/project/admin/services/services-add-modal.mustache
@@ -1,135 +1,0 @@
-<form method="POST"
-      action="/project/{{ project_id }}/admin/services/add"
-      class="tlp-modal"
-      role="dialog"
-      id="project-admin-services-add-modal"
-      aria-labelledby="project-admin-services-add-modal-title"
->
-    <div class="tlp-modal-header">
-        <h1 class="tlp-modal-title" id="project-admin-services-add-modal-title">
-            <i class="fa fa-plus tlp-modal-title-icon"></i>
-            {{# gettext }} Create a new service {{/ gettext }}
-        </h1>
-        <div class="tlp-modal-close" data-dismiss="modal" aria-label="{{# gettext }}Close{{/ gettext }}">
-            ×
-        </div>
-    </div>
-    <div class="tlp-modal-body">
-        {{# csrf }}
-            {{> csrf_token_input }}
-        {{/ csrf }}
-
-        <div class="tlp-form-element">
-            <label class="tlp-label" for="project-admin-services-add-modal-label">
-                {{# gettext }}Label{{/ gettext }}
-                <i class="fa fa-asterisk"></i>
-            </label>
-            <input type="text"
-                   class="tlp-input"
-                   id="project-admin-services-add-modal-label"
-                   name="label"
-                   placeholder="{{# gettext }}My service{{/ gettext }}"
-                   size="30"
-                   maxlength="40"
-                   required>
-        </div>
-
-        <div class="tlp-form-element">
-            <label class="tlp-label" for="project-admin-services-add-modal-link">
-                {{# gettext }}Link{{/ gettext }}
-                <i class="fa fa-asterisk"></i>
-            </label>
-            <input type="text"
-                   class="tlp-input"
-                   id="project-admin-services-add-modal-link"
-                   name="link"
-                   placeholder="https://example.com/my-service/"
-                   maxlength="255"
-                   pattern="(https?://|#|/|\?).+"
-                   title="{{# gettext }}Please, enter a http:// or https:// link{{/ gettext }}"
-                   required>
-        </div>
-
-        {{# is_default_template }}
-            <div class="tlp-form-element">
-                <label class="tlp-label" for="project-admin-services-add-modal-shortname">
-                    {{# gettext }}Short name{{/ gettext }}
-                    <i class="fa fa-asterisk"></i>
-                </label>
-                <input type="text"
-                       class="tlp-input"
-                       id="project-admin-services-add-modal-shortname"
-                       name="short_name"
-                       placeholder="{{# gettext }}my_service{{/ gettext }}"
-                       size="15"
-                       maxlength="40"
-                       required>
-            </div>
-        {{/ is_default_template }}
-
-        <div class="tlp-form-element">
-            <label class="tlp-label" for="project-admin-services-add-modal-description">
-                {{# gettext }}Description{{/ gettext }}
-            </label>
-            <input type="text"
-                   class="tlp-input"
-                   id="project-admin-services-add-modal-description"
-                   name="description"
-                   placeholder="{{# gettext }}Awesome service to manage extra stuff{{/ gettext }}"
-                   size="70"
-                   maxlength="255">
-        </div>
-
-        <hr class="tlp-modal-separator">
-
-        {{# is_default_template }}
-            <div class="tlp-form-element">
-                <label class="tlp-label tlp-checkbox">
-                    <input type="checkbox" name="is_active" value="1" checked>
-                    {{# gettext }}Available (also available for new projects){{/ gettext }}
-                </label>
-            </div>
-        {{/ is_default_template }}
-        {{^ is_default_template }}
-            <input type="hidden" name="is_active" value="1">
-        {{/ is_default_template }}
-
-        <div class="tlp-form-element">
-            <label class="tlp-label tlp-checkbox">
-                <input type="checkbox" name="is_used" value="1" checked>
-                {{# gettext }}Enabled{{/ gettext }}
-            </label>
-        </div>
-
-        <div class="tlp-form-element">
-            <label class="tlp-label tlp-checkbox">
-                <input type="checkbox" name="is_in_iframe" value="1">
-                {{# gettext }}Display in iframe{{/ gettext }}
-            </label>
-        </div>
-
-        <div class="tlp-form-element">
-            <label class="tlp-label" for="project-admin-services-add-modal-rank">
-                {{# gettext }}Rank{{/ gettext }}
-                <i class="fa fa-asterisk"></i>
-            </label>
-            <input type="number"
-                   class="tlp-input"
-                   id="project-admin-services-add-modal-rank"
-                   name="rank"
-                   placeholder="150"
-                   size="5"
-                   maxlength="5"
-                   min="{{ minimal_rank }}"
-                   required>
-        </div>
-    </div>
-    <div class="tlp-modal-footer">
-        <button type="reset" class="tlp-button-primary tlp-button-outline tlp-modal-action" data-dismiss="modal">
-            {{# gettext }}Cancel{{/ gettext }}
-        </button>
-        <button type="submit" class="tlp-button-primary tlp-modal-action user-group-modal-button">
-            <i class="fa fa-save tlp-button-icon"></i> {{# gettext }}Create service{{/ gettext }}
-        </button>
-    </div>
-</form>

src/templates/project/admin/services/services.mustache
@@ -11,13 +11,11 @@
                     <button
                        id="project-admin-services-add-button"
                        type="button"
-                       data-target-modal-id="project-admin-services-add-modal"
                        class="tlp-button-primary tlp-table-actions-element"
                     >
                         <i class="tlp-button-icon fa fa-plus"></i>
-                        {{# gettext }}Create a new service{{/ gettext }}
+                        {{# gettext }}Add a new service{{/ gettext }}
                     </button>
-                    {{> services-add-modal }}
                 </div>
                 <table class="tlp-table">
                     <thead>
@@ -92,6 +90,13 @@
                     </tbody>
                 </table>
                 {{> services-delete-modal }}
+                <div id="service-add-modal"
+                     data-project-id="{{ project_id }}"
+                     data-minimal-rank="{{ minimal_rank }}"
+                     data-csrf-token-name="{{ csrf_token_name }}"
+                     data-csrf-token="{{ csrf_token }}"
+                     data-allowed-icons="{{ allowed_icons }}"
+                 ></div>
                 <div id="service-edit-modal"
                      data-project-id="{{ project_id }}"
                      data-minimal-rank="{{ minimal_rank }}"

commit f19cffe9fd38b90eb66fd7f7e2864887a7ddf33e

Preview should show the "New tab" icon
Part of story #13567 decide that custom services opens in a new
tab/window by default

How to test:
- As project admin or site admin, when you add a new service and check
"Open in a new tab", an arrow icon should show to the right of the label
in the preview
- Same when editing a custom service
- When the service was opened in an iframe and you check "Open in a new
tab", the "Open in an iframe" is now correctly disabled (it was broken
in 3f686d9af896e51f6c622cb75f709d5754d04875)

Change-Id: Iafa55285dc2a22a608ec6c10f049647dbf0b6971

src/www/scripts/project/admin/services/src/components/BaseProjectAdminAddModal.vue
@@ -28,6 +28,7 @@
             <sidebar-previewer
                 v-bind:label="preview_label"
                 v-bind:icon_name="service.icon_name"
+                v-bind:is_in_new_tab="service.is_in_new_tab"
             />
             <in-creation-custom-service
                 v-bind:minimal_rank="minimal_rank"

src/www/scripts/project/admin/services/src/components/BaseProjectAdminEditModal.vue
@@ -28,6 +28,7 @@
             <sidebar-previewer
                 v-bind:label="service.label"
                 v-bind:icon_name="service.icon_name"
+                v-bind:is_in_new_tab="service.is_in_new_tab"
             />
             <in-edition-custom-service
                 v-if="service.is_project_scope && is_shown"

src/www/scripts/project/admin/services/src/components/BaseSiteAdminAddModal.vue
@@ -28,6 +28,7 @@
             <sidebar-previewer
                 v-bind:label="preview_label"
                 v-bind:icon_name="service.icon_name"
+                v-bind:is_in_new_tab="service.is_in_new_tab"
             />
             <in-creation-custom-service
                 v-bind:minimal_rank="minimal_rank"

src/www/scripts/project/admin/services/src/components/BaseSiteAdminEditModal.vue
@@ -24,6 +24,7 @@
             <sidebar-previewer
                 v-bind:label="service.label"
                 v-bind:icon_name="service.icon_name"
+                v-bind:is_in_new_tab="service.is_in_new_tab"
             />
             <in-edition-custom-service
                 v-if="service.is_project_scope && is_shown"

src/www/scripts/project/admin/services/src/components/Service/InCreationCustomService.vue
@@ -52,6 +52,7 @@
         <service-open-in-new-tab
             id="project-service-add-modal-new-tab"
             v-bind:value="service.is_in_new_tab"
+            v-on:change="onNewTabChange"
         />
     </div>
 </template>
@@ -83,6 +84,11 @@ export default {
         allowed_icons: {
             type: Object,
             required: true
+        }
+    },
+    methods: {
+        onNewTabChange($event) {
+            this.service.is_in_new_tab = $event.target.checked;
         }
     }
 };

src/www/scripts/project/admin/services/src/components/Service/ServiceOpenInNewTab.vue
@@ -30,7 +30,7 @@
                 name="is_in_new_tab"
                 value="1"
                 v-bind:checked="props.value"
-                v-on:change="listeners.change || (() => {})"
+                v-on="listeners"
             >
             <label
                 class="tlp-switch-button"

src/www/scripts/project/admin/services/src/components/SidebarPreviewer.vue
@@ -25,6 +25,7 @@
                 <span class="project-sidebar-nav-item-label project-admin-services-modal-preview-label">
                     {{ props.label }}
                 </span>
+                <i v-if="props.is_in_new_tab" class="fa fa-arrow-right project-sidebar-nav-item-new-tab"></i>
             </div>
         </div>
     </div>