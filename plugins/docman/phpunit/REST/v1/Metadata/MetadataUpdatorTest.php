b925fafb53439660c2801394e46f4a6cbd264aec
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-08-23 10:35:34 +0200

    request #13785 can not update properties of root folder
    update properties of your root folder
    No exception should occur
    
    Exception is thrown by REST route:
    2019-08-23T10:31:17+02:00 [177] [error] Unhandled exception: Parent item 0 not found!:
    
    Change-Id: Ieee99b3ebb684afc45e3682ff16100e7e954c53f

--- plugins/docman/phpunit/REST/v1/Metadata/MetadataUpdatorTest.php
+++ plugins/docman/phpunit/REST/v1/Metadata/MetadataUpdatorTest.php
@@ -159,6 +159,66 @@ final class MetadataUpdatorTest extends TestCase
         $this->updator->updateDocumentMetadata($representation, $item, $current_user);
     }
 
+    public function testDocumentCustomMetadataCanBeChangedForRootFolder(): void
+    {
+        $date = new \DateTimeImmutable();
+        $this->status_mapper->shouldReceive('getItemStatusIdFromItemStatusString')->andReturn(
+            PLUGIN_DOCMAN_ITEM_STATUS_APPROVED
+        );
+        $this->status_mapper->shouldReceive('getItemStatusStringFormId')->andReturn(
+            ItemStatusMapper::ITEM_STATUS_APPROVED
+        );
+        $this->obsolescence_date_retriever->shouldReceive('getTimeStampOfDate')->andReturn($date->getTimestamp());
+
+        $old_user_id = 101;
+        $item        = \Mockery::mock(\Docman_Item::class);
+        $item->shouldReceive('getOwnerId')->andReturn($old_user_id);
+        $item->shouldReceive('getId')->andReturn(10);
+        $item->shouldReceive('getStatus')->andReturn(PLUGIN_DOCMAN_ITEM_STATUS_APPROVED);
+        $item->shouldReceive('getTitle')->andReturn("my title");
+        $item->shouldReceive('getParentId')->andReturn(0);
+
+        $old_user = \Mockery::mock(\PFUser::class);
+        $old_user->shouldReceive('getUserName')->andReturn('owner name');
+        $this->owner_retriever->shouldReceive('getUserFromRepresentationId')->andReturn($old_user);
+        $this->user_manager->shouldReceive('getUserById')->andReturn($old_user);
+        $old_user->shouldReceive('getUserName')->andReturn('user name');
+
+        $representation                             = new PUTMetadataRepresentation();
+        $representation->title                      = "title";
+        $representation->description                = "";
+        $representation->owner_id                   = $old_user_id;
+        $representation->status                     = ItemStatusMapper::ITEM_STATUS_APPROVED;
+        $representation->obsolescence_date          = ItemRepresentation::OBSOLESCENCE_DATE_NONE;
+        $custom_metadata_representation             = new POSTCustomMetadataRepresentation();
+        $custom_metadata_representation->short_name = "field_1";
+        $custom_metadata_representation->value      = "value";
+        $representation->metadata                   = [$custom_metadata_representation];
+
+        $current_user = \Mockery::mock(\PFUser::class);
+
+        $this->item_factory->shouldReceive('doesTitleCorrespondToExistingDocument')->andReturn(false);
+        $this->item_factory->shouldReceive('update')->once();
+
+        $project_configured_metadata = \Mockery::mock(Docman_Metadata::class);
+
+        $metadata_to_update_representation = MetadataToUpdate::buildMetadataRepresentation(
+            $project_configured_metadata,
+            $custom_metadata_representation->value,
+            PUTRecursiveStatusRepresentation::RECURSION_ALL_ITEMS
+        );
+        $this->representation_retriever->shouldReceive('checkAndBuildMetadataToUpdate')->once()->andReturn(
+            [$metadata_to_update_representation]
+        );
+
+        $this->item_factory->shouldReceive('getItemFromDb')->never();
+        $this->document_on_going_retriever->shouldReceive('isThereAlreadyAnUploadOngoing')->never();
+        $this->metadata_value_updator->shouldReceive('updateMetadata')
+            ->withArgs([$metadata_to_update_representation->getMetadata(), 10, $metadata_to_update_representation->getValue()])->once();
+
+        $this->updator->updateDocumentMetadata($representation, $item, $current_user);
+    }
+
     public function testDocumentCustomMetadataCanBeChanged(): void
     {
         $date = new \DateTimeImmutable();
