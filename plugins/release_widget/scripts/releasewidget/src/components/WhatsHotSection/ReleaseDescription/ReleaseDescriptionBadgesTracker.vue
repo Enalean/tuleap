d34e115fc9a98372b2a19e113883f4ca656e1eb2
Thomas Gerbet <thomas.gerbet@enalean.com>
2019-09-02 15:34:39 +0200

    Move vue.shims.d.ts in core
    This reduce the boilerplate needed each time we bootstrap a new Vue app
    and also avoid complaints from IDEs that a type is defined multiple times.
    
    Part of story #13729: have taskboard empty state
    
    Change-Id: Ib45a3db28fb877aa42addfe48e92232f9b2fd40f

--- plugins/release_widget/scripts/releasewidget/src/components/WhatsHotSection/ReleaseDescription/ReleaseDescriptionBadgesTracker.vue
+++ plugins/release_widget/scripts/releasewidget/src/components/WhatsHotSection/ReleaseDescription/ReleaseDescriptionBadgesTracker.vue
@@ -31,3 +31,10 @@
         </div>
     </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class ReleaseDescriptionBadgesTracker extends Vue {}
+</script>
