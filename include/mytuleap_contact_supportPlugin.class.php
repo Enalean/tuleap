c37037e4030d5790eae2536e35ead970786d4cc5
Thomas Gorka <thomas.gorka@enalean.com>
2019-09-02 15:44:03 +0200

    Generate autoload file with composer
    part of: request #13840 Generate autoload file with composer
    
    Change-Id: I2fd298f6d88c12f4af06e4a1c6b37de0957c6fbb

--- include/mytuleap_contact_supportPlugin.class.php
+++ include/mytuleap_contact_supportPlugin.class.php
@@ -18,7 +18,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once 'autoload.php';
+require_once __DIR__  . '/../vendor/autoload.php';
 require_once 'constants.php';
 
 use Tuleap\Layout\IncludeAssets;
