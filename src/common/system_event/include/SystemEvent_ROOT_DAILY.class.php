Enalean/tuleap

commit fbb466efe4a71d5bcb205cfbf919f7aa5e08c3d5

Have a dedicated command to launch Daily event
Welcome the big commit that groups all the events that were previously
run with different crontab entries:
- tuleap daily
- tmpwatch for temp file purge
- various stats from underworld

tmpwatch was rewritten in pure php to:
- remove a dependency
- have tests around the code
- ensure some rules (no recursion, etc) to avoid chooging ourselves in the feets

This is part of request #13394 tuleap-aio should run on centos7

Change-Id: I5510fbb8f379a273a40aba733b1f418f599264ee

src/common/system_event/include/SystemEvent_ROOT_DAILY.class.php
@@ -1,28 +1,30 @@
 <?php
 /**
+ * Copyright (c) Enalean, 2012-Present. All rights reserved
  * Copyright (c) STMicroelectronics, 2004-2009. All rights reserved
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * System Event classes
- *
- */
-class SystemEvent_ROOT_DAILY extends SystemEvent {
+use Symfony\Component\Process\Process;
+use Tuleap\SystemEvent\RootDailyStartEvent;
+
+class SystemEvent_ROOT_DAILY extends SystemEvent // phpcs:ignore
+{
+    private const DAY_OF_WEEKLY_STATS = 'Monday';
 
     /**
      * Verbalize the parameters so they are readable and much user friendly in
@@ -33,20 +35,27 @@ class SystemEvent_ROOT_DAILY extends SystemEvent {
      *
      * @return string
      */
-    public function verbalizeParameters($with_link) {
+    public function verbalizeParameters($with_link)
+    {
         return '-';
     }
 
     /**
      * Process stored event
      */
-    public function process() {
+    public function process()
+    {
+        $logger = BackendLogger::getDefaultLogger();
+        $logger->info(self::class.' Start');
+
         // Re-dumping ssh keys should be done only once a day as:
         // - It's I/O intensive
         // - It's stress gitolite backend
         // - SSH keys should already be dumped via EDIT_SSH_KEY event
         $backend_system = Backend::instance('System');
+        assert($backend_system instanceof BackendSystem);
         $backend_system->dumpSSHKeys();
+
 
         // User home sanity check should be done only once a day as
         // it is slooow (due to libnss-mysql)
@@ -57,49 +66,82 @@ class SystemEvent_ROOT_DAILY extends SystemEvent {
 
         $warnings = array();
 
+        $this->runComputeAllDailyStats($logger, $warnings);
+
+        $this->runWeeklyStats($logger, $warnings);
+
         try {
-            $this->_getEventManager()->processEvent(
-                'root_daily_start',
-                array('warnings' => &$warnings)
-            );
+            $frs_directory_cleaner = new \Tuleap\FRS\FRSIncomingDirectoryCleaner();
+            $frs_directory_cleaner->run();
+        } catch (Exception $exception) {
+            $warnings[] = $exception->getMessage();
+        }
+
+        try {
+            $root_daily_event = $this->getEventManager()->dispatch(new RootDailyStartEvent($logger));
+            assert($root_daily_event instanceof RootDailyStartEvent);
+            $warnings = array_merge($warnings, $root_daily_event->getWarnings());
 
             if (count($warnings) > 0) {
                 $this->warning(implode(PHP_EOL, $warnings));
             } else {
                 $this->done();
             }
-
         } catch (Exception $e) {
             $this->error($e->getMessage());
         }
 
+        $logger->info(self::class.' Completed');
         return true;
     }
 
-    /**
-     * Wrapper for EventManager
-     *
-     * @return EventManager
-     */
-    protected function _getEventManager() {
-        return EventManager::instance();
-    }
-
-    private function userHomeSanityCheck(BackendSystem $backend_system) {
+    private function userHomeSanityCheck(BackendSystem $backend_system)
+    {
         $dao   = new UserDao();
         $users = $dao
             ->searchByStatus(array(PFUser::STATUS_ACTIVE, PFUser::STATUS_RESTRICTED))
             ->instanciateWith(array(UserManager::instance(), 'getUserInstanceFromRow'));
 
-        foreach($users as $user) {
+        foreach ($users as $user) {
             $backend_system->userHomeSanityCheck($user);
         }
     }
 
-    private function purgeSystemEventsDataOlderThanOneYear() {
+    private function purgeSystemEventsDataOlderThanOneYear()
+    {
         $dao                 = new SystemEventDao();
         $system_event_purger = new SystemEventPurger($dao);
 
         return $system_event_purger->purgeSystemEventsDataOlderThanOneYear();
+    }
+
+    private function runComputeAllDailyStats(Logger $logger, array &$warnings)
+    {
+        $process = new Process([__DIR__.'/../../../utils/compute_all_daily_stats.sh']);
+        $this->runCommand($process, $logger, $warnings);
+    }
+
+    /**
+     * run the weekly stats for projects. Run it on Monday morning so that
+     * it computes the stats for the week before
+     */
+    private function runWeeklyStats(Logger $logger, array &$warnings)
+    {
+        $now = new DateTimeImmutable();
+        if ($now->format('l') === self::DAY_OF_WEEKLY_STATS) {
+            $process = new Process(['./db_project_weekly_metric.pl'], __DIR__.'/../../../utils/underworld-root');
+            $this->runCommand($process, $logger, $warnings);
+        }
+    }
+
+    private function runCommand(Process $process, Logger $logger, array &$warnings)
+    {
+        $process->run();
+        if (! $process->isSuccessful()) {
+            $warnings[] = $process->getCommandLine().' ran with errors, check '.ForgeConfig::get('codendi_log');
+            $logger->error(sprintf("%s %s errors. Stdout:\n%s\nStderr:\n%s", self::class, $process->getCommandLine(), $process->getOutput(), $process->getErrorOutput()));
+        } else {
+            $logger->debug(sprintf("%s %s Stdout:\n%s\nStderr:\n%s", self::class, $process->getCommandLine(), $process->getOutput(), $process->getErrorOutput()));
+        }
     }
 }

plugins/forumml/include/forummlPlugin.class.php
@@ -23,6 +23,7 @@ use FastRoute\RouteCollector;
 use Tuleap\Request\CollectRoutesEvent;
 use Tuleap\Request\DispatchableWithRequest;
 use Tuleap\ForumML;
+use Tuleap\SystemEvent\RootDailyStartEvent;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -35,6 +36,7 @@ class ForumMLPlugin extends Plugin {
         $this->addHook('browse_archives','forumml_browse_archives');
         $this->addHook('cssfile','cssFile');
         $this->addHook('javascript_file', 'jsFile');
+        $this->addHook(RootDailyStartEvent::NAME);
 
         // Search
         $this->addHook(Event::SEARCH_TYPE);
@@ -234,5 +236,16 @@ class ForumMLPlugin extends Plugin {
             $r->get('/index.php', $this->getRouteHandler('routeWriteMail'));
             $r->post('/index.php', $this->getRouteHandler('routeSendMail'));
         });
+    }
+
+    public function rootDailyStart(RootDailyStartEvent $event)
+    {
+        try {
+            $tmp_watch = new \Tuleap\TmpWatch((string) $this->getPluginInfo()->getPropertyValueForName('forumml_tmp'), 24);
+            $tmp_watch->run();
+        } catch (Exception $exception) {
+            $event->getLogger()->error('ForumML root_daily_start '.get_class($exception).': '.$exception->getMessage(), $exception);
+            $event->addWarning($exception->getMessage());
+        }
     }
 }

plugins/ldap/include/ldapPlugin.class.php
@@ -42,6 +42,7 @@ use Tuleap\Project\Admin\ProjectUGroup\UGroupEditProcessAction;
 use Tuleap\Request\CollectRoutesEvent;
 use Tuleap\Request\DispatchableWithRequest;
 use Tuleap\svn\Event\GetSVNLoginNameEvent;
+use Tuleap\SystemEvent\RootDailyStartEvent;
 use Tuleap\User\Admin\UserDetailsPresenter;
 use Tuleap\Project\UserRemover;
 use Tuleap\Project\UserRemoverDao;
@@ -139,6 +140,7 @@ class LdapPlugin extends Plugin {
 
         // Daily codendi job
         $this->addHook('codendi_daily_start', 'codendi_daily_start', false);
+        $this->addHook(RootDailyStartEvent::NAME);
 
         // SystemEvent
         $this->addHook(Event::SYSTEM_EVENT_GET_TYPES_FOR_DEFAULT_QUEUE);
@@ -156,7 +158,6 @@ class LdapPlugin extends Plugin {
         }
 
         $this->addHook(UserDetailsPresenter::ADDITIONAL_DETAILS);
-        $this->addHook('root_daily_start');
         $this->addHook('ugroup_duplication');
         $this->addHook(BindingAdditionalModalPresenterCollection::NAME);
         $this->addHook(UGroupEditProcessAction::NAME);
@@ -968,13 +969,14 @@ class LdapPlugin extends Plugin {
         $ldapUserGroupManager->synchronizeUgroups();
     }
 
-    public function root_daily_start($params) {
+    public function rootDailyStart(RootDailyStartEvent $event)
+    {
         if ($this->isLdapAuthType()) {
             $retriever       = new NonUniqueUidRetriever(new LDAP_UserDao());
             $non_unique_uids = $retriever->getNonUniqueLdapUid();
             if ($non_unique_uids) {
-                $params['warnings'][] = 'The following ldap_uids are non unique: ' . implode(', ', $non_unique_uids)
-                                      . PHP_EOL .' This might lead to some SVN misbehaviours for concerned users';
+                $event->addWarning('The following ldap_uids are non unique: ' . implode(', ', $non_unique_uids)
+                                      . PHP_EOL .' This might lead to some SVN misbehaviours for concerned users');
             }
         }
     }

plugins/statistics/include/statisticsPlugin.class.php
@@ -37,6 +37,7 @@ use Tuleap\SVN\DiskUsage\Retriever as SVNRetriever;
 use Tuleap\CVS\DiskUsage\Retriever as CVSRetriever;
 use Tuleap\CVS\DiskUsage\Collector as CVSCollector;
 use Tuleap\CVS\DiskUsage\FullHistoryDao;
+use Tuleap\SystemEvent\RootDailyStartEvent;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once 'constants.php';
@@ -49,7 +50,7 @@ class StatisticsPlugin extends Plugin
         parent::__construct($id);
         $this->addHook('cssfile',                  'cssFile',                false);
         $this->addHook('site_admin_option_hook',   'site_admin_option_hook', false);
-        $this->addHook('root_daily_start',         'root_daily_start',       false);
+        $this->addHook(RootDailyStartEvent::NAME);
         $this->addHook(\Tuleap\Widget\Event\GetWidget::NAME);
         $this->addHook(\Tuleap\Widget\Event\GetProjectWidgetList::NAME);
         $this->addHook('usergroup_data',           'usergroup_data',         false);
@@ -153,10 +154,8 @@ class StatisticsPlugin extends Plugin
         );
     }
 
-    /**
-     * @see root_daily_start
-     */
-    public function root_daily_start($params) {
+    public function rootDailyStart(RootDailyStartEvent $event)
+    {
         SystemEventManager::instance()->createEvent(
             SystemEvent_STATISTICS_DAILY::NAME,
             null,

src/common/CLI/Command/DailyJobCommand.php
@@ -1,0 +1,74 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\CLI\Command;
+
+use EventManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use UserManager;
+
+class DailyJobCommand extends Command
+{
+    public const NAME = 'daily-job';
+
+    /**
+     * @var EventManager
+     */
+    private $event_manager;
+    /**
+     * @var UserManager
+     */
+    private $user_manager;
+
+    public function __construct(EventManager $event_manager, UserManager $user_manager)
+    {
+        parent::__construct(self::NAME);
+        $this->event_manager = $event_manager;
+        $this->user_manager  = $user_manager;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Execute time consuming, low priority housekeeping jobs that should run once a day');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (getenv('TLP_DELAY_CRON_CMD') === '1') {
+            try {
+                sleep(random_int(0, 1799));
+            } catch (\Exception $e) {
+                $error_output = $output instanceof ConsoleOutputInterface ? $output->getErrorOutput() : $output;
+                $error_output->writeln('Unable to get a random time for delay, aborting');
+                return 1;
+            }
+        }
+
+        $this->event_manager->processEvent("codendi_daily_start");
+
+        $this->user_manager->checkUserAccountValidity();
+    }
+}

src/common/frs/FRSIncomingDirectoryCleaner.php
@@ -1,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\FRS;
+
+use Tuleap\TmpWatch;
+
+/**
+ * Delete all files in FTP incoming that are older than 2 weeks
+ */
+class FRSIncomingDirectoryCleaner
+{
+    private const TWO_WEEKS_IN_HOURS = 2*7*24;
+
+    public function run() : void
+    {
+        $tmp_watch = new TmpWatch(\ForgeConfig::get('ftp_incoming_dir'), self::TWO_WEEKS_IN_HOURS);
+        $tmp_watch->run();
+    }
+}

src/common/include/InvalidDirectoryException.php
@@ -1,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap;
+
+class InvalidDirectoryException extends \RuntimeException
+{
+
+}

src/common/include/TmpWatch.php
@@ -1,0 +1,64 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap;
+
+class TmpWatch
+{
+    /**
+     * @var string
+     */
+    private $target_directory;
+    /**
+     * @var int
+     */
+    private $nb_hours;
+
+    public function __construct(string $target_directory, int $nb_hours)
+    {
+        $this->target_directory = $target_directory;
+        $this->nb_hours = $nb_hours;
+    }
+
+    /**
+     * @throws InvalidDirectoryException
+     * @throws \Exception
+     */
+    public function run(): void
+    {
+        if (! is_dir($this->target_directory)) {
+            throw new InvalidDirectoryException('Cannot delete content of invalid directory: `' .$this->target_directory. '`');
+        }
+        $now = new \DateTimeImmutable();
+        $some_hours_ago = $now->sub(new \DateInterval(sprintf('PT%dH', $this->nb_hours)));
+        foreach (new \DirectoryIterator($this->target_directory) as $file) {
+            assert($file instanceof \DirectoryIterator);
+            if ($file->isDir()) {
+                continue;
+            }
+            if ($file->getMTime() <= $some_hours_ago->getTimestamp()) {
+                unlink($file->getPathname());
+            }
+        }
+    }
+}

commit 40b1b5b3a25b6170301c20b8dbe2935ff2e9ae8c

request #13849: Processes launched by the ROOT_DAILY system event should not timeout after 60 seconds
Those processes can take quite a long time on large Tuleap instances.
The 60 seconds limit is a sane default value from the symfony/process
component but in this specific case we need to disable it. On large
instances with weak hardware those processes can take a few hours.

Issue has been introduced by fbb466efe4a71d5bcb205cfbf919f7aa5e08c3d5.

Change-Id: I622f931752ae04a67e998f485a9f4c89888879fd

src/common/system_event/include/SystemEvent_ROOT_DAILY.class.php
@@ -133,8 +133,9 @@ class SystemEvent_ROOT_DAILY extends SystemEvent // phpcs:ignore
         }
     }
 
-    private function runCommand(Process $process, Logger $logger, array &$warnings)
+    private function runCommand(Process $process, Logger $logger, array &$warnings) : void
     {
+        $process->setTimeout(null);
         $process->run();
         if (! $process->isSuccessful()) {
             $warnings[] = $process->getCommandLine().' ran with errors, check '.ForgeConfig::get('codendi_log');