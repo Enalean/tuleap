a7908506e3f4b1e03556f5a60469eb402a610499
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2019-09-10 11:48:15 +0200

    Add REST read only tests for user_memberships/
    This is part story #13669 permission delegation for a read-only site admin (REST)
    
    Change-Id: Ia82600aaadfb2639e447262087a1fe8e3785178f

--- tests/rest/tests/UserMembershipTest.php
+++ tests/rest/tests/UserMembershipTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2015 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2015 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,10 +19,12 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+use Guzzle\Http\Message\Response;
+
 /**
  * @group UserMembershipTests
  */
-class UserMembershipsTest extends RestBase
+final class UserMembershipsTest extends RestBase //phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
 {
 
     protected function getResponse($request, $user_name = REST_TestDataBuilder::TEST_USER_1_NAME)
@@ -30,30 +32,64 @@ class UserMembershipsTest extends RestBase
         return parent::getResponse($request, $user_name);
     }
 
-    public function testGET()
+    public function testGET(): void
     {
         $response = $this->getResponse(
             $this->client->get('users_memberships/?query=with_ssh_key&limit=3&offset=0'),
             REST_TestDataBuilder::ADMIN_USER_NAME
         );
 
+        $this->assertGET($response);
+    }
+
+    public function testGETWithRESTReadOnlyUser(): void
+    {
+        $response = $this->getResponse(
+            $this->client->get('users_memberships/?query=with_ssh_key&limit=3&offset=0'),
+            REST_TestDataBuilder::TEST_BOT_USER_NAME
+        );
+
+        $this->assertGET($response);
+    }
+
+    private function assertGET(Response $response): void
+    {
         $user2_groups = array(
             "site_active",
             "private-member_project_members",
             "ug_102"
         );
 
+        $this->assertEquals(200, $response->getStatusCode());
+
         $response_json = $response->json();
         $this->assertCount(1, $response_json);
-        $this->assertEquals($response_json[0]["user_groups"], $user2_groups);
-        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertEquals($user2_groups, $response_json[0]["user_groups"]);
     }
 
-    public function testOptions()
+    public function testOptions(): void
     {
-        $response = $this->getResponse($this->client->options('users_memberships'), REST_TestDataBuilder::ADMIN_USER_NAME);
+        $response = $this->getResponse(
+            $this->client->options('users_memberships'),
+            REST_TestDataBuilder::ADMIN_USER_NAME
+        );
 
-        $this->assertEquals(array('OPTIONS', 'GET'), $response->getHeader('Allow')->normalize()->toArray());
-        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertOPTIONS($response);
+    }
+
+    public function testOptionsWithRESTReadOnlyUser(): void
+    {
+        $response = $this->getResponse(
+            $this->client->options('users_memberships'),
+            REST_TestDataBuilder::TEST_BOT_USER_NAME
+        );
+
+        $this->assertOPTIONS($response);
+    }
+
+    private function assertOPTIONS(Response $response): void
+    {
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(['OPTIONS', 'GET'], $response->getHeader('Allow')->normalize()->toArray());
     }
 }
