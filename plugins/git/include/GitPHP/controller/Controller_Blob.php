Enalean/tuleap

commit adbfdcffa4bd34426af238ea86bdb87b4b98f5ce

Remove legacy Git view
part of request #13554 remove old gitphp view

How to test:
 - navigate in old view no error are displayed, git should work as usual
 - forgeConfig option is no longer taken in account

This patch only deal with template that have been migrated to burning parrot
There is still plenty old template under /tmplate/gitphp
I will check their usage and remove them in a dedicted commit

Change-Id: I10e9277687ef6fc31158ae59177b0fc984f2a557

plugins/git/include/GitPHP/controller/Controller_Blob.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - present. All Rights Reserved.
  * Copyright (C) 2010 Christopher Han <xiphux@gmail.com>
  *
  * This file is a part of Tuleap.
@@ -22,7 +22,6 @@
 namespace Tuleap\Git\GitPHP;
 
 use EventManager;
-use GeSHi;
 use Tuleap\Git\BinaryDetector;
 use Tuleap\Git\Repository\View\LanguageDetectorForPrismJS;
 use Tuleap\Git\GitPHP\Events\DisplayFileContentInGitView;
@@ -34,8 +33,6 @@ use Tuleap\Layout\IncludeAssets;
  */
 class Controller_Blob extends ControllerBase // @codingStandardsIgnoreLine
 {
-    use \Tuleap\Git\Repository\View\FeatureFlag;
-
     /**
      * __construct
      *
@@ -62,13 +59,7 @@ class Controller_Blob extends ControllerBase // @codingStandardsIgnoreLine
      */
     protected function GetTemplate() // @codingStandardsIgnoreLine
     {
-        if (isset($this->params['plain']) && $this->params['plain']) {
-            return 'blobplain.tpl';
-        }
-        if ($this->isTuleapBeauGitActivated()) {
-            return 'tuleap/blob.tpl';
-        }
-        return 'blob.tpl';
+        return 'tuleap/blob.tpl';
     }
 
     /**
@@ -233,28 +224,12 @@ class Controller_Blob extends ControllerBase // @codingStandardsIgnoreLine
 
         $this->tpl->assign('extrascripts', array('blame'));
 
-        if ($this->isTuleapBeauGitActivated()) {
-            $detector = new LanguageDetectorForPrismJS();
-            $this->tpl->assign('language', $detector->getLanguageFromExtension(substr(strrchr($blob->GetName(), '.'), 1)));
-            $this->tpl->assign('bloblines', $blob->GetData(true));
-            $include_assets = new IncludeAssets(__DIR__ . '/../../../www/assets', GIT_BASE_URL . '/assets');
-            $GLOBALS['Response']->includeFooterJavascriptFile(
-                $include_assets->getFileURL('repository-blob.js')
-            );
-            return;
-        }
-
-        $geshi = new GeSHi("", 'php');
-        $lang = $geshi->get_language_name_from_extension(substr(strrchr($blob->GetName(), '.'), 1));
-        $geshi->enable_classes();
-        $geshi->enable_strict_mode(GESHI_MAYBE);
-        $geshi->set_source($blob->GetData());
-        $geshi->set_language($lang);
-        $geshi->set_header_type(GESHI_HEADER_PRE_TABLE);
-        $geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
-        $geshi->set_overall_id('git-repository-blob-file');
-        $this->tpl->assign('geshiout', $geshi->parse_code());
-        $this->tpl->assign('extracss', $geshi->get_stylesheet());
-        $this->tpl->assign('geshi', true);
+        $detector = new LanguageDetectorForPrismJS();
+        $this->tpl->assign('language', $detector->getLanguageFromExtension(substr(strrchr($blob->GetName(), '.'), 1)));
+        $this->tpl->assign('bloblines', $blob->GetData(true));
+        $include_assets = new IncludeAssets(__DIR__ . '/../../../www/assets', GIT_BASE_URL . '/assets');
+        $GLOBALS['Response']->includeFooterJavascriptFile(
+            $include_assets->getFileURL('repository-blob.js')
+        );
     }
 }

plugins/git/include/GitPHP/controller/Controller_Commitdiff.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - present. All Rights Reserved.
  * Copyright (c) 2010 Christopher Han <xiphux@gmail.com>
  *
  * This file is a part of Tuleap.
@@ -29,8 +29,6 @@ use UserManager;
 
 class Controller_Commitdiff extends Controller_DiffBase // @codingStandardsIgnoreLine
 {
-    use \Tuleap\Git\Repository\View\FeatureFlag;
-
     /**
      * __construct
      *
@@ -57,17 +55,11 @@ class Controller_Commitdiff extends Controller_DiffBase // @codingStandardsIgnor
      */
     protected function GetTemplate() // @codingStandardsIgnoreLine
     {
-        if (isset($this->params['plain']) && ($this->params['plain'] === true)) {
-            return 'commitdiffplain.tpl';
-        }
-        if ($this->isTuleapBeauGitActivated() && ! isset($this->params['diff-mode'])) {
+        if (! isset($this->params['diff-mode'])) {
             return 'tuleap/commit-diff.tpl';
         }
-        if ($this->isTuleapBeauGitActivated() && isset($this->params['diff-mode'])) {
-            return 'tuleap/commit-diff-side-by-side.tpl';
-        }
 
-        return 'commitdiff.tpl';
+        return 'tuleap/commit-diff-side-by-side.tpl';
     }
 
     /**
@@ -152,18 +144,16 @@ class Controller_Commitdiff extends Controller_DiffBase // @codingStandardsIgnor
             $this->params['hash'],
             (isset($this->params['hashparent']) ? $this->params['hashparent'] : '')
         );
-        if ($this->isTuleapBeauGitActivated()) {
-            $commit_metadata_retriever = new CommitMetadataRetriever(
-                new CommitStatusRetriever(new CommitStatusDAO()),
-                UserManager::instance()
-            );
-            $commit_metadata = $commit_metadata_retriever->getMetadataByRepositoryAndCommits(
-                $this->getTuleapGitRepository(),
-                $commit
-            );
-            $commit_presenter = new CommitPresenter($commit, $commit_metadata[0], $treediff);
-            $this->tpl->assign('commit_presenter', $commit_presenter);
-        }
+        $commit_metadata_retriever = new CommitMetadataRetriever(
+            new CommitStatusRetriever(new CommitStatusDAO()),
+            UserManager::instance()
+        );
+        $commit_metadata = $commit_metadata_retriever->getMetadataByRepositoryAndCommits(
+            $this->getTuleapGitRepository(),
+            $commit
+        );
+        $commit_presenter = new CommitPresenter($commit, $commit_metadata[0], $treediff);
+        $this->tpl->assign('commit_presenter', $commit_presenter);
         $this->tpl->assign('treediff', $treediff);
     }
 }

plugins/git/templates/gitphp/blobplain.tpl
@@ -1,8 +1,0 @@
-{*
- *  blobplain.tpl
- *  gitphp: A PHP git repository browser
- *  Component: Blob plain template
- *
- *  Copyright (C) 2009 Christopher Han <xiphux@gmail.com>
- *}
-{$blob->GetData()}

plugins/git/templates/gitphp/commitdiffplain.tpl
@@ -1,25 +1,0 @@
-{*
- *  commitdiffplain.tpl
- *  gitphp: A PHP git repository browser
- *  Component: Plaintext diff template
- *
- *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
- *}
-From: {$commit->GetAuthor()}
-Date: {$commit->GetAuthorEpoch()|date_format:"%a, %d %b %Y %H:%M:%S %z"}
-Subject: {$commit->GetTitle()}
-{assign var=tag value=$commit->GetContainingTag()}
-{if $tag}
-X-Git-Tag: {$tag->GetName()}
-{/if}
-X-Git-Url: {scripturl}?a=commitdiff&amp;h={$commit->GetHash()|urlencode}
----
-{foreach from=$commit->GetComment() item=line}
-{$line}
-{/foreach}
----
-
-
-{foreach from=$treediff item=filediff}
-{$filediff->GetDiff()}
-{/foreach}

plugins/git/include/Git/Repository/GitRepositoryHeaderDisplayer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -33,8 +33,6 @@ use Tuleap\Layout\IncludeAssets;
 
 class GitRepositoryHeaderDisplayer
 {
-    use \Tuleap\Git\Repository\View\FeatureFlag;
-
     /**
      * @var HeaderRenderer
      */
@@ -80,7 +78,7 @@ class GitRepositoryHeaderDisplayer
             __DIR__ . '/../../../www/themes/BurningParrot/assets',
             GIT_BASE_URL . '/themes/BurningParrot/assets'
         );
-        if ($this->isTuleapBeauGitActivated() && in_array($request->get('a'), ['blob', 'blame'], true)) {
+        if (in_array($request->get('a'), ['blob', 'blame'], true)) {
             $layout->addCssAsset(new CssAssetWithoutVariantDeclinaisons($include_assets, 'syntax-highlight'));
         }
         $layout->addCssAsset(new CssAsset($include_assets, 'git'));

plugins/git/include/Git/Repository/View/FeatureFlag.php
@@ -1,44 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-namespace Tuleap\Git\Repository\View;
-
-use ForgeConfig;
-use HTTPRequest;
-
-trait FeatureFlag
-{
-    private function isTuleapBeauGitActivated()
-    {
-        $whitelist = ForgeConfig::get('sys_project_whitelist_that_should_use_deprecated_git_interface');
-        if (empty($whitelist)) {
-            return true;
-        }
-
-        $current_project_id = (int) HTTPRequest::instance()->getProject()->getID();
-        foreach (explode(',', $whitelist) as $whitelisted_project_id) {
-            if ($current_project_id === (int) $whitelisted_project_id) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}

plugins/git/include/Git/Repository/View/FilesHeaderPresenterBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -30,8 +30,6 @@ use Tuleap\Git\Repository\GitPHPProjectRetriever;
 
 class FilesHeaderPresenterBuilder
 {
-    use \Tuleap\Git\Repository\View\FeatureFlag;
-
     /**
      * @var CommitForCurrentTreeRetriever
      */
@@ -76,9 +74,6 @@ class FilesHeaderPresenterBuilder
             '',
             []
         );
-        if (! $this->isTuleapBeauGitActivated()) {
-            return $cannot_be_displayed_presenter;
-        }
 
         if (! $repository->isCreated()) {
             return $cannot_be_displayed_presenter;

plugins/git/include/Git/Repository/View/RepositoryHeaderPresenterBuilder.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018 - 2019. All Rights Reserved.
+ * Copyright (c) Enalean, 2018 - present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -33,8 +33,6 @@ use PFUser;
 
 class RepositoryHeaderPresenterBuilder
 {
-    use \Tuleap\Git\Repository\View\FeatureFlag;
-
     public const TAB_FILES   = 'tab-files';
     public const TAB_COMMITS = 'tab-commits';
 
@@ -191,10 +189,7 @@ class RepositoryHeaderPresenterBuilder
 
     private function buildTabsPresenter(GitRepository $repository)
     {
-        $tabs = [$this->getFilesTab($repository)];
-        if ($this->isTuleapBeauGitActivated()) {
-            $tabs[] = $this->getCommitsTab($repository);
-        }
+        $tabs = [$this->getFilesTab($repository), $this->getCommitsTab($repository)];
 
         $external_tabs = $this->getExternalsTabs($repository);
         if (count($external_tabs) > 0) {

commit 24291cc29434317c8fca3d0eb4183ad3d3193a61

request #13955: Download of a file or a patch is polluted by some HTML content
Issue was introduced by adbfdcffa4bd34426af238ea86bdb87b4b98f5ce.

Change-Id: Ib23274723401d23a5f4aca9f5497c8f75ffb5caa

plugins/git/include/GitPHP/controller/Controller_Blob.php
@@ -51,6 +51,9 @@ class Controller_Blob extends ControllerBase // @codingStandardsIgnoreLine
      */
     protected function GetTemplate() // @codingStandardsIgnoreLine
     {
+        if (isset($this->params['plain']) && $this->params['plain']) {
+            return 'blobplain.tpl';
+        }
         return 'tuleap/blob.tpl';
     }
 

plugins/git/include/GitPHP/controller/Controller_Commitdiff.php
@@ -47,6 +47,9 @@ class Controller_Commitdiff extends Controller_DiffBase // @codingStandardsIgnor
      */
     protected function GetTemplate() // @codingStandardsIgnoreLine
     {
+        if (isset($this->params['plain']) && ($this->params['plain'] === true)) {
+            return 'commitdiffplain.tpl';
+        }
         if (! isset($this->params['diff-mode'])) {
             return 'tuleap/commit-diff.tpl';
         }

plugins/git/templates/gitphp/blobplain.tpl
@@ -1,0 +1,8 @@
+{*
+ *  blobplain.tpl
+ *  gitphp: A PHP git repository browser
+ *  Component: Blob plain template
+ *
+ *  Copyright (C) 2009 Christopher Han <xiphux@gmail.com>
+ *}
+{$blob->GetData()}

plugins/git/templates/gitphp/commitdiffplain.tpl
@@ -1,0 +1,36 @@
+{*
+ * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *}
+From: {$commit->GetAuthor()}
+Date: {$commit->GetAuthorEpoch()|date_format:"%a, %d %b %Y %H:%M:%S %z"}
+Subject: {$commit->GetTitle()}
+{assign var=tag value=$commit->GetContainingTag()}
+{if $tag}
+X-Git-Tag: {$tag->GetName()}
+{/if}
+---
+{foreach from=$commit->GetComment() item=line}
+{$line}
+{/foreach}
+---
+
+
+{foreach from=$treediff item=filediff}
+{$filediff->GetDiff()}
+{/foreach}