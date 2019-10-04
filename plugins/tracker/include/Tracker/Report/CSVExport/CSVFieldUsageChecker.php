4dad9978110fe9171f3ec94c679163f978a551df
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2019-10-04 13:48:05 +0200

    request #13961 PerTrackerId field should be exported in csv
    Export a tracker report with a tracker_per_id field
    => it should be exported
    
    Change-Id: I763a83886057a710dd63067ee13ba3bd245b8942

---
+++ plugins/tracker/include/Tracker/Report/CSVExport/CSVFieldUsageChecker.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Tuleap\Tracker\Report\CSVExport;
+
+use Tracker_FormElement_Field;
+use Tracker_FormElement_Field_ArtifactId;
+use Tracker_FormElement_Field_Burndown;
+use Tracker_FormElement_Field_PerTrackerArtifactId;
+
+class CSVFieldUsageChecker
+{
+    public static function canFieldBeExportedToCSV(Tracker_FormElement_Field $field): bool
+    {
+        return $field->isUsed()
+            && $field->userCanRead()
+            && ! (
+                ($field instanceof Tracker_FormElement_Field_ArtifactId && ! $field instanceof Tracker_FormElement_Field_PerTrackerArtifactId)
+                || $field instanceof Tracker_FormElement_Field_Burndown
+            );
+    }
+}
---
+++ plugins/tracker/phpunit/Tracker/Report/CSVExport/CSVFieldUsageCheckerTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Tuleap\Tracker\Report\CSVExport;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class CSVFieldUsageCheckerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    public function testUnusedFieldsAreNotExportedInCSV(): void
+    {
+        $field = \Mockery::mock(\Tracker_FormElement_Field_Integer::class);
+        $field->shouldReceive('isUsed')->andReturnFalse();
+        $this->assertFalse(CSVFieldUsageChecker::canFieldBeExportedToCSV($field));
+    }
+
+    public function testUserCantExportFieldHeCanNotReadInCSV(): void
+    {
+        $field = \Mockery::mock(\Tracker_FormElement_Field_Integer::class);
+        $field->shouldReceive('isUsed')->andReturnTrue();
+        $field->shouldReceive('userCanRead')->andReturnFalse();
+        $this->assertFalse(CSVFieldUsageChecker::canFieldBeExportedToCSV($field));
+    }
+
+
+    public function testBurndownFieldIsNotExportedInCSV(): void
+    {
+        $field = \Mockery::mock(\Tracker_FormElement_Field_Burndown::class);
+        $field->shouldReceive('isUsed')->andReturnTrue();
+        $field->shouldReceive('userCanRead')->andReturnTrue();
+        $this->assertFalse(CSVFieldUsageChecker::canFieldBeExportedToCSV($field));
+    }
+
+    public function testArtifactIdIsNotExportedInCSV(): void
+    {
+        $field = \Mockery::mock(\Tracker_FormElement_Field_ArtifactId::class);
+        $field->shouldReceive('isUsed')->andReturnTrue();
+        $field->shouldReceive('userCanRead')->andReturnTrue();
+        $this->assertFalse(CSVFieldUsageChecker::canFieldBeExportedToCSV($field));
+    }
+
+    public function testPerTrackerIdFieldIsExportedInCSV(): void
+    {
+        $field = \Mockery::mock(\Tracker_FormElement_Field_PerTrackerArtifactId::class);
+        $field->shouldReceive('isUsed')->andReturnTrue();
+        $field->shouldReceive('userCanRead')->andReturnTrue();
+        $this->assertTrue(CSVFieldUsageChecker::canFieldBeExportedToCSV($field));
+    }
+}
