d37ad0660dcaf9e4c02155552d35a39cea343866
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2019-10-08 15:22:04 +0200

    Computed default value is used in artifact creation through REST
    This is part of story #13569 define a default value for computed fields
    
    When creating an artifact with REST endoint POST artifacts/
    If the computed field has a default value and it's not provided in the body of
    the request, then the default value is used.
    
    The same behaviour must occurs if the user does not have the right to
    submit this computed field.
    
    Change-Id: I6cee1d0a75d2bc3a81af9ea356b9827acd6cd0b0

--- plugins/tracker/tests/rest/ComputedFieldsDefaultValue/ComputedFieldsDefaultValueTest.php
+++ plugins/tracker/tests/rest/ComputedFieldsDefaultValue/ComputedFieldsDefaultValueTest.php
@@ -20,6 +20,7 @@
 
 namespace Tuleap\Tracker\Tests\REST\ComputedFieldsDefaultValue;
 
+use REST_TestDataBuilder;
 use Tuleap\Tracker\Tests\REST\TrackerBase;
 
 require_once __DIR__ .'/../TrackerBase.php';
@@ -39,6 +40,57 @@ class ComputedFieldsDefaultValueTest extends TrackerBase
 
                 $this->assertSame('manual_value', $field['default_value']['type']);
                 $this->assertSame(5.2, $field['default_value']['value']);
+            }
+        }
+
+        if (! $computed_field_found) {
+            $this->fail('Computed field not found to check default value.');
+        }
+    }
+
+    public function testProjectMembersCanCreateAnArtifactAndComputedDefaultValueIsCorrectlySet()
+    {
+        $string_field_id = null;
+        $tracker_representation = $this->tracker_representations[$this->computed_value_tracker_id];
+        foreach ($tracker_representation['fields'] as $field) {
+            if ($field['type'] === 'string') {
+                $string_field_id = $field['field_id'];
+            }
+        }
+
+        $payload = [
+            'tracker' => ['id' => $this->computed_value_tracker_id],
+            'values'  => [
+                [
+                    'field_id' => $string_field_id,
+                    'value' => 'Title 01'
+                ]
+            ]
+        ];
+
+        $response = $this->getResponse(
+            $this->client->post('artifacts', null, json_encode($payload)),
+            REST_TestDataBuilder::TEST_USER_3_NAME
+        );
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        $created_artifact     = $response->json();
+        $created_artifact_uri = $created_artifact['uri'];
+
+        $get_response = $this->getResponse(
+            $this->client->get($created_artifact_uri)
+        );
+
+        $computed_field_found = false;
+        $artifact = $get_response->json();
+        foreach ($artifact['values'] as $field_value) {
+            if ($field_value['type'] === 'computed') {
+                $computed_field_found = true;
+
+                $this->assertFalse($field_value['is_autocomputed']);
+                $this->assertSame(5.2, $field_value['manual_value']);
+                $this->assertNull($field_value['value']);
             }
         }
 
