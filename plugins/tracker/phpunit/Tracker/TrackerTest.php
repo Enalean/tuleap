583b224aed38eceb3433702f66a9a705c021aac9
lorentzr <romain.lorentz@enalean.com>
2019-10-18 15:09:25 +0200

    request #14059 Wrong error on CSV preview import
    When a none value on tracker dependencies, an error is log on preview when is not not suppose to be.
    
    How to test :
     	- Without this patch, lunch import preview with an artifact without value when the dependencie is authorized.
    		--> You should receive an error feedback
    		--> when you import the artifact, that work without error feedback
    	- with the patch, the feedback is not logged anymore
    
    Change-Id: I03a6519e047563b3beb0dc12ea0fa6b52a15779e

--- plugins/tracker/phpunit/Tracker/TrackerTest.php
+++ plugins/tracker/phpunit/Tracker/TrackerTest.php
@@ -74,11 +74,11 @@ class TrackerTest extends TestCase
         $this->tracker->shouldReceive('getTrackerArtifactFactory')->andReturns($tracker_artifact_factory);
         $this->tracker->shouldReceive('aidExists')->with('0')->andReturns(false);
 
-        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 1', $artifact)->andReturns('summary 1');
-        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 2', $artifact)->andReturns('summary 2');
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 1', $artifact)->andReturns('summary 1')->once();
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 2', $artifact)->andReturns('summary 2')->once();
 
-        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 1', $artifact)->andReturns('details 1');
-        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 2', $artifact)->andReturns('details 2');
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 1', $artifact)->andReturns('details 1')->once();
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 2', $artifact)->andReturns('details 2')->once();
 
         $field1->shouldReceive('isCSVImportable')->andReturns(true);
         $field2->shouldReceive('isCSVImportable')->andReturns(true);
@@ -123,11 +123,60 @@ class TrackerTest extends TestCase
         $this->tracker->shouldReceive('getTrackerArtifactFactory')->andReturns($tracker_artifact_factory);
         $this->tracker->shouldReceive('aidExists')->with('0')->andReturns(false);
 
-        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 1', $artifact)->andReturns('summary 1');
-        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 2', $artifact)->andReturns('summary 2');
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 1', $artifact)->andReturns('summary 1')->once();
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 2', $artifact)->andReturns('summary 2')->once();
 
-        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 1', $artifact)->andReturns('details 1');
-        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 2', $artifact)->andReturns('details 2');
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 1', $artifact)->andReturns('details 1')->once();
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 2', $artifact)->andReturns('details 2')->once();
+
+        $field1->shouldReceive('isCSVImportable')->andReturns(true);
+        $field2->shouldReceive('isCSVImportable')->andReturns(true);
+
+        $this->formelement_factory->shouldReceive('getUsedFieldByName')->with(110, 'summary')->andReturns($field1);
+        $this->formelement_factory->shouldReceive('getUsedFieldByName')->with(110, 'details')->andReturns($field2);
+        $this->tracker->shouldReceive('getWorkflow')->andReturns($this->workflow_factory);
+
+        $user_manager = \Mockery::mock(\UserManager::class);
+        $user = \Mockery::mock(\PFUser::class);
+        $user->shouldReceive('getId')->andReturns('107');
+        $this->tracker->shouldReceive('getUserManager')->andReturns($user_manager);
+        $user_manager->shouldReceive('getCurrentUser')->andReturns($user);
+
+        $tracker_artifact_factory->shouldReceive('getInstanceFromRow')->andReturns($artifact);
+
+        $this->workflow_factory->shouldReceive('getGlobalRulesManager')->andReturns(\Mockery::spy(\Tracker_RulesManager::class));
+
+        $GLOBALS['Response']->shouldNotReceive('addFeedback')->with('error', Mockery::any(), Mockery::any());
+        $this->assertFalse($this->tracker->hasBlockingError($header, $lines));
+    }
+
+    public function testHasBlockingErrorReturnNoErrorWhenEmptyValue() : void
+    {
+        $header = ['summary', 'details'];
+        $lines = [
+            ['summary 1', 'details 1'],
+            ['summary 2', ''],
+        ];
+        $field1 = \Mockery::spy(\Tracker_FormElement_Field_String::class);
+        $field2 = \Mockery::spy(\Tracker_FormElement_Field_String::class);
+        $this->formelement_factory->shouldReceive("getUsedFields")->andReturns(array($field1, $field2));
+
+        $field1->shouldReceive('validateFieldWithPermissionsAndRequiredStatus')->andReturns(true);
+        $field2->shouldReceive('validateFieldWithPermissionsAndRequiredStatus')->andReturns(true);
+
+        $field1->shouldReceive('getId')->andReturns(1);
+        $field2->shouldReceive('getId')->andReturns(2);
+
+        $artifact = \Mockery::spy(\Tracker_Artifact::class);
+        $tracker_artifact_factory = \Mockery::mock(\Tracker_ArtifactFactory::class);
+        $this->tracker->shouldReceive('getTrackerArtifactFactory')->andReturns($tracker_artifact_factory);
+        $this->tracker->shouldReceive('aidExists')->with('0')->andReturns(false);
+
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 1', $artifact)->andReturns('summary 1')->once();
+        $field1->shouldReceive('getFieldDataFromCSVValue')->with('summary 2', $artifact)->andReturns('summary 2')->once();
+
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('details 1', $artifact)->andReturns('details 1')->once();
+        $field2->shouldReceive('getFieldDataFromCSVValue')->with('', $artifact)->andReturns(100)->once();
 
         $field1->shouldReceive('isCSVImportable')->andReturns(true);
         $field2->shouldReceive('isCSVImportable')->andReturns(true);
