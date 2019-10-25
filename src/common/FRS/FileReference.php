0bab17558edba5fb41fb041cee215626ebd4b9c5
Manuel VACELET <manuel.vacelet@enalean.com>
2019-10-25 15:20:43 +0200

    Remove entirely the old "confirm_download" stuff
    That was still use to force people to read the license agreement when
    the file was referenced with a 'file' keyword.
    
    The fix is not iso-func as it would have needed to deal with the modal
    in Flaming Parrot and Burning Parrot on each and every page of the
    app. That's not worth it for this kind of feature so, for the ones who
    are relying on that, they are redirected on the parent release of the
    file (in FRS) so they can get the popup confirmation.
    
    The DB part is only purged for new references so developers can switch
    on an old version of Tuleap and still have a working version of 'file'
    keyword (so no forgeupgrade on purpose).
    
    Part of story #14008 customize FRS Download Agreement in my project
    
    Change-Id: If0cd65122c0583b9fc5c8e982a5580d86e7a677d

---
+++ src/common/FRS/FileReference.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\FRS;
+
+use Tuleap\Request\NotFoundException;
+
+class FileReference extends \Reference
+{
+    private $reference_value;
+
+    public function __construct(
+        $myid,
+        $mykeyword,
+        $mydescription,
+        $mylink,
+        $myscope,
+        $myservice_short_name,
+        $nature,
+        $myis_active,
+        $mygroup_id,
+        $reference_value
+    ) {
+        parent::__construct(
+            $myid,
+            $mykeyword,
+            $mydescription,
+            $mylink,
+            $myscope,
+            $myservice_short_name,
+            $nature,
+            $myis_active,
+            $mygroup_id,
+        );
+        $this->reference_value = $reference_value;
+    }
+
+    public function getLink()
+    {
+        if (! $this->reference_value) {
+            throw new NotFoundException(self::class . ': no reference value found for ' . $this->reference_value);
+        }
+        $file_factory = new \FRSFileFactory();
+        $file = $file_factory->getFRSFileFromDb($this->reference_value);
+        if (! $file) {
+            throw new NotFoundException(self::class . ': no valid file found for ' . $this->reference_value);
+        }
+        $package_factory = new \FRSPackageFactory();
+        $package = $package_factory->getFRSPackageByFileIdFromDb($this->reference_value);
+        if (! $package) {
+            throw new NotFoundException(self::class . ': no valid package found for ' . $this->reference_value);
+        }
+        if (! $package->getApproveLicense()) {
+            return '/file/download/'.urlencode((string) $file->getFileID());
+        }
+        return '/file/shownotes.php?release_id='.urlencode((string) $file->getReleaseID());
+    }
+}
