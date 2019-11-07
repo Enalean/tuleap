7692e1dd0747bbc3a351c941a3e3eb16ea3064d3
Nicolas Terray <nicolas.terray@enalean.com>
2019-11-07 13:03:40 +0100

    Update remaining effort via REST
    You can now update the remaining effort of a card via REST API
    
    PATCH taskboard_cards/:id
    {
       remaining_effort: 3.1416
    }
    
    No visible change on the interface yet.
    
    Part of story #14115: update the remaining effort of a parent card
    
    Change-Id: I63f19e3785c1c8270cf887e74fc2f1f8be5325a0

---
+++ plugins/taskboard/include/REST/v1/Card/CardPatcher.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Taskboard\REST\v1\Card;
+
+use Luracast\Restler\RestException;
+use PFUser;
+use Tracker_Artifact;
+use Tracker_Exception;
+use Tracker_FormElement_Field_Numeric;
+use Tracker_FormElement_InvalidFieldException;
+use Tracker_FormElement_InvalidFieldValueException;
+use Tracker_FormElementFactory;
+use Tracker_NoChangeException;
+use Tracker_REST_Artifact_ArtifactUpdater;
+use Tracker_REST_Artifact_ArtifactValidator;
+use Tuleap\REST\I18NRestException;
+use Tuleap\Tracker\REST\v1\ArtifactValuesRepresentation;
+
+class CardPatcher
+{
+    /**
+     * @var Tracker_FormElementFactory
+     */
+    private $form_element_factory;
+    /**
+     * @var Tracker_REST_Artifact_ArtifactUpdater
+     */
+    private $updater;
+
+    public function __construct(
+        Tracker_FormElementFactory $form_element_factory,
+        Tracker_REST_Artifact_ArtifactUpdater $updater
+    ) {
+        $this->form_element_factory = $form_element_factory;
+
+        $this->updater = $updater;
+    }
+
+    public static function build(): self
+    {
+        $form_element_factory = Tracker_FormElementFactory::instance();
+        $updater              = new Tracker_REST_Artifact_ArtifactUpdater(
+            new Tracker_REST_Artifact_ArtifactValidator(
+                $form_element_factory
+            )
+        );
+
+        return new self($form_element_factory, $updater);
+    }
+
+    /**
+     * @throws I18NRestException
+     * @throws RestException
+     */
+    public function patchCard(Tracker_Artifact $artifact, PFUser $user, CardPatchRepresentation $payload): void
+    {
+        $remaining_effort_field = $this->getRemainingEffortField($artifact, $user);
+        $values                 = $this->getUpdateValues($payload, $remaining_effort_field);
+
+        try {
+            $this->updater->update($user, $artifact, $values);
+        } catch (Tracker_FormElement_InvalidFieldException $exception) {
+            throw new RestException(400, $exception->getMessage());
+        } catch (Tracker_FormElement_InvalidFieldValueException $exception) {
+            throw new RestException(400, $exception->getMessage());
+        } catch (Tracker_NoChangeException $exception) {
+            // Do nothing
+        } catch (Tracker_Exception $exception) {
+            if ($GLOBALS['Response']->feedbackHasErrors()) {
+                throw new RestException(500, $GLOBALS['Response']->getRawFeedback());
+            }
+            throw new RestException(500, $exception->getMessage());
+        }
+    }
+
+    /**
+     * @throws I18NRestException
+     * @throws RestException
+     */
+    private function getRemainingEffortField(
+        Tracker_Artifact $artifact,
+        PFUser $user
+    ): Tracker_FormElement_Field_Numeric {
+        $remaining_effort_field = $this->form_element_factory->getNumericFieldByNameForUser(
+            $artifact->getTracker(),
+            $user,
+            \Tracker::REMAINING_EFFORT_FIELD_NAME
+        );
+        if (! $remaining_effort_field instanceof Tracker_FormElement_Field_Numeric) {
+            throw new I18NRestException(
+                400,
+                dgettext('tuleap-taskboard', "The artifact does not have a remaining effort numeric field")
+            );
+        }
+        if (! $remaining_effort_field->userCanUpdate($user)) {
+            throw new RestException(403);
+        }
+
+        return $remaining_effort_field;
+    }
+
+    /**
+     * @return ArtifactValuesRepresentation[]
+     */
+    private function getUpdateValues(
+        CardPatchRepresentation $payload,
+        Tracker_FormElement_Field_Numeric $remaining_effort_field
+    ): array {
+        $representation           = new ArtifactValuesRepresentation();
+        $representation->field_id = (int) $remaining_effort_field->getId();
+        if ($remaining_effort_field instanceof \Tracker_FormElement_Field_Computed) {
+            $representation->manual_value    = $payload->remaining_effort;
+            $representation->is_autocomputed = false;
+        } else {
+            $representation->value = $payload->remaining_effort;
+        }
+
+        return [$representation];
+    }
+}
---
+++ plugins/taskboard/phpunit/REST/v1/Card/CardPatcherTest.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Taskboard\REST\v1\Card;
+
+use Luracast\Restler\RestException;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+use Tracker;
+use Tracker_Artifact;
+use Tracker_FormElement_Field_Computed;
+use Tracker_FormElement_Field_Float;
+use Tracker_FormElement_Field_Integer;
+use Tracker_FormElement_Field_Numeric;
+use Tracker_FormElementFactory;
+use Tracker_NoChangeException;
+use Tracker_REST_Artifact_ArtifactUpdater;
+use Tuleap\Tracker\REST\v1\ArtifactValuesRepresentation;
+
+class CardPatcherTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker_FormElementFactory
+     */
+    private $factory;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker_REST_Artifact_ArtifactUpdater
+     */
+    private $updater;
+    /**
+     * @var CardPatcher
+     */
+    private $patcher;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PFUser
+     */
+    private $user;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker_Artifact
+     */
+    private $artifact;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker
+     */
+    private $tracker;
+    /**
+     * @var CardPatchRepresentation
+     */
+    private $payload;
+
+    protected function setUp(): void
+    {
+        $this->user     = Mockery::mock(PFUser::class);
+        $this->artifact = Mockery::mock(Tracker_Artifact::class);
+        $this->tracker  = Mockery::mock(Tracker::class);
+
+        $this->artifact->shouldReceive('getTracker')->andReturn($this->tracker);
+
+        $this->payload                   = new CardPatchRepresentation();
+        $this->payload->remaining_effort = 3.14;
+
+        $this->factory = Mockery::mock(Tracker_FormElementFactory::class);
+        $this->updater = Mockery::mock(Tracker_REST_Artifact_ArtifactUpdater::class);
+
+        $this->patcher = new CardPatcher($this->factory, $this->updater);
+    }
+
+    public function testItRaisesExceptionIfNoField(): void
+    {
+        $this->factory
+            ->shouldReceive('getNumericFieldByNameForUser')
+            ->with($this->tracker, $this->user, \Tracker::REMAINING_EFFORT_FIELD_NAME)
+            ->once()
+            ->andReturn(null);
+
+        $this->expectException(RestException::class);
+
+        $this->patcher->patchCard($this->artifact, $this->user, $this->payload);
+    }
+
+    public function testItRaisesExceptionIfFieldIsNotUpdatable(): void
+    {
+        $field = Mockery::mock(Tracker_FormElement_Field_Float::class);
+        $field->shouldReceive('userCanUpdate')
+              ->with($this->user)
+              ->andReturn(false);
+
+        $this->factory
+            ->shouldReceive('getNumericFieldByNameForUser')
+            ->with($this->tracker, $this->user, \Tracker::REMAINING_EFFORT_FIELD_NAME)
+            ->once()
+            ->andReturn($field);
+
+        $this->expectException(RestException::class);
+
+        $this->patcher->patchCard($this->artifact, $this->user, $this->payload);
+    }
+
+    public function testItUpdatesTheArtifactWithFormattedValueForFloatField(): void
+    {
+        $field = Mockery::mock(Tracker_FormElement_Field_Float::class);
+
+        $expected_value           = new ArtifactValuesRepresentation();
+        $expected_value->field_id = 1001;
+        $expected_value->value    = 3.14;
+
+        $this->assertUpdateIsCalledWithExpectedValue($field, $expected_value);
+    }
+
+    public function testItUpdatesTheArtifactWithFormattedValueForIntegerField(): void
+    {
+        $field = Mockery::mock(Tracker_FormElement_Field_Integer::class);
+
+        $expected_value           = new ArtifactValuesRepresentation();
+        $expected_value->field_id = 1001;
+        $expected_value->value    = 3.14;
+
+        $this->assertUpdateIsCalledWithExpectedValue($field, $expected_value);
+    }
+
+    public function testItUpdatesTheArtifactWithFormattedValueForComputedField(): void
+    {
+        $field = Mockery::mock(Tracker_FormElement_Field_Computed::class);
+
+        $expected_value                  = new ArtifactValuesRepresentation();
+        $expected_value->field_id        = 1001;
+        $expected_value->manual_value    = 3.14;
+        $expected_value->is_autocomputed = false;
+
+        $this->assertUpdateIsCalledWithExpectedValue($field, $expected_value);
+    }
+
+    public function testItDoesNotRaisesExceptionIfThereIsNoChange(): void
+    {
+        $field = Mockery::mock(Tracker_FormElement_Field_Integer::class);
+
+        $field->shouldReceive('userCanUpdate')
+              ->with($this->user)
+              ->andReturn(true);
+        $field->shouldReceive('getId')
+              ->andReturn("1001");
+
+        $this->factory
+            ->shouldReceive('getNumericFieldByNameForUser')
+            ->with($this->tracker, $this->user, \Tracker::REMAINING_EFFORT_FIELD_NAME)
+            ->once()
+            ->andReturn($field);
+
+        $this->updater
+            ->shouldReceive('update')
+            ->andThrow(Mockery::mock(Tracker_NoChangeException::class));
+
+        $this->patcher->patchCard($this->artifact, $this->user, $this->payload);
+    }
+
+    private function assertUpdateIsCalledWithExpectedValue(Tracker_FormElement_Field_Numeric $field, ArtifactValuesRepresentation $expected_value): void
+    {
+        $field->shouldReceive('userCanUpdate')
+              ->with($this->user)
+              ->andReturn(true);
+        $field->shouldReceive('getId')
+              ->andReturn("1001");
+
+        $this->factory
+            ->shouldReceive('getNumericFieldByNameForUser')
+            ->with($this->tracker, $this->user, \Tracker::REMAINING_EFFORT_FIELD_NAME)
+            ->once()
+            ->andReturn($field);
+
+        $this->updater
+            ->shouldReceive('update')
+            ->with($this->user, $this->artifact, [$expected_value])
+            ->once();
+
+        $this->patcher->patchCard($this->artifact, $this->user, $this->payload);
+    }
+}
