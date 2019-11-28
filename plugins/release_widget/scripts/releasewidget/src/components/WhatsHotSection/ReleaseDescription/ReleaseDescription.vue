b6860353b02bbc4bae21a76da5dfa03389da2164
Benjamin Bouillot <benjamin.bouillot@enalean.com>
2019-11-28 15:38:55 +0100

    story #14232 More legible if description and link are at the bottom when there is no place.
    When the widget is small, the link 'Go to overview' (between badges and burndown) can be ugly and don't really legible.
    
    How to test:
    	- Reduce the widget at the left or right
    
    Expect Results:
    	- The Link go to the bottom of badges
    
    Change-Id: I3c53bd8e6f9ceedd8f684c4d7dd98465811d7a63

--- plugins/release_widget/scripts/releasewidget/src/components/WhatsHotSection/ReleaseDescription/ReleaseDescription.vue
+++ plugins/release_widget/scripts/releasewidget/src/components/WhatsHotSection/ReleaseDescription/ReleaseDescription.vue
@@ -21,7 +21,9 @@
     <div class="release-content-description">
         <release-description-badges-tracker v-bind:release_data="release_data"/>
         <div class="release-description-row">
-            <div class="release-description" v-dompurify-html="release_data.description"></div>
+            <div v-if="release_data.description" class="tlp-tooltip tlp-tooltip-top" v-bind:data-tlp-tooltip="release_data.description" data-test="tooltip-description">
+                <div class="release-description" v-dompurify-html="release_data.description"></div>
+            </div>
             <a v-if="get_overview_link !== null" v-bind:href="get_overview_link" data-test="overview-link">
                 <i class="release-description-link-icon fa fa-long-arrow-right"></i>
                 <translate v-bind:translate-params="{label_tracker: label_tracker_planning}"> Go to %{label_tracker} overview </translate>
