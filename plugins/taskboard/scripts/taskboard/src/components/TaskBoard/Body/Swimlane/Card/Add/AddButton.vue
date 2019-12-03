bd737e175f32f30d7d2e591429da9889b571bd33
Nicolas Terray <nicolas.terray@enalean.com>
2019-12-03 16:09:19 +0100

    Add [+] button
    On every cell of a parent card, there is a new button that will allow us
    to create new card.
    
    Card with invalid status and solo card do not have such button yet.
    
    The click on the button does nothing.
    
    Part of story #14148: Add in place a child card
    
    Change-Id: Icb0cf281405c87e20ca9c9933ef59c30c749396a

---
+++ plugins/taskboard/scripts/taskboard/src/components/TaskBoard/Body/Swimlane/Card/Add/AddButton.vue
@@ -0,0 +1,41 @@
+<!--
+  - Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+    <button type="button"
+            class="taskboard-add-in-place-button tlp-button-primary tlp-button-outline tlp-button-small tlp-tooltip tlp-tooltip-top"
+            v-bind:data-tlp-tooltip="title"
+            v-bind:aria-label="title"
+    >
+        <i class="fa fa-plus" aria-hidden="true"></i>
+    </button>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class AddButton extends Vue {
+    get title(): string {
+        return this.$gettext("Add new card");
+    }
+}
+</script>
