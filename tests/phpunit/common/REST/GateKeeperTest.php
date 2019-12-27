8860d9de072fbcd67f80b9bb8340fd029015dd4f
Thomas Gerbet <thomas.gerbet@enalean.com>
2019-12-27 10:21:41 +0100

    Migrate to PHPUnit remaining tests of Tuleap\REST
    Part of request #14150: Get rid of SimpleTest
    
    Change-Id: I895d03c49f6429d33b1fe6947b55940182190202

---
+++ tests/phpunit/common/REST/GateKeeperTest.php
@@ -0,0 +1,149 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\REST;
+
+use HTTPRequest;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+
+final class GateKeeperTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var PFUser
+     */
+    private $user;
+    /**
+     * @var PFUser
+     */
+    private $anonymous;
+    /**
+     * @var HTTPRequest|\Mockery\LegacyMockInterface|\Mockery\MockInterface
+     */
+    private $request;
+    /**
+     * @var GateKeeper
+     */
+    private $gate_keeper;
+
+    protected function setUp() : void
+    {
+        $this->user        = new PFUser(array('user_id' => 112, 'language_id' => 'en'));
+        $this->anonymous   = new PFUser(array('user_id' => 0, 'language_id' => 'en'));
+        $this->gate_keeper = new GateKeeper();
+        $this->request     = \Mockery::spy(HTTPRequest::class);
+    }
+
+    protected function tearDown() : void
+    {
+        unset($GLOBALS['DEBUG_MODE'], $_SERVER['HTTP_REFERER']);
+    }
+
+    public function testItThrowsExceptionWhenTokenOrAccessKeyAuthenticationWithoutSSL() : void
+    {
+        $this->request->shouldReceive('isSecure')->andReturns(false);
+        $this->expectException(\Exception::class);
+
+        $this->gate_keeper->assertAccess($this->anonymous, $this->request);
+    }
+
+    public function testItLetsPassWhenTokenOrAccessKeyAuthenticationWithSSL() : void
+    {
+        $this->request->shouldReceive('isSecure')->andReturns(true);
+
+        $this->gate_keeper->assertAccess($this->anonymous, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetsPassWhenTokenOrAccessKeyAuthenticationWithoutSSLButWithDebug() : void
+    {
+        $this->request->shouldReceive('isSecure')->andReturns(false);
+        $GLOBALS['DEBUG_MODE'] = 1;
+
+        $this->gate_keeper->assertAccess($this->anonymous, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetPassHTTPWhenCookieAuthentication() : void
+    {
+        $this->request->shouldReceive('isSecure')->andReturns(false);
+
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetPassWhenReferMatchesHost() : void
+    {
+        $_SERVER['HTTP_REFERER'] = 'http://example.com/bla';
+        $this->request->shouldReceive('getServerUrl')->andReturns('http://example.com');
+
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetPassWhenReferMatchesAnEquivalentHostWithCase() : void
+    {
+        $_SERVER['HTTP_REFERER'] = 'https://example.com/';
+        $this->request->shouldReceive('getServerUrl')->andReturns('https://EXAMPLE.COM');
+
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetPassWhenReferMatchesAnEquivalentIDNHost() : void
+    {
+        $_SERVER['HTTP_REFERER'] = 'https://チューリップ.example.com/';
+        $this->request->shouldReceive('getServerUrl')->andReturns('https://xn--7cke4dscza1i.example.com');
+
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItLetPassWhenReferMatchesAnEquivalentIDNHostWithCase() : void
+    {
+        $_SERVER['HTTP_REFERER'] = 'https://チューリップ.example.com/';
+        $this->request->shouldReceive('getServerUrl')->andReturns('https://チューリップ.EXAMPLE.COM');
+
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+        $this->assertTrue(true, 'No exception should be raised');
+    }
+
+    public function testItThrowsExceptionWhenReferIsDiffentFromHost() : void
+    {
+        $_SERVER['HTTP_REFERER'] = 'http://wannabe_attacker.com/bla';
+        $this->request->shouldReceive('getServerUrl')->andReturns('http://example.com');
+
+        $this->expectException(\Exception::class);
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+    }
+
+    public function testItThrowsExceptionWhenNoReferer() : void
+    {
+        unset($_SERVER['HTTP_REFERER']);
+        $this->request->shouldReceive('getServerUrl')->andReturns('http://example.com');
+
+        $this->expectException(\Exception::class);
+        $this->gate_keeper->assertAccess($this->user, $this->request);
+    }
+}
---
+++ tests/phpunit/common/REST/TokenManagerTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2013-Present. All Rights Reserved.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\REST;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Rest_Token;
+use Rest_TokenDao;
+use Rest_TokenFactory;
+use Rest_TokenManager;
+
+final class TokenManagerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /** @var  Rest_TokenManager */
+    private $token_manager;
+
+    /** @var  Rest_TokenDao */
+    private $token_dao;
+
+    /** @var  Rest_TokenFactory */
+    private $token_factory;
+    /**
+     * @var \Mockery\LegacyMockInterface|\Mockery\MockInterface|\UserManager
+     */
+    private $user_manager;
+    /**
+     * @var string
+     */
+    private $user_id;
+    /**
+     * @var string
+     */
+    private $token_value;
+    /**
+     * @var Rest_Token
+     */
+    private $token;
+    /**
+     * @var \PFUser
+     */
+    private $user;
+
+    protected function setUp() : void
+    {
+        $this->user_manager  = \Mockery::spy(\UserManager::class);
+        $this->token_dao     = \Mockery::spy(\Rest_TokenDao::class);
+        $this->token_factory = new Rest_TokenFactory($this->token_dao);
+        $this->token_manager = new Rest_TokenManager($this->token_dao, $this->token_factory, $this->user_manager);
+
+        $this->user_id     = 'whatever';
+        $this->token_value = 'whateverSha1Token';
+        $this->token       = new Rest_Token($this->user_id, $this->token_value);
+        $this->user        = new \PFUser(['user_id' => $this->user_id, 'language_id' => 'en']);
+    }
+
+    public function testItReturnsTheUserIfTokenIsValid() : void
+    {
+        $this->user_manager->shouldReceive('getUserById')->with($this->user_id)->andReturns($this->user);
+
+        $this->token_dao->shouldReceive('checkTokenExistenceForUserId')->with($this->user_id, $this->token_value)->andReturns(\TestHelper::arrayToDar(array()));
+        $this->assertEquals($this->user, $this->token_manager->checkToken($this->token));
+    }
+
+    public function testItThrowsAnExceptionIfTokenIsInvalid() : void
+    {
+        $this->token_dao->shouldReceive('checkTokenExistenceForUserId')->with($this->user_id, $this->token_value)->andReturns(\TestHelper::emptyDar());
+        $this->expectException(\Rest_Exception_InvalidTokenException::class);
+
+        $this->token_manager->checkToken($this->token);
+    }
+
+    public function testItExpiresATokenIfItBelongsToUser() : void
+    {
+        $this->user_manager->shouldReceive('getUserById')->with($this->user_id)->andReturns($this->user);
+
+        $this->token_dao->shouldReceive('checkTokenExistenceForUserId')->with($this->user_id, $this->token_value)->andReturns(\TestHelper::arrayToDar(array()));
+        $this->token_dao->shouldReceive('deleteToken')->with($this->token_value)->andReturns(true)->once();
+
+        $this->token_manager->expireToken($this->token);
+    }
+
+    public function testItExpiresAllTokensForAUser() : void
+    {
+        $this->token_dao->shouldReceive('deleteAllTokensForUser')->with($this->user_id)->once();
+
+        $this->token_manager->expireAllTokensForUser($this->user);
+    }
+
+    public function testItDoesNotExpireATokenIfItDoesNotBelongToUser() : void
+    {
+        $this->token_dao->shouldReceive('checkTokenExistenceForUserId')->with($this->user_id, $this->token_value)->andReturns(\TestHelper::emptyDar());
+
+        $this->token_dao->shouldReceive('deleteToken')->with($this->token_value)->never();
+        $this->expectException(\Rest_Exception_InvalidTokenException::class);
+
+        $this->token_manager->expireToken($this->token);
+    }
+
+    public function testItAddsATokenToDatabase() : void
+    {
+        $this->token_dao->shouldReceive('addTokenForUserId')->with($this->user_id, \Mockery::any(), \Mockery::any())->andReturns(true)->once();
+
+        $this->assertNotNull($this->token_manager->generateTokenForUser($this->user));
+    }
+}
