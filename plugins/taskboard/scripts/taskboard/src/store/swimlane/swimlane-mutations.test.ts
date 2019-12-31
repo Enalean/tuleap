Enalean/tuleap

commit cb27d4fe0ab9c3e11087fac444c1e2959c3e89bb

Do not erase remaining effort flags when refreshing the card
Part of story #14249 Taskboard technical improvements

How to test:
- Drag and drop a child card to another column.
- Click on the parent card's remaining effort to edit it.
- The remaining effort should open its "edition mode".
- Same for solo swimlane.

Without this patch, the remaining effort's "is_in_edit_mode" flag is
erased and clicking on it has no effect.

Change-Id: I06cea4c50ff66f4755305bb78b6efaf87fb5bfe5

plugins/taskboard/scripts/taskboard/src/store/swimlane/swimlane-mutations.ts
@@ -44,7 +44,11 @@ export function addSwimlanes(state: SwimlaneState, swimlanes: Array<Swimlane>): 
 
 export function refreshCard(state: SwimlaneState, payload: RefreshCardMutationPayload): void {
     const state_card = findCard(state, payload.refreshed_card);
-    Object.assign(state_card, payload.refreshed_card);
+    const remaining_effort = Object.assign(
+        state_card.remaining_effort,
+        payload.refreshed_card.remaining_effort
+    );
+    Object.assign(state_card, payload.refreshed_card, { remaining_effort });
 }
 
 export function beginLoadingSwimlanes(state: SwimlaneState): void {

plugins/taskboard/scripts/taskboard/src/store/swimlane/swimlane-mutations.test.ts
@@ -229,7 +229,7 @@ describe(`Swimlane state mutations`, () => {
             };
             const state = {
                 swimlanes: [
-                    { card: { id: 21, label: "Unrelated swimlane " }, children_cards: [] },
+                    { card: { id: 21, label: "Unrelated swimlane" }, children_cards: [] },
                     swimlane
                 ]
             } as SwimlaneState;
@@ -244,6 +244,53 @@ describe(`Swimlane state mutations`, () => {
             mutations.refreshCard(state, { swimlane, refreshed_card: card });
 
             expect(state.swimlanes[1].card).toStrictEqual(card);
+        });
+
+        it(`When the user is editing the remaining effort of the swimlane,
+            it does not erase remaining effort's edit flags`, () => {
+            const swimlane = {
+                card: {
+                    id: 78,
+                    label: "Parent card",
+                    has_children: true,
+                    mapped_list_value: { id: 1234, label: "Todo" },
+                    initial_effort: 2,
+                    remaining_effort: {
+                        value: 1.5,
+                        is_in_edit_mode: true,
+                        is_being_saved: false,
+                        can_update: true
+                    }
+                } as Card,
+                children_cards: [{ id: 61, label: "Unrelated card" } as Card],
+                is_loading_children_cards: false
+            };
+            const state = {
+                swimlanes: [
+                    { card: { id: 21, label: "Unrelated swimlane" }, children_cards: [] },
+                    swimlane
+                ]
+            } as SwimlaneState;
+            const card = {
+                id: 78,
+                label: "Refreshed parent card",
+                has_children: true,
+                mapped_list_value: { id: 3456, label: "Done" },
+                initial_effort: 2,
+                remaining_effort: { value: 0, can_update: true }
+            } as Card;
+            mutations.refreshCard(state, { swimlane, refreshed_card: card });
+
+            const state_swimlane = state.swimlanes[1];
+            expect(state_swimlane.card.remaining_effort).not.toBeNull();
+            if (state_swimlane.card.remaining_effort) {
+                expect(state_swimlane.card.remaining_effort).toStrictEqual({
+                    value: 0,
+                    can_update: true,
+                    is_in_edit_mode: true,
+                    is_being_saved: false
+                });
+            }
         });
     });
 

commit 186d805d4866e9bb994acdf790c07d129b5f77ab

Moving a taskboard child card that does not have a remaining effort should give an error
Issue has been introduced by cb27d4fe0ab9c3e11087fac444c1e2959c3e89bb.

Part of story #14249: Taskboard technical improvements

Change-Id: Ie67104e40ed1bbdbd86c8a3046f0a5ffe431e471

plugins/taskboard/scripts/taskboard/src/store/swimlane/swimlane-mutations.ts
@@ -44,10 +44,13 @@ export function addSwimlanes(state: SwimlaneState, swimlanes: Array<Swimlane>): 
 
 export function refreshCard(state: SwimlaneState, payload: RefreshCardMutationPayload): void {
     const state_card = findCard(state, payload.refreshed_card);
-    const remaining_effort = Object.assign(
-        state_card.remaining_effort,
-        payload.refreshed_card.remaining_effort
-    );
+    let remaining_effort = null;
+    if (state_card.remaining_effort !== null) {
+        remaining_effort = Object.assign(
+            state_card.remaining_effort,
+            payload.refreshed_card.remaining_effort
+        );
+    }
     Object.assign(state_card, payload.refreshed_card, { remaining_effort });
 }
 

plugins/taskboard/scripts/taskboard/src/store/swimlane/swimlane-mutations.test.ts
@@ -213,6 +213,41 @@ describe(`Swimlane state mutations`, () => {
         });
 
         it(`Given a swimlane and a card,
+            when it is a child card without remaining effort,
+            it will find it in the state and leave it as is`, () => {
+            const swimlane = {
+                card: { id: 78, label: "Parent card", has_children: true } as Card,
+                children_cards: [
+                    { id: 61, label: "Unrelated card" } as Card,
+                    {
+                        id: 59,
+                        label: "Child card without remaining effort in state",
+                        mapped_list_value: { id: 1234, label: "Todo" },
+                        initial_effort: 8,
+                        remaining_effort: null
+                    } as Card
+                ],
+                is_loading_children_cards: false
+            };
+            const state = {
+                swimlanes: [
+                    { card: { id: 21, label: "Unrelated swimlane" }, children_cards: [] },
+                    swimlane
+                ]
+            } as SwimlaneState;
+            const card = {
+                id: 59,
+                label: "Refreshed child card",
+                mapped_list_value: { id: 2345, label: "On going" },
+                initial_effort: 2,
+                remaining_effort: null
+            } as Card;
+            mutations.refreshCard(state, { swimlane, refreshed_card: card });
+
+            expect(state.swimlanes[1].children_cards[1]).toStrictEqual(card);
+        });
+
+        it(`Given a swimlane and a card,
             when it is a parent card,
             it will find it in the state and replace it`, () => {
             const swimlane = {