0d94d188de32f707adbc6f4e9d1414826aed7fa6
Romain LORENTZ <romain.lorentz@enalean.com>
2020-01-08 16:53:30 +0100

    Move static field separator test to phpunit
    Part of request #14150: Get rid of SimpleTest
    
    Change-Id: Ieadbaa75109411323976beddad75648a8f6230ca

--- plugins/tracker/phpunit/Tracker/FormElement/Tracker_FormElement_SeparatorTest.php
+++ plugins/tracker/phpunit/Tracker/FormElement/Tracker_FormElement_SeparatorTest.php
@@ -1,27 +1,34 @@
 <?php
 /**
- * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Xerox Corporation, Codendi Team 2001-2009. All rights reserved
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
  *
- * This file is a part of Codendi.
+ * This file is a part of Tuleap.
  *
- * Codendi is free software; you can redistribute it and/or modify
+ * Tuleap is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * Codendi is distributed in the hope that it will be useful,
+ * Tuleap is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Codendi. If not, see <http://www.gnu.org/licenses/>.
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('bootstrap.php');
+namespace Tuleap\Tracker\FormElement;
 
-class Tracker_FormElement_StaticField_SeparatorTest extends TuleapTestCase
+use PHPUnit\Framework\TestCase;
+use Tracker_FormElement_StaticField_Separator;
+use Tuleap\GlobalLanguageMock;
+
+//phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
+class Tracker_FormElement_SeparatorTest extends TestCase
 {
+    use GlobalLanguageMock;
 
     public function testFetchDescription()
     {
@@ -41,8 +48,8 @@ class Tracker_FormElement_StaticField_SeparatorTest extends TuleapTestCase
 
         $separator = new Tracker_FormElement_StaticField_Separator($id, $tracker_id, $parent_id, $name, $label, $description, $use_it, $scope, $required, $notifications, $rank, $original_field);
 
-        $this->assertEqual('Separator Label', $separator->getLabel());
-        $this->assertEqual('', $separator->getDescription());
-        $this->assertEqual($expected_message, $separator->getCannotRemoveMessage());
+        $this->assertEquals('Separator Label', $separator->getLabel());
+        $this->assertEquals('', $separator->getDescription());
+        $this->assertEquals($expected_message, $separator->getCannotRemoveMessage());
     }
 }
