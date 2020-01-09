05876e0d15347cc2b6b24319c13e5b48effd0dc8
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2020-01-09 17:59:41 +0100

    Move PermissionsManagerTest to PHPUnit
    This is part of request #14150: Get rid of SimpleTest
    
    Change-Id: I40844721a88d93206f7c3ac3ecb8c2e9e441d319

---
+++ tests/phpunit/common/permission/PermissionsManagerGetAuthorizedUGroupIdsForProjectTest.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Tuleap\ForgeConfigSandbox;
+
+//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
+class PermissionsManagerGetAuthorizedUGroupIdsForProjectTest extends TestCase
+{
+    use MockeryPHPUnitIntegration, ForgeConfigSandbox;
+
+    private $permissions_manager;
+    private $project;
+    private $permission_type;
+    private $object_id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->project             = \Mockery::spy(\Project::class);
+        $this->permission_type     = 'FOO';
+        $this->object_id           = 'BAR';
+        $this->permissions_manager = \Mockery::mock(\PermissionsManager::class)->makePartial()->shouldAllowMockingProtectedMethods();
+    }
+
+    public function testItReturnsTheListOfStaticGroups()
+    {
+        $this->stubAuthorizedUgroups(array('ugroup_id' => 102));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(102));
+    }
+
+    public function testItReturnsProjectMembersWhenProjectIsPrivateAndUGroupIsAnonymous()
+    {
+        $this->project->shouldReceive('isPublic')->andReturns(false);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::ANONYMOUS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::PROJECT_MEMBERS));
+    }
+
+    public function testItReturnsProjectMembersWhenProjectIsPrivateAndUGroupIsAuthenticated()
+    {
+        $this->project->shouldReceive('isPublic')->andReturns(false);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::AUTHENTICATED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::PROJECT_MEMBERS));
+    }
+
+    public function testItReturnsProjectMembersWhenProjectIsPrivateAndUGroupIsRegistered()
+    {
+        $this->project->shouldReceive('isPublic')->andReturns(false);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::REGISTERED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::PROJECT_MEMBERS));
+    }
+
+    public function testItReturnsRegisteredUsersWhenPlatformIsRegularProjectIsPublicAndUGroupIsAnonymous()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::REGULAR);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::ANONYMOUS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsRegisteredUsersWhenPlatformIsRegularProjectIsPublicAndUGroupIsRegisteredUsers()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::REGULAR);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::REGISTERED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsRegisteredUsersWhenPlatformIsRegularProjectIsPublicAndUGroupIsAuthenticatedUsers()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::REGULAR);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::AUTHENTICATED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsProjectMembersWhenPlatformIsRegularProjectIsPublicAndUGroupIsProjectMembers()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::REGULAR);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::PROJECT_MEMBERS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::PROJECT_MEMBERS));
+    }
+
+    public function testItReturnsAnonymousWhenPlatformIsAllowedToAnonymousProjectIsPublicAndUGroupIsAnonymous()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::ANONYMOUS);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::ANONYMOUS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::ANONYMOUS));
+    }
+
+    public function testItReturnsRegisteredWhenPlatformIsAllowedToAnonymousProjectIsPublicAndUGroupIsAuthenticated()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::ANONYMOUS);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::AUTHENTICATED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsRegisteredWhenPlatformIsAllowedToAnonymousProjectIsPublicAndUGroupIsRegistered()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::ANONYMOUS);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::REGISTERED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsRegisteredWhenPlatformIsRestrictedProjectIsPublicAndUGroupIsAnonymous()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::RESTRICTED);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::ANONYMOUS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsRegisteredWhenPlatformIsRestrictedProjectIsPublicAndUGroupIsRegistered()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::RESTRICTED);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::REGISTERED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::REGISTERED));
+    }
+
+    public function testItReturnsAuthenticatedWhenPlatformIsRestrictedProjectIsPublicAndUGroupIsAnonymous()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::RESTRICTED);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->project->shouldReceive('allowsRestricted')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::ANONYMOUS));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::AUTHENTICATED));
+    }
+
+    public function testItReturnsAuthenticatedWhenPlatformIsRestrictedProjectIsPublicAndUGroupIsAuthenticated()
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::RESTRICTED);
+        $this->project->shouldReceive('isPublic')->andReturns(true);
+        $this->project->shouldReceive('allowsRestricted')->andReturns(true);
+        $this->stubAuthorizedUgroups(array('ugroup_id' => ProjectUGroup::AUTHENTICATED));
+
+        $this->assertAuthorizedUGroupIdsForProjectEqual(array(ProjectUGroup::AUTHENTICATED));
+    }
+
+    private function stubAuthorizedUgroups($groups)
+    {
+        $this->permissions_manager->shouldReceive('getAuthorizedUgroups')->with($this->object_id, $this->permission_type, false)->andReturns(\TestHelper::arrayToDar($groups));
+    }
+
+    private function assertAuthorizedUGroupIdsForProjectEqual($groups)
+    {
+        $this->assertEquals(
+            $groups,
+            $this->permissions_manager->getAuthorizedUGroupIdsForProject($this->project, $this->object_id, $this->permission_type)
+        );
+    }
+}
---
+++ tests/phpunit/common/permission/PermissionsManagerSavePermissionsCommonTest.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use Tuleap\ForgeConfigSandbox;
+use Tuleap\GlobalResponseMock;
+
+//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
+class PermissionsManagerSavePermissionsCommonTest extends TestCase
+{
+    use MockeryPHPUnitIntegration, ForgeConfigSandbox, GlobalResponseMock;
+
+    protected $permissions_manager;
+    protected $project;
+    protected $permission_type;
+    protected $object_id;
+    protected $permissions_dao;
+    protected $project_id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->project_id          = 404;
+        $this->project             = \Mockery::spy(\Project::class)->shouldReceive('getId')->andReturns($this->project_id)->getMock();
+        $this->permissions_dao     = \Mockery::spy(\PermissionsDao::class);
+        $this->permission_type     = 'FOO';
+        $this->object_id           = 'BAR';
+        $this->permissions_manager = new PermissionsManager($this->permissions_dao);
+    }
+
+    protected function expectPermissionsOnce($ugroup)
+    {
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, $ugroup)
+            ->once()
+            ->andReturns(true);
+    }
+
+    protected function savePermissions($ugroups)
+    {
+        $this->permissions_manager->savePermissions($this->project, $this->object_id, $this->permission_type, $ugroups);
+    }
+
+    public function testItSavesTheValueForStaticUGroupId()
+    {
+        $this->permissions_dao->shouldReceive('clearPermission')
+            ->with($this->permission_type, $this->object_id)
+            ->once()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, 104)
+            ->once()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addHistory')
+            ->with($this->project_id, $this->permission_type, $this->object_id)
+            ->once();
+
+        $this->savePermissions(array(104));
+    }
+
+    public function testItSavesTheValueForSeveralStaticUGroupIds()
+    {
+        $this->permissions_dao->shouldReceive('clearPermission')
+            ->with($this->permission_type, $this->object_id)
+            ->once()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, 104)
+            ->once()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, 201)
+            ->once()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addHistory')
+            ->with($this->project_id, $this->permission_type, $this->object_id)
+            ->once()
+            ->andReturns(true);
+
+        $this->savePermissions(array(104, 201));
+    }
+
+    public function testItSavesOnlyOneInstanceOfGroups()
+    {
+        $this->permissions_dao->shouldReceive('clearPermission')->andReturns(true);
+
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, 104)
+            ->ordered()
+            ->andReturns(true);
+        $this->permissions_dao->shouldReceive('addPermission')
+            ->with($this->permission_type, $this->object_id, 201)
+            ->ordered()
+            ->andReturns(true);
+
+        $this->savePermissions(array(104, 201, 104));
+    }
+}
