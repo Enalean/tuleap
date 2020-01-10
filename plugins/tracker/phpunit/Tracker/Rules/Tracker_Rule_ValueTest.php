153c8969f42b91811d5e640b7ec699b4618b915e
Romain LORENTZ <romain.lorentz@enalean.com>
2020-01-10 14:38:45 +0100

    Move tracker rule value test to phpunit
    Part of request #14150: Get rid of SimpleTest
    
    Change-Id: I0230523e4a854785c08ca2014278f831643769f6

--- plugins/tracker/phpunit/Tracker/Rules/Tracker_Rule_ValueTest.php
+++ plugins/tracker/phpunit/Tracker/Rules/Tracker_Rule_ValueTest.php
@@ -1,16 +1,32 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
+ * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
  *
+ * This file is a part of Tuleap.
  *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * Tests the class Tracker_RuleValue
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once('bootstrap.php');
-class Tracker_Rule_ListTest extends TuleapTestCase
-{
 
-    function testApplyTo()
+namespace Tuleap\Tracker\Rule;
+
+use PHPUnit\Framework\TestCase;
+use Tracker_Rule_List;
+
+class Tracker_Rule_ListTest extends TestCase
+{
+    public function testApplyTo()
     {
         $trv  = new Tracker_Rule_List();
         $trv->setSourceValue('source_value')
@@ -31,7 +47,7 @@ class Tracker_Rule_ListTest extends TuleapTestCase
         $this->assertFalse($trv->applyTo('tracker_id', 'source_field', 'source_value', 'false_target_field', 'false_target_value'));
     }
 
-    function testCanApplyTo()
+    public function testCanApplyTo()
     {
         $trv  = new Tracker_Rule_List();
         $trv->setSourceValue('source_value')
