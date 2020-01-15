f87e73f117fc002f0bd70ac066b642ee75c414a3
Romain LORENTZ <romain.lorentz@enalean.com>
2020-01-15 15:24:45 +0100

    Move some tracker form element tests to PHPUnit
    This is part of request #14150: Get rid of SimpleTest
    
    Change-Id: Ia3773030c9b084b2c1db6c6e70f45861150c5b03

---
+++ plugins/tracker/phpunit/Tracker/FormElement/TrackerFormElementTest.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\FormElement;
+
+use HTTPRequest;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+use Project;
+use Tracker;
+use Tracker_FormElement_Field_Selectbox;
+use Tracker_FormElement_StaticField_Separator;
+use Tracker_FormElementFactory;
+use TrackerManager;
+use Tuleap\GlobalLanguageMock;
+use Tuleap\GlobalResponseMock;
+
+class TrackerFormElementTest extends TestCase
+{
+    use MockeryPHPUnitIntegration, GlobalLanguageMock, GlobalResponseMock;
+
+    protected function tearDown(): void
+    {
+        unset($GLOBALS['HTML']);
+    }
+
+    public function testGetOriginalProjectAndOriginalTracker()
+    {
+        $project = Mockery::mock(Project::class);
+        $tracker = Mockery::mock(Tracker::class);
+        $tracker->shouldReceive('getId')->andReturn(888);
+        $tracker->shouldReceive('getProject')->andReturn($project);
+        $original = Mockery::mock(Tracker_FormElement_Field_Selectbox::class);
+        $original->shouldReceive('getTracker')->andReturns($tracker);
+
+        $element = $this->GivenAFormElementWithIdAndOriginalField(null, $original);
+
+        $this->assertEquals($tracker, $element->getOriginalTracker());
+        $this->assertEquals($project, $element->getOriginalProject());
+    }
+
+    public function testGetOriginalFieldIdShouldReturnTheFieldId()
+    {
+        $original = $this->GivenAFormElementWithIdAndOriginalField(112, null);
+        $element  = $this->GivenAFormElementWithIdAndOriginalField(null, $original);
+        $this->assertEquals($element->getOriginalFieldId(), 112);
+    }
+
+    public function testGetOriginalFieldIdShouldReturn0IfNoOriginalField()
+    {
+        $element = $this->GivenAFormElementWithIdAndOriginalField(null, null);
+        $this->assertEquals($element->getOriginalFieldId(), 0);
+    }
+
+    protected function givenAFormElementWithIdAndOriginalField($id, $originalField)
+    {
+        return new Tracker_FormElement_StaticField_Separator(
+            $id,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            $originalField
+        );
+    }
+
+    public function testDisplayUpdateFormShouldDisplayAForm()
+    {
+        $formElement = $this->GivenAFormElementWithIdAndOriginalField(null, null);
+        $factory = Mockery::mock(Tracker_FormElementFactory::class);
+        $factory->shouldReceive('getUsedFormElementForTracker')->andReturns([]);
+        $factory->shouldReceive('getSharedTargets');
+        $tracker = Mockery::mock(Tracker::class);
+
+        $tracker->shouldReceive('getId')->andReturn(888);
+        $tracker->shouldReceive('displayAdminFormElementsHeader');
+        $tracker->shouldReceive('displayFooter');
+        $formElement->setTracker($tracker);
+        $formElement->setFormElementFactory($factory);
+
+        $content = $this->WhenIDisplayAdminFormElement($formElement);
+
+        $this->assertRegExp('%Update%', $content);
+        $this->assertRegExp('%</form>%', $content);
+    }
+
+    private function whenIDisplayAdminFormElement($formElement)
+    {
+        $GLOBALS['Language']->shouldReceive('getText')->withArgs([
+            'plugin_tracker_include_type', 'upd_label', null
+        ])->andReturns('Update');
+        $GLOBALS['HTML'] = $GLOBALS['Response'];
+        $GLOBALS['HTML']->shouldReveive('getFactoryIconUseIt');
+
+        $tracker_manager = Mockery::mock(TrackerManager::class);
+        $user            = Mockery::mock(PFUser::class);
+        $request         = Mockery::mock(HTTPRequest::class);
+        $request->shouldReceive('isAjax');
+        ob_start();
+        $formElement->displayAdminFormElement($tracker_manager, $request, $user);
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+}
---
+++ plugins/tracker/phpunit/Tracker/FormElement/TrackerFormElementUserPermissionsTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Tracker\FormElement;
+
+use Mockery;
+use PFUser;
+use PHPUnit\Framework\TestCase;
+use Tracker_FormElement_Field_String;
+use Tracker_Workflow_WorkflowUser;
+use Tuleap\GlobalLanguageMock;
+
+class TrackerFormElementUserPermissionsTest extends TestCase
+{
+    use GlobalLanguageMock;
+
+    private $user;
+    private $form_element;
+    private $workflow_user;
+
+    protected function setUp() : void
+    {
+        $this->form_element = Mockery::mock(Tracker_FormElement_Field_String::class)->makePartial();
+        $this->form_element->shouldReceive('getId')->andReturn(300);
+        $this->form_element->shouldReceive('getLabel')->andReturn("My field");
+        $this->form_element->shouldReceive('getName')->andReturn('my_field');
+
+        $this->user = Mockery::mock(PFUser::class);
+        $this->workflow_user = new Tracker_Workflow_WorkflowUser();
+    }
+
+    public function testItGrantsReadAccessToWorkflowUser()
+    {
+        $this->assertTrue($this->form_element->userCanRead($this->workflow_user));
+    }
+
+    public function testItGrantsUpdateAccessToWorkflowUser()
+    {
+        $this->assertTrue($this->form_element->userCanUpdate($this->workflow_user));
+    }
+
+    public function testItGrantsSubmitAccessToWorkflowUser()
+    {
+        $this->assertTrue($this->form_element->userCanSubmit($this->workflow_user));
+    }
+}
