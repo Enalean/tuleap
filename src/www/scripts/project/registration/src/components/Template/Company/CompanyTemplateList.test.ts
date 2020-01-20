4489e7b0d99ee1a0d8c6f3c600e603f76d6ae673
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2020-01-20 13:44:09 +0100

    request #14420 Custom template should not displayed if platform does not have any templates
    How to test:
    Make sure your platform does not provide any templates.
    
    create a new project with /project/new url
     => company template section must not be displayed
    
    Change-Id: Ie1036a2e2f1766b2b9c38b71b3072f8bbdbbffd8

--- src/www/scripts/project/registration/src/components/Template/Company/CompanyTemplateList.test.ts
+++ src/www/scripts/project/registration/src/components/Template/Company/CompanyTemplateList.test.ts
@@ -25,68 +25,99 @@ import { createProjectRegistrationLocalVue } from "../../../helpers/local-vue-fo
 import TemplateCardContent from "../TemplateCard.vue";
 import CompanyTemplateList from "./CompanyTemplateList.vue";
 import { State } from "../../../store/type";
+import { TemplateData } from "../../../type";
 
 describe("CompanyTemplateList", () => {
     let local_vue = createLocalVue();
     let store: Store;
     let wrapper: Wrapper<CompanyTemplateList>;
 
-    beforeEach(async () => {
-        const company_templates = [
-            {
-                id: "10",
-                title: "scrum",
-                description: "scrum desc",
-                glyph: "<svg></svg>",
-                is_built_in: false
-            },
-            {
-                id: "11",
-                title: "kanban",
-                description: "kanban desc",
-                glyph: "<svg>kanban</svg>",
-                is_built_in: false
-            }
-        ];
+    describe("has no templates", () => {
+        beforeEach(async () => {
+            const company_templates: TemplateData[] = [];
+            const state: State = {
+                company_templates: company_templates,
+                default_project_template: null,
+                company_name: ""
+            } as State;
 
-        const state: State = {
-            company_templates: company_templates,
-            default_project_template: null,
-            company_name: ""
-        } as State;
+            const store_options = {
+                state
+            };
+            store = createStoreMock(store_options);
+            local_vue = await createProjectRegistrationLocalVue();
 
-        const store_options = {
-            state
-        };
-        store = createStoreMock(store_options);
-        local_vue = await createProjectRegistrationLocalVue();
+            wrapper = shallowMount(CompanyTemplateList, {
+                localVue: local_vue,
+                mocks: { $store: store }
+            });
+        });
 
-        wrapper = shallowMount(CompanyTemplateList, {
-            localVue: local_vue,
-            mocks: { $store: store }
+        it(`display nothing when company hasn't defined any templates`, () => {
+            expect(
+                wrapper.contains("[data-test=project-registration-company-template-title]")
+            ).toBe(false);
         });
     });
 
-    it(`spawns the component and sub component`, () => {
-        expect(wrapper.contains(TemplateCardContent)).toBe(true);
-        expect(wrapper.findAll(TemplateCardContent)).toHaveLength(2);
-    });
+    describe("has several project templates -", () => {
+        beforeEach(async () => {
+            const company_templates = [
+                {
+                    id: "10",
+                    title: "scrum",
+                    description: "scrum desc",
+                    glyph: "<svg></svg>",
+                    is_built_in: false
+                },
+                {
+                    id: "11",
+                    title: "kanban",
+                    description: "kanban desc",
+                    glyph: "<svg>kanban</svg>",
+                    is_built_in: false
+                }
+            ];
 
-    it(`displays the company name if the platform name is not Tuleap`, () => {
-        wrapper.vm.$store.state.company_name = "Nichya company";
-        expect(
-            wrapper
-                .find("[data-test=project-registration-company-template-title]")
-                .element.innerHTML.trim()
-        ).toEqual("Nichya company templates");
-    });
+            const state: State = {
+                company_templates: company_templates,
+                default_project_template: null,
+                company_name: ""
+            } as State;
 
-    it(`displays 'Custom templates' if the platform name is Tuleap`, () => {
-        wrapper.vm.$store.state.company_name = "Tuleap";
-        expect(
-            wrapper
-                .find("[data-test=project-registration-company-template-title]")
-                .element.innerHTML.trim()
-        ).toEqual("Custom templates");
+            const store_options = {
+                state
+            };
+            store = createStoreMock(store_options);
+            local_vue = await createProjectRegistrationLocalVue();
+
+            wrapper = shallowMount(CompanyTemplateList, {
+                localVue: local_vue,
+                mocks: { $store: store }
+            });
+        });
+
+        it(`spawns the component and sub component`, () => {
+            expect(wrapper.contains(TemplateCardContent)).toBe(true);
+            expect(wrapper.findAll(TemplateCardContent)).toHaveLength(2);
+        });
+
+        it(`displays the company name if the platform name is not Tuleap`, () => {
+            wrapper.vm.$store.state.company_name = "Nichya company";
+            expect(
+                wrapper
+                    .find("[data-test=project-registration-company-template-title]")
+                    .element.innerHTML.trim()
+            ).toEqual("Nichya company templates");
+        });
+
+        it(`displays 'Custom templates' if the platform name is Tuleap`, () => {
+            wrapper.vm.$store.state.company_name = "Tuleap";
+            expect(
+                wrapper
+                    .find("[data-test=project-registration-company-template-title]")
+                    .element.innerHTML.trim()
+            ).toEqual("Custom templates");
+        });
     });
 });
