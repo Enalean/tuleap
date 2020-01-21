67ae13da2057928e2974e5c2e3d4fcbeb4bc4335
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2020-01-21 11:17:12 +0100

    part of request #14426 admin should not be explicitly defined in xml to be activated on new project
    How to reproduce :
    have a xml without admin service
    => import your project
    => admin service must be present and active
    
    Or make test_cypress should end without any error in test_results_e2e_full
    
    Same logic will be done for summary service
    
    Change-Id: Ic16e488900286ca06e734fcb526721ac1e8ae45b

---
+++ src/common/Project/ProjectCreationDataServiceFromXmlInheritor.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Tuleap\Project;
+
+use Project;
+use Service;
+use ServiceManager;
+use SimpleXMLElement;
+
+class ProjectCreationDataServiceFromXmlInheritor
+{
+    /**
+     * @var ServiceManager
+     */
+    private $service_manager;
+
+    public function __construct(ServiceManager $service_manager)
+    {
+        $this->service_manager = $service_manager;
+    }
+
+    /**
+     * Read the template and XML and mark services as being in use if they are
+     * allowed in the template and enabled in the XML.
+     */
+    public function markUsedServicesFromXML(
+        SimpleXMLElement $xml,
+        Project $template
+    ): array {
+        $data_services = [];
+
+        $services_by_name = [];
+        foreach ($this->service_manager->getListOfAllowedServicesForProject($template) as $template_service) {
+            $services_by_name[$template_service->getShortName()] = $template_service;
+        }
+
+        foreach ($xml->services->children() as $service) {
+            if (! ($service instanceof SimpleXMLElement)) {
+                continue;
+            }
+            if ($service->getName() !== "service") {
+                continue;
+            }
+            $attrs = $service->attributes();
+
+            if (! isset($attrs['shortname']) || ! isset($attrs['enabled'])) {
+                continue;
+            }
+
+            $name = (string) $attrs['shortname'];
+
+            $enabled = \Tuleap\XML\PHPCast::toBoolean($attrs['enabled']);
+            if (isset($services_by_name[$name])) {
+                $service_id                 = $services_by_name[$name]->getId();
+                $data_services[$service_id] = ['is_used' => $enabled];
+            }
+        }
+
+        return $this->forceAdminServiceUsage($services_by_name, $data_services);
+    }
+
+    protected function forceAdminServiceUsage(array $services_by_name, array $data_services): array
+    {
+        $service_id                 = $services_by_name[Service::ADMIN]->getId();
+        $data_services[$service_id] = ['is_used' => true];
+
+        return $data_services;
+    }
+}
