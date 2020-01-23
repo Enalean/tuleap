81ccf9c28a996cf48e68a6f025fd399ea4ec1b95
Martin Goyot <martin.goyot@enalean.com>
2020-01-23 17:35:30 +0100

    request #14436: Cypress svn test for distlp are broken with removal of default Scrum template
    Change-Id: I2565c8f6d6407b46192b293bc516d70754443fb0

--- tests/e2e/distlp/cypress/support/commands.js
+++ tests/e2e/distlp/cypress/support/commands.js
@@ -22,3 +22,8 @@ Cypress.Commands.add("login", () => {
     cy.get("#form_loginname").type("alice");
     cy.get("#form_pw").type("Correct Horse Battery Staple{enter}");
 });
+
+Cypress.Commands.add("getProjectId", project_shortname => {
+    cy.visit(`/projects/${project_shortname}/`);
+    return cy.get("[data-test=project-sidebar]").should("have.attr", "data-project-id");
+});
