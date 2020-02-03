b29e7a3f1b17ef51086d4c697a30e2cf890924dc
Joris Masson <joris.masson@enalean.com>
2020-02-03 13:48:17 +0100

    request #14472 Update gulp in trackers to v4.x
    How to test:
    
    If you are using Node.js 12.x, "npm run watch" should no longer fail in
    plugins/tracker/www/scripts/
    
    No other functional change expected.
    
    Change-Id: I725d0ae5eb096f3498136cb288587c321b7829c4

--- plugins/tracker/www/scripts/gulpfile.js
+++ plugins/tracker/www/scripts/gulpfile.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const gulp = require("gulp");
 const gettext = require("gulp-angular-gettext");
 const path = require("path");
@@ -5,15 +24,7 @@ const path = require("path");
 const templates_with_translated_strings_glob = "src/**/*.tpl.html";
 const javascript_with_translated_strings_glob = "src/**/*.js";
 
-gulp.task("watch", function() {
-    return gulp.watch(
-        [templates_with_translated_strings_glob, javascript_with_translated_strings_glob],
-        { cwd: path.resolve("./angular-artifact-modal") },
-        ["gettext-extract"]
-    );
-});
-
-gulp.task("gettext-extract", function() {
+function extractGettext() {
     return gulp
         .src([templates_with_translated_strings_glob, javascript_with_translated_strings_glob], {
             cwd: path.resolve("./angular-artifact-modal"),
@@ -25,4 +36,16 @@ gulp.task("gettext-extract", function() {
             })
         )
         .pipe(gulp.dest("angular-artifact-modal/po/"));
-});
+}
+
+function watchTask() {
+    gulp.watch(
+        [templates_with_translated_strings_glob, javascript_with_translated_strings_glob],
+        { cwd: path.resolve("./angular-artifact-modal") },
+        extractGettext
+    );
+}
+
+module.exports = {
+    watch: watchTask
+};
