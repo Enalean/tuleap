Enalean/tuleap

commit 1ed9c87020989a27b78f327d17adedad7f1222b3

Glossify new project creation page - Step 1
* Add a page max-width
* Title is now under the svg in small screen size view
* Add more space between template sections
* Fix extra space after .register-new-project-section-badge
* Remove subtitle in project services modal
* ALign card content

Part of request #14405 - Glossify new project creation

Change-Id: I878cd77c686664c667ef82ec6920e34494b30c03

src/www/scripts/project/registration/src/components/Information/ProjectInformation.vue
@@ -19,7 +19,7 @@
   -->
 
 <template>
-    <div>
+    <div class="project-registration-content">
         <div class="tlp-alert-danger" v-if="has_error" data-test="project-creation-failed">
             {{ error }}
         </div>

src/glyphs/issues.svg
@@ -1,7 +1,7 @@
 <svg width="74" height="46" viewBox="0 0 74 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="1.5" y="1.5" width="9" height="9" rx="1.5" class="project-registration-template-card-svg" stroke-width="3"/>
-    <rect x="1.5" y="35.5" width="9" height="9" rx="1.5" class="project-registration-template-card-svg" stroke-width="3"/>
+    <rect x="1.5" y="1.5" width="9" height="9" rx="1.5" class="project-registration-template-card-svg-stroke" stroke-width="3"/>
+    <rect x="1.5" y="35.5" width="9" height="9" rx="1.5" class="project-registration-template-card-svg-stroke" stroke-width="3"/>
     <path class="project-registration-template-card-svg" fill-rule="evenodd" clip-rule="evenodd" d="M0 22.7445C0.128905 22.9901 0.295605 23.2181 0.500102 23.4181L3 25.7712V26H3.24311L5.66667 28.2812L7.93748 26H9V24.9326L12 21.9188V26C12 27.6569 10.6569 29 9 29H3C1.34315 29 2.58456e-08 27.6569 2.58456e-08 26L0 22.7445ZM0 20.026C0.128916 19.7804 0.295634 19.5524 0.500152 19.3524C1.60664 18.2705 3.73032 18.2705 4.8368 19.3524L5.66667 20L8.81597 17H3C1.34315 17 2.58456e-08 18.3431 2.58456e-08 20L0 20.026Z"/>
-    <path d="M3 21.6667L5.5 24L13 17" class="project-registration-template-card-svg-stroke" stroke-width="2.5" stroke-linecap="round"/>
+    <path d="M3 21.6667L5.5 24L13 17" class="project-registration-template-card-svg-stroke" stroke-width="2.5" stroke-linecap=" round"/>
     <path class="project-registration-template-card-svg" fill-rule="evenodd" clip-rule="evenodd" d="M19 6C19 4.89543 19.8954 4 21 4H72C73.1046 4 74 4.89543 74 6C74 7.10457 73.1046 8 72 8H21C19.8954 8 19 7.10457 19 6ZM19 23C19 21.8954 19.8954 21 21 21H50C51.1046 21 52 21.8954 52 23C52 24.1046 51.1046 25 50 25H21C19.8954 25 19 24.1046 19 23ZM21 38C19.8954 38 19 38.8954 19 40C19 41.1046 19.8954 42 21 42H61C62.1046 42 63 41.1046 63 40C63 38.8954 62.1046 38 61 38H21Z"/>
 </svg>

src/www/scripts/project/registration/po/fr_FR.po
@@ -208,7 +208,7 @@ msgid "What kind of project is it?"
 msgstr "Quel type de projet est-ce ?"
 
 msgid "What's inside?"
-msgstr "Qu'y a-t-il a l'intérieur ?"
+msgstr "Qu'y a-t-il à l'intérieur ?"
 
 msgid "You are not administrator of any project."
 msgstr "Vous êtes administrateur d'aucun projet."

src/www/scripts/project/registration/src/components/Template/Advanced/AdvancedTemplateList.vue
@@ -19,7 +19,7 @@
   -->
 
 <template>
-    <div>
+    <div class="project-registration-templates">
         <h3 data-test="project-registration-company-template-title" v-translate>
             For advanced users
         </h3>

src/www/scripts/project/registration/src/components/Template/AllProjectTemplates.vue
@@ -19,12 +19,11 @@
   -->
 
 <template>
-    <div>
-        <h1 class="project-registration-title" v-translate>Start a new project</h1>
-
+    <div class="project-registration-content">
         <div class="register-new-project-section">
             <new-project-boxes />
             <div class="register-new-project-list">
+                <h1 class="project-registration-title" v-translate>Start a new project</h1>
                 <h2>
                     <span class="tlp-badge-primary register-new-project-section-badge">1</span>
                     <span v-translate>What kind of project is it?</span>

src/www/scripts/project/registration/src/components/Template/Company/CompanyTemplateList.vue
@@ -19,7 +19,7 @@
   -->
 
 <template>
-    <div>
+    <div class="project-registration-templates">
         <h3 data-test="project-registration-company-template-title">
             {{ platform_template_name }}
         </h3>

src/www/scripts/project/registration/src/components/Template/NewProjectBoxesSvg.vue
@@ -18,7 +18,7 @@
   -->
 
 <template functional>
-    <div class="register-new-project-box-spacing">
+    <div class="register-new-project-boxes">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -139,7 +139,7 @@
                 d="M117.993 59.1034L121.734 54.8356H94.4399L101.559 51.3161H124.819L127.802 47.9137L139.45 47.9271L136.348 51.3161H178.532L175.43 47.9271L187.078 47.9137L190.061 51.3161H213.321L220.44 54.8356H193.146L196.887 59.1034L185.658 59.1034L181.752 54.8356H133.128L129.222 59.1034L117.993 59.1034Z"
                 clip-rule="evenodd"
                 fill-rule="evenodd"
-                class="tlp-illustration-primary-box"
+                class="tlp-illustration-primary-fill"
             ></path>
         </svg>
     </div>

src/www/scripts/project/registration/src/components/Template/Services/ProjectServices.test.ts
@@ -47,7 +47,7 @@ describe("ProjectServices", () => {
     it("Displays a spinner when services are loading", () => {
         wrapper.vm.$data.is_loading = true;
 
-        expect(wrapper.find("[data-test=project-service-link]").exists()).toBeFalsy();
+        expect(wrapper.find("[data-test=project-service-link]").exists()).toBeTruthy();
         expect(wrapper.find("[data-test=project-service-spinner]").exists()).toBeTruthy();
         expect(wrapper.find("[data-test=project-service-error]").exists()).toBeFalsy();
     });

commit b922c5e62d0a0dd95652eaa20bfb50bb56ae6b4a

request #14500 error is no longer properly displayed in project creation
Introduced by 1ed9c87020989a27b78f327d17adedad7f1222b3

Change-Id: If27b629a89fc7b2bee5c27de66251e0a807fab4d

src/www/scripts/project/registration/src/components/Information/ProjectInformation.vue
@@ -19,46 +19,50 @@
   -->
 
 <template>
-    <div class="project-registration-content">
+    <div>
         <div class="tlp-alert-danger" v-if="has_error" data-test="project-creation-failed">
             {{ error }}
         </div>
 
-        <form v-on:submit.prevent="createProject" data-test="project-registration-form">
-            <div class="register-new-project-section ">
-                <project-information-svg />
-                <div class="register-new-project-list register-new-project-information">
-                    <h1 class="project-registration-title" v-translate>Start a new project</h1>
-                    <h2>
-                        <span class="tlp-badge-primary register-new-project-section-badge">2</span>
-                        <span v-translate>Project information</span>
-                    </h2>
-                    <div
-                        class="register-new-project-information-form-container"
-                        data-test="register-new-project-information-form"
-                    ></div>
-                    <project-name v-model="name_properties" />
-                    <project-information-input-privacy-list
-                        v-model="selected_visibility"
-                        data-test="register-new-project-information-list"
-                    />
-                    <field-description v-model="field_description" />
-                    <trove-category-list
-                        v-model="trove_cats"
-                        v-for="trovecat in trove_categories"
-                        v-bind:key="trovecat.id"
-                        v-bind:trovecat="trovecat"
-                    />
-                    <fields-list
-                        v-for="field in project_fields"
-                        v-bind:key="field.group_desc_id + field.desc_name"
-                        v-bind:field="field"
-                    />
-                    <policy-agreement />
-                    <project-information-footer />
+        <div class="project-registration-content">
+            <form v-on:submit.prevent="createProject" data-test="project-registration-form">
+                <div class="register-new-project-section ">
+                    <project-information-svg />
+                    <div class="register-new-project-list register-new-project-information">
+                        <h1 class="project-registration-title" v-translate>Start a new project</h1>
+                        <h2>
+                            <span class="tlp-badge-primary register-new-project-section-badge">
+                                2
+                            </span>
+                            <span v-translate>Project information</span>
+                        </h2>
+                        <div
+                            class="register-new-project-information-form-container"
+                            data-test="register-new-project-information-form"
+                        ></div>
+                        <project-name v-model="name_properties" />
+                        <project-information-input-privacy-list
+                            v-model="selected_visibility"
+                            data-test="register-new-project-information-list"
+                        />
+                        <field-description v-model="field_description" />
+                        <trove-category-list
+                            v-model="trove_cats"
+                            v-for="trovecat in trove_categories"
+                            v-bind:key="trovecat.id"
+                            v-bind:trovecat="trovecat"
+                        />
+                        <fields-list
+                            v-for="field in project_fields"
+                            v-bind:key="field.group_desc_id + field.desc_name"
+                            v-bind:field="field"
+                        />
+                        <policy-agreement />
+                        <project-information-footer />
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 