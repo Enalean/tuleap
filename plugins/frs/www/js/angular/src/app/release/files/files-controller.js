f991ac0284f13afdb6394f9f9eec0a3246d68ba1
Joris Masson <joris.masson@enalean.com>
2020-02-24 17:27:58 +0100

    Add empty state for files and release notes
    Part of request #14539 Move FRS plugin to BurningParrot
    
    How to test:
    - run "npm run build" in plugins/frs/www/js/angular
    - Create a FRS release with no files, no links, no release notes and no
    changelog.
    - Browse it in the FRS plugin. It should display an empty state.
    
    Change-Id: I4f25c91b5b0fb94afb3ab1b3bc9a9d3824b6e712

--- plugins/frs/www/js/angular/src/app/release/files/files-controller.js
+++ plugins/frs/www/js/angular/src/app/release/files/files-controller.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) Enalean, 2016-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 export default FilesController;
 
 FilesController.$inject = ["SharedPropertiesService"];
@@ -7,6 +26,14 @@ function FilesController(SharedPropertiesService) {
 
     Object.assign(self, {
         release: SharedPropertiesService.getRelease(),
-        custom_license_agreement: SharedPropertiesService.getCustomLicenseAgreement()
+        custom_license_agreement: SharedPropertiesService.getCustomLicenseAgreement(),
+        isEmpty() {
+            return (
+                self.release.files.length === 0 &&
+                self.release.links.length === 0 &&
+                !self.release.release_note &&
+                !self.release.changelog
+            );
+        }
     });
 }
---
+++ plugins/frs/www/js/angular/src/app/release/files/files-controller.test.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import angular from "angular";
+import tuleap_frs_module from "../../app";
+import BaseController from "./files-controller";
+
+import "angular-mocks";
+
+describe(`FilesController`, () => {
+    let $controller, SharedPropertiesService;
+    beforeEach(() => {
+        angular.mock.module(tuleap_frs_module);
+        angular.mock.inject(function(_$rootScope_, _$controller_, _SharedPropertiesService_) {
+            $controller = _$controller_;
+            SharedPropertiesService = _SharedPropertiesService_;
+        });
+
+        jest.spyOn(SharedPropertiesService, "getRelease").mockImplementation(() => {});
+        jest.spyOn(
+            SharedPropertiesService,
+            "getCustomLicenseAgreement"
+        ).mockImplementation(() => {});
+    });
+
+    describe(`isEmpty()`, () => {
+        it(`when there are files in the release, it will return false`, () => {
+            const release = {
+                files: [{ id: 12 }],
+                links: [],
+                release_note: "",
+                changelog: ""
+            };
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+
+            const FilesController = $controller(BaseController);
+            expect(FilesController.isEmpty()).toBe(false);
+        });
+
+        it(`when there are links in the release, it will return false`, () => {
+            const release = {
+                files: [],
+                links: [{ id: 23 }],
+                release_note: "",
+                changelog: ""
+            };
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+
+            const FilesController = $controller(BaseController);
+            expect(FilesController.isEmpty()).toBe(false);
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+        });
+
+        it(`when there is a release note, it will return false`, () => {
+            const release = {
+                files: [],
+                links: [],
+                release_note: "Lorem ipsum dolor sit amet",
+                changelog: ""
+            };
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+
+            const FilesController = $controller(BaseController);
+            expect(FilesController.isEmpty()).toBe(false);
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+        });
+
+        it(`when there is a changelog, it will return false`, () => {
+            const release = {
+                files: [],
+                links: [],
+                release_note: "",
+                changelog: "Lorem ipsum dolor sit amet"
+            };
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+
+            const FilesController = $controller(BaseController);
+            expect(FilesController.isEmpty()).toBe(false);
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+        });
+
+        it(`when there is none of the above, it will return true`, () => {
+            const release = {
+                files: [],
+                links: [],
+                release_note: "",
+                changelog: ""
+            };
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+
+            const FilesController = $controller(BaseController);
+            expect(FilesController.isEmpty()).toBe(true);
+            jest.spyOn(SharedPropertiesService, "getRelease").mockReturnValue(release);
+        });
+    });
+});
