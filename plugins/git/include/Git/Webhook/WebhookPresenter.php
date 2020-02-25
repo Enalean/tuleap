0a11d36d7c340b63fdd7ab45f6ff261e4c58649c
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2020-02-25 14:21:27 +0100

    Display project servers in repository admin view
    This is part of story #14019 automate jenkins job creation & run
    
    Jenkins project servers are now display in a simple way in the webhook
    section of the git repository administration.
    
    Change-Id: I08d8cf202fd8eebdec6df88d2d064cff01106300

--- plugins/git/include/Git/Webhook/WebhookPresenter.php
+++ plugins/git/include/Git/Webhook/WebhookPresenter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2016. All Rights Reserved.
+ * Copyright (c) Enalean, 2020 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -20,56 +20,7 @@
 
 namespace Tuleap\Git\Webhook;
 
-use GitRepository;
-use CSRFSynchronizerToken;
-
-class WebhookPresenter
+interface WebhookPresenter
 {
-    public $use_default_edit_modal;
-    public $edit_modal;
-    public $repository_id;
-    public $hooklogs;
-    public $csrf_token;
-    public $webhook_url;
-    public $last_push_info;
-    public $modal_logs_time_label;
-    public $modal_logs_info_label;
-    public $logs_for;
-    public $empty_logs;
-    public $remove_webhook_desc;
-    public $remove_webhook_confirm;
-    public $remove_form_action;
-    public $id;
-
-    public function __construct(
-        GitRepository $repository,
-        $id,
-        $webhook_url,
-        array $hooklogs,
-        CSRFSynchronizerToken $csrf,
-        $use_default_edit_modal
-    ) {
-        $this->id                      = $id;
-        $this->webhook_url             = $webhook_url;
-        $this->purified_last_push_info = '';
-        $this->hooklogs                = $hooklogs;
-        $this->csrf_token              = $csrf->getToken();
-        $this->use_default_edit_modal  = $use_default_edit_modal;
-
-        $this->repository_id = $repository->getId();
-
-        $this->modal_logs_time_label = dgettext('tuleap-git', 'Time');
-        $this->modal_logs_info_label = dgettext('tuleap-git', 'Return type');
-
-        $this->remove_webhook_desc    = dgettext('tuleap-git', 'You are about to remove the webhook. Please confirm your action.');
-        $this->remove_webhook_confirm = dgettext('tuleap-git', 'Confirm deletion');
-        $this->remove_form_action     = GIT_BASE_URL .'/?group_id='. (int)$repository->getProjectId();
-
-        $this->logs_for   = sprintf(dgettext('tuleap-git', 'Logs for %1$s'), $webhook_url);
-        $this->empty_logs = dgettext('tuleap-git', 'No logs yet');
-
-        if (count($hooklogs) > 0) {
-            $this->purified_last_push_info = $hooklogs[0]->purified_information;
-        }
-    }
+    public function isSimple(): bool;
 }
