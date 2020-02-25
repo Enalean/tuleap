07668864cdae846bdd5b34383b7de6aaf07df4de
benjamin Bouillot <benjamin.bouillot@enalean.com>
2020-02-25 11:20:44 +0100

    story #14581 Project Milestones doesn't display under edge
    The widget didn't display under edge beceause spread is not (yet) supported by Edge.
    (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax)
    
    How to test:
    	- Have project milestones plugin
    	- Test under edge
    
    Expected results:
    	- The widget is displayed (as on chrome or firefox)
    
    Change-Id: I87f8207cf160bac9b28c435ed3edb23c416dee89

--- src/www/scripts/charts-builders/chart-dates-service.js
+++ src/www/scripts/charts-builders/chart-dates-service.js
@@ -70,11 +70,10 @@ function getDifference(start_date, end_date) {
 
 function getFormattedDates(dataset) {
     return dataset.map(data => {
-        return {
-            ...data,
-            date: moment(data.date, moment.ISO_8601)
-                .utc()
-                .format("YYYY-MM-DD")
-        };
+        const new_data = Object.assign({}, data);
+        new_data.date = moment(data.date, moment.ISO_8601)
+            .utc()
+            .format("YYYY-MM-DD");
+        return new_data;
     });
 }
