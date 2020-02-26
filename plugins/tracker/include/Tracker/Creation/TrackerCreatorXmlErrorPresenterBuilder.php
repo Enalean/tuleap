967caf5434490cf50f6c13f45c74bb60330dde27
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2020-02-26 09:02:31 +0100

    Refactoring of TrackerCreatorXmlDisplayer
    part of story #14291 Beautiful tracker creation
    
    This change aims to increase readability and maintenance of xml error display for tracker creation UI.
    
    The string render have been extracted into a mustache file.
    It also correct a bug:
     - when you file has more than a single error, before only the first error was displayed
     - with this patch, every single line in error will have its associated error displayed
    
    Please test that display is correctly rendered in legacy and new tracker creation UI
    
    Change-Id: Ic71e7d31f426ba531743e082fef237920b45187f

---
+++ plugins/tracker/include/Tracker/Creation/TrackerCreatorXmlErrorPresenterBuilder.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Tuleap\Tracker\Creation;
+
+use XML_ParseError;
+
+class TrackerCreatorXmlErrorPresenterBuilder
+{
+    /**
+     * protected for testing purpose
+     */
+    public function buildErrorLineDiff(array $xml_file, array $errors): TrackerCreatorXmlErrorPresenter
+    {
+        $lines = [];
+        foreach ($xml_file as $number => $line) {
+            $next_line                      = (int) $number + 1;
+            $lines[$number]['line_id']      = $next_line;
+            $lines[$number]['line_content'] = $line;
+            $lines[$number]['has_error']    = false;
+
+            if (! isset($errors[$next_line])) {
+                continue;
+            }
+
+            foreach ($errors[$next_line] as $column => $error) {
+                $lines[$number]['has_error'] = true;
+                $error_message               = $this->getErrorMessagesForLine($error);
+
+                if (count($error_message) > 0) {
+                    $lines[$number]['error_message']    = implode(' - ', $error_message);
+                    $lines[$number]['add_extra_spaces'] = $this->getAlwaysThereSpaces()
+                        . $this->getSpacesAccordingToPreviousLineSize($column);
+                }
+            }
+        }
+
+        return new TrackerCreatorXmlErrorPresenter($lines);
+    }
+
+    private function getAlwaysThereSpaces(): string
+    {
+        return sprintf('%3s', '');
+    }
+
+    private function getSpacesAccordingToPreviousLineSize(int $column): string
+    {
+        return sprintf('%' . ($column - 1) . 's', '');
+    }
+
+    private function getErrorMessagesForLine(array $error): array
+    {
+        $error_message = [];
+        foreach ($error as $parse_error) {
+            $error_message[] = $parse_error->getMessage();
+        }
+
+        return $error_message;
+    }
+
+    public function buildErrors(array $parse_errors): array
+    {
+        $errors = [];
+        foreach ($parse_errors as $error) {
+            /** @var XML_ParseError $error */
+            $errors[$error->getLine()][$error->getColumn()][] = $error;
+        }
+
+        return $errors;
+    }
+}
---
+++ plugins/tracker/phpunit/Tracker/Creation/TrackerCreatorXmlErrorPresenterBuilderTest.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020 - present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace Tuleap\Tracker\Creation;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+use XML_ParseError;
+
+class TrackerCreatorXmlErrorPresenterBuilderTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var TrackerCreatorXmlErrorDisplayer
+     */
+    private $displayer;
+
+    protected function setUp(): void
+    {
+        $this->displayer = new TrackerCreatorXmlErrorPresenterBuilder();
+    }
+
+    public function testItBuildAndCollectXmlError(): void
+    {
+        $error_invalid   = new XML_ParseError(1, 2, 'type', 'invalid line');
+        $same_line_error = new XML_ParseError(1, 2, 'type', 'other error on same line');
+        $error_required  = new XML_ParseError(10, 20, 'required', 'missing required property');
+        $errors          = [
+            $error_invalid,
+            $same_line_error,
+            $error_required
+        ];
+
+        $expected [1][2]   = [$error_invalid, $same_line_error];
+        $expected [10][20] = [$error_required];
+
+        $this->assertEquals($expected, $this->displayer->buildErrors($errors));
+    }
+
+    public function testItBuildAStringOfXmlLinesAndBreaksOnTheFirstErrorSeen(): void
+    {
+        $xml_file[] = '<project unix-name="taskboard" full-name="taskboard" access="public">';
+        $xml_file[] = '<services>';
+        $xml_file[] = '<service enabled="1"/>';
+        $xml_file[] = '<service shortname="taskboard"/>';
+        $xml_file[] = '<service shortname="mail" enabled="0"/>';
+        $xml_file[] = '</services>';
+        $xml_file[] = '</project>';
+
+        $error       = new XML_ParseError(3, 12, 'type', 'missing shortname attributes');
+        $other_error = new XML_ParseError(4, 12, 'type', 'missing enable attributes');
+
+        $errors = $this->displayer->buildErrors([$error, $other_error]);
+
+        $expected_lines[] = [
+            'line_id'      => 1,
+            'line_content' => '<project unix-name="taskboard" full-name="taskboard" access="public">',
+            'has_error'    => false
+        ];
+
+        $expected_lines[] = [
+            'line_id'      => 2,
+            'line_content' => '<services>',
+            'has_error'    => false
+        ];
+
+        $expected_lines[] = [
+            'line_id'          => 3,
+            'line_content'     => '<service enabled="1"/>',
+            'has_error'        => true,
+            'error_message'    => 'missing shortname attributes',
+            'add_extra_spaces' => '              '
+        ];
+
+        $expected_lines[] = [
+            'line_id'          => 4,
+            'line_content'     => '<service shortname="taskboard"/>',
+            'has_error'        => true,
+            'error_message'    => 'missing enable attributes',
+            'add_extra_spaces' => '              '
+        ];
+
+        $expected_lines[] = [
+            'line_id'      => 5,
+            'line_content' => '<service shortname="mail" enabled="0"/>',
+            'has_error'    => false
+        ];
+
+        $expected_lines[] = [
+            'line_id'      => 6,
+            'line_content' => '</services>',
+            'has_error'    => false
+        ];
+
+        $expected_lines[] = [
+            'line_id'      => 7,
+            'line_content' => '</project>',
+            'has_error'    => false
+        ];
+
+        $expected_text = new TrackerCreatorXmlErrorPresenter($expected_lines);
+
+        $built_string = $this->displayer->buildErrorLineDiff($xml_file, $errors);
+
+        $this->assertEquals($expected_text, $built_string);
+    }
+}
