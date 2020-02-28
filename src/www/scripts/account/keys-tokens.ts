Enalean/tuleap

commit 8ee7305214ab1dc17a865e556026d815278e4430

Transfer SSH Keys management to dedicated tab
Everything should work in dedicated tab as it worked in fat page.

The git/gerrit button was kept in the original page as it will be moved
in a dedicated page later on with all the gerrit related stuff.

Part of story #14552 User preferences goes Burning Parrot

Change-Id: I15ca599eaba8d26f9eb1fbaeb5698d946f9e670f

src/www/scripts/account/keys-tokens.ts
@@ -20,14 +20,87 @@
 import { modal as createModal, datePicker } from "tlp";
 
 document.addEventListener("DOMContentLoaded", () => {
-    addAccessKeyButton();
-    addAccessKeyDatePicker();
-    toggleButtonsAccordingToCheckBoxesStates();
+    handleSSHKeys();
+    handleAccessKeys();
 });
 
-function addAccessKeyButton(): void {
-    const button = document.getElementById("generate-access-key-button");
+function handleSSHKeys(): void {
+    addSSHKeyButton();
 
+    toggleButtonAccordingToCheckBoxesStateWithIds("remove-ssh-keys-button", "ssh_key_selected[]");
+
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const ssh_key = document.getElementById("ssh-key") as HTMLTextAreaElement;
+    ssh_key.addEventListener("input", () => {
+        const text = ssh_key.value;
+        if (!text) {
+            return;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        const button = document.getElementById("submit-new-ssh-key-button") as HTMLButtonElement;
+        if (text.trim() === "") {
+            button.disabled = true;
+        } else {
+            button.disabled = false;
+        }
+    });
+
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const ssh_keys_list = document.querySelectorAll("[data-ssh_key_value]") as NodeListOf<
+        HTMLTableRowElement
+    >;
+    ssh_keys_list.forEach(row => {
+        row.addEventListener("click", () => {
+            const full_ssh_key = row.getAttribute("data-ssh_key_value");
+            if (!full_ssh_key) {
+                return;
+            }
+            row.innerHTML = full_ssh_key;
+            row.className = "ssh-key-value-reset-cursor";
+        });
+    });
+}
+
+function addSSHKeyButton(): void {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const button = document.getElementById("add-ssh-key-button") as HTMLButtonElement;
+
+    popupModal(button);
+}
+
+function handleAccessKeys(): void {
+    addAccessKeyButton();
+    addAccessKeyDatePicker();
+
+    toggleButtonAccordingToCheckBoxesStateWithIds(
+        "button-revoke-access-tokens",
+        "access-keys-selected[]"
+    );
+    toggleButtonAccordingToCheckBoxesStateWithIds(
+        "generate-new-access-key-button",
+        "access-key-scopes[]"
+    );
+}
+
+function addAccessKeyButton(): void {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const button = document.getElementById("generate-access-key-button") as HTMLButtonElement;
+
+    popupModal(button);
+}
+
+function addAccessKeyDatePicker(): void {
+    const date_picker = document.getElementById("access-key-expiration-date-picker");
+
+    if (!date_picker) {
+        return;
+    }
+
+    datePicker(date_picker);
+}
+
+function popupModal(button: HTMLButtonElement): void {
     if (button && button.dataset) {
         const modal_target_id = button.dataset.targetModalId;
 
@@ -45,27 +118,6 @@ function addAccessKeyButton(): void {
             modal.show();
         });
     }
-}
-
-function addAccessKeyDatePicker(): void {
-    const date_picker = document.getElementById("access-key-expiration-date-picker");
-
-    if (!date_picker) {
-        return;
-    }
-
-    datePicker(date_picker);
-}
-
-function toggleButtonsAccordingToCheckBoxesStates(): void {
-    toggleButtonAccordingToCheckBoxesStateWithIds(
-        "button-revoke-access-tokens",
-        "access-keys-selected[]"
-    );
-    toggleButtonAccordingToCheckBoxesStateWithIds(
-        "generate-new-access-key-button",
-        "access-key-scopes[]"
-    );
 }
 
 function toggleButtonAccordingToCheckBoxesStateWithIds(

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -163,6 +163,9 @@ msgstr "Actions"
 msgid "Activate synchronized project membership management"
 msgstr "Activer la gestion synchronisée de l'appartenance au projet"
 
+msgid "Add SSH key"
+msgstr "Ajouter une clef SSH"
+
 msgid "Add a new service"
 msgstr "Ajouter un nouveau service"
 
@@ -180,6 +183,9 @@ msgstr "Ajouter un lien"
 
 msgid "Add member"
 msgstr "Ajouter un membre"
+
+msgid "Add new key"
+msgstr "Ajouter une nouvelle clef"
 
 msgid "Add parent project"
 msgstr "Ajouter projet parent"
@@ -553,6 +559,9 @@ msgstr "Supprimer le tableau de bord"
 msgid "Delete parent"
 msgstr "Supprimer le parent"
 
+msgid "Delete selected SSH keys"
+msgstr "Supprimer les clefs SSH sélectionnées"
+
 msgid "Delete service"
 msgstr "Supprimer le service"
 
@@ -806,6 +815,9 @@ msgstr "Erreur JpGraph pour le graphique \"%s\": %s"
 
 msgid "Kanban"
 msgstr "Kanban"
+
+msgid "Key"
+msgstr "Clef"
 
 msgid "Keys & Tokens"
 msgstr "Clefs et Tokens"
@@ -1371,6 +1383,12 @@ msgstr "Révoquer les clés d'accès sélectionnées"
 
 msgid "SOAP API"
 msgstr "API SOAP"
+
+msgid "SSH key"
+msgstr "Clef SSH"
+
+msgid "SSH keys"
+msgstr "Clefs SSH"
 
 msgid "SVN"
 msgstr "SVN"

src/common/Request/RouteCollector.php
@@ -108,6 +108,8 @@ use Tuleap\User\Account\UserAvatarSaver;
 use Tuleap\User\Profile\AvatarController;
 use Tuleap\User\Profile\ProfileController;
 use Tuleap\User\Profile\ProfilePresenterBuilder;
+use Tuleap\User\SSHKey\SSHKeyCreateController;
+use Tuleap\User\SSHKey\SSHKeyDeleteController;
 use Tuleap\Widget\WidgetFactory;
 use URLVerification;
 use User_ForgeUserGroupPermissionsDao;
@@ -268,6 +270,16 @@ class RouteCollector
             DisplayKeysTokensController::getCSRFToken(),
             AccessKeyPresenterBuilder::build(),
         );
+    }
+
+    public static function postAccountSSHKeyCreate(): DispatchableWithRequest
+    {
+        return new SSHKeyCreateController(DisplayKeysTokensController::getCSRFToken(), \UserManager::instance());
+    }
+
+    public static function postAccountSSHKeyDelete(): DispatchableWithRequest
+    {
+        return new SSHKeyDeleteController(DisplayKeysTokensController::getCSRFToken(), \UserManager::instance());
     }
 
     public static function postAccountAccessKeyCreate(): DispatchableWithRequest
@@ -550,6 +562,8 @@ class RouteCollector
 
         $r->addGroup('/account', function (FastRoute\RouteCollector $r) {
             $r->get('/keys-tokens', [self::class, 'getAccountToken']);
+            $r->post('/ssh_key/create', [self::class, 'postAccountSSHKeyCreate']);
+            $r->post('/ssh_key/delete', [self::class, 'postAccountSSHKeyDelete']);
             $r->post('/access_key/create', [self::class, 'postAccountAccessKeyCreate']);
             $r->post('/access_key/revoke', [self::class, 'postAccountAccessKeyRevoke']);
             $r->post('/avatar', [self::class, 'postAccountAvatar']);

src/common/User/Account/DisplayKeysTokensController.php
@@ -98,6 +98,7 @@ final class DisplayKeysTokensController implements DispatchableWithRequest, Disp
             'keys-tokens',
             new KeysTokensPresenter(
                 $this->csrf_token,
+                new SSHKeysPresenter($user),
                 $this->access_key_presenter_builder->getForUser($user, $_SESSION)
             )
         );

src/common/User/Account/SSHKeyPresenter.php
@@ -23,34 +23,28 @@ declare(strict_types=1);
 
 namespace Tuleap\User\Account;
 
-use CSRFSynchronizerToken;
-
 /**
  * @psalm-immutable
  */
-final class KeysTokensPresenter
+final class SSHKeyPresenter
 {
+    /**
+     * @var int
+     */
+    public $ssh_key_number;
     /**
      * @var string
      */
-    public $keys_tokens_url = DisplayKeysTokensController::URL;
+    public $ssh_key_value;
     /**
-     * @var CSRFSynchronizerToken
+     * @var string
      */
-    public $csrf_token;
-    /**
-     * @var SSHKeysPresenter
-     */
-    public $ssh_keys_presenter;
-    /**
-     * @var AccessKeyPresenter
-     */
-    public $access_key_presenter;
+    public $ssh_key_ellipsis_value;
 
-    public function __construct(CSRFSynchronizerToken $csrf_token, SSHKeysPresenter $ssh_keys_presenter, AccessKeyPresenter $access_key_presenter)
+    public function __construct(int $ssh_key_number, string $ssh_key_value)
     {
-        $this->csrf_token = $csrf_token;
-        $this->access_key_presenter = $access_key_presenter;
-        $this->ssh_keys_presenter = $ssh_keys_presenter;
+        $this->ssh_key_number         = $ssh_key_number;
+        $this->ssh_key_value          = $ssh_key_value;
+        $this->ssh_key_ellipsis_value = substr($ssh_key_value, 0, 40).'...'.substr($ssh_key_value, -40);
     }
 }

src/common/User/Account/SSHKeysPresenter.php
@@ -23,34 +23,27 @@ declare(strict_types=1);
 
 namespace Tuleap\User\Account;
 
-use CSRFSynchronizerToken;
+use PFUser;
 
 /**
  * @psalm-immutable
  */
-final class KeysTokensPresenter
+final class SSHKeysPresenter
 {
     /**
-     * @var string
+     * @var SSHKeyPresenter[]
      */
-    public $keys_tokens_url = DisplayKeysTokensController::URL;
+    public $ssh_keys_list = [];
     /**
-     * @var CSRFSynchronizerToken
+     * @var bool
      */
-    public $csrf_token;
-    /**
-     * @var SSHKeysPresenter
-     */
-    public $ssh_keys_presenter;
-    /**
-     * @var AccessKeyPresenter
-     */
-    public $access_key_presenter;
+    public $has_ssh_keys;
 
-    public function __construct(CSRFSynchronizerToken $csrf_token, SSHKeysPresenter $ssh_keys_presenter, AccessKeyPresenter $access_key_presenter)
+    public function __construct(PFUser $user)
     {
-        $this->csrf_token = $csrf_token;
-        $this->access_key_presenter = $access_key_presenter;
-        $this->ssh_keys_presenter = $ssh_keys_presenter;
+        foreach ($user->getAuthorizedKeysArray() as $ssh_key_number => $ssh_key_value) {
+            $this->ssh_keys_list[] = new SSHKeyPresenter($ssh_key_number, $ssh_key_value);
+        }
+        $this->has_ssh_keys = count($this->ssh_keys_list) > 0;
     }
 }

src/common/User/Account/templates/keys-tokens.mustache
@@ -3,6 +3,10 @@
     <a href="{{ keys_tokens_url }}" class="tlp-tab tlp-tab-active"> <i class="tlp-tab-icon fa fa-key"></i> {{# gettext }}Keys & tokens{{/ gettext }}</a>
 </nav>
 
+{{# ssh_keys_presenter }}
+    {{> ssh-keys }}
+{{/ ssh_keys_presenter }}
+
 {{# access_key_presenter }}
     {{> access-keys }}
 {{/ access_key_presenter }}

src/common/User/Account/templates/ssh-keys.mustache
@@ -1,0 +1,86 @@
+<section class="tlp-pane">
+    <div class="tlp-pane-container">
+        <div class="tlp-pane-header">
+            <h1 class="tlp-pane-title">{{# gettext }}SSH keys{{/ gettext }}</h1>
+        </div>
+        <section class="tlp-pane-section">
+            <form action="/account/ssh_key/delete" method="post">
+                {{# csrf_token }}
+                    {{> csrf_token_input }}
+                {{/ csrf_token }}
+
+                <div class="tlp-table-actions">
+                    <button
+                            type="button"
+                            class="tlp-button-primary tlp-table-actions-element"
+                            id="add-ssh-key-button"
+                            data-target-modal-id="ssh-keys-add-modal"
+                    >
+                        <i class="tlp-button-icon fa fa-plus"></i> {{# gettext }}Add SSH key{{/ gettext }}
+                    </button>
+                    {{# has_ssh_keys }}
+                        <button type="submit"
+                                id="remove-ssh-keys-button"
+                                name="remove-ssh-keys"
+                                class="tlp-button-danger tlp-button-outline tlp-table-actions-element"
+                                disabled
+                        >
+                            <i class="tlp-button-icon fa fa-trash-o"></i> {{# gettext }}Delete selected SSH keys{{/ gettext }}
+                        </button>
+                    {{/ has_ssh_keys }}
+                </div>
+
+                <table class="tlp-table ssh-keys-table">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>{{# gettext }}Key{{/ gettext }}</th>
+                    </tr>
+                    </thead>
+                        <tbody>
+                        {{# ssh_keys_list }}
+                            <tr>
+                                <td>
+                                    <input type="checkbox" name="ssh_key_selected[]" value="{{ ssh_key_number }}"/>
+                                </td>
+                                <td data-ssh_key_value="{{ ssh_key_value }}">
+                                    {{ ssh_key_ellipsis_value }}
+                                </td>
+                            </tr>
+                        {{/ ssh_keys_list }}
+                        </tbody>
+                </table>
+            </form>
+        </section>
+    </div>
+</section>
+
+
+<div id="ssh-keys-add-modal" class="tlp-modal" role="dialog" aria-labelledby="ssh-keys-add-modal-title">
+    <div class="tlp-modal-header">
+        <h1 class="tlp-modal-title" id="ssh-keys-add-modal-title">
+            <i class="fa fa-unlock-alt tlp-modal-title-icon"></i> {{# gettext }}Add SSH key{{/ gettext }}
+        </h1>
+        <div class="tlp-modal-close" data-dismiss="modal" aria-label="Close">
+            &times;
+        </div>
+    </div>
+
+    <form action="/account/ssh_key/create" method="post">
+        {{# csrf_token }}
+            {{> csrf_token_input }}
+        {{/ csrf_token }}
+
+        <div class="tlp-modal-body">
+
+            <div class="tlp-form-element">
+                <label class="tlp-label" for="ssh-key">{{# gettext }}SSH key{{/ gettext }}</label>
+                <textarea id="ssh-key" name="ssh-key" rows="5" class="tlp-textarea" required></textarea>
+            </div>
+        </div>
+        <div class="tlp-modal-footer">
+            <button type="button" class="tlp-button-primary tlp-button-outline tlp-modal-action" data-dismiss="modal">{{# gettext }}Cancel{{/ gettext }}</button>
+            <button type="submit" class="tlp-button-primary tlp-modal-action" id="submit-new-ssh-key-button" disabled>{{# gettext }}Add new key{{/ gettext }}</button>
+        </div>
+    </form>
+</div>

commit f19a5109847d8cfea4d99e8636033180519808ff

Self-XSS when fully displaying a SSH key
Reproduction steps:
 1. Go to your preferences in the "Keys & Tokens" section
 2. Add a new SSH key with this content: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2m55RysmAuqX4RiCRIH2g2V8cbAGz/BKBFCHviQ01L <img src=a onerror=alert(1)>"
 3. Click on the key in the table

Issue introduced by 8ee7305214ab1dc17a865e556026d815278e4430.

Part of story #14552: User preferences goes Burning Parrot

Change-Id: I60738a759776430b7c4a5698dd6fd8b4ee517d0e

src/www/scripts/account/keys-tokens.ts
@@ -46,7 +46,7 @@ function handleSSHKeys(): void {
             if (!full_ssh_key) {
                 return;
             }
-            row.innerHTML = full_ssh_key;
+            row.innerText = full_ssh_key;
             row.className = "ssh-key-value-reset-cursor";
         });
     });