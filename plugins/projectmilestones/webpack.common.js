b1d5c44174ca8b79e55586f5ea6c3acff4b0dd7b
Joris Masson <joris.masson@enalean.com>
2020-03-10 14:46:46 +0100

    Unify themes and scripts builds for projectmilestones
    Part of request #14652 Hoist webpack configs at plugins root folders
    
    Everything is supposed to work as before.
    
    Change-Id: Ic8ce8ecae45e18f0a74ee3aed97abc27c4b941b4

---
+++ plugins/projectmilestones/webpack.common.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+const path = require("path");
+const webpack_configurator = require("../../tools/utils/scripts/webpack-configurator.js");
+
+const context = __dirname;
+const output = webpack_configurator.configureOutput(
+    path.resolve(__dirname, "../../src/www/assets/projectmilestones"),
+    "/assets/projectmilestones/"
+);
+
+let entry_points = {
+    projectmilestones: "./scripts/projectmilestones/index.ts"
+};
+
+const colors = ["blue", "green", "grey", "orange", "purple", "red"];
+for (const color of colors) {
+    entry_points[`style-${color}`] = `./themes/BurningParrot/style-${color}.scss`;
+    entry_points[
+        `style-${color}-condensed`
+    ] = `./themes/BurningParrot/style-${color}-condensed.scss`;
+}
+
+module.exports = [
+    {
+        entry: entry_points,
+        context,
+        output,
+        resolve: {
+            extensions: [".js", ".ts", ".vue"]
+        },
+        externals: {
+            tlp: "tlp"
+        },
+        module: {
+            rules: [
+                ...webpack_configurator.configureTypescriptRules(
+                    webpack_configurator.babel_options_chrome_firefox
+                ),
+                {
+                    test: /\.po$/,
+                    include: /scripts\/charts-builders\/po/,
+                    use: [{ loader: "json-loader" }, { loader: "po-gettext-loader" }]
+                },
+                {
+                    test: /\.po$/,
+                    include: /projectmilestones\/po\//,
+                    use: [{ loader: "json-loader" }, { loader: "easygettext-loader" }]
+                },
+                webpack_configurator.rule_vue_loader,
+                webpack_configurator.rule_scss_loader
+            ]
+        },
+        plugins: [
+            webpack_configurator.getCleanWebpackPlugin(),
+            webpack_configurator.getManifestPlugin(),
+            webpack_configurator.getVueLoaderPlugin(),
+            webpack_configurator.getTypescriptCheckerPlugin(true),
+            ...webpack_configurator.getCSSExtractionPlugins()
+        ],
+        resolveLoader: {
+            alias: webpack_configurator.easygettext_loader_alias
+        }
+    }
+];
