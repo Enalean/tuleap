2409f34aaa32eb297137d6ecaefc34d6c60268e3
Joris Masson <joris.masson@enalean.com>
2020-03-10 18:33:33 +0100

    Unify themes and scripts builds for timetracking
    Part of request #14652 Hoist webpack configs at plugins root folders
    
    Everything is supposed to work as before.
    
    Change-Id: I6bef3987effcfd38bcbc10c4df9f35681db788c3

---
+++ plugins/timetracking/webpack.common.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+const path = require("path");
+const webpack_configurator = require("../../tools/utils/scripts/webpack-configurator.js");
+
+const context = __dirname;
+const output = webpack_configurator.configureOutput(
+    path.resolve(__dirname, "../../src/www/assets/timetracking"),
+    "/assets/timetracking/"
+);
+
+let entry_points = {
+    "style-fp": "./themes/FlamingParrot/css/style.scss",
+    "widget-timetracking": "./scripts/personal-timetracking-widget/src/index.js",
+    "timetracking-overview": "./scripts/timetracking-overview-widget/src/index.js"
+};
+
+const colors_burning_parrot = ["orange", "blue", "green", "red", "grey", "purple"];
+for (const color of colors_burning_parrot) {
+    entry_points[`style-bp-${color}`] = `./themes/BurningParrot/css/style-${color}.scss`;
+    entry_points[
+        `style-bp-${color}-condensed`
+    ] = `./themes/BurningParrot/css/style-${color}-condensed.scss`;
+}
+
+module.exports = [
+    {
+        entry: entry_points,
+        context,
+        output,
+        externals: {
+            tlp: "tlp"
+        },
+        module: {
+            rules: [
+                webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+                webpack_configurator.rule_easygettext_loader,
+                webpack_configurator.rule_vue_loader,
+                webpack_configurator.rule_scss_loader,
+                webpack_configurator.rule_css_assets
+            ]
+        },
+        plugins: [
+            webpack_configurator.getCleanWebpackPlugin(),
+            webpack_configurator.getManifestPlugin(),
+            webpack_configurator.getVueLoaderPlugin(),
+            ...webpack_configurator.getCSSExtractionPlugins()
+        ],
+        resolveLoader: {
+            alias: webpack_configurator.easygettext_loader_alias
+        }
+    }
+];
