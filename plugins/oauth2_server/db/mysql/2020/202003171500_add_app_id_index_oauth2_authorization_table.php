02e099916a3386465b5436b82fc2e5cde9ec0087
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-03-17 15:08:16 +0100

    Add missing index on the plugin_oauth2_authorization table
    The app_id column is used to filter the authorizations when deleting
    and app. Since there is only a composite index on (user_id, app_id)
    MySQL is forced to do a full scan to find the rows. This can be a source
    of slowness when deleting an app.
    
    Part of story #14541: have OAuth2 project admin
    
    Change-Id: Ibb6c2856cf0d61d3dc2bd249a6aae06ccb213c5b

---
+++ plugins/oauth2_server/db/mysql/2020/202003171500_add_app_id_index_oauth2_authorization_table.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ *  Tuleap is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tuleap is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202003171500_add_app_id_index_oauth2_authorization_table extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Add index on the app_id column on the plugin_oauth2_authorization table';
+    }
+
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up(): void
+    {
+        $this->db->addIndex(
+            'plugin_oauth2_authorization',
+            'idx_app_id',
+            'ALTER TABLE plugin_oauth2_authorization ADD INDEX idx_app_id (app_id)'
+        );
+    }
+}
