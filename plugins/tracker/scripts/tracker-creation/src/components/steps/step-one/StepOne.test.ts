1dc8550156322ac30dff41df00df8326754b5660
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2020-03-18 15:42:28 +0100

    Templates titles should not be displayed in step 2
    part of story #14640 Complete tracker creation
    
    How to test:
     - step one is correclty displayed
     - step two does not display template titles
    
    Change-Id: Iac08fc550ef340691dda6577d1133f8fe3ef0629

--- plugins/tracker/scripts/tracker-creation/src/components/steps/step-one/StepOne.test.ts
+++ plugins/tracker/scripts/tracker-creation/src/components/steps/step-one/StepOne.test.ts
@@ -21,17 +21,49 @@ import StepOne from "./StepOne.vue";
 import { shallowMount, Wrapper } from "@vue/test-utils";
 import { createStoreMock } from "../../../../../../../../src/www/scripts/vue-components/store-wrapper-jest";
 import { State } from "../../../store/type";
+import { createTrackerCreationLocalVue } from "../../../helpers/local-vue-for-tests";
 
 describe("StepOne", () => {
-    it("resets the slugify mode when it is mounted", () => {
+    it("resets the slugify mode when it is mounted", async () => {
         const wrapper: Wrapper<StepOne> = shallowMount(StepOne, {
             mocks: {
                 $store: createStoreMock({
                     state: {} as State
                 })
-            }
+            },
+            localVue: await createTrackerCreationLocalVue()
         });
 
         expect(wrapper.vm.$store.commit).toHaveBeenCalledWith("setSlugifyShortnameMode", true);
+    });
+
+    it(`displays the company name if the platform name is not Tuleap`, async () => {
+        const wrapper = shallowMount(StepOne, {
+            mocks: {
+                $store: createStoreMock({
+                    state: {
+                        company_name: "Nichya company"
+                    } as State
+                })
+            },
+            localVue: await createTrackerCreationLocalVue()
+        });
+
+        expect(wrapper.vm.$data.title_company_name).toEqual("Nichya company templates");
+    });
+
+    it(`displays 'Custom templates' if the platform name is Tuleap`, async () => {
+        const wrapper = shallowMount(StepOne, {
+            mocks: {
+                $store: createStoreMock({
+                    state: {
+                        company_name: "Tuleap"
+                    } as State
+                })
+            },
+            localVue: await createTrackerCreationLocalVue()
+        });
+
+        expect(wrapper.vm.$data.title_company_name).toEqual("Custom templates");
     });
 });
