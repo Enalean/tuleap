92dfde81b9e1e86b60b31286704604957e937f03
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-03-21 18:02:41 +0100

    Unify themes and scripts builds for velocity
    Part of request #14652 Hoist webpack configs at plugins root folders
    
    Everything is supposed to work as before.
    
    Change-Id: Ifa72567875deae6d92eea5e98a60da828f622246

--- plugins/velocity/webpack.common.js
+++ plugins/velocity/webpack.common.js
@@ -18,15 +18,16 @@
  */
 
 const path = require("path");
-const webpack_configurator = require("../../../tools/utils/scripts/webpack-configurator.js");
+const webpack_configurator = require("../../tools/utils/scripts/webpack-configurator.js");
 
 let entry_points = {
-    "style-fp": "./FlamingParrot/css/style.scss"
+    "velocity-chart": "./scripts/velocity-chart/src/index.js",
+    "style-fp": "./themes/FlamingParrot/css/style.scss"
 };
 
 const colors_burning_parrot = ["orange", "blue", "green", "red", "grey", "purple"];
 for (const color of colors_burning_parrot) {
-    entry_points[`velocity-${color}`] = `./BurningParrot/css/velocity-${color}.scss`;
+    entry_points[`velocity-${color}`] = `./themes/BurningParrot/css/velocity-${color}.scss`;
 }
 
 module.exports = [
@@ -34,15 +35,25 @@ module.exports = [
         entry: entry_points,
         context: path.resolve(__dirname),
         output: webpack_configurator.configureOutput(
-            path.resolve(__dirname, "../../../src/www/assets/velocity/themes")
+            path.resolve(__dirname, "../../src/www/assets/velocity")
         ),
+        resolve: {
+            alias: {
+                "charts-builders": path.resolve(__dirname, "../../src/www/scripts/charts-builders/")
+            }
+        },
         module: {
-            rules: [webpack_configurator.rule_scss_loader]
+            rules: [
+                webpack_configurator.rule_scss_loader,
+                webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+                webpack_configurator.rule_po_files
+            ]
         },
         plugins: [
             webpack_configurator.getCleanWebpackPlugin(),
+            ...webpack_configurator.getCSSExtractionPlugins(),
             webpack_configurator.getManifestPlugin(),
-            ...webpack_configurator.getCSSExtractionPlugins()
+            webpack_configurator.getMomentLocalePlugin()
         ]
     }
 ];
