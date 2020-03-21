639242018d041c9ef1d65a7d1d6b32e122dab19c
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-03-21 17:50:42 +0100

    Unify themes and scripts builds for tuleap_synchro
    Part of request #14652 Hoist webpack configs at plugins root folders
    
    Everything is supposed to work as before.
    
    Change-Id: Iee609bf72ccd6f3ae80e2426a39094a7bfb15ccf

--- plugins/tuleap_synchro/webpack.common.js
+++ plugins/tuleap_synchro/webpack.common.js
@@ -18,16 +18,20 @@
  */
 
 const path = require("path");
-const webpack_configurator = require("../../../tools/utils/scripts/webpack-configurator.js");
+const webpack_configurator = require("../../tools/utils/scripts/webpack-configurator.js");
 
-const entry_points = {};
+const entry_points = {
+    tuleap_synchro: "./scripts/site-admin/src/index.js"
+};
 
 const colors = ["blue", "green", "grey", "orange", "purple", "red"];
 for (const color of colors) {
-    entry_points[`tuleap-synchro-${color}`] = `./BurningParrot/css/tuleap-synchro-${color}.scss`;
+    entry_points[
+        `tuleap-synchro-${color}`
+    ] = `./themes/BurningParrot/css/tuleap-synchro-${color}.scss`;
     entry_points[
         `tuleap-synchro-${color}-condensed`
-    ] = `./BurningParrot/css/tuleap-synchro-${color}-condensed.scss`;
+    ] = `./themes/BurningParrot/css/tuleap-synchro-${color}-condensed.scss`;
 }
 
 module.exports = [
@@ -35,10 +39,16 @@ module.exports = [
         entry: entry_points,
         context: path.resolve(__dirname),
         output: webpack_configurator.configureOutput(
-            path.resolve(__dirname, "../../../src/www/assets/tuleap_synchro/themes/")
+            path.resolve(__dirname, "../../src/www/assets/tuleap_synchro/")
         ),
+        externals: {
+            tlp: "tlp"
+        },
         module: {
-            rules: [webpack_configurator.rule_scss_loader]
+            rules: [
+                webpack_configurator.rule_scss_loader,
+                webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11)
+            ]
         },
         plugins: [
             webpack_configurator.getCleanWebpackPlugin(),
