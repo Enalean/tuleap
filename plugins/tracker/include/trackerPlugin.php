Enalean/tuleap

commit 129f69cfd43fefb6fbc76bdbb312911de53d59e1

story #14131: Add suspension-day email for inactive users
Send suspension notification in the locale of the user
Other changes like dependency injection were made, to ensure a propper test is setup for the change.

Change-Id: I0d0916becd08dfc37ee26b20895bb9a2119a5fd7

src/common/User/UserSuspensionManager.php
@@ -109,16 +109,15 @@ class UserSuspensionManager
     /**
      * Sends email alerts for all idle user accounts
      */
-    public function sendNotificationMailToIdleAccounts() : bool
+    public function sendNotificationMailToIdleAccounts(DateTimeImmutable $date)
     {
         $inactive_delay = (int) ForgeConfig::get(self::CONFIG_INACTIVE_DELAY);
         $notification_delay = (int) ForgeConfig::get(self::CONFIG_NOTIFICATION_DELAY);
-        $result = true;
 
         if (($notification_delay > 0) && ($inactive_delay > 0)) {
-            $idle_users = $this->getIdleAccounts($notification_delay, $inactive_delay);
-
+            $idle_users = $this->getIdleAccounts($notification_delay, $inactive_delay, $date);
             $users = array_column($idle_users, 'user_id');
+
             if ($users) {
                 $this->logger->info(
                     "Sending the suspension notification to the following users (ID): " .
@@ -128,33 +127,41 @@ class UserSuspensionManager
                 $this->logger->info("No users to notify (suspension notification).");
             }
 
-
             foreach ($idle_users as $idle_user) {
                 $user = $this->user_manager->getUserbyId($idle_user['user_id']);
                 if ($user) {
-                    $suspension_date = $this->getSuspensionDate($notification_delay);
+                    $locale = $user->getLocale();
+                    $suspension_date = $this->getSuspensionDate($notification_delay, $date);
                     $last_access_date = new DateTimeImmutable('@' . $idle_user['last_access_date']);
-                    $language = $this->lang_factory->getBaseLanguage(ForgeConfig::get('sys_lang'));
-                    $status = $this->sendNotificationMail($user, $last_access_date, $suspension_date, $language);
-                    if ($status) {
-                        $this->logger->info(
-                            "Suspension notification is sent to user: ID=" .
-                            $user->getId() . " username=" . $user->getUserName() .
-                            " email=" . $user->getEmail() . " last_access_date=" .
-                            date('Y-m-d\TH:i:sO', $idle_user['last_access_date'])
-                        );
-                    } else {
-                        $this->logger->error(
-                            "Unable to send suspension notification to user: ID=" .
-                            $user->getId() . " username=" . $user->getUserName() .
-                            " email=" . $user->getEmail()
-                        );
-                    }
-                    $result = $result && $status;
+                    $language = $this->lang_factory->getBaseLanguage($locale);
+
+                    $this->locale_switcher->setLocaleForSpecificExecutionContext(
+                        $locale,
+                        function () use ($user, $last_access_date, $suspension_date, $language, $idle_user) {
+                            $this->sendAndLogNotificationMailToUser($user, $last_access_date, $suspension_date, $language, $idle_user);
+                        }
+                    );
                 }
             }
         }
-        return $result;
+    }
+
+    private function sendAndLogNotificationMailToUser(PFUser $user, DateTimeImmutable $last_access_date, DateTimeImmutable $suspension_date, BaseLanguage $language, array $idle_user)
+    {
+        if ($this->sendNotificationMail($user, $last_access_date, $suspension_date, $language)) {
+            $this->logger->info(
+                "Suspension notification is sent to user: ID=" .
+                $user->getId() . " username=" . $user->getUserName() .
+                " email=" . $user->getEmail() . " last_access_date=" .
+                date('Y-m-d\TH:i:sO', $idle_user['last_access_date'])
+            );
+        } else {
+            $this->logger->error(
+                "Unable to send suspension notification to user: ID=" .
+                $user->getId() . " username=" . $user->getUserName() .
+                " email=" . $user->getEmail()
+            );
+        }
     }
 
     /**
@@ -176,10 +183,11 @@ class UserSuspensionManager
     /**
      * @param int $notification_delay Suspension notification delay (number of days before suspension)
      * @param int $inactive_delay Inactive accounts delay (number of days after since login)
+     * @param DateTimeImmutable $date Execution date
      */
-    private function getIdleAccounts(int $notification_delay, int $inactive_delay) : array
+    private function getIdleAccounts(int $notification_delay, int $inactive_delay, DateTimeImmutable $date) : array
     {
-        $start_date = $this->getLastAccessDate($notification_delay, $inactive_delay);
+        $start_date = $this->getLastAccessDate($notification_delay, $inactive_delay, $date);
         $end_date = $start_date->modify('+23hours 59 minutes 59 seconds');
         $this->logger->info(
             "Idle accounts: querying users that last accessed " . ForgeConfig::get('sys_name') .
@@ -191,10 +199,10 @@ class UserSuspensionManager
     /**
      * @param int $notification_delay Suspension notification delay (number of days before suspension)
      * @param int $inactive_delay   Inactive accounts delay (number of days after last login)
+     * @param DateTimeImmutable $date Execution date
      */
-    private function getLastAccessDate(int $notification_delay, int $inactive_delay) : DateTimeImmutable
+    private function getLastAccessDate(int $notification_delay, int $inactive_delay, DateTimeImmutable $date) : DateTimeImmutable
     {
-        $date = new DateTimeImmutable('today');
         $date_param = '- ' . ($inactive_delay - $notification_delay) . ' day';
         return $date->modify($date_param);
     }
@@ -202,9 +210,8 @@ class UserSuspensionManager
     /**
      * @param int $notification_delay Suspension notification delay (number of days before suspension)
      */
-    private function getSuspensionDate(int $notification_delay) : DateTimeImmutable
+    private function getSuspensionDate(int $notification_delay, DateTimeImmutable $date) : DateTimeImmutable
     {
-        $date = new DateTimeImmutable('today');
         $date_param = '+ ' .  $notification_delay . ' day';
         return $date->modify($date_param);
     }

src/common/CLI/Command/DailyJobCommand.php
@@ -84,7 +84,7 @@ class DailyJobCommand extends Command
         $this->execution_delayed_launcher->execute(function () {
             $this->db_connection->reconnectAfterALongRunningProcess();
             $this->event_manager->processEvent('codendi_daily_start');
-            $this->user_suspension_manager->sendNotificationMailToIdleAccounts();
+            $this->user_suspension_manager->sendNotificationMailToIdleAccounts(new \DateTimeImmutable('today'));
             $this->user_suspension_manager->sendSuspensionMailToInactiveAccounts(new \DateTimeImmutable('today'));
             $this->user_suspension_manager->checkUserAccountValidity(new \DateTimeImmutable('today'));
 

tests/phpunit/common/User/UserSuspensionManagerTest.php
@@ -46,16 +46,12 @@ class UserSuspensionManagerTest extends TestCase
     use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration, ForgeConfigSandbox;
 
     private $dao;
-    private $email;
     private $lang_factory;
     private $language;
     private $mail;
     private $mail_account_suspension_alert_presenter;
     private $mail_presenter_factory;
-    private $pf_user;
-    private $query;
     private $template_renderer;
-    private $user_info;
     private $user_manager;
     private $user_suspension_logger;
     private $user_suspension_manager;
@@ -70,10 +66,6 @@ class UserSuspensionManagerTest extends TestCase
         ForgeConfig::set('sys_suspend_non_project_member_delay', 4);
         ForgeConfig::set('sys_suspend_send_account_suspension_email', 1);
 
-        $this->query = array(array('user_id' => 102, 'last_access_date' => 1579267252));
-        $this->email = "jane.doe@domain.com";
-        $this->user_info = array('user_id' => 102, 'user_name' => 'janedoe');
-
         $this->mail_account_suspension_alert_presenter = Mockery::mock(MailAccountSuspensionAlertPresenter::class);
         $this->mail_account_suspension_presenter = Mockery::mock(MailAccountSuspensionPresenter::class);
         $this->mail_presenter_factory = Mockery::mock(MailPresenterFactory::class);
@@ -82,7 +74,6 @@ class UserSuspensionManagerTest extends TestCase
         $this->dao = Mockery::mock(UserSuspensionDao::class);
         $this->language = Mockery::mock(BaseLanguage::class);
         $this->lang_factory = Mockery::mock(BaseLanguageFactory::class);
-        $this->pf_user = Mockery::mock(PFUser::class);
         $this->user_manager = Mockery::mock(UserManager::class);
         $this->user_manager->shouldReceive('instance')->andReturn($this->user_manager);
         $this->user_suspension_logger = Mockery::mock(LoggerInterface::class);
@@ -101,24 +92,34 @@ class UserSuspensionManagerTest extends TestCase
 
     public function testSendNotificationMailToIdleAccountsIsCalled()
     {
-        $this->mail->shouldReceive('send')->andReturn(true);
-        $this->mail->shouldReceive('setFrom');
-        $this->mail->shouldReceive('setTo');
+        $test_date = (new \DateTimeImmutable())->setTimestamp(1585090800);
+        $last_access_date = (new \DateTimeImmutable())->setTimestamp(1584576000);
+        $suspension_date = (new \DateTimeImmutable())->setTimestamp(1585436400);
+
+        // Disable mail to inactive accounts
+        ForgeConfig::set('sys_suspend_send_account_suspension_email', 0);
+
+        $idle_user = new PFUser(array("email" => "valid_mail@domain.com", "user_id" => 111, "user_name" => "idle_user", "language_id" => "fr_FR"));
+
+        $this->dao->shouldReceive('getIdleAccounts')
+            ->andReturn(array(array('user_id' => 111, 'last_access_date' => $last_access_date->getTimestamp())));
+        $this->user_manager->shouldReceive('getUserbyId')->with(111)->andReturn($idle_user);
+        $this->lang_factory->shouldReceive('getBaseLanguage')->andReturn($this->language);
+        $this->user_suspension_logger->shouldReceive('info');
+        $this->mail_presenter_factory->shouldReceive('createMailAccountSuspensionAlertPresenter')
+            ->with(Matchers::equalTo($last_access_date), Matchers::equalTo($suspension_date), $this->language)
+            ->andReturn($this->mail_account_suspension_alert_presenter);
+
+        $this->mail->shouldReceive('setFrom')->with(ForgeConfig::get('sys_noreply'));
+        $this->mail->shouldReceive('setTo')->with($idle_user->getEmail());
         $this->mail->shouldReceive('setSubject');
         $this->mail->shouldReceive('setBodyHtml');
-        $this->dao->shouldReceive('getIdleAccounts')->andReturn($this->query);
-        $this->mail_presenter_factory->shouldReceive('createMailAccountSuspensionAlertPresenter')->andReturn($this->mail_account_suspension_alert_presenter);
-        $this->template_renderer->shouldReceive('renderToString')->andReturn('Rendered_Email');
-        $this->lang_factory->shouldReceive('getBaseLanguage')->with(ForgeConfig::get('sys_lang'))->andReturn($this->language);
-        $this->pf_user->shouldReceive('getEmail')->andReturn($this->email);
-        $this->pf_user->shouldReceive('getId')->andReturn($this->user_info['user_id']);
-        $this->pf_user->shouldReceive('getUserName')->andReturn($this->user_info['user_name']);
-        $this->user_manager->shouldReceive('getUserbyId')->andReturn($this->pf_user);
-        $this->user_suspension_logger->shouldReceive('info');
-        $this->user_suspension_logger->shouldReceive('error');
-        $this->dao = Mockery::mock(\Tuleap\Dao\UserSuspensionDao::class);
+        $this->mail->shouldReceive('send')->andReturn(true);
+        $this->template_renderer->shouldReceive('renderToString')
+            ->with('mail-suspension-alert', $this->mail_account_suspension_alert_presenter)
+            ->andReturn('Rendered_Email');
 
-        $this->assertEquals(true, $this->user_suspension_manager->sendNotificationMailToIdleAccounts());
+        $this->user_suspension_manager->sendNotificationMailToIdleAccounts($test_date);
     }
 
     public function testCheckUserAccountValidity()

commit d4fd51f25c2466eec9b98d7c526405172306df33

Add missing types
Issues have been introduced by 129f69cfd43fefb6fbc76bdbb312911de53d59e1
and 51c55a2413a17e5f3f30d84f70c97ce55c047b86.

Part of request #14727: Properties, parameters and return types of methods/functions should always be defined

Change-Id: I0b521cc70b258bcdf2a5f1a951442b927e2f2522

plugins/tracker/include/trackerPlugin.php
@@ -340,7 +340,7 @@ class trackerPlugin extends Plugin //phpcs:ignore PSR1.Classes.ClassDeclaration.
         $params['entries'][$this->getServiceShortname()] = 'Opened artifacts';
     }
 
-    public function getUriFromCrossReference(GetUriFromCrossReference $event)
+    public function getUriFromCrossReference(GetUriFromCrossReference $event): void
     {
         if ($event->getTargetType() === Tracker_Artifact::REFERENCE_NATURE) {
             $artifact = Tracker_ArtifactFactory::instance()->getArtifactById($event->getSourceId());

src/common/User/UserSuspensionManager.php
@@ -109,7 +109,7 @@ class UserSuspensionManager
     /**
      * Sends email alerts for all idle user accounts
      */
-    public function sendNotificationMailToIdleAccounts(DateTimeImmutable $date)
+    public function sendNotificationMailToIdleAccounts(DateTimeImmutable $date): void
     {
         $inactive_delay = (int) ForgeConfig::get(self::CONFIG_INACTIVE_DELAY);
         $notification_delay = (int) ForgeConfig::get(self::CONFIG_NOTIFICATION_DELAY);
@@ -146,7 +146,7 @@ class UserSuspensionManager
         }
     }
 
-    private function sendAndLogNotificationMailToUser(PFUser $user, DateTimeImmutable $last_access_date, DateTimeImmutable $suspension_date, BaseLanguage $language, array $idle_user)
+    private function sendAndLogNotificationMailToUser(PFUser $user, DateTimeImmutable $last_access_date, DateTimeImmutable $suspension_date, BaseLanguage $language, array $idle_user): void
     {
         if ($this->sendNotificationMail($user, $last_access_date, $suspension_date, $language)) {
             $this->logger->info(