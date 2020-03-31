172811db5721d9a6dbcd0a5693f008e68047fca5
Nicolas Terray <nicolas.terray@enalean.com>
2020-03-31 15:19:48 +0200

    Do not duplicate tracker definition for default templates
    No need to duplicate the tracker name, item_name or color since they are
    already in the xml file.
    
    request #14717: Cleanup of tracker templates
    
    Change-Id: Ia74fef69842ff7ffe34c74a0cc31699eb1cbeb26

--- plugins/tracker/phpunit/Tracker/Creation/DefaultTemplatesCollectionBuilderTest.php
+++ plugins/tracker/phpunit/Tracker/Creation/DefaultTemplatesCollectionBuilderTest.php
@@ -24,6 +24,7 @@ namespace Tuleap\Tracker\Creation;
 
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class DefaultTemplatesCollectionBuilderTest extends TestCase
@@ -35,14 +36,100 @@ class DefaultTemplatesCollectionBuilderTest extends TestCase
         $event_manager = Mockery::mock(\EventManager::class);
         $event_manager
             ->shouldReceive('processEvent')
-            ->with(Mockery::on(static function (DefaultTemplatesCollection $collection) {
-                $collection->add('default-plugin', Mockery::mock(DefaultTemplate::class));
+            ->with(Mockery::on(static function (DefaultTemplatesXMLFileCollection $collection) {
+                $plugin_path = vfsStream::setup()->url() . '/file.xml';
+                file_put_contents($plugin_path, <<<EOS
+                    <tracker>
+                          <name>Releases</name>
+                          <item_name>release</item_name>
+                          <description>Release tracker defined in a plugin</description>
+                          <color>acid-green</color>
+                    </tracker>
+                    EOS
+                );
+                $collection->add($plugin_path);
                 return true;
             }));
 
         $collection = (new DefaultTemplatesCollectionBuilder($event_manager))->build();
 
+        $this->assertCount(2, $collection->getSortedDefaultTemplatesRepresentations());
         $this->assertTrue($collection->has('default-bug'));
-        $this->assertTrue($collection->has('default-plugin'));
+        $this->assertTrue($collection->has('default-release'));
+    }
+
+    public function testItIgnoresXMLFilesThatDoNotContainATrackerName(): void
+    {
+        $event_manager = Mockery::mock(\EventManager::class);
+        $event_manager
+            ->shouldReceive('processEvent')
+            ->with(Mockery::on(static function (DefaultTemplatesXMLFileCollection $collection) {
+                $plugin_path = vfsStream::setup()->url() . '/file.xml';
+                file_put_contents($plugin_path, <<<EOS
+                    <tracker>
+                          <item_name>release</item_name>
+                          <description>Release tracker defined in a plugin</description>
+                          <color>acid-green</color>
+                    </tracker>
+                    EOS
+                );
+                $collection->add($plugin_path);
+                return true;
+            }));
+
+        $collection = (new DefaultTemplatesCollectionBuilder($event_manager))->build();
+
+        $this->assertCount(1, $collection->getSortedDefaultTemplatesRepresentations());
+        $this->assertTrue($collection->has('default-bug'));
+    }
+
+    public function testItIgnoresXMLFilesThatDoNotContainATrackerItemName(): void
+    {
+        $event_manager = Mockery::mock(\EventManager::class);
+        $event_manager
+            ->shouldReceive('processEvent')
+            ->with(Mockery::on(static function (DefaultTemplatesXMLFileCollection $collection) {
+                $plugin_path = vfsStream::setup()->url() . '/file.xml';
+                file_put_contents($plugin_path, <<<EOS
+                    <tracker>
+                          <name>Releases</name>
+                          <description>Release tracker defined in a plugin</description>
+                          <color>acid-green</color>
+                    </tracker>
+                    EOS
+                );
+                $collection->add($plugin_path);
+                return true;
+            }));
+
+        $collection = (new DefaultTemplatesCollectionBuilder($event_manager))->build();
+
+        $this->assertCount(1, $collection->getSortedDefaultTemplatesRepresentations());
+        $this->assertTrue($collection->has('default-bug'));
+    }
+
+    public function testItIgnoresXMLFilesThatDoNotContainATrackerColor(): void
+    {
+        $event_manager = Mockery::mock(\EventManager::class);
+        $event_manager
+            ->shouldReceive('processEvent')
+            ->with(Mockery::on(static function (DefaultTemplatesXMLFileCollection $collection) {
+                $plugin_path = vfsStream::setup()->url() . '/file.xml';
+                file_put_contents($plugin_path, <<<EOS
+                    <tracker>
+                          <name>Releases</name>
+                          <item_name>release</item_name>
+                          <description>Release tracker defined in a plugin</description>
+                    </tracker>
+                    EOS
+                );
+                $collection->add($plugin_path);
+                return true;
+            }));
+
+        $collection = (new DefaultTemplatesCollectionBuilder($event_manager))->build();
+
+        $this->assertCount(1, $collection->getSortedDefaultTemplatesRepresentations());
+        $this->assertTrue($collection->has('default-bug'));
     }
 }
