ab41a263a79bffd85776d2e69efbb4e613ee308b
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-04-16 18:16:26 +0200

    Test that a user can approve an OAuth2 authorization request
    In order to achieve that a new service is spawn with the E2E stack to
    play the role of the relying party. For now, this relying party is very
    basic as its only role is to redirect the user with a valid
    authorization request. The retrieved authorization code is not yet used,
    there is no proper validation that the OAuth2/OIDC flow is correct.
    
    To make easy to generate a certificate for the mandatory TLS usage and
    have the capability to reuse bits of the PHP ecosystem in the upcoming
    contributions, the service uses AmpPHP [0]. That makes possible to
    dynamically generate a certificate when the service starts without having
    to deal with the setup of more "standard" PHP stack (e.g. nginx/PHP FPM,
    Apache/mod_php) since only the PHP CLI is needed.
    
    Due to the nature of the test scenario, it has been needed to allow
    Cypress to do cross-origin requests.
    
    Part of story #14714: be an OpenID Connect provider
    
    [0] https://amphp.org/
    
    Change-Id: Ifdf2995f3914644a8405d90df1ebc7130c73d240

--- tests/e2e/full/wrap_for_dev_context.sh
+++ tests/e2e/full/wrap_for_dev_context.sh
@@ -4,7 +4,8 @@
 # This will bring-up the platform, open cypress
 
 set -euxo pipefail
-DOCKERCOMPOSE="docker-compose -f docker-compose-e2e-full-tests-with-app.yml -p e2e-tests"
+plugins_compose_file="$(find ./plugins/*/tests/e2e/ -name docker-compose.yml -printf '-f %p ')"
+DOCKERCOMPOSE="docker-compose -f docker-compose-e2e-full-tests-with-app.yml $plugins_compose_file -p e2e-tests"
 
 test_results_folder='./test_results_e2e_full'
 if [ "$#" -eq "1" ]; then
@@ -25,4 +26,5 @@ $DOCKERCOMPOSE up -d --build
 TEST_RESULT_OUTPUT="$test_results_folder" $DOCKERCOMPOSE up -d --build
 
 TULEAP_IP="$(docker inspect "$($DOCKERCOMPOSE ps -q tuleap)" --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')"
-echo "Please set in /etc/hosts: $TULEAP_IP tuleap"
+OAUTH2_SERVER_RP_OIDC_IP="$(docker inspect "$($DOCKERCOMPOSE ps -q oauth2-server-rp-oidc)" --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')"
+echo "Please set in /etc/hosts: \`$TULEAP_IP tuleap\` and \`$OAUTH2_SERVER_RP_OIDC_IP oauth2-server-rp-oidc\`"
