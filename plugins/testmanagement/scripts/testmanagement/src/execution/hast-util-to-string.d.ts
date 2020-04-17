ddbe3c7adfb71e63451cb3620936f5e6592ad938
Nicolas Terray <nicolas.terray@enalean.com>
2020-04-17 16:14:42 +0200

    Add missing TS declarations
    This is a follow-up to git #tuleap/stable/0d0955aaf745240544ad46e62244ff58c501b39f
    
    References used to write those definitions:
    
    * rehype-parse: [0] & [1]
    * hast-util-to-string: [2]
    * unist-util-remove: [3]
    
    [0] https://github.com/rehypejs/rehype/blob/master/packages/rehype-parse/index.js
    [1] https://github.com/JS-DevTools/rehype-inline-svg/blob/master/src/typings/rehype-parse/index.d.ts
    [2] https://github.com/rehypejs/rehype-minify/blob/master/packages/hast-util-to-string/index.js
    [3] https://github.com/syntax-tree/unist-util-remove/blob/master/index.js
    
    Part of story #14735: include images in test execution comment
    
    Change-Id: Id9ef45cad84045e09b91d596ef3dc39504380e65

--- plugins/testmanagement/scripts/testmanagement/src/execution/hast-util-to-string.d.ts
+++ plugins/testmanagement/scripts/testmanagement/src/execution/hast-util-to-string.d.ts
@@ -1,1 +1,7 @@
-declare module "hast-util-to-string";
+declare module "hast-util-to-string" {
+    import { Node } from "unist";
+
+    function toString(node: Node): string;
+
+    export = toString;
+}
--- plugins/testmanagement/scripts/testmanagement/src/execution/unist-util-remove.d.ts
+++ plugins/testmanagement/scripts/testmanagement/src/execution/unist-util-remove.d.ts
@@ -1,1 +1,12 @@
-declare module "unist-util-remove";
+declare module "unist-util-remove" {
+    import { Node } from "unist";
+
+    interface Options {
+        readonly cascade?: boolean;
+    }
+
+    function remove(ast: Node, test: (node: Node) => boolean): void;
+    function remove(ast: Node, opts: Options, test: (node: Node) => boolean): void;
+
+    export = remove;
+}
