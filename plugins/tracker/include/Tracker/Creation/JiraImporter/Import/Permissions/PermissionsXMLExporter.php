6f13a79238e1ee9ba58d96fbf869d929e4b4d8bf
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2020-04-22 11:32:12 +0200

    Add default permissions on field in mapping
    This is part of story #14766 Have an import from jira structure
    
    Change-Id: I196c39c2e946197cff51f7ee5bea3363eb10bca9

--- plugins/tracker/include/Tracker/Creation/JiraImporter/Import/Permissions/PermissionsXMLExporter.php
+++ plugins/tracker/include/Tracker/Creation/JiraImporter/Import/Permissions/PermissionsXMLExporter.php
@@ -24,20 +24,60 @@ declare(strict_types=1);
 namespace Tuleap\Tracker\Creation\JiraImporter\Import\Permissions;
 
 use SimpleXMLElement;
+use Tuleap\Tracker\Creation\JiraImporter\Import\JiraXmlExporter;
+use Tuleap\Tracker\Creation\JiraImporter\Import\Structure\FieldMapping;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Structure\FieldMappingCollection;
 
 class PermissionsXMLExporter
 {
+    /**
+     * @var array
+     */
+    private $read_only_field_ids = [
+        JiraXmlExporter::JIRA_ARTIFACT_ID_FIELD_ID,
+        JiraXmlExporter::JIRA_LINK_FIELD_ID
+    ];
+
     public function exportFieldsPermissions(SimpleXMLElement $node_tracker, FieldMappingCollection $field_mapping_collection): void
     {
         $permissions_node = $node_tracker->addChild('permissions');
 
         foreach ($field_mapping_collection->getAllMappings() as $mapping) {
-            $permission_node  = $permissions_node->addChild('permission');
-            $permission_node->addAttribute("scope", "field");
-            $permission_node->addAttribute("REF", $mapping->getXMLId());
-            $permission_node->addAttribute("ugroup", "UGROUP_PROJECT_MEMBERS");
-            $permission_node->addAttribute("type", "PLUGIN_TRACKER_FIELD_READ");
+            $this->exportReadPermission($permissions_node, $mapping);
+
+            if (in_array($mapping->getJiraFieldId(), $this->read_only_field_ids)) {
+                continue;
+            }
+
+            $this->exportSubmitPermission($permissions_node, $mapping);
+            $this->exportUpdatePermission($permissions_node, $mapping);
         }
+    }
+
+    private function exportReadPermission(SimpleXMLElement $permissions_node, FieldMapping $mapping): void
+    {
+        $read_permission_node = $permissions_node->addChild('permission');
+        $read_permission_node->addAttribute("scope", "field");
+        $read_permission_node->addAttribute("REF", $mapping->getXMLId());
+        $read_permission_node->addAttribute("ugroup", "UGROUP_ANONYMOUS");
+        $read_permission_node->addAttribute("type", "PLUGIN_TRACKER_FIELD_READ");
+    }
+
+    private function exportSubmitPermission(SimpleXMLElement $permissions_node, FieldMapping $mapping): void
+    {
+        $submit_permission_node = $permissions_node->addChild('permission');
+        $submit_permission_node->addAttribute("scope", "field");
+        $submit_permission_node->addAttribute("REF", $mapping->getXMLId());
+        $submit_permission_node->addAttribute("ugroup", "UGROUP_REGISTERED");
+        $submit_permission_node->addAttribute("type", "PLUGIN_TRACKER_FIELD_SUBMIT");
+    }
+
+    private function exportUpdatePermission(SimpleXMLElement $permissions_node, FieldMapping $mapping): void
+    {
+        $update_permission_node = $permissions_node->addChild('permission');
+        $update_permission_node->addAttribute("scope", "field");
+        $update_permission_node->addAttribute("REF", $mapping->getXMLId());
+        $update_permission_node->addAttribute("ugroup", "UGROUP_PROJECT_MEMBERS");
+        $update_permission_node->addAttribute("type", "PLUGIN_TRACKER_FIELD_UPDATE");
     }
 }
