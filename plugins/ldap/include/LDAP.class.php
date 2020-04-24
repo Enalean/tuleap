Enalean/tuleap

commit d17c7fabdfc0a672e5561f3782a2babd78904ce8

A user without a password cannot set one herself
As of today this situation can happen when a user has been automatically
registered after a sign in with an OIDC provider marked as being the only
authentication method. With upcoming contributions it will also be the
case for any users that have registered after signing in on any OIDC
provider proposed by the instance.

In order to make sure there was no magic trick (e.g. a null casted
silently to an empty string) the plaintext user password has been
changed for a Tuleap\Cryptography\ConcealedString instance. It makes
easier for the static analysis to catch misuses and anyway it limits the
chance of leaking a user's password.

To test, you can the password field of one of your user to NULL. When
you are authenticated with this user, the password change form should be
gone. If you try to authenticate with this user the authentication must
be denied.

Part of request #14812: Users signing in via an OIDC provider should be
able to create an account without setting a password

Change-Id: I4522f126be60b314c0dbb780fe5491ed64bda7ad

plugins/ldap/include/LDAP.class.php
@@ -19,6 +19,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Cryptography\ConcealedString;
+
 /**
  * LDAP class definition
  * Provides LDAP facilities:
@@ -177,11 +179,11 @@ class LDAP
      *   means that the user/password is valid.
      *
      * @param String $binddn DN to use to bind with
-     * @param String $bindpw Password associated to the DN
+     * @param ConcealedString $bindpw Password associated to the DN
      *
      * @return bool true if bind was successful, false otherwise.
      */
-    public function bind($binddn = null, $bindpw = null)
+    public function bind($binddn = null, ?ConcealedString $bindpw = null)
     {
         if (!$this->bound) {
             if (!$binddn) {
@@ -197,7 +199,7 @@ class LDAP
                 $this->bound = false;
             }
 
-            if ($bind_result = @ldap_bind($this->ds, $binddn, $bindpw)) {
+            if ($bind_result = @ldap_bind($this->ds, $binddn, $bindpw === null ? null : $bindpw->getString())) {
                 $this->bound = true;
             } else {
                 $error_message = 'Unable to bind to LDAP server: ' . $this->ldapParams['server'] .
@@ -260,13 +262,12 @@ class LDAP
      * with this DN and the given password
      *
      * @param string $login  Login name to authenticate with
-     * @param string $passwd Password associated to the login
      *
      * @return bool true if the login and password match, false otherwise
      */
-    public function authenticate($login, $passwd)
+    public function authenticate($login, ConcealedString $passwd)
     {
-        if (!$passwd) {
+        if ($passwd->isIdenticalTo(new ConcealedString(''))) {
             // avoid a successful bind on LDAP servers accepting anonymous connections
             //$this->setError($Language->getText('ldap_class','err_nopasswd'));
             return false;

commit 2f19f307b67c1660463a89ca83f7b1acb00755d0

Cannot login against a LDAP server that does not allow anonymous binds
The login process crashes with:
```
PHP Fatal error:  Uncaught Error: Call to a member function getString() on string in /usr/share/tuleap/plugins/ldap/include/LDAP.class.php:202
```

Issue introduced by d17c7fabdfc0a672e5561f3782a2babd78904ce8.

Part of request #14812: Users signing in via an OIDC provider should be
able to create an account without setting a password

Change-Id: I19c9035bfe5d7d1ce7ac0bbe0fe535fcd7cf6c4e

plugins/ldap/include/LDAP.class.php
@@ -188,7 +188,7 @@ class LDAP
         if (!$this->bound) {
             if (!$binddn) {
                 $binddn = isset($this->ldapParams['bind_dn']) ? $this->ldapParams['bind_dn'] : null;
-                $bindpw = isset($this->ldapParams['bind_passwd']) ? $this->ldapParams['bind_passwd'] : null;
+                $bindpw = isset($this->ldapParams['bind_passwd']) ? new ConcealedString((string) $this->ldapParams['bind_passwd']) : null;
             }
             if ($binddn && (!$bindpw)) {
                 // Prevent successful binding if a username is given and the server