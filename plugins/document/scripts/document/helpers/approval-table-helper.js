cf91ee434f41d29e5001bf9c76a03f2ad8dcf874
Thomas Gorka <thomas.gorka@enalean.com>
2020-05-07 10:55:37 +0200

    request #14870 Wrong approval table badge displayed in french
    I've also removed the unused states, as Commented and Declined states
    are only used during the approval state arbitration.
    
    How to test:
    - Have documents with various approval table states (not yet, approved, rejected)
    - Switch to French
    --> Badges are the same than in English, but translated in French
    
    Change-Id: I2c546fd41d46f80f34720c0bb248120c470b29b0

--- plugins/document/scripts/document/helpers/approval-table-helper.js
+++ plugins/document/scripts/document/helpers/approval-table-helper.js
@@ -17,59 +17,43 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-    APPROVAL_APPROVED,
-    APPROVAL_COMMENTED,
-    APPROVAL_DECLINED,
-    APPROVAL_NOT_YET,
-    APPROVAL_REJECTED,
-} from "../constants.js";
+import { APPROVAL_APPROVED, APPROVAL_NOT_YET, APPROVAL_REJECTED } from "../constants.js";
 
 export function extractApprovalTableData(
-    translated_states,
-    approval_table,
+    translated_approval_states_map,
+    approval_table_state,
     is_in_folder_content_row
 ) {
     let additional_class = "";
     if (is_in_folder_content_row) {
         additional_class = "document-tree-item-toggle-quicklook-approval-badge";
     }
-    switch (approval_table) {
+
+    const state = translated_approval_states_map[approval_table_state];
+
+    switch (state) {
         case APPROVAL_NOT_YET:
             return {
                 icon_badge: "fa-tlp-gavel-pending",
-                badge_label: translated_states[APPROVAL_NOT_YET],
+                badge_label: approval_table_state,
                 badge_class: `tlp-badge-chrome-silver ${additional_class}`,
             };
         case APPROVAL_APPROVED:
             return {
                 icon_badge: "fa-tlp-gavel-approved",
-                badge_label: translated_states[APPROVAL_APPROVED],
+                badge_label: approval_table_state,
                 badge_class: `tlp-badge-success ${additional_class}`,
             };
         case APPROVAL_REJECTED:
             return {
                 icon_badge: "fa-tlp-gavel-rejected",
-                badge_label: translated_states[APPROVAL_REJECTED],
+                badge_label: approval_table_state,
                 badge_class: `tlp-badge-danger ${additional_class}`,
             };
-        case APPROVAL_DECLINED:
-            return {
-                icon_badge: "fa-tlp-gavel-rejected",
-                badge_label: translated_states[APPROVAL_DECLINED],
-                badge_class: `tlp-badge-danger ${additional_class}`,
-            };
-        case APPROVAL_COMMENTED:
-            return {
-                icon_badge: "fa-tlp-gavel-comment",
-                badge_label: translated_states[APPROVAL_COMMENTED],
-                badge_class: `tlp-badge-info ${additional_class}`,
-            };
-
         default:
             return {
                 icon_badge: "fa-tlp-gavel-pending",
-                badge_label: translated_states[APPROVAL_NOT_YET],
+                badge_label: approval_table_state,
                 badge_class: `tlp-badge-secondary ${additional_class}`,
             };
     }
