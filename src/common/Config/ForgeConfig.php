Enalean/tuleap

commit cfaf02210de11ad9aebadc38d04a2db29d22770a

Introduce configuration of syslog for Docker TEE image
As of today it only focus on Tuleap & Nginx to introduce
the logic.

There are 2 new environement variables that can be used:
- TULEAP_LOGGER (either `syslog` or not set / empty)
- TULEAP_LOG_SERVER (a given log host server like `myserver:514`)

As of today, you can set `TULEAP_LOGGER` to syslog and observe
the log traffic in /var/log/messages (nginx traffic for instance).

For `TULEAP_LOG_SERVER`, I didn't tested myself as it requires
a log server to test. My goal is to test `tuleap-devel` and our
brand new log server.

nginx configuration was also updated to not log access to assets
in 03-location.conf in order to reduce the amount of logs being
captured and have more meaningful access logs.

go in `plugins/tee_container/docker` and run `docker-compose up`
You need to have nexus account with Read access to docker.tuleap.org
and you need to login on this registry first.

When the stack is up and running, you should go in the web container
and see in /var/log/messages that nginx access and error logs are
there.

Part of request #14864 Centralize logs in syslog & log forwarding

Change-Id: I979de8cc358d04d8c8f4d203bb46a8a87a6dd119

src/common/Config/ForgeConfig.php
@@ -54,6 +54,16 @@ class ForgeConfig
         return $local_inc_file_path;
     }
 
+    public static function loadDatabaseInc(): string
+    {
+        $database_config_file = self::get('db_config_file');
+        if (! file_exists($database_config_file)) {
+            throw new RuntimeException('Database configuration file cannot be read, did you loadLocalIn first ?');
+        }
+        self::loadFromFile($database_config_file);
+        return $database_config_file;
+    }
+
     public static function loadFromFile($file)
     {
         self::load(new ConfigValueFileProvider($file));

plugins/tee_container/docker/docker-compose.yml
@@ -22,11 +22,13 @@ services:
       - DB_ADMIN_PASSWORD=welcome0
       - TULEAP_FPM_SESSION_MODE=redis
       - TULEAP_REDIS_SERVER=redis
-      - TULEAP_EMAIL_RELAYHOST=mailhog:1025
-#      - TULEAP_EMAIL_ADMIN=someone@example.com
 #      - TULEAP_REDIS_PORT=6380
 #      - TULEAP_REDIS_PASSWORD=some_password
 #      - TULEAP_REDIS_USE_TLS=1
+      - TULEAP_EMAIL_RELAYHOST=mailhog:1025
+#      - TULEAP_EMAIL_ADMIN=someone@example.com
+      - TULEAP_LOGGER=syslog
+#      - TULEAP_LOG_SERVER=some-server:514
 
   db:
     image: mysql:5.7

plugins/tee_container/include/StartContainerCommand.php
@@ -32,6 +32,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Tuleap\BuildVersion\FlavorFinderFromFilePresence;
 use Tuleap\BuildVersion\VersionPresenter;
 use TuleapCfg\Command\Docker\DataPersistence;
+use TuleapCfg\Command\Docker\LogToSyslog;
 use TuleapCfg\Command\Docker\Postfix;
 use TuleapCfg\Command\Docker\Realtime;
 use TuleapCfg\Command\Docker\Rsyslog;
@@ -99,14 +100,18 @@ final class StartContainerCommand extends Command
                 $this->data_persistence->restore($output);
                 $tuleap->update($output);
             }
-            $realtime = new Realtime(new ConsoleLogger($output, [LogLevel::INFO => OutputInterface::VERBOSITY_NORMAL]));
+            $console_logger = new ConsoleLogger($output, [LogLevel::INFO => OutputInterface::VERBOSITY_NORMAL]);
+            $realtime = new Realtime($console_logger);
             $realtime->setup($tuleap_fqdn);
 
             $rsyslog = new Rsyslog();
-            $rsyslog->setup($output);
+            $rsyslog->setup($output, $tuleap_fqdn);
 
             $postfix = new Postfix($this->process_factory);
             $postfix->setup($output, $tuleap_fqdn);
+
+            $log_to_syslog = new LogToSyslog($console_logger);
+            $log_to_syslog->configure();
 
             $supervisord = new Supervisord(
                 Supervisord::UNIT_CROND,

src/common/CLI/Command/ConfigSetCommand.php
@@ -21,34 +21,27 @@
 
 namespace Tuleap\CLI\Command;
 
-use ConfigDao;
-use EventManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Tuleap\CLI\Events\GetWhitelistedKeys;
+use Tuleap\Config\ConfigSet;
+use Tuleap\Config\InvalidConfigKeyException;
 
 class ConfigSetCommand extends Command
 {
     public const NAME = 'config-set';
 
     /**
-     * @var ConfigDao
+     * @var ConfigSet
      */
-    private $config_dao;
+    private $config_set;
 
-    /**
-     * @var EventManager
-     */
-    private $event_manager;
-
-    public function __construct(ConfigDao $config_dao, EventManager $event_manager)
+    public function __construct(ConfigSet $config_set)
     {
         parent::__construct(self::NAME);
-        $this->config_dao    = $config_dao;
-        $this->event_manager = $event_manager;
+        $this->config_set = $config_set;
     }
 
     protected function configure()
@@ -60,28 +53,18 @@ class ConfigSetCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $key = $input->getArgument('key');
+        try {
+            $key = $input->getArgument('key');
+            assert(is_string($key));
+            $value = $input->getArgument('value');
+            assert(is_string($value));
 
-        $event = new GetWhitelistedKeys();
-        $this->event_manager->processEvent($event);
-
-        $white_listed_keys = $event->getWhiteListedKeys();
-
-        if (! $this->keyIsWhitelisted($white_listed_keys, $key)) {
-            throw new InvalidArgumentException(self::NAME . " only supports a subset of keys:\n* " . implode("\n* ", array_keys($white_listed_keys)));
+            $this->config_set->set($key, $value);
+        } catch (InvalidConfigKeyException $exception) {
+            $keys = $exception->getWhiteListedKeys();
+            sort($keys, SORT_STRING);
+            throw new InvalidArgumentException(self::NAME . " only supports a subset of keys:\n* " . implode("\n* ", $keys));
         }
-
-        $value = $input->getArgument('value');
-
-        $this->config_dao->save($key, $value);
         return 0;
-    }
-
-    /**
-     * @return bool
-     */
-    private function keyIsWhitelisted(array $white_listed_keys, $key)
-    {
-        return isset($white_listed_keys[$key]);
     }
 }

src/common/CLI/Events/GetWhitelistedKeys.php
@@ -38,7 +38,7 @@ class GetWhitelistedKeys implements Dispatchable
     public const NAME = 'getWhitelistedKeys';
 
     /**
-     * @var array
+     * @var array<string, bool>
      */
     private $white_listed_keys = [
         \ProjectManager::CONFIG_PROJECT_APPROVAL => true,
@@ -62,16 +62,21 @@ class GetWhitelistedKeys implements Dispatchable
         UserSuspensionManager::CONFIG_INACTIVE_EMAIL => true
     ];
 
-    public function addPluginsKeys($key_name)
+    public function addPluginsKeys(string $key_name): void
     {
         $this->white_listed_keys[$key_name] = true;
     }
 
     /**
-     * @return array
+     * @return string[]
      */
-    public function getWhiteListedKeys()
+    public function getWhiteListedKeys(): array
     {
-        return $this->white_listed_keys;
+        return array_keys($this->white_listed_keys);
+    }
+
+    public function isKeyWhiteListed(string $key): bool
+    {
+        return isset($this->white_listed_keys[$key]);
     }
 }

src/common/Config/ConfigSet.php
@@ -1,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Config;
+
+use ConfigDao;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Tuleap\CLI\Events\GetWhitelistedKeys;
+
+final class ConfigSet
+{
+    /**
+     * @var ConfigDao
+     */
+    private $config_dao;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $event_dispatcher;
+
+    public function __construct(EventDispatcherInterface $event_dispatcher, ConfigDao $config_dao)
+    {
+        $this->event_dispatcher = $event_dispatcher;
+        $this->config_dao       = $config_dao;
+    }
+
+    /**
+     * @throws InvalidConfigKeyException
+     */
+    public function set(string $key, string $value): void
+    {
+        $white_listed_keys = $this->event_dispatcher->dispatch(new GetWhitelistedKeys());
+        assert($white_listed_keys instanceof GetWhitelistedKeys);
+
+        if (! $white_listed_keys->isKeyWhiteListed($key)) {
+            throw new InvalidConfigKeyException($white_listed_keys);
+        }
+
+        $this->config_dao->save($key, $value);
+    }
+}

src/common/Config/InvalidConfigKeyException.php
@@ -1,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Config;
+
+use Tuleap\CLI\Events\GetWhitelistedKeys;
+
+final class InvalidConfigKeyException extends \Exception
+{
+    /**
+     * @var GetWhitelistedKeys
+     */
+    private $white_listed_keys;
+
+    public function __construct(GetWhitelistedKeys $white_listed_keys)
+    {
+        $this->white_listed_keys = $white_listed_keys;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getWhiteListedKeys(): array
+    {
+        return $this->white_listed_keys->getWhiteListedKeys();
+    }
+}

src/common/backend/BackendLogger.class.php
@@ -26,7 +26,7 @@ class BackendLogger extends \Psr\Log\AbstractLogger implements LoggerInterface
 {
     public const CONFIG_LOGGER = 'sys_logger';
 
-    private const CONFIG_LOGGER_SYSLOG = 'syslog';
+    public const CONFIG_LOGGER_SYSLOG = 'syslog';
 
     public const FILENAME = 'codendi_syslog';
 

commit 76cee178ab120695a1dff50bab4d5afb969dd0d9

Fix gl-membership.pl on centos6
This fix a regression introduced by cfaf02210d.

In this commit we refactored the way database.inc was loaded and guard was
added to ensure file was reachable to produce a nice error message instead
of a weird stack trace.

However, the check done with file_exists doesnt work on centos6 / perl setuid
because there bugs in file_exists implementation in this context.
The workaround is to use is_file that does the same job but without the bug.

The problem doesn't arise on centos7 because it uses sudo which is a better
way to solve the problem. I choose not to go the sudo way in order to stay
as close as possible of the original code and avoid painful upgrades.

Change-Id: I1acfc7989816d79d8a94cff98b4d757c0fdbfba8

src/common/Config/ForgeConfig.php
@@ -57,8 +57,8 @@ class ForgeConfig
     public static function loadDatabaseInc(): string
     {
         $database_config_file = self::get('db_config_file');
-        if (! file_exists($database_config_file)) {
-            throw new RuntimeException('Database configuration file cannot be read, did you loadLocalIn first ?');
+        if (! is_file($database_config_file)) {
+            throw new RuntimeException('Database configuration file cannot be read, did you loadLocalInc first ?');
         }
         self::loadFromFile($database_config_file);
         return $database_config_file;