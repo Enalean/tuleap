6c049c323ed53cd6efdf9f2151b733db429998c1
Joris Masson <joris.masson@enalean.com>
2020-06-01 11:14:19 +0200

    request #14924 Project creation automatic shortname should not go above 30 characters
    How to test:
    
    - Go to the new project creation screen
    - Type a project name above 30 characters, for example: "Agile Multi
    Project Teal Crayon"
    - The automatic shortname should be "agile-multi-project-teal-crayo"
    without the final "n".
    - Submit, it should succeed.
    
    Without this patch, if you submit, you will get a 400 Bad Request
    because the shortname is above 30 characters.
    
    Change-Id: I37dca65bf0d4a60dc01f0dfb0df5d598d35a3bc9

--- src/scripts/project/registration/src/components/Information/Input/ProjectShortName.vue
+++ src/scripts/project/registration/src/components/Information/Input/ProjectShortName.vue
@@ -135,7 +135,10 @@ export default class ProjectShortName extends Vue {
             "©": "-",
             "®": "-",
         });
-        this.slugified_project_name = slugify(value, { lower: true });
+        this.slugified_project_name = slugify(value, { lower: true }).slice(
+            0,
+            this.max_project_length
+        );
         this.checkValidity(this.slugified_project_name);
 
         this.$refs.shortname.value = this.slugified_project_name;
