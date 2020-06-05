cce5f771037699c62889819c5fe9ae047fcdc379
Lorentz Romain <romain.lorentz@enalean.com>
2020-06-05 15:34:30 +0200

    Insert git reference group for project with git service with no git reference
    part of request #14916 References by git commit are not well created
    
    Change-Id: I6f6f70164d454ca417499088378ee85594dd5a74

---
+++ plugins/git/db/mysql/updates/2020/202006081606_add_reference_group_for_project.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202006081606_add_reference_group_for_project extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Add reference_group for project with git service with no git reference';
+    }
+
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up(): void
+    {
+        $reference_id = (int) $this->getGitReference();
+        $group_ids    = $this->getGroupIdsWithBrokenGitReferences($reference_id);
+        $this->insertReferenceGroup($reference_id, $group_ids);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getGroupIdsWithBrokenGitReferences(int $reference_id): array
+    {
+        $sql = "SELECT group_id
+                FROM service
+                WHERE short_name = 'plugin_git'
+                    AND is_active = 1
+                    AND group_id != 0
+                    AND group_id NOT IN (
+                        SELECT group_id
+                        FROM reference_group
+                        WHERE reference_id = ?
+                )";
+
+        $pdo_statement = $this->db->dbh->prepare($sql);
+
+        if ($pdo_statement->execute([$reference_id]) === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete(
+                'Error while retrieving groups with broken git references.'
+            );
+        }
+
+        return $pdo_statement->fetchAll();
+    }
+
+    private function getGitReference(): string
+    {
+        $sql = "SELECT id
+                FROM reference
+                WHERE service_short_name = 'plugin_git'
+                LIMIT 1";
+
+        $result = $this->db->dbh->query($sql)->fetch()[0];
+
+        if ($result === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete(
+                'Error while retrieving git reference.'
+            );
+        }
+
+        return $result;
+    }
+
+    private function insertReferenceGroup(int $reference_id, array $group_ids): void
+    {
+        $data_to_insert = [];
+        $question_mark  = [];
+
+        foreach ($group_ids as $data) {
+            $data_to_insert = array_merge($data_to_insert, [$reference_id, $data['group_id'], 1]);
+            $question_mark[]  = '(?,?,?)';
+        }
+        $sql = "INSERT INTO reference_group (reference_id, group_id, is_active) VALUES " . implode(',', $question_mark);
+
+        $pdo_statement = $this->db->dbh->prepare($sql);
+
+        if ($pdo_statement->execute($data_to_insert) === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete(
+                'Error while retrieving git reference.'
+            );
+        }
+    }
+}
