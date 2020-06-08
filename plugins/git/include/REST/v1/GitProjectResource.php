Enalean/tuleap

commit 0301188fc4ab7ba1877bf38da23dce0405c8319f

request #14945: Update to Deptrac 0.7.1
Changelog:
https://github.com/sensiolabs-de/deptrac/releases/tag/0.7.0

A new violation between the core and the Git plugin has been detected
by deptrac and is also fixed in this contribution.

Change-Id: I20385ec76ae2a224bf8358f0c7fceda4c568f2e8

plugins/git/include/REST/v1/GitProjectResource.php
@@ -1,0 +1,273 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2013-Present. All Rights Reserved.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Git\REST\v1;
+
+use Git_PermissionsDao;
+use Git_RemoteServer_Dao;
+use Git_RemoteServer_GerritServerFactory;
+use Git_SystemEventManager;
+use GitPermissionsManager;
+use GitRepository;
+use GitRepositoryFactory;
+use Luracast\Restler\RestException;
+use PFUser;
+use ProjectManager;
+use SystemEventManager;
+use Tuleap\Git\Permissions\FineGrainedDao;
+use Tuleap\Git\Permissions\FineGrainedRetriever;
+use Tuleap\REST\AuthenticatedResource;
+use Tuleap\REST\Exceptions\InvalidJsonException;
+use Tuleap\REST\Header;
+use Tuleap\REST\JsonDecoder;
+use Tuleap\REST\MissingMandatoryParameterException;
+use Tuleap\REST\ProjectAuthorization;
+use Tuleap\REST\QueryParameterException;
+use Tuleap\REST\QueryParameterParser;
+use URLVerification;
+
+final class GitProjectResource extends AuthenticatedResource
+{
+    private const MAX_LIMIT              = 50;
+    private const PROJECT                = 'project';
+    private const INDIVIDUAL             = 'individual';
+    private const SCOPES_REPRESENTATIONS = [
+        self::PROJECT    => GitRepository::REPO_SCOPE_PROJECT,
+        self::INDIVIDUAL => GitRepository::REPO_SCOPE_INDIVIDUAL
+    ];
+
+    /**
+     * @url    OPTIONS {id}/git
+     *
+     * @param int $id Id of the project
+     */
+    public function optionsGit(int $id): void
+    {
+        Header::allowOptionsGet();
+    }
+
+    /**
+     * Get git
+     *
+     * Get info about project Git repositories. Repositories are returned ordered by last_push date, if there are no push
+     * yet, it's the creation date of the repository that is taken into account.
+     * <br>
+     * The total number of repositories returned by 'x-pagination-size' header corresponds to ALL repositories, including
+     * those you cannot view so you might retrieve a lower number of repositories than 'x-pagination-size'.
+     * <br>
+     * <br>
+     * With fields = 'basic', permissions is always set as <strong>NULL</strong>
+     * <br>
+     * <br>
+     * Basic example:
+     * <br>
+     * <br>
+     * <pre>
+     * "repositories": [{<br>
+     *   &nbsp;"id" : 90,<br>
+     *   &nbsp;"uri": "git/90",<br>
+     *   &nbsp;"name": "repo",<br>
+     *   &nbsp;"path": "project/repo.git",<br>
+     *   &nbsp;"description": "-- Default description --",<br>
+     *   &nbsp;"permissions": null<br>
+     *  }<br>
+     * ...<br>
+     * ]
+     * </pre>
+     * <br>
+     *
+     * <br>
+     * All example:
+     * <br>
+     * <br>
+     * <pre>
+     * "repositories": [{<br>
+     *   &nbsp;"id" : 90,<br>
+     *   &nbsp;"uri": "git/90",<br>
+     *   &nbsp;"name": "repo",<br>
+     *   &nbsp;"path": "project/repo.git",<br>
+     *   &nbsp;"description": "-- Default description --",<br>
+     *   &nbsp;"permissions": {<br>
+     *   &nbsp;   "read": [<br>
+     *   &nbsp;     &nbsp;{<br>
+     *   &nbsp;     &nbsp;  "id": "116_2",<br>
+     *   &nbsp;     &nbsp;  "uri": "user_groups/116_2",<br>
+     *   &nbsp;     &nbsp;  "label": "registered_users",<br>
+     *   &nbsp;     &nbsp;  "users_uri": "user_groups/116_2/users"<br>
+     *   &nbsp;     &nbsp;}<br>
+     *   &nbsp;   ],<br>
+     *   &nbsp;   "write": [<br>
+     *   &nbsp;     &nbsp;{<br>
+     *   &nbsp;     &nbsp;  "id": "116_3",<br>
+     *   &nbsp;     &nbsp;  "uri": "user_groups/116_3",<br>
+     *   &nbsp;     &nbsp;  "label": "project_members",<br>
+     *   &nbsp;     &nbsp;  "users_uri": "user_groups/116_3/users"<br>
+     *   &nbsp;     &nbsp;}<br>
+     *   &nbsp;   ]<br>
+     *   &nbsp;   "rewind": [<br>
+     *   &nbsp;     &nbsp;{<br>
+     *   &nbsp;     &nbsp;  "id": "116_122",<br>
+     *   &nbsp;     &nbsp;  "uri": "user_groups/116_122",<br>
+     *   &nbsp;     &nbsp;  "label": "admins",<br>
+     *   &nbsp;     &nbsp;  "users_uri": "user_groups/116_122/users"<br>
+     *   &nbsp;     &nbsp;}<br>
+     *   &nbsp;   ],<br>
+     *   &nbsp;}<br>
+     *  }<br>
+     * ...<br>
+     * ]
+     * </pre>
+     * <br>
+     * You can use <code>query</code> parameter in order to filter results. Currently you can only filter on scope or
+     * owner_id. By default, all repositories are returned.
+     * <br>
+     * { "scope": "project" } will return only project repositories.
+     * <br>
+     * { "scope": "individual" } will return only forked repositories.
+     * <br>
+     * { "owner_id": 123 } will return all repositories created by user with id 123.
+     * <br>
+     * { "scope": "individual", "owner_id": 123 } will return all repositories forked by user with id 123.
+     *
+     * @url    GET {id}/git
+     * @access hybrid
+     *
+     * @param int    $id       Id of the project
+     * @param int    $limit    Number of elements displayed per page {@from path}
+     * @param int    $offset   Position of the first element to display {@from path}
+     * @param string $fields   Whether you want to fetch permissions or just repository info {@from path}{@choice basic,all}
+     * @param string $query    Filter repositories {@from path}
+     * @param string $order_by {@from path}{@choice push_date,path}
+     *
+     * @return GitRepositoryListRepresentation
+     *
+     * @throws RestException
+     */
+    public function getGit(
+        int $id,
+        int $limit = 10,
+        int $offset = 0,
+        string $fields = GitRepositoryRepresentation::FIELDS_BASIC,
+        string $query = '',
+        string $order_by = 'push_date'
+    ) {
+        $this->checkAccess();
+
+        $user                   = \UserManager::instance()->getCurrentUser();
+        $project                = $this->getProject($id, $user);
+        $result                 = new GitRepositoryListRepresentation();
+        $total_git_repositories = 0;
+
+        $query_parameter_parser = new QueryParameterParser(new JsonDecoder());
+
+        try {
+            $scope    = $this->getScopeFromQueryParameter($query_parameter_parser, $query);
+            $owner_id = $this->getOwnerIdFromQueryParameter($query_parameter_parser, $query);
+        } catch (QueryParameterException $e) {
+            throw new RestException(400, $e->getMessage());
+        } catch (InvalidJsonException $e) {
+            throw new RestException(400, $e->getMessage());
+        }
+
+        $repository_factory        = new GitRepositoryFactory(new \GitDao(), ProjectManager::instance());
+        $total_number_repositories = 0;
+        $git_repositories   = $repository_factory->getPaginatedRepositoriesUserCanSee(
+            $project,
+            $user,
+            $scope,
+            $owner_id,
+            $order_by,
+            $limit,
+            $offset,
+            $total_number_repositories
+        );
+
+        $git_plugin                  = \PluginManager::instance()->getPluginByName('git');
+        $git_system_event_manager    = new Git_SystemEventManager(SystemEventManager::instance(), $repository_factory);
+        $fine_grained_dao            = new FineGrainedDao();
+        $repository_resource_builder = new RepositoryRepresentationBuilder(
+            new GitPermissionsManager(
+                new Git_PermissionsDao(),
+                new Git_SystemEventManager(SystemEventManager::instance(), $repository_factory),
+                $fine_grained_dao,
+                new FineGrainedRetriever($fine_grained_dao)
+            ),
+            new Git_RemoteServer_GerritServerFactory(
+                new Git_RemoteServer_Dao(),
+                new \GitDao(),
+                $git_system_event_manager,
+                ProjectManager::instance()
+            ),
+            new \Git_LogDao(),
+            \EventManager::instance(),
+            new \Git_GitRepositoryUrlManager($git_plugin, new \Tuleap\InstanceBaseURLBuilder())
+        );
+        $result->repositories = $repository_resource_builder->buildWithList($user, $git_repositories, $fields);
+
+        $this->optionsGit($id);
+        Header::sendPaginationHeaders($limit, $offset, $total_git_repositories, self::MAX_LIMIT);
+        return $result;
+    }
+
+    /**
+     * @throws RestException 403
+     * @throws RestException 404
+     */
+    private function getProject(int $id, PFUser $user): \Project
+    {
+        $project = \ProjectManager::instance()->getProject($id);
+        ProjectAuthorization::userCanAccessProject($user, $project, new URLVerification());
+        return $project;
+    }
+
+    /**
+     * @throws InvalidJsonException
+     * @throws \Tuleap\REST\InvalidParameterTypeException
+     * @throws RestException
+     */
+    private function getScopeFromQueryParameter(QueryParameterParser $query_parameter_parser, string $query): string
+    {
+        try {
+            $requested_scope = $query_parameter_parser->getString($query, 'scope');
+            if (! array_key_exists($requested_scope, self::SCOPES_REPRESENTATIONS)) {
+                throw new RestException(400, 'Invalid value supplied for scope. Expected: "project" or "individual".');
+            }
+            return self::SCOPES_REPRESENTATIONS[$requested_scope];
+        } catch (MissingMandatoryParameterException $e) {
+            // no scope is provided, skip it
+        }
+        return '';
+    }
+
+    /**
+     * @throws QueryParameterException
+     * @throws InvalidJsonException
+     */
+    private function getOwnerIdFromQueryParameter(QueryParameterParser $query_parameter_parser, string $query): int
+    {
+        try {
+            return $query_parameter_parser->getInt($query, 'owner_id');
+        } catch (MissingMandatoryParameterException $e) {
+            // no owner_id is provided, skip it
+        }
+        return 0;
+    }
+}

Makefile
@@ -248,7 +248,7 @@ phpcbf: ## Execute PHPCBF with the "strict" ruleset enforced on all the codebase
 
 deptrac: ## Execute deptrac. Use CONFIG to choose the configuration file to evaluate (default to tests/deptrac/core_on_plugins.yml).
 	$(eval CONFIG ?= tests/deptrac/core_on_plugins.yml)
-	@$(PHP) ./src/vendor/bin/deptrac analyze --no-banner -- $(CONFIG)
+	@$(PHP) ./src/vendor/bin/deptrac analyze -- $(CONFIG)
 
 eslint: ## Execute eslint. Use FILES parameter to execute on specific file or directory.
 	$(eval FILES ?= .)

plugins/git/include/REST/ResourcesInjector.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014. All Rights Reserved.
+ * Copyright (c) Enalean, 2014-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,8 +18,10 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Git\REST\v1\GitProjectResource;
 use Tuleap\Git\REST\v1\GitRepositoryRepresentation;
 use Tuleap\Git\REST\v1\GerritServerRepresentation;
+use Tuleap\Project\REST\ProjectRepresentation;
 use Tuleap\Project\REST\ProjectResourceReference;
 
 /**
@@ -30,6 +32,7 @@ class Git_REST_ResourcesInjector
 
     public function populate(Luracast\Restler\Restler $restler)
     {
+        $restler->addAPIClass(GitProjectResource::class, ProjectRepresentation::ROUTE);
         $restler->addAPIClass('\\Tuleap\\Git\\REST\\v1\\RepositoryResource', GitRepositoryRepresentation::ROUTE);
         $restler->addAPIClass('\\Tuleap\\Git\\REST\\v1\\GerritResource', GerritServerRepresentation::ROUTE);
     }

plugins/git/include/REST/v1/GitRepositoryListRepresentation.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -19,12 +19,12 @@
  *
  */
 
-namespace Tuleap\REST\v1;
+namespace Tuleap\Git\REST\v1;
 
 class GitRepositoryListRepresentation
 {
     /**
-     * @var GitRepositoryRepresentationBase[]
+     * @var GitRepositoryRepresentation[]
      */
     public $repositories;
 }

plugins/git/include/REST/v1/GitRepositoryRepresentation.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -23,10 +23,89 @@ namespace Tuleap\Git\REST\v1;
 
 use GitRepository;
 use Tuleap\REST\JsonCast;
-use Tuleap\REST\v1\GitRepositoryRepresentationBase;
 
-class GitRepositoryRepresentation extends GitRepositoryRepresentationBase
+class GitRepositoryRepresentation
 {
+    public const ROUTE = 'git';
+
+    public const FIELDS_BASIC = 'basic';
+    public const FIELDS_ALL   = 'all';
+
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $uri;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The last part of the repository's name
+     * For example:
+     * name = tuleap/rhel6/stable
+     * label = stable
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var string
+     */
+    public $path;
+
+    /**
+     * The repository's path without the project and without the .git suffix.
+     * Used for presentation purposes to highlight a repository's label.
+     * For example:
+     * name = tuleap/rhel6/stable
+     * path = myproject/tuleap/rhel6/stable.git
+     * path_without_project = tuleap/rhel6
+     *
+     * Presentation will be:
+     * tuleap/rhel6
+     * <h1>stable</h1>
+     *
+     * @var string
+     */
+    public $path_without_project;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var string
+     */
+    public $last_update_date;
+
+    /**
+     * @var \Tuleap\Git\REST\v1\GitRepositoryPermissionRepresentation | null
+     */
+    public $permissions = null;
+
+    /**
+     * @var \Tuleap\Git\REST\v1\GerritServerRepresentation | null
+     */
+    public $server = null;
+
+    /**
+     * @var string
+     */
+    public $html_url;
+
+    /**
+     * @var array
+     */
+    public $additional_information;
+
     /**
      * @param string        $html_url
      * @param               $server_representation

plugins/git/include/REST/v1/ProjectResource.class.php
@@ -1,150 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-namespace Tuleap\Git\REST\v1;
-
-use GitRepository;
-use GitRepositoryFactory;
-use Luracast\Restler\RestException;
-use PFUser;
-use Project;
-use Tuleap\REST\Exceptions\InvalidJsonException;
-use Tuleap\REST\MissingMandatoryParameterException;
-use Tuleap\REST\QueryParameterException;
-use Tuleap\REST\QueryParameterParser;
-
-include_once __DIR__ . '/../../../../../src/www/project/admin/permissions.php';
-
-class ProjectResource
-{
-    public const PROJECT                = 'project';
-    public const INDIVIDUAL             = 'individual';
-    public const SCOPES_REPRESENTATIONS = [
-        self::PROJECT    => GitRepository::REPO_SCOPE_PROJECT,
-        self::INDIVIDUAL => GitRepository::REPO_SCOPE_INDIVIDUAL
-    ];
-
-    /**
-     * @var RepositoryRepresentationBuilder
-     */
-    private $repository_resource_builder;
-    /**
-     * @var GitRepositoryFactory
-     */
-    private $repository_factory;
-    /**
-     * @var QueryParameterParser
-     */
-    private $query_parameter_parser;
-
-    public function __construct(
-        GitRepositoryFactory $repository_factory,
-        RepositoryRepresentationBuilder $repository_resource_builder,
-        QueryParameterParser $query_parameter_parser
-    ) {
-        $this->repository_factory          = $repository_factory;
-        $this->repository_resource_builder = $repository_resource_builder;
-        $this->query_parameter_parser      = $query_parameter_parser;
-    }
-
-    /**
-     * @param int $limit
-     * @param int $offset
-     * @param string $fields
-     * @param string $query
-     * @param string $order_by
-     * @param int $total_number_repositories
-     *
-     * @return \Generator
-     * @throws RestException
-     */
-    public function getGit(
-        Project $project,
-        PFUser $user,
-        $limit,
-        $offset,
-        $fields,
-        $query,
-        $order_by,
-        &$total_number_repositories
-    ) {
-        try {
-            $scope    = $this->getScopeFromQueryParameter($query);
-            $owner_id = $this->getOwnerIdFromQueryParameter($query);
-        } catch (QueryParameterException $e) {
-            throw new RestException(400, $e->getMessage());
-        } catch (InvalidJsonException $e) {
-            throw new RestException(400, $e->getMessage());
-        }
-
-        $git_repositories = $this->repository_factory->getPaginatedRepositoriesUserCanSee(
-            $project,
-            $user,
-            $scope,
-            $owner_id,
-            $order_by,
-            $limit,
-            $offset,
-            $total_number_repositories
-        );
-
-        return $this->repository_resource_builder->buildWithList($user, $git_repositories, $fields);
-    }
-
-    /**
-     * @param string $query
-     *
-     * @return string mixed
-     * @throws InvalidJsonException
-     * @throws \Tuleap\REST\InvalidParameterTypeException
-     * @throws RestException
-     */
-    private function getScopeFromQueryParameter($query)
-    {
-        try {
-            $requested_scope = $this->query_parameter_parser->getString($query, 'scope');
-            if (! in_array($requested_scope, array_keys(self::SCOPES_REPRESENTATIONS))) {
-                throw new RestException(400, 'Invalid value supplied for scope. Expected: "project" or "individual".');
-            }
-            $scope = self::SCOPES_REPRESENTATIONS[$requested_scope];
-
-            return $scope;
-        } catch (MissingMandatoryParameterException $e) {
-            // no scope is provided, skip it
-        }
-    }
-
-    /**
-     * @param string $query
-     *
-     * @return string mixed
-     * @throws QueryParameterException
-     * @throws InvalidJsonException
-     */
-    private function getOwnerIdFromQueryParameter($query)
-    {
-        try {
-            return $this->query_parameter_parser->getInt($query, 'owner_id');
-        } catch (MissingMandatoryParameterException $e) {
-            // no owner_id is provided, skip it
-        }
-    }
-}

plugins/git/include/REST/v1/RepositoryRepresentationBuilder.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2014 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -77,16 +77,19 @@ class RepositoryRepresentationBuilder
     /**
      * @param GitRepository[] $repositories
      * @param $fields
-     * @return \Generator
+     * @return GitRepositoryRepresentation[]
      */
-    public function buildWithList(PFUser $user, array $repositories, $fields)
+    public function buildWithList(PFUser $user, array $repositories, $fields): array
     {
+        $representations = [];
         if (count($repositories) > 0) {
             $this->cacheRepositoriesMetadata($repositories);
             foreach ($repositories as $repository) {
-                yield $this->build($user, $repository, $fields);
+                $representations[] = $this->build($user, $repository, $fields);
             }
         }
+
+        return $representations;
     }
 
     private function cacheRepositoriesMetadata(array $repositories)

plugins/git/include/REST/v1/RepositoryResource.class.php
@@ -94,7 +94,6 @@ use Tuleap\PullRequest\REST\v1\RepositoryPullRequestRepresentation;
 use Tuleap\REST\AuthenticatedResource;
 use Tuleap\REST\Header;
 use Tuleap\REST\ProjectStatusVerificator;
-use Tuleap\REST\v1\GitRepositoryRepresentationBase;
 use UserManager;
 
 include_once __DIR__ . '/../../../../../src/www/project/admin/permissions.php';
@@ -370,7 +369,7 @@ class RepositoryResource extends AuthenticatedResource
 
         $this->sendAllowHeaders();
 
-        return $this->representation_builder->build($user, $repository, GitRepositoryRepresentationBase::FIELDS_ALL);
+        return $this->representation_builder->build($user, $repository, GitRepositoryRepresentation::FIELDS_ALL);
     }
 
     /**

commit e0285b35f36967a6f920b4122491fafc904f261b

request #14956: GET /projects/:id/git always returns a pagination size of 0
Issue introduced by 0301188fc4ab7ba1877bf38da23dce0405c8319f.

Change-Id: Ia8ac0b8eb964a82f7c480f540925edd966e668d9

plugins/git/include/REST/v1/GitProjectResource.php
@@ -171,10 +171,9 @@ final class GitProjectResource extends AuthenticatedResource
     ) {
         $this->checkAccess();
 
-        $user                   = \UserManager::instance()->getCurrentUser();
-        $project                = $this->getProject($id, $user);
-        $result                 = new GitRepositoryListRepresentation();
-        $total_git_repositories = 0;
+        $user    = \UserManager::instance()->getCurrentUser();
+        $project = $this->getProject($id, $user);
+        $result  = new GitRepositoryListRepresentation();
 
         $query_parameter_parser = new QueryParameterParser(new JsonDecoder());
 
@@ -223,7 +222,7 @@ final class GitProjectResource extends AuthenticatedResource
         $result->repositories = $repository_resource_builder->buildWithList($user, $git_repositories, $fields);
 
         $this->optionsGit($id);
-        Header::sendPaginationHeaders($limit, $offset, $total_git_repositories, self::MAX_LIMIT);
+        Header::sendPaginationHeaders($limit, $offset, $total_number_repositories, self::MAX_LIMIT);
         return $result;
     }
 

plugins/git/tests/rest/Git/ProjectTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2014 - 2018. All rights reserved
+ * Copyright (c) Enalean, 2014 - Present. All rights reserved
  *
  * This file is a part of Tuleap.
  *
@@ -48,6 +48,7 @@ class ProjectTest extends TestBase
         $repositories          = $repositories_response['repositories'];
 
         $this->assertCount(1, $repositories);
+        $this->assertEquals(1, (int) (string) $response->getHeader('X-Pagination-Size'));
 
         $repository = $repositories[0];
         $this->assertArrayHasKey('id', $repository);