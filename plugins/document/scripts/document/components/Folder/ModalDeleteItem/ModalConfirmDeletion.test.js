Enalean/tuleap

commit 48ce45be4b42cee3db758ebde3489ba78018e71c

Delete should update preview link
part of story #13424 Be able to share a preview link

How to test:
Open an item in quick look mode, then delete it
=> url should be rpelace by its parent
=> actualise your page, no error is displayed

Change-Id: Ib2bfbfe0ce0f8b50d54a820f79ebdc604464f038

plugins/document/scripts/document/components/Folder/ModalDeleteItem/ModalConfirmDeletion.vue
@@ -124,12 +124,6 @@ export default {
                 !this.wiki_page_referencers_loading &&
                 this.wiki_page_referencers !== null
             );
-        },
-        should_redirect_to_parent_folder() {
-            return (
-                (this.is_item_a_folder(this.item) && this.item.id === this.current_folder.id) ||
-                this.shouldRedirectToParentAfterDeletion
-            );
         }
     },
     mounted() {
@@ -150,12 +144,11 @@ export default {
             await this.$store.dispatch("deleteItem", [this.item, this.additional_options]);
 
             if (!this.has_modal_error) {
-                if (this.should_redirect_to_parent_folder) {
-                    this.$router.replace(
-                        { name: "folder", params: { item_id: deleted_item.parent_id } },
-                        this.$store.commit("showPostDeletionNotification")
-                    );
-                }
+                this.$router.replace({
+                    name: "folder",
+                    params: { item_id: deleted_item.parent_id }
+                });
+                this.$store.commit("showPostDeletionNotification");
 
                 this.modal.hide();
             }

plugins/document/scripts/document/components/Folder/ModalDeleteItem/ModalConfirmDeletion.spec.js
@@ -24,20 +24,31 @@ import { USER_CANNOT_PROPAGATE_DELETION_TO_WIKI_SERVICE } from "../../../constan
 import ConfirmationModal from "./ModalConfirmDeletion.vue";
 import { tlp } from "tlp-mocks";
 import { createStoreMock } from "@tuleap-vue-components/store-wrapper.js";
+import VueRouter from "vue-router";
 
 describe("ModalConfirmDeletion", () => {
-    let state, store_options, store;
+    let router, state, store_options, store;
 
-    function getDeletionModal(item = {}) {
+    function getDeletionModal(props = {}) {
+        router = new VueRouter({
+            routes: [
+                {
+                    path: "folder/42",
+                    name: "folder"
+                }
+            ]
+        });
+
         return shallowMount(ConfirmationModal, {
             localVue,
             mocks: { $store: store },
             propsData: {
-                item: { ...item }
+                ...props
             },
             stubs: {
                 "delete-associated-wiki-page-checkbox": `<div data-test="checkbox"></div>`
-            }
+            },
+            router
         });
     }
 
@@ -54,7 +65,8 @@ describe("ModalConfirmDeletion", () => {
 
         tlp.modal.and.returnValue({
             addEventListener: () => {},
-            show: jasmine.createSpy("show")
+            show: () => {},
+            hide: () => {}
         });
 
         store.getters.is_item_a_wiki = () => false;
@@ -91,7 +103,7 @@ describe("ModalConfirmDeletion", () => {
                 }
             ]);
 
-            const deletion_modal = await getDeletionModal(item);
+            const deletion_modal = await getDeletionModal({ item });
 
             expect(store.dispatch).toHaveBeenCalledWith("getWikisReferencingSameWikiPage", item);
             expect(deletion_modal.contains("[data-test=checkbox]")).toBeTruthy();
@@ -102,7 +114,7 @@ describe("ModalConfirmDeletion", () => {
                 .withArgs("getWikisReferencingSameWikiPage", item)
                 .and.returnValue(USER_CANNOT_PROPAGATE_DELETION_TO_WIKI_SERVICE);
 
-            const deletion_modal = await getDeletionModal(item);
+            const deletion_modal = await getDeletionModal({ item });
 
             expect(store.dispatch).toHaveBeenCalledWith("getWikisReferencingSameWikiPage", item);
             expect(deletion_modal.contains("[data-test=checkbox]")).toBeFalsy();
@@ -111,7 +123,7 @@ describe("ModalConfirmDeletion", () => {
         it(`when it does not reference an existing wiki page, then it should not add a checkbox`, () => {
             item.wiki_properties.page_id = null;
 
-            const deletion_modal = getDeletionModal(item);
+            const deletion_modal = getDeletionModal({ item });
 
             expect(store.dispatch).not.toHaveBeenCalled();
             expect(deletion_modal.contains("[data-test=checkbox]")).toBeFalsy();
@@ -127,10 +139,30 @@ describe("ModalConfirmDeletion", () => {
 
         store.getters.is_item_a_folder = () => true;
 
-        const deletion_modal = getDeletionModal(item);
+        const deletion_modal = getDeletionModal({ item });
 
         expect(store.dispatch).not.toHaveBeenCalled();
         expect(deletion_modal.contains("[data-test=delete-folder-warning]")).toBeTruthy();
         expect(deletion_modal.contains("[data-test=checkbox]")).toBeFalsy();
+    });
+
+    it("Delete the item, and update the url link", async () => {
+        const item = {
+            id: 42,
+            title: "my folder",
+            type: "folder"
+        };
+
+        const additional_options = {};
+
+        store.getters.is_item_a_folder = () => true;
+
+        const deletion_modal = getDeletionModal({ item, additional_options });
+        deletion_modal.find("[data-test=confirm-deletion-button]").trigger("click");
+
+        await deletion_modal.vm.$nextTick().then(() => {});
+        expect(store.dispatch).toHaveBeenCalledWith("deleteItem", [item, additional_options]);
+        expect(store.commit).toHaveBeenCalledWith("showPostDeletionNotification");
+        expect(deletion_modal.vm.$route.path).toBe("folder/42");
     });
 });

commit 35319daf6f82e8295541d93856a3b54c561dc389

request #14966 Error when I delete an item when it is not being previewed
Bug introduced by 48ce45be4b42cee3db758ebde3489ba78018e71c

How to test:
- Deleting an item without should not lead to any error, when :
  - Quick look pane is open
  - Quick look pane is closed

- After deleting the current folder
--> You are redirected to the parent folder

Change-Id: I230019ef9e39b1f5109bbd29960c7f4c4ab3c0c7

plugins/document/scripts/document/components/Folder/ModalDeleteItem/ModalConfirmDeletion.vue
@@ -121,7 +121,7 @@ export default {
     },
     computed: {
         ...mapState("error", ["has_modal_error"]),
-        ...mapState(["current_folder"]),
+        ...mapState(["current_folder", "currently_previewed_item"]),
         ...mapGetters(["is_item_a_wiki", "is_item_a_folder"]),
         close() {
             return this.$gettext("Close");
@@ -166,10 +166,7 @@ export default {
             await this.$store.dispatch("deleteItem", [this.item, this.additional_options]);
 
             if (!this.has_modal_error) {
-                await this.$router.replace({
-                    name: "folder",
-                    params: { item_id: deleted_item.parent_id },
-                });
+                await this.redirectToParentFolderIfNeeded(deleted_item);
                 this.$store.commit("showPostDeletionNotification");
 
                 this.modal.hide();
@@ -191,6 +188,21 @@ export default {
         resetModal() {
             this.$store.commit("error/resetModalError");
             this.$emit("delete-modal-closed");
+        },
+        async redirectToParentFolderIfNeeded(deleted_item) {
+            const is_item_the_current_folder = this.item.id === this.current_folder.id;
+            const is_item_being_previewed =
+                this.currently_previewed_item !== null &&
+                this.currently_previewed_item.id === this.item.id;
+
+            if (!is_item_the_current_folder && !is_item_being_previewed) {
+                return;
+            }
+
+            await this.$router.replace({
+                name: "folder",
+                params: { item_id: deleted_item.parent_id },
+            });
         },
     },
 };

plugins/document/scripts/document/components/Folder/ModalDeleteItem/ModalConfirmDeletion.test.js
@@ -56,6 +56,8 @@ describe("ModalConfirmDeletion", () => {
             error: {
                 has_modal_error: false,
             },
+            currently_previewed_item: null,
+            current_folder: { id: 42 },
         };
         store_options = {
             state,
@@ -173,25 +175,62 @@ describe("ModalConfirmDeletion", () => {
         expect(deleteItem).toHaveBeenCalled();
     });
 
-    it("Delete the item, and update the url link", async () => {
-        const item = {
-            id: 42,
-            title: "my folder",
-            type: "folder",
-        };
+    describe("Redirection after deletion", () => {
+        it("Closes the quick look pane when the item to be deleted is currently previewed", async () => {
+            const item = {
+                id: 50,
+                title: "my file",
+                type: "file",
+                parent_id: 42,
+            };
 
-        const additional_options = {};
+            const additional_options = {};
 
-        store.getters.is_item_a_folder = () => true;
+            store.state.currently_previewed_item = item;
+            store.getters.is_item_a_folder = () => false;
 
-        const deletion_modal = getDeletionModal({ item, additional_options });
-        await deletion_modal.vm.deleteItem();
+            const deletion_modal = getDeletionModal({ item, additional_options });
+            jest.spyOn(deletion_modal.vm.$router, "replace");
 
-        await deletion_modal.vm.$nextTick();
-        expect(store.dispatch).toHaveBeenCalledWith("deleteItem", [item, additional_options]);
-        expect(deletion_modal.vm.$store.commit).toHaveBeenCalledWith(
-            "showPostDeletionNotification"
-        );
-        expect(deletion_modal.vm.$route.path).toBe("folder/42");
+            await deletion_modal.vm.$router.push("preview/50");
+            await deletion_modal.vm.deleteItem();
+            await deletion_modal.vm.$nextTick();
+
+            expect(store.dispatch).toHaveBeenCalledWith("deleteItem", [item, additional_options]);
+            expect(deletion_modal.vm.$store.commit).toHaveBeenCalledWith(
+                "showPostDeletionNotification"
+            );
+            expect(deletion_modal.vm.$router.replace).toHaveBeenCalledWith({
+                name: "folder",
+                params: { item_id: 42 },
+            });
+        });
+
+        it("redirects to the parent folder when the item to be deleted is the current folder", async () => {
+            const item = {
+                id: 42,
+                title: "my folder",
+                type: "folder",
+                parent_id: 41,
+            };
+
+            const additional_options = {};
+            const deletion_modal = getDeletionModal({ item, additional_options });
+
+            store.getters.is_item_a_folder = () => true;
+            jest.spyOn(deletion_modal.vm.$router, "replace");
+
+            await deletion_modal.vm.deleteItem();
+            await deletion_modal.vm.$nextTick();
+
+            expect(store.dispatch).toHaveBeenCalledWith("deleteItem", [item, additional_options]);
+            expect(deletion_modal.vm.$store.commit).toHaveBeenCalledWith(
+                "showPostDeletionNotification"
+            );
+            expect(deletion_modal.vm.$router.replace).toHaveBeenCalledWith({
+                name: "folder",
+                params: { item_id: 41 },
+            });
+        });
     });
 });