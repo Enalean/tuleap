ad127718659b9f28b9c3c97c56a802224b2f45f5
Nicolas Terray <nicolas.terray@enalean.com>
2020-06-15 14:46:25 +0200

    Add error state illustration
    If an error happen during the load of backlog items, then we should display an
    illustration.
    
    It also fix error illustration for campaigns when the theme is different
    than blue (the crack should follow the theme color and not stay in
    blue).
    
    Part of story #14884: Display list of backlog items with their tests definition
    
    Change-Id: Ia54d2452af8dd1a09bc8dba754974d37e927ff70

---
+++ plugins/testplan/scripts/test-plan/src/components/BacklogItems/BacklogItemErrorStateSvg.vue
@@ -0,0 +1,85 @@
+<!--
+  - Copyright (c) Enalean, 2020 - present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template functional>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 426 69"
+        height="69"
+        width="426"
+    >
+        <path
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.43448"
+            stroke="#D5E8F3"
+            d="M385.829 47H415.878M420.751 47H424M380.956 43H391.514M396.632 43H399.881M113.117 67H143.167M148.04 67H151.289M104.996 67H108.244M134.233 63H151.289M156.162 63H158.598M19.0553 49.4H42.6078M47.4807 49.4H50.7293M10.1216 53.4H31.4831M2 53.4H5.24862"
+            class="tlp-illustration-tertiary-stroke"
+        ></path>
+        <rect
+            stroke-width="3"
+            stroke="#92CEE5"
+            fill="#E0F6FF"
+            rx="4"
+            height="30.8952"
+            width="287"
+            y="2.99976"
+            x="72"
+            class="tlp-illustration-primary-box"
+        ></rect>
+        <path
+            stroke-width="3"
+            stroke="#92CEE5"
+            d="M108.619 11.9431H88.2606C84.6684 11.9431 81.7563 14.8552 81.7563 18.4474V18.4474C81.7563 22.0396 84.6684 24.9516 88.2606 24.9516H112.619M124.05 11.9431H132.977C136.57 11.9431 139.482 14.8552 139.482 18.4474V18.4474C139.482 22.0396 136.57 24.9516 132.977 24.9516H127.05"
+            class="tlp-illustration-primary-stroke"
+        ></path>
+        <path
+            stroke-linejoin="round"
+            stroke-width="0.5"
+            stroke="#92CEE5"
+            fill="#92CEE5"
+            d="M115.752 21.9021L115.752 21.9022L115.757 21.9041L120.996 23.7228L122.458 28.679C122.493 28.7952 122.572 28.8927 122.679 28.9494L124.502 29.9146C124.984 30.1697 125.42 29.5088 124.995 29.1666L123.615 28.0536L122.438 23.1131C122.404 22.9709 122.303 22.854 122.167 22.7996L117.465 20.9169L121.357 17.4017C121.466 17.3025 121.52 17.1557 121.501 17.0089L120.905 12.4859C120.868 12.2084 120.59 12.0313 120.323 12.1157L120.398 12.3541L120.323 12.1157L115.279 13.7106L118.246 3.33683L118.005 3.2681L118.246 3.33683C118.322 3.07043 118.141 2.79913 117.866 2.76629L116.462 2.59865C116.242 2.57242 116.036 2.70988 115.976 2.92282L112.413 15.498C112.317 15.8386 112.633 16.152 112.973 16.0525L119.321 14.1921L119.791 16.9019L115.762 20.2395L112.989 19.922C112.461 19.8616 112.283 20.6046 112.78 20.7906L115.752 21.9021ZM120.989 23.6981L120.989 23.6982L120.989 23.6981ZM115.191 13.7387C115.191 13.7387 115.191 13.7387 115.191 13.7387L115.191 13.7387ZM119.813 16.8834L119.813 16.8836L119.813 16.8834Z"
+            class="tlp-illustration-primary-stroke tlp-illustration-primary-fill"
+        ></path>
+        <path
+            fill="#C1EDFF"
+            d="M324.302 32.5H318L340.477 4L341.5 10L324.302 32.5Z"
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            class="tlp-illustration-secondary-dark-fill"
+        ></path>
+        <path
+            stroke-width="3"
+            stroke="#FFC96A"
+            fill="#FCEFD8"
+            d="M345.893 2.71125C345.771 2.51344 345.604 2.34678 345.406 2.22445C344.702 1.78872 343.777 2.00667 343.342 2.71125L320.224 40.0926C320.078 40.3297 320 40.6028 320 40.8816C320 41.71 320.672 42.3816 321.5 42.3816H367.735C368.014 42.3816 368.287 42.3039 368.524 42.1573C369.228 41.7216 369.446 40.7972 369.011 40.0926L345.893 2.71125Z"
+        ></path>
+        <path
+            fill="#FFC96A"
+            d="M344.094 14.7964H345.141C346.245 14.7964 347.141 15.6918 347.141 16.7964C347.141 16.8438 347.139 16.8912 347.135 16.9385L346.334 28.1981C346.269 29.0986 345.52 29.7964 344.617 29.7964C343.715 29.7964 342.965 29.0986 342.901 28.1981L342.099 16.9385C342.021 15.8367 342.85 14.8799 343.952 14.8014C344 14.7981 344.047 14.7964 344.094 14.7964ZM344.117 31.7964H345.117C346.222 31.7964 347.117 32.6918 347.117 33.7964V34.7964C347.117 35.901 346.222 36.7964 345.117 36.7964H344.117C343.013 36.7964 342.117 35.901 342.117 34.7964V33.7964C342.117 32.6918 343.013 31.7964 344.117 31.7964Z"
+        ></path>
+        <path
+            fill="white"
+            d="M342.982 6.57239L344.627 3.79639L367.117 40.5724H363.531L342.982 6.57239Z"
+            opacity="0.526"
+        ></path>
+    </svg>
+</template>
