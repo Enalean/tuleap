Enalean/tuleap

commit 48597d1a1bbc49a3cdf0e9aa6dd0f4901f864446

Remove taint analysis false positives in TLP doc
The taint analysis detects some LFI and XSS in the TLP doc because the
code does not make it easy to actually detect the `$current_section`
variable is built from known values. This small code changes make it
easier for the taint analysis engine to detect that.

Note that you may need to comment the line `require($local_inc)` in
pre.php to see the issue.

Part request #15015: Initiate usage of Psalm taint analysis feature

Change-Id: Iacaed6ce6862d85dd9b90d1614bb3bb7c88ce50e

src/www/tlp-doc/index.php
@@ -61,8 +61,8 @@ function discoverSection(string $basepath): array
 
 $sections = discoverSection('resources');
 $current_section = key($sections);
-if (isset($_GET['section']) && isset($sections[$_GET['section']])) {
-    $current_section = $_GET['section'];
+if (isset($_GET['section'], $sections[$_GET['section']])) {
+    $current_section = key(array_intersect_key($sections, [$_GET['section'] => true]));
 }
 $sections[$current_section]['selected'] = true;
 

commit 9540df216efa24d8c6aae1131cc48448c681836f

Fix SA issues in TLP doc
Introduced by 48597d1a1bbc49a3cdf0e9aa6dd0f4901f864446.

Part of request #15015: Initiate usage of Psalm taint analysis feature

Change-Id: Id11b030e81f3a327907da7e2425d741dd4781751

src/www/tlp-doc/index.php
@@ -220,7 +220,7 @@ $main_doc_script     = $include_asset_doc->getFileURL('script.js');
                 <?php endif; ?>
             </h2>
             <?php
-            displaySectionContent($documentation, $example, $demo, $id);
+            displaySectionContent($documentation, $example, $demo, (string) $id);
 
             if (isset($section['children']) && $section['children']) {
                 foreach ($section['children'] as $subsection_id => $section) {
@@ -252,7 +252,7 @@ $main_doc_script     = $include_asset_doc->getFileURL('script.js');
                             </h3>
                         <?php endif;
 
-                        displaySectionContent($documentation, $example, $demo, $id, $subsection_id);
+                        displaySectionContent($documentation, $example, $demo, (string) $id, $subsection_id);
                         ?>
                     </section>
                     <?php