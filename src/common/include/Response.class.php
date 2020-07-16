Enalean/tuleap

commit 2ed8472fc4bc67c6e4d7dd1d519707c27a61dd01

Fix false positive issues identified by Psalm taint analysis
Some of the issue are not false positives but are not exploitable in any standard
configurations.

`make psalm-taint-analysis` does not find any error.

Part of request #15015: Initiate usage of Psalm taint analysis feature

Change-Id: Ieac7566e21b7d6646b42daca8c336565efc1b2a4

src/common/include/Response.class.php
@@ -146,8 +146,7 @@ class Response
 
     public function sendStatusCode($code)
     {
-        header("HTTP/1.0 $code");
-        echo $this->getRawFeedback();
+        http_send_status($code);
     }
 
     public function setContentType($content_type)

plugins/docman/include/view/Docman_View_Redirect.class.php
@@ -73,7 +73,11 @@ class Docman_View_Redirect extends Docman_View_View /* implements Visitor */
             return '/';
         }
 
-        header('Location: ' . $url);
+        /**
+         * @psalm-taint-escape text
+         */
+        $header = 'Location: ' . $url;
+        header($header);
         exit();
     }
 

plugins/git/include/GitPHP/git/FileDiff.php
@@ -670,7 +670,7 @@ class FileDiff
                 }
             }
 
-            $this->diffData = DiffExe::Diff((empty($fromTmpFile) ? null : escapeshellarg($tmpdir->GetDir() . $fromTmpFile)), $fromName, (empty($toTmpFile) ? null : escapeshellarg($tmpdir->GetDir() . $toTmpFile)), $toName);
+            $this->diffData = DiffExe::Diff((empty($fromTmpFile) ? null : escapeshellarg($tmpdir->GetDir() . $fromTmpFile)), $fromName === null ? null : escapeshellarg($fromName), (empty($toTmpFile) ? null : escapeshellarg($tmpdir->GetDir() . $toTmpFile)), $toName);
 
             if (!empty($fromTmpFile)) {
                 $tmpdir->RemoveFile($fromTmpFile);

plugins/ldap/include/WelcomeDisplayController.php
@@ -89,12 +89,12 @@ class WelcomeDisplayController implements DispatchableWithRequest
         if ($pv === 2) {
             $layout->pv_header(array());
         } else {
-            $layout->header(array('title' => $GLOBALS['Language']->getText('plugin_ldap', 'welcome_title', array($lr->getCommonName())),
+            $layout->header(array('title' => $GLOBALS['Language']->getText('plugin_ldap', 'welcome_title', array($this->html_purifier->purify($lr->getCommonName()))),
                                 'registeration_process' => true));
         }
 
         print '<h2>';
-        print $GLOBALS['Language']->getText('plugin_ldap', 'welcome_title', array($lr->getCommonName()));
+        print $GLOBALS['Language']->getText('plugin_ldap', 'welcome_title', array($this->html_purifier->purify($lr->getCommonName())));
         print '</h2>';
 
         print '<h3>';
@@ -115,10 +115,10 @@ class WelcomeDisplayController implements DispatchableWithRequest
         }
 
         print '
-<form name="welcome" action="' . $this->base_url . '/welcome" method="post">
+<form name="welcome" action="' . $this->html_purifier->purify($this->base_url) . '/welcome" method="post">
 <input type="hidden" name="return_to" value="' . $this->html_purifier->purify($return_to, CODENDI_PURIFIER_CONVERT_HTML) . '">
 <input type="hidden" name="action" value="update_reg">
-<input type="hidden" name="pv" value="' . $pv . '">
+<input type="hidden" name="pv" value="' . $this->html_purifier->purify($pv) . '">
 
 <p>' . $star . ' ' . $GLOBALS['Language']->getText('plugin_ldap', 'welcome_tz') . ':';
 
@@ -141,15 +141,15 @@ class WelcomeDisplayController implements DispatchableWithRequest
         print '<table>
 <tr>
 <td>' . $GLOBALS['Language']->getText('plugin_ldap', 'welcome_ldap_login') . '</td>
-<td><strong>' . $ldap_name . '</strong></td>
+<td><strong>' . $this->html_purifier->purify($ldap_name) . '</strong></td>
 </tr>
 <tr>
 <td>' . $GLOBALS['Language']->getText('plugin_ldap', 'welcome_email') . '</td>
-<td><strong>' . $currentUser->getEmail() . '</strong></td>
+<td><strong>' . $this->html_purifier->purify($currentUser->getEmail()) . '</strong></td>
 </tr>
 <tr>
 <td>' . $GLOBALS['Language']->getText('plugin_ldap', 'welcome_codendi_login', array(\ForgeConfig::get('sys_name'))) . '</td>
-<td>' . $currentUser->getUserName() . '<br>
+<td>' . $this->html_purifier->purify($currentUser->getUserName()) . '<br>
 ' . $GLOBALS['Language']->getText('plugin_ldap', 'welcome_codendi_login_j', array(\ForgeConfig::get('sys_name'))) . '
 </td>
 </tr>

plugins/projectlinks/include/form_utils.php
@@ -1081,6 +1081,5 @@ function update_database($tableName, $items, $selectCriteria = "")
         }
         $sql = "UPDATE $tableName SET $SQLstr WHERE ($selectCriteria);";
     }
-    global $show_dbtx;  // control echo of database update SQL
-    return db_query($sql, isset($show_dbtx));
+    return db_query($sql);
 }

src/common/language/BaseLanguage.class.php
@@ -285,6 +285,9 @@ class BaseLanguage
         $this->text_array = $this->compileLanguage($lang);
     }
 
+    /**
+     * @psalm-taint-specialize
+     */
     public function getText($pagename, $category, $args = "")
     {
         // If the language files were modified by an update, the compiled version might not have been generated,

src/www/include/database.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2012-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
  * Copyright 1999-2000 (c) The SourceForge Crew
  *
  * This file is a part of Tuleap.
@@ -28,11 +28,8 @@ if (!defined('CODENDI_DB_NOT_NULL')) {
 /**
  * @deprecated
  */
-function db_query($sql, $print = 0)
+function db_query($sql)
 {
-    if ($print) {
-        print "<br>Query is: $sql<br>";
-    }
     /** @psalm-suppress DeprecatedFunction */
     return db_query_params($sql, array());
 }

src/www/soap/index.php
@@ -60,7 +60,9 @@ try {
     // include the <Plugin> API (only if plugin is available)
     $event_manager->processEvent('soap', array());
 } catch (Exception $e) {
-    echo $e;
+    header('Content-Type: text/plain', true, 500);
+    echo $e->getMessage();
+    exit();
 }
 
 

commit 005e702e23a71797cd4411ac637a91cc25e4822e

Crash when \Response::sendStatusCode() is called
Introduced by 2ed8472fc4bc67c6e4d7dd1d519707c27a61dd01.

SA did not find the issue because \http_send_status is part of the PECL
pecl_http which is stubbed.

Easiest way to reproduce the issue is to call GET /goto without any
parameters.

Part of request #15015: Initiate usage of Psalm taint analysis feature

Change-Id: Iaee38a9eee3cfb4a3fbd4ffacd04adead47a23ae

src/common/include/Response.class.php
@@ -127,7 +127,7 @@ class Response
 
     public function sendStatusCode($code)
     {
-        http_send_status($code);
+        \http_response_code($code);
     }
 
     public function setContentType($content_type)