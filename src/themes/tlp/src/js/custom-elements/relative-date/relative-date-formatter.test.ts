8c366b651be926d1632e2a6de812508d8857652d
Nicolas Terray <nicolas.terray@enalean.com>
2020-07-17 15:45:12 +0200

    Do not rely on current time in tests
    This can produce flaky tests such as:
    https://ci.tuleap.org/jenkins/job/tuleap-gerrit-tests/24367/testReport/junit/(root)/relative-date-formatter%20Displays%20X%20minutes%20ago/Tests___Jest___relative_date_formatter_Displays_X_minutes_ago/
    
    Part of story #14828: display dates with both absolute and relative dates
    
    Change-Id: Ie1fdfe0965083564136271e307e91382355be6c4

--- src/themes/tlp/src/js/custom-elements/relative-date/relative-date-formatter.test.ts
+++ src/themes/tlp/src/js/custom-elements/relative-date/relative-date-formatter.test.ts
@@ -25,51 +25,59 @@ const a_day_in_ms = 24 * a_hour_in_ms;
 const a_month_in_ms = 30 * a_day_in_ms;
 const a_year_in_ms = 12 * a_month_in_ms;
 
+const now = 1234567890000;
+
 describe("relative-date-formatter", () => {
     it("Displays X minutes ago", () => {
-        expect(formatRelativeDate("en-US", new Date())).toBe("0 minutes ago");
-        expect(formatRelativeDate("en-US", new Date(Date.now() - a_minute_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now), new Date(now))).toBe("0 minutes ago");
+        expect(formatRelativeDate("en-US", new Date(now - a_minute_in_ms), new Date(now))).toBe(
             "1 minute ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 44 * a_minute_in_ms))).toBe(
-            "44 minutes ago"
-        );
+        expect(
+            formatRelativeDate("en-US", new Date(now - 44 * a_minute_in_ms), new Date(now))
+        ).toBe("44 minutes ago");
     });
     it("Displays X hours ago", () => {
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 45 * a_minute_in_ms))).toBe(
+        expect(
+            formatRelativeDate("en-US", new Date(now - 45 * a_minute_in_ms), new Date(now))
+        ).toBe("1 hour ago");
+        expect(formatRelativeDate("en-US", new Date(now - a_hour_in_ms), new Date(now))).toBe(
             "1 hour ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - a_hour_in_ms))).toBe("1 hour ago");
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 2 * a_hour_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - 2 * a_hour_in_ms), new Date(now))).toBe(
             "2 hours ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 23 * a_hour_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - 23 * a_hour_in_ms), new Date(now))).toBe(
             "23 hours ago"
         );
     });
     it("Displays X days ago", () => {
-        expect(formatRelativeDate("en-US", new Date(Date.now() - a_day_in_ms))).toBe("1 day ago");
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 2 * a_day_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - a_day_in_ms), new Date(now))).toBe(
+            "1 day ago"
+        );
+        expect(formatRelativeDate("en-US", new Date(now - 2 * a_day_in_ms), new Date(now))).toBe(
             "2 days ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 29 * a_day_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - 29 * a_day_in_ms), new Date(now))).toBe(
             "29 days ago"
         );
     });
     it("Displays X months ago", () => {
-        expect(formatRelativeDate("en-US", new Date(Date.now() - a_month_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - a_month_in_ms), new Date(now))).toBe(
             "1 month ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 2 * a_month_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - 2 * a_month_in_ms), new Date(now))).toBe(
             "2 months ago"
         );
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 11 * a_month_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - 11 * a_month_in_ms), new Date(now))).toBe(
             "11 months ago"
         );
     });
     it("Displays X years ago", () => {
-        expect(formatRelativeDate("en-US", new Date(Date.now() - a_year_in_ms))).toBe("1 year ago");
-        expect(formatRelativeDate("en-US", new Date(Date.now() - 2 * a_year_in_ms))).toBe(
+        expect(formatRelativeDate("en-US", new Date(now - a_year_in_ms), new Date(now))).toBe(
+            "1 year ago"
+        );
+        expect(formatRelativeDate("en-US", new Date(now - 2 * a_year_in_ms), new Date(now))).toBe(
             "2 years ago"
         );
     });
