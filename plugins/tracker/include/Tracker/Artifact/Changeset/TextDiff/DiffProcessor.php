66215cbf07e8955da4f13670f110492835532c08
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2020-08-03 17:00:58 +0200

    Add the switch to markup format button
    part of story #14897 see rendered html diff in artifact changes
    
    How to test:
    toggle a text changeset
    
    there is now a new button "switch to markup"
    click on switch to markup => content is loaded in text format
    
    This patch should handle correcty network requests, we should not load N times the same resource.
    
    Change-Id: I8533d6d108fbc70db1049186d415fa0473b0409e

--- plugins/tracker/include/Tracker/Artifact/Changeset/TextDiff/DiffProcessor.php
+++ plugins/tracker/include/Tracker/Artifact/Changeset/TextDiff/DiffProcessor.php
@@ -39,18 +39,24 @@ class DiffProcessor
 
     public function processDiff(
         \Tracker_Artifact_ChangesetValue_Text $next_changeset_value,
-        array $previous_value,
-        array $next_value,
+        \Tracker_Artifact_ChangesetValue_Text $previous_changeset_value,
         string $format
     ): string {
         switch ($format) {
             case 'text':
+                $previous_value = explode(PHP_EOL, $previous_changeset_value->getText());
+                $next_value     = explode(PHP_EOL, $next_changeset_value->getText());
+
                 $diff = new Codendi_Diff($previous_value, $next_value);
 
                 return PHP_EOL . $this->diff_formatter->format($diff);
             case 'strip-html':
+                $previous_value = explode(PHP_EOL, $previous_changeset_value->getValue());
+                $next_value     = explode(PHP_EOL, $next_changeset_value->getValue());
                 return $next_changeset_value->getFormattedDiff($previous_value, $next_value, CODENDI_PURIFIER_STRIP_HTML);
             case 'html':
+                $previous_value = explode(PHP_EOL, $previous_changeset_value->getText());
+                $next_value     = explode(PHP_EOL, $next_changeset_value->getText());
                 return $next_changeset_value->getFormattedDiff($previous_value, $next_value, CODENDI_PURIFIER_CONVERT_HTML);
             default:
                 return "";
