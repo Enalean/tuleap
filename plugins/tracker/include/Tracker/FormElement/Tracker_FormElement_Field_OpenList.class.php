Enalean/tuleap

commit 5e769e9084a5d9a80a1debfb77c5f17f8b520b04

request #15095 Add a way to manage open list custom values
Add the possibility to see, edit and hide custom values like a regular value.

Change-Id: Ic259dd84809c8be51d375109b3753e1e3f42bafb

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_OpenList.class.php
@@ -260,7 +260,8 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
             foreach ($this->getOpenValueDao()->searchByKeyword($this->getId(), $keyword, $limit - $nb) as $row) {
                 $matching_values[] = new Tracker_FormElement_Field_List_OpenValue(
                     $row['id'],
-                    $row['label']
+                    $row['label'],
+                    $row['is_hidden']
                 );
             }
         }
@@ -371,7 +372,8 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
             if ($row = $this->getOpenValueDao()->searchById($this->getId(), $oid)->getRow()) {
                 $this->cache_openvalues[$oid] = new Tracker_FormElement_Field_List_OpenValue(
                     $row['id'],
-                    $row['label']
+                    $row['label'],
+                    $row['is_hidden']
                 );
             }
         }
@@ -521,7 +523,8 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
             if ($row['openvalue_label']) {
                 $v = new Tracker_FormElement_Field_List_OpenValue(
                     $row['id'],
-                    $row['openvalue_label']
+                    $row['openvalue_label'],
+                    $row['openvalue_is_hidden']
                 );
             } else {
                 $v = $this->getBind()->getValueFromRow($row);
@@ -569,7 +572,8 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
             if ($row['openvalue_label']) {
                 $v = new Tracker_FormElement_Field_List_OpenValue(
                     $row['id'],
-                    $row['openvalue_label']
+                    $row['openvalue_label'],
+                    $row['is_hidden']
                 );
             } else {
                 $v = $this->getBind()->getValueFromRow($row);

plugins/tracker/db/install.sql
@@ -208,6 +208,7 @@ CREATE TABLE tracker_field_openlist_value(
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     field_id INT(11) UNSIGNED NOT NULL,
     label VARCHAR(255) NOT NULL DEFAULT '',
+    is_hidden BOOL DEFAULT FALSE,
     INDEX idx_search(field_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=101;
 

plugins/tracker/db/mysql/updates/2020/202007221453_add_is_hidden_collumns_for_open_value.php
@@ -1,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+class b202007221453_add_is_hidden_collumns_for_open_value extends ForgeUpgrade_Bucket
+{
+    public function description()
+    {
+        return 'Add is_hidden columns for open value table';
+    }
+
+    public function preUp()
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up()
+    {
+        $sql = "alter table tracker_field_openlist_value ADD COLUMN is_hidden BOOL DEFAULT FALSE";
+        $this->db->alterTable(
+            'tracker_field_openlist_value',
+            'tuleap',
+            'title',
+            $sql
+        );
+    }
+}

plugins/tracker/include/Tracker/FormElement/FormElementListValueAdminViewPresenter.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Tracker\FormElement;
 
-use Tracker_FormElement_Field_List_Bind_StaticValue;
+use Tracker_FormElement_Field_List_Value;
 use Tuleap\Tracker\Colorpicker\ColorpickerMountPointPresenter;
 
 /**
@@ -34,7 +34,7 @@ class FormElementListValueAdminViewPresenter
      */
     public $id;
     /**
-     * @var ColorpickerMountPointPresenter
+     * @var ColorpickerMountPointPresenter | null
      */
     public $decorator;
     /**
@@ -73,16 +73,21 @@ class FormElementListValueAdminViewPresenter
      * @var bool
      */
     public $is_none_value;
+    /**
+     * @var bool
+     */
+    public $is_custom_value;
 
     public function __construct(
-        Tracker_FormElement_Field_List_Bind_StaticValue $value,
-        ColorpickerMountPointPresenter $decorator,
+        Tracker_FormElement_Field_List_Value $value,
+        ?ColorpickerMountPointPresenter $decorator,
         bool $value_can_be_hidden,
         bool $value_can_be_deleted,
         ?string $delete_url,
         string $image_hidden_title,
         string $image_hidden_alt,
-        string $image_hidden_prefix
+        string $image_hidden_prefix,
+        bool $is_custom_value
     ) {
         $this->id                   = $value->getId();
         $this->label                = $value->getLabel();
@@ -95,5 +100,6 @@ class FormElementListValueAdminViewPresenter
         $this->image_hidden_prefix  = $image_hidden_prefix;
         $this->decorator            = $decorator;
         $this->is_none_value        = (int) $value->getId() === \Tracker_FormElement_Field_List::NONE_VALUE;
+        $this->is_custom_value      = $is_custom_value;
     }
 }

plugins/tracker/include/Tracker/FormElement/FormElementListValueAdminViewPresenterBuilder.php
@@ -24,7 +24,8 @@ namespace Tuleap\Tracker\FormElement;
 use Tracker_FormElement_Field;
 use Tracker_FormElement_Field_List;
 use Tracker_FormElement_Field_List_Bind_Static_ValueDao;
-use Tracker_FormElement_Field_List_Bind_StaticValue;
+use Tracker_FormElement_Field_List_OpenValue;
+use Tracker_FormElement_Field_List_Value;
 use Tuleap\Tracker\Colorpicker\ColorpickerMountPointPresenter;
 
 class FormElementListValueAdminViewPresenterBuilder
@@ -42,8 +43,9 @@ class FormElementListValueAdminViewPresenterBuilder
 
     public function buildPresenter(
         \Tracker_FormElement_Field $field,
-        Tracker_FormElement_Field_List_Bind_StaticValue $value,
-        ColorpickerMountPointPresenter $decorator
+        Tracker_FormElement_Field_List_Value $value,
+        ?ColorpickerMountPointPresenter $decorator,
+        bool $is_custom_value
     ): FormElementListValueAdminViewPresenter {
         $value_can_be_hidden  = $this->canValueBeHidden($value, $field);
         $value_can_be_deleted = $this->canValueBeDeleted($value, $field);
@@ -85,6 +87,7 @@ class FormElementListValueAdminViewPresenterBuilder
             $image_title,
             $image_hidden_alt,
             $image_hidden_prefix,
+            $is_custom_value
         );
     }
 
@@ -100,14 +103,22 @@ class FormElementListValueAdminViewPresenterBuilder
         return '';
     }
 
-    public function canValueBeHidden(Tracker_FormElement_Field_List_Bind_StaticValue $value, Tracker_FormElement_Field $field): bool
+    public function canValueBeHidden(Tracker_FormElement_Field_List_Value $value, Tracker_FormElement_Field $field): bool
     {
+        if ($value instanceof Tracker_FormElement_Field_List_OpenValue) {
+            return true;
+        }
+
         return $value->getId() !== Tracker_FormElement_Field_List::NONE_VALUE
             && $this->value_dao->canValueBeHidden($field, $value->getId());
     }
 
-    public function canValueBeDeleted(Tracker_FormElement_Field_List_Bind_StaticValue $value, Tracker_FormElement_Field $field): bool
+    public function canValueBeDeleted(Tracker_FormElement_Field_List_Value $value, Tracker_FormElement_Field $field): bool
     {
+        if ($value instanceof Tracker_FormElement_Field_List_OpenValue) {
+            return false;
+        }
+
         return $value->getId() !== Tracker_FormElement_Field_List::NONE_VALUE
             && $this->value_dao->canValueBeDeleted($field, $value->getId());
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.class.php
@@ -429,12 +429,18 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
     }
 
     private function getAdminEditRowModifiable(
-        Tracker_FormElement_Field_List_Bind_StaticValue $value,
-        ColorpickerMountPointPresenter $decorator
+        Tracker_FormElement_Field_List_Value $value,
+        ?ColorpickerMountPointPresenter $decorator,
+        bool $is_custom_value
     ): string {
         return $this->getTemplateRenderer()->renderToString(
             'admin-edit-row-modifiable',
-            $this->getFormElementListAdminViewBuilder()->buildPresenter($this->field, $value, $decorator)
+            $this->getFormElementListAdminViewBuilder()->buildPresenter(
+                $this->field,
+                $value,
+                $decorator,
+                $is_custom_value
+            )
         );
     }
 
@@ -471,10 +477,8 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
 
     /**
      * Fetch the form to edit the formElement
-     *
-     * @return string html
      */
-    public function fetchAdminEditForm()
+    public function fetchAdminEditForm(): string
     {
         if ($this->field->isTargetSharedField()) {
             return $this->fetchAdminEditFormNotModifiable();
@@ -486,16 +490,23 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
     private function fetchAdminEditFormModifiable(): string
     {
         $html = '';
+
+        $html .= $this->fetchAdminEditFormModifiableWithUsersValues();
+
         $html .= '<h3>' . dgettext('tuleap-tracker', 'Static values') . '</h3>';
 
-        $h = new HTML_Element_Input_Checkbox(dgettext('tuleap-tracker', 'alphabetically sort values'), 'bind[is_rank_alpha]', $this->is_rank_alpha);
+        $h = new HTML_Element_Input_Checkbox(
+            dgettext('tuleap-tracker', 'alphabetically sort values'),
+            'bind[is_rank_alpha]',
+            $this->is_rank_alpha
+        );
         $h->setId('is_rank_alpha');
         $h->addParam('class', 'is_rank_alpha');
         $html .= '<p>' . $h->render() . '</p>';
 
         $html .= '<table><tr><td>';
         $html .= '<input type="hidden" name="bind[order]" class="bind_order_values" value="" />';
-        $html .= '<ul class="tracker-admin-bindvalue_list">';
+        $html .= '<ul class="tracker-admin-bindvalue_list tracker-admin-bindvalue_list_sortable">';
 
         $retriever = new BindValuesAdder();
         $possible_values = $retriever->addNoneValue($this->getAllValues());
@@ -510,11 +521,48 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
         $html .= '<p id="tracker-admin-bind-static-addnew">';
         $html .= '<strong>' . dgettext('tuleap-tracker', 'Add new values') . '</strong><br />';
         $html .= '<textarea name="bind[add]" rows="5" cols="30"></textarea><br />';
-        $html .= '<span style="color:#999; font-size:0.8em;">' . dgettext('tuleap-tracker', 'Add one value per row') . '</span><br />';
+        $html .= '<span style="color:#999; font-size:0.8em;">' . dgettext(
+            'tuleap-tracker',
+            'Add one value per row'
+        ) . '</span><br />';
         $html .= '</p>';
 
         //Select default values
         $html .= $this->getField()->getSelectDefaultValues($this->default_values);
+
+        return $html;
+    }
+
+    private function fetchAdminEditFormModifiableWithUsersValues(): string
+    {
+        $html            = '';
+        $user_row_values = $this->getOpenValueDao()->searchByFieldId($this->getField()->getId());
+
+        if ($user_row_values->count() === 0) {
+            return $html;
+        }
+
+        $html        .= '<h3>' . dgettext('tuleap-tracker', 'Values added by users') . '</h3>';
+        $user_values = [];
+
+        foreach ($user_row_values as $row_value) {
+            $user_values[] = new Tracker_FormElement_Field_List_OpenValue(
+                $row_value['id'],
+                $row_value['label'],
+                $row_value['is_hidden']
+            );
+        }
+
+        $html .= '<table><tr><td>';
+        $html .= '<input type="hidden" name="bind[order]" value="" />';
+        $html .= '<ul class="tracker-admin-bindvalue_list">';
+
+        foreach ($user_values as $value) {
+            $html .= $this->getAdminEditRowModifiable($value, null, true);
+        }
+
+        $html .= '</ul>';
+        $html .= '</td></tr></table>';
 
         return $html;
     }
@@ -538,7 +586,7 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
             );
         }
 
-        return $this->getAdminEditRowModifiable($value, $decorator);
+        return $this->getAdminEditRowModifiable($value, $decorator, false);
     }
 
     private function fetchAdminEditFormNotModifiable()
@@ -654,6 +702,17 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
                                 unset($new_label, $new_description);
                             }
                         }
+                    }
+                    break;
+                case "edit_custom":
+                    $values = $this->getOpenValueDao()->searchByFieldId($this->field->getId());
+                    foreach ($values as $row) {
+                        $new_is_hidden = ! isset($value[$row['id']]['is_hidden']);
+                        $this->getOpenValueDao()->updateOpenValue(
+                            (int) $row['id'],
+                            $new_is_hidden,
+                            $value[$row['id']]['label']
+                        );
                     }
                     break;
                 case 'add':

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_OpenValue.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright (c) Xerox Corporation, Codendi Team, 2001-2009. All rights reserved
- * Copyright (c) Enalean, 2015-2018. All Rights Reserved.
+ * Copyright (c) Enalean, 2015-present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -27,9 +27,9 @@ class Tracker_FormElement_Field_List_OpenValue extends Tracker_FormElement_Field
 
     protected $label;
 
-    public function __construct($id, $label)
+    public function __construct($id, $label, bool $is_hidden)
     {
-        parent::__construct($id, false);
+        parent::__construct($id, $is_hidden);
         $this->label = $label;
     }
 

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Value.class.php
@@ -40,6 +40,9 @@ abstract class Tracker_FormElement_Field_List_Value implements Tracker_IProvideJ
         $this->is_hidden = $is_hidden;
     }
 
+    /**
+     * @psalm-mutation-free
+    */
     public function getId()
     {
         return $this->id;
@@ -88,6 +91,9 @@ abstract class Tracker_FormElement_Field_List_Value implements Tracker_IProvideJ
 
     abstract public function __toString();
 
+    /**
+     * @psalm-mutation-free
+     */
     abstract public function getLabel();
 
     public function fetchFormatted()

commit 104300a4c1252dbee6cbe526fae6811ede4a3e51

request #16194 Export csv of openvalue is broken
introduced by 5e769e9084a5d9a80a1debfb77c5f17f8b520b04

Change-Id: Ifa631d5d658f004fb93c8a36f549bf1d22450db0

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_OpenList.class.php
@@ -573,7 +573,7 @@ class Tracker_FormElement_Field_OpenList extends Tracker_FormElement_Field_List 
                 $v = new Tracker_FormElement_Field_List_OpenValue(
                     $row['id'],
                     $row['openvalue_label'],
-                    $row['is_hidden']
+                    $row['openvalue_is_hidden']
                 );
             } else {
                 $v = $this->getBind()->getValueFromRow($row);