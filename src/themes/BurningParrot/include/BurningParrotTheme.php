Enalean/tuleap

commit a7826dee5d3c13d7d0f4d5f1219182d49b4ba8a5

auto update release note on major upgrade
part of : story #15102 highlight informations to the end user

Every major change, the new link is created. You can try it by changing tuleap_link on table release_note_link
This is only possible to the link on lab mod

We keep the possibility to edit actual_link value on database for extrem cases

Change-Id: Ib492746ae6b0109e2a861dcfc510bee4a867d7a8

src/themes/BurningParrot/include/BurningParrotTheme.php
@@ -28,6 +28,8 @@ use TemplateRendererFactory;
 use Tuleap\BuildVersion\FlavorFinderFromFilePresence;
 use Tuleap\BuildVersion\VersionPresenter;
 use Tuleap\HelpDropdown\HelpDropdownPresenterBuilder;
+use Tuleap\HelpDropdown\ReleaseLinkDao;
+use Tuleap\HelpDropdown\ReleaseNoteManager;
 use Tuleap\Layout\BaseLayout;
 use Tuleap\Layout\BreadCrumbDropdown\BreadCrumbPresenterBuilder;
 use Tuleap\layout\NewDropdown\NewDropdownPresenterBuilder;
@@ -36,9 +38,11 @@ use Tuleap\OpenGraph\NoOpenGraphPresenter;
 use Tuleap\Project\Flags\ProjectFlagsBuilder;
 use Tuleap\Project\Flags\ProjectFlagsDao;
 use Tuleap\Project\Registration\ProjectRegistrationUserPermissionChecker;
+use Tuleap\Sanitizer\URISanitizer;
 use Tuleap\Theme\BurningParrot\Navbar\PresenterBuilder as NavbarPresenterBuilder;
 use URLRedirect;
 use UserManager;
+use Valid_HTTPURI;
 use Widget_Static;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -124,8 +128,12 @@ class BurningParrotTheme extends BaseLayout
 
         $open_graph = isset($params['open_graph']) ? $params['open_graph'] : new NoOpenGraphPresenter();
 
-        $dropdown_presenter_builder = new HelpDropdownPresenterBuilder($this->event_manager);
-        $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user, $this->version->version_number);
+        $dropdown_presenter_builder = new HelpDropdownPresenterBuilder(
+            new ReleaseNoteManager(new ReleaseLinkDao(), $this->version->version_number),
+            $this->event_manager,
+            new URISanitizer(new Valid_HTTPURI())
+        );
+        $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user);
 
         $new_dropdown_presenter_builder = new NewDropdownPresenterBuilder(
             $this->event_manager,

src/www/themes/FlamingParrot/FlamingParrot_Theme.class.php
@@ -24,6 +24,8 @@ use Tuleap\Dashboard\User\UserDashboardDao;
 use Tuleap\Dashboard\User\UserDashboardRetriever;
 use Tuleap\Dashboard\Widget\DashboardWidgetDao;
 use Tuleap\HelpDropdown\HelpDropdownPresenterBuilder;
+use Tuleap\HelpDropdown\ReleaseLinkDao;
+use Tuleap\HelpDropdown\ReleaseNoteManager;
 use Tuleap\Layout\BreadCrumbDropdown\BreadCrumbPresenterBuilder;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\layout\NewDropdown\NewDropdownPresenterBuilder;
@@ -32,6 +34,7 @@ use Tuleap\Project\Banner\BannerDisplay;
 use Tuleap\Project\Flags\ProjectFlagsBuilder;
 use Tuleap\Project\Flags\ProjectFlagsDao;
 use Tuleap\Project\Registration\ProjectRegistrationUserPermissionChecker;
+use Tuleap\Sanitizer\URISanitizer;
 use Tuleap\Widget\WidgetFactory;
 
 require_once __DIR__ . '/../../../themes/FlamingParrot/vendor/autoload.php';
@@ -184,8 +187,12 @@ class FlamingParrot_Theme extends Layout // phpcs:ignore PSR1.Classes.ClassDecla
         $body_class[] = $has_sidebar;
         $body_class[] = $sidebar_state;
 
-        $dropdown_presenter_builder = new HelpDropdownPresenterBuilder($this->getEventManager());
-        $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user, $this->tuleap_version->version_number);
+        $dropdown_presenter_builder = new HelpDropdownPresenterBuilder(
+            new ReleaseNoteManager(new ReleaseLinkDao(), $this->tuleap_version->version_number),
+            $this->getEventManager(),
+            new URISanitizer(new Valid_HTTPURI()),
+        );
+        $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user);
 
         $this->render('body', new FlamingParrot_BodyPresenter(
             $current_user,

src/common/HelpDropdown/HelpDropdownPresenterBuilder.php
@@ -23,6 +23,7 @@ namespace Tuleap\HelpDropdown;
 
 use PFUser;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Tuleap\Sanitizer\URISanitizer;
 
 class HelpDropdownPresenterBuilder
 {
@@ -30,36 +31,51 @@ class HelpDropdownPresenterBuilder
      * @var EventDispatcherInterface
      */
     private $event_dispatcher;
+    /**
+     * @var ReleaseNoteManager
+     */
+    private $release_note_manager;
+    /**
+     * @var URISanitizer
+     */
+    private $uri_sanitizer;
 
-    public function __construct(EventDispatcherInterface $event_dispatcher)
-    {
-        $this->event_dispatcher = $event_dispatcher;
+    public function __construct(
+        ReleaseNoteManager $release_note_manager,
+        EventDispatcherInterface $event_dispatcher,
+        URISanitizer $uri_sanitizer
+    ) {
+        $this->event_dispatcher     = $event_dispatcher;
+        $this->uri_sanitizer        = $uri_sanitizer;
+        $this->release_note_manager = $release_note_manager;
     }
 
-    public function build(PFUser $current_user, string $tuleap_version): HelpDropdownPresenter
+    public function build(PFUser $current_user): HelpDropdownPresenter
     {
         $documentation = "/doc/" . urlencode($current_user->getShortLocale()) . "/";
 
         $main_items = [
-            new HelpLinkPresenter(
+            HelpLinkPresenter::build(
                 dgettext(
                     'tuleap-core',
                     'Get help'
                 ),
                 "/help/",
-                "fa-life-saver"
+                "fa-life-saver",
+                $this->uri_sanitizer
             ),
-            new HelpLinkPresenter(
+            HelpLinkPresenter::build(
                 dgettext(
                     'tuleap-core',
                     'Documentation'
                 ),
                 $documentation,
-                "fa-book"
+                "fa-book",
+                $this->uri_sanitizer
             )
         ];
 
-        $release_note = $this->getReleaseNoteLink($current_user, $tuleap_version);
+        $release_note = $this->getReleaseNoteLink($current_user);
 
         $explorer_endpoint_event = $this->event_dispatcher->dispatch(new \Tuleap\REST\ExplorerEndpointAvailableEvent());
 
@@ -70,25 +86,20 @@ class HelpDropdownPresenterBuilder
         );
     }
 
-    private function getReleaseNoteLink(PFUser $current_user, string $tuleap_version): ?HelpLinkPresenter
+    private function getReleaseNoteLink(PFUser $current_user): ?HelpLinkPresenter
     {
+        $release_note_link = $this->release_note_manager->getReleaseNoteLink();
         if ($current_user->useLabFeatures()) {
-            return new HelpLinkPresenter(
+            return HelpLinkPresenter::build(
                 dgettext(
                     'tuleap-core',
                     'Release Note'
                 ),
-                $this->getActualReleaseLink($tuleap_version),
-                "fa-star"
+                $release_note_link,
+                "fa-star",
+                $this->uri_sanitizer
             );
         }
         return null;
-    }
-
-    private function getActualReleaseLink(string $tuleap_version): string
-    {
-        $version_number = str_replace(".", "-", substr($tuleap_version, 0, 5));
-
-        return 'https://www.tuleap.org/ressources/release-notes/tuleap-' . urlencode($version_number);
     }
 }

src/common/HelpDropdown/ReleaseLinkDao.php
@@ -21,36 +21,31 @@ declare(strict_types=1);
 
 namespace Tuleap\HelpDropdown;
 
-use Tuleap\Sanitizer\URISanitizer;
+use Tuleap\DB\DataAccessObject;
 
-/**
- * @psalm-immutable
- */
-class HelpLinkPresenter
+class ReleaseLinkDao extends DataAccessObject
 {
-    /**
-     * @var string
-     */
-    public $title;
-
-    /**
-     * @var string
-     */
-    public $link;
-    /**
-     * @var string
-     */
-    public $icon;
-
-    private function __construct(string $title, string $link, string $icon)
+    public function getReleaseLink(): ?array
     {
-        $this->title = $title;
-        $this->link  = $link;
-        $this->icon  = $icon;
+        $sql = "SELECT * FROM release_note_link LIMIT 1";
+
+        return $this->getDB()->row($sql);
     }
 
-    public static function build(string $title, string $link, string $icon, URISanitizer $uri_sanitizer): self
+    public function updateTuleapVersion(string $tuleap_version): void
     {
-        return new self($title, $uri_sanitizer->sanitizeForHTMLAttribute($link), $icon);
+        $sql = "UPDATE release_note_link SET tuleap_version = ?";
+
+        $this->getDB()->run($sql, $tuleap_version);
+    }
+
+    public function createReleaseNoteLink(string $tuleap_version): void
+    {
+        $this->getDB()->insert(
+            "release_note_link",
+            [
+                "tuleap_version" => $tuleap_version
+            ]
+        );
     }
 }

src/common/HelpDropdown/ReleaseNoteManager.php
@@ -1,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Tuleap\HelpDropdown;
+
+class ReleaseNoteManager
+{
+    /**
+     * @var ReleaseLinkDao
+     */
+    private $release_note_dao;
+    /**
+     * @var string
+     */
+    private $tuleap_version;
+
+    public function __construct(ReleaseLinkDao $release_note_dao, string $tuleap_version)
+    {
+        $this->release_note_dao = $release_note_dao;
+        $this->tuleap_version   = str_replace(".", "-", substr($tuleap_version, 0, 5));
+    }
+
+    public function getReleaseNoteLink(): string
+    {
+        $link = $this->release_note_dao->getReleaseLink();
+
+        $actual_version_link = 'https://www.tuleap.org/ressources/release-notes/tuleap-' . urlencode($this->tuleap_version);
+
+        if ($link === null) {
+            $this->release_note_dao->createReleaseNoteLink($this->tuleap_version);
+            return $actual_version_link;
+        }
+
+        if ($link["tuleap_version"] !== $this->tuleap_version) {
+            $this->release_note_dao->updateTuleapVersion($this->tuleap_version);
+            return $actual_version_link;
+        }
+
+        if ($link["actual_link"] === "" || $link["actual_link"] === null) {
+            return $actual_version_link;
+        }
+
+        return $link["actual_link"];
+    }
+}

src/db/mysql/database_structure.sql
@@ -1683,6 +1683,13 @@ CREATE TABLE project_banner (
     project_id INT(11) NOT NULL PRIMARY KEY,
     message text
 );
+
+DROP TABLE IF EXISTS release_note_link;
+CREATE TABLE release_note_link (
+    enforce_one_row_table ENUM('SHOULD_HAVE_AT_MOST_ONE_ROW') NOT NULL PRIMARY KEY DEFAULT 'SHOULD_HAVE_AT_MOST_ONE_ROW',
+    actual_link TEXT,
+    tuleap_version TEXT NOT NULL
+);
 #
 # EOF
 #

src/db/mysql/updates/2020/202008171622_create_release_note_link_table.php
@@ -1,0 +1,46 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+declare(strict_types=1);
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202008171622_create_release_note_link_table extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Add release_note_link table';
+    }
+
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up(): void
+    {
+        $sql = "CREATE TABLE release_note_link (
+                    enforce_one_row_table ENUM('SHOULD_HAVE_AT_MOST_ONE_ROW') NOT NULL PRIMARY KEY DEFAULT 'SHOULD_HAVE_AT_MOST_ONE_ROW',
+                    actual_link TEXT,
+                    tuleap_version TEXT NOT NULL
+        )";
+
+        $this->db->createTable('release_note_link', $sql);
+    }
+}

tests/unit/common/HelpDropdown/HelpDropdownPresenterBuilderTest.php
@@ -26,6 +26,7 @@ use PFUser;
 use PHPUnit\Framework\TestCase;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Tuleap\REST\ExplorerEndpointAvailableEvent;
+use Tuleap\Sanitizer\URISanitizer;
 
 class HelpDropdownPresenterBuilderTest extends TestCase
 {
@@ -40,6 +41,14 @@ class HelpDropdownPresenterBuilderTest extends TestCase
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PFUser
      */
     private $user;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|ReleaseNoteManager
+     */
+    private $release_note_manager;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|URISanitizer
+     */
+    private $uri_sanitizer;
 
     protected function setUp(): void
     {
@@ -51,35 +60,64 @@ class HelpDropdownPresenterBuilderTest extends TestCase
             new ExplorerEndpointAvailableEvent()
         );
 
-        $this->help_dropdown_builder = new HelpDropdownPresenterBuilder($event_dispatcher);
+        $this->release_note_manager = Mockery::mock(ReleaseNoteManager::class);
+        $this->uri_sanitizer        = Mockery::mock(URISanitizer::class);
+
+        $this->uri_sanitizer
+            ->shouldReceive('sanitizeForHTMLAttribute')
+            ->withArgs(["/help/"])
+            ->andReturn("/help/");
+
+        $this->uri_sanitizer
+            ->shouldReceive('sanitizeForHTMLAttribute')
+            ->withArgs(["/doc/en/"])
+            ->andReturn("/doc/en/");
+
+        $this->help_dropdown_builder = new HelpDropdownPresenterBuilder(
+            $this->release_note_manager,
+            $event_dispatcher,
+            $this->uri_sanitizer
+        );
     }
 
     public function testBuildPresenterWithLabMod(): void
     {
         $this->user->shouldReceive('useLabFeatures')->andReturn(true);
 
+        $this->uri_sanitizer
+            ->shouldReceive('sanitizeForHTMLAttribute')
+            ->withArgs(["https://www.tuleap.org/ressources/release-notes/tuleap-11-17"])
+            ->andReturn("https://www.tuleap.org/ressources/release-notes/tuleap-11-17");
+
         $expected_result = new HelpDropdownPresenter(
             [
-                new HelpLinkPresenter(
+                HelpLinkPresenter::build(
                     'Get help',
                     "/help/",
-                    "fa-life-saver"
+                    "fa-life-saver",
+                    $this->uri_sanitizer
                 ),
-                new HelpLinkPresenter(
+                HelpLinkPresenter::build(
                     'Documentation',
                     "/doc/en/",
-                    "fa-book"
+                    "fa-book",
+                    $this->uri_sanitizer
                 )
             ],
             null,
-            new HelpLinkPresenter(
+            HelpLinkPresenter::build(
                 'Release Note',
                 'https://www.tuleap.org/ressources/release-notes/tuleap-11-17',
-                "fa-star"
+                "fa-star",
+                $this->uri_sanitizer
             )
         );
 
-        $this->assertEquals($expected_result, $this->help_dropdown_builder->build($this->user, "11.17.99.234"));
+        $this->release_note_manager
+            ->shouldReceive('getReleaseNoteLink')
+            ->andReturn("https://www.tuleap.org/ressources/release-notes/tuleap-11-17");
+
+        $this->assertEquals($expected_result, $this->help_dropdown_builder->build($this->user));
     }
 
     public function testBuildPresenterWithoutLabMod(): void
@@ -88,21 +126,24 @@ class HelpDropdownPresenterBuilderTest extends TestCase
 
         $expected_result = new HelpDropdownPresenter(
             [
-                new HelpLinkPresenter(
+                HelpLinkPresenter::build(
                     'Get help',
                     "/help/",
-                    "fa-life-saver"
+                    "fa-life-saver",
+                    $this->uri_sanitizer
                 ),
-                new HelpLinkPresenter(
+                HelpLinkPresenter::build(
                     'Documentation',
                     "/doc/en/",
-                    "fa-book"
+                    "fa-book",
+                    $this->uri_sanitizer
                 )
             ],
             null,
             null
         );
+        $this->release_note_manager->shouldReceive('getReleaseNoteLink');
 
-        $this->assertEquals($expected_result, $this->help_dropdown_builder->build($this->user, "11.17.99.234"));
+        $this->assertEquals($expected_result, $this->help_dropdown_builder->build($this->user));
     }
 }

commit a2ef6ba8879d46461e62c0e79f049ac59a31a06e

Help and documentation links are broken
Issue introduced by a7826dee5d3c13d7d0f4d5f1219182d49b4ba8a5.

Part of story #15102: highlight informations to the end user

Change-Id: I48c9acfe9b2f9573a7d9602b5913b075fa80adf5

src/themes/BurningParrot/include/BurningParrotTheme.php
@@ -43,6 +43,7 @@ use Tuleap\Theme\BurningParrot\Navbar\PresenterBuilder as NavbarPresenterBuilder
 use URLRedirect;
 use UserManager;
 use Valid_HTTPURI;
+use Valid_LocalURI;
 use Widget_Static;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -131,7 +132,7 @@ class BurningParrotTheme extends BaseLayout
         $dropdown_presenter_builder = new HelpDropdownPresenterBuilder(
             new ReleaseNoteManager(new ReleaseLinkDao(), $this->version->version_number),
             $this->event_manager,
-            new URISanitizer(new Valid_HTTPURI())
+            new URISanitizer(new Valid_HTTPURI(), new Valid_LocalURI())
         );
         $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user);
 

src/www/themes/FlamingParrot/FlamingParrot_Theme.class.php
@@ -190,7 +190,7 @@ class FlamingParrot_Theme extends Layout // phpcs:ignore PSR1.Classes.ClassDecla
         $dropdown_presenter_builder = new HelpDropdownPresenterBuilder(
             new ReleaseNoteManager(new ReleaseLinkDao(), $this->tuleap_version->version_number),
             $this->getEventManager(),
-            new URISanitizer(new Valid_HTTPURI()),
+            new URISanitizer(new Valid_HTTPURI(), new Valid_LocalURI()),
         );
         $help_dropdown_presenter    = $dropdown_presenter_builder->build($current_user);
 