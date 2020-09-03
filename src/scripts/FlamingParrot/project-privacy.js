165c16c056b4148d1419ad2fd9ded50cd5cae34b
Benjamin Dauton <benjamin.dauton@enalean.com>
2020-09-03 10:32:57 +0200

    Pimp project privacy/flag popover part 4
    Part of story #16210 Remove navigation bar
    
    Covered by this commit:
    - FlamigParrot - Sidebar - Privacy
    A popover displays the privacy title and description
    
    - FlamingParrot - Breadcrumb - Privacy+Flag
    A popover displays the privacy in the same way the flags
    are displayed
    
    Change-Id: Ib4582449fccf1fda7493f417467ae9fa1d632979

--- src/scripts/FlamingParrot/project-privacy.js
+++ src/scripts/FlamingParrot/project-privacy.js
@@ -21,10 +21,19 @@ import $ from "jquery";
 import { render } from "mustache";
 
 document.addEventListener("DOMContentLoaded", () => {
+    handleBreadcrumbPrivacyPopover();
+    handleSidebarPrivacyPopover();
+});
+
+function handleBreadcrumbPrivacyPopover() {
     const project_privacy_element = document.getElementById("breadcrumb-project-privacy-icon");
     if (!project_privacy_element) {
         return;
     }
+
+    const svg = project_privacy_element.querySelector(".tuleap-svg-project-shield");
+    const shield_icon_html = svg ? new XMLSerializer().serializeToString(svg) : "";
+    const project_flags = JSON.parse(project_privacy_element.dataset.projectFlags);
 
     const template = render(
         `<div class="popover current-project-nav-popover current-project-nav-popover-privacy">
@@ -32,35 +41,46 @@ document.addEventListener("DOMContentLoaded", () => {
                 <h3>{{ title }}</h3>
                 <div class="popover-content"></div>
         </div>`,
-        { title: project_privacy_element.title }
+        {
+            title:
+                project_flags.length > 0
+                    ? project_privacy_element.dataset.projectName
+                    : project_privacy_element.dataset.privacyTitle,
+        }
     );
 
-    const svg = project_privacy_element.querySelector(".tuleap-svg-project-shield");
-    const shield_icon_html = svg ? new XMLSerializer().serializeToString(svg) : "";
-
-    const project_flags = JSON.parse(project_privacy_element.dataset.projectFlags);
-
     const content = render(
-        `<p class="current-project-nav-flag-popover-content-description">{{ content }}</p>
-            {{# has_project_flags }}
-                <hr class="current-project-nav-flag-popover-separator">
-                {{# project_flags }}
-                    <div class="current-project-nav-flag-popover-flag">
-                        ${shield_icon_html}
-                        <h2 class="current-project-nav-flag-popover-content-title">
-                            {{ label }}
-                        </h2>
-                        {{# description }}
-                            <p class="current-project-nav-flag-popover-content-description">
-                                {{ description }}
-                            </p>
-                        {{/ description }}
-                    </div>
-                {{/ project_flags }}
-            {{/ has_project_flags }}
-            `,
+        `{{# has_project_flags }}
+            <div class="current-project-nav-flag-popover-flag">
+                <i class="fa {{ privacy_icon}}"></i>
+                <h2 class="current-project-nav-flag-popover-content-title">
+                    {{ privacy_title }}
+                </h2>
+                <p class="current-project-nav-flag-popover-content-description">{{ privacy_description }}</p>
+            </div>
+            <hr class="current-project-nav-flag-popover-separator">
+            {{# project_flags }}
+                <div class="current-project-nav-flag-popover-flag">
+                    ${shield_icon_html}
+                    <h2 class="current-project-nav-flag-popover-content-title">
+                        {{ label }}
+                    </h2>
+                    {{# description }}
+                        <p class="current-project-nav-flag-popover-content-description">
+                            {{ description }}
+                        </p>
+                    {{/ description }}
+                </div>
+            {{/ project_flags }}
+        {{/ has_project_flags }}
+        {{^ has_project_flags }}
+            <p class="current-project-nav-flag-popover-content-description">{{ privacy_description }}</p>
+        {{/ has_project_flags }}
+        `,
         {
-            content: project_privacy_element.dataset.content,
+            privacy_icon: project_privacy_element.dataset.privacyIcon,
+            privacy_title: project_privacy_element.dataset.privacyTitle,
+            privacy_description: project_privacy_element.dataset.privacyDescription,
             project_flags,
             has_project_flags: project_flags.length > 0,
         }
@@ -73,4 +93,36 @@ document.addEventListener("DOMContentLoaded", () => {
         template,
         content,
     });
-});
+}
+
+function handleSidebarPrivacyPopover() {
+    const project_privacy_trigger = document.querySelector("#sidebar-dashboard > .fa");
+    if (!project_privacy_trigger) {
+        return;
+    }
+
+    const template = render(
+        `<div class="popover current-project-nav-popover current-project-sidebar-popover-privacy">
+            <div class="arrow"></div>
+            <h3>{{ title }}</h3>
+            <div class="popover-content"></div>
+        </div>`,
+        { title: project_privacy_trigger.dataset.title }
+    );
+
+    const content = render(
+        `<p class="privacy-description">
+            {{ content }}
+        </p>`,
+        { content: project_privacy_trigger.dataset.content }
+    );
+
+    $(project_privacy_trigger).popover({
+        placement: "right",
+        trigger: "hover",
+        container: "body",
+        html: true,
+        template,
+        content,
+    });
+}
