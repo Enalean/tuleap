4c6f3d05d49fa456a1d8d0b82a20cdc2b3715a95
Joris Masson <joris.masson@enalean.com>
2020-09-10 16:28:30 +0200

    request #16684 Have a cypress test for implicit backlog planning view
    No functional change.
    
    make tests_cypress with the agiledashboard plugin should be successful.
    
    Change-Id: I8190eb68689c7c92377ced4a7ed31f3c2832aa7b

---
+++ plugins/agiledashboard/tests/e2e/cypress/cypress/integration/implicit_backlog.spec.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe(`Planning view Implicit Backlog`, function () {
+    before(function () {
+        cy.clearCookie("__Host-TULEAP_session_hash");
+
+        cy.projectMemberLogin();
+        cy.visitProjectService("implicit-backlog", "Agile Dashboard");
+    });
+
+    it(`Project Member can browse the top backlog`, function () {
+        cy.get("[data-test=go-to-top-backlog]").click();
+        cy.get("[data-test=backlog]").within(() => {
+            cy.contains("[data-test=backlog-item]", "Crossbow Everyday");
+            cy.contains("[data-test=backlog-item]", "Restless Weeknight");
+            cy.contains("[data-test=backlog-item]", "Raw Wrench");
+            cy.contains("[data-test=backlog-item]", "Forgotten Trombone");
+        });
+
+        cy.contains("[data-test=milestone]", "Summer Swift");
+
+        // load closed milestones
+        cy.get("[data-test=load-closed-milestones-button]").click();
+        cy.contains("[data-test=milestone]", "New Alpha");
+    });
+});
