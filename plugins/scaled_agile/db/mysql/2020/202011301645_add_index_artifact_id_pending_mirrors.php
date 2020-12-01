8bf32d91671fcf022b065ecd4da90501b75f5829
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-12-01 09:55:48 +0100

    Add missing index on the plugin_scaled_agile_pending_mirrors table for existing instance
    This change was not commited in 08f883f44e2e9fbf7d2fc05fa615f80929b4c56b 😢.
    
    Part of story #17879: add Teams to Programs
    
    Change-Id: Ib60dad6ae58c90f77a9a4d7bbd123eb31c0e149d

---
+++ plugins/scaled_agile/db/mysql/2020/202011301645_add_index_artifact_id_pending_mirrors.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2020-Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ *  Tuleap is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tuleap is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202011301645_add_index_artifact_id_pending_mirrors extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Add index on program_artifact_id column on the plugin_scaled_agile_pending_mirrors table';
+    }
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+    public function up(): void
+    {
+        $this->db->addIndex(
+            'plugin_scaled_agile_pending_mirrors',
+            'idx_program_artifact_id',
+            'ALTER TABLE plugin_scaled_agile_pending_mirrors ADD INDEX idx_program_artifact_id(program_artifact_id)'
+        );
+    }
+}
