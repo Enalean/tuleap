7169a65a32547ac88ba1671869520cc0719a1fd2
Thomas Gerbet <thomas.gerbet@enalean.com>
2020-12-28 11:52:47 +0100

    Display parents from another project on PV2 cards with their project background color
    The color is only shown when:
     * the card has a parent from a project different than the current
       project thanks to a hierarchy defined through the scaled agile plugin
     * the project of the parent artifact has defined a background
     * the card does not have a background color
    
    The background color of the element is derived from the proeminent color
    of the background image. The way to obtain it is temporary until we have
    a clue on how the design team expected us to obtain it.
    
    Part of story #18405: distinguish Features and P.I. that comes from a Program in a Team
    
    Change-Id: I38bdbe9029ae773a8ebb3bf80812fb9a8a098c52

--- plugins/agiledashboard/scripts/planning-v2/src/app/backlog-item/card-parent-link/card-parent-link-directive.js
+++ plugins/agiledashboard/scripts/planning-v2/src/app/backlog-item/card-parent-link/card-parent-link-directive.js
@@ -8,9 +8,18 @@ export default () => {
             url: "@",
             parent_project_label: "@parentProjectLabel",
             parent_project_id: "@parentProjectId",
+            parent_project_color_name: "@parentProjectColorName",
             project_id: "@projectId",
             filter_terms: "@filterTerms",
         },
         templateUrl: "card-parent-link.tpl.html",
+        link(scope) {
+            scope.getColorClass = function (color_name, parent_project_id, project_id) {
+                if (!color_name || project_id === parent_project_id) {
+                    return {};
+                }
+                return { [`project-color-${color_name}`]: true };
+            };
+        },
     };
 };
