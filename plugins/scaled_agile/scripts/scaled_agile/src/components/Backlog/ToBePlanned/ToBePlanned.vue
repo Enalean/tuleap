Enalean/tuleap

commit 1eb792e2d975199c6408fe9154fea484f668b359

display planable items as card
part of story #18408 plan Features in Program Increment

This is a first step:
 - depending on your configuration, if you have plannable items, they are displayed in the `to be planned` column
 - loader and error message are handled

Missing:
 - tracker color on artifact_id/mrgin left of card
 - tracker card background color (and accessibility pattern)

Change-Id: I61bc13fb9c3d954ce2fffd611e4f585dd0d0beec

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlanned.vue
@@ -20,17 +20,64 @@
 <template>
     <div>
         <h2 v-translate class="program-to-be-planned-title">To Be Planned</h2>
-        <empty-state />
+        <empty-state
+            v-if="to_be_planned_elements.length === 0 && !is_loading && !has_error"
+            data-test="empty-state"
+        />
+        <to-be-planned-card
+            v-for="element in to_be_planned_elements"
+            v-bind:key="element.artifact_id"
+            v-bind:element="element"
+            data-test="to-be-planned-elements"
+        />
+
+        <div
+            id="to-be-planned-backlog-error"
+            class="tlp-alert-danger"
+            v-if="has_error"
+            data-test="to-be-planned-error"
+        >
+            {{ error_message }}
+        </div>
+
+        <to-be-planned-skeleton v-if="is_loading" data-test="to-be-planned-skeleton" />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import { programId } from "../../../configuration";
+import {
+    getToBePlannedElements,
+    ToBePlannedElement,
+} from "../../../helpers/ToBePlanned/element-to-plan-retriver";
 import EmptyState from "./EmptyState.vue";
+import ToBePlannedCard from "./ToBePlannedCard.vue";
+import ToBePlannedSkeleton from "./ToBePlannedSkeleton.vue";
 
 @Component({
-    components: { EmptyState },
+    components: { ToBePlannedSkeleton, ToBePlannedCard, EmptyState },
 })
-export default class ToBePlanned extends Vue {}
+export default class ToBePlanned extends Vue {
+    private error_message = "";
+    private has_error = false;
+    private to_be_planned_elements: Array<ToBePlannedElement> = [];
+    private is_loading = false;
+
+    async mounted(): Promise<void> {
+        try {
+            this.is_loading = true;
+            this.to_be_planned_elements = await getToBePlannedElements(programId());
+        } catch (e) {
+            this.has_error = true;
+            this.error_message = this.$gettext(
+                "The retrieval of the elements to be planned in program has failed"
+            );
+            throw e;
+        } finally {
+            this.is_loading = false;
+        }
+    }
+}
 </script>

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlannedCard.vue
@@ -1,0 +1,49 @@
+<!---
+  - Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="element-card">
+        <div class="element-card-content">
+            <div class="element-card-xref-label">
+                <a
+                    v-bind:href="`/plugins/tracker/?aid=${element.artifact_id}`"
+                    class="element-card-xref"
+                >
+                    {{ element.tracker_name }} #{{ element.artifact_id }}
+                </a>
+                <span class="element-card-label">{{ element.artifact_title }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import EmptyState from "./EmptyState.vue";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+
+@Component({
+    components: { EmptyState },
+})
+export default class ToBePlannedCard extends Vue {
+    @Prop({ required: true })
+    readonly element!: ToBePlannedElement;
+}
+</script>

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlanned.test.ts
@@ -1,0 +1,98 @@
+/**
+ * Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import ToBePlanned from "./ToBePlanned.vue";
+import * as retriever from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import * as configuration from "../../../configuration";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import { createScaledAgileLocalVue } from "../../../helpers/local-vue-for-test";
+import { DefaultData } from "vue/types/options";
+
+describe("ToBePlanned", () => {
+    it("Displays the empty state when no artifact are found", async () => {
+        jest.spyOn(retriever, "getToBePlannedElements").mockResolvedValue([]);
+        jest.spyOn(configuration, "programId").mockImplementation(() => 202);
+
+        const wrapper = shallowMount(ToBePlanned, {
+            localVue: await createScaledAgileLocalVue(),
+            data(): DefaultData<ToBePlanned> {
+                return {
+                    to_be_planned_elements: [],
+                    is_loading: false,
+                    has_error: false,
+                };
+            },
+        });
+
+        expect(wrapper.find("[data-test=empty-state]").exists()).toBe(true);
+    });
+
+    it("Displays an error when rest route fail", async () => {
+        jest.spyOn(retriever, "getToBePlannedElements").mockResolvedValue([]);
+        jest.spyOn(configuration, "programId").mockImplementation(() => 202);
+        const wrapper = shallowMount(ToBePlanned, {
+            localVue: await createScaledAgileLocalVue(),
+            data(): DefaultData<ToBePlanned> {
+                return {
+                    to_be_planned_elements: [],
+                    is_loading: false,
+                    has_error: true,
+                    error_message: "Oups, something happened",
+                };
+            },
+        });
+
+        expect(wrapper.find("[data-test=to-be-planned-error]").exists()).toBe(true);
+    });
+
+    it("Displays the elements to be planned", async () => {
+        const element_one = {
+            artifact_id: 1,
+            artifact_title: "My artifact",
+            tracker_name: "bug",
+        } as ToBePlannedElement;
+        const element_two = {
+            artifact_id: 2,
+            artifact_title: "My user story",
+            tracker_name: "user_stories",
+        } as ToBePlannedElement;
+
+        jest.spyOn(retriever, "getToBePlannedElements").mockResolvedValue([
+            element_one,
+            element_two,
+        ]);
+        jest.spyOn(configuration, "programId").mockImplementation(() => 202);
+
+        const wrapper = shallowMount(ToBePlanned, {
+            localVue: await createScaledAgileLocalVue(),
+            data(): DefaultData<ToBePlanned> {
+                return {
+                    to_be_planned_elements: [element_one, element_two],
+                    is_loading: false,
+                    has_error: true,
+                    error_message: "Oups, something happened",
+                };
+            },
+        });
+
+        expect(wrapper.find("[data-test=to-be-planned-skeleton]").exists()).toBe(false);
+        expect(wrapper.find("[data-test=to-be-planned-elements]").exists()).toBe(true);
+    });
+});

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlannedCard.test.ts
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { shallowMount, ShallowMountOptions } from "@vue/test-utils";
+import ToBePlannedCard from "./ToBePlannedCard.vue";
+import { createScaledAgileLocalVue } from "../../../helpers/local-vue-for-test";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+
+describe("ToBePlannedCard", () => {
+    let component_options: ShallowMountOptions<ToBePlannedCard>;
+
+    it("Displays the empty state for Backlog unplanned section", async () => {
+        component_options = {
+            propsData: {
+                element: {
+                    artifact_id: 100,
+                    artifact_title: "My artifact",
+                    tracker_name: "bug",
+                } as ToBePlannedElement,
+            },
+            localVue: await createScaledAgileLocalVue(),
+        };
+
+        const wrapper = shallowMount(ToBePlannedCard, component_options);
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});

plugins/scaled_agile/include/ProgramBacklogPresenter.php
@@ -46,6 +46,10 @@ final class ProgramBacklogPresenter
      * @var string
      */
     public $project_flags;
+    /**
+     * @var int|null
+     */
+    public $program_id;
 
     /**
      * @param ProjectFlagPresenter[] $project_flags
@@ -56,5 +60,6 @@ final class ProgramBacklogPresenter
         $this->project_short_name = $project->getUnixNameLowerCase();
         $this->project_privacy    = json_encode(ProjectPrivacyPresenter::fromProject($project), JSON_THROW_ON_ERROR);
         $this->project_flags      = json_encode($project_flags, JSON_THROW_ON_ERROR);
+        $this->program_id         = (int) $project->getID();
     }
 }

plugins/scaled_agile/scripts/scaled_agile/index.ts
@@ -51,7 +51,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
     const project_flags = JSON.parse(vue_mount_point.dataset.projectFlags);
 
-    build(project_name, project_short_name, project_privacy, project_flags);
+    if (!vue_mount_point.dataset.programId) {
+        throw new Error("Missing program_id dataset");
+    }
+    const program_id = parseInt(vue_mount_point.dataset.programId, 10);
+
+    build(project_name, project_short_name, project_privacy, project_flags, program_id);
 
     await initVueGettext(
         Vue,

plugins/scaled_agile/scripts/scaled_agile/po/fr_FR.po
@@ -21,6 +21,9 @@ msgstr "Programme Incrément"
 msgid "Scaled Agile"
 msgstr "Agilité à l'échelle"
 
+msgid "The retrieval of the elements to be planned in program has failed"
+msgstr "La récupération des éléments à planifier dans le programme a échoué"
+
 msgid "There are no iterations yet"
 msgstr "Il n'y a pas d'itération pour l'instant"
 

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlannedSkeleton.vue
@@ -1,0 +1,25 @@
+<!---
+  - Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template functional>
+    <div class="tlp-card tlp-skeleton-card" id="element-skeleton">
+        <span class="tlp-skeleton-text element-skeleton-xref"></span>
+        <span class="tlp-skeleton-text element-skeleton-title"></span>
+    </div>
+</template>

commit 5d9870b137f9b74036084ed1d6082bf6b746e77c

Fix spelling mistake
Introduced in 1eb792e2d975199c6408fe9154fea484f668b359.

Part of story #18408: plan Features in Program Increment

Change-Id: If49ff7d6c49c5c29d8c073552dd88b23b882f612

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlanned.vue
@@ -51,7 +51,7 @@ import { programId } from "../../../configuration";
 import {
     getToBePlannedElements,
     ToBePlannedElement,
-} from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+} from "../../../helpers/ToBePlanned/element-to-plan-retriever";
 import EmptyState from "./EmptyState.vue";
 import ToBePlannedCard from "./ToBePlannedCard.vue";
 import ToBePlannedSkeleton from "./ToBePlannedSkeleton.vue";

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlannedCard.vue
@@ -37,7 +37,7 @@
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import EmptyState from "./EmptyState.vue";
-import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriever";
 
 @Component({
     components: { EmptyState },

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlanned.test.ts
@@ -19,9 +19,9 @@
 
 import { shallowMount } from "@vue/test-utils";
 import ToBePlanned from "./ToBePlanned.vue";
-import * as retriever from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import * as retriever from "../../../helpers/ToBePlanned/element-to-plan-retriever";
 import * as configuration from "../../../configuration";
-import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriever";
 import { createScaledAgileLocalVue } from "../../../helpers/local-vue-for-test";
 import { DefaultData } from "vue/types/options";
 

plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ToBePlanned/ToBePlannedCard.test.ts
@@ -20,7 +20,7 @@
 import { shallowMount, ShallowMountOptions } from "@vue/test-utils";
 import ToBePlannedCard from "./ToBePlannedCard.vue";
 import { createScaledAgileLocalVue } from "../../../helpers/local-vue-for-test";
-import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriver";
+import { ToBePlannedElement } from "../../../helpers/ToBePlanned/element-to-plan-retriever";
 
 describe("ToBePlannedCard", () => {
     let component_options: ShallowMountOptions<ToBePlannedCard>;

plugins/scaled_agile/scripts/scaled_agile/src/helpers/ToBePlanned/element-to-plan-retriever.test.ts
@@ -18,7 +18,7 @@
  */
 
 import * as tlp from "tlp";
-import { getToBePlannedElements, ToBePlannedElement } from "./element-to-plan-retriver";
+import { getToBePlannedElements, ToBePlannedElement } from "./element-to-plan-retriever";
 
 jest.mock("tlp");
 