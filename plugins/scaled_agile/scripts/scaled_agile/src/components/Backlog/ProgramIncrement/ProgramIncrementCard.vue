cbb86293b8033234ddbec565b660222eef6e4a83
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2021-01-22 15:45:41 +0100

    Program increment can be toggle
    part of story #18408: plan Features in Program Increment
    
    Uou can be toggle the content of PI
    
    Change-Id: I280c274eb0addad067a084e19d80ae7bc3f8257a

--- plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ProgramIncrement/ProgramIncrementCard.vue
+++ plugins/scaled_agile/scripts/scaled_agile/src/components/Backlog/ProgramIncrement/ProgramIncrementCard.vue
@@ -19,8 +19,19 @@
 
 <template>
     <div class="tlp-pane-container program-increment-display">
-        <div class="tlp-pane-header program-increment-header">
-            <span class="tlp-pane-title program-increment-header-label">{{ increment.title }}</span>
+        <div
+            class="tlp-pane-header program-increment-header"
+            v-on:click="toggleIsOpen"
+            data-test="program-increment-toggle"
+        >
+            <span class="tlp-pane-title program-increment-header-label">
+                <i
+                    class="tlp-pane-title-icon fa fa-fw"
+                    v-bind:class="[is_open ? 'fa-caret-down' : 'fa-caret-right']"
+                    data-test="program-increment-toggle-icon"
+                />
+                {{ increment.title }}
+            </span>
             <div class="program-increment-header-spacer"></div>
             <span class="program-increment-header-dates" v-if="increment.start_date !== null">
                 {{ formatDate(increment.start_date) }}
@@ -29,7 +40,11 @@
             </span>
             <span class="tlp-badge-outline tlp-badge-primary">{{ increment.status }}</span>
         </div>
-        <div class="program-increment-info">
+        <div
+            class="program-increment-info"
+            v-bind:class="{ 'program-increment-info-hidden': !is_open }"
+            data-test="program-increment-info"
+        >
             <a
                 v-bind:href="`/plugins/tracker/?aid=${increment.id}`"
                 class="tlp-button-primary tlp-button-outline tlp-button-mini"
@@ -39,7 +54,11 @@
                 <span v-translate>Edit</span>
             </a>
         </div>
-        <section class="tlp-pane-section program-increment-content">
+        <section
+            class="tlp-pane-section program-increment-content"
+            v-bind:class="{ 'program-increment-content-hidden': !is_open }"
+            data-test="program-increment-content"
+        >
             <div class="program-increment-content-items">
                 <program-increment-no-content />
             </div>
@@ -61,6 +80,12 @@ export default class ProgramIncrement extends Vue {
     @Prop({ required: true })
     readonly increment!: ProgramIncrement;
 
+    is_open = false;
+
     formatDate = (date: string): string => formatDateYearMonthDay(getUserLocale(), date);
+
+    toggleIsOpen(): void {
+        this.is_open = !this.is_open;
+    }
 }
 </script>
