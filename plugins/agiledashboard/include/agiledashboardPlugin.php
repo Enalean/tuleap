Enalean/tuleap

commit 17547216c2a68ad2f804bd82682b63bf775d7718

Block 'Add to the top backlog' action of the Agile Dashboard when in a program
Programs needs to have their own "top backlog" since they do not have
the same rule than the Agile Dashboard.

You might notice the workflow post action is still visible in the
administration. The action will be hidden in a upcoming contribution, as
it requires to change the way the tracker plugin does this detection.

Part of story #18408: plan Features in Program Increment

Change-Id: I88c935f137a6a733968ab59c97136b69d103cf4d

plugins/agiledashboard/include/agiledashboardPlugin.php
@@ -1847,7 +1847,8 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
             new ArtifactsInExplicitBacklogDao(),
             new PlannedArtifactDao(),
             $this->getScriptAssetByName('artifact-additional-action.js'),
-            new PlanningTrackerBacklogChecker($this->getPlanningFactory())
+            new PlanningTrackerBacklogChecker($this->getPlanningFactory()),
+            EventManager::instance()
         );
 
         $action = $builder->buildArtifactAction($artifact, $user);
@@ -1862,7 +1863,8 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
         $builder = new \Tuleap\AgileDashboard\Masschange\AdditionalMasschangeActionBuilder(
             new ExplicitBacklogDao(),
             $this->getPlanningFactory(),
-            TemplateRendererFactory::build()->getRenderer(__DIR__ . '/../templates/masschange')
+            TemplateRendererFactory::build()->getRenderer(__DIR__ . '/../templates/masschange'),
+            EventManager::instance()
         );
 
         $additional_action = $builder->buildMasschangeAction($event->getTracker(), $event->getUser());
@@ -1881,7 +1883,8 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
         $processor = new AdditionalMasschangeActionProcessor(
             new ArtifactsInExplicitBacklogDao(),
             new PlannedArtifactDao(),
-            $this->getUnplannedArtifactsAdder()
+            $this->getUnplannedArtifactsAdder(),
+            EventManager::instance()
         );
 
         $processor->processAction(
@@ -2034,7 +2037,7 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
             $this->getCurrentUser()
         );
 
-        if (! $is_agile_dashboard_used || ! $is_explicit_backlog_used || ! $is_tracker_backlog_of_root_planning) {
+        if (! $is_agile_dashboard_used || ! $is_explicit_backlog_used || ! $is_tracker_backlog_of_root_planning || ! $this->isScrumAccessBlocked($tracker->getProject())) {
             return;
         }
 
@@ -2054,14 +2057,17 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
         foreach ($external_post_actions as $post_action) {
             if (
                 $post_action instanceof AddToTopBacklogValue &&
-                ! $planning_tracker_backlog_checker->isTrackerBacklogOfProjectRootPlanning(
-                    $tracker,
-                    $this->getCurrentUser(),
+                (
+                    ! $planning_tracker_backlog_checker->isTrackerBacklogOfProjectRootPlanning(
+                        $tracker,
+                        $this->getCurrentUser(),
+                    ) ||
+                    $this->isScrumAccessBlocked($tracker->getProject())
                 )
             ) {
                 $message = dgettext(
                     'tuleap-agiledashboard',
-                    'The post actions cannot be saved because this tracker not a top backlog tracker and a "AddToTopBacklog" is defined.'
+                    'The post actions cannot be saved because this tracker is not a top backlog tracker and a "AddToTopBacklog" is defined.'
                 );
 
                 $event->setErrorMessage($message);
@@ -2146,5 +2152,12 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
     private function getTemplateRenderer()
     {
         return TemplateRendererFactory::build()->getRenderer(AGILEDASHBOARD_TEMPLATE_DIR);
+    }
+
+    private function isScrumAccessBlocked(Project $project): bool
+    {
+        $block_scrum_access = new \Tuleap\AgileDashboard\BlockScrumAccess($project);
+        EventManager::instance()->dispatch($block_scrum_access);
+        return ! $block_scrum_access->isScrumAccessEnabled();
     }
 }

plugins/agiledashboard/include/AgileDashboard/Artifact/AdditionalArtifactActionBuilder.php
@@ -23,6 +23,7 @@ namespace Tuleap\AgileDashboard\Artifact;
 use PFUser;
 use PlanningFactory;
 use PlanningPermissionsManager;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Tuleap\AgileDashboard\ExplicitBacklog\ArtifactsInExplicitBacklogDao;
 use Tuleap\AgileDashboard\ExplicitBacklog\ExplicitBacklogDao;
 use Tuleap\AgileDashboard\Planning\PlanningTrackerBacklogChecker;
@@ -67,6 +68,10 @@ class AdditionalArtifactActionBuilder
      * @var PlanningTrackerBacklogChecker
      */
     private $planning_tracker_backlog_checker;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $event_dispatcher;
 
     public function __construct(
         ExplicitBacklogDao $explicit_backlog_dao,
@@ -75,7 +80,8 @@ class AdditionalArtifactActionBuilder
         ArtifactsInExplicitBacklogDao $artifacts_in_explicit_backlog_dao,
         PlannedArtifactDao $planned_artifact_dao,
         JavascriptAsset $include_assets,
-        PlanningTrackerBacklogChecker $planning_tracker_backlog_checker
+        PlanningTrackerBacklogChecker $planning_tracker_backlog_checker,
+        EventDispatcherInterface $event_dispatcher
     ) {
         $this->explicit_backlog_dao              = $explicit_backlog_dao;
         $this->planning_factory                  = $planning_factory;
@@ -84,6 +90,7 @@ class AdditionalArtifactActionBuilder
         $this->planned_artifact_dao              = $planned_artifact_dao;
         $this->include_assets                    = $include_assets;
         $this->planning_tracker_backlog_checker  = $planning_tracker_backlog_checker;
+        $this->event_dispatcher                  = $event_dispatcher;
     }
 
     public function buildArtifactAction(Artifact $artifact, PFUser $user): ?AdditionalButtonAction
@@ -95,6 +102,12 @@ class AdditionalArtifactActionBuilder
         $artifact_id = (int) $artifact->getId();
 
         if ($this->explicit_backlog_dao->isProjectUsingExplicitBacklog($project_id) === false) {
+            return null;
+        }
+
+        $block_scrum_access = new \Tuleap\AgileDashboard\BlockScrumAccess($project);
+        $this->event_dispatcher->dispatch($block_scrum_access);
+        if (! $block_scrum_access->isScrumAccessEnabled()) {
             return null;
         }
 

plugins/agiledashboard/include/AgileDashboard/Masschange/AdditionalMasschangeActionBuilder.php
@@ -24,6 +24,7 @@ namespace Tuleap\AgileDashboard\Masschange;
 
 use PFUser;
 use PlanningFactory;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use TemplateRenderer;
 use Tracker;
 use Tuleap\AgileDashboard\ExplicitBacklog\ExplicitBacklogDao;
@@ -44,15 +45,21 @@ class AdditionalMasschangeActionBuilder
      * @var TemplateRenderer
      */
     private $template_renderer;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $event_dispatcher;
 
     public function __construct(
         ExplicitBacklogDao $explicit_backlog_dao,
         PlanningFactory $planning_factory,
-        TemplateRenderer $template_renderer
+        TemplateRenderer $template_renderer,
+        EventDispatcherInterface $event_dispatcher
     ) {
         $this->explicit_backlog_dao = $explicit_backlog_dao;
         $this->planning_factory     = $planning_factory;
         $this->template_renderer    = $template_renderer;
+        $this->event_dispatcher     = $event_dispatcher;
     }
 
     public function buildMasschangeAction(Tracker $tracker, PFUser $user): ?string
@@ -61,7 +68,15 @@ class AdditionalMasschangeActionBuilder
             return null;
         }
 
-        $project_id = (int) $tracker->getProject()->getID();
+        $project = $tracker->getProject();
+
+        $block_scrum_access = new \Tuleap\AgileDashboard\BlockScrumAccess($project);
+        $this->event_dispatcher->dispatch($block_scrum_access);
+        if (! $block_scrum_access->isScrumAccessEnabled()) {
+            return null;
+        }
+
+        $project_id = (int) $project->getID();
         if ($this->explicit_backlog_dao->isProjectUsingExplicitBacklog($project_id) === false) {
             return null;
         }

plugins/agiledashboard/include/AgileDashboard/Masschange/AdditionalMasschangeActionProcessor.php
@@ -25,6 +25,7 @@ namespace Tuleap\AgileDashboard\Masschange;
 use Codendi_Request;
 use Feedback;
 use PFUser;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Tracker;
 use Tuleap\AgileDashboard\Artifact\PlannedArtifactDao;
 use Tuleap\AgileDashboard\ExplicitBacklog\ArtifactAlreadyPlannedException;
@@ -47,15 +48,21 @@ class AdditionalMasschangeActionProcessor
      * @var UnplannedArtifactsAdder
      */
     private $unplanned_artifacts_adder;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $event_dispatcher;
 
     public function __construct(
         ArtifactsInExplicitBacklogDao $artifacts_in_explicit_backlog_dao,
         PlannedArtifactDao $planned_artifact_dao,
-        UnplannedArtifactsAdder $unplanned_artifacts_adder
+        UnplannedArtifactsAdder $unplanned_artifacts_adder,
+        EventDispatcherInterface $event_dispatcher
     ) {
         $this->artifacts_in_explicit_backlog_dao = $artifacts_in_explicit_backlog_dao;
         $this->planned_artifact_dao              = $planned_artifact_dao;
         $this->unplanned_artifacts_adder         = $unplanned_artifacts_adder;
+        $this->event_dispatcher                  = $event_dispatcher;
     }
 
     public function processAction(
@@ -65,6 +72,12 @@ class AdditionalMasschangeActionProcessor
         array $masschange_aids
     ): void {
         if (! $tracker->userIsAdmin($user)) {
+            return;
+        }
+
+        $block_scrum_access = new \Tuleap\AgileDashboard\BlockScrumAccess($tracker->getProject());
+        $this->event_dispatcher->dispatch($block_scrum_access);
+        if (! $block_scrum_access->isScrumAccessEnabled()) {
             return;
         }
 

plugins/agiledashboard/site-content/fr_FR/LC_MESSAGES/tuleap-agiledashboard.po
@@ -616,7 +616,7 @@ msgstr ""
 "d'éléments ici."
 
 msgid ""
-"The post actions cannot be saved because this tracker not a top backlog "
+"The post actions cannot be saved because this tracker is not a top backlog "
 "tracker and a \"AddToTopBacklog\" is defined."
 msgstr ""
 "Les post actions ne peuvent être enregistrées car ce tracker n'est pas "

plugins/agiledashboard/tests/unit/AgileDashboard/Artifact/AdditionalArtifactActionBuilderTest.php
@@ -18,6 +18,8 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 namespace Tuleap\AgileDashboard\Artifact;
 
 use Mockery;
@@ -28,7 +30,9 @@ use Planning;
 use PlanningFactory;
 use PlanningPermissionsManager;
 use Project;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Tracker;
+use Tuleap\AgileDashboard\BlockScrumAccess;
 use Tuleap\AgileDashboard\ExplicitBacklog\ArtifactsInExplicitBacklogDao;
 use Tuleap\AgileDashboard\ExplicitBacklog\ExplicitBacklogDao;
 use Tuleap\AgileDashboard\Planning\PlanningTrackerBacklogChecker;
@@ -90,6 +94,10 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PlanningTrackerBacklogChecker
      */
     private $planning_tracker_backlog_checker;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|EventDispatcherInterface
+     */
+    private $event_dispatcher;
 
     protected function setUp(): void
     {
@@ -101,6 +109,7 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
         $this->artifacts_explicit_backlog_dao   = Mockery::mock(ArtifactsInExplicitBacklogDao::class);
         $this->planned_artifact_dao             = Mockery::mock(PlannedArtifactDao::class);
         $this->planning_tracker_backlog_checker = Mockery::mock(PlanningTrackerBacklogChecker::class);
+        $this->event_dispatcher                 = Mockery::mock(EventDispatcherInterface::class);
 
         $this->builder = new AdditionalArtifactActionBuilder(
             $this->explicit_backlog_dao,
@@ -109,7 +118,8 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
             $this->artifacts_explicit_backlog_dao,
             $this->planned_artifact_dao,
             new JavascriptAsset(Mockery::mock(IncludeAssets::class)->shouldReceive('getFileURL')->getMock(), 'mock.js'),
-            $this->planning_tracker_backlog_checker
+            $this->planning_tracker_backlog_checker,
+            $this->event_dispatcher
         );
 
         $project = Mockery::mock(Project::class);
@@ -146,6 +156,7 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
             ->once()
             ->with(101)
             ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->planning_factory->shouldReceive('getRootPlanning')
             ->once()
@@ -155,12 +166,29 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
         $this->assertNull($this->builder->buildArtifactAction($this->artifact, $this->user));
     }
 
+    public function testItReturnsNullScrumAccessIsBlockedForThisProject(): void
+    {
+        $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
+            ->once()
+            ->with(101)
+            ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch')->andReturnUsing(function (object $event) {
+            if ($event instanceof BlockScrumAccess) {
+                $event->disableScrumAccess();
+            }
+            return $event;
+        });
+
+        self::assertNull($this->builder->buildArtifactAction($this->artifact, $this->user));
+    }
+
     public function testItReturnsNullIfArtifactNotInBacklogTrackerOfRootPlanning(): void
     {
         $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
             ->once()
             ->with(101)
             ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->planning_factory->shouldReceive('getRootPlanning')
             ->once()
@@ -180,6 +208,7 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
             ->once()
             ->with(101)
             ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->planning_factory->shouldReceive('getRootPlanning')
             ->once()
@@ -204,6 +233,7 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
             ->once()
             ->with(101)
             ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->planning_factory->shouldReceive('getRootPlanning')
             ->once()
@@ -233,6 +263,7 @@ final class AdditionalArtifactActionBuilderTest extends TestCase
             ->once()
             ->with(101)
             ->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->planning_factory->shouldReceive('getRootPlanning')
             ->once()

plugins/agiledashboard/tests/unit/AgileDashboard/Masschange/AdditionalMasschangeActionBuilderTest.php
@@ -29,8 +29,10 @@ use PHPUnit\Framework\TestCase;
 use Planning;
 use PlanningFactory;
 use Project;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use TemplateRenderer;
 use Tracker;
+use Tuleap\AgileDashboard\BlockScrumAccess;
 use Tuleap\AgileDashboard\ExplicitBacklog\ExplicitBacklogDao;
 
 final class AdditionalMasschangeActionBuilderTest extends TestCase
@@ -58,6 +60,11 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
     private $template_renderer;
 
     /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|EventDispatcherInterface
+     */
+    private $event_dispatcher;
+
+    /**
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker
      */
     private $tracker;
@@ -66,7 +73,6 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PFUser
      */
     private $user;
-
     /**
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Planning
      */
@@ -79,11 +85,13 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
         $this->explicit_backlog_dao = Mockery::mock(ExplicitBacklogDao::class);
         $this->planning_factory     = Mockery::mock(PlanningFactory::class);
         $this->template_renderer    = Mockery::mock(TemplateRenderer::class);
+        $this->event_dispatcher     = Mockery::mock(EventDispatcherInterface::class);
 
         $this->builder = new AdditionalMasschangeActionBuilder(
             $this->explicit_backlog_dao,
             $this->planning_factory,
-            $this->template_renderer
+            $this->template_renderer,
+            $this->event_dispatcher
         );
 
         $this->tracker = Mockery::mock(Tracker::class);
@@ -96,9 +104,11 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
         $this->root_planning = Mockery::mock(Planning::class);
     }
 
-    public function testItRendersTheMasschangeAdditionalAction()
+    public function testItRendersTheMasschangeAdditionalAction(): void
     {
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
             ->with(101)
@@ -131,9 +141,11 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
         $this->assertNull($additional_action);
     }
 
-    public function testItReturnsNullIfProjectDoesNotUseExplicitBacklog()
+    public function testItReturnsNullIfProjectDoesNotUseExplicitBacklog(): void
     {
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
             ->with(101)
@@ -146,9 +158,11 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
         $this->assertNull($additional_action);
     }
 
-    public function testItReturnsNullIfProjectDoesNotHaveARootPlanning()
+    public function testItReturnsNullIfProjectDoesNotHaveARootPlanning(): void
     {
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
             ->with(101)
@@ -166,9 +180,11 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
         $this->assertNull($additional_action);
     }
 
-    public function testItReturnsNullIfTrackerNotABacklogTracker()
+    public function testItReturnsNullIfTrackerNotABacklogTracker(): void
     {
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
             ->with(101)
@@ -188,5 +204,21 @@ final class AdditionalMasschangeActionBuilderTest extends TestCase
 
         $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
         $this->assertNull($additional_action);
+    }
+
+    public function testReturnsNullWhenScrumAccessIsBlocked(): void
+    {
+        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch')->andReturnUsing(function (object $event) {
+            if ($event instanceof BlockScrumAccess) {
+                $event->disableScrumAccess();
+            }
+            return $event;
+        });
+
+        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+
+        self::assertNull($additional_action);
     }
 }

plugins/agiledashboard/tests/unit/AgileDashboard/Masschange/AdditionalMasschangeActionProcessorTest.php
@@ -28,8 +28,10 @@ use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PFUser;
 use PHPUnit\Framework\TestCase;
 use Project;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Tracker;
 use Tuleap\AgileDashboard\Artifact\PlannedArtifactDao;
+use Tuleap\AgileDashboard\BlockScrumAccess;
 use Tuleap\AgileDashboard\ExplicitBacklog\ArtifactsInExplicitBacklogDao;
 use Tuleap\AgileDashboard\ExplicitBacklog\UnplannedArtifactsAdder;
 use Tuleap\GlobalResponseMock;
@@ -68,6 +70,10 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
      * @var Mockery\LegacyMockInterface|Mockery\MockInterface|UnplannedArtifactsAdder
      */
     private $unplanned_artifacts_adder;
+    /**
+     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|EventDispatcherInterface
+     */
+    private $event_dispatcher;
 
     protected function setUp(): void
     {
@@ -76,11 +82,13 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $this->artifacts_in_explicit_backlog_dao = Mockery::mock(ArtifactsInExplicitBacklogDao::class);
         $this->planned_artifact_dao              = Mockery::mock(PlannedArtifactDao::class);
         $this->unplanned_artifacts_adder         = Mockery::mock(UnplannedArtifactsAdder::class);
+        $this->event_dispatcher                  = Mockery::mock(EventDispatcherInterface::class);
 
         $this->processor = new AdditionalMasschangeActionProcessor(
             $this->artifacts_in_explicit_backlog_dao,
             $this->planned_artifact_dao,
-            $this->unplanned_artifacts_adder
+            $this->unplanned_artifacts_adder,
+            $this->event_dispatcher
         );
 
         $this->user    = Mockery::mock(PFUser::class);
@@ -108,11 +116,37 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         );
     }
 
+    public function testItDoesNothingWhenScrumAccessIsBlocked(): void
+    {
+        $request = new Codendi_Request([]);
+
+        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+
+        $this->event_dispatcher->shouldReceive('dispatch')->andReturnUsing(function (object $event) {
+            if ($event instanceof BlockScrumAccess) {
+                $event->disableScrumAccess();
+            }
+            return $event;
+        });
+
+        $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('removeItemsFromExplicitBacklogOfProject');
+        $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('addArtifactToProjectBacklog');
+        $this->planned_artifact_dao->shouldNotReceive('isArtifactPlannedInAMilestoneOfTheProject');
+
+        $this->processor->processAction(
+            $this->user,
+            $this->tracker,
+            $request,
+            ['125', '144']
+        );
+    }
+
     public function testItDoesNothingIfMasschangeActionIsNotInRequest(): void
     {
         $request = new Codendi_Request([]);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('removeItemsFromExplicitBacklogOfProject');
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('addArtifactToProjectBacklog');
@@ -131,6 +165,7 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $request = new Codendi_Request(['masschange-action-explicit-backlog' => 'unchanged']);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('removeItemsFromExplicitBacklogOfProject');
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('addArtifactToProjectBacklog');
@@ -149,6 +184,7 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $request = new Codendi_Request(['masschange-action-explicit-backlog' => 'remove']);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldReceive('removeItemsFromExplicitBacklogOfProject')
             ->once()
@@ -171,6 +207,7 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $request = new Codendi_Request(['masschange-action-explicit-backlog' => 'remove']);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldReceive('removeItemsFromExplicitBacklogOfProject')
             ->once()
@@ -193,6 +230,7 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $request = new Codendi_Request(['masschange-action-explicit-backlog' => 'add']);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('removeItemsFromExplicitBacklogOfProject');
         $this->unplanned_artifacts_adder->shouldReceive('addArtifactToTopBacklogFromIds')
@@ -216,6 +254,7 @@ class AdditionalMasschangeActionProcessorTest extends TestCase
         $request = new Codendi_Request(['masschange-action-explicit-backlog' => 'whatever']);
 
         $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->event_dispatcher->shouldReceive('dispatch');
 
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('removeItemsFromExplicitBacklogOfProject');
         $this->artifacts_in_explicit_backlog_dao->shouldNotReceive('addArtifactToProjectBacklog');

commit e202d2abfbf4df2eac0fe2146544f27918ac78f0

Do not show the "Add to Scrum top backlog" action in program project
Also the action must be visible when the project is not a program.
This fixes a regression introduced in
17547216c2a68ad2f804bd82682b63bf775d7718, that was caused by me
misinterpreting the front end code 😢.

Part of story #18408: plan Features in Program Increment

Change-Id: I6178df324669ce4cd74405187da45ba24567255c

plugins/agiledashboard/include/agiledashboardPlugin.php
@@ -2037,7 +2037,7 @@ class AgileDashboardPlugin extends Plugin  // phpcs:ignore PSR1.Classes.ClassDec
             $this->getCurrentUser()
         );
 
-        if (! $is_agile_dashboard_used || ! $is_explicit_backlog_used || ! $is_tracker_backlog_of_root_planning || ! $this->isScrumAccessBlocked($tracker->getProject())) {
+        if (! $is_agile_dashboard_used || ! $is_explicit_backlog_used || ! $is_tracker_backlog_of_root_planning || $this->isScrumAccessBlocked($tracker->getProject())) {
             return;
         }
 