cc8cf8093c9833c029a10bbaae02808591097022
Camille Croquet <camille.croquet@enalean.com>
2021-01-28 18:31:21 +0100

    Have shortcuts in test campaign page in Test Management
    Part of story #18461 : Have keyboard shortcuts in TestManagement
    
    Newly added shortcuts are displayed in Shortcuts help modal (`?` key).
    They are divided into two categories:
    * Shortcuts for actions on Campaign page
    * Shortcuts for actions on opened test
    
    Change-Id: Ie8059111c5e1e29cd9068647f525f8148997508d

--- plugins/testmanagement/scripts/testmanagement/src/keyboard-navigation/setup-shortcuts.ts
+++ plugins/testmanagement/scripts/testmanagement/src/keyboard-navigation/setup-shortcuts.ts
@@ -23,8 +23,10 @@ import {
     addShortcutsGroup,
 } from "@tuleap/core/scripts/keyboard-navigation/add-shortcuts-group";
 
-import { moveFocus } from "./move-focus";
 import { Direction, GettextProvider } from "./type";
+
+import { moveFocus } from "./move-focus";
+import { clickOnElement, focusElement } from "./trigger-datashortcut-element";
 
 export function setupTestManagementShortcuts(gettextCatalog: GettextProvider): void {
     const move_to_previous_test: Shortcut = {
@@ -63,6 +65,110 @@ export function setupTestManagementShortcuts(gettextCatalog: GettextProvider): v
         },
     };
 
+    const edit_campaign: Shortcut = {
+        keyboard_inputs: "e",
+        description: gettextCatalog.getString("Edit campaign"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-edit-campaign]");
+        },
+    };
+
+    const focus_search_filter: Shortcut = {
+        keyboard_inputs: "f",
+        description: gettextCatalog.getString("Set focus in search filter"),
+        handle: (): void => {
+            focusElement(document, "[data-shortcut-search-filter]");
+        },
+    };
+
+    const toggle_automated_tests_filter: Shortcut = {
+        keyboard_inputs: "a",
+        description: gettextCatalog.getString("Toggle automated tests"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-filter-automated]");
+        },
+    };
+
+    const focus_test_list: Shortcut = {
+        keyboard_inputs: "l",
+        description: gettextCatalog.getString("Set focus in tests list"),
+        handle: (): void => {
+            focusElement(document, "[data-shortcut-navigation][tabindex='0']");
+        },
+    };
+
+    const show_scores: Shortcut = {
+        keyboard_inputs: "g",
+        description: gettextCatalog.getString("Show test campaign scores"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-campaign-scores]");
+        },
+    };
+
+    const mark_as_success: Shortcut = {
+        keyboard_inputs: "m + p",
+        description: gettextCatalog.getString("Mark test as passed"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-passed]");
+        },
+    };
+
+    const mark_as_blocked: Shortcut = {
+        keyboard_inputs: "m + b",
+        description: gettextCatalog.getString("Mark test as blocked"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-blocked]");
+        },
+    };
+
+    const mark_as_not_run: Shortcut = {
+        keyboard_inputs: "m + n",
+        description: gettextCatalog.getString("Mark test as not run"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-not-run]");
+        },
+    };
+
+    const focus_comment: Shortcut = {
+        keyboard_inputs: "t + c",
+        description: gettextCatalog.getString("Set focus in comment field"),
+        handle: (): void => {
+            focusElement(document, "[data-shortcut-current-test-comment]");
+        },
+    };
+
+    const edit_test: Shortcut = {
+        keyboard_inputs: "t + e",
+        description: gettextCatalog.getString("Edit test"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-edit-test]");
+        },
+    };
+
+    const new_bug: Shortcut = {
+        keyboard_inputs: "t + b",
+        description: gettextCatalog.getString("Create a new bug"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-new-bug]");
+        },
+    };
+
+    const link_bug: Shortcut = {
+        keyboard_inputs: "t + l",
+        description: gettextCatalog.getString("Link to an existing bug"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-link-bug]");
+        },
+    };
+
+    const show_dependency_graph: Shortcut = {
+        keyboard_inputs: "t + d",
+        description: gettextCatalog.getString("Show dependencies graph"),
+        handle: (): void => {
+            clickOnElement(document, "[data-shortcut-dependency-graph]");
+        },
+    };
+
     const test_management_navigation_shortcuts_group: ShortcutsGroup = {
         title: gettextCatalog.getString("Navigation in Test Management"),
         details: gettextCatalog.getString(
@@ -76,5 +182,35 @@ export function setupTestManagementShortcuts(gettextCatalog: GettextProvider): v
         ],
     };
 
+    const test_management_campaign_shortcuts_group: ShortcutsGroup = {
+        title: gettextCatalog.getString("Campaign page shortcuts in Test Management"),
+        shortcuts: [
+            edit_campaign,
+            focus_search_filter,
+            toggle_automated_tests_filter,
+            focus_test_list,
+            show_scores,
+        ],
+    };
+
+    const test_management_actions_shortcuts_group: ShortcutsGroup = {
+        title: gettextCatalog.getString("Test shortcuts in Test Management"),
+        details: gettextCatalog.getString(
+            "Test shortcuts are available on the test currently open."
+        ),
+        shortcuts: [
+            mark_as_success,
+            mark_as_blocked,
+            mark_as_not_run,
+            focus_comment,
+            edit_test,
+            new_bug,
+            link_bug,
+            show_dependency_graph,
+        ],
+    };
+
     addShortcutsGroup(document, test_management_navigation_shortcuts_group);
+    addShortcutsGroup(document, test_management_campaign_shortcuts_group);
+    addShortcutsGroup(document, test_management_actions_shortcuts_group);
 }
