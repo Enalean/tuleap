Enalean/tuleap

commit a0ed87fa219948f67ecaf3d86ad5b928c3c8bb31

Add Markdown support in new follow-ups
Part of story #18336 add support of markdown

How to test:
- Rebuild tracker
- Go to the Artifact view, edit an existing artifact
- When writing a new follow-up comment, you can choose the "Markdown"
format. When you save, the follow-up comment will be saved in
"commonmark" format.
- Image drag/drop and upload still functions as before in the new
follow-up editor.
- Switching to HTML from Text or Markdown should "render" the text
content in HTML (Markdown is converted to HTML)
- Switching from HTML to Text or Markdown should convert the HTML to
Markdown (to preserve formatting as much as possible).
- No other functional change expected to the new follow-up editor.

Notes:
This contribution introduces a new tracker/rich-text-editor library. It
is meant to replace completely codendi/RichTextEditor.js +
tuleap/textarea_rte.js. The new lib is written in vanilla TypeScript in
order to have as few dependencies as possible and be easily testable and
(more importantly) easy to change, which is not the case in the former
codendi/tuleap combination.
The library resides in Tracker and not in core because it is very
dependant on FlamingParrot and Tracker Artifact view form. As for now it
will be used only in Tracker, it makes sense to have it here. Using it
elsewhere will likely require a lot of work and is not in the scope of
this story for now, but it should be possible to isolate the library
from such dependencies.
Some options have been voluntarily left behind. There is only one set of
options for CKEditor (others are not used).
Some functionality will be added in later patches. For example, there is
a snippet of code for default values. This will be done in future
contributions (it is not needed for the follow-up comment).

Change-Id: I9abf3f3124501c05d45a36448dd427c6ddc6bb54

plugins/tracker/webpack.common.js
@@ -52,14 +52,16 @@ const webpack_config_for_burndown_chart = {
 
 const webpack_config_for_ts = {
     entry: {
-        "tracker-homepage": "./scripts/tracker-homepage/src/index.ts",
-        "list-fields": "./scripts/artifact/list-fields.ts",
         "cross-references-fields": "./scripts/form-element/src/cross-references-fields.ts",
+        "edit-view": "./scripts/artifact/edit-view.ts",
+        "list-fields": "./scripts/artifact/list-fields.ts",
+        "tracker-homepage": "./scripts/tracker-homepage/src/index.ts",
     },
     context,
     output,
     externals: {
         tlp: "tlp",
+        CKEDITOR: "CKEDITOR",
     },
     resolve: {
         extensions: [".js", ".ts"],
@@ -93,7 +95,6 @@ const webpack_config_for_vue = {
         "tracker-email-copy-paste-fp": "./scripts/artifact/tracker-email-copy-paste-fp.ts",
         MoveArtifactModal: "./scripts/artifact-action-buttons/src/index.js",
         TrackerAdminFields: "./scripts/TrackerAdminFields.js",
-        "text-follow-up": "./scripts/artifact/text-follow-up.ts",
     },
     context,
     output,
@@ -157,7 +158,6 @@ const config_for_legacy_scripts = {
                 "./scripts/legacy/artifactChildren.js",
                 "./scripts/legacy/load-artifactChildren.js",
                 "./scripts/legacy/modal-in-place.js",
-                "./scripts/legacy/TrackerArtifactEditionSwitcher.js",
                 "./scripts/legacy/FixAggregatesHeaderHeight.js",
                 "./scripts/legacy/TrackerSettings.js",
                 "./scripts/legacy/TrackerCollapseFieldset.js",

plugins/tracker/scripts/lib/rich-text-editor/webpack.common.js
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const path = require("path");
+const webpack_configurator = require("../../../../../tools/utils/scripts/webpack-configurator.js");
+
+const context = __dirname;
+
+const webpack_config = {
+    entry: {
+        "rich-text-editor": "./src/index.ts",
+    },
+    context,
+    output: {
+        path: path.join(context, "./dist/"),
+        library: "PluginTrackerRichTextEditor",
+        libraryTarget: "umd",
+    },
+    externals: {
+        ckeditor4: "CKEDITOR",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            ...webpack_configurator.configureTypescriptLibraryRules(
+                webpack_configurator.babel_options_ie11
+            ),
+            webpack_configurator.rule_po_files,
+        ],
+    },
+    plugins: [webpack_configurator.getCleanWebpackPlugin()],
+};
+
+module.exports = [webpack_config];

plugins/tracker/include/Tracker/Artifact/Renderer/EditRenderer.class.php
@@ -150,8 +150,7 @@ class Tracker_Artifact_EditRenderer extends Tracker_Artifact_EditAbstractRendere
             '/assets/trackers'
         );
 
-        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('text-follow-up.js'));
-        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('tracker-email-copy-paste-fp.js'));
+        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
         $GLOBALS['HTML']->includeFooterJavascriptFile(RelativeDatesAssetsRetriever::retrieveAssetsUrl());
         ListPickerIncluder::includeListPickerAssets($request, $this->tracker->getId());
 

plugins/tracker/scripts/artifact/edit-view.ts
@@ -1,0 +1,62 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { TextFieldFormat } from "../constants/fields-constants";
+import { getUploadImageOptions, UploadImageFormFactory } from "@tuleap/ckeditor-image-upload-form";
+import {
+    RichTextEditorFactory,
+    RichTextEditorOptions,
+} from "@tuleap/plugin-tracker-rich-text-editor";
+
+const NEW_FOLLOWUP_TEXTAREA_ID = "tracker_followup_comment_new";
+const NEW_FOLLOWUP_ID_PREFIX = "new";
+
+const locale = getUserLocale();
+const image_upload_factory = new UploadImageFormFactory(document, locale);
+const editor_factory = new RichTextEditorFactory(document, locale);
+const new_followup_textarea = document.getElementById(NEW_FOLLOWUP_TEXTAREA_ID);
+if (!(new_followup_textarea instanceof HTMLTextAreaElement)) {
+    throw new Error(`Could not find textarea by id #${NEW_FOLLOWUP_TEXTAREA_ID}`);
+}
+
+const help_block = image_upload_factory.createHelpBlock(new_followup_textarea);
+const options: RichTextEditorOptions = {
+    format_selectbox_id: NEW_FOLLOWUP_ID_PREFIX,
+    getAdditionalOptions: (textarea: HTMLTextAreaElement) => getUploadImageOptions(textarea),
+    onFormatChange: (new_format: TextFieldFormat) => {
+        help_block.onFormatChange(new_format);
+    },
+    onEditorInit: (ckeditor: CKEDITOR.editor, textarea: HTMLTextAreaElement) =>
+        image_upload_factory.initiateImageUpload(ckeditor, textarea),
+};
+editor_factory.createRichTextEditor(new_followup_textarea, options);
+
+// Edition-switcher must execute after CKEditors are initialized, otherwise the
+// "submission bar" won't work properly.
+import "./TrackerArtifactEditionSwitcher.js";
+import "./text-follow-up";
+import "./tracker-email-copy-paste-fp";
+
+function getUserLocale(): string {
+    const locale = document.body.dataset.userLocale;
+    if (locale === undefined) {
+        throw new Error("Could not determine user locale from document body");
+    }
+    return locale;
+}

plugins/tracker/scripts/constants/fields-constants.d.ts
@@ -1,0 +1,87 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export type FieldSetIdentifier = "fieldset";
+
+export const CONTAINER_FIELDSET: FieldSetIdentifier;
+
+export type StructuralFieldIdentifier =
+    | "column"
+    | FieldSetIdentifier
+    | "linebreak"
+    | "separator"
+    | "staticrichtext";
+
+export const STRUCTURAL_FIELDS: StructuralFieldIdentifier[];
+
+export type ReadOnlyFieldIdentifier =
+    | "aid"
+    | "atid"
+    | "burndown"
+    | "cross"
+    | "luby"
+    | "lud"
+    | "priority"
+    | "subby"
+    | "subon";
+
+export const READ_ONLY_FIELDS: ReadOnlyFieldIdentifier[];
+
+export type ComputedFieldIdentifier = "computed";
+export type SelectBoxFieldIdentifier = "sb";
+export type DateFieldIdentifier = "date";
+export type IntFieldIdentifier = "int";
+export type FloatFieldIdentifier = "float";
+export type TextFieldIdentifier = "text";
+export type FileFieldIdentifier = "file";
+
+export const COMPUTED_FIELD: ComputedFieldIdentifier;
+export const SELECTBOX_FIELD: SelectBoxFieldIdentifier;
+export const DATE_FIELD: DateFieldIdentifier;
+export const INT_FIELD: IntFieldIdentifier;
+export const FLOAT_FIELD: FloatFieldIdentifier;
+
+export type StaticBindIdentifier = "static";
+export type UserGroupsBindIdentifier = "ugroups";
+export type UsersBindIdentifier = "users";
+
+export const LIST_BIND_STATIC: StaticBindIdentifier;
+export const LIST_BIND_UGROUPS: UserGroupsBindIdentifier;
+export const LIST_BIND_USERS: UsersBindIdentifier;
+
+export type TextFormat = "text";
+export type HTMLFormat = "html";
+export type CommonMarkFormat = "commonmark";
+export type TextFieldFormat = TextFormat | HTMLFormat | CommonMarkFormat;
+
+export const TEXT_FIELD: TextFieldIdentifier;
+export const TEXT_FORMAT_TEXT: TextFormat;
+export const TEXT_FORMAT_HTML: HTMLFormat;
+export const TEXT_FORMAT_COMMONMARK: CommonMarkFormat;
+
+export const FILE_FIELD: FileFieldIdentifier;
+
+export type ReadPermission = "read";
+export type CreatePermission = "create";
+export type UpdatePermission = "update";
+export type Permission = ReadPermission | CreatePermission | UpdatePermission;
+
+export const FIELD_PERMISSION_READ: ReadPermission;
+export const FIELD_PERMISSION_CREATE: CreatePermission;
+export const FIELD_PERMISSION_UPDATE: UpdatePermission;

plugins/tracker/scripts/legacy/TrackerRichTextEditor.js
@@ -30,17 +30,6 @@ document.observe("dom:loaded", function () {
         html_by_default = true;
     }
 
-    var newFollowup = $("tracker_followup_comment_new");
-    if (newFollowup) {
-        new tuleap.textarea.RTE(newFollowup, {
-            toggle: true,
-            default_in_html: false,
-            id: "new",
-            full_width: true,
-            htmlFormat: html_by_default,
-        });
-    }
-
     var massChangeFollowup = $("artifact_masschange_followup_comment");
     if (massChangeFollowup) {
         new tuleap.textarea.RTE(massChangeFollowup, {

plugins/tracker/scripts/lib/rich-text-editor/.gitignore
@@ -1,0 +1,1 @@
+dist/

commit f7f2299af8fcc0af9c24c62f79c6cce5d59e80b0

Fix email copy paste broken in FP
Part of story #18336 add support of markdown
Introduced by a0ed87fa219948f67ecaf3d86ad5b928c3c8bb31

How to test:
- Set up a tracker with "reply by mail" enabled. There is a checkbox in
General options in administration and you also need some semantics like
Description and Title
- Edit an artifact from this tracker. Below the "new follow-up comment"
textarea, there is a little paragraph with a link. The link should open
a modal.
- No functional change in other parts of Tracker.

Notes:
I took this opportunity to separate scripts between "FlamingParrot"
where there are often dependencies on jQuery and codendi, and
"BurningParrot" which depends on tlp. The two should not be mixed as it
can encourage depending on tlp in FlamingParrot, which will crash. Same
for depending on codendi in BurningParrot.

Change-Id: I2357ec8b39690af4efe7625e9d6f80ed1d298530

plugins/tracker/webpack.common.js
@@ -50,18 +50,24 @@ const webpack_config_for_burndown_chart = {
     plugins: [manifest_plugin, webpack_configurator.getMomentLocalePlugin()],
 };
 
-const webpack_config_for_ts = {
+const config_for_flaming_parrot = {
     entry: {
         "cross-references-fields": "./scripts/form-element/src/cross-references-fields.ts",
         "edit-view": "./scripts/artifact/edit-view.ts",
         "list-fields": "./scripts/artifact/list-fields.ts",
-        "tracker-homepage": "./scripts/tracker-homepage/src/index.ts",
+        "tracker-admin": "./scripts/tracker-admin/index.js",
+        "tracker-creation-success": "./scripts/tracker-creation-success-modal/index.ts",
+        "tracker-email-copy-paste-fp": "./scripts/artifact/tracker-email-copy-paste-fp.ts",
+        "tracker-report-expert-mode": "./scripts/report/index.js",
+        "tracker-semantic-timeframe-option-selector":
+            "./scripts/semantic-timeframe-option-selector.js",
     },
     context,
     output,
     externals: {
-        tlp: "tlp",
         CKEDITOR: "CKEDITOR",
+        codendi: "codendi",
+        jquery: "jQuery",
     },
     resolve: {
         extensions: [".js", ".ts"],
@@ -71,28 +77,15 @@ const webpack_config_for_ts = {
             ...webpack_configurator.configureTypescriptRules(
                 webpack_configurator.babel_options_ie11
             ),
-            webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11), // gettext-init.js needs Babel
+            webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
             webpack_configurator.rule_po_files,
         ],
     },
     plugins: [manifest_plugin, webpack_configurator.getTypescriptCheckerPlugin(false)],
 };
 
-const webpack_config_for_vue = {
+const config_for_vue_flaming_parrot = {
     entry: {
-        "admin-nature": "./scripts/admin-nature.js",
-        "global-admin-artifact-links": "./scripts/global-admin/artifact-links.js",
-        "global-admin-trackers": "./scripts/global-admin/trackers.ts",
-        "tracker-admin": "./scripts/tracker-admin/index.js",
-        "tracker-creation": "./scripts/tracker-creation/index.ts",
-        "tracker-creation-success": "./scripts/tracker-creation-success-modal/index.ts",
-        "tracker-permissions-per-group": "./scripts/permissions-per-group/src/index.js",
-        "tracker-report-expert-mode": "./scripts/report/index.js",
-        "tracker-semantic-timeframe-option-selector":
-            "./scripts/semantic-timeframe-option-selector.js",
-        "tracker-workflow-transitions": "./scripts/workflow-transitions/src/index.js",
-        "tracker-email-copy-paste-bp": "./scripts/artifact/tracker-email-copy-paste-bp.ts",
-        "tracker-email-copy-paste-fp": "./scripts/artifact/tracker-email-copy-paste-fp.ts",
         MoveArtifactModal: "./scripts/artifact-action-buttons/src/index.js",
         TrackerAdminFields: "./scripts/TrackerAdminFields.js",
     },
@@ -100,6 +93,38 @@ const webpack_config_for_vue = {
     output,
     externals: {
         codendi: "codendi",
+        jquery: "jQuery",
+    },
+    resolve: {
+        extensions: [".js", ".vue"],
+    },
+    module: {
+        rules: [
+            webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+            webpack_configurator.rule_easygettext_loader,
+            webpack_configurator.rule_vue_loader,
+        ],
+    },
+    plugins: [manifest_plugin, webpack_configurator.getVueLoaderPlugin()],
+    resolveLoader: {
+        alias: webpack_configurator.easygettext_loader_alias,
+    },
+};
+
+const config_for_burning_parrot = {
+    entry: {
+        "admin-nature": "./scripts/admin-nature.js",
+        "global-admin-artifact-links": "./scripts/global-admin/artifact-links.js",
+        "global-admin-trackers": "./scripts/global-admin/trackers.ts",
+        "tracker-creation": "./scripts/tracker-creation/index.ts",
+        "tracker-email-copy-paste-bp": "./scripts/artifact/tracker-email-copy-paste-bp.ts",
+        "tracker-homepage": "./scripts/tracker-homepage/src/index.ts",
+        "tracker-permissions-per-group": "./scripts/permissions-per-group/src/index.js",
+        "tracker-workflow-transitions": "./scripts/workflow-transitions/src/index.js",
+    },
+    context,
+    output,
+    externals: {
         jquery: "jQuery",
         tlp: "tlp",
     },
@@ -111,7 +136,7 @@ const webpack_config_for_vue = {
             ...webpack_configurator.configureTypescriptRules(
                 webpack_configurator.babel_options_ie11
             ),
-            webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+            webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11), // gettext-init.js needs Babel
             webpack_configurator.rule_easygettext_loader,
             webpack_configurator.rule_vue_loader,
         ],
@@ -208,8 +233,9 @@ const config_for_themes = {
 
 module.exports = [
     webpack_config_for_burndown_chart,
-    webpack_config_for_ts,
-    webpack_config_for_vue,
+    config_for_flaming_parrot,
+    config_for_vue_flaming_parrot,
+    config_for_burning_parrot,
     config_for_legacy_scripts,
     config_for_themes,
 ];