413e68a7359bbafc6cfc2705bf7f8ab4020ec5b8
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-02-02 16:27:51 +0100

    Rename changeset comments and text fields body format from markdown to commonmark
    While it should not have been encountered on production instances it is
    possible that on some dev/test instances a few comments still have the
    body format 'markdown' instead of 'commonmark'.
    
    Part of story #18336: add support of markdown
    
    Change-Id: I2567847fc2475d802e42e5779b0778906641c1d3

---
+++ plugins/tracker/db/mysql/updates/2021/202102021620_change_format_markdown_commonmark.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202102021620_change_format_markdown_commonmark extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Rename format from Markdown to CommonMark (comments and text fields)';
+    }
+
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up(): void
+    {
+        $sql_comment = 'UPDATE tracker_changeset_comment SET body_format = "commonmark" WHERE body_format = "markdown"';
+        if ($this->db->dbh->exec($sql_comment) === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete(
+                'An error occurred while changing the body format of the changeset comments'
+            );
+        }
+
+        $sql_text_fields = 'UPDATE tracker_changeset_value_text SET body_format = "commonmark" WHERE body_format = "markdown"';
+        if ($this->db->dbh->exec($sql_text_fields) === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete(
+                'An error occurred while changing the body format of the text fields'
+            );
+        }
+    }
+}
