Enalean/tuleap

commit 807fd75874c4bdb08afe98e2e385af97507a0266

Extract a lib for BreadcrumbPrivacy Vue component
Part of request #19247 Extract an internal lib for BreadcrumbPrivacy vue
component

How to test:
- Rebuild tuleap (make post-checkout)
- No functional change expected. The Breadcrumb for project privacy
functions as before in Document, Git, ScaledAgile and the "Switch to"
modal.

Note:
An additional patch is needed for Baseline plugin.
The unit test no longer really runs createPopover() from "tlp" but a
"no-op" mock instead. It was getting far too painful to make it work for
very limited gain. As a consequence, the snapshots have changed
marginally (a x-placement attribute and a CSS style have disappeared).
If you know how to make it work, be my guest and submit a contribution
:).

Change-Id: Ic1f1bbb5a58dc8461f0c63bca555da73581968a0

src/scripts/switch-to/src/components/Body/Projects/ProjectLink.vue
@@ -44,10 +44,7 @@
 import Vue from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 import { Project } from "../../../type";
-import {
-    getProjectPrivacyIcon,
-    ProjectPrivacy,
-} from "../../../../../project/privacy/project-privacy-helper";
+import { getProjectPrivacyIcon, ProjectPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 import { Action, State } from "vuex-class";
 import { sprintf } from "sprintf-js";
 import { FocusFromProjectPayload } from "../../../store/type";

plugins/document/scripts/document/components/Breadcrumb/DocumentBreadcrumb.vue
@@ -91,7 +91,7 @@
 import { mapState } from "vuex";
 import DocumentBreadcrumbElement from "./DocumentBreadcrumbElement.vue";
 import DocumentBreadcrumbDocument from "./DocumentBreadcrumbDocument.vue";
-import BreadcrumbPrivacy from "@tuleap/core/scripts/vue-components/breadcrumb-privacy/dist/breadcrumb-privacy";
+import { BreadcrumbPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 
 export default {
     name: "DocumentBreadcrumb",

plugins/git/scripts/repositories/src/breadcrumb-presenter.ts
@@ -17,10 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-    ProjectPrivacy,
-    ProjectFlag,
-} from "@tuleap/core/scripts/project/privacy/project-privacy-helper";
+import { ProjectPrivacy, ProjectFlag } from "@tuleap/vue-breadcrumb-privacy";
 
 export {
     setBreadcrumbSettings,

plugins/git/scripts/repositories/src/components/GitBreadcrumbs.vue
@@ -78,7 +78,7 @@ import {
     getProjectFlags,
 } from "../breadcrumb-presenter";
 import { getUserIsAdmin } from "../repository-list-presenter";
-import BreadcrumbPrivacy from "@tuleap/core/scripts/vue-components/breadcrumb-privacy/dist/breadcrumb-privacy";
+import { BreadcrumbPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 
 export default {
     name: "GitBreadcrumbs",

plugins/scaled_agile/scripts/scaled_agile/src/components/App.vue
@@ -45,10 +45,7 @@ import {
 } from "../configuration";
 import ProgramIncrementList from "./Backlog/ProgramIncrement/ProgramIncrementList.vue";
 import ToBePlanned from "./Backlog/ToBePlanned/ToBePlanned.vue";
-import {
-    ProjectFlag,
-    ProjectPrivacy,
-} from "@tuleap/core/scripts/project/privacy/project-privacy-helper";
+import { ProjectFlag, ProjectPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 
 @Component({
     components: { ToBePlanned, ProgramIncrementList: ProgramIncrementList, Breadcrumb },

plugins/scaled_agile/scripts/scaled_agile/src/components/Breadcrumb.vue
@@ -40,11 +40,7 @@
 <script lang="ts">
 import { Component, Prop } from "vue-property-decorator";
 import Vue from "vue";
-import BreadcrumbPrivacy from "@tuleap/core/scripts/vue-components/breadcrumb-privacy/BreadcrumbPrivacy.vue";
-import {
-    ProjectFlag,
-    ProjectPrivacy,
-} from "@tuleap/core/scripts/project/privacy/project-privacy-helper";
+import { BreadcrumbPrivacy, ProjectFlag, ProjectPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 
 @Component({ components: { BreadcrumbPrivacy } })
 export default class Breadcrumb extends Vue {

plugins/scaled_agile/scripts/scaled_agile/src/configuration.ts
@@ -17,10 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {
-    ProjectFlag,
-    ProjectPrivacy,
-} from "@tuleap/core/scripts/project/privacy/project-privacy-helper";
+import { ProjectFlag, ProjectPrivacy } from "@tuleap/vue-breadcrumb-privacy";
 
 let project_public_name: string;
 let project_short_name: string;

commit c59f4e68e7c6a9f22587996b9953075f90816c24

Switch to… component does not work on FlamingParrot pages
Since Webpack does not have the capability to build ES2015 module to
export the vue-breadcrumb-privacy module [0], the library cannot be tree
shaken when imported. As a result in src/scripts/switch-to/src/components/Body/Projects/ProjectLink.vue
we need a dependency that is not needed for the part of the imported
library.

Issue introduced by 807fd75874c4bdb08afe98e2e385af97507a0266.

Part of request #19247: Extract an internal lib for BreadcrumbPrivacy vue
component

[0] https://github.com/webpack/webpack/issues/2933

Change-Id: Ibc8357150ac7bfe12e887baa811e281d86198792

src/scripts/switch-to/src/components/Body/Projects/ProjectLink.vue
@@ -44,7 +44,10 @@
 import Vue from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 import { Project } from "../../../type";
-import { getProjectPrivacyIcon, ProjectPrivacy } from "@tuleap/vue-breadcrumb-privacy";
+import {
+    getProjectPrivacyIcon,
+    ProjectPrivacy,
+} from "@tuleap/vue-breadcrumb-privacy/src/project-privacy-helper";
 import { Action, State } from "vuex-class";
 import { sprintf } from "sprintf-js";
 import { FocusFromProjectPayload } from "../../../store/type";