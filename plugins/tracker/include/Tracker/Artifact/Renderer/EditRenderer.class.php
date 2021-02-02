Enalean/tuleap

commit a0ed87fa219948f67ecaf3d86ad5b928c3c8bb31

Add Markdown support in new follow-ups
Part of story #18336 add support of markdown

How to test:
- Rebuild tracker
- Go to the Artifact view, edit an existing artifact
- When writing a new follow-up comment, you can choose the "Markdown"
format. When you save, the follow-up comment will be saved in
"commonmark" format.
- Image drag/drop and upload still functions as before in the new
follow-up editor.
- Switching to HTML from Text or Markdown should "render" the text
content in HTML (Markdown is converted to HTML)
- Switching from HTML to Text or Markdown should convert the HTML to
Markdown (to preserve formatting as much as possible).
- No other functional change expected to the new follow-up editor.

Notes:
This contribution introduces a new tracker/rich-text-editor library. It
is meant to replace completely codendi/RichTextEditor.js +
tuleap/textarea_rte.js. The new lib is written in vanilla TypeScript in
order to have as few dependencies as possible and be easily testable and
(more importantly) easy to change, which is not the case in the former
codendi/tuleap combination.
The library resides in Tracker and not in core because it is very
dependant on FlamingParrot and Tracker Artifact view form. As for now it
will be used only in Tracker, it makes sense to have it here. Using it
elsewhere will likely require a lot of work and is not in the scope of
this story for now, but it should be possible to isolate the library
from such dependencies.
Some options have been voluntarily left behind. There is only one set of
options for CKEditor (others are not used).
Some functionality will be added in later patches. For example, there is
a snippet of code for default values. This will be done in future
contributions (it is not needed for the follow-up comment).

Change-Id: I9abf3f3124501c05d45a36448dd427c6ddc6bb54

plugins/tracker/include/Tracker/Artifact/Renderer/EditRenderer.class.php
@@ -150,8 +150,7 @@ class Tracker_Artifact_EditRenderer extends Tracker_Artifact_EditAbstractRendere
             '/assets/trackers'
         );
 
-        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('text-follow-up.js'));
-        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('tracker-email-copy-paste-fp.js'));
+        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
         $GLOBALS['HTML']->includeFooterJavascriptFile(RelativeDatesAssetsRetriever::retrieveAssetsUrl());
         ListPickerIncluder::includeListPickerAssets($request, $this->tracker->getId());
 

plugins/tracker/scripts/artifact/edit-view.ts
@@ -1,0 +1,62 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { TextFieldFormat } from "../constants/fields-constants";
+import { getUploadImageOptions, UploadImageFormFactory } from "@tuleap/ckeditor-image-upload-form";
+import {
+    RichTextEditorFactory,
+    RichTextEditorOptions,
+} from "@tuleap/plugin-tracker-rich-text-editor";
+
+const NEW_FOLLOWUP_TEXTAREA_ID = "tracker_followup_comment_new";
+const NEW_FOLLOWUP_ID_PREFIX = "new";
+
+const locale = getUserLocale();
+const image_upload_factory = new UploadImageFormFactory(document, locale);
+const editor_factory = new RichTextEditorFactory(document, locale);
+const new_followup_textarea = document.getElementById(NEW_FOLLOWUP_TEXTAREA_ID);
+if (!(new_followup_textarea instanceof HTMLTextAreaElement)) {
+    throw new Error(`Could not find textarea by id #${NEW_FOLLOWUP_TEXTAREA_ID}`);
+}
+
+const help_block = image_upload_factory.createHelpBlock(new_followup_textarea);
+const options: RichTextEditorOptions = {
+    format_selectbox_id: NEW_FOLLOWUP_ID_PREFIX,
+    getAdditionalOptions: (textarea: HTMLTextAreaElement) => getUploadImageOptions(textarea),
+    onFormatChange: (new_format: TextFieldFormat) => {
+        help_block.onFormatChange(new_format);
+    },
+    onEditorInit: (ckeditor: CKEDITOR.editor, textarea: HTMLTextAreaElement) =>
+        image_upload_factory.initiateImageUpload(ckeditor, textarea),
+};
+editor_factory.createRichTextEditor(new_followup_textarea, options);
+
+// Edition-switcher must execute after CKEditors are initialized, otherwise the
+// "submission bar" won't work properly.
+import "./TrackerArtifactEditionSwitcher.js";
+import "./text-follow-up";
+import "./tracker-email-copy-paste-fp";
+
+function getUserLocale(): string {
+    const locale = document.body.dataset.userLocale;
+    if (locale === undefined) {
+        throw new Error("Could not determine user locale from document body");
+    }
+    return locale;
+}

plugins/tracker/scripts/constants/fields-constants.d.ts
@@ -1,0 +1,87 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export type FieldSetIdentifier = "fieldset";
+
+export const CONTAINER_FIELDSET: FieldSetIdentifier;
+
+export type StructuralFieldIdentifier =
+    | "column"
+    | FieldSetIdentifier
+    | "linebreak"
+    | "separator"
+    | "staticrichtext";
+
+export const STRUCTURAL_FIELDS: StructuralFieldIdentifier[];
+
+export type ReadOnlyFieldIdentifier =
+    | "aid"
+    | "atid"
+    | "burndown"
+    | "cross"
+    | "luby"
+    | "lud"
+    | "priority"
+    | "subby"
+    | "subon";
+
+export const READ_ONLY_FIELDS: ReadOnlyFieldIdentifier[];
+
+export type ComputedFieldIdentifier = "computed";
+export type SelectBoxFieldIdentifier = "sb";
+export type DateFieldIdentifier = "date";
+export type IntFieldIdentifier = "int";
+export type FloatFieldIdentifier = "float";
+export type TextFieldIdentifier = "text";
+export type FileFieldIdentifier = "file";
+
+export const COMPUTED_FIELD: ComputedFieldIdentifier;
+export const SELECTBOX_FIELD: SelectBoxFieldIdentifier;
+export const DATE_FIELD: DateFieldIdentifier;
+export const INT_FIELD: IntFieldIdentifier;
+export const FLOAT_FIELD: FloatFieldIdentifier;
+
+export type StaticBindIdentifier = "static";
+export type UserGroupsBindIdentifier = "ugroups";
+export type UsersBindIdentifier = "users";
+
+export const LIST_BIND_STATIC: StaticBindIdentifier;
+export const LIST_BIND_UGROUPS: UserGroupsBindIdentifier;
+export const LIST_BIND_USERS: UsersBindIdentifier;
+
+export type TextFormat = "text";
+export type HTMLFormat = "html";
+export type CommonMarkFormat = "commonmark";
+export type TextFieldFormat = TextFormat | HTMLFormat | CommonMarkFormat;
+
+export const TEXT_FIELD: TextFieldIdentifier;
+export const TEXT_FORMAT_TEXT: TextFormat;
+export const TEXT_FORMAT_HTML: HTMLFormat;
+export const TEXT_FORMAT_COMMONMARK: CommonMarkFormat;
+
+export const FILE_FIELD: FileFieldIdentifier;
+
+export type ReadPermission = "read";
+export type CreatePermission = "create";
+export type UpdatePermission = "update";
+export type Permission = ReadPermission | CreatePermission | UpdatePermission;
+
+export const FIELD_PERMISSION_READ: ReadPermission;
+export const FIELD_PERMISSION_CREATE: CreatePermission;
+export const FIELD_PERMISSION_UPDATE: UpdatePermission;

plugins/tracker/scripts/legacy/TrackerRichTextEditor.js
@@ -30,17 +30,6 @@ document.observe("dom:loaded", function () {
         html_by_default = true;
     }
 
-    var newFollowup = $("tracker_followup_comment_new");
-    if (newFollowup) {
-        new tuleap.textarea.RTE(newFollowup, {
-            toggle: true,
-            default_in_html: false,
-            id: "new",
-            full_width: true,
-            htmlFormat: html_by_default,
-        });
-    }
-
     var massChangeFollowup = $("artifact_masschange_followup_comment");
     if (massChangeFollowup) {
         new tuleap.textarea.RTE(massChangeFollowup, {

plugins/tracker/scripts/lib/rich-text-editor/.gitignore
@@ -1,0 +1,1 @@
+dist/

plugins/tracker/scripts/lib/rich-text-editor/jest.config.js
@@ -1,0 +1,23 @@
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    ...require("../../../../../tests/jest/jest.base.config.js"),
+    displayName: "@tuleap/plugin-tracker-rich-text-editor",
+};

plugins/tracker/scripts/lib/rich-text-editor/po/fr_FR.po
@@ -1,0 +1,25 @@
+msgid ""
+msgstr ""
+"Project-Id-Version: \n"
+"POT-Creation-Date: \n"
+"PO-Revision-Date: \n"
+"Last-Translator: Joris MASSON <joris.masson@enalean.com>\n"
+"Language-Team: \n"
+"Language: fr_FR\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"Plural-Forms: nplurals=2; plural=(n > 1);\n"
+"X-Generator: Poedit 2.4.2\n"
+
+msgid "Format:"
+msgstr "Format :"
+
+msgid "HTML"
+msgstr "HTML"
+
+msgid "Markdown"
+msgstr "Markdown"
+
+msgid "Text"
+msgstr "Texte"

commit 6039ac653b74fd664af47eb579e8782ad821a5a8

Do not load JS dedicated to the follow comment edition when there is no follow up comments to post
No JS error should be thrown when displaying other artifact views such
as the timetracking view or when the follow up comments cannot be
created by the current user.
Issue introduced by a0ed87fa219948f67ecaf3d86ad5b928c3c8bb31.

Part of story #18336: add support of markdown

Change-Id: Ia9eaca5d92d70af44492c9fb17f3d0ec39df1194

plugins/tracker/include/Tracker/Artifact/Renderer/EditRenderer.class.php
@@ -144,13 +144,6 @@ class Tracker_Artifact_EditRenderer extends Tracker_Artifact_EditAbstractRendere
             )
         ];
 
-
-        $include_assets = new \Tuleap\Layout\IncludeAssets(
-            __DIR__ . '/../../../../../../src/www/assets/trackers',
-            '/assets/trackers'
-        );
-
-        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
         $GLOBALS['HTML']->includeFooterJavascriptFile(RelativeDatesAssetsRetriever::retrieveAssetsUrl());
         ListPickerIncluder::includeListPickerAssets($request, $this->tracker->getId());
 

plugins/tracker/include/Tracker/Artifact/View/Edit.class.php
@@ -77,6 +77,14 @@ class Tracker_Artifact_View_Edit extends Tracker_Artifact_View_View
     /** @see Tracker_Artifact_View_View::fetch() */
     public function fetch()
     {
+        if ($this->artifact->userCanUpdate($this->user)) {
+            $include_assets = new \Tuleap\Layout\IncludeAssets(
+                __DIR__ . '/../../../../../../src/www/assets/trackers',
+                '/assets/trackers'
+            );
+            $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
+        }
+
         $html  = '';
         $html .= '<div class="tracker_artifact">';
 