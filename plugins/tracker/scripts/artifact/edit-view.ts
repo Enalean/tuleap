Enalean/tuleap

commit 518ac368b65790f19238c3e5a72127d52d1eaf34

Cannot edit fields of an artifact while copying it
Issue introduced by 6039ac653b74fd664af47eb579e8782ad821a5a8.

Part of story #18336: add support of markdown

Change-Id: I6b49f801511f736d9546bb56ac0052cb108bf2b3

plugins/tracker/scripts/artifact/edit-view.ts
@@ -27,23 +27,25 @@ import {
 const NEW_FOLLOWUP_TEXTAREA_ID = "tracker_followup_comment_new";
 const NEW_FOLLOWUP_ID_PREFIX = "new";
 
-const locale = getUserLocale();
-const image_upload_factory = new UploadImageFormFactory(document, locale);
-const editor_factory = new RichTextEditorFactory(document, locale);
 const new_followup_textarea = document.getElementById(NEW_FOLLOWUP_TEXTAREA_ID);
-if (!(new_followup_textarea instanceof HTMLTextAreaElement)) {
-    throw new Error(`Could not find textarea by id #${NEW_FOLLOWUP_TEXTAREA_ID}`);
+if (
+    new_followup_textarea instanceof HTMLTextAreaElement &&
+    new_followup_textarea.isContentEditable
+) {
+    // This can happen when copying artifacts
+    const locale = getUserLocale();
+    const image_upload_factory = new UploadImageFormFactory(document, locale);
+    const editor_factory = new RichTextEditorFactory(document, locale);
+    const help_block = image_upload_factory.createHelpBlock(new_followup_textarea);
+    const options: RichTextEditorOptions = {
+        format_selectbox_id: NEW_FOLLOWUP_ID_PREFIX,
+        getAdditionalOptions: (textarea: HTMLTextAreaElement) => getUploadImageOptions(textarea),
+        onFormatChange: (new_format: TextFieldFormat) => help_block?.onFormatChange(new_format),
+        onEditorInit: (ckeditor: CKEDITOR.editor, textarea: HTMLTextAreaElement) =>
+            image_upload_factory.initiateImageUpload(ckeditor, textarea),
+    };
+    editor_factory.createRichTextEditor(new_followup_textarea, options);
 }
-
-const help_block = image_upload_factory.createHelpBlock(new_followup_textarea);
-const options: RichTextEditorOptions = {
-    format_selectbox_id: NEW_FOLLOWUP_ID_PREFIX,
-    getAdditionalOptions: (textarea: HTMLTextAreaElement) => getUploadImageOptions(textarea),
-    onFormatChange: (new_format: TextFieldFormat) => help_block?.onFormatChange(new_format),
-    onEditorInit: (ckeditor: CKEDITOR.editor, textarea: HTMLTextAreaElement) =>
-        image_upload_factory.initiateImageUpload(ckeditor, textarea),
-};
-editor_factory.createRichTextEditor(new_followup_textarea, options);
 
 // Edition-switcher must execute after CKEditors are initialized, otherwise the
 // "submission bar" won't work properly.

plugins/tracker/include/Tracker/Artifact/View/Copy.class.php
@@ -41,6 +41,8 @@ class Tracker_Artifact_View_Copy extends Tracker_Artifact_View_Edit
     /** @see Tracker_Artifact_View_Edit::fetch() */
     public function fetch()
     {
+        self::fetchEditViewJSCode();
+
         $html  = '';
         $html .= '<div class="tracker_artifact">';
         $html .= $this->renderer->fetchFieldsForCopy($this->artifact, $this->request->get('artifact'));

plugins/tracker/include/Tracker/Artifact/View/Edit.class.php
@@ -78,11 +78,7 @@ class Tracker_Artifact_View_Edit extends Tracker_Artifact_View_View
     public function fetch()
     {
         if ($this->artifact->userCanUpdate($this->user)) {
-            $include_assets = new \Tuleap\Layout\IncludeAssets(
-                __DIR__ . '/../../../../../../src/www/assets/trackers',
-                '/assets/trackers'
-            );
-            $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
+            self::fetchEditViewJSCode();
         }
 
         $html  = '';
@@ -99,6 +95,15 @@ class Tracker_Artifact_View_Edit extends Tracker_Artifact_View_View
         $html .= '</div>';
 
         return $html;
+    }
+
+    final protected static function fetchEditViewJSCode(): void
+    {
+        $include_assets = new \Tuleap\Layout\IncludeAssets(
+            __DIR__ . '/../../../../../../src/www/assets/trackers',
+            '/assets/trackers'
+        );
+        $GLOBALS['HTML']->includeFooterJavascriptFile($include_assets->getFileURL('edit-view.js'));
     }
 
     /**

src/common/layout/Layout.class.php
@@ -704,12 +704,14 @@ abstract class Layout extends Tuleap\Layout\BaseLayout //phpcs:ignore PSR1.Class
     public function pv_header($params) // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps
     {
         $this->generic_header($params);
+        $current_user = UserManager::instance()->getCurrentUser();
+        $user_locale  = $current_user->getLocale();
+        $hp           = Codendi_HTMLPurifier::instance();
         echo '
-<body class="bg_help">
+<body class="bg_help" data-user-locale="' . $hp->purify($user_locale) . '">
 ';
         if (isset($params['pv']) && $params['pv'] < 2) {
             if (isset($params['title']) && $params['title']) {
-                $hp    = Codendi_HTMLPurifier::instance();
                 $title = $params['title'] . ' - ' . format_date(
                     $GLOBALS['Language']->getText('system', 'datefmt'),
                     time()

commit c9aa960157e7616ba8db2bd61fb23fb866bcfe7c

Do not check if content is editable
Part of story #18336: add support of markdown
Issue introduced by 518ac368b65790f19238c3e5a72127d52d1eaf34

How to test:
- As project member, edit an existing artifact. You can add a new
follow-up comment and change the format of the textarea.
- Duplicate this artifact. There is no "new follow-up textarea" and no
JS error in the console.
- Show the artifact in "print view" by adding "&pv=1". The textarea is
present and you can change its format.
- same with "&pv=2"
- As anonymous, browse an existing artifact. There is no JS error in the
console.

Notes:
Even when the textarea is present and I have permission to edit (I'm not
anonymous), `isContentEditable` returns false...

Change-Id: I64fb55c0b4adff695957bd6d5ba26bd93a82c36b

plugins/tracker/scripts/artifact/edit-view.ts
@@ -28,11 +28,8 @@ const NEW_FOLLOWUP_TEXTAREA_ID = "tracker_followup_comment_new";
 const NEW_FOLLOWUP_ID_PREFIX = "new";
 
 const new_followup_textarea = document.getElementById(NEW_FOLLOWUP_TEXTAREA_ID);
-if (
-    new_followup_textarea instanceof HTMLTextAreaElement &&
-    new_followup_textarea.isContentEditable
-) {
-    // This can happen when copying artifacts
+if (new_followup_textarea instanceof HTMLTextAreaElement) {
+    // When copying artifacts or browsing as anonymous, there is no "new follow-up" textarea
     const locale = getUserLocale();
     const image_upload_factory = new UploadImageFormFactory(document, locale);
     const editor_factory = new RichTextEditorFactory(document, locale);