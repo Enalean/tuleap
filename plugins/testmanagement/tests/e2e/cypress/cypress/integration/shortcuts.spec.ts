b5bdca8420b1832467e7215d26a3e80b581dcd65
Camille Croquet <camille.croquet@enalean.com>
2021-02-22 17:04:35 +0100

    Have e2e tests for Test Management shortcuts and tests list navigation
    Part of story #19340 : Have keyboard shortcuts in TestManagement (suite)
    
    * Focusing the campaign search field and opening the `Select tests` modal using shortcuts makes sure that shortcuts are available in Test Management.
    * Setting focus in tests list makes sure one test tab can always take focus.
    * Navigating in tests list makes sure user can move between test tabs and categories.
    * Filtering remaining tests makes sure filtering works properly using a shortcut.
    
    Change-Id: I097e32a1ba44250cb09fb35e1c54640e1d36311a

---
+++ plugins/testmanagement/tests/e2e/cypress/cypress/integration/shortcuts.spec.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe("TTM shortcuts", () => {
+    before(() => {
+        cy.clearCookie("__Host-TULEAP_session_hash");
+        cy.projectMemberLogin();
+        cy.visitProjectService("test-management-project", "Test Management");
+    });
+
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce("__Host-TULEAP_PHPSESSID", "__Host-TULEAP_session_hash");
+        // eslint-disable-next-line cypress/require-data-selectors
+        cy.get("body").as("body");
+    });
+
+    context("In test campaigns list page", () => {
+        it("focuses search field", () => {
+            cy.get("@body").type("f");
+            cy.focused().should("have.attr", "data-test", "search-filter");
+        });
+    });
+
+    context("In test campaign page", () => {
+        before(() => {
+            cy.contains("A test campaign").click();
+            cy.contains("First test case");
+        });
+
+        it("allows user to navigate in tests list using its keyboard", () => {
+            cy.get("@body").type("l{home}");
+            cy.focused().should("contain", "First test case");
+
+            cy.get("@body").type("l{downarrow}{downarrow}");
+            cy.focused().should("contain", "Third test case");
+
+            cy.get("@body").type("{end}{downarrow}");
+            cy.focused().should("contain", "First test case");
+        });
+
+        it("filters remaining tests with `r` key shortcut", () => {
+            cy.contains("First test case").click();
+            cy.get("[data-test=mark-test-as-passed]").click();
+            cy.get("[data-test=current-test").should("have.class", "passed");
+
+            cy.get("@body").type("r");
+            cy.get("[data-test=campaign-tests-list]")
+                .as("tests-list")
+                .should("not.contain", "First test case");
+
+            cy.get("[data-test=mark-test-as-notrun]").click();
+            cy.get("@tests-list").should("contain", "First test case");
+        });
+
+        it("opens `Select tests` modal", () => {
+            cy.get("@body").type("e");
+            cy.get("[data-test=close-select-tests-modal]").click();
+        });
+    });
+});
