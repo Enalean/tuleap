7b5b71e3946890471e25da6f902bb44a4551e316
Clarck Robinson <clarck.robinson@enalean.com>
2021-02-26 16:57:41 +0100

    Add the common mark syntax helper in text field in  artifact view
    Part of story #18336 add support of markdown
    
    How to test:
     - Choose the 'Markdown' format in the format selector of a text field
    => The 'Help' should be displayed
    => By clicking on it, the popover should be displayed
    => If the format is not the Markdown format, then the button is not
    displayed.
    
     - You can also do the test for the follow up comment in artifact view
       and mass change view.
    
    Change-Id: I7782cb3cf1cb5ac113720349690d9fc749f009e9

--- plugins/tracker/scripts/lib/rich-text-editor/src/FlamingParrotDocumentAdapter.ts
+++ plugins/tracker/scripts/lib/rich-text-editor/src/FlamingParrotDocumentAdapter.ts
@@ -18,7 +18,13 @@
  */
 
 import type { TextFieldFormat } from "../../../constants/fields-constants";
-import { TEXT_FORMAT_HTML, TEXT_FORMAT_TEXT } from "../../../constants/fields-constants";
+import {
+    TEXT_FORMAT_COMMONMARK,
+    TEXT_FORMAT_HTML,
+    TEXT_FORMAT_TEXT,
+} from "../../../constants/fields-constants";
+
+import $ from "jquery";
 
 export const HTML_FORMAT_CLASSNAME = "default_format_html";
 const FLAMING_PARROT_SELECTBOX_CLASSNAME = "input-small";
@@ -39,9 +45,15 @@ export interface SelectboxPresenter {
     options: HTMLOptionElement[];
 }
 
-export interface SelectboxWrapperPresenter {
+export interface FormatWrapperPresenter {
     label: string;
-    child: HTMLSelectElement;
+    selectbox: HTMLSelectElement;
+    button_helper: HTMLButtonElement;
+}
+
+export interface SyntaxButtonPresenter {
+    label: string;
+    popover_content: string;
 }
 
 export class FlamingParrotDocumentAdapter {
@@ -73,15 +85,47 @@ export class FlamingParrotDocumentAdapter {
         return select;
     }
 
-    public createSelectBoxWrapper(presenter: SelectboxWrapperPresenter): HTMLDivElement {
+    public createFormatWrapper(presenter: FormatWrapperPresenter): HTMLDivElement {
         const wrapper = this.doc.createElement("div");
         wrapper.textContent = presenter.label;
         wrapper.classList.add(FLAMING_PARROT_WRAPPER_CLASSNAME);
-        wrapper.append(presenter.child);
+        wrapper.append(presenter.selectbox, presenter.button_helper);
         return wrapper;
     }
 
     public insertFormatWrapper(textarea: HTMLTextAreaElement, wrapper: HTMLDivElement): void {
         textarea.insertAdjacentElement("beforebegin", wrapper);
+    }
+
+    public createCommonMarkSyntaxHelperButton(
+        button_presenter: SyntaxButtonPresenter,
+        format: TextFieldFormat
+    ): HTMLButtonElement {
+        const button = this.doc.createElement("button");
+        button.type = "button";
+        button.classList.add("btn", "btn-small", "commonmark-button-help");
+
+        if (format === TEXT_FORMAT_COMMONMARK) {
+            button.classList.add("commonmark-button-help-show");
+        }
+
+        const help_icon = this.doc.createElement("i");
+        help_icon.classList.add("fas", "fa-question-circle", "help-button-icon");
+        button.append(help_icon);
+        button.insertAdjacentText("beforeend", button_presenter.label);
+        this.bindToButtonHelpThePopover(button, button_presenter.popover_content);
+        return button;
+    }
+
+    private bindToButtonHelpThePopover(
+        help_button: HTMLButtonElement,
+        popover_content: string
+    ): void {
+        $(help_button).popover({
+            content: popover_content,
+            trigger: "click",
+            html: true,
+            placement: "right",
+        });
     }
 }
