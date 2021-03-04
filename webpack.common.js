a2d0affb73e291cf8ebad2ecf74eff93abe2328f
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-03-04 14:12:40 +0100

    Stop rewriting code for IE with Babel
    Part of request #19709: Delete the polyfills for IE11
    
    Change-Id: Id00c192764a30b869e4c19de30d86457f8ceccc8

--- webpack.common.js
+++ webpack.common.js
@@ -35,7 +35,7 @@ module.exports = [
         },
         module: {
             rules: [
-                webpack_configurator.configureBabelRule(webpack_configurator.babel_options_ie11),
+                webpack_configurator.configureBabelRule(),
                 webpack_configurator.rule_scss_loader,
             ],
         },
