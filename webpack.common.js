04d4ec3c50ab2b0a10040689d518dc88afeb5302
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-03-29 19:40:19 +0200

    request #20149: Use ESBuild to transpile and minimize the JS/TS and CSS code
    Change-Id: I4377a2ad91783f52de63e952c623cad23ad20265

--- webpack.common.js
+++ webpack.common.js
@@ -46,7 +46,6 @@ module.exports = [
         },
         module: {
             rules: [
-                webpack_configurator.configureBabelRule(),
                 webpack_configurator.rule_easygettext_loader,
                 webpack_configurator.rule_vue_loader,
                 webpack_configurator.rule_scss_loader,
