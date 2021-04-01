f7a801257aa512afb7de9088589626a72abfdf58
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-04-01 17:52:37 +0200

    Stop building the condensed stylesheets in the agiledashboard plugin
    Part of request #20573: Stop building condensed stylesheets
    
    Change-Id: I647ce82f638bebbe223070c2c381f4440b0518d4

--- plugins/agiledashboard/include/AgileDashboard/AgileDashboardLegacyController.php
+++ plugins/agiledashboard/include/AgileDashboard/AgileDashboardLegacyController.php
@@ -28,7 +28,7 @@ use Feedback;
 use HTTPRequest;
 use Tuleap\AgileDashboard\Kanban\KanbanURL;
 use Tuleap\Layout\BaseLayout;
-use Tuleap\Layout\CssAsset;
+use Tuleap\Layout\CssAssetWithoutDensityVariants;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\Request\DispatchableWithRequest;
 use Tuleap\Request\ForbiddenException;
@@ -64,7 +64,7 @@ class AgileDashboardLegacyController implements DispatchableWithRequest
 
         if (KanbanURL::isKanbanURL($request)) {
             $layout->addCssAsset(
-                new CssAsset(
+                new CssAssetWithoutDensityVariants(
                     new IncludeAssets(
                         __DIR__ . '/../../../../src/www/assets/agiledashboard',
                         '/assets/agiledashboard'
