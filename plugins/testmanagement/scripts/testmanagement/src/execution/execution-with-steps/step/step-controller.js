Enalean/tuleap

commit 193af416ea977bf00434f548f294becd49bd8757

request #16686 Rename tlp.dropdown to createDropdown
No functional change expected.

Change-Id: Iff9c4e22ec1e0a8b4d853c84839faf7d8f0a27eb

plugins/testmanagement/scripts/testmanagement/src/execution/execution-with-steps/step/step-controller.js
@@ -4,7 +4,7 @@ import {
     BLOCKED_STATUS,
     NOT_RUN_STATUS,
 } from "../../execution-constants.js";
-import { dropdown } from "tlp";
+import { createDropdown } from "tlp";
 import { setError, resetError } from "../../../feedback-state.js";
 import { updateStatusWithStepResults, updateStepResults } from "./execution-with-steps-updater.js";
 
@@ -43,7 +43,7 @@ export default function controller($element, gettextCatalog, ExecutionRestServic
         const $trigger = $element.find(".steps-step-action-dropdown-trigger");
         const $dropdown_menu = $element.find(".steps-step-action-dropdown");
 
-        self.dropdown = dropdown($trigger[0], {
+        self.createDropdown = createDropdown($trigger[0], {
             dropdown_menu: $dropdown_menu[0],
         });
     }

plugins/agiledashboard/scripts/kanban/src/app/add-to-dashboard/add-to-dashboard-controller.js
@@ -1,4 +1,4 @@
-import { dropdown } from "tlp";
+import { createDropdown } from "tlp";
 
 export default AddToDashboardController;
 
@@ -19,7 +19,7 @@ function AddToDashboardController($element, SharedPropertiesService) {
     function init() {
         const dashboard_dropdown = $element[0].querySelector("#dashboard-dropdown-button");
         if (dashboard_dropdown) {
-            dropdown(dashboard_dropdown);
+            createDropdown(dashboard_dropdown);
         }
     }
 

plugins/agiledashboard/scripts/kanban/src/app/wip-popover/wip-popover-directive.js
@@ -1,6 +1,6 @@
 import "./wip-popover.tpl.html";
 import angular from "angular";
-import { dropdown as createDropdown } from "tlp";
+import { createDropdown } from "tlp";
 
 export default WipPopover;
 

plugins/agiledashboard/scripts/planning-v2/src/app/open-tlp-dropdown-directive.js
@@ -17,13 +17,13 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { dropdown } from "tlp";
+import { createDropdown } from "tlp";
 
 export default function OpenTlpDropdown() {
     return {
         restrict: "A",
         link(scope, element) {
-            dropdown(element[0]);
+            createDropdown(element[0]);
         },
     };
 }

plugins/agiledashboard/scripts/scrum-header.js
@@ -17,7 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { dropdown } from "tlp";
+import { createDropdown } from "tlp";
 
 document.addEventListener("DOMContentLoaded", () => {
     const menu = document.getElementById("agiledashboard-scrum-milestone-menu");
@@ -25,5 +25,5 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-    dropdown(menu);
+    createDropdown(menu);
 });

plugins/document/scripts/document/components/Folder/DropDown/DropDownButton.test.js
@@ -28,7 +28,7 @@ jest.mock("tlp");
 describe("DropDownButton", () => {
     let dropdown_factory;
     beforeEach(() => {
-        jest.spyOn(tlp, "dropdown");
+        jest.spyOn(tlp, "createDropdown");
         dropdown_factory = (props = {}) => {
             return shallowMount(DropDownButton, {
                 localVue,

plugins/document/scripts/document/components/Folder/DropDown/DropDownButton.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script>
-import { dropdown as createDropdown } from "tlp";
+import { createDropdown } from "tlp";
 import EventBus from "../../../helpers/event-bus.js";
 
 export default {

plugins/git/scripts/repository/branch-tag-selector/src/components/App.vue
@@ -58,7 +58,7 @@
     </div>
 </template>
 <script>
-import { dropdown as createDropdown } from "tlp";
+import { createDropdown } from "tlp";
 import BranchesSection from "./BranchesSection.vue";
 import TagsSection from "./TagsSection.vue";
 

commit 74e9646de48a1867df969f133095bf8e2a51c4b6

request #20576 JavaScript error while opening the dropdown of steps
Bug introduced by 193af416ea977bf00434f548f294becd49bd8757

While opening the dropdown to select a status for a test step,
we have the following error in the browser console:

"TypeError: self.dropdown is undefined openDropdown step-controller.js:34"

Change-Id: I7b51b219677160eadb6a3067833d9f0350fee8bf

plugins/testmanagement/scripts/testmanagement/src/execution/execution-with-steps/step/step-controller.js
@@ -45,7 +45,7 @@ export default function controller($sce, $element, gettextCatalog, ExecutionRest
         const $trigger = $element.find(".steps-step-action-dropdown-trigger");
         const $dropdown_menu = $element.find(".steps-step-action-dropdown");
 
-        self.createDropdown = createDropdown($trigger[0], {
+        self.dropdown = createDropdown($trigger[0], {
             dropdown_menu: $dropdown_menu[0],
         });
     }