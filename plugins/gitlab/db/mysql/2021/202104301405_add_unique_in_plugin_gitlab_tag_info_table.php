114f5dac270b498cf4657f5fcde11894bceb133a
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2021-04-30 14:16:53 +0200

    Add missing unique constraint in tag table
    This is part of request #20918 GitLab tags that contains a reference to Tuleap should be backlinked
    
    Change-Id: I62a9b332b9aafe7553255a8c5e7014f79da1a35e

---
+++ plugins/gitlab/db/mysql/2021/202104301405_add_unique_in_plugin_gitlab_tag_info_table.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see http://www.gnu.org/licenses/.
+ */
+
+declare(strict_types=1);
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202104301405_add_unique_in_plugin_gitlab_tag_info_table extends ForgeUpgrade_Bucket
+{
+    public function description(): string
+    {
+        return 'Add unique constraint in the table plugin_gitlab_tag_info';
+    }
+
+    public function preUp(): void
+    {
+        $this->db = $this->getApi('ForgeUpgrade_Bucket_Db');
+    }
+
+    public function up(): void
+    {
+        $sql    = "ALTER TABLE plugin_gitlab_tag_info DROP INDEX idx_tag, ADD UNIQUE(repository_id, tag_name(255))";
+        $result = $this->db->dbh->exec($sql);
+
+        if ($result === false) {
+            throw new ForgeUpgrade_Bucket_Exception_UpgradeNotComplete('Could not alter plugin_gitlab_tag_info table');
+        }
+    }
+}
