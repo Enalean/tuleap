897dad51bc187656686bddfa933eae77efcfe79e
Manuel VACELET <manuel.vacelet@enalean.com>
2021-06-01 10:59:01 +0200

    Purge tools/Configuration
    This is no longer needed.
    
    Part of request #21741 Simplify Distributed Tuleap dev stack
    
    Change-Id: I6a9fde20fb9aea100aee2d1c609be879148941f8

--- tools/distlp/reverse-proxy/run.php
+++ tools/distlp/reverse-proxy/run.php
@@ -19,10 +19,82 @@
  *
  */
 
-require_once __DIR__ . '/../../Configuration/vendor/autoload.php';
 require_once __DIR__ . '/../../../src/vendor/autoload.php';
 
-$setup = new \Tuleap\Configuration\Setup\DistributedSVN();
-$setup->setTuleapBaseDir('/tuleap');
-$setup->setTuleapConfDir('/data/etc/tuleap');
-$setup->reverseProxy();
+use Psr\Log\LoggerInterface;
+use TuleapCfg\Command\SiteDeploy\Nginx\NginxCommon;
+
+class ReverseProxy // phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
+{
+    private LoggerInterface $logger;
+    private string $tuleap_base_dir;
+    private string $nginx_base_dir;
+    private string $server_name;
+    /**
+     * @var NginxCommon
+     */
+    private $common;
+
+    public function __construct(LoggerInterface $logger, string $tuleap_base_dir, string $nginx_base_dir, string $server_name)
+    {
+        $this->logger          = $logger;
+        $this->tuleap_base_dir = $tuleap_base_dir;
+        $this->nginx_base_dir  = $nginx_base_dir;
+        $this->server_name     = $server_name;
+        $this->common          = new NginxCommon($logger, $tuleap_base_dir, $nginx_base_dir);
+    }
+
+    public function configure()
+    {
+        $this->logger->info("Configure Nginx as front Reverse Proxy");
+        if (is_file($this->nginx_base_dir . '/nginx.conf.orig')) {
+            $this->logger->warning($this->nginx_base_dir . '/nginx.conf.orig already exists, skip configuration');
+            return;
+        }
+        $this->backupOriginalFile($this->nginx_base_dir . '/nginx.conf');
+        copy($this->tuleap_base_dir . '/tools/distlp/reverse-proxy/nginx.conf', $this->nginx_base_dir . '/nginx.conf');
+
+        copy($this->tuleap_base_dir . '/tools/distlp/reverse-proxy/proxy-vars.conf', $this->nginx_base_dir . '/proxy-vars.conf');
+        copy($this->tuleap_base_dir . '/tools/distlp/reverse-proxy/tcp_ssh.conf', $this->nginx_base_dir . '/conf.d/tcp_ssh.conf');
+        $this->deployHTTPConfFromTemplate();
+
+        $this->logger->info("Generate SSL certificate");
+        $this->common->generateSSLCertificate($this->server_name, '/etc/pki/tls/certs/localhost.cert.pem', '/etc/pki/tls/private/localhost.key.pem');
+        $this->logger->info("Done");
+    }
+
+    private function deployHTTPConfFromTemplate()
+    {
+        $template = file_get_contents($this->tuleap_base_dir . '/tools/distlp/reverse-proxy/http_tuleap.conf');
+        $searches = [
+            '%sys_default_domain%',
+        ];
+        $replaces = [
+            $this->server_name,
+        ];
+
+        $conf = str_replace($searches, $replaces, $template);
+        file_put_contents($this->nginx_base_dir . '/conf.d/http_tuleap.conf', $conf);
+    }
+
+    private function backupOriginalFile($file)
+    {
+        if (! file_exists($file . '.orig')) {
+            copy($file, $file . '.orig');
+        }
+    }
+}
+
+\ForgeConfig::loadFromFile('/data/etc/tuleap/conf/local.inc');
+
+$logger = new \Monolog\Logger('reverse-proxy');
+$logger->pushHandler(new \Monolog\Handler\StreamHandler(STDOUT));
+
+$reverse_proxy = new ReverseProxy(
+    $logger,
+    '/tuleap',
+    '/etc/nginx',
+    \ForgeConfig::get('sys_default_domain'),
+);
+
+$reverse_proxy->configure();
