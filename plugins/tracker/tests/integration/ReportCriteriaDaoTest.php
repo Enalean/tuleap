b9d54bf698613a31e3691170063807a14e11dc02
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2021-06-15 09:49:38 +0200

    List criteria report are not duplicated when there is more a list in criterion
    part of request #21773 On project creation values filters are not duplicated
    
    How to reproduce:
    Have a project with tracker report,
    Bind several static list values to some data.
    
    Use this project as template for creating a new project
     => tracker criterion for static list values are duplicated
    
    Change-Id: I7eedda573aeca9c83bc7a12dc2be8fc5f94b8473

--- plugins/tracker/tests/integration/ReportCriteriaDaoTest.php
+++ plugins/tracker/tests/integration/ReportCriteriaDaoTest.php
@@ -22,6 +22,7 @@
 namespace Tuleap\Tracker\Report\dao;
 
 use ParagonIE\EasyDB\EasyDB;
+use Tracker_FormElement_Field_List_Bind_StaticValue_None;
 use Tuleap\DB\DBFactory;
 
 final class ReportCriteriaDaoTest extends \Tuleap\Test\PHPUnit\TestCase
@@ -37,11 +38,13 @@ final class ReportCriteriaDaoTest extends \Tuleap\Test\PHPUnit\TestCase
 
     public function testItDuplicateListFieldValues(): void
     {
-        $from_report_id = 1;
-        $to_report_id   = 2;
-        $from_field_id  = 100;
-        $to_field_id    = 200;
-        $rank           = 1;
+        $from_report_id      = 10;
+        $to_report_id        = 20;
+        $from_field_id       = 100;
+        $from_other_field_id = 101;
+        $to_field_id         = 200;
+        $to_other_field_id   = 201;
+        $rank                = 1;
 
         $field_mapping[] = [
             'values' => [
@@ -51,20 +54,33 @@ final class ReportCriteriaDaoTest extends \Tuleap\Test\PHPUnit\TestCase
             'from' => $from_field_id,
             'to' => $to_field_id
         ];
+        $field_mapping[] = [
+            'values' => [
+                201 => Tracker_FormElement_Field_List_Bind_StaticValue_None::VALUE_ID
+            ],
+            'from' => $from_other_field_id,
+            'to' => $to_other_field_id
+        ];
 
         $criteria_id = $this->create($from_report_id, $from_field_id, $rank, 0);
         $this->insertListValue($criteria_id, 200);
         $this->insertListValue($criteria_id, 300);
+        $other_criteria_id = $this->create($from_report_id, $from_other_field_id, $rank, 0);
+        $this->insertListValue($other_criteria_id, 201);
 
-        $new_criteria_id = $this->dao->duplicate($from_report_id, $to_report_id, $field_mapping);
+        $this->dao->duplicate($from_report_id, $to_report_id, $field_mapping);
 
-        $duplicated_data = $this->getNewReportCriteriaListValues($new_criteria_id);
-
-        $expected = [
+        $duplicated_data = $this->getNewReportCriteriaListValues($to_field_id);
+        $expected        = [
             ['value' => 400],
             ['value' => 600],
         ];
+        $this->assertEquals($expected, $duplicated_data);
 
+        $duplicated_data = $this->getNewReportCriteriaListValues($to_other_field_id);
+        $expected        = [
+            ['value' => Tracker_FormElement_Field_List_Bind_StaticValue_None::VALUE_ID],
+        ];
         $this->assertEquals($expected, $duplicated_data);
     }
 
@@ -86,8 +102,11 @@ final class ReportCriteriaDaoTest extends \Tuleap\Test\PHPUnit\TestCase
         ]);
     }
 
-    private function getNewReportCriteriaListValues(int $new_criteria_id): array
+    private function getNewReportCriteriaListValues(int $field_id): array
     {
-        return $this->db->run('SELECT value FROM tracker_report_criteria_list_value WHERE criteria_id = ?', $new_criteria_id);
+        $sql = 'SELECT value FROM tracker_report_criteria AS criteria
+                    INNER JOIN tracker_report_criteria_list_value AS list_values ON criteria.id = list_values.criteria_id WHERE field_id = ?';
+
+        return $this->db->run($sql, $field_id);
     }
 }
