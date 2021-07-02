80fd4bfd828ca468d286ec08ea6dae70158a3fdd
Benjamin Bouillot <benjamin.bouillot@enalean.com>
2021-07-02 15:57:10 +0200

    request #22199 Error is thrown when user has not access to a team in Program service
    How to reproduce:
     - Have a Program with 2 teams
     - Have a user that can not access to 1 team
     - In a feature, have 2 User Stories (one in each team)
     - With this user, go to Program service
    
    Expected results:
     - Before an error was displayed with a log to explain the problem
     ➡️  Now there are no errors in the UI
     ➡️  In a feature, there is only one US (only one where user has access to the project)
    
    Change-Id: I1c199c47be55674a1f6347cdfd57db5e73867fb2

--- plugins/program_management/tests/unit/Adapter/Program/Feature/Links/UserStoryLinkedToFeatureCheckerTest.php
+++ plugins/program_management/tests/unit/Adapter/Program/Feature/Links/UserStoryLinkedToFeatureCheckerTest.php
@@ -22,67 +22,40 @@ declare(strict_types=1);
 
 namespace Tuleap\ProgramManagement\Adapter\Program\Feature\Links;
 
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Tuleap\ProgramManagement\Domain\Program\Backlog\Feature\Content\Links\VerifyLinkedUserStoryIsNotPlanned;
 use Tuleap\ProgramManagement\Domain\Program\Backlog\Feature\FeatureIdentifier;
-use Tuleap\ProgramManagement\Domain\Program\BuildPlanning;
 use Tuleap\ProgramManagement\Domain\Program\ProgramIdentifier;
-use Tuleap\ProgramManagement\Domain\Project;
+use Tuleap\ProgramManagement\Stub\BuildPlanningStub;
 use Tuleap\ProgramManagement\Stub\BuildProgramStub;
 use Tuleap\ProgramManagement\Stub\VerifyIsVisibleFeatureStub;
-use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
 
 final class UserStoryLinkedToFeatureCheckerTest extends \Tuleap\Test\PHPUnit\TestCase
 {
-    use MockeryPHPUnitIntegration;
-
     /**
-     * @var \Mockery\LegacyMockInterface|\Mockery\MockInterface|ArtifactsLinkedToParentDao
+     * @var \PHPUnit\Framework\MockObject\MockObject|ArtifactsLinkedToParentDao
      */
     private $feature_dao;
     /**
-     * @var \Mockery\LegacyMockInterface|\Mockery\MockInterface|\Tracker_ArtifactFactory
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Tracker_ArtifactFactory
      */
     private $artifact_factory;
-    /**
-     * @var VerifyLinkedUserStoryIsNotPlanned
-     */
-    private $checker;
-    /**
-     * @var \PFUser
-     */
-    private $user;
+    private \PFUser $user;
+    private BuildPlanningStub $planning_builder;
 
     protected function setUp(): void
     {
-        $planning_builder = new class implements BuildPlanning
-        {
-            public function getRootPlanning(\PFUser $user, int $project_id): \Planning
-            {
-                $planning = new \Planning(1, "Root planning", $project_id, '', '');
-                $planning->setPlanningTracker(TrackerTestBuilder::aTracker()->withId(20)->build());
-                return $planning;
-            }
-
-            public function getProjectFromPlanning(\Planning $root_planning): Project
-            {
-                return new \Tuleap\ProgramManagement\Domain\Project(101, 'my-project', "My project", '/my-project');
-            }
-        };
-
-        $this->feature_dao      = \Mockery::mock(ArtifactsLinkedToParentDao::class);
-        $this->artifact_factory = \Mockery::mock(\Tracker_ArtifactFactory::class);
+        $this->planning_builder = BuildPlanningStub::withValidRootPlanning();
+        $this->feature_dao      = $this->createMock(ArtifactsLinkedToParentDao::class);
+        $this->artifact_factory = $this->createMock(\Tracker_ArtifactFactory::class);
         $this->user             = new \PFUser(['language_id' => 'en']);
-        $this->checker          = new UserStoryLinkedToFeatureChecker($this->feature_dao, $planning_builder, $this->artifact_factory);
     }
 
     public function testHasNotAPlannedUserStoryIfNoUserStoryIsLinked(): void
     {
         $this->feature_dao
-            ->shouldReceive("getPlannedUserStory")
-            ->once()
-            ->andReturn([]);
-        self::assertFalse($this->checker->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
+            ->expects(self::once())
+            ->method('getPlannedUserStory')
+            ->willReturn([]);
+        self::assertFalse($this->getChecker()->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
     }
 
     public function testHasAPlannedUserStory(): void
@@ -91,23 +64,18 @@ final class UserStoryLinkedToFeatureCheckerTest extends \Tuleap\Test\PHPUnit\Tes
         $user_story_236 = ['project_id' => 25, 'user_story_id' => 236];
 
         $this->feature_dao
-            ->shouldReceive("getPlannedUserStory")
-            ->once()
+            ->expects(self::once())
+            ->method('getPlannedUserStory')
             ->with(101)
-            ->andReturn([$user_story_666, $user_story_236]);
+            ->willReturn([$user_story_666, $user_story_236]);
 
         $this->feature_dao
-            ->shouldReceive("isLinkedToASprintInMirroredProgramIncrement")
-            ->once()
-            ->with(666, 20, 666)
-            ->andReturn(false);
-        $this->feature_dao
-            ->shouldReceive("isLinkedToASprintInMirroredProgramIncrement")
-            ->once()
-            ->with(236, 20, 25)
-            ->andReturn(true);
+            ->expects(self::exactly(2))
+            ->method('isLinkedToASprintInMirroredProgramIncrement')
+            ->withConsecutive([666, 20, 666], [236, 20, 25])
+            ->willReturnOnConsecutiveCalls(false, true);
 
-        self::assertTrue($this->checker->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
+        self::assertTrue($this->getChecker()->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
     }
 
     public function testHasNotAPlannedUserStoryIfUserStoriesAreLinkedButNotPlanned(): void
@@ -115,68 +83,87 @@ final class UserStoryLinkedToFeatureCheckerTest extends \Tuleap\Test\PHPUnit\Tes
         $user_story_666 = ['project_id' => 666, 'user_story_id' => 666];
 
         $this->feature_dao
-            ->shouldReceive("getPlannedUserStory")
-            ->once()
+            ->expects(self::once())
+            ->method('getPlannedUserStory')
             ->with(101)
-            ->andReturn([$user_story_666]);
+            ->willReturn([$user_story_666]);
 
         $this->feature_dao
-            ->shouldReceive("isLinkedToASprintInMirroredProgramIncrement")
-            ->once()
+            ->expects(self::once())
+            ->method('isLinkedToASprintInMirroredProgramIncrement')
             ->with(666, 20, 666)
-            ->andReturn(false);
+            ->willReturn(false);
 
-        self::assertFalse($this->checker->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
+        self::assertFalse($this->getChecker()->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
     }
 
     public function testHasNotALinkedUserStoryToFeature(): void
     {
         $this->feature_dao
-            ->shouldReceive("getChildrenOfFeatureInTeamProjects")
-            ->once()
-            ->andReturn([]);
+            ->expects(self::once())
+            ->method('getChildrenOfFeatureInTeamProjects')
+            ->willReturn([]);
 
-        self::assertFalse($this->checker->hasStoryLinked($this->user, $this->buildFeature(101)));
+        self::assertFalse($this->getChecker()->hasStoryLinked($this->user, $this->buildFeature(101)));
     }
 
     public function testHasNotALinkedUserStoryToFeatureThatUserCanSee(): void
     {
         $user_story = ['children_id' => 666];
         $this->feature_dao
-            ->shouldReceive("getChildrenOfFeatureInTeamProjects")
-            ->once()
+            ->expects(self::once())
+            ->method('getChildrenOfFeatureInTeamProjects')
             ->with(101)
-            ->andReturn([$user_story]);
+            ->willReturn([$user_story]);
 
         $this->artifact_factory
-            ->shouldReceive('getArtifactByIdUserCanView')
-            ->once()
+            ->expects(self::once())
+            ->method('getArtifactByIdUserCanView')
             ->with($this->user, 666)
-            ->andReturnNull();
+            ->willReturn(null);
 
-        self::assertFalse($this->checker->hasStoryLinked($this->user, $this->buildFeature(101)));
+        self::assertFalse($this->getChecker()->hasStoryLinked($this->user, $this->buildFeature(101)));
     }
 
     public function testHasALinkedUserStoryToFeature(): void
     {
         $user_story = ['children_id' => 236];
         $this->feature_dao
-            ->shouldReceive("getChildrenOfFeatureInTeamProjects")
-            ->once()
+            ->expects(self::once())
+            ->method('getChildrenOfFeatureInTeamProjects')
             ->with(101)
-            ->andReturn([$user_story]);
+            ->willReturn([$user_story]);
 
         $this->artifact_factory
-            ->shouldReceive('getArtifactByIdUserCanView')
-            ->once()
+            ->expects(self::once())
+            ->method('getArtifactByIdUserCanView')
             ->with($this->user, 236)
-            ->andReturn(\Mockery::mock(\Artifact::class));
+            ->willReturn($this->createMock(\Artifact::class));
 
-        self::assertTrue($this->checker->hasStoryLinked($this->user, $this->buildFeature(101)));
+        self::assertTrue($this->getChecker()->hasStoryLinked($this->user, $this->buildFeature(101)));
+    }
+
+    public function testReturnFalseWhenUserHasAccessedToTheUserStory(): void
+    {
+        $user_story = ['project_id' => 101, 'user_story_id' => 666];
+        $this->feature_dao
+            ->expects(self::once())
+            ->method('getPlannedUserStory')
+            ->with(101)
+            ->willReturn([$user_story]);
+
+        $this->planning_builder = BuildPlanningStub::withoutRootValid();
+
+        self::assertFalse($this->getChecker()->isLinkedToAtLeastOnePlannedUserStory($this->user, $this->buildFeature(101)));
     }
 
     private function buildFeature(int $feature_id): FeatureIdentifier
     {
         return FeatureIdentifier::fromId(VerifyIsVisibleFeatureStub::buildVisibleFeature(), $feature_id, $this->user, ProgramIdentifier::fromId(BuildProgramStub::stubValidProgram(), 110, $this->user));
+    }
+
+    private function getChecker(): UserStoryLinkedToFeatureChecker
+    {
+        return new UserStoryLinkedToFeatureChecker($this->feature_dao, $this->planning_builder, $this->artifact_factory);
     }
 }
---
+++ plugins/program_management/tests/unit/Stub/BuildPlanningStub.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProgramManagement\Stub;
+
+use Tuleap\ProgramManagement\Domain\Program\BuildPlanning;
+use Tuleap\ProgramManagement\Domain\Program\PlanningConfiguration\TopPlanningNotFoundInProjectException;
+use Tuleap\ProgramManagement\Domain\Project;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
+
+class BuildPlanningStub implements BuildPlanning
+{
+    private bool $valid_root_planning;
+
+    public function __construct(bool $valid_root_planning)
+    {
+        $this->valid_root_planning = $valid_root_planning;
+    }
+
+    public function getRootPlanning(\PFUser $user, int $project_id): \Planning
+    {
+        if ($this->valid_root_planning) {
+            $planning = new \Planning(1, 'Root planning', $project_id, '', '');
+            $planning->setPlanningTracker(TrackerTestBuilder::aTracker()->withId(20)->build());
+            return $planning;
+        }
+
+        throw new TopPlanningNotFoundInProjectException($project_id);
+    }
+
+    public function getProjectFromPlanning(\Planning $root_planning): Project
+    {
+        return new Project(101, 'my-project', 'My project', '/project');
+    }
+
+    public static function withValidRootPlanning(): self
+    {
+        return new self(true);
+    }
+
+    public static function withoutRootValid(): self
+    {
+        return new self(false);
+    }
+}
