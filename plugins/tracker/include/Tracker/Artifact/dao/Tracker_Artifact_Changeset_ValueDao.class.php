Enalean/tuleap

commit 8589c64e3cbd84468742e560c727ad2976b8536f

request #22229: Migrate Tracker_Artifact_Changeset_ValueDao to \Tuleap\DB\DataAccessObject
No functional changes expected.

Change-Id: Ie347366a3cf563b1e5f0875ecd5c9c196bb839d8

plugins/tracker/include/Tracker/Artifact/dao/Tracker_Artifact_Changeset_ValueDao.class.php
@@ -19,93 +19,81 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Tracker_Artifact_Changeset_ValueDao extends DataAccessObject
+class Tracker_Artifact_Changeset_ValueDao extends \Tuleap\DB\DataAccessObject
 {
-    public function __construct()
+    /**
+     * @psalm-return array{id: int, field_id: int, has_changed:0|1}[]
+     */
+    public function searchById($id): array
     {
-        parent::__construct();
-        $this->table_name = 'tracker_changeset_value';
+        return $this->getDB()->run(
+            'SELECT id, field_id, has_changed FROM tracker_changeset_value WHERE changeset_id = ?',
+            $id
+        );
     }
 
-    public function searchById($id)
+    /**
+     * @psalm-return array{id: int, has_changed:0|1}|null
+     */
+    public function searchByFieldId($changeset_id, $field_id): ?array
     {
-        $id  = $this->da->escapeInt($id);
-        $sql = "SELECT * FROM $this->table_name
-                WHERE changeset_id = $id";
-        return $this->retrieve($sql);
+        return $this->getDB()->row(
+            'SELECT id, has_changed FROM tracker_changeset_value WHERE changeset_id = ? AND field_id = ?',
+            $changeset_id,
+            $field_id
+        );
     }
 
-    public function searchByFieldId($changeset_id, $field_id)
+    /**
+     * @psalm-return array<int, array{id: int, changeset_id: int, field_id: int, has_changed:0|1}>
+     */
+    public function searchByArtifactId($artifact_id): array
     {
-        $changeset_id = $this->da->escapeInt($changeset_id);
-        $field_id     = $this->da->escapeInt($field_id);
-        $sql          = "SELECT * FROM $this->table_name
-                WHERE changeset_id = $changeset_id
-                    AND field_id = $field_id";
-        return $this->retrieve($sql);
-    }
-
-    public function searchByArtifactId($artifact_id)
-    {
-        $artifact_id = $this->da->escapeInt($artifact_id);
-        $sql         = "SELECT changeset_value.*
+        return $this->getDB()->safeQuery(
+            'SELECT changeset_value.changeset_id, changeset_value.id, changeset_value.changeset_id, changeset_value.field_id, changeset_value.has_changed
                 FROM tracker_changeset_value AS changeset_value
                 JOIN tracker_changeset AS changeset ON (changeset.id = changeset_value.changeset_id)
-                WHERE changeset.artifact_id = $artifact_id";
-        $results     = [];
-        foreach ($this->retrieve($sql) as $row) {
-            $results[$row['changeset_id']][] = $row;
-        }
-        return $results;
+                WHERE changeset.artifact_id = ?',
+            [$artifact_id],
+            \PDO::FETCH_UNIQUE | \PDO::FETCH_ASSOC
+        );
     }
 
-    public function save($changeset_id, $field_id, $has_changed)
+    public function save($changeset_id, $field_id, $has_changed): int
     {
-        $changeset_id = $this->da->escapeInt($changeset_id);
-        $field_id     = $this->da->escapeInt($field_id);
-        $has_changed  = $has_changed ? 1 : 0;
-        $sql          = "INSERT INTO $this->table_name(changeset_id, field_id, has_changed)
-                VALUES ($changeset_id, $field_id, $has_changed)";
-        return $this->updateAndGetLastId($sql);
+        $this->getDB()->run(
+            'INSERT INTO tracker_changeset_value(changeset_id, field_id, has_changed) VALUES (?,?,?)',
+            $changeset_id,
+            $field_id,
+            $has_changed ? 1 : 0
+        );
+        return (int) $this->getDB()->lastInsertId();
     }
 
+    /**
+     * @psalm-return list<int>
+     */
     public function createFromLastChangesetByTrackerId($tracker_id, $field_id, $has_changed)
     {
-        $tracker_id = $this->da->escapeInt($tracker_id);
-        $field_id   = $this->da->escapeInt($field_id);
-
-        $sql = "INSERT INTO tracker_changeset_value(changeset_id, field_id, has_changed)
+        $this->getDB()->run(
+            'INSERT INTO tracker_changeset_value(changeset_id, field_id, has_changed)
                 SELECT A.last_changeset_id as changeset_id, $field_id, 1
                 FROM tracker_artifact AS A
-                WHERE A.tracker_id = $tracker_id";
-        $this->update($sql);
-        $sql = "SELECT CV.id as cv
+                WHERE A.tracker_id = ?',
+            $tracker_id
+        );
+
+        return $this->getDB()->column(
+            'SELECT CV.id as cv
                 FROM tracker_changeset_value AS CV
-                    INNER JOIN tracker_artifact AS A ON (CV.changeset_id = A.last_changeset_id)
-                WHERE A.tracker_id = $tracker_id
-                      AND CV.field_id = $field_id
-                      AND has_changed = 1";
-
-        $result = $this->retrieve($sql);
-
-        if (! $result) {
-            return false;
-        }
-
-        $changesetValueIds = [];
-        foreach ($result as $row) {
-            $changesetValueIds[] = $row['cv'];
-        }
-
-        return $changesetValueIds;
+                INNER JOIN tracker_artifact AS A ON (CV.changeset_id = A.last_changeset_id)
+                WHERE A.tracker_id = ? AND CV.field_id = ? AND has_changed = 1',
+            [$tracker_id, $field_id]
+        );
     }
 
-    public function delete($changeset_id)
+    public function delete($changeset_id): void
     {
-        $changeset_id = $this->da->escapeInt($changeset_id);
-        $sql          = "DELETE
-                FROM $this->table_name
-                WHERE changeset_id = $changeset_id";
-        return $this->update($sql);
+        $this->getDB()->run('DELETE FROM tracker_changeset_value WHERE changeset_id = ?', $changeset_id);
     }
 }

plugins/tracker/include/Tracker/Artifact/Tracker_Artifact_Changeset.class.php
@@ -90,10 +90,9 @@ class Tracker_Artifact_Changeset extends Tracker_Artifact_Followup_Item
 
     private function getChangesetValueFromDB(Tracker_FormElement_Field $field)
     {
-        $dar = $this->getValueDao()->searchByFieldId($this->id, $field->getId());
-        if ($dar && count($dar)) {
-            $row = $dar->getRow();
-            return $field->getChangesetValue($this, $row['id'], $row['has_changed']);
+        $row = $this->getValueDao()->searchByFieldId($this->id, $field->getId());
+        if ($row !== null) {
+            return $field->getChangesetValue($this, $row['id'], (bool) $row['has_changed']);
         }
         return null;
     }
@@ -157,7 +156,7 @@ class Tracker_Artifact_Changeset extends Tracker_Artifact_Followup_Item
         $factory      = $this->getFormElementFactory();
         foreach ($this->getValueDao()->searchById($this->id) as $row) {
             if ($field = $factory->getFieldById($row['field_id'])) {
-                $this->values[$field->getId()] = $field->getChangesetValue($this, $row['id'], $row['has_changed']);
+                $this->values[$field->getId()] = $field->getChangesetValue($this, $row['id'], (bool) $row['has_changed']);
             }
         }
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_PermissionsOnArtifact.php
@@ -73,8 +73,11 @@ class Tracker_FormElement_Field_PermissionsOnArtifact extends Tracker_FormElemen
         $values   = [];
         $artifact = Tracker_ArtifactFactory::instance()->getArtifactById($artifact_id);
         if ($artifact->useArtifactPermissions()) {
-            $dao                = new Tracker_Artifact_Changeset_ValueDao();
-            $row                = $dao->searchByFieldId($changeset_id, $this->id)->getRow();
+            $dao = new Tracker_Artifact_Changeset_ValueDao();
+            $row = $dao->searchByFieldId($changeset_id, $this->id);
+            if ($row === null) {
+                return '';
+            }
             $changeset_value_id = $row['id'];
 
             foreach ($this->getValueDao()->searchByChangesetValueId($changeset_value_id) as $value) {

plugins/tracker/tests/unit/Tracker_Artifact_ChangesetTest.php
@@ -187,8 +187,7 @@ final class Tracker_Artifact_ChangesetTest extends \Tuleap\Test\PHPUnit\TestCase
         $field = \Mockery::spy(\Tracker_FormElement_Field_Date::class);
         $value = \Mockery::spy(\Tracker_Artifact_ChangesetValue_Date::class);
 
-        $dar = TestHelper::arrayToDar(['changeset_id' => 1, 'field_id' => 2, 'id' => 3, 'has_changed' => 0]);
-        $this->dao->shouldReceive('searchByFieldId')->once()->andReturns($dar);
+        $this->dao->shouldReceive('searchByFieldId')->once()->andReturns(['changeset_id' => 1, 'field_id' => 2, 'id' => 3, 'has_changed' => 0]);
 
         $field->shouldReceive('getId')->andReturns(2);
         $field->shouldReceive('getChangesetValue')->once()->andReturns($value);
@@ -217,11 +216,10 @@ final class Tracker_Artifact_ChangesetTest extends \Tuleap\Test\PHPUnit\TestCase
         $previous_changeset->shouldReceive('getValue')->never()->with($field2);
         $artifact->shouldReceive('getPreviousChangeset')->once()->with(66)->andReturns($previous_changeset);
 
-        $dar = TestHelper::arrayToDar(
+        $this->dao->shouldReceive('searchById')->once()->andReturns([
             ['changeset_id' => 66, 'field_id' => 1, 'id' => 11, 'has_changed' => 1],
             ['changeset_id' => 66, 'field_id' => 2, 'id' => 21, 'has_changed' => 0]
-        );
-        $this->dao->shouldReceive('searchById')->once()->andReturns($dar);
+        ]);
 
         $fact->shouldReceive('getFieldById')->with(1)->andReturns($field1);
         $fact->shouldReceive('getFieldById')->with(2)->andReturns($field2);
@@ -298,7 +296,7 @@ final class Tracker_Artifact_ChangesetTest extends \Tuleap\Test\PHPUnit\TestCase
         $changeset->shouldReceive('getValueDao')->andReturns($value_dao);
 
         $value_dao->shouldReceive('searchById')->with($changeset_id)->andReturns(
-            \TestHelper::arrayToDar(['id' => 1025, 'field_id' => 125], ['id' => 1026, 'field_id' => 126])
+            [['id' => 1025, 'field_id' => 125], ['id' => 1026, 'field_id' => 126]]
         );
 
         $formelement_factory = \Mockery::spy(\Tracker_FormElementFactory::class);

commit b96906ac7ba29d03856986c5b88ed5843901a9a0

PHP warnings when displaying an artifact
Issue introduced by 8589c64e3cbd84468742e560c727ad2976b8536f.

Part of request #22229: Migrate Tracker_Artifact_Changeset_ValueDao to \Tuleap\DB\DataAccessObject

Change-Id: I2e5a04f23f53b998068d4e6070d9dc46cdd923fa

plugins/tracker/include/Tracker/Artifact/dao/Tracker_Artifact_Changeset_ValueDao.class.php
@@ -45,7 +45,7 @@ class Tracker_Artifact_Changeset_ValueDao extends \Tuleap\DB\DataAccessObject
     }
 
     /**
-     * @psalm-return array<int, array{id: int, changeset_id: int, field_id: int, has_changed:0|1}>
+     * @psalm-return array<int, list<array{id: int, changeset_id: int, field_id: int, has_changed:0|1}>>
      */
     public function searchByArtifactId($artifact_id): array
     {
@@ -55,7 +55,7 @@ class Tracker_Artifact_Changeset_ValueDao extends \Tuleap\DB\DataAccessObject
                 JOIN tracker_changeset AS changeset ON (changeset.id = changeset_value.changeset_id)
                 WHERE changeset.artifact_id = ?',
             [$artifact_id],
-            \PDO::FETCH_UNIQUE | \PDO::FETCH_ASSOC
+            \PDO::FETCH_GROUP | \PDO::FETCH_ASSOC
         );
     }
 