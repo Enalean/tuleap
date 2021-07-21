Enalean/tuleap

commit 646be67d7d9fc1a89f1ed3c8fc38f783599337cd

Introduce Team Template
part of: story #22210: select SAFe templates at project creation

How to test:
- make sure you have already run `make generate-templates` and
  `make dev-clear-cache`
- make sure you have plugin program_management installed and enabled
- Create a new project
--> There is a category 'SAFe'

- Click on it
- Select `Essential SAFe - ART and Scrum Team`
- Finalize the project creation
--> It works

Change-Id: I79c9a2791e6bbada07b028ad315af55b2de59b35

plugins/program_management/include/program_managementPlugin.php
@@ -122,10 +122,12 @@ use Tuleap\ProgramManagement\ProgramService;
 use Tuleap\ProgramManagement\RedirectParameterInjector;
 use Tuleap\ProgramManagement\REST\ResourcesInjector;
 use Tuleap\ProgramManagement\Templates\ProgramTemplate;
+use Tuleap\ProgramManagement\Templates\TeamTemplate;
 use Tuleap\Project\Admin\PermissionsPerGroup\PermissionPerGroupPaneCollector;
 use Tuleap\Project\Admin\PermissionsPerGroup\PermissionPerGroupUGroupFormatter;
 use Tuleap\Project\Event\ProjectServiceBeforeActivation;
 use Tuleap\Project\ProjectAccessChecker;
+use Tuleap\Project\Registration\Template\Events\CollectAuthorizedXMLTemplateNamesEvent;
 use Tuleap\Project\Registration\Template\Events\CollectCategorisedExternalTemplatesEvent;
 use Tuleap\Project\RestrictedUserCanAccessProjectVerifier;
 use Tuleap\Project\Service\ServiceDisabledCollector;
@@ -225,6 +227,7 @@ final class program_managementPlugin extends Plugin
         $this->addHook(GetWhitelistedKeys::NAME);
         $this->addHook(DisplayCreatedProjectModalPresenter::NAME);
         $this->addHook(CollectCategorisedExternalTemplatesEvent::NAME);
+        $this->addHook(CollectAuthorizedXMLTemplateNamesEvent::NAME);
         $this->addHook(ServiceEnableForXmlImportRetriever::NAME);
         $this->addHook(\Tuleap\Glyph\GlyphLocationsCollector::NAME);
 
@@ -958,15 +961,23 @@ final class program_managementPlugin extends Plugin
 
     public function collectCategorisedExternalTemplatesEvent(CollectCategorisedExternalTemplatesEvent $event): void
     {
-        $event_manager = EventManager::instance();
+        $event_manager       = EventManager::instance();
+        $glyph_finder        = new GlyphFinder($event_manager);
+        $consistency_checker = new ConsistencyChecker(
+            new XMLFileContentRetriever(),
+            $event_manager,
+            new ServiceEnableForXmlImportRetriever(\PluginFactory::instance())
+        );
         $event->addCategorisedTemplate(
             new ProgramTemplate(
-                new GlyphFinder($event_manager),
-                new ConsistencyChecker(
-                    new XMLFileContentRetriever(),
-                    $event_manager,
-                    new ServiceEnableForXmlImportRetriever(\PluginFactory::instance())
-                )
+                $glyph_finder,
+                $consistency_checker
+            )
+        );
+        $event->addCategorisedTemplate(
+            new TeamTemplate(
+                $glyph_finder,
+                $consistency_checker
             )
         );
     }
@@ -977,5 +988,11 @@ final class program_managementPlugin extends Plugin
             'tuleap-program-management',
             new GlyphLocation(__DIR__ . '/../glyphs')
         );
+    }
+
+    public function collectAuthorizedXMLTemplateNamesEvent(CollectAuthorizedXMLTemplateNamesEvent $event): void
+    {
+        $event->addAuthorizedTemplateName(ProgramTemplate::NAME);
+        $event->addAuthorizedTemplateName(TeamTemplate::NAME);
     }
 }

src/common/Project/REST/v1/ProjectResource.class.php
@@ -71,7 +71,9 @@ use Tuleap\Project\Registration\ProjectRegistrationPermissionsChecker;
 use Tuleap\Project\Registration\ProjectRegistrationRESTChecker;
 use Tuleap\Project\Registration\ProjectRegistrationUserPermissionChecker;
 use Tuleap\Project\Registration\ProjectRegistrationBaseChecker;
+use Tuleap\Project\Registration\Template\Events\CollectAuthorizedXMLTemplateNamesEvent;
 use Tuleap\Project\Registration\Template\InvalidTemplateException;
+use Tuleap\Project\Registration\Template\InvalidXMLTemplateNameException;
 use Tuleap\Project\Registration\Template\TemplateFactory;
 use Tuleap\Project\REST\HeartbeatsRepresentation;
 use Tuleap\Project\REST\ProjectRepresentation;
@@ -269,6 +271,8 @@ class ProjectResource extends AuthenticatedResource
                 ),
                 $this->getBackendLogger()
             );
+
+            $this->checkXMLTemplateName($post_representation);
 
             $creation_data = $creation_data_post_project_builder->buildProjectCreationDataFromPOSTRepresentation(
                 $post_representation,
@@ -1339,5 +1343,22 @@ class ProjectResource extends AuthenticatedResource
         }
 
         return $project_field_representations;
+    }
+
+    /**
+     * @throws InvalidTemplateException
+     */
+    protected function checkXMLTemplateName(ProjectPostRepresentation $post_representation): void
+    {
+        if (! $post_representation->xml_template_name) {
+            return;
+        }
+
+        $event = new CollectAuthorizedXMLTemplateNamesEvent();
+        $this->event_manager->dispatch($event);
+
+        if (! array_search($post_representation->xml_template_name, $event->getAuthorizedTemplateNames())) {
+            throw new InvalidXMLTemplateNameException();
+        }
     }
 }

src/common/Project/Registration/Template/Events/CollectAuthorizedXMLTemplateNamesEvent.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Project\Registration\Template\Events;
+
+use Tuleap\Event\Dispatchable;
+
+class CollectAuthorizedXMLTemplateNamesEvent implements Dispatchable
+{
+    public const NAME = 'collectAuthorizedXMLTemplateNamesEvent';
+
+    /**
+     * @var string[]
+     */
+    private array $authorized_template_names = [
+        'agile_alm',
+        'scrum',
+        'kanban',
+        'issues',
+        'empty'
+    ];
+
+    public function addAuthorizedTemplateName(string $template_name): void
+    {
+        $this->authorized_template_names[] = $template_name;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAuthorizedTemplateNames(): array
+    {
+        return $this->authorized_template_names;
+    }
+}

plugins/program_management/include/Templates/TeamTemplate.php
@@ -1,0 +1,120 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProgramManagement\Templates;
+
+use Tuleap\Glyph\Glyph;
+use Tuleap\Glyph\GlyphFinder;
+use Tuleap\Project\Registration\Template\CategorisedTemplate;
+use Tuleap\Project\Registration\Template\TemplateCategory;
+use Tuleap\Project\XML\ConsistencyChecker;
+
+class TeamTemplate implements CategorisedTemplate
+{
+    public const NAME = 'program_management_team';
+
+    private const PROGRAM_XML = __DIR__ . '/../../resources/templates/scrum_team_template.xml';
+    private const TTM_XML     = __DIR__ . '/../../resources/templates/testmanagement.xml';
+
+    private string $title;
+    private string $description;
+    private ?string $xml_path = null;
+
+    private ?bool $available = null;
+
+    private GlyphFinder $glyph_finder;
+
+    private ConsistencyChecker $consistency_checker;
+
+    private TemplateCategorySAFe $template_category;
+
+    public function __construct(GlyphFinder $glyph_finder, ConsistencyChecker $consistency_checker)
+    {
+        $this->title               = dgettext('tuleap-program_management', 'Essential SAFe(R) - Scrum Team');
+        $this->description         = dgettext(
+            'tuleap-program_management',
+            'This Tuleap template helps cross-functional group of people to define, build, test, and deliver an increment of value in a short time box. This is a ready-to-use environment based on Scrum approach, linked with the above Agile Release Train and Program Increments. Scrum Master, Product Owner and other agile teammates can manage Team Events, Backlog and Team Artifacts, as Stories, Tasks and Tests. This template has to be used with the Tuleap template "Essential SAFe(R) - ART and Program Management"'
+        );
+        $this->glyph_finder        = $glyph_finder;
+        $this->consistency_checker = $consistency_checker;
+        $this->template_category   = TemplateCategorySAFe::build();
+    }
+
+    public function getId(): string
+    {
+        return self::NAME;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getGlyph(): Glyph
+    {
+        return $this->glyph_finder->get('tuleap-program-management-' . $this->template_category->shortname);
+    }
+
+    public function isBuiltIn(): bool
+    {
+        return true;
+    }
+
+    public function getXMLPath(): string
+    {
+        if ($this->xml_path === null) {
+            $base_dir = \ForgeConfig::getCacheDir() . '/program_management_template/team';
+            if (! is_dir($base_dir) && ! mkdir($base_dir, 0755, true) && ! is_dir($base_dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $base_dir));
+            }
+            $this->xml_path = $base_dir . '/project.xml';
+
+            if (! copy(self::PROGRAM_XML, $this->xml_path)) {
+                throw new \RuntimeException("Can not copy Team file for tuleap template import");
+            }
+
+            $testmanagment_file = $base_dir . '/testmanagement.xml';
+            if (! copy(self::TTM_XML, $testmanagment_file)) {
+                throw new \RuntimeException("Can not copy TTM file for tuleap template import");
+            }
+        }
+        return $this->xml_path;
+    }
+
+    public function isAvailable(): bool
+    {
+        if ($this->available === null) {
+            $this->available = $this->consistency_checker->areAllServicesAvailable($this->getXMLPath());
+        }
+        return $this->available;
+    }
+
+    public function getTemplateCategory(): TemplateCategory
+    {
+        return $this->template_category;
+    }
+}

plugins/program_management/site-content/fr_FR/LC_MESSAGES/tuleap-program_management.po
@@ -93,6 +93,9 @@ msgstr ""
 msgid "Essential SAFe(R) - ART and Program Management"
 msgstr "Essential SAFe(R) - ART et Gestion de Programme"
 
+msgid "Essential SAFe(R) - Scrum Team"
+msgstr "Essential SAFe(R) - Équipe Scrum"
+
 #, php-format
 msgid ""
 "Expected tracker with id <a href=\"%s\">#%d</a> to have a %s field "
@@ -406,6 +409,24 @@ msgstr ""
 "Roadmap et ART artéfacts en tant que Features, Enablers ou Program Epics. Ce "
 "modèle doit être utilisé avec le modèle Tuleap \"Essential SAFe(R) - Équipe "
 "Scrum\""
+
+msgid ""
+"This Tuleap template helps cross-functional group of people to define, "
+"build, test, and deliver an increment of value in a short time box. This is "
+"a ready-to-use environment based on Scrum approach, linked with the above "
+"Agile Release Train and Program Increments. Scrum Master, Product Owner and "
+"other agile teammates can manage Team Events, Backlog and Team Artifacts, as "
+"Stories, Tasks and Tests. This template has to be used with the Tuleap "
+"template \"Essential SAFe(R) - ART and Program Management\""
+msgstr ""
+"Ce modèle de projet Tuleap aide un groupe interfonctionnel de personnes à "
+"définir, créer, tester et fournir un incrément de valeur dans un court laps "
+"de temps. Il s'agit d'un environnement prêt à l'emploi basé sur l'approche "
+"Scrum, lié au train de version Agile et aux incréments de programme ci-"
+"dessus. Scrum Master, Product Owner et autres coéquipiers agiles peuvent "
+"gérer les événements d'équipe, le backlog et les artéfacts d'équipe, sous "
+"forme de Stories, de tâches et de tests. Ce modèle doit être utilisé avec le "
+"modèle Tuleap \"Essential SAFe(R) - ART et Gestion de Programmes\""
 
 msgid "Timeframe"
 msgstr "Plage de temps"

commit 888e3b0f1db9cc1c3c1f068a68a761ae73104ea8

Cannot create project based on the "Agile ALM" template
\array_search() can return 0 (which is evaluated as false) so to test
for the existance (or absence) of a key you either need to explicitly
check for \false or use the more appropriate \in_array().

In the present case the issue is deeper than that, the verification is
done twice in two different ways.

This contribution fixes the initial issue by removing the broken
verification and improves the error message so it's easier to understand
for the users what's the actual issue.

Issue introduced by 646be67d7d9fc1a89f1ed3c8fc38f783599337cd.

Part of story #22210: select SAFe templates at project creation

Change-Id: I3bac40bdc85820f8542675a8930793c2ad9c6e5f

plugins/program_management/include/program_managementPlugin.php
@@ -125,7 +125,6 @@ use Tuleap\Project\Admin\PermissionsPerGroup\PermissionPerGroupPaneCollector;
 use Tuleap\Project\Admin\PermissionsPerGroup\PermissionPerGroupUGroupFormatter;
 use Tuleap\Project\Event\ProjectServiceBeforeActivation;
 use Tuleap\Project\ProjectAccessChecker;
-use Tuleap\Project\Registration\Template\Events\CollectAuthorizedXMLTemplateNamesEvent;
 use Tuleap\Project\Registration\Template\Events\CollectCategorisedExternalTemplatesEvent;
 use Tuleap\Project\RestrictedUserCanAccessProjectVerifier;
 use Tuleap\Project\Service\ServiceDisabledCollector;
@@ -225,7 +224,6 @@ final class program_managementPlugin extends Plugin
         $this->addHook(GetWhitelistedKeys::NAME);
         $this->addHook(DisplayCreatedProjectModalPresenter::NAME);
         $this->addHook(CollectCategorisedExternalTemplatesEvent::NAME);
-        $this->addHook(CollectAuthorizedXMLTemplateNamesEvent::NAME);
         $this->addHook(ServiceEnableForXmlImportRetriever::NAME);
         $this->addHook(\Tuleap\Glyph\GlyphLocationsCollector::NAME);
 
@@ -984,11 +982,5 @@ final class program_managementPlugin extends Plugin
             'tuleap-program-management',
             new GlyphLocation(__DIR__ . '/../glyphs')
         );
-    }
-
-    public function collectAuthorizedXMLTemplateNamesEvent(CollectAuthorizedXMLTemplateNamesEvent $event): void
-    {
-        $event->addAuthorizedTemplateName(ProgramTemplate::NAME);
-        $event->addAuthorizedTemplateName(TeamTemplate::NAME);
     }
 }

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -2298,6 +2298,9 @@ msgstr "Aucune catégorie de projet"
 msgid "No projects to validate"
 msgstr "Aucun projet à valider"
 
+msgid "No template information have been provided to create the project"
+msgstr "Aucune information de template a été fournie pour créer le projet"
+
 msgid "No users to validate"
 msgstr "Aucun utilisateur à valider"
 
@@ -3442,6 +3445,10 @@ msgid "The %s team."
 msgstr "L'équipe %s."
 
 #, php-format
+msgid "The XML template \"%s\" provided is not valid"
+msgstr "Le name modèle XML \"%s\" n'est pas valide"
+
+#, php-format
 msgid "The category %d only allows %d values"
 msgstr "La catégorie %d autorise seulement %d valeurs"
 
@@ -3627,9 +3634,6 @@ msgstr "Le widget est déjà utilisé"
 
 msgid "The widget is disabled in project dashboard."
 msgstr "Le widget est désactivé dans les tableaux de bord de projet."
-
-msgid "The xml template name provided is not valid"
-msgstr "Le nom du modèle xml fourni n'est pas valide"
 
 msgid "The {{tuleap_server_name}} team."
 msgstr "L'équipe de {{tuleap_server_name}}."

src/common/Project/REST/v1/ProjectCreationDataPOSTProjectBuilder.php
@@ -34,7 +34,6 @@ use Tuleap\Project\ProjectCreationDataServiceFromXmlInheritor;
 use Tuleap\Project\Registration\Template\InsufficientPermissionToUseProjectAsTemplateException;
 use Tuleap\Project\Registration\Template\ProjectIDTemplateNotProvidedException;
 use Tuleap\Project\Registration\Template\ProjectTemplateIDInvalidException;
-use Tuleap\Project\Registration\Template\ProjectTemplateNotActiveException;
 use Tuleap\Project\Registration\Template\TemplateFactory;
 use Tuleap\Project\Registration\Template\TemplateFromProjectForCreation;
 use Tuleap\Project\XML\XMLFileContentRetriever;
@@ -69,7 +68,7 @@ class ProjectCreationDataPOSTProjectBuilder
      * @throws InsufficientPermissionToUseProjectAsTemplateException
      * @throws ProjectIDTemplateNotProvidedException
      * @throws ProjectTemplateIDInvalidException
-     * @throws ProjectTemplateNotActiveException
+     * @throws \Tuleap\Project\Registration\Template\InvalidTemplateException
      */
     public function buildProjectCreationDataFromPOSTRepresentation(
         ProjectPostRepresentation $post_representation,

src/common/Project/REST/v1/ProjectResource.class.php
@@ -71,9 +71,7 @@ use Tuleap\Project\Registration\ProjectRegistrationPermissionsChecker;
 use Tuleap\Project\Registration\ProjectRegistrationRESTChecker;
 use Tuleap\Project\Registration\ProjectRegistrationUserPermissionChecker;
 use Tuleap\Project\Registration\ProjectRegistrationBaseChecker;
-use Tuleap\Project\Registration\Template\Events\CollectAuthorizedXMLTemplateNamesEvent;
 use Tuleap\Project\Registration\Template\InvalidTemplateException;
-use Tuleap\Project\Registration\Template\InvalidXMLTemplateNameException;
 use Tuleap\Project\Registration\Template\TemplateFactory;
 use Tuleap\Project\REST\HeartbeatsRepresentation;
 use Tuleap\Project\REST\ProjectRepresentation;
@@ -271,8 +269,6 @@ class ProjectResource extends AuthenticatedResource
                 ),
                 $this->getBackendLogger()
             );
-
-            $this->checkXMLTemplateName($post_representation);
 
             $creation_data = $creation_data_post_project_builder->buildProjectCreationDataFromPOSTRepresentation(
                 $post_representation,
@@ -1343,22 +1339,5 @@ class ProjectResource extends AuthenticatedResource
         }
 
         return $project_field_representations;
-    }
-
-    /**
-     * @throws InvalidTemplateException
-     */
-    protected function checkXMLTemplateName(ProjectPostRepresentation $post_representation): void
-    {
-        if (! $post_representation->xml_template_name) {
-            return;
-        }
-
-        $event = new CollectAuthorizedXMLTemplateNamesEvent();
-        $this->event_manager->dispatch($event);
-
-        if (! array_search($post_representation->xml_template_name, $event->getAuthorizedTemplateNames())) {
-            throw new InvalidXMLTemplateNameException();
-        }
     }
 }

src/common/Project/REST/v1/RestProjectCreator.php
@@ -37,7 +37,7 @@ use Tuleap\Project\Registration\ProjectInvalidShortNameException;
 use Tuleap\Project\Registration\RegistrationErrorException;
 use Tuleap\Project\Registration\RegistrationForbiddenException;
 use Tuleap\Project\Registration\Template\InvalidTemplateException;
-use Tuleap\Project\Registration\Template\InvalidXMLTemplateNameException;
+use Tuleap\Project\Registration\Template\NoTemplateProvidedException;
 use Tuleap\Project\Registration\Template\TemplateFactory;
 use Tuleap\Project\SystemEventRunnerForProjectCreationFromXMLTemplate;
 use Tuleap\Project\XML\Import\DirectoryArchive;
@@ -133,6 +133,6 @@ class RestProjectCreator
             );
         }
 
-        throw new InvalidXMLTemplateNameException();
+        throw new NoTemplateProvidedException();
     }
 }

src/common/Project/Registration/Template/Events/CollectAuthorizedXMLTemplateNamesEvent.php
@@ -1,54 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-declare(strict_types=1);
-
-namespace Tuleap\Project\Registration\Template\Events;
-
-use Tuleap\Event\Dispatchable;
-
-class CollectAuthorizedXMLTemplateNamesEvent implements Dispatchable
-{
-    public const NAME = 'collectAuthorizedXMLTemplateNamesEvent';
-
-    /**
-     * @var string[]
-     */
-    private array $authorized_template_names = [
-        'agile_alm',
-        'scrum',
-        'kanban',
-        'issues',
-        'empty'
-    ];
-
-    public function addAuthorizedTemplateName(string $template_name): void
-    {
-        $this->authorized_template_names[] = $template_name;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getAuthorizedTemplateNames(): array
-    {
-        return $this->authorized_template_names;
-    }
-}

src/common/Project/Registration/Template/Events/CollectCategorisedExternalTemplatesEvent.php
@@ -26,7 +26,7 @@ use Tuleap\Event\Dispatchable;
 use Tuleap\Project\Registration\Template\CategorisedTemplate;
 use Tuleap\Project\Registration\Template\ProjectTemplate;
 
-class CollectCategorisedExternalTemplatesEvent implements Dispatchable
+final class CollectCategorisedExternalTemplatesEvent implements Dispatchable
 {
     public const NAME = 'collectCategorisedExternalTemplatesEvent';
 

src/common/Project/Registration/Template/NoTemplateProvidedException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2019 - present. All Rights Reserved.
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
  *
  *  This file is a part of Tuleap.
  *
@@ -23,21 +23,15 @@ declare(strict_types=1);
 
 namespace Tuleap\Project\Registration\Template;
 
-final class InvalidXMLTemplateNameException extends \RuntimeException implements InvalidTemplateException
+final class NoTemplateProvidedException extends \RuntimeException implements InvalidTemplateException
 {
-    /**
-     * @psalm-readonly
-     */
-    private string $name;
-
-    public function __construct(string $name)
+    public function __construct()
     {
-        parent::__construct(sprintf('The XML template "%s" provided is not valid', $name));
-        $this->name = $name;
+        parent::__construct('No template information have been provided to create the project');
     }
 
     public function getI18NMessage(): string
     {
-        return sprintf(_('The XML template "%s" provided is not valid'), $this->name);
+        return _('No template information have been provided to create the project');
     }
 }

src/common/Project/Registration/Template/TemplateFactory.php
@@ -24,6 +24,7 @@ declare(strict_types=1);
 namespace Tuleap\Project\Registration\Template;
 
 use ProjectManager;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Tuleap\Glyph\GlyphFinder;
 use Tuleap\Project\Registration\Template\Events\CollectCategorisedExternalTemplatesEvent;
 use Tuleap\Project\XML\ConsistencyChecker;
@@ -55,17 +56,13 @@ class TemplateFactory
      */
     private $glyph_finder;
 
-    private \EventManager $event_manager;
-    private CollectCategorisedExternalTemplatesEvent $categorised_external_templates_event;
-
     public function __construct(
         GlyphFinder $glyph_finder,
         ProjectXMLMerger $project_xml_merger,
         ConsistencyChecker $consistency_checker,
         TemplateDao $template_dao,
         ProjectManager $project_manager,
-        \EventManager $event_manager,
-        CollectCategorisedExternalTemplatesEvent $categorised_external_templates_event
+        EventDispatcherInterface $event_dispatcher
     ) {
         $this->template_dao    = $template_dao;
         $this->templates       = [
@@ -77,10 +74,8 @@ class TemplateFactory
         ];
         $this->project_manager = $project_manager;
         $this->glyph_finder    = $glyph_finder;
-        $this->event_manager   = $event_manager;
 
-        $this->categorised_external_templates_event = $categorised_external_templates_event;
-        $this->external_templates                   = $this->getExternalTemplatesByName();
+        $this->external_templates = self::getExternalTemplatesByName($event_dispatcher);
     }
 
     public static function build(): self
@@ -98,8 +93,7 @@ class TemplateFactory
             ),
             new TemplateDao(),
             \ProjectManager::instance(),
-            $event_manager,
-            new CollectCategorisedExternalTemplatesEvent()
+            $event_manager
         );
     }
 
@@ -138,7 +132,7 @@ class TemplateFactory
         if (isset($this->external_templates[$name]) && $this->external_templates[$name]->isAvailable()) {
             return $this->external_templates[$name];
         }
-        throw new InvalidXMLTemplateNameException();
+        throw new InvalidXMLTemplateNameException($name);
     }
 
     public function recordUsedTemplate(\Project $project, ProjectTemplate $template): \Project
@@ -189,10 +183,10 @@ class TemplateFactory
         return $categorised_templates;
     }
 
-    private function getExternalTemplatesByName(): array
+    private static function getExternalTemplatesByName(EventDispatcherInterface $event_dispatcher): array
     {
-        $this->event_manager->dispatch($this->categorised_external_templates_event);
-        $external_templates = $this->categorised_external_templates_event->getCategorisedTemplates();
+        $event              = $event_dispatcher->dispatch(new CollectCategorisedExternalTemplatesEvent());
+        $external_templates = $event->getCategorisedTemplates();
         $templates_by_name  = [];
 
         foreach ($external_templates as $template) {

tests/unit/common/Project/Registration/Template/TemplateFactoryTest.php
@@ -26,11 +26,10 @@ namespace Tuleap\Project\Registration\Template;
 use org\bovigo\vfs\vfsStream;
 use Tuleap\ForgeConfigSandbox;
 use Tuleap\Glyph\GlyphFinder;
-use Tuleap\Project\Registration\Template\Events\CollectCategorisedExternalTemplatesEvent;
 use Tuleap\Project\XML\ConsistencyChecker;
 use Tuleap\XML\ProjectXMLMerger;
 
-class TemplateFactoryTest extends \Tuleap\Test\PHPUnit\TestCase
+final class TemplateFactoryTest extends \Tuleap\Test\PHPUnit\TestCase
 {
     use ForgeConfigSandbox;
 
@@ -58,7 +57,6 @@ class TemplateFactoryTest extends \Tuleap\Test\PHPUnit\TestCase
             $this->createMock(TemplateDao::class),
             $this->project_manager,
             new \EventManager(),
-            new CollectCategorisedExternalTemplatesEvent()
         );
     }
 