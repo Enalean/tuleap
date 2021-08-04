49c434d5be729c60edb6be58aff32b6e36452ffa
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-08-04 17:43:53 +0200

    npm lockfile also needs to be "cleaned" after the removal of a package
    Part of request #22592: Move to npm 7
    
    Change-Id: I9e6d54d7aac001fc7a02f17cf42830464becc9ff

--- tools/utils/nix/build-tools/npm-cli-clean-up-local-tuleap-dep.sh
+++ tools/utils/nix/build-tools/npm-cli-clean-up-local-tuleap-dep.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 npm "$@"
 
-if [[ "$1" =~ ^(install|i|in|ins|inst|insta|instal|isnt|isnta|isntal|add)$ ]] || [[ "$1" =~ ^(update|up|upgrade|udpate)$ ]]; then
+if [[ "$1" =~ ^(install|i|in|ins|inst|insta|instal|isnt|isnta|isntal|add)$ ]] || [[ "$1" =~ ^(update|up|upgrade|udpate)$ ]] || \
+    [[ "$1" =~ ^(uninstall|un|unlink|remove|rm|r)$ ]]; then
     php "$(dirname "$(readlink -f "$0")")"/clean-lockfile-from-local-tuleap-dep.php "$(pwd)"
 fi
