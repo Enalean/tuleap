Enalean/tuleap

commit 6efeffcf3a03333a98efb03e2a86ebc41001d0d3

Service icons are displayed directly using FontAwesome 5
The shim is no more needed for them.

To avoid a potentially dangerous DB migration, the existing icon ID are
kept as is. Instead of directly the icon ID we now search in lookup
table the corresponding FA icon. This will also makes easier future
migrations to new versions because we do not need to strictly adhere to
the FA naming system.

The easiest way to test the patch is to open the add/edit modal of the
service and to check if all the icons are properly displayed in the
selectbox. Note there is an issue with the TripAdvisor icon which was
introduced by 78cb0e1b3dc34eb0a81fab1935b2426eea41fa02 and will dealt
with in a dedicated contribution.

Part of request #17110: Update to Font Awesome 5

Change-Id: Ic815d086217851b994287f40fbb2a758a5c45e5e

src/templates/project/admin/services/services.mustache
@@ -13,7 +13,7 @@
                        type="button"
                        class="tlp-button-primary tlp-table-actions-element"
                     >
-                        <i class="tlp-button-icon fa fa-plus"></i>
+                        <i class="tlp-button-icon fas fa-plus"></i>
                         {{# gettext }}Add a new service{{/ gettext }}
                     </button>
                 </div>
@@ -36,13 +36,13 @@
                         {{# services }}
                             <tr>
                                 <td title="{{ description }}">
-                                    {{# icon }}<i class="fa fa-fw {{ icon }} project-admin-services-icon"></i>{{/icon}}
+                                    {{# icon }}<i class="fa-fw {{ icon }} project-admin-services-icon" aria-hidden="true"></i>{{/icon}}
                                     {{ label }}
                                 </td>
                                 {{# is_default_template }}
-                                    <td>{{# is_active }}<i class="fa fa-check"></i>{{/ is_active }}</td>
+                                    <td>{{# is_active }}<i class="fas fa-check"></i>{{/ is_active }}</td>
                                 {{/ is_default_template }}
-                                <td>{{# is_used }}<i class="fa fa-check"></i>{{/ is_used }}</td>
+                                <td>{{# is_used }}<i class="fas fa-check"></i>{{/ is_used }}</td>
                                 {{# is_default_template }}
                                     <td>{{ scope }}</td>
                                 {{/ is_default_template }}

plugins/agiledashboard/include/AgileDashboardService.php
@@ -27,6 +27,6 @@ class AgileDashboardService extends \Service
 {
     public function getIconName(): string
     {
-        return 'fa-tlp-taskboard';
+        return 'fas fa-tlp-taskboard';
     }
 }

plugins/docman/include/ServiceDocman.php
@@ -31,7 +31,7 @@ class ServiceDocman extends \Service
 
     public function getIconName(): string
     {
-        return 'fa-folder-open';
+        return 'fas fa-folder-open';
     }
 
     public function getInternationalizedName(): string

plugins/git/include/Git/GitService.php
@@ -29,7 +29,7 @@ class GitService extends Service
 {
     public function getIconName(): string
     {
-        return 'fa-tlp-versioning-git';
+        return 'fas fa-tlp-versioning-git';
     }
 
     public function getInternationalizedName(): string

plugins/hudson/include/HudsonService.php
@@ -27,6 +27,6 @@ class HudsonService extends \Service
 {
     public function getIconName(): string
     {
-        return 'fa-refresh';
+        return 'fas fa-sync';
     }
 }

plugins/mediawiki/include/ServiceMediawiki.class.php
@@ -24,7 +24,7 @@ class ServiceMediawiki extends Service
 {
     public function getIconName(): string
     {
-        return 'fa-tlp-mediawiki';
+        return 'fas fa-tlp-mediawiki';
     }
 
     public function renderInPage($title, $template, $presenter = null)

plugins/proftpd/include/ProFTPd/ServiceProFTPd.class.php
@@ -35,7 +35,7 @@ class ServiceProFTPd extends Service
 
     public function getIconName(): string
     {
-        return 'fa-tlp-folder-globe';
+        return 'fas fa-tlp-folder-globe';
     }
 
     public function renderInPage(HTTPRequest $request, $title, $template, $presenter = null)

commit 14deaf80e8b0618440199782e72db490f85aaf04

fa-fw class is duplicated on the icons of the project admin service panel
Also, the Figma icon class is displayed with both fas and fab classes.

Issues introduced by 6efeffcf3a03333a98efb03e2a86ebc41001d0d3.

Part of request #17110: Update to Font Awesome 5

Change-Id: I552f8c4985b4d9d91517da86139a1e1019e73a8f

src/common/Project/Service/ServiceIconValidator.php
@@ -957,7 +957,7 @@ class ServiceIconValidator
             ],
         'fab fa-figma' =>
              [
-                'fa-icon' => 'fas fab fa-figma',
+                'fa-icon' => 'fab fa-figma',
                 'description' => 'Figma Logo',
             ],
         'fa-fighter-jet' =>

src/templates/project/admin/services/services.mustache
@@ -36,7 +36,7 @@
                         {{# services }}
                             <tr>
                                 <td title="{{ description }}">
-                                    {{# icon }}<i class="fa-fw {{ icon }} project-admin-services-icon" aria-hidden="true"></i>{{/icon}}
+                                    {{# icon }}<i class="{{ icon }} project-admin-services-icon" aria-hidden="true"></i>{{/icon}}
                                     {{ label }}
                                 </td>
                                 {{# is_default_template }}