5a1e201eb86f50d55e1b47b02cae88c11475ce16
Manuel VACELET <manuel.vacelet@enalean.com>
2021-09-22 13:33:46 +0200

    chore(forge-config) Python code, bye bye local.inc
    Part-of: story #22708 flexibility in the management of Tuleap Configuration
    
    Change-Id: I68afe7e4b21a262748eeca5e21ba4aafd067648a

---
+++ src/common/CLI/Command/ConfigDumpCommand.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\CLI\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class ConfigDumpCommand extends Command
+{
+    public const NAME = 'config-dump';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME);
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Dump Tuleap configuration for external re-use (in JSON)')
+            ->addArgument('keys', InputArgument::IS_ARRAY, 'Name of the variables requested');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $keys = $input->getArgument('keys');
+        if (count($keys) === 0) {
+            $io = new SymfonyStyle($input, $output);
+            $io->getErrorStyle()->error('Requested variables should be given in argument (keys)');
+            return 1;
+        }
+
+        $json = [];
+        foreach ($keys as $key) {
+            if (! \ForgeConfig::exists($key)) {
+                continue;
+            }
+            $json[$key] = \ForgeConfig::get($key);
+        }
+        $output->write(\json_encode($json, JSON_THROW_ON_ERROR));
+
+        return 0;
+    }
+}
