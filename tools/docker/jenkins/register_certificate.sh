3781620d05fc0900a65d09cddd6459a488682991
Manuel VACELET <manuel.vacelet@enalean.com>
2021-10-08 17:39:04 +0200

    request #23418 Bump dev Jenkins to 2.303.2
    Change-Id: I6a8f9ef1ba3a3620b7308d3771665549a665a01a

--- tools/docker/jenkins/register_certificate.sh
+++ tools/docker/jenkins/register_certificate.sh
@@ -27,7 +27,7 @@ while [ ! -f /opt/reverse-proxy-cert/certs/localhost.cert.pem ]; do
     sleep 1
 done
 
-keytool -keystore $JAVA_HOME/jre/lib/security/cacerts -import -trustcacerts -storepass changeit -noprompt -alias tuleap-web-dev -file /opt/reverse-proxy-cert/certs/localhost.cert.pem
+keytool -keystore $JAVA_HOME/lib/security/cacerts -import -trustcacerts -storepass changeit -noprompt -alias tuleap-web-dev -file /opt/reverse-proxy-cert/certs/localhost.cert.pem
 
 cp -af /opt/reverse-proxy-cert/certs/localhost.cert.pem /usr/local/share/ca-certificates/tuleap.crt
 update-ca-certificates
