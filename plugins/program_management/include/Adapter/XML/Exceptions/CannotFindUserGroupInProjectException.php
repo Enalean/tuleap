6969cacd0b70253135906de926861431b1cc9754
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2021-10-18 11:00:05 +0200

    Be sure that classes of Adapter/TopBacklog are never mocked in test in XML namespace
    part of request #23433 Clean program management tests
    
    No functional changes
    
    Change-Id: I9da48ea8cb809c3510ac5c673758ace6aa7967a1

--- plugins/program_management/include/Adapter/XML/Exceptions/CannotFindUserGroupInProjectException.php
+++ plugins/program_management/include/Adapter/XML/Exceptions/CannotFindUserGroupInProjectException.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\ProgramManagement\Adapter\XML\Exceptions;
 
-class CannotFindUserGroupInProjectException extends \Exception implements XMLConfigExtractionException
+final class CannotFindUserGroupInProjectException extends \Exception implements XMLConfigExtractionException
 {
     public function __construct(string $ugroup_name)
     {
