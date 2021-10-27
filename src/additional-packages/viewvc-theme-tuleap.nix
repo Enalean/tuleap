b44bdfa17b907954dd40ded847d1921f000cc8d1
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-10-27 09:47:24 +0200

    Build the viewvc-theme-tuleap package in the main build pipeline
    To test, you can build the package wiht `nix-build
    viewvc-theme-tuleap.nix` and install the resulting package in your dev
    container. The web browser of a SVN or CVS repository should look the
    same.
    
    To fetch the node_modules we use a fixed output derivation (aka FOD) to
    execute pnpm. In some Nix circles this is considered as an abused of the
    FOD but it is likely to be good enough for us. We do not have to manage
    multiple OSes and architectures during the build so it reduces the
    chances of something going wrong.
    
    Part of request #23451: Build the "additional" packages in the main build process
    
    Change-Id: Iea79b413bac24c3dec458ad23d13b956f7b6fc53

---
+++ src/additional-packages/viewvc-theme-tuleap.nix
@@ -0,0 +1,68 @@
+{
+  pkgs ? (import ../../tools/utils/nix/pinned-nixpkgs.nix) {},
+  nixpkgsPinEpoch ? (import ../../tools/utils/nix/nixpkgs-pin-epoch.nix) { inherit pkgs; },
+  jsBuildTool ? (import ../../tools/utils/nix/build-tools/build-tools-js.nix) { inherit pkgs; }
+}:
+
+let
+  fetchNodeModules = { src, pnpm, sha256 }:
+    pkgs.stdenvNoCC.mkDerivation {
+      name = "node_modules";
+
+      outputHashAlgo = "sha256";
+      outputHash = sha256;
+      outputHashMode = "recursive";
+
+      nativeBuildInputs = [ pnpm ];
+
+      buildCommand = ''
+        cp -r ${src}/* .
+        export HOME=.
+        pnpm config set extend-node-path false
+        pnpm install --frozen-lockfile
+        rm node_modules/.modules.yaml
+        mv node_modules $out
+      '';
+    };
+  name = "viewvc-theme-tuleap";
+  src = pkgs.fetchgit {
+    url = "https://tuleap.net/plugins/git/tuleap/deps/3rdparty/viewvc-theme-tuleap.git";
+    rev = "2d3dc5b627f59153cdaafd04b77a97ed1f2329c0";
+    sha256 = "0zv3b5ajsylm1azf2p28v0lqpmj7111bsvw33q5l5307kb1bjdkm"; # Please also check if the node_modules sha256 does not need to be updated
+  };
+  node_modules = fetchNodeModules {
+    inherit src;
+    pnpm = jsBuildTool;
+    sha256 = "1j0l66l68caw0rarjinjm8s52mnxw96s6ivwrxncnnk4259jrcvq";
+  };
+in pkgs.stdenvNoCC.mkDerivation {
+  inherit name src;
+
+  nativeBuildInputs = [ pkgs.rpm pkgs.file jsBuildTool ];
+
+  dontConfigure = true;
+
+  buildPhase = ''
+    cp -r ${node_modules} ./node_modules
+    pnpm run build
+    tar cfz ${name}.tar.gz src
+    rpmbuild \
+        --define "nixpkgs_epoch .${nixpkgsPinEpoch}" \
+        --define "_sourcedir $(pwd)" \
+        --define "_rpmdir $(pwd)" \
+        --dbpath="$(pwd)"/rpmdb \
+        --define "%_topdir $(pwd)" \
+        --define "%_tmppath %{_topdir}/TMP" \
+        --define "_rpmdir $(pwd)/RPMS" \
+        --define "%_datadir /usr/share" \
+        --define "%_sysconfdir /etc" \
+        -bb ${name}.spec
+  '';
+
+  installPhase = ''
+    mkdir $out/
+    mv RPMS/noarch/*.rpm $out/
+  '';
+
+  dontFixUp = true;
+}
