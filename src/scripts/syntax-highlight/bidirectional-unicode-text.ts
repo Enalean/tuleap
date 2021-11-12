41f1db5ad126a58bdabbe64a07fd93f5884f670d
Thomas Gerbet <thomas.gerbet@enalean.com>
2021-11-12 10:35:30 +0100

    tlp-syntax-highlighting element shows potentially dangerous bidirectional Unicode characters
    This is visible when viewing the content of a file in the Git web UI.
    
    Part of request #23496: Add warnings when displaying code with bidirectional Unicode text in Git/PR web UI
    
    Change-Id: I19e9386ea370c5b3ca9c349d5bd22e6114dc6160

---
+++ src/scripts/syntax-highlight/bidirectional-unicode-text.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import potentially_dangerous_bidirectional_characters from "../../common/Code/potentially-dangerous-bidirectional-characters.json";
+
+export function markPotentiallyDangerousBidirectionalUnicodeText(html_content: string): string {
+    const regex_potentially_dangerous_bidirectional_characters = new RegExp(
+        "[" + potentially_dangerous_bidirectional_characters.join("") + "]",
+        "g"
+    );
+
+    return html_content.replaceAll(
+        regex_potentially_dangerous_bidirectional_characters,
+        replaceMatchedChar
+    );
+}
+
+function replaceMatchedChar(match: string): string {
+    const span = document.createElement("span");
+    span.classList.add("syntax-highlight-invisible-char");
+    span.dir = "ltr";
+    span.title = "\\u" + match.charCodeAt(0).toString(16);
+    span.textContent = match;
+    return span.outerHTML;
+}
