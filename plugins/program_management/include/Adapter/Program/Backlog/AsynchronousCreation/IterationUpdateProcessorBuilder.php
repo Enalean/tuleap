8ed2b5ab05b160022791ac9fcc9eaea429f5e623
Joris Masson <joris.masson@enalean.com>
2021-11-17 15:48:31 +0100

    Use FormElementFactoryAdapter in iteration updates
    part of story #23499 re-synchronize Iterations
    
    Update of iterations should not lead to fatal error
    
    Change-Id: Id59a08fb6d7bdc2f2715326727fc4d0f3afa4881

--- plugins/program_management/include/Adapter/Program/Backlog/AsynchronousCreation/IterationUpdateProcessorBuilder.php
+++ plugins/program_management/include/Adapter/Program/Backlog/AsynchronousCreation/IterationUpdateProcessorBuilder.php
@@ -36,6 +36,7 @@ use Tuleap\ProgramManagement\Adapter\Program\Backlog\ProgramIncrement\Source\Fie
 use Tuleap\ProgramManagement\Adapter\Team\MirroredTimeboxes\MirroredTimeboxesDao;
 use Tuleap\ProgramManagement\Adapter\Workspace\MessageLog;
 use Tuleap\ProgramManagement\Adapter\Workspace\Tracker\Artifact\ArtifactFactoryAdapter;
+use Tuleap\ProgramManagement\Adapter\Workspace\Tracker\Fields\FormElementFactoryAdapter;
 use Tuleap\ProgramManagement\Adapter\Workspace\Tracker\TrackerFactoryAdapter;
 use Tuleap\ProgramManagement\Adapter\Workspace\Tracker\TrackerOfArtifactRetriever;
 use Tuleap\ProgramManagement\Adapter\Workspace\UserManagerAdapter;
@@ -79,6 +80,7 @@ final class IterationUpdateProcessorBuilder implements BuildIterationUpdateProce
         $visibility_verifier      = new ArtifactVisibleVerifier($artifact_factory, $user_retriever);
         $tracker_retriever        = new TrackerFactoryAdapter($tracker_factory);
         $artifact_retriever       = new ArtifactFactoryAdapter($artifact_factory);
+        $field_retriever          = new FormElementFactoryAdapter($tracker_retriever, $form_element_factory);
 
         $transaction_executor = new DBTransactionExecutorWithConnection(DBFactory::getMainTuleapDBConnection());
 
@@ -138,7 +140,7 @@ final class IterationUpdateProcessorBuilder implements BuildIterationUpdateProce
                     $artifact_factory
                 )
             ),
-            $form_element_factory
+            $field_retriever
         );
         return new IterationUpdateProcessor(
             MessageLog::buildFromLogger($logger),
