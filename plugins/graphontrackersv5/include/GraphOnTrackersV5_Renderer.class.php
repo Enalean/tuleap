Enalean/tuleap

commit d32f105fdd38e818dd03698bc2200f8885626861

request #23821 Widget reports shouldn't be based on session reports
When a user updates a report without saving it, dashboards widget should use database data.

Change-Id: I15203f3d8fa6d96ebcbf434d403e8892d6f3eb10

plugins/graphontrackersv5/include/GraphOnTrackersV5_Renderer.class.php
@@ -184,7 +184,7 @@ class GraphOnTrackersV5_Renderer extends Tracker_Report_Renderer
         }
 
         $report_charts = $this->getChartFactory()->getCharts($this);
-        $matching_ids  = $this->report->getMatchingIds();
+        $matching_ids  = $this->report->getMatchingIds(null, $in_dashboard);
         assert(is_array($matching_ids));
 
         if ($this->widgetMustDisplayEmptyState($in_dashboard, $report_charts, $matching_ids)) {
@@ -198,7 +198,7 @@ class GraphOnTrackersV5_Renderer extends Tracker_Report_Renderer
 
         foreach ($report_charts as $chart) {
             $html .= '<div class="widget_report_graph">';
-            $html .= $chart->fetchOnReport($this, $current_user, $readonly, $store_in_session);
+            $html .= $chart->fetchOnReport($this, $current_user, $readonly, $in_dashboard, $store_in_session);
             $html .= '</div>';
         }
 
@@ -301,7 +301,7 @@ class GraphOnTrackersV5_Renderer extends Tracker_Report_Renderer
                     $chart = $this->getChartFactory()
                                   ->getChart($this, $renderer_parameters['stroke'], $store_in_session)
                 ) {
-                    $chart->stroke();
+                    $chart->stroke(! $store_in_session);
                     exit;
                 }
             }

plugins/cardwall/include/Cardwall_Renderer.class.php
@@ -239,7 +239,13 @@ class Cardwall_Renderer extends Tracker_Report_Renderer
         $renderer = TemplateRendererFactory::build()->getRenderer(dirname(__FILE__) . '/../templates');
 
         $html .= $renderer->renderToString('additional-button', $additional_button_presenter);
-        $html .= $this->fetchCards($this->report->getMatchingIds(), $user);
+
+        $use_data_from_db = true;
+        if (ForgeConfig::getFeatureFlag(Tracker_Report::FEATURE_FLAG_KEY)) {
+            $use_data_from_db = false;
+        }
+
+        $html .= $this->fetchCards($this->report->getMatchingIds(null, $use_data_from_db), $user);
         $html .= $this->fetchWidgetGoToReport();
 
         return $html;

plugins/graphontrackersv5/include/Async/ChartDataController.php
@@ -22,10 +22,12 @@ declare(strict_types=1);
 
 namespace Tuleap\GraphOnTrackersV5\Async;
 
+use ForgeConfig;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Tracker_Report;
 use Tuleap\GraphOnTrackersV5\DataTransformation\ChartFieldNotFoundException;
 use Tuleap\Http\Response\JSONResponseBuilder;
 use Tuleap\Request\DispatchablePSR15Compatible;
@@ -66,9 +68,15 @@ final class ChartDataController extends DispatchablePSR15Compatible
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $report_id   = $request->getAttribute('report_id');
-        $renderer_id = $request->getAttribute('renderer_id');
-        $chart_id    = $request->getAttribute('chart_id');
+        $report_id    = $request->getAttribute('report_id');
+        $renderer_id  = $request->getAttribute('renderer_id');
+        $chart_id     = $request->getAttribute('chart_id');
+        $params       = $request->getQueryParams();
+        $in_dashboard = $params['in_dashboard'] ?? false;
+
+        if (ForgeConfig::getFeatureFlag(Tracker_Report::FEATURE_FLAG_KEY)) {
+            $in_dashboard = false;
+        }
 
         $current_user = $this->user_manager->getCurrentUser();
 
@@ -88,7 +96,7 @@ final class ChartDataController extends DispatchablePSR15Compatible
         }
 
         try {
-            $chart_data = $chart->fetchAsArray();
+            $chart_data = $chart->fetchAsArray((bool) $in_dashboard);
             if ($chart_data) {
                 return $this->json_response_builder->fromData($chart_data);
             } else {

plugins/graphontrackersv5/include/data-access/GraphOnTrackersV5_Chart.class.php
@@ -196,19 +196,11 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
                      'renderer_plugin_graphontrackersv5[stroke]' => $this->getId()]);
     }
 
-    /**
-     * Display both <img /> and <map /> tags to embed the chart in a html page
-     */
-    public function display()
-    {
-        echo $this->fetch();
-    }
-
-    public function fetch($store_in_session = true)
+    public function fetch(bool $store_in_session = true)
     {
         $html = '';
         if ($this->userCanVisualize()) {
-            $e = $this->buildGraph();
+            $e = $this->buildGraph(! $store_in_session);
             if ($e) {
                 $html  = $this->getHTMLImageMapWithoutInterruptingExecutionFlow($e, 'map' . $this->getId());
                 $html .= $this->fetchImgTag($store_in_session);
@@ -234,7 +226,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         return $html;
     }
 
-    private function fetchGraphAnchor($content)
+    private function fetchGraphAnchor($content, bool $in_dashboard)
     {
         $renderer_id = $this->renderer->getId();
         $report_id   = $this->renderer->report->getId();
@@ -242,6 +234,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
                      data-graph-id="' . $this->getId() . '"
                      data-renderer-id="' . $renderer_id . '"
                      data-report-id="' . $report_id . '"
+                     data-in-dashboard="' . $in_dashboard . '"
                 >' . $content . '</div>';
     }
 
@@ -262,7 +255,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         return TemplateRendererFactory::build()->getRenderer(TRACKER_TEMPLATE_DIR . '/report');
     }
 
-    public function fetchOnReport(GraphOnTrackersV5_Renderer $renderer, PFUser $current_user, $read_only, $store_in_session = true)
+    public function fetchOnReport(GraphOnTrackersV5_Renderer $renderer, PFUser $current_user, $read_only, bool $in_dashboard, $store_in_session = true)
     {
         if ($this->isGraphDrawnByD3()) {
             $content   = '';
@@ -282,7 +275,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         $html .= '</div>';
         $html .= '</div>';
         $html .= '<div class="widget_content">';
-        $html .= $this->fetchGraphAnchor($content);
+        $html .= $this->fetchGraphAnchor($content, $in_dashboard);
         $html .= '</div>'; // content
         $html .= '</div>'; // widget
 
@@ -367,9 +360,9 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
     /**
      * Fetch chart data as an array
      */
-    public function fetchAsArray()
+    public function fetchAsArray(bool $use_data_from_db)
     {
-        $engine = $this->getEngineWithData();
+        $engine = $this->getEngineWithData($use_data_from_db);
         if (! $this->userCanVisualize() || ! $engine) {
             return [];
         }
@@ -393,9 +386,9 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
      * Stroke the chart.
      * Build the image and send it to the client
      */
-    public function stroke()
+    public function stroke(bool $use_data_from_db)
     {
-        $e = $this->buildGraph();
+        $e = $this->buildGraph($use_data_from_db);
         if ($e && is_object($e->graph)) {
             $e->graph->StrokeCSIM();
         }
@@ -405,9 +398,9 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
      * Prepare the building of the graph
      * @return GraphOnTracker_Chart_Engine
      */
-    protected function buildGraph()
+    protected function buildGraph(bool $use_data_from_db)
     {
-        $e = $this->getEngineWithData();
+        $e = $this->getEngineWithData($use_data_from_db);
         if ($e) {
             //build the chart
             $e->buildGraph();
@@ -421,7 +414,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
     /**
      * @return GraphOnTrackersV5_Engine|false
      */
-    protected function getEngineWithData()
+    protected function getEngineWithData(bool $use_data_from_db)
     {
         if (! $this->engine) {
             //Get the chart engine
@@ -429,7 +422,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         }
 
         //Define the artifacts which must be added to the chart
-        $artifacts = $this->renderer->report->getMatchingIds();
+        $artifacts = $this->renderer->report->getMatchingIds(null, $use_data_from_db);
 
         //Get the ChartDataBuilder for this chart
         $pcdb = $this->getChartDataBuilder($artifacts);
@@ -612,13 +605,13 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
      */
     abstract protected function getDao();
 
-    public function getContent()
+    public function getContent(bool $in_dashboard)
     {
         $content          = '';
         $store_in_session = false;
 
         if ($this->isGraphDrawnByD3()) {
-            $content .= $this->fetchContentD3Graph();
+            $content .= $this->fetchContentD3Graph($in_dashboard);
         } else {
             $content .= $this->fetchContentJPGraph($store_in_session);
         }
@@ -629,7 +622,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
     public function getWidgetContent()
     {
         $content  = $this->fetchAdditionnalButton();
-        $content .= $this->getContent();
+        $content .= $this->getContent(true);
         $content .= $this->renderer->fetchWidgetGoToReport();
 
         return $content;
@@ -641,7 +634,7 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         return $this->accept($d3_visitor);
     }
 
-    private function fetchContentJPGraph($store_in_session)
+    private function fetchContentJPGraph(bool $store_in_session)
     {
         $content  = $this->fetch($store_in_session);
         $content .= '<br />';
@@ -649,9 +642,9 @@ abstract class GraphOnTrackersV5_Chart implements Visitable
         return $content;
     }
 
-    private function fetchContentD3Graph()
+    private function fetchContentD3Graph(bool $in_dashboard)
     {
-        return $this->fetchGraphAnchor('');
+        return $this->fetchGraphAnchor('', $in_dashboard);
     }
 
     private function getAvailableDashboardsForUser(PFUser $user)

plugins/graphontrackersv5/scripts/src/loadGraphs.js
@@ -41,7 +41,7 @@ const TYPE_PIE = "pie";
 const TYPE_CUMULATIVE_FLOW_CHART = "cumulativeflow";
 
 async function buildGraph(graph_node) {
-    const { graphId, rendererId, reportId, doesNotUseD3 } = graph_node.dataset;
+    const { graphId, rendererId, reportId, doesNotUseD3, inDashboard } = graph_node.dataset;
 
     if (doesNotUseD3 === "true") {
         return;
@@ -49,7 +49,7 @@ async function buildGraph(graph_node) {
 
     const spinner_node = showSpinner(graph_node);
     try {
-        const graph_data = await getChartData(reportId, rendererId, graphId);
+        const graph_data = await getChartData(reportId, rendererId, graphId, inDashboard);
         graphFactory(graphId, graph_data);
     } catch (e) {
         if (!Object.prototype.hasOwnProperty.call(e, "response")) {

plugins/graphontrackersv5/scripts/src/rest-querier.js
@@ -19,9 +19,14 @@
 
 import { get } from "@tuleap/tlp-fetch";
 
-export async function getChartData(report_id, renderer_id, chart_id) {
+export async function getChartData(report_id, renderer_id, chart_id, in_dashboard) {
     const response = await get(
-        `/plugins/graphontrackersv5/report/${report_id}/renderer/${renderer_id}/chart/${chart_id}`
+        `/plugins/graphontrackersv5/report/${report_id}/renderer/${renderer_id}/chart/${chart_id}`,
+        {
+            params: {
+                in_dashboard: in_dashboard,
+            },
+        }
     );
 
     return response.json();

plugins/tracker/include/Tracker/Report/Tracker_Report.class.php
@@ -19,6 +19,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Tuleap\Config\FeatureFlagConfigKey;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\Project\MappingRegistry;
 use Tuleap\Tracker\Admin\ArtifactLinksUsageDao;
@@ -66,6 +67,8 @@ use Tuleap\Tracker\Report\TrackerReportConfigDao;
  */
 class Tracker_Report implements Tracker_Dispatchable_Interface
 {
+    #[FeatureFlagConfigKey("Feature flag to allow tracker's report widgets with session data")]
+    public const FEATURE_FLAG_KEY = 'get_session_data_for_reports_widget';
 
     public const ACTION_SAVE            = 'report-save';
     public const ACTION_SAVEAS          = 'report-saveas';

plugins/tracker/include/trackerPlugin.php
@@ -2346,6 +2346,7 @@ class trackerPlugin extends Plugin //phpcs:ignore PSR1.Classes.ClassDeclaration.
     public function getWhitelistedKeys(GetWhitelistedKeys $event): void
     {
         $event->addConfigClass(ListPickerIncluder::class);
+        $event->addConfigClass(Tracker_Report::class);
         $event->addConfigClass(\Tuleap\Tracker\Creation\JiraImporter\ClientWrapper::class);
     }
 

commit 81c4a5a62c6db484742a08927ba182413b828856

Fatal error when adding a new chart in a tracker report
Issue was introduced by d32f105fdd38e818dd03698bc2200f8885626861.

When adding a new chart in a tracker report a fatal error is thrown:
```
Fatal error: Uncaught ArgumentCountError: Too few arguments to function GraphOnTrackersV5_Chart::getContent(), 0 passed in /usr/share/tuleap/plugins/graphontrackersv5/include/GraphOnTrackersV5_Renderer.class.php on line 120 and exactly 1 expected in /usr/share/tuleap/plugins/graphontrackersv5/include/data-access/GraphOnTrackersV5_Chart.class.php on line 608
```

Part of request #23821: Widget reports shouldn't be based on session reports

Change-Id: I7d919fef3c4c63b8403bee07a2dbffce942813f9

plugins/graphontrackersv5/include/GraphOnTrackersV5_Renderer.class.php
@@ -117,7 +117,7 @@ class GraphOnTrackersV5_Renderer extends Tracker_Report_Renderer
             $html .= '</td><td style="text-align:center">';
             //{{{ Chart Preview
 
-            $html .= $this->chart_to_edit->getContent();
+            $html .= $this->chart_to_edit->getContent(false);
             //}}}
             $html .= '</tr>';
             $html .= '</tbody></table>';