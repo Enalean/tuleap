0920ee8bdebc28fcc3544553fe8cfb695c74d453
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2021-11-26 15:49:37 +0100

    RedirectParameterInjector must not be called directly from plugin (bridge between adapters)
    part of: story #21385 display Program's iterations
    
    No functional changes:
    
    How to test::
     - submit a new PI => you are properly redirected
     - submit and continue a new PI => you stay
       - submit a new PI you are redirected on program app
     - submit and stay a new PI => you are not redirected
    
     - submit a new	Iteration => you are properly redirected
     - submit and continue a new Iteration => you stay
       - submit a new Iteration you are redirected	on iteration app
     - submit and stay a new iteration =>	you are	not redirected
    
    Change-Id: I381ab94e9005b79624e3a4597731774eacc1d59a

---
+++ plugins/program_management/tests/unit/Adapter/Events/BuildRedirectFormActionEventProxyTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright (c) Enalean 2021 -  Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProgramManagement\Adapter\Redirections;
+
+use Tracker_Artifact_Redirect;
+use Tuleap\GlobalResponseMock;
+use Tuleap\ProgramManagement\Adapter\Events\BuildRedirectFormActionEventProxy;
+use Tuleap\ProgramManagement\Domain\Redirections\IterationRedirectionParameters;
+use Tuleap\ProgramManagement\Domain\Redirections\ProgramRedirectionParameters;
+use Tuleap\ProgramManagement\Tests\Stub\IterationRedirectionParametersStub;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Tracker\Artifact\Renderer\BuildArtifactFormActionEvent;
+
+final class BuildRedirectFormActionEventProxyTest extends TestCase
+{
+    use GlobalResponseMock;
+
+    private BuildRedirectFormActionEventProxy $proxy;
+    private Tracker_Artifact_Redirect $redirect;
+
+    protected function setUp(): void
+    {
+        $this->redirect = new Tracker_Artifact_Redirect();
+        $this->proxy    = BuildRedirectFormActionEventProxy::fromEvent(
+            new BuildArtifactFormActionEvent(
+                new \Codendi_Request(
+                    [],
+                    null
+                ),
+                $this->redirect
+            )
+        );
+    }
+
+    public function testItInjectAndInformUserAboutUpdatingProgramItem(): void
+    {
+        $GLOBALS['Response']->expects(self::once())->method('addFeedback')->with(
+            \Feedback::INFO,
+            $this->stringContains('update')
+        );
+        $this->proxy->injectAndInformUserAboutUpdatingProgramItem();
+        self::assertCount(1, $this->redirect->query_parameters);
+        self::assertSame(
+            ProgramRedirectionParameters::REDIRECT_AFTER_UPDATE_ACTION,
+            $this->redirect->query_parameters[ProgramRedirectionParameters::FLAG]
+        );
+    }
+
+    public function testItInjectAndInformUserAboutCreatingProgramIncrement(): void
+    {
+        $GLOBALS['Response']->expects(self::once())->method('addFeedback')->with(
+            \Feedback::INFO,
+            $this->stringContains('create')
+        );
+        $this->proxy->injectAndInformUserAboutCreatingProgramIncrement();
+        self::assertCount(1, $this->redirect->query_parameters);
+        self::assertSame(
+            ProgramRedirectionParameters::REDIRECT_AFTER_CREATE_ACTION,
+            $this->redirect->query_parameters[ProgramRedirectionParameters::FLAG]
+        );
+    }
+
+    public function testItInjectAndInformUserAboutCreatingIncrementIteration(): void
+    {
+        $GLOBALS['Response']->expects(self::once())->method('addFeedback')->with(
+            \Feedback::INFO,
+            $this->stringContains('create')
+        );
+        $increment_id = "100";
+        $this->proxy->injectAndInformUserAboutCreatingIteration(
+            IterationRedirectionParametersStub::withValues(
+                IterationRedirectionParameters::REDIRECT_AFTER_CREATE_ACTION,
+                $increment_id
+            )
+        );
+        self::assertCount(5, $this->redirect->query_parameters);
+        self::assertSame(
+            IterationRedirectionParameters::REDIRECT_AFTER_CREATE_ACTION,
+            $this->redirect->query_parameters[IterationRedirectionParameters::FLAG]
+        );
+        self::assertSame(
+            $increment_id,
+            $this->redirect->query_parameters[IterationRedirectionParameters::PARAM_INCREMENT_ID]
+        );
+        self::assertSame(
+            $increment_id,
+            $this->redirect->query_parameters["link-artifact-id"]
+        );
+        self::assertSame(
+            \Tracker_FormElement_Field_ArtifactLink::TYPE_IS_CHILD,
+            $this->redirect->query_parameters["link-type"]
+        );
+        self::assertSame("true", $this->redirect->query_parameters["immediate"]);
+    }
+}
---
+++ plugins/program_management/tests/unit/Domain/Redirections/BuildRedirectFormActionHandlerTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright (c) Enalean 2021 -  Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\ProgramManagement\Domain\Redirections;
+
+use Tuleap\ProgramManagement\Tests\Stub\BuildRedirectFormActionEventStub;
+use Tuleap\ProgramManagement\Tests\Stub\IterationRedirectionParametersStub;
+use Tuleap\ProgramManagement\Tests\Stub\ProgramRedirectionParametersStub;
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class BuildRedirectFormActionHandlerTest extends TestCase
+{
+    private BuildRedirectFormActionEventStub $event;
+    private ProgramRedirectionParameters $program_redirection_parameters;
+    private IterationRedirectionParameters $iteration_redirection_parameters;
+
+    protected function setUp(): void
+    {
+        $this->program_redirection_parameters   = ProgramRedirectionParametersStub::withOtherValue();
+        $this->iteration_redirection_parameters = IterationRedirectionParametersStub::withValues("redirect", "100");
+        $this->event                            = BuildRedirectFormActionEventStub::withCount();
+    }
+
+    public function testItDoesNotRedirect(): void
+    {
+        BuildRedirectFormActionHandler::injectParameters(
+            $this->program_redirection_parameters,
+            $this->iteration_redirection_parameters,
+            $this->event
+        );
+        self::assertSame(0, $this->event->getCreateIterationCount());
+        self::assertSame(0, $this->event->getCreateProgramIncrementCount());
+        self::assertSame(0, $this->event->getUpdateProgramIncrementCount());
+    }
+
+    public function testItRedirectsAfterAProgramIncrementUpdate(): void
+    {
+        $this->program_redirection_parameters = ProgramRedirectionParametersStub::withUpdate();
+        BuildRedirectFormActionHandler::injectParameters(
+            $this->program_redirection_parameters,
+            $this->iteration_redirection_parameters,
+            $this->event
+        );
+        self::assertSame(0, $this->event->getCreateIterationCount());
+        self::assertSame(0, $this->event->getCreateProgramIncrementCount());
+        self::assertSame(1, $this->event->getUpdateProgramIncrementCount());
+    }
+
+    public function testItRedirectsAfterAnIterationCreation(): void
+    {
+        $this->iteration_redirection_parameters = IterationRedirectionParametersStub::withCreate();
+        BuildRedirectFormActionHandler::injectParameters(
+            $this->program_redirection_parameters,
+            $this->iteration_redirection_parameters,
+            $this->event
+        );
+
+        self::assertSame(1, $this->event->getCreateIterationCount());
+        self::assertSame(0, $this->event->getCreateProgramIncrementCount());
+        self::assertSame(0, $this->event->getUpdateProgramIncrementCount());
+    }
+
+    public function testItRedirectsAfterAProgramIncrementCreation(): void
+    {
+        $this->program_redirection_parameters = ProgramRedirectionParametersStub::withCreate();
+        BuildRedirectFormActionHandler::injectParameters(
+            $this->program_redirection_parameters,
+            $this->iteration_redirection_parameters,
+            $this->event
+        );
+        self::assertSame(0, $this->event->getCreateIterationCount());
+        self::assertSame(1, $this->event->getCreateProgramIncrementCount());
+        self::assertSame(0, $this->event->getUpdateProgramIncrementCount());
+    }
+}
