e37dc7e15d879fd2b822f3e89e78aa6ea6c8d13d
Nicolas Terray <nicolas.terray@enalean.com>
2021-12-15 14:40:15 +0100

    feat: Introduce Tests section
    Only the section title and its link in the TOC is added.
    
    Part of story #23462: export test plan as a docx document
    
    Change-Id: I8d345d0033ec99e05ff0f59c6e78d96348d52d4f

---
+++ plugins/testplan/scripts/test-plan/src/helpers/ExportAsDocument/Exporter/DOCX/testplan-builder.ts
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Enalean, 2021 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { VueGettextProvider } from "../../../vue-gettext-provider";
+import type { ExportDocument, GlobalExportProperties } from "../../../../type";
+import { Bookmark, Paragraph, TextRun } from "docx";
+import {
+    HEADER_LEVEL_SECTION_TITLE,
+    HEADER_STYLE_SECTION_TITLE,
+    MAIN_TITLES_NUMBERING_ID,
+} from "./document-properties";
+
+export function getMilestoneTestPlanTitle(
+    gettext_provider: VueGettextProvider,
+    global_export_properties: GlobalExportProperties
+): { id: string; text: string } {
+    return {
+        id: "testplan",
+        text: gettext_provider.$gettextInterpolate(
+            gettext_provider.$gettext("%{ milestone_title } tests"),
+            { milestone_title: global_export_properties.milestone_name }
+        ),
+    };
+}
+
+export function buildMilestoneTestPlan(
+    docemunt: ExportDocument,
+    gettext_provider: VueGettextProvider,
+    global_export_properties: GlobalExportProperties
+): Paragraph[] {
+    const title = getMilestoneTestPlanTitle(gettext_provider, global_export_properties);
+
+    const section_title = new Paragraph({
+        heading: HEADER_LEVEL_SECTION_TITLE,
+        style: HEADER_STYLE_SECTION_TITLE,
+        numbering: {
+            reference: MAIN_TITLES_NUMBERING_ID,
+            level: 0,
+        },
+        children: [
+            new Bookmark({
+                id: title.id,
+                children: [new TextRun(title.text)],
+            }),
+        ],
+    });
+
+    return [
+        section_title,
+        new Paragraph(gettext_provider.$gettext("There are no tests planned in the milestone.")),
+    ];
+}
