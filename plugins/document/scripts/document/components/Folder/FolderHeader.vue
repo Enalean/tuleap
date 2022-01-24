Enalean/tuleap

commit 7b063a2908074afd30bd62bd03634ccdf6afb16c

Convert "New document" into typescript
part of request #18402 Convert document plugin into typescript

No functional changes,
you can click on "+ New document" everywhere (global button, quick look button and overview button) the modal will open and will enable you to create new document.
When folder have custom properties, they are prefilled in modal.

It also replace replace the EventBus[0] calls by vuex store

[0] https://v3.vuejs.org/guide/migration/events-api.html#overview

Change-Id: Ic61aa31b1b732ed44137ba1944c0538c495b396d

plugins/document/scripts/document/components/Folder/ModalNewItem/NewItemModal.vue
@@ -100,6 +100,7 @@ import { getCustomMetadata } from "../../../helpers/metadata-helpers/custom-meta
 import { handleErrors } from "../../../store/actions-helpers/handle-errors";
 import CreationModalPermissionsSection from "./CreationModalPermissionsSection.vue";
 import { transformCustomMetadataForItemCreation } from "../../../helpers/metadata-helpers/creation-data-transformatter-helper";
+import emitter from "../../../helpers/emitter";
 
 export default {
     name: "NewItemModal",
@@ -147,12 +148,11 @@ export default {
     },
     mounted() {
         this.modal = createModal(this.$el);
-        EventBus.$on("show-new-document-modal", this.show);
+        emitter.on("createItem", this.show);
         EventBus.$on("update-multiple-metadata-list-value", this.updateMultipleMetadataListValue);
         this.modal.addEventListener("tlp-modal-hidden", this.reset);
     },
     beforeDestroy() {
-        EventBus.$off("show-new-document-modal", this.show);
         EventBus.$off("update-multiple-metadata-list-value", this.updateMultipleMetadataListValue);
         this.modal.removeEventListener("tlp-modal-hidden", this.reset);
     },
@@ -185,7 +185,7 @@ export default {
         },
         async show(event) {
             this.item = this.getDefaultItem();
-            this.parent = event.detail.parent;
+            this.parent = event.item;
             this.addParentMetadataToDefaultItem();
             this.item.permissions_for_groups = JSON.parse(
                 JSON.stringify(this.parent.permissions_for_groups)

plugins/document/pnpm-lock.yaml
@@ -9,6 +9,7 @@ specifiers:
   '@tuleap/vue-breadcrumb-privacy': file:../../src/scripts/lib/vue-breadcrumb-privacy
   '@vue/test-utils': ^1.3.0
   fast-deep-equal: ^3.1.3
+  mitt: ^3.0.0
   moment: ^2.29.1
   moment-timezone: ^0.5.34
   moment-timezone-data-webpack-plugin: ^1.5.0
@@ -34,6 +35,7 @@ dependencies:
   '@tuleap/tlp-fetch': link:../../src/scripts/lib/tlp-fetch
   '@tuleap/vue-breadcrumb-privacy': link:../../src/scripts/lib/vue-breadcrumb-privacy
   fast-deep-equal: 3.1.3
+  mitt: 3.0.0
   moment: 2.29.1
   moment-timezone: 0.5.34
   pretty-kibibytes: 4.0.4
@@ -324,6 +326,10 @@ packages:
     dependencies:
       brace-expansion: 1.1.11
     dev: true
+
+  /mitt/3.0.0:
+    resolution: {integrity: sha512-7dX2/10ITVyqh4aOSVI9gdape+t9l2/8QxHrFmUXu4EEUpdlxl6RudZUPZoc+zuY2hk1j7XxVroIVIan/pD/SQ==}
+    dev: false
 
   /moment-timezone-data-webpack-plugin/1.5.0_moment-timezone@0.5.34:
     resolution: {integrity: sha512-eidUVGn6Fc8jR0tBf8xAhBR1C3jqknFJe0rfzThnglnJjmzqTXRYVTOeobUzWvlEfgTSu+b0W7GgOdqAWvGbYA==}

plugins/document/scripts/document/components/Folder/ActionsButton/NewItemButton.test.ts
@@ -16,47 +16,35 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
+import type { Wrapper } from "@vue/test-utils";
 import { shallowMount } from "@vue/test-utils";
 import NewItemButton from "./NewItemButton.vue";
-
+import type { Item } from "../../../type";
 import localVue from "../../../helpers/local-vue";
-import { createStoreMock } from "../../../../../../../src/scripts/vue-components/store-wrapper-jest.js";
-import EventBus from "../../../helpers/event-bus.js";
+import { TYPE_FOLDER } from "../../../constants";
+import mitt from "../../../helpers/emitter";
+
+jest.mock("../../../helpers/emitter");
 
 describe("NewItemButton", () => {
-    let factory;
-    beforeEach(() => {
-        const state = {};
-
-        const store_options = {
-            state,
-        };
-
-        const store = createStoreMock(store_options);
-
-        factory = (props = {}) => {
-            return shallowMount(NewItemButton, {
-                localVue,
-                propsData: { ...props },
-                mocks: { $store: store },
-            });
-        };
-    });
+    function createWrapper(item: Item): Wrapper<NewItemButton> {
+        return shallowMount(NewItemButton, {
+            localVue,
+            propsData: { item: item },
+        });
+    }
 
     it(`When user clicks on New item button
         Then it should open a modal`, () => {
-        const event_bus_emit = jest.spyOn(EventBus, "$emit");
-        const wrapper = factory({
-            item: {
-                id: 1,
-                title: "my item title",
-                type: "file",
-                user_can_write: true,
-            },
-        });
+        const item = {
+            type: TYPE_FOLDER,
+            user_can_write: true,
+        } as Item;
+
+        const wrapper = createWrapper(item);
 
         wrapper.get("[data-test=docman-new-item-button]").trigger("click");
 
-        expect(event_bus_emit).toHaveBeenCalledWith("show-new-document-modal", expect.any(Object));
+        expect(mitt.emit).toHaveBeenCalledWith("createItem", { item: item });
     });
 });

plugins/document/scripts/document/components/Folder/ActionsButton/NewItemButton.vue
@@ -31,17 +31,20 @@
     </button>
 </template>
 
-<script>
-import EventBus from "../../../helpers/event-bus.js";
+<script lang="ts">
+import emitter from "../../../helpers/emitter";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import type { Item } from "../../../type";
+import Vue from "vue";
 
-export default {
-    props: {
-        item: Object,
-    },
-    methods: {
-        showNewDocumentModal() {
-            EventBus.$emit("show-new-document-modal", { detail: { parent: this.item } });
-        },
-    },
-};
+@Component
+export default class NewItemButton extends Vue {
+    @Prop({ required: true })
+    readonly item!: Item;
+
+    showNewDocumentModal(): void {
+        emitter.emit("createItem", { item: this.item });
+    }
+}
 </script>

plugins/document/scripts/document/components/Folder/DropDown/NewDocument.test.ts
@@ -17,55 +17,50 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import type { Wrapper } from "@vue/test-utils";
 import { shallowMount } from "@vue/test-utils";
 import localVue from "../../../helpers/local-vue";
-import { createStoreMock } from "../../../../../../../src/scripts/vue-components/store-wrapper-jest.js";
 import NewDocument from "./NewDocument.vue";
 import { TYPE_FOLDER } from "../../../constants";
-import EventBus from "../../../helpers/event-bus.js";
+import type { Item } from "../../../type";
+import mitt from "../../../helpers/emitter";
+
+jest.mock("../../../helpers/emitter");
 
 describe("NewDocument", () => {
-    let new_item, store;
-    beforeEach(() => {
-        store = createStoreMock({});
-        new_item = (props = {}) => {
-            return shallowMount(NewDocument, {
-                localVue,
-                propsData: { ...props },
-                mocks: { $store: store },
-            });
-        };
-    });
+    function createWrapper(item: Item): Wrapper<NewDocument> {
+        return shallowMount(NewDocument, {
+            localVue,
+            propsData: { item: item },
+        });
+    }
 
     it(`User can create add document to folder when he is docman writer`, () => {
         const item = {
             type: TYPE_FOLDER,
             user_can_write: true,
-        };
+        } as Item;
 
-        const wrapper = new_item({ item });
+        const wrapper = createWrapper(item);
         expect(wrapper.find("[data-test=document-new-item]").exists()).toBeTruthy();
     });
     it(`User can NOT add document to folder when he is docman reader`, () => {
         const item = {
             type: TYPE_FOLDER,
             user_can_write: false,
-        };
+        } as Item;
 
-        const wrapper = new_item({ item });
+        const wrapper = createWrapper(item);
         expect(wrapper.find("[data-test=document-new-item]").exists()).toBeFalsy();
     });
     it(`Click on new document open the corresponding modal`, () => {
         const item = {
             type: TYPE_FOLDER,
             user_can_write: true,
-        };
-        const event_bus_emit = jest.spyOn(EventBus, "$emit");
+        } as Item;
 
-        const wrapper = new_item({ item });
+        const wrapper = createWrapper(item);
         wrapper.get("[data-test=document-new-item]").trigger("click");
-        expect(event_bus_emit).toHaveBeenCalledWith("show-new-document-modal", {
-            detail: { parent: item },
-        });
+        expect(mitt.emit).toHaveBeenCalledWith("createItem", { item: item });
     });
 });

plugins/document/scripts/document/components/Folder/DropDown/NewDocument.vue
@@ -23,7 +23,7 @@
         class="tlp-dropdown-menu-item"
         type="button"
         role="menuitem"
-        v-on:click.prevent="showNewDocumentModal"
+        v-on:click.prevent="showNewDocumentModal()"
         data-test="document-new-item"
         data-shortcut-create-document
     >
@@ -32,21 +32,22 @@
     </button>
 </template>
 
-<script>
-import EventBus from "../../../helpers/event-bus.js";
+<script lang="ts">
 import { isFolder } from "../../../helpers/type-check-helper";
-export default {
-    name: "NewDocument",
-    props: {
-        item: Object,
-    },
-    methods: {
-        showNewDocumentModal() {
-            EventBus.$emit("show-new-document-modal", { detail: { parent: this.item } });
-        },
-        is_item_a_folder(item) {
-            return isFolder(item);
-        },
-    },
-};
+import type { Item } from "../../../type";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import emitter from "../../../helpers/emitter";
+
+@Component
+export default class NewDocument extends Vue {
+    @Prop({ required: true })
+    readonly item!: Item;
+
+    showNewDocumentModal(): void {
+        emitter.emit("createItem", { item: this.item });
+    }
+    is_item_a_folder(item: Item): boolean {
+        return isFolder(item);
+    }
+}
 </script>

plugins/document/scripts/document/components/Folder/ModalNewItem/NewItemModal.test.js
@@ -22,8 +22,7 @@ import localVue from "../../../helpers/local-vue";
 
 import NewItemModal from "./NewItemModal.vue";
 import { createStoreMock } from "../../../../../../../src/scripts/vue-components/store-wrapper-jest.js";
-
-import EventBus from "../../../helpers/event-bus.js";
+import mitt from "../../../helpers/emitter";
 import * as tlp from "tlp";
 
 jest.mock("tlp");
@@ -95,9 +94,6 @@ describe("NewItemModal", () => {
 
         const wrapper = factory();
 
-        EventBus.$emit("show-new-document-modal", {
-            detail: { parent: store.state.current_folder },
-        });
         await wrapper.vm.$nextTick().then(() => {});
 
         expect(store.dispatch).not.toHaveBeenCalledWith("metadata/loadProjectMetadata");
@@ -120,9 +116,8 @@ describe("NewItemModal", () => {
         };
 
         const wrapper = factory();
-
-        EventBus.$emit("show-new-document-modal", {
-            detail: { parent: store.state.current_folder },
+        mitt.emit("createItem", {
+            item: store.state.current_folder,
         });
         await wrapper.vm.$nextTick().then(() => {});
 

plugins/document/scripts/document/helpers/emitter.ts
@@ -1,0 +1,22 @@
+/*
+ * Copyright (c) Enalean 2022 -  Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import mitt from "mitt";
+export default mitt();

commit a6afae8c6fa2f77abe0af7a428c2967f0b51c274

Make sure event listeners used to open the modals are destroyed with the components
This makes sure we do call modal.show() multiple times (and so multiple
backdrop elements).

Issues introduced by 7b063a2908074afd30bd62bd03634ccdf6afb16c and
06bbc7991d523a2d1538d14677a8adfca5d9880f.

Part of request #18402: Convert document plugin into typescript

Change-Id: I6beccc8b41c01004a58e7b7d741fcda084640564

plugins/document/scripts/document/components/Folder/FolderHeader.vue
@@ -148,7 +148,6 @@ export default {
     },
     computed: {
         ...mapState(["is_loading_ascendant_hierarchy", "current_folder"]),
-        ...mapState("modals", ["delete_item"]),
         ...mapGetters(["current_folder_title", "is_folder_empty"]),
         title_class() {
             return this.is_loading_ascendant_hierarchy
@@ -178,6 +177,7 @@ export default {
         EventBus.$on("show-changelog-modal", this.showChangelogModal);
     },
     beforeDestroy() {
+        emitter.off("deleteItem", this.showDeleteItemModal);
         EventBus.$off("show-create-new-item-version-modal", this.showCreateNewItemVersionModal);
         EventBus.$off("show-update-item-metadata-modal", this.showUpdateItemMetadataModal);
         EventBus.$off("show-update-permissions-modal", this.showUpdateItemPermissionsModal);

plugins/document/scripts/document/components/Folder/ItemDisplay/DisplayEmbeddedContent.vue
@@ -133,6 +133,7 @@ export default {
         EventBus.$on("show-update-permissions-modal", this.showUpdateItemPermissionsModal);
     },
     beforeDestroy() {
+        emitter.off("deleteItem", this.showDeleteItemModal);
         EventBus.$off("show-create-new-item-version-modal", this.showCreateNewItemVersionModal);
         EventBus.$off("show-update-item-metadata-modal", this.showUpdateMetadataModal);
         EventBus.$off("show-update-permissions-modal", this.showUpdateItemPermissionsModal);

plugins/document/scripts/document/components/Folder/ModalNewItem/NewItemModal.vue
@@ -153,6 +153,7 @@ export default {
         this.modal.addEventListener("tlp-modal-hidden", this.reset);
     },
     beforeDestroy() {
+        emitter.off("createItem", this.show);
         EventBus.$off("update-multiple-metadata-list-value", this.updateMultipleMetadataListValue);
         this.modal.removeEventListener("tlp-modal-hidden", this.reset);
     },

plugins/document/scripts/document/components/Folder/FolderHeader.test.js
@@ -33,7 +33,6 @@ describe("FolderHeader", () => {
         const general_store = {
             state: {
                 is_loading_ascendant_hierarchy: false,
-                modals: { delete_item: null },
             },
             getters: {
                 current_folder_title: "My folder title",

plugins/document/scripts/document/components/Folder/ItemDisplay/DisplayEmbeddedContent.test.js
@@ -27,9 +27,7 @@ describe("DisplayEmbeddedContent", () => {
 
     beforeEach(() => {
         const store_options = {
-            state: {
-                modals: { delete_item: null },
-            },
+            state: {},
         };
 
         store = createStoreMock(store_options);