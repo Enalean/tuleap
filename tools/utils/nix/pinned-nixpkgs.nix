0f472533eccba3affbfca2fe0444d50b97141cd2
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-02-03 15:39:19 +0100

    request #25346: Fix dev environment for Apple Silicon mac
    This contribution does two things:
    * it pulls an upstream fix so we can build `rpm`
    * it sets explicitly DOCKER_DEFAULT_PLATFORM env variable
    
    To limit the chances of breaking other environments the changes are only
    when running on macOS with a aarch64 CPU.
    
    Developers not relying on the environment provided through Nix will be
    forced to deal with it themselves as usual.
    
    Change-Id: Ic6d778ec3b5ba682b9addc639534852b5851b382

--- tools/utils/nix/pinned-nixpkgs.nix
+++ tools/utils/nix/pinned-nixpkgs.nix
@@ -5,5 +5,19 @@ let
   pinnedNixpkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsJson.rev}.tar.gz";
     sha256 = nixpkgsJson.sha256;
-  } ) {};
+  } ) {
+    overlays = [
+      (self: super: {
+        # Override rpm to fix build issues macOS ARM
+        rpm = super.rpm.overrideAttrs (oldAttrs: rec {
+          patches = oldAttrs.patches or [] ++ self.lib.optionals (self.stdenvNoCC.hostPlatform.isDarwin && self.stdenvNoCC.hostPlatform.isAarch64) [
+            (self.fetchpatch {
+              url = "https://github.com/rpm-software-management/rpm/commit/ad87ced3990c7e14b6b593fa411505e99412e248.patch";
+              sha256 = "sha256-WYlxPGcPB5lGQmkyJ/IpGoqVfAKtMxKzlr5flTqn638=";
+            })
+          ];
+        });
+      })
+    ];
+  };
 in pinnedNixpkgs
