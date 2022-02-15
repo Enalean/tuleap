Enalean/tuleap

commit 1ea4d41e9a6273915943155e60dcdf3bf388085f

Project sidebar is now a block element with an explicit height and width
By default custom element are considered to be inline element which does
not make a lot of sense for our situation. Also we know set a width and
height to the component so it is easier to do manipulation on it (e.g.
moving of XXXpx from the top to add a platform banner).

Part of story #24227: have private oauth2 endpoints for Mediawiki

Change-Id: I3cfa4d113a9ab6e60a4abe313e4766897e0f30ea

src/scripts/lib/project-sidebar-internal/src/ProjectSidebar.vue
@@ -21,24 +21,23 @@
   -->
 
 <template>
-    <div
+    <aside
         v-if="sidebar_configuration !== undefined"
+        class="sidebar"
         v-bind:class="{ 'sidebar-collapsed': props.collapsed }"
     >
-        <aside class="sidebar">
-            <sidebar-logo />
-            <div class="sidebar-content-vertical-scroll">
-                <sidebar-header />
-                <tools />
-                <sidebar-collapse-button
-                    v-model:is_sidebar_collapsed="is_sidebar_collapsed"
-                    v-bind:can_sidebar_be_collapsed="can_sidebar_be_collapsed"
-                />
-            </div>
-            <div class="sidebar-spacer"></div>
-            <sidebar-footer />
-        </aside>
-    </div>
+        <sidebar-logo />
+        <div class="sidebar-content-vertical-scroll">
+            <sidebar-header />
+            <tools />
+            <sidebar-collapse-button
+                v-model:is_sidebar_collapsed="is_sidebar_collapsed"
+                v-bind:can_sidebar_be_collapsed="can_sidebar_be_collapsed"
+            />
+        </div>
+        <div class="sidebar-spacer"></div>
+        <sidebar-footer />
+    </aside>
 </template>
 <script setup lang="ts">
 import { unserializeConfiguration } from "./configuration";
@@ -77,6 +76,7 @@ watch(is_sidebar_collapsed, (): void => {
 </script>
 <style lang="scss">
 @use "../../../themes/tlp/src/scss/components/typography";
+@use "../../../themes/BurningParrot/css/includes/global-variables";
 @use "../../../themes/BurningParrot/css/includes/sidebar/sidebar-generic";
 @use "../../../themes/BurningParrot/css/includes/sidebar/sidebar-collapsed";
 @use "../../../themes/BurningParrot/css/includes/sidebar/sidebar-project";
@@ -86,11 +86,30 @@ watch(is_sidebar_collapsed, (): void => {
 @use "../../../themes/tlp/src/fonts/tlp-font/icons";
 @use "@tuleap/tlp-popovers";
 @use "../../../themes/BurningParrot/css/includes/project-privacy-popover";
+
+:host {
+    display: block;
+    contain: content;
+    width: global-variables.$sidebar-expanded-width;
+    height: 100vh;
+}
+
+:host([collapsed]) {
+    width: global-variables.$sidebar-collapsed-width;
+}
 </style>
-<style scoped>
+<style lang="scss" scoped>
+@use "../../../themes/BurningParrot/css/includes/global-variables";
+
 .sidebar {
+    height: 100%;
     font-family: var(--tlp-font-family);
     font-size: 100%;
+}
+
+.sidebar-collapsed {
+    width: global-variables.$sidebar-collapsed-width;
+    padding: 0;
 }
 
 /* stylelint-disable-next-line selector-pseudo-class-no-unknown -- Stylelint does not know about the Vue :deep() selector */

src/scripts/lib/project-sidebar-internal/src/__snapshots__/ProjectSidebar.test.ts.snap
@@ -1,27 +1,23 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
 exports[`ProjectSidebar displays sidebar 1`] = `
-<div
-  class=""
+<aside
+  class="sidebar"
 >
-  <aside
-    class="sidebar"
+  <sidebar-logo-stub />
+  <div
+    class="sidebar-content-vertical-scroll"
   >
-    <sidebar-logo-stub />
-    <div
-      class="sidebar-content-vertical-scroll"
-    >
-      <sidebar-header-stub />
-      <tools-stub />
-      <sidebar-collapse-button-stub
-        can_sidebar_be_collapsed="true"
-        is_sidebar_collapsed="false"
-      />
-    </div>
-    <div
-      class="sidebar-spacer"
+    <sidebar-header-stub />
+    <tools-stub />
+    <sidebar-collapse-button-stub
+      can_sidebar_be_collapsed="true"
+      is_sidebar_collapsed="false"
     />
-    <sidebar-footer-stub />
-  </aside>
-</div>
+  </div>
+  <div
+    class="sidebar-spacer"
+  />
+  <sidebar-footer-stub />
+</aside>
 `;

commit 739f85a557fe0bc8db19dc8713a56b8962904d52

Fix display of privacy/badges popovers
Issue introduced by 1ea4d41e9a6273915943155e60dcdf3bf388085f.
Containement instruction was a bit too strict for the popovers that are
displayed outside the limit of the component itself.

Part of story #24227: have private oauth2 endpoints for Mediawiki

Change-Id: I5f9cbea191dfecd5f8e46e6f04f3fabe5ac4e2ed

src/scripts/lib/project-sidebar-internal/src/ProjectSidebar.vue
@@ -89,7 +89,7 @@ watch(is_sidebar_collapsed, (): void => {
 
 :host {
     display: block;
-    contain: content;
+    contain: layout;
     width: global-variables.$sidebar-expanded-width;
     height: 100vh;
 }