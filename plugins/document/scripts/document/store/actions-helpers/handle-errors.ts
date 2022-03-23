331db291fa6bf87ab820824328b54fec99e23cdd
Nicolas Terray <nicolas.terray@enalean.com>
2022-03-23 16:39:43 +0100

    chore: Convert deleteItem action to TypeScript
    No functional changes.
    
    Note: shouldRedirectToParentAfterDeletion was not mandatory before the
    switch to TS at 45c9f924297ebcd97d902dec20096013ccf9f57d We set it back
    to default false to avoid error in console.
    
    Note: handleErrors*() do not depends anymore on
    ActionContext<ErrorState, ErrorState> because those methods can be
    called from actions (ActionContext) and from vue componenents
    (Store with this.$store). In order to support both usage we now require
    to pass an object with `commit` method (shared with both ActionContext
    and Store).
    
    Note: in order to not loose our mind to convert the whole actions.js to
    TypeScript, an independant actions-typescript.ts has been extracted. It
    will be renamed after the conversion is finished.
    
    Part of request #18402: Convert document plugin into typescript
    
    Change-Id: Id8fcc25ea3d98a0a7329d8ba7402b8287173854c

--- plugins/document/scripts/document/store/actions-helpers/handle-errors.ts
+++ plugins/document/scripts/document/store/actions-helpers/handle-errors.ts
@@ -17,10 +17,9 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import type { ActionContext } from "vuex";
-import type { ErrorState } from "../error/module";
-import type { Item } from "../../type";
+import type { Item, RootState } from "../../type";
 import { FetchWrapperError } from "tlp";
+import type { Store } from "vuex";
 
 interface DocumentJsonError {
     error: JsonError;
@@ -32,73 +31,79 @@ interface JsonError {
 }
 
 export async function handleErrors(
-    context: ActionContext<ErrorState, ErrorState>,
+    store: {
+        commit: Store<RootState>["commit"];
+    },
     exception: unknown
 ): Promise<void> {
     const message = "Internal server error";
     if (!(exception instanceof FetchWrapperError) || exception.response === undefined) {
-        context.commit("error/setFolderLoadingError", message);
+        store.commit("error/setFolderLoadingError", message);
         throw exception;
     }
 
     const status = exception.response.status;
     if (status === 403) {
-        context.commit("error/switchFolderPermissionError");
+        store.commit("error/switchFolderPermissionError");
         return;
     }
 
     try {
         const json = await exception.response.json();
-        context.commit("error/setFolderLoadingError", getErrorMessage(json));
+        store.commit("error/setFolderLoadingError", getErrorMessage(json));
     } catch (error) {
-        context.commit("error/setFolderLoadingError", message);
+        store.commit("error/setFolderLoadingError", message);
     }
 }
 
 export async function handleErrorsForDocument(
-    context: ActionContext<ErrorState, ErrorState>,
+    store: {
+        commit: Store<RootState>["commit"];
+    },
     exception: unknown
 ): Promise<void> {
     const message = "Internal server error";
     if (!(exception instanceof FetchWrapperError) || exception.response === undefined) {
-        context.commit("error/setItemLoadingError", message);
+        store.commit("error/setItemLoadingError", message);
         throw exception;
     }
 
     const status = exception.response.status;
     if (status === 403) {
-        context.commit("error/switchItemPermissionError");
+        store.commit("error/switchItemPermissionError");
         return;
     }
 
     try {
         const json = await exception.response.json();
-        context.commit("error/setItemLoadingError", getErrorMessage(json));
+        store.commit("error/setItemLoadingError", getErrorMessage(json));
     } catch (error) {
-        context.commit("error/setItemLoadingError", message);
+        store.commit("error/setItemLoadingError", message);
     }
 }
 
 export async function handleErrorsForDeletionModal(
-    context: ActionContext<ErrorState, ErrorState>,
+    store: {
+        commit: Store<RootState>["commit"];
+    },
     exception: unknown,
     item: Item
 ): Promise<void> {
     const message = "Internal server error";
     if (!(exception instanceof FetchWrapperError) || exception.response === undefined) {
-        context.commit("error/setModalError", message);
+        store.commit("error/setModalError", message);
         throw exception;
     }
     try {
         const json = await exception.response.json();
-        context.commit("error/setModalError", getErrorMessage(json));
+        store.commit("error/setModalError", getErrorMessage(json));
 
         if (json.error.code === 404) {
-            context.commit("removeItemFromFolderContent", item);
-            context.commit("updateCurrentlyPreviewedItem", null);
+            store.commit("removeItemFromFolderContent", item);
+            store.commit("updateCurrentlyPreviewedItem", null);
         }
     } catch (error) {
-        context.commit("error/setModalError", message);
+        store.commit("error/setModalError", message);
     }
 }
 
---
+++ plugins/document/scripts/document/store/actions-typescript.test.ts
@@ -0,0 +1,215 @@
+/**
+ * Copyright (c) Enalean, 2022 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+    TYPE_EMBEDDED,
+    TYPE_EMPTY,
+    TYPE_FILE,
+    TYPE_FOLDER,
+    TYPE_LINK,
+    TYPE_WIKI,
+} from "../constants";
+import * as rest_querier from "../api/rest-querier";
+import { deleteItem } from "./actions-typescript";
+import { mockFetchError, mockFetchSuccess } from "@tuleap/tlp-fetch/mocks/tlp-fetch-mock-helper";
+import type { Item, RootState } from "../type";
+import type { ActionContext } from "vuex";
+
+describe("actions", () => {
+    describe("deleteItem()", () => {
+        let context: ActionContext<RootState, RootState>;
+
+        beforeEach(() => {
+            context = {
+                commit: jest.fn(),
+            } as unknown as ActionContext<RootState, RootState>;
+        });
+
+        it("when item is a file, then the delete file route is called", async () => {
+            const file_item = {
+                id: 111,
+                title: "My File",
+                type: TYPE_FILE,
+            } as Item;
+
+            const deleteFile = jest.spyOn(rest_querier, "deleteFile");
+            mockFetchSuccess(deleteFile);
+
+            await deleteItem(context, [file_item]);
+            expect(deleteFile).toHaveBeenCalledWith(file_item);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                file_item
+            );
+        });
+
+        it("when item is a link, then the delete link route is called", async () => {
+            const link_item = {
+                id: 222,
+                title: "My Link",
+                type: TYPE_LINK,
+            } as Item;
+
+            mockFetchSuccess(jest.spyOn(rest_querier, "deleteLink"));
+
+            await deleteItem(context, [link_item]);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                link_item
+            );
+        });
+
+        it("when item is an embedded file, then the delete embedded file route is called", async () => {
+            const embedded_file_item = {
+                id: 222,
+                title: "My embedded file",
+                type: TYPE_EMBEDDED,
+            } as Item;
+
+            const deleteEmbeddedFile = jest.spyOn(rest_querier, "deleteEmbeddedFile");
+            mockFetchSuccess(deleteEmbeddedFile);
+
+            await deleteItem(context, [embedded_file_item]);
+            expect(deleteEmbeddedFile).toHaveBeenCalledWith(embedded_file_item);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                embedded_file_item
+            );
+        });
+
+        it("when item is a wiki, then the delete wiki route is called", async () => {
+            const wiki_item = {
+                id: 222,
+                title: "My Wiki",
+                type: TYPE_WIKI,
+            } as Item;
+
+            const deleteWiki = jest.spyOn(rest_querier, "deleteWiki");
+            mockFetchSuccess(deleteWiki);
+
+            const additional_options = { delete_associated_wiki_page: true };
+
+            await deleteItem(context, [wiki_item, additional_options]);
+            expect(deleteWiki).toHaveBeenCalledWith(wiki_item, additional_options);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                wiki_item
+            );
+        });
+
+        it("when item is an empty document, then the delete empty document route is called", async () => {
+            const empty_doc_item = {
+                id: 222,
+                title: "My empty document",
+                type: TYPE_EMPTY,
+            } as Item;
+
+            const deleteEmptyDocument = jest.spyOn(rest_querier, "deleteEmptyDocument");
+            mockFetchSuccess(deleteEmptyDocument);
+
+            await deleteItem(context, [empty_doc_item]);
+            expect(deleteEmptyDocument).toHaveBeenCalledWith(empty_doc_item);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                empty_doc_item
+            );
+        });
+
+        it("when item is a folder, then the delete folder route is called", async () => {
+            const folder_item = {
+                id: 222,
+                title: "My folder",
+                type: TYPE_FOLDER,
+            } as Item;
+
+            const deleteFolder = jest.spyOn(rest_querier, "deleteFolder");
+            mockFetchSuccess(deleteFolder);
+
+            await deleteItem(context, [folder_item]);
+            expect(deleteFolder).toHaveBeenCalledWith(folder_item);
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                folder_item
+            );
+        });
+
+        it("deletes the given item and removes it from the tree view", async () => {
+            const item_to_delete = {
+                id: 123,
+                title: "My file",
+                type: TYPE_FILE,
+            } as Item;
+
+            mockFetchSuccess(jest.spyOn(rest_querier, "deleteFile"));
+
+            await deleteItem(context, [item_to_delete]);
+
+            expect(context.commit).toHaveBeenCalledWith(
+                "removeItemFromFolderContent",
+                item_to_delete
+            );
+            expect(context.commit).toHaveBeenCalledWith(
+                "clipboard/emptyClipboardAfterItemDeletion",
+                item_to_delete
+            );
+        });
+
+        it("display error if something wrong happens", async () => {
+            const folder_item = {
+                id: 222,
+                title: "My folder",
+                type: TYPE_FOLDER,
+            } as Item;
+
+            mockFetchError(jest.spyOn(rest_querier, "deleteFolder"), {
+                status: 400,
+            });
+
+            await deleteItem(context, [folder_item]);
+
+            expect(context.commit).toHaveBeenCalledWith(
+                "error/setModalError",
+                "Internal server error"
+            );
+        });
+
+        it("mark item as unknown when rest route fails with 404", async () => {
+            const folder_item = {
+                id: 222,
+                title: "My folder",
+                type: TYPE_FOLDER,
+            } as Item;
+
+            mockFetchError(jest.spyOn(rest_querier, "deleteFolder"), {
+                error_json: {
+                    error: {
+                        code: 404,
+                        i18n_error_message: "not found",
+                    },
+                },
+            });
+
+            await deleteItem(context, [folder_item]);
+
+            expect(context.commit).toHaveBeenCalledWith("error/setModalError", "not found");
+            expect(context.commit).toHaveBeenCalledWith("removeItemFromFolderContent", folder_item);
+            expect(context.commit).toHaveBeenCalledWith("updateCurrentlyPreviewedItem", null);
+        });
+    });
+});
