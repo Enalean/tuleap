43cb2f29250ba25840fba86393f8c5f88459b0d1
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2022-04-04 10:36:51 +0200

    chore: Extract common fit* methods in the XLSX shared lib
    This is part of story #23469: export reports with linked artifact in xlsx format
    
    No functional change expected here.
    
    Change-Id: I3641f83765eefa4e6f58b04cd267539b6aec054c

---
+++ plugins/document_generation/scripts/lib/xlsx/src/build-xlsx.ts
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) Enalean, 2022 - present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { ColInfo, RowInfo } from "xlsx";
+import type { CellObjectWithExtraInfo } from "./type";
+
+const CELL_MAX_CHARACTER_WIDTH = 65;
+const LINE_HEIGHT_POINTS = 12;
+
+export function fitColumnWidthsToContent(cells: CellObjectWithExtraInfo[][]): ColInfo[] {
+    const max_column_width: number[] = [];
+
+    cells.forEach((row: CellObjectWithExtraInfo[]): void => {
+        row.forEach((cell: CellObjectWithExtraInfo, column_position: number): void => {
+            const current_max_value = max_column_width[column_position];
+            max_column_width[column_position] = Math.min(
+                Math.max(isNaN(current_max_value) ? 0 : current_max_value, cell.character_width),
+                CELL_MAX_CHARACTER_WIDTH
+            );
+        });
+    });
+
+    return max_column_width.map((column_width: number): ColInfo => {
+        return { wch: column_width };
+    });
+}
+
+export function fitRowHeightsToContent(cells: CellObjectWithExtraInfo[][]): RowInfo[] {
+    const row_info: RowInfo[] = [];
+    cells.forEach((row: CellObjectWithExtraInfo[]): void => {
+        const nb_lines_row = row.reduce(
+            (previous: { nb_lines: number }, current: { nb_lines: number }) =>
+                previous.nb_lines > current.nb_lines ? previous : current,
+            { nb_lines: 1 }
+        ).nb_lines;
+        if (nb_lines_row >= 2) {
+            row_info.push({ hpt: nb_lines_row * LINE_HEIGHT_POINTS });
+        } else {
+            row_info.push({});
+        }
+    });
+
+    return row_info;
+}
--- plugins/document_generation/scripts/tracker-cross-report-action/src/Exporter/XLSX/download-xlsx.ts
+++ plugins/document_generation/scripts/tracker-cross-report-action/src/Exporter/XLSX/download-xlsx.ts
@@ -17,20 +17,13 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import type { CellObject, ColInfo, RowInfo } from "xlsx";
 import { utils, writeFile } from "xlsx";
 import type { ReportSection } from "../../Data/data-formator";
-import type { ReportCell } from "@tuleap/plugin-docgen-xlsx";
+import type { ReportCell, CellObjectWithExtraInfo } from "@tuleap/plugin-docgen-xlsx";
+import { fitColumnWidthsToContent, fitRowHeightsToContent } from "@tuleap/plugin-docgen-xlsx";
 import type { ExportSettings } from "../../export-document";
 
-const CELL_MAX_CHARACTER_WIDTH = 65;
-const LINE_HEIGHT_POINTS = 12;
 const CELL_BASE_CHARACTER_WIDTH = 10;
-
-type CellObjectWithWidthAndLines = CellObject & {
-    character_width: number;
-    nb_lines: number;
-};
 
 export function downloadXLSX(export_settings: ExportSettings, formatted_data: ReportSection): void {
     const book = utils.book_new();
@@ -54,9 +47,9 @@ export function downloadXLSX(export_settings: ExportSettings, formatted_data: Re
 function buildContent(
     export_settings: ExportSettings,
     formatted_data: ReportSection
-): Array<Array<CellObjectWithWidthAndLines>> {
-    const content: CellObjectWithWidthAndLines[][] = [];
-    const report_columns_label: CellObjectWithWidthAndLines[] = [];
+): Array<Array<CellObjectWithExtraInfo>> {
+    const content: CellObjectWithExtraInfo[][] = [];
+    const report_columns_label: CellObjectWithExtraInfo[] = [];
     if (formatted_data.headers && formatted_data.headers.length > 0) {
         for (const header of formatted_data.headers) {
             report_columns_label.push(transformFormattedCellIntoASheetCell(header));
@@ -64,7 +57,7 @@ function buildContent(
         content.push(report_columns_label);
     }
 
-    let artifact_value_rows: CellObjectWithWidthAndLines[] = [];
+    let artifact_value_rows: CellObjectWithExtraInfo[] = [];
     if (formatted_data.rows && formatted_data.rows.length > 0) {
         for (const row of formatted_data.rows) {
             artifact_value_rows = [];
@@ -78,9 +71,7 @@ function buildContent(
     return content;
 }
 
-function transformFormattedCellIntoASheetCell(
-    formatted_cell: ReportCell
-): CellObjectWithWidthAndLines {
+function transformFormattedCellIntoASheetCell(formatted_cell: ReportCell): CellObjectWithExtraInfo {
     switch (formatted_cell.type) {
         case "text":
             return buildSheetTextCell(formatted_cell.value);
@@ -111,7 +102,7 @@ function transformFormattedCellIntoASheetCell(
     }
 }
 
-function buildSheetTextCell(value: string): CellObjectWithWidthAndLines {
+function buildSheetTextCell(value: string): CellObjectWithExtraInfo {
     const text_value = value.replace(/\r\n/g, "\n").trim();
     const text_value_by_lines = text_value.split("\n");
     const max_length_line = text_value_by_lines.reduce(
@@ -130,40 +121,4 @@ function buildSheetTextCell(value: string): CellObjectWithWidthAndLines {
 function extractPlaintextFromHTMLString(html: string): string {
     const dom_parser = new DOMParser();
     return dom_parser.parseFromString(html, "text/html").body.textContent ?? "";
-}
-
-function fitColumnWidthsToContent(cells: CellObjectWithWidthAndLines[][]): ColInfo[] {
-    const max_column_width: number[] = [];
-
-    cells.forEach((row: CellObjectWithWidthAndLines[]): void => {
-        row.forEach((cell: CellObjectWithWidthAndLines, column_position: number): void => {
-            const current_max_value = max_column_width[column_position];
-            max_column_width[column_position] = Math.min(
-                Math.max(isNaN(current_max_value) ? 0 : current_max_value, cell.character_width),
-                CELL_MAX_CHARACTER_WIDTH
-            );
-        });
-    });
-
-    return max_column_width.map((column_width: number): ColInfo => {
-        return { wch: column_width };
-    });
-}
-
-function fitRowHeightsToContent(cells: CellObjectWithWidthAndLines[][]): RowInfo[] {
-    const row_info: RowInfo[] = [];
-    cells.forEach((row: CellObjectWithWidthAndLines[]): void => {
-        const nb_lines_row = row.reduce(
-            (previous: { nb_lines: number }, current: { nb_lines: number }) =>
-                previous.nb_lines > current.nb_lines ? previous : current,
-            { nb_lines: 1 }
-        ).nb_lines;
-        if (nb_lines_row >= 2) {
-            row_info.push({ hpt: nb_lines_row * LINE_HEIGHT_POINTS });
-        } else {
-            row_info.push({});
-        }
-    });
-
-    return row_info;
 }
