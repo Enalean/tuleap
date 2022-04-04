7cf71eb7ffba8f47dded2d9caa1ff785a42dd62f
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2022-04-04 14:51:54 +0200

    chore: Move report cell transformation into the shared XLSX lib
    This is part of story #23469: export reports with linked artifact in xlsx format
    
    No functional change expected here.
    
    Change-Id: I037e098cc71e52dc876d0983986501244bb1bde1

--- plugins/testplan/scripts/test-plan/src/helpers/ExportAsSpreadsheet/Exporter/XLSX/transform-report-to-xlsx-sheet.ts
+++ plugins/testplan/scripts/test-plan/src/helpers/ExportAsSpreadsheet/Exporter/XLSX/transform-report-to-xlsx-sheet.ts
@@ -17,11 +17,17 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import type { Comments, Range, WorkSheet } from "xlsx";
+import type { Range, WorkSheet } from "xlsx";
 import { utils } from "xlsx";
 import type { ExportReport, ReportSection } from "../../Report/report-creator";
 import type { ReportCell, CellObjectWithExtraInfo } from "@tuleap/plugin-docgen-xlsx";
-import { fitColumnWidthsToContent, fitRowHeightsToContent } from "@tuleap/plugin-docgen-xlsx";
+import {
+    fitColumnWidthsToContent,
+    fitRowHeightsToContent,
+    transformReportCellIntoASheetCell,
+    buildSheetTextCell,
+    buildSheetEmptyCell,
+} from "@tuleap/plugin-docgen-xlsx";
 
 const CELL_BASE_CHARACTER_WIDTH = 10;
 
@@ -72,79 +78,6 @@ function transformReportSectionRowsIntoSheetRows(
     report_section_row: ReadonlyArray<ReportCell>
 ): CellObjectWithExtraInfo[] {
     return report_section_row.map(transformReportCellIntoASheetCell);
-}
-
-function transformReportCellIntoASheetCell(report_cell: ReportCell): CellObjectWithExtraInfo {
-    let sheet_cell: CellObjectWithExtraInfo;
-    switch (report_cell.type) {
-        case "text":
-            sheet_cell = buildSheetTextCell(report_cell.value);
-            break;
-        case "html":
-            sheet_cell = buildSheetTextCell(extractPlaintextFromHTMLString(report_cell.value));
-            break;
-        case "date":
-            sheet_cell = {
-                t: "d",
-                v: report_cell.value,
-                character_width: CELL_BASE_CHARACTER_WIDTH,
-                nb_lines: 1,
-            };
-            break;
-        case "number":
-            sheet_cell = {
-                t: "n",
-                v: report_cell.value,
-                character_width: String(report_cell.value).length,
-                nb_lines: 1,
-            };
-            break;
-        case "empty":
-            sheet_cell = buildSheetEmptyCell();
-            break;
-        default:
-            return ((val: never): never => val)(report_cell);
-    }
-
-    if (report_cell.comment) {
-        const comments: Comments = [{ t: report_cell.comment }];
-        comments.hidden = true;
-        return {
-            ...sheet_cell,
-            c: comments,
-        };
-    }
-
-    return sheet_cell;
-}
-
-function buildSheetTextCell(value: string): CellObjectWithExtraInfo {
-    const text_value = value.replace(/\r\n/g, "\n").trim();
-    const text_value_by_lines = text_value.split("\n");
-    const max_length_line = text_value_by_lines.reduce(
-        (previous: string, current: string) =>
-            previous.length > current.length ? previous : current,
-        ""
-    ).length;
-    return {
-        t: "s",
-        v: text_value,
-        character_width: max_length_line,
-        nb_lines: text_value_by_lines.length,
-    };
-}
-
-function buildSheetEmptyCell(): CellObjectWithExtraInfo {
-    return {
-        t: "z",
-        character_width: 0,
-        nb_lines: 1,
-    };
-}
-
-function extractPlaintextFromHTMLString(html: string): string {
-    const dom_parser = new DOMParser();
-    return dom_parser.parseFromString(html, "text/html").body.textContent ?? "";
 }
 
 function createMerges(cells: CellObjectWithExtraInfo[][]): Range[] {
