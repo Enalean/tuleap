08ab0f85918d8e84436ba7039adf9f39ed6ee3e7
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-04-05 10:50:17 +0200

    `cachix watch-exec` conflicts with `nix-build` options
    Two mistakes were made in 340de830e37a56bc60a228beeb5eed9160e8bee0:
    * the name of the cache to push to was forgotten
    * the options we use with `nix-build` conflicts with `cachix`
    
    As a result some packages are not built in the build pipeline.
    To avoid more surprises, the call to `nix-build` is moved to a dedicated
    script.
    
    Part of request #26358: Cache the results of the Nix derivations used when building the packages
    
    Change-Id: I02a49ff12b99327321336eb0ba8f8a76c773aa08

--- tools/rpm/build_rpm_inside_container.sh
+++ tools/rpm/build_rpm_inside_container.sh
@@ -7,7 +7,7 @@ RPM_BUILD="/rpms"
 
 push_nix_build_cache=''
 if [ -s "/cachix_auth_token" ]; then
-    push_nix_build_cache='cachix watch-exec'
+    push_nix_build_cache='cachix watch-exec tuleap-community'
     cachix authtoken --stdin < /cachix_auth_token
 fi
 
@@ -18,7 +18,7 @@ OS='rhel7' make -C "$TULEAP_SOURCES/tools/rpm" rpm RPM_TMP="$RPM_BUILD"
 find "$TULEAP_SOURCES" \( -wholename "$TULEAP_SOURCES/src/additional-packages/*.nix" -o -wholename "$TULEAP_SOURCES/plugins/*/additional-packages/*.nix" \) \
     -type f -print0 | while IFS= read -r -d '' nix_file; do
     echo "Processing $nix_file"
-    ${push_nix_build_cache} nix-build "$nix_file" --out-link /tmp/result
+    ${push_nix_build_cache} "$(dirname "$0")"/build_nix_derivation.sh "$nix_file" /tmp/result
     cp -v -r /tmp/result/. "$RPM_BUILD/RPMS/noarch/"
 done
 chmod -v --recursive u+w "$RPM_BUILD/RPMS/noarch/"
