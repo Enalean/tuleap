3f53d6e0498fd514a423f74b0690f1ab6d2d044c
Clarck Robinson <clarck.robinson@enalean.com>
2022-04-08 09:49:56 +0200

    request #26378 Fix the file creation modal from DnD location
    No functional change expected.
    
    Change-Id: I61c13209bbae02a6d0ad7e12226d374a6ac93f3f

--- plugins/document/scripts/document/components/Folder/DropDown/NewDocument/FileCreationModal.vue
+++ plugins/document/scripts/document/components/Folder/DropDown/NewDocument/FileCreationModal.vue
@@ -1,3 +1,22 @@
+<!--
+  - Copyright (c) Enalean, 2022 - present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  -  along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
 <template>
     <form
         class="tlp-modal"
@@ -31,7 +50,7 @@
 <script setup lang="ts">
 import type { Modal } from "tlp";
 import { createModal } from "tlp";
-import DocumentGlobalPropertyForCreate from "../NewDocument/PropertiesForCreate/DocumentGlobalPropertyForCreate.vue";
+import DocumentGlobalPropertyForCreate from "./PropertiesForCreate/DocumentGlobalPropertyForCreate.vue";
 import type { DefaultFileItem, Folder, RootState } from "../../../../type";
 import { TYPE_FILE } from "../../../../constants";
 import ModalHeader from "../../ModalCommon/ModalHeader.vue";
