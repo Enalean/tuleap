Enalean/tuleap

commit 775b113cc7eca0c4e7f55c826a4b441610d0deb1

Convert permissions store to typescript
part of request #18402 Convert document plugin into typescript

No functional changes:
 - you can update document property directly on a document
 - you can update folder properties and apply changes to its content

Change-Id: Ia4e51b3556935778465a1dc70edc56b4b948199d

plugins/document/scripts/document/store/error/error-actions.ts
@@ -18,8 +18,11 @@
  */
 
 import type { ActionContext } from "vuex";
-import type { State } from "../../type";
+import type { RootState, State } from "../../type";
 import type { FetchWrapperError } from "@tuleap/tlp-fetch";
+import type { DocumentException } from "../actions-helpers/handle-errors";
+import { getErrorMessage } from "../actions-helpers/handle-errors";
+import type { ErrorState } from "./module";
 
 export async function handleGlobalModalError(
     context: ActionContext<State, State>,
@@ -30,5 +33,22 @@ export async function handleGlobalModalError(
         context.commit("setGlobalModalErrorMessage", error.code + " " + error.message);
     } catch (e) {
         context.commit("setGlobalModalErrorMessage", "");
+    }
+}
+
+export async function handleErrorsForModal(
+    context: ActionContext<ErrorState, RootState>,
+    exception: DocumentException
+): Promise<void> {
+    const message = "Internal server error";
+    if (exception.response === undefined) {
+        context.commit("error/setModalError", message);
+        throw exception;
+    }
+    try {
+        const json = await exception.response.json();
+        context.commit("error/setModalError", getErrorMessage(json));
+    } catch (error) {
+        context.commit("error/setModalError", message);
     }
 }

plugins/document/scripts/document/api/permissions-rest-querier.test.ts
@@ -34,6 +34,7 @@ jest.mock("tlp");
 describe("Update item permissions", () => {
     const item_id = 123;
     const permissions = {
+        apply_permissions_on_children: true,
         can_read: [],
         can_write: [],
         can_manage: [],

plugins/document/scripts/document/api/permissions-rest-querier.ts
@@ -29,8 +29,8 @@ export {
     putFolderPermissions,
 };
 
-function putEmbeddedFilePermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_embedded_files/${encodeURIComponent(id)}/permissions`, {
+async function putEmbeddedFilePermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_embedded_files/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -38,8 +38,8 @@ function putEmbeddedFilePermissions(id: number, permissions: Permissions): Promi
     });
 }
 
-function putFilePermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_files/${encodeURIComponent(id)}/permissions`, {
+async function putFilePermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_files/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -47,8 +47,8 @@ function putFilePermissions(id: number, permissions: Permissions): Promise<Respo
     });
 }
 
-function putLinkPermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_links/${encodeURIComponent(id)}/permissions`, {
+async function putLinkPermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_links/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -56,8 +56,8 @@ function putLinkPermissions(id: number, permissions: Permissions): Promise<Respo
     });
 }
 
-function putWikiPermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_wikis/${encodeURIComponent(id)}/permissions`, {
+async function putWikiPermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_wikis/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -65,8 +65,8 @@ function putWikiPermissions(id: number, permissions: Permissions): Promise<Respo
     });
 }
 
-function putEmptyDocumentPermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_empty_documents/${encodeURIComponent(id)}/permissions`, {
+async function putEmptyDocumentPermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_empty_documents/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -74,8 +74,8 @@ function putEmptyDocumentPermissions(id: number, permissions: Permissions): Prom
     });
 }
 
-function putFolderPermissions(id: number, permissions: Permissions): Promise<Response> {
-    return put(`/api/docman_folders/${encodeURIComponent(id)}/permissions`, {
+async function putFolderPermissions(id: number, permissions: Permissions): Promise<void> {
+    await put(`/api/docman_folders/${encodeURIComponent(id)}/permissions`, {
         headers: {
             "Content-Type": "application/json",
         },

plugins/document/scripts/document/components/Folder/Permissions/PermissionsUpdateModal.test.js
@@ -173,7 +173,7 @@ describe("PermissionsUpdateModal", () => {
     });
 
     it("Send update request when form is submitted", async () => {
-        const item_to_update = {
+        const item = {
             id: 104,
             title: "My item",
             permissions_for_groups: {
@@ -184,7 +184,7 @@ describe("PermissionsUpdateModal", () => {
         };
 
         store.state.permissions.project_ugroups = [];
-        const wrapper = factory({ item: item_to_update });
+        const wrapper = factory({ item });
 
         const expectedActionName = "permissions/updatePermissions";
         store.dispatch.mockImplementation(function (actionName) {
@@ -202,10 +202,10 @@ describe("PermissionsUpdateModal", () => {
             can_write: wrapper.vm.updated_permissions.can_write,
             can_manage: wrapper.vm.updated_permissions.can_manage,
         };
-        expect(store.dispatch).toHaveBeenCalledWith(expectedActionName, [
-            item_to_update,
-            permissions_to_update,
-        ]);
+        expect(store.dispatch).toHaveBeenCalledWith(expectedActionName, {
+            item: item,
+            updated_permissions: permissions_to_update,
+        });
         await wrapper.vm.$nextTick();
         expect(wrapper.vm.can_be_submitted).toBe(true);
     });

plugins/document/scripts/document/components/Folder/Permissions/PermissionsUpdateModal.vue
@@ -157,10 +157,10 @@ export default {
         async updatePermissions() {
             this.is_submitting_new_permissions = true;
             this.$store.commit("error/resetModalError");
-            await this.$store.dispatch("permissions/updatePermissions", [
-                this.item,
-                this.updated_permissions,
-            ]);
+            await this.$store.dispatch("permissions/updatePermissions", {
+                item: this.item,
+                updated_permissions: this.updated_permissions,
+            });
             this.is_submitting_new_permissions = false;
             if (this.has_modal_error === false) {
                 this.modal.hide();

plugins/document/scripts/document/helpers/permissions/ugroups.test.ts
@@ -19,22 +19,23 @@
 
 import * as rest_querier from "../../api/rest-querier";
 import { getProjectUserGroupsWithoutServiceSpecialUGroups } from "./ugroups";
+import type { UserGroup } from "../../type";
 
 describe("User groups", () => {
     it("filters special service user groups from the list", async () => {
         const getProjectUserGroupsSpy = jest.spyOn(rest_querier, "getProjectUserGroups");
 
-        const project_members_ugroup = {
+        const project_members_ugroup: UserGroup = {
             id: "102_3",
-        };
-        const project_special_service_ugroup = {
+        } as UserGroup;
+        const project_special_service_ugroup: UserGroup = {
             id: "102_17",
-        };
-        const project_static_ugroup = {
+        } as UserGroup;
+        const project_static_ugroup: UserGroup = {
             id: "130",
-        };
+        } as UserGroup;
 
-        getProjectUserGroupsSpy.mockReturnValue([
+        getProjectUserGroupsSpy.mockResolvedValue([
             project_members_ugroup,
             project_special_service_ugroup,
             project_static_ugroup,

plugins/document/scripts/document/helpers/permissions/ugroups.ts
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
@@ -18,11 +18,12 @@
  */
 
 import { getProjectUserGroups } from "../../api/rest-querier";
+import type { UserGroup } from "../../type";
 
 const PROJECT_MEMBERS_ID = "3";
 const PROJECT_ADMINS_ID = "4";
 
-function isUGroupAServiceSpecialUGroup(project_id, ugroup) {
+function isUGroupAServiceSpecialUGroup(project_id: number, ugroup: UserGroup): boolean {
     return (
         ugroup.id.includes("_") &&
         ugroup.id !== `${project_id}_${PROJECT_MEMBERS_ID}` &&
@@ -30,10 +31,12 @@ function isUGroupAServiceSpecialUGroup(project_id, ugroup) {
     );
 }
 
-export async function getProjectUserGroupsWithoutServiceSpecialUGroups(project_id) {
+export async function getProjectUserGroupsWithoutServiceSpecialUGroups(
+    project_id: number
+): Promise<Array<UserGroup>> {
     const ugroups = await getProjectUserGroups(project_id);
 
-    const filtered_groups = [];
+    const filtered_groups: Array<UserGroup> = [];
 
     ugroups.forEach((ugroup) => {
         if (isUGroupAServiceSpecialUGroup(project_id, ugroup)) {

plugins/document/scripts/document/store/actions-helpers/handle-errors.ts
@@ -21,7 +21,7 @@ import type { ActionContext } from "vuex";
 import type { ErrorState } from "../error/module";
 import type { Item } from "../../type";
 
-interface DocumentException {
+export interface DocumentException {
     response: Response;
 }
 
@@ -92,23 +92,6 @@ export async function handleErrorsForDocument(
         context.commit("error/setItemLoadingError", getErrorMessage(json));
     } catch (error) {
         context.commit("error/setItemLoadingError", message);
-    }
-}
-
-export async function handleErrorsForModal(
-    context: ActionContext<ErrorState, ErrorState>,
-    exception: DocumentException
-): Promise<void> {
-    const message = "Internal server error";
-    if (exception.response === undefined) {
-        context.commit("error/setModalError", message);
-        throw exception;
-    }
-    try {
-        const json = await exception.response.json();
-        context.commit("error/setModalError", getErrorMessage(json));
-    } catch (error) {
-        context.commit("error/setModalError", message);
     }
 }
 

commit f61c3c821209932ad8e720849c4270da50931fb5

request #26391 Error is not displayed when user creates a new document version
Introduced by 775b113cc7eca0c4e7f55c826a4b441610d0deb1

The "error" prefix is not needed anymore since the moved method is now in
the same "error" namespace as "setErrorModal" mutation method.

How to test:
 - Throws an error when a new version is created from any document type. e.g: add a : throw new
   RestException(400,'NOOOOOOO'); in DocmanFileResource::createNewFileVersion
 - Try to create a new version
=> The error should be displayed

Change-Id: I8143a2eeef13eedf76a5c399d2b256d1d604fc33

plugins/document/scripts/document/store/error/error-actions.ts
@@ -42,14 +42,14 @@ export async function handleErrorsForModal(
     exception: unknown
 ): Promise<void> {
     if (!(exception instanceof FetchWrapperError) || exception.response === undefined) {
-        context.commit("error/setModalError", message);
+        context.commit("setModalError", message);
         throw exception;
     }
     try {
         const json = await exception.response.json();
-        context.commit("error/setModalError", getErrorMessage(json));
+        context.commit("setModalError", getErrorMessage(json));
     } catch (error) {
-        context.commit("error/setModalError", message);
+        context.commit("setModalError", message);
     }
 }
 

plugins/document/scripts/document/store/error/error-actions.test.ts
@@ -21,7 +21,7 @@ import * as actions from "./error-actions";
 import { FetchWrapperError } from "@tuleap/tlp-fetch";
 import type { ActionContext } from "vuex";
 import type { ErrorState } from "./module";
-import type { State } from "../../type";
+import type { RootState, State } from "../../type";
 
 describe(`Error module actions`, () => {
     describe(`handleGlobalModalError`, () => {
@@ -98,6 +98,48 @@ describe(`Error module actions`, () => {
             await actions.handleErrorsForLock(context, error);
 
             expect(context.commit).toHaveBeenCalledWith("setLockError", "Internal server error");
+        });
+    });
+
+    describe("handleErrorsForModal", () => {
+        let context: ActionContext<ErrorState, RootState>;
+
+        beforeEach(() => {
+            context = {
+                commit: jest.fn(),
+            } as unknown as ActionContext<ErrorState, RootState>;
+        });
+        it(`when a message is not from FetchWrapperError,
+            it will throw an error`, async () => {
+            const error = new Error();
+            await expect(actions.handleErrorsForModal(context, error)).rejects.toThrowError();
+
+            expect(context.commit).not.toHaveBeenCalledWith("setModalError", "Oh snap");
+        });
+
+        it(`when a message can be extracted from the FetchWrapperError,
+            it will set an error message that will show up in the opened modal`, async () => {
+            const error = new FetchWrapperError("Internal Server Error", {
+                json: () =>
+                    Promise.resolve({
+                        error: { code: 500, message: "Oh snap" },
+                    }),
+            } as Response);
+            await actions.handleErrorsForModal(context, error);
+
+            expect(context.commit).toHaveBeenCalledWith("setModalError", "Oh snap");
+        });
+
+        it(`when a message can't be extracted from the FetchWrapperError,
+            it will display the default message in the opened modal`, async () => {
+            const response = {
+                json: () => Promise.reject(),
+            } as Response;
+            const error = new FetchWrapperError("Oh snap", response);
+
+            await actions.handleErrorsForModal(context, error);
+
+            expect(context.commit).toHaveBeenCalledWith("setModalError", "Internal server error");
         });
     });
 });