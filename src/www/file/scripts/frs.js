Enalean/tuleap

commit 38b6ab394c31248e7743719b6fa7ed38160af976

Remove direct usages of Element.remove()
This was removed from the prototype of the built Element object in
2ecdd2a6e8ab6922237f756a184a9c23d3934c00 so direct accesses now fails.

Dropped a 'unittest' file of Scriptaculous using it since we do not run
it (and have not clue how to do it).

Part of story #23469: export reports with linked artifact in xlsx

Change-Id: I457200581e540f8c364b430851b292aea3d44db9

src/www/file/scripts/frs.js
@@ -257,8 +257,8 @@ function onselectchange(select, number, id, cell_trash, image) {
         //the http mode was selected
         Element.show("file_" + id);
         //Element.hide(select);
-        Element.remove(select);
-        Element.remove("ftp_file_" + id);
+        select.remove();
+        document.getElementById("ftp_file_" + id)?.remove();
         $("processor_" + id).name = "file_processor[]";
         $("type_" + id).name = "file_type[]";
         $("reference_md5_" + id).name = "reference_md5[]";
@@ -274,8 +274,8 @@ function onselectchange(select, number, id, cell_trash, image) {
         //the ftp/scp move was selected, wa change the select box to a readonly text field
         //we add the file to the used_ftp_files list
         //Element.hide(select);
-        Element.remove(select);
-        Element.remove("file_" + id);
+        select.remove();
+        document.getElementById("ftp_file_" + id)?.remove();
         Element.show("ftp_file_" + id);
         $("ftp_file_" + id).value = select.options[select.selectedIndex].value;
         used_ftp_files.push(select.options[select.selectedIndex].value);
@@ -309,7 +309,7 @@ function delete_file(row_id, id) {
                 build_select_file(number);
             });
     }
-    Element.remove(row_id);
+    document.getElementById(row_id)?.remove();
     if (
         (release_mode == "creation" && nb_files == 0) ||
         (release_mode == "edition" && $("nb_files").value == 0 && nb_files == 0)
@@ -331,7 +331,7 @@ function cancel_update_change_log() {
     Element.show("change_log_area");
     Element.show("cl_upload_link");
     //remove the file input and add it just after to set the file value to "" otherwise the file will be upload when saving the release
-    Element.remove("upload_change_log");
+    document.getElementById("upload_change_log")?.remove();
     Element.insert("change_log_title", {
         after: '<TR id="upload_change_log"><TD><input type="file" id="uploaded_change_log" name="uploaded_change_log"  size="30"></TD></TR>',
     });
@@ -373,7 +373,7 @@ function cancel_update_notes() {
     Element.show("release_notes_area");
     Element.show("rn_upload_link");
     //remove the file input and add it just after to set the file value to "" otherwise the file will be upload when saving the release
-    Element.remove("upload_notes");
+    document.getElementById("upload_notes")?.parentNode?.removeChild();
     Element.insert("notes_title", {
         after: '<TR id="upload_notes"><TD><input id="uploaded_notes" type="file" name="uploaded_release_notes"  size="30"></TD></TR>',
     });
@@ -412,7 +412,7 @@ function refresh_file_list() {
 Event.observe(window, "load", function () {
     //Add new file part
     //Element.hide('row_0');
-    Element.remove("row_0");
+    document.getElementById("row_0")?.remove();
     if (release_mode == "creation" || (release_mode == "edition" && $("nb_files").value == 0)) {
         add_new_file();
     }

plugins/tracker/scripts/legacy/TrackerArtifact.js
@@ -50,7 +50,9 @@ function invertFollowups(followupSection) {
     var elements = [];
     var len = element.childNodes.length;
     for (var i = len - 1; i >= 0; --i) {
-        elements.push(Element.remove(element.childNodes[i]));
+        const child_node = element.childNodes[i];
+        child_node.remove();
+        elements.push(child_node);
     }
     for (var j = 0; j < len; ++j) {
         element.appendChild(elements[j]);
@@ -201,9 +203,9 @@ document.observe("dom:loaded", function () {
             }
             value += sb.getValue();
             $("tracker_followup_comment_new").value = value;
-            if (CKEDITOR.instances && CKEDITOR.instances["tracker_followup_comment_new"]) {
-                CKEDITOR.instances["tracker_followup_comment_new"].setData(
-                    CKEDITOR.instances["tracker_followup_comment_new"].getData() +
+            if (CKEDITOR.instances && CKEDITOR.instances.tracker_followup_comment_new) {
+                CKEDITOR.instances.tracker_followup_comment_new.setData(
+                    CKEDITOR.instances.tracker_followup_comment_new.getData() +
                         "<p>" +
                         value +
                         "</p>"

plugins/tracker/scripts/legacy/TrackerReports.js
@@ -228,7 +228,7 @@ codendi.tracker.report.table.AddRemoveColumn = Class.create({
                         'select[name="tracker_aggregate_function_add[' + field_id + ']"]'
                     );
                     if (selectbox) {
-                        var report_id = $F($("tracker_report_query_form")["report"]);
+                        var report_id = $F($("tracker_report_query_form").report);
                         var renderer_id = $("tracker_report_renderer_current").readAttribute(
                             "data-renderer-id"
                         );
@@ -524,11 +524,11 @@ codendi.tracker.report.loadAggregates = function (selectbox, report_id, renderer
         '<img src="' + codendi.imgroot + 'ic/sum--plus.png" style="vertical-align: middle;" />'
     );
 
-    Element.remove(
-        selectbox.insert({
+    selectbox
+        .insert({
             after: handle,
         })
-    );
+        .remove();
     ul.id = selectbox.id;
     handle.insert({
         after: panel.insert(

src/common/tracker/HTML/ArtifactHtml.class.php
@@ -174,7 +174,9 @@ class ArtifactHtml extends Artifact //phpcs:ignore PSR1.Classes.ClassDeclaration
                     var elements = [];
                     var len = element.childNodes.length;
                     for(var i = len - 1 ; i >= 0 ; --i) {
-                        elements.push(Element.remove(element.childNodes[i]));
+                        const child_node = element.childNodes[i];
+                        child_node.remove();
+                        elements.push(child_node);
                     }
                     for(var i = 0 ; i < len ; ++i) {
                         element.appendChild(elements[i]);

src/www/scripts/lightwindow/lightwindow.js
@@ -568,7 +568,7 @@ lightwindow.prototype = {
 	_clearWindowContents : function(contents) {
 		// If there is an iframe, its got to go
 		if ($('lightwindow_iframe')) {
-			Element.remove($('lightwindow_iframe'));
+			document.getElementById('lightwindow_iframe')?.remove();
 		}
 
 		// Stop playing an object if its still around
@@ -576,7 +576,7 @@ lightwindow.prototype = {
 			try {
 				$('lightwindow_media_primary').Stop();
 			} catch(e) {}
-			Element.remove($('lightwindow_media_primary'));
+			document.getElementById('lightwindow_media_primary')?.remove();
 		}
 
 		// Stop playing an object if its still around		
@@ -584,7 +584,7 @@ lightwindow.prototype = {
 			try {
 				$('lightwindow_media_secondary').Stop();
 			} catch(e) {}
-			Element.remove($('lightwindow_media_secondary'));
+			document.getElementById('lightwindow_media_secondary')?.remove();
 		}
 		
 		this.activeGallery = false;
@@ -1050,7 +1050,7 @@ lightwindow.prototype = {
 	    scrollDiv.style.overflow = 'auto';
     	var withScroll = $('lightwindow_content_scroll_div').offsetWidth;
 
-	   	Element.remove($('lightwindow_scroll_div'));
+		document.getElementById('lightwindow_scroll_div')?.remove();
 
 	    this.scrollbarOffset = noScroll-withScroll;
 	},

src/www/scripts/scriptaculous/dragdrop.js
@@ -436,7 +436,7 @@ var Draggable = Class.create({
       if (!this._originallyAbsolute)
         Position.relativize(this.element);
       delete this._originallyAbsolute;
-      Element.remove(this._clone);
+      this._clone.remove();
       this._clone = null;
     }
 

src/www/scripts/scriptaculous/effects.js
@@ -119,7 +119,7 @@ var Effect = {
               character == ' ' ? String.fromCharCode(160) : character),
               child);
         });
-        Element.remove(child);
+        child.remove();
       }
     });
   },
@@ -150,7 +150,7 @@ var Effect = {
   toggle: function(element, effect, options) {
     element = $(element);
     effect  = (effect || 'appear').toLowerCase();
-    
+
     return Effect[ Effect.PAIRS[ effect ][ element.visible() ? 1 : 0 ] ](element, Object.extend({
       queue: { position:'end', scope:(element.id || 'global'), limit: 1 }
     }, options || {}));

commit 50af86359e532d25e35223a12beef1f48280aee0

request #26725: Can no longer release files from "incoming" area in FRS
Issue introduced by 38b6ab394c31248e7743719b6fa7ed38160af976.

Change-Id: Ia8bba8b215a957b01ec6444c94accacd1fae5226

src/www/file/scripts/frs.js
@@ -275,7 +275,7 @@ function onselectchange(select, number, id, cell_trash, image) {
         //we add the file to the used_ftp_files list
         //Element.hide(select);
         select.remove();
-        document.getElementById("ftp_file_" + id)?.remove();
+        document.getElementById("file_" + id)?.remove();
         Element.show("ftp_file_" + id);
         $("ftp_file_" + id).value = select.options[select.selectedIndex].value;
         used_ftp_files.push(select.options[select.selectedIndex].value);