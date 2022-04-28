Enalean/tuleap

commit 9e4e1173b2e784d589882a32e835a55379cbf437

Use @tuleap/project-sidebar component to replace existing sidebar on BP pages
Part of story #24227: have private oauth2 endpoints for Mediawiki

Change-Id: Id429e3fad232338de83acdd889c9c3b52b019aca

src/common/Project/Sidebar/ProjectContextPresenter.php
@@ -24,11 +24,8 @@ namespace Tuleap\Project\Sidebar;
 
 use Codendi_HTMLPurifier;
 use Project;
-use Tuleap\Project\Admin\Access\ProjectAdministrationLinkPresenter;
 use Tuleap\Project\Banner\BannerDisplay;
 use Tuleap\Project\Flags\ProjectFlagPresenter;
-use Tuleap\Project\Icons\EmojiCodepointConverter;
-use Tuleap\Project\ProjectPrivacyPresenter;
 use Tuleap\Project\REST\v1\ProjectSidebarDataRepresentation;
 
 /**
@@ -37,11 +34,6 @@ use Tuleap\Project\REST\v1\ProjectSidebarDataRepresentation;
 final class ProjectContextPresenter
 {
     public int $project_id;
-    public ProjectPrivacyPresenter $privacy;
-    /**
-     * @var ProjectFlagPresenter[]
-     */
-    public array $project_flags;
     public bool $has_project_banner;
     public bool $project_banner_is_visible;
     public string $purified_banner;
@@ -49,50 +41,29 @@ final class ProjectContextPresenter
      * @var false|string
      */
     public $json_encoded_project_flags;
-    public int $nb_project_flags;
-    public bool $has_project_flags;
-    public bool $has_administration_link;
-    public string $administration_link;
-    public ?LinkedProjectsCollectionPresenter $linked_projects;
-    public ?string $project_icon;
 
     /**
      * @param ProjectFlagPresenter[] $project_flags
      */
     private function __construct(
         Project $project,
-        ProjectPrivacyPresenter $privacy,
-        ?ProjectAdministrationLinkPresenter $administration_link_presenter,
-        ?LinkedProjectsCollectionPresenter $linked_projects_presenter,
         array $project_flags,
         ?BannerDisplay $banner,
         string $purified_banner,
-        ?string $project_icon,
-        public ?ProjectSidebarDataRepresentation $sidebar_data,
+        public ProjectSidebarDataRepresentation $sidebar_data,
     ) {
         $this->project_id                 = (int) $project->getID();
-        $this->privacy                    = $privacy;
-        $this->project_flags              = $project_flags;
-        $this->nb_project_flags           = count($project_flags);
-        $this->has_project_flags          = $this->nb_project_flags > 0;
         $this->has_project_banner         = $banner !== null;
         $this->project_banner_is_visible  = $banner && $banner->isVisible();
-        $this->has_administration_link    = $administration_link_presenter !== null;
-        $this->administration_link        = $administration_link_presenter->uri ?? '';
         $this->purified_banner            = $purified_banner;
         $this->json_encoded_project_flags = json_encode($project_flags, JSON_THROW_ON_ERROR);
-        $this->linked_projects            = $linked_projects_presenter;
-        $this->project_icon               = $project_icon;
     }
 
     public static function build(
         Project $project,
-        ProjectPrivacyPresenter $privacy,
-        ?ProjectAdministrationLinkPresenter $administration_link,
-        ?LinkedProjectsCollectionPresenter $linked_projects_presenter,
         array $project_flags,
         ?BannerDisplay $banner,
-        ?ProjectSidebarDataRepresentation $project_sidebar_data,
+        ProjectSidebarDataRepresentation $project_sidebar_data,
     ): self {
         $purified_banner = '';
         if ($banner) {
@@ -101,18 +72,11 @@ final class ProjectContextPresenter
                 Codendi_HTMLPurifier::CONFIG_MINIMAL_FORMATTING_NO_NEWLINE
             );
         }
-        $project_icon = EmojiCodepointConverter::convertStoredEmojiFormatToEmojiFormat(
-            $project->getIconUnicodeCodepoint()
-        );
         return new self(
             $project,
-            $privacy,
-            $administration_link,
-            $linked_projects_presenter,
             $project_flags,
             $banner,
             $purified_banner,
-            $project_icon,
             $project_sidebar_data,
         );
     }

src/scripts/BurningParrot/src/index.ts
@@ -24,9 +24,6 @@ import { initMainPosition } from "../../tuleap/main-position";
 import { initHeaderPosition } from "../../tuleap/header-position";
 import { init as initNavbarPinned } from "../../tuleap/navbar-pinned";
 import { init as initSidebar } from "./sidebar";
-import { init as initProjectFlags } from "./project-flags";
-import { init as initProjectPrivacy } from "./project-privacy";
-import { init as initLinkedProjects } from "./linked-projects";
 import { initHelpDropdown } from "./help-dropdown";
 import { init as initInviteBuddies } from "./invite-buddies";
 import * as autocomplete from "../../tuleap/autocomplete-for-select2";
@@ -50,10 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
     initNavbarDropdown(createPopover);
     initSidebar();
     initHelpDropdown();
-    initProjectFlags();
-    initProjectPrivacy();
     initInviteBuddies();
-    initLinkedProjects();
 });
 
 // tuleap.autocomplete* is still used by siteadmin scripts which may run without listening to DOMContentLoaded

src/scripts/BurningParrot/src/project-privacy.ts
@@ -1,71 +1,0 @@
-/**
- * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-import { createPopover } from "tlp";
-
-export function init(): void {
-    handleBreadcrumbPrivacyPopover();
-    handleSidebarPrivacyPopover();
-}
-
-function handleBreadcrumbPrivacyPopover(): void {
-    const project_privacy_element = document.getElementById("breadcrumb-project-privacy-icon");
-    if (!project_privacy_element) {
-        return;
-    }
-
-    project_privacy_element.title = "";
-
-    const popover_content = document.getElementById("current-project-nav-title-popover");
-
-    if (!popover_content) {
-        return;
-    }
-
-    createPopover(project_privacy_element, popover_content, {
-        anchor: project_privacy_element,
-        placement: "bottom-start",
-    });
-}
-
-function handleSidebarPrivacyPopover(): void {
-    const project_privacy_element = document.querySelector("[data-project-privacy-icon]");
-    if (!project_privacy_element || !(project_privacy_element instanceof HTMLElement)) {
-        return;
-    }
-
-    project_privacy_element.title = "";
-
-    const popover_content = document.getElementById("project-sidebar-nav-title-popover");
-
-    if (!popover_content) {
-        return;
-    }
-
-    const project_sidebar_header = document.querySelector(".project-sidebar-header-name");
-
-    if (!project_sidebar_header || !(project_sidebar_header instanceof HTMLElement)) {
-        return;
-    }
-
-    createPopover(project_privacy_element, popover_content, {
-        anchor: project_sidebar_header,
-        placement: "right-start",
-    });
-}

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -2402,9 +2402,6 @@ msgstr "Ouvrir la barre latérale"
 msgid "Open user dropdown"
 msgstr "Ouvrir le menu utilisateur"
 
-msgid "Open/close sidebar"
-msgstr "Ouvrir/fermer la barre latérale"
-
 msgid "Optional message…"
 msgstr "Message optionnel…"
 

src/common/Layout/BaseLayout.php
@@ -353,17 +353,6 @@ abstract class BaseLayout extends Response
         $this->is_rendered_through_service = $value;
     }
 
-    protected function getProjectSidebar($params, $project)
-    {
-        $builder = new ProjectSidebarToolsBuilder(
-            EventManager::instance(),
-            ProjectManager::instance(),
-            $this->uri_sanitizer
-        );
-
-        return $builder->getSidebarTools($this->getUser(), $params['toptab'], $project);
-    }
-
     final protected function getProjectSidebarData(array $params, Project $project, PFUser $user): ProjectSidebarDataRepresentation
     {
         $event_manager = EventManager::instance();

src/common/Project/Admin/Access/ProjectAdministrationLinkPresenter.php
@@ -1,44 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-declare(strict_types=1);
-
-namespace Tuleap\Project\Admin\Access;
-
-final class ProjectAdministrationLinkPresenter
-{
-    public string $uri;
-
-    private function __construct(string $uri)
-    {
-        $this->uri = $uri;
-    }
-
-    public static function fromProject(
-        VerifyUserCanAccessProjectAdministration $access_verifier,
-        \Project $project,
-        \PFUser $user,
-    ): ?self {
-        if (! $access_verifier->canUserAccessProjectAdministration($user, $project)) {
-            return null;
-        }
-        return new self('/project/admin/?group_id=' . $project->getID());
-    }
-}

src/scripts/BurningParrot/src/linked-projects.ts
@@ -1,35 +1,0 @@
-/**
- * Copyright (c) Enalean, 2021 - Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-import { createPopover } from "@tuleap/tlp-popovers";
-
-export function init(): void {
-    const linked_projects_element = document.getElementById("project-sidebar-linked-projects");
-
-    if (!linked_projects_element) {
-        return;
-    }
-
-    const popover_element = document.getElementById("project-sidebar-linked-projects-popover");
-
-    if (!popover_element) {
-        return;
-    }
-
-    createPopover(linked_projects_element, popover_element, { placement: "right-start" });
-}

src/scripts/BurningParrot/src/project-flags.ts
@@ -1,37 +1,0 @@
-/**
- * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-import { createPopover } from "tlp";
-
-export function init(): void {
-    const project_flags_element = document.getElementById("project-sidebar-project-flags");
-    if (!project_flags_element) {
-        return;
-    }
-
-    const popover_content = document.getElementById("project-sidebar-project-flags-popover");
-
-    if (!popover_content) {
-        return;
-    }
-
-    createPopover(project_flags_element, popover_content, {
-        placement: "right-start",
-    });
-}

commit 47f38aa1323ca7e07012e91c1c00fbdda138a568

request #26731: Project privacy is not displayed on pages with the default breadcrumbs when sidebar collapsed
Issue has been introduced with 9e4e1173b2e784d589882a32e835a55379cbf437.

Change-Id: I49d6804c716e6eca4f7bd1e06ac689b52fb332d0

src/common/Project/Sidebar/ProjectContextPresenter.php
@@ -26,6 +26,7 @@ use Codendi_HTMLPurifier;
 use Project;
 use Tuleap\Project\Banner\BannerDisplay;
 use Tuleap\Project\Flags\ProjectFlagPresenter;
+use Tuleap\Project\ProjectPrivacyPresenter;
 use Tuleap\Project\REST\v1\ProjectSidebarDataRepresentation;
 
 /**
@@ -47,6 +48,7 @@ final class ProjectContextPresenter
      */
     private function __construct(
         Project $project,
+        public ProjectPrivacyPresenter $privacy,
         array $project_flags,
         ?BannerDisplay $banner,
         string $purified_banner,
@@ -74,6 +76,7 @@ final class ProjectContextPresenter
         }
         return new self(
             $project,
+            ProjectPrivacyPresenter::fromProject($project),
             $project_flags,
             $banner,
             $purified_banner,

src/scripts/BurningParrot/src/index.ts
@@ -22,6 +22,7 @@ import { init as initNavbarDropdown } from "../../navbar-dropdowns/navbar-dropdo
 import { initSidebarPosition } from "../../tuleap/sidebar-position";
 import { initMainPosition } from "../../tuleap/main-position";
 import { initHeaderPosition } from "../../tuleap/header-position";
+import { init as initProjectPrivacy } from "./project-privacy";
 import { init as initNavbarPinned } from "../../tuleap/navbar-pinned";
 import { init as initSidebar } from "./sidebar";
 import { initHelpDropdown } from "./help-dropdown";
@@ -47,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
     initNavbarDropdown(createPopover);
     initSidebar();
     initHelpDropdown();
+    initProjectPrivacy();
     initInviteBuddies();
 });
 

src/scripts/BurningParrot/src/project-privacy.ts
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) Enalean, 2019 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { createPopover } from "tlp";
+
+export function init(): void {
+    handleBreadcrumbPrivacyPopover();
+}
+
+function handleBreadcrumbPrivacyPopover(): void {
+    const project_privacy_element = document.getElementById("breadcrumb-project-privacy-icon");
+    if (!project_privacy_element) {
+        return;
+    }
+
+    project_privacy_element.title = "";
+
+    const popover_content = document.getElementById("current-project-nav-title-popover");
+
+    if (!popover_content) {
+        return;
+    }
+
+    createPopover(project_privacy_element, popover_content, {
+        anchor: project_privacy_element,
+        placement: "bottom-start",
+    });
+}