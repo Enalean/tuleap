7739e418f663803fc95f99b0a7aa96478fc704ec
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-04-28 16:02:37 +0200

    request #26734: Add basic support of security.txt file (RFC 9116)
    This contribution adds a new plugin. When this plugin is installed and
    activated, an administrator can set up a new global option `security_txt_primary_contact`.
    
    When this option is defined, a security.txt file is available at `/.well-known/security.txt`.
    
    Change-Id: I27910cd909b6f23d661c345ce169c26b1daf6f1d

---
+++ plugins/securitytxt/include/SecurityTxtController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\SecurityTxt;
+
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Tuleap\Request\DispatchablePSR15Compatible;
+use Tuleap\Request\DispatchableWithRequestNoAuthz;
+use Tuleap\ServerHostname;
+
+final class SecurityTxtController extends DispatchablePSR15Compatible implements DispatchableWithRequestNoAuthz
+{
+    public const WELL_KNOWN_SECURITY_TXT_HREF = '/.well-known/security.txt';
+
+    public function __construct(
+        private ResponseFactoryInterface $response_factory,
+        private StreamFactoryInterface $stream_factory,
+        EmitterInterface $emitter,
+        MiddlewareInterface ...$middleware_stack,
+    ) {
+        parent::__construct($emitter, ...$middleware_stack);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $primary_contact = \ForgeConfig::get(SecurityTxtOptions::CONTACT);
+        if (! $primary_contact) {
+            return $this->response_factory->createResponse(404)
+                ->withHeader('Content-Type', 'text/plain')
+                ->withBody($this->stream_factory->createStream('No security.txt file defined'));
+        }
+
+        $canonical = ServerHostname::HTTPSUrl() . self::WELL_KNOWN_SECURITY_TXT_HREF;
+        $expires   = (new \DateTimeImmutable())->add(new \DateInterval('P1W'))->format(\DateTimeInterface::ATOM);
+
+        return $this->response_factory->createResponse(200)
+            ->withHeader('Content-Type', 'text/plain;charset=utf-8')
+            ->withBody(
+                $this->stream_factory->createStream(
+                    <<<EOF
+                    Canonical: $canonical
+                    Expires: $expires
+                    Contact: $primary_contact
+                    EOF
+                )
+            );
+    }
+}
---
+++ plugins/securitytxt/tests/unit/SecurityTxtControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\SecurityTxt;
+
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Tuleap\ForgeConfigSandbox;
+use Tuleap\Http\HTTPFactoryBuilder;
+use Tuleap\Http\Server\NullServerRequest;
+use Tuleap\ServerHostname;
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class SecurityTxtControllerTest extends TestCase
+{
+    use ForgeConfigSandbox;
+
+    private SecurityTxtController $controller;
+
+    protected function setUp(): void
+    {
+        $this->controller = new SecurityTxtController(
+            HTTPFactoryBuilder::responseFactory(),
+            HTTPFactoryBuilder::streamFactory(),
+            new SapiEmitter()
+        );
+    }
+
+    public function testDisplaysSecurityTxtFile(): void
+    {
+        \ForgeConfig::set(SecurityTxtOptions::CONTACT, 'mailto:security@example.com');
+        \ForgeConfig::set(ServerHostname::DEFAULT_DOMAIN, 'tuleap.example.com');
+
+        $response = $this->controller->handle(new NullServerRequest());
+
+        self::assertEquals(200, $response->getStatusCode());
+
+        $content = $response->getBody()->getContents();
+
+        self::assertStringContainsString('Contact: mailto:security@example.com', $content);
+        self::assertStringContainsString('Canonical: https://tuleap.example.com/.well-known/security.txt', $content);
+        self::assertStringContainsString('Expires: ', $content);
+    }
+
+    public function testNotFoundWhenNoPrimaryContactIsDefined(): void
+    {
+        $response = $this->controller->handle(new NullServerRequest());
+
+        self::assertEquals(404, $response->getStatusCode());
+    }
+}
