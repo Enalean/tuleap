Enalean/tuleap

commit 8e99e7c82d9fe569799019b9e1d614d38a184313

request #26729 Tracker report renderer and chart widgets leak information user cannot access
Tracker report renderer and chart widgets leak information user cannot access

Change-Id: Ibdd7d1b8e72dd44bbb2b747b7d8f264603f98024

plugins/graphontrackersv5/include/data-access/GraphOnTrackersV5_ChartFactory.class.php
@@ -252,7 +252,7 @@ class GraphOnTrackersV5_ChartFactory
                 $report   = null; //We don't know the report
                 $renderer = Tracker_Report_RendererFactory::instance()->getReportRendererById($chart_data['report_graphic_id'], $report, $store_in_session);
             }
-            if ($renderer) {
+            if ($renderer && $renderer->getReport()->getTracker()->userCanView()) {
                 $c = $this->instanciateChart($chart_data, $renderer, $store_in_session);
             }
         }

plugins/tracker/include/Tracker/Widget/Tracker_Widget_Renderer.class.php
@@ -58,12 +58,8 @@ abstract class Tracker_Widget_Renderer extends Widget
         $store_in_session = false;
         $arrf             = Tracker_Report_RendererFactory::instance();
         $renderer         = $arrf->getReportRendererById($this->renderer_id, null, $store_in_session);
-        if ($renderer) {
-            $tracker = $renderer->report->getTracker();
-            $project = $tracker->getProject();
-            if ($tracker->isActive() && $project->isActive()) {
-                return $renderer;
-            }
+        if ($renderer && $renderer->report->getTracker()->userCanView()) {
+            return $renderer;
         }
         return null;
     }

commit 55b0a57a7c6a1f4a167dba5d6749838d3432a642

request #26752 Project creation is broken when there is report's graphs
Introduced by 8e99e7c82d9fe569799019b9e1d614d38a184313

The charts permissions are checked when the project is still pending.

Change-Id: Ie0c304fda45b61bc848dcb7cad3b1a2ce8a3c91d

plugins/graphontrackersv5/include/GraphOnTrackersV5_Widget_Chart.class.php
@@ -52,7 +52,7 @@ abstract class GraphOnTrackersV5_Widget_Chart extends Widget
             false
         );
 
-        if ($chart) {
+        if ($chart && $chart->getRenderer()->getReport()->getTracker()->userCanView()) {
             $content = $chart->getWidgetContent();
         } else {
             $content = '<em>Chart does not exist</em>';

plugins/graphontrackersv5/include/data-access/GraphOnTrackersV5_ChartFactory.class.php
@@ -252,7 +252,7 @@ class GraphOnTrackersV5_ChartFactory
                 $report   = null; //We don't know the report
                 $renderer = Tracker_Report_RendererFactory::instance()->getReportRendererById($chart_data['report_graphic_id'], $report, $store_in_session);
             }
-            if ($renderer && $renderer->getReport()->getTracker()->userCanView()) {
+            if ($renderer) {
                 $c = $this->instanciateChart($chart_data, $renderer, $store_in_session);
             }
         }