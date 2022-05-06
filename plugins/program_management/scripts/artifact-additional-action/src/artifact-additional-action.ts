8ef0fed908375d2df0ca8c480c6ff1d3be8f05e8
Joris Masson <joris.masson@enalean.com>
2022-05-06 16:15:18 +0200

    replace relative import of tuleap feedback with a package import
    Part of request #26381: Prevent relative imports outside of a package
    
    No functional change expected in the "additional actions" buttons ("Add
    to top backlog") for Program Management and AgileDashboard.
    
    Change-Id: Ifea1dc5d31d7ed627a4cc837566ea3d7fc1b4eef

--- plugins/program_management/scripts/artifact-additional-action/src/artifact-additional-action.ts
+++ plugins/program_management/scripts/artifact-additional-action/src/artifact-additional-action.ts
@@ -19,7 +19,7 @@
 
 import { patch } from "@tuleap/tlp-fetch";
 import { getPOFileFromLocale, initGettext } from "@tuleap/gettext";
-import { addFeedback, clearAllFeedbacks } from "@tuleap/core/scripts/tuleap/feedback";
+import { addFeedback, clearAllFeedbacks } from "@tuleap/fp-feedback";
 
 export function initArtifactAdditionalAction(mount_point: Document): void {
     const action_button = mount_point.getElementById(
