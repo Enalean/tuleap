c80240fb52dac7340b62b48caa27beae974abade
Romain LORENTZ <romain.lorentz@enalean.com>
2022-05-19 10:19:59 +0200

    request #26812 REST Route: API GET trackers/{id}/artifacts return all artifact if wrong field in query
    To test it, try to use the route using normal query with a non existing
    field.
    With the patch, you should have an error about non existing fields
    
    Change-Id: Ibf19ef3a857f2df50863cccf2e35d547eb6ad206

--- plugins/tracker/tests/unit/Tracker/Report/Tracker_Report_RESTTest.php
+++ plugins/tracker/tests/unit/Tracker/Report/Tracker_Report_RESTTest.php
@@ -17,29 +17,34 @@
  * You should have received a copy of the GNU General Public License
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/
  */
+
+use PHPUnit\Framework\MockObject\Stub;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
+
 final class Tracker_Report_RESTTest extends \Tuleap\Test\PHPUnit\TestCase //phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps
 {
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-
-    /**
-     * @var \Mockery\LegacyMockInterface|\Mockery\MockInterface|Tracker_FormElementFactory
-     */
-    private $formelement_factory;
-
     /**
      * @var Tracker_Report_REST
      */
     private $report;
+    private Tracker_FormElementFactory|Stub $formelement_factory;
 
     protected function setUp(): void
     {
-        $current_user              = \Mockery::spy(\PFUser::class);
-        $tracker                   = \Mockery::spy(\Tracker::class);
-        $permissions_manager       = \Mockery::spy(\PermissionsManager::class);
-        $dao                       = \Mockery::spy(\Tracker_ReportDao::class);
-        $this->formelement_factory = \Mockery::mock(\Tracker_FormElementFactory::class);
+        $current_user              = UserTestBuilder::aUser()->build();
+        $this->tracker             = TrackerTestBuilder::aTracker()->withId(122)->build();
+        $permissions_manager       = $this->createStub(\PermissionsManager::class);
+        $dao                       = $this->createStub(\Tracker_ReportDao::class);
+        $this->formelement_factory = $this->createStub(\Tracker_FormElementFactory::class);
 
-        $this->report = new Tracker_Report_REST($current_user, $tracker, $permissions_manager, $dao, $this->formelement_factory);
+        $this->report = new Tracker_Report_REST(
+            $current_user,
+            $this->tracker,
+            $permissions_manager,
+            $dao,
+            $this->formelement_factory
+        );
     }
 
     public function testItThrowsAnExceptionForBadJSON(): void
@@ -53,11 +58,15 @@ final class Tracker_Report_RESTTest extends \Tuleap\Test\PHPUnit\TestCase //phpc
     {
         $this->expectException(\Tracker_Report_InvalidRESTCriterionException::class);
 
-        $query = new ArrayObject([
-            "my_field" => new ArrayObject([
-                Tracker_Report_REST::VALUE_PROPERTY_NAME => "true",
-            ]),
-        ]);
+        $query = new ArrayObject(
+            [
+                "my_field" => new ArrayObject(
+                    [
+                        Tracker_Report_REST::VALUE_PROPERTY_NAME => "true",
+                    ]
+                ),
+            ]
+        );
 
         $this->report->setRESTCriteria(json_encode($query));
     }
@@ -66,41 +75,99 @@ final class Tracker_Report_RESTTest extends \Tuleap\Test\PHPUnit\TestCase //phpc
     {
         $this->expectException(\Tracker_Report_InvalidRESTCriterionException::class);
 
-        $query = new ArrayObject([
-            "my_field" => new ArrayObject([
-                Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR,
-            ]),
-        ]);
+        $query = new ArrayObject(
+            [
+                "my_field" => new ArrayObject(
+                    [
+                        Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR,
+                    ]
+                ),
+            ]
+        );
 
         $this->report->setRESTCriteria(json_encode($query));
     }
 
-    public function testItThrowsAnExceptionForAnInvalidQueryWithInvlidOperator(): void
+    public function testItThrowsAnExceptionForAnInvalidQueryWithInvalidOperator(): void
     {
         $this->expectException(\Tracker_Report_InvalidRESTCriterionException::class);
 
-        $query = new ArrayObject([
-            "my_field" => new ArrayObject([
-                Tracker_Report_REST::VALUE_PROPERTY_NAME    => "true",
-                Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR . 'xxx',
-            ]),
-        ]);
+        $query = new ArrayObject(
+            [
+                "my_field" => new ArrayObject(
+                    [
+                        Tracker_Report_REST::VALUE_PROPERTY_NAME    => "true",
+                        Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR . 'xxx',
+                    ]
+                ),
+            ]
+        );
 
         $this->report->setRESTCriteria(json_encode($query));
     }
 
     public function testItTransformsBasicCriteriaToTheCorrectFormat(): void
     {
-        $query = new ArrayObject([
-            "my_field" => new ArrayObject([
-                Tracker_Report_REST::VALUE_PROPERTY_NAME    => "true",
-                Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR,
-            ]),
-            "my_other_field" => "bla",
-        ]);
+        $field_1 = $this->createStub(Tracker_FormElement_Field_Selectbox::class);
+        $field_2 = $this->createStub(Tracker_FormElement_Field_Selectbox::class);
 
-        $this->formelement_factory->shouldReceive('getFormElementById')->twice();
-        $this->formelement_factory->shouldReceive('getFormElementByName')->twice();
+        $field_1->method('userCanRead')->willReturn(true);
+        $field_1->method('getId')->willReturn(1);
+        $field_1->method('setCriteriaValueFromREST')->willReturn(true);
+
+        $field_2->method('userCanRead')->willReturn(true);
+        $field_2->method('getId')->willReturn(2);
+        $field_2->method('setCriteriaValueFromREST')->willReturn(true);
+
+        $query = new ArrayObject(
+            [
+                "my_field"       => new ArrayObject(
+                    [
+                        Tracker_Report_REST::VALUE_PROPERTY_NAME    => "true",
+                        Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR,
+                    ]
+                ),
+                "my_other_field" => "bla",
+            ]
+        );
+
+        $this->formelement_factory->expects(self::exactly(2))->method('getFormElementById');
+
+        $this->formelement_factory->method('getFormElementByName')->withConsecutive(
+            [$this->tracker->getId(), "my_field"],
+            [$this->tracker->getId(), "my_other_field"]
+        )->willReturnOnConsecutiveCalls($field_1, $field_2);
+
+        $this->report->setRESTCriteria(json_encode($query));
+        $this->report->getCriteria();
+    }
+
+    public function testItThrowExceptionIfGivenFormelementsDontExist(): void
+    {
+        $field_1 = $this->createStub(Tracker_FormElement_Field_Selectbox::class);
+
+        $query = new ArrayObject(
+            [
+                "my_field"       => new ArrayObject(
+                    [
+                        Tracker_Report_REST::VALUE_PROPERTY_NAME    => "true",
+                        Tracker_Report_REST::OPERATOR_PROPERTY_NAME => Tracker_Report_REST::DEFAULT_OPERATOR,
+                    ]
+                ),
+                "my_other_field" => "bla",
+                "another_field"  => "nope",
+            ]
+        );
+
+        $this->formelement_factory->expects(self::exactly(3))->method('getFormElementById');
+
+        $this->formelement_factory->method('getFormElementByName')->withConsecutive(
+            [$this->tracker->getId(), "my_field"],
+            [$this->tracker->getId(), "my_other_field"],
+            [$this->tracker->getId(), "another_field"]
+        )->willReturnOnConsecutiveCalls(null, $field_1, null);
+
+        $this->expectException(Tracker_Report_InvalidRESTCriterionException::class);
 
         $this->report->setRESTCriteria(json_encode($query));
         $this->report->getCriteria();
