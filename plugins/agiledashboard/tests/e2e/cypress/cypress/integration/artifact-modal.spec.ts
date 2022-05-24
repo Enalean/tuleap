edd426984e40a16d8d5a3bcc70ee1f6c2d6ab373
Thomas Gorka <thomas.gorka@enalean.com>
2022-05-24 12:36:29 +0200

    fix: adapt e2e modal test to new link field
    part of story #24969 [modal] add and remove links (replace current
    artifact link)
    
    Now the old link field has been trashed, we have to adapt the existing
    tests to the new link. To do so we have now to:
    - Open the link selector
    - Type a query in the search field
    - Select the artifact found
    - Click on the [remove] button
    
    Change-Id: I05749cd94a95a5706ccb61d4422ed0faac6ce0b9

--- plugins/agiledashboard/tests/e2e/cypress/cypress/integration/artifact-modal.spec.ts
+++ plugins/agiledashboard/tests/e2e/cypress/cypress/integration/artifact-modal.spec.ts
@@ -180,8 +180,9 @@ describe(`Artifact Modal`, function () {
                 });
             });
 
-            getFieldWithLabel("Artifact link", "[data-test=artifactlink-field]").within(() => {
-                cy.get("[data-test=artifactlink-field-input]").type(this.artifact_link_id);
+            getFieldWithLabel("Artifact link", "[data-test=artifact-link-field]").within(() => {
+                selectLabelInLinkSelectorDropdown("Linked Artifact", this.artifact_link_id);
+                cy.get("[data-test=add-new-link-button]").click();
             });
 
             cy.get("[data-test=artifact-modal-save-button]").click();
@@ -342,8 +343,8 @@ describe(`Artifact Modal`, function () {
                 });
             });
 
-            getFieldWithLabel("Artifact link", "[data-test=artifactlink-field]").within(() => {
-                cy.get("[data-test=artifactlink-field-input]").clear().type(this.artifact_link_id);
+            getFieldWithLabel("Artifact link", "[data-test=artifact-link-field]").within(() => {
+                cy.get("[data-test=action-button]").click();
             });
 
             cy.get("[data-test=add-comment-form]").within(() => {
@@ -436,6 +437,22 @@ function selectLabelInListPickerDropdown(
         .within(() => {
             cy.get("[data-test-list-picker-dropdown-open]").within(() => {
                 cy.get("[data-test=list-picker-item]").contains(label).click();
+            });
+        });
+}
+
+function selectLabelInLinkSelectorDropdown(
+    label: string,
+    artifact_link_id: number
+): Cypress.Chainable<JQuery<HTMLBodyElement>> {
+    cy.get("[data-test=link-selector-selection]").click();
+    return cy
+        .root()
+        .parents("body")
+        .within(() => {
+            cy.get("[data-test=link-selector-search-field]").type(String(artifact_link_id));
+            cy.get("[data-test-link-selector-dropdown-open]").within(() => {
+                cy.get("[data-test=link-selector-item]").contains(label).click();
             });
         });
 }
