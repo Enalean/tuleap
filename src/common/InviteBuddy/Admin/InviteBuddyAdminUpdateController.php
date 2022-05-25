Enalean/tuleap

commit d6548abeb4b86f6e7db258307154677b34d5f22d

chore(forge-config): convert ConfigDao to EasyDB
Convert to ease writing of tests in a future commit.

Part-of: story #22708 flexibility in the management of Tuleap Configuration

Change-Id: Iae9b2793e08d85fde1756f0fff2ba44d64ecf465

src/common/InviteBuddy/Admin/InviteBuddyAdminUpdateController.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\InviteBuddy\Admin;
 
-use ConfigDao;
+use Tuleap\Config\ConfigDao;
 use HTTPRequest;
 use Tuleap\InviteBuddy\InviteBuddyConfiguration;
 use Tuleap\Layout\BaseLayout;
@@ -93,7 +93,7 @@ class InviteBuddyAdminUpdateController implements DispatchableWithRequest
         }
 
         if (
-            $this->dao->save(
+            $this->dao->saveInt(
                 InviteBuddyConfiguration::CONFIG_MAX_INVITATIONS_BY_DAY,
                 $submitted_max_invitations_by_day
             )

plugins/docman/include/DocmanSettingsSiteAdmin/FilesUploadLimits/DocumentFilesUploadLimitsSaver.php
@@ -28,11 +28,11 @@ use Tuleap\Layout\BaseLayout;
 class DocumentFilesUploadLimitsSaver
 {
     /**
-     * @var \ConfigDao
+     * @var \Tuleap\Config\ConfigDao
      */
     private $config_dao;
 
-    public function __construct(\ConfigDao $dao)
+    public function __construct(\Tuleap\Config\ConfigDao $dao)
     {
         $this->config_dao = $dao;
     }
@@ -48,7 +48,7 @@ class DocumentFilesUploadLimitsSaver
             return;
         }
 
-        $this->config_dao->save(DocmanPlugin::PLUGIN_DOCMAN_MAX_NB_FILE_UPLOADS_SETTING, $nb_files);
+        $this->config_dao->saveInt(DocmanPlugin::PLUGIN_DOCMAN_MAX_NB_FILE_UPLOADS_SETTING, $nb_files);
     }
 
     public function saveMaxFileSize(\HTTPRequest $request, BaseLayout $layout): void
@@ -62,6 +62,6 @@ class DocumentFilesUploadLimitsSaver
             return;
         }
 
-        $this->config_dao->save(DocmanPlugin::PLUGIN_DOCMAN_MAX_FILE_SIZE_SETTING, $max_file_size);
+        $this->config_dao->saveInt(DocmanPlugin::PLUGIN_DOCMAN_MAX_FILE_SIZE_SETTING, $max_file_size);
     }
 }

plugins/docman/include/docmanPlugin.php
@@ -30,6 +30,7 @@ use Tuleap\admin\PendingElements\PendingDocumentsRetriever;
 use Tuleap\Admin\SiteAdministrationAddOption;
 use Tuleap\Admin\SiteAdministrationPluginOption;
 use Tuleap\CLI\Events\GetWhitelistedKeys;
+use Tuleap\Config\ConfigDao;
 use Tuleap\Config\ConfigKey;
 use Tuleap\date\RelativeDatesAssetsRetriever;
 use Tuleap\DB\DBFactory;

plugins/document/include/Config/Admin/FilesDownloadLimitsAdminSaveController.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Document\Config\Admin;
 
-use ConfigDao;
+use Tuleap\Config\ConfigDao;
 use CSRFSynchronizerToken;
 use HTTPRequest;
 use Tuleap\Document\Config\FileDownloadLimits;
@@ -101,15 +101,14 @@ class FilesDownloadLimitsAdminSaveController implements DispatchableWithRequest
 
     private function save(int $max_archive_size, int $warning_threshold, BaseLayout $layout): void
     {
-        $success = $this->config_dao->save(FileDownloadLimits::WARNING_THRESHOLD_NAME, $warning_threshold);
-        $success = $this->config_dao->save(FileDownloadLimits::MAX_ARCHIVE_SIZE_NAME, $max_archive_size) && $success;
-
-        if ($success) {
+        try {
+            $this->config_dao->saveInt(FileDownloadLimits::WARNING_THRESHOLD_NAME, $warning_threshold);
+            $this->config_dao->saveInt(FileDownloadLimits::MAX_ARCHIVE_SIZE_NAME, $max_archive_size);
             $layout->addFeedback(
                 \Feedback::INFO,
                 dgettext('tuleap-document', 'Settings have been saved successfully.')
             );
-        } else {
+        } catch (\Exception) {
             $layout->addFeedback(
                 \Feedback::ERROR,
                 dgettext('tuleap-document', 'An error occurred while saving configuration.')

plugins/document/include/Config/Admin/HistoryEnforcementAdminSaveController.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Document\Config\Admin;
 
-use ConfigDao;
+use Tuleap\Config\ConfigDao;
 use CSRFSynchronizerToken;
 use HTTPRequest;
 use Tuleap\Document\Config\HistoryEnforcementSettings;
@@ -73,17 +73,16 @@ class HistoryEnforcementAdminSaveController implements DispatchableWithRequest
 
     private function save(bool $is_changelog_proposed_after_dnd, BaseLayout $layout): void
     {
-        $success = $this->config_dao->save(
-            HistoryEnforcementSettings::IS_CHANGELOG_PROPOSED_AFTER_DND,
-            $is_changelog_proposed_after_dnd
-        );
-
-        if ($success) {
+        try {
+            $this->config_dao->saveBool(
+                HistoryEnforcementSettings::IS_CHANGELOG_PROPOSED_AFTER_DND,
+                $is_changelog_proposed_after_dnd
+            );
             $layout->addFeedback(
                 \Feedback::INFO,
                 dgettext('tuleap-document', 'Settings have been saved successfully.')
             );
-        } else {
+        } catch (\Exception) {
             $layout->addFeedback(
                 \Feedback::ERROR,
                 dgettext('tuleap-document', 'An error occurred while saving configuration.')

plugins/document/tests/unit/Config/Admin/FilesDownloadLimitsAdminSaveControllerTest.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Document\Config\Admin;
 
-use ConfigDao;
+use Tuleap\Config\ConfigDao;
 use CSRFSynchronizerToken;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
@@ -85,12 +85,12 @@ class FilesDownloadLimitsAdminSaveControllerTest extends \Tuleap\Test\PHPUnit\Te
         $this->token->shouldReceive('check')->once();
 
         $this->config_dao
-            ->shouldReceive('save')
+            ->shouldReceive('saveInt')
             ->with('plugin_document_max_archive_size', 2000)
             ->once()
             ->andReturnTrue();
         $this->config_dao
-            ->shouldReceive('save')
+            ->shouldReceive('saveInt')
             ->with('plugin_document_warning_threshold', 25)
             ->once()
             ->andReturnTrue();

plugins/document/tests/unit/Config/Admin/HistoryEnforcementAdminSaveControllerTest.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace Tuleap\Document\Config\Admin;
 
-use ConfigDao;
+use Tuleap\Config\ConfigDao;
 use CSRFSynchronizerToken;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
@@ -85,8 +85,8 @@ final class HistoryEnforcementAdminSaveControllerTest extends \Tuleap\Test\PHPUn
         $this->token->shouldReceive('check')->once();
 
         $this->config_dao
-            ->shouldReceive('save')
-            ->with(HistoryEnforcementSettings::IS_CHANGELOG_PROPOSED_AFTER_DND, 1)
+            ->shouldReceive('saveBool')
+            ->with(HistoryEnforcementSettings::IS_CHANGELOG_PROPOSED_AFTER_DND, true)
             ->once()
             ->andReturnTrue();
 

plugins/svn/include/svnPlugin.php
@@ -29,6 +29,7 @@ use Tuleap\BurningParrotCompatiblePageDetector;
 use Tuleap\BurningParrotCompatiblePageEvent;
 use Tuleap\CLI\CLICommandsCollector;
 use Tuleap\CLI\Events\GetWhitelistedKeys;
+use Tuleap\Config\ConfigDao;
 use Tuleap\Config\ConfigSet;
 use Tuleap\Error\ProjectAccessSuspendedController;
 use Tuleap\Event\Events\ExportXmlProject;

commit 533efb6fb1cb460c114cf49ecc77463910fa86fe

Fix request #27131 Updating invitations generates an error
Was introduced by d6548abeb4b86f6e7db258307154677b34d5f22d

Previously the code was expected to return something if update was wrong
but today it will trigger an exception that will be caught by the catch
all of exceptions. As an exception will only be thrown if there is an
error that prevent to write in the database, it's safe to let the catch
all do its job.

Change-Id: I023bf049b9992adbf209f595c23a961a601b6a56

site-content/fr_FR/LC_MESSAGES/tuleap-core.po
@@ -628,9 +628,6 @@ msgstr ""
 "Une erreur est survenue lors de la mise à jour des délégations de "
 "permissions."
 
-msgid "An error occurred while updating the settings."
-msgstr "Une erreur est survenue lors de sauvegarde de la configuration."
-
 msgid ""
 "An error occurred with your upload. Please try again or choose another image."
 msgstr ""

site-content/pt_BR/LC_MESSAGES/tuleap-core.po
@@ -622,9 +622,6 @@ msgstr[1] "Ocorreu um erro ao tentar enviar os convites"
 msgid "An error occurred while trying to update permissions delegations."
 msgstr "Ocorreu um erro ao tentar atualizar as permissões das delegações."
 
-msgid "An error occurred while updating the settings."
-msgstr "Ocorreu um erro ao atualizar as configurações."
-
 msgid ""
 "An error occurred with your upload. Please try again or choose another image."
 msgstr ""

src/common/InviteBuddy/Admin/InviteBuddyAdminUpdateController.php
@@ -92,22 +92,15 @@ class InviteBuddyAdminUpdateController implements DispatchableWithRequest
             return;
         }
 
-        if (
-            $this->dao->saveInt(
-                InviteBuddyConfiguration::CONFIG_MAX_INVITATIONS_BY_DAY,
-                $submitted_max_invitations_by_day
-            )
-        ) {
-            $layout->addFeedback(
-                \Feedback::INFO,
-                _("Invitations settings successfully updated.")
-            );
-        } else {
-            $layout->addFeedback(
-                \Feedback::ERROR,
-                _("An error occurred while updating the settings.")
-            );
-        }
+        $this->dao->saveInt(
+            InviteBuddyConfiguration::CONFIG_MAX_INVITATIONS_BY_DAY,
+            $submitted_max_invitations_by_day
+        );
+
+        $layout->addFeedback(
+            \Feedback::INFO,
+            _("Invitations settings successfully updated.")
+        );
 
         $this->redirect($layout);
     }