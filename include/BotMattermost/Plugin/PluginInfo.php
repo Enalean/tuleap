8ab13e8e0e2562bf972109be5de4396d7c3df510
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-06-03 17:22:24 +0200

    Stop setting the plugin version
    This information is not used anymore.
    We set the `$version` attribute to the same value than `$description` so
    we can this unused attribute without breaking anything.
    
    request #27152: Drop plugin version information in the web UI
    
    Change-Id: If552d06dfcb9bad0a55511ac637625f15e8b5687

--- include/BotMattermost/Plugin/PluginInfo.php
+++ include/BotMattermost/Plugin/PluginInfo.php
@@ -26,6 +26,12 @@ class PluginInfo extends \PluginInfo
     {
         parent::__construct($plugin);
 
-        $this->setPluginDescriptor(new PluginDescriptor());
+        $this->setPluginDescriptor(
+            new \PluginDescriptor(
+                dgettext('tuleap-botmattermost', 'Bot Mattermost'),
+                dgettext('tuleap-botmattermost', 'Bot to send notifications in Mattermost'),
+                dgettext('tuleap-botmattermost', 'Bot to send notifications in Mattermost')
+            )
+        );
     }
 }
