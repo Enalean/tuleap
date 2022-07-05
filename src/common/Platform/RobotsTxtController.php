161ec8cf1b557e53ff3ea9a0ad0e377434073daa
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-07-05 16:54:05 +0200

    Closes request #27541: /robots.txt should be accessible even if the instance is not accessible to anonymous users
    Change-Id: Ic933b257abe39dcc55f7b3e8d71af5efafe9513c

---
+++ src/common/Platform/RobotsTxtController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Platform;
+
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Tuleap\Request\DispatchablePSR15Compatible;
+use Tuleap\Request\DispatchableWithRequestNoAuthz;
+
+final class RobotsTxtController extends DispatchablePSR15Compatible implements DispatchableWithRequestNoAuthz
+{
+    public function __construct(
+        private ResponseFactoryInterface $response_factory,
+        private StreamFactoryInterface $stream_factory,
+        EmitterInterface $emitter,
+        MiddlewareInterface ...$middleware_stack,
+    ) {
+        parent::__construct($emitter, ...$middleware_stack);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->response_factory->createResponse()
+            ->withHeader('Content-type', 'text/plain')
+            ->withBody($this->stream_factory->createStreamFromFile($GLOBALS['Language']->getContent('include/robots', 'en_US')));
+    }
+}
---
+++ tests/unit/common/Platform/RobotsTxtControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Platform;
+
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+use Tuleap\GlobalLanguageMock;
+use Tuleap\Http\HTTPFactoryBuilder;
+use Tuleap\Http\Server\NullServerRequest;
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class RobotsTxtControllerTest extends TestCase
+{
+    use GlobalLanguageMock;
+
+    public function testSuccessfullyAnswersToRobotsTxtRequest(): void
+    {
+        $controller = new RobotsTxtController(
+            HTTPFactoryBuilder::responseFactory(),
+            HTTPFactoryBuilder::streamFactory(),
+            $this->createStub(EmitterInterface::class)
+        );
+
+        $GLOBALS['Language']->method('getContent')->willReturn(__FILE__);
+
+        $response = $controller->handle(new NullServerRequest());
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertNotEmpty($response->getBody()->getContents());
+    }
+}
