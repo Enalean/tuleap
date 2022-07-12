45ec04e089c19201fba8ea6da00bf48473fd2095
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-07-12 14:44:32 +0200

    Bootstrap ONLYOFFICE plugin
    The plugin does nothing but it can be installed on a dev instance.
    
    Part of story #27555: display Office documents stored in the Document plugin
    
    Change-Id: Iffaabd55c433df5653b2af8001eb58c9bd9a897c

---
+++ plugins/onlyoffice/include/onlyofficePlugin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../docman/include/docmanPlugin.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class onlyofficePlugin extends Plugin
+{
+    public function __construct(?int $id)
+    {
+        parent::__construct($id);
+        $this->setScope(self::SCOPE_SYSTEM);
+        bindtextdomain('tuleap-onlyoffice', __DIR__ . '/../site-content');
+    }
+
+    public function getPluginInfo(): PluginInfo
+    {
+        if ($this->pluginInfo === null) {
+            $plugin_info = new PluginInfo($this);
+            $plugin_info->setPluginDescriptor(
+                new PluginDescriptor(
+                    dgettext('tuleap-onlyoffice', 'ONLYOFFICE integration'),
+                    dgettext('tuleap-onlyoffice', 'Allow to open documents into ONLYOFFICE')
+                )
+            );
+            $this->pluginInfo = $plugin_info;
+        }
+
+        return $this->pluginInfo;
+    }
+
+    public function getDependencies(): array
+    {
+        return ['docman'];
+    }
+}
