70658b8cbb39078b4ad70ab766f155c62b66b101
Fabio Silva <105317332+fabiofsilva79@users.noreply.github.com>
2022-07-22 20:00:11 -0300

    WIP - pt_BR translation

---
+++ site-content/pt_BR/layout/footer_links.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<h3 class="footer-section-title">Contato</h3>
+<p class="footer-paragraph">
+    <a class="footer-link" href="/contact.php"><i class="fas fa-fw fa-envelope" aria-hidden="true"></i> Fale conosco</a>
+    <br>
+    <a class="footer-link" href="https://twitter.com/TuleapOpenALM" target="_blank" rel="noreferrer" title="Siga o Tuleap no Twitter">
+        <i class="fab fa-fw fa-twitter" aria-hidden="true"></i> @TuleapOpenALM
+    </a>
+    <br>
+    <a class="footer-link" href="https://www.linkedin.com/company/tuleap" target="_blank" rel="noreferrer" title="Siga o Tuleap no LinkedIn">
+        <i class="fab fa-fw fa-linkedin-in" aria-hidden="true"></i> Siga o Tuleap
+    </a>
+</p>
