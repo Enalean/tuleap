700b5db0371569178da81e901c3bf3747439a399
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2022-07-25 09:32:32 +0200

    request #27890 Improve cypress configuration
    have only one cypress file configuration in TypeScript
    have only one folder for screenshots and videos
    have only one result.xml to help debugging
    
    Change-Id: If72765f9e6cbb67c015ce5421bbdcce01a695f78

---
+++ lib/test-utils/cypress-configurator/src/cypress-config.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export const cypress_config = {
+    reporter: "junit",
+    reporterOptions: {
+        mochaFile: "/output/e2e-results-[hash].xml",
+    },
+    screenshotsFolder: "/output/e2e-screenshots",
+    videosFolder: "/output/e2e-videos",
+    video: true,
+    videoUploadOnPasses: false,
+    pageLoadTimeout: 120000,
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    trashAssetsBeforeRuns: false,
+    e2e: {
+        baseUrl: "https://tuleap",
+    },
+};
