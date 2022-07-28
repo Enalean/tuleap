Enalean/tuleap

commit 165960f19d8b4d63872e2ef2aaddcb3335bfed53

Convert most of the plugin libraries to Vitest
Only the `@tuleap/plugin-agiledashboard-card-fields` package has not yet
been migrated. It seems to require a bit more work than importing
`vitest` functions explicitly.

Part of request #27556: Migrate Vite-based packages to Vitest

Change-Id: Iab03795f9c0b7dddb8fc4b4f2e7dce41b1d88ef1

plugins/document_generation/jest.config.mjs
@@ -1,30 +1,0 @@
-/**
- * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-process.env.DISABLE_TS_TYPECHECK = "true";
-
-import { jest_base_config }  from "@tuleap/build-system-configurator";
-export default {
-    ...jest_base_config,
-    displayName: "document_generation",
-    transform: {
-        ...jest_base_config.transform,
-        "^.+\\.vue$": "@vue/vue3-jest",
-    },
-};

lib/frontend/build-system-configurator/src/vite/vite-configurator.ts
@@ -19,6 +19,7 @@
 
 import path from "path";
 import type { UserConfigExport } from "vitest/config";
+import { configDefaults as config_defaults_vitest } from "vitest/config";
 import type { BuildOptions, CSSOptions, ServerOptions, UserConfig } from "vite";
 import { defineConfig as viteDefineConfig } from "vitest/config";
 import type { C8Options } from "vitest";
@@ -115,14 +116,13 @@ function defineBaseConfig(config: UserConfig): UserConfigExport {
             environment: "jsdom",
             include: ["**/?(*.)+(test).{js,ts}"],
             exclude: [
-                "**/node_modules/**",
+                ...config_defaults_vitest.exclude,
                 "**/vendor/**",
                 "**/assets/**",
                 "**/frontend-assets/**",
-                "**/dist/**",
                 "**/tests/**",
                 "**/*.d.ts",
-                "**/scripts/lib/**",
+                "./scripts/lib/**",
                 "**/js-test-results/**",
             ],
             setupFiles: [

lib/frontend/tlp-fetch/mocks/tlp-fetch-mock-helper.d.ts
@@ -17,6 +17,9 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import type { SpyInstance } from "vitest";
+import type { get, recursiveGet, options, head, post, put, del, patch } from "../src/fetch-wrapper";
+
 interface MockFetchSuccessOptions {
     headers?: Partial<Headers>;
     return_json?: unknown;
@@ -28,11 +31,22 @@ interface MockFetchErrorOptions {
     error_json?: Record<string, unknown>;
 }
 
+type MockedTlpFetchFunction<TypeOfArrayItem> =
+    | jest.SpyInstance<Promise<Response | Array<TypeOfArrayItem>>>
+    | SpyInstance<Parameters<typeof get>, Promise<Response>>
+    | SpyInstance<Parameters<typeof options>, Promise<Response>>
+    | SpyInstance<Parameters<typeof head>, Promise<Response>>
+    | SpyInstance<Parameters<typeof post>, Promise<Response>>
+    | SpyInstance<Parameters<typeof put>, Promise<Response>>
+    | SpyInstance<Parameters<typeof del>, Promise<Response>>
+    | SpyInstance<Parameters<typeof patch>, Promise<Response>>
+    | SpyInstance<Parameters<typeof recursiveGet>, Promise<Array<TypeOfArrayItem>>>;
+
 export function mockFetchSuccess<TypeOfArrayItem>(
-    mocked_function: jest.SpyInstance<Promise<Response | Array<TypeOfArrayItem>>>,
+    mocked_function: MockedTlpFetchFunction<TypeOfArrayItem>,
     options?: MockFetchSuccessOptions
 ): void;
 export function mockFetchError<TypeOfArrayItem>(
-    mocked_function: jest.SpyInstance<Promise<Response | Array<TypeOfArrayItem>>>,
+    mocked_function: MockedTlpFetchFunction<TypeOfArrayItem>,
     options?: MockFetchErrorOptions
 ): void;

plugins/agiledashboard/scripts/lib/shared/jest.config.js
@@ -1,26 +1,0 @@
-/*
- * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-process.env.DISABLE_TS_TYPECHECK = "true";
-
-import { jest_base_config } from "@tuleap/build-system-configurator";
-export default {
-    ...jest_base_config,
-    displayName: "agiledashboard-shared",
-};

plugins/agiledashboard/scripts/lib/shared/src/error-message-helper.test.ts
@@ -17,6 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { describe, it, expect } from "vitest";
 import { extractErrorMessage } from "./error-message-helper";
 import { FetchWrapperError } from "@tuleap/tlp-fetch";
 

plugins/document_generation/scripts/lib/docx/jest.config.js
@@ -1,26 +1,0 @@
-/*
- * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-process.env.DISABLE_TS_TYPECHECK = "true";
-
-import { jest_base_config } from "@tuleap/build-system-configurator";
-export default {
-    ...jest_base_config,
-    displayName: "@tuleap/plugin-docgen-docx",
-};

plugins/document_generation/scripts/lib/docx/src/Artifact/artifacts-retriever.test.ts
@@ -17,14 +17,17 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { describe, it, expect, vi } from "vitest";
 import * as tlp from "@tuleap/tlp-fetch";
 import type { ArtifactResponse } from "../type";
 import { mockFetchSuccess } from "@tuleap/tlp-fetch/mocks/tlp-fetch-mock-helper";
 import { getArtifacts } from "./artifacts-retriever";
 
+vi.mock("@tuleap/tlp-fetch");
+
 describe("getArtifacts", () => {
     it("retrieves a bunch of artifacts", async () => {
-        const tlpGet = jest.spyOn(tlp, "get");
+        const tlpGet = vi.spyOn(tlp, "get");
 
         const expected_artifacts = [{ id: 12 } as ArtifactResponse, { id: 14 } as ArtifactResponse];
 
@@ -39,7 +42,7 @@ describe("getArtifacts", () => {
     });
 
     it("sends no queries when no artifacts needs to be retrieved", async () => {
-        const tlpGet = jest.spyOn(tlp, "get");
+        const tlpGet = vi.spyOn(tlp, "get");
         const artifacts = await getArtifacts(new Set());
         expect(artifacts.size).toBe(0);
         expect(tlpGet).not.toBeCalled();

plugins/document_generation/scripts/lib/docx/src/Artifact/artifacts-structure-retriever.test.ts
@@ -17,6 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { describe, it, expect, vi } from "vitest";
 import * as rest_querier from "./artifacts-retriever";
 import type {
     ArtifactReportResponseUserRepresentation,
@@ -187,7 +188,7 @@ describe("retrieveArtifactsStructure", () => {
             },
         ];
 
-        jest.spyOn(rest_querier, "getArtifacts").mockResolvedValue(
+        vi.spyOn(rest_querier, "getArtifacts").mockResolvedValue(
             new Map([
                 [
                     359,

commit 79de663e8426210f5bee1a0622ba368675dcd7a0

@tuleap/plugin-document_generation is not yet converted to Vitest
Issue introduced by 165960f19d8b4d63872e2ef2aaddcb3335bfed53.
Pipeline did not catch the mistake on the patch introducing it, will
deal with that in a dedicated contribution.

Part of request #27556: Migrate Vite-based packages to Vitest

Change-Id: Ifaaeeeedf569bcefedb40c81b4a4c04dcd8a69cb

plugins/document_generation/jest.config.mjs
@@ -1,0 +1,30 @@
+/**
+ * Copyright (c) Enalean, 2021-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+process.env.DISABLE_TS_TYPECHECK = "true";
+
+import { jest_base_config }  from "@tuleap/build-system-configurator";
+export default {
+    ...jest_base_config,
+    displayName: "document_generation",
+    transform: {
+        ...jest_base_config.transform,
+        "^.+\\.vue$": "@vue/vue3-jest",
+    },
+};