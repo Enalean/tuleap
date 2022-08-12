b4c3aa0f8b541e56531740586e1c6bc32ad8042f
Joris Masson <joris.masson@enalean.com>
2022-08-12 17:37:18 +0200

    fix: Fatal error in mention tooltips
    part of request #27932 Replace PFUser::getName and PFUser::getUnixName
    with PFUser::getUserName
    
    How to test:
    - Without this patch, hover your mouse over a `@username` mention, for
    example in Tracker artifact view. You should get an exception with a
    stack trace in codendi_syslog
    - With it, the tooltip correctly shows the user's avatar image, full
    name and username, id and email.
    
    Introduced by 1026a3805d1194115ca0f6a20218bd8d23829ef1
    
    Note: instead of just fixing, I added presenters. If/when we rename
    something else, we can look for usage in PHP code instead of mustache
    template (where we don't have any context).
    
    Change-Id: I8ab78d53dedb2fc4c7f0838db98d990988dc9365

---
+++ tests/unit/common/User/Profile/UserTooltipBodyPresenterTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\User\Profile;
+
+use Tuleap\Test\Builders\UserTestBuilder;
+
+final class UserTooltipBodyPresenterTest extends \Tuleap\Test\PHPUnit\TestCase
+{
+    private const USER_ID = 132;
+    private const EMAIL   = 'goosish@example.com';
+
+    public function testItBuildsFromUser(): void
+    {
+        $user = UserTestBuilder::aUser()
+            ->withId(self::USER_ID)
+            ->withEmail(self::EMAIL)
+            ->build();
+
+        $presenter = UserTooltipBodyPresenter::fromUser($user);
+
+        self::assertSame(self::USER_ID, $presenter->user_id);
+        self::assertSame(self::EMAIL, $presenter->email);
+    }
+}
