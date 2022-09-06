0e573ab9860b91ce7c68bfab7019927c544afcbb
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2022-09-06 14:48:29 +0200

    fix: Approval mail to new user can have multiple languages
    This closes request #28281 Approval mail to new user can have multiple languages
    
    Given a site administrator that approves a new user from the global administration interface,
    if its Tuleap language is not the same as the platform default language (or en_US if not defined),
    then the approval mail received by the user will have multiple content languages.
    
    The title is translated using the site administrator defined language,
    and the body content of the mail uses the platform default language (or en_US if not defined).
    
    Both title and body contents must use the newly approved user language.
    
    Please note that these emails contain non clickable links. This will be handled in a dedicated request if needed.
    Please also note that other user involved emails sent by an action done by a site administrator
    will use the user language and no more the site administrator language
    
    Change-Id: Ifc5288b280e79977c02e22d47e8f7e038f7951c7

--- src/www/account/register.php
+++ src/www/account/register.php
@@ -36,8 +36,6 @@ $request               = HTTPRequest::instance();
 $page                  = $request->get('page');
 $confirmation_register = false;
 
-$GLOBALS['Language']->loadLanguage($request->getCurrentUser()->getLocale());
-
 // ###### function register_valid()
 // ###### checks for valid register from form post
 if ($page == "admin_creation") {
@@ -338,7 +336,7 @@ if ($request->isPost() && $request->exist('Register')) {
                     );
                 }
                 $presenter       = new MailPresenterFactory();
-                $email_presenter = $presenter->createMailAccountPresenter($user_name, $mail_confirm_code, "user", (string) $logo_retriever->getLegacyUrl());
+                $email_presenter = $presenter->createMailAccountPresenter($request->getCurrentUser(), $user_name, $mail_confirm_code, "user", (string) $logo_retriever->getLegacyUrl());
             }
 
             $title = _('Your account is created, </br>it now needs to be activated.');
@@ -365,7 +363,7 @@ if ($request->isPost() && $request->exist('Register')) {
             $redirect_url     = '/';
             $redirect_content = _('Go to the home page');
             $presenter        = new MailPresenterFactory();
-            $email_presenter  = $presenter->createMailAccountPresenter($user_name, $mail_confirm_code, "user", (string) $logo_retriever->getLegacyUrl());
+            $email_presenter  = $presenter->createMailAccountPresenter($request->getCurrentUser(), $user_name, $mail_confirm_code, "user", (string) $logo_retriever->getLegacyUrl());
         }
         $presenter = new Account_ConfirmationPresenter(
             $title,
