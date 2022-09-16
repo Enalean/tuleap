311dd7653f3d3106bc6faba5a6b33781487ad031
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-09-16 16:31:45 +0200

    Init `fts_meilisearch` plugin and `tuleap-meilisearch-server` package
    To test the `tuleap-meilisearch-server` package you can build it with
    `nix-build ./plugins/fts_meilisearch/additional-packages/tuleap-meilisearch-server.nix`,
    copy the resulting inside your dev env and install it. You should be
    able to execute `/usr/bin/tuleap-meilisearch -V`.
    
    Meilisearch is built statically (like the other binaries we deploy) to
    avoid pain with incompatible glibc versions or even other libs.
    Meilisearch is mainly written in Rust but some of the dependencies it
    relies on links against C code. To make our (sort of) cross-compilation
    easy and not battle against Musl and gcc we use Zig which make the process
    seamless [0].
    
    Part of story #28835: have a full-text search backend based on Meilisearch
    
    [0] https://andrewkelley.me/post/zig-cc-powerful-drop-in-replacement-gcc-clang.html
    
    Change-Id: I781acc50c6aa9fa3a573358ba32e4bd9470a0047

---
+++ plugins/fts_meilisearch/additional-packages/tuleap-meilisearch-server.nix
@@ -0,0 +1,84 @@
+{
+  pkgs ? (import ../../../tools/utils/nix/pinned-nixpkgs.nix) {},
+  nixpkgsPinEpoch ? (import ../../../tools/utils/nix/nixpkgs-pin-epoch.nix) { inherit pkgs; }
+}:
+
+let
+  baseArchTarget = "x86_64";
+  buildTargetRust = "${baseArchTarget}-unknown-linux-musl";
+  rustBinWithMuslTarget = pkgs.rust-bin.stable.latest.minimal.override {
+    targets = [ buildTargetRust ];
+  };
+  zigCC = pkgs.writeShellScriptBin "zigcc" ''
+    ${pkgs.zig}/bin/zig cc -target ${baseArchTarget}-linux-musl $@
+  '';
+  tuleapMeilisearchBin = pkgs.stdenvNoCC.mkDerivation rec {
+    pname = "tuleap-meilisearch";
+    version = "0.28.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "meilisearch";
+      repo = "MeiliSearch";
+      rev = "v${version}";
+      sha256 = "sha256-AjSzzOQ1K47tyc8Zn7kAU3B1UE9Tfvd3SvP7W13m6/o=";
+    };
+    cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+      inherit src;
+      hash = "sha256-RfH782A2+rngS7WHHa+nkR1HQpHieS1U+0LdHC99v0k=";
+    };
+
+    nativeBuildInputs = [ pkgs.rustPlatform.cargoSetupHook rustBinWithMuslTarget zigCC ];
+
+    # Tests will try to compile with mini-dashboard features which downloads something from the internet.
+    doCheck = false;
+
+    buildPhase = ''
+      runHook preBuild
+      HOME="$TMPDIR" CC="zigcc" cargo build --release --no-default-features --target ${buildTargetRust}
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/bin/
+      cp target/${buildTargetRust}/release/meilisearch $out/bin/${pname}
+      runHook postInstall
+    '';
+  };
+  tuleapVersion = builtins.readFile ../../../VERSION;
+in pkgs.stdenvNoCC.mkDerivation {
+  name = "tuleap-meilisearch-server-rpm-package";
+  srcs = [
+    "${tuleapMeilisearchBin}/bin/tuleap-meilisearch"
+  ];
+
+  nativeBuildInputs = [ pkgs.rpm ];
+
+  dontConfigure = true;
+
+  unpackPhase = ''
+    for srcFile in $srcs; do
+     cp -a $srcFile $(stripHash $srcFile)
+    done
+  '';
+
+  buildPhase = ''
+    rpmbuild \
+     --define "tuleap_version ${tuleapVersion}" \
+     --define "nixpkgs_epoch .${nixpkgsPinEpoch}" \
+     --define "_sourcedir $(pwd)" \
+     --define "_rpmdir $(pwd)" \
+     --dbpath="$(pwd)"/rpmdb \
+     --define "%_topdir $(pwd)" \
+     --define "%_tmppath %{_topdir}/TMP" \
+     --define "_rpmdir $(pwd)/RPMS" \
+     --define "%_bindir /usr/bin" \
+     -bb ${./tuleap-meilisearch-server/tuleap-meilisearch-server.spec}
+  '';
+
+  installPhase = ''
+    mkdir $out/
+    mv RPMS/${baseArchTarget}/*.rpm $out/
+  '';
+
+  dontFixUp = true;
+}
