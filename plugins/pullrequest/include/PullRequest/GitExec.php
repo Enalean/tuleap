Enalean/tuleap

commit e2b6a5bfca360063ef64879d94207b2ac1cd26fe

Closes request #28889: Harden calls to Git CLI against argument injection
The goal is to prevent the possibility for a malicious user to execute
arbitrary command/write arbitrary files (see CVE-2021-29472 for
example).

No significant functional changes expected.

Change-Id: I5d03f184acbde07faaf1890a713c2b02779c6f21

plugins/pullrequest/include/PullRequest/GitExec.php
@@ -34,7 +34,7 @@ class GitExec extends Git_Exec
         $output = [];
 
         try {
-            $this->gitCmdWithOutput('show-ref --hash ' . escapeshellarg($branch_name), $output);
+            $this->gitCmdWithOutput('show-ref --hash -- ' . escapeshellarg($branch_name), $output);
         } catch (Git_Command_Exception $exception) {
             throw new UnknownBranchNameException($branch_name, 0, $exception);
         }
@@ -82,7 +82,7 @@ class GitExec extends Git_Exec
         $output = [];
         $remote = escapeshellarg($remote);
         $branch = escapeshellarg($branch_name);
-        $cmd    = "clone --shared -b $branch $remote " . escapeshellarg($this->getPath());
+        $cmd    = "clone --shared -b -- $branch $remote " . escapeshellarg($this->getPath());
 
         $retVal = 1;
         $git    = self::getGitCommand();
@@ -103,12 +103,12 @@ class GitExec extends Git_Exec
         $reference = escapeshellarg($reference);
 
         $this->setLocalCommiter($user->getRealName(), $user->getEmail());
-        return $this->gitCmdWithOutput("merge --no-edit " . $reference, $output);
+        return $this->gitCmdWithOutput("merge --no-edit -- " . $reference, $output);
     }
 
     public function fastForwardMergeOnly($reference)
     {
-        $this->gitCmdWithOutput('merge --ff-only ' . escapeshellarg($reference), $output);
+        $this->gitCmdWithOutput('merge --ff-only -- ' . escapeshellarg($reference), $output);
     }
 
     /**
@@ -121,7 +121,7 @@ class GitExec extends Git_Exec
         $first_commit_reference  = escapeshellarg($first_commit_reference);
         $second_commit_reference = escapeshellarg($second_commit_reference);
 
-        $this->gitCmdWithOutput("merge-base $first_commit_reference $second_commit_reference", $output);
+        $this->gitCmdWithOutput("merge-base -- $first_commit_reference $second_commit_reference", $output);
 
         return $output;
     }
@@ -136,7 +136,7 @@ class GitExec extends Git_Exec
         $conflict_marker_regex   = escapeshellarg(self::GIT_MERGE_CONFLICT_MARKER_REGEX);
 
         $this->gitCmdWithOutput(
-            "merge-tree $merge_base $second_commit_reference $first_commit_reference | /usr/bin/grep --max-count=1 --only-matching $conflict_marker_regex || [[ $? == 1 ]]",
+            "merge-tree -- $merge_base $second_commit_reference $first_commit_reference | /usr/bin/grep --max-count=1 --only-matching $conflict_marker_regex || [[ $? == 1 ]]",
             $output
         );
 
@@ -154,7 +154,7 @@ class GitExec extends Git_Exec
     {
         $ref1   = escapeshellarg($ref1);
         $ref2   = escapeshellarg($ref2);
-        $cmd    = "merge-base $ref1 $ref2";
+        $cmd    = "merge-base -- $ref1 $ref2";
         $output = [];
 
         $this->gitCmdWithOutput($cmd, $output);
@@ -168,7 +168,7 @@ class GitExec extends Git_Exec
         $merged_ref = escapeshellarg($merged_ref);
 
         try {
-            $this->gitCmd("merge-base --is-ancestor $merged_ref $base_ref");
+            $this->gitCmd("merge-base --is-ancestor -- $merged_ref $base_ref");
             return true;
         } catch (Git_Command_Exception $e) {
             return false;
@@ -210,7 +210,7 @@ class GitExec extends Git_Exec
     public function removeReference($reference)
     {
         $output = null;
-        $this->execAsGitoliteGroup('update-ref -d ' . escapeshellarg($reference), $output);
+        $this->execAsGitoliteGroup('update-ref -d -- ' . escapeshellarg($reference), $output);
     }
 
     private function parseDiffNumStatOutput($output)

plugins/git/include/CommitMetadata/AuthorRetriever.php
@@ -35,12 +35,11 @@ final class AuthorRetriever implements RetrieveAuthor
      */
     public function getAuthor(string $sha1): UserName
     {
-        $author      = $this->git_exec->getAuthorEmail($sha1);
-        $tuleap_user = $this->user_manager->getUserByEmail($author);
+        $author_information = $this->git_exec->getAuthorInformation($sha1);
+        $tuleap_user        = $this->user_manager->getUserByEmail($author_information['email']);
 
         if (! $tuleap_user) {
-            $author = $this->git_exec->getAuthorName($sha1);
-            return UserName::fromUsername($author);
+            return UserName::fromUsername($author_information['name']);
         }
         return UserName::fromUser($tuleap_user);
     }

plugins/git/include/CommitMetadata/CommitMessageRetriever.php
@@ -30,7 +30,6 @@ final class CommitMessageRetriever implements RetrieveCommitMessage
 
     public function getCommitMessage(string $ref): string
     {
-        $commit_message = $this->git_exec->getCommitMessage($ref);
-        return implode("\n", $commit_message);
+        return $this->git_exec->getCommitMessage($ref);
     }
 }

plugins/git/include/Git/Driver/Gerrit/ProjectCreator.class.php
@@ -280,7 +280,7 @@ class Git_Driver_Gerrit_ProjectCreator
             $this->git_exec->remoteAdd($gerrit_project_url);
         }
         $this->git_exec->pullBranch('origin', 'refs/meta/config');
-        $this->git_exec->checkoutBranch('FETCH_HEAD');
+        $this->git_exec->checkoutToFetchHead();
     }
 
     /**

plugins/git/include/GitPHP/git/Archive.php
@@ -389,6 +389,7 @@ class Archive
         }
 
         $args[] = '--prefix=' . escapeshellarg($this->GetPrefix());
+        $args[] = '--';
         $args[] = escapeshellarg($this->gitObject->GetHash());
 
         $this->handle = $exe->Open(GitExe::ARCHIVE, $args);

plugins/git/include/GitPHP/git/Commit.php
@@ -777,6 +777,7 @@ class Commit extends GitObject
         $exe    = new GitExe($this->GetProject());
         $args   = [];
         $args[] = '--tags';
+        $args[] = '--';
         $args[] = escapeshellarg($this->hash);
         $revs   = explode("\n", $exe->Execute(GitExe::NAME_REV, $args));
 

plugins/git/include/GitPHP/git/Ref.php
@@ -109,6 +109,7 @@ abstract class Ref extends GitObject
         $args   = [];
         $args[] = '--hash';
         $args[] = '--verify';
+        $args[] = '--';
         $args[] = escapeshellarg($this->GetRefPath());
         $hash   = trim($exe->Execute(GitExe::SHOW_REF, $args));
 

plugins/git/include/Git_Exec.class.php
@@ -116,40 +116,32 @@ class Git_Exec
     }
 
     /**
+     * @return array{email: string, name: string}
      * @throws Git_Command_Exception
      */
-    public function getAuthorEmail(string $sha1): string
+    public function getAuthorInformation(string $commit_reference): array
     {
-        $output = [];
-        $this->gitCmdWithOutput('show -s --format=\'%ae\' ' . escapeshellarg($sha1), $output);
+        $commit_content = $this->catFile($commit_reference);
+        if (preg_match('/^author (?<name>.*) <(?<email>.*)> \d+.*$/m', $commit_content, $matches) === 1) {
+            return $matches;
+        }
 
-        return $output[0];
-    }
-
-    /**
-     * @throws Git_Command_Exception
-     */
-    public function getAuthorName(string $sha1): string
-    {
-        $output = [];
-        $this->gitCmdWithOutput('show -s --format=\'%aN\' ' . escapeshellarg($sha1), $output);
-
-        return $output[0];
+        return ['name' => '', 'email' => ''];
     }
 
     public function remoteAdd($remote)
     {
-        $this->gitCmd('remote add origin ' . escapeshellarg($remote));
+        $this->gitCmd('remote add origin -- ' . escapeshellarg($remote));
     }
 
     public function pullBranch($remote, $branch)
     {
-        $this->gitCmd('pull --quiet ' . $remote . ' ' . $branch);
+        $this->gitCmd('pull --quiet -- ' . escapeshellarg($remote) . ' ' . escapeshellarg($branch));
     }
 
-    public function checkoutBranch($branch)
+    public function checkoutToFetchHead(): void
     {
-        $this->gitCmd('checkout --quiet ' . $branch);
+        $this->gitCmd('checkout --quiet FETCH_HEAD');
     }
 
     public function configFile($file, $config)
@@ -170,7 +162,7 @@ class Git_Exec
     {
         $to_name = basename($to);
         $to_path = realpath(dirname($to)) . '/' . $to_name;
-        $cmd     = 'mv ' . escapeshellarg(realpath($from)) . ' ' . escapeshellarg($to_path);
+        $cmd     = 'mv -- ' . escapeshellarg(realpath($from)) . ' ' . escapeshellarg($to_path);
         return $this->gitCmd($cmd);
     }
 
@@ -184,7 +176,7 @@ class Git_Exec
      */
     public function add($file)
     {
-        $cmd = 'add ' . escapeshellarg(realpath($file));
+        $cmd = 'add -- ' . escapeshellarg(realpath($file));
         return $this->gitCmd($cmd);
     }
 
@@ -199,7 +191,7 @@ class Git_Exec
     public function rm($file)
     {
         if ($this->canRemove($file)) {
-            $cmd = 'rm ' . escapeshellarg(realpath($file));
+            $cmd = 'rm -- ' . escapeshellarg(realpath($file));
             return $this->gitCmd($cmd);
         }
         return true;
@@ -208,7 +200,7 @@ class Git_Exec
     public function recursiveRm($file)
     {
         if ($this->canRemove($file)) {
-            $cmd = 'rm -r ' . escapeshellarg(realpath($file));
+            $cmd = 'rm -r -- ' . escapeshellarg(realpath($file));
             return $this->gitCmd($cmd);
         }
         return true;
@@ -217,7 +209,7 @@ class Git_Exec
     private function canRemove($file)
     {
         $output = [];
-        $this->gitCmdWithOutput('status --porcelain ' . escapeshellarg(realpath($file)), $output);
+        $this->gitCmdWithOutput('status --porcelain -- ' . escapeshellarg(realpath($file)), $output);
         return count($output) == 0;
     }
 
@@ -302,7 +294,7 @@ class Git_Exec
     {
         $output = [];
         try {
-            $this->gitCmdWithOutput('symbolic-ref --short ' . escapeshellarg($name), $output);
+            $this->gitCmdWithOutput('symbolic-ref --short -- ' . escapeshellarg($name), $output);
         } catch (Git_Command_Exception $e) {
             return null;
         }
@@ -316,7 +308,7 @@ class Git_Exec
 
     private function setSymbolicRef(string $name, string $reference): void
     {
-        $this->gitCmd(sprintf('symbolic-ref %s %s', escapeshellarg($name), escapeshellarg($reference)));
+        $this->gitCmd(sprintf('symbolic-ref -- %s %s', escapeshellarg($name), escapeshellarg($reference)));
     }
 
     public function getAllTagsSortedByCreationDate(): array
@@ -329,14 +321,14 @@ class Git_Exec
     /**
      * @throws Git_Command_Exception
      */
-    public function getCommitMessage(string $ref): array
+    public function getCommitMessage(string $ref): string
     {
-        $ref    = escapeshellarg($ref);
-        $cmd    = "log -1 $ref --pretty=%B";
-        $output = [];
+        $commit_content = $this->catFile($ref);
+        if (preg_match('/(?<=\n\n)(?<message>[\s\S]*)/', $commit_content, $matches) === 1) {
+            return $matches['message'];
+        }
 
-        $this->gitCmdWithOutput($cmd, $output);
-        return $output;
+        return '';
     }
 
     /**
@@ -347,7 +339,7 @@ class Git_Exec
     public function catFile(string $rev): string
     {
         $output = [];
-        $this->gitCmdWithOutput('cat-file -p ' . escapeshellarg($rev), $output);
+        $this->gitCmdWithOutput('cat-file -p -- ' . escapeshellarg($rev), $output);
         return implode(PHP_EOL, $output);
     }
 
@@ -360,7 +352,7 @@ class Git_Exec
     public function getObjectType(string $rev): string
     {
         $output = [];
-        $this->gitCmdWithOutput('cat-file -t ' . escapeshellarg($rev), $output);
+        $this->gitCmdWithOutput('cat-file -t -- ' . escapeshellarg($rev), $output);
         if (count($output) == 1) {
             return $output[0];
         }
@@ -371,7 +363,7 @@ class Git_Exec
     {
         $output = [];
         try {
-            $this->gitCmdWithOutput('cat-file -e ' . escapeshellarg($rev), $output);
+            $this->gitCmdWithOutput('cat-file -e -- ' . escapeshellarg($rev), $output);
         } catch (Git_Command_Exception $exception) {
             return false;
         }
@@ -422,7 +414,7 @@ class Git_Exec
      */
     public function push($origin = 'origin master')
     {
-        $cmd = 'push --porcelain ' . $origin;
+        $cmd = 'push --porcelain -- ' . $origin;
         return $this->gitCmd($cmd);
     }
 
@@ -433,8 +425,8 @@ class Git_Exec
     {
         $destination_url = escapeshellarg($destination_url);
 
-        $push_heads = "push $destination_url refs/heads/*:refs/heads/*";
-        $push_tags  = "push $destination_url refs/tags/*:refs/tags/*";
+        $push_heads = "push -- $destination_url refs/heads/*:refs/heads/*";
+        $push_tags  = "push -- $destination_url refs/tags/*:refs/tags/*";
 
         $this->gitCmd($push_heads);
         $this->gitCmd($push_tags);
@@ -540,7 +532,7 @@ class Git_Exec
      */
     public function updateRef(string $reference, string $new_value): bool
     {
-        $cmd = 'update-ref ' . escapeshellarg($reference) . ' ' . escapeshellarg($new_value);
+        $cmd = 'update-ref -- ' . escapeshellarg($reference) . ' ' . escapeshellarg($new_value);
 
         return $this->gitCmd($cmd);
     }

commit 351db63d58cff441a843b4a6f7d5e5f10e8b77c5

Cannot merge a PR via the web UI
Issue introduced by e2b6a5bfca360063ef64879d94207b2ac1cd26fe.

request #28889: Harden calls to Git CLI against argument injection

Change-Id: I75b975503e479ed5084277af7df4637f5b85bb6e

plugins/pullrequest/include/PullRequest/GitExec.php
@@ -82,7 +82,7 @@ class GitExec extends Git_Exec
         $output = [];
         $remote = escapeshellarg($remote);
         $branch = escapeshellarg($branch_name);
-        $cmd    = "clone --shared -b -- $branch $remote " . escapeshellarg($this->getPath());
+        $cmd    = "clone --shared -b $branch -- $remote " . escapeshellarg($this->getPath());
 
         $retVal = 1;
         $git    = self::getGitCommand();