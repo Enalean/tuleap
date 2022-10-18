Enalean/tuleap

commit b91bcd57c8344ec2a4c1833629e400cef4dd901a

request #27166: Naming a field "id" breaks the report
Field names are now prefixed in the SQL query in order to avoid conflict
with the static SQL columns we always retrieve.

Change-Id: I7eaaf95e24f52f6e52647d679b5058bc09139b5f

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_LastModifiedBy.php
@@ -67,9 +67,9 @@ class Tracker_FormElement_Field_LastModifiedBy extends Tracker_FormElement_Field
         return '';
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
-        return "c.submitted_by AS `" . $this->name . "`";
+        return "c.submitted_by AS " . $this->getQuerySelectName();
     }
 
     public function getQueryFrom()
@@ -84,14 +84,9 @@ class Tracker_FormElement_Field_LastModifiedBy extends Tracker_FormElement_Field
         return " LEFT JOIN  user AS $R2 ON ($R2.user_id = c.submitted_by ) ";
     }
 
-    public function getQueryGroupby()
+    public function getQueryGroupby(): string
     {
         return '';
-    }
-
-    public function getQueryOrderby()
-    {
-        return $this->name;
     }
 
     public static function getFactoryLabel()

plugins/agiledashboard/include/AgileDashboard/FormElement/Burnup.php
@@ -285,8 +285,9 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
     {
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
+        return '';
     }
 
     public function getRESTValue(PFUser $user, Tracker_Artifact_Changeset $changeset)

plugins/testmanagement/include/TestManagement/Step/Execution/Field/StepExecution.php
@@ -123,7 +123,7 @@ class StepExecution extends Tracker_FormElement_Field implements TrackerFormElem
         return '';
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
         return '';
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.php
@@ -41,6 +41,8 @@ use Tuleap\Tracker\Workflow\SimpleMode\State\TransitionRetriever;
 // phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps
 abstract class Tracker_FormElement_Field extends Tracker_FormElement implements Tracker_Report_Field, Tracker_FormElement_IAcceptFieldVisitor
 {
+    public const PREFIX_NAME_SQL_COLUMN = 'user_defined_';
+
     protected $has_errors = false;
 
     /**
@@ -203,15 +205,19 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
         $this->updateCriteriaValue($criteria, $value);
     }
 
+    final public function getQuerySelectName(): string
+    {
+        return \Tuleap\DB\DBFactory::getMainTuleapDBConnection()->getDB()->escapeIdentifier(self::PREFIX_NAME_SQL_COLUMN . $this->name);
+    }
+
     /**
      * Get the "select" statement to retrieve field values
-     * @return string
      * @see getQueryFrom
      */
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
         $R = 'R_' . $this->id;
-        return "$R.value_id AS `" . $this->name . "`";
+        return "$R.value_id AS " . $this->getQuerySelectName();
     }
 
     /**
@@ -229,15 +235,15 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
     /**
      * Get the "order by" statement to retrieve field values
      */
-    public function getQueryOrderby()
+    public function getQueryOrderby(): string
     {
-        return '`' . $this->name . '`';
+        return $this->getQuerySelectName();
     }
 
     /**
      * Get the "group by" statement to retrieve field values
      */
-    public function getQueryGroupby()
+    public function getQueryGroupby(): string
     {
         $R = 'R_' . $this->id;
         return "$R.value_id";

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Alphanum.php
@@ -70,9 +70,8 @@ abstract class Tracker_FormElement_Field_Alphanum extends Tracker_FormElement_Fi
     /**
      * Get the "group by" statement to retrieve field values
      */
-    public function getQueryGroupby()
+    public function getQueryGroupby(): string
     {
-        $R1 = 'R1_' . $this->id;
         $R2 = 'R2_' . $this->id;
         return "$R2.value";
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactId.php
@@ -38,9 +38,9 @@ class Tracker_FormElement_Field_ArtifactId extends Tracker_FormElement_Field_Int
         return '';
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
-        return "a.id AS `" . $this->name . "`";
+        return "a.id AS " . $this->getQuerySelectName();
     }
 
     public function getQueryFrom()
@@ -51,7 +51,7 @@ class Tracker_FormElement_Field_ArtifactId extends Tracker_FormElement_Field_Int
     /**
      * Get the "group by" statement to retrieve field values
      */
-    public function getQueryGroupby()
+    public function getQueryGroupby(): string
     {
         return "a.id";
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.php
@@ -418,7 +418,7 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field
         return '';
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
         return '';
     }

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.php
@@ -447,8 +447,9 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
     {
     }
 
-    public function getQuerySelect()
+    public function getQuerySelect(): string
     {
+        return '';
     }
 
     public function getQueryFrom()

commit 9548c0d0d472840741516039d5c9609359d0e077

Fixes request #29227: Sorting a tracker report using a "last updated by" field leads to a crash
Introduced by b91bcd57c8344ec2a4c1833629e400cef4dd901a.

Change-Id: Id4b0039115ca5c5a903c4adc6368061abd9ab20d

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_LastModifiedBy.php
@@ -72,6 +72,11 @@ class Tracker_FormElement_Field_LastModifiedBy extends Tracker_FormElement_Field
         return "c.submitted_by AS " . $this->getQuerySelectName();
     }
 
+    public function getQueryOrderby(): string
+    {
+        return $this->getQuerySelectName();
+    }
+
     public function getQueryFrom()
     {
         return '';