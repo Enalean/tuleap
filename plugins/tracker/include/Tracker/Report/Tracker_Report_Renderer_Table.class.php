Enalean/tuleap

commit fbb81f1579fd74b6c751df0bc3a2da974b769cbf

request #27180 Export csv are broken
When we try to export artifacts with CSV export, most of the values are null. For user bound fields, it can throw errors and make the export fail.

Change-Id: I4e4a1183545407ef171b7bb077bd7919093e4840

plugins/tracker/include/Tracker/Report/Tracker_Report_Renderer_Table.class.php
@@ -1213,7 +1213,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                     }
                     foreach ($columns as $key => $column) {
                         if ($column['field']->isUsed()) {
-                            $field_name = Tracker_FormElement_Field::PREFIX_NAME_SQL_COLUMN . $column['field']->name;
+                            $field_name = $column['field']->getPrefixedName();
                             $value      = isset($row[$field_name]) ? $row[$field_name] : null;
                             $html      .= '<td data-column-id="' . $purifier->purify($key) . '">';
 
@@ -1714,7 +1714,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                     if (isset($aggregates[$column['field']->getId()])) {
                         if ($a = $column['field']->getQuerySelectAggregate($aggregates[$column['field']->getId()])) {
                             foreach ($a['separate_queries'] as $sel) {
-                                $queries['aggregates_group_by'][$column['field']->getName() . '_' . $sel['function']] = "SELECT " .
+                                $queries['aggregates_group_by'][$column['field']->getPrefixedName() . '_' . $sel['function']] = "SELECT " .
                                     $sel['select'] .
                                     $from . ' ' . $column['field']->getQueryFromAggregate() .
                                     $where .
@@ -2184,7 +2184,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
     {
         $line = [];
 
-        $value  = isset($row[$column['field']->getName()]) ? $row[$column['field']->getName()] : null;
+        $value  = isset($row[$column['field']->getPrefixedName()]) ? $row[$column['field']->getPrefixedName()] : null;
         $line[] = $column['field']->fetchCSVChangesetValue($row['id'], $row['changeset_id'], $value, $this->report);
 
         if (
@@ -2215,7 +2215,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                 $format
             );
         } else {
-            $value  = isset($row[$column['field']->getName()]) ? $row[$column['field']->getName()] : null;
+            $value  = isset($row[$column['field']->getPrefixedName()]) ? $row[$column['field']->getPrefixedName()] : null;
             $line[] = $column['field']->fetchCSVChangesetValue($row['id'], $row['changeset_id'], $value, $this->report);
         }
 

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.php
@@ -41,7 +41,7 @@ use Tuleap\Tracker\Workflow\SimpleMode\State\TransitionRetriever;
 // phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps
 abstract class Tracker_FormElement_Field extends Tracker_FormElement implements Tracker_Report_Field, Tracker_FormElement_IAcceptFieldVisitor
 {
-    public const PREFIX_NAME_SQL_COLUMN = 'user_defined_';
+    private const PREFIX_NAME_SQL_COLUMN = 'user_defined_';
 
     protected $has_errors = false;
 
@@ -207,7 +207,14 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
 
     final public function getQuerySelectName(): string
     {
-        return \Tuleap\DB\DBFactory::getMainTuleapDBConnection()->getDB()->escapeIdentifier(self::PREFIX_NAME_SQL_COLUMN . $this->name);
+        return \Tuleap\DB\DBFactory::getMainTuleapDBConnection()->getDB()->escapeIdentifier(
+            $this->getPrefixedName()
+        );
+    }
+
+    final public function getPrefixedName(): string
+    {
+        return self::PREFIX_NAME_SQL_COLUMN . $this->name;
     }
 
     /**

tests/psalm/tuleap-baseline.xml
@@ -48364,7 +48364,7 @@
       <code>retrieve</code>
     </DeprecatedMethod>
     <InvalidArrayOffset occurrences="1">
-      <code>$queries['aggregates_group_by'][$column['field']-&gt;getName() . '_' . $sel['function']]</code>
+      <code>$queries['aggregates_group_by'][$column['field']-&gt;getPrefixedName() . '_' . $sel['function']]</code>
     </InvalidArrayOffset>
     <InvalidScalarArgument occurrences="2">
       <code>$value</code>

commit f6a6f20db47fbd84bcc7bd730abfa83421b5ce16

fix: Count (group by) in renderer columns does not work anymore
This closes request #28891 Count (group by) in renderer columns does not work anymore

Count (group by) is not displayed at the bottom of the renderers.
This issue has been introduced by fbb81f1579fd74b6c751df0bc3a2da974b769cbf (Tuleap 13.10)

How to test
----------

* Go to a tracker report
* Add an aggregate count (group by)

=> The aggregater result must be displayed in table report.

/!\ The patch that introduced this issue fixed an error at CSV export.
Please try that this CSV export still works with this fix.

Change-Id: I7b3ff9e96de91537fba9ee9b3572e23addc11242

plugins/tracker/include/Tracker/Report/Tracker_Report_Renderer_Table.class.php
@@ -1697,12 +1697,13 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
 
         //Add group by aggregates
         if ($aggregates) {
+            $queries_aggregates_group_by = [];
             foreach ($columns as $column) {
                 if ($column['field']->isUsed()) {
                     if (isset($aggregates[$column['field']->getId()])) {
                         if ($a = $column['field']->getQuerySelectAggregate($aggregates[$column['field']->getId()])) {
                             foreach ($a['separate_queries'] as $sel) {
-                                $queries['aggregates_group_by'][$column['field']->getPrefixedName() . '_' . $sel['function']] = "SELECT " .
+                                $queries_aggregates_group_by[$column['field']->getName() . '_' . $sel['function']] = "SELECT " .
                                     $sel['select'] .
                                     $from . ' ' . $column['field']->getQueryFromAggregate() .
                                     $where .
@@ -1711,6 +1712,10 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
                         }
                     }
                 }
+            }
+
+            if (count($queries_aggregates_group_by) > 0) {
+                $queries['aggregates_group_by'] = $queries_aggregates_group_by;
             }
         }
 