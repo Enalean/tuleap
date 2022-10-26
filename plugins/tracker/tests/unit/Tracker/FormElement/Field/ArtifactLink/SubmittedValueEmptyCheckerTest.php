2428ca56f233a2993a4a8b2264208f4cc22e747c
Yannis ROSSETTO <yannis.rossetto@enalean.com>
2022-10-26 15:20:55 +0200

    feat: Artifact link field is not empty when there are reverse links
    This is part of request #29254 Artifact link field is not empty when there are reverse links
    
    Given a mandatory artifact link field in a tracker, if I create a new artifact
    by providing only the parent (a reverse link), the field must not be considered as empty.
    
    Tuleap artifact links can be either direct or reverse.
    Having a link field as mandatory implies to have defined links.
    We have to take into account reverse links in this case.
    
    A submitted artifact link value is not empty if:
    
    * Submitting a new links
    * Submitting a new parent
    * Have any reverse links
    * Delete some values but still have at least one
    
    In other cases :
    
    * Does not submit anything and does not have any reverse
    links
    * Deletes all links and does not have any reverse links
    
    The value will be considered empty.
    
    Change-Id: I2383b76465211bfb1343510212f69925d62d6fe2

---
+++ plugins/tracker/tests/unit/Tracker/FormElement/Field/ArtifactLink/SubmittedValueEmptyCheckerTest.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2022 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\FormElement\Field\ArtifactLink;
+
+use Tracker_Artifact_ChangesetValue_ArtifactLink;
+use Tracker_ArtifactLinkInfo;
+use Tracker_FormElement_Field_ArtifactLink;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Tracker\Test\Builders\ArtifactTestBuilder;
+
+final class SubmittedValueEmptyCheckerTest extends TestCase
+{
+    private SubmittedValueEmptyChecker $empty_checker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->empty_checker = new SubmittedValueEmptyChecker();
+    }
+
+    public function testValueIsNotEmptyIfSubmittingParent(): void
+    {
+        $submitted_value = [
+            "parent" => [
+                '1004',
+            ],
+        ];
+
+        self::assertFalse(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->createMock(Tracker_FormElement_Field_ArtifactLink::class),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    public function testValueIsNotEmptyIfSubmittingNewLinks(): void
+    {
+        $submitted_value = [
+            'new_values' => '1004,1005',
+            'type'       => '',
+        ];
+
+        self::assertFalse(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->buildArtifactLinkFieldWithoutData(),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    public function testValueIsNotEmptyIfThereIsAtLeastOneReverseLink(): void
+    {
+        $submitted_value = [];
+
+        self::assertFalse(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->buildArtifactLinkFieldWithReverseLinks(),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    public function testValueIsNotEmptyIfRemovingSomeLinks(): void
+    {
+        $submitted_value = [
+            "removed_values" => [1004],
+        ];
+
+        self::assertFalse(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->buildArtifactLinkFieldWithLinks(),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    public function testValueIsEmptyIfNoDataAtAllForField(): void
+    {
+        $submitted_value = [
+            "new_values" => '',
+            "type" => '',
+            "parent" => [],
+        ];
+
+        self::assertTrue(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->buildArtifactLinkFieldWithoutData(),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    public function testValueIsEmptyIfRemovingAllLinks(): void
+    {
+        $submitted_value = [
+            "removed_values" => [1004, 1005],
+        ];
+
+        self::assertTrue(
+            $this->empty_checker->isSubmittedValueEmpty(
+                $submitted_value,
+                $this->buildArtifactLinkFieldWithLinks(),
+                ArtifactTestBuilder::anArtifact(1)->build(),
+            )
+        );
+    }
+
+    private function buildArtifactLinkFieldWithReverseLinks(): Tracker_FormElement_Field_ArtifactLink
+    {
+        $field = $this->createMock(Tracker_FormElement_Field_ArtifactLink::class);
+        $field->method('getReverseLinks')->willReturn([
+            new Tracker_ArtifactLinkInfo(1, "key", 101, 1, 1, null),
+        ]);
+
+        return $field;
+    }
+
+    private function buildArtifactLinkFieldWithLinks(): Tracker_FormElement_Field_ArtifactLink
+    {
+        $field = $this->createMock(Tracker_FormElement_Field_ArtifactLink::class);
+        $field->method('getReverseLinks')->willReturn([]);
+
+        $last_changeset_value = $this->createMock(Tracker_Artifact_ChangesetValue_ArtifactLink::class);
+        $last_changeset_value->method('getArtifactIds')->willReturn([
+            1004,
+            1005,
+        ]);
+        $field->method('getLastChangesetValue')->willReturn($last_changeset_value);
+
+        return $field;
+    }
+
+    private function buildArtifactLinkFieldWithoutData(): Tracker_FormElement_Field_ArtifactLink
+    {
+        $field = $this->createMock(Tracker_FormElement_Field_ArtifactLink::class);
+        $field->method('getReverseLinks')->willReturn([]);
+
+        $last_changeset_value = $this->createMock(Tracker_Artifact_ChangesetValue_ArtifactLink::class);
+        $last_changeset_value->method('getArtifactIds')->willReturn([]);
+        $field->method('getLastChangesetValue')->willReturn($last_changeset_value);
+
+        return $field;
+    }
+}
