Enalean/tuleap

commit dc0cad4a2c41dd6f2595ce7fd3604f6b95b46c8c

request #28875 Improve burnup performances
According to blackfire ananlysis [0], burnup display can take a large amount of time for release burnup:
We can see in analysis that 4m5s are spend in method `searchLinkedArtifactsAtGivenTimestamp`

An explain on query show that something is goin wrong on table plugin_agiledashboard_planning:
|  1 | SIMPLE      | plugin_agiledashboard_planning                 | NULL       | index  | idx                                        | idx               | 8       | NULL                                                             |    2 |    50.00 | Using where; Using index; Using join buffer (Block Nested Loop) |

I tried to add an index on plugin_agiledashboard_planning, but it worse the issue instead of improving performances
I also tried to add the reverse index (planning_tracker_id, id) without any performances improvement
When I tried to force index usage, the explain[1] looks good, but the query is still slow

When we execute the query without the `INNER JOIN` but with a `IN` with configured tracker, execution time of query is improved with a time who should solve the issue
`491 rows in set (0.32 sec)`

All tests have been done over a project where the burnup is computed on a backlog of ~500 elements that where added one by one (it seems that the number of changeset on the milestone artifact matters)

[0] https://blackfire.io/profiles/69b40cdd-5fc6-43cc-bb80-fbd742c70ab6/graph?settings%5Bdimension%5D=wt&settings%5Bdisplay%5D=landscape&settings%5BtabPane%5D=nodes&selected=&callname=main%28%29&constraintDoc=

[1] explain whith `USE INDEX`
| id | select_type | table                                          | partitions | type   | possible_keys                              | key                 | key_len | ref                                                              | rows | filtered | Extra                              |

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupDataDAO.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) Enalean, 2017 - Present. All Rights Reserved.
+ * Copyright (c) Enalean 2022 - Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -18,13 +18,15 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 namespace Tuleap\AgileDashboard\FormElement;
 
-use DataAccessObject;
+use Tuleap\DB\DataAccessObject;
 
-class BurnupDao extends DataAccessObject
+class BurnupDataDAO extends DataAccessObject
 {
-    public function searchArtifactsWithBurnup()
+    public function searchArtifactsWithBurnup(): array
     {
         $sql = "SELECT
                   tracker_artifact.id,
@@ -86,15 +88,11 @@ class BurnupDao extends DataAccessObject
               ON duration_value.changeset_value_id = duration_changeset_value.id
                 AND duration_value.value IS NOT NULL";
 
-        return $this->retrieve($sql);
+        return $this->getDB()->run($sql);
     }
 
-    public function getBurnupInformationBasedOnDuration(int $artifact_id, int $start_date_field_id, int $duration_field_id)
+    public function getBurnupInformationBasedOnDuration(int $artifact_id, int $start_date_field_id, int $duration_field_id): array
     {
-        $artifact_id         = $this->da->escapeInt($artifact_id);
-        $start_date_field_id = $this->da->escapeInt($start_date_field_id);
-        $duration_field_id   = $this->da->escapeInt($duration_field_id);
-
         $sql = "SELECT
                   tracker_artifact.id,
                   SUM(tracker_changeset_value_date.value) AS start_date,
@@ -104,10 +102,10 @@ class BurnupDao extends DataAccessObject
               ON tracker.id = burnup_field.tracker_id
             INNER JOIN tracker_field AS tracker_field_for_start_date
               ON tracker.id = tracker_field_for_start_date.tracker_id
-              AND tracker_field_for_start_date.id = $start_date_field_id
+              AND tracker_field_for_start_date.id = ?
             INNER JOIN tracker_field AS tracker_field_for_duration
               ON tracker.id = tracker_field_for_duration.tracker_id
-              AND tracker_field_for_duration.id = $duration_field_id
+              AND tracker_field_for_duration.id = ?
             INNER JOIN tracker_artifact
               ON tracker.id = tracker_artifact.tracker_id
             INNER JOIN tracker_changeset
@@ -123,20 +121,16 @@ class BurnupDao extends DataAccessObject
             WHERE
               burnup_field.formElement_type = 'burnup'
               AND burnup_field.use_it = 1
-              AND tracker_artifact.id = $artifact_id
+              AND tracker_artifact.id = ?
               GROUP BY tracker_artifact.id, burnup_field.id
               HAVING start_date IS NOT NULL
               AND duration IS NOT NULL";
 
-        return $this->retrieveFirstRow($sql);
+        return $this->getDB()->row($sql, $start_date_field_id, $duration_field_id, $artifact_id);
     }
 
-    public function getBurnupInformationBasedOnEndDate(int $artifact_id, int $start_date_field_id, int $end_date_field_id)
+    public function getBurnupInformationBasedOnEndDate(int $artifact_id, int $start_date_field_id, int $end_date_field_id): array
     {
-        $artifact_id         = $this->da->escapeInt($artifact_id);
-        $start_date_field_id = $this->da->escapeInt($start_date_field_id);
-        $end_date_field_id   = $this->da->escapeInt($end_date_field_id);
-
         $sql = "SELECT
                   tracker_artifact.id,
                   SUM(start_date_value.value) AS start_date,
@@ -146,10 +140,10 @@ class BurnupDao extends DataAccessObject
               ON tracker.id = burnup_field.tracker_id
             INNER JOIN tracker_field AS tracker_field_for_start_date
               ON tracker.id = tracker_field_for_start_date.tracker_id
-              AND tracker_field_for_start_date.id = $start_date_field_id
+              AND tracker_field_for_start_date.id = ?
             INNER JOIN tracker_field AS tracker_field_for_end_date
               ON tracker.id = tracker_field_for_end_date.tracker_id
-              AND tracker_field_for_end_date.id = $end_date_field_id
+              AND tracker_field_for_end_date.id = ?
             INNER JOIN tracker_artifact
               ON tracker.id = tracker_artifact.tracker_id
             INNER JOIN tracker_changeset
@@ -165,19 +159,16 @@ class BurnupDao extends DataAccessObject
             WHERE
               burnup_field.formElement_type = 'burnup'
               AND burnup_field.use_it = 1
-              AND tracker_artifact.id = $artifact_id
+              AND tracker_artifact.id = ?
               GROUP BY tracker_artifact.id, burnup_field.id
               HAVING start_date IS NOT NULL
               AND end_date IS NOT NULL";
 
-        return $this->retrieveFirstRow($sql);
+        return $this->getDB()->row($sql, $start_date_field_id, $end_date_field_id, $artifact_id);
     }
 
-    public function searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp)
+    public function searchLinkedArtifactsAtGivenTimestamp(int $artifact_id, int $timestamp, array $backlog_trackers_ids): array
     {
-        $artifact_id = $this->da->escapeInt($artifact_id);
-        $timestamp   = $this->da->escapeInt($timestamp);
-
         $sql = "SELECT linked_art.id AS id
                 FROM tracker_artifact AS parent_art
                     INNER JOIN tracker ON parent_art.tracker_id = tracker.id
@@ -186,7 +177,7 @@ class BurnupDao extends DataAccessObject
                         ON (
                             changeset2.artifact_id = parent_art.id
                             AND changeset1.id < changeset2.id
-                            AND changeset2.submitted_on <= $timestamp
+                            AND changeset2.submitted_on <= ?
                         )
                     INNER JOIN tracker_field AS f
                         ON (f.tracker_id = parent_art.tracker_id AND f.formElement_type = 'art_link' AND use_it = 1)
@@ -196,16 +187,13 @@ class BurnupDao extends DataAccessObject
                         ON (artlink.changeset_value_id = cv.id)
                     INNER JOIN tracker_artifact AS linked_art
                         ON (linked_art.id = artlink.artifact_id)
-                    INNER JOIN plugin_agiledashboard_planning
-                        ON plugin_agiledashboard_planning.planning_tracker_id = parent_art.tracker_id
-                    INNER JOIN plugin_agiledashboard_planning_backlog_tracker
-                        ON plugin_agiledashboard_planning_backlog_tracker.planning_id = plugin_agiledashboard_planning.id
-                        AND linked_art.tracker_id = plugin_agiledashboard_planning_backlog_tracker.tracker_id
-                WHERE parent_art.id = $artifact_id
+                WHERE parent_art.id = ?
                     AND tracker.deletion_date IS NULL
                     AND changeset2.id IS NULL
-                    AND changeset1.submitted_on <= $timestamp";
+                    AND changeset1.submitted_on <= ?
+                    AND linked_art.tracker_id IN ( ? )";
 
-        return $this->retrieve($sql);
+
+        return $this->getDB()->run($sql, $timestamp, $artifact_id, $timestamp, implode(',', $backlog_trackers_ids));
     }
 }

plugins/agiledashboard/include/AgileDashboard/FormElement/SystemEvent/SystemEvent_BURNUP_GENERATE.php
@@ -30,67 +30,37 @@ use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsCalculator;
 use Tuleap\AgileDashboard\FormElement\BurnupCacheDao;
 use Tuleap\AgileDashboard\FormElement\BurnupCacheDateRetriever;
 use Tuleap\AgileDashboard\FormElement\BurnupCalculator;
-use Tuleap\AgileDashboard\FormElement\BurnupDao;
+use Tuleap\AgileDashboard\FormElement\BurnupDataDAO;
+use Tuleap\AgileDashboard\Planning\PlanningDao;
 use Tuleap\Tracker\Semantic\Timeframe\SemanticTimeframeBuilder;
 
-class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreLine
+final class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreLine
 {
-    /**
-     * @var BurnupCalculator
-     */
-    public $burnup_calculator;
-
-    /**
-     * @var BurnupDao
-     */
-    private $burnup_dao;
-
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
-     * @var BurnupCacheDao
-     */
-    private $cache_dao;
-
-    /**
-     * @var BurnupCacheDateRetriever
-     */
-    private $date_retriever;
-
-    /**
-     * @var Tracker_ArtifactFactory
-     */
-    private $artifact_factory;
-
-    /**
-     * @var SemanticTimeframeBuilder
-     */
-    private $semantic_timeframe_builder;
-
-    /**
-     * @var CountElementsCalculator
-     */
-    private $burnup_count_elements_calculator;
-
-    /**
-     * @var CountElementsCacheDao
-     */
-    private $count_elements_cache_dao;
+    public BurnupCalculator $burnup_calculator;
+    private BurnupDataDAO $burnup_dao;
+    private LoggerInterface $logger;
+    private BurnupCacheDao $cache_dao;
+    private BurnupCacheDateRetriever $date_retriever;
+    private Tracker_ArtifactFactory $artifact_factory;
+    private SemanticTimeframeBuilder $semantic_timeframe_builder;
+    private CountElementsCalculator $burnup_count_elements_calculator;
+    private CountElementsCacheDao $count_elements_cache_dao;
+    private \PlanningFactory $planning_factory;
+    private PlanningDao $planning_dao;
 
     public function injectDependencies(
         Tracker_ArtifactFactory $artifact_factory,
         SemanticTimeframeBuilder $semantic_timeframe_builder,
-        BurnupDao $burnup_dao,
+        BurnupDataDAO $burnup_dao,
         BurnupCalculator $burnup_calculator,
         CountElementsCalculator $burnup_count_elements_calculator,
         BurnupCacheDao $cache_dao,
         CountElementsCacheDao $count_elements_cache_dao,
         LoggerInterface $logger,
         BurnupCacheDateRetriever $date_retriever,
-    ) {
+        PlanningDao $planning_dao,
+        \PlanningFactory $planning_factory,
+    ): void {
         $this->artifact_factory                 = $artifact_factory;
         $this->semantic_timeframe_builder       = $semantic_timeframe_builder;
         $this->burnup_dao                       = $burnup_dao;
@@ -100,6 +70,8 @@ class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreL
         $this->cache_dao                        = $cache_dao;
         $this->count_elements_cache_dao         = $count_elements_cache_dao;
         $this->date_retriever                   = $date_retriever;
+        $this->planning_dao                     = $planning_dao;
+        $this->planning_factory                 = $planning_factory;
     }
 
     private function getArtifactIdFromParameters()
@@ -172,10 +144,19 @@ class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreL
             $burnup_information['id']
         );
 
+        $planning_infos = $this->planning_dao->searchByMilestoneTrackerId($artifact->getTrackerId());
+        if (! $planning_infos) {
+            $warning = "Artifact artifact #" . $artifact_id . "does not belong to a planning";
+            $this->warning($warning);
+            $this->logger->debug($warning);
+            return false;
+        }
+
+        $backlog_trackers_ids = $this->planning_factory->getBacklogTrackersIds($planning_infos['id']);
         foreach ($this->date_retriever->getWorkedDaysToCacheForPeriod($burnup_period, $yesterday) as $worked_day) {
             $this->logger->debug("Day " . date("Y-m-d H:i:s", $worked_day));
 
-            $effort       = $this->burnup_calculator->getValue($burnup_information['id'], $worked_day);
+            $effort       = $this->burnup_calculator->getValue($burnup_information['id'], $worked_day, $backlog_trackers_ids);
             $team_effort  = $effort->getTeamEffort();
             $total_effort = $effort->getTotalEffort();
 
@@ -187,9 +168,11 @@ class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsIgnoreL
                 $team_effort
             );
 
+
             $subelements_cache_info = $this->burnup_count_elements_calculator->getValue(
                 $burnup_information['id'],
-                $worked_day
+                $worked_day,
+                $backlog_trackers_ids
             );
 
             $closed_subelements = $subelements_cache_info->getClosedElements();

plugins/agiledashboard/include/AgileDashboard/FormElement/Burnup.php
@@ -40,6 +40,7 @@ use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsCacheDao;
 use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsCalculator;
 use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsModeChecker;
 use Tuleap\AgileDashboard\FormElement\Burnup\ProjectsCountModeDao;
+use Tuleap\AgileDashboard\Planning\PlanningDao;
 use Tuleap\AgileDashboard\v1\Artifact\BurnupRepresentation;
 use Tuleap\Layout\IncludeAssets;
 use Tuleap\Tracker\Artifact\Artifact;
@@ -385,10 +386,7 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
         );
     }
 
-    /**
-     * @return BurnupDataBuilder
-     */
-    private function getBurnupDataBuilder()
+    private function getBurnupDataBuilder(): BurnupDataBuilder
     {
         $burnup_cache_dao = new BurnupCacheDao();
 
@@ -413,23 +411,22 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
                 Tracker_Artifact_ChangesetFactoryBuilder::build(),
                 Tracker_ArtifactFactory::instance(),
                 Tracker_FormElementFactory::instance(),
-                new BurnupDao()
+                new BurnupDataDAO()
             ),
-            $this->getCountElementsModeChecker()
+            $this->getCountElementsModeChecker(),
+            new PlanningDao(),
+            \PlanningFactory::build()
         );
     }
 
-    /**
-     * @return BurnupCalculator
-     */
-    private function getBurnupCalculator()
+    private function getBurnupCalculator(): BurnupCalculator
     {
         $changeset_factory = Tracker_Artifact_ChangesetFactoryBuilder::build();
 
         return new BurnupCalculator(
             $changeset_factory,
             Tracker_ArtifactFactory::instance(),
-            new BurnupDao(),
+            new BurnupDataDAO(),
             AgileDashboard_Semantic_InitialEffortFactory::instance(),
             new SemanticDoneFactory(new SemanticDoneDao(), new SemanticDoneValueChecker())
         );

plugins/agiledashboard/include/AgileDashboard/FormElement/Burnup/CountElementsCalculator.php
@@ -27,47 +27,22 @@ use Tracker_Artifact_ChangesetFactory;
 use Tracker_Artifact_ChangesetValue_ArtifactLink;
 use Tracker_ArtifactFactory;
 use Tracker_FormElementFactory;
-use Tuleap\AgileDashboard\FormElement\BurnupDao;
+use Tuleap\AgileDashboard\FormElement\BurnupDataDAO;
 use Tuleap\Tracker\Artifact\Artifact;
 
 class CountElementsCalculator
 {
-    /**
-     * @var Tracker_Artifact_ChangesetFactory
-     */
-    private $changeset_factory;
-
-    /**
-     * @var BurnupDao
-     */
-    private $burnup_dao;
-
-    /**
-     * @var Tracker_ArtifactFactory
-     */
-    private $artifact_factory;
-
-    /**
-     * @var Tracker_FormElementFactory
-     */
-    private $form_element_factory;
-
-
     public function __construct(
-        Tracker_Artifact_ChangesetFactory $changeset_factory,
-        Tracker_ArtifactFactory $artifact_factory,
-        Tracker_FormElementFactory $form_element_factory,
-        BurnupDao $burnup_dao,
+        private Tracker_Artifact_ChangesetFactory $changeset_factory,
+        private Tracker_ArtifactFactory $artifact_factory,
+        private Tracker_FormElementFactory $form_element_factory,
+        private BurnupDataDAO $burnup_dao,
     ) {
-        $this->changeset_factory    = $changeset_factory;
-        $this->burnup_dao           = $burnup_dao;
-        $this->artifact_factory     = $artifact_factory;
-        $this->form_element_factory = $form_element_factory;
     }
 
-    public function getValue(int $artifact_id, int $timestamp): CountElementsInfo
+    public function getValue(int $artifact_id, int $timestamp, array $backlog_trackers_ids): CountElementsInfo
     {
-        $items_dar     = $this->burnup_dao->searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp);
+        $items_dar     = $this->burnup_dao->searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp, $backlog_trackers_ids);
         $backlog_items = [];
         array_push($backlog_items, ...$items_dar);
         $elements_count = array_reduce(

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupCalculator.php
@@ -32,55 +32,30 @@ use Tuleap\Tracker\Semantic\Status\Done\SemanticDoneFactory;
 
 class BurnupCalculator
 {
-    /**
-     * @var Tracker_Artifact_ChangesetFactory
-     */
-    private $changeset_factory;
-    /**
-     * @var BurnupDao
-     */
-    private $burnup_dao;
-    /**
-     * @var Tracker_ArtifactFactory
-     */
-    private $artifact_factory;
-    /**
-     * @var AgileDashboard_Semantic_InitialEffortFactory
-     */
-    private $initial_effort_factory;
-    /**
-     * @var SemanticDoneFactory
-     */
-    private $semantic_done_factory;
-
     public function __construct(
-        Tracker_Artifact_ChangesetFactory $changeset_factory,
-        Tracker_ArtifactFactory $artifact_factory,
-        BurnupDao $burnup_dao,
-        AgileDashboard_Semantic_InitialEffortFactory $initial_effort_factory,
-        SemanticDoneFactory $semantic_done_factory,
+        private Tracker_Artifact_ChangesetFactory $changeset_factory,
+        private Tracker_ArtifactFactory $artifact_factory,
+        private BurnupDataDAO $burnup_dao,
+        private AgileDashboard_Semantic_InitialEffortFactory $initial_effort_factory,
+        private SemanticDoneFactory $semantic_done_factory,
     ) {
-        $this->changeset_factory      = $changeset_factory;
-        $this->burnup_dao             = $burnup_dao;
-        $this->artifact_factory       = $artifact_factory;
-        $this->initial_effort_factory = $initial_effort_factory;
-        $this->semantic_done_factory  = $semantic_done_factory;
     }
 
-    /**
-     * @return BurnupEffort
-     */
-    public function getValue($artifact_id, $timestamp)
+    public function getValue(int $artifact_id, int $timestamp, array $backlog_trackers_ids): BurnupEffort
     {
         $backlog_items = $this->getPlanningLinkedArtifactAtTimestamp(
             $artifact_id,
-            $timestamp
+            $timestamp,
+            $backlog_trackers_ids
         );
 
         $total_effort = 0;
         $team_effort  = 0;
         foreach ($backlog_items as $item) {
             $artifact = $this->artifact_factory->getArtifactById($item['id']);
+            if (! $artifact) {
+                continue;
+            }
 
             $effort        = $this->getEffort($artifact, $timestamp);
             $total_effort += $effort->getTotalEffort();
@@ -91,10 +66,11 @@ class BurnupCalculator
     }
 
     private function getPlanningLinkedArtifactAtTimestamp(
-        $artifact_id,
-        $timestamp,
+        int $artifact_id,
+        int $timestamp,
+        array $backlog_trackers_ids,
     ) {
-        return $this->burnup_dao->searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp);
+        return $this->burnup_dao->searchLinkedArtifactsAtGivenTimestamp($artifact_id, $timestamp, $backlog_trackers_ids);
     }
 
     /**

commit 44caae3b4d73e623bd4306c4ddbc2a2b1b6bb69b

Fixes request #29261: Crash when computing burnup graphs
Introduced by dc0cad4a2c41dd6f2595ce7fd3604f6b95b46c8c.

Change-Id: Iaee2b00ad54d7a3fb80000e8906009b83d85848e

plugins/agiledashboard/include/AgileDashboard/FormElement/BurnupDataDAO.php
@@ -91,7 +91,10 @@ class BurnupDataDAO extends DataAccessObject
         return $this->getDB()->run($sql);
     }
 
-    public function getBurnupInformationBasedOnDuration(int $artifact_id, int $start_date_field_id, int $duration_field_id): array
+    /**
+     * @psalm-return array{id:int, start_date:int, duration:int}|null
+     */
+    public function getBurnupInformationBasedOnDuration(int $artifact_id, int $start_date_field_id, int $duration_field_id): ?array
     {
         $sql = "SELECT
                   tracker_artifact.id,
@@ -129,7 +132,10 @@ class BurnupDataDAO extends DataAccessObject
         return $this->getDB()->row($sql, $start_date_field_id, $duration_field_id, $artifact_id);
     }
 
-    public function getBurnupInformationBasedOnEndDate(int $artifact_id, int $start_date_field_id, int $end_date_field_id): array
+    /**
+     * @psalm-return array{id:int, start_date:int, end_date:int}|null
+     */
+    public function getBurnupInformationBasedOnEndDate(int $artifact_id, int $start_date_field_id, int $end_date_field_id): ?array
     {
         $sql = "SELECT
                   tracker_artifact.id,

plugins/agiledashboard/include/AgileDashboard/FormElement/SystemEvent/SystemEvent_BURNUP_GENERATE.php
@@ -124,17 +124,25 @@ final class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsI
             return false;
         }
 
-        if (! isset($burnup_information['duration'])) {
+        $burnup_period = null;
+        if (isset($burnup_information['end_date'])) {
             $burnup_period = TimePeriodWithoutWeekEnd::buildFromEndDate(
                 $burnup_information['start_date'],
                 $burnup_information['end_date'],
                 $this->logger
             );
-        } else {
+        } elseif (isset($burnup_information['duration'])) {
             $burnup_period = TimePeriodWithoutWeekEnd::buildFromDuration(
                 $burnup_information['start_date'],
                 $burnup_information['duration']
             );
+        }
+
+        if ($burnup_period === null) {
+            $warning = "Skipped cache for artifact #" . $artifact_id . ". Not able to compute burnup period.";
+            $this->warning($warning);
+            $this->logger->debug($warning);
+            return false;
         }
 
         $yesterday = new DateTime();
@@ -146,7 +154,7 @@ final class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsI
 
         $planning_infos = $this->planning_dao->searchByMilestoneTrackerId($artifact->getTrackerId());
         if (! $planning_infos) {
-            $warning = "Artifact artifact #" . $artifact_id . "does not belong to a planning";
+            $warning = "Artifact artifact #" . $artifact_id . " does not belong to a planning";
             $this->warning($warning);
             $this->logger->debug($warning);
             return false;
@@ -180,7 +188,7 @@ final class SystemEvent_BURNUP_GENERATE extends SystemEvent // @codingStandardsI
 
             $this->logger->debug("Caching subelements value $closed_subelements/$total_subelements for artifact #" . $burnup_information['id']);
             $this->count_elements_cache_dao->saveCachedFieldValueAtTimestampForSubelements(
-                (int) $burnup_information['id'],
+                $burnup_information['id'],
                 (int) $worked_day,
                 (int) $total_subelements,
                 (int) $closed_subelements

plugins/agiledashboard/tests/integration/FormElement/BurnupDataDAOTest.php
@@ -1,0 +1,41 @@
+<?php
+/**
+ * Copyright (c) Enalean 2022 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\AgileDashboard\FormElement;
+
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class BurnupDataDAOTest extends TestCase
+{
+    private BurnupDataDAO $dao;
+
+    protected function setUp(): void
+    {
+        $this->dao = new BurnupDataDAO();
+    }
+
+    public function testRetrievesNoBurnupInformationWhenNoBurnupMatchesRequestedParameters(): void
+    {
+        self::assertNull($this->dao->getBurnupInformationBasedOnDuration(PHP_INT_MAX, PHP_INT_MAX, PHP_INT_MAX));
+        self::assertNull($this->dao->getBurnupInformationBasedOnEndDate(PHP_INT_MAX, PHP_INT_MAX, PHP_INT_MAX));
+    }
+}