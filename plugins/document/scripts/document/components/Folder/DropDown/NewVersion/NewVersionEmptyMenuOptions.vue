ebcf41f30d7899b1d01abd0fecabba4011004236
Nicolas Terray <nicolas.terray@enalean.com>
2022-11-24 16:42:33 +0100

    feat: Convert empty item to online office file
    Like for creation of new item, except than we don't need a modal since
    item is already created.
    
    Part of story #28273: create OnlyOffice documents
    
    Change-Id: Id95c407229204f5ba79dac43484ae7f52498cdac

--- plugins/document/scripts/document/components/Folder/DropDown/NewVersion/NewVersionEmptyMenuOptions.vue
+++ plugins/document/scripts/document/components/Folder/DropDown/NewVersion/NewVersionEmptyMenuOptions.vue
@@ -51,6 +51,32 @@
             <i class="fa-fw tlp-dropdown-menu-item-icon" v-bind:class="ICON_WIKI"></i>
             {{ $gettext("Wiki page") }}
         </button>
+        <template v-for="section in create_new_item_alternatives">
+            <span
+                class="tlp-dropdown-menu-title document-dropdown-menu-title"
+                v-bind:key="section.title"
+                >{{ section.title }}</span
+            >
+            <button
+                v-for="alternative in section.alternatives"
+                class="tlp-dropdown-menu-item"
+                role="menuitem"
+                v-bind:key="section.title + alternative.title"
+                data-test="alternative"
+                v-on:click.prevent="convertEmptyDocument(alternative)"
+            >
+                <i
+                    class="fa fa-fw tlp-dropdown-menu-item-icon"
+                    v-bind:class="iconForMimeType(alternative.mime_type)"
+                ></i>
+                {{ alternative.title }}
+            </button>
+        </template>
+        <span
+            class="tlp-dropdown-menu-separator"
+            role="separator"
+            v-if="create_new_item_alternatives.length > 0"
+        ></span>
         <button
             type="button"
             class="tlp-dropdown-menu-item"
@@ -65,7 +91,7 @@
 </template>
 
 <script setup lang="ts">
-import type { Empty, ItemType } from "../../../../type";
+import type { Empty, ItemType, NewItemAlternative } from "../../../../type";
 import {
     ICON_EMBEDDED,
     TYPE_FILE,
@@ -75,19 +101,69 @@ import {
     TYPE_WIKI,
     ICON_WIKI,
 } from "../../../../constants";
+import type { ItemHasJustBeenUpdatedEvent } from "../../../../helpers/emitter";
 import emitter from "../../../../helpers/emitter";
-import { useState } from "vuex-composition-helpers";
+import { useActions, useState } from "vuex-composition-helpers";
 import type { ConfigurationState } from "../../../../store/configuration";
+import { inject } from "vue";
+import type { NewItemAlternativeArray } from "../../../../type";
+import { iconForMimeType } from "../../../../helpers/icon-for-mime-type";
+import { getEmptyOfficeFileFromMimeType } from "../../../../helpers/office/get-empty-office-file";
+import type { RootActionsUpdate } from "../../../../store/actions-update";
+import type { NewVersionFromEmptyInformation } from "../../../../store/actions-update";
+import { isFile } from "../../../../helpers/type-check-helper";
 
+const { createNewVersionFromEmpty } = useActions<
+    Pick<RootActionsUpdate, "createNewVersionFromEmpty">
+>(["createNewVersionFromEmpty"]);
 const { embedded_are_allowed, user_can_create_wiki } = useState<
     Pick<ConfigurationState, "embedded_are_allowed" | "user_can_create_wiki">
 >("configuration", ["embedded_are_allowed", "user_can_create_wiki"]);
 
-const props = defineProps<{ item: Empty }>();
+const create_new_item_alternatives = inject<NewItemAlternativeArray>(
+    "create_new_item_alternatives",
+    []
+);
+
+const props = defineProps<{ item: Empty; location?: Location }>();
+const location = props.location || window.location;
+
 function showNewVersionModal(type: ItemType): void {
     emitter.emit("show-create-new-version-modal-for-empty", {
         item: props.item,
         type,
     });
+}
+
+async function convertEmptyDocument(alternative: NewItemAlternative): Promise<void> {
+    const office_file = await getEmptyOfficeFileFromMimeType(alternative.mime_type);
+    const file = new File([office_file.file], props.item.title + "." + office_file.extension, {
+        type: alternative.mime_type,
+    });
+
+    const new_version: NewVersionFromEmptyInformation = {
+        link_properties: {
+            link_url: "",
+        },
+        embedded_properties: {
+            content: "",
+        },
+        file_properties: {
+            file,
+        },
+    };
+
+    await createNewVersionFromEmpty([TYPE_FILE, props.item, new_version]);
+
+    const redirectToEditor = (event: ItemHasJustBeenUpdatedEvent) => {
+        const item = event.item;
+        if (item.id === props.item.id) {
+            emitter.off("item-has-just-been-updated", redirectToEditor);
+            if (isFile(item) && item.file_properties && item.file_properties.open_href) {
+                location.href = item.file_properties.open_href;
+            }
+        }
+    };
+    emitter.on("item-has-just-been-updated", redirectToEditor);
 }
 </script>
