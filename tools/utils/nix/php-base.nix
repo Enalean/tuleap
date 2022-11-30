17f92bf3abb5c4b3e82a96e33eff2d61388d681f
Thomas Gerbet <thomas.gerbet@enalean.com>
2022-11-30 15:53:59 +0100

    Workaround for request #29643: PHP segfault when running dev tools
    We are using the latest version of nixpkgs that did not include the
    issue.
    Two other ways have been considered:
    * rebuilding PHP with PCRE that does not have the option enabled
    * setting pcre.jit to 0
    
    The first one, while doable, is a bit resource intensive and second one
    impact performances sensibly so rolling back temporarily to the latest
    known working version seems fine.
    
    Change-Id: Id4e5a31e8212de5e105a312849f6cdf9a9d6f48a

--- tools/utils/nix/php-base.nix
+++ tools/utils/nix/php-base.nix
@@ -1,5 +1,9 @@
 { pkgs }:
 
 let
-    phpBase = pkgs.php80.withExtensions ({ all, ... }: []);
+    pkgsWithoutJITSEAllocEnabled = import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/1f05f18f28eb8f9b20fc538a3450c183ffe94b77.tar.gz";
+      sha256 = "1qb7yifx46yzhdvdj9np90m3gf0pifkhldbns1vnhfpllw7gr399";
+    } ) { };
+    phpBase = pkgsWithoutJITSEAllocEnabled.php80.withExtensions ({ all, ... }: []);
 in phpBase
