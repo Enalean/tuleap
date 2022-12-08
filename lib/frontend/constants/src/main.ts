76b945dc88e46a89b08a1537fee7d4df12f55df9
Joris Masson <joris.masson@enalean.com>
2022-12-08 11:39:25 +0100

    chore: Rename lib to core constants
    No functional change expected in documents app.
    
    Notes:
    Just as there is a lib for tracker constants, there should be a lib to
    share core constants. For now, it lists user group constants and user
    preferences / locale constants and their types (that are stricter than
    "string").
    
    part of request #22648 Convert AngularJS directives and controllers to
    hybrids web components
    
    Change-Id: I0b365da626cb558ab177e14c1bc46cf29807849e

--- lib/frontend/constants/src/main.ts
+++ lib/frontend/constants/src/main.ts
@@ -19,3 +19,16 @@
 
 export const PROJECT_MEMBERS_ID = 3;
 export const PROJECT_ADMINISTRATORS_ID = 4;
+
+type DateFormat = "d/m/Y" | "Y-m-d";
+type DateWithTimeFormat = "d/m/Y H:i" | "Y-m-d H:i";
+export type DateTimeFormat = DateFormat | DateWithTimeFormat;
+export const en_US_DATE_TIME_FORMAT = "Y-m-d H:i";
+export const en_US_DATE_FORMAT = "Y-m-d";
+export const fr_FR_DATE_TIME_FORMAT = "d/m/Y H:i";
+export const fr_FR_DATE_FORMAT = "d/m/Y";
+
+export type LocaleString = "fr_FR" | "en_US" | "pt_BR";
+export const en_US_LOCALE = "en_US";
+export const fr_FR_LOCALE = "fr_FR";
+export const pt_BR_LOCALE = "pt_BR";
--- lib/frontend/date-helper/src/index.test.ts
+++ lib/frontend/date-helper/src/index.test.ts
@@ -17,19 +17,25 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import {
+    en_US_DATE_FORMAT,
+    en_US_DATE_TIME_FORMAT,
+    fr_FR_DATE_FORMAT,
+    fr_FR_DATE_TIME_FORMAT,
+} from "@tuleap/core-constants";
 import { describe, it, expect } from "vitest";
 import { formatFromPhpToMoment, formatDateYearMonthDay } from "./index";
 
 describe("formatFromPhpToMoment", () => {
     it.each([
-        ["french date", "d/m/Y", "DD/MM/YYYY"],
-        ["english date", "Y-m-d", "YYYY-MM-DD"],
-        ["french time", "d/m/Y H:i", "DD/MM/YYYY HH:mm"],
-        ["english time", "Y-m-d H:i", "YYYY-MM-DD HH:mm"],
+        ["french date", fr_FR_DATE_FORMAT, "DD/MM/YYYY"],
+        ["english date", en_US_DATE_FORMAT, "YYYY-MM-DD"],
+        ["french time", fr_FR_DATE_TIME_FORMAT, "DD/MM/YYYY HH:mm"],
+        ["english time", en_US_DATE_TIME_FORMAT, "YYYY-MM-DD HH:mm"],
     ])(
         `Given %s is provided, then it returns the moment format`,
         (format_title, php_date_format, expected_moment_format) => {
-            expect(formatFromPhpToMoment(php_date_format)).toEqual(expected_moment_format);
+            expect(formatFromPhpToMoment(php_date_format)).toBe(expected_moment_format);
         }
     );
 
