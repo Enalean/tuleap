6dc9a9ef058ae95c24d445cbc2b10cb1ee781b38
Manuel VACELET <manuel.vacelet@enalean.com>
2022-12-19 16:29:19 +0100

    feat(el9): align packages for el9
    As mailman and cvs will never be supported on el9 there is no need to
    build the packages.
    
    Moreover, this contribution aligns the installation in build & run now
    that both git & subversion can be installed properly.
    
    Part of: request #29619 Add support of Rocky Linux 9
    
    Change-Id: I44509d5bab3d11d2f08dc46e25566f9005210064

--- tools/docker/install-rpms/install.el9.sh
+++ tools/docker/install-rpms/install.el9.sh
@@ -7,11 +7,14 @@ createrepo /rpms/RPMS/noarch/
 systemctl start systemd-user-sessions.service
 systemctl start mysqld
 
-yum install -y tuleap-plugin-agiledashboard \
+dnf install -y tuleap-plugin-agiledashboard \
   tuleap-plugin-graphontrackers \
   tuleap-theme-burningparrot \
   tuleap-theme-flamingparrot \
-  tuleap-plugin-svn
+  tuleap-plugin-svn \
+  tuleap-plugin-git \
+  tuleap-plugin-svn \
+  tuleap-plugin-hudson\*
 
 /usr/share/tuleap/tools/setup.el7.sh \
     --assumeyes \
