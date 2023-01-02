ec67d8d89e4863c648b353ee88ed27fa6f4e741c
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-01-02 15:09:53 +0100

    Move back to Vitest in project-sidebar-internal and fp-feedback
    The change made in 211e2eeaddb396b9827c04530eb06c4afa0ae47e seems to be
    good enough to avoid issue when running tests while collecting coverage.
    
    This reverts commit 0560daf60d9c2aa4d128756189261dbbb07bb1f0.
    
    Part of request #27556: Migrate Vite-based packages to Vitest
    
    Change-Id: If7dbe882040d3e677e13d0920da40565f4d324f7

--- lib/frontend/project-sidebar-internal/src/ProjectSidebar.test.ts
+++ lib/frontend/project-sidebar-internal/src/ProjectSidebar.test.ts
@@ -20,6 +20,7 @@
  * SOFTWARE.
  */
 
+import { describe, it, expect } from "vitest";
 import ProjectSidebar from "./ProjectSidebar.vue";
 import { shallowMount } from "@vue/test-utils";
 import { example_config } from "./project-sidebar-example-config";
--- lib/frontend/project-sidebar-internal/src/url-sanitizer.test.ts
+++ lib/frontend/project-sidebar-internal/src/url-sanitizer.test.ts
@@ -20,6 +20,7 @@
  * SOFTWARE.
  */
 
+import { describe, it, expect } from "vitest";
 import { sanitizeURL } from "./url-sanitizer";
 
 describe("url-sanitize", () => {
