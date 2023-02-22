Enalean/tuleap

commit c44db8d8b06377089bcc9328c5e9b0d53b71f963

Centralize gitolite hooks deployment inside site-deploy:gitolite3-hooks cmd
Move the creation of a symlink for the git post-receive hook to SiteDeployGitolite3Hooks.php
If the symlink is created we run gitolite setup --hooks-only

How to test:
- make bash-web
- tuleap-cfg site-deploy:gitolite3-hooks
- You should see the following message if the symlink does not exist:
[info] Creating post-receive hook symlink at /var/lib/gitolite/.gitolite/hooks/common/post-receive
[info] Executing gitolite setup --hooks-only

Part of story #31077 deploy a WebAssembly module to validate incoming references as a pre-receive hook

Change-Id: If162dee6ca7bb716019332db7b8edde9e9b7f853

src/tuleap-cfg/Command/SiteDeploy/Gitolite3/SiteDeployGitolite3Hooks.php
@@ -1,0 +1,85 @@
+<?php
+/*
+ * Copyright (c) Enalean, 2023-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace TuleapCfg\Command\SiteDeploy\Gitolite3;
+
+use Psr\Log\LoggerInterface;
+use TuleapCfg\Command\ProcessFactory;
+
+final class SiteDeployGitolite3Hooks
+{
+    private const GITOLITE_BIN      = '/usr/bin/gitolite';
+    private const GITOLITE_BASE_DIR = '/var/lib/gitolite';
+
+    private ProcessFactory $process_factory;
+
+    public function __construct(ProcessFactory $process_factory)
+    {
+        $this->process_factory = $process_factory;
+    }
+
+    public function deploy(LoggerInterface $logger): void
+    {
+        if (! $this->hasGitPlugin()) {
+            $logger->debug('Git plugin not detected');
+            return;
+        }
+
+        if (! $this->isGitolite3Setup()) {
+            $logger->debug('Gitolite3 is not setup yet');
+            return;
+        }
+
+        if ($this->createPostReceiveHookSymlink($logger)) {
+            $this->deployHooks($logger);
+        }
+    }
+
+    private function hasGitPlugin(): bool
+    {
+        return is_dir(__DIR__ . '/../../../../../plugins/git/');
+    }
+
+    private function isGitolite3Setup(): bool
+    {
+        $gitolite_conf_size = @filesize(self::GITOLITE_BASE_DIR . '/.gitolite/conf/gitolite.conf');
+        return $gitolite_conf_size !== false && $gitolite_conf_size > 0;
+    }
+
+    private function createPostReceiveHookSymlink(LoggerInterface $logger): bool
+    {
+        if (! file_exists(self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive')) {
+            $logger->info('Creating post-receive hook symlink at ' . self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive');
+            symlink(self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive', __DIR__ . '/../../../../../plugins/git/hooks/post-receive-gitolite');
+            return true;
+        }
+        return false;
+    }
+
+    private function deployHooks(LoggerInterface $logger): void
+    {
+        $logger->info('Executing gitolite setup --hooks-only');
+
+        $this->process_factory->getProcess(['/usr/bin/sudo', '-u', 'gitolite', self::GITOLITE_BIN, 'setup', '--hooks-only'])->mustRun();
+    }
+}

src/tuleap-cfg/Command/SiteDeploy/Gitolite3/SiteDeployGitolite3HooksCommand.php
@@ -1,0 +1,55 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace TuleapCfg\Command\SiteDeploy\Gitolite3;
+
+use Psr\Log\LogLevel;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\OutputInterface;
+use TuleapCfg\Command\ProcessFactory;
+
+final class SiteDeployGitolite3HooksCommand extends Command
+{
+    public const NAME = 'site-deploy:gitolite3-hooks';
+
+    public function __construct()
+    {
+        parent::__construct(self::NAME);
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Deploy Gitolite3 git hooks');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        (new SiteDeployGitolite3Hooks(new ProcessFactory()))->deploy(
+            new ConsoleLogger($output, [LogLevel::INFO => OutputInterface::VERBOSITY_NORMAL])
+        );
+
+        return Command::SUCCESS;
+    }
+}

src/tuleap-cfg/tuleap-cfg.php
@@ -52,6 +52,7 @@ $application->add(new \TuleapCfg\Command\SiteDeploy\SiteDeployCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\Images\SiteDeployImagesCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\FPM\SiteDeployFPMCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\Gitolite3\SiteDeployGitolite3Command());
+$application->add(new \TuleapCfg\Command\SiteDeploy\Gitolite3\SiteDeployGitolite3HooksCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\Nginx\SiteDeployNginxCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\Apache\SiteDeployApacheCommand());
 $application->add(new \TuleapCfg\Command\SiteDeploy\ForgeUpgrade\SiteDeployForgeUpgradeCommand());

tools/setup/el7/include/plugins.sh
@@ -132,11 +132,7 @@ _pluginGit() {
         plugin_git_configured="true"
     fi
 
-    if [ ! -L "${git_home}/.gitolite/hooks/common/post-receive" ]; then
-        ${ln} -s "${tuleap_src_plugins}/git/hooks/post-receive-gitolite" \
-            "${git_home}/.gitolite/hooks/common/post-receive"
-        plugin_git_configured="true"
-    fi
+    ${tuleapcfg} site-deploy:gitolite3-hooks
 
     if ! $(${tuleapcfg} systemctl is-enabled tuleap-process-system-events-git.timer); then
         ${tuleapcfg} systemctl enable "tuleap-process-system-events-git.timer"

commit 5a410e1934a70dce82f0f63736105fba4a90b328

Fix deployment of the gitolite post-receive hook
Issue introduced in c44db8d8b06377089bcc9328c5e9b0d53b71f963.
Instances that have been installed since this patch was merged will be
fixed once they upgrade and run `tuleap-cfg site-deploy`. Instances that
were installed before it are not affected.

Code has been adapted a bit in order to:
* make possible to add a unit test covering the creation of the symlink
  (this is left as an exercice :) )
* make sure that link is created or generate an error is that is not the
  case
* use an absolute path as the target of the link to avoid possible
  issues when doing the resolution of path when it is used

Part of story #31077: deploy a WebAssembly module to validate incoming references as a pre-receive hook

Change-Id: I4b08580952ca99d91c3cdc5d61ab09f25394d059

src/tuleap-cfg/Command/SiteDeploy/Gitolite3/SiteDeployGitolite3Hooks.php
@@ -31,11 +31,11 @@ final class SiteDeployGitolite3Hooks
     private const GITOLITE_BIN      = '/usr/bin/gitolite';
     private const GITOLITE_BASE_DIR = '/var/lib/gitolite';
 
-    private ProcessFactory $process_factory;
 
-    public function __construct(ProcessFactory $process_factory)
-    {
-        $this->process_factory = $process_factory;
+    public function __construct(
+        private readonly ProcessFactory $process_factory,
+        private readonly string $gitolite_base_dir = self::GITOLITE_BASE_DIR,
+    ) {
     }
 
     public function deploy(LoggerInterface $logger): void
@@ -62,15 +62,16 @@ final class SiteDeployGitolite3Hooks
 
     private function isGitolite3Setup(): bool
     {
-        $gitolite_conf_size = @filesize(self::GITOLITE_BASE_DIR . '/.gitolite/conf/gitolite.conf');
+        $gitolite_conf_size = @filesize($this->gitolite_base_dir . '/.gitolite/conf/gitolite.conf');
         return $gitolite_conf_size !== false && $gitolite_conf_size > 0;
     }
 
     private function createPostReceiveHookSymlink(LoggerInterface $logger): bool
     {
-        if (! file_exists(self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive')) {
-            $logger->info('Creating post-receive hook symlink at ' . self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive');
-            symlink(self::GITOLITE_BASE_DIR . '/.gitolite/hooks/common/post-receive', __DIR__ . '/../../../../../plugins/git/hooks/post-receive-gitolite');
+        $hook_path = $this->gitolite_base_dir . '/.gitolite/hooks/common/post-receive';
+        if (! file_exists($hook_path)) {
+            $logger->info('Creating post-receive hook symlink at ' . $hook_path);
+            self::symlink(__DIR__ . '/../../../../../plugins/git/hooks/post-receive-gitolite', $hook_path);
             return true;
         }
         return false;
@@ -81,5 +82,15 @@ final class SiteDeployGitolite3Hooks
         $logger->info('Executing gitolite setup --hooks-only');
 
         $this->process_factory->getProcess(['/usr/bin/sudo', '-u', 'gitolite', self::GITOLITE_BIN, 'setup', '--hooks-only'])->mustRun();
+    }
+
+    private static function symlink(string $target_path, string $link_path): void
+    {
+        $target_realpath = realpath($target_path);
+        error_clear_last();
+        $symlink_result = symlink($target_realpath, $link_path);
+        if ($symlink_result === false) {
+            throw new \RuntimeException(sprintf('Cannot create link %s (%s)', $link_path, error_get_last()['message'] ?? 'Unknown error'));
+        }
     }
 }