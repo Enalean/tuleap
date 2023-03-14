bf547a6819ae44c1af7b001bac115e376bd7fc93
Nicolas Terray <nicolas.terray@enalean.com>
2023-03-14 22:15:29 +0100

    chore: Configure theme variant via config-set
    Instead of forcing platform administrators to edit `local.inc` file, we
    should let them configure allowed and default variants via `tuleap
    config-set`.
    
    This has the advantage to be able to validate what they are trying to
    configure (typo, inconsistency, …).
    
    No functional changes, current configuration is kept without doing
    anything.
    
    Part of request #31137: Ease configuration of available theme variants
    
    Change-Id: If20d981b1ff9ce16d9e35a9613b044f474d5c11f

---
+++ src/common/Layout/ConfigAvailableThemeVariantValidator.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Layout;
+
+use Tuleap\Config\InvalidConfigKeyValueException;
+use Tuleap\Config\ValueValidator;
+
+final class ConfigAvailableThemeVariantValidator implements ValueValidator
+{
+    private function __construct()
+    {
+    }
+
+    public static function buildSelf(): ValueValidator
+    {
+        return new self();
+    }
+
+    public function checkIsValid(string $value): void
+    {
+        $variant_names = explode(',', $value);
+        $variant_names = array_map('trim', $variant_names);
+        $variant_names = array_filter($variant_names);
+
+        $already_seen = [];
+
+        foreach ($variant_names as $name) {
+            $color = ThemeVariantColor::tryFrom($name);
+            if (! $color) {
+                throw new InvalidConfigKeyValueException("'$name' is not a valid variant. Allowed values: orange, blue, grey, green, purple, and red.");
+            }
+
+            if (isset($already_seen[$name])) {
+                throw new InvalidConfigKeyValueException("'$name' cannot be listed more than once.");
+            }
+
+            $already_seen[$name] = true;
+        }
+
+        $default_config_name = \ThemeVariant::CONFIG_DEFAULT;
+        $default             = \ForgeConfig::get($default_config_name);
+        if (! isset($already_seen[$default])) {
+            throw new InvalidConfigKeyValueException("Default theme '$default' is not part of '$value', this brings inconsistency. Please add '$default' to the list of allowed theme variants or change $default_config_name first.");
+        }
+    }
+}
---
+++ tests/unit/common/Layout/ConfigDefaultThemeVariantValidatorTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tuleap\Layout;
+
+use Tuleap\Config\InvalidConfigKeyValueException;
+use Tuleap\ForgeConfigSandbox;
+
+final class ConfigDefaultThemeVariantValidatorTest extends \Tuleap\Test\PHPUnit\TestCase
+{
+    use ForgeConfigSandbox;
+
+    public function testRejectUnknownVariant(): void
+    {
+        $this->expectException(InvalidConfigKeyValueException::class);
+
+        ConfigDefaultThemeVariantValidator::buildSelf()
+            ->checkIsValid('invalid');
+    }
+
+    public function testRejectDefaultVariantNotPartOfAllowed(): void
+    {
+        \ForgeConfig::set(\ThemeVariant::CONFIG_ALLOWED, 'orange,blue');
+
+        $this->expectException(InvalidConfigKeyValueException::class);
+
+        ConfigDefaultThemeVariantValidator::buildSelf()
+            ->checkIsValid('purple');
+    }
+
+    public function testHappyPath(): void
+    {
+        \ForgeConfig::set(\ThemeVariant::CONFIG_ALLOWED, 'orange,blue');
+
+        $this->expectNotToPerformAssertions();
+
+        ConfigDefaultThemeVariantValidator::buildSelf()
+            ->checkIsValid('blue');
+    }
+}
