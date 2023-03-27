7c38de584dae16b6d1232225c7832edb7583e5bb
Thomas Gorka <thomas.gorka@enalean.com>
2023-03-27 14:54:13 +0200

    design: Don't display a border below PR infos when user cannot write
    part of story #31186 Add management of "Merge"
    
    As per the mockups, when the PR is open and the user has not the right
    to merge the pull-request, the component containing this button is not
    displayed. However, there should not be a border below the PR infos if
    the component is hidden.
    
    How to test:
    - Go to a pull-request with a user only having READ permissions in the
      repository
    
    --> No border is displayed below the PR info section
    
    Change-Id: Ia6b478be97006c12ced7aad509eea35fd69ecb84

--- plugins/pullrequest/scripts/pullrequest-overview/src/components/Actions/merge-status-helper.ts
+++ plugins/pullrequest/scripts/pullrequest-overview/src/components/Actions/merge-status-helper.ts
@@ -17,21 +17,33 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import type { PullRequest, PullRequestMerged } from "@tuleap/plugin-pullrequest-rest-api-types";
+import type {
+    PullRequest,
+    PullRequestAbandoned,
+    PullRequestInReview,
+    PullRequestMerged,
+} from "@tuleap/plugin-pullrequest-rest-api-types";
 import {
     BUILD_STATUS_SUCCESS,
     PULL_REQUEST_MERGE_STATUS_CONFLICT,
     PULL_REQUEST_MERGE_STATUS_FF,
     PULL_REQUEST_MERGE_STATUS_UNKNOWN,
+    PULL_REQUEST_STATUS_ABANDON,
     PULL_REQUEST_STATUS_MERGED,
     PULL_REQUEST_STATUS_REVIEW,
 } from "@tuleap/plugin-pullrequest-constants";
 
-export const isPullRequestOpen = (pull_request: PullRequest): boolean =>
-    pull_request.status === PULL_REQUEST_STATUS_REVIEW;
+export const isPullRequestInReview = (
+    pull_request: PullRequest
+): pull_request is PullRequestInReview => pull_request.status === PULL_REQUEST_STATUS_REVIEW;
 
-export const isAlreadyMerged = (pull_request: PullRequest): pull_request is PullRequestMerged =>
-    pull_request.status === PULL_REQUEST_STATUS_MERGED;
+export const isPullRequestAlreadyMerged = (
+    pull_request: PullRequest
+): pull_request is PullRequestMerged => pull_request.status === PULL_REQUEST_STATUS_MERGED;
+
+export const isPullRequestAbandoned = (
+    pull_request: PullRequest
+): pull_request is PullRequestAbandoned => pull_request.status === PULL_REQUEST_STATUS_ABANDON;
 
 export const isSameReferenceMerge = (pull_request: PullRequest): boolean =>
     pull_request.reference_src === pull_request.reference_dest;
@@ -56,11 +68,11 @@ export const canPullRequestBeMerged = (
     are_merge_commits_allowed_in_repository: boolean
 ): boolean => {
     if (
-        !isPullRequestOpen(pull_request) ||
+        !isPullRequestInReview(pull_request) ||
         !hasUserPermissionToMerge(pull_request) ||
         isSameReferenceMerge(pull_request) ||
         isMergeConflicting(pull_request) ||
-        isAlreadyMerged(pull_request) ||
+        isPullRequestAlreadyMerged(pull_request) ||
         isUnknownMerge(pull_request)
     ) {
         return false;
