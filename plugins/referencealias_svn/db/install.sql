600d29695a396cfdbfde1114ca0b44b67e374020
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2023-03-30 15:59:20 +0200

    Remove referencealias_svn plugin
    part of request #30737 remove reference_alias_* and artifact_folder plugins
    
    Change-Id: I903b08ce181163bfe498d56eb4e0352dca8f19ec

--- plugins/referencealias_svn/db/install.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE plugin_referencealias_svn (
-  source VARCHAR(100) UNIQUE,
-  repository_id INT(11) UNSIGNED NOT NULL,
-  revision_id INT(11) UNSIGNED NOT NULL,
-  INDEX idx(source(10), repository_id)
-);
