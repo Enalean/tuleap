29f88032b1000f95c3847ac32d056fe8aa549cb7
Joris Masson <joris.masson@enalean.com>
2023-03-31 17:06:16 +0200

    refactor: Use cy.session to log in TestManagement tests
    No functional change.
    End-to-end tests for TestManagement should succeed.
    
    Note: almost all TTM tests depended on each other. They have been reorganized.
    
    part of request #29992 Bump cypress to 12.1.0
    
    Change-Id: Ifee9e1c669a738ded523ec21d8df582da488d950

--- plugins/testmanagement/tests/e2e/cypress/cypress/e2e/shortcuts.cy.ts
+++ plugins/testmanagement/tests/e2e/cypress/cypress/e2e/shortcuts.cy.ts
@@ -18,59 +18,49 @@
  */
 
 describe("TTM shortcuts", () => {
-    before(() => {
-        cy.clearSessionCookie();
-        cy.projectMemberLogin();
-        cy.visitProjectService("test-management-project", "Test Management");
-    });
-
-    beforeEach(() => {
-        cy.preserveSessionCookies();
-        // eslint-disable-next-line cypress/require-data-selectors
-        cy.get("body").as("body");
-    });
-
     context("In test campaigns list page", () => {
         it("focuses search field", () => {
-            cy.get("@body").type("f");
+            cy.projectMemberSession();
+            cy.visitProjectService("test-management-project", "Test Management");
+            cy.root().type("f");
             cy.focused().should("have.attr", "data-test", "search-filter");
         });
     });
 
-    context("In test campaign page", () => {
-        before(() => {
-            cy.contains("A test campaign").click();
-            cy.contains("First test case");
-        });
+    it(`In test campaign page`, () => {
+        cy.projectMemberSession();
+        cy.visitProjectService("test-management-project", "Test Management");
+        cy.contains("A test campaign").click();
+        cy.contains("First test case");
 
-        it("allows user to navigate in tests list using its keyboard", () => {
-            cy.get("@body").type("l{home}");
-            cy.focused().should("contain", "First test case");
+        // eslint-disable-next-line cypress/require-data-selectors
+        cy.get("body").as("body");
 
-            cy.get("@body").type("l{downarrow}{downarrow}");
-            cy.focused().should("contain", "Third test case");
+        cy.log("allows user to navigate in tests list using its keyboard");
+        cy.get("@body").type("l{home}");
+        cy.focused().should("contain", "First test case");
 
-            cy.get("@body").type("{end}{downarrow}");
-            cy.focused().should("contain", "First test case");
-        });
+        cy.get("@body").type("l{downarrow}{downarrow}");
+        cy.focused().should("contain", "Third test case");
 
-        it("filters remaining tests with `r` key shortcut", () => {
-            cy.contains("First test case").click();
-            cy.get("[data-test=mark-test-as-passed]").click();
-            cy.get("[data-test=current-test").should("have.class", "passed");
+        cy.get("@body").type("{end}{downarrow}");
+        cy.focused().should("contain", "First test case");
 
-            cy.get("@body").type("r");
-            cy.get("[data-test=campaign-tests-list]")
-                .as("tests-list")
-                .should("not.contain", "First test case");
+        cy.log("filters remaining tests with `r` key shortcut");
+        cy.contains("First test case").click();
+        cy.get("[data-test=mark-test-as-passed]").click();
+        cy.get("[data-test=current-test").should("have.class", "passed");
 
-            cy.get("[data-test=mark-test-as-notrun]").click();
-            cy.get("@tests-list").should("contain", "First test case");
-        });
+        cy.get("@body").type("r");
+        cy.get("[data-test=campaign-tests-list]")
+            .as("tests-list")
+            .should("not.contain", "First test case");
 
-        it("opens `Select tests` modal", () => {
-            cy.get("@body").type("e");
-            cy.get("[data-test=close-select-tests-modal]").click();
-        });
+        cy.get("[data-test=mark-test-as-notrun]").click();
+        cy.get("@tests-list").should("contain", "First test case");
+
+        cy.log("opens `Select tests` modal");
+        cy.get("@body").type("e");
+        cy.get("[data-test=close-select-tests-modal]").click();
     });
 });
