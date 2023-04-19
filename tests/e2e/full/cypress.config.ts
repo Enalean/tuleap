2f7a431dd49695e2c0f23b5d897919677066e07d
Joris Masson <joris.masson@enalean.com>
2023-04-19 10:48:07 +0200

    refactor: Centralize session configuration
    No functional change, end-to-end tests should pass.
    
    Note: Now that all plugins use session, we can pull back the
    configuration option to the shared lib and stop repeating it.
    
    part of request #29992 Bump cypress to 12.1.0
    
    Change-Id: I362945c082c56a076abc48ec5e49ca55d1229913

--- tests/e2e/full/cypress.config.ts
+++ tests/e2e/full/cypress.config.ts
@@ -1,5 +1,5 @@
 import { cypress_config } from "@tuleap/cypress-configurator";
 import { defineConfig } from "cypress";
 
-const e2e = { ...cypress_config.e2e, experimentalSessionAndOrigin: true };
+const e2e = { ...cypress_config.e2e };
 export default defineConfig({ ...cypress_config, e2e });
