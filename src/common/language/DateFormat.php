ef06a8668b970442e18a0b467e48375dfde322f8
Nicolas Terray <nicolas.terray@enalean.com>
2023-04-24 15:54:19 +0200

    feat: i18n of timeframe dates
    The timeframe dates in roadmap are displayed like "april 24, 2023". This
    is done by using the integrated Intl.DateTimeFormatter in the browser.
    
    For timeframe dates in tooltips, since they are generated by the backend
    we should use PHP's counterpart IntlDateFormatter. This will allow us to
    have "24 avril 2023" in french for example, as soon as we provide the
    pattern.
    
    Until now, we were relying of tab files to get the format of a date. Tab
    files are things of the past and we don't want to platform
    administrators to be able to customize the pattern. A new helper class
    is now available to provide the pattern according to current user's
    locale. This means that when Tuleap will support a new language (for
    example pt_BR), we will have to edit this file to insert the expected
    format.
    
    Ideally we should remove entries datefmt* from tab files to replace
    them with dedicated function in this new DateFormat helper.
    
    To test: have an artifact with a timeframe semantic displayed in the
    tooltip. The start/end dates of the semantic should respect user's
    locale.
    
    Part of story #26777: have artifact tooltips on roadmap
    
    Change-Id: I4ba0418447746a763493a13b24a590f7751c1472

---
+++ src/common/language/DateFormat.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Language;
+
+use IntlDateFormatter;
+
+final class DateFormat
+{
+    public static function getYearFullMonthAndDayFormatter(\PFUser $user): IntlDateFormatter
+    {
+        $pattern = match ($user->getLocale()) {
+            'fr_FR' => 'd MMMM YYYY',
+            default => 'MMMM d, YYYY', // default to en_US
+        };
+
+        return new IntlDateFormatter(
+            $user->getLocale(),
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            $user->getTimezone(),
+            IntlDateFormatter::GREGORIAN,
+            $pattern,
+        );
+    }
+}
---
+++ tests/unit/common/language/DateFormatTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\language;
+
+use DateTimeImmutable;
+use Tuleap\Language\DateFormat;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class DateFormatTest extends TestCase
+{
+    public function testGetYearFullMonthAndDayFormatter(): void
+    {
+        $dylan = UserTestBuilder::aUser()->withLocale('en_US')->build();
+        $jean  = UserTestBuilder::aUser()->withLocale('fr_FR')->build();
+
+        $date = (new DateTimeImmutable())->setTimestamp(1234567890);
+
+        self::assertEquals('February 14, 2009', DateFormat::getYearFullMonthAndDayFormatter($dylan)->format($date));
+        self::assertEquals('14 février 2009', DateFormat::getYearFullMonthAndDayFormatter($jean)->format($date));
+    }
+}
