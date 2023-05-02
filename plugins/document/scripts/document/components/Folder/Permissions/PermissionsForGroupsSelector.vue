Enalean/tuleap

commit dc7b9ece246b4eb58e86a416b18e9df434114552

Chore: Convert document to Vue3 in compat mode
part of request #30736 Convert document app in vue3

Notes:
 - component render have changes, vue renders stub during test
- it impacts our slot usage, they must be declare outside subcomponent
(visible in dropdown)
- when we need to test compnent rendered inside a component, most of
time we want to ensure that our computed is correct
=> the only way I found to keep those tests was to expose the
computed with `defineExpose`

Change-Id: If404e9d17a27fc9355790e75a02806e67e92f895

plugins/document/scripts/document/components/Folder/Permissions/PermissionsForGroupsSelector.vue
@@ -63,7 +63,7 @@
 </template>
 
 <script setup>
-import { useGettext } from "@tuleap/vue2-gettext-composition-helper";
+import { useGettext } from "vue3-gettext";
 
 const { $gettext } = useGettext();
 </script>

commit 7e0161fd426f794306ce096e9b3eba633ee9e5af

Fix document coverage
part of request #30736 Convert document app in vue3

How to test

Run coverage under document plugin, no error
Introduced by: dc7b9ece246b4eb58e86a416b18e9df434114552

Change-Id: I34aa3e8c98c5e2598d90aeb59670d2637490a1f1

plugins/document/scripts/document/components/Folder/Permissions/PermissionsForGroupsSelector.vue
@@ -62,12 +62,6 @@
     </div>
 </template>
 
-<script setup>
-import { useGettext } from "vue3-gettext";
-
-const { $gettext } = useGettext();
-</script>
-
 <script>
 import PermissionsSelector from "./PermissionsSelector.vue";
 