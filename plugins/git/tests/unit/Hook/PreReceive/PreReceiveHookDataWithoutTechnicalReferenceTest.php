8a50dbb333d1b49d434958f961e49b70b0662cb7
Thomas PIRAS <thomas.piras@enalean.com>
2023-05-05 14:54:09 +0200

    Do not filter protected git references
    We now remove protected git references from the references passed to WASM modules, as this could interfere with the Pull Request plugin.
    
    How to test:
    - tuleap config-set feature_flag_enable_pre_receive_command 1 (set the feature flag)
    - cp plugins/git/etc/sudoers.d/tuleap-git-prereceive /etc/sudoers.d/
    - tuleap-cfg site-deploy:gitolite3-hooks
    - Place a valid WASM module in /var/lib/tuleap/untrusted-code/git/pre-receive-hook/<repo-id>.wasm
    (If you don't want to write your own, you can use https://gerrit.tuleap.net/plugins/gitiles/tuleap/+/refs/changes/75/27375/12/src/additional-packages/pre-receive-hook-example)
    - Enable the Pull Request plugin in Tuleap's interface
    - In a git repository, push some content on two separate, valid, git branches.
    - Create a pull request between these branches
    - The PR is created an can be merged successfully
    
    Part of story #31077 deploy a WebAssembly module to validate incoming references as a pre-receive hook
    
    Change-Id: I5320058f7adaf4436c9a90f3d0b81fe85d939e95

---
+++ plugins/git/tests/unit/Hook/PreReceive/PreReceiveHookDataWithoutTechnicalReferenceTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Git\Hook\PreReceive;
+
+use Psr\Log\NullLogger;
+use Tuleap\Git\MarkTechnicalReference;
+use Tuleap\NeverThrow\Result;
+use Tuleap\Test\Stubs\EventDispatcherStub;
+
+final class PreReceiveHookDataWithoutTechnicalReferenceTest extends \Tuleap\Test\PHPUnit\TestCase
+{
+    public function testItRemovesTechnicalReference(): void
+    {
+        $event_dispatcher = EventDispatcherStub::withCallback(function (MarkTechnicalReference $event): MarkTechnicalReference {
+            $event->markAsTechnical();
+            return $event;
+        });
+
+        $input = "a b refs/tlpr/42/head\n\r";
+
+        $result = PreReceiveHookData::fromRawStdinHook($input, new NullLogger())->map(
+            fn (PreReceiveHookData $hook_data): PreReceiveHookDataWithoutTechnicalReference => PreReceiveHookDataWithoutTechnicalReference::fromHookData($hook_data, $event_dispatcher, new NullLogger())
+        );
+
+        self::assertTrue(Result::isOk($result));
+        self::assertEquals([], $result->value->updated_references);
+    }
+
+    public function testItDoesNotRemoveNonTechnicalReference(): void
+    {
+        $input                                     = "a b refs/heads/tuleap-pr\n\r";
+        $updated_reference['refs/heads/tuleap-pr'] = new PreReceiveHookUpdatedReference('a', 'b');
+
+        $result = PreReceiveHookData::fromRawStdinHook($input, new NullLogger())->map(
+            fn (PreReceiveHookData $hook_data): PreReceiveHookDataWithoutTechnicalReference => PreReceiveHookDataWithoutTechnicalReference::fromHookData($hook_data, EventDispatcherStub::withIdentityCallback(), new NullLogger())
+        );
+
+        self::assertTrue(Result::isOk($result));
+        self::assertEquals($updated_reference, $result->value->updated_references);
+    }
+}
