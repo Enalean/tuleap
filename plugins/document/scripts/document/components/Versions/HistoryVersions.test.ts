Enalean/tuleap

commit dc7b9ece246b4eb58e86a416b18e9df434114552

Chore: Convert document to Vue3 in compat mode
part of request #30736 Convert document app in vue3

Notes:
 - component render have changes, vue renders stub during test
- it impacts our slot usage, they must be declare outside subcomponent
(visible in dropdown)
- when we need to test compnent rendered inside a component, most of
time we want to ensure that our computed is correct
=> the only way I found to keep those tests was to expose the
computed with `defineExpose`

Change-Id: If404e9d17a27fc9355790e75a02806e67e92f895

plugins/document/scripts/document/components/Folder/DropDown/DropDownMenu.vue
@@ -91,11 +91,11 @@
             <span>{{ $gettext("Approval tables") }}</span>
         </a>
 
-        <drop-down-separator />
+        <drop-down-separator v-if="item.user_can_write" />
 
-        <cut-item v-bind:item="item" />
-        <copy-item v-bind:item="item" />
-        <paste-item v-bind:destination="item" />
+        <cut-item v-bind:item="item" v-if="item.user_can_write" />
+        <copy-item v-bind:item="item" v-if="item.user_can_write" />
+        <paste-item v-bind:destination="item" v-if="item.user_can_write" />
 
         <template v-if="is_item_a_folder">
             <drop-down-separator />
@@ -106,7 +106,7 @@
         </template>
 
         <slot name="delete-item-separator" v-if="is_deletion_allowed" />
-        <slot name="delete-item" />
+        <slot name="delete-item" v-if="is_deletion_allowed" />
     </fragment>
 </template>
 <script setup lang="ts">
@@ -147,4 +147,6 @@ const should_display_versions_link = computed(
 function getUrlForPane(pane_name: string): string {
     return `/plugins/docman/?group_id=${project_id.value}&id=${props.item.id}&action=details&section=${pane_name}`;
 }
+
+defineExpose({ is_item_an_empty_document, is_item_a_folder, should_display_versions_link });
 </script>

plugins/document/scripts/document/components/Folder/DropDown/NewDocument/NewItemMenuOptions.vue
@@ -62,12 +62,10 @@
             <i class="fa-fw tlp-dropdown-menu-item-icon" v-bind:class="ICON_WIKI"></i>
             {{ $gettext("Wiki page") }}
         </button>
-        <template v-for="section in create_new_item_alternatives">
-            <span
-                class="tlp-dropdown-menu-title document-dropdown-menu-title"
-                v-bind:key="section.title"
-                >{{ section.title }}</span
-            >
+        <template v-for="section in create_new_item_alternatives" v-bind:key="section.title">
+            <span class="tlp-dropdown-menu-title document-dropdown-menu-title">{{
+                section.title
+            }}</span>
             <button
                 v-for="alternative in section.alternatives"
                 class="tlp-dropdown-menu-item"

plugins/document/scripts/document/components/History/DisplayHistory.vue
@@ -29,7 +29,7 @@
             <router-link
                 class="tlp-tab"
                 v-bind:to="{ name: 'versions', params: { item_id: item.id } }"
-                v-if="item_type_has_versions"
+                v-if="item_has_versions"
                 data-test="versions-link"
                 >{{ $gettext("Versions") }}</router-link
             >
@@ -43,9 +43,9 @@
 
 <script setup lang="ts">
 import DocumentTitleLockInfo from "../Folder/LockInfo/DocumentTitleLockInfo.vue";
-import { useRoute } from "../../helpers/use-router";
+import { useRoute } from "vue-router";
 import { useActions } from "vuex-composition-helpers";
-import { inject, onBeforeMount, ref } from "vue";
+import { computed, inject, onBeforeMount, ref } from "vue";
 import type { Item } from "../../type";
 import HistoryLogs from "./HistoryLogs.vue";
 import { isEmbedded, isFile, isLink } from "../../helpers/type-check-helper";
@@ -65,4 +65,10 @@ onBeforeMount(async () => {
             isFile(item.value) || isLink(item.value) || isEmbedded(item.value);
     }
 });
+
+const item_has_versions = computed((): boolean => {
+    return item_type_has_versions.value;
+});
+
+defineExpose({ item_has_versions });
 </script>

plugins/document/scripts/document/components/Versions/DisplayVersions.vue
@@ -47,7 +47,7 @@
 
 <script setup lang="ts">
 import DocumentTitleLockInfo from "../Folder/LockInfo/DocumentTitleLockInfo.vue";
-import { useRoute } from "../../helpers/use-router";
+import { useRoute } from "vue-router";
 import { useActions } from "vuex-composition-helpers";
 import { inject, onBeforeMount, provide, ref } from "vue";
 import type { Item } from "../../type";

plugins/document/scripts/document/components/Versions/HistoryVersionsContentRow.vue
@@ -140,7 +140,7 @@ import type { Modal } from "@tuleap/tlp-modal";
 import { createModal, EVENT_TLP_MODAL_HIDDEN } from "@tuleap/tlp-modal";
 import { deleteFileVersion } from "../../api/version-rest-querier";
 import DocumentRelativeDate from "../Date/DocumentRelativeDate.vue";
-import { useGettext } from "@tuleap/vue2-gettext-composition-helper";
+import { useGettext } from "vue3-gettext";
 import { FEEDBACK } from "../../injection-keys";
 import { noop_feedack_handler } from "../../helpers/noop-feedback-handler";
 

plugins/document/scripts/document/index.js
@@ -17,17 +17,17 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Vue from "vue";
+import Vue, { createApp } from "vue";
 import VueDOMPurifyHTML from "vue-dompurify-html";
 
 import App from "./components/App.vue";
-import { createStore } from "./store/index.js";
-import { createRouter } from "./router/index.js";
+import { createInitializedStore } from "./store/index.js";
+import { createInitializedRouter } from "./router/router";
 import moment from "moment";
 import "moment-timezone";
-import { createPinia, PiniaVuePlugin } from "pinia";
-
-import { getPOFileFromLocale, initVueGettext } from "@tuleap/vue2-gettext-init";
+import { createPinia } from "pinia";
+import { getPOFileFromLocale, initVueGettext } from "@tuleap/vue3-gettext-init";
+import { createGettext } from "vue3-gettext";
 
 import { setupDocumentShortcuts } from "./keyboard-navigation/keyboard-navigation";
 
@@ -100,9 +100,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     moment.tz(user_timezone);
     moment.locale(user_locale);
 
-    await initVueGettext(Vue, (locale) =>
-        import(/* webpackChunkName: "document-po-" */ "./po/" + getPOFileFromLocale(locale))
-    );
+    const app = createApp(App, {
+        csrf_token_name,
+        csrf_token,
+    });
 
     const configuration_state = {
         user_id,
@@ -137,31 +138,23 @@ document.addEventListener("DOMContentLoaded", async () => {
         can_user_switch_to_old_ui,
     };
 
-    const AppComponent = Vue.extend(App);
-    const store = createStore(configuration_state);
-    const router = createRouter(store, project_name);
+    const store = createInitializedStore(user_id, project_id, configuration_state);
+    app.use(store);
+    const gettext = await initVueGettext(createGettext, (locale) =>
+        import(`./po/${getPOFileFromLocale(locale)}`)
+    );
 
-    Vue.use(PiniaVuePlugin);
     const pinia = createPinia();
+    app.use(pinia);
 
-    new AppComponent({
-        store,
-        router,
-        propsData: {
-            csrf_token_name,
-            csrf_token,
-        },
-        provide: {
-            should_display_history_in_document,
-            should_display_source_column_for_versions,
-            create_new_item_alternatives,
-        },
-        pinia,
-    }).$mount(vue_mount_point);
+    app.use(gettext);
+    app.use(createInitializedRouter(store, project_name, gettext));
 
-    const gettext_provider = {
-        $gettext: Vue.prototype.$gettext,
-        $pgettext: Vue.prototype.$pgettext,
-    };
-    setupDocumentShortcuts(gettext_provider);
+    app.provide(should_display_history_in_document);
+    app.provide(should_display_source_column_for_versions);
+    app.provide(create_new_item_alternatives);
+
+    app.mount(vue_mount_point);
+
+    setupDocumentShortcuts(gettext);
 });

commit 05775d6d5568631ff8477ca372966b0c87237d93

Fixes request #31942: No longer possible to create OnlyOffice documents
Issue has been introduced by dc7b9ece246b4eb58e86a416b18e9df434114552.

It also impacts the feature flags/options to display the history and the
source column for versions.

Change-Id: I440c78eb45a38aaf42ff0ab84453b1c935089540

plugins/document/pnpm-lock.yaml
@@ -22,6 +22,7 @@ specifiers:
   '@tuleap/tlp-popovers': workspace:*
   '@tuleap/tlp-relative-date': workspace:*
   '@tuleap/tlp-swatch-colors': workspace:*
+  '@tuleap/vue-strict-inject': workspace:*
   '@tuleap/vue3-breadcrumb-privacy': workspace:*
   '@tuleap/vue3-gettext-init': workspace:*
   '@tuleap/vuex-store-wrapper-jest': workspace:*
@@ -67,6 +68,7 @@ dependencies:
   '@tuleap/tlp-popovers': link:../../lib/frontend/tlp-popovers
   '@tuleap/tlp-relative-date': link:../../lib/frontend/tlp-relative-date
   '@tuleap/tlp-swatch-colors': link:../../lib/frontend/tlp-swatch-colors
+  '@tuleap/vue-strict-inject': link:../../lib/frontend/vue-strict-inject
   '@tuleap/vue3-breadcrumb-privacy': link:../../lib/frontend/vue3-breadcrumb-privacy
   '@tuleap/vue3-gettext-init': link:../../lib/frontend/vue3-gettext-init
   '@vue/compat': 3.2.47_vue@3.2.47

plugins/document/scripts/document/components/Folder/DropDown/DropDownMenu.vue
@@ -120,7 +120,9 @@ import { isFolder, isEmpty, isFile, isLink, isEmbedded } from "../../../helpers/
 import type { Item } from "../../../type";
 import { useNamespacedState } from "vuex-composition-helpers";
 import type { ConfigurationState } from "../../../store/configuration";
-import { computed, inject } from "vue";
+import { computed } from "vue";
+import { strictInject } from "@tuleap/vue-strict-inject";
+import { SHOULD_DISPLAY_HISTORY_IN_DOCUMENT } from "../../../injection-keys";
 
 const props = defineProps<{ item: Item }>();
 
@@ -136,7 +138,7 @@ const is_item_a_folder = computed((): boolean => isFolder(props.item));
 
 const is_item_an_empty_document = computed((): boolean => isEmpty(props.item));
 
-const should_display_history_in_document = inject("should_display_history_in_document", false);
+const should_display_history_in_document = strictInject(SHOULD_DISPLAY_HISTORY_IN_DOCUMENT);
 
 const should_display_versions_link = computed(
     (): boolean =>

plugins/document/scripts/document/components/Folder/DropDown/NewDocument/NewItemMenuOptions.vue
@@ -126,18 +126,15 @@ import {
 import emitter from "../../../../helpers/emitter";
 import { useNamespacedState } from "vuex-composition-helpers";
 import type { ConfigurationState } from "../../../../store/configuration";
-import { inject } from "vue";
-import type { NewItemAlternativeArray } from "../../../../type";
 import { iconForMimeType } from "../../../../helpers/icon-for-mime-type";
+import { strictInject } from "@tuleap/vue-strict-inject";
+import { NEW_ITEMS_ALTERNATIVES } from "../../../../injection-keys";
 
 const { embedded_are_allowed, user_can_create_wiki } = useNamespacedState<
     Pick<ConfigurationState, "embedded_are_allowed" | "user_can_create_wiki">
 >("configuration", ["embedded_are_allowed", "user_can_create_wiki"]);
 
-const create_new_item_alternatives = inject<NewItemAlternativeArray>(
-    "create_new_item_alternatives",
-    []
-);
+const create_new_item_alternatives = strictInject(NEW_ITEMS_ALTERNATIVES);
 
 const props = defineProps<{ item: Item }>();
 function showNewDocumentModal(type: ItemType): void {

plugins/document/scripts/document/components/History/DisplayHistory.vue
@@ -45,12 +45,14 @@
 import DocumentTitleLockInfo from "../Folder/LockInfo/DocumentTitleLockInfo.vue";
 import { useRoute } from "vue-router";
 import { useActions } from "vuex-composition-helpers";
-import { computed, inject, onBeforeMount, ref } from "vue";
+import { computed, onBeforeMount, ref } from "vue";
 import type { Item } from "../../type";
 import HistoryLogs from "./HistoryLogs.vue";
 import { isEmbedded, isFile, isLink } from "../../helpers/type-check-helper";
+import { strictInject } from "@tuleap/vue-strict-inject";
+import { SHOULD_DISPLAY_HISTORY_IN_DOCUMENT } from "../../injection-keys";
 
-const should_display_history_in_document = inject("should_display_history_in_document", false);
+const should_display_history_in_document = strictInject(SHOULD_DISPLAY_HISTORY_IN_DOCUMENT);
 
 const item = ref<Item | null>(null);
 const item_type_has_versions = ref(false);

plugins/document/scripts/document/components/Versions/DisplayVersions.vue
@@ -49,11 +49,12 @@
 import DocumentTitleLockInfo from "../Folder/LockInfo/DocumentTitleLockInfo.vue";
 import { useRoute } from "vue-router";
 import { useActions } from "vuex-composition-helpers";
-import { inject, onBeforeMount, provide, ref } from "vue";
+import { onBeforeMount, provide, ref } from "vue";
 import type { Item } from "../../type";
 import HistoryVersions from "./HistoryVersions.vue";
 import { isEmbedded, isFile, isLink } from "../../helpers/type-check-helper";
-import { FEEDBACK } from "../../injection-keys";
+import { FEEDBACK, SHOULD_DISPLAY_HISTORY_IN_DOCUMENT } from "../../injection-keys";
+import { strictInject } from "@tuleap/vue-strict-inject";
 
 const success_feedback = ref<string | null>(null);
 
@@ -63,7 +64,7 @@ provide(FEEDBACK, {
     },
 });
 
-const should_display_history_in_document = inject("should_display_history_in_document", false);
+const should_display_history_in_document = strictInject(SHOULD_DISPLAY_HISTORY_IN_DOCUMENT);
 
 const item = ref<Item | null>(null);
 const item_type_has_versions = ref(false);

plugins/document/scripts/document/components/Versions/HistoryVersions.vue
@@ -72,7 +72,7 @@ import HistoryVersionsErrorState from "./HistoryVersionsErrorState.vue";
 import HistoryVersionsEmptyState from "./HistoryVersionsEmptyState.vue";
 import HistoryVersionsContent from "./HistoryVersionsContent.vue";
 import type { EmbeddedFileVersion, FileHistory, Item, LinkVersion } from "../../type";
-import { computed, inject, onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import {
     getAllEmbeddedFileVersionHistory,
     getAllFileVersionHistory,
@@ -80,6 +80,8 @@ import {
 } from "../../api/version-rest-querier";
 import { isEmbedded, isLink } from "../../helpers/type-check-helper";
 import HistoryVersionsContentForLink from "./HistoryVersionsContentForLink.vue";
+import { strictInject } from "@tuleap/vue-strict-inject";
+import { SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS } from "../../injection-keys";
 
 const props = defineProps<{ item: Item }>();
 
@@ -91,9 +93,8 @@ const is_empty = computed(
     (): boolean => file_versions.value.length === 0 && link_versions.value.length === 0
 );
 
-const should_display_source_column_for_versions = inject(
-    "should_display_source_column_for_versions",
-    false
+const should_display_source_column_for_versions = strictInject(
+    SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS
 );
 
 const is_link = computed((): boolean => isLink(props.item));

plugins/document/scripts/document/components/Versions/HistoryVersionsContentRow.vue
@@ -141,8 +141,9 @@ import { createModal, EVENT_TLP_MODAL_HIDDEN } from "@tuleap/tlp-modal";
 import { deleteFileVersion } from "../../api/version-rest-querier";
 import DocumentRelativeDate from "../Date/DocumentRelativeDate.vue";
 import { useGettext } from "vue3-gettext";
-import { FEEDBACK } from "../../injection-keys";
+import { FEEDBACK, SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS } from "../../injection-keys";
 import { noop_feedack_handler } from "../../helpers/noop-feedback-handler";
+import { strictInject } from "@tuleap/vue-strict-inject";
 
 const props = defineProps<{
     item: Item;
@@ -156,9 +157,8 @@ const delete_button = ref<HTMLButtonElement | null>(null);
 const is_deleting = ref(false);
 const got_error_while_trying_to_delete = ref(false);
 
-const should_display_source_column_for_versions = inject(
-    "should_display_source_column_for_versions",
-    false
+const should_display_source_column_for_versions = strictInject(
+    SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS
 );
 
 const gettext_provider = useGettext();

plugins/document/scripts/document/index.js
@@ -17,7 +17,7 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Vue, { createApp } from "vue";
+import { createApp } from "vue";
 import VueDOMPurifyHTML from "vue-dompurify-html";
 
 import App from "./components/App.vue";
@@ -30,12 +30,14 @@ import { getPOFileFromLocale, initVueGettext } from "@tuleap/vue3-gettext-init";
 import { createGettext } from "vue3-gettext";
 
 import { setupDocumentShortcuts } from "./keyboard-navigation/keyboard-navigation";
+import {
+    NEW_ITEMS_ALTERNATIVES,
+    SHOULD_DISPLAY_HISTORY_IN_DOCUMENT,
+    SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS,
+} from "./injection-keys";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    Vue.use(VueDOMPurifyHTML);
-
     let user_locale = document.body.dataset.userLocale;
-    Vue.config.language = user_locale;
     user_locale = user_locale.replace(/_/g, "-");
 
     const vue_mount_point = document.getElementById("document-tree-view");
@@ -150,9 +152,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     app.use(gettext);
     app.use(createInitializedRouter(store, project_name, gettext));
 
-    app.provide(should_display_history_in_document);
-    app.provide(should_display_source_column_for_versions);
-    app.provide(create_new_item_alternatives);
+    app.provide(SHOULD_DISPLAY_HISTORY_IN_DOCUMENT, should_display_history_in_document);
+    app.provide(
+        SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS,
+        should_display_source_column_for_versions
+    );
+    app.provide(NEW_ITEMS_ALTERNATIVES, create_new_item_alternatives);
+    app.use(VueDOMPurifyHTML);
 
     app.mount(vue_mount_point);
 

plugins/document/scripts/document/injection-keys.ts
@@ -18,6 +18,16 @@
  */
 
 import type { InjectionKey } from "vue";
-import type { FeedbackHandler } from "./type";
+import type { FeedbackHandler, NewItemAlternativeArray } from "./type";
+import type { StrictInjectionKey } from "@tuleap/vue-strict-inject";
 
-export const FEEDBACK: InjectionKey<FeedbackHandler> = Symbol();
+export const FEEDBACK: InjectionKey<FeedbackHandler> = Symbol("feedback");
+export const SHOULD_DISPLAY_HISTORY_IN_DOCUMENT: StrictInjectionKey<boolean> = Symbol(
+    "should_display_history_in_document"
+);
+export const SHOULD_DISPLAY_SOURCE_COLUMN_FOR_VERSIONS: StrictInjectionKey<boolean> = Symbol(
+    "should_display_source_column_for_versions"
+);
+export const NEW_ITEMS_ALTERNATIVES: StrictInjectionKey<NewItemAlternativeArray> = Symbol(
+    "create_new_item_alternatives"
+);

plugins/document/scripts/document/components/Folder/DropDown/DropDownMenu.test.ts
@@ -33,12 +33,16 @@ import {
 import { getGlobalTestOptions } from "../../../helpers/global-options-for-test";
 
 import { nextTick } from "vue";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 describe("DropDownMenu", () => {
     function createWrapper(
         item: Item,
         should_display_history_in_document = false
     ): VueWrapper<InstanceType<typeof DropDownMenu>> {
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(
+            should_display_history_in_document
+        );
         return shallowMount(DropDownMenu, {
             props: { isInFolderEmptyState: false, isInQuickLookMode: false, item },
             global: {
@@ -53,9 +57,6 @@ describe("DropDownMenu", () => {
                         },
                     },
                 }),
-                provide: {
-                    should_display_history_in_document,
-                },
                 stubs: {
                     RouterLink: RouterLinkStub,
                 },

plugins/document/scripts/document/components/Folder/DropDown/NewDocument/NewItemMenuOptions.test.ts
@@ -32,6 +32,7 @@ import {
 } from "../../../../constants";
 import emitter from "../../../../helpers/emitter";
 import { getGlobalTestOptions } from "../../../../helpers/global-options-for-test";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 jest.mock("../../../../helpers/emitter");
 
@@ -52,6 +53,7 @@ describe("NewItemMenuOptions", function () {
         },
         create_new_item_alternatives: NewItemAlternativeArray = []
     ): VueWrapper<InstanceType<typeof NewItemMenuOptions>> {
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(create_new_item_alternatives);
         return shallowMount(NewItemMenuOptions, {
             props: {
                 item: CURRENT_FOLDER,
@@ -68,9 +70,6 @@ describe("NewItemMenuOptions", function () {
                         },
                     },
                 }),
-                provide: {
-                    create_new_item_alternatives,
-                },
             },
         });
     }

plugins/document/scripts/document/components/History/DisplayHistory.test.ts
@@ -33,6 +33,7 @@ import { nextTick } from "vue";
 
 import * as router from "vue-router";
 import type { Item } from "../../type";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 jest.mock("vue-router");
 describe("DisplayHistory", () => {
@@ -47,6 +48,7 @@ describe("DisplayHistory", () => {
 
     it("should not display anything if user tries direct access while feature flag is off", async () => {
         load_document.mockReturnValue({ id: 10 } as Item);
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(false);
         const wrapper = shallowMount(DisplayHistory, {
             global: {
                 ...getGlobalTestOptions({
@@ -54,9 +56,6 @@ describe("DisplayHistory", () => {
                         loadDocumentWithAscendentHierarchy: load_document,
                     },
                 }),
-                provide: {
-                    should_display_history_in_document: false,
-                },
                 stubs: {
                     RouterLink: RouterLinkStub,
                 },
@@ -72,6 +71,7 @@ describe("DisplayHistory", () => {
 
     it("should display logs", async () => {
         load_document.mockReturnValue({ id: 10 } as Item);
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(true);
         const wrapper = shallowMount(DisplayHistory, {
             global: {
                 ...getGlobalTestOptions({
@@ -79,9 +79,6 @@ describe("DisplayHistory", () => {
                         loadDocumentWithAscendentHierarchy: load_document,
                     },
                 }),
-                provide: {
-                    should_display_history_in_document: true,
-                },
                 stubs: {
                     RouterLink: RouterLinkStub,
                 },
@@ -106,6 +103,7 @@ describe("DisplayHistory", () => {
         `should display a Versions link for %s: %s`,
         async (type, should_versions_link_be_displayed) => {
             load_document.mockReturnValue({ id: 10, type } as Item);
+            jest.spyOn(strict_inject, "strictInject").mockReturnValue(true);
             const wrapper = shallowMount(DisplayHistory, {
                 global: {
                     ...getGlobalTestOptions({
@@ -113,9 +111,6 @@ describe("DisplayHistory", () => {
                             loadDocumentWithAscendentHierarchy: load_document,
                         },
                     }),
-                    provide: {
-                        should_display_history_in_document: true,
-                    },
                     stubs: {
                         RouterLink: RouterLinkStub,
                     },

plugins/document/scripts/document/components/Versions/DisplayVersions.test.ts
@@ -33,6 +33,7 @@ import { getGlobalTestOptions } from "../../helpers/global-options-for-test";
 import type { Item } from "../../type";
 import * as router from "vue-router";
 import type { RouteLocationNormalizedLoaded } from "vue-router";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 jest.mock("vue-router");
 
@@ -58,6 +59,8 @@ describe("DisplayVersions", () => {
             } as Item);
         });
 
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(true);
+
         const wrapper = shallowMount(DisplayVersions, {
             global: {
                 ...getGlobalTestOptions({
@@ -65,9 +68,6 @@ describe("DisplayVersions", () => {
                         loadDocumentWithAscendentHierarchy: load_with_hierarchy,
                     },
                 }),
-                provide: {
-                    should_display_history_in_document: true,
-                },
                 stubs: ["router-link"],
             },
         });
@@ -87,6 +87,8 @@ describe("DisplayVersions", () => {
             } as Item);
         });
 
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(false);
+
         const wrapper = shallowMount(DisplayVersions, {
             global: {
                 ...getGlobalTestOptions({
@@ -94,9 +96,6 @@ describe("DisplayVersions", () => {
                         loadDocumentWithAscendentHierarchy: load_with_hierarchy,
                     },
                 }),
-                provide: {
-                    should_display_history_in_document: false,
-                },
                 stubs: ["router-link"],
             },
         });

plugins/document/scripts/document/components/Versions/HistoryVersions.test.ts
@@ -46,8 +46,13 @@ import type {
     LinkVersion,
 } from "../../type";
 import { nextTick } from "vue";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 describe("HistoryVersions", () => {
+    beforeEach(() => {
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(false);
+    });
+
     it("should display a loading state", () => {
         getAllFileVersionHistory.mockReturnValue(okAsync([]));
 

plugins/document/scripts/document/components/Versions/HistoryVersionsContentRow.test.ts
@@ -44,6 +44,7 @@ import type { RestUser } from "../../api/rest-querier";
 import type { FileHistory, Item, ItemFile } from "../../type";
 import { FEEDBACK } from "../../injection-keys";
 import { getGlobalTestOptions } from "../../helpers/global-options-for-test";
+import * as strict_inject from "@tuleap/vue-strict-inject";
 
 describe("HistoryVersionsContentRow", () => {
     let location: Pick<Location, "reload">;
@@ -55,6 +56,7 @@ describe("HistoryVersionsContentRow", () => {
         has_more_than_one_version: boolean,
         authoring_tool = ""
     ): VueWrapper<InstanceType<typeof HistoryVersionsContentRow>> {
+        jest.spyOn(strict_inject, "strictInject").mockReturnValue(true);
         return shallowMount(HistoryVersionsContentRow, {
             props: {
                 item,
@@ -80,7 +82,6 @@ describe("HistoryVersionsContentRow", () => {
             global: {
                 ...getGlobalTestOptions({}),
                 provide: {
-                    should_display_source_column_for_versions: true,
                     [FEEDBACK as symbol]: { success },
                 },
             },