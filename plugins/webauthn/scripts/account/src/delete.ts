77d902bdb77a0bc6d3230f51381df5355129de28
Kevin Traini <kevin.traini@enalean.com>
2023-05-30 14:39:17 +0200

    feat: Add route DELETE /webauthn/key/{key_id}
    Part of story #31175 delete a registered passkey
    
    This route will delete a passkey based on id passed in uri. This id must
    be base64 encoded. The route returns 501 as the method in dao to delete
    the source is not yet implemented.
    
    *Testing:*
    
    Call DELETE /webauthn/key/{key_id}:
    - 401 if you are not connected
    - 400 if the source id is not base64
    - 200 if there is no corresponding source
    - 200 if you are not the owner
    - 501 if all is fine
    
    Change-Id: Ia4f9709f5f2051d5835f9906d42f4a0c23a6ef8c

---
+++ plugins/webauthn/scripts/account/src/delete.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Enalean, 2023-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+import type { ResultAsync } from "neverthrow";
+import type { Fault } from "@tuleap/fault";
+import { del, uri } from "@tuleap/fetch-result";
+
+export function deleteKey(key_id: string): ResultAsync<null, Fault> {
+    return del(uri`/webauthn/key/${key_id}`).map(() => null);
+}
