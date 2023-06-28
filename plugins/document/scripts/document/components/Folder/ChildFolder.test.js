Enalean/tuleap

commit dc7b9ece246b4eb58e86a416b18e9df434114552

Chore: Convert document to Vue3 in compat mode
part of request #30736 Convert document app in vue3

Notes:
 - component render have changes, vue renders stub during test
- it impacts our slot usage, they must be declare outside subcomponent
(visible in dropdown)
- when we need to test compnent rendered inside a component, most of
time we want to ensure that our computed is correct
=> the only way I found to keep those tests was to expose the
computed with `defineExpose`

Change-Id: If404e9d17a27fc9355790e75a02806e67e92f895

plugins/document/scripts/document/components/Folder/ChildFolder.vue
@@ -26,6 +26,7 @@
 <script>
 import { mapState } from "vuex";
 import FolderContainer from "./FolderContainer.vue";
+import { useRoute } from "vue-router";
 
 export default {
     name: "ChildFolder",
@@ -33,23 +34,12 @@ export default {
     computed: {
         ...mapState(["current_folder", "currently_previewed_item"]),
     },
-    watch: {
-        $route(to) {
-            if (this.$route.name !== "preview") {
-                this.$store.dispatch("removeQuickLook");
-                if (this.current_folder && this.current_folder.id !== this.$route.params.item_id) {
-                    this.$store.dispatch("loadFolder", parseInt(this.$route.params.item_id, 10));
-                }
-            } else {
-                this.$store.dispatch("toggleQuickLook", to.params.preview_item_id);
-            }
-        },
-    },
     async mounted() {
-        if (this.$route.name === "preview") {
+        const route = useRoute();
+        if (route.name === "preview") {
             await this.$store.dispatch(
                 "toggleQuickLook",
-                parseInt(this.$route.params.preview_item_id, 10)
+                parseInt(route.params.preview_item_id, 10)
             );
 
             if (!this.current_folder && this.currently_previewed_item) {
@@ -59,7 +49,7 @@ export default {
                 );
             }
         } else {
-            this.$store.dispatch("loadFolder", parseInt(this.$route.params.item_id, 10));
+            this.$store.dispatch("loadFolder", parseInt(route.params.item_id, 10));
             this.$store.dispatch("removeQuickLook");
         }
     },

plugins/document/scripts/document/components/Folder/ChildFolder.test.js
@@ -18,45 +18,49 @@
  */
 
 import { shallowMount } from "@vue/test-utils";
-import VueRouter from "vue-router";
-import localVue from "../../helpers/local-vue";
-
-import { createStoreMock } from "@tuleap/vuex-store-wrapper-jest";
 import ChildFolder from "./ChildFolder.vue";
+import { getGlobalTestOptions } from "../../helpers/global-options-for-test";
+import { createRouter, createWebHistory } from "vue-router";
+
+import { routes } from "../../router/router";
+import { nextTick } from "vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+});
 
 describe("ChildFolder", () => {
-    let factory, store, router;
-    beforeEach(() => {
-        store = createStoreMock({});
+    let state, load_folder, remove_quick_look, toggle_quick_look;
 
-        router = new VueRouter({
-            routes: [
-                {
-                    path: "/folder/10",
-                    name: "folder",
-                },
-                {
-                    path: "/preview/20",
-                    name: "preview",
-                },
-            ],
+    const factory = () => {
+        const config = getGlobalTestOptions({
+            state,
+            actions: {
+                loadFolder: load_folder,
+                removeQuickLook: remove_quick_look,
+                toggleQuickLook: toggle_quick_look,
+            },
         });
+        return shallowMount(ChildFolder, {
+            global: {
+                plugins: [...config.plugins, router],
+            },
+        });
+    };
 
-        factory = (props = {}) => {
-            return shallowMount(ChildFolder, {
-                localVue,
-                propsData: { ...props },
-                mocks: { $store: store },
-                router,
-            });
-        };
+    beforeEach(() => {
+        state = {};
+        load_folder = jest.fn();
+        remove_quick_look = jest.fn();
+        toggle_quick_look = jest.fn();
     });
 
     it(`Given preview_id parameter is not set
         Then route only deals with tree view
         And we call loadFolder to load current folder content,
-        and we remove quick look properties to be sure to have initial quick look state`, () => {
-        router.push({
+        and we remove quick look properties to be sure to have initial quick look state`, async () => {
+        await router.push({
             name: "folder",
             params: {
                 item_id: 10,
@@ -64,36 +68,36 @@ describe("ChildFolder", () => {
         });
 
         factory();
-        expect(store.dispatch).toHaveBeenCalledWith("loadFolder", 10);
-        expect(store.dispatch).toHaveBeenCalledWith("removeQuickLook");
+        expect(load_folder).toHaveBeenCalledWith(expect.anything(), 10);
+        expect(remove_quick_look).toHaveBeenCalled();
     });
 
     it(`Given a preview id and the current folder is not defined
         Then we should load folder and open document quick look`, async () => {
-        router.push({
+        await router.push({
             name: "preview",
             params: {
                 preview_item_id: 20,
             },
         });
-        const wrapper = factory();
 
-        store.state.currently_previewed_item = {
+        state.currently_previewed_item = {
             id: 20,
             parent_id: 10,
         };
 
-        expect(store.dispatch).toHaveBeenCalledWith("toggleQuickLook", 20);
+        factory();
 
-        await wrapper.vm.$nextTick().then(() => {});
-        expect(store.dispatch).toHaveBeenCalledWith("loadFolder", 10);
+        await nextTick();
+        await nextTick();
+        expect(load_folder).toHaveBeenCalledWith(expect.anything(), 10);
     });
 
     it(`Given a preview id and the current folder is defined
-        Then only open document quick look`, () => {
-        store.state.current_folder = { id: 10, title: "current folder" };
+        Then only open document quick look`, async () => {
+        state.current_folder = { id: 10, title: "current folder" };
 
-        router.push({
+        await router.push({
             name: "preview",
             params: {
                 preview_item_id: 20,
@@ -101,55 +105,7 @@ describe("ChildFolder", () => {
         });
         factory();
 
-        expect(store.dispatch).toHaveBeenCalledWith("toggleQuickLook", 20);
-        expect(store.dispatch).not.toHaveBeenCalledWith("loadFolder");
-    });
-
-    it(`Given route is updated to "folder" and given folder has changed (=> redirection into a folder)
-        Then the folder is loaded`, async () => {
-        store.state.current_folder = { id: 10, title: "current folder" };
-
-        router.push({
-            name: "preview",
-            params: {
-                preview_item_id: 10,
-            },
-        });
-        const wrapper = factory();
-
-        router.push({
-            name: "folder",
-            params: {
-                item_id: 20,
-            },
-        });
-        await wrapper.vm.$nextTick();
-
-        expect(store.dispatch).toHaveBeenCalledWith("removeQuickLook");
-        expect(store.dispatch).toHaveBeenCalledWith("loadFolder", 20);
-    });
-
-    it(`Given route is updated to "folder" and given folder is the same (=> close preview)
-        Then we only close quick look`, async () => {
-        store.state.current_folder = { id: 10, title: "current folder" };
-
-        router.push({
-            name: "preview",
-            params: {
-                preview_item_id: 20,
-            },
-        });
-        const wrapper = factory();
-
-        router.push({
-            name: "folder",
-            params: {
-                preview_item_id: 20,
-            },
-        });
-        await wrapper.vm.$nextTick();
-
-        expect(store.dispatch).toHaveBeenCalledWith("removeQuickLook");
-        expect(store.dispatch).not.toHaveBeenCalledWith("loadFolder");
+        expect(toggle_quick_look).toHaveBeenCalledWith(expect.anything(), 20);
+        expect(load_folder).not.toHaveBeenCalled();
     });
 });

commit 3827c99e5635ef2a27a7d1c4317b622cc79d0bb2

Closes request #32308: Document breadcrumb is broken
Issue has been introduced in dc7b9ece246b4eb58e86a416b18e9df434114552.

Change-Id: Ia9399cc42d7089004928b5bd4f92e44842cdd59e

plugins/document/scripts/document/components/Folder/ChildFolder.vue
@@ -24,13 +24,34 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, useStore } from "vuex";
 import FolderContainer from "./FolderContainer.vue";
 import { useRoute } from "vue-router";
+import { watch } from "vue";
+import { useState } from "vuex-composition-helpers";
 
 export default {
     name: "ChildFolder",
     components: { FolderContainer },
+    setup() {
+        const route = useRoute();
+        const store = useStore();
+        const { current_folder } = useState(["current_folder"]);
+        watch(
+            () => route.path,
+            () => {
+                if (route.name === "preview") {
+                    store.dispatch("toggleQuickLook", parseInt(route.params.preview_item_id, 10));
+                    return;
+                }
+
+                store.dispatch("removeQuickLook");
+                if (current_folder && current_folder.id !== route.params.item_id) {
+                    store.dispatch("loadFolder", parseInt(route.params.item_id, 10));
+                }
+            }
+        );
+    },
     computed: {
         ...mapState(["current_folder", "currently_previewed_item"]),
     },

plugins/document/scripts/document/components/Folder/ChildFolder.test.js
@@ -108,4 +108,24 @@ describe("ChildFolder", () => {
         expect(toggle_quick_look).toHaveBeenCalledWith(expect.anything(), 20);
         expect(load_folder).not.toHaveBeenCalled();
     });
+
+    it(`Given route is updated to "folder" and given folder has changed (=> redirection into a folder)
+        Then the folder is loaded`, async () => {
+        state.current_folder = { id: 10, title: "current folder" };
+        await router.push({
+            name: "preview",
+            params: {
+                preview_item_id: 10,
+            },
+        });
+        factory();
+        await router.push({
+            name: "folder",
+            params: {
+                item_id: 20,
+            },
+        });
+        expect(remove_quick_look).toHaveBeenCalled();
+        expect(load_folder).toHaveBeenCalledWith(expect.anything(), 20);
+    });
 });