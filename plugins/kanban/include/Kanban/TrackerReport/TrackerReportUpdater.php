Enalean/tuleap

commit daad4ac015f62487ee9560671ab3a2e3965bc718

refactor: move "all" kanban files to kanban
The remaining kanban files that are still in agiledashboard are:
* db upgrade scripts
* kanban dedicated mustache files for a.d homepage

Those don't need to be moved.

No functional changes.

Part of request #32645: Extract kanban from agiledashboard

Change-Id: I79b5491c96af2f4d45b436f6dbaf59de3490f402

plugins/kanban/include/Kanban/TrackerReport/TrackerReportUpdater.php
@@ -22,29 +22,25 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace Tuleap\AgileDashboard\Kanban\TrackerReport;
+declare(strict_types=1);
+
+namespace Tuleap\Kanban\TrackerReport;
 
 use Tuleap\Kanban\Kanban;
 use Tracker_Report;
 
 class TrackerReportUpdater
 {
-    /**
-     * @var TrackerReportDao
-     */
-    private $dao;
-
-    public function __construct(TrackerReportDao $dao)
+    public function __construct(private readonly TrackerReportDao $dao)
     {
-        $this->dao = $dao;
     }
 
-    public function save(Kanban $kanban, array $tracker_report_ids)
+    public function save(Kanban $kanban, array $tracker_report_ids): void
     {
         $this->dao->save($kanban->getId(), $tracker_report_ids);
     }
 
-    public function deleteAllForReport(Tracker_Report $report)
+    public function deleteAllForReport(Tracker_Report $report): void
     {
         $this->dao->deleteAllForReport($report->getId());
     }

plugins/agiledashboard/Makefile
@@ -3,5 +3,3 @@ SHELL=/usr/bin/env bash
 generate-templates:
 	xsltproc $(CURDIR)/../../tools/utils/setup_templates/generate-templates/generate-scrum_dashboard.xml \
 		-o resources/templates/scrum_dashboard_template.xml
-	cp -f $(CURDIR)/../../tools/utils/setup_templates/generate-templates/trackers/activity.xml \
-		resources/templates/Tracker_activity.xml

plugins/agiledashboard/include/AgileDashboard/AdminController.class.php
@@ -20,15 +20,15 @@
 
 namespace Tuleap\AgileDashboard;
 
-use AdminKanbanPresenter;
+use Tuleap\Kanban\AdminKanbanPresenter;
 use AgileDashboard_BacklogItemDao;
 use AgileDashboard_ConfigurationManager;
-use AgileDashboard_FirstKanbanCreator;
+use Tuleap\Kanban\FirstKanbanCreator;
 use AgileDashboard_FirstScrumCreator;
 use Tuleap\Kanban\KanbanFactory;
-use AgileDashboard_KanbanManager;
+use Tuleap\Kanban\KanbanManager;
 use AgileDashboardConfigurationResponse;
-use AgileDashboardKanbanConfigurationUpdater;
+use Tuleap\Kanban\KanbanConfigurationUpdater;
 use AgileDashboardScrumConfigurationUpdater;
 use Codendi_Request;
 use CSRFSynchronizerToken;
@@ -54,8 +54,8 @@ use Tuleap\AgileDashboard\ExplicitBacklog\UnplannedArtifactsAdder;
 use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsModeChecker;
 use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsModeUpdater;
 use Tuleap\AgileDashboard\FormElement\Burnup\ProjectsCountModeDao;
-use Tuleap\AgileDashboard\Kanban\TrackerReport\TrackerReportDao;
-use Tuleap\AgileDashboard\Kanban\TrackerReport\TrackerReportUpdater;
+use Tuleap\Kanban\TrackerReport\TrackerReportDao;
+use Tuleap\Kanban\TrackerReport\TrackerReportUpdater;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneChecker;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneDao;
 use Tuleap\AgileDashboard\MonoMilestone\ScrumForMonoMilestoneDisabler;
@@ -78,7 +78,7 @@ class AdminController extends BaseController
     /** @var PlanningFactory */
     private $planning_factory;
 
-    /** @var AgileDashboard_KanbanManager */
+    /** @var KanbanManager */
     private $kanban_manager;
 
     /** @var AgileDashboard_ConfigurationManager */
@@ -116,7 +116,7 @@ class AdminController extends BaseController
     public function __construct(
         Codendi_Request $request,
         PlanningFactory $planning_factory,
-        AgileDashboard_KanbanManager $kanban_manager,
+        KanbanManager $kanban_manager,
         KanbanFactory $kanban_factory,
         AgileDashboard_ConfigurationManager $config_manager,
         TrackerFactory $tracker_factory,
@@ -241,11 +241,11 @@ class AdminController extends BaseController
         );
 
         if ($this->request->exist('activate-kanban')) {
-            $updater = new AgileDashboardKanbanConfigurationUpdater(
+            $updater = new KanbanConfigurationUpdater(
                 $this->request,
                 $this->config_manager,
                 $response,
-                new AgileDashboard_FirstKanbanCreator(
+                new FirstKanbanCreator(
                     $this->request->getProject(),
                     $this->kanban_manager,
                     $this->tracker_factory,

plugins/agiledashboard/include/AgileDashboard/AdminKanbanPresenter.class.php
@@ -1,85 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2012-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-class AdminKanbanPresenter
-{
-    /** @var int */
-    public $group_id;
-
-    /** @var bool */
-    public $kanban_activated;
-
-    /** @var bool */
-    public $has_kanban;
-
-    /**
-     * @var bool
-     * @psalm-readonly
-     */
-    public $is_scrum_accessible;
-
-    public function __construct(
-        $group_id,
-        $kanban_activated,
-        $has_kanban,
-        bool $is_scrum_accessible,
-    ) {
-        $this->group_id            = $group_id;
-        $this->kanban_activated    = $kanban_activated;
-        $this->has_kanban          = $has_kanban;
-        $this->is_scrum_accessible = $is_scrum_accessible;
-    }
-
-    public function config_submit_label()
-    {
-        return dgettext('tuleap-agiledashboard', 'Save');
-    }
-
-    public function general_settings_section()
-    {
-        return dgettext('tuleap-agiledashboard', 'General settings');
-    }
-
-    public function activate_kanban_label()
-    {
-        return dgettext('tuleap-agiledashboard', 'Activate Kanban');
-    }
-
-    public function kanban_activated_label()
-    {
-        return dgettext('tuleap-agiledashboard', 'Kanban is currently active.');
-    }
-
-    public function kanban_not_activated_label()
-    {
-        return dgettext('tuleap-agiledashboard', 'Kanban is not currently active.');
-    }
-
-    public function first_kanban_will_be_created()
-    {
-        return dgettext('tuleap-agiledashboard', 'A first Kanban will be created during the activation. This operation can take a few seconds.');
-    }
-
-    public function token()
-    {
-        $token = new CSRFSynchronizerToken('/plugins/agiledashboard/?action=admin');
-        return $token->fetchHTMLInput();
-    }
-}

plugins/agiledashboard/include/AgileDashboard/AgileDashboardLegacyController.php
@@ -26,7 +26,7 @@ namespace Tuleap\AgileDashboard;
 use AgileDashboardRouterBuilder;
 use Feedback;
 use HTTPRequest;
-use Tuleap\AgileDashboard\Kanban\KanbanURL;
+use Tuleap\Kanban\KanbanURL;
 use Tuleap\AgileDashboard\Milestone\Pane\Details\DetailsPaneInfo;
 use Tuleap\AgileDashboard\Planning\PlanningJavascriptDependenciesProvider;
 use Tuleap\Kanban\KanbanJavascriptDependenciesProvider;

plugins/agiledashboard/include/AgileDashboard/FirstKanbanCreator.php
@@ -1,172 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2015-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-use Tuleap\AgileDashboard\Kanban\TrackerReport\TrackerReportUpdater;
-use Tuleap\Kanban\Kanban;
-use Tuleap\Kanban\KanbanFactory;
-
-class AgileDashboard_FirstKanbanCreator
-{
-    public const ASSIGNED_TO_ME_REPORT = "Assigned to me";
-
-    /** @var Project */
-    private $project;
-
-    /** @var AgileDashboard_KanbanManager */
-    private $kanban_manager;
-
-    /** @var TrackerFactory */
-    private $tracker_factory;
-
-    /** @var TrackerXmlImport */
-    private $xml_import;
-
-    /** @var string */
-    private $template_path;
-
-    /** @var string */
-    private $tracker_itemname;
-
-    /**
-     * @var TrackerReportUpdater
-     */
-    private $tracker_report_updater;
-
-    /**
-     * @var KanbanFactory
-     */
-    private $kanban_factory;
-
-    /**
-     * @var Tracker_ReportFactory
-     */
-    private $report_factory;
-
-    public function __construct(
-        Project $project,
-        AgileDashboard_KanbanManager $kanban_manager,
-        TrackerFactory $tracker_factory,
-        TrackerXmlImport $xml_import,
-        KanbanFactory $kanban_factory,
-        TrackerReportUpdater $tracker_report_updater,
-        Tracker_ReportFactory $report_factory,
-    ) {
-        $this->project                = $project;
-        $this->kanban_manager         = $kanban_manager;
-        $this->tracker_factory        = $tracker_factory;
-        $this->xml_import             = $xml_import;
-        $this->template_path          = __DIR__ . '/../../resources/templates/Tracker_activity.xml';
-        $this->tracker_itemname       = $this->xml_import->getTrackerItemNameFromXMLFile($this->template_path);
-        $this->tracker_report_updater = $tracker_report_updater;
-        $this->kanban_factory         = $kanban_factory;
-        $this->report_factory         = $report_factory;
-    }
-
-    public function createFirstKanban(PFUser $user)
-    {
-        if (! $this->isFirstKanbanNeeded()) {
-            return;
-        }
-
-        if ($this->isTrackerAlreadyCreated()) {
-            $this->warn(
-                sprintf(dgettext('tuleap-agiledashboard', 'We tried to create a first kanban for you but an existing tracker (%1$s) prevented it.'), $this->tracker_itemname)
-            );
-            return;
-        }
-
-        $tracker = $this->importTrackerStructure();
-        if (! $tracker) {
-            $this->warn(dgettext('tuleap-agiledashboard', 'We tried to create a first kanban for you but an internal error prevented it.'));
-            return;
-        }
-
-        $kanban_id = $this->kanban_manager->createKanban($tracker->getName(), $tracker->getId());
-        if (! $kanban_id) {
-            $this->warn(dgettext('tuleap-agiledashboard', 'We tried to create a first kanban for you but an internal error prevented it.'));
-            return;
-        }
-
-        $kanban = $this->kanban_factory->getKanban($user, $kanban_id);
-        $this->addAssignedToMeReport($tracker, $kanban);
-
-        $GLOBALS['Response']->addFeedback(
-            Feedback::INFO,
-            sprintf(dgettext('tuleap-agiledashboard', 'We created <a href="%1$s">a first kanban</a> for you. Enjoy!'), '?' . http_build_query(
-                [
-                    'group_id' => $this->project->getId(),
-                    'action'   => 'showKanban',
-                    'id'       => $kanban_id,
-                ]
-            )),
-            CODENDI_PURIFIER_DISABLED
-        );
-    }
-
-    private function warn($message)
-    {
-        $GLOBALS['Response']->addFeedback(Feedback::WARN, $message);
-    }
-
-    /** @return Tracker|null */
-    private function importTrackerStructure()
-    {
-        try {
-            return $this->xml_import->createFromXMLFile($this->project, $this->template_path);
-        } catch (Exception $exception) {
-            $logger = BackendLogger::getDefaultLogger();
-            $logger->error('Unable to create first kanban for ' . $this->project->getId() . ': ' . $exception->getMessage());
-            return;
-        }
-    }
-
-    private function isFirstKanbanNeeded()
-    {
-        $used_trackers = $this->kanban_manager->getTrackersUsedAsKanban($this->project);
-
-        return count($used_trackers) === 0;
-    }
-
-    private function isTrackerAlreadyCreated()
-    {
-        $is_tracker_already_created = $this->tracker_factory->isShortNameExists(
-            $this->tracker_itemname,
-            $this->project->getId()
-        );
-
-        return $is_tracker_already_created;
-    }
-
-    private function addAssignedToMeReport(Tracker $tracker, Kanban $kanban)
-    {
-        $reports = $this->report_factory->getReportsByTrackerId(
-            $tracker->getId(),
-            null
-        );
-
-        foreach ($reports as $tracker_report) {
-            if ($tracker_report->isPublic() && $tracker_report->getName() === self::ASSIGNED_TO_ME_REPORT) {
-                $this->tracker_report_updater->save($kanban, [(int) $tracker_report->getId()]);
-
-                return;
-            }
-        }
-    }
-}

plugins/agiledashboard/include/AgileDashboard/KanbanItemManager.class.php
@@ -1,87 +1,0 @@
-<?php
-/**
- * Copyright (c) Enalean, 2015 - Present. All Rights Reserved.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
-use Tuleap\Kanban\KanbanItemDao;
-use Tuleap\Tracker\Artifact\Artifact;
-
-class AgileDashboard_KanbanItemManager
-{
-    /**
-     * @var KanbanItemDao
-     */
-    private $item_dao;
-
-    public function __construct(KanbanItemDao $item_dao)
-    {
-        $this->item_dao = $item_dao;
-    }
-
-    public function isKanbanItemInBacklog(Artifact $artifact)
-    {
-        return count($this->item_dao->isKanbanItemInBacklog($artifact->getTrackerId(), $artifact->getId())) > 0;
-    }
-
-    public function isKanbanItemInArchive(Artifact $artifact)
-    {
-        return count($this->item_dao->isKanbanItemInArchive($artifact->getTrackerId(), $artifact->getId())) > 0;
-    }
-
-    public function getColumnIdOfKanbanItem(Artifact $artifact)
-    {
-        return $this->item_dao->getColumnIdOfKanbanItem($artifact->getTrackerId(), $artifact->getId());
-    }
-
-    public function getKanbanItemIndexInBacklog(Artifact $artifact)
-    {
-        $column_item_ids = [];
-        foreach ($this->item_dao->getKanbanBacklogItemIds($artifact->getTrackerId()) as $row) {
-            array_push($column_item_ids, $row['id']);
-        }
-        return array_search($artifact->getId(), $column_item_ids);
-    }
-
-    public function getKanbanItemIndexInArchive(Artifact $artifact)
-    {
-        $column_item_ids = [];
-        foreach ($this->item_dao->getKanbanArchiveItemIds($artifact->getTrackerId()) as $row) {
-            array_push($column_item_ids, $row['id']);
-        }
-        return array_search($artifact->getId(), $column_item_ids);
-    }
-
-    public function getKanbanItemIndexInColumn(Artifact $artifact, $column)
-    {
-        $column_item_ids = [];
-        foreach ($this->item_dao->getItemsInColumn($artifact->getTrackerId(), $column) as $row) {
-            array_push($column_item_ids, $row['id']);
-        }
-        return array_search($artifact->getId(), $column_item_ids);
-    }
-
-    public function getIndexOfKanbanItem(Artifact $artifact, $column)
-    {
-        if ($column === 'backlog') {
-            return $this->getKanbanItemIndexInBacklog($artifact);
-        } elseif ($column === 'archive') {
-            return $this->getKanbanItemIndexInArchive($artifact);
-        } else {
-            return $this->getKanbanItemIndexInColumn($artifact, $column);
-        }
-    }
-}

plugins/agiledashboard/include/AgileDashboardRouter.class.php
@@ -30,7 +30,8 @@ use Tuleap\AgileDashboard\ExplicitBacklog\XMLImporter;
 use Tuleap\AgileDashboard\FormElement\Burnup\CountElementsModeChecker;
 use Tuleap\AgileDashboard\FormElement\BurnupCacheGenerator;
 use Tuleap\AgileDashboard\FormElement\FormElementController;
-use Tuleap\AgileDashboard\Kanban\BreadCrumbBuilder;
+use Tuleap\Kanban\BreadCrumbBuilder;
+use Tuleap\Kanban\KanbanManager;
 use Tuleap\Kanban\NewDropdown\NewDropdownCurrentContextSectionForKanbanProvider;
 use Tuleap\Kanban\KanbanCannotAccessException;
 use Tuleap\Kanban\KanbanNotFoundException;
@@ -103,7 +104,7 @@ class AgileDashboardRouter
      */
     private $xml_exporter;
 
-    /** @var AgileDashboard_KanbanManager */
+    /** @var KanbanManager */
     private $kanban_manager;
 
     /** @var AgileDashboard_ConfigurationManager */
@@ -194,7 +195,7 @@ class AgileDashboardRouter
         Planning_MilestoneControllerFactory $milestone_controller_factory,
         ProjectManager $project_manager,
         AgileDashboard_XMLFullStructureExporter $xml_exporter,
-        AgileDashboard_KanbanManager $kanban_manager,
+        KanbanManager $kanban_manager,
         AgileDashboard_ConfigurationManager $config_manager,
         KanbanFactory $kanban_factory,
         PlanningPermissionsManager $planning_permissions_manager,

commit 3b16904e9792fc0ae0120e1e8ecf869fa89618dc

Fixes crash when deleting a tracker report
Introduced by daad4ac015f62487ee9560671ab3a2e3965bc718.

Part of request #32645: Extract kanban from agiledashboard

Change-Id: Ie764227bf6d1384c9204b4f24bf608556b3d5c1e

plugins/kanban/include/Kanban/TrackerReport/TrackerReportUpdater.php
@@ -42,6 +42,6 @@ class TrackerReportUpdater
 
     public function deleteAllForReport(Tracker_Report $report): void
     {
-        $this->dao->deleteAllForReport($report->getId());
+        $this->dao->deleteAllForReport((int) $report->getId());
     }
 }