f4373d7115aa2533391794511e82f205e5374219
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2023-08-16 11:41:07 +0200

    Core user preferences are no longer player
    part of request #32636 project notifications test can be covered with cypress
    
    following 08de4a6f1135771ce2940beb81296c7222b87dc7
    
    When feature flag is set, the field purpose is displayed when a user register
    this is an expected behaviour, so the test have been updated
    and request #33974 will fix the issue
    
    Change-Id: I9474bff33b67ece0da20964ac3ea8a0a02662eba

--- tests/e2e/full/tuleap/setup.sh
+++ tests/e2e/full/tuleap/setup.sh
@@ -53,6 +53,7 @@ enable_plugins() {
 
     sed -i -e 's#/var/lib/codendi#/var/lib/tuleap#g' /etc/tuleap/plugins/docman/etc/docman.inc
     sudo -u codendiadm /usr/bin/tuleap config-set feature_flag_activate_split_kanban_for_project 1
+    sudo -u codendiadm /usr/bin/tuleap config-set sys_user_approval 1
 }
 
 load_project() {
