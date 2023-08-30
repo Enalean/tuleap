4aedf21ffb6ba1ff2fccd95279b8835f01784a17
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-08-30 17:04:26 +0200

    Closes request #34003 pnpm: 8.6.12 -> 8.7.0
    Changelog:
    https://github.com/pnpm/pnpm/releases/tag/v8.7.0
    
    Change-Id: I46ccdd7a2c411363791f67618ce54bedc99be231

--- tools/utils/nix/build-tools/pnpm/node-packages.nix
+++ tools/utils/nix/build-tools/pnpm/node-packages.nix
@@ -9,10 +9,10 @@ in
   "pnpm-^8" = nodeEnv.buildNodePackage {
     name = "pnpm";
     packageName = "pnpm";
-    version = "8.6.12";
+    version = "8.7.0";
     src = fetchurl {
-      url = "https://registry.npmjs.org/pnpm/-/pnpm-8.6.12.tgz";
-      sha512 = "Eza4C5SO/Xl5IYozupbZ5NOA5leBRPYxmXmXfe7G4/4uCkRLhks84rB33aitxNZU/uMrnDGGjwrLktoKvPjqHA==";
+      url = "https://registry.npmjs.org/pnpm/-/pnpm-8.7.0.tgz";
+      sha512 = "HWH4wQ6KWl2/vd6g8fXvt9vVF3IjBzrslTzyMKpGQWiEuUJ6ZCHbp48orQ+T++3ji6VwgyN7NQJD3mseOoznHQ==";
     };
     buildInputs = globalBuildInputs;
     meta = {
