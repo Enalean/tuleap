3fb800677b5ce56584ab4ca543c2dd28e10de27c
Clarck Robinson <clarck.robinson@enalean.com>
2023-09-04 12:07:36 +0200

    Rename the `Top Planning Backlog`/`Top Backlog` to `Backlog` in Masschange
    Part of #story #33979 have dedicated Backlog service
    
    To see the change you have to make a masschange in Tracker:
     - `Top backlog` mention has been renamed to `Backlog`
     - Now the term `Backlog` becomes `Backlog` in French instead of
      `Plannification général`
    
    This patch take in account the feature flag value, so if you want to see
    the change you have to enable the feature flag:
    `tuleap config-set feature_flag_activate_split_kanban_for_project 1`
    
    Note: I take the opportunity to remove the usage of Mockery in the test
    
    Change-Id: I28ca69fd6a77f29f2832bb781953e968e581eb9f

--- plugins/agiledashboard/tests/unit/AgileDashboard/Masschange/AdditionalMasschangeActionBuilderTest.php
+++ plugins/agiledashboard/tests/unit/AgileDashboard/Masschange/AdditionalMasschangeActionBuilderTest.php
@@ -22,201 +22,154 @@ declare(strict_types=1);
 
 namespace Tuleap\AgileDashboard\Masschange;
 
-use Mockery;
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use PFUser;
+use PHPUnit\Framework\MockObject\MockObject;
 use Planning;
-use PlanningFactory;
-use Project;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use TemplateRenderer;
 use Tracker;
 use Tuleap\AgileDashboard\BlockScrumAccess;
-use Tuleap\AgileDashboard\ExplicitBacklog\ExplicitBacklogDao;
+use Tuleap\AgileDashboard\Stub\VerifyProjectUsesExplicitBacklogStub;
+use Tuleap\AgileDashboard\Test\Builders\PlanningBuilder;
+use Tuleap\Kanban\CheckSplitKanbanConfiguration;
+use Tuleap\ProgramManagement\Tests\Stub\RetrieveRootPlanningStub;
+use Tuleap\Test\Builders\ProjectTestBuilder;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Test\Stubs\EventDispatcherStub;
+use Tuleap\Test\Stubs\TemplateRendererStub;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
 
-final class AdditionalMasschangeActionBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
+final class AdditionalMasschangeActionBuilderTest extends TestCase
 {
-    use MockeryPHPUnitIntegration;
+    private AdditionalMasschangeActionBuilder $builder;
 
-    /**
-     * @var AdditionalMasschangeActionBuilder
-     */
-    private $builder;
+    private VerifyProjectUsesExplicitBacklogStub $explicit_backlog_dao;
 
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|ExplicitBacklogDao
-     */
-    private $explicit_backlog_dao;
+    private RetrieveRootPlanningStub $planning_factory;
 
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PlanningFactory
-     */
-    private $planning_factory;
+    private TemplateRenderer $template_renderer;
 
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|TemplateRenderer
-     */
-    private $template_renderer;
+    private EventDispatcherInterface $event_dispatcher;
 
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|EventDispatcherInterface
-     */
-    private $event_dispatcher;
+    private Tracker|MockObject $tracker;
 
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Tracker
-     */
-    private $tracker;
-
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|PFUser
-     */
-    private $user;
-    /**
-     * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Planning
-     */
-    private $root_planning;
+    private Planning $root_planning;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $this->explicit_backlog_dao = Mockery::mock(ExplicitBacklogDao::class);
-        $this->planning_factory     = Mockery::mock(PlanningFactory::class);
-        $this->template_renderer    = Mockery::mock(TemplateRenderer::class);
-        $this->event_dispatcher     = Mockery::mock(EventDispatcherInterface::class);
+        $this->explicit_backlog_dao = VerifyProjectUsesExplicitBacklogStub::withAlwaysUsesExplicitBacklog();
+        $this->template_renderer    = new TemplateRendererStub();
+        $this->event_dispatcher     = EventDispatcherStub::withIdentityCallback();
 
+        $project = ProjectTestBuilder::aProject()->withId(101)->build();
+
+        $this->tracker = $this->createMock(Tracker::class);
+        $this->tracker
+            ->method('getProject')
+            ->willReturn($project);
+        $this->tracker
+            ->method('getId')
+            ->willReturn(149);
+
+        $this->root_planning = PlanningBuilder::aPlanning(101)->withBacklogTrackers($this->tracker)->build();
+
+        $this->planning_factory = RetrieveRootPlanningStub::withProjectAndPlanning(101, $this->root_planning);
+    }
+
+    private function buildMasschangeAction(): ?string
+    {
         $this->builder = new AdditionalMasschangeActionBuilder(
             $this->explicit_backlog_dao,
             $this->planning_factory,
             $this->template_renderer,
-            $this->event_dispatcher
+            $this->event_dispatcher,
+            new CheckSplitKanbanConfiguration()
         );
-
-        $this->tracker = Mockery::mock(Tracker::class);
-        $this->user    = Mockery::mock(PFUser::class);
-
-        $project = Mockery::mock(Project::class)->shouldReceive('getID')->andReturn('101')->getMock();
-        $this->tracker->shouldReceive('getProject')->andReturn($project);
-        $this->tracker->shouldReceive('getId')->andReturn('149');
-
-        $this->root_planning = Mockery::mock(Planning::class);
+        $user          = UserTestBuilder::buildWithDefaults();
+        return $this->builder->buildMasschangeAction($this->tracker, $user);
     }
 
     public function testItRendersTheMasschangeAdditionalAction(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(true);
 
-        $this->event_dispatcher->shouldReceive('dispatch');
-
-        $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
-            ->with(101)
-            ->once()
-            ->andReturnTrue();
-
-        $this->planning_factory->shouldReceive('getRootPlanning')
-            ->once()
-            ->with($this->user, 101)
-            ->andReturn($this->root_planning);
-
-        $this->root_planning->shouldReceive('getBacklogTrackersIds')
-            ->once()
-            ->andReturn([149]);
-
-        $this->template_renderer->shouldReceive('renderToString')->once()->andReturn('whatever');
-
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
 
         $this->assertNotNull($additional_action);
     }
 
     public function testItReturnsNullIfUserIsNotTrackerAdmin(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnFalse();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(false);
 
-        $this->template_renderer->shouldNotReceive('renderToString');
-
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
         $this->assertNull($additional_action);
     }
 
     public function testItReturnsNullIfProjectDoesNotUseExplicitBacklog(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(true);
 
-        $this->event_dispatcher->shouldReceive('dispatch');
+        $this->explicit_backlog_dao = VerifyProjectUsesExplicitBacklogStub::withNeverUsesExplicitBacklog();
 
-        $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
-            ->with(101)
-            ->once()
-            ->andReturnFalse();
-
-        $this->template_renderer->shouldNotReceive('renderToString');
-
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
         $this->assertNull($additional_action);
     }
 
     public function testItReturnsNullIfProjectDoesNotHaveARootPlanning(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(true);
 
-        $this->event_dispatcher->shouldReceive('dispatch');
+        $this->planning_factory =  RetrieveRootPlanningStub::withProjectAndPlanning(1848, $this->root_planning);
 
-        $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
-            ->with(101)
-            ->once()
-            ->andReturnTrue();
-
-        $this->planning_factory->shouldReceive('getRootPlanning')
-            ->once()
-            ->with($this->user, 101)
-            ->andReturnFalse();
-
-        $this->template_renderer->shouldNotReceive('renderToString');
-
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
         $this->assertNull($additional_action);
     }
 
     public function testItReturnsNullIfTrackerNotABacklogTracker(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(true);
 
-        $this->event_dispatcher->shouldReceive('dispatch');
+        $not_backlog_tracker    = TrackerTestBuilder::aTracker()->withId(172)->build();
+        $this->root_planning    = PlanningBuilder::aPlanning(101)->withBacklogTrackers($not_backlog_tracker)->build();
+        $this->planning_factory = RetrieveRootPlanningStub::withProjectAndPlanning(101, $this->root_planning);
 
-        $this->explicit_backlog_dao->shouldReceive('isProjectUsingExplicitBacklog')
-            ->with(101)
-            ->once()
-            ->andReturnTrue();
-
-        $this->planning_factory->shouldReceive('getRootPlanning')
-            ->once()
-            ->with($this->user, 101)
-            ->andReturn($this->root_planning);
-
-        $this->root_planning->shouldReceive('getBacklogTrackersIds')
-            ->once()
-            ->andReturn([172]);
-
-        $this->template_renderer->shouldNotReceive('renderToString');
-
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
         $this->assertNull($additional_action);
     }
 
     public function testReturnsNullWhenScrumAccessIsBlocked(): void
     {
-        $this->tracker->shouldReceive('userIsAdmin')->once()->andReturnTrue();
+        $this->tracker
+            ->expects(self::once())
+            ->method('userIsAdmin')
+            ->willReturn(true);
 
-        $this->event_dispatcher->shouldReceive('dispatch')->andReturnUsing(function (object $event) {
+        $this->event_dispatcher = EventDispatcherStub::withCallback(function (object $event) {
             if ($event instanceof BlockScrumAccess) {
                 $event->disableScrumAccess();
             }
             return $event;
         });
 
-        $additional_action = $this->builder->buildMasschangeAction($this->tracker, $this->user);
+        $additional_action = $this->buildMasschangeAction();
 
         self::assertNull($additional_action);
     }
