Enalean/tuleap

commit c5fd16af111104c9c066f7362d5590ac471c132d

Closes request #33997: Drop support of CVS
Some restrictions regarding the name of users or projects have been kept
"just in case" (and also because they are unlikely to be an issue for
real world use cases).

Note that CVS was the last feature relying on having local unix accounts
for users. The removal of multiple pieces related to the managed of that
will happen in dedicated contributions as it requires a more gentle
touch.

Change-Id: Ia1c6d430e6a581984721c567975b1b1501c12a3f

src/common/Project/Admin/ProjectDetailsPresenter.php
@@ -70,7 +70,6 @@ class ProjectDetailsPresenter
     public $is_system;
     public $is_active;
     public $is_status_invalid;
-    public $is_suspended_status;
     public $plugin_suspended_and_not_blocked_warnings;
     /**
      * @var ProjectAccessPresenter
@@ -125,10 +124,9 @@ class ProjectDetailsPresenter
             ['project' => $project, 'links' => &$this->links]
         );
 
-        $this->status              = $this->getStatus($project);
-        $this->can_change_status   = $project->getStatus() === Project::STATUS_DELETED;
-        $this->is_suspended_status = $project->getStatus() === Project::STATUS_SUSPENDED;
-        $this->types               = $this->getTypes($project);
+        $this->status            = $this->getStatus($project);
+        $this->can_change_status = $project->getStatus() === Project::STATUS_DELETED;
+        $this->types             = $this->getTypes($project);
 
         $template_factory = TemplateFactory::build();
         $xml_template     = $template_factory->getTemplateForProject($project);

src/templates/admin/projects/project-info.mustache
@@ -10,20 +10,17 @@
         {{/ is_active }}
     </nav>
 
-    {{# is_suspended_status }}
+    {{^ plugin_suspended_and_not_blocked_warnings }}
         <section class="tlp-alert-warning site-admin-project-is-supended-warning">
             {{# gettext }}This project is suspended. Only site administrators may access it.
                 However, the following services are not blocked and may still be used:{{/ gettext }}
             <ul>
-                <li>{{# gettext }}Mailing lists (Lists Service){{/ gettext }}</li>
-                <li>{{# gettext }}Project web pages (Home Page Service){{/ gettext }}</li>
-                <li>{{# gettext }}CVS{{/ gettext }}</li>
                 {{# plugin_suspended_and_not_blocked_warnings}}
                     <li>{{ . }}</li>
                 {{/ plugin_suspended_and_not_blocked_warnings }}
             </ul>
         </section>
-    {{/ is_suspended_status }}
+    {{/ plugin_suspended_and_not_blocked_warnings }}
 
     <section class="tlp-framed-horizontally" id="siteadmin-project-information">
         <div id="siteadmin-project-information-details">

commit fb55a3a460ff05120ade7f02ecc7dcdee8b663d5

Site admin warnings about accessible services in suspendend projects should only be displayed for suspended services
Issue was introduced in c5fd16af111104c9c066f7362d5590ac471c132d.
The warning was displayed for non suspended projects.

Took the opportunity to fix the class name and to improve a bit the
types around the impacted code.

Part of request #33997: Drop support of CVS

Change-Id: I8828ec15807ba43f084586e328c27143998c953a

src/common/Project/Admin/ProjectDetailsPresenter.php
@@ -70,7 +70,7 @@ class ProjectDetailsPresenter
     public $is_system;
     public $is_active;
     public $is_status_invalid;
-    public $plugin_suspended_and_not_blocked_warnings;
+    public readonly bool $has_suspension_warnings_to_display;
     /**
      * @var ProjectAccessPresenter
      */
@@ -94,12 +94,15 @@ class ProjectDetailsPresenter
      */
     public $not_editable_reason;
 
+    /**
+     * @param list<string> $plugin_suspended_and_not_blocked_warnings
+     */
     public function __construct(
         Project $project,
         $all_custom_fields,
         ProjectAccessPresenter $access_presenter,
         \CSRFSynchronizerToken $csrf_token,
-        array $plugin_suspended_and_not_blocked_warnings,
+        public readonly array $plugin_suspended_and_not_blocked_warnings,
         bool $is_unix_name_editable,
         string $not_editable_reason,
     ) {
@@ -145,28 +148,28 @@ class ProjectDetailsPresenter
         $this->custom_fields     = $all_custom_fields;
         $this->has_custom_fields = count($this->custom_fields) > 0;
 
-        $this->information_label                         = $GLOBALS['Language']->getText('admin_project', 'information_label');
-        $this->history_label                             = $GLOBALS['Language']->getText('admin_project', 'history_label');
-        $this->project_details_label                     = $GLOBALS['Language']->getText('admin_project', 'project_details_label');
-        $this->access_label                              = $GLOBALS['Language']->getText('admin_project', 'access_label');
-        $this->description_label                         = $GLOBALS['Language']->getText('admin_project', 'description_label');
-        $this->more_label                                = $GLOBALS['Language']->getText('admin_project', 'more_label');
-        $this->homepage_label                            = $GLOBALS['Language']->getText('admin_project', 'homepage_label');
-        $this->admin_label                               = $GLOBALS['Language']->getText('admin_project', 'admin_label');
-        $this->members_label                             = $GLOBALS['Language']->getText('admin_project', 'members_label');
-        $this->pending_label                             = $GLOBALS['Language']->getText('admin_project', 'pending_label');
-        $this->manage_access_label                       = $GLOBALS['Language']->getText('admin_project', 'manage_access_label');
-        $this->unix_name_label                           = $GLOBALS['Language']->getText('admin_project', 'unix_name_label');
-        $this->save_label                                = $GLOBALS['Language']->getText('admin_project', 'save_label');
-        $this->status_label                              = $GLOBALS['Language']->getText('admin_project', 'status_label');
-        $this->type_label                                = _('Project Type');
-        $this->built_from_label                          = _('Built from template');
-        $this->instructions_label                        = $GLOBALS['Language']->getText('admin_project', 'instructions_label');
-        $this->send_email_label                          = $GLOBALS['Language']->getText('admin_project', 'send_email_label');
-        $this->instructions_desc                         = $GLOBALS['Language']->getText('admin_project', 'instructions_desc');
-        $this->access_presenter                          = $access_presenter;
-        $this->csrf_token                                = $csrf_token;
-        $this->plugin_suspended_and_not_blocked_warnings = $plugin_suspended_and_not_blocked_warnings;
+        $this->information_label                  = $GLOBALS['Language']->getText('admin_project', 'information_label');
+        $this->history_label                      = $GLOBALS['Language']->getText('admin_project', 'history_label');
+        $this->project_details_label              = $GLOBALS['Language']->getText('admin_project', 'project_details_label');
+        $this->access_label                       = $GLOBALS['Language']->getText('admin_project', 'access_label');
+        $this->description_label                  = $GLOBALS['Language']->getText('admin_project', 'description_label');
+        $this->more_label                         = $GLOBALS['Language']->getText('admin_project', 'more_label');
+        $this->homepage_label                     = $GLOBALS['Language']->getText('admin_project', 'homepage_label');
+        $this->admin_label                        = $GLOBALS['Language']->getText('admin_project', 'admin_label');
+        $this->members_label                      = $GLOBALS['Language']->getText('admin_project', 'members_label');
+        $this->pending_label                      = $GLOBALS['Language']->getText('admin_project', 'pending_label');
+        $this->manage_access_label                = $GLOBALS['Language']->getText('admin_project', 'manage_access_label');
+        $this->unix_name_label                    = $GLOBALS['Language']->getText('admin_project', 'unix_name_label');
+        $this->save_label                         = $GLOBALS['Language']->getText('admin_project', 'save_label');
+        $this->status_label                       = $GLOBALS['Language']->getText('admin_project', 'status_label');
+        $this->type_label                         = _('Project Type');
+        $this->built_from_label                   = _('Built from template');
+        $this->instructions_label                 = $GLOBALS['Language']->getText('admin_project', 'instructions_label');
+        $this->send_email_label                   = $GLOBALS['Language']->getText('admin_project', 'send_email_label');
+        $this->instructions_desc                  = $GLOBALS['Language']->getText('admin_project', 'instructions_desc');
+        $this->access_presenter                   = $access_presenter;
+        $this->csrf_token                         = $csrf_token;
+        $this->has_suspension_warnings_to_display = $project->getStatus() === Project::STATUS_SUSPENDED && count($this->plugin_suspended_and_not_blocked_warnings) > 0;
     }
 
     private function getTypes(Project $project): array

src/common/Project/Status/ProjectSuspendedAndNotBlockedWarningCollector.php
@@ -28,15 +28,15 @@ class ProjectSuspendedAndNotBlockedWarningCollector implements Dispatchable
 
     /** @var \Project */
     private $project;
-    /** @var string[] */
-    private $warnings = [];
+    /** @var list<string> */
+    private array $warnings = [];
 
     public function __construct(\Project $project)
     {
         $this->project = $project;
     }
 
-    public function addWarning($warning)
+    public function addWarning(string $warning): void
     {
         $this->warnings[] = $warning;
     }
@@ -50,9 +50,9 @@ class ProjectSuspendedAndNotBlockedWarningCollector implements Dispatchable
     }
 
     /**
-     * @return string[]
+     * @return list<string>
      */
-    public function getWarnings()
+    public function getWarnings(): array
     {
         return $this->warnings;
     }

src/templates/admin/projects/project-info.mustache
@@ -10,8 +10,8 @@
         {{/ is_active }}
     </nav>
 
-    {{^ plugin_suspended_and_not_blocked_warnings }}
-        <section class="tlp-alert-warning site-admin-project-is-supended-warning">
+    {{# has_suspension_warnings_to_display }}
+        <section class="tlp-alert-warning site-admin-project-is-suspended-warning">
             {{# gettext }}This project is suspended. Only site administrators may access it.
                 However, the following services are not blocked and may still be used:{{/ gettext }}
             <ul>
@@ -20,7 +20,7 @@
                 {{/ plugin_suspended_and_not_blocked_warnings }}
             </ul>
         </section>
-    {{/ plugin_suspended_and_not_blocked_warnings }}
+    {{/ has_suspension_warnings_to_display }}
 
     <section class="tlp-framed-horizontally" id="siteadmin-project-information">
         <div id="siteadmin-project-information-details">

src/themes/BurningParrot/css/includes/_siteadmin-project.scss
@@ -87,6 +87,6 @@
     }
 }
 
-.site-admin-project-is-supended-warning {
+.site-admin-project-is-suspended-warning {
     margin: var(--tlp-medium-spacing);
 }