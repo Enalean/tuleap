Enalean/tuleap

commit ea4002c1ccc919455fa6656bd8679c1a7f4b408d

refactor: regroup getCriteriaFrom and getCriteriaWhere
Since we have parametrized FromWhere at our disposal, we don't need
anymore to have distinctive calls to retrieve the from and the where of
a criterion.

We now have the different SQL fragment at the same place (mostly), this
avoid to go back and forth between the construction of the from and the
where to understand what's going on.

No functional changes

Note: Some issues have been fixed during development of this refactoring
(extra placeholder in Integer query, missing spread operator in Float
😭). This will probably have to be backported.

Closes request #32643: regroup getCriteriaFrom and getCriteriaWhere

Change-Id: Ic48c536e1cb0a1124fb3e043c5736bc6b5b7a33a

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind.php
@@ -25,8 +25,7 @@ use Tuleap\Tracker\FormElement\Field\ListFields\Bind\BoundDecoratorSaver;
 use Tuleap\Tracker\FormElement\Field\ListFields\Bind\BindVisitable;
 use Tuleap\Tracker\FormElement\Field\ListFields\Bind\BindDefaultValueDao;
 use Tuleap\Tracker\FormElement\Field\ListFields\Bind\BindDecoratorDao;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\REST\FieldValueRepresentation;
 
 /**
@@ -254,98 +253,66 @@ abstract class Tracker_FormElement_Field_List_Bind implements //phpcs:ignore PSR
      * Get the "from" statement to allow search with this field
      * You can join on 'c' which is a pseudo table used to retrieve
      * the last changeset of all artifacts.
+     *
      * @param array $criteria_value array of criteria_value (which are array)
-     * @return Option<ParametrizedFrom>
+     *
+     * @return Option<ParametrizedFromWhere>
      */
-    public function getCriteriaFrom($criteria_value): Option
+    public function getCriteriaFromWhere($criteria_value): Option
     {
         if (! $this->getField()->isUsed()) {
-            return Option::nothing(ParametrizedFrom::class);
+            return Option::nothing(ParametrizedFromWhere::class);
         }
 
         //Only filter query if criteria is valuated
-        if ($criteria_value) {
-            $a = 'A_' . $this->field->id;
-            $b = 'B_' . $this->field->id;
-            if ($this->isSearchingNone($criteria_value)) {
-                return Option::fromValue(
-                    new ParametrizedFrom(
-                        " LEFT JOIN (
-                            tracker_changeset_value AS $a
-                            INNER JOIN tracker_changeset_value_list AS $b ON (
-                                $b.changeset_value_id = $a.id
-                            )
-                        ) ON ($a.changeset_id = c.id
-                            AND $a.field_id = ?
-                        )",
-                        [$this->field->id]
-                    )
-                );
-            }
-
-            return Option::fromValue(
-                new ParametrizedFrom(
-                    " INNER JOIN tracker_changeset_value AS $a
-                             ON ($a.changeset_id = c.id
-                                 AND $a.field_id = ?
-                             )
-                             INNER JOIN tracker_changeset_value_list AS $b ON (
-                                $b.changeset_value_id = $a.id
-                             ) ",
-                    [$this->field->id]
-                )
-            );
+        if (! $criteria_value) {
+            return Option::nothing(ParametrizedFromWhere::class);
         }
 
-        return Option::nothing(ParametrizedFrom::class);
-    }
+        $a = 'A_' . $this->field->id;
+        $b = 'B_' . $this->field->id;
 
-    /**
-     * Get the "where" statement to allow search with this field
-     *
-     * @param array $criteria_value array of id => criteria_value (which are array)
-     *
-     * @return Option<ParametrizedSQLFragment>
-     * @see getCriteriaFrom
-     */
-    public function getCriteriaWhere($criteria_value): Option
-    {
-        if (! $this->getField()->isUsed()) {
-            return Option::nothing(ParametrizedSQLFragment::class);
-        }
-
-        //Only filter query if criteria is valuated
-        if ($criteria_value) {
-            $a = 'A_' . $this->field->id;
-            $b = 'B_' . $this->field->id;
-
-            if ($this->isSearchingNone($criteria_value)) {
-                $in = \ParagonIE\EasyDB\EasyStatement::open()->in('?*', array_values($criteria_value));
-
-                return Option::fromValue(
-                    new ParametrizedSQLFragment(
-                        "$b.bindvalue_id IN ($in) OR $b.bindvalue_id IS NULL",
-                        $in->values(),
-                    )
-                );
-            }
-
-            $ids_to_search = $this->getIdsToSearch($criteria_value);
-            if ($ids_to_search === '') {
-                return Option::nothing(ParametrizedSQLFragment::class);
-            }
-
-            $in = \ParagonIE\EasyDB\EasyStatement::open()->in('?*', $ids_to_search);
+        if ($this->isSearchingNone($criteria_value)) {
+            $in = \ParagonIE\EasyDB\EasyStatement::open()->in('?*', array_values($criteria_value));
 
             return Option::fromValue(
-                new ParametrizedSQLFragment(
-                    "$b.bindvalue_id IN ($in)",
+                new ParametrizedFromWhere(
+                    " LEFT JOIN (
+                        tracker_changeset_value AS $a
+                        INNER JOIN tracker_changeset_value_list AS $b ON (
+                            $b.changeset_value_id = $a.id
+                        )
+                    ) ON ($a.changeset_id = c.id
+                        AND $a.field_id = ?
+                    )",
+                    "$b.bindvalue_id IN ($in) OR $b.bindvalue_id IS NULL",
+                    [$this->field->id],
                     $in->values(),
                 )
             );
         }
 
-        return Option::nothing(ParametrizedSQLFragment::class);
+        $ids_to_search = $this->getIdsToSearch($criteria_value);
+        if ($ids_to_search === '') {
+            return Option::nothing(ParametrizedFromWhere::class);
+        }
+
+        $in = \ParagonIE\EasyDB\EasyStatement::open()->in('?*', $ids_to_search);
+
+        return Option::fromValue(
+            new ParametrizedFromWhere(
+                " INNER JOIN tracker_changeset_value AS $a
+                         ON ($a.changeset_id = c.id
+                             AND $a.field_id = ?
+                         )
+                         INNER JOIN tracker_changeset_value_list AS $b ON (
+                            $b.changeset_value_id = $a.id
+                         ) ",
+                "$b.bindvalue_id IN ($in)",
+                [$this->field->id],
+                $in->values(),
+            )
+        );
     }
 
     protected function getIdsToSearch($criteria_value)

plugins/agiledashboard/include/AgileDashboard/FormElement/Burnup.php
@@ -54,8 +54,7 @@ use Tuleap\Tracker\FormElement\ChartFieldUsage;
 use Tuleap\Tracker\FormElement\ChartMessageFetcher;
 use Tuleap\Tracker\FormElement\Field\File\CreatedFileURLMapping;
 use Tuleap\Tracker\FormElement\TrackerFormElementExternalField;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\REST\Artifact\ArtifactFieldValueFullRepresentation;
 use Tuleap\Tracker\Semantic\Status\Done\SemanticDoneDao;
 use Tuleap\Tracker\Semantic\Status\Done\SemanticDoneFactory;
@@ -250,14 +249,9 @@ class Burnup extends Tracker_FormElement_Field implements Tracker_FormElement_Fi
     {
     }
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
-        return Option::nothing(ParametrizedFrom::class);
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        return Option::nothing(ParametrizedSQLFragment::class);
+        return Option::nothing(ParametrizedFromWhere::class);
     }
 
     protected function getDao()

plugins/testmanagement/include/TestManagement/Step/Definition/Field/StepDefinition.php
@@ -49,8 +49,7 @@ use Tuleap\Tracker\FormElement\Field\File\FileURLSubstitutor;
 use Tuleap\Tracker\FormElement\FieldContentIndexer;
 use Tuleap\Tracker\FormElement\TrackerFormElementExternalField;
 use Tuleap\Tracker\FormElement\XML\XMLFormElement;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 
 class StepDefinition extends Tracker_FormElement_Field implements TrackerFormElementExternalField
 {
@@ -135,14 +134,9 @@ class StepDefinition extends Tracker_FormElement_Field implements TrackerFormEle
         return '';
     }
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
-        return Option::nothing(ParametrizedFrom::class);
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        return Option::nothing(ParametrizedSQLFragment::class);
+        return Option::nothing(ParametrizedFromWhere::class);
     }
 
     public function getQueryFrom(): string

plugins/testmanagement/include/TestManagement/Step/Execution/Field/StepExecution.php
@@ -34,8 +34,7 @@ use Tuleap\TestManagement\Step\StepPresenter;
 use Tuleap\Tracker\Artifact\Artifact;
 use Tuleap\Tracker\FormElement\Field\File\CreatedFileURLMapping;
 use Tuleap\Tracker\FormElement\TrackerFormElementExternalField;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 
 class StepExecution extends Tracker_FormElement_Field implements TrackerFormElementExternalField
 {
@@ -117,14 +116,9 @@ class StepExecution extends Tracker_FormElement_Field implements TrackerFormElem
         return '';
     }
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
-        return Option::nothing(ParametrizedFrom::class);
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        return Option::nothing(ParametrizedSQLFragment::class);
+        return Option::nothing(ParametrizedFromWhere::class);
     }
 
     public function getQuerySelect(): string

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.php
@@ -25,8 +25,7 @@ use Tuleap\Search\ItemToIndexQueue;
 use Tuleap\Tracker\Artifact\Artifact;
 use Tuleap\Tracker\FormElement\Field\File\CreatedFileURLMapping;
 use Tuleap\Tracker\FormElement\Field\XMLCriteriaValueCache;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\Rule\TrackerRulesDateValidator;
 use Tuleap\Tracker\Rule\TrackerRulesListValidator;
 use Tuleap\Tracker\Semantic\CollectionOfSemanticsUsingAParticularTrackerField;
@@ -91,18 +90,9 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
      * You can join on 'c' which is a pseudo table used to retrieve
      * the last changeset of all artifacts.
      *
-     * @return Option<ParametrizedFrom>
+     * @return Option<ParametrizedFromWhere>
      */
-    abstract public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option;
-
-    /**
-     * Get the "where" statement to allow search with this field
-     *
-     *
-     * @return Option<ParametrizedSQLFragment>
-     * @see getCriteriaFrom
-     */
-    abstract public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option;
+    abstract public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option;
 
     /**
      * Return the dao of the criteria value used with this field.

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactId.php
@@ -21,7 +21,7 @@
 
 use Tuleap\Option\Option;
 use Tuleap\Tracker\Artifact\Artifact;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
 
 // phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps
@@ -29,18 +29,19 @@ class Tracker_FormElement_Field_ArtifactId extends Tracker_FormElement_Field_Int
 {
     public $default_properties = [];
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
-        return Option::nothing(ParametrizedFrom::class);
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        if ($criteria_value = $this->getCriteriaValue($criteria)) {
-            return $this->buildMatchExpression("c.artifact_id", $criteria_value);
+        $criteria_value = $this->getCriteriaValue($criteria);
+        if (! $criteria_value) {
+            return Option::nothing(ParametrizedFromWhere::class);
         }
 
-        return Option::nothing(ParametrizedSQLFragment::class);
+        return $this->buildMatchExpression("c.artifact_id", $criteria_value)->mapOr(
+            static fn (ParametrizedSQLFragment $match) => Option::fromValue(
+                new ParametrizedFromWhere('', $match->sql, [], $match->parameters)
+            ),
+            Option::nothing(ParametrizedFromWhere::class)
+        );
     }
 
     public function getQuerySelect(): string

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_ArtifactLink.php
@@ -48,6 +48,7 @@ use Tuleap\Tracker\FormElement\Field\ArtifactLink\Type\TypePresenterFactory;
 use Tuleap\Tracker\FormElement\Field\ArtifactLink\Type\TypeTablePresenter;
 use Tuleap\Tracker\FormElement\Field\File\CreatedFileURLMapping;
 use Tuleap\Tracker\Report\Query\ParametrizedFrom;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
 
 #[ConfigKeyCategory('Tracker')]
@@ -314,34 +315,43 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field /
         return $link_ids;
     }
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
         //Only filter query if field is used
-        if ($this->isUsed()) {
-            //Only filter query if criteria is valuated
-            if ($criteria_value = $this->getCriteriaValue($criteria)) {
-                $a = 'A_' . $this->id;
-                $b = 'B_' . $this->id;
-
-                $match_expression = $this->buildMatchExpression("$b.artifact_id", $criteria_value);
-
-                return Option::fromValue(
-                    new ParametrizedFrom(
-                        " INNER JOIN tracker_changeset_value AS $a ON ($a.changeset_id = c.id AND $a.field_id = ? )
-                         INNER JOIN tracker_changeset_value_artifactlink AS $b ON (
-                            $b.changeset_value_id = $a.id
-                            AND " . $match_expression->sql . "
-                         ) ",
-                        [
-                            $this->id,
-                            ...$match_expression->parameters,
-                        ]
-                    )
-                );
-            }
+        if (! $this->isUsed()) {
+            return Option::nothing(ParametrizedFromWhere::class);
         }
 
-        return Option::nothing(ParametrizedFrom::class);
+        //Only filter query if criteria is valuated
+        $criteria_value = $this->getCriteriaValue($criteria);
+
+        if ($criteria_value === '' || $criteria_value === null) {
+            return Option::nothing(ParametrizedFromWhere::class);
+        }
+
+        $a = 'A_' . $this->id;
+        $b = 'B_' . $this->id;
+
+        return $this->buildMatchExpression("$b.artifact_id", $criteria_value)->mapOr(
+            function (ParametrizedSQLFragment $match_expression) use ($a, $b) {
+                return Option::fromValue(
+                    ParametrizedFromWhere::fromParametrizedFrom(
+                        new ParametrizedFrom(
+                            " INNER JOIN tracker_changeset_value AS $a ON ($a.changeset_id = c.id AND $a.field_id = ? )
+                             INNER JOIN tracker_changeset_value_artifactlink AS $b ON (
+                                $b.changeset_value_id = $a.id
+                                AND " . $match_expression->sql . "
+                             ) ",
+                            [
+                                $this->id,
+                                ...$match_expression->parameters,
+                            ]
+                        )
+                    )
+                );
+            },
+            Option::nothing(ParametrizedFromWhere::class)
+        );
     }
 
     /**
@@ -353,38 +363,36 @@ class Tracker_FormElement_Field_ArtifactLink extends Tracker_FormElement_Field /
         return (int) $value;
     }
 
-    private function buildMatchExpression(string $field_name, string $criteria_value): ParametrizedSQLFragment
+    /**
+     * @return Option<ParametrizedSQLFragment>
+     */
+    private function buildMatchExpression(string $field_name, string $criteria_value): Option
     {
         $matches = [];
         if (preg_match('/\/(.*)\//', $criteria_value, $matches)) {
             // If it is sourrounded by /.../ then assume a regexp
-            return new ParametrizedSQLFragment($field_name . " RLIKE ?", [$matches[1]]);
+            return Option::fromValue(new ParametrizedSQLFragment($field_name . " RLIKE ?", [$matches[1]]));
         }
 
         $matches = [];
         if (preg_match("/^(<|>|>=|<=)\s*($this->pattern)\$/", $criteria_value, $matches)) {
             // It's < or >,  = and a number then use as is
             $number = (string) ($this->cast($matches[2]));
-            return new ParametrizedSQLFragment($field_name . ' ' . $matches[1] . ' ?', [$number]);
+            return Option::fromValue(new ParametrizedSQLFragment($field_name . ' ' . $matches[1] . ' ?', [$number]));
         } elseif (preg_match("/^($this->pattern)\$/", $criteria_value, $matches)) {
             // It's a number so use  equality
             $number = $this->cast($matches[1]);
-            return new ParametrizedSQLFragment($field_name . ' = ?', [$number]);
+            return Option::fromValue(new ParametrizedSQLFragment($field_name . ' = ?', [$number]));
         } elseif (preg_match("/^($this->pattern)\s*-\s*($this->pattern)\$/", $criteria_value, $matches)) {
             // it's a range number1-number2
             $min  = (string) ($this->cast($matches[1]));
             $max  = (string) ($this->cast($matches[2]));
             $expr = $field_name . ' >= ' . $matches[1] . ' AND ' . $field_name . ' <= ' . $matches[2];
-            return new ParametrizedSQLFragment($field_name . ' >= ? AND ' . $field_name . ' <= ?', [$min, $max]);
+            return Option::fromValue(new ParametrizedSQLFragment($field_name . ' >= ? AND ' . $field_name . ' <= ?', [$min, $max]));
         } else {
             // Invalid syntax - no condition
-            return new ParametrizedSQLFragment('1', []);
+            return Option::nothing(ParametrizedSQLFragment::class);
         }
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        return Option::nothing(ParametrizedSQLFragment::class);
     }
 
     public function getQuerySelect(): string

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_Burndown.php
@@ -39,8 +39,7 @@ use Tuleap\Tracker\FormElement\Field\Burndown\BurndownRemainingEffortAdderForLeg
 use Tuleap\Tracker\FormElement\Field\Burndown\BurndownRemainingEffortAdderForREST;
 use Tuleap\Tracker\FormElement\Field\Computed\ComputedFieldDao;
 use Tuleap\Tracker\FormElement\Field\File\CreatedFileURLMapping;
-use Tuleap\Tracker\Report\Query\ParametrizedFrom;
-use Tuleap\Tracker\Report\Query\ParametrizedSQLFragment;
+use Tuleap\Tracker\Report\Query\ParametrizedFromWhere;
 use Tuleap\Tracker\REST\Artifact\ArtifactFieldValueFullRepresentation;
 use Tuleap\Tracker\Semantic\Timeframe\IComputeTimeframes;
 use Tuleap\Tracker\Semantic\Timeframe\SemanticTimeframeBuilder;
@@ -441,14 +440,9 @@ class Tracker_FormElement_Field_Burndown extends Tracker_FormElement_Field imple
         return true;
     }
 
-    public function getCriteriaFrom(Tracker_Report_Criteria $criteria): Option
+    public function getCriteriaFromWhere(Tracker_Report_Criteria $criteria): Option
     {
-        return Option::nothing(ParametrizedFrom::class);
-    }
-
-    public function getCriteriaWhere(Tracker_Report_Criteria $criteria): Option
-    {
-        return Option::nothing(ParametrizedSQLFragment::class);
+        return Option::nothing(ParametrizedFromWhere::class);
     }
 
     public function getQuerySelect(): string

commit 8a321022bf7fb70139437dfa9ead504c3d50e2ef

Closes request #34380: Tracker reports can crash when searching on a bind list without any values
Introduced by ea4002c1ccc919455fa6656bd8679c1a7f4b408d.

Not exactly sure what the conditions are to reproduce the crash but in
any case the code is wrong at the type level.

Change-Id: I252116428b86ee48db76ac500215ca428d5c3312

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind.php
@@ -293,7 +293,7 @@ abstract class Tracker_FormElement_Field_List_Bind implements //phpcs:ignore PSR
         }
 
         $ids_to_search = $this->getIdsToSearch($criteria_value);
-        if ($ids_to_search === '') {
+        if (count($ids_to_search) <= 0) {
             return Option::nothing(ParametrizedFromWhere::class);
         }
 
@@ -315,7 +315,7 @@ abstract class Tracker_FormElement_Field_List_Bind implements //phpcs:ignore PSR
         );
     }
 
-    protected function getIdsToSearch($criteria_value)
+    protected function getIdsToSearch($criteria_value): array
     {
         return array_intersect(
             array_values($criteria_value),

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.php
@@ -269,7 +269,7 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
         return $values;
     }
 
-    protected function getIdsToSearch($criteria_value)
+    protected function getIdsToSearch($criteria_value): array
     {
         return array_values($criteria_value);
     }