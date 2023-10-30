b5f51888d912d296403d7df43d81dc3b48ee3785
Nicolas Terray <nicolas.terray@enalean.com>
2023-10-30 11:35:22 +0100

    Reapply timeout for popover closing
    Fix of `hover` behavior in popovers brought in
    git #tuleap/stable/06ae345b1958db1fa31f764a46e7bbf9698fa21f is not
    limited to sidebar and should not have been removed when the sidebar
    have been switched to `click` behavior in
    git #tuleap/stable/3b7cf3b9d46b41033fdc54871f044c4d4e56b738.
    
    No functional change in sidebar
    
    If you don't have a tracker available to create a new planning in
    backlog administration, then you can hover the popover content (to
    select text for example) that is displayed over "Create new planning"
    button.
    
    Part of story #34009: see aggregated & parents projects
    
    Change-Id: Ie26f0784a7f5924806019be5480adfe52f591346

--- lib/frontend/tlp-popovers/src/popovers.test.ts
+++ lib/frontend/tlp-popovers/src/popovers.test.ts
@@ -40,6 +40,8 @@ vi.mock("@floating-ui/dom", async () => {
     };
 });
 
+vi.useFakeTimers();
+
 describe(`Popovers`, () => {
     let trigger_element: HTMLElement, content_element: HTMLElement;
     let doc: Document;
@@ -489,21 +491,47 @@ describe(`Popovers`, () => {
                 trigger_element.dispatchEvent(new MouseEvent("mouseover"));
             });
 
-            it(`will hide the popover`, () => {
+            it(`will hide the popover after a delay`, () => {
                 trigger_element.dispatchEvent(new MouseEvent("mouseout"));
+
+                vi.advanceTimersToNextTimer();
 
                 expectThePopoverToBeHidden(content_element);
 
                 expect(getEventType(dispatchEvent, 2)).toBe(EVENT_TLP_POPOVER_HIDDEN);
             });
 
-            it(`will hide all shown popovers`, () => {
+            it(`will hide all shown popovers after a delay`, () => {
                 const docDispatchEvent = vi.spyOn(doc, "dispatchEvent");
 
                 trigger_element.dispatchEvent(new MouseEvent("mouseout"));
 
+                vi.advanceTimersToNextTimer();
+
                 expect(docDispatchEvent).toHaveBeenCalledOnce();
                 expect(getEventType(docDispatchEvent)).toBe(EVENT_POPOVER_FORCE_CLOSE);
+            });
+
+            describe("and I try to reach the popover content before the delay", () => {
+                beforeEach(() => {
+                    trigger_element.dispatchEvent(new MouseEvent("mouseout"));
+
+                    content_element.dispatchEvent(new MouseEvent("mouseenter"));
+
+                    vi.advanceTimersToNextTimer();
+                });
+
+                it(`should not hide the popover so I can interact with the popover content`, () => {
+                    expectThePopoverToBeShown(content_element);
+                });
+
+                it(`should hide the popover after a delay as soon as I leave the popover content`, () => {
+                    content_element.dispatchEvent(new MouseEvent("mouseleave"));
+
+                    vi.advanceTimersToNextTimer();
+
+                    expectThePopoverToBeHidden(content_element);
+                });
             });
         });
     });
@@ -543,6 +571,8 @@ describe(`Popovers`, () => {
 
                     trigger_element.dispatchEvent(new MouseEvent("click"));
 
+                    vi.advanceTimersToNextTimer();
+
                     expect(docDispatchEvent).toHaveBeenCalledOnce();
                     expect(getEventType(docDispatchEvent)).toBe(EVENT_POPOVER_FORCE_CLOSE);
                 });
@@ -569,6 +599,8 @@ describe(`Popovers`, () => {
 
                     trigger_element.dispatchEvent(new MouseEvent("click"));
                     doc.body.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+                    vi.advanceTimersToNextTimer();
 
                     expect(docDispatchEvent).toHaveBeenCalledTimes(2);
                     expect(getEventType(docDispatchEvent)).toBe(EVENT_POPOVER_FORCE_CLOSE);
