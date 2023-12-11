2eadb256b129fcc87a4c2075b3898db701589b43
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-12-11 15:20:39 +0100

    Supervisord should start the Tuleap Realtime server using the "Tuleap Node"
    The easiest way to test this is to bind mount the modified to
    `/usr/share/tuleap/tools/docker/tuleap-aio-c7/supervisor.d/start-tuleap-realtime.sh`
    
    This missed in b68fe295e7dab26c984dacf8a272a3811ce8651c.
    
    Part of request #35159: Use NodeJS 20.x LTS for Tuleap node apps
    
    Change-Id: Ifb3cbff23bb962b9878419047b982c139d53d438

--- tools/docker/tuleap-aio-c7/supervisor.d/start-tuleap-realtime.sh
+++ tools/docker/tuleap-aio-c7/supervisor.d/start-tuleap-realtime.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 export "$(cat /var/lib/tuleap/tuleap-realtime-key)"
-exec sudo --preserve-env=PRIVATE_KEY -u tuleaprt NODE_ENV=production /usr/lib/tuleap-realtime/tuleap-realtime
+exec sudo --preserve-env=PRIVATE_KEY -u tuleaprt NODE_ENV=production /usr/bin/tuleap-node /usr/lib/tuleap-realtime/tuleap-realtime.js
