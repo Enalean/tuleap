627461b7b176a2b9bb47434f9be67c6069452899
Thomas Gorka <thomas.gorka@enalean.com>
2023-12-22 11:41:21 +0100

    feat: limit displayed reviewers to three
    part of story #33006 Create filters on my PR
    
    How to test:
    - Pick up a pull-request
    - Add it three reviewers
    --> They are displayed as usual
    
    - Add it a fourth one
    --> Only three are displayed
    --> There is a +1 on the right
    
    - click on +1
    --> A dropdown opens
    --> The fourth reviewer is displayed inside it
    
    Change-Id: I6db64a81c614d358102d0ff081d2853cb2ec5e1f

--- plugins/pullrequest/scripts/pullrequest-homepage/src/components/List/PullRequest/PullRequestReviewers.vue
+++ plugins/pullrequest/scripts/pullrequest-homepage/src/components/List/PullRequest/PullRequestReviewers.vue
@@ -21,7 +21,7 @@
     <div class="pull-request-card-reviewers">
         <div class="pull-request-card-reviewers-overlap-container">
             <div
-                v-for="reviewer in pull_request.reviewers"
+                v-for="reviewer in reviewers_to_display"
                 v-bind:key="reviewer.id"
                 class="tlp-avatar-medium pull-request-card-reviewer"
             >
@@ -33,16 +33,74 @@
                     data-test="pull-request-card-reviewer-avatar"
                 />
             </div>
+            <div v-if="has_more_reviewers" class="tlp-dropdown">
+                <div
+                    class="pull-request-card-remaining-reviewers-count"
+                    ref="dropdown_trigger"
+                    data-test="pull-request-card-remaining-reviewer-count"
+                >
+                    +{{ nb_reviewers_not_displayed }}
+                </div>
+                <div
+                    class="tlp-dropdown-menu pull-request-card-remaining-reviewers-dropdown"
+                    role="menu"
+                    ref="dropdown_content"
+                >
+                    <div
+                        v-for="remaining_reviewer in remaining_reviewers"
+                        v-bind:key="remaining_reviewer.id"
+                        class="tlp-dropdown-menu-item"
+                    >
+                        <div class="tlp-avatar-mini">
+                            <img
+                                v-bind:src="remaining_reviewer.avatar_url"
+                                class="media-object"
+                                aria-hidden="true"
+                                data-test="pull-request-card-remaining-reviewer-avatar"
+                            />
+                        </div>
+                        <span class="pull-request-card-remaining-reviewer-name">{{
+                            remaining_reviewer.display_name
+                        }}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { Ref } from "vue";
+import { createDropdown } from "@tuleap/tlp-dropdown";
 import type { PullRequest } from "@tuleap/plugin-pullrequest-rest-api-types";
 
-defineProps<{
+const props = defineProps<{
     pull_request: PullRequest;
 }>();
+
+const dropdown_trigger: Ref<HTMLElement | undefined> = ref();
+const dropdown_content: Ref<HTMLElement | undefined> = ref();
+
+const nb_max_displayed_reviewers = 3;
+const reviewers_to_display = props.pull_request.reviewers.slice(0, nb_max_displayed_reviewers);
+const remaining_reviewers = props.pull_request.reviewers.slice(nb_max_displayed_reviewers);
+const has_more_reviewers = props.pull_request.reviewers.length > nb_max_displayed_reviewers;
+const nb_reviewers_not_displayed = props.pull_request.reviewers.length - nb_max_displayed_reviewers;
+
+const preventClickInDropdownToTriggerRedirection = (dropdown: HTMLElement) => {
+    dropdown.addEventListener("click", (event) => event.preventDefault());
+};
+
+onMounted(() => {
+    if (!dropdown_trigger.value || !dropdown_content.value) {
+        return;
+    }
+
+    preventClickInDropdownToTriggerRedirection(dropdown_content.value);
+
+    createDropdown(dropdown_trigger.value);
+});
 </script>
 
 <style scoped lang="scss">
@@ -64,11 +122,34 @@ defineProps<{
 .pull-request-card-reviewer {
     $border-width: 4px;
 
-    box-sizing: content-box;
     border: $border-width solid var(--tlp-white-color);
 
     &:not(:first-child) {
         margin-left: calc(-#{$border-width} * 2);
     }
+}
+
+.pull-request-card-remaining-reviewers-count {
+    margin: 0 0 0 2px;
+    color: var(--tlp-dimmed-color);
+
+    &:hover {
+        border-bottom: 1px solid var(--tlp-dimmed-color);
+    }
+}
+
+.pull-request-card-remaining-reviewers-dropdown {
+    transform: translate(-12px, 10px);
+    cursor: default;
+
+    > .tlp-dropdown-menu-item {
+        display: flex;
+        align-items: center;
+        cursor: default;
+    }
+}
+
+.pull-request-card-remaining-reviewer-name {
+    margin: 0 0 0 var(--tlp-small-spacing);
 }
 </style>
