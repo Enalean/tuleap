e0a9a9777d68c334cca4c5c5073481d6131ce6ab
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-02-09 18:13:53 +0100

    fix: Optimize SQL query when filtering PRs with labels
    The existing query works but is heavy as it requires to compare all the
    PRs of the instances against a set of labels. The changes made in this
    contribution allows the DB engine to use the indexes of the PRs and
    labels tables.
    
    No functional changes expected.
    
    Part of story #35825 Pull request new home page - Filter on labels
    
    Change-Id: I5a98e58d48fcf8eccc90af09e8eaf5470411ba67

---
+++ plugins/pullrequest/include/PullRequest/PullRequestDAOSearchCriteria.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\PullRequest;
+
+use ParagonIE\EasyDB\EasyStatement;
+
+final readonly class PullRequestDAOSearchCriteria
+{
+    /**
+     * @param list<non-empty-string> $tables
+     */
+    public function __construct(
+        public EasyStatement $where_statement,
+        public EasyStatement $having_statement,
+        public array $tables,
+    ) {
+    }
+}
