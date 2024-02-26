cf4e9b56b59287b0e8cfb0e88b75dbd1e62c7124
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-02-26 11:27:50 +0100

    feat: progress bar and JSON output for `tuleap svn:repositories-with-duplicated-sections-access-file`
    It aligns the behavior to what is available in
    `tuleap git:repositories-with-object-over-the-size-limit`.
    
    Part of request #31541 svn 1.10+ no longer supports duplicate sections
    
    Change-Id: I41ef52707c46f7468145f2eca794d1edfa274c24

--- plugins/svn/include/SVN/AccessControl/SVNCheckRepositoriesWithDuplicatedAccessFileSections.php
+++ plugins/svn/include/SVN/AccessControl/SVNCheckRepositoriesWithDuplicatedAccessFileSections.php
@@ -23,13 +23,17 @@ declare(strict_types=1);
 namespace Tuleap\SVN\AccessControl;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Tuleap\SVN\Repository\RepositoryManager;
+use Tuleap\SVNCore\Repository;
 use Tuleap\SVNCore\SVNAccessFileReader;
 
-class SVNCheckRepositoriesWithDuplicatedAccessFileSections extends Command
+final class SVNCheckRepositoriesWithDuplicatedAccessFileSections extends Command
 {
     public const NAME = 'svn:repositories-with-duplicated-sections-access-file';
 
@@ -41,47 +45,100 @@ class SVNCheckRepositoriesWithDuplicatedAccessFileSections extends Command
         parent::__construct(self::NAME);
     }
 
+    protected function configure(): void
+    {
+        $this->setDescription('List SVN multi repository access files with duplicated sections.')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format (txt or json)', 'txt');
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln("<comment>Start checking of duplicated sections in platform access files:</comment>");
+        match ($format = $input->getOption('format')) {
+            'txt' => $this->displayAsText($output),
+            'json' => $this->displayAsJSON($output),
+            default => throw new \RuntimeException(sprintf('Unsupported format "%s". See help for supported formats.', $format)),
+        };
 
-        $all_active_repositories      = $this->repository_manager->getRepositoriesOfNonDeletedProjects();
-        $repositories_with_duplicates = [];
+        return self::SUCCESS;
+    }
+
+    /**
+     * @psalm-return list<Repository>
+     */
+    private function getRepositoriesWithDuplicatedSectionInAccessFile(OutputInterface $output): array
+    {
+        $all_active_repositories = $this->repository_manager->getRepositoriesOfNonDeletedProjects();
+
+        $all_active_repositories_count = 0;
+        foreach ($all_active_repositories as $repository_by_project_collection) {
+            $all_active_repositories_count += count($repository_by_project_collection->getRepositoryList());
+        }
+
+        $progress_bar = new ProgressBar($output, $all_active_repositories_count);
+        $progress_bar->start();
+
+        $repositories_with_issue = [];
 
         foreach ($all_active_repositories as $repository_by_project_collection) {
             foreach ($repository_by_project_collection->getRepositoryList() as $repository) {
                 $svn_access_file_content = $this->access_file_reader->getAccessFileContent($repository);
                 $collection_of_faults    = $this->duplicate_section_detector->inspect($svn_access_file_content);
                 if (iterator_count($collection_of_faults) > 0) {
-                    $repositories_with_duplicates[] = $repository;
+                    $repositories_with_issue[] = $repository;
                 }
+
+                $progress_bar->advance();
             }
         }
 
-        $total_number_of_repositories_with_duplicates = count($repositories_with_duplicates);
-        if ($total_number_of_repositories_with_duplicates === 0) {
-            $output->writeln("<comment>No duplicated sections in platform access files found.</comment>");
+        $progress_bar->finish();
+        $progress_bar->clear();
 
-            return 0;
+        return $repositories_with_issue;
+    }
+
+    private function displayAsText(OutputInterface $output): void
+    {
+        $table_rows   = [];
+        $repositories = $this->getRepositoriesWithDuplicatedSectionInAccessFile($output);
+
+        if (count($repositories) <= 0) {
+            $output->writeln("<info>No duplicated sections in platform access files found.</info>");
+            return;
+        }
+
+        foreach ($repositories as $repository) {
+            $project      = $repository->getProject();
+            $table_rows[] = [
+                $project->getID(),
+                $project->getUnixName(),
+                $repository->getId(),
+                $repository->getName(),
+            ];
         }
 
         $table = new Table($output);
-        $table->setHeaders(["Project Id", "Project name", 'Repository']);
-        $table->setStyle('box');
-
-        foreach ($repositories_with_duplicates as $repository) {
-            $project = $repository->getProject();
-            $table->addRow([$project->getId(), $project->getUnixName(), $repository->getName()]);
-        }
+        $table->setHeaders(['Project ID', 'Project name', 'Repository ID', 'Repository name'])
+            ->setRows($table_rows);
 
         $table->render();
 
-        $output->writeln("<info>" . $total_number_of_repositories_with_duplicates . " SVN access files with duplicated sections found.</info>");
-        return 0;
+        $output->writeln("<info>" . count($repositories) . " SVN access files with duplicated sections found.</info>");
     }
 
-    protected function configure()
+    private function displayAsJSON(OutputInterface $output): void
     {
-        $this->setDescription('List SVN multi repository access files with duplicated sections.');
+        $rows = [];
+        foreach ($this->getRepositoriesWithDuplicatedSectionInAccessFile($output) as $repository) {
+            $project = $repository->getProject();
+
+            $rows[] = [
+                'project_id' => (int) $project->getID(),
+                'project_unixname' => $project->getUnixName(),
+                'repository_id' => $repository->getId(),
+                'repository_name' => $repository->getName(),
+            ];
+        }
+        $output->write(OutputFormatter::escape(\Psl\Json\encode($rows)));
     }
 }
