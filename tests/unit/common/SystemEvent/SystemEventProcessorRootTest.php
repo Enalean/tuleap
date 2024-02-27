1854fa286a8c8cc207c8f211ec6401d1b339622d
Kevin Traini <kevin.traini@enalean.com>
2024-02-27 14:18:39 +0100

    chore: Drop Mockery usage in SystemEvent core tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    Change-Id: I50701c626b26bb24f8fd6da8a3daa8014e81c115

--- tests/unit/common/SystemEvent/SystemEventProcessorRootTest.php
+++ tests/unit/common/SystemEvent/SystemEventProcessorRootTest.php
@@ -26,10 +26,11 @@ namespace Tuleap\SystemEvent;
 use BackendAliases;
 use BackendSVN;
 use BackendSystem;
+use ColinODell\PsrTestLogger\TestLogger;
+use DataAccessResult;
 use Exception;
 use ForgeConfig;
-use Mockery as M;
-use Mockery\MockInterface;
+use PHPUnit\Framework\MockObject\MockObject;
 use RuntimeException;
 use SiteCache;
 use SystemEvent;
@@ -38,134 +39,154 @@ use SystemEventManager;
 use SystemEventProcessor_Root;
 use SystemEventProcessRootDefaultQueue;
 use Tuleap\DB\DBConnection;
+use Tuleap\ForgeConfigSandbox;
 use Tuleap\SVNCore\ApacheConfGenerator;
+use Tuleap\Test\PHPUnit\TestCase;
 
-class SystemEventProcessorRootTest extends \Tuleap\Test\PHPUnit\TestCase
+final class SystemEventProcessorRootTest extends TestCase
 {
-    use M\Adapter\Phpunit\MockeryPHPUnitIntegration;
+    use ForgeConfigSandbox;
 
-    private $system_event_manager;
-    /**
-     * @var MockInterface|SystemEventDao
-     */
-    private $system_event_dao;
-    /**
-     * @var SystemEventProcessor_Root
-     */
-    private $processor;
-    private $sys_http_user = 'www-data';
-    private $logger;
-    private $site_cache;
+    private SystemEventManager&MockObject $system_event_manager;
+    private SystemEventDao&MockObject $system_event_dao;
+    private SystemEventProcessor_Root&MockObject $processor;
+    private string $sys_http_user = 'www-data';
+    private TestLogger $logger;
+    private SiteCache&MockObject $site_cache;
 
     protected function setUp(): void
     {
-        $this->system_event_manager = M::mock(SystemEventManager::class);
-        $this->system_event_dao     = M::mock(SystemEventDao::class);
-        $this->logger               = M::spy(\Psr\Log\LoggerInterface::class);
-        $this->site_cache           = M::mock(SiteCache::class);
+        $this->system_event_manager = $this->createMock(SystemEventManager::class);
+        $this->system_event_dao     = $this->createMock(SystemEventDao::class);
+        $this->logger               = new TestLogger();
+        $this->site_cache           = $this->createMock(SiteCache::class);
 
-        $this->processor = M::mock(
-            SystemEventProcessor_Root::class,
-            [
+        $backend_aliases = $this->createMock(BackendAliases::class);
+        $backend_aliases->method('aliasesNeedUpdate')->willReturn(false);
+        $backend_svn = $this->createMock(BackendSVN::class);
+        $backend_svn->method('getSVNApacheConfNeedUpdate')->willReturn(false);
+        $this->processor = $this->getMockBuilder(SystemEventProcessor_Root::class)
+            ->setConstructorArgs([
                 new SystemEventProcessRootDefaultQueue(),
                 $this->system_event_manager,
                 $this->system_event_dao,
                 $this->logger,
-                M::spy(BackendAliases::class),
-                M::spy(BackendSVN::class),
-                M::spy(BackendSystem::class),
+                $backend_aliases,
+                $backend_svn,
+                $this->createMock(BackendSystem::class),
                 $this->site_cache,
-                M::spy(ApacheConfGenerator::class),
-                M::mock(DBConnection::class),
-            ]
-        )->makePartial()->shouldAllowMockingProtectedMethods();
+                $this->createMock(ApacheConfGenerator::class),
+                $this->createMock(DBConnection::class),
+            ])
+            ->onlyMethods([
+                'launchAs',
+            ])
+            ->getMock();
 
-        ForgeConfig::store();
         ForgeConfig::set('codendi_dir', '/usr/share/codendi');
         ForgeConfig::set('sys_http_user', $this->sys_http_user);
     }
 
-    protected function tearDown(): void
-    {
-        ForgeConfig::restore();
-    }
-
-    public function testItFetchesEventsForRoot()
+    public function testItFetchesEventsForRoot(): void
     {
         $category = SystemEvent::DEFAULT_QUEUE;
 
         $types = ['some_type'];
-        $this->system_event_manager->shouldReceive('getTypesForQueue')->once()->andReturn($types);
+        $this->system_event_manager->expects(self::once())->method('getTypesForQueue')->willReturn($types);
 
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->with('root', $types)->once()->andReturn(false);
+        $this->system_event_dao->expects(self::once())->method('checkOutNextEvent')->with('root', $types)->willReturn(false);
         $this->processor->execute($category);
     }
 
-    public function testItCatchExceptionsInSystemEvents()
+    public function testItCatchExceptionsInSystemEvents(): void
     {
-        $system_event = M::mock(SystemEvent::class)->makePartial();
-        $system_event->shouldReceive('notify')->once();
-        $system_event->shouldReceive('process')->once()->andThrow(new RuntimeException('Something wrong happened'));
+        $system_event = $this->createPartialMock(SystemEvent::class, [
+            'verbalizeParameters',
+            'notify',
+            'process',
+        ]);
+        $system_event->expects(self::once())->method('notify');
+        $system_event->expects(self::once())->method('process')->willThrowException(new RuntimeException('Something wrong happened'));
 
         $types = ['some_type'];
-        $this->system_event_manager->shouldReceive('getTypesForQueue')->andReturn($types);
+        $this->system_event_manager->method('getTypesForQueue')->willReturn($types);
 
-        $this->system_event_dao->shouldReceive('getElapsedTime')->once();
-        $this->system_event_dao->shouldReceive('close')->once();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(M::mock(\DataAccessResult::class, ['getRow' => ['whatever']]))->ordered();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(null)->ordered();
-        $this->system_event_manager->shouldReceive('getInstanceFromRow')->once()->andReturns($system_event);
+        $this->system_event_dao->expects(self::once())->method('getElapsedTime');
+        $this->system_event_dao->expects(self::once())->method('close');
+        $dar = $this->createMock(DataAccessResult::class);
+        $dar->method('getRow')->willReturn(['whatever']);
+        $this->system_event_dao->expects(self::exactly(2))->method('checkOutNextEvent')
+            ->willReturnOnConsecutiveCalls(
+                $dar,
+                null
+            );
+        $this->system_event_manager->expects(self::once())->method('getInstanceFromRow')->willReturn($system_event);
 
         $category = SystemEvent::DEFAULT_QUEUE;
         $this->processor->execute($category);
 
-        $this->assertEquals($system_event->getStatus(), SystemEvent::STATUS_ERROR);
-        $this->assertEquals($system_event->getLog(), 'Something wrong happened');
+        self::assertEquals(SystemEvent::STATUS_ERROR, $system_event->getStatus());
+        self::assertEquals('Something wrong happened', $system_event->getLog());
     }
 
-    public function testItProcessApplicationOwnerEvents()
+    public function testItProcessApplicationOwnerEvents(): void
     {
-        $this->site_cache->shouldReceive('restoreOwnership')->once();
+        $this->site_cache->expects(self::once())->method('restoreOwnership');
 
-        $system_event = M::mock(SystemEvent::class)->makePartial();
-        $system_event->shouldReceive('notify')->once();
+        $system_event = $this->createPartialMock(SystemEvent::class, [
+            'verbalizeParameters',
+            'notify',
+        ]);
+        $system_event->expects(self::once())->method('notify');
         $system_event->setStatus(SystemEvent::STATUS_DONE);
 
-        $this->system_event_manager->shouldReceive('getTypesForQueue')->andReturn(['SOME_EVENT']);
-        $this->system_event_manager->shouldReceive('getInstanceFromRow')->once()->andReturns($system_event);
+        $this->system_event_manager->method('getTypesForQueue')->willReturn(['SOME_EVENT']);
+        $this->system_event_manager->expects(self::once())->method('getInstanceFromRow')->willReturn($system_event);
 
-        $this->system_event_dao->shouldReceive('getElapsedTime')->once();
-        $this->system_event_dao->shouldReceive('close')->once();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(M::mock(\DataAccessResult::class, ['getRow' => ['whatever']]))->ordered();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(null)->ordered();
+        $this->system_event_dao->expects(self::once())->method('getElapsedTime');
+        $this->system_event_dao->expects(self::once())->method('close');
+        $dar = $this->createMock(DataAccessResult::class);
+        $dar->method('getRow')->willReturn(['whatever']);
+        $this->system_event_dao->expects(self::exactly(2))->method('checkOutNextEvent')
+            ->willReturnOnConsecutiveCalls(
+                $dar,
+                null
+            );
 
         $command = '/usr/bin/tuleap process-system-events ' . SystemEvent::OWNER_APP;
-        $this->processor->shouldReceive('launchAs')->with($this->sys_http_user, $command)->once();
+        $this->processor->expects(self::once())->method('launchAs')->with($this->sys_http_user, $command);
         $category = SystemEvent::DEFAULT_QUEUE;
         $this->processor->execute($category);
     }
 
-    public function testItCatchesExceptionsThrownInPostActions()
+    public function testItCatchesExceptionsThrownInPostActions(): void
     {
-        $this->site_cache->shouldReceive('restoreOwnership')->once();
+        $this->site_cache->expects(self::once())->method('restoreOwnership');
 
-        $system_event = M::mock(SystemEvent::class)->makePartial();
-        $system_event->shouldReceive('notify')->once();
+        $system_event = $this->createPartialMock(SystemEvent::class, [
+            'verbalizeParameters',
+            'notify',
+        ]);
+        $system_event->expects(self::once())->method('notify');
         $system_event->setStatus(SystemEvent::STATUS_DONE);
 
-        $this->system_event_manager->shouldReceive('getTypesForQueue')->andReturn(['SOME_EVENT']);
-        $this->system_event_manager->shouldReceive('getInstanceFromRow')->once()->andReturns($system_event);
+        $this->system_event_manager->method('getTypesForQueue')->willReturn(['SOME_EVENT']);
+        $this->system_event_manager->expects(self::once())->method('getInstanceFromRow')->willReturn($system_event);
 
-        $this->system_event_dao->shouldReceive('getElapsedTime')->once();
-        $this->system_event_dao->shouldReceive('close')->once();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(M::mock(\DataAccessResult::class, ['getRow' => ['whatever']]))->ordered();
-        $this->system_event_dao->shouldReceive('checkOutNextEvent')->once()->andReturn(null)->ordered();
+        $this->system_event_dao->expects(self::once())->method('getElapsedTime');
+        $this->system_event_dao->expects(self::once())->method('close');
+        $dar = $this->createMock(DataAccessResult::class);
+        $dar->method('getRow')->willReturn(['whatever']);
+        $this->system_event_dao->expects(self::exactly(2))->method('checkOutNextEvent')
+            ->willReturnOnConsecutiveCalls(
+                $dar,
+                null
+            );
 
-        $this->processor->shouldReceive('launchAs')->andThrow(new Exception('Something weird happend'));
-
-        $this->logger->shouldReceive('error')->once();
+        $this->processor->method('launchAs')->willThrowException(new Exception('Something weird happend'));
 
         $category = SystemEvent::DEFAULT_QUEUE;
         $this->processor->execute($category);
+        self::assertTrue($this->logger->hasErrorRecords());
     }
 }
