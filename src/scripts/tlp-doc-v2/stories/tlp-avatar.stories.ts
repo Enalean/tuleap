9f3978c0b98a144ce167ba03d80baa5f036c7328
Lou-Evan Rouby <lou-evan.rouby@enalean.com>
2024-04-16 12:05:06 +0200

    feat: storybook, visual assets: avatars
    part of request #37211 Replace TLP-doc documentation
    
    run 'pnpm --filter=tlp-doc-v2 run storybook'
    
    Avatars added to section visual assets.
    
    Change-Id: I681a4b2e8dbb30520e1038a514af4caa54a8bb16

---
+++ src/scripts/tlp-doc-v2/stories/tlp-avatar.stories.ts
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ *  Tuleap is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tuleap is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html, type TemplateResult } from "lit";
+
+type AvatarProps = {
+    size: string;
+    img: boolean;
+    all_variations: boolean;
+};
+
+const avatar_image = new URL("./assets/random_avatar.jpg", import.meta.url).href;
+const SIZES = ["jumbo", "extra-large", "large", "default", "medium", "small", "mini"];
+const new_line = "\n";
+
+function getClass(size: string): string {
+    if (size === "default") {
+        return "tlp-avatar";
+    }
+    return "tlp-avatar-" + size;
+}
+
+function avatarWithSelectedImage(size: string): TemplateResult {
+    return html`<div class="${getClass(size)}">
+        <img src="${avatar_image}" alt="User avatar" />
+    </div>`;
+}
+
+function defaultAvatar(size: string): TemplateResult {
+    return html`<div class="${getClass(size)}"></div>`;
+}
+
+function avatar(size: string, with_image: boolean): TemplateResult {
+    return with_image
+        ? html`${avatarWithSelectedImage(size)}${new_line}`
+        : html`${defaultAvatar(size)}${new_line}`;
+}
+
+function createAllVariations(args: AvatarProps): TemplateResult[] {
+    return SIZES.map((size) => avatar(size, args.img));
+}
+
+const meta: Meta<AvatarProps> = {
+    title: "TLP/Visual assets/Avatars",
+    parameters: {
+        controls: {
+            exclude: ["all_variations"],
+        },
+    },
+    render: (args) => {
+        return args.all_variations
+            ? html`${createAllVariations(args)}`
+            : avatar(args.size, args.img);
+    },
+    args: {
+        all_variations: false,
+        img: false,
+        size: "default",
+    },
+    argTypes: {
+        size: {
+            name: "Size",
+            control: "select",
+            if: { arg: "all_variations", truthy: false },
+            options: SIZES,
+            description: "Avatar size",
+            table: {
+                type: { summary: null },
+            },
+        },
+        img: {
+            name: "With image",
+            description: "Example replacing the default avatar",
+            table: {
+                type: { summary: null },
+            },
+        },
+    },
+};
+
+export default meta;
+type Story = StoryObj<AvatarProps>;
+
+export const Avatar: Story = {};
+
+export const AllVariations: Story = {
+    args: { all_variations: true },
+};
+
+export const AllVariationsWithImage: Story = {
+    args: {
+        all_variations: true,
+        img: true,
+    },
+};
