42846589e8fdfbfddc307ca0569ea615fd1390c0
Lou-Evan Rouby <lou-evan.rouby@enalean.com>
2024-04-19 14:09:20 +0200

    feat: storybook, visual assets: relative dates
    part of request #37211 Replace TLP-doc documentation
    
    run 'pnpm --filter=tlp-doc-v2 run storybook'
    
    Relative dates added to section visual assets.
    
    Change-Id: I59b201d0fdbccede78c6f5e293698145b2ff97bd

---
+++ src/scripts/tlp-doc-v2/stories/tlp-relative-date.stories.ts
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+ *
+ *  This file is a part of Tuleap.
+ *
+ *  Tuleap is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Tuleap is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html, type TemplateResult } from "lit";
+import "@tuleap/tlp-relative-date";
+
+type RelativeDatesProps = {
+    table: boolean;
+};
+
+function getDates(): TemplateResult {
+    // prettier-ignore
+    return html`
+<div class="tlp-property">
+    <label class="tlp-label">Date in french (relative, on the right)</label>
+    <tlp-relative-date
+        date="2020-07-13T14:16:04.467Z"
+        absolute-date="13/07/2020 16:16"
+        placement="right"
+        preference="relative"
+        locale="fr_FR"
+    >13/07/2020 16:16</tlp-relative-date>
+</div>
+
+<div class="tlp-property">
+    <label class="tlp-label">Date in english (absolute, on the right)</label>
+    <tlp-relative-date
+        date="2020-07-13T14:16:04.467Z"
+        absolute-date="2020/07/13 16:16"
+        placement="right"
+        preference="absolute"
+        locale="en_US"
+    >13/07/2020 16:16</tlp-relative-date>
+</div>
+
+<div class="tlp-property">
+    <label class="tlp-label">Date in brazilian portuguese (relative, tooltip)</label>
+    <tlp-relative-date
+        date="2020-07-13T14:16:04.467Z"
+        absolute-date="2020/07/13 16:16"
+        placement="tooltip"
+        preference="relative"
+        locale="pt_BR"
+    >13/07/2020 16:16</tlp-relative-date>
+</div>
+
+<div class="tlp-property">
+    <label class="tlp-label">Date in brazilian portuguese (absolute, tooltip)</label>
+    <tlp-relative-date
+        date="2020-07-13T14:16:04.467Z"
+        absolute-date="2020/07/13 16:16"
+        placement="tooltip"
+        preference="absolute"
+        locale="pt_BR"
+    >13/07/2020 16:16</tlp-relative-date>
+</div>`
+}
+
+function getDatesInTable(): TemplateResult {
+    // prettier-ignore
+    return html`
+<table class="tlp-table">
+    <thead>
+        <tr>
+            <th>Text</th>
+            <th>Date inside a table</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Absolute date</td>
+            <td>
+                <tlp-relative-date
+                    date="2020-07-13T14:16:04.467Z"
+                    absolute-date="2020/07/13 16:16"
+                    placement="top"
+                    preference="absolute"
+                    locale="en_US"
+                >13/07/2020 16:16</tlp-relative-date>
+            </td>
+        </tr>
+        <tr>
+            <td>Relative date</td>
+            <td>
+                <tlp-relative-date
+                    date="2020-07-13T14:16:04.467Z"
+                    absolute-date="2020/07/13 16:16"
+                    placement="top"
+                    preference="relative"
+                    locale="en_US"
+                >13/07/2020 16:16</tlp-relative-date>
+            </td>
+        </tr>
+    </tbody>
+</table>`
+}
+
+const meta: Meta<RelativeDatesProps> = {
+    title: "TLP/Visual assets/Relative dates",
+    parameters: {
+        controls: {
+            exclude: ["element", "table"],
+        },
+    },
+    render: (args) => {
+        return args.table ? getDatesInTable() : getDates();
+    },
+};
+
+export default meta;
+type Story = StoryObj<RelativeDatesProps>;
+
+export const RelativeDates: Story = {};
+
+export const RelativeDatesInTable: Story = {
+    args: { table: true },
+};
