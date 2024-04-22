c99a4f9fd5578b722fbd80fe1d5921adddcf9838
Nicolas Terray <nicolas.terray@enalean.com>
2024-04-22 09:40:06 +0200

    feat: display xref tooltips on artidoc
    If an artidoc document contains cross references to other items in
    tuleap (like art #123), then an enriched tooltip is displayed whenever
    the mouse is over such reference, like it is done in the artifact view.
    
    Part of story #37542: display a read only Document
    
    Change-Id: I06dba05a91c8e6dc654428eb1b98a594cfe57298

---
+++ plugins/artidoc/scripts/artidoc/src/components/SectionDescription.test.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Enalean, 2024 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { describe, expect, it, vi } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import SectionDescription from "@/components/SectionDescription.vue";
+import * as tooltip from "@tuleap/tooltip";
+import VueDOMPurifyHTML from "vue-dompurify-html";
+
+describe("SectionDescription", () => {
+    it("should display text with tooltips", () => {
+        const loadTooltips = vi.spyOn(tooltip, "loadTooltips");
+
+        const wrapper = shallowMount(SectionDescription, {
+            props: {
+                description_value: "Lorem ipsum",
+            },
+            global: {
+                plugins: [VueDOMPurifyHTML],
+            },
+        });
+
+        expect(wrapper.text()).toContain("Lorem ipsum");
+        expect(loadTooltips).toHaveBeenCalled();
+    });
+});
