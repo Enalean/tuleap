ac0856f301ead17641d9c02852ca488238506147
Clarisse Deschamps <clarisse.deschamps@enalean.com>
2024-05-23 11:07:09 +0200

    feat: Be able to select predefined time periods
    Part of story #37941 Timetracking management widget
    
    Added a predefined period selector to facilitate date selection.
    We use the library of predefined periods, previously extracted in timetacking, for consistency.
    The time period selected is in the props, because we're going to save it in the database to enable persistence on it.
    No data is currently retrieved, because this commit only allows you to select and display dates through the predefined period selector.
    
    The widget is currently disabled by a feature flag. To set the feature flag to 1:
    tuleap config-set feature_flag_allow_timetracking_management_widget 1
    
    How to test:
    Click on the dates. You can select predefined time periods in the selector.
    The dates in the date selectors should change automatically according to the predefined period.
    When you search and click again on the dates, then the last predefined period selection should be retained.
    
    Change-Id: I8dda1a35af17a9b826f53279e3f8015508763403

--- plugins/timetracking/scripts/timetracking-management-widget/src/components/WidgetQueryEditor.test.ts
+++ plugins/timetracking/scripts/timetracking-management-widget/src/components/WidgetQueryEditor.test.ts
@@ -22,6 +22,15 @@ import type { VueWrapper } from "@vue/test-utils";
 import { shallowMount } from "@vue/test-utils";
 import WidgetQueryEditor from "./WidgetQueryEditor.vue";
 import { getGlobalTestOptions } from "../../tests/global-options-for-tests";
+import {
+    CURRENT_WEEK,
+    LAST_7_DAYS,
+    LAST_MONTH,
+    LAST_WEEK,
+    TODAY,
+    YESTERDAY,
+} from "@tuleap/plugin-timetracking-predefined-time-periods";
+import * as predefined_time_periods from "@tuleap/plugin-timetracking-predefined-time-periods";
 
 vi.mock("tlp", () => ({
     datePicker: (): { setDate(): void } => ({
@@ -34,12 +43,14 @@ vi.mock("tlp", () => ({
 describe("Given a timetracking management widget query editor", () => {
     const start_date_test = "2024-05-22";
     const end_date_test = "2024-05-22";
+    const predefined_time_selected = TODAY;
 
     function getWidgetQueryEditorInstance(): VueWrapper {
         return shallowMount(WidgetQueryEditor, {
             props: {
                 start_date: start_date_test,
                 end_date: end_date_test,
+                predefined_time_selected: predefined_time_selected,
             },
             global: {
                 ...getGlobalTestOptions(),
@@ -64,6 +75,7 @@ describe("Given a timetracking management widget query editor", () => {
             expect(set_dates_event[0]).toStrictEqual([
                 start_date_input.element.value,
                 end_date_input.element.value,
+                "",
             ]);
         }
 
@@ -80,5 +92,77 @@ describe("Given a timetracking management widget query editor", () => {
 
         expect(set_dates_event).toBeUndefined();
         expect(close_edit_mode_event).toBeDefined();
+    });
+
+    it("When Today is selected, then 'getTodayPeriod' should be called", () => {
+        const getTodayPeriod = vi.spyOn(predefined_time_periods, "getTodayPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(TODAY);
+
+        expect(getTodayPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When Yesterday is selected, then 'getYesterdayPeriod' should be called", () => {
+        const getYesterdayPeriod = vi.spyOn(predefined_time_periods, "getYesterdayPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(YESTERDAY);
+
+        expect(getYesterdayPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When Last 7 days is selected, then 'getLastSevenDaysPeriod' should be called", () => {
+        const getLastSevenDaysPeriod = vi.spyOn(predefined_time_periods, "getLastSevenDaysPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(LAST_7_DAYS);
+
+        expect(getLastSevenDaysPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When Current week is selected, then 'getCurrentWeekPeriod' should be called", () => {
+        const getCurrentWeekPeriod = vi.spyOn(predefined_time_periods, "getCurrentWeekPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(CURRENT_WEEK);
+
+        expect(getCurrentWeekPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When Last week is selected, then 'getLastWeekPeriod' should be called", () => {
+        const getLastWeekPeriod = vi.spyOn(predefined_time_periods, "getLastWeekPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(LAST_WEEK);
+
+        expect(getLastWeekPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When Last month is selected, then 'getLastMonthPeriod' should be called", () => {
+        const getLastMonthPeriod = vi.spyOn(predefined_time_periods, "getLastMonthPeriod");
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        select.setValue(LAST_MONTH);
+
+        expect(getLastMonthPeriod).toHaveBeenCalledOnce();
+    });
+
+    it("When start date is selected manually, then the selected predefined time period should be cleared", async () => {
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        const input = wrapper.find<HTMLInputElement>("[data-test=start-date-input]");
+        await input.setValue("2024-23-05");
+
+        expect(select.element.value).toBe("");
+    });
+
+    it("When end date is selected manually, then the selected predefined time period should be cleared", async () => {
+        const wrapper = getWidgetQueryEditorInstance();
+        const select = wrapper.find<HTMLSelectElement>("[data-test=predefined-periods-select]");
+        const input = wrapper.find<HTMLInputElement>("[data-test=end-date-input]");
+        await input.setValue("2024-23-05");
+
+        expect(select.element.value).toBe("");
     });
 });
