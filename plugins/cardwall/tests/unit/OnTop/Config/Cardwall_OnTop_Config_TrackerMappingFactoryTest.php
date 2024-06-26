7d74126f49e0247535529362fe286837920ff41d
Kevin Traini <kevin.traini@enalean.com>
2024-06-26 10:04:19 +0200

    chore: Drop Mockery usage in Cardwall/OnTop/Config tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    3/3
    
    *No functional changes expected*
    
    Change-Id: I71b382705a0386cb698e9780b0ddc7e5d80a21b5

--- plugins/cardwall/tests/unit/OnTop/Config/Cardwall_OnTop_Config_TrackerMappingFactoryTest.php
+++ plugins/cardwall/tests/unit/OnTop/Config/Cardwall_OnTop_Config_TrackerMappingFactoryTest.php
@@ -20,93 +20,95 @@
 
 declare(strict_types=1);
 
-// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
-final class Cardwall_OnTop_Config_TrackerMappingFactoryTest extends \Tuleap\Test\PHPUnit\TestCase
-{
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+namespace Tuleap\Cardwall\OnTop\Config;
 
+use Cardwall_Column;
+use Cardwall_OnTop_ColumnMappingFieldDao;
+use Cardwall_OnTop_Config_ColumnFreestyleCollection;
+use Cardwall_OnTop_Config_TrackerMappingFactory;
+use Cardwall_OnTop_Config_TrackerMappingFreestyle;
+use Cardwall_OnTop_Config_TrackerMappingNoField;
+use Cardwall_OnTop_Config_TrackerMappingStatus;
+use Cardwall_OnTop_Config_ValueMappingFactory;
+use PHPUnit\Framework\MockObject\MockObject;
+use TestHelper;
+use Tracker;
+use Tracker_FormElement_Field_Selectbox;
+use Tracker_FormElementFactory;
+use Tracker_Semantic_Status;
+use TrackerFactory;
+use Tuleap\Test\Builders\ProjectTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Tracker\Test\Builders\Fields\ListFieldBuilder;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
+
+final class Cardwall_OnTop_Config_TrackerMappingFactoryTest extends TestCase // phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
+{
     private Tracker_FormElement_Field_Selectbox $field_122;
     private Tracker_FormElement_Field_Selectbox $field_123;
     private Tracker_FormElement_Field_Selectbox $field_124;
     private Tracker_FormElement_Field_Selectbox $status_field;
-    /**
-     * @var Tracker&\Mockery\MockInterface
-     */
-    private $tracker;
-    /**
-     * @var Tracker&\Mockery\MockInterface
-     */
-    private $tracker_10;
-    /**
-     * @var Tracker&\Mockery\MockInterface
-     */
-    private $tracker_20;
-    /**
-     * @var Cardwall_OnTop_ColumnMappingFieldDao&\Mockery\MockInterface
-     */
-    private $dao;
-    /**
-     * @var Cardwall_OnTop_Config_ValueMappingFactory&\Mockery\MockInterface
-     */
-    private $value_mapping_factory;
+    private Tracker $tracker;
+    private Tracker $tracker_10;
+    private Tracker $tracker_20;
+    private Cardwall_OnTop_ColumnMappingFieldDao&MockObject $dao;
+    private Cardwall_OnTop_Config_ValueMappingFactory&MockObject $value_mapping_factory;
     private Cardwall_OnTop_Config_ColumnFreestyleCollection $columns;
     private Cardwall_OnTop_Config_TrackerMappingFactory $factory;
 
     protected function setUp(): void
     {
-        parent::setUp();
-        $this->field_122    = $this->buildSelectBoxField(122);
-        $this->field_123    = $this->buildSelectBoxField(123);
-        $this->field_124    = $this->buildSelectBoxField(124);
-        $this->status_field = $this->buildSelectBoxField(125);
+        $this->field_122    = ListFieldBuilder::aListField(122)->build();
+        $this->field_123    = ListFieldBuilder::aListField(123)->build();
+        $this->field_124    = ListFieldBuilder::aListField(124)->build();
+        $this->status_field = ListFieldBuilder::aListField(125)->build();
 
-        $group_id      = 234;
-        $this->tracker = Mockery::mock(Tracker::class);
-        $this->tracker->shouldReceive('getId')->andReturn(3);
-        $this->tracker->shouldReceive('getGroupId')->andReturn($group_id);
-        $this->tracker_10 = Mockery::mock(Tracker::class);
-        $this->tracker_10->shouldReceive('getId')->andReturn(10);
-        $this->tracker_10->shouldReceive('getStatusField')->andReturn($this->status_field);
-        $this->tracker_20 = Mockery::mock(Tracker::class);
-        $this->tracker_20->shouldReceive('getId')->andReturn(20);
-        $this->tracker_20->shouldReceive('getStatusField')->andReturn(null);
+        $group_id         = 234;
+        $project          = ProjectTestBuilder::aProject()->withId($group_id)->build();
+        $this->tracker    = TrackerTestBuilder::aTracker()->withId(3)->withProject($project)->build();
+        $this->tracker_10 = TrackerTestBuilder::aTracker()->withId(10)->build();
+        Tracker_Semantic_Status::setInstance(
+            new Tracker_Semantic_Status($this->tracker_10, $this->status_field),
+            $this->tracker_10,
+        );
+        $this->tracker_20 = TrackerTestBuilder::aTracker()->withId(20)->build();
+        Tracker_Semantic_Status::setInstance(new Tracker_Semantic_Status($this->tracker_20, null), $this->tracker_20);
         $project_trackers = [
             3  => $this->tracker,
             10 => $this->tracker_10,
             20 => $this->tracker_20,
         ];
 
-        $tracker_factory = \Mockery::spy(\TrackerFactory::class)->shouldReceive('getTrackersByGroupId')->with($group_id)->andReturns($project_trackers)->getMock();
-        foreach ($project_trackers as $t) {
-            $tracker_factory->shouldReceive('getTrackerById')->with($t->getId())->andReturns($t);
-        }
+        $tracker_factory = $this->createMock(TrackerFactory::class);
+        $tracker_factory->method('getTrackersByGroupId')->with($group_id)->willReturn($project_trackers);
+        $tracker_factory->method('getTrackerById')->willReturnCallback(fn(int $tracker_id) => match ($tracker_id) {
+            3       => $this->tracker,
+            10      => $this->tracker_10,
+            20      => $this->tracker_20,
+            default => self::fail("Should not have been called with $tracker_id"),
+        });
 
-        $element_factory = \Mockery::spy(\Tracker_FormElementFactory::class);
-        $element_factory->shouldReceive('getFieldById')->with(123)->andReturns($this->field_123);
-        $element_factory->shouldReceive('getFieldById')->with(124)->andReturns($this->field_124);
-        $element_factory->shouldReceive('getUsedSbFields')->with($this->tracker_10)->andReturns([$this->field_122, $this->field_123]);
-        $element_factory->shouldReceive('getUsedSbFields')->andReturns([]);
+        $element_factory = $this->createMock(Tracker_FormElementFactory::class);
+        $element_factory->method('getFieldById')->willReturnCallback(fn(?int $field_id) => match ($field_id) {
+            123     => $this->field_123,
+            124     => $this->field_124,
+            default => null,
+        });
+        $element_factory->method('getUsedSbFields')->willReturnCallback(fn(Tracker $tracker) => match ($tracker->getId()) {
+            10      => [$this->field_122, $this->field_123],
+            default => [],
+        });
 
-        $this->dao                   = \Mockery::spy(\Cardwall_OnTop_ColumnMappingFieldDao::class);
-        $this->value_mapping_factory = \Mockery::spy(\Cardwall_OnTop_Config_ValueMappingFactory::class);
+        $this->dao                   = $this->createMock(Cardwall_OnTop_ColumnMappingFieldDao::class);
+        $this->value_mapping_factory = $this->createMock(Cardwall_OnTop_Config_ValueMappingFactory::class);
 
-        $this->columns = new Cardwall_OnTop_Config_ColumnFreestyleCollection(
-            [
-                new Cardwall_Column(1, 'Todo', 'white'),
-                new Cardwall_Column(2, 'On Going', 'white'),
-                new Cardwall_Column(3, 'Done', 'white'),
-            ]
-        );
+        $this->columns = new Cardwall_OnTop_Config_ColumnFreestyleCollection([
+            new Cardwall_Column(1, 'Todo', 'white'),
+            new Cardwall_Column(2, 'On Going', 'white'),
+            new Cardwall_Column(3, 'Done', 'white'),
+        ]);
 
         $this->factory = new Cardwall_OnTop_Config_TrackerMappingFactory($tracker_factory, $element_factory, $this->dao, $this->value_mapping_factory);
-    }
-
-    private function buildSelectBoxField(int $id): Tracker_FormElement_Field_Selectbox
-    {
-        $selectbox_field = Mockery::mock(Tracker_FormElement_Field_Selectbox::class);
-        $selectbox_field->shouldReceive('getId')->andReturn($id);
-
-        return $selectbox_field;
     }
 
     public function testItRemovesTheCurrentTrackerFromTheProjectTrackers(): void
@@ -116,80 +118,80 @@ final class Cardwall_OnTop_Config_TrackerMappingFactoryTest extends \Tuleap\Test
             20 => $this->tracker_20,
         ];
 
-        $this->assertSame($expected_trackers, $this->factory->getTrackers($this->tracker));
+        self::assertSame($expected_trackers, $this->factory->getTrackers($this->tracker));
     }
 
     public function testItLoadsMappingsFromTheDatabase(): void
     {
-        $this->value_mapping_factory->shouldReceive('getMappings')->andReturns([]);
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->value_mapping_factory->method('getMappings')->willReturn([]);
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 10, 'field_id' => 123],
             ['tracker_id' => 20, 'field_id' => 124]
         ));
 
         $mappings = $this->factory->getMappings($this->tracker, $this->columns);
-        $this->assertEquals(2, count($mappings));
-        $this->assertEquals($this->tracker_10, $mappings[10]->getTracker());
-        $this->assertEquals($this->field_123, $mappings[10]->getField());
-        $this->assertInstanceOf(\Cardwall_OnTop_Config_TrackerMappingFreestyle::class, $mappings[10]);
-        $this->assertEquals([$this->field_122, $this->field_123], $mappings[10]->getAvailableFields());
-        $this->assertEquals($this->tracker_20, $mappings[20]->getTracker());
-        $this->assertEquals($this->field_124, $mappings[20]->getField());
+        self::assertEquals(2, count($mappings));
+        self::assertEquals($this->tracker_10, $mappings[10]->getTracker());
+        self::assertEquals($this->field_123, $mappings[10]->getField());
+        self::assertInstanceOf(Cardwall_OnTop_Config_TrackerMappingFreestyle::class, $mappings[10]);
+        self::assertEquals([$this->field_122, $this->field_123], $mappings[10]->getAvailableFields());
+        self::assertEquals($this->tracker_20, $mappings[20]->getTracker());
+        self::assertEquals($this->field_124, $mappings[20]->getField());
     }
 
     public function testItUsesStatusFieldIfNoField(): void
     {
-        $this->value_mapping_factory->shouldReceive('getStatusMappings')->andReturns([]);
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->value_mapping_factory->method('getStatusMappings')->willReturn([]);
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 10, 'field_id' => null]
         ));
 
         $mappings = $this->factory->getMappings($this->tracker, $this->columns);
-        $this->assertCount(1, $mappings);
-        $this->assertEquals($this->status_field, $mappings[10]->getField());
-        $this->assertInstanceOf(\Cardwall_OnTop_Config_TrackerMappingStatus::class, $mappings[10]);
+        self::assertCount(1, $mappings);
+        self::assertEquals($this->status_field, $mappings[10]->getField());
+        self::assertInstanceOf(Cardwall_OnTop_Config_TrackerMappingStatus::class, $mappings[10]);
     }
 
     public function testItReturnsANoFieldMappingIfNothingInDBAndNoStatus(): void
     {
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 20, 'field_id' => null]
         ));
 
         $mappings = $this->factory->getMappings($this->tracker, $this->columns);
-        $this->assertCount(1, $mappings);
-        $this->assertInstanceOf(\Cardwall_OnTop_Config_TrackerMappingNoField::class, $mappings[20]);
+        self::assertCount(1, $mappings);
+        self::assertInstanceOf(Cardwall_OnTop_Config_TrackerMappingNoField::class, $mappings[20]);
     }
 
     public function testItReturnsEmptyMappingIfNoStatus(): void
     {
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 20, 'field_id' => null]
         ));
 
         $mappings = $this->factory->getMappings($this->tracker, $this->columns);
-        $this->assertCount(1, $mappings);
-        //TDOD: check type of what is returned
+        self::assertCount(1, $mappings);
+        self::assertInstanceOf(Cardwall_OnTop_Config_TrackerMappingNoField::class, $mappings[20]);
     }
 
     public function testItLoadValueMappings(): void
     {
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 20, 'field_id' => 124]
         ));
-        $this->value_mapping_factory->shouldReceive('getMappings')->with($this->tracker, $this->tracker_20, $this->field_124)
-            ->once()->andReturns([]);
+        $this->value_mapping_factory->expects(self::once())->method('getMappings')->with($this->tracker, $this->tracker_20, $this->field_124)
+            ->willReturn([]);
 
         $this->factory->getMappings($this->tracker, $this->columns);
     }
 
     public function testItLoadValueMappingsEvenForStatusField(): void
     {
-        $this->dao->shouldReceive('searchMappingFields')->with($this->tracker->getId())->andReturns(TestHelper::arrayToDar(
+        $this->dao->method('searchMappingFields')->with($this->tracker->getId())->willReturn(TestHelper::arrayToDar(
             ['tracker_id' => 10, 'field_id' => null]
         ));
-        $this->value_mapping_factory->shouldReceive('getStatusMappings')->with($this->tracker_10, $this->columns)
-            ->once()->andReturns([]);
+        $this->value_mapping_factory->expects(self::once())->method('getStatusMappings')->with($this->tracker_10, $this->columns)
+            ->willReturn([]);
 
         $this->factory->getMappings($this->tracker, $this->columns);
     }
