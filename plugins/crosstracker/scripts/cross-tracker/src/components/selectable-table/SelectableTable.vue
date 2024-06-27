1912f87c27cb6cfa53e2cd421e8bb630daa41db3
Joris Masson <joris.masson@enalean.com>
2024-06-27 15:45:16 +0200

    feat: Branch table depending on feature flag
    part of story #38263 Choose my own columns based on field name (numeric,
    text, dates)
    
    How to test:
    - No functional change in the "flag-less" widget.
    - Run the following command to enable the feature flag:
    
    $ tuleap config-set feature_flag_enable_tql_select 1
    
    - Reload the page. The widget should fetch the feature flag and show
    "Nothing here yet." in place of the artifact table.
    
    Change-Id: I951d2dfb46cfc396542d2c413476d156b0ef3c30

---
+++ plugins/crosstracker/scripts/cross-tracker/src/components/selectable-table/SelectableTable.vue
@@ -0,0 +1,32 @@
+<!--
+  - Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="selectable-table">
+        <p>Nothing here yet.</p>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.selectable-table {
+    margin: var(--tlp-medium-spacing) var(--tlp-medium-spacing) 0;
+}
+</style>
