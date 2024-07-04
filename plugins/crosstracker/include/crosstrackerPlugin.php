4fc52350f4da5b264315959abd0c352f8d2a566f
Kevin Traini <kevin.traini@enalean.com>
2024-07-04 15:39:42 +0200

    feat: Add select builder for description semantic
    Part of story #38648 Choose my own columns based on semantics and always there fields
    
    User can now select on description semantic with TQL. But nothing
    changes in API as the return format is not done yet
    
    *Testing:*
    
    In API explorer, Cross_Tracker_Reports -> GET
    /cross_tracker_reports/{id}/content
    
    You can select on description semantic, rest_api_log should display a
    json with columns @description and @description_format
    
    Change-Id: I900827ab1d88b962324aca0b75797347a2f85c0e

--- plugins/crosstracker/include/crosstrackerPlugin.php
+++ plugins/crosstracker/include/crosstrackerPlugin.php
@@ -65,6 +65,7 @@ use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\Text\TextSelec
 use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\UGroupList\UGroupListSelectFromBuilder;
 use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\UserList\UserListSelectFromBuilder;
 use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\MetadataSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\Description\DescriptionSelectFromBuilder;
 use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\Title\TitleSelectFromBuilder;
 use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilderVisitor;
 use Tuleap\CrossTracker\Report\SimilarField\BindNameVisitor;
@@ -322,6 +323,7 @@ class crosstrackerPlugin extends Plugin
             ),
             new MetadataSelectFromBuilder(
                 new TitleSelectFromBuilder(),
+                new DescriptionSelectFromBuilder(),
             ),
         );
         $purifier                = Codendi_HTMLPurifier::instance();
