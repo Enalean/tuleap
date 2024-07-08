88a4e5cc94bb41ca45044b5e8fa4a9708820aa74
Kevin Traini <kevin.traini@enalean.com>
2024-07-08 15:51:51 +0200

    feat: Add select builder for always there fields
    Part of story #38648 Choose my own columns based on semantics and always there fields
    
    USer can now select on always there fields with TQL. But nothing changes
    in API as the return format is not done yet
    
    *Testing:*
    
    The feature flag `feature_flag_enable_tql_select` need to be set to 1
    
    In your XTS widget write a query selecting always there fields
    @submitted_on, @last_update_date, @submitted_by, @last_update_by and
    @id. rest_api_log should display a json with columns:
    
    - @submitted_on -> a timestamp
    - @last_update_date -> a timestamp
    - @submitted_by -> a user id
    - @last_update_by -> a user id
    - @id -> an artifact id
    
    Change-Id: I96e458fd5a73ded42ad1f6a0e5b3dc5d045066ff

---
+++ plugins/crosstracker/tests/integration/CrossTracker/Report/Query/Advanced/Select/LastUpdateBySelectBuilderTest.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\CrossTracker\Report\Query\Advanced\Select;
+
+use PFUser;
+use ProjectUGroup;
+use Tracker;
+use Tracker_FormElementFactory;
+use Tuleap\CrossTracker\Report\Query\Advanced\CrossTrackerFieldTestCase;
+use Tuleap\CrossTracker\Report\Query\Advanced\DuckTypedField\FieldTypeRetrieverWrapper;
+use Tuleap\CrossTracker\Report\Query\Advanced\QueryBuilder\CrossTrackerExpertQueryReportDao;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\Date\DateSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\FieldSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\Numeric\NumericSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\StaticList\StaticListSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\Text\TextSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\UGroupList\UGroupListSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Field\UserList\UserListSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\MetadataSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\AssignedTo\AssignedToSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\Description\DescriptionSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\Status\StatusSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilder\Metadata\Semantic\Title\TitleSelectFromBuilder;
+use Tuleap\CrossTracker\Report\Query\Advanced\SelectBuilderVisitor;
+use Tuleap\DB\DBFactory;
+use Tuleap\Test\Builders\CoreDatabaseBuilder;
+use Tuleap\Tracker\Permission\TrackersPermissionsRetriever;
+use Tuleap\Tracker\Report\Query\Advanced\Grammar\Metadata;
+use Tuleap\Tracker\Test\Builders\TrackerDatabaseBuilder;
+
+final class LastUpdateBySelectBuilderTest extends CrossTrackerFieldTestCase
+{
+    private PFUser $user;
+    /**
+     * @var Tracker[]
+     */
+    private array $trackers;
+    /**
+     * @var list<int>
+     */
+    private array $artifact_ids;
+    /**
+     * @var array<int, ?int>
+     */
+    private array $expected_results;
+    private CrossTrackerExpertQueryReportDao $dao;
+    private SelectBuilderVisitor $builder;
+
+    public function setUp(): void
+    {
+        $db              = DBFactory::getMainTuleapDBConnection()->getDB();
+        $tracker_builder = new TrackerDatabaseBuilder($db);
+        $core_builder    = new CoreDatabaseBuilder($db);
+
+        $project    = $core_builder->buildProject();
+        $project_id = (int) $project->getID();
+        $this->user = $core_builder->buildUser('project_member', 'Project Member', 'project_member@example.com');
+        $core_builder->addUserToProjectMembers((int) $this->user->getId(), $project_id);
+
+        $alice = $core_builder->buildUser('alice', 'Alice', 'alice@example.com');
+        $bob   = $core_builder->buildUser('bob', 'Bob', 'bob@example.com');
+        $core_builder->addUserToProjectMembers((int) $alice->getId(), $project_id);
+        $core_builder->addUserToProjectMembers((int) $bob->getId(), $project_id);
+
+        $release_tracker = $tracker_builder->buildTracker($project_id, 'Release');
+        $sprint_tracker  = $tracker_builder->buildTracker($project_id, 'Sprint');
+        $this->trackers  = [$release_tracker, $sprint_tracker];
+
+        $release_last_update_by_field_id = $tracker_builder->buildLastUpdateByField($release_tracker->getId());
+        $sprint_last_update_by_field_id  = $tracker_builder->buildLastUpdateByField($sprint_tracker->getId());
+
+        $tracker_builder->setReadPermission(
+            $release_last_update_by_field_id,
+            ProjectUGroup::PROJECT_MEMBERS
+        );
+        $tracker_builder->setReadPermission(
+            $sprint_last_update_by_field_id,
+            ProjectUGroup::PROJECT_MEMBERS
+        );
+
+        $release_artifact_id = $tracker_builder->buildArtifact($release_tracker->getId());
+        $sprint_artifact_id  = $tracker_builder->buildArtifact($sprint_tracker->getId());
+        $this->artifact_ids  = [$release_artifact_id, $sprint_artifact_id];
+
+        $tracker_builder->buildLastChangeset($release_artifact_id, 1234567890, (int) $alice->getId());
+        $tracker_builder->buildLastChangeset($sprint_artifact_id, 1234567890, (int) $bob->getId());
+
+        $this->expected_results = [
+            $release_artifact_id => (int) $alice->getId(),
+            $sprint_artifact_id  => (int) $bob->getId(),
+        ];
+
+        $this->dao            = new CrossTrackerExpertQueryReportDao();
+        $form_element_factory = Tracker_FormElementFactory::instance();
+        $this->builder        = new SelectBuilderVisitor(
+            new FieldSelectFromBuilder(
+                $form_element_factory,
+                new FieldTypeRetrieverWrapper($form_element_factory),
+                TrackersPermissionsRetriever::build(),
+                new DateSelectFromBuilder(),
+                new TextSelectFromBuilder(),
+                new NumericSelectFromBuilder(),
+                new StaticListSelectFromBuilder(),
+                new UGroupListSelectFromBuilder(),
+                new UserListSelectFromBuilder(),
+            ),
+            new MetadataSelectFromBuilder(
+                new TitleSelectFromBuilder(),
+                new DescriptionSelectFromBuilder(),
+                new StatusSelectFromBuilder(),
+                new AssignedToSelectFromBuilder(),
+            ),
+        );
+    }
+
+    public function testItReturnsColumns(): void
+    {
+        $fragments = $this->builder->buildSelectFrom([new Metadata('last_update_by')], $this->trackers, $this->user);
+        $results   = $this->dao->searchArtifactsColumnsMatchingIds($fragments, $this->artifact_ids);
+
+        self::assertCount(2, $results);
+        $values = [];
+        foreach ($results as $result) {
+            self::assertArrayHasKey('id', $result);
+            self::assertArrayHasKey('@last_update_by', $result);
+            $values[$result['id']] = $result['@last_update_by'];
+        }
+        self::assertEqualsCanonicalizing($values, $this->expected_results);
+    }
+}
