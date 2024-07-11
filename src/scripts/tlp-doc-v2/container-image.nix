82752977f255a8d74f69aebf410936d3d4bb21c2
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-07-11 15:26:38 +0200

    feat: Have a Docker image to serve tlp-doc-v2 content
    This image is expected to be put behind a reverse proxy dealing with the
    TLS terminaison.
    
    The image is built with Nix, this way the image will at least be updated
    when we bump the nixpkgs pin. Building from a Dockerfile and a `nginx`
    base image is likely to not be kept updated on regular basis.
    
    To test:
    1. Build Storybook content with `TURBO_LOG_ORDER=grouped pnpm turbo run build-storybook --no-daemon --filter=@tuleap/tlp-doc-v2`
    2. Build and start the Docker image with
       `docker load < $(nix-build --no-out-link ./src/scripts/tlp-doc-v2/container-image.nix) && docker run --rm -p 127.0.0.1:8080:8080 tlp-doc-v2`
    3. Go to http://127.0.0.1:8080
    
    Part of request #37211 Replace TLP-doc documentation
    
    Change-Id: Ieb8537703b7772acc86c57ee4ac77e7bfd6862ba

---
+++ src/scripts/tlp-doc-v2/container-image.nix
@@ -0,0 +1,53 @@
+{
+  pkgs ? (import ../../../tools/utils/nix/pinned-nixpkgs.nix) { },
+}:
+
+let
+  nginxPort = "8080";
+  nginxConf = pkgs.writeText "nginx.conf" ''
+    user nobody nobody;
+    daemon off;
+    error_log /dev/stdout info;
+    pid /dev/null;
+    events {}
+    http {
+      access_log /dev/stdout;
+      server {
+        listen ${nginxPort};
+        index index.html;
+        location / {
+          root ${./storybook-static};
+        }
+        include ${pkgs.mailcap}/etc/nginx/mime.types;
+        add_header Referrer-Policy "no-referrer" always;
+        add_header Cross-Origin-Embedder-Policy "require-corp" always;
+        add_header Cross-Origin-Resource-Policy "same-origin" always;
+        add_header Cross-Origin-Opener-Policy "same-origin" always;
+        add_header Vary "Sec-Fetch-Site" always;
+        add_header Content-Security-Policy "default-src 'none'; script-src 'self' 'unsafe-eval'; script-src-elem 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline'; img-src 'self'; font-src 'self'; connect-src 'self'; media-src 'self'; frame-src 'self'; block-all-mixed-content; disown-opener; base-uri 'none'" always;
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+      }
+    }
+  '';
+in pkgs.dockerTools.buildLayeredImage {
+  name = "tlp-doc-v2";
+  tag = "latest";
+  contents = [
+    pkgs.fakeNss
+  ];
+
+  extraCommands = ''
+    mkdir -p tmp/nginx_client_body
+    mkdir -p var/log/nginx
+  '';
+
+  config = {
+    Cmd = [ "${pkgs.nginx}/bin/nginx" "-c" nginxConf ];
+    ExposedPorts = {
+      "${nginxPort}/tcp" = {};
+    };
+  };
+}
