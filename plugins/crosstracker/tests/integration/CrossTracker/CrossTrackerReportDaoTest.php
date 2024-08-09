bb7abaebdc3edc13fc938def23580dd5c2f45213
Joris Masson <joris.masson@enalean.com>
2024-08-09 16:37:37 +0200

    chore: DB test for XTS report DAO
    part of request #40099 Cross-Tracker search finishing touches 16.1
    
    No functional change. DB integration tests should pass.
    
    Why?
    In order to properly duplicate the widget data when inheriting from
    another project, we will need to touch this DAO. The tests will ensure
    non-regression.
    Note that I still had to change to tryFlatTransaction() in the same
    change, because the tests start a transaction and then roll it back.
    Without this change, the tests fail because a transaction is already
    ongoing.
    
    Change-Id: Ib061109834e9134e8c7072feff46ac98bc238ddf

---
+++ plugins/crosstracker/tests/integration/CrossTracker/CrossTrackerReportDaoTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\CrossTracker;
+
+use Tuleap\DB\DBFactory;
+use Tuleap\Test\PHPUnit\TestIntegrationTestCase;
+use Tuleap\Tracker\Test\Builders\TrackerDatabaseBuilder;
+
+final class CrossTrackerReportDaoTest extends TestIntegrationTestCase
+{
+    private const PROJECT_ID = 134;
+    private CrossTrackerReportDao $report_dao;
+    private TrackerDatabaseBuilder $tracker_builder;
+
+    protected function setUp(): void
+    {
+        $this->report_dao      = new CrossTrackerReportDao();
+        $this->tracker_builder = new TrackerDatabaseBuilder(DBFactory::getMainTuleapDBConnection()->getDB());
+    }
+
+    public function testCRUD(): void
+    {
+        $first_tracker  = $this->tracker_builder->buildTracker(self::PROJECT_ID, 'stories');
+        $second_tracker = $this->tracker_builder->buildTracker(self::PROJECT_ID, 'bugs');
+        $this->tracker_builder->buildTracker(self::PROJECT_ID, 'tasks'); // To check that not all trackers are used
+
+        $report_id = $this->createAndRetrieveEmptyReport();
+        $this->updateAndRetrieveDefaultModeReport($report_id, $first_tracker, $second_tracker);
+        $this->checkTrackersAreUsed($first_tracker, $second_tracker);
+        $this->updateAndRetrieveExpertModeReport($report_id);
+        $this->deleteAndRetrieve($report_id);
+    }
+
+    private function createAndRetrieveEmptyReport(): int
+    {
+        $report_id = (int) $this->report_dao->create();
+
+        $retrieved_report = $this->report_dao->searchReportById($report_id);
+        self::assertNotNull($retrieved_report);
+        self::assertSame('', $retrieved_report['expert_query']);
+        self::assertSame(0, $retrieved_report['expert_mode']);
+
+        return $report_id;
+    }
+
+    private function updateAndRetrieveDefaultModeReport(
+        int $report_id,
+        \Tracker $first_tracker,
+        \Tracker $second_tracker,
+    ): void {
+        $expert_query = '@title != ""';
+        $this->report_dao->updateReport($report_id, [$first_tracker, $second_tracker], $expert_query, false);
+
+        $retrieved_report = $this->report_dao->searchReportById($report_id);
+        self::assertNotNull($retrieved_report);
+        self::assertSame($expert_query, $retrieved_report['expert_query']);
+        self::assertSame(0, $retrieved_report['expert_mode']);
+
+        $retrieved_trackers = $this->report_dao->searchReportTrackersById($report_id);
+        self::assertCount(2, $retrieved_trackers);
+        [$first_retrieved_tracker, $second_retrieved_tracker] = $retrieved_trackers;
+        self::assertSame($first_tracker->getId(), $first_retrieved_tracker['tracker_id']);
+        self::assertSame($second_tracker->getId(), $second_retrieved_tracker['tracker_id']);
+    }
+
+    private function checkTrackersAreUsed(\Tracker $first_tracker, \Tracker $second_tracker): void
+    {
+        $rows             = $this->report_dao->searchTrackersIdUsedByCrossTrackerByProjectId(self::PROJECT_ID);
+        $used_tracker_ids = array_map(static fn(array $row): int => $row['id'], $rows);
+        self::assertCount(2, $used_tracker_ids);
+        self::assertContains($first_tracker->getId(), $used_tracker_ids);
+        self::assertContains($second_tracker->getId(), $used_tracker_ids);
+    }
+
+    private function updateAndRetrieveExpertModeReport(int $report_id): void
+    {
+        $expert_query = "SELECT @id FROM @project = 'self' WHERE @title != ''";
+        $this->report_dao->updateReport($report_id, [], $expert_query, true);
+
+        $retrieved_report = $this->report_dao->searchReportById($report_id);
+        self::assertNotNull($retrieved_report);
+        self::assertSame($expert_query, $retrieved_report['expert_query']);
+        self::assertSame(1, $retrieved_report['expert_mode']);
+
+        self::assertEmpty($this->report_dao->searchReportTrackersById($report_id));
+    }
+
+    private function deleteAndRetrieve(int $report_id): void
+    {
+        $this->report_dao->delete($report_id);
+
+        self::assertNull($this->report_dao->searchReportById($report_id));
+        self::assertEmpty($this->report_dao->searchReportTrackersById($report_id));
+        self::assertEmpty($this->report_dao->searchTrackersIdUsedByCrossTrackerByProjectId(self::PROJECT_ID));
+    }
+}
