821baab45ae832d3a528434a44ca02ac1594364d
Joris Masson <joris.masson@enalean.com>
2024-08-20 15:16:51 +0200

    chore: Drop Mockery usage in MasschangeUpdaterTest
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    No functional change. Unit tests should pass.
    
    Change-Id: I057e44bce3d7e37a2efc5293b991dbb1afc2e192

--- plugins/tracker/tests/unit/Tracker/Masschange/MasschangeUpdaterTest.php
+++ plugins/tracker/tests/unit/Tracker/Masschange/MasschangeUpdaterTest.php
@@ -22,221 +22,196 @@ declare(strict_types=1);
 
 namespace Tuleap\Tracker\Masschange;
 
-use Codendi_Request;
-use EventManager;
-use Mockery;
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use PFUser;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\MockObject\Stub;
 use Tracker;
-use Tracker_Artifact_Changeset;
-use Tracker_Artifact_ChangesetValue;
-use Tracker_ArtifactDao;
-use Tracker_ArtifactFactory;
-use Tracker_FormElementFactory;
-use Tracker_MasschangeDataValueExtractor;
-use Tracker_Report;
 use Tracker_Rule_List;
-use Tracker_RuleFactory;
-use Tuleap\Layout\BaseLayout;
+use Tuleap\GlobalResponseMock;
+use Tuleap\Test\Builders\UserTestBuilder;
 use Tuleap\Tracker\Artifact\Artifact;
+use Tuleap\Tracker\Test\Builders\ChangesetTestBuilder;
+use Tuleap\Tracker\Test\Builders\ChangesetValueListTestBuilder;
+use Tuleap\Tracker\Test\Builders\Fields\List\ListStaticBindBuilder;
 use Tuleap\Tracker\Test\Builders\Fields\ListFieldBuilder;
 
 final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
 {
-    use MockeryPHPUnitIntegration;
+    use GlobalResponseMock;
+
+    private const USER_ID = 963;
+    private \Tracker_ArtifactDao & MockObject $artifact_dao;
+    private \Tracker_FormElementFactory & Stub $form_element_factory;
+    private \Tracker_ArtifactFactory & Stub $artifact_factory;
 
     protected function setUp(): void
     {
-        parent::setUp();
-        $GLOBALS['Response'] = Mockery::spy(BaseLayout::class);
+        $this->artifact_dao         = $this->createMock(\Tracker_ArtifactDao::class);
+        $this->form_element_factory = $this->createStub(\Tracker_FormElementFactory::class);
+        $this->artifact_factory     = $this->createStub(\Tracker_ArtifactFactory::class);
     }
 
-    protected function tearDown(): void
-    {
-        unset($GLOBALS['Response']);
-        parent::tearDown();
+    /**
+     * @param list<int>               $masschange_artifact_ids
+     * @param list<Tracker_Rule_List> $rules
+     */
+    private function update(
+        \Tracker $tracker,
+        array $new_values,
+        array $masschange_artifact_ids,
+        bool $unsubscribe_from_notifications,
+        array $rules,
+    ): void {
+        $request = new \Codendi_Request([
+            'masschange_aids' => $masschange_artifact_ids,
+            'masschange-unsubscribe-option' => $unsubscribe_from_notifications,
+            'artifact' => $new_values,
+            'artifact_masschange_followup_comment' => '',
+        ]);
+
+        $rules_factory = $this->createStub(\Tracker_RuleFactory::class);
+        $rules_factory->method('getAllListRulesByTrackerWithOrder')->willReturn($rules);
+
+        $masschange_data_value_extractor = $this->createStub(\Tracker_MasschangeDataValueExtractor::class);
+        $masschange_data_value_extractor->method('getNewValues')->willReturn($new_values);
+
+        $event_manager = $this->createMock(\EventManager::class);
+        $event_manager->expects(self::once())->method('processEvent');
+
+        $updater = new MasschangeUpdater(
+            $tracker,
+            $this->createStub(\Tracker_Report::class),
+            $masschange_data_value_extractor,
+            $rules_factory,
+            $this->form_element_factory,
+            $this->artifact_factory,
+            $this->artifact_dao,
+            $event_manager
+        );
+        $updater->updateArtifacts(UserTestBuilder::buildWithId(self::USER_ID), $request);
     }
 
     public function testUpdateArtifactsWithoutBeenUnsubscribedFromNotifications(): void
     {
-        $tracker = Mockery::mock(Tracker::class);
-        $tracker->shouldReceive('getId')->andReturn(123);
-        $tracker->shouldReceive('userIsAdmin')->andReturn(true);
-        $tracker->shouldReceive('augmentDataFromRequest');
+        $tracker = $this->createMock(Tracker::class);
+        $tracker->method('getId')->willReturn(123);
+        $tracker->method('userIsAdmin')->willReturn(true);
+        $tracker->expects(self::once())->method('augmentDataFromRequest');
 
-        $request = Mockery::spy(Codendi_Request::class);
-        $request->shouldReceive('get')->with('masschange_aids')->andReturn([201, 202]);
-        $request->shouldReceive('get')->with('masschange-unsubscribe-option')->andReturn(false);
         $new_values = [1 => 'Value01'];
-        $request->shouldReceive('get')->with('artifact')->andReturn($new_values);
-        $request->shouldReceive('get')->with('artifact_masschange_followup_comment')->andReturn('');
 
-        $masschange_data_value_extractor = Mockery::mock(Tracker_MasschangeDataValueExtractor::class);
-        $masschange_data_value_extractor->shouldReceive('getNewValues')->andReturn($new_values);
+        $artifact_201 = $this->createMock(Artifact::class);
+        $artifact_201->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset('352')->build());
+        $artifact_202 = $this->createMock(Artifact::class);
+        $artifact_202->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset('311')->build());
+        $this->artifact_factory->method('getArtifactById')->willReturnMap([
+            [201, $artifact_201],
+            [202, $artifact_202],
+        ]);
 
-        $rules_factory = Mockery::mock(Tracker_RuleFactory::class);
-        $rules_factory->shouldReceive('getAllListRulesByTrackerWithOrder')->andReturn([]);
+        $artifact_201->expects(self::once())->method('createNewChangeset');
+        $artifact_202->expects(self::once())->method('createNewChangeset');
 
-        $artifact_factory = Mockery::mock(Tracker_ArtifactFactory::class);
-        $artifact_201     = Mockery::mock(Artifact::class);
-        $artifact_201->shouldReceive('getLastChangeset')->andReturn(Mockery::mock(Tracker_Artifact_Changeset::class));
-        $artifact_201->shouldReceive('createNewChangeset')->once();
-        $artifact_factory->shouldReceive('getArtifactById')->with(201)->andReturn($artifact_201);
-        $artifact_202 = Mockery::mock(Artifact::class);
-        $artifact_202->shouldReceive('getLastChangeset')->andReturn(Mockery::spy(Tracker_Artifact_Changeset::class));
-        $artifact_202->shouldReceive('createNewChangeset')->once();
-        $artifact_factory->shouldReceive('getArtifactById')->with(202)->andReturn($artifact_202);
-
-        $event_manager = Mockery::mock(EventManager::class);
-        $event_manager->shouldReceive('processEvent')->once();
-
-        $masschange_updater = new MasschangeUpdater(
-            $tracker,
-            Mockery::mock(Tracker_Report::class),
-            $masschange_data_value_extractor,
-            $rules_factory,
-            Mockery::mock(Tracker_FormElementFactory::class),
-            $artifact_factory,
-            Mockery::mock(Tracker_ArtifactDao::class),
-            $event_manager
-        );
-        $masschange_updater->updateArtifacts(Mockery::mock(PFUser::class), $request);
+        $this->update($tracker, $new_values, [201, 202], false, []);
     }
 
     public function testUpdateArtifactsAndUserHasBeenUnsubscribedFromNotifications(): void
     {
-        $tracker = Mockery::mock(Tracker::class);
-        $tracker->shouldReceive('getId')->andReturn(123);
-        $tracker->shouldReceive('userIsAdmin')->andReturn(true);
-        $tracker->shouldReceive('augmentDataFromRequest');
+        $tracker = $this->createMock(Tracker::class);
+        $tracker->method('getId')->willReturn(123);
+        $tracker->method('userIsAdmin')->willReturn(true);
+        $tracker->expects(self::once())->method('augmentDataFromRequest');
 
-        $request = Mockery::spy(Codendi_Request::class);
-        $request->shouldReceive('get')->with('masschange_aids')->andReturn([201, 202]);
-        $request->shouldReceive('get')->with('masschange-unsubscribe-option')->andReturn(true);
         $new_values = [1 => 'Value01'];
-        $request->shouldReceive('get')->with('artifact')->andReturn($new_values);
-        $request->shouldReceive('get')->with('artifact_masschange_followup_comment')->andReturn('');
 
-        $masschange_data_value_extractor = Mockery::mock(Tracker_MasschangeDataValueExtractor::class);
-        $masschange_data_value_extractor->shouldReceive('getNewValues')->andReturn($new_values);
+        $artifact_201 = $this->createMock(Artifact::class);
+        $artifact_201->method('getId')->willReturn(201);
+        $artifact_201->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset('352')->build());
+        $artifact_201->method('userCanView')->willReturn(true);
+        $artifact_202 = $this->createMock(Artifact::class);
+        $artifact_202->method('getId')->willReturn(202);
+        $artifact_202->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset('311')->build());
+        $artifact_202->method('userCanView')->willReturn(true);
+        $this->artifact_factory->method('getArtifactById')->willReturnMap([
+            [201, $artifact_201],
+            [202, $artifact_202],
+        ]);
 
-        $rules_factory = Mockery::mock(Tracker_RuleFactory::class);
-        $rules_factory->shouldReceive('getAllListRulesByTrackerWithOrder')->andReturn([]);
-
-        $artifact_factory = Mockery::mock(Tracker_ArtifactFactory::class);
-        $artifact_201     = Mockery::mock(Artifact::class);
-        $artifact_201->shouldReceive('getId')->andReturn(201);
-        $artifact_201->shouldReceive('getLastChangeset')->andReturn(Mockery::mock(Tracker_Artifact_Changeset::class));
-        $artifact_201->shouldReceive('userCanView')->andReturn(true);
-        $artifact_201->shouldReceive('createNewChangeset');
-        $artifact_factory->shouldReceive('getArtifactById')->with(201)->andReturn($artifact_201);
-        $artifact_202 = Mockery::mock(Artifact::class);
-        $artifact_202->shouldReceive('getLastChangeset')->andReturn(Mockery::spy(Tracker_Artifact_Changeset::class));
-        $artifact_202->shouldReceive('getId')->andReturn(202);
-        $artifact_202->shouldReceive('userCanView')->andReturn(true);
-        $artifact_202->shouldReceive('createNewChangeset');
-        $artifact_factory->shouldReceive('getArtifactById')->with(202)->andReturn($artifact_202);
-
-        $artifact_dao = Mockery::mock(Tracker_ArtifactDao::class);
-
-        $event_manager = Mockery::mock(EventManager::class);
-        $event_manager->shouldReceive('processEvent')->once();
-
-        $masschange_updater = new MasschangeUpdater(
-            $tracker,
-            Mockery::mock(Tracker_Report::class),
-            $masschange_data_value_extractor,
-            $rules_factory,
-            Mockery::mock(Tracker_FormElementFactory::class),
-            $artifact_factory,
-            $artifact_dao,
-            $event_manager
+        $this->artifact_dao->expects(self::exactly(2))->method('createUnsubscribeNotification')->withConsecutive(
+            [201, self::USER_ID],
+            [202, self::USER_ID]
         );
+        $artifact_201->expects(self::once())->method('createNewChangeset');
+        $artifact_202->expects(self::once())->method('createNewChangeset');
 
-        $user = Mockery::mock(PFUser::class);
-        $user->shouldReceive('getId')->andReturn(963);
-
-        $artifact_dao->shouldReceive('createUnsubscribeNotification')->with(201, 963)->atLeast()->once();
-        $artifact_dao->shouldReceive('createUnsubscribeNotification')->with(202, 963)->atLeast()->once();
-
-        $masschange_updater->updateArtifacts($user, $request);
+        $this->update($tracker, $new_values, [201, 202], true, []);
     }
 
     public function testDataRetrievedFromTheRequestIsConsolidated(): void
     {
-        $tracker = Mockery::mock(Tracker::class);
-        $tracker->shouldReceive('getId')->andReturn(123);
-        $tracker->shouldReceive('userIsAdmin')->andReturn(true);
-        $tracker->shouldReceive('augmentDataFromRequest')->with(
-            Mockery::on(static function (array $data): bool {
-                return isset($data['request_method_called']) && $data['request_method_called'] === 'artifact-masschange';
-            })
+        $tracker = $this->createMock(Tracker::class);
+        $tracker->method('getId')->willReturn(123);
+        $tracker->method('userIsAdmin')->willReturn(true);
+        $tracker->expects(self::once())->method('augmentDataFromRequest')->with(
+            self::callback(
+                static fn(
+                    array $data,
+                ) => isset($data['request_method_called']) && $data['request_method_called'] === 'artifact-masschange'
+            )
         );
 
-        $request = Mockery::spy(Codendi_Request::class);
-        $request->shouldReceive('get')->with('masschange_aids')->andReturn([201]);
-        $request->shouldReceive('get')->with('masschange-unsubscribe-option')->andReturn(false);
         $new_values = [1 => 'Value01'];
-        $request->shouldReceive('get')->with('artifact')->andReturn($new_values);
-        $request->shouldReceive('get')->with('artifact_masschange_followup_comment')->andReturn('');
 
-        $masschange_data_value_extractor = Mockery::mock(Tracker_MasschangeDataValueExtractor::class);
-        $masschange_data_value_extractor->shouldReceive('getNewValues')->andReturn($new_values);
+        $rule_1 = $this->createStub(Tracker_Rule_List::class);
+        $rule_1->method('getSourceFieldId')->willReturn(3201);
+        $rule_1->method('getTargetFieldId')->willReturn(3202);
+        $rule_2 = $this->createStub(Tracker_Rule_List::class);
+        $rule_2->method('getSourceFieldId')->willReturn(3202);
+        $rule_2->method('getTargetFieldId')->willReturn(3203);
 
-        $rules_factory = Mockery::mock(Tracker_RuleFactory::class);
-        $rule_1        = Mockery::mock(Tracker_Rule_List::class);
-        $rule_1->shouldReceive('getSourceFieldId')->andReturn(3201);
-        $rule_1->shouldReceive('getTargetFieldId')->andReturn(3202);
-        $rule_2 = Mockery::mock(Tracker_Rule_List::class);
-        $rule_2->shouldReceive('getSourceFieldId')->andReturn(3202);
-        $rule_2->shouldReceive('getTargetFieldId')->andReturn(3203);
-        $rules_factory->shouldReceive('getAllListRulesByTrackerWithOrder')->andReturn([$rule_1, $rule_2]);
+        $source_bind_rule1                 = ListStaticBindBuilder::aStaticBind(
+            ListFieldBuilder::aListField(3201)->build()
+        )->withStaticValues([321 => 'Open', 322 => 'Closed'])->build();
+        $source_and_target_bind_both_rules = ListStaticBindBuilder::aStaticBind(
+            ListFieldBuilder::aListField(3202)->build()
+        )->withStaticValues([261 => 'Open', 262 => 'Closed'])->build();
+        $target_bind_rule2                 = ListStaticBindBuilder::aStaticBind(
+            ListFieldBuilder::aListField(3303)->build()
+        )->withStaticValues([333 => 'Code Yellow', 334 => 'Code Green'])->build();
+        $this->form_element_factory->method('getUsedListFieldById')->willReturnMap([
+            [$tracker, 3201, $source_bind_rule1->getField()],
+            [$tracker, 3202, $source_and_target_bind_both_rules->getField()],
+            [$tracker, 3203, $target_bind_rule2->getField()],
+        ]);
 
-        $form_element_factory               = Mockery::mock(Tracker_FormElementFactory::class);
-        $source_field_rule1                 = ListFieldBuilder::aListField(3201)->build();
-        $source_and_target_field_both_rules = ListFieldBuilder::aListField(3202)->build();
-        $target_field_rule2                 = ListFieldBuilder::aListField(3203)->build();
-        $form_element_factory->shouldReceive('getUsedListFieldById')->with($tracker, 3201)->andReturn($source_field_rule1);
-        $form_element_factory->shouldReceive('getUsedListFieldById')->with($tracker, 3202)->andReturn($source_and_target_field_both_rules);
-        $form_element_factory->shouldReceive('getUsedListFieldById')->with($tracker, 3203)->andReturn($target_field_rule2);
+        $artifact       = $this->createMock(Artifact::class);
+        $last_changeset = ChangesetTestBuilder::aChangeset('968')->build();
+        $artifact->method('getLastChangeset')->willReturn($last_changeset);
+        $this->artifact_factory->method('getArtifactById')->willReturn($artifact);
 
-        $artifact       = Mockery::mock(Artifact::class);
-        $last_changeset = Mockery::mock(Tracker_Artifact_Changeset::class);
-        $artifact->shouldReceive('getLastChangeset')->andReturn($last_changeset);
-        $artifact_factory = Mockery::mock(Tracker_ArtifactFactory::class);
-        $artifact_factory->shouldReceive('getArtifactById')->andReturn($artifact);
+        ChangesetValueListTestBuilder::aListOfValue(
+            418,
+            $last_changeset,
+            $source_bind_rule1->getField()
+        )->withValues([$source_bind_rule1->getValue(321)])->build();
+        ChangesetValueListTestBuilder::aListOfValue(
+            618,
+            $last_changeset,
+            $source_and_target_bind_both_rules->getField()
+        )->withValues([])->build();
+        ChangesetValueListTestBuilder::aListOfValue(
+            374,
+            $last_changeset,
+            $target_bind_rule2->getField()
+        )->withValues([$target_bind_rule2->getValue(333), $target_bind_rule2->getValue(334)])->build();
 
-        $changeset_value_source_rule1 = Mockery::mock(Tracker_Artifact_ChangesetValue::class);
-        $changeset_value_source_rule1->shouldReceive('getValue')->andReturn([321]);
-        $last_changeset->shouldReceive('getValue')->with($source_field_rule1)->andReturn($changeset_value_source_rule1);
-        $changeset_value_source_and_target = Mockery::mock(Tracker_Artifact_ChangesetValue::class);
-        $changeset_value_source_and_target->shouldReceive('getValue')->andReturn('');
-        $last_changeset->shouldReceive('getValue')->with($source_and_target_field_both_rules)->andReturn($changeset_value_source_and_target);
-        $target_value_rule2 = Mockery::mock(Tracker_Artifact_ChangesetValue::class);
-        $target_value_rule2->shouldReceive('getValue')->andReturn([333, 334]);
-        $last_changeset->shouldReceive('getValue')->with($target_field_rule2)->andReturn($target_value_rule2);
-
-        $artifact->shouldReceive('createNewChangeset')->with(
+        $artifact->expects(self::once())->method('createNewChangeset')->with(
             [1 => 'Value01', 3202 => 100],
-            Mockery::any(),
-            Mockery::any(),
-            Mockery::any(),
-            Mockery::any()
+            self::anything(),
+            self::anything(),
+            self::anything(),
+            self::anything(),
         );
 
-        $event_manager = Mockery::mock(EventManager::class);
-        $event_manager->shouldReceive('processEvent')->once();
-
-        $masschange_updater = new MasschangeUpdater(
-            $tracker,
-            Mockery::mock(Tracker_Report::class),
-            $masschange_data_value_extractor,
-            $rules_factory,
-            $form_element_factory,
-            $artifact_factory,
-            Mockery::mock(Tracker_ArtifactDao::class),
-            $event_manager
-        );
-        $masschange_updater->updateArtifacts(Mockery::mock(PFUser::class), $request);
+        $this->update($tracker, $new_values, [201], false, [$rule_1, $rule_2]);
     }
 }
