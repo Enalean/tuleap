6066707307727424672515aa1cb64c846a79844a
Kevin Traini <kevin.traini@enalean.com>
2024-09-18 09:22:02 +0200

    chore: Drop Mockery usage in some Docman tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    3/12
    
    *No functional changes expected*
    
    Change-Id: I538e6baa20408bd27d7a1ee4fd4e7e2492958188

--- plugins/docman/tests/unit/NotificationsManager_DeleteTest.php
+++ plugins/docman/tests/unit/NotificationsManager_DeleteTest.php
@@ -20,84 +20,96 @@
  *
  */
 
-require_once 'bootstrap.php';
+declare(strict_types=1);
 
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Tuleap\Docman\ExternalLinks\ILinkUrlProvider;
+namespace Tuleap\Docman;
 
-//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
-class NotificationsManager_DeleteTest extends \Tuleap\Test\PHPUnit\TestCase
+use ArrayIterator;
+use Docman_Item;
+use Docman_NotificationsManager_Delete;
+use Docman_PermissionsManager;
+use Feedback;
+use MailBuilder;
+use Tuleap\Docman\Notifications\NotifiedPeopleRetriever;
+use Tuleap\Docman\Notifications\UGroupsRetriever;
+use Tuleap\Docman\Notifications\UgroupsUpdater;
+use Tuleap\Docman\Notifications\UsersRetriever;
+use Tuleap\Docman\Notifications\UsersToNotifyDao;
+use Tuleap\Docman\Notifications\UsersUpdater;
+use Tuleap\Document\LinkProvider\DocumentLinkProvider;
+use Tuleap\Test\Builders\ProjectTestBuilder;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
+use UserManager;
+
+final class NotificationsManager_DeleteTest extends TestCase //phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
 {
-    use MockeryPHPUnitIntegration;
-
     /*
      * Test the case when deleting a docman item the notification mail
      * is sent to all monitoring users not only one of them
      */
-    public function testStoreEventsDoNotOverrideUsers()
+    public function testStoreEventsDoNotOverrideUsers(): void
     {
-        $listeningUsers = new ArrayIterator([['user_id' => 1,
-            'item_id' => 1,
-        ],
-            ['user_id' => 2,
-                'item_id' => 1,
-            ],
-            ['user_id' => 3,
-                'item_id' => 1,
-            ],
+        $listening_users = new ArrayIterator([
+            ['user_id' => 1, 'item_id' => 1],
+            ['user_id' => 2, 'item_id' => 1],
+            ['user_id' => 3, 'item_id' => 1],
         ]);
 
-        $user1 = \Mockery::spy(\PFUser::class);
-        $user1->shouldReceive('getId')->andReturns(1);
-        $user2 = \Mockery::spy(\PFUser::class);
-        $user2->shouldReceive('getId')->andReturns(2);
-        $user3 = \Mockery::spy(\PFUser::class);
-        $user3->shouldReceive('getId')->andReturns(3);
-        $um = \Mockery::spy(\UserManager::class);
-        $um->shouldReceive('getUserById')->once()->andReturns($user1);
-        $um->shouldReceive('getUserById')->once()->andReturns($user2);
-        $um->shouldReceive('getUserById')->once()->andReturns($user3);
+        $user1 = UserTestBuilder::buildWithId(1);
+        $user2 = UserTestBuilder::buildWithId(2);
+        $user3 = UserTestBuilder::buildWithId(3);
+        $um    = $this->createMock(UserManager::class);
+        $um->method('getUserById')->willReturnCallback(static fn(int $id) => match ($id) {
+            1 => $user1,
+            2 => $user2,
+            3 => $user3,
+        });
 
-        $dpm = \Mockery::spy(\Docman_PermissionsManager::class);
-        $dpm->shouldReceive('userCanRead')->andReturns(true);
-        $dpm->shouldReceive('userCanAccess')->andReturns(true);
+        $dpm = $this->createMock(Docman_PermissionsManager::class);
+        $dpm->method('userCanRead')->willReturn(true);
+        $dpm->method('userCanAccess')->willReturn(true);
 
-        $item = \Mockery::spy(\Docman_Item::class);
-        $item->shouldReceive('getId')->andReturns(1);
-        $params = ['item' => $item];
+        $params = ['item' => new Docman_Item(['item_id' => 1])];
 
-        $project                   = \Mockery::spy(\Project::class, ['getID' => 101, 'getUserName' => false, 'isPublic' => false]);
-        $feedback                  = \Mockery::spy(\Feedback::class);
-        $mail_builder              = \Mockery::spy(\MailBuilder::class);
-        $notifications_dao         = \Mockery::spy(\Tuleap\Docman\Notifications\UsersToNotifyDao::class);
-        $notified_people_retriever = \Mockery::spy(\Tuleap\Docman\Notifications\NotifiedPeopleRetriever::class);
-        $notified_people_retriever->shouldReceive('getNotifiedUsers')->andReturns($listeningUsers);
-        $users_remover   = \Mockery::spy(\Tuleap\Docman\Notifications\UsersUpdater::class);
-        $ugroups_remover = \Mockery::spy(\Tuleap\Docman\Notifications\UgroupsUpdater::class);
+        $project                   = ProjectTestBuilder::aProject()->withId(101)->withAccessPrivate()->build();
+        $feedback                  = $this->createMock(Feedback::class);
+        $mail_builder              = $this->createMock(MailBuilder::class);
+        $notifications_dao         = $this->createMock(UsersToNotifyDao::class);
+        $notified_people_retriever = $this->createMock(NotifiedPeopleRetriever::class);
+        $notified_people_retriever->method('getNotifiedUsers')->willReturn($listening_users);
+        $users_remover   = $this->createMock(UsersUpdater::class);
+        $ugroups_remover = $this->createMock(UgroupsUpdater::class);
 
-        $notifications_manager = \Mockery::mock(\Docman_NotificationsManager_Delete::class)->makePartial()->shouldAllowMockingProtectedMethods();
-        $link_url_provider     = Mockery::mock(ILinkUrlProvider::class);
-        $notifications_manager->__construct(
-            $project,
-            $link_url_provider,
-            $feedback,
-            $mail_builder,
-            $notifications_dao,
-            \Mockery::spy(\Tuleap\Docman\Notifications\UsersRetriever::class),
-            \Mockery::spy(\Tuleap\Docman\Notifications\UGroupsRetriever::class),
-            $notified_people_retriever,
-            $users_remover,
-            $ugroups_remover
-        );
-        $notifications_manager->shouldReceive('_getUserManager')->andReturns($um);
-        $notifications_manager->shouldReceive('_getPermissionsManager')->andReturns($dpm);
-        $notifications_manager->shouldReceive('getUrlProvider')->andReturns($link_url_provider);
+        $link_url_provider     = new DocumentLinkProvider('', $project);
+        $notifications_manager = $this->getMockBuilder(Docman_NotificationsManager_Delete::class)
+            ->setConstructorArgs([
+                $project,
+                $link_url_provider,
+                $feedback,
+                $mail_builder,
+                $notifications_dao,
+                $this->createMock(UsersRetriever::class),
+                $this->createMock(UGroupsRetriever::class),
+                $notified_people_retriever,
+                $users_remover,
+                $ugroups_remover,
+            ])
+            ->onlyMethods([
+                '_getUserManager',
+                '_getPermissionsManager',
+                'getUrlProvider',
+            ])
+            ->getMock();
+        $notifications_manager->method('_getUserManager')->willReturn($um);
+        $notifications_manager->method('_getPermissionsManager')->willReturn($dpm);
+        $notifications_manager->method('getUrlProvider')->willReturn($link_url_provider);
         $notifications_manager->_listeners = [];
 
         $notifications_manager->_storeEvents(1, 'removed', $params);
 
-        $this->assertEquals($user1, $notifications_manager->_listeners[1]['user']);
-        $this->assertEquals($user2, $notifications_manager->_listeners[2]['user']);
-        $this->assertEquals($user3, $notifications_manager->_listeners[3]['user']);
+        self::assertEquals($user1, $notifications_manager->_listeners[1]['user']);
+        self::assertEquals($user2, $notifications_manager->_listeners[2]['user']);
+        self::assertEquals($user3, $notifications_manager->_listeners[3]['user']);
     }
 }
