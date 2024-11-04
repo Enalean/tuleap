e6e901f64cf0ae671e2d178a2ee321cba3243fe7
Thomas Gerbet <thomas.gerbet@enalean.com>
2024-11-04 17:21:57 +0100

    Drop old configuration info related to the dbauthuser
    Support for it was removed in Tuleap 15.0 (97d0bbeb5d5e382ae7e9635741031a5662d680fd),
    we should not keep credentials in the DB if we do not need it.
    
    Part of request #40123 Do not export old config key sys_dbauth_passwd when collecting system data
    
    Change-Id: Ib07cc58ebefdf84f3081dfff0094652c5868c940

---
+++ src/db/mysql/updates/2024/202411041700_drop_old_dbauthuser_information.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024-present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
+final class b202411041700_drop_old_dbauthuser_information extends \Tuleap\ForgeUpgrade\Bucket
+{
+    public function description(): string
+    {
+        return 'Drop old information related to the dbauthuser';
+    }
+
+    public function up(): void
+    {
+        $this->api->dbh->exec("DELETE FROM forgeconfig WHERE name IN ('sys_dbauth_user', 'sys_dbauth_passwd')");
+    }
+}
