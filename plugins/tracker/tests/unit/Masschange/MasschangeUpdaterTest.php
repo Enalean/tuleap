2b7447923115ceeaed5bf68d38c1e68b8872626c
Clarck Robinson <clarck.robinson@enalean.com>
2024-11-18 16:44:04 +0100

    refacto: Modernize Masschangeupdater class
    Part of story #38631 @ notifications in trackers
    
    This change will make easier to handle the notification send after the
    updates of the artifacts in the mass change. A new paramater will be
    introduced in PostCreationContext to handle the users mentioned in the
    follow up comment.
    Also, Artifact::createNewChangeset is deperacted, so, it is also an
    opportunity to replace it with NewChangesetCreator::create in the mass change case.
    
    Some mocks has also been removed in the test
    
    No functional change expected in the mass change, the update and post
    action update should work as before.
    
    Change-Id: I953035ccde437792d02d2126ff281c753879419e

--- plugins/tracker/tests/unit/Masschange/MasschangeUpdaterTest.php
+++ plugins/tracker/tests/unit/Masschange/MasschangeUpdaterTest.php
@@ -22,32 +22,46 @@ declare(strict_types=1);
 
 namespace Tuleap\Tracker\Masschange;
 
+use Codendi_Request;
 use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\MockObject\Stub;
 use Tracker;
+use Tracker_ArtifactDao;
+use Tracker_MasschangeDataValueExtractor;
+use Tracker_Report;
 use Tracker_Rule_List;
+use Tracker_RuleFactory;
 use Tuleap\GlobalResponseMock;
 use Tuleap\Test\Builders\UserTestBuilder;
-use Tuleap\Tracker\Artifact\Artifact;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Test\Stubs\EventDispatcherStub;
+use Tuleap\Tracker\Artifact\Changeset\NewChangeset;
+use Tuleap\Tracker\Artifact\RetrieveArtifact;
+use Tuleap\Tracker\FormElement\Field\ListFields\RetrieveUsedListField;
+use Tuleap\Tracker\Test\Builders\ArtifactTestBuilder;
 use Tuleap\Tracker\Test\Builders\ChangesetTestBuilder;
 use Tuleap\Tracker\Test\Builders\ChangesetValueListTestBuilder;
 use Tuleap\Tracker\Test\Builders\Fields\List\ListStaticBindBuilder;
 use Tuleap\Tracker\Test\Builders\Fields\ListFieldBuilder;
+use Tuleap\Tracker\Test\Stub\CreateNewChangesetStub;
+use Tuleap\Tracker\Test\Stub\RetrieveArtifactStub;
+use Tuleap\Tracker\Test\Stub\Tracker\FormElement\Field\ListFields\RetrieveUsedListFieldStub;
 
-final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
+final class MasschangeUpdaterTest extends TestCase
 {
     use GlobalResponseMock;
 
     private const USER_ID = 963;
-    private \Tracker_ArtifactDao & MockObject $artifact_dao;
-    private \Tracker_FormElementFactory & Stub $form_element_factory;
-    private \Tracker_ArtifactFactory & Stub $artifact_factory;
+    private Tracker_ArtifactDao & MockObject $artifact_dao;
+    private RetrieveUsedListField $form_element_factory;
+    private RetrieveArtifact $artifact_factory;
+    private CreateNewChangesetStub $changeset_creator;
 
     protected function setUp(): void
     {
-        $this->artifact_dao         = $this->createMock(\Tracker_ArtifactDao::class);
-        $this->form_element_factory = $this->createStub(\Tracker_FormElementFactory::class);
-        $this->artifact_factory     = $this->createStub(\Tracker_ArtifactFactory::class);
+        $this->artifact_dao         = $this->createMock(Tracker_ArtifactDao::class);
+        $this->form_element_factory = RetrieveUsedListFieldStub::withNoField();
+        $this->artifact_factory     = RetrieveArtifactStub::withNoArtifact();
+        $this->changeset_creator    = CreateNewChangesetStub::withNullReturnChangeset();
     }
 
     /**
@@ -55,39 +69,40 @@ final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
      * @param list<Tracker_Rule_List> $rules
      */
     private function update(
-        \Tracker $tracker,
+        Tracker $tracker,
         array $new_values,
         array $masschange_artifact_ids,
         bool $unsubscribe_from_notifications,
         array $rules,
     ): void {
-        $request = new \Codendi_Request([
+        $request = new Codendi_Request([
             'masschange_aids' => $masschange_artifact_ids,
             'masschange-unsubscribe-option' => $unsubscribe_from_notifications,
             'artifact' => $new_values,
             'artifact_masschange_followup_comment' => '',
         ]);
 
-        $rules_factory = $this->createStub(\Tracker_RuleFactory::class);
+        $rules_factory = $this->createStub(Tracker_RuleFactory::class);
         $rules_factory->method('getAllListRulesByTrackerWithOrder')->willReturn($rules);
 
-        $masschange_data_value_extractor = $this->createStub(\Tracker_MasschangeDataValueExtractor::class);
+        $masschange_data_value_extractor = $this->createStub(Tracker_MasschangeDataValueExtractor::class);
         $masschange_data_value_extractor->method('getNewValues')->willReturn($new_values);
 
-        $event_manager = $this->createMock(\EventManager::class);
-        $event_manager->expects(self::once())->method('processEvent');
+        $event_manager = EventDispatcherStub::withIdentityCallback();
 
         $updater = new MasschangeUpdater(
             $tracker,
-            $this->createStub(\Tracker_Report::class),
+            $this->createStub(Tracker_Report::class),
             $masschange_data_value_extractor,
             $rules_factory,
             $this->form_element_factory,
             $this->artifact_factory,
             $this->artifact_dao,
-            $event_manager
+            $event_manager,
+            $this->changeset_creator
         );
         $updater->updateArtifacts(UserTestBuilder::buildWithId(self::USER_ID), $request);
+        self::assertSame(1, $event_manager->getCallCount());
     }
 
     public function testUpdateArtifactsWithoutBeenUnsubscribedFromNotifications(): void
@@ -99,18 +114,33 @@ final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
 
         $new_values = [1 => 'Value01'];
 
-        $artifact_201 = $this->createMock(Artifact::class);
-        $artifact_201->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset(352)->build());
-        $artifact_202 = $this->createMock(Artifact::class);
-        $artifact_202->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset(311)->build());
-        $this->artifact_factory->method('getArtifactById')->willReturnMap([
-            [201, $artifact_201],
-            [202, $artifact_202],
-        ]);
+        $artifact_201 = ArtifactTestBuilder::anArtifact(201)
+                ->withChangesets(
+                    ChangesetTestBuilder::aChangeset(352)
+                                                                   ->build()
+                )
+                    ->build();
+        $artifact_202 = ArtifactTestBuilder::anArtifact(202)
+                ->withChangesets(
+                    ChangesetTestBuilder::aChangeset(311)->build()
+                )
+                ->build();
 
-        $artifact_201->expects(self::once())->method('createNewChangeset');
-        $artifact_202->expects(self::once())->method('createNewChangeset');
+        $this->artifact_factory = RetrieveArtifactStub::withArtifacts($artifact_201, $artifact_202);
+        $expected_artifacts     = [201 => $artifact_201, 202 => $artifact_202];
 
+        $this->changeset_creator = CreateNewChangesetStub::withCallback(
+            function (NewChangeset $changeset) use ($expected_artifacts) {
+                $expected_artifact = $expected_artifacts[$changeset->getArtifact()->getId()];
+                self::assertSame($changeset->getArtifact(), $expected_artifact);
+                $expected_artifact_changeset = $changeset->getArtifact()->getLastChangeset();
+                self::assertNotNull($expected_artifact_changeset);
+                unset(
+                    $expected_artifacts[$changeset->getArtifact()->getId()]
+                ); //ensure we are not testing against the same artifact twice
+                return $expected_artifact_changeset;
+            }
+        );
         $this->update($tracker, $new_values, [201, 202], false, []);
     }
 
@@ -123,25 +153,44 @@ final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
 
         $new_values = [1 => 'Value01'];
 
-        $artifact_201 = $this->createMock(Artifact::class);
-        $artifact_201->method('getId')->willReturn(201);
-        $artifact_201->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset(352)->build());
-        $artifact_201->method('userCanView')->willReturn(true);
-        $artifact_202 = $this->createMock(Artifact::class);
-        $artifact_202->method('getId')->willReturn(202);
-        $artifact_202->method('getLastChangeset')->willReturn(ChangesetTestBuilder::aChangeset(311)->build());
-        $artifact_202->method('userCanView')->willReturn(true);
-        $this->artifact_factory->method('getArtifactById')->willReturnMap([
-            [201, $artifact_201],
-            [202, $artifact_202],
-        ]);
+        $user_can_view = UserTestBuilder::buildWithId(self::USER_ID);
+
+        $artifact_201 = ArtifactTestBuilder::anArtifact(201)
+                ->withChangesets(
+                    ChangesetTestBuilder::aChangeset(352)
+                                                                   ->build()
+                )
+                    ->userCanView($user_can_view)
+                        ->build();
+        $artifact_202 = ArtifactTestBuilder::anArtifact(202)
+                ->withChangesets(
+                    ChangesetTestBuilder::aChangeset(311)
+                                                                   ->build()
+                )
+                    ->userCanView($user_can_view)
+                        ->build();
+
+        $this->artifact_factory = RetrieveArtifactStub::withArtifacts($artifact_201, $artifact_202);
 
         $this->artifact_dao->expects(self::exactly(2))->method('createUnsubscribeNotification')->withConsecutive(
             [201, self::USER_ID],
             [202, self::USER_ID]
         );
-        $artifact_201->expects(self::once())->method('createNewChangeset');
-        $artifact_202->expects(self::once())->method('createNewChangeset');
+
+        $expected_artifacts = [$artifact_201, $artifact_202];
+
+        $this->changeset_creator = CreateNewChangesetStub::withCallback(
+            function (NewChangeset $changeset) use ($expected_artifacts) {
+                $expected_artifact = $expected_artifacts[$changeset->getArtifact()->getId()];
+                self::assertSame($changeset->getArtifact(), $expected_artifact);
+                $expected_artifact_changeset = $changeset->getArtifact()->getLastChangeset();
+                self::assertNotNull($expected_artifact_changeset);
+                unset(
+                    $expected_artifacts[$changeset->getArtifact()->getId()]
+                ); //ensure we are not testing against the same artifact twice
+                return $expected_artifact_changeset;
+            }
+        );
 
         $this->update($tracker, $new_values, [201, 202], true, []);
     }
@@ -168,25 +217,30 @@ final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
         $rule_2->method('getSourceFieldId')->willReturn(3202);
         $rule_2->method('getTargetFieldId')->willReturn(3203);
 
-        $source_bind_rule1                 = ListStaticBindBuilder::aStaticBind(
-            ListFieldBuilder::aListField(3201)->build()
+        $list_field_3201   = ListFieldBuilder::aListField(3201)->build();
+        $source_bind_rule1 = ListStaticBindBuilder::aStaticBind(
+            $list_field_3201
         )->withStaticValues([321 => 'Open', 322 => 'Closed'])->build();
-        $source_and_target_bind_both_rules = ListStaticBindBuilder::aStaticBind(
-            ListFieldBuilder::aListField(3202)->build()
-        )->withStaticValues([261 => 'Open', 262 => 'Closed'])->build();
-        $target_bind_rule2                 = ListStaticBindBuilder::aStaticBind(
-            ListFieldBuilder::aListField(3303)->build()
-        )->withStaticValues([333 => 'Code Yellow', 334 => 'Code Green'])->build();
-        $this->form_element_factory->method('getUsedListFieldById')->willReturnMap([
-            [$tracker, 3201, $source_bind_rule1->getField()],
-            [$tracker, 3202, $source_and_target_bind_both_rules->getField()],
-            [$tracker, 3203, $target_bind_rule2->getField()],
-        ]);
 
-        $artifact       = $this->createMock(Artifact::class);
-        $last_changeset = ChangesetTestBuilder::aChangeset(968)->build();
-        $artifact->method('getLastChangeset')->willReturn($last_changeset);
-        $this->artifact_factory->method('getArtifactById')->willReturn($artifact);
+        $list_field_3202                   = ListFieldBuilder::aListField(3202)->build();
+        $source_and_target_bind_both_rules = ListStaticBindBuilder::aStaticBind(
+            $list_field_3202
+        )->withStaticValues([261 => 'Open', 262 => 'Closed'])->build();
+
+        $list_field_3203   = ListFieldBuilder::aListField(3203)->build();
+        $target_bind_rule2 = ListStaticBindBuilder::aStaticBind(
+            $list_field_3203
+        )->withStaticValues([333 => 'Code Yellow', 334 => 'Code Green'])->build();
+
+        $this->form_element_factory = RetrieveUsedListFieldStub::withFields(
+            $list_field_3201,
+            $list_field_3202,
+            $list_field_3203
+        );
+
+        $last_changeset         = ChangesetTestBuilder::aChangeset(968)->build();
+        $artifact               = ArtifactTestBuilder::anArtifact(201)->withChangesets($last_changeset)->build();
+        $this->artifact_factory = RetrieveArtifactStub::withArtifacts($artifact);
 
         ChangesetValueListTestBuilder::aListOfValue(
             418,
@@ -204,12 +258,13 @@ final class MasschangeUpdaterTest extends \Tuleap\Test\PHPUnit\TestCase
             $target_bind_rule2->getField()
         )->withValues([$target_bind_rule2->getValue(333), $target_bind_rule2->getValue(334)])->build();
 
-        $artifact->expects(self::once())->method('createNewChangeset')->with(
-            [1 => 'Value01', 3202 => 100],
-            self::anything(),
-            self::anything(),
-            self::anything(),
-            self::anything(),
+        $this->changeset_creator = CreateNewChangesetStub::withCallback(
+            function (NewChangeset $changeset) {
+                self::assertSame([1 => 'Value01', 3202 => 100], $changeset->getFieldsData());
+                $expected_artifact_changeset = $changeset->getArtifact()->getLastChangeset();
+                self::assertNotNull($expected_artifact_changeset);
+                return $expected_artifact_changeset;
+            }
         );
 
         $this->update($tracker, $new_values, [201], false, [$rule_1, $rule_2]);
