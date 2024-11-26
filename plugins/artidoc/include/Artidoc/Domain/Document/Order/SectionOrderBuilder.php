af7a9b72d970fb4045157eaead5382b22546a733
Nicolas Terray <nicolas.terray@enalean.com>
2024-11-26 16:31:02 +0100

    feat: Introduce PATCH artidoc/:id/sections
    To not clutter the review only the verifications are done in this
    contribution. Actual reorder will be done in a dedicated one.
    
    To test, have an artidoc with 2 or more sections.
    Open the api exlorer and with :id being the document id, execute the
    following query (adapt UUIDs to your own):
    ```
    PATCH artidoc/:id/sections
    {
      "ids": [
        "01936439-d93c-736f-af80-868d2f184cdf"
      ],
      "direction": "before",
      "compared_to": "01935460-ee9a-7248-886f-740a4ee06f27"
    }
    ```
    
    Note: You can get the UUID of the section to move by clicking on the section
    in the TOC, the UUID will be displayed in the URL.
    
    If everything is ok, then you should get a 404: Not Yet Implemented.
    
    You can test errors:
    * `ids` and `compared_to` are the same
    * `direction` is unknown
    * `ids` contains more than one item
    * invalid UUIDS
    
    Implementation note: the handler of the PATCH is not moved yet in the
    Adapter/Domain namespace, it will be done later in dedicated contribution. The
    route is not fully hexagonal however: 1) most of the business logic of the
    move (verifications) is in the Domain, 2) implementing the REST route
    right now allows the frontend developpers to use the REST route in
    parallel of hexagonalification of the backend.
    
    Part of story #40457: Modify flat document outline
    
    Change-Id: Ic8e87545063aeb1a27921b8a396d39e4a8633db7

---
+++ plugins/artidoc/include/Artidoc/Domain/Document/Order/SectionOrderBuilder.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Artidoc\Domain\Document\Order;
+
+use Tuleap\Artidoc\Domain\Document\Section\Identifier\InvalidSectionIdentifierStringException;
+use Tuleap\Artidoc\Domain\Document\Section\Identifier\SectionIdentifier;
+use Tuleap\Artidoc\Domain\Document\Section\Identifier\SectionIdentifierFactory;
+use Tuleap\NeverThrow\Err;
+use Tuleap\NeverThrow\Fault;
+use Tuleap\NeverThrow\Ok;
+use Tuleap\NeverThrow\Result;
+
+final readonly class SectionOrderBuilder
+{
+    public function __construct(private SectionIdentifierFactory $identifier_factory)
+    {
+    }
+
+    /**
+     * @return Ok<SectionOrder>|Err<Fault>
+     */
+    public function buildFromRest(
+        array $submitted_ids,
+        string $submitted_direction,
+        string $submitted_compared_to,
+    ): Ok|Err {
+        $direction = Direction::tryFrom($submitted_direction);
+        if ($direction === null) {
+            return Result::err(InvalidDirectionFault::build($submitted_direction));
+        }
+
+        try {
+            $compared_to = $this->identifier_factory->buildFromHexadecimalString($submitted_compared_to);
+        } catch (InvalidSectionIdentifierStringException) {
+            return Result::err(InvalidComparedToFault::build($submitted_compared_to));
+        }
+
+        if (count($submitted_ids) !== 1) {
+            return Result::err(InvalidIdsFault::build());
+        }
+
+        if (in_array($submitted_compared_to, $submitted_ids, true)) {
+            return Result::err(CannotMoveSectionRelativelyToItselfFault::build());
+        }
+
+        try {
+            $identifiers = array_map(
+                fn($id): SectionIdentifier => $this->identifier_factory->buildFromHexadecimalString($id),
+                $submitted_ids,
+            );
+        } catch (InvalidSectionIdentifierStringException) {
+            return Result::err(InvalidIdsFault::build());
+        }
+
+        return Result::ok(SectionOrder::build($identifiers[0], $direction, $compared_to));
+    }
+}
---
+++ plugins/artidoc/include/Artidoc/REST/v1/PATCHSectionsHandler.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2024 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Artidoc\REST\v1;
+
+use Tuleap\Artidoc\Document\ArtidocDocumentInformation;
+use Tuleap\Artidoc\Document\RetrieveArtidoc;
+use Tuleap\Artidoc\Domain\Document\Order\SectionOrder;
+use Tuleap\Artidoc\Domain\Document\Order\SectionOrderBuilder;
+use Tuleap\NeverThrow\Err;
+use Tuleap\NeverThrow\Fault;
+use Tuleap\NeverThrow\Ok;
+use Tuleap\NeverThrow\Result;
+
+final readonly class PATCHSectionsHandler
+{
+    public function __construct(
+        private RetrieveArtidoc $retrieve_artidoc,
+        private SectionOrderBuilder $section_order_builder,
+    ) {
+    }
+
+    /**
+     * @return Ok<ArtidocSectionRepresentation>|Err<Fault>
+     */
+    public function handle(int $id, OrderRepresentation $order, \PFUser $user): Ok|Err
+    {
+        return $this->retrieve_artidoc
+            ->retrieveArtidoc($id, $user)
+            ->andThen(fn (ArtidocDocumentInformation $document_information) => $this->ensureThatUserCanWriteDocument($document_information, $user))
+            ->andThen(fn (ArtidocDocumentInformation $document_information) => $this->reorder($id, $order));
+    }
+
+    /**
+     * @return Ok<ArtidocDocumentInformation>|Err<Fault>
+     */
+    private function ensureThatUserCanWriteDocument(ArtidocDocumentInformation $document_information, \PFUser $user): Ok|Err
+    {
+        $permissions_manager = \Docman_PermissionsManager::instance($document_information->document->getProjectId());
+        if (! $permissions_manager->userCanWrite($user, $document_information->document->getId())) {
+            return Result::err(Fault::fromMessage('User cannot write document'));
+        }
+
+        return Result::ok($document_information);
+    }
+
+    private function reorder(int $id, OrderRepresentation $order): Ok|Err
+    {
+        return $this->section_order_builder
+            ->buildFromRest($order->ids, $order->direction, $order->compared_to)
+            ->andThen(fn (SectionOrder $order) => Result::err(Fault::fromMessage('Not yet implemented')));
+    }
+}
