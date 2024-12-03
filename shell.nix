Enalean/tuleap

commit bdb65ea417bbbf9b935046496c8b8f8e658d6d6f

chore: request #40481 pnpm: 8.7.0 -> 8.15.9
Scripts executed during the lifecycle of packages installation are now
disabled. This is primarily to prevent an issue with the side effect
caches that is now enabled when `shared-workspace-lockfile` is set
to `false` [0]. While this could cause us some troubles by forcing us to
execute what we need manually it is likely a good thing. The pre and
post install hooks have been used in the past in situations where
deps were compromised as a way to run payloads.
Disabling the scripts execution also disable the recursive PATH update
in the repo which we compensate by adding our dev tools in the PATH
ourselves via the Nix shell.

Main changes:
https://github.com/pnpm/pnpm/releases/tag/v8.15.0
https://github.com/pnpm/pnpm/releases/tag/v8.14.0
https://github.com/pnpm/pnpm/releases/tag/v8.12.0
https://github.com/pnpm/pnpm/releases/tag/v8.11.0
https://github.com/pnpm/pnpm/releases/tag/v8.10.0
https://github.com/pnpm/pnpm/releases/tag/v8.9.0
https://github.com/pnpm/pnpm/releases/tag/v8.8.0

[0] https://github.com/pnpm/pnpm/commit/ef3609049f32e00718b073cd60fade03da30fbbf

Change-Id: I878ce0d972329f2a007cd2e29c403099ce01409c

shell.nix
@@ -11,4 +11,8 @@ pkgs.mkShellNoCC {
 
   # Enable Console Do Not Track
   DO_NOT_TRACK = 1;
+
+  shellHook = ''
+    export PATH="$PATH:$(pwd)/node_modules/.bin"
+  '';
 }

.npmrc
@@ -2,3 +2,4 @@ shared-workspace-lockfile=false
 update-notifier=false
 strict-peer-dependencies=false
 auto-install-peers=false
+ignore-scripts=true

plugins/timetracking/scripts/timetracking-overview-widget/pnpm-lock.yaml
@@ -58,6 +58,9 @@ devDependencies:
   '@vue/test-utils':
     specifier: ^1.3
     version: 1.3.5(vue@2.7.4)
+  vue-demi:
+    specifier: ^0.14.10
+    version: 0.14.10(vue@2.7.4)
 
 packages:
 
@@ -90,7 +93,7 @@ packages:
       pinia: '>=2.1.5'
     dependencies:
       pinia: 2.1.7(vue@2.7.4)
-      vue-demi: 0.14.6(vue@2.7.4)
+      vue-demi: 0.14.10(vue@2.7.4)
     transitivePeerDependencies:
       - '@vue/composition-api'
       - vue
@@ -296,7 +299,7 @@ packages:
     dependencies:
       '@vue/devtools-api': 6.5.1
       vue: 2.7.4
-      vue-demi: 0.14.6(vue@2.7.4)
+      vue-demi: 0.14.10(vue@2.7.4)
 
   /postcss@8.4.14:
     resolution: {integrity: sha512-E398TUmfAYFPBSdzgeieK2Y1+1cpdxJx8yXbK/m57nRhKSmk1GB2tO4lbLBtlkfPQTDKfe4Xqv1ASWPpayPEig==}
@@ -344,8 +347,8 @@ packages:
     resolution: {integrity: sha512-/OaKK0xYrs3DmxRYqL/yDc+FxFUVYhDlXMhRmv3z915w2HF1tnN1omB354j8VUGO/hbRzyD6Y3sA7v7GS/ceog==}
     engines: {node: '>=4'}
 
-  /vue-demi@0.14.6(vue@2.7.4):
-    resolution: {integrity: sha512-8QA7wrYSHKaYgUxDA5ZC24w+eHm3sYCbp0EzcDwKqN3p6HqtTCGR/GVsPyZW92unff4UlcSh++lmqDWN3ZIq4w==}
+  /vue-demi@0.14.10(vue@2.7.4):
+    resolution: {integrity: sha512-nMZBOwuzabUO0nLgIcc6rycZEebF6eeUfaiQx9+WSk8e29IbLvPU9feI6tqW4kTo3hvoYAJkMh8n8D0fuISphg==}
     engines: {node: '>=12'}
     hasBin: true
     requiresBuild: true

pnpm-lock.yaml
@@ -3715,9 +3715,6 @@ packages:
 
   /ajv-formats@2.1.1:
     resolution: {integrity: sha512-Wx0Kx52hxE7C18hkMEggYlEifqWZtYaRgouJor+WMdPnQyEK13vgEWyVNup7SoeeoLMsr4kf5h6dOW11I15MUA==}
-    peerDependenciesMeta:
-      ajv:
-        optional: true
     dependencies:
       ajv: 8.12.0
     dev: true

tools/utils/nix/build-tools/pnpm.nix
@@ -2,11 +2,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "pnpm";
-  version = "8.7.0";
+  version = "8.15.9";
 
   src = fetchurl {
     url = "https://registry.npmjs.org/pnpm/-/pnpm-${finalAttrs.version}.tgz";
-    hash = "sha256-rvjSa8F2FsYNyxXS2zCAN2YFGicg7cw71guH+4ySVHA=";
+    hash = "sha256-2qJ6C1QbxjUyP/lsLe2ZVGf/n+bWn/ZwIVWKqa2dzDY=";
   };
 
   buildInputs = [ nodejs ];

commit 49739b0a436586531bce098cec61d05042d5a097

fix: Fix the pipeline building the TLP Doc image
Not all our shells were setting the PATH properly. We now make sure
it is the case. As a side effect the telemetry of various packages is
now disabled everywhere.

Issue introduced by bdb65ea417bbbf9b935046496c8b8f8e658d6d6f.

Part of request #40481 pnpm: 8.7.0 -> 8.15.9

Change-Id: I593f7046a49bd083438f9c3e308d0563c9aeabed

shell.nix
@@ -1,18 +1,11 @@
 { pkgs ? (import ./tools/utils/nix/pinned-nixpkgs.nix) {} }:
 
-pkgs.mkShellNoCC {
-  buildInputs = (import ./tools/utils/nix/dev-tools { inherit pkgs; }).buildInputs;
-
+let
+  devShell = import ./tools/utils/nix/dev-tools { inherit pkgs; };
+in devShell.overrideAttrs {
   # Use the SSH client provided by the system (FHS only) to avoid issues with Fedora/RedHat default settings
   GIT_SSH = if pkgs.lib.pathExists "/usr/bin/ssh" then "/usr/bin/ssh" else "ssh";
   # Explicitly target linux/amd64, our base image are linux/amd64 only at this time and not specifying this does not
   # play well when using Apple silicon
   DOCKER_DEFAULT_PLATFORM = "linux/amd64";
-
-  # Enable Console Do Not Track
-  DO_NOT_TRACK = 1;
-
-  shellHook = ''
-    export PATH="$PATH:$(pwd)/node_modules/.bin"
-  '';
 }

tools/utils/nix/build-tools/default.nix
@@ -7,4 +7,12 @@ in pkgs.mkShellNoCC {
     (import ./build-tools-packages.nix { inherit pkgs; }) ++
     (php) ++
     [(import ./build-tools-js.nix { inherit pkgs; })];
+
+  # Enable Console Do Not Track / disable telemetry
+  DO_NOT_TRACK = 1;
+  STORYBOOK_DISABLE_TELEMETRY = 1;
+
+  shellHook = ''
+    export PATH="$PATH:$(pwd)/node_modules/.bin"
+  '';
 }

tools/utils/nix/dev-tools/default.nix
@@ -2,10 +2,10 @@
 
 let
   devToolsPhpBase = import ./dev-tools-php.nix { inherit pkgs; };
-  buildTools = import ../build-tools { inherit pkgs; phpBase = devToolsPhpBase; };
-in pkgs.mkShellNoCC {
+  buildToolsShell = import ../build-tools { inherit pkgs; phpBase = devToolsPhpBase; };
+in buildToolsShell.overrideAttrs {
   name = "dev-tools";
-  buildInputs = buildTools.buildInputs ++ [
+  buildInputs = buildToolsShell.buildInputs ++ [
     pkgs.osv-scanner
     (import ./dev-tools-docker.nix { inherit pkgs; })
     (import ./dev-tools-old-browsers.nix { inherit pkgs; })