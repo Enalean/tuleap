Enalean/tuleap

commit 890789271ba923ce0e95075309f6b2ad2f17ff7f

chore: Build flaming parrot styles in their own context
Part of request #34724 Build timetracking widgets in their own contexts

- Build the Flaming Parrot style independently
- Refactor the display of the timetracking tab

Change-Id: I495e7a4deb171404d43335f047a50fb37b9644fb

plugins/timetracking/include/timetrackingPlugin.php
@@ -18,7 +18,6 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Tuleap\Layout\IncludeAssets;
 use Tuleap\Plugin\PluginWithLegacyInternalRouting;
 use Tuleap\Timetracking\Admin\AdminController;
 use Tuleap\Timetracking\Admin\AdminDao;
@@ -26,6 +25,7 @@ use Tuleap\Timetracking\Admin\TimetrackingEnabler;
 use Tuleap\Timetracking\Admin\TimetrackingUgroupDao;
 use Tuleap\Timetracking\Admin\TimetrackingUgroupRetriever;
 use Tuleap\Timetracking\Admin\TimetrackingUgroupSaver;
+use Tuleap\Timetracking\ArtifactView\ArtifactView;
 use Tuleap\Timetracking\ArtifactView\ArtifactViewBuilder;
 use Tuleap\Timetracking\JiraImporter\Configuration\JiraTimetrackingConfigurationRetriever;
 use Tuleap\Timetracking\JiraImporter\JiraXMLExport;
@@ -47,11 +47,14 @@ use Tuleap\Timetracking\Widget\TimeTrackingOverview;
 use Tuleap\Timetracking\Widget\UserWidget;
 use Tuleap\Timetracking\XML\XMLImport;
 use Tuleap\Timetracking\XML\XMLExport;
+use Tuleap\Tracker\Artifact\Renderer\GetAdditionalCssAssetsForArtifactDisplay;
 use Tuleap\Tracker\Creation\JiraImporter\Configuration\PlatformConfigurationForExternalPluginsEvent;
 use Tuleap\Tracker\Creation\JiraImporter\Import\JiraImporterExternalPluginsEvent;
 use Tuleap\Tracker\REST\v1\Event\GetTrackersWithCriteria;
 use Tuleap\Tracker\XML\Exporter\TrackerEventExportFullXML;
 use Tuleap\Tracker\XML\Importer\ImportXMLProjectTrackerDone;
+use Tuleap\Layout\IncludeViteAssets;
+use Tuleap\Layout\CssViteAsset;
 
 require_once __DIR__ . '/../../tracker/include/trackerPlugin.php';
 require_once 'constants.php';
@@ -81,24 +84,18 @@ class timetrackingPlugin extends PluginWithLegacyInternalRouting // @codingStand
         return ['tracker'];
     }
 
-    #[\Tuleap\Plugin\ListeningToEventName('cssfile')]
-    public function cssfile($params): void
-    {
-        if (
-            strpos($_SERVER['REQUEST_URI'], $this->getPluginPath()) === 0 ||
-            strpos($_SERVER['REQUEST_URI'], TRACKER_BASE_URL) === 0
-        ) {
-            $style_css_url = $this->getAssets()->getFileURL('style-fp.css');
-
-            echo '<link rel="stylesheet" type="text/css" href="' . $style_css_url . '" />';
+    #[\Tuleap\Plugin\ListeningToEventClass]
+    public function getAdditionalCssAssetsForArtifactDisplay(
+        GetAdditionalCssAssetsForArtifactDisplay $event,
+    ): void {
+        if ($event->getViewIdentifier() !== ArtifactView::IDENTIFIER) {
+            return;
         }
-    }
-
-    private function getAssets(): IncludeAssets
-    {
-        return new IncludeAssets(
-            __DIR__ . '/../frontend-assets',
-            '/assets/timetracking'
+        $event->addCssAsset(
+            CssViteAsset::fromFileName(
+                new IncludeViteAssets(__DIR__ . '/../scripts/timetracking-tab-styles/frontend-assets', '/assets/timetracking/timetracking-tab-styles'),
+                'themes/style.scss'
+            ),
         );
     }
 

plugins/timetracking/scripts/timetracking-tab-styles/pnpm-lock.yaml
@@ -7,4 +7,4 @@ settings:
 devDependencies:
   '@tuleap/build-system-configurator':
     specifier: workspace:*
-    version: link:../../lib/frontend/build-system-configurator
+    version: link:../../../../lib/frontend/build-system-configurator

plugins/timetracking/scripts/timetracking-tab-styles/vite.config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
+ * Copyright (c) Enalean, 2023-Present. All Rights Reserved.
  *
  * This file is a part of Tuleap.
  *
@@ -17,7 +17,21 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-const common = require("./webpack.common.js");
-const { webpack_configurator } = require("@tuleap/build-system-configurator");
+import { vite } from "@tuleap/build-system-configurator";
+import * as path from "path";
 
-module.exports = webpack_configurator.extendDevConfiguration(common);
+export default vite.defineAppConfig(
+    {
+        plugin_name: path.basename(path.resolve(__dirname, "../..")),
+        sub_app_name: path.basename(__dirname),
+    },
+    {
+        build: {
+            rollupOptions: {
+                input: {
+                    "timetracking-tab-styles": path.resolve(__dirname, "themes/style.scss"),
+                },
+            },
+        },
+    },
+);

plugins/timetracking/webpack.common.js
@@ -1,49 +1,0 @@
-/*
- * Copyright (c) Enalean, 2018-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-const path = require("path");
-const { webpack_configurator } = require("@tuleap/build-system-configurator");
-
-const context = __dirname;
-const output = webpack_configurator.configureOutput(
-    path.resolve(__dirname, "./frontend-assets"),
-    "/assets/timetracking/script/personal-timetracking-widget",
-);
-
-let entry_points = {
-    "style-fp": "./themes/FlamingParrot/css/style.scss",
-};
-
-module.exports = [
-    {
-        entry: entry_points,
-        context,
-        output,
-        externals: {
-            tlp: "tlp",
-        },
-        module: {
-            rules: [webpack_configurator.rule_scss_loader, webpack_configurator.rule_css_assets],
-        },
-        plugins: [
-            webpack_configurator.getCleanWebpackPlugin(),
-            webpack_configurator.getManifestPlugin(),
-            ...webpack_configurator.getCSSExtractionPlugins(),
-        ],
-    },
-];

plugins/timetracking/webpack.prod.js
@@ -1,23 +1,0 @@
-/*
- * Copyright (c) Enalean, 2019-Present. All Rights Reserved.
- *
- * This file is a part of Tuleap.
- *
- * Tuleap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Tuleap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
- */
-
-const common = require("./webpack.common.js");
-const { webpack_configurator } = require("@tuleap/build-system-configurator");
-
-module.exports = webpack_configurator.extendProdConfiguration(common);

plugins/tracker/include/Tracker/Artifact/Renderer/CopyRenderer.class.php
@@ -119,7 +119,7 @@ class Tracker_Artifact_CopyRenderer extends Tracker_Artifact_ReadOnlyRenderer
 
     protected function fetchView(Codendi_Request $request, PFUser $user)
     {
-        $view_collection = new Tracker_Artifact_View_ViewCollection();
+        $view_collection = new Tracker_Artifact_View_ViewCollection($this->event_manager);
         $view_collection->add(new Tracker_Artifact_View_Copy($this->artifact, $request, $user, $this));
 
         return $view_collection->fetchRequestedView($request);

plugins/tracker/include/Tracker/Artifact/Renderer/EditRenderer.class.php
@@ -196,7 +196,7 @@ class Tracker_Artifact_EditRenderer extends Tracker_Artifact_EditAbstractRendere
 
     protected function fetchView(Codendi_Request $request, PFUser $user)
     {
-        $view_collection = new Tracker_Artifact_View_ViewCollection();
+        $view_collection = new Tracker_Artifact_View_ViewCollection($this->event_manager);
         $view_collection->add(new Tracker_Artifact_View_Edit($this->artifact, $request, $user, $this));
 
         if ($this->artifact->getTracker()->isProjectAllowedToUseType()) {

commit 4856607f8f46f976832059fca76286bcd4c09ac8

fix: request #41082 tracker FlamingParrot styles should be loaded only where needed
How to test:
- Go to an SVN repository's settings > Notifications.
- Tracker FlamingParrot styles should not be loaded on the page.
- Add any notification (it does not matter). Then click on Delete
button. The popover to confirm deletion should have padding in its
content, it should not be glued to its borders.
- Go to a Tracker's administration > Hamburger menu "≡" > Time tracking
- The administration page should be styled, there should not be a margin
between the content and the header and sidebar.

Why?
The fix in 2481453e0e1b3ee1a266d6c8da107cf23b513207 was causing the very
bad side-effect of loading Tracker FlamingParrot styles on _every_
FlamingParrot page, ever. It works with two events: cssFile() event is
fired on every page, Tracker plugin handles it, and fires another event
TRACKER_EVENT_INCLUDE_CSS_FILE. If somebody responds to that second
event (in our case timetrackingPlugin does) and sets the parameter to
true (it always does), then tracker will include its style. The problem
is that timetrackingPlugin _always_ sets it true, regardless of the
page.

The original issue was probably caused by
890789271ba923ce0e95075309f6b2ad2f17ff7f. As it changed the way the
styles are built, the administration could have been overlooked, but the
same stylesheet also had rules for it. In order to fix the problem, we
make the Admin controller load the stylesheet too. It's not necessary
but we also rearrange some rules to drop linter disables.

Change-Id: I5d83fb1f9a21d6f760ea2dc9346308e2859decf5

plugins/timetracking/include/Timetracking/Admin/AdminController.php
@@ -29,6 +29,8 @@ use ProjectHistoryDao;
 use TemplateRendererFactory;
 use Tracker;
 use TrackerManager;
+use Tuleap\Layout\CssViteAsset;
+use Tuleap\Layout\IncludeViteAssets;
 use User_ForgeUserGroupFactory;
 
 class AdminController
@@ -97,9 +99,9 @@ class AdminController
         return new CSRFSynchronizerToken($tracker->getAdministrationUrl());
     }
 
-    public function displayAdminForm(Tracker $tracker)
+    public function displayAdminForm(Tracker $tracker): void
     {
-        $renderer  = TemplateRendererFactory::build()->getRenderer(TIMETRACKING_TEMPLATE_DIR);
+        $renderer  = TemplateRendererFactory::build()->getRenderer(__DIR__ . '/../../../templates/');
         $presenter = new AdminPresenter(
             $tracker,
             $this->getCSRFSynchronizerToken($tracker),
@@ -107,7 +109,15 @@ class AdminController
             $this->getReadersUGroupPresenters($tracker),
             $this->getWritersUGroupPresenters($tracker)
         );
-
+        $GLOBALS['HTML']->addCssAsset(
+            CssViteAsset::fromFileName(
+                new IncludeViteAssets(
+                    __DIR__ . '/../../../scripts/timetracking-tab-styles/frontend-assets',
+                    '/assets/timetracking/timetracking-tab-styles'
+                ),
+                'themes/style.scss'
+            )
+        );
         $tracker->displayAdminItemHeader(
             $this->tracker_manager,
             'timetracking',

plugins/timetracking/include/timetrackingPlugin.php
@@ -447,12 +447,6 @@ class timetrackingPlugin extends PluginWithLegacyInternalRouting implements Plug
         }
     }
 
-    #[\Tuleap\Plugin\ListeningToEventName(trackerPlugin::TRACKER_EVENT_INCLUDE_CSS_FILE)]
-    public function trackerEventIncludeCssFile(array $params): void
-    {
-        $params['include_tracker_css_file'] = true;
-    }
-
     public function getConfigKeys(\Tuleap\Config\ConfigClassProvider $event): void
     {
         $event->addConfigClass(FeatureFlagTimetrackingManagementWidget::class);

plugins/timetracking/scripts/timetracking-tab-styles/themes/style.scss
@@ -73,21 +73,19 @@
     vertical-align: middle;
 }
 
-/* stylelint-disable selector-no-qualifying-type */
-body.tracker-administration {
+.tracker-administration {
     background: var(--tlp-background-color);
 
     .main {
         #feedback > ul {
             margin: 0 20px 20px;
         }
-
-        .content {
-            padding: 0;
-        }
     }
 }
-/* stylelint-enable */
+
+.tracker-administration .content {
+    padding: 0;
+}
 
 .tracker-administration-content {
     margin: 20px;