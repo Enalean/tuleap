862fcf26451697c7aff5f657c7512c1a631b5937
Nicolas Terray <nicolas.terray@enalean.com>
2024-12-09 12:12:02 +0100

    fix: adjust index of table plugin_artidoc_document
    1. With the upcoming venue of freetext section, the UNIQUE index is not
       anymore relevant. Furthermore there was already a check in the
       backend so it should not be necessary. It is switched to a regular
       INDEX.
    2. There was a missing index when we are searching artidoc sections for
       an artifact. This is needed to avoid a full table scan when we delete
       an artifact, or when we will implement the possibility to see every
       artidoc documents containing current artifact in the artifact view.
    
    No functional change
    
    Part of story #41089: Prepare Artidoc backend for Structure
    
    Change-Id: Ifa10ee6dc7655b8f6ad76f96f4afb3a2feb7d2ab

--- plugins/artidoc/db/install.sql
+++ plugins/artidoc/db/install.sql
@@ -5,7 +5,8 @@ CREATE TABLE plugin_artidoc_document
     item_id     INT(11) UNSIGNED NOT NULL,
     artifact_id INT(11)          NOT NULL,
     `rank`      INT(11) unsigned NOT NULL,
-    UNIQUE idx_uniq_artifact (item_id, artifact_id),
+    INDEX idx_item_artifact (item_id, artifact_id),
+    INDEX idx_artifact (artifact_id),
     INDEX idx_rank (item_id, `rank`)
 ) ENGINE = InnoDB;
 
