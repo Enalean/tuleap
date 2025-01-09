4e5b34fd324116541fe2520405efc21e16ab05f8
Kevin Traini <kevin.traini@enalean.com>
2025-01-09 15:40:00 +0100

    chore: Drop Mockery usage in some Git/RemoteServer tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    2/3
    
    *No functional changes expected*
    
    Change-Id: I709c526d45182b0e2f14cee3354c2db4eb1fc5ad

--- plugins/git/tests/unit/RemoteServer/GerritServerTest.php
+++ plugins/git/tests/unit/RemoteServer/GerritServerTest.php
@@ -18,79 +18,53 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
+
 namespace Tuleap\Git\RemoteServer;
 
+use Git_Driver_Gerrit_User;
 use Git_RemoteServer_GerritServer;
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Tuleap\Test\PHPUnit\TestCase;
 
-require_once __DIR__ . '/../../bootstrap.php';
-
-//phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps,PSR1.Classes.ClassDeclaration.MissingNamespace
-class GerritServerTest extends \Tuleap\Test\PHPUnit\TestCase
+final class GerritServerTest extends TestCase
 {
-    use MockeryPHPUnitIntegration;
-
     public function testItDoesNotNeedToCustomizeSSHConfigOfCodendiadmOrRoot(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = 'le_http_port';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            'le_ssh_port',
+            'le_http_port',
+            'le_login',
+            'le_identity_file',
+            '',
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
         $expected = 'ext::ssh -p le_ssh_port -i le_identity_file le_login@le_host %S le_project';
-        $this->assertEquals($expected, $server->getCloneSSHUrl('le_project'));
+        self::assertEquals($expected, $server->getCloneSSHUrl('le_project'));
     }
 
     public function testItPrunesDefaultHTTPPortForAdminUrl(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '80';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            'le_ssh_port',
+            '80',
+            'le_login',
+            'le_identity_file',
+            '',
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $this->assertEquals(
+        self::assertEquals(
             'http://le_host/#/admin/projects/gerrit_project_name',
             $server->getProjectAdminUrl('gerrit_project_name')
         );
@@ -98,33 +72,21 @@ class GerritServerTest extends \Tuleap\Test\PHPUnit\TestCase
 
     public function testItUseTheCustomHTTPPortForAdminUrl(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '8080';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            'le_ssh_port',
+            '8080',
+            'le_login',
+            'le_identity_file',
+            '',
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $this->assertEquals(
+        self::assertEquals(
             'http://le_host:8080/#/admin/projects/gerrit_project_name',
             $server->getProjectAdminUrl('gerrit_project_name')
         );
@@ -132,33 +94,21 @@ class GerritServerTest extends \Tuleap\Test\PHPUnit\TestCase
 
     public function testItGivesTheUrlToProjectRequests(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '8080';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            'le_ssh_port',
+            '8080',
+            'le_login',
+            'le_identity_file',
+            '',
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $this->assertEquals(
+        self::assertEquals(
             'http://le_host:8080/#/q/project:gerrit_project_name,n,z',
             $server->getProjectUrl('gerrit_project_name')
         );
@@ -166,33 +116,21 @@ class GerritServerTest extends \Tuleap\Test\PHPUnit\TestCase
 
     public function testItGivesTheUrlWithHTTPSToProjectRequestsIfWeUseSSL(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '8080';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = true;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            'le_ssh_port',
+            '8080',
+            'le_login',
+            'le_identity_file',
+            '',
+            true,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $this->assertEquals(
+        self::assertEquals(
             'https://le_host:8080/#/q/project:gerrit_project_name,n,z',
             $server->getProjectUrl('gerrit_project_name')
         );
@@ -200,69 +138,47 @@ class GerritServerTest extends \Tuleap\Test\PHPUnit\TestCase
 
     public function testItGivesTheReplicationKeyToProjectRequests(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '8080';
-        $ssh_port             = 'le_ssh_port';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
-        $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
+        $replication_key = '';
+        $server          = new Git_RemoteServer_GerritServer(
+            1,
+            'le_host',
+            'le_ssh_port',
+            '8080',
+            'le_login',
+            'le_identity_file',
             $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $this->assertEquals(
+        self::assertEquals(
             $replication_key,
-            $server->getReplicationKey('gerrit_project_name')
+            $server->getReplicationKey()
         );
     }
 
     public function testItGivesTheCloneUrlForTheEndUserWhoWantToCloneRepository(): void
     {
-        $id                   = 1;
-        $host                 = 'le_host';
-        $http_port            = '8080';
-        $ssh_port             = '29418';
-        $login                = 'le_login';
-        $identity_file        = 'le_identity_file';
-        $replication_key      = '';
-        $use_ssl              = false;
-        $gerrit_version       = '2.5';
-        $http_password        = '1234';
-        $replication_password = '';
-
         $server = new Git_RemoteServer_GerritServer(
-            $id,
-            $host,
-            $ssh_port,
-            $http_port,
-            $login,
-            $identity_file,
-            $replication_key,
-            $use_ssl,
-            $gerrit_version,
-            $http_password,
-            $replication_password
+            1,
+            'le_host',
+            '29418',
+            '8080',
+            'le_login',
+            'le_identity_file',
+            '',
+            false,
+            '2.5',
+            '1234',
+            ''
         );
 
-        $user = \Mockery::spy(\Git_Driver_Gerrit_User::class)->shouldReceive('getSshUserName')->andReturns('blurp')->getMock();
+        $user = $this->createMock(Git_Driver_Gerrit_User::class);
+        $user->method('getSshUserName')->willReturn('blurp');
 
-        $this->assertEquals(
+        self::assertEquals(
             'ssh://blurp@le_host:29418/gerrit_project_name.git',
             $server->getEndUserCloneUrl('gerrit_project_name', $user)
         );
