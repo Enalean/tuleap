6d8bf04ccbfb3acad1a1d917fabadf139bffeec5
Kevin Traini <kevin.traini@enalean.com>
2025-01-22 11:43:59 +0100

    chore: Drop Mockery usage in some Git tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    3/19
    
    *No functional changes expected*
    
    Change-Id: I74412c22dc34c148a6a82d47a66e6d71ae7663e7

--- plugins/git/tests/unit/GitViewsRepositoriesTraversalStrategySelectboxTest.php
+++ plugins/git/tests/unit/GitViewsRepositoriesTraversalStrategySelectboxTest.php
@@ -18,35 +18,38 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+declare(strict_types=1);
 
-require_once __DIR__ . '/bootstrap.php';
+namespace Tuleap\Git;
 
-//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
-class GitViewsRepositoriesTraversalStrategySelectboxTest extends \Tuleap\Test\PHPUnit\TestCase
+use Git_Backend_Gitolite;
+use GitRepository;
+use GitViews;
+use GitViewsRepositoriesTraversalStrategy_Selectbox;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
+
+final class GitViewsRepositoriesTraversalStrategySelectboxTest extends TestCase
 {
-    use MockeryPHPUnitIntegration;
-
     public function testEmptyListShouldReturnEmptyString(): void
     {
-        $view         = \Mockery::spy(\GitViews::class);
-        $user         = \Mockery::spy(\PFUser::class);
-        $repositories = [];
-        $strategy     = new GitViewsRepositoriesTraversalStrategy_Selectbox($view);
-        $this->assertSame('', $strategy->fetch($repositories, $user));
+        $strategy = new GitViewsRepositoriesTraversalStrategy_Selectbox($this->createMock(GitViews::class));
+        self::assertSame('', $strategy->fetch([], UserTestBuilder::buildWithDefaults()));
     }
 
     public function testFlatTreeShouldReturnRepresentation(): void
     {
-        $view     = \Mockery::spy(\GitViews::class);
-        $user     = \Mockery::spy(\PFUser::class);
-        $strategy = \Mockery::mock(\GitViewsRepositoriesTraversalStrategy_Selectbox::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $strategy = $this->getMockBuilder(GitViewsRepositoriesTraversalStrategy_Selectbox::class)
+            ->setConstructorArgs([$this->createMock(GitViews::class)])->onlyMethods(['getRepository'])->getMock();
 
         $repositories    = $this->getFlatTree($strategy);
         $expectedPattern = $this->getExpectedPattern($repositories);
 
-        $strategy->__construct($view);
-        $this->assertMatchesRegularExpression('`' . $expectedPattern . '`', $strategy->fetch($repositories, $user));
+        self::assertMatchesRegularExpression(
+            '`' . $expectedPattern . '`',
+            $strategy->fetch($repositories, UserTestBuilder::buildWithDefaults()),
+        );
     }
 
     public function getExpectedPattern($repositories): string
@@ -54,37 +57,46 @@ class GitViewsRepositoriesTraversalStrategySelectboxTest extends \Tuleap\Test\PH
         $nb_repositories                         = count($repositories);
         $li_regexp_for_repository_representation = '<option value="(?P<value>[^"]*)">(?P<repo>[^\(<]*)</option>';
 
-        $pattern = sprintf('<select (?P<args>[^>]*)>(?:%s){%d}</select>', $li_regexp_for_repository_representation, $nb_repositories);
-        return $pattern;
+        return sprintf('<select (?P<args>[^>]*)>(?:%s){%d}</select>', $li_regexp_for_repository_representation, $nb_repositories);
     }
 
     public function testRepoIDShouldBeTheValue(): void
     {
-        $view     = \Mockery::spy(\GitViews::class);
-        $user     = \Mockery::spy(\PFUser::class);
-        $strategy = \Mockery::mock(\GitViewsRepositoriesTraversalStrategy_Selectbox::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $strategy = $this->getMockBuilder(GitViewsRepositoriesTraversalStrategy_Selectbox::class)
+            ->setConstructorArgs([$this->createMock(GitViews::class)])->onlyMethods(['getRepository'])->getMock();
 
         $repositories    = $this->getFlatTree($strategy);
         $a_repository_id = 4;
 
-        $strategy->__construct($view);
-        $this->assertMatchesRegularExpression('`value="' . $a_repository_id . '"`', $strategy->fetch($repositories, $user));
+        self::assertMatchesRegularExpression(
+            '`value="' . $a_repository_id . '"`',
+            $strategy->fetch($repositories, UserTestBuilder::buildWithDefaults()),
+        );
     }
 
-    private function getFlatTree($strategy): array
+    private function getFlatTree(GitViewsRepositoriesTraversalStrategy_Selectbox&MockObject $strategy): array
     {
         //go find the variable $repositories
-        $repositories = $this->getFLatTreeOfRepositories();
-        foreach ($repositories as $row) {
-            $repository = \Mockery::mock(\GitRepository::class)->makePartial()->shouldAllowMockingProtectedMethods();
-            \assert($repository instanceof GitRepository);
-            $repository->setId($row['repository_id']);
-            $repository->setName($row['repository_name']);
-            $repository->setDescription($row['repository_description']);
-            $repository->shouldReceive('userCanRead')->andReturns(true);
-            $repository->setBackend(\Mockery::spy(\Git_Backend_Gitolite::class));
-            $strategy->shouldReceive('getRepository')->with($row)->andReturns($repository);
+        $repositories         = $this->getFLatTreeOfRepositories();
+        $builded_repositories = [];
+        foreach ($repositories as $id => $row) {
+            $repository = $this->createMock(GitRepository::class);
+            assert($repository instanceof GitRepository);
+            $repository->method('getId')->willReturn($row['repository_id']);
+            $repository->method('getName')->willReturn($row['repository_name']);
+            $repository->method('getDescription')->willReturn($row['repository_description']);
+            $repository->method('userCanRead')->willReturn(true);
+            $repository->method('getBackend')->willReturn($this->createMock(Git_Backend_Gitolite::class));
+            $builded_repositories[$id] = $repository;
         }
+        $strategy->method('getRepository')->willReturnCallback(static function (array $row) use ($repositories, $builded_repositories) {
+            $index = array_search($row, $repositories);
+            if ($index === false) {
+                return null;
+            }
+
+            return $builded_repositories[$index];
+        });
         return $repositories;
     }
 
@@ -107,125 +119,118 @@ class GitViewsRepositoriesTraversalStrategySelectboxTest extends \Tuleap\Test\PH
          *
          */
         return [
-            2 =>
-                 [
-                     'repository_id' => '2',
-                     'repository_name' => 'abc',
-                     'repository_namespace' => '',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/abc.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:24:58',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            3 =>
-                 [
-                     'repository_id' => '3',
-                     'repository_name' => 'Python',
-                     'repository_namespace' => 'automaticTests',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/automaticTests/Python.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:24:58',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            4 =>
-                 [
-                     'repository_id' => '4',
-                     'repository_name' => 'Ruby',
-                     'repository_namespace' => 'automaticTests',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/automaticTests/Ruby.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:25:06',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            5 =>
-                 [
-                     'repository_id' => '5',
-                     'repository_name' => 'cvsgraph',
-                     'repository_namespace' => 'deps/3rdparty',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/deps/3rdparty/cvsgraph.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:25:14',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            6 =>
-                 [
-                     'repository_id' => '6',
-                     'repository_name' => 'geshi',
-                     'repository_namespace' => 'deps/3rdparty',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/deps/3rdparty/geshi.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:25:23',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            7 =>
-                 [
-                     'repository_id' => '7',
-                     'repository_name' => 'gitolite',
-                     'repository_namespace' => 'deps/3rdparty',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/deps/3rdparty/gitolite.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:25:33',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
-            8 =>
-                 [
-                     'repository_id' => '8',
-                     'repository_name' => 'lxc/tools',
-                     'repository_namespace' => '',
-                     'repository_description' => '-- Default description --',
-                     'repository_path' => 'ngt/tools/lxc.git',
-                     'repository_parent_id' => '0',
-                     'project_id' => '101',
-                     'repository_creation_user_id' => '102',
-                     'repository_creation_date' => '2011-12-06 17:25:46',
-                     'repository_deletion_date' => '0000-00-00 00:00:00',
-                     'repository_is_initialized' => '0',
-                     'repository_access' => 'private',
-                     'repository_events_mailing_prefix' => '[SCM]',
-                     'repository_backend_type' => 'gitolite',
-                 ],
+            2 => [
+                'repository_id'                    => '2',
+                'repository_name'                  => 'abc',
+                'repository_namespace'             => '',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/abc.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:24:58',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            3 => [
+                'repository_id'                    => '3',
+                'repository_name'                  => 'Python',
+                'repository_namespace'             => 'automaticTests',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/automaticTests/Python.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:24:58',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            4 => [
+                'repository_id'                    => '4',
+                'repository_name'                  => 'Ruby',
+                'repository_namespace'             => 'automaticTests',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/automaticTests/Ruby.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:25:06',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            5 => [
+                'repository_id'                    => '5',
+                'repository_name'                  => 'cvsgraph',
+                'repository_namespace'             => 'deps/3rdparty',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/deps/3rdparty/cvsgraph.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:25:14',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            6 => [
+                'repository_id'                    => '6',
+                'repository_name'                  => 'geshi',
+                'repository_namespace'             => 'deps/3rdparty',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/deps/3rdparty/geshi.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:25:23',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            7 => [
+                'repository_id'                    => '7',
+                'repository_name'                  => 'gitolite',
+                'repository_namespace'             => 'deps/3rdparty',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/deps/3rdparty/gitolite.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:25:33',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
+            8 => [
+                'repository_id'                    => '8',
+                'repository_name'                  => 'lxc/tools',
+                'repository_namespace'             => '',
+                'repository_description'           => '-- Default description --',
+                'repository_path'                  => 'ngt/tools/lxc.git',
+                'repository_parent_id'             => '0',
+                'project_id'                       => '101',
+                'repository_creation_user_id'      => '102',
+                'repository_creation_date'         => '2011-12-06 17:25:46',
+                'repository_deletion_date'         => '0000-00-00 00:00:00',
+                'repository_is_initialized'        => '0',
+                'repository_access'                => 'private',
+                'repository_events_mailing_prefix' => '[SCM]',
+                'repository_backend_type'          => 'gitolite',
+            ],
         ];
     }
 }
