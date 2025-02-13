d451f0472ac88e544217b7189ff9936e1e5c10db
Kevin Traini <kevin.traini@enalean.com>
2025-02-13 11:46:53 +0100

    chore: Drop Mockery usage in some Tracker/Artifact/XMLImport tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    - Tracker_Artifact_XMLImport_XMLImportZipArchiveTest.php
    - XMLImportFieldStrategyComputedTest.php
    
    *No functional changes expected*
    
    Change-Id: I666415ce1443d6cb8a2fb959a1d1ad44ba6bf142

--- plugins/tracker/tests/unit/Artifact/XMLImport/Tracker_Artifact_XMLImport_XMLImportZipArchiveTest.php
+++ plugins/tracker/tests/unit/Artifact/XMLImport/Tracker_Artifact_XMLImport_XMLImportZipArchiveTest.php
@@ -20,50 +20,37 @@
 
 declare(strict_types=1);
 
-use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Tuleap\TemporaryTestDirectory;
+namespace Tuleap\Tracker\Artifact\XMLImport;
 
-// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
-final class Tracker_Artifact_XMLImport_XMLImportZipArchiveTest extends \Tuleap\Test\PHPUnit\TestCase
+use Tracker_Artifact_XMLImport_XMLImportZipArchive;
+use Tuleap\TemporaryTestDirectory;
+use Tuleap\Test\PHPUnit\TestCase;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
+use ZipArchive;
+
+// phpcs:ignore Squiz.Classes.ValidClassName.NotCamelCaps
+final class Tracker_Artifact_XMLImport_XMLImportZipArchiveTest extends TestCase
 {
-    use MockeryPHPUnitIntegration;
     use TemporaryTestDirectory;
 
-    /** @var Tracker */
-    private $tracker;
-    /**
-     * @var int
-     */
-    private $tracker_id;
-
-    /** @var ZipArchive */
-    private $zip;
-
-    /** @var string */
-    private $fixtures_dir;
-
-    /** @var string */
-    private $tmp_dir;
-
-    /** @var Tracker_Artifact_XMLImport_XMLImportZipArchive */
-    private $archive;
+    private int $tracker_id;
+    private ZipArchive $zip;
+    private string $fixtures_dir;
+    private string $tmp_dir;
+    private Tracker_Artifact_XMLImport_XMLImportZipArchive $archive;
 
     protected function setUp(): void
     {
-        $this->tmp_dir      =  $this->getTmpDir();
+        $this->tmp_dir      = $this->getTmpDir();
         $this->fixtures_dir = __DIR__ . '/_fixtures';
         $this->tracker_id   = 1;
-        $tracker            = Mockery::mock(Tracker::class);
-        $tracker->shouldReceive('getId')->andReturn(1);
-        $this->tracker = $tracker;
-
-        $this->zip = new ZipArchive();
+        $this->zip          = new ZipArchive();
         if ($this->zip->open($this->fixtures_dir . '/archive.zip') !== true) {
             $this->fail('unable to open fixture archive.zip');
         }
 
         $this->archive = new Tracker_Artifact_XMLImport_XMLImportZipArchive(
-            $this->tracker,
+            TrackerTestBuilder::aTracker()->withId($this->tracker_id)->build(),
             $this->zip,
             $this->tmp_dir
         );
@@ -76,26 +63,26 @@ final class Tracker_Artifact_XMLImport_XMLImportZipArchiveTest extends \Tuleap\T
 
     public function testItGivesTheXMLFile(): void
     {
-        $this->assertXmlStringEqualsXmlFile($this->fixtures_dir . '/artifacts.xml', $this->archive->getXML());
+        self::assertXmlStringEqualsXmlFile($this->fixtures_dir . '/artifacts.xml', $this->archive->getXML());
     }
 
     public function testItExtractAttachmentsIntoARandomTemporaryDirectory(): void
     {
         $extraction_path = $this->archive->getExtractionPath();
-        $this->assertDirectoryExists($extraction_path);
+        self::assertDirectoryExists($extraction_path);
 
         $expected_prefix = $this->tmp_dir . '/import_tv5_' . $this->tracker_id . '_';
-        $this->assertStringStartsWith($expected_prefix, $extraction_path);
+        self::assertStringStartsWith($expected_prefix, $extraction_path);
 
         $this->archive->extractFiles();
-        $this->assertFileEquals($this->fixtures_dir . '/data/123/file.txt', $extraction_path . '/data/123/file.txt');
+        self::assertFileEquals($this->fixtures_dir . '/data/123/file.txt', $extraction_path . '/data/123/file.txt');
     }
 
     public function testItEnsuresThatTemporaryDirectoryIsNotReadableByEveryone(): void
     {
         $extraction_path = $this->archive->getExtractionPath();
         $perms           = fileperms($extraction_path) & 0777;
-        $this->assertEquals(0700, $perms);
+        self::assertEquals(0700, $perms);
     }
 
     public function testItCleansUp(): void
@@ -103,6 +90,6 @@ final class Tracker_Artifact_XMLImport_XMLImportZipArchiveTest extends \Tuleap\T
         $extraction_path = $this->archive->getExtractionPath();
         $this->archive->extractFiles();
         $this->archive->cleanUp();
-        $this->assertFileDoesNotExist($extraction_path);
+        self::assertFileDoesNotExist($extraction_path);
     }
 }
---
+++ plugins/tracker/tests/unit/Test/Builders/Fields/ComputedFieldBuilder.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2023-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\Test\Builders\Fields;
+
+use Tracker_FormElement_Field_Computed;
+use Tuleap\Tracker\Test\Builders\TrackerTestBuilder;
+
+final class ComputedFieldBuilder
+{
+    private \Tracker $tracker;
+
+    private function __construct(private readonly int $id)
+    {
+        $this->tracker = TrackerTestBuilder::aTracker()->withId(10)->build();
+    }
+
+    public static function aComputedField(int $id): self
+    {
+        return new self($id);
+    }
+
+    public function build(): Tracker_FormElement_Field_Computed
+    {
+        $field = new Tracker_FormElement_Field_Computed(
+            $this->id,
+            $this->tracker->getId(),
+            15,
+            'computed',
+            'Computed field',
+            '',
+            true,
+            'P',
+            false,
+            '',
+            10,
+            null
+        );
+        $field->setTracker($this->tracker);
+        return $field;
+    }
+}
