e3929ae2d302bcfe4c0fdb1f0d1ef9010b0bdf11
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2025-02-20 14:37:10 +0100

    OpenListDAO should be moved into a specific DAO and implementing delete and search interfaces
    part of story #41466 Have new Artifact Links in Artifact View
    
    Why?
    In user story AC, it is indicated that we'll need to add a checkbox to enable use to use legacy artifact link view
    in order to add this checkbox, I'll need to add a specific property for Tracker_FormElement_View_Admin_Create|Update::fetchForm
    in order to implement this specific property I'll need to have a DAO extending `SpecificPropertiesDao`
    I should not depends on legacyInterface for new commits
    
    Note:
    this file have been forgotten during development, I found it while moving the `search` method and deleting the legacy DAO objects
    
    No functional changes:
    Open list should still display a hint when you click on it
    
    Change-Id: Ia1e60bfc4f5eae331cce9b7f7e107cf1141b145d

---
+++ plugins/tracker/include/Artifact/FormElement/FieldSpecificProperties/OpenListSpecificPropertiesDAO.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\Artifact\FormElement\FieldSpecificProperties;
+
+use Tuleap\DB\DataAccessObject;
+
+final class OpenListSpecificPropertiesDAO extends DataAccessObject implements DeleteSpecificProperties, SearchSpecificProperties
+{
+    public function deleteFieldProperties(int $field_id): void
+    {
+        $this->getDB()->delete('tracker_field_openlist', ['field_id' => $field_id]);
+    }
+
+    /**
+     * @return array{field_id: int, hint: string}
+     */
+    public function searchByFieldId(int $field_id): ?array
+    {
+        $sql = 'SELECT *
+                FROM tracker_field_openlist
+                WHERE field_id = ? ';
+
+        return $this->getDB()->row($sql, $field_id);
+    }
+}
