3d63bedc87ac5a9c58cd7856685be40573561f77
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2025-02-25 11:41:31 +0100

    Add integration tests over fields properties
    part of story #41466 Have new Artifact Links in Artifact View
    
    How to test:
    make test-db should succeed
    
    Change-Id: I8a2a9dff2c909e526533d545d10f8be0f40b51b5

---
+++ plugins/tracker/tests/integration/Artifact/FormElement/FieldSpecificProperties/FloatFieldSpecificPropertiesDAOTest.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\Artifact\FormElement\FieldSpecificProperties;
+
+use ProjectUGroup;
+use Tracker;
+use Tuleap\DB\DBFactory;
+use Tuleap\Test\Builders\CoreDatabaseBuilder;
+use Tuleap\Test\PHPUnit\TestIntegrationTestCase;
+use Tuleap\Tracker\FormElement\Field\FloatingPointNumber\FloatFieldDao;
+use Tuleap\Tracker\Test\Builders\TrackerDatabaseBuilder;
+
+final class FloatFieldSpecificPropertiesDAOTest extends TestIntegrationTestCase
+{
+    private FloatFieldSpecificPropertiesDAO $dao;
+    private int $float_field_id;
+    private FloatFieldDao $old_dao;
+    private int $duplicate_field_id;
+
+    protected function setUp(): void
+    {
+        $db              = DBFactory::getMainTuleapDBConnection()->getDB();
+        $tracker_builder = new TrackerDatabaseBuilder($db);
+        $core_builder    = new CoreDatabaseBuilder($db);
+
+        $this->dao     = new FloatFieldSpecificPropertiesDAO();
+        $this->old_dao = new FloatFieldDao();
+        $project       = $core_builder->buildProject('project_name');
+        $project_id    = (int) $project->getID();
+        $user          = $core_builder->buildUser('project_member', 'Project Member', 'project_member@example.com');
+        $core_builder->addUserToProjectMembers((int) $user->getId(), $project_id);
+
+        $tracker = $tracker_builder->buildTracker($project_id, 'MyTracker');
+        $tracker_builder->setViewPermissionOnTracker($tracker->getId(), Tracker::PERMISSION_FULL, ProjectUGroup::PROJECT_MEMBERS);
+
+        $this->float_field_id     = $tracker_builder->buildFloatField($tracker->getId(), 'float_name');
+        $this->duplicate_field_id = $tracker_builder->buildFloatField($tracker->getId(), 'float_name');
+    }
+
+    public function testDefaultProperties(): void
+    {
+        $properties = $this->dao->searchByFieldId($this->float_field_id);
+        self::assertEquals(['field_id' => $this->float_field_id, 'maxchars' => 0, 'size' => 0, 'default_value' => null], $properties);
+
+        $this->old_dao->save($this->float_field_id, []);
+        $properties = $this->dao->searchByFieldId($this->float_field_id);
+        self::assertEquals(['field_id' => $this->float_field_id, 'maxchars' => 0, 'size' => 30, 'default_value' => null], $properties);
+
+        $this->dao->deleteFieldProperties($this->float_field_id);
+
+        $properties = $this->dao->searchByFieldId($this->float_field_id);
+        self::assertNull($properties);
+    }
+
+    public function testManualProperties(): void
+    {
+        $properties = $this->dao->searchByFieldId($this->float_field_id);
+        self::assertEquals(['field_id' => $this->float_field_id, 'maxchars' => 0, 'size' => 0, 'default_value' => null], $properties);
+
+        $this->old_dao->save($this->float_field_id, ['maxchars' => 34, 'size' => 12, 'default_value' => '45.01']);
+        $properties = $this->dao->searchByFieldId($this->float_field_id);
+        self::assertEquals(['field_id' => $this->float_field_id, 'maxchars' => 34, 'size' => 12, 'default_value' => 45.01], $properties);
+
+        $this->dao->duplicate($this->float_field_id, $this->duplicate_field_id);
+        $properties = $this->dao->searchByFieldId($this->duplicate_field_id);
+        self::assertEquals(['field_id' => $this->duplicate_field_id, 'maxchars' => 34, 'size' => 12, 'default_value' => 45.01], $properties);
+    }
+}
---
+++ plugins/tracker/tests/integration/Artifact/FormElement/FieldSpecificProperties/RichTextFieldSpecificPropertiesDAOTest.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\Artifact\FormElement\FieldSpecificProperties;
+
+use ProjectUGroup;
+use Tracker;
+use Tuleap\DB\DBFactory;
+use Tuleap\Test\Builders\CoreDatabaseBuilder;
+use Tuleap\Test\PHPUnit\TestIntegrationTestCase;
+use Tuleap\Tracker\FormElement\Field\StaticField\RichText\RichTextFieldDao;
+use Tuleap\Tracker\Test\Builders\TrackerDatabaseBuilder;
+
+final class RichTextFieldSpecificPropertiesDAOTest extends TestIntegrationTestCase
+{
+    private RichTextFieldSpecificPropertiesDAO $dao;
+    private int $rich_text_field_id;
+    private RichTextFieldDao $old_dao;
+
+    protected function setUp(): void
+    {
+        $db              = DBFactory::getMainTuleapDBConnection()->getDB();
+        $tracker_builder = new TrackerDatabaseBuilder($db);
+        $core_builder    = new CoreDatabaseBuilder($db);
+
+        $this->dao     = new RichTextFieldSpecificPropertiesDAO();
+        $this->old_dao = new RichTextFieldDao();
+        $project       = $core_builder->buildProject('project_name');
+        $project_id    = (int) $project->getID();
+        $user          = $core_builder->buildUser('project_member', 'Project Member', 'project_member@example.com');
+        $core_builder->addUserToProjectMembers((int) $user->getId(), $project_id);
+
+        $tracker = $tracker_builder->buildTracker($project_id, 'MyTracker');
+        $tracker_builder->setViewPermissionOnTracker($tracker->getId(), Tracker::PERMISSION_FULL, ProjectUGroup::PROJECT_MEMBERS);
+
+        $this->rich_text_field_id = $tracker_builder->buildStaticRichTextField($tracker->getId(), 'rich_text_name');
+    }
+
+    public function testDefaultProperties(): void
+    {
+        $properties = $this->dao->searchByFieldId($this->rich_text_field_id);
+        self::assertNull($properties);
+
+        $this->old_dao->save($this->rich_text_field_id, []);
+        $properties = $this->dao->searchByFieldId($this->rich_text_field_id);
+
+        self::assertNull($properties);
+    }
+
+    public function testManualProperties(): void
+    {
+        $properties = $this->dao->searchByFieldId($this->rich_text_field_id);
+        self::assertNull($properties);
+
+        $this->old_dao->save($this->rich_text_field_id, ['static_value' => 'My value']);
+        $properties = $this->dao->searchByFieldId($this->rich_text_field_id);
+        self::assertEquals(['field_id' => $this->rich_text_field_id, 'static_value' => 'My value'], $properties);
+
+        $this->dao->deleteFieldProperties($this->rich_text_field_id);
+
+        $properties = $this->dao->searchByFieldId($this->rich_text_field_id);
+        self::assertNull($properties);
+    }
+}
