a2aa3039c9600f07f2f0ded171e37dae67162d18
Kevin Traini <kevin.traini@enalean.com>
2025-03-17 11:27:37 +0100

    chore: Drop Mockery usage in some Tracker/Creation/JiraImporter tests
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    - ChangelogSnapshotBuilderTest.php
    - CurrentSnapshotBuilderTest.php
    - XmlReportCreatedRecentlyExporterTest.php
    
    *No functional changes expected*
    
    Change-Id: Icf64a6f6524720f95bac2d29d9a1d14c9a287d9d

--- plugins/tracker/tests/unit/Creation/JiraImporter/Import/Artifact/Snapshot/CurrentSnapshotBuilderTest.php
+++ plugins/tracker/tests/unit/Creation/JiraImporter/Import/Artifact/Snapshot/CurrentSnapshotBuilderTest.php
@@ -23,15 +23,18 @@ declare(strict_types=1);
 
 namespace Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\Snapshot;
 
+use ColinODell\PsrTestLogger\TestLogger;
 use Mockery;
-use PFUser;
-use Psr\Log\LoggerInterface;
+use PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles;
 use Psr\Log\NullLogger;
+use Tracker_FormElement_Field_List_Bind_Static;
+use Tracker_FormElement_Field_List_Bind_Users;
 use Tracker_FormElementFactory;
 use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\PHPUnit\TestCase;
 use Tuleap\Tracker\Creation\JiraImporter\Import\AlwaysThereFieldsExporter;
-use Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\GetExistingArtifactLinkTypes;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\Changelog\CreationStateListValueFormatter;
+use Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\GetExistingArtifactLinkTypes;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\IssueAPIRepresentation;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Artifact\LinkedIssuesCollection;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Structure\FieldMappingCollection;
@@ -39,32 +42,30 @@ use Tuleap\Tracker\Creation\JiraImporter\Import\Structure\ListFieldMapping;
 use Tuleap\Tracker\Creation\JiraImporter\Import\Structure\ScalarFieldMapping;
 use Tuleap\Tracker\Creation\JiraImporter\Import\User\JiraUserRetriever;
 
-#[\PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles]
-class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
+#[DisableReturnValueGenerationForTestDoubles]
+final class CurrentSnapshotBuilderTest extends TestCase
 {
-    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-
     public function testItBuildsSnapshotForIssueCurrentData(): void
     {
-        $logger              = Mockery::mock(LoggerInterface::class);
-        $jira_user_retriever = Mockery::mock(JiraUserRetriever::class);
+        $logger              = new TestLogger();
+        $jira_user_retriever = $this->createMock(JiraUserRetriever::class);
         $builder             = new CurrentSnapshotBuilder(
             $logger,
             new CreationStateListValueFormatter(),
             $jira_user_retriever
         );
 
-        $user                          = Mockery::mock(PFUser::class);
-        $john_doe                      = Mockery::mock(PFUser::class);
-        $mysterio                      = Mockery::mock(PFUser::class);
+        $user                          = UserTestBuilder::buildWithDefaults();
+        $john_doe                      = UserTestBuilder::buildWithId(105);
+        $mysterio                      = UserTestBuilder::buildWithId(106);
         $jira_issue_api                = $this->buildIssueAPIResponse();
         $jira_field_mapping_collection = $this->buildFieldMappingCollection();
 
-        $john_doe->shouldReceive('getId')->andReturn(105);
-        $mysterio->shouldReceive('getId')->andReturn(106);
-        $jira_user_retriever->shouldReceive('retrieveUserFromAPIData')->with(['accountId' => 'e6a7dae9', 'displayName' => 'John Doe', 'emailAddress' => 'john.doe@example.com'])->andReturn($john_doe);
-        $jira_user_retriever->shouldReceive('retrieveUserFromAPIData')->with(['accountId' => 'd45a6r4f', 'displayName' => 'Mysterio', 'emailAddress' => 'myster.io@example.com'])->andReturn($mysterio);
-        $logger->shouldReceive('debug');
+        $jira_user_retriever->method('retrieveUserFromAPIData')
+            ->willReturnCallback(static fn(array $data) => match (true) {
+                $data['accountId'] === 'e6a7dae9' && $data['displayName'] === 'John Doe' && $data['emailAddress'] === 'john.doe@example.com'  => $john_doe,
+                $data['accountId'] === 'd45a6r4f' && $data['displayName'] === 'Mysterio' && $data['emailAddress'] === 'myster.io@example.com' => $mysterio,
+            });
 
         $snapshot = $builder->buildCurrentSnapshot(
             $user,
@@ -75,19 +76,19 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
 
         self::assertSame(1587820210, $snapshot->getDate()->getTimestamp());
         self::assertSame($user, $snapshot->getUser());
-        $this->assertCount(4, $snapshot->getAllFieldsSnapshot());
+        self::assertCount(4, $snapshot->getAllFieldsSnapshot());
 
         foreach ($snapshot->getAllFieldsSnapshot() as $field_snapshot) {
             $field_id = $field_snapshot->getFieldMapping()->getJiraFieldId();
             if ($field_id === 'summary') {
                 self::assertSame('summary01', $field_snapshot->getValue());
-                $this->assertNull($field_snapshot->getRenderedValue());
+                self::assertNull($field_snapshot->getRenderedValue());
             } elseif ($field_id === 'issuetype') {
                 self::assertSame(['id' => '10004'], $field_snapshot->getValue());
-                $this->assertNull($field_snapshot->getRenderedValue());
+                self::assertNull($field_snapshot->getRenderedValue());
             } elseif ($field_id === 'assignee') {
                 self::assertSame(['id' => '105'], $field_snapshot->getValue());
-                $this->assertNull($field_snapshot->getRenderedValue());
+                self::assertNull($field_snapshot->getRenderedValue());
             } elseif ($field_id === 'homies') {
                 self::assertSame(
                     [
@@ -96,34 +97,35 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
                     ],
                     $field_snapshot->getValue()
                 );
-                $this->assertNull($field_snapshot->getRenderedValue());
+                self::assertNull($field_snapshot->getRenderedValue());
             } else {
-                $this->fail("Unexpected field $field_id in mapping");
+                self::fail("Unexpected field $field_id in mapping");
             }
         }
+        self::assertTrue($logger->hasDebugRecords());
     }
 
     private function buildIssueAPIResponse(): IssueAPIRepresentation
     {
         return IssueAPIRepresentation::buildFromAPIResponse(
             [
-                'id'     => '10042',
-                'self'   => 'https://jira_instance/rest/api/3/issue/10042',
-                'key'    => 'key01',
-                'fields' => [
+                'id'             => '10042',
+                'self'           => 'https://jira_instance/rest/api/3/issue/10042',
+                'key'            => 'key01',
+                'fields'         => [
                     'summary'   => 'summary01',
                     'issuetype' =>
                         [
                             'id' => '10004',
                         ],
-                    'created' => '2020-03-25T14:10:10.823+0100',
-                    'updated' => '2020-04-25T14:10:10.823+0100',
-                    'assignee' => [
+                    'created'   => '2020-03-25T14:10:10.823+0100',
+                    'updated'   => '2020-04-25T14:10:10.823+0100',
+                    'assignee'  => [
                         'accountId'    => 'e6a7dae9',
                         'displayName'  => 'John Doe',
                         'emailAddress' => 'john.doe@example.com',
                     ],
-                    'homies' => [
+                    'homies'    => [
                         [
                             'accountId'    => 'e6a7dae9',
                             'displayName'  => 'John Doe',
@@ -161,7 +163,7 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
                 'Fissuetype',
                 'issuetype',
                 Tracker_FormElementFactory::FIELD_SELECT_BOX_TYPE,
-                \Tracker_FormElement_Field_List_Bind_Static::TYPE,
+                Tracker_FormElement_Field_List_Bind_Static::TYPE,
                 [],
             )
         );
@@ -173,7 +175,7 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
                 'Fassignee',
                 'assignee',
                 Tracker_FormElementFactory::FIELD_SELECT_BOX_TYPE,
-                \Tracker_FormElement_Field_List_Bind_Users::TYPE,
+                Tracker_FormElement_Field_List_Bind_Users::TYPE,
                 [],
             )
         );
@@ -185,7 +187,7 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
                 'Fhomies',
                 'homies',
                 Tracker_FormElementFactory::FIELD_MULTI_SELECT_BOX_TYPE,
-                \Tracker_FormElement_Field_List_Bind_Users::TYPE,
+                Tracker_FormElement_Field_List_Bind_Users::TYPE,
                 [],
             )
         );
@@ -236,7 +238,7 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
                 'self'           => 'https://jira_instance/rest/api/3/issue/10042',
                 'key'            => 'key01',
                 'fields'         => [
-                    'updated' => '2020-04-25T14:10:10.823+0100',
+                    'updated'    => '2020-04-25T14:10:10.823+0100',
                     'issuelinks' => [
                         'issue links representation',
                     ],
@@ -297,7 +299,7 @@ class CurrentSnapshotBuilderTest extends \Tuleap\Test\PHPUnit\TestCase
             new ArtifactLinkValue(
                 [
                     [
-                        'type' => [
+                        'type'         => [
                             'name' => GetExistingArtifactLinkTypes::FAKE_JIRA_TYPE_TO_RECREATE_CHILDREN,
                         ],
                         'outwardIssue' => [
