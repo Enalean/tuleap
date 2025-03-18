f14528e5b371fc51da24f2fd7f12c3f0b724a41d
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2025-03-18 13:29:06 +0100

    Spaces can be added in front of open list values in project administration
    part of request #42233 some checks are missing for open list values in project administration field
    
    Ho to test:
    Go on an open list field with some values
    As project administrator edit the values and add spaces arroud label:
     - spaces are stored in database
    
    Note:
    I renamed `values` into `exting_values` because it tooks me a while to understand that at the same place there is valueS and value
    
    Change-Id: Ic3639b4ddafce1e1df0a674b1fe83939b629bf02

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field_List_Bind_Static.php
@@ -715,13 +715,14 @@ class Tracker_FormElement_Field_List_Bind_Static extends Tracker_FormElement_Fie
                     }
                     break;
                 case 'edit_custom':
-                    $values = $this->getOpenValueDao()->searchByFieldId($this->field->getId());
-                    foreach ($values as $row) {
-                        $new_is_hidden = ! isset($value[$row['id']]['is_hidden']);
+                    $existing_values = $this->getOpenValueDao()->searchByFieldId($this->field->getId());
+                    foreach ($existing_values as $row) {
+                        $existing_value_id = $row['id'];
+                        $new_is_hidden     = ! isset($value[$existing_value_id]['is_hidden']);
                         $this->getOpenValueDao()->updateOpenValue(
-                            (int) $row['id'],
+                            (int) $existing_value_id,
                             $new_is_hidden,
-                            $value[$row['id']]['label']
+                            trim($value[$existing_value_id]['label'])
                         );
                     }
                     break;
