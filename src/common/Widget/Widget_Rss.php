Enalean/tuleap

commit 54cce3f5e883d16055cb0239e023f48cdf5eb25f

fix: request #42243 XSS via the content of RSS feeds in the RSS widgets
In addition to the fix itself, additional defense in depth has been
added: accesses that do not seem to come from a `fetch()` request
are now rejected.

Change-Id: I1d854c0a108c57901accc2c2bd1f18cda86acf96

src/common/Widget/Widget_Rss.php
@@ -66,9 +66,11 @@ abstract class Widget_Rss extends Widget // phpcs:ignore PSR1.Classes.ClassDecla
             fn (EntryInterface $a, EntryInterface $b): int => $b->getDateModified() <=> $a->getDateModified(),
         );
 
+        $uri_sanitizer = new \Tuleap\Sanitizer\URISanitizer(new Valid_HTTPURI());
+
         foreach (\Psl\Dict\slice($feed_entries, 0, 10) as $entry) {
             $content .= '<tr><td>';
-            $content .= '<a href="' . $hp->purify($entry->getLink()) . '">' . $hp->purify($entry->getTitle(), CODENDI_PURIFIER_STRIP_HTML) . '</a>';
+            $content .= '<a href="' . $hp->purify($uri_sanitizer->sanitizeForHTMLAttribute($entry->getLink())) . '">' . $hp->purify($entry->getTitle(), CODENDI_PURIFIER_STRIP_HTML) . '</a>';
             $date     = $entry->getDateCreated();
             if ($date !== null) {
                 $content .= '<span style="color:#999;" title="' . format_date($GLOBALS['Language']->getText('system', 'datefmt'), $date->getTimestamp()) . '"> - ' . DateHelper::timeAgoInWords($date->getTimestamp()) . '</span>';

src/common/Dashboard/Widget/Router.php
@@ -55,9 +55,11 @@ class Router
 
         switch ($action) {
             case 'get-add-modal-content':
+                $this->rejectIfRequestDoesNotAppearToBeFetched($request);
                 $this->add_widget_controller->display($request);
                 break;
             case 'get-edit-modal-content':
+                $this->rejectIfRequestDoesNotAppearToBeFetched($request);
                 $this->preferences_controller->display($request);
                 break;
             case 'add-widget':
@@ -83,6 +85,7 @@ class Router
                 $instance_id = (int) $param[$name];
 
                 if ($widget->isAjax()) {
+                    $this->rejectIfRequestDoesNotAppearToBeFetched($request);
                     $widget->loadContent($instance_id);
                     echo $widget->getContent();
                 }
@@ -90,10 +93,29 @@ class Router
             case 'rss':
                 $widget = $this->getWidgetFromUrl($request);
                 if ($widget) {
+                    $this->rejectIfRequestDoesNotAppearToBeFetched($request);
                     $widget->displayRss();
                 }
                 break;
         }
+    }
+
+    private function rejectIfRequestDoesNotAppearToBeFetched(HTTPRequest $request): void
+    {
+        $sec_fetch_dest = $request->getFromServer('HTTP_SEC_FETCH_DEST');
+        $sec_fetch_mode = $request->getFromServer('HTTP_SEC_FETCH_MODE');
+
+        if ($sec_fetch_dest === false || $sec_fetch_mode === false) {
+            return;
+        }
+
+        if ($sec_fetch_dest === 'empty' && $sec_fetch_mode === 'cors') {
+            return;
+        }
+
+        $GLOBALS['Response']->sendStatusCode(400);
+        echo 'Direct accesses to this page are not expected';
+        exit;
     }
 
     private function getWidgetFromUrl(HTTPRequest $request)

commit 19e68b36b12ca15779d1e85efaa17caac39880c5

fix: request #42261 RSS widget crashes when a feed entry do not have an associated link
Introduced by 54cce3f5e883d16055cb0239e023f48cdf5eb25f.

Issue can be reproduced using the PHP releases feed: https://www.php.net/releases/feed.php

Change-Id: Idf7d8a0c4f4de50fd0209ac1d0254b77a943c3c5

src/common/Widget/Widget_Rss.php
@@ -69,8 +69,13 @@ abstract class Widget_Rss extends Widget // phpcs:ignore PSR1.Classes.ClassDecla
         $uri_sanitizer = new \Tuleap\Sanitizer\URISanitizer(new Valid_HTTPURI());
 
         foreach (\Psl\Dict\slice($feed_entries, 0, 10) as $entry) {
-            $content .= '<tr><td>';
-            $content .= '<a href="' . $hp->purify($uri_sanitizer->sanitizeForHTMLAttribute($entry->getLink())) . '">' . $hp->purify($entry->getTitle(), CODENDI_PURIFIER_STRIP_HTML) . '</a>';
+            $content      .= '<tr><td>';
+            $entry_content = $hp->purify($entry->getTitle(), CODENDI_PURIFIER_STRIP_HTML);
+            $link          = $entry->getPermalink();
+            if ($link !== null) {
+                $entry_content = '<a href="' . $hp->purify($uri_sanitizer->sanitizeForHTMLAttribute($link)) . '">' . $entry_content . '</a>';
+            }
+            $content .= $entry_content;
             $date     = $entry->getDateCreated();
             if ($date !== null) {
                 $content .= '<span style="color:#999;" title="' . format_date($GLOBALS['Language']->getText('system', 'datefmt'), $date->getTimestamp()) . '"> - ' . DateHelper::timeAgoInWords($date->getTimestamp()) . '</span>';