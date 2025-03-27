d55b7e1c3ca5533fcd07d0aadfb1ec327f2600f4
Joris Masson <joris.masson@enalean.com>
2025-03-27 17:52:28 +0100

    refactor: Composition API in BaseCard
    part of request #41443 convert taskboard into vue3
    
    How to test:
    - You can still edit a card's title, remaining effort or assignees
    - No functional changes
    
    Change-Id: I7d5e032eea66f6d5daaaa56c82924091acfd6a17

--- plugins/taskboard/scripts/taskboard/src/components/TaskBoard/Body/Swimlane/Card/BaseCard.vue
+++ plugins/taskboard/scripts/taskboard/src/components/TaskBoard/Body/Swimlane/Card/BaseCard.vue
@@ -19,7 +19,7 @@
   -->
 
 <template>
-    <div class="taskboard-card" v-bind:class="additional_classnames">
+    <div ref="root" class="taskboard-card" v-bind:class="additional_classnames">
         <div class="taskboard-card-content">
             <card-xref-label v-bind:card="card" v-bind:label="label" />
             <card-info v-bind:card="card" v-bind:tracker="tracker" v-model="assignees">
@@ -44,166 +44,158 @@
         <slot name="remaining_effort" />
     </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+<script setup lang="ts">
+import { computed, onMounted, onUnmounted, ref } from "vue";
+import {
+    useGetters,
+    useNamespacedActions,
+    useNamespacedMutations,
+    useNamespacedState,
+} from "vuex-composition-helpers";
 import CardXrefLabel from "./CardXrefLabel.vue";
 import type { Card, Tracker, User } from "../../../../../type";
 import { TaskboardEvent } from "../../../../../type";
-import { namespace, Getter } from "vuex-class";
 import EventBus from "../../../../../helpers/event-bus";
 import type { UpdateCardPayload } from "../../../../../store/swimlane/card/type";
 import LabelEditor from "./Editor/Label/LabelEditor.vue";
 import CardInfo from "./CardInfo.vue";
 import { haveAssigneesChanged } from "../../../../../helpers/have-assignees-changed";
 import { scrollToItemIfNeeded } from "../../../../../helpers/scroll-to-item";
+import type { UserState } from "../../../../../store/user/type";
+import type { FullscreenState } from "../../../../../store/fullscreen/type";
 
-const user = namespace("user");
-const swimlane = namespace("swimlane");
-const fullscreen = namespace("fullscreen");
+const { user_has_accessibility_mode } = useNamespacedState<UserState>("user", [
+    "user_has_accessibility_mode",
+]);
 
-@Component({
-    components: {
-        CardInfo,
-        LabelEditor,
-        CardXrefLabel,
-    },
-})
-export default class BaseCard extends Vue {
-    @user.State
-    readonly user_has_accessibility_mode!: boolean;
+const props = defineProps<{ card: Card }>();
 
-    @Prop({ required: true })
-    readonly card!: Card;
+const emit = defineEmits<{
+    (e: "editor-closed"): void;
+}>();
 
-    @Getter
-    readonly tracker_of_card!: (card: Card) => Tracker;
+const { tracker_of_card } = useGetters(["tracker_of_card"]);
 
-    @swimlane.Mutation
-    readonly addCardToEditMode!: (card: Card) => void;
+const { addCardToEditMode, removeCardFromEditMode } = useNamespacedMutations("swimlane", [
+    "addCardToEditMode",
+    "removeCardFromEditMode",
+]);
 
-    @swimlane.Mutation
-    readonly removeCardFromEditMode!: (card: Card) => void;
+const { saveCard } = useNamespacedActions("swimlane", ["saveCard"]);
 
-    @swimlane.Action
-    readonly saveCard!: (payload: UpdateCardPayload) => Promise<void>;
+const { is_taskboard_in_fullscreen_mode } = useNamespacedState<FullscreenState>("fullscreen", [
+    "is_taskboard_in_fullscreen_mode",
+]);
 
-    @fullscreen.State
-    readonly is_taskboard_in_fullscreen_mode!: boolean;
+const root = ref<HTMLElement>();
+const label = ref("");
+const assignees = ref<User[]>([]);
 
-    label = "";
-    assignees: User[] = [];
+const tracker = computed((): Tracker => tracker_of_card.value(props.card));
+const can_user_update_card = computed(
+    (): boolean => tracker.value.title_field !== null && !props.card.is_being_saved,
+);
+const show_accessibility_pattern = computed(
+    (): boolean => user_has_accessibility_mode.value && props.card.background_color.length > 0,
+);
+const is_label_changed = computed((): boolean => label.value !== props.card.label);
 
-    mounted(): void {
-        this.label = this.card.label;
-        this.assignees = this.card.assignees;
-        EventBus.$on(TaskboardEvent.CANCEL_CARD_EDITION, this.cancelButtonCallback);
-        EventBus.$on(TaskboardEvent.SAVE_CARD_EDITION, this.saveButtonCallback);
-    }
+onMounted(() => {
+    label.value = props.card.label;
+    assignees.value = props.card.assignees;
+    EventBus.$on(TaskboardEvent.CANCEL_CARD_EDITION, cancelButtonCallback);
+    EventBus.$on(TaskboardEvent.SAVE_CARD_EDITION, saveButtonCallback);
+});
 
-    beforeDestroy(): void {
-        EventBus.$off(TaskboardEvent.CANCEL_CARD_EDITION, this.cancelButtonCallback);
-        EventBus.$off(TaskboardEvent.SAVE_CARD_EDITION, this.saveButtonCallback);
-    }
+onUnmounted(() => {
+    EventBus.$off(TaskboardEvent.CANCEL_CARD_EDITION, cancelButtonCallback);
+    EventBus.$off(TaskboardEvent.SAVE_CARD_EDITION, saveButtonCallback);
+});
 
-    cancelButtonCallback(card: Card): void {
-        if (card.id === this.card.id) {
-            this.cancel();
-        }
-    }
-
-    saveButtonCallback(card: Card): void {
-        if (card.id === this.card.id) {
-            this.save();
-        }
-    }
-
-    save(): void {
-        if (!this.is_label_changed && !haveAssigneesChanged(this.card.assignees, this.assignees)) {
-            this.cancel();
-            return;
-        }
-
-        const payload: UpdateCardPayload = {
-            card: this.card,
-            label: this.label,
-            assignees: this.assignees,
-            tracker: this.tracker,
-        };
-        this.saveCard(payload);
-
-        this.$emit("editor-closed");
-    }
-
-    cancel(): void {
-        this.removeCardFromEditMode(this.card);
-        this.label = this.card.label;
-        this.$emit("editor-closed");
-    }
-
-    switchToEditMode(): void {
-        if (this.card.is_in_edit_mode) {
-            return;
-        }
-
-        if (this.card.is_being_saved) {
-            return;
-        }
-
-        this.addCardToEditMode(this.card);
-
-        setTimeout((): void => {
-            let fullscreen_element = null;
-
-            if (this.is_taskboard_in_fullscreen_mode) {
-                fullscreen_element = document.querySelector(".taskboard");
-            }
-
-            scrollToItemIfNeeded(this.$el, fullscreen_element);
-        }, 100);
-    }
-
-    get is_label_changed(): boolean {
-        return this.label !== this.card.label;
-    }
-
-    get additional_classnames(): string {
-        const classnames = [`taskboard-card-${this.card.color}`];
-
-        if (this.card.background_color) {
-            classnames.push(`taskboard-card-background-${this.card.background_color}`);
-        }
-
-        if (this.show_accessibility_pattern) {
-            classnames.push("taskboard-card-with-accessibility");
-        }
-
-        if (this.card.is_in_edit_mode) {
-            classnames.push("taskboard-card-edit-mode");
-        } else if (this.card.is_being_saved) {
-            classnames.push("taskboard-card-is-being-saved");
-        } else if (this.card.is_just_saved) {
-            classnames.push("taskboard-card-is-just-saved");
-        }
-
-        if (this.can_user_update_card) {
-            classnames.push("taskboard-card-editable");
-        }
-
-        return classnames.join(" ");
-    }
-
-    get can_user_update_card(): boolean {
-        return this.tracker.title_field !== null && !this.card.is_being_saved;
-    }
-
-    get show_accessibility_pattern(): boolean {
-        return this.user_has_accessibility_mode && this.card.background_color.length > 0;
-    }
-
-    get tracker(): Tracker {
-        return this.tracker_of_card(this.card);
+function cancelButtonCallback(card: Card): void {
+    if (card.id === props.card.id) {
+        cancel();
     }
 }
+
+function saveButtonCallback(card: Card): void {
+    if (card.id === props.card.id) {
+        save();
+    }
+}
+
+function save(): void {
+    if (!is_label_changed.value && !haveAssigneesChanged(props.card.assignees, assignees.value)) {
+        cancel();
+        return;
+    }
+
+    const payload: UpdateCardPayload = {
+        card: props.card,
+        label: label.value,
+        assignees: assignees.value,
+        tracker: tracker.value,
+    };
+    saveCard(payload);
+
+    emit("editor-closed");
+}
+
+function cancel(): void {
+    removeCardFromEditMode(props.card);
+    label.value = props.card.label;
+    emit("editor-closed");
+}
+
+function switchToEditMode(): void {
+    if (props.card.is_in_edit_mode) {
+        return;
+    }
+
+    if (props.card.is_being_saved) {
+        return;
+    }
+
+    addCardToEditMode(props.card);
+
+    setTimeout((): void => {
+        let fullscreen_element = null;
+
+        if (is_taskboard_in_fullscreen_mode.value) {
+            fullscreen_element = document.querySelector(".taskboard");
+        }
+
+        if (root.value) {
+            scrollToItemIfNeeded(root.value, fullscreen_element);
+        }
+    }, 100);
+}
+
+const additional_classnames = computed((): string => {
+    const classnames = [`taskboard-card-${props.card.color}`];
+
+    if (props.card.background_color) {
+        classnames.push(`taskboard-card-background-${props.card.background_color}`);
+    }
+
+    if (show_accessibility_pattern.value) {
+        classnames.push("taskboard-card-with-accessibility");
+    }
+
+    if (props.card.is_in_edit_mode) {
+        classnames.push("taskboard-card-edit-mode");
+    } else if (props.card.is_being_saved) {
+        classnames.push("taskboard-card-is-being-saved");
+    } else if (props.card.is_just_saved) {
+        classnames.push("taskboard-card-is-just-saved");
+    }
+
+    if (can_user_update_card.value) {
+        classnames.push("taskboard-card-editable");
+    }
+
+    return classnames.join(" ");
+});
+
+defineExpose({ label });
 </script>
