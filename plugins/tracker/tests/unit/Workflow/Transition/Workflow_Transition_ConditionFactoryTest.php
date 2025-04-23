1498bb8487826da76bf2f7ebaea666f1027665c9
Nicolas Terray <nicolas.terray@enalean.com>
2025-04-23 15:00:57 +0200

    chore: Drop Mockery usage in Workflow/Transition
    Part of request #21775 Favor PHPUnit mock system over Mockery - part 2
    
    *No functional changes expected*
    
    Change-Id: Ic5df8747d796f2693f6aa26edad5ace0d1838847

--- plugins/tracker/tests/unit/Workflow/Transition/Workflow_Transition_ConditionFactoryTest.php
+++ plugins/tracker/tests/unit/Workflow/Transition/Workflow_Transition_ConditionFactoryTest.php
@@ -20,38 +20,35 @@
 
 declare(strict_types=1);
 
+use PHPUnit\Framework\MockObject\MockObject;
 use Tuleap\Project\Duplication\DuplicationUserGroupMapping;
+use Tuleap\Test\Builders\ProjectTestBuilder;
 
-// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
 #[\PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles]
-final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUnit\TestCase
+final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUnit\TestCase // phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotCamelCaps
 {
-    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+    private array $xml_mapping = [];
 
-    private $xml_mapping = [];
+    private Workflow_Transition_ConditionFactory $condition_factory;
 
-    /** @var Workflow_Transition_ConditionFactory */
-    private $condition_factory;
+    private Workflow_Transition_Condition_CommentNotEmpty_Factory&MockObject $commentnotempty_factory;
 
-    /** @var Workflow_Transition_Condition_CommentNotEmpty_Factory */
-    private $commentnotempty_factory;
+    private Workflow_Transition_Condition_FieldNotEmpty_Factory&MockObject $fieldnotempty_factory;
 
-    /** @var Workflow_Transition_Condition_FieldNotEmpty_Factory */
-    private $fieldnotempty_factory;
+    private Workflow_Transition_Condition_Permissions_Factory&MockObject $permissions_factory;
 
-    /** @var Workflow_Transition_Condition_Permissions_Factory */
-    private $permissions_factory;
-
-    private $project;
-    private $transition;
-    private $legacy_permissions_xml;
-    private $from_5_7_permissions_xml;
+    private Project $project;
+    private Transition&MockObject $transition;
+    private SimpleXMLElement $legacy_permissions_xml;
+    private SimpleXMLElement $from_5_7_permissions_xml;
 
     protected function setUp(): void
     {
-        $this->permissions_factory     = \Mockery::spy(\Workflow_Transition_Condition_Permissions_Factory::class);
-        $this->fieldnotempty_factory   = \Mockery::spy(\Workflow_Transition_Condition_FieldNotEmpty_Factory::class);
-        $this->commentnotempty_factory = \Mockery::spy(\Workflow_Transition_Condition_CommentNotEmpty_Factory::class);
+        $this->permissions_factory     = $this->createMock(\Workflow_Transition_Condition_Permissions_Factory::class);
+        $this->fieldnotempty_factory   = $this->createMock(\Workflow_Transition_Condition_FieldNotEmpty_Factory::class);
+        $this->commentnotempty_factory = $this->createMock(\Workflow_Transition_Condition_CommentNotEmpty_Factory::class);
+
+        $this->commentnotempty_factory->method('duplicate');
 
         $this->condition_factory = new Workflow_Transition_ConditionFactory(
             $this->permissions_factory,
@@ -59,11 +56,11 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
             $this->commentnotempty_factory
         );
 
-        $this->project = \Mockery::spy(\Project::class);
+        $this->project = ProjectTestBuilder::aProject()->build();
 
-        PermissionsManager::setInstance(\Mockery::spy(\PermissionsManager::class));
+        PermissionsManager::setInstance($this->createMock(\PermissionsManager::class));
 
-        $this->transition = \Mockery::spy(\Transition::class);
+        $this->transition = $this->createMock(\Transition::class);
 
         $this->legacy_permissions_xml = new SimpleXMLElement('
             <transition>
@@ -73,14 +70,6 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
                 </permissions>
             </transition>
         ');
-        $this->permissions_factory->shouldReceive('getInstanceFromXML')->with(
-            Mockery::on(static function (SimpleXMLElement $xml): bool {
-                return isset($xml->permission);
-            }),
-            $this->xml_mapping,
-            $this->transition,
-            $this->project
-        )->andReturns(\Mockery::spy(\Workflow_Transition_Condition_Permissions::class));
 
         $this->from_5_7_permissions_xml = new SimpleXMLElement('
             <transition>
@@ -94,14 +83,14 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
                 </conditions>
             </transition>
         ');
-        $this->permissions_factory->shouldReceive('getInstanceFromXML')->with(
-            Mockery::on(static function (SimpleXMLElement $xml): bool {
-                return isset($xml->permissions);
-            }),
-            $this->xml_mapping,
-            $this->transition,
-            $this->project
-        )->andReturns(\Mockery::spy(\Workflow_Transition_Condition_Permissions::class));
+
+        $this->permissions_factory->method('getInstanceFromXML')
+            ->willReturnCallback(
+                fn (SimpleXMLElement $xml) => match (true) {
+                    isset($xml->permission),
+                    isset($xml->permissions) => $this->createMock(\Workflow_Transition_Condition_Permissions::class),
+                }
+            );
     }
 
     protected function tearDown(): void
@@ -111,8 +100,8 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
 
     public function testItDelegatesToFieldNotEmptyFactory(): void
     {
-        $field = \Mockery::spy(\Tracker_FormElement_Field_Date::class);
-        $this->fieldnotempty_factory->shouldReceive('isFieldUsedInConditions')->with($field)->once()->andReturns(true);
+        $field = $this->createMock(\Tracker_FormElement_Field_Date::class);
+        $this->fieldnotempty_factory->expects($this->once())->method('isFieldUsedInConditions')->with($field)->willReturn(true);
         $this->assertTrue($this->condition_factory->isFieldUsedInConditions($field));
     }
 
@@ -152,16 +141,12 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
             </transition>
         ');
 
-        $condition = \Mockery::spy(\Workflow_Transition_Condition_FieldNotEmpty::class);
-        $this->fieldnotempty_factory->shouldReceive('getInstanceFromXML')->with(
-            Mockery::on(
-                static function (SimpleXMLElement $xml): bool {
-                    return (string) $xml['type'] === 'notempty';
-                }
-            ),
-            \Mockery::any(),
-            \Mockery::any()
-        )->andReturns($condition);
+        $condition = $this->createMock(\Workflow_Transition_Condition_FieldNotEmpty::class);
+        $this->fieldnotempty_factory->method('getInstanceFromXML')->willReturnCallback(
+            static fn (SimpleXMLElement $xml) => match ((string) $xml['type']) {
+                'notempty' => $condition
+            }
+        );
 
         $conditions = $this->condition_factory->getAllInstancesFromXML(
             $xml,
@@ -183,7 +168,7 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
             </transition>
         ');
 
-        $this->fieldnotempty_factory->shouldReceive('getInstanceFromXML')->andReturns(null);
+        $this->fieldnotempty_factory->method('getInstanceFromXML')->willReturn(null);
 
         $conditions = $this->condition_factory->getAllInstancesFromXML(
             $xml,
@@ -202,8 +187,8 @@ final class Workflow_Transition_ConditionFactoryTest extends \Tuleap\Test\PHPUni
 
         $mapping = DuplicationUserGroupMapping::fromNewProjectWithMapping([103 => 122]);
 
-        $this->permissions_factory->shouldReceive('duplicate')->with($this->transition, $new_transition_id, $mapping)->once();
-        $this->fieldnotempty_factory->shouldReceive('duplicate')->with($this->transition, $new_transition_id, $field_mapping)->once();
+        $this->permissions_factory->expects($this->once())->method('duplicate')->with($this->transition, $new_transition_id, $mapping);
+        $this->fieldnotempty_factory->expects($this->once())->method('duplicate')->with($this->transition, $new_transition_id, $field_mapping);
         $this->condition_factory->duplicate($this->transition, $new_transition_id, $field_mapping, $mapping);
     }
 }
