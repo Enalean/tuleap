4cbd842e7abb733bada290c692ec81f5bc4f7f69
Kevin Traini <kevin.traini@enalean.com>
2025-05-02 11:04:39 +0200

    fix: request #42964 WorkflowXMLExporter should skip missing transitions
    If the mapping doesn't have the value then we just skip it
    
    *Testing:*
    
    Tracker XML export should still work
    
    Change-Id: I5da8362258ed2d7bbfae250e8854c67981bf160d

--- plugins/tracker/tests/unit/Workflow/SimpleMode/SimpleWorkflowXMLExporterTest.php
+++ plugins/tracker/tests/unit/Workflow/SimpleMode/SimpleWorkflowXMLExporterTest.php
@@ -22,15 +22,20 @@ declare(strict_types=1);
 
 namespace Tuleap\Tracker\Workflow\SimpleMode;
 
+use PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles;
 use SimpleXMLElement;
-use Tuleap\Tracker\Workflow\SimpleMode\State\TransitionExtractor;
+use Transition;
+use Transition_PostAction_CIBuild;
+use Transition_PostAction_Field_Int;
+use Tuleap\Test\PHPUnit\TestCase;
 use Tuleap\Tracker\Workflow\SimpleMode\State\State;
 use Tuleap\Tracker\Workflow\SimpleMode\State\StateFactory;
+use Tuleap\Tracker\Workflow\SimpleMode\State\TransitionExtractor;
 use Workflow;
 use Workflow_Transition_ConditionsCollection;
 
-#[\PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles]
-final class SimpleWorkflowXMLExporterTest extends \Tuleap\Test\PHPUnit\TestCase
+#[DisableReturnValueGenerationForTestDoubles]
+final class SimpleWorkflowXMLExporterTest extends TestCase
 {
     public function testItExportsTheSimpleWorkflowInXML(): void
     {
@@ -51,16 +56,16 @@ final class SimpleWorkflowXMLExporterTest extends \Tuleap\Test\PHPUnit\TestCase
             ['to_id' => 201],
         ]);
 
-        $transition_01 = $this->createMock(\Transition::class);
-        $transition_02 = $this->createMock(\Transition::class);
-        $transition_03 = $this->createMock(\Transition::class);
+        $transition_01 = $this->createMock(Transition::class);
+        $transition_02 = $this->createMock(Transition::class);
+        $transition_03 = $this->createMock(Transition::class);
 
         $transition_01->method('getIdFrom')->willReturn('');
         $transition_02->method('getIdFrom')->willReturn('');
         $transition_03->method('getIdFrom')->willReturn('410');
 
-        $post_action_01 = $this->createMock(\Transition_PostAction_CIBuild::class);
-        $post_action_02 = $this->createMock(\Transition_PostAction_Field_Int::class);
+        $post_action_01 = $this->createMock(Transition_PostAction_CIBuild::class);
+        $post_action_02 = $this->createMock(Transition_PostAction_Field_Int::class);
 
         $transition_01->method('getPostActions')->willReturn([]);
         $transition_03->method('getPostActions')->willReturn([
@@ -81,13 +86,13 @@ final class SimpleWorkflowXMLExporterTest extends \Tuleap\Test\PHPUnit\TestCase
         $conditions_collection_02->expects($this->once())->method('exportToXML');
 
         $state_factory->method('getStateFromValueId')
-            ->willReturnCallback(static fn (Workflow $workflow, int $value_id) => match ($value_id) {
+            ->willReturnCallback(static fn(Workflow $workflow, int $value_id) => match ($value_id) {
                 200 => new State(200, [$transition_01]),
                 201 => new State(201, [$transition_02, $transition_03])
             });
 
         $mapping = [
-            'F114' => '114',
+            'F114'   => '114',
             'values' => [
                 'V114-0' => '200',
                 'V114-1' => '201',
@@ -97,11 +102,11 @@ final class SimpleWorkflowXMLExporterTest extends \Tuleap\Test\PHPUnit\TestCase
 
         $exporter->exportToXML($workflow, $xml, $mapping);
 
-        $this->assertEquals((string) $xml->field_id['REF'], 'F114');
-        $this->assertEquals((string) $xml->is_used, '1');
+        self::assertEquals('F114', (string) $xml->field_id['REF']);
+        self::assertEquals('1', (string) $xml->is_used);
 
-        $this->assertTrue(isset($xml->states));
-        $this->assertCount(2, $xml->states->state);
+        self::assertTrue(isset($xml->states));
+        self::assertCount(2, $xml->states->state);
 
         $xml_state_01 = $xml->states->state[0];
         $xml_state_02 = $xml->states->state[1];
@@ -112,11 +117,44 @@ final class SimpleWorkflowXMLExporterTest extends \Tuleap\Test\PHPUnit\TestCase
         $xml_state_01_transitions = $xml_state_01->transitions;
         $xml_state_02_transitions = $xml_state_02->transitions;
 
-        $this->assertCount(1, $xml_state_01_transitions->transition);
-        $this->assertCount(2, $xml_state_02_transitions->transition);
+        self::assertCount(1, $xml_state_01_transitions->transition);
+        self::assertCount(2, $xml_state_02_transitions->transition);
 
         self::assertSame((string) $xml_state_01_transitions->transition->from_id['REF'], 'null');
         self::assertSame((string) $xml_state_02_transitions->transition[0]->from_id['REF'], 'null');
         self::assertSame((string) $xml_state_02_transitions->transition[1]->from_id['REF'], 'V114-2');
+    }
+
+    public function testItSkipsMissingMappingValues(): void
+    {
+        $xml           = new SimpleXMLElement('<simple_workflow/>');
+        $dao           = $this->createMock(SimpleWorkflowDao::class);
+        $extractor     = new TransitionExtractor();
+        $state_factory = $this->createMock(StateFactory::class);
+
+        $exporter = new SimpleWorkflowXMLExporter($dao, $state_factory, $extractor);
+
+        $workflow = $this->createMock(Workflow::class);
+        $workflow->expects($this->once())->method('getFieldId')->willReturn(114);
+        $workflow->expects($this->once())->method('isUsed')->willReturn(true);
+        $workflow->expects($this->once())->method('getId')->willReturn('999');
+
+        $dao->method('searchStatesForWorkflow')->with('999')->willReturn([
+            ['to_id' => 200],
+            ['to_id' => 201],
+        ]);
+
+        $state_factory->expects($this->never())->method('getStateFromValueId');
+
+        $exporter->exportToXML($workflow, $xml, [
+            'F114'   => '114',
+            'values' => [],
+        ]);
+
+        self::assertEquals('F114', (string) $xml->field_id['REF']);
+        self::assertEquals('1', (string) $xml->is_used);
+
+        self::assertTrue(isset($xml->states));
+        self::assertCount(0, $xml->states->state);
     }
 }
