bd595b4dbe1337500c78762dabb5f566a742d382
Clarisse Deschamps <clarisse.deschamps@enalean.com>
2025-05-09 15:18:35 +0200

    test: add a test to check the specific propoerties of the new artifact link field
    Part of story #41466 Have new Artifact Links in Artifact View
    
    What is the test scenario?
    - Add an artifact link field to the empty tracker
    - Check that specific property is enabled by default
    - Check that the new interface is displayed
    - Change the specific property
    - Check that the old interface is displayed
    
    How to test:
    make tests-e2e should pass
    
    Change-Id: I5a0c4ad384d84a7d8f860c14c0746561d929147e

--- plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.php
+++ plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Field.php
@@ -842,9 +842,9 @@ abstract class Tracker_FormElement_Field extends Tracker_FormElement implements 
 
         $usage_in_semantics = $this->getUsagesInSemantics();
 
-        $html         .= '<div class="tracker-admin-field" id="tracker-admin-formElements_' . $this->id . '" data-test="tracker-admin-field" >';
-        $html         .= '<div class="tracker-admin-field-controls">';
-                $html .= '<a class="edit-field" href="' . $this->getAdminEditUrl() . '">' . $GLOBALS['HTML']->getImage('ic/edit.png', ['alt' => 'edit']) . '</a> ';
+        $html .= '<div class="tracker-admin-field" id="tracker-admin-formElements_' . $this->id . '" data-test="tracker-admin-field" >';
+        $html .= '<div class="tracker-admin-field-controls">';
+        $html .= '<a class="edit-field" data-test="edit-field" href="' . $this->getAdminEditUrl() . '">' . $GLOBALS['HTML']->getImage('ic/edit.png', ['alt' => 'edit']) . '</a> ';
         if ($usage_in_semantics->areThereSemanticsUsingField() === false && $this->canBeRemovedFromUsage()) {
             $csrf_token = $this->getCSRFTokenForElementUpdate();
             $html      .= '<form method="POST" action="?">';
