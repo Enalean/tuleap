Enalean/tuleap

commit 6c81952847b4571fefff2eb9158ab18db5b05715

chore: Remove vendoring of jQuery/jQuery-ui
Some adjustments have been made to keep things working the same way
after the upgrades.

Closes request #42980 Library jquery 1.9.1 is vulnerable

Change-Id: I8bc918074ee20040e68ffb04ba5362fcf7ce8952

plugins/tracker/include/Tracker/Report/Tracker_Report_Renderer_Table.php
@@ -557,7 +557,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
 
         $my_items            = ['export' => ''];
         $my_items['export'] .= '<div class="btn-group">';
-        $my_items['export'] .= '<a class="btn btn-mini dropdown-toggle" data-toggle="dropdown" href="#">';
+        $my_items['export'] .= '<a class="btn btn-mini dropdown-toggle" data-toggle="dropdown">';
         $my_items['export'] .= '<i class="fa fa-download"></i> ';
         $my_items['export'] .= dgettext('tuleap-tracker', 'Export');
         $my_items['export'] .= ' <span class="caret"></span>';

plugins/docman/templates/admin-header-fp.mustache
@@ -8,7 +8,7 @@
         {{/ tabs }}
         {{# extra_tabs }}
             <li class="dropdown{{# is_active }} active{{/ is_active }}">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <a class="dropdown-toggle" data-toggle="dropdown">
                     <i class="fas fa-bars"></i>
                     <i class="caret"></i>
                 </a>

plugins/git/templates/admin-gerrit-templates.mustache
@@ -27,7 +27,7 @@
         <div class="control-group info">
             <div class="controls">
                 <div class="btn-group dropdown" id="git_admin_config_btn_create">
-                     <span class="dropdown-toggle btn btn-primary" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="#">
+                     <span class="dropdown-toggle btn btn-primary" id="dLabel" role="button" data-toggle="dropdown">
                             {{ create_new_template_text }}
                             <b class="caret"></b>
                     </span>

plugins/graphontrackersv5/include/GraphOnTrackersV5_Renderer.php
@@ -177,7 +177,7 @@ class GraphOnTrackersV5_Renderer extends Tracker_Report_Renderer
         if (! $readonly) {
             $html   .= '<div id="tracker_report_renderer_view_controls">';
             $html   .= '<div class="btn-group">';
-            $html   .= '<a href="#" class="btn btn-mini dropdown-toggle" data-toggle="dropdown">';
+            $html   .= '<a class="btn btn-mini dropdown-toggle" data-toggle="dropdown">';
             $html   .= '<i class="fa fa-plus"></i> ';
             $html   .= dgettext('tuleap-graphontrackersv5', 'Add a Chart');
             $html   .= ' <span class="caret"></span>';

plugins/graphontrackersv5/templates/graph-actions.mustache
@@ -9,7 +9,7 @@
 {{/ can_be_updated }}
 
 <div class="widget_titlebar_prefs dropdown">
-    <a class="fa fa-cog dropdown-toggle" data-toggle="dropdown" href="#" title="#"></a>
+    <a class="fa fa-cog dropdown-toggle" data-toggle="dropdown"></a>
 
     <ul class="dropdown-menu pull-right">
         {{# can_be_updated }}

plugins/tracker/include/Artifact/View/ArtifactViewEdit.php
@@ -178,7 +178,7 @@ readonly class ArtifactViewEdit extends TrackerArtifactView
 
         $html  = '<div class="tracker_artifact_followup_comments_display_settings">';
         $html .= '<div class="btn-group">';
-        $html .= '<a href="#" class="btn btn-small dropdown-toggle" data-toggle="dropdown">';
+        $html .= '<a class="btn btn-small dropdown-toggle" data-toggle="dropdown">';
         $html .= '<i class="fa fa-cog"></i> ' . $settings_label . ' <span class="caret"></span>';
         $html .= '</a>';
         $html .= '<ul class="dropdown-menu pull-right">';

plugins/tracker/include/Tracker/Report/Tracker_Report.php
@@ -954,7 +954,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                         $parameters['only-renderer']    = 1;
                     }
 
-                    $url   = $active ? '#' : '?' . http_build_query($parameters);
+                    $url   = $active ? '' : '?' . http_build_query($parameters);
                     $html .= '<li id="tracker_report_renderer_' . $r->id . '"
                                   class="' . $active . '
                                             tracker_report_renderer_tab

plugins/tracker/scripts/artifact/src/edition/TrackerArtifactEditionSwitcher.js
@@ -40,7 +40,7 @@ tuleap.tracker.artifact.editionSwitcher = function () {
         observeRequiredElements();
         initDoubleArtifactFormSubmitionGuard();
 
-        if ($("#artifact_informations").size() > 0) {
+        if ($("#artifact_informations").length > 0) {
             bindSubmissionBarToFollowups(
                 document,
                 CKEDITOR.instances.tracker_followup_comment_new,
@@ -64,7 +64,7 @@ tuleap.tracker.artifact.editionSwitcher = function () {
     };
 
     var toggleFieldsIfAlreadySubmittedArtifact = function () {
-        if ($(".submitted_artifact").size() > 0) {
+        if ($(".submitted_artifact").length > 0) {
             $(".tracker_artifact_field").each(function (index, element) {
                 if (fieldIsEditable(element)) {
                     toggleField(element);
@@ -95,9 +95,9 @@ tuleap.tracker.artifact.editionSwitcher = function () {
             }
 
             if (
-                field.find(".highlight").size() > 0 &&
-                field.find(".empty_value").size() > 0 &&
-                field.find(".has-reverse-links").size() === 0
+                field.find(".highlight").length > 0 &&
+                field.find(".empty_value").length > 0 &&
+                field.find(".has-reverse-links").length === 0
             ) {
                 toggleField(field);
             }
@@ -379,11 +379,11 @@ tuleap.tracker.artifact.editionSwitcher = function () {
     };
 
     var fieldIsEditable = function (element) {
-        return $(".tracker_hidden_edition_field", element).size() > 0;
+        return $(".tracker_hidden_edition_field", element).length > 0;
     };
 
     var fieldIsCreatable = function (element) {
-        return $(".add-field", element).size() > 0;
+        return $(".add-field", element).length > 0;
     };
 
     var toggleHiddenImageViewing = function () {

commit 8b090d4be01d89659b60918b32346d225814faba

fix: Cannot toggle tracker aggregation functions in the tracker view
Issue introduced by 6c81952847b4571fefff2eb9158ab18db5b05715.

Related to request #42980 Library jquery 1.9.1 is vulnerable

Change-Id: Idfe5f6088066edf0e1733f7238f805bd05a611f1

plugins/tracker/include/Tracker/Report/Tracker_Report_Renderer_Table.php
@@ -1567,7 +1567,7 @@ class Tracker_Report_Renderer_Table extends Tracker_Report_Renderer implements T
 
         $html  = '';
         $html .= '<div class="btn-group">';
-        $html .= '<a href="#"
+        $html .= '<a
             class="btn btn-mini dropdown-toggle"
             title="' . dgettext('tuleap-tracker', 'Toggle an aggregate function') . '"
             data-toggle="dropdown">';