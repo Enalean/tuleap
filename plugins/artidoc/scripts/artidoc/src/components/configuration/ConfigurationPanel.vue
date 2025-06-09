8906fe705ff6e2cfab9472d66252476f4e6a8d54
Joris Masson <joris.masson@enalean.com>
2025-06-09 16:08:13 +0200

    refactor: Remove config helper from ConfigurationPanel
    part of story #42248 Artidoc read-only fields: User Groups list fields
    
    No functional change expected. When you land on an artidoc without a
    Tracker selected, you can still choose one. When you choose a tracker
    without the submit permission on both Title and Description fields, you
    should see a warning appear.
    You can still choose a tracker and fields using the modal.
    
    Why?
    The configuration screen helper and its store introduce tight coupling
    between data and presentation-related information: success state, error
    state, even the icon for the button get stored alongside the choice of
    tracker and fields. Given that the configuration Modal now has to store
    data about fields, it makes life harder for us because the configuration
    Panel (the full-page one) knows nothing about fields, yet it depends on
    the same helper and store.
    Presentation-related information should live in components (that is
    their job after all). We can look for another way to store business
    data, but first we should untangle things by removing the helper and its
    store.
    This commit focuses on removing the config helper from
    ConfigurationPanel first, to produce a smaller step. Another one will
    remove it from the Modal.
    
    Change-Id: I7ff326f502a0e2c4e5df7b52281525cdf3494074

--- plugins/artidoc/scripts/artidoc/src/components/configuration/ConfigurationPanel.vue
+++ plugins/artidoc/scripts/artidoc/src/components/configuration/ConfigurationPanel.vue
@@ -29,11 +29,13 @@
 
             <section class="tlp-pane-section">
                 <tracker-selection-introductory-text
-                    v-bind:configuration_helper="configuration_helper"
+                    v-bind:selected_tracker="new_selected_tracker"
                 />
                 <tracker-selection
-                    v-bind:configuration_helper="configuration_helper"
+                    v-bind:allowed_trackers="allowed_trackers"
+                    v-bind:selected_tracker="new_selected_tracker"
                     v-bind:is_tracker_selection_disabled="false"
+                    v-on:select-tracker="onSelectTracker"
                 />
             </section>
 
@@ -63,29 +65,54 @@
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from "vue";
 import { strictInject } from "@tuleap/vue-strict-inject";
 import { useGettext } from "vue3-gettext";
+import { Option } from "@tuleap/option";
 import TrackerSelectionIntroductoryText from "@/components/configuration/TrackerSelectionIntroductoryText.vue";
 import ErrorFeedback from "@/components/configuration/ErrorFeedback.vue";
 import { TITLE } from "@/title-injection-key";
 import TrackerSelection from "@/components/configuration/TrackerSelection.vue";
-import { useConfigurationScreenHelper } from "@/composables/useConfigurationScreenHelper";
+import type { Tracker } from "@/stores/configuration-store";
 import { CONFIGURATION_STORE } from "@/stores/configuration-store";
 
-const { $gettext, interpolate } = useGettext();
+const { $gettext } = useGettext();
 
 const title = strictInject(TITLE);
+const {
+    allowed_trackers,
+    error_message,
+    is_error,
+    is_saving,
+    saveTrackerConfiguration,
+    selected_tracker,
+} = strictInject(CONFIGURATION_STORE);
 
-const pane_title = interpolate($gettext("Configuration of %{ title }"), { title });
+const pane_title = $gettext("Configuration of %{ title }", { title });
 
-const configuration_helper = useConfigurationScreenHelper(strictInject(CONFIGURATION_STORE));
+const new_selected_tracker = ref<Option<Tracker>>(Option.fromNullable(selected_tracker.value));
 
-const { is_submit_button_disabled, submit_button_icon, is_error, error_message } =
-    configuration_helper;
+const is_submit_button_disabled = computed(
+    () =>
+        allowed_trackers.length === 0 ||
+        is_saving.value ||
+        new_selected_tracker.value.mapOr(
+            (tracker) => tracker.id === selected_tracker.value?.id,
+            false,
+        ),
+);
+
+const submit_button_icon = computed(() =>
+    is_saving.value ? "fa-solid fa-spin fa-circle-notch" : "fa-solid fa-floppy-disk",
+);
 
 function onSubmit(event: Event): void {
     event.preventDefault();
-    configuration_helper.onSubmit();
+    new_selected_tracker.value.apply(saveTrackerConfiguration);
+}
+
+function onSelectTracker(tracker: Option<Tracker>): void {
+    new_selected_tracker.value = tracker;
 }
 </script>
 
--- plugins/artidoc/scripts/artidoc/src/components/configuration/TrackerSelectionIntroductoryText.vue
+++ plugins/artidoc/scripts/artidoc/src/components/configuration/TrackerSelectionIntroductoryText.vue
@@ -44,43 +44,43 @@
 </template>
 
 <script setup lang="ts">
-import { useGettext } from "vue3-gettext";
-import type { ConfigurationScreenHelper } from "@/composables/useConfigurationScreenHelper";
 import { computed } from "vue";
+import { useGettext } from "vue3-gettext";
+import type { Option } from "@tuleap/option";
+import type { Tracker } from "@/stores/configuration-store";
 
 const { $gettext } = useGettext();
 
-const props = defineProps<{ configuration_helper: ConfigurationScreenHelper }>();
+const props = defineProps<{
+    selected_tracker: Option<Tracker>;
+}>();
 
-const reason = computed(() => {
-    const tracker = props.configuration_helper.new_selected_tracker.value;
-    if (!tracker) {
-        return null;
-    }
+const reason = computed((): string =>
+    props.selected_tracker.mapOr((tracker): string => {
+        const has_title = Boolean(tracker.title);
+        const has_description = Boolean(tracker.description);
 
-    const has_title = Boolean(tracker.title);
-    const has_description = Boolean(tracker.description);
+        if (has_title && has_description) {
+            return "";
+        }
 
-    if (has_title && has_description) {
-        return null;
-    }
+        if (has_title && !has_description) {
+            return $gettext(
+                "You cannot create new sections because you don't have submit permission for the description field of the selected tracker.",
+            );
+        }
 
-    if (has_title && !has_description) {
+        if (!has_title && has_description) {
+            return $gettext(
+                "You cannot create new sections because you don't have submit permission for the title field of the selected tracker.",
+            );
+        }
+
         return $gettext(
-            "You cannot create new sections because you don't have submit permission for the description field of the selected tracker.",
+            "You cannot create new sections because you don't have submit permission for the title and description fields of the selected tracker.",
         );
-    }
+    }, ""),
+);
 
-    if (!has_title && has_description) {
-        return $gettext(
-            "You cannot create new sections because you don't have submit permission for the title field of the selected tracker.",
-        );
-    }
-
-    return $gettext(
-        "You cannot create new sections because you don't have submit permission for the title and description fields of the selected tracker.",
-    );
-});
-
-const should_display_warning = computed(() => Boolean(reason.value));
+const should_display_warning = computed(() => reason.value !== "");
 </script>
