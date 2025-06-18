Enalean/tuleap

commit 8e0e7a20fce52d6a859d1a414b3c36b440a99a09

fix: request #43357 Missing CSRF protection on tracker reports manipulation
Change-Id: I5a27ca014703ace5c3cc82242fed7ab50d79630c

plugins/tracker/include/Tracker/Report/Tracker_Report.php
@@ -562,6 +562,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
         $html_purifier = Codendi_HTMLPurifier::instance();
         $form_post_url = $html_purifier->purify('/plugins/tracker/?' . http_build_query($query_params));
         $html         .= '<form action="' . $form_post_url . '" method="POST" id="tracker_report_query_form" class="tracker-report-query-form">';
+        $html         .= $this->getCSRFTokenReportChange($this->getTracker())->fetchHTMLInput();
         $html         .= '<input type="hidden" name="report" value="' . $this->id . '" />';
         $id            = 'tracker_report_query_' . $this->id;
         $html         .= '<h4 class="backlog-planning-search-title ' . Toggler::getClassname($id, $this->is_query_displayed ? true : false) . '" id="' . $id . '">';
@@ -650,8 +651,9 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
     {
         $id            = 'tracker-report-expert-query-' . $this->id;
         $class_toggler = Toggler::getClassname($id, $this->is_query_displayed ? true : false);
+        $tracker       = $this->getTracker();
         $fields        = $this->getFormElementFactory()->getUsedFieldsForExpertModeUserCanRead(
-            $this->getTracker(),
+            $tracker,
             $this->getCurrentUser()
         );
 
@@ -665,7 +667,8 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
             $this->expert_query,
             $fields,
             $is_normal_mode_button_displayed,
-            $is_query_modifiable
+            $is_query_modifiable,
+            $this->getCSRFTokenReportChange($tracker),
         );
 
         $renderer = TemplateRendererFactory::build()->getRenderer(
@@ -702,7 +705,9 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
         $fields_for_criteria = [];
         $fields_for_sort     = [];
 
-        foreach ($this->getFormElementFactory()->getFields($this->getTracker()) as $field) {
+        $tracker = $this->getTracker();
+
+        foreach ($this->getFormElementFactory()->getFields($tracker) as $field) {
             if ($dropdown_type === self::TYPE_CRITERIA && ! $field->canBeUsedAsReportCriterion()) {
                 continue;
             }
@@ -720,6 +725,8 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
         $criteria_options          = [];
         $criteria_advanced_options = [];
 
+        $csrf_token = $this->getCSRFTokenReportChange($tracker);
+
         foreach ($fields_for_sort as $id => $nop) {
             $option     = new Templating_Presenter_ButtonDropdownsOption(
                 $id_prefix . '_' . $id,
@@ -730,6 +737,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
             $parameters = [
                 'data-field-id'      => $id,
                 'data-field-is-used' => intval(isset($used[$id])),
+                'data-challenge' => $csrf_token->getToken(),
             ];
             if ($dropdown_type !== self::TYPE_CRITERIA) {
                 $parameters['data-column-id'] = $id;
@@ -763,9 +771,11 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
 
     private function getOptionsForCustomColumn($id, $used)
     {
-        $project = $this->getTracker()->getProject();
-        $options = [];
-        $types   = $this->getTypePresenterFactory()->getAllTypesEditableInProject($project);
+        $tracker    = $this->getTracker();
+        $csrf_token = $this->getCSRFTokenReportChange($tracker);
+        $project    = $tracker->getProject();
+        $options    = [];
+        $types      = $this->getTypePresenterFactory()->getAllTypesEditableInProject($project);
 
         $column_id = $id . '_';
         $option    = new Templating_Presenter_ButtonDropdownsOption(
@@ -780,6 +790,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 'data-field-id'           => $id,
                 'data-field-is-used'      => intval(isset($used[$column_id])),
                 'data-field-artlink-type' => '',
+                'data-challenge'          => $csrf_token->getToken(),
             ]
         );
         $options[] = $option;
@@ -807,6 +818,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                     'data-field-id'           => $id,
                     'data-field-is-used'      => intval(isset($used[$column_id])),
                     'data-field-artlink-type' => $type->shortname,
+                    'data-challenge'          => $csrf_token->getToken(),
                 ]
             );
             $options[] = $option;
@@ -1335,9 +1347,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 dgettext('tuleap-tracker', 'The request is not valid.')
             );
 
-            $GLOBALS['Response']->redirect('?' . http_build_query([
-                'tracker'   => $tracker->getId(),
-            ]));
+            $this->redirectToTracker($tracker);
         }
 
         switch ($request->get('func')) {
@@ -1366,6 +1376,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'update-masschange-aids':
+                $this->checkReportUpdateRequest($request, $tracker);
                 $form_element_factory = $this->getFormElementFactory();
                 $artifact_factory     = Tracker_ArtifactFactory::instance();
 
@@ -1383,12 +1394,14 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 $masschange_updater->updateArtifacts($current_user, $request);
                 break;
             case 'remove-criteria':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($request->get('field') && ! $current_user->isAnonymous()) {
                     $this->report_session->removeCriterion($request->get('field'));
                     $this->report_session->setHasChanged();
                 }
                 break;
             case 'add-criteria':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($request->get('field') && ! $current_user->isAnonymous()) {
                     //TODO: make sure that the formElement exists and the user can read it
                     if ($request->isAjax()) {
@@ -1402,6 +1415,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'toggle-advanced':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($request->get('field') && ! $current_user->isAnonymous()) {
                     $this->toggleAdvancedCriterion($request->get('field'));
                     $this->report_session->setHasChanged();
@@ -1414,6 +1428,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case self::ACTION_CLEANSESSION:
+                $this->checkReportUpdateRequest($request, $tracker);
                 $this->report_session->clean();
                 $GLOBALS['Response']->redirect('?' . http_build_query([
                     'tracker'   => $this->tracker_id,
@@ -1429,6 +1444,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'rename-renderer':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($request->get('new_name') == '') {
                     $GLOBALS['Response']->addFeedback('error', dgettext('tuleap-tracker', 'Renderer name is mandatory.'));
                 } elseif (! $current_user->isAnonymous() && (int) $request->get('renderer') && trim($request->get('new_name'))) {
@@ -1440,6 +1456,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 ]));
                 break;
             case 'delete-renderer':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if (! $current_user->isAnonymous() && (int) $request->get('renderer')) {
                     $this->report_session->removeRenderer((int) $request->get('renderer'));
                     $this->report_session->setHasChanged();
@@ -1449,6 +1466,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'move-renderer':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if (! $current_user->isAnonymous() && (int) $request->get('renderer')) {
                     if ($request->isAjax()) {
                         $this->report_session->moveRenderer($request->get('tracker_report_renderers'));
@@ -1464,6 +1482,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'add-renderer':
+                $this->checkReportUpdateRequest($request, $tracker);
                 $new_name        = trim($request->get('new_name'));
                 $new_description = trim($request->get('new_description'));
                 $new_type        = trim($request->get('new_type'));
@@ -1476,6 +1495,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case self::ACTION_SAVE:
+                $this->checkReportUpdateRequest($request, $tracker);
                 Tracker_ReportFactory::instance()->save($this);
                 $this->saveCriteria();
                 $this->saveAdditionalCriteria();
@@ -1489,6 +1509,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 ]));
                 break;
             case self::ACTION_SAVEAS:
+                $this->checkReportUpdateRequest($request, $tracker);
                 $redirect_to_report_id = $this->id;
                 $report_copy_name      = trim($request->get('report_copy_name'));
                 if ($report_copy_name) {
@@ -1518,15 +1539,15 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 ]));
                 break;
             case self::ACTION_DELETE:
+                $this->checkReportUpdateRequest($request, $tracker);
                 $transaction = new DBTransactionExecutorWithConnection(DBFactory::getMainTuleapDBConnection());
                 $transaction->execute(function () {
                     $this->delete();
                 });
-                $GLOBALS['Response']->redirect('?' . http_build_query([
-                    'tracker'   => $this->tracker_id,
-                ]));
-                break;
+                $this->redirectToTracker($tracker);
+                // No break here, redirectToTracker never returns but PHPCS cannot see it
             case self::ACTION_SCOPE:
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($this->getTracker()->userIsAdmin($current_user) && (! $this->user_id || $this->user_id == $current_user->getId())) {
                     if ($request->exist('report_scope_public')) {
                         $is_scope_public = $request->get('report_scope_public');
@@ -1551,6 +1572,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case self::ACTION_DEFAULT:
+                $this->checkReportUpdateRequest($request, $tracker);
                 if ($this->getTracker()->userIsAdmin($current_user)) {
                     if ($request->exist('report_default')) {
                         if ($request->get('report_default')) {
@@ -1566,25 +1588,24 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 }
                 break;
             case 'store-expert-mode':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if (! $current_user->isAnonymous()) {
-                    if ($request->isPost()) {
-                        $this->report_session->storeExpertMode();
-                        $this->report_session->setHasChanged();
-                        $this->is_in_expert_mode = true;
-                    }
+                    $this->report_session->storeExpertMode();
+                    $this->report_session->setHasChanged();
+                    $this->is_in_expert_mode = true;
                 }
                 break;
             case 'store-normal-mode':
+                $this->checkReportUpdateRequest($request, $tracker);
                 if (! $current_user->isAnonymous()) {
-                    if ($request->isPost()) {
-                        $this->report_session->storeNormalMode();
-                        $this->report_session->setHasChanged();
-                        $this->is_in_expert_mode = false;
-                    }
+                    $this->report_session->storeNormalMode();
+                    $this->report_session->setHasChanged();
+                    $this->is_in_expert_mode = false;
                 }
                 break;
             default:
                 if ($request->exist('tracker_query_submit')) {
+                    $this->checkReportUpdateRequest($request, $tracker);
                     $criteria_values = $request->get('criteria');
                     if (! empty($criteria_values)) {
                         $this->updateCriteriaValues($criteria_values);
@@ -1598,6 +1619,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                     $this->report_session->setHasChanged();
                 }
                 if ($request->exist('tracker_expert_query_submit') && ! $current_user->isAnonymous()) {
+                    $this->checkReportUpdateRequest($request, $tracker);
                     $expert_query = $request->get('expert_query');
                     $this->report_session->storeExpertQuery($expert_query);
 
@@ -1633,6 +1655,29 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
                 $this->display($layout, $request, $current_user);
                 break;
         }
+    }
+
+    private function checkReportUpdateRequest(HTTPRequest $request, Tracker $tracker): void
+    {
+        if (! $request->isPost()) {
+            $GLOBALS['Response']->addFeedback(
+                Feedback::ERROR,
+                dgettext('tuleap-tracker', 'The request is not valid.')
+            );
+            $this->redirectToTracker($tracker);
+        }
+        $this->getCSRFTokenReportChange($tracker)->check();
+    }
+
+    private function getCSRFTokenReportChange(Tracker $tracker): CSRFSynchronizerToken
+    {
+        return new CSRFSynchronizerToken($tracker->getUri());
+    }
+
+    private function redirectToTracker(Tracker $tracker): never
+    {
+        $GLOBALS['Response']->redirect($tracker->getUri());
+        exit;
     }
 
     public function setDefaultReport(): void

plugins/tracker/include/Tracker/Masschange_Presenter.php
@@ -51,6 +51,7 @@ class Tracker_Masschange_Presenter
         $javascript_rules,
         array $external_actions,
         public readonly bool $has_notifications,
+        public readonly CSRFSynchronizerToken $csrf_token,
     ) {
         $this->project_id       = (int) $project->getID();
         $this->masschange_aids  = $masschange_aids;

plugins/tracker/include/Tracker/Report/ExpertModePresenter.php
@@ -53,6 +53,7 @@ class ExpertModePresenter
         array $allowed_fields,
         $is_normal_mode_button_displayed,
         $is_query_modifiable,
+        public readonly \CSRFSynchronizerToken $csrf_token,
     ) {
         $this->id                              = $id;
         $this->class_toggler                   = $class_toggler;

plugins/tracker/include/Tracker/Report/Tracker_Report_HeaderInReportPresenter.php
@@ -79,6 +79,8 @@ class Tracker_Report_HeaderInReportPresenter
      */
     public $warnings;
 
+    public readonly CSRFSynchronizerToken $csrf_token;
+
     public function __construct(
         string $browse_instructions,
         string $title,
@@ -103,9 +105,11 @@ class Tracker_Report_HeaderInReportPresenter
         $this->has_changed_classname = $has_changed_classname;
         $this->report_name           = $report->getName();
         $this->warnings              = $warnings;
-        $this->item_name             = $report->getTracker()->getItemName();
+        $tracker                     = $report->getTracker();
+        $this->item_name             = $tracker->getItemName();
         $this->is_user_anonymous     = $user->isAnonymous();
-        $this->artifact_creation_url = $report->getTracker()->getSubmitUrl();
+        $this->artifact_creation_url = $tracker->getSubmitUrl();
+        $this->csrf_token            = new CSRFSynchronizerToken($tracker->getUri());
     }
 
     public function has_browse_instructions(): bool

plugins/tracker/include/Tracker/Tracker.php
@@ -1662,6 +1662,7 @@ class Tracker implements Tracker_Dispatchable_Interface //phpcs:ignore PSR1.Clas
                 $this->displayRulesAsJavascript(),
                 $event->getExternalActions(),
                 ! $this->isNotificationStopped(),
+                new CSRFSynchronizerToken($this->getUri()),
             )
         );
 

plugins/tracker/scripts/legacy/src/ReorderColumns.js
@@ -88,6 +88,7 @@ codendi.ReorderColumns = Class.create({
                     var parameters = {
                             func: "renderer",
                             renderer: renderer_id,
+                            challenge: cell.closest("table").readAttribute("data-challenge"),
                         },
                         param_name =
                             "renderer_table[reorder-column][" +

plugins/tracker/scripts/legacy/src/TrackerReports.js
@@ -88,6 +88,7 @@ codendi.tracker.report.table.saveColumnsWidth = function (table, onComplete) {
     var parameters = {
         func: "renderer",
         renderer: $("tracker_report_renderer_current").readAttribute("data-renderer-id"),
+        challenge: table.readAttribute("data-challenge"),
     };
     var cells = table.rows[0].cells;
     var n = cells.length;
@@ -163,6 +164,7 @@ codendi.tracker.report.table.AddRemoveColumn = Class.create({
      * Add a column to the table
      */
     add: function (dropdown, li) {
+        const challenge = li.readAttribute("data-challenge");
         var column_id = li.readAttribute("data-column-id"),
             field_id = li.readAttribute("data-field-id"),
             artlink_type = li.readAttribute("data-field-artlink-type"),
@@ -188,6 +190,7 @@ codendi.tracker.report.table.AddRemoveColumn = Class.create({
                         "data-renderer-id",
                     ),
                     "renderer_table[add-column][field-id]": field_id,
+                    challenge,
                 };
             if (artlink_type !== null) {
                 parameters["renderer_table[add-column][artlink-type]"] = artlink_type;
@@ -247,6 +250,7 @@ codendi.tracker.report.table.AddRemoveColumn = Class.create({
      * remove a column to the table
      */
     remove: function (dropdown, li) {
+        const challenge = li.readAttribute("data-challenge");
         var column_id = li.readAttribute("data-column-id"),
             field_id = li.readAttribute("data-field-id");
         if ($("tracker_report_table_sort_by_" + field_id)) {
@@ -290,6 +294,7 @@ codendi.tracker.report.table.AddRemoveColumn = Class.create({
                             "data-renderer-id",
                         ),
                         "renderer_table[remove-column]": column_id,
+                        challenge,
                     },
                     onSuccess: function () {
                         $$("th[data-column-id=" + column_id + "]").each(function (col) {
@@ -336,6 +341,7 @@ codendi.tracker.report.AddRemoveCriteria = Class.create({
         if (!this.request_sent) {
             this.request_sent = true;
             var field_id = li.readAttribute("data-field-id");
+            const challenge = li.readAttribute("data-challenge");
             if ($("tracker_report_crit_" + field_id)) {
                 $$(".tracker_report_crit_" + field_id).invoke("show");
                 $("tracker_report_crit_" + field_id).show();
@@ -345,6 +351,7 @@ codendi.tracker.report.AddRemoveCriteria = Class.create({
                     parameters: {
                         func: "add-criteria",
                         field: field_id,
+                        challenge,
                     },
                     onComplete: function () {
                         self.request_sent = false;
@@ -355,6 +362,7 @@ codendi.tracker.report.AddRemoveCriteria = Class.create({
                     parameters: {
                         func: "add-criteria",
                         field: field_id,
+                        challenge,
                     },
                     onComplete: function () {
                         self.request_sent = false;
@@ -389,10 +397,12 @@ codendi.tracker.report.AddRemoveCriteria = Class.create({
      */
     remove: function (dropdown, li) {
         var field_id = li.readAttribute("data-field-id");
+        const challenge = li.readAttribute("data-challenge");
         new Ajax.Request(location.href, {
             parameters: {
                 func: "remove-criteria",
                 field: field_id,
+                challenge,
             },
             onSuccess: function () {
                 $$(".tracker_report_crit_" + field_id).invoke("hide");
@@ -422,11 +432,14 @@ codendi.tracker.report.loadAdvancedCriteria = function (element) {
                 .next()
                 .down("label")
                 .htmlFor.match(/_(\d+)$/)[1];
+            const form = li.closest("form");
+            const form_data = new FormData(form);
             //eslint-disable-next-line @typescript-eslint/no-unused-vars
             var req = new Ajax.Updater(li, location.href, {
                 parameters: {
                     func: "toggle-advanced",
                     field: field_id,
+                    challenge: form_data.get("challenge"),
                 },
                 onComplete: function (transport) {
                     //Force refresh of decorators and calendar

commit a40a53035dae6f61760a3cbaf27ea58e15ebb476

fix: Cannot add/modify a tracker report renderer
Issue introduced by 8e0e7a20fce52d6a859d1a414b3c36b440a99a09.

Part of request #43357 Missing CSRF protection on tracker reports manipulation

Change-Id: I5c23f121851f87b4ca1c5aff426d9e4db01cb288

plugins/tracker/include/Tracker/Report/Tracker_Report.php
@@ -1966,14 +1966,15 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
         );
     }
 
-    private function fetchUpdateRendererForm(Tracker_Report_Renderer $renderer)
+    private function fetchUpdateRendererForm(Tracker_Report_Renderer $renderer): string
     {
         $hp = Codendi_HTMLPurifier::instance();
 
         $update_renderer  = '';
         $update_renderer .= '<form action="" method="POST">';
-        $update_renderer .= '<input type="hidden" name="report" value="' . $this->id . '" />';
-        $update_renderer .= '<input type="hidden" name="renderer" value="' . (int) $renderer->id . '" />';
+        $update_renderer .= '<input type="hidden" name="report" value="' . $hp->purify($this->id) . '" />';
+        $update_renderer .= '<input type="hidden" name="renderer" value="' . $hp->purify((int) $renderer->id) . '" />';
+        $update_renderer .= $this->getCSRFTokenReportChange($renderer->getReport()->getTracker())->fetchHTMLInput();
         $update_renderer .= '
             <label class="radio">
                 <input type="radio" name="func" value="rename-renderer" id="tracker_renderer_updater_rename" />
@@ -2002,7 +2003,7 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
         return $update_renderer;
     }
 
-    private function fetchAddRendererForm($current_renderer)
+    private function fetchAddRendererForm($current_renderer): string
     {
         $hp = Codendi_HTMLPurifier::instance();
 
@@ -2010,19 +2011,20 @@ class Tracker_Report implements Tracker_Dispatchable_Interface // phpcs:ignore P
 
         $add_renderer  = '';
         $add_renderer .= '<form action="" method="POST">';
-        $add_renderer .= '<input type="hidden" name="report" value="' . $this->id . '" />';
-        $add_renderer .= '<input type="hidden" name="renderer" value="' . $current_renderer_id . '" />';
+        $add_renderer .= '<input type="hidden" name="report" value="' . $hp->purify($this->id) . '" />';
+        $add_renderer .= '<input type="hidden" name="renderer" value="' . $hp->purify($current_renderer_id) . '" />';
+        $add_renderer .= $this->getCSRFTokenReportChange($this->getTracker())->fetchHTMLInput();
         $add_renderer .= '<input type="hidden" name="func" value="add-renderer" />';
         $rrf           = Tracker_Report_RendererFactory::instance();
         $types         = $rrf->getTypes();
         if (count($types) > 1) { //No need to ask for type if there is only one
             $type = '<select name="new_type" id="tracker_renderer_add_type">';
             foreach ($types as $key => $label) {
-                $type .= '<option value="' . $key . '">' .  $hp->purify($label, CODENDI_PURIFIER_CONVERT_HTML)  . '</option>';
+                $type .= '<option value="' . $hp->purify($key) . '">' .  $hp->purify($label, CODENDI_PURIFIER_CONVERT_HTML)  . '</option>';
             }
             $type .= '</select>';
         } else {
-            $type = current($types);
+            $type = $hp->purify(current($types));
         }
         $add_renderer .= '<p><strong>' . dgettext('tuleap-tracker', 'Add a new') . ' ' . $type . '</strong></p>';
         $add_renderer .= '<p>';