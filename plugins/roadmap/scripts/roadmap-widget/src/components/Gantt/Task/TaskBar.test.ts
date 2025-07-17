10a6e52ffafc3345846a0744382c0655fac0d2cd
Clarisse Deschamps <clarisse.deschamps@enalean.com>
2025-07-17 09:17:10 +0200

    refactor: adapt TaskBar tests
    Part of request #41441 Migrate Roadmap widget to Vue 3
    
    The tests in TestBar checked the styles of the TaskBarProgress component. The problem is that in Vue 3 we no longer have access to the HTML of child components, as the current component is stubbed.
    In this patch I moved the tests to TaskBarProgress.test.ts so that they could check the required styles.
    
    No functional changes expected
    
    Change-Id: Ibe7feddc7079096203a1fbefa32a4740badd7a12

--- plugins/roadmap/scripts/roadmap-widget/src/components/Gantt/Task/TaskBar.test.ts
+++ plugins/roadmap/scripts/roadmap-widget/src/components/Gantt/Task/TaskBar.test.ts
@@ -17,176 +17,52 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { DateTime } from "luxon";
 import { mount } from "@vue/test-utils";
 import type { Wrapper } from "@vue/test-utils";
 import type { Task } from "../../../type";
 import TaskBarMilestone from "./TaskBarMilestone.vue";
+import TaskBarProgress from "./TaskBarProgress.vue";
 import TaskBar from "./TaskBar.vue";
 
 describe("TaskBar", () => {
-    let task: Task,
-        progress: number | null,
-        progress_error_message: string,
-        start: DateTime | null,
-        is_milestone: boolean,
-        width: number,
-        percentage: string | null,
-        is_text_displayed_inside_progress_bar: boolean,
-        is_text_displayed_outside_progress_bar: boolean,
-        is_text_displayed_outside_bar: boolean,
-        is_error_sign_displayed_outside_bar: boolean,
-        is_error_sign_displayed_inside_bar: boolean;
+    let is_milestone: boolean;
 
     beforeEach(() => {
-        task = {} as Task;
-        progress = null;
-        progress_error_message = "";
-        start = DateTime.fromObject({ year: 2020, month: 4, day: 15 });
         is_milestone = false;
-        width = 66;
-        percentage = "";
-        is_text_displayed_inside_progress_bar = false;
-        is_text_displayed_outside_progress_bar = false;
-        is_text_displayed_outside_bar = false;
-        is_error_sign_displayed_outside_bar = false;
-        is_error_sign_displayed_inside_bar = false;
     });
 
     function getWrapper(): Wrapper<Vue> {
-        task = {
-            color_name: "acid-green",
-            progress,
-            progress_error_message,
-            start,
-            end: DateTime.fromObject({ year: 2020, month: 4, day: 20 }),
-            is_milestone,
-        } as Task;
-
         return mount(TaskBar, {
             propsData: {
-                task,
+                task: {
+                    is_milestone,
+                    progress_error_message: "",
+                } as Task,
                 left: 42,
-                width,
-                percentage,
-                is_text_displayed_inside_progress_bar,
-                is_text_displayed_outside_progress_bar,
-                is_text_displayed_outside_bar,
-                is_error_sign_displayed_outside_bar,
-                is_error_sign_displayed_inside_bar,
+                width: 66,
+                percentage: "",
+                is_text_displayed_inside_progress_bar: false,
+                is_text_displayed_outside_progress_bar: false,
+                is_text_displayed_outside_bar: false,
+                is_error_sign_displayed_outside_bar: false,
+                is_error_sign_displayed_inside_bar: false,
                 popover_element_id: "10",
             },
         });
     }
 
-    it("should adapt the width of the progress bar", async () => {
-        progress = 0.4;
+    it("should display TaskBarMilestone", () => {
+        is_milestone = true;
         const wrapper = getWrapper();
 
-        const progress_bar = wrapper.find("[data-test=progress]");
-        expect((progress_bar.element as HTMLElement).style.width).toBe("40%");
-
-        await wrapper.setProps({ task: { ...task, progress: 0.7 } });
-        expect((progress_bar.element as HTMLElement).style.width).toBe("70%");
+        expect(wrapper.findComponent(TaskBarMilestone).exists()).toBe(true);
+        expect(wrapper.findComponent(TaskBarProgress).exists()).toBe(false);
     });
 
-    it("should display a full progress bar when the task does not have any progress", () => {
-        progress = null;
+    it("should display TaskBarProgress", () => {
+        const wrapper = getWrapper();
 
-        const progress_bar = getWrapper().find("[data-test=progress]");
-        expect(progress_bar.element.getAttribute("style")).toBeFalsy();
-    });
-
-    it("Displays a milestone", () => {
-        start = null;
-        is_milestone = true;
-        width = 21;
-
-        expect(getWrapper().findComponent(TaskBarMilestone).exists()).toBe(true);
-    });
-
-    describe("percentage", () => {
-        it("should be displayed next to the progress bar if there are enough room", () => {
-            progress = 0.42;
-            width = 100;
-            percentage = "42%";
-            is_text_displayed_outside_progress_bar = true;
-
-            expect(
-                getWrapper()
-                    .find("[data-test=container] > [data-test=bar] > [data-test=percentage]")
-                    .text(),
-            ).toBe("42%");
-        });
-
-        it("should be displayed inside the progress bar if there are not anymore enough room", () => {
-            progress = 0.98;
-            width = 100;
-            percentage = "98%";
-            is_text_displayed_inside_progress_bar = true;
-
-            expect(
-                getWrapper()
-                    .find(
-                        "[data-test=container] > [data-test=bar] > [data-test=progress] > [data-test=percentage]",
-                    )
-                    .text(),
-            ).toBe("98%");
-        });
-
-        it("should be displayed outside of the task bar if the latter is too small", () => {
-            progress = 0.5;
-            width = 10;
-            percentage = "50%";
-            is_text_displayed_outside_bar = true;
-
-            expect(getWrapper().find("[data-test=container] > [data-test=percentage]").text()).toBe(
-                "50%",
-            );
-        });
-    });
-
-    describe("progress in error", () => {
-        beforeEach(() => {
-            progress = null;
-            progress_error_message = "You fucked up!";
-            width = 10;
-            percentage = "50%";
-            is_text_displayed_outside_bar = true;
-        });
-
-        it("should display the error sign inside the bar", () => {
-            is_error_sign_displayed_inside_bar = true;
-            const wrapper = getWrapper();
-
-            expect(
-                wrapper
-                    .find(
-                        "[data-test=container] > [data-test=bar] > [data-test=progress-error-sign]",
-                    )
-                    .exists(),
-            ).toBe(true);
-            expect(
-                wrapper.find("[data-test=container] > [data-test=progress-error-sign]").exists(),
-            ).toBe(false);
-            expect(wrapper.find("[data-test=progress]").exists()).toBe(false);
-        });
-
-        it("should display the error sign outside the bar", () => {
-            is_error_sign_displayed_outside_bar = true;
-            const wrapper = getWrapper();
-
-            expect(
-                wrapper
-                    .find(
-                        "[data-test=container] > [data-test=bar] > [data-test=progress-error-sign]",
-                    )
-                    .exists(),
-            ).toBe(false);
-            expect(
-                wrapper.find("[data-test=container] > [data-test=progress-error-sign]").exists(),
-            ).toBe(true);
-            expect(wrapper.find("[data-test=progress]").exists()).toBe(false);
-        });
+        expect(wrapper.findComponent(TaskBarMilestone).exists()).toBe(false);
+        expect(wrapper.findComponent(TaskBarProgress).exists()).toBe(true);
     });
 });
