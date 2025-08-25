63263471c62046cc98ab20bf820c4456e3f9683a
Manuel VACELET <manuel.vacelet@enalean.com>
2025-08-25 13:30:19 +0200

    chore(👋 svn-core): Remove Backend factories
    Backend factories is cumbersome and not used, two good reasons to
    delete.
    
    Part of: request #29976 Remove SVN core
    
    Change-Id: If043e6d37002f0a6ae7435cc5adf3d751b2bf849

--- tests/unit/common/Backend/BackendTest.php
+++ tests/unit/common/Backend/BackendTest.php
@@ -27,8 +27,6 @@ declare(strict_types=1);
 namespace Tuleap\Backend;
 
 use Backend;
-use BackendSVN;
-use Event;
 use EventManager;
 use org\bovigo\vfs\vfsStream;
 
@@ -71,146 +69,6 @@ final class BackendTest extends \Tuleap\Test\PHPUnit\TestCase
         Backend::instance('plugin_fake');
     }
 
-    public function testFactoryOverride(): void
-    {
-        //Create a fake backend class which simulate an override of BackendSVN by a plugin
-        $backend_overridden_by_plugin = new class extends BackendSVN
-        {
-            public function __construct()
-            {
-            }
-        };
-        //Plugins can override default backends.
-        // For example, plugin_ldap can override the backend define in plugin_svn
-        EventManager::instance()->addListener(
-            Event::BACKEND_FACTORY_GET_SVN,
-            $this->buildPluginTestBackend($backend_overridden_by_plugin),
-            'getBackend',
-            false
-        );
-        self::assertInstanceOf($backend_overridden_by_plugin::class, Backend::instance(Backend::SVN));
-    }
-
-    public function testFactoryOverrideWithoutParameters(): void
-    {
-        $backend_overridden_by_plugin_and_has_setup = new class extends BackendSVN
-        {
-            public $a_variable_for_tests = -25;
-
-            public function __construct()
-            {
-            }
-
-            protected function setUp($a, $b, $c): void
-            {
-                $this->a_variable_for_tests = ($a + $b) * $c;
-            }
-        };
-        //Plugins can override default backends.
-        // For example, plugin_ldap can override the backend define in plugin_svn
-        EventManager::instance()->addListener(
-            Event::BACKEND_FACTORY_GET_SVN,
-            $this->buildPluginTestBackend($backend_overridden_by_plugin_and_has_setup),
-            'getBackend',
-            false
-        );
-        $b = Backend::instance(Backend::SVN);
-        self::assertEquals(-25, $b->a_variable_for_tests);
-    }
-
-    public function testFactoryOverrideWithParameters(): void
-    {
-        $backend_overridden_by_plugin_and_has_setup = new class extends BackendSVN
-        {
-            public $a_variable_for_tests = -25;
-
-            public function __construct()
-            {
-            }
-
-            protected function setUp($a, $b, $c): void
-            {
-                $this->a_variable_for_tests = ($a + $b) * $c;
-            }
-        };
-        //Plugins can override default backends.
-        // For example, plugin_ldap can override the backend define in plugin_svn
-        EventManager::instance()->addListener(
-            Event::BACKEND_FACTORY_GET_SVN,
-            $this->buildPluginTestBackend($backend_overridden_by_plugin_and_has_setup),
-            'getBackend',
-            false
-        );
-        $b = Backend::instance(Backend::SVN, null, [1, 2, 3]);
-        self::assertEquals(9, $b->a_variable_for_tests);
-    }
-
-    public function testFactoryOverrideWithParametersDefinedInPlugin(): void
-    {
-        $backend_overridden_by_plugin_and_has_setup = new class extends BackendSVN
-        {
-            public $a_variable_for_tests = -25;
-
-            public function __construct()
-            {
-            }
-
-            protected function setUp($a, $b, $c): void
-            {
-                $this->a_variable_for_tests = ($a + $b) * $c;
-            }
-        };
-        $backend_plugins_with_setup_and_params      = new class ($backend_overridden_by_plugin_and_has_setup)
-        {
-            /**
-             * @var BackendSVN
-             */
-            private $test_override_backend;
-
-            public function __construct(BackendSVN $test_override_backend)
-            {
-                $this->test_override_backend = $test_override_backend;
-            }
-
-            public function getBackend(array $params): void
-            {
-                $params['base']  = get_class($this->test_override_backend);
-                $params['setup'] = [1, 2, 3];
-            }
-        };
-        //Plugins can override default backends.
-        // For example, plugin_ldap can override the backend define in plugin_svn
-        EventManager::instance()->addListener(
-            Event::BACKEND_FACTORY_GET_SVN,
-            $backend_plugins_with_setup_and_params,
-            'getBackend',
-            false
-        );
-        $b = Backend::instance(Backend::SVN);
-        self::assertEquals(9, $b->a_variable_for_tests);
-    }
-
-    public function testFactoryOverrideWithParametersButNoSetUp(): void
-    {
-        $test_backend = new class extends BackendSVN
-        {
-            public function __construct()
-            {
-            }
-        };
-        //Plugins can override default backends.
-        // For example, plugin_ldap can override the backend define in plugin_svn
-        EventManager::instance()->addListener(
-            Event::BACKEND_FACTORY_GET_SVN,
-            $this->buildPluginTestBackend($test_backend),         //no setup !!!
-            'getBackend',
-            false
-        );
-        $this->expectException(\Exception::class);
-        $this->expectExceptionMessage('does not have setUp');
-        Backend::instance(Backend::SVN, null, [1, 2, 3]);
-    }
-
     public function testRecurseDeleteInDir(): void
     {
         $test_dir = vfsStream::setup()->url();
@@ -237,25 +95,5 @@ final class BackendTest extends \Tuleap\Test\PHPUnit\TestCase
         }
         closedir($d);
         rmdir($test_dir);
-    }
-
-    private function buildPluginTestBackend(BackendSVN $backend): object
-    {
-        return new class ($backend) {
-            /**
-             * @var BackendSVN
-             */
-            private $test_override_backend;
-
-            public function __construct(BackendSVN $test_override_backend)
-            {
-                $this->test_override_backend = $test_override_backend;
-            }
-
-            public function getBackend(array $params): void
-            {
-                $params['base'] = get_class($this->test_override_backend);
-            }
-        };
     }
 }
