85f1941ce2f1062fee2c61585c65f008cd0e0a84
Nicolas Terray <nicolas.terray@enalean.com>
2025-09-19 14:30:10 +0200

    feat: Initiate display of fake versions
    A fake list of versions is now displayed.
    
    * 100 versions are generated
    * some of them have a specific title
    * Date start from current date and goes back in the past
    * Users are taken from the current project members
    * If an error occured it is displayed
    * The under construction message is enhanced to enforce the presence of
      fake data.
    * The under construction message is dismissable in order to not have to
      fix height issue (when displayed there is a scroll issue). Since the
      intent is to remove this message when we release the feature, we should
      not have to worry about this issue now.
    
    Part of story #44469: Visualize list of versions
    
    Change-Id: I7dafea581972ac895e446a00eff1e11bdcd42f22

--- plugins/artidoc/scripts/artidoc/src/components/sidebar/versions/ListOfVersions.vue
+++ plugins/artidoc/scripts/artidoc/src/components/sidebar/versions/ListOfVersions.vue
@@ -19,19 +19,140 @@
   -->
 
 <template>
-    <div class="tlp-alert-info">
-        {{ $gettext("This feature of artidoc is under construction") }}
+    <div class="tlp-alert-info" v-if="should_display_under_construction_message">
+        <p>{{ $gettext("This feature of artidoc is under construction.") }}</p>
+        <p>{{ $gettext("Data is fake in order to gather feedback about the feature.") }}</p>
+        <button type="button" class="tlp-button-small tlp-button-primary" v-on:click="gotit()">
+            {{ $gettext("Ok, got it") }}
+        </button>
     </div>
+    <div class="tlp-alert-danger" v-if="error">
+        {{ error }}
+    </div>
+    <ul v-if="!error">
+        <li v-for="version in versions" v-bind:key="version.id">
+            <i class="fa-solid fa-circle disc" aria-hidden="true"></i>
+            <version-entry v-bind:version="version" />
+        </li>
+    </ul>
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
 import { useGettext } from "vue3-gettext";
+import type { Version } from "./fake-list-of-versions";
+import { getVersions } from "./fake-list-of-versions";
+import { strictInject } from "@tuleap/vue-strict-inject";
+import { PROJECT_ID } from "@/project-id-injection-key";
+import VersionEntry from "./VersionEntry.vue";
 
 const { $gettext } = useGettext();
+const versions = ref<ReadonlyArray<Version>>([]);
+const project_id = strictInject(PROJECT_ID);
+const error = ref("");
+const should_display_under_construction_message = ref(true);
+
+onMounted(() => {
+    getVersions(project_id).match(
+        (fetched_versions: ReadonlyArray<Version>) => {
+            versions.value = fetched_versions;
+        },
+        (fault) => {
+            error.value = $gettext("An error occurred while getting versions: %{ error }", {
+                error: String(fault),
+            });
+        },
+    );
+});
+
+function gotit(): void {
+    should_display_under_construction_message.value = false;
+}
 </script>
 
 <style scoped lang="scss">
+@use "@/themes/includes/viewport-breakpoint";
+
+$border-width: 5px;
+$timeline-width: 4px;
+$timeline-whitespace: var(--tlp-small-spacing);
+$disc-width: 12px;
+$timeline-offset-left: calc($timeline-whitespace + 0.5 * $disc-width - 0.5 * $timeline-width);
+
+.tlp-alert-danger,
 .tlp-alert-info {
-    border: 0;
+    margin: 0;
+    border-radius: 0;
+}
+
+ul {
+    --timeline-color: var(--tlp-main-color-lighter-80);
+
+    position: relative;
+    height: var(--artidoc-sidebar-content-height);
+    padding: 0 0 var(--tlp-medium-spacing);
+    overflow: hidden auto;
+    list-style-type: none;
+    color: var(--tlp-dimmed-color);
+
+    @media (max-width: viewport-breakpoint.$small-screen-size) {
+        height: fit-content;
+    }
+
+    &::after {
+        content: "specimen";
+        position: absolute;
+        top: 20vh;
+        transform: rotate(310deg);
+        opacity: 0.7;
+        color: var(--tlp-main-color-transparent-90);
+        font-size: 5rem;
+        font-weight: 600;
+    }
+}
+
+li {
+    display: flex;
+    position: relative;
+    align-items: baseline;
+    padding: calc(var(--tlp-small-spacing) / 2) var(--tlp-medium-spacing)
+        calc(var(--tlp-small-spacing) / 2) #{$timeline-whitespace};
+    border-left: #{$border-width} solid transparent;
+    gap: var(--tlp-small-spacing);
+
+    &:first-child {
+        border-left-color: var(--tlp-main-color);
+        background: var(--tlp-main-color-hover-background);
+    }
+
+    &:hover {
+        background: var(--tlp-main-color-hover-background);
+    }
+
+    &::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: #{$timeline-offset-left};
+        width: #{$timeline-width};
+        height: 100%;
+        background: var(--timeline-color);
+    }
+
+    &:first-child::before {
+        top: var(--tlp-medium-spacing);
+        height: calc(100% - var(--tlp-medium-spacing));
+    }
+
+    &:last-child::before {
+        height: var(--tlp-medium-spacing);
+    }
+}
+
+.disc {
+    // Make sure that the disc is above the timeline line
+    z-index: 1;
+    color: var(--timeline-color);
+    font-size: #{$disc-width};
 }
 </style>
---
+++ plugins/artidoc/scripts/artidoc/src/components/sidebar/versions/VersionEntry.vue
@@ -0,0 +1,88 @@
+<!--
+  - Copyright (c) Enalean, 2025 - present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+    <div>
+        {{ title }}
+        <div class="metadata">
+            <tlp-relative-date
+                v-bind:date="version.created_on.toISOString()"
+                v-bind:absolute-date="formatted_date"
+                v-bind:placement="relative_date_placement"
+                v-bind:preference="relative_date_preference"
+                v-bind:locale="user_preferences.locale"
+            >
+                {{ formatted_date }}
+            </tlp-relative-date>
+            {{ $gettext("by:") }}
+            <span>
+                <span class="tlp-avatar-mini"
+                    ><img
+                        loading="lazy"
+                        v-bind:src="version.created_by.avatar_url"
+                        data-test="user-list-item-avatar"
+                        v-bind:alt="$gettext('User avatar')"
+                /></span>
+                {{ version.created_by.display_name }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useGettext } from "vue3-gettext";
+import type { Version } from "./fake-list-of-versions";
+import type { UserPreferences } from "@/user-preferences-injection-key";
+import { USER_PREFERENCES } from "@/user-preferences-injection-key";
+import { IntlFormatter } from "@tuleap/date-helper";
+import { relativeDatePlacement, relativeDatePreference } from "@tuleap/tlp-relative-date";
+import { strictInject } from "@tuleap/vue-strict-inject";
+
+const props = defineProps<{ version: Version }>();
+
+const { $gettext } = useGettext();
+
+const user_preferences = strictInject<UserPreferences>(USER_PREFERENCES);
+
+const formatter = IntlFormatter(
+    user_preferences.locale,
+    user_preferences.timezone,
+    "date-with-time",
+);
+
+const relative_date_preference = relativeDatePreference(user_preferences.relative_date_display);
+const relative_date_placement = relativeDatePlacement(
+    user_preferences.relative_date_display,
+    "right",
+);
+
+const formatted_date = computed((): string =>
+    formatter.format(props.version.created_on.toISOString()),
+);
+
+const title = computed(() => props.version.title ?? formatted_date.value);
+</script>
+
+<style scoped lang="scss">
+.metadata {
+    font-size: 0.75rem;
+}
+</style>
