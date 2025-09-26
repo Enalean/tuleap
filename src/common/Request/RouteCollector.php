Enalean/tuleap

commit 8e4a87a9405be3f9c0fc5ea92ddcd12e44ec0c29

fix: request #44834  Avatars can be retrieved via cross-origin API calls
The avatar controller has been refactored in order to support the REST
authentication and existing CORS middleware.

Change-Id: I5bae1244191173f9f99411a09ce1411d66903204

src/common/Request/RouteCollector.php
@@ -720,28 +720,23 @@ class RouteCollector
         );
     }
 
-    public static function getUsersNameAvatar()
+    public static function getAvatarController(): AvatarController
     {
         $storage             = new AvatarHashDao();
         $compute_avatar_hash = new ComputeAvatarHash();
+        $response_factory    = HTTPFactoryBuilder::responseFactory();
 
         return new AvatarController(
+            new SapiEmitter(),
+            new BinaryFileResponseBuilder($response_factory, HTTPFactoryBuilder::streamFactory()),
+            $response_factory,
+            new RESTCurrentUserMiddleware(\Tuleap\REST\UserManager::build(), new BasicAuthentication()),
+            \UserManager::instance(),
             new AvatarGenerator($storage, $compute_avatar_hash),
             $storage,
             $compute_avatar_hash,
-        );
-    }
-
-    public static function getUsersNameAvatarHash()
-    {
-        $storage             = new AvatarHashDao();
-        $compute_avatar_hash = new ComputeAvatarHash();
-
-        return new AvatarController(
-            new AvatarGenerator($storage, $compute_avatar_hash),
-            $storage,
-            $compute_avatar_hash,
-            ['expires' => 'never'],
+            new SessionWriteCloseMiddleware(),
+            new TuleapRESTCORSMiddleware(),
         );
     }
 
@@ -1672,8 +1667,8 @@ class RouteCollector
 
         $r->addGroup('/users', function (FastRoute\RouteCollector $r) {
             $r->get('/{name}[/]', [self::class, 'getUsersName']);
-            $r->get('/{name}/avatar.png', [self::class, 'getUsersNameAvatar']);
-            $r->get('/{name}/avatar-{hash}.png', [self::class, 'getUsersNameAvatarHash']);
+            $r->get('/{name}/avatar.png', [self::class, 'getAvatarController']);
+            $r->get('/{name}/avatar-{hash}.png', [self::class, 'getAvatarController']);
         });
 
         $r->post('/join-private-project-mail/', [self::class, 'postJoinPrivateProjectMail']);

src/common/User/Profile/AvatarController.php
@@ -21,108 +21,112 @@
 namespace Tuleap\User\Profile;
 
 use ForgeConfig;
-use HTTPRequest;
-use Tuleap\Layout\BaseLayout;
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Tuleap\Http\Response\BinaryFileResponseBuilder;
 use Tuleap\Option\Option;
-use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\DispatchablePSR15Compatible;
 use Tuleap\Request\DispatchableWithRequestNoAuthz;
+use Tuleap\ServerHostname;
 use Tuleap\User\Avatar\AvatarHashStorage;
 use Tuleap\User\Avatar\ComputeAvatarHash;
+use Tuleap\User\ProvideCurrentRequestUser;
 use UserManager;
 
-class AvatarController implements DispatchableWithRequest, DispatchableWithRequestNoAuthz
+final class AvatarController extends DispatchablePSR15Compatible implements DispatchableWithRequestNoAuthz
 {
-    public const DEFAULT_AVATAR = __DIR__ . '/../../../www/themes/common/images/avatar_default.png';
-
-    private $never_expires = false;
-
-    public const ONE_YEAR_IN_SECONDS = 3600 * 24 * 365;
-    /**
-     * @var AvatarGenerator
-     */
-    private $avatar_generator;
+    private const string DEFAULT_AVATAR   = __DIR__ . '/../../../www/themes/common/images/avatar_default.png';
+    private const int ONE_YEAR_IN_SECONDS = 3600 * 24 * 365;
 
     public function __construct(
-        AvatarGenerator $avatar_generator,
-        private AvatarHashStorage $avatar_hash_storage,
-        private ComputeAvatarHash $compute_avatar_hash,
-        array $options = [],
+        EmitterInterface $emitter,
+        private readonly BinaryFileResponseBuilder $binary_file_response_builder,
+        private readonly ResponseFactoryInterface $response_factory,
+        private readonly ProvideCurrentRequestUser $current_request_user_provider,
+        private readonly UserManager $user_manager,
+        private readonly AvatarGenerator $avatar_generator,
+        private readonly AvatarHashStorage $avatar_hash_storage,
+        private readonly ComputeAvatarHash $compute_avatar_hash,
+        MiddlewareInterface ...$middleware_stack,
     ) {
-        if (isset($options['expires']) && $options['expires'] === 'never') {
-            $this->never_expires = true;
-        }
-        $this->avatar_generator = $avatar_generator;
+        parent::__construct($emitter, ...$middleware_stack);
     }
 
     #[\Override]
-    public function process(HTTPRequest $request, BaseLayout $layout, array $variables)
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $current_user = $this->current_request_user_provider->getCurrentRequestUser($request);
         // Avatar is a public information for all authenticated users
-        if (! ForgeConfig::areAnonymousAllowed() && $request->getCurrentUser()->isAnonymous()) {
-            $this->displayDefaultAvatarAsError();
-        }
-        \session_write_close();
-
-        $user_manager = UserManager::instance();
-        $user         = $user_manager->getUserByUserName($variables['name']);
-        if ($user === null) {
-            $this->displayDefaultAvatarAsError();
+        if (! ForgeConfig::areAnonymousAllowed() && ($current_user === null || $current_user->isAnonymous())) {
+            return $this->getDefaultAvatarErrorResponse($request);
         }
 
-        if ($user->hasAvatar()) {
-            $user_avatar_path = $user->getAvatarFilePath();
-            if (! is_file($user_avatar_path)) {
-                $this->avatar_generator->generate($user, $user_avatar_path);
-                $user->setHasCustomAvatar(false);
-                $user_manager->updateDb($user);
-            }
+        $user_name = (string) $request->getAttribute('name');
+        $user      = $this->user_manager->getUserByUserName($user_name);
 
-            if (is_file($user_avatar_path)) {
-                if (isset($variables['hash'])) {
-                    $this->redirectIfStalled($layout, $user, $variables['hash'], $variables['name']);
-                }
-                $this->displayAvatar($user_avatar_path);
-                return;
-            }
+        if ($user === null || ! $user->hasAvatar()) {
+            return $this->getDefaultAvatarErrorResponse($request);
         }
 
-        $this->displayAvatar(self::DEFAULT_AVATAR);
-    }
+        $user_avatar_path = $user->getAvatarFilePath();
+        if (! is_file($user_avatar_path)) {
+            $this->avatar_generator->generate($user, $user_avatar_path);
+            $user->setHasCustomAvatar(false);
+            $this->user_manager->updateDb($user);
+        }
 
-    private function redirectIfStalled(BaseLayout $layout, \PFUser $user, $hash, $user_name)
-    {
-        $this->avatar_hash_storage
+        $hash = (string) $request->getAttribute('hash');
+
+        return $this->avatar_hash_storage
             ->retrieve($user)
-            ->orElse(function () use ($user) {
-                return Option::fromValue(
-                    $this->compute_avatar_hash->computeAvatarHash($user->getAvatarFilePath())
-                );
-            })->andThen(function (string $current_hash) use ($layout, $hash, $user_name) {
-                if ($current_hash !== $hash) {
-                    $layout->permanentRedirect('/users/' . $user_name . '/avatar-' . $current_hash . '.png');
-                }
+            ->orElse(
+                /** @return Option<string> */
+                fn(): Option => Option::fromValue($this->compute_avatar_hash->computeAvatarHash($user_avatar_path))
+            )->mapOr(
+                function (string $current_hash) use ($request, $hash, $user): ResponseInterface {
+                    if ($current_hash === $hash || $hash === '') {
+                        return $this->getUserAvatarResponse($request, $user, $hash);
+                    }
 
-                return Option::fromValue(null);
-            });
+                    return $this->response_factory
+                        ->createResponse(301)
+                        ->withHeader(
+                            'Location',
+                            ServerHostname::HTTPSUrl() . '/users/' . urlencode($user->getUserName()) . '/avatar-' . urlencode($current_hash) . '.png'
+                        );
+                },
+                $this->getDefaultAvatarErrorResponse($request)
+            );
     }
 
-    private function displayAvatar($path)
+    private function getDefaultAvatarErrorResponse(ServerRequestInterface $request): ResponseInterface
     {
-        header('Content-Type: image/png');
-        if ($this->never_expires) {
-            header('Cache-Control: max-age=' . self::ONE_YEAR_IN_SECONDS . ',immutable');
-        } else {
-            header('Cache-Control: max-age=60');
+        return $this->binary_file_response_builder->fromFilePath(
+            $request,
+            self::DEFAULT_AVATAR,
+            'default-avatar.png',
+            'image/png'
+        )
+            ->withStatus(404)
+            ->withHeader('Cache-Control', 'max-age=60');
+    }
+
+    private function getUserAvatarResponse(ServerRequestInterface $request, \PFUser $user, string $hash): ResponseInterface
+    {
+        $response = $this->binary_file_response_builder->fromFilePath(
+            $request,
+            $user->getAvatarFilePath(),
+            sprintf('avatar-%s-%s.png', $user->getUserName(), $hash),
+            'image/png'
+        );
+
+        if ($hash === '') {
+            return $response->withHeader('Cache-Control', 'max-age=60');
         }
-        readfile($path);
-    }
 
-    private function displayDefaultAvatarAsError(): void
-    {
-        http_response_code(404);
-        header('Content-Type: image/png');
-        header('Cache-Control: max-age=60');
-        readfile(self::DEFAULT_AVATAR);
-        exit;
+        return $response->withHeader('Cache-Control', 'max-age=' . self::ONE_YEAR_IN_SECONDS . ',immutable');
     }
 }

src/etc/nginx/tuleap.d/03-locations.conf
@@ -40,8 +40,8 @@ location /images/ {
 }
 
 location ~ "^/users/[a-zA-Z0-9_\-\.]+/avatar\-[0-9a-f]+\.png$" {
-    add_header Cross-Origin-Embedder-Policy "require-corp";
-    add_header Cross-Origin-Resource-Policy "same-origin";
+    add_header Cross-Origin-Embedder-Policy "require-corp" always;
+    add_header Cross-Origin-Resource-Policy "cross-origin" always;
     include conf.d/tuleap.d/09-content-security-policy.conf;
     include conf.d/tuleap.d/10-cross-origin-opener-policy.conf;
 

src/etc/nginx/tuleap.d/12-fetch-metadata-resource-isolation.conf
@@ -15,6 +15,11 @@ if ($request_uri ~ ^/uploads/) {
     set $reject_request_for_resource_isolation 0;
 }
 
+# Exclude calls to avatars from the global resource isolation, they can be requested by API calls
+if ($request_uri ~ (^/users/[a-zA-Z0-9_\-\.]+/avatar\-[0-9a-f]+\.png$)) {
+    set $reject_request_for_resource_isolation 0;
+}
+
 # Allow requests from browsers/tools that does not send Fetch Metadata headers
 if ($http_sec_fetch_site = "") {
     set $reject_request_for_resource_isolation 0;

tests/unit/common/User/Profile/AvatarControllerTest.php
@@ -1,0 +1,167 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2025-Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\User\Profile;
+
+use ForgeAccess;
+use ForgeConfig;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+use Tuleap\ForgeConfigSandbox;
+use Tuleap\Http\HTTPFactoryBuilder;
+use Tuleap\Http\Response\BinaryFileResponseBuilder;
+use Tuleap\Http\Server\NullServerRequest;
+use Tuleap\Test\Builders\UserTestBuilder;
+use Tuleap\Test\Helpers\NoopSapiEmitter;
+use Tuleap\Test\Stubs\CurrentRequestUserProviderStub;
+use Tuleap\Test\Stubs\User\Avatar\AvatarHashStorageStub;
+use Tuleap\User\Avatar\ComputeAvatarHash;
+
+#[\PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles]
+final class AvatarControllerTest extends TestCase
+{
+    use ForgeConfigSandbox;
+
+    private \UserManager&\PHPUnit\Framework\MockObject\Stub $user_manager;
+    private AvatarController $avatar_controller;
+
+    #[\Override]
+    protected function setUp(): void
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::ANONYMOUS);
+        ForgeConfig::set('sys_avatar_path', vfsStream::setup()->url());
+
+        $storage                 = AvatarHashStorageStub::withStoredHash('expected_hash');
+        $compute_avatar_hash     = new ComputeAvatarHash();
+        $response_factory        = HTTPFactoryBuilder::responseFactory();
+        $this->user_manager      = $this->createStub(\UserManager::class);
+        $this->avatar_controller = new AvatarController(
+            new NoopSapiEmitter(),
+            new BinaryFileResponseBuilder($response_factory, HTTPFactoryBuilder::streamFactory()),
+            $response_factory,
+            new CurrentRequestUserProviderStub(UserTestBuilder::anAnonymousUser()->build()),
+            $this->user_manager,
+            new AvatarGenerator($storage, $compute_avatar_hash),
+            $storage,
+            $compute_avatar_hash
+        );
+    }
+
+    public function testRetrievesUserAvatarUsingANonPermanentURL(): void
+    {
+        $user = UserTestBuilder::anActiveUser()->withUserName('user1')->withRealName('User 1')->build();
+
+        $request = (new NullServerRequest())->withAttribute('name', $user->getUserName());
+
+        $this->user_manager->method('getUserByUserName')->with($user->getUserName())
+            ->willReturn($user);
+        $this->user_manager->method('updateDb');
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame('image/png', $response->getHeaderLine('Content-Type'));
+        self::assertSame('max-age=60', $response->getHeaderLine('Cache-Control'));
+        self::assertStringContainsString("PNG\r\n", $response->getBody()->read(8));
+    }
+
+    public function testRetrievesUserAvatarUsingAPermanentURL(): void
+    {
+        $user = UserTestBuilder::anActiveUser()->withUserName('user1')->withRealName('User 1')->build();
+
+        $request = (new NullServerRequest())
+            ->withAttribute('name', $user->getUserName())
+            ->withAttribute('hash', 'expected_hash');
+
+        $this->user_manager->method('getUserByUserName')->with($user->getUserName())->willReturn($user);
+        $this->user_manager->method('updateDb');
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame('image/png', $response->getHeaderLine('Content-Type'));
+        self::assertSame('max-age=31536000,immutable', $response->getHeaderLine('Cache-Control'));
+        self::assertStringContainsString("PNG\r\n", $response->getBody()->read(8));
+    }
+
+    public function testRedirectsWhenTheUserAvatarHashIsSomethingElseThanTheProvidedValue(): void
+    {
+        $user = UserTestBuilder::anActiveUser()->withUserName('user1')->withRealName('User 1')->build();
+
+        $request = (new NullServerRequest())
+            ->withAttribute('name', $user->getUserName())
+            ->withAttribute('hash', 'wrong_hash');
+
+        $this->user_manager->method('getUserByUserName')->with($user->getUserName())->willReturn($user);
+        $this->user_manager->method('updateDb');
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(301, $response->getStatusCode());
+        self::assertStringEndsWith('/users/user1/avatar-expected_hash.png', $response->getHeaderLine('Location'));
+        self::assertEmpty($response->getHeaderLine('Cache-Control'));
+    }
+
+    public function testReturnsA404WithADefaultAvatarWhenTheUserCannotBeFound(): void
+    {
+        $request = (new NullServerRequest())->withAttribute('name', 'does_not_exist');
+
+        $this->user_manager->method('getUserByUserName')->willReturn(null);
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(404, $response->getStatusCode());
+        self::assertSame('image/png', $response->getHeaderLine('Content-Type'));
+        self::assertSame('max-age=60', $response->getHeaderLine('Cache-Control'));
+        self::assertStringContainsString("PNG\r\n", $response->getBody()->read(8));
+    }
+
+    public function testReturnsA404WhenTheUserCannotHaveADefaultAvatar(): void
+    {
+        $user = UserTestBuilder::anActiveUser()->withUserName('not_avatar_possible')->withRealName('')->build();
+
+        $request = (new NullServerRequest())->withAttribute('name', $user->getUserName());
+
+        $this->user_manager->method('getUserByUserName')->willReturn($user);
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(404, $response->getStatusCode());
+        self::assertSame('image/png', $response->getHeaderLine('Content-Type'));
+        self::assertSame('max-age=60', $response->getHeaderLine('Cache-Control'));
+        self::assertStringContainsString("PNG\r\n", $response->getBody()->read(8));
+    }
+
+    public function testReturnsA404ForAnonymousUserWhenTheInstanceDoNotAllowThem(): void
+    {
+        ForgeConfig::set(ForgeAccess::CONFIG, ForgeAccess::REGULAR);
+
+        $request = (new NullServerRequest())->withAttribute('name', 'username');
+
+        $response = $this->avatar_controller->handle($request);
+
+        self::assertSame(404, $response->getStatusCode());
+        self::assertSame('image/png', $response->getHeaderLine('Content-Type'));
+        self::assertSame('max-age=60', $response->getHeaderLine('Cache-Control'));
+        self::assertStringContainsString("PNG\r\n", $response->getBody()->read(8));
+    }
+}

commit 28a39da7c0444028a66d4b48f4d1603b4b25ea19

fix: Avatars are not displayed on instances not allowing anonymous
Issue introduced by 8e4a87a9405be3f9c0fc5ea92ddcd12e44ec0c29.

Part of request #44834  Avatars can be retrieved via cross-origin API calls

Change-Id: I1f74eb34fbdb93dc6abf9edf8c888e93b016473d

src/common/Request/RouteCollector.php
@@ -726,17 +726,20 @@ class RouteCollector
         $compute_avatar_hash = new ComputeAvatarHash();
         $response_factory    = HTTPFactoryBuilder::responseFactory();
 
+        $current_user_middleware = new RESTCurrentUserMiddleware(\Tuleap\REST\UserManager::build(), new BasicAuthentication());
+
         return new AvatarController(
             new SapiEmitter(),
             new BinaryFileResponseBuilder($response_factory, HTTPFactoryBuilder::streamFactory()),
             $response_factory,
-            new RESTCurrentUserMiddleware(\Tuleap\REST\UserManager::build(), new BasicAuthentication()),
+            $current_user_middleware,
             \UserManager::instance(),
             new AvatarGenerator($storage, $compute_avatar_hash),
             $storage,
             $compute_avatar_hash,
             new SessionWriteCloseMiddleware(),
             new TuleapRESTCORSMiddleware(),
+            $current_user_middleware,
         );
     }
 