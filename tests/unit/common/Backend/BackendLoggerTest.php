Enalean/tuleap

commit 3df6a7663f31a94aa2beb6f95ec73bd822f33bdb

feat: check the license signature
Part of story #44839 Enforce expiration date

Parse and check signature license with available public key.
If the license signature is invalid it should build an expired TEE
license.

Co-authored-by: Clarisse Deschamps <clarisse.deschamps@enalean.com>
Co-authored-by: Kevin Traini <kevin.traini@enalean.com>
Change-Id: I355ec81eb136c0ec6f983f0d517e7e351b19dbec

src/common/SeatManagement/CachedLicenseBuilder.php
@@ -22,7 +22,13 @@ declare(strict_types=1);
 
 namespace Tuleap\SeatManagement;
 
+use BackendLogger;
+use Lcobucci\JWT\Encoding\JoseEncoder;
+use Lcobucci\JWT\Token\Parser;
+use Lcobucci\JWT\Validation\Validator;
 use Override;
+use Ramsey\Uuid\Uuid;
+use Tuleap\Mapper\ValinorMapperBuilderFactory;
 
 final class CachedLicenseBuilder implements BuildLicense
 {
@@ -36,7 +42,15 @@ final class CachedLicenseBuilder implements BuildLicense
     public static function instance(): self
     {
         if (self::$instance === null) {
-            self::$instance = new self(new LicenseBuilder(new PublicKeyPresenceChecker()));
+            self::$instance = new self(new LicenseBuilder(
+                new PublicKeyPresenceChecker(),
+                new LicenseSignatureChecker(
+                    BackendLogger::getDefaultLogger(),
+                    new Parser(new JoseEncoder()),
+                    new Validator(),
+                    ValinorMapperBuilderFactory::mapperBuilder()->registerConstructor(Uuid::fromString(...))->mapper(),
+                ),
+            ));
         }
 
         return self::$instance;

src/common/Mapper/Base64UrlSafeDecode.php
@@ -20,26 +20,24 @@
 
 declare(strict_types=1);
 
-namespace Tuleap\SeatManagement;
+namespace Tuleap\Mapper;
 
-use PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles;
-use Tuleap\Test\PHPUnit\TestCase;
-use Tuleap\Test\Stubs\SeatManagement\BuildLicenseStub;
+use Attribute;
+use CuyZ\Valinor\Mapper\AsConverter;
+use Psl\Encoding\Base64\Variant;
+use function Psl\Encoding\Base64\decode as base64_decode;
 
-#[DisableReturnValueGenerationForTestDoubles]
-final class CachedLicenseBuilderTest extends TestCase
+#[AsConverter]
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final class Base64UrlSafeDecode
 {
-    public function testItUsesCache(): void
+    /**
+     * @param callable(mixed): mixed $next
+     */
+    public function map(string $value, callable $next): mixed
     {
-        $license         = License::buildEnterpriseEdition(null);
-        $license_builder = BuildLicenseStub::buildWithLicense($license);
+        $decoded = base64_decode($value, Variant::UrlSafe, false);
 
-        $cache = new CachedLicenseBuilder($license_builder);
-
-        $cache->build();
-        $cache->build();
-
-        self::assertSame(1, $license_builder->getCallCount());
-        self::assertSame($license, $cache->build());
+        return $next($decoded);
     }
 }

src/common/SeatManagement/CheckLicenseSignature.php
@@ -1,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\SeatManagement;
+
+interface CheckLicenseSignature
+{
+    /**
+     * @param non-empty-string $license_file_path
+     * @param non-empty-string $keys_directory
+     */
+    public function checkLicenseSignature(string $license_file_path, string $keys_directory): bool;
+}

src/common/SeatManagement/License.php
@@ -22,23 +22,36 @@ declare(strict_types=1);
 
 namespace Tuleap\SeatManagement;
 
+use DateTimeImmutable;
+use Tuleap\Option\Option;
+
 /**
  * @psalm-immutable
  */
 final readonly class License
 {
+    /**
+     * @param Option<DateTimeImmutable> $expiration_date
+     */
     private function __construct(
         public bool $is_enterprise_edition,
+        public Option $expiration_date,
+        public bool $has_valid_signature,
     ) {
     }
 
-    public static function buildEnterpriseEdition(): self
+    public static function buildEnterpriseEdition(?DateTimeImmutable $expiration_date): self
     {
-        return new self(true);
+        return new self(true, Option::fromNullable($expiration_date), true);
+    }
+
+    public static function buildInvalidEnterpriseEdition(): self
+    {
+        return new self(true, Option::nothing(DateTimeImmutable::class), false);
     }
 
     public static function buildCommunityEdition(): self
     {
-        return new self(false);
+        return new self(false, Option::nothing(DateTimeImmutable::class), true);
     }
 }

src/common/SeatManagement/LicenseBuilder.php
@@ -23,25 +23,36 @@ declare(strict_types=1);
 namespace Tuleap\SeatManagement;
 
 use Override;
+use function Psl\Filesystem\is_file;
 
 final readonly class LicenseBuilder implements BuildLicense
 {
     private const string PUBLIC_KEY_DIRECTORY = __DIR__ . '/keys';
 
     public function __construct(
-        private CheckPublicKeyPresence $public_key_retriever,
+        private CheckPublicKeyPresence $public_key_presence_checker,
+        private CheckLicenseSignature $license_signature_checker,
     ) {
     }
 
     #[Override]
     public function build(): License
     {
-        $is_public_key_present = $this->public_key_retriever->checkPresence(self::PUBLIC_KEY_DIRECTORY);
+        $is_public_key_present = $this->public_key_presence_checker->checkPresence(self::PUBLIC_KEY_DIRECTORY);
 
         if (! $is_public_key_present) {
             return License::buildCommunityEdition();
         }
 
-        return License::buildEnterpriseEdition();
+        $licence_key_path = ((string) \ForgeConfig::get('sys_custom_dir')) . '/conf/license.key';
+        if (! is_file($licence_key_path)) {
+            return License::buildEnterpriseEdition(null);
+        }
+
+        if (! $this->license_signature_checker->checkLicenseSignature($licence_key_path, self::PUBLIC_KEY_DIRECTORY)) {
+            return License::buildInvalidEnterpriseEdition();
+        }
+
+        return License::buildEnterpriseEdition(null);
     }
 }

src/common/SeatManagement/LicenseHeaders.php
@@ -22,24 +22,20 @@ declare(strict_types=1);
 
 namespace Tuleap\SeatManagement;
 
-use PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles;
-use Tuleap\Test\PHPUnit\TestCase;
-use Tuleap\Test\Stubs\SeatManagement\BuildLicenseStub;
+use Ramsey\Uuid\UuidInterface;
 
-#[DisableReturnValueGenerationForTestDoubles]
-final class CachedLicenseBuilderTest extends TestCase
+/**
+ * @psalm-immutable
+ */
+final readonly class LicenseHeaders
 {
-    public function testItUsesCache(): void
-    {
-        $license         = License::buildEnterpriseEdition(null);
-        $license_builder = BuildLicenseStub::buildWithLicense($license);
-
-        $cache = new CachedLicenseBuilder($license_builder);
-
-        $cache->build();
-        $cache->build();
-
-        self::assertSame(1, $license_builder->getCallCount());
-        self::assertSame($license, $cache->build());
-    }
+    /**
+     * @var 'EdDSA'
+     */
+    public string $alg;
+    /**
+     * @var 'JWT'
+     */
+    public string $typ;
+    public UuidInterface $kid;
 }

src/common/SeatManagement/LicensePublicKey.php
@@ -22,24 +22,27 @@ declare(strict_types=1);
 
 namespace Tuleap\SeatManagement;
 
-use PHPUnit\Framework\Attributes\DisableReturnValueGenerationForTestDoubles;
-use Tuleap\Test\PHPUnit\TestCase;
-use Tuleap\Test\Stubs\SeatManagement\BuildLicenseStub;
+use Ramsey\Uuid\UuidInterface;
+use Tuleap\Mapper\Base64UrlSafeDecode;
 
-#[DisableReturnValueGenerationForTestDoubles]
-final class CachedLicenseBuilderTest extends TestCase
+final readonly class LicensePublicKey
 {
-    public function testItUsesCache(): void
-    {
-        $license         = License::buildEnterpriseEdition(null);
-        $license_builder = BuildLicenseStub::buildWithLicense($license);
-
-        $cache = new CachedLicenseBuilder($license_builder);
-
-        $cache->build();
-        $cache->build();
-
-        self::assertSame(1, $license_builder->getCallCount());
-        self::assertSame($license, $cache->build());
-    }
+    /**
+     * @var 'OKP'
+     */
+    public string $kty;
+    /**
+     * @var 'EdDSA'
+     */
+    public string $alg;
+    public UuidInterface $kid;
+    /**
+     * @var 'Ed25519'
+     */
+    public string $crv;
+    /**
+     * @var non-empty-string
+     */
+    #[Base64UrlSafeDecode]
+    public string $x;
 }

src/common/SeatManagement/LicenseSignatureChecker.php
@@ -1,0 +1,93 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\SeatManagement;
+
+use CuyZ\Valinor\Mapper\MappingError;
+use CuyZ\Valinor\Mapper\Source\Exception\InvalidSource;
+use CuyZ\Valinor\Mapper\Source\JsonSource;
+use CuyZ\Valinor\Mapper\TreeMapper;
+use Lcobucci\JWT\Encoding\CannotDecodeContent;
+use Lcobucci\JWT\Parser as ParserInterface;
+use Lcobucci\JWT\Signer\Eddsa;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Token\InvalidTokenStructure;
+use Lcobucci\JWT\Token\UnsupportedHeaderFound;
+use Lcobucci\JWT\Validation\Constraint\IssuedBy;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validator;
+use Psr\Log\LoggerInterface;
+use function Psl\File\read;
+use function Psl\Filesystem\is_file;
+
+final readonly class LicenseSignatureChecker implements CheckLicenseSignature
+{
+    private const string ISSUER = 'enalean-tuleap-enterprise';
+
+    public function __construct(
+        private LoggerInterface $logger,
+        private ParserInterface $parser,
+        private Validator $validator,
+        private TreeMapper $mapper,
+    ) {
+    }
+
+    #[\Override]
+    public function checkLicenseSignature(string $license_file_path, string $keys_directory): bool
+    {
+        $license_content = trim(read($license_file_path));
+        if ($license_content === '') {
+            $this->logger->error('License file is empty');
+            return false;
+        }
+
+        try {
+            $token = $this->parser->parse($license_content);
+        } catch (CannotDecodeContent | InvalidTokenStructure | UnsupportedHeaderFound $error) {
+            $this->logger->error('Failed parsing license: ' . $error->getMessage(), ['exception' => $error]);
+            return false;
+        }
+
+        try {
+            $headers = $this->mapper->map(LicenseHeaders::class, $token->headers()->all());
+        } catch (MappingError $error) {
+            $this->logger->error('Failed parsing license headers: ' . $error->getMessage(), ['exception' => $error]);
+            return false;
+        }
+
+        $kid             = $headers->kid->toString();
+        $public_key_file = "$keys_directory/$kid.key";
+        if (! is_file($public_key_file)) {
+            $this->logger->error('License uses non-existent public key.');
+            return false;
+        }
+
+        try {
+            $public_key = $this->mapper->map(LicensePublicKey::class, new JsonSource(read($public_key_file)));
+        } catch (MappingError | InvalidSource $error) {
+            $this->logger->error('Failed parsing public key: ' . $error->getMessage(), ['exception' => $error]);
+            return false;
+        }
+
+        return $this->validator->validate($token, new IssuedBy(self::ISSUER), new SignedWith(new Eddsa(), InMemory::plainText($public_key->x)));
+    }
+}

commit a35371201d5aa78ccd4b893617a80209672b905f

fix: Seat Management verification should not crash when a Graylog2 log sink is used
To reproduce the issue you to:
* set `sys_logger` to `graylog2`
* set `graylog2_server` and `graylog2_port` to some values

Issue was introduced in 3df6a7663f31a94aa2beb6f95ec73bd822f33bdb
because the code tries to ask to initialize a logger while initializing
a logger.

Part of story #44839 Enforce expiration date

Change-Id: I81cc640d0b27da1a2f4570472698e0b0224ddce5

src/common/Log/LogToGraylog2.php
@@ -73,7 +73,7 @@ final class LogToGraylog2
 
         $logger->pushProcessor(new IntrospectionProcessor());
         $logger->pushProcessor(new IncludeBacktraceProcessor());
-        $logger->pushProcessor(new TuleapIdentifierProcessor(VersionPresenter::fromFlavorFinder(new FlavorFinderFromLicense(CachedLicenseBuilder::instance()))));
+        $logger->pushProcessor(new TuleapIdentifierProcessor(VersionPresenter::fromFlavorFinder(new FlavorFinderFromLicense(CachedLicenseBuilder::instance($logger)))));
 
         return $logger;
     }

src/common/SeatManagement/CachedLicenseBuilder.php
@@ -27,6 +27,7 @@ use Lcobucci\JWT\Encoding\JoseEncoder;
 use Lcobucci\JWT\Token\Parser;
 use Lcobucci\JWT\Validation\Validator;
 use Override;
+use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
 use Tuleap\Mapper\ValinorMapperBuilderFactory;
 
@@ -39,13 +40,13 @@ final class CachedLicenseBuilder implements BuildLicense
     {
     }
 
-    public static function instance(): self
+    public static function instance(?LoggerInterface $logger = null): self
     {
         if (self::$instance === null) {
             self::$instance = new self(new LicenseBuilder(
                 new PublicKeyPresenceChecker(),
                 new LicenseSignatureChecker(
-                    BackendLogger::getDefaultLogger(),
+                    $logger ?? BackendLogger::getDefaultLogger(),
                     new Parser(new JoseEncoder()),
                     new Validator(),
                     ValinorMapperBuilderFactory::mapperBuilder()->registerConstructor(Uuid::fromString(...))->mapper(),

tests/unit/common/Backend/BackendLoggerTest.php
@@ -94,6 +94,16 @@ class BackendLoggerTest extends \Tuleap\Test\PHPUnit\TestCase
         $this->assertInstanceOf(TruncateLevelLogger::class, $logger);
     }
 
+    public function testReturnsAGraylog2BaseLogger(): void
+    {
+        ForgeConfig::set('sys_logger', 'graylog2');
+        ForgeConfig::set('graylog2_server', 'localhost');
+        ForgeConfig::set('graylog2_port', '9200');
+        $logger = BackendLogger::getDefaultLogger();
+
+        self::assertInstanceOf(\Monolog\Logger::class, $logger);
+    }
+
     public function testItReturnsFileLoggerWhenCannotConfigure(): void
     {
         ForgeConfig::set('sys_logger', 'graylog2');