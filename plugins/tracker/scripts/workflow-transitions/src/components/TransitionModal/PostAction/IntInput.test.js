Enalean/tuleap

commit ee7345600225801f8616a783bb84a4c578275910

remove vue-compatmode broken v-model
request #35095 Convert workflow in vue3

No functional changes
Create a workflow with every kind of actions, they should be stored
Note:
almost every actions are covered by e2e test suite only jenkins job is not covered yet

Why?
It's required to use vue-compat mode, without it many e2e tests fail
and many errors are raised by cypress

Change-Id: I4342a08ce06a50d34f8002ce6878be4269343e08

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/DateInput.vue
@@ -21,18 +21,31 @@
 <template>
     <select
         class="tlp-select"
-        v-model="field_value"
+        v-on:change="onChange"
         v-bind:disabled="disabled"
         required
         data-test="select-date"
     >
-        <option data-test-type="placeholder" v-bind:value="null" disabled>
+        <option
+            data-test-type="placeholder"
+            v-bind:value="null"
+            disabled
+            v-bind:selected="input_value === null"
+        >
             {{ $gettext("Please choose") }}
         </option>
-        <option data-test-type="clear" v-bind:value="DATE_FIELD_VALUE.CLEAR">
+        <option
+            data-test-type="clear"
+            v-bind:value="DATE_FIELD_VALUE.CLEAR"
+            v-bind:selected="input_value === DATE_FIELD_VALUE.CLEAR"
+        >
             {{ $gettext("Clear") }}
         </option>
-        <option data-test-type="current" v-bind:value="DATE_FIELD_VALUE.CURRENT">
+        <option
+            data-test-type="current"
+            v-bind:value="DATE_FIELD_VALUE.CURRENT"
+            v-bind:selected="input_value === DATE_FIELD_VALUE.CURRENT"
+        >
             {{ $gettext("Current time") }}
         </option>
     </select>
@@ -43,7 +56,7 @@ import { DATE_FIELD_VALUE } from "../../../constants/workflow-constants.js";
 export default {
     name: "DateInput",
     props: {
-        value: {
+        input_value: {
             type: String,
         },
         disabled: {
@@ -56,13 +69,13 @@ export default {
         };
     },
     computed: {
-        field_value: {
-            get() {
-                return this.value;
-            },
-            set(value) {
-                this.$emit("input", value);
-            },
+        field_value() {
+            return this.value;
+        },
+    },
+    methods: {
+        onChange(event) {
+            this.$emit("change", event.target.value);
         },
     },
 };

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/FloatInput.vue
@@ -23,7 +23,7 @@
         class="tlp-input"
         type="number"
         step="any"
-        v-bind:value="value"
+        v-bind:value="input_value"
         v-on:input="onInput"
         v-bind:disabled="disabled"
         required
@@ -34,7 +34,7 @@
 export default {
     name: "FloatInput",
     props: {
-        value: {
+        input_value: {
             type: Number,
         },
         disabled: {

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/IntInput.vue
@@ -23,7 +23,7 @@
         class="tlp-input"
         type="number"
         pattern="\d*"
-        v-bind:value="value"
+        v-bind:value="input_value"
         v-on:input="onInput"
         v-bind:disabled="disabled"
         required
@@ -34,7 +34,7 @@
 export default {
     name: "IntInput",
     props: {
-        value: {
+        input_value: {
             type: Number,
         },
         disabled: {

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/SetValueAction.vue
@@ -30,11 +30,13 @@
                 class="tlp-select"
                 data-test-type="field"
                 data-test="field"
-                v-model="post_action_field"
+                v-on:change="updatePostActionField"
                 required
                 v-bind:disabled="is_modal_save_running"
             >
-                <option v-bind:value="null" disabled>{{ $gettext("Please choose") }}</option>
+                <option v-bind:value="null" disabled v-bind:selected="post_action_field === null">
+                    {{ $gettext("Please choose") }}
+                </option>
                 <optgroup
                     v-for="group in available_fields_by_groups"
                     v-bind:key="group.label"
@@ -44,9 +46,13 @@
                     <option
                         v-for="field in group.fields"
                         v-bind:key="field.field_id"
-                        v-bind:value="field"
+                        v-bind:value="field.field_id"
                         v-bind:disabled="field.disabled && field !== post_action_field"
                         v-bind:data-test-type="`field_${field.field_id}`"
+                        v-bind:selected="
+                            post_action_field !== null &&
+                            field.field_id === post_action_field.field_id
+                        "
                     >
                         {{ field.label }}
                     </option>
@@ -62,8 +68,10 @@
             <component
                 v-bind:is="value_input_component"
                 v-bind:id="value_input_id"
-                v-model="value"
+                v-on:input="updateValue"
+                v-on:change="updateValue"
                 v-bind:disabled="is_modal_save_running"
+                v-bind:input_value="value"
             />
         </div>
     </post-action>
@@ -80,7 +88,7 @@ import PlaceholderInput from "./PlaceholderInput.vue";
 
 export default {
     name: "SetValueAction",
-    components: { PostAction, DateInput, FloatInput, IntInput },
+    components: { PlaceholderInput, PostAction, DateInput, FloatInput, IntInput },
     props: {
         post_action: {
             type: Object,
@@ -128,41 +136,43 @@ export default {
             }
             return PlaceholderInput;
         },
-        post_action_field: {
-            get() {
-                if (!this.post_action.field_id) {
-                    return null;
-                }
-                const matching_fields = this.available_fields.filter(
-                    (field) => field.field_id === this.post_action.field_id,
-                );
-                if (matching_fields.length === 0) {
-                    return null;
-                }
-                return matching_fields[0];
-            },
-            set(new_field) {
-                this.$store.commit("transitionModal/updateSetValuePostActionField", {
-                    post_action: this.post_action,
-                    new_field,
-                });
-            },
+        post_action_field() {
+            if (!this.post_action.field_id) {
+                return null;
+            }
+            const matching_fields = this.available_fields.filter(
+                (field) => field.field_id === this.post_action.field_id,
+            );
+            if (matching_fields.length === 0) {
+                return null;
+            }
+            return matching_fields[0];
         },
-        value: {
-            get() {
-                return this.post_action.value;
-            },
-            set(value) {
-                this.$store.commit("transitionModal/updateSetValuePostActionValue", {
-                    post_action: this.post_action,
-                    value,
-                });
-            },
+        value() {
+            return this.post_action.value;
         },
     },
     methods: {
         getAvailableFieldsOfType(type) {
             return this.available_fields.filter((field) => field.type === type);
+        },
+        updatePostActionField(event) {
+            const field = this.available_fields.filter(
+                (field) => field.field_id === parseInt(event.target.value, 10),
+            );
+            if (field.length === 0) {
+                throw new Error("Can not update postActionField, field is not found");
+            }
+            this.$store.commit("transitionModal/updateSetValuePostActionField", {
+                post_action: this.post_action,
+                new_field: field[0],
+            });
+        },
+        updateValue(event) {
+            this.$store.commit("transitionModal/updateSetValuePostActionValue", {
+                post_action: this.post_action,
+                value: event,
+            });
         },
     },
 };

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/DateInput.test.js
@@ -28,7 +28,7 @@ describe("DateInput", () => {
 
     beforeEach(async () => {
         wrapper = shallowMount(DateInput, {
-            propsData: { value: DATE_FIELD_VALUE.CLEAR },
+            propsData: { input_value: DATE_FIELD_VALUE.CLEAR },
             localVue: await createLocalVueForTests(),
         });
     });
@@ -39,7 +39,7 @@ describe("DateInput", () => {
     };
 
     describe("without value", () => {
-        beforeEach(() => wrapper.setProps({ value: null }));
+        beforeEach(() => wrapper.setProps({ input_value: null }));
 
         it("Shows placeholder", () => {
             expect(findSelectedOption().dataset.testType).toBe("placeholder");
@@ -47,7 +47,7 @@ describe("DateInput", () => {
     });
 
     describe('with "current" value', () => {
-        beforeEach(() => wrapper.setProps({ value: DATE_FIELD_VALUE.CURRENT }));
+        beforeEach(() => wrapper.setProps({ input_value: DATE_FIELD_VALUE.CURRENT }));
 
         it('Select "current" option', () => {
             expect(findSelectedOption().dataset.testType).toBe("current");
@@ -61,8 +61,8 @@ describe("DateInput", () => {
         });
 
         it("emits input event with corresponding value", () => {
-            expect(wrapper.emitted().input).toBeTruthy();
-            expect(wrapper.emitted().input[0]).toStrictEqual([DATE_FIELD_VALUE.CURRENT]);
+            expect(wrapper.emitted().change).toBeTruthy();
+            expect(wrapper.emitted().change[0]).toStrictEqual([DATE_FIELD_VALUE.CURRENT]);
         });
     });
 });

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/AuthorizedUGroupsSelect.vue
@@ -26,7 +26,7 @@
             multiple
             class="tracker-workflow-transition-modal-authorized-ugroups"
             v-bind:disabled="is_modal_save_running"
-            v-model="authorized_user_group_ids"
+            v-on:change="updateUGroups"
             data-test="authorized-ugroups-select"
             required
             ref="workflow_configuration_permission"
@@ -35,6 +35,7 @@
                 v-for="user_group in user_groups"
                 v-bind:key="user_group.id"
                 v-bind:value="user_group.id"
+                v-bind:selected="authorized_user_group_ids.includes(user_group.id)"
             >
                 {{ user_group.label }}
             </option>
@@ -60,16 +61,11 @@ export default defineComponent({
             "user_groups",
             "is_modal_save_running",
         ]),
-        authorized_user_group_ids: {
-            get() {
-                if (!this.current_transition) {
-                    return [];
-                }
-                return this.current_transition.authorized_user_group_ids;
-            },
-            set(authorized_user_group_ids) {
-                this.updateAuthorizedUserGroupIds(authorized_user_group_ids);
-            },
+        authorized_user_group_ids() {
+            if (!this.current_transition) {
+                return [];
+            }
+            return this.current_transition.authorized_user_group_ids;
         },
     },
     mounted() {
@@ -81,7 +77,7 @@ export default defineComponent({
             },
         );
     },
-    beforeDestroy() {
+    beforeUnmount() {
         this.configuration_permission_list_picker.destroy();
     },
     methods: {
@@ -90,6 +86,17 @@ export default defineComponent({
                 "transitionModal/updateAuthorizedUserGroupIds",
                 authorized_user_group_ids,
             );
+        },
+        updateUGroups(event) {
+            const select = event.target;
+            const selected_option = Array.from(select.options).filter((option) => {
+                return option.selected;
+            });
+            const values = selected_option.map((option) => {
+                return option.value;
+            });
+
+            this.updateAuthorizedUserGroupIds(values);
         },
     },
 });

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/CommentNotEmptyCheckBox.vue
@@ -27,9 +27,10 @@
                 id="workflow-configuration-not-empty-comment"
                 type="checkbox"
                 name="transition-comment-not-empty"
-                v-model="transition_comment_not_empty"
+                v-on:change="updateNotEmpty"
                 v-bind:disabled="is_modal_save_running"
                 data-test="not-empty-comment-checkbox"
+                v-bind:checked="transition_comment_not_empty"
             />
             <span>{{ $gettext("Comment must not be empty") }}</span>
         </label>
@@ -46,16 +47,18 @@ export default defineComponent({
     computed: {
         ...mapState("transitionModal", ["current_transition", "is_modal_save_running"]),
         ...mapGetters("transitionModal", ["is_transition_from_new_artifact"]),
-        transition_comment_not_empty: {
-            get() {
-                if (!this.current_transition) {
-                    return false;
-                }
-                return this.current_transition.is_comment_required;
-            },
-            set(value) {
-                this.$store.commit("transitionModal/updateIsCommentRequired", value);
-            },
+        transition_comment_not_empty() {
+            if (!this.current_transition) {
+                return false;
+            }
+            return this.current_transition.is_comment_required;
+        },
+    },
+    methods: {
+        updateNotEmpty(event) {
+            const checkbox = event.target;
+
+            this.$store.commit("transitionModal/updateIsCommentRequired", checkbox.checked);
         },
     },
 });

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/Externals/AddToBacklogAgileDashboardPostActionOption.vue
@@ -25,15 +25,14 @@
         v-bind:disabled="!add_to_backlog_information.valid"
         v-bind:title="add_to_backlog_information.title"
         data-test="add-to-backlog"
+        v-bind:selected="post_action_type && post_action_type === getPostActionType()"
     >
         {{ add_to_backlog_information.option }}
     </option>
 </template>
-
 <script>
 import { mapGetters } from "vuex";
 import { EXTERNAL_POST_ACTION_TYPE } from "../../../constants/workflow-constants.js";
-
 export default {
     name: "AddToBacklogAgileDashboardPostActionOption",
     props: {
@@ -44,7 +43,7 @@ export default {
         add_to_backlog_information() {
             if (
                 this.add_to_top_backlog_is_already_present &&
-                this.post_action_type !== EXTERNAL_POST_ACTION_TYPE.ADD_TO_BACKLOG_AGILE_DASHBOARD
+                this.post_action_type !== this.getPostActionType()
             ) {
                 return {
                     valid: false,
@@ -61,11 +60,14 @@ export default {
         add_to_top_backlog_is_already_present() {
             return (
                 this.post_actions.filter(
-                    (post_action) =>
-                        post_action.type ===
-                        EXTERNAL_POST_ACTION_TYPE.ADD_TO_BACKLOG_AGILE_DASHBOARD,
+                    (post_action) => post_action.type === this.getPostActionType(),
                 ).length > 0
             );
+        },
+    },
+    methods: {
+        getPostActionType() {
+            return EXTERNAL_POST_ACTION_TYPE.ADD_TO_BACKLOG_AGILE_DASHBOARD;
         },
     },
 };

commit 5c73e67ead023b482d6fa5d8bba4b338703aa55f

fix: request #45233 Unable to enter a numeric value in Actions automatically performed
What has been fixed:
The 'input' event was declared twice, once in the parent and once in the
child. The value could not be updated because at each input, the parent
(`SetValueAction`) received the value sent by the child, and then an
input event received by itself.
The bug seems to have been introduced by:
ee7345600225801f8616a783bb84a4c578275910

To avoid confusion, I renamed the event `new-input-value`.

How to test:
-- Have a tracker with transition rules and Date, Int and Float fileds
-- In transitions rules configuration, click on a `Configure state` button
-- On the right pane click on `+ Add another action`
-- Select `Change the value of a field`
-- Select an Integer field
=> You should be able to write a value and save

-- Click on `+ Add another action`
-- Select a Float field
=> You should be able to write a value and save

-- Click on `+ Add another action`
-- Select a Date field
=> You should be able to select `Clear` or `Current time` and save

Change-Id: Ibed27c28eaef0e0b054a15abfcd3c1573d4cc20c

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/DateInput.vue
@@ -55,7 +55,7 @@
 import { DATE_FIELD_VALUE } from "../../../constants/workflow-constants.js";
 export default {
     name: "DateInput",
-    emits: ["change"],
+    emits: ["new-input-value"],
     props: {
         input_value: {
             type: String,
@@ -76,7 +76,7 @@ export default {
     },
     methods: {
         onChange(event) {
-            this.$emit("change", event.target.value);
+            this.$emit("new-input-value", event.target.value);
         },
     },
 };

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/FloatInput.vue
@@ -26,6 +26,7 @@
         v-bind:value="input_value"
         v-on:input="onInput"
         v-bind:disabled="disabled"
+        data-test="float-input"
         required
     />
 </template>
@@ -47,7 +48,7 @@ export default {
             if (isNaN(float_value)) {
                 return;
             }
-            return this.$emit("input", float_value);
+            this.$emit("new-input-value", float_value);
         },
     },
 };

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/IntInput.vue
@@ -26,6 +26,7 @@
         v-bind:value="input_value"
         v-on:input="onInput"
         v-bind:disabled="disabled"
+        data-test="int-input"
         required
     />
 </template>
@@ -47,7 +48,7 @@ export default {
             if (isNaN(int_value)) {
                 return;
             }
-            return this.$emit("input", int_value);
+            this.$emit("new-input-value", int_value);
         },
     },
 };

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/SetValueAction.vue
@@ -68,8 +68,7 @@
             <component
                 v-bind:is="value_input_component"
                 v-bind:id="value_input_id"
-                v-on:input="updateValue"
-                v-on:change="updateValue"
+                v-on:new-input-value="updateValue"
                 v-bind:disabled="is_modal_save_running"
                 v-bind:input_value="value"
             />
@@ -168,10 +167,10 @@ export default {
                 new_field: field[0],
             });
         },
-        updateValue(event) {
+        updateValue(value) {
             this.$store.commit("transitionModal/updateSetValuePostActionValue", {
                 post_action: this.post_action,
-                value: event,
+                value: value,
             });
         },
     },

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/DateInput.test.js
@@ -62,9 +62,11 @@ describe("DateInput", () => {
             wrapper.get("select").trigger("change");
         });
 
-        it("emits input event with corresponding value", () => {
-            expect(wrapper.emitted().change).toBeTruthy();
-            expect(wrapper.emitted().change[0]).toStrictEqual([DATE_FIELD_VALUE.CURRENT]);
+        it("emits 'new-input-value' event with corresponding value", () => {
+            expect(wrapper.emitted()["new-input-value"]).toBeTruthy();
+            expect(wrapper.emitted()["new-input-value"][0]).toStrictEqual([
+                DATE_FIELD_VALUE.CURRENT,
+            ]);
         });
     });
 });

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/FloatInput.test.js
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import FloatInput from "./FloatInput.vue";
+import { getGlobalTestOptions } from "../../../helpers/global-options-for-tests.js";
+
+describe("IntInput", () => {
+    function getWrapper() {
+        return shallowMount(FloatInput, {
+            propsData: { input_value: null },
+            global: {
+                ...getGlobalTestOptions(),
+            },
+        });
+    }
+
+    describe("when the input value is set", () => {
+        it("should emit a 'new-input-value' event with corresponding value", () => {
+            const wrapper = getWrapper();
+            wrapper.find("[data-test=float-input]").setValue(6.66);
+
+            expect(wrapper.emitted()["new-input-value"]).toBeTruthy();
+            expect(wrapper.emitted()["new-input-value"][0]).toStrictEqual([6.66]);
+        });
+    });
+});

plugins/tracker/scripts/workflow-transitions/src/components/TransitionModal/PostAction/IntInput.test.js
@@ -1,0 +1,43 @@
+/**
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { shallowMount } from "@vue/test-utils";
+import IntInput from "./IntInput.vue";
+import { getGlobalTestOptions } from "../../../helpers/global-options-for-tests.js";
+
+describe("IntInput", () => {
+    function getWrapper() {
+        return shallowMount(IntInput, {
+            propsData: { input_value: null },
+            global: {
+                ...getGlobalTestOptions(),
+            },
+        });
+    }
+
+    describe("when the input value is set", () => {
+        it("should emit a 'new-input-value' event with corresponding value", () => {
+            const wrapper = getWrapper();
+            wrapper.find("[data-test=int-input]").setValue(6);
+
+            expect(wrapper.emitted()["new-input-value"]).toBeTruthy();
+            expect(wrapper.emitted()["new-input-value"][0]).toStrictEqual([6]);
+        });
+    });
+});