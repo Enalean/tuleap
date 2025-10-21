Enalean/tuleap

commit 4c643ed72964f5fe33877433e3c259c989ed30b8

chore: Add missing CSP nonces tag on <script> tags in docman
Part of request #17967: Deploy a useful content security policy

Change-Id: I5abc238117ce6628f807409b7e5308420acc36ea

src/common/wiki/phpwiki/lib/HtmlElement.php
@@ -199,10 +199,12 @@ class HTML extends HtmlElement
         return $el->_init2(func_get_args());
     }
 
-    public static function script(/*...*/)
+    public static function script(array $attributes, string ...$content)
     {
-        $el = new HtmlElement('script');
-        return $el->_init2(func_get_args());
+        $el         = new HtmlElement('script');
+        $purifier   = Codendi_HTMLPurifier::instance();
+        $attributes = [...$attributes, 'nonce' => $purifier->purify(\Tuleap\ContentSecurityPolicy\CSPNonce::build()->value)];
+        return $el->_init2([$attributes, ...$content]);
     }
 
     public static function noscript(/*...*/)

plugins/docman/include/view/Docman_View_ItemDetailsSectionMove.php
@@ -34,6 +34,7 @@ class Docman_View_ItemDetailsSectionMove extends Docman_View_ItemDetailsSectionA
     #[\Override]
     public function getContent($params = [])
     {
+        $purifier = Codendi_HTMLPurifier::instance();
         $content  = '';
         $content .= '<dl><dt>' . dgettext('tuleap-docman', 'Move') . '</dt><dd>';
         $content .= '<form action="' . $this->url . '" method="POST">';
@@ -46,7 +47,8 @@ class Docman_View_ItemDetailsSectionMove extends Docman_View_ItemDetailsSectionA
             'input_name'   => 'id',
             'excludes'     => [$this->item->getId()],
         ]);
-        $content     .= '<script type="text/javascript">docman.options.move.item_id = ' . $this->item->getId() . ';</script>';
+        $csp_nonce    = \Tuleap\ContentSecurityPolicy\CSPNonce::build();
+        $content     .= '<script type="text/javascript" nonce="' . $purifier->purify($csp_nonce->value) . '">docman.options.move.item_id = ' . $this->item->getId() . ';</script>';
         $content     .=  '<br />';
 
         //submit

plugins/docman/include/view/Docman_View_ParentsTree.php
@@ -68,13 +68,14 @@ class Docman_View_ParentsTree /* implements Visitor*/
         $disabled      = ($this->docman->userCanWrite($folder['id'])) ? '' : 'disabled="disabled"';
         $label_classes = $selected ? 'docman_item_actual_parent' : '';
 
-        $h  = '<li  class="' . Docman_View_Browse::getItemClasses(['is_last' => $params['is_last']]) . '">';
-        $h .= '<label for="item_parent_id_' . $folder['id'] . '" class="' . $label_classes . '" >';
-        $h .= '<input type="radio" ' . $selected . ' name="' . $params['input_name'] . '" value="' . $folder['id'] . '" id="item_parent_id_' . $folder['id'] . '" ' . $disabled . ' />';
-        $h .= '<img src="' . $folder['icon_src'] . '" class="docman_item_icon" />';
-        $h .=  $hp->purify($folder['title'], CODENDI_PURIFIER_CONVERT_HTML)  . '</label>';
-        $h .= '<script type="text/javascript">docman.addParentFoldersForNewItem(' . $folder['id'] . ', ' . $folder['parent_id'] . ", '" .  $hp->purify(addslashes($folder['title']), CODENDI_PURIFIER_CONVERT_HTML) . "');</script>\n";
-        $h .= '<ul class="docman_items">';
+        $h         = '<li  class="' . Docman_View_Browse::getItemClasses(['is_last' => $params['is_last']]) . '">';
+        $h        .= '<label for="item_parent_id_' . $folder['id'] . '" class="' . $label_classes . '" >';
+        $h        .= '<input type="radio" ' . $selected . ' name="' . $params['input_name'] . '" value="' . $folder['id'] . '" id="item_parent_id_' . $folder['id'] . '" ' . $disabled . ' />';
+        $h        .= '<img src="' . $folder['icon_src'] . '" class="docman_item_icon" />';
+        $h        .=  $hp->purify($folder['title'], CODENDI_PURIFIER_CONVERT_HTML)  . '</label>';
+        $csp_nonce = \Tuleap\ContentSecurityPolicy\CSPNonce::build();
+        $h        .= '<script type="text/javascript" nonce="' . $hp->purify($csp_nonce->value) . '">docman.addParentFoldersForNewItem(' . $folder['id'] . ', ' . $folder['parent_id'] . ", '" .  $hp->purify(addslashes($folder['title']), CODENDI_PURIFIER_CONVERT_HTML) . "');</script>\n";
+        $h        .= '<ul class="docman_items">';
 
         $params['is_last'] = false;
         $nb                = count($folder['items']);

plugins/docman/include/view/Docman_View_PasteInProgress.php
@@ -51,15 +51,18 @@ class Docman_View_PasteInProgress extends Docman_View_ProjectHeader
         $actions = new Docman_Actions($this->_controller);
         $actions->doPaste($params['itemToPaste'], $params['item'], $params['rank'], $params['importMd'], $params['srcMode']);
 
+        $purifier  = Codendi_HTMLPurifier::instance();
+        $csp_nonce = \Tuleap\ContentSecurityPolicy\CSPNonce::build();
+
         // Remove wait mesage and the spinner
-        echo '<script type="text/javascript">$("paste_' . $params['itemToPaste']->getId() . '").hide();</script>';
+        echo '<script type="text/javascript" nonce="' . $purifier->purify($csp_nonce->value) . '">$("paste_' . $params['itemToPaste']->getId() . '").hide();</script>';
 
         //Display paste sucessfully complete.
         echo dgettext('tuleap-docman', '<p>Paste operation successfully completed.</p>');
 
         $url = $this->_controller->getDefaultUrl() . 'action=show&id=' . $params['item']->getId();
         echo sprintf(dgettext('tuleap-docman', '<p>You will be redirected to <a href="%1$s"> %2$s </a> in %3$s seconds</p>'), $url, $url, 5);
-        echo '<script type="text/javascript">setTimeout(function () {location.href="' . $url . '";}, 5000);</script>';
+        echo '<script type="text/javascript" nonce="' . $purifier->purify($csp_nonce->value) . '">setTimeout(function () {location.href="' . $url . '";}, 5000);</script>';
     }
 
     #[\Override]

plugins/docman/include/view/Docman_View_ProjectHeader.php
@@ -24,8 +24,10 @@
     /* protected */ #[Override]
     public function _scripts($params) //phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
     {
-        $project = ProjectManager::instance()->getProject((int) $params['group_id']);
-        echo '<script type="text/javascript"> var docman = new com.xerox.codendi.Docman(' . $params['group_id'] . ', ';
+        $project   = ProjectManager::instance()->getProject((int) $params['group_id']);
+        $purifier  = Codendi_HTMLPurifier::instance();
+        $csp_nonce = \Tuleap\ContentSecurityPolicy\CSPNonce::build();
+        echo '<script type="text/javascript" nonce="' . $purifier->purify($csp_nonce->value) . '"> var docman = new com.xerox.codendi.Docman(' . $params['group_id'] . ', ';
         $di = $this->_getDocmanIcons($params);
         echo json_encode(array_merge(
             [

plugins/docman/include/view/Docman_View_View.php
@@ -97,7 +97,9 @@
     /* protected */ public function _javascript($params)
     {
         if ($this->javascript != '') {
-            echo "<script type=\"text/javascript\">\n" .
+            $purifier  = Codendi_HTMLPurifier::instance();
+            $csp_nonce = \Tuleap\ContentSecurityPolicy\CSPNonce::build();
+            echo '<script type="text/javascript" nonce="' . $purifier->purify($csp_nonce->value) . '">' . "\n" .
             "//<!--\n" .
             $this->javascript .
             "//-->\n" .

commit 75cb3d35d42cd03ef005f2b1f2cc8c1517052cfe

fix: PHPWiki should not crash when displaying the page history or pages using some plugins
Introduced by 4c643ed72964f5fe33877433e3c259c989ed30b8.

Part of request #17967: Deploy a useful content security policy

Change-Id: Id4b3d6d3d5cc6e5f181c939354d4991cb1fb36e7

src/common/wiki/phpwiki/lib/HtmlElement.php
@@ -199,7 +199,7 @@ class HTML extends HtmlElement
         return $el->_init2(func_get_args());
     }
 
-    public static function script(array $attributes, string ...$content)
+    public static function script(array $attributes, mixed ...$content)
     {
         $el         = new HtmlElement('script');
         $purifier   = Codendi_HTMLPurifier::instance();