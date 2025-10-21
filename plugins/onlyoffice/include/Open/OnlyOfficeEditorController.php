Enalean/tuleap

commit 0f632bac0ca06b6aea7605c297f5dd640f905770

feat: Restrict loading of script attributes based on nonces and origins
Only scripts from a known origin and having a nonce can now be loaded.
This achieved by setting 2 CSP in the header, one is responsible to
restrict the origins the other one to ensure the presence of a nonce.
Browsers apply restrictions in a cumulative way.

The policy is still quite weak since we still allow unsafe script
attributes.

Additionally, the variables used to set the CSP header in pre.php do not
pollute the global scope anymore.

Part of request #17967: Deploy a useful content security policy

Change-Id: I08db1e195ef707e367df61afdfcd308bcc89f52f

plugins/onlyoffice/include/Open/OnlyOfficeEditorController.php
@@ -94,8 +94,9 @@ final class OnlyOfficeEditorController extends DispatchablePSR15Compatible
     {
         $document_server_url_csp_encoded = str_replace([',', ';'], ['%2C', '%3B'], $document_server_url);
 
-        $csp_header  = "default-src 'report-sample'; object-src 'none'; base-uri 'none'; frame-ancestors 'self'; sandbox allow-scripts allow-same-origin allow-downloads allow-popups allow-popups-to-escape-sandbox; report-uri /csp-violation;";
-        $csp_header .= "style-src 'nonce-$csp_nonce'; script-src 'nonce-$csp_nonce' 'strict-dynamic'; frame-src $document_server_url_csp_encoded;";
+        $csp_header  = "script-src-elem 'self', ";
+        $csp_header .= "default-src 'report-sample'; object-src 'none'; base-uri 'none'; frame-ancestors 'self'; sandbox allow-scripts allow-same-origin allow-downloads allow-popups allow-popups-to-escape-sandbox; report-uri /csp-violation;";
+        $csp_header .= "style-src 'nonce-$csp_nonce'; script-src-elem 'nonce-$csp_nonce' 'strict-dynamic'; frame-src $document_server_url_csp_encoded;";
 
         return $this->response_factory->createResponse()
             ->withHeader(

plugins/oauth2_server/include/AuthorizationServer/OAuth2ConsentRequiredResponseBuilder.php
@@ -24,15 +24,18 @@ namespace Tuleap\OAuth2Server\AuthorizationServer;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Tuleap\ContentSecurityPolicy\CSPNonce;
 use Tuleap\Layout\BaseLayout;
 use Tuleap\OAuth2ServerCore\App\OAuth2App;
 use Tuleap\OAuth2ServerCore\AuthorizationServer\AuthorizationEndpointController;
 use Tuleap\OAuth2ServerCore\AuthorizationServer\ConsentRequiredResponseBuilder;
 
-final class OAuth2ConsentRequiredResponseBuilder implements ConsentRequiredResponseBuilder
+final readonly class OAuth2ConsentRequiredResponseBuilder implements ConsentRequiredResponseBuilder
 {
-    public function __construct(private AuthorizationFormRenderer $form_renderer)
-    {
+    public function __construct(
+        private AuthorizationFormRenderer $form_renderer,
+        private CSPNonce $csp_nonce,
+    ) {
     }
 
     /**
@@ -55,7 +58,8 @@ final class OAuth2ConsentRequiredResponseBuilder implements ConsentRequiredRespo
         return $this->form_renderer->renderForm($data, $layout)
             ->withHeader(
                 'Content-Security-Policy',
-                "default-src 'report-sample'; base-uri 'none'; script-src 'self' 'unsafe-inline' 'report-sample'; style-src 'self' 'report-sample'; font-src 'self'; img-src 'self'; connect-src 'self'; manifest-src 'self';"
+                "script-src-elem 'self', "
+                . "default-src 'report-sample'; base-uri 'none'; script-src-elem 'nonce-{$this->csp_nonce->value}' 'strict-dynamic'; style-src 'self' 'report-sample'; font-src 'self'; img-src 'self'; connect-src 'self'; manifest-src 'self';"
                 . "form-action 'self' " . $redirect_uri . ';'
                 . "frame-ancestors 'none'; block-all-mixed-content; report-uri /csp-violation;"
             );

plugins/oauth2_server/include/oauth2_serverPlugin.php
@@ -423,6 +423,7 @@ final class oauth2_serverPlugin extends Plugin
                     TemplateRendererFactory::build(),
                     new \Tuleap\OAuth2Server\AuthorizationServer\AuthorizationFormPresenterBuilder($redirect_uri_builder)
                 ),
+                \Tuleap\ContentSecurityPolicy\CSPNonce::build(),
             ),
             \UserManager::instance(),
             new \Tuleap\OAuth2ServerCore\App\AppFactory(

plugins/oauth2_server/tests/unit/AuthorizationServer/OAuth2AuthorizationFormResponseBuilderTest.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace Tuleap\OAuth2Server\AuthorizationServer;
 
 use Tuleap\Authentication\Scope\AuthenticationScope;
+use Tuleap\ContentSecurityPolicy\CSPNonce;
 use Tuleap\Http\HTTPFactoryBuilder;
 use Tuleap\Http\Server\NullServerRequest;
 use Tuleap\Layout\BaseLayout;
@@ -46,7 +47,8 @@ final class OAuth2AuthorizationFormResponseBuilderTest extends TestCase
         $form_renderer->expects($this->once())->method('renderForm')->willReturn(HTTPFactoryBuilder::responseFactory()->createResponse());
 
         $form_response_builder = new OAuth2ConsentRequiredResponseBuilder(
-            $form_renderer
+            $form_renderer,
+            CSPNonce::build()
         );
 
         $request  = (new NullServerRequest())->withAttribute(BaseLayout::class, LayoutBuilder::build());

src/www/include/pre.php
@@ -120,36 +120,40 @@ $loader_scheduler = new LoaderScheduler($cookie_manager, $plugin_loader, new \Tu
 $loader_scheduler->loadPluginsThenStartSession(IS_SCRIPT, $_SERVER);
 
 if (! IS_SCRIPT) {
-    header('Referrer-Policy: no-referrer-when-downgrade, strict-origin, same-origin');
+    (static function () use ($event_manager) {
+        header('Referrer-Policy: no-referrer-when-downgrade, strict-origin, same-origin');
 
-    // Protection against clickjacking
-    header('X-Frame-Options: DENY');
-    $csp_rules = "frame-ancestors 'self'; ";
+        // Protection against clickjacking
+        header('X-Frame-Options: DENY');
+        $csp_rules = "frame-ancestors 'self'; ";
 
-    // XSS prevention
-    header('X-Content-Type-Options: nosniff');
-    $whitelist_scripts = [];
-    $event_manager->processEvent(
-        Event::CONTENT_SECURITY_POLICY_SCRIPT_WHITELIST,
-        [
-            'whitelist_scripts' => &$whitelist_scripts,
-        ]
-    );
-    $csp_whitelist_script_scr  = implode(' ', $whitelist_scripts);
-    $csp_whitelist_script_scr .= ' ' . ForgeConfig::get('sys_csp_script_scr_whitelist');
-    $csp_rules                .= "script-src 'self' 'unsafe-inline' 'report-sample' $csp_whitelist_script_scr ; ";
-    $csp_rules                .= "style-src 'self' 'unsafe-inline' 'report-sample'; ";
-    $csp_rules                .= "font-src 'self'; ";
-    $csp_rules                .= "form-action 'self'; ";
-    $csp_rules                .= "manifest-src 'self'; ";
-    $csp_rules                .= 'img-src * data: blob:; ';
-    $csp_rules                .= "media-src 'self'; ";
-    $csp_rules                .= 'connect-src *; ';
-    $csp_rules                .= 'child-src *; ';
-    $csp_rules                .= 'upgrade-insecure-requests; ';
-    $csp_rules                .= 'report-uri /csp-violation; ';
+        // XSS prevention
+        header('X-Content-Type-Options: nosniff');
+        $allowlist_scripts = [];
+        $event_manager->processEvent(
+            Event::CONTENT_SECURITY_POLICY_SCRIPT_WHITELIST,
+            [
+                'whitelist_scripts' => &$allowlist_scripts,
+            ]
+        );
+        $csp_script_origin = "script-src-elem 'self' 'unsafe-inline' " . implode(' ', $allowlist_scripts) . ' ' . ForgeConfig::get('sys_csp_script_scr_whitelist');
 
-    header("Content-Security-Policy: default-src 'report-sample'; base-uri 'self'; $csp_rules");
+        $csp_nonce_value = \Tuleap\ContentSecurityPolicy\CSPNonce::build()->value;
+        $csp_rules      .= "script-src-elem 'nonce-$csp_nonce_value' 'strict-dynamic'; ";
+        $csp_rules      .= "script-src-attr 'unsafe-inline' 'report-sample'; ";
+        $csp_rules      .= "style-src 'self' 'unsafe-inline' 'report-sample'; ";
+        $csp_rules      .= "font-src 'self'; ";
+        $csp_rules      .= "form-action 'self'; ";
+        $csp_rules      .= "manifest-src 'self'; ";
+        $csp_rules      .= 'img-src * data: blob:; ';
+        $csp_rules      .= "media-src 'self'; ";
+        $csp_rules      .= 'connect-src *; ';
+        $csp_rules      .= 'child-src *; ';
+        $csp_rules      .= 'upgrade-insecure-requests; ';
+        $csp_rules      .= 'report-uri /csp-violation; ';
+
+        header("Content-Security-Policy: $csp_script_origin, default-src 'report-sample'; base-uri 'self'; $csp_rules");
+    })();
 }
 
 $feedback = ''; // Initialize global var

commit 9a59f39d5c80def0646d2b390826758bbda438fe

fix: ONLYOFFICE editor does not load
Issue introduced by 0f632bac0ca06b6aea7605c297f5dd640f905770.

The issue is not visible on the dev instance setup because we load the
ONLYOFFICE editor from the same origin.

Part of request #17967: Deploy a useful content security policy

Change-Id: I2e0b6d2a7ad61157dd3b887daef42f3ddabd01b1

plugins/onlyoffice/include/Open/OnlyOfficeEditorController.php
@@ -94,7 +94,7 @@ final class OnlyOfficeEditorController extends DispatchablePSR15Compatible
     {
         $document_server_url_csp_encoded = str_replace([',', ';'], ['%2C', '%3B'], $document_server_url);
 
-        $csp_header  = "script-src-elem 'self', ";
+        $csp_header  = "script-src-elem 'self' $document_server_url_csp_encoded, ";
         $csp_header .= "default-src 'report-sample'; object-src 'none'; base-uri 'none'; frame-ancestors 'self'; sandbox allow-scripts allow-same-origin allow-downloads allow-popups allow-popups-to-escape-sandbox; report-uri /csp-violation;";
         $csp_header .= "style-src 'nonce-$csp_nonce'; script-src-elem 'nonce-$csp_nonce' 'strict-dynamic'; frame-src $document_server_url_csp_encoded;";
 