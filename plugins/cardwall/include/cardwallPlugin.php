Enalean/tuleap

commit 8d6b901c1a673fdc82470c68faeeec22021818a4

chore: Move some files from include/Tracker/.../Admin to include/.../Admin
Part of request #17148 Tracker move to PSR4 😱

There is remaining files as they all have missing namespace and non
pascal case names, making the changes huge

*No functional changes expected*

Change-Id: I365d3e2901f3023d58ed8655780251b5e8108be4

plugins/tracker/include/FormElement/TrackerFormElement.php
@@ -35,8 +35,6 @@ use SimpleXMLElement;
 use Tracker_Artifact_ChangesetValue;
 use Tracker_FormElement_Field_ReadOnly;
 use Tracker_FormElement_Interface;
-use Tracker_FormElement_View_Admin_UpdateSharedVisitor;
-use Tracker_FormElement_View_Admin_UpdateVisitor;
 use Tracker_FormElementFactory;
 use Tracker_IDisplayTrackerLayout;
 use Tracker_IProvideJsonFormatOfMyself;
@@ -54,6 +52,8 @@ use Tuleap\Tracker\FormElement\FieldSpecificProperties\FieldPropertiesRetriever;
 use Tuleap\Tracker\FormElement\FieldSpecificProperties\SaveSpecificFieldProperties;
 use Tuleap\Tracker\FormElement\FieldSpecificProperties\SearchSpecificProperties;
 use Tuleap\Tracker\FormElement\FieldSpecificProperties\SpecificPropertiesWithMappingDuplicator;
+use Tuleap\Tracker\FormElement\View\Admin\UpdateSharedVisitorAdminView;
+use Tuleap\Tracker\FormElement\View\Admin\UpdateVisitorAdminView;
 use Tuleap\Tracker\FormElement\XML\XMLFormElement;
 use Tuleap\Tracker\FormElement\XML\XMLFormElementImpl;
 use Tuleap\Tracker\Tracker;
@@ -473,9 +473,9 @@ abstract class TrackerFormElement extends ProvideFactoryButtonInformation implem
     {
         $allUsedElements = $this->getFormElementFactory()->getUsedFormElementForTracker($this->getTracker());
         if ($this->isTargetSharedField()) {
-            $visitor = new Tracker_FormElement_View_Admin_UpdateSharedVisitor($allUsedElements);
+            $visitor = new UpdateSharedVisitorAdminView($allUsedElements);
         } else {
-            $visitor = new Tracker_FormElement_View_Admin_UpdateVisitor($allUsedElements);
+            $visitor = new UpdateVisitorAdminView($allUsedElements);
             EventManager::instance()->processEvent(
                 self::VIEW_ADMIN_UPDATE_VISITOR,
                 [

plugins/tracker/include/FormElement/View/Admin/FieldAdminView.php
@@ -17,8 +17,11 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+namespace Tuleap\Tracker\FormElement\View\Admin;
 
-class Tracker_FormElement_View_Admin_Field extends Tracker_FormElement_View_Admin
+use Tuleap\Tracker\FormElement\View\TrackerFormElementAdminView;
+
+class FieldAdminView extends TrackerFormElementAdminView
 {
     /**
      * If the formElement has specific properties then this method

plugins/tracker/include/FormElement/View/Admin/SharedAdminView.php
@@ -18,8 +18,12 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+namespace Tuleap\Tracker\FormElement\View\Admin;
 
-class Tracker_FormElement_View_Admin_Shared extends Tracker_FormElement_View_Admin
+use Tracker_FormElement_Shared;
+use Tuleap\Tracker\FormElement\View\TrackerFormElementAdminView;
+
+class SharedAdminView extends TrackerFormElementAdminView
 {
     public function __construct(Tracker_FormElement_Shared $formElement, $allUsedElements)
     {

plugins/tracker/include/FormElement/View/TrackerFormElementAdminView.php
@@ -17,6 +17,10 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+namespace Tuleap\Tracker\FormElement\View;
+
+use Codendi_HTMLPurifier;
+use LogicException;
 use Tuleap\Tracker\FormElement\TrackerFormElement;
 use Tuleap\Tracker\Permission\Fields\ByField\ByFieldController;
 
@@ -25,24 +29,10 @@ use Tuleap\Tracker\Permission\Fields\ByField\ByFieldController;
  *
  * This is the top most element of the hierarchy and correspond to Tracker_FormElement
  */
-
-//phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotPascalCase
-class Tracker_FormElement_View_Admin
+class TrackerFormElementAdminView
 {
-    /**
-     * @var TrackerFormElement
-     */
-    protected $formElement;
-
-    /**
-     * @var
-     */
-    protected $allUsedElements;
-
-    public function __construct(TrackerFormElement $formElement, $allUsedElements)
+    public function __construct(protected TrackerFormElement $formElement, protected $allUsedElements)
     {
-        $this->formElement     = $formElement;
-        $this->allUsedElements = $allUsedElements;
     }
 
     public function fetchTypeNotModifiable()
@@ -106,7 +96,7 @@ class Tracker_FormElement_View_Admin
         $html .= '<p>';
 
         $html .= '<label for="formElement_description">' . dgettext('tuleap-tracker', 'Description') . ':</label>';
-        $html .= '<textarea name="formElement_data[description]" id="formElement_description" cols="40">' .  $hp->purify($this->formElement->description, CODENDI_PURIFIER_CONVERT_HTML)  . '</textarea>';
+        $html .= '<textarea name="formElement_data[description]" id="formElement_description" cols="40">' . $hp->purify($this->formElement->description, CODENDI_PURIFIER_CONVERT_HTML) . '</textarea>';
 
         $html .= '</p>';
         return $html;
@@ -234,11 +224,11 @@ class Tracker_FormElement_View_Admin
     /**
      * Fetch a unique property edit form
      *
-     * @param string $key      The key of the property
-     * @param array  $property The property to display
+     * @param string $key The key of the property
+     * @param array $property The property to display
      *
-     * @see fetchAdminSpecificProperties
      * @return string html
+     * @see fetchAdminSpecificProperties
      */
     protected function fetchAdminSpecificProperty(string $key, array $property): string
     {
@@ -266,7 +256,7 @@ class Tracker_FormElement_View_Admin
                            cols="50" rows="10"
                            name="formElement_data[specific_properties][' . $purifier->purify($key) . ']"
                            id="formElement_properties_' . $key . '">' .
-                           $purifier->purify($property['value']) . '</textarea>';
+                         $purifier->purify($property['value']) . '</textarea>';
                 break;
             case 'rich_text':
                 $html .= '<label data-test="rich-text-specific-properties" for="formElement_properties_' . $purifier->purify($key) . '">' . $purifier->purify($this->formElement->getPropertyLabel($key)) . '</label> ';
@@ -275,7 +265,7 @@ class Tracker_FormElement_View_Admin
                            cols="50" rows="10"
                            name="formElement_data[specific_properties][' . $purifier->purify($key) . ']"
                            id="formElement_properties_' . $purifier->purify($key) . '">' .
-                           $purifier->purify($property['value'], CODENDI_PURIFIER_FULL) . '</textarea>';
+                         $purifier->purify($property['value'], CODENDI_PURIFIER_FULL) . '</textarea>';
                 break;
             case 'radio':
                 $html .= '<label data-test="radio-specific-properties" for="formElement_properties_' . $purifier->purify($key) . '">' . $purifier->purify($this->formElement->getPropertyLabel($key)) . '</label> ';

plugins/agiledashboard/include/AgileDashboard/FormElement/ViewAdminBurnupField.php
@@ -21,9 +21,9 @@
 namespace Tuleap\AgileDashboard\FormElement;
 
 use Override;
-use Tracker_FormElement_View_Admin_Field;
+use Tuleap\Tracker\FormElement\View\Admin\FieldAdminView;
 
-class ViewAdminBurnupField extends Tracker_FormElement_View_Admin_Field
+class ViewAdminBurnupField extends FieldAdminView
 {
     #[Override]
     protected function fetchRequired()

plugins/testmanagement/include/TestManagement/Step/Definition/Field/ViewAdmin.php
@@ -20,9 +20,9 @@
 
 namespace Tuleap\TestManagement\Step\Definition\Field;
 
-use Tracker_FormElement_View_Admin_Field;
+use Tuleap\Tracker\FormElement\View\Admin\FieldAdminView;
 
-class ViewAdmin extends Tracker_FormElement_View_Admin_Field
+class ViewAdmin extends FieldAdminView
 {
     #[\Override]
     protected function fetchRequired()

plugins/testmanagement/include/TestManagement/Step/Execution/Field/ViewAdmin.php
@@ -20,7 +20,7 @@
 
 namespace Tuleap\TestManagement\Step\Execution\Field;
 
-class ViewAdmin extends \Tracker_FormElement_View_Admin_Field
+class ViewAdmin extends \Tuleap\Tracker\FormElement\View\Admin\FieldAdminView
 {
     #[\Override]
     protected function fetchRequired()

plugins/tracker/include/FormElement/Field/ArtifactLink/ArtifactLinkFieldAdmin.php
@@ -23,10 +23,10 @@ declare(strict_types=1);
 namespace Tuleap\Tracker\FormElement\Field\ArtifactLink;
 
 use TemplateRenderer;
-use Tracker_FormElement_View_Admin_Field;
 use Tuleap\Tracker\FormElement\TrackerFormElement;
+use Tuleap\Tracker\FormElement\View\Admin\FieldAdminView;
 
-final class ArtifactLinkFieldAdmin extends Tracker_FormElement_View_Admin_Field
+final class ArtifactLinkFieldAdmin extends FieldAdminView
 {
     public function __construct(
         private readonly TemplateRenderer $renderer,

commit 3e69586c00533ae31b794f1f9199c74d389bd3f3

fix: request #45591 Cannot add a shared field to a tracker
When everything is `mixed` static analysis cannot help (especially not
in what should be called hierarchy hell).

Issue introduced by 8d6b901c1a673fdc82470c68faeeec22021818a4 because
the class properties types of TrackerFormElementAdminView did not match
the ones directly used in SharedAdminView.

Change-Id: Ib5d757e28fb4036970cb4a1424993e230bfa4f85

plugins/cardwall/include/cardwallPlugin.php
@@ -697,7 +697,7 @@ class cardwallPlugin extends Plugin //phpcs:ignore PSR1.Classes.ClassDeclaration
             Tracker_FormElementFactory::instance(),
             new FieldUsedInSemanticObjectChecker(new BackgroundColorDao())
         );
-        $allowed_types = $retriever->retrieveAllowedFieldType($event_retriever->getField());
+        $allowed_types = $retriever->retrieveAllowedFieldType($event_retriever->field);
 
         $event_retriever->setAllowedTypes($allowed_types);
     }

plugins/tracker/include/FormElement/Field/LastUpdateBy/LastUpdateByField.php
@@ -375,10 +375,9 @@ final class LastUpdateByField extends ListField implements Tracker_FormElement_F
     /**
      * Say if this fields suport notifications
      *
-     * @return bool
      */
     #[Override]
-    public function isNotificationsSupported()
+    public function isNotificationsSupported(): bool
     {
         return true;
     }

plugins/tracker/include/FormElement/Field/List/ListField.php
@@ -1425,10 +1425,9 @@ abstract class ListField extends TrackerField implements Tracker_FormElement_Fie
     /**
      * Say if this fields suport notifications
      *
-     * @return bool
      */
     #[Override]
-    public function isNotificationsSupported()
+    public function isNotificationsSupported(): bool
     {
         if ($b = $this->getBind()) {
             return $b->isNotificationsSupported();

plugins/tracker/include/FormElement/Field/SubmittedBy/SubmittedByField.php
@@ -416,10 +416,9 @@ final class SubmittedByField extends ListField implements Tracker_FormElement_Fi
     /**
      * Say if this fields suport notifications
      *
-     * @return bool
      */
     #[Override]
-    public function isNotificationsSupported()
+    public function isNotificationsSupported(): bool
     {
         return true;
     }

plugins/tracker/include/FormElement/Field/TrackerField.php
@@ -926,9 +926,8 @@ abstract class TrackerField extends TrackerFormElement implements Tracker_Report
     /**
      * Say if this fields suport notifications
      *
-     * @return bool
      */
-    public function isNotificationsSupported()
+    public function isNotificationsSupported(): bool
     {
         return false;
     }
@@ -1110,14 +1109,6 @@ abstract class TrackerField extends TrackerFormElement implements Tracker_Report
         }
 
         return true;
-    }
-
-    /**
-     * @return bool true if the field is considered to be required
-     */
-    public function isRequired()
-    {
-        return $this->required;
     }
 
     /**

plugins/tracker/include/FormElement/TrackerFormElement.php
@@ -125,12 +125,7 @@ abstract class TrackerFormElement extends ProvideFactoryButtonInformation implem
      */
     public $label;
 
-    /**
-     * The description
-     *
-     * @var string $description
-     */
-    public $description;
+    public string $description;
 
     /**
      * Is the field used?
@@ -182,7 +177,6 @@ abstract class TrackerFormElement extends ProvideFactoryButtonInformation implem
      * @param int|string $parent_id The id of the parent element
      * @param ?string $name The short name of the field
      * @param string $label The label of the element
-     * @param string $description The description of the element
      * @param int|bool|null|string $use_it Is the element used?
      * @param string $scope The scope of the plugin 'S' | 'P'
      * @param int|bool|null|string $required Is the element required? Todo: move this in field?
@@ -191,14 +185,14 @@ abstract class TrackerFormElement extends ProvideFactoryButtonInformation implem
      *
      * @return void
      */
-    public function __construct($id, $tracker_id, $parent_id, $name, $label, $description, $use_it, $scope, $required, $notifications, $rank, ?TrackerFormElement $original_field = null)
+    public function __construct($id, $tracker_id, $parent_id, $name, $label, ?string $description, $use_it, $scope, $required, $notifications, $rank, ?TrackerFormElement $original_field = null)
     {
         $this->id          = (int) $id;
         $this->tracker_id  = (int) $tracker_id;
         $this->parent_id   = (int) $parent_id;
         $this->name        = $name === null ? '' : trim($name);
         $this->label       = $label;
-        $this->description = $description;
+        $this->description = (string) $description;
         $this->use_it      = false;
         if (is_bool($use_it)) {
             $this->use_it = $use_it;
@@ -258,6 +252,11 @@ abstract class TrackerFormElement extends ProvideFactoryButtonInformation implem
             'permissions'         => $this->getPermissionsByUgroupId(),
             'specific_properties' => $this->getFlattenPropertiesValues(),
         ];
+    }
+
+    public function isRequired(): bool
+    {
+        return $this->required;
     }
 
     public function isCSVImportable(): bool

plugins/tracker/include/FormElement/View/Admin/Field/ComputedFieldAdminView.php
@@ -85,9 +85,9 @@ class ComputedFieldAdminView extends FieldAdminView
         return $html;
     }
 
-    private function canUpgrade()
+    private function canUpgrade(): bool
     {
-        return $this->formElement->name === $this->formElement->getProperty('target_field_name');
+        return $this->formElement->getName() === $this->formElement->getProperty('target_field_name');
     }
 
     private function isMigrated()

plugins/tracker/include/FormElement/View/Admin/Field/SelectboxFieldAdminView.php
@@ -22,6 +22,7 @@ namespace Tuleap\Tracker\FormElement\View\Admin\Field;
 use Codendi_HTMLPurifier;
 use EventManager;
 use Tuleap\Tracker\Events\AllowedFieldTypeChangesRetriever;
+use Tuleap\Tracker\FormElement\Field\TrackerField;
 use WorkflowFactory;
 
 class SelectboxFieldAdminView extends ListFieldAdminView
@@ -37,7 +38,7 @@ class SelectboxFieldAdminView extends ListFieldAdminView
 
         //do not change from SB to MSB if the field is used to define the workflow
         $wf = WorkflowFactory::instance();
-        if (! $wf->isWorkflowField($this->formElement)) {
+        if ($this->formElement instanceof TrackerField && ! $wf->isWorkflowField($this->formElement)) {
             $html .= ' (' . dgettext('tuleap-tracker', 'Switch to:') . ' ';
 
             $change_links = [];
@@ -71,10 +72,12 @@ class SelectboxFieldAdminView extends ListFieldAdminView
         return $html;
     }
 
-    public function getAvailableTypes()
+    public function getAvailableTypes(): array
     {
-        $event = new AllowedFieldTypeChangesRetriever();
-        $event->setField($this->formElement);
+        if (! $this->formElement instanceof TrackerField) {
+            return [];
+        }
+        $event = new AllowedFieldTypeChangesRetriever($this->formElement);
 
         EventManager::instance()->processEvent($event);
 

plugins/tracker/include/FormElement/View/Admin/FieldAdminView.php
@@ -19,6 +19,7 @@
 
 namespace Tuleap\Tracker\FormElement\View\Admin;
 
+use Tuleap\Tracker\FormElement\Field\TrackerField;
 use Tuleap\Tracker\FormElement\View\TrackerFormElementAdminView;
 
 class FieldAdminView extends TrackerFormElementAdminView
@@ -55,13 +56,11 @@ class FieldAdminView extends TrackerFormElementAdminView
 
     /**
      * Fetch the "notifications" part of field admin
-     *
-     * @return string html
      */
-    protected function fetchNotifications()
+    protected function fetchNotifications(): string
     {
         $html = '';
-        if ($this->formElement->isNotificationsSupported()) {
+        if ($this->formElement instanceof TrackerField && $this->formElement->isNotificationsSupported()) {
             $html .= ' <p>';
             $html .= ' <label for="formElement_notifications" class="checkbox">';
             $html .= '<input type="hidden" name="formElement_data[notifications]" value="0" />';
@@ -85,7 +84,7 @@ class FieldAdminView extends TrackerFormElementAdminView
         $html .= '<p>';
         $html .= '<input type="hidden" name="formElement_data[required]" value="0" />';
         $html .= '<label class="checkbox">';
-        $html .= '<input type="checkbox" name="formElement_data[required]" id="formElement_required" value="1" ' . ($this->formElement->required ? 'checked="checked"' : '') . '" />';
+        $html .= '<input type="checkbox" name="formElement_data[required]" id="formElement_required" value="1" ' . ($this->formElement->isRequired() ? 'checked="checked"' : '') . '" />';
         $html .= dgettext('tuleap-tracker', 'Required');
         $html .= '</label>';
         $html .= '</p>';

plugins/tracker/include/FormElement/View/Admin/SharedAdminView.php
@@ -25,9 +25,8 @@ use Tuleap\Tracker\FormElement\View\TrackerFormElementAdminView;
 
 class SharedAdminView extends TrackerFormElementAdminView
 {
-    public function __construct(Tracker_FormElement_Shared $formElement, $allUsedElements)
+    public function __construct(Tracker_FormElement_Shared $formElement, array $allUsedElements)
     {
-        $this->formElement     = $formElement;
-        $this->allUsedElements = $allUsedElements;
+        parent::__construct($formElement, $allUsedElements);
     }
 }

plugins/tracker/include/FormElement/View/TrackerFormElementAdminView.php
@@ -21,6 +21,7 @@ namespace Tuleap\Tracker\FormElement\View;
 
 use Codendi_HTMLPurifier;
 use LogicException;
+use Tracker_FormElement_Shared;
 use Tuleap\Tracker\FormElement\TrackerFormElement;
 use Tuleap\Tracker\Permission\Fields\ByField\ByFieldController;
 
@@ -31,7 +32,7 @@ use Tuleap\Tracker\Permission\Fields\ByField\ByFieldController;
  */
 class TrackerFormElementAdminView
 {
-    public function __construct(protected TrackerFormElement $formElement, protected $allUsedElements)
+    public function __construct(protected(set) readonly Tracker_FormElement_Shared|TrackerFormElement $formElement, private readonly array $allUsedElements)
     {
     }
 
@@ -96,7 +97,7 @@ class TrackerFormElementAdminView
         $html .= '<p>';
 
         $html .= '<label for="formElement_description">' . dgettext('tuleap-tracker', 'Description') . ':</label>';
-        $html .= '<textarea name="formElement_data[description]" id="formElement_description" cols="40">' . $hp->purify($this->formElement->description, CODENDI_PURIFIER_CONVERT_HTML) . '</textarea>';
+        $html .= '<textarea name="formElement_data[description]" id="formElement_description" cols="40">' . $hp->purify($this->formElement->getDescription(), CODENDI_PURIFIER_CONVERT_HTML) . '</textarea>';
 
         $html .= '</p>';
         return $html;
@@ -142,7 +143,7 @@ class TrackerFormElementAdminView
         $html  = '';
         $html .= '<p>';
         $html .= '<label>' . dgettext('tuleap-tracker', 'Description') . '</label>';
-        $html .= $hp->purify($this->formElement->description, CODENDI_PURIFIER_CONVERT_HTML);
+        $html .= $hp->purify($this->formElement->getDescription(), CODENDI_PURIFIER_CONVERT_HTML);
         $html .= '</p>';
         return $html;
     }
@@ -154,6 +155,9 @@ class TrackerFormElementAdminView
 
     public function fetchCustomHelpForShared(): string
     {
+        if (! $this->formElement instanceof TrackerFormElement) {
+            return '';
+        }
         $hp                  = Codendi_HTMLPurifier::instance();
         $originalTrackerName = $this->formElement->getOriginalTracker()->getName();
         $originalProjectName = $this->formElement->getOriginalProject()->getPublicName();
@@ -184,8 +188,8 @@ class TrackerFormElementAdminView
             $items[] = $field->getRankSelectboxDefinition();
         }
         $html .= $GLOBALS['HTML']->selectRank(
-            $this->formElement->id,
-            $this->formElement->rank,
+            $this->formElement->getId(),
+            $this->formElement->getRank(),
             $items,
             [
                 'id'   => 'formElement_rank',
@@ -351,7 +355,7 @@ class TrackerFormElementAdminView
         }
         $html  = '';
         $html .= '<p>';
-        $html .= '<a href="' . ByFieldController::getUrl($tracker) . '?selected_id=' . $this->formElement->id . '">';
+        $html .= '<a href="' . ByFieldController::getUrl($tracker) . '?selected_id=' . $this->formElement->getId() . '">';
         $html .= $GLOBALS['HTML']->getImage('ic/lock-small.png', [
             'style' => 'vertical-align:middle;',
         ]);
@@ -370,7 +374,7 @@ class TrackerFormElementAdminView
             $trackers = [];
             foreach ($fields as $field) {
                 $tracker = $field->getTracker();
-                if (! $tracker->isDeleted() && $tracker->getProject() && ! $tracker->getProject()->isDeleted()) {
+                if ($tracker->isDeleted() === false && $tracker->getProject()->isDeleted() === false) {
                     $trackers[$tracker->getId()] = '<a href="' . TRACKER_BASE_URL . '/?tracker=' . $hp->purify(urlencode((string) $tracker->getId())) . '&func=admin-formElements">' . $hp->purify($tracker->getName()) . ' (' . $hp->purify($tracker->getProject()->getPublicName()) . ')</a>';
                 }
             }

plugins/tracker/include/Tracker.php
@@ -1867,11 +1867,9 @@ class Tracker implements Tracker_Dispatchable_Interface
     /**
      * Test if tracker is deleted
      *
-     * @return bool
-     *
      * @psalm-mutation-free
      */
-    public function isDeleted()
+    public function isDeleted(): bool
     {
         return ($this->deletion_date != '');
     }

plugins/tracker/include/Tracker/Events/AllowedFieldTypeChangesRetriever.php
@@ -21,34 +21,19 @@
 namespace Tuleap\Tracker\Events;
 
 use Tuleap\Event\Dispatchable;
-use Tuleap\Tracker\FormElement\TrackerFormElement;
+use Tuleap\Tracker\FormElement\Field\TrackerField;
 
 class AllowedFieldTypeChangesRetriever implements Dispatchable
 {
     public const string NAME = 'semanticAllowedFieldTypeRetriever';
 
     /**
-     * @var \Tuleap\Tracker\FormElement\TrackerFormElement
-     */
-    private $field;
-
-    /**
      * @var array
      */
     private $allowed_types = [];
 
-
-    /**
-     * @return \Tuleap\Tracker\FormElement\TrackerFormElement
-     */
-    public function getField()
+    public function __construct(public readonly TrackerField $field)
     {
-        return $this->field;
-    }
-
-    public function setField(TrackerFormElement $field)
-    {
-        $this->field = $field;
     }
 
     /**

plugins/tracker/include/Tracker/FormElement/Tracker_FormElement_Shared.php
@@ -28,6 +28,26 @@ class Tracker_FormElement_Shared extends ProvideFactoryButtonInformation
      */
     private $tracker;
 
+    public function __construct(
+        private readonly int $id,
+        private int $tracker_id,
+        $parent_id,
+        private readonly ?string $name,
+        private readonly string $label,
+        private readonly ?string $description,
+        $use_it,
+        $scope,
+        $required,
+        $notifications,
+        private readonly ?int $rank,
+    ) {
+        $this->parent_id     = $parent_id;
+        $this->use_it        = $use_it;
+        $this->scope         = $scope;
+        $this->required      = $required;
+        $this->notifications = $notifications;
+    }
+
     #[\Override]
     public static function getFactoryLabel()
     {
@@ -79,23 +99,69 @@ class Tracker_FormElement_Shared extends ProvideFactoryButtonInformation
         $visitor->visit($this);
     }
 
-    public function __construct($id, private int $tracker_id, $parent_id, $name, $label, $description, $use_it, $scope, $required, $notifications, $rank)
-    {
-        $this->id            = $id;
-        $this->parent_id     = $parent_id;
-        $this->name          = $name;
-        $this->label         = $label;
-        $this->description   = $description;
-        $this->use_it        = $use_it;
-        $this->scope         = $scope;
-        $this->required      = $required;
-        $this->notifications = $notifications;
-        $this->rank          = $rank;
-    }
-
     #[\Override]
     public function getTrackerId(): int
     {
         return $this->tracker_id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getPropertyLabel(): string
+    {
+        return '';
+    }
+
+    public function getProperty(): null
+    {
+        return null;
+    }
+
+    public function getProperties(): array
+    {
+        return [];
+    }
+
+    public function getRank(): int
+    {
+        return (int) $this->rank;
+    }
+
+    public function getDescription(): string
+    {
+        return (string) $this->description;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getName(): string
+    {
+        return (string) $this->name;
+    }
+
+    public function isNotificationsSupported(): false
+    {
+        return false;
+    }
+
+    public function isRequired(): false
+    {
+        return false;
+    }
+
+    public function formatDate(): string
+    {
+        return '';
+    }
+
+    public function getSharedTargets(): array
+    {
+        return [];
     }
 }

src/common/Project/Group.php
@@ -152,7 +152,7 @@ class Group //phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
         return $this->getStatus() == 'A';
     }
 
-    public function isDeleted()
+    public function isDeleted(): bool
     {
         return $this->getStatus() == 'D';
     }