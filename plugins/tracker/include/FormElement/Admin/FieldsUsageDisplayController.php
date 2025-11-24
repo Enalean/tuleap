507632d9715b426b6dcbcff690951c3a0bffe39c
Nicolas Terray <nicolas.terray@enalean.com>
2025-11-24 10:32:33 +0100

    feat: introduce new route for fields administration
    The current user interface for managing fields relies on several legacy
    JavaScript files built on PrototypeJS/Scriptaculous and Bootstrap.
    On the backend side, it also uses a polymorphic call to
    `Tuleap\Tracker\FormElement\TrackerFormElement::fetchAdmin`.
    
    Refactoring all of this at once would result in a large, hard-to-review
    change set. To enable an incremental migration, this commit introduces
    a new route dedicated to the future fields administration UI.
    
    This new UI requires dynamic interactions and will be implemented using
    a modern Vue application. For now, the route remains hidden from end
    users until the migration is complete.
    
    You can test it at: `/plugins/tracker/fields/<tracker-id>/`
    (same URL format as for notifications).
    
    Part of request #39759: Get Rid of Flaming Parrot in Trackers
    
    Change-Id: Ibcaddaa8690a3919d25680ee36d6d49ce93e446f

---
+++ plugins/tracker/include/FormElement/Admin/FieldsUsageDisplayController.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace Tuleap\Tracker\FormElement\Admin;
+
+use Tracker_IDisplayTrackerLayout;
+use Tuleap\Layout\BaseLayout;
+use Tuleap\Layout\IncludeViteAssets;
+use Tuleap\Request\DispatchableWithBurningParrot;
+use Tuleap\Request\DispatchableWithRequest;
+use Tuleap\Request\NotFoundException;
+use Tuleap\Tracker\RetrieveTracker;
+use Tuleap\Tracker\Tracker;
+
+final readonly class FieldsUsageDisplayController implements DispatchableWithRequest, DispatchableWithBurningParrot
+{
+    public function __construct(
+        private RetrieveTracker $tracker_factory,
+        private Tracker_IDisplayTrackerLayout $layout,
+        private \TemplateRendererFactory $renderer_factory,
+    ) {
+    }
+
+    #[\Override]
+    public function process(\Tuleap\HTTPRequest $request, BaseLayout $layout, array $variables): void
+    {
+        $tracker = $this->getTrackerFromTrackerID((int) $variables['id']);
+
+        $current_user = $request->getCurrentUser();
+        if (! $tracker->userIsAdmin($current_user)) {
+            throw new NotFoundException(dgettext('tuleap-tracker', 'Access denied. You don\'t have permissions to perform this action.'));
+        }
+
+        $layout->addJavascriptAsset(
+            new \Tuleap\Layout\JavascriptViteAsset(
+                new IncludeViteAssets(
+                    __DIR__ . '/../../../scripts/tracker-admin-fields/frontend-assets',
+                    '/assets/trackers/tracker-admin-fields',
+                ),
+                'src/tracker-admin-fields.ts',
+            ),
+        );
+
+        $tracker->displayAdminItemHeaderBurningParrot($this->layout, 'editformElements', dgettext('tuleap-tracker', 'Manage Field Usage'));
+        $this->renderer_factory
+            ->getRenderer(__DIR__)
+            ->renderToPage('fields-usage', FieldsUsageDisplayPresenter::build($tracker));
+        $tracker->displayFooter($this->layout);
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    private function getTrackerFromTrackerId(int $id): Tracker
+    {
+        $tracker = $this->tracker_factory->getTrackerById($id);
+        if ($tracker === null) {
+            throw new NotFoundException(dgettext('tuleap-tracker', 'That tracker does not exist.'));
+        }
+
+        return $tracker;
+    }
+}
---
+++ plugins/tracker/scripts/tracker-admin-fields/src/tracker-admin-fields.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { createApp } from "vue";
+import { createGettext } from "vue3-gettext";
+import { getPOFileFromLocaleWithoutExtension, initVueGettext } from "@tuleap/vue3-gettext-init";
+import FieldsUsage from "./components/FieldsUsage.vue";
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const mount_point = document.getElementById("tracker-admin-fields-usage-mount-point");
+    if (mount_point === null) {
+        return;
+    }
+
+    createApp(FieldsUsage)
+        .use(
+            /** @ts-expect-error vue3-gettext-init is tested with Vue 3.4, but here we use Vue 3.5 */
+            await initVueGettext(createGettext, (locale) => {
+                return import(`../po/${getPOFileFromLocaleWithoutExtension(locale)}.po`);
+            }),
+        )
+        .mount(mount_point);
+});
