Enalean/tuleap

commit a1d954ca8f80151ccd07d9bea10d55df5bc66722

chore: Re-encrypt the Git Jenkins tokens with the new cryptography API
No functional changes.

A minimal version of the decryption code of the legacy crypto API has
been integrated into the ForgeUpgrade namespace. The one in the
production/runtime code will be removed once everything has been
migrated out of it.

Part of request #44081 Internal symmetric cryptographic API should be
resistant against Invisible Salamanders / Confused Deputies attacks

Change-Id: I31933f10f9026a2a71614edc8ee69a889b419319

src/common/ForgeUpgrade/Bucket/BucketDb.php
@@ -23,7 +23,12 @@ namespace Tuleap\ForgeUpgrade\Bucket;
 
 use PDO;
 use Psr\Log\LoggerInterface;
+use Tuleap\Cryptography\ConcealedString;
+use Tuleap\Cryptography\KeyFactoryFromFileSystem;
+use Tuleap\Cryptography\Symmetric\EncryptionAdditionalData;
+use Tuleap\Cryptography\Symmetric\SymmetricCrypto;
 use Tuleap\DB\DatabaseUUIDFactory;
+use Tuleap\ForgeUpgrade\LegacyCryptography2025\EncryptionKey;
 
 /**
  * Wrap access to the DB and provide a set of convenient tools to write
@@ -254,5 +259,77 @@ class BucketDb
         $this->dbh->commit();
 
         $this->dbh->exec('ALTER TABLE ' . $table_name . ' MODIFY COLUMN ' . $uuid_column_name . ' BINARY(16) NOT NULL');
+    }
+
+    /**
+     * @param literal-string $table_name
+     * @param literal-string $uuid_column_name
+     * @param literal-string $encrypted_data_column_name
+     */
+    public function reencrypt2025ContentWithTheCurrentCryptographyAPI(
+        string $table_name,
+        string $uuid_column_name,
+        string $encrypted_data_column_name,
+    ): void {
+        if ($table_name === '' || $uuid_column_name === '' || $encrypted_data_column_name === '') {
+            throw new \RuntimeException('table_name, uuid_column_name and encrypted_data_column_name must not be empty');
+        }
+        $this->dbh->beginTransaction();
+        $rows = $this->dbh->query(
+            sprintf(
+                'SELECT %s, %s FROM %s WHERE %s IS NOT NULL FOR UPDATE',
+                $uuid_column_name,
+                $encrypted_data_column_name,
+                $table_name,
+                $encrypted_data_column_name
+            )
+        )->fetchAll();
+
+        if (count($rows) === 0) {
+            $this->dbh->commit();
+            return;
+        }
+
+        $legacy_encryption_key = EncryptionKey::build();
+        if ($legacy_encryption_key === null) {
+            $this->log->warning('Not able to find the encryption key, not re-encrypting values of ' . $table_name . '. Please investigate, it will cause issues later on.');
+            $this->dbh->commit();
+            return;
+        }
+
+        $update_statement = $this->dbh->prepare('UPDATE ' . $table_name . ' SET ' . $encrypted_data_column_name . ' = ? WHERE ' . $uuid_column_name . ' = ?');
+
+        $current_encryption_key = (new KeyFactoryFromFileSystem())->getEncryptionKey();
+
+        foreach ($rows as $row) {
+            $uuid = $row[$uuid_column_name];
+
+            try {
+                $decrypted_data = $legacy_encryption_key->decrypt($row[$encrypted_data_column_name]);
+            } catch (\RuntimeException $exception) {
+                $this->log->warning(
+                    sprintf(
+                        'Could not decrypt row %s of %s, skipping. Please investigate, it will cause issues later on.',
+                        $this->uuid_factory->buildUUIDFromBytesData($uuid)->toString(),
+                        $table_name
+                    ),
+                    ['exception' => $exception]
+                );
+                continue;
+            }
+
+            $update_statement->execute([
+                SymmetricCrypto::encrypt(
+                    new ConcealedString(''),
+                    new EncryptionAdditionalData($table_name, $encrypted_data_column_name, $uuid),
+                    $current_encryption_key
+                ),
+                $uuid,
+            ]);
+
+            \sodium_memzero($decrypted_data);
+        }
+
+        $this->dbh->commit();
     }
 }

plugins/hudson_git/db/mysql/updates/2025/202510031545_encrypt_tokens_with_new_api.php
@@ -20,26 +20,25 @@
 
 declare(strict_types=1);
 
-namespace Tuleap\DB;
-
-use Tuleap\Option\Option;
-
-interface DatabaseUUIDFactory
+// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace,Squiz.Classes.ValidClassName.NotPascalCase
+final class b202510031545_encrypt_tokens_with_new_api extends \Tuleap\ForgeUpgrade\Bucket
 {
-    /**
-     * @return non-empty-string
-     */
-    public function buildUUIDBytes(): string;
+    public function description(): string
+    {
+        return 'Re-encrypt tokens using the current Tuleap cryptography API';
+    }
 
-    /**
-     * @return non-empty-string
-     */
-    public function buildUUIDBytesFromTime(\DateTimeInterface $time): string;
-
-    public function buildUUIDFromBytesData(string $bytes): UUID;
-
-    /**
-     * @return Option<UUID>
-     */
-    public function buildUUIDFromHexadecimalString(string $string): Option;
+    public function up(): void
+    {
+        $this->api->reencrypt2025ContentWithTheCurrentCryptographyAPI(
+            'plugin_hudson_git_project_server',
+            'id',
+            'encrypted_token'
+        );
+        $this->api->reencrypt2025ContentWithTheCurrentCryptographyAPI(
+            'plugin_hudson_git_server',
+            'repository_id',
+            'encrypted_token'
+        );
+    }
 }

plugins/hudson_git/include/HudsonGit/Git/Administration/JenkinsServer.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace Tuleap\HudsonGit\Git\Administration;
 
 use Project;
+use Tuleap\Cryptography\ConcealedString;
 use Tuleap\DB\UUID;
 
 class JenkinsServer implements \JsonSerializable
@@ -42,8 +43,7 @@ class JenkinsServer implements \JsonSerializable
     public function __construct(
         public readonly UUID $id,
         string $jenkins_server_url,
-        /** @psalm-readonly */
-        private ?string $encrypted_token,
+        private readonly ?ConcealedString $token,
         Project $project,
     ) {
         $this->jenkins_server_url = $jenkins_server_url;
@@ -61,9 +61,9 @@ class JenkinsServer implements \JsonSerializable
     /**
      * @psalm-mutation-free
      */
-    public function getEncryptedToken(): ?string
+    public function getToken(): ?ConcealedString
     {
-        return $this->encrypted_token;
+        return $this->token;
     }
 
     /**

plugins/hudson_git/include/HudsonGit/Git/Administration/JenkinsServerAdder.php
@@ -24,26 +24,12 @@ namespace Tuleap\HudsonGit\Git\Administration;
 
 use Project;
 use Tuleap\Cryptography\ConcealedString;
-use Tuleap\Cryptography\SymmetricLegacy2025\EncryptionKey;
-use Tuleap\Cryptography\SymmetricLegacy2025\SymmetricCrypto;
 use Valid_HTTPURI;
 
-class JenkinsServerAdder
+readonly class JenkinsServerAdder
 {
-    /**
-     * @var JenkinsServerDao
-     */
-    private $jenkins_server_dao;
-
-    /**
-     * @var Valid_HTTPURI
-     */
-    private $valid_HTTPURI;
-
-    public function __construct(JenkinsServerDao $jenkins_server_dao, Valid_HTTPURI $valid_HTTPURI, private EncryptionKey $encryption_key)
+    public function __construct(private JenkinsServerDao $jenkins_server_dao, private Valid_HTTPURI $valid_HTTPURI)
     {
-        $this->jenkins_server_dao = $jenkins_server_dao;
-        $this->valid_HTTPURI      = $valid_HTTPURI;
     }
 
     /**
@@ -68,15 +54,10 @@ class JenkinsServerAdder
             throw new JenkinsServerAlreadyDefinedException();
         }
 
-        $encrypted_token = null;
-        if ($token !== null) {
-            $encrypted_token = SymmetricCrypto::encrypt($token, $this->encryption_key);
-        }
-
         $this->jenkins_server_dao->addJenkinsServer(
             $project_id,
             $jenkins_server_url,
-            $encrypted_token
+            $token
         );
     }
 }

plugins/hudson_git/include/HudsonGit/Git/Administration/JenkinsServerDao.php
@@ -22,17 +22,28 @@ declare(strict_types=1);
 
 namespace Tuleap\HudsonGit\Git\Administration;
 
+use Tuleap\Cryptography\ConcealedString;
+use Tuleap\Cryptography\Symmetric\EncryptionAdditionalData;
 use Tuleap\DB\DataAccessObject;
 use Tuleap\DB\UUID;
 
 class JenkinsServerDao extends DataAccessObject
 {
-    public function addJenkinsServer(int $project_id, string $jenkins_server_url, ?string $encrypted_token): void
+    public function addJenkinsServer(int $project_id, string $jenkins_server_url, ?ConcealedString $token): void
     {
+        $uuid = $this->uuid_factory->buildUUIDBytes();
+
+        $encrypted_token = null;
+        if ($token !== null) {
+            $encrypted_token = $this->encryptDataToStoreInATableRow(
+                $token,
+                $this->getTokenEncryptionAdditionalData($uuid),
+            );
+        }
         $this->getDB()->insert(
             'plugin_hudson_git_project_server',
             [
-                'id' => $this->uuid_factory->buildUUIDBytes(),
+                'id' => $uuid,
                 'project_id' => $project_id,
                 'jenkins_server_url' => $jenkins_server_url,
                 'encrypted_token' => $encrypted_token,
@@ -41,7 +52,7 @@ class JenkinsServerDao extends DataAccessObject
     }
 
     /**
-     * @return array{id: UUID, jenkins_server_url: string, encrypted_token: string|null}[]
+     * @return array{id: UUID, jenkins_server_url: string, token: ConcealedString|null}[]
      */
     public function getJenkinsServerOfProject(int $project_id): array
     {
@@ -53,8 +64,18 @@ class JenkinsServerDao extends DataAccessObject
         $rows   = [];
 
         foreach ($result as $row) {
-            $row['id'] = $this->uuid_factory->buildUUIDFromBytesData($row['id']);
-            $rows[]    = $row;
+            $uuid         = $this->uuid_factory->buildUUIDFromBytesData($row['id']);
+            $row['id']    = $uuid;
+            $row['token'] = null;
+            if ($row['encrypted_token'] !== null) {
+                $row['token'] = $this->decryptDataStoredInATableRow(
+                    $row['encrypted_token'],
+                    $this->getTokenEncryptionAdditionalData($uuid->getBytes())
+                );
+            }
+            unset($row['encrypted_token']);
+
+            $rows[] = $row;
         }
 
         return $rows;
@@ -117,6 +138,18 @@ class JenkinsServerDao extends DataAccessObject
                     'id' => $uuid->getBytes(),
                 ]
             ),
+        );
+    }
+
+    /**
+     * @param non-empty-string $uuid
+     */
+    private function getTokenEncryptionAdditionalData(string $uuid): EncryptionAdditionalData
+    {
+        return new EncryptionAdditionalData(
+            'plugin_hudson_git_project_server',
+            'encrypted_token',
+            $uuid
         );
     }
 }

plugins/hudson_git/include/HudsonGit/Git/Administration/JenkinsServerFactory.php
@@ -55,7 +55,7 @@ class JenkinsServerFactory
             $servers[] = new JenkinsServer(
                 $jenkins_server['id'],
                 $jenkins_server['jenkins_server_url'],
-                $jenkins_server['encrypted_token'],
+                $jenkins_server['token'],
                 $project
             );
         }

plugins/hudson_git/include/HudsonGit/Git/Administration/XML/XMLExporter.php
@@ -25,8 +25,6 @@ namespace Tuleap\HudsonGit\Git\Administration\XML;
 use Project;
 use Psr\Log\LoggerInterface;
 use SimpleXMLElement;
-use Tuleap\Cryptography\SymmetricLegacy2025\EncryptionKey;
-use Tuleap\Cryptography\SymmetricLegacy2025\SymmetricCrypto;
 use Tuleap\HudsonGit\Git\Administration\JenkinsServerFactory;
 
 class XMLExporter
@@ -41,7 +39,7 @@ class XMLExporter
      */
     private $logger;
 
-    public function __construct(JenkinsServerFactory $jenkins_server_factory, LoggerInterface $logger, private EncryptionKey $encryption_key)
+    public function __construct(JenkinsServerFactory $jenkins_server_factory, LoggerInterface $logger)
     {
         $this->jenkins_server_factory = $jenkins_server_factory;
         $this->logger                 = $logger;
@@ -60,9 +58,9 @@ class XMLExporter
             $this->logger->info('Exporting project jenkins server: ' . $jenkins_server->getServerURL());
             $jenkins_server_node = $jenkins_servers_admin_node->addChild('jenkins-server');
             $jenkins_server_node->addAttribute('url', $jenkins_server->getServerURL());
-            $encrypted_token = $jenkins_server->getEncryptedToken();
-            if ($encrypted_token !== null) {
-                $jenkins_server_node->addAttribute('jenkins_token', SymmetricCrypto::decrypt($encrypted_token, $this->encryption_key)->getString());
+            $token = $jenkins_server->getToken();
+            if ($token !== null) {
+                $jenkins_server_node->addAttribute('jenkins_token', $token->getString());
             }
         }
     }

plugins/hudson_git/include/HudsonGit/Hook/HookController.php
@@ -23,8 +23,6 @@ namespace Tuleap\HudsonGit\Hook;
 use GitRepository;
 use GitRepositoryFactory;
 use Tuleap\Cryptography\ConcealedString;
-use Tuleap\Cryptography\SymmetricLegacy2025\EncryptionKey;
-use Tuleap\Cryptography\SymmetricLegacy2025\SymmetricCrypto;
 use Tuleap\Git\GitViews\RepoManagement\Pane\Hooks;
 use Codendi_Request;
 use Feedback;
@@ -64,7 +62,6 @@ class HookController
         HookDao $dao,
         CSRFSynchronizerToken $csrf,
         Valid_HTTPURI $valid_HTTPURI,
-        private EncryptionKey $encryption_key,
     ) {
         $this->request                = $request;
         $this->git_repository_factory = $git_repository_factory;
@@ -99,13 +96,8 @@ class HookController
         }
         $token = new ConcealedString($cleartext_token);
         sodium_memzero($cleartext_token);
-        $encrypted_token = null;
-        if ($token !== null) {
-            $encrypted_token = SymmetricCrypto::encrypt($token, $this->encryption_key);
-        }
 
-
-        $this->dao->save($repository->getId(), $jenkins_server, $encrypted_token, $is_commit_reference_needed);
+        $this->dao->save($repository->getId(), $jenkins_server, $token, $is_commit_reference_needed);
         $GLOBALS['Response']->addFeedback(Feedback::INFO, dgettext('tuleap-hudson_git', 'Jenkins webhook successfully saved'));
         $GLOBALS['Response']->redirect($this->getRedirectUrl($repository));
     }

commit 7c68c0125d140cd2ce170f42a64573133a24b7d8

fix: Re-encrypting Git Jenkins token must not always set an empty string
Issue introduced in a1d954ca8f80151ccd07d9bea10d55df5bc66722.
Instances having already migrated to it will need to reset the
credentials.

Part of request #44081 Internal symmetric cryptographic API should be
resistant against Invisible Salamanders / Confused Deputies attacks

Change-Id: I6d98bf34d5bf9ff7de275d2e4d1a552a0315b588

src/common/ForgeUpgrade/Bucket/BucketDb.php
@@ -320,7 +320,7 @@ class BucketDb
 
             $update_statement->execute([
                 SymmetricCrypto::encrypt(
-                    new ConcealedString(''),
+                    new ConcealedString($decrypted_data),
                     new EncryptionAdditionalData($table_name, $encrypted_data_column_name, $uuid),
                     $current_encryption_key
                 ),