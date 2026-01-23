24616f3053cabeaf28714b8edafd6883ebf0a633
Thomas Gerbet <thomas.gerbet@enalean.com>
2026-01-23 16:28:51 +0100

    fix: Fix incorrect paths during Nix cache setup
    Followup to 787ddc63a3d1f50e4190c04e07e6ee150e8e7278
    
    Part of request #46397 Use Nix cache provided by Enalean instead of Cachix
    
    Change-Id: I9929657daeb2499e5bead3aca4d52e491de6ac84

--- tools/utils/nix/setup-cache.sh
+++ tools/utils/nix/setup-cache.sh
@@ -21,15 +21,15 @@ if [ -z "${AUTH_TOKEN:-}" ]; then
 fi
 
 # Create netrc file consumed by Nix
-echo "machine nix-cache.enalean.com" > ~/.config/nix/netrc-nix-cache-enalean
-echo "password ${AUTH_TOKEN}" >> ~/.config/nix/netrc-nix-cache-enalean
+echo "machine nix-cache.enalean.com" > ~/.config/nix/netrc
+echo "password ${AUTH_TOKEN}" >> ~/.config/nix/netrc
 
 # Add netrc file to Nix configuration
 echo "netrc-file = $(realpath ~/.config/nix/netrc)" >> ~/.config/nix/nix.conf
 
 # Setup credential consumed by Attic
 mkdir -p ~/.config/attic/
-echo 'default-server = "enalean"' > ~/.config/attic/attic.toml
-echo '[servers.enalean]' >> ~/.config/attic/attic.toml
-echo 'endpoint = "https://nix-cache.enalean.com"' >> ~/.config/attic/attic.toml
-echo "token = \"${AUTH_TOKEN}\"" >> ~/.config/attic/attic.toml
+echo 'default-server = "enalean"' > ~/.config/attic/config.toml
+echo '[servers.enalean]' >> ~/.config/attic/config.toml
+echo 'endpoint = "https://nix-cache.enalean.com"' >> ~/.config/attic/config.toml
+echo "token = \"${AUTH_TOKEN}\"" >> ~/.config/attic/config.toml
