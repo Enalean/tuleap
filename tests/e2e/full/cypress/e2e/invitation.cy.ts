0dea1df2c294758c5c35cb11612b4574db323c06
Marie Ange Garnier <marie-ange.garnier@enalean.com>
2026-01-29 10:46:51 +0100

    Invite budies in a project should be covered by e2e tests
    part of request #44418 finish convert TTM test to e2e for invite people to project
    
    converts test_case #31075
    
    Note:
    I'm not fan of the regexp to retrieve the url to connect in mail body, but I did not have a better idea to know the url and its token
    
    I had to update the test in core notification to clean the user approval back
    it introduces test dependency and running the whole test suite will require to provide the purpose filed
    runnning only the invitation file would not needed the purpose field
    
    Change-Id: I83359dcee22cbf5eb017b90dcb4e27c050907d1c

--- tests/e2e/full/cypress/e2e/invitation.cy.ts
+++ tests/e2e/full/cypress/e2e/invitation.cy.ts
@@ -17,15 +17,17 @@
  * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
  */
 
+import * as quotedPrintable from "quoted-printable";
 import { getAntiCollisionNamePart } from "@tuleap/cypress-utilities-support";
 
 describe("Invitations", () => {
     describe("In a project", () => {
-        let project_name: string, invitee_email: string;
+        let anti_collision: string, project_name: string, invitee_email: string;
 
         before(() => {
-            project_name = "test-invitations-" + getAntiCollisionNamePart();
-            invitee_email = `email-${getAntiCollisionNamePart()}@example.com`;
+            anti_collision = getAntiCollisionNamePart();
+            project_name = `test-invitations-${anti_collision}`;
+            invitee_email = `email-${anti_collision}@example.com`;
 
             cy.projectAdministratorSession();
             cy.createNewPublicProject(project_name, "issues");
@@ -76,8 +78,103 @@ describe("Invitations", () => {
             cy.contains("Invitation has been withdrawn");
             cy.contains("tr", invitee_email).should("not.exist");
         });
+
+        it("users should be able to invite teamworkers", () => {
+            cy.deleteAllMessagesInMailbox();
+
+            cy.log("Ensure that users can invite other people");
+            cy.siteAdministratorSession();
+            cy.visit("/admin/");
+            cy.get("[data-test=site-admin-invitations]").click();
+            cy.get("[data-test=max-invitations-by-day]").clear().type("50");
+            cy.get("[data-test=save-invitations-settings]").click();
+
+            cy.log("Invite a Tuleap user and an external email");
+            cy.projectAdministratorSession();
+            cy.visit(`/projects/${project_name}`);
+            cy.get("[data-test=invite-buddies-button]").click();
+            const user_invited_in_project = `InvitedInProject${anti_collision}`;
+            const user_invited_in_project_mail = `user-invited-in-project${anti_collision}@example.com`;
+            cy.get("[data-test=invite-buddies-email]").type(
+                `SecondProjectAdministrator@example.com,${user_invited_in_project_mail}`,
+            );
+            cy.get("[data-test=invite-buddies-submit]").click();
+
+            cy.log("Tuleap user receive a mail to connect to Tuleap");
+            cy.assertEmailWithContentReceived(
+                "SecondProjectAdministrator@example.com",
+                "You are now a member",
+            );
+
+            cy.log("external user receive a mail to register to Tuleap");
+            cy.assertEmailWithContentReceived(
+                user_invited_in_project_mail,
+                "ProjectAdministrator invited you to register to Tuleap and join the project",
+            );
+
+            cy.log("Project administrators can re sent invitation");
+            cy.visit(`/projects/${project_name}`);
+            cy.get('[data-test="project-administration-link"]', { includeShadowDom: true }).click();
+            cy.get("[data-test=resend-user-invitations-pane]").contains(
+                user_invited_in_project_mail,
+            );
+
+            cy.log("Check that users are created without approbation");
+            extractInviteUrlFromEmail(user_invited_in_project_mail).then((url) => {
+                cy.clearCookies();
+                cy.clearLocalStorage();
+                cy.visit(url);
+            });
+
+            cy.log("As non existing user register to Tuleap...");
+            cy.get("[data-test=user-login]").type(user_invited_in_project);
+            cy.get("[data-test=user-pw]").type("welcome0");
+            cy.get("[data-test=user-pw2]").type("welcome0");
+            cy.get("[data-test=user-name]").type(user_invited_in_project);
+
+            cy.get("[data-test=register-user-button]").click();
+
+            cy.log("...and be welcomed on Tuleap project");
+            cy.get("[data-test=first-timer-success-modal-user-greetings]").contains(
+                user_invited_in_project,
+            );
+            cy.get("[data-test=first-timer-success-modal-project-greetings]").contains(
+                project_name,
+            );
+        });
     });
 });
+
+function extractInviteUrlFromEmail(user_email: string): Cypress.Chainable<string> {
+    return cy
+        .request({
+            method: "GET",
+            url:
+                "http://mailhog:8025/api/v2/search?kind=to&query=" + encodeURIComponent(user_email),
+            headers: {
+                accept: "application/json",
+            },
+        })
+        .then((response) => {
+            const last_email = response.body.items[0];
+            const body = last_email.Content.Body;
+
+            const url = extractInvitationUrlFromEmail(body);
+
+            if (!url) {
+                throw new Error("Invitation Url not found in mail of user " + user_email);
+            }
+
+            return url;
+        });
+}
+
+function extractInvitationUrlFromEmail(email_content: string): string | null {
+    const decoded = quotedPrintable.decode(email_content);
+
+    const match = decoded.match(/https:\/\/\S+/);
+    return match ? match[0] : null;
+}
 
 function invite(invitee_email: string): void {
     cy.get("[data-test=invite-buddies-button]").click();
