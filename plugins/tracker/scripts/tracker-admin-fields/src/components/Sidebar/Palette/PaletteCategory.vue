4ae782c0f8f7567850c909f2abfcc2115afc1b85
Nicolas Terray <nicolas.terray@enalean.com>
2026-02-14 17:18:37 +0100

    feat: Group fields by categories
    Fields in the palette are now grouped into two main categories:
    * Fields
    * Layout & structure
    
    Now fields are alphabetically sorted.
    
    No functional change in the search feature, category that does not
    contain a matching field is not displayed.
    
    Part of request #39759: Get Rid of Flaming Parrot in Trackers
    
    Change-Id: Iaad2c68de1c013177d77f11b467e0fc23d47b6a2

---
+++ plugins/tracker/scripts/tracker-admin-fields/src/components/Sidebar/Palette/PaletteCategory.vue
@@ -0,0 +1,58 @@
+<!--
+  - Copyright (c) Enalean, 2025 - Present. All Rights Reserved.
+  -
+  - This file is a part of Tuleap.
+  -
+  - Tuleap is free software; you can redistribute it and/or modify
+  - it under the terms of the GNU General Public License as published by
+  - the Free Software Foundation; either version 2 of the License, or
+  - (at your option) any later version.
+  -
+  - Tuleap is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  - GNU General Public License for more details.
+  -
+  - You should have received a copy of the GNU General Public License
+  - along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+  -->
+
+<template>
+    <div class="category">
+        <i class="fa-regular fa-folder-open" aria-hidden="true"></i>
+        {{ category.label }}
+    </div>
+    <div class="field" v-for="field of matching" v-bind:key="field.label">
+        <i class="fa-fw" v-bind:class="field.icon" aria-hidden="true"></i>
+        {{ field.label }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import type { CategoryOfPaletteFields } from "./type";
+
+const props = defineProps<{ category: CategoryOfPaletteFields; search: string }>();
+
+const matching = computed(() =>
+    props.search.trim() === ""
+        ? props.category.fields
+        : props.category.fields.filter((field) =>
+              field.label.toLowerCase().includes(props.search.trim().toLowerCase()),
+          ),
+);
+</script>
+
+<style scoped lang="scss">
+.category,
+.field {
+    display: flex;
+    margin: 0 0 var(--tlp-medium-spacing);
+    cursor: move;
+    gap: var(--tlp-small-spacing);
+}
+
+.field {
+    margin: 0 0 var(--tlp-medium-spacing) var(--tlp-medium-spacing);
+}
+</style>
