bdf42b5e1c15541dd9d1c9b00a8157f97820610d
Nicolas Terray <nicolas.terray@enalean.com>
2026-02-19 11:46:45 +0100

    fix: Add some DB tests
    We need a safety net before being able to refactor sql query with some
    level of confidency.
    
    This contribution adds basic coverage for:
    
    * `ArtifactDao::getLinkedArtifactsOfTrackersConcatenatedToCustomList`
    * `ArtifactDao::getLinkedOpenArtifactsOfTrackersNotLinkedToOthersWithLimitAndOffset`
    
    Part of request #46805: Simplify sql plan by replacing OR with UNION
    
    Change-Id: Ic18a2af1756b99a9ccd10ea7e7b0b236524fc60a

--- plugins/tracker/tests/integration/Artifact/Dao/ArtifactDaoTest.php
+++ plugins/tracker/tests/integration/Artifact/Dao/ArtifactDaoTest.php
@@ -35,6 +35,8 @@ final class ArtifactDaoTest extends TestIntegrationTestCase
     private int $parent_artifact;
     private int $child_artifact;
     private int $linked_artifact;
+    private int $tracker_child_id;
+    private int $not_linked_artifact;
 
     #[\Override]
     protected function setUp(): void
@@ -49,18 +51,22 @@ final class ArtifactDaoTest extends TestIntegrationTestCase
         $tracker_parent = $tracker_builder->buildTracker($project_id, 'Parent');
         $tracker_child  = $tracker_builder->buildTracker($project_id, 'Child');
 
+        $this->tracker_child_id = $tracker_child->getId();
+
         $parent_link_field = $tracker_builder->buildArtifactLinkField($tracker_parent->getId());
 
-        $this->parent_artifact = $tracker_builder->buildArtifact($tracker_parent->getId());
-        $this->child_artifact  = $tracker_builder->buildArtifact($tracker_child->getId());
-        $not_linked_artifact   = $tracker_builder->buildArtifact($tracker_child->getId());
-        $this->linked_artifact = $tracker_builder->buildArtifact($tracker_child->getId());
+        $this->parent_artifact     = $tracker_builder->buildArtifact($tracker_parent->getId());
+        $this->child_artifact      = $tracker_builder->buildArtifact($this->tracker_child_id);
+        $this->not_linked_artifact = $tracker_builder->buildArtifact($this->tracker_child_id);
+        $this->linked_artifact     = $tracker_builder->buildArtifact($this->tracker_child_id);
 
-        $tracker_builder->buildArtifactRank($this->child_artifact);
+        $tracker_builder->buildArtifactRank($this->child_artifact, 1);
+        $tracker_builder->buildArtifactRank($this->not_linked_artifact, 2);
+        $tracker_builder->buildArtifactRank($this->linked_artifact, 3);
 
         $parent_changeset = $tracker_builder->buildLastChangeset($this->parent_artifact);
         $tracker_builder->buildLastChangeset($this->child_artifact);
-        $tracker_builder->buildLastChangeset($not_linked_artifact);
+        $tracker_builder->buildLastChangeset($this->not_linked_artifact);
         $tracker_builder->buildLastChangeset($this->linked_artifact);
 
         $tracker_builder->buildArtifactLinkValue(
@@ -81,28 +87,113 @@ final class ArtifactDaoTest extends TestIntegrationTestCase
 
     public function testItGetLinkedArtifacts(): void
     {
-        $result = $this->dao->getLinkedArtifactsByIds([$this->parent_artifact]);
-
-        self::assertCount(2, $result);
-        self::assertEqualsCanonicalizing([$this->child_artifact, $this->linked_artifact], array_map(
-            static fn(array $row) => $row['id'],
-            $result,
-        ));
+        $this->assertResults(
+            [$this->child_artifact, $this->linked_artifact],
+            $this->dao->getLinkedArtifactsByIds([$this->parent_artifact])
+        );
     }
 
     public function testItGetLinkedArtifactsMinusExcluded(): void
     {
-        $result = $this->dao->getLinkedArtifactsByIds([$this->parent_artifact], [$this->linked_artifact]);
-
-        self::assertCount(1, $result);
-        self::assertSame($this->child_artifact, $result[0]['id']);
+        $this->assertResults(
+            [$this->child_artifact],
+            $this->dao->getLinkedArtifactsByIds([$this->parent_artifact], [$this->linked_artifact])
+        );
     }
 
     public function testItGetChildArtifacts(): void
     {
-        $result = $this->dao->getChildrenForArtifacts([$this->parent_artifact]);
+        $this->assertResults(
+            [
+                $this->child_artifact,
+            ],
+            $this->dao->getChildrenForArtifacts([$this->parent_artifact])
+        );
+    }
 
-        self::assertCount(1, $result);
-        self::assertSame($this->child_artifact, $result[0]['id']);
+    public function testGetLinkedArtifactsOfTrackersConcatenatedToCustomList(): void
+    {
+        $this->assertResults(
+            [
+                $this->child_artifact,
+                $this->linked_artifact,
+            ],
+            $this->dao->getLinkedArtifactsOfTrackersConcatenatedToCustomList(
+                $this->parent_artifact,
+                [$this->tracker_child_id],
+                [],
+            )
+        );
+
+        $this->assertResults(
+            [
+                $this->child_artifact,
+                $this->not_linked_artifact,
+                $this->linked_artifact,
+            ],
+            $this->dao->getLinkedArtifactsOfTrackersConcatenatedToCustomList(
+                $this->parent_artifact,
+                [$this->tracker_child_id],
+                [$this->not_linked_artifact],
+            )
+        );
+    }
+
+    public function testGetLinkedOpenArtifactsOfTrackersNotLinkedToOthersWithLimitAndOffset(): void
+    {
+        $this->assertResults(
+            [
+                $this->child_artifact,
+                $this->linked_artifact,
+            ],
+            $this->dao->getLinkedOpenArtifactsOfTrackersNotLinkedToOthersWithLimitAndOffset(
+                $this->parent_artifact,
+                [$this->tracker_child_id],
+                [],
+                [],
+                100,
+                0,
+            )
+        );
+        self::assertSame(2, $this->dao->foundRows());
+
+        $this->assertResults(
+            [
+                $this->child_artifact,
+                $this->not_linked_artifact,
+            ],
+            $this->dao->getLinkedOpenArtifactsOfTrackersNotLinkedToOthersWithLimitAndOffset(
+                $this->parent_artifact,
+                [$this->tracker_child_id],
+                [],
+                [$this->not_linked_artifact],
+                2,
+                0,
+            )
+        );
+        self::assertSame(3, $this->dao->foundRows());
+
+        $this->assertResults(
+            [
+                $this->linked_artifact,
+            ],
+            $this->dao->getLinkedOpenArtifactsOfTrackersNotLinkedToOthersWithLimitAndOffset(
+                $this->parent_artifact,
+                [$this->tracker_child_id],
+                [],
+                [$this->not_linked_artifact],
+                2,
+                2,
+            )
+        );
+        self::assertSame(3, $this->dao->foundRows());
+    }
+
+    private function assertResults(array $expected, array $actual): void
+    {
+        self::assertEqualsCanonicalizing($expected, array_map(
+            static fn(array $row): int => $row['id'],
+            $actual,
+        ));
     }
 }
